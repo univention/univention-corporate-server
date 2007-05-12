93f1c7063e0442cce68e9d0148727b68b101e45c
Stefan Gohmann <gohmann@univention.de>
2007-05-12 11:36:23 +0000

    import from cvs
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@32 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ base/univention-setup/usr/lib/univention-setup/scripts/ldif_rewrite.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python2.4
+#
+# Univention Setup
+#  ldap rewrite script
+#
+# Copyright (C) 2004, 2005, 2006 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# Binary versions of this file provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import sys, ldap, string, ldap, ldif
+
+# Parameter 1: "old_dn"
+# Parameter 2: "new_dn"
+# Parameter 3: "infile"
+# Parameter 4: "outfile"
+
+if len(sys.argv) != 5:
+	sys.exit(1)
+
+old_dn=sys.argv[1]
+new_dn=sys.argv[2]
+infile=sys.argv[3]
+outfile=sys.argv[4]
+
+f_infile=open(infile)
+ldif_input=ldif.ParseLDIF(f_infile)
+f_infile.close()
+
+
+base_object_dn=ldif_input[0][0]
+base_object_attr=ldif_input[0][1]
+
+new_base_type=new_dn.split('=')[0]
+old_base_type=old_dn.split('=')[0]
+
+new_base_object_domain=new_dn.split('=')[1].split(',')[0]
+
+if old_base_type == new_base_type:
+	new_base_object_attr={}
+	new_base_object_dn=new_dn
+	for key in base_object_attr.keys():
+		new_base_object_attr[key]=[]
+		if key == new_base_type:
+			new_base_object_attr[key].append(new_base_object_domain)
+		else:
+			for member in base_object_attr[key]:
+				new_base_object_attr[key].append(member.replace(old_dn,new_dn))
+else:
+	attribute_append=[]
+	if old_base_type == 'dc':
+		objectClass_remove='domain'
+		attribute_remove=['dc', 'structuralObjectClass']
+	elif old_base_type == 'o':
+		objectClass_remove='organization'
+		attribute_remove=['o', 'structuralObjectClass']
+	elif old_base_type == 'ou':
+		objectClass_remove='organizationalRole'
+		attribute_remove=['ou', 'structuralObjectClass']
+	elif old_base_type == 'l':
+		objectClass_remove='organization'
+		attribute_remove=['l', 'o', 'structuralObjectClass']
+	elif old_base_type == 'cn':
+		objectClass_remove='organizationUnit'
+		attribute_remove=['cn', 'structuralObjectClass']
+	elif old_base_type == 'c':
+		objectClass_remove='country'
+		attribute_remove=['c', 'structuralObjectClass']
+
+	if new_base_type == 'dc':
+		objectClass_append='domain'
+		attribute_append=['dc']
+	elif new_base_type == 'o':
+		objectClass_append='organization'
+		attribute_append=['o']
+	elif new_base_type == 'ou':
+		objectClass_append='organizationalRole'
+		attribute_append=['ou']
+	elif new_base_type == 'l':
+		objectClass_append='organization'
+		attribute_append=['l', 'o']
+	elif new_base_type == 'cn':
+		objectClass_append='organizationUnit'
+		attribute_append=['cn']
+	elif new_base_type == 'c':
+		objectClass_append='country'
+		attribute_append=['c']
+
+	new_base_object_dn=new_dn
+	new_base_object_attr={}
+	for key in base_object_attr.keys():
+		if not key in attribute_append:
+			new_base_object_attr[key]=[]
+			if key == 'objectClass':
+				for member in base_object_attr[key]:
+					if member != objectClass_remove:
+						new_base_object_attr[key].append(member.replace(old_dn,new_dn))
+				new_base_object_attr[key].append(objectClass_append)
+			elif key in attribute_remove:
+				continue
+			else:
+				for member in base_object_attr[key]:
+					new_base_object_attr[key].append(member.replace(old_dn,new_dn))
+		for i in attribute_append:
+			new_base_object_attr[i]=[new_base_object_domain]
+
+
+f=open(outfile, 'w+')
+ldif_writer=ldif.LDIFWriter(f)
+
+ldif_writer.unparse(new_base_object_dn,new_base_object_attr)
+
+for attr in ldif_input[1:-1]:
+	attr_dict={}
+	for key in attr[1].keys():
+		attr_dict[key]=[]
+		for member in attr[1][key]:
+			attr_dict[key].append(member.replace(old_dn,new_dn))
+	ldif_writer.unparse(attr[0].replace(old_dn, new_dn),attr_dict)
+f.close()
+
---
+++ base/univention-setup/usr/lib/univention-setup/scripts/setup_utils.sh
@@ -0,0 +1,122 @@
+#!/bin/sh -e
+#
+# Univention Setup
+#  setup utils helper script
+#
+# Copyright (C) 2004, 2005, 2006 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# Binary versions of this file provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+profile_file="/var/cache/univention-setup/profile"
+check_ldap_access=0
+
+while [ $# -gt 0 ]
+do
+	case "$1" in
+		"--check_ldap_access")
+			check_ldap_access=1
+			shift
+			;;
+ 		"--check_ldap_availability")
+ 			check_ldap_availability=1
+ 			shift
+ 			;;
+		*)
+			;;
+	esac
+done
+
+is_variable_set()
+{
+	if [ ! -e $profile_file ]; then
+		return 0
+	fi
+
+	if [ -z "$1" ]; then
+		return 0
+	fi
+	value=`egrep "^$1=" $profile_file `
+	if [ -z "$value" ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+get_profile_var ()
+{
+	if [ ! -e $profile_file ]; then
+		return
+	fi
+
+	if [ -z "$1" ]; then
+		return
+	fi
+
+	value=`egrep "^$1=" $profile_file |sed -e 's|#.*||' | sed -e "s|^$1=||" | sed -e 's|"||g;s| $||g'`
+	echo "$value"
+}
+
+service_stop ()
+{
+	for service in $@; do
+		if [ -x /etc/init.d/$service ]; then
+			/etc/init.d/$service stop
+		fi
+	done
+}
+service_start ()
+{
+	for service in $@; do
+		if [ -x /etc/init.d/$service ]; then
+			/etc/init.d/$service start
+		fi
+	done
+}
+
+ldap_binddn ()
+{
+	eval $(univention-baseconfig shell server/role ldap/base ldap/master)
+	if [ "$server_role" = "domaincontroller_master" ] || [ "$server_role" = "domaincontroller_backup" ]; then
+		echo "cn=admin,$ldap_base"
+	else
+		ldap_username=`get_profile_var ldap_username`
+		if [ -n "$ldap_username" ]; then
+			dn=`ldapsearch -x -h $ldap_master "(&(objectClass=person)(uid=$ldap_username))" | grep "dn: " | sed -e 's|dn: ||' | head -n 1`
+			echo "$dn"
+		fi
+	fi
+}
+
+ldap_bindpwd ()
+{
+	eval $(univention-baseconfig shell server/role ldap/base ldap/master)
+	if [ "$server_role" = "domaincontroller_master" ] || [ "$server_role" = "domaincontroller_backup" ]; then
+		echo "`cat /etc/ldap.secret`"
+	else
+		ldap_password=`get_profile_var ldap_password`
+		if [ -n "$ldap_password" ]; then
+			echo "$ldap_password"
+		fi
+	fi
+}
