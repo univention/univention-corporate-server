348a095e23f9f6fa781ddab3cc07949a061bebc9
Arne Brix <brix@univention.de>
2007-06-05 09:35:51 +0000

    worked on Bug #3205 #3206 and #4305
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@227 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-updater/script/postup.sh
+++ base/univention-updater/script/postup.sh
@@ -7,6 +7,9 @@ rm -f /var/cache/univention-config/cache
 eval $( /usr/sbin/univention-baseconfig shell version/version version/patchlevel)
 
 univention-baseconfig set version/version="$version_version" version/patchlevel="$version_patchlevel"
-
+#Die folgenden Zeilen können nach dem Update auf 2.0 wieder entfernt werden {------------------------------------------------------
+univention-baseconfig unset repository/patchlevel
+univention-baseconfig unset repository/version
+#} --------------------------------------------------------------------------------------------------------------------------------
 exit 0
 
