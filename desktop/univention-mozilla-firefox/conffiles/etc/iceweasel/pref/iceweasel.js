8de0e586adbe8399f55476224d8af7a9005b75d3
Janis Meybohm <meybohm@univention.de>
2007-06-12 14:06:08 +0000

    forgot adding of many conffiles
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@308 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ desktop/univention-mozilla-firefox/conffiles/etc/iceweasel/pref/iceweasel.js
@@ -0,0 +1,41 @@
+// Warning: This file is auto-generated and might be overwritten by
+//          univention-baseconfig.
+//          Please edit the following file instead:
+// Warnung: Diese Datei wurde automatisch generiert und kann durch
+//          univention-baseconfig überschrieben werden.
+//          Bitte bearbeiten Sie an Stelle dessen die folgende Datei:
+//
+//         /etc/univention/templates/files/etc/iceweasel/pref/iceweasel.js
+//
+
+// This is the Debian specific preferences file for Mozilla Firefox
+// You can make any change in here, it is the purpose of this file.
+// You can, with this file and all files present in the
+// /etc/mozilla-firefox/pref directory, override any preference that is
+// present in /usr/lib/mozilla-firefox/defaults/pref directory.
+// While your changes will be kept on upgrade if you modify files in
+// /etc/mozilla-firefox/pref, please note that they won't be kept if you
+// do them in /usr/lib/mozilla-firefox/defaults/pref.
+
+pref("extensions.update.enabled", true);
+pref("extensions.update.autoUpdateEnabled", false);
+pref("extensions.update.autoUpdate", false);
+
+// Use LANG environment variable to choose locale
+pref("intl.locale.matchOS", true);
+
+// Disable default browser checking.
+pref("browser.shell.checkDefaultBrowser", false);
+
+pref("print.print_command", "kprinter");
+pref("print.postscript.print_command", "kprinter");
+pref("print.postscript.paper_size", "A4");
+
+@!@
+if baseConfig.has_key('firefox/prefs/mailto') and baseConfig['firefox/prefs/mailto']:
+	print 'pref("network.protocol-handler.app.mailto", "%s");' % baseConfig['firefox/prefs/mailto']
+
+if baseConfig.has_key('firefox/prefs/conffile') and baseConfig['firefox/prefs/conffile']:
+	print 'pref("general.config.filename", "%s");' % baseConfig['firefox/prefs/conffile']
+@!@
+
