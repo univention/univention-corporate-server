5f90486eaa64c1514a5a5acd5875a8f3468d1cf6
Stefan Gohmann <gohmann@univention.de>
2007-06-14 06:44:22 +0000

    set unit to MB for the parted print calls
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@341 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-installer/installer/modules/12_partition.py
+++ base/univention-installer/installer/modules/12_partition.py
@@ -534,7 +534,7 @@ class object(content):
 		devices_remove=[]
 		for dev in devices:
 			dev=dev.strip()
-			p = os.popen('/sbin/parted -s %s p 2>&1'% dev)
+			p = os.popen('/sbin/parted -s unit MB %s p 2>&1'% dev)
 
 			first_line=p.readline().strip()
 			self.debug('fist line: [%s]' % first_line)
@@ -545,7 +545,7 @@ class object(content):
 				continue
 			elif _re_error.match(first_line):
 				os.system('/sbin/install-mbr -f %s' % dev)
-				p = os.popen('/sbin/parted %s p'% dev)
+				p = os.popen('/sbin/parted unit MB %s p'% dev)
 				first_line=p.readline()
 				if _re_error.match(first_line):
 					self.debug('Firstline starts with error')
@@ -574,8 +574,8 @@ class object(content):
 				cols=line.split()
 				num=cols[0]
 				part=dev+cols[0]
-				start=float(cols[1].replace(',','.'))
-				end=float(cols[2].replace(',','.'))
+				start=float(cols[1].split('MB')[0].replace(',','.'))
+				end=float(cols[2].split('MB')[0].replace(',','.'))
 				size=end-start
 				type=cols[3]
 				if type == 'extended':
