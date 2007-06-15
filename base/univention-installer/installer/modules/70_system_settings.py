5ad8dded0380e7493a7da0bac698285e9e63ad17
Stefan Gohmann <gohmann@univention.de>
2007-06-15 09:14:15 +0000

    fix a traceback in the settings dialog
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@393 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-installer/installer/modules/70_system_settings.py
+++ base/univention-installer/installer/modules/70_system_settings.py
@@ -91,8 +91,11 @@ class object(content):
 			result['local_repository']='true'
 		else:
 			result['local_repository']='false'
-		if len(self.elements[3].selected) > 0:
-			result['create_home_share']='true'
+		if not (self.all_results.has_key('system_role') and self.all_results['system_role'] in ['basesystem']):
+			if len(self.elements[3].selected) > 0:
+				result['create_home_share']='true'
+			else:
+				result['create_home_share']='false'
 		else:
 			result['create_home_share']='false'
 		return result
