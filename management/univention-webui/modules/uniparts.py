b2474fe3529c6af505cf6a96ac79fb4c3234dc9c
Andreas Büsching <buesching@univention.de>
2007-06-18 07:12:33 +0000

    removed unused code
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@423 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-webui/modules/uniparts.py
+++ management/univention-webui/modules/uniparts.py
@@ -46,6 +46,3 @@ from notebook import *
 from icon import *
 from waitdialog import waitstatus
 from tabbing import Tabbing
-# import codecs
-
-# (utf8_encode,utf8_decode,utf8_reader,utf8_writer)=codecs.lookup('utf-8')
