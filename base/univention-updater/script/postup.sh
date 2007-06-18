05b85de0d4ef3f3771f850116f5afb63e2d53cdf
Stefan Gohmann <gohmann@univention.de>
2007-06-18 06:17:59 +0000

    remove some unused stuff from the postup.sh and from the postinst script
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@418 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-updater/script/postup.sh
+++ base/univention-updater/script/postup.sh
@@ -4,12 +4,12 @@
 
 rm -f /var/cache/univention-config/cache
 
-eval $( /usr/sbin/univention-baseconfig shell version/version version/patchlevel)
-
-univention-baseconfig set version/version="$version_version" version/patchlevel="$version_patchlevel"
 #Die folgenden Zeilen können nach dem Update auf 2.0 wieder entfernt werden {------------------------------------------------------
-univention-baseconfig unset repository/patchlevel
-univention-baseconfig unset repository/version
+
+univention-baseconfig unset repository/patchlevel \
+							repository/version
+
 #} --------------------------------------------------------------------------------------------------------------------------------
+
 exit 0
 
