c7cbf1f2ab1debbbde5335358981f7aa6c9ddfb7
Stefan Gohmann <gohmann@univention.de>
2007-07-24 14:09:25 +0000

    import univention-flashplugin
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@837 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ desktop/univention-flashplugin/conffiles/etc/update-flashplugin.conf.rb
@@ -0,0 +1,20 @@
+# Warning: This file is auto-generated and might be overwritten by
+#          univention-baseconfig.
+#          Please edit the files in the following directory instead:
+# Warnung: Diese Datei wurde automatisch generiert und kann durch
+#          univention-baseconfig ueberschrieben werden.
+#          Bitte bearbeiten Sie an Stelle dessen die Dateien in
+#          folgendem Verzeichnis:
+#
+#       /etc/univention/templates/files/etc/update-flashplugin.conf.rb
+#
+
+# -*- ruby -*-
+#
+
+module UpdateFlashPluginConf
+  SITES = {
+    "@%@flashplugin/downloadserver@%@" => "@%@flashplugin/downloadpath@%@",
+    #http://fpdownload.macromedia.com/get/flashplayer/current/install_flash_player_7_linux.tar.gz
+  }
+end
