02be97489e4fe1e2863b64a51c2265ac41ebce16
Moritz Mühlenhoff <muehlenhoff@univention.de>
2007-07-24 07:52:50 +0000

    remove obsolete code
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@810 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-firewall/firewall.py
+++ base/univention-firewall/firewall.py
@@ -4,7 +4,7 @@
 # Univention Firewall
 #  firewall script
 #
-# Copyright (C) 2004, 2005, 2006 Univention GmbH
+# Copyright (C) 2004-2007 Univention GmbH
 #
 # http://www.univention.de/
 #
@@ -33,10 +33,6 @@ import univention_baseconfig, time, os
 
 baseConfig=univention_baseconfig.baseConfig()
 baseConfig.load()
-
-#if baseConfig['security/firewall/enable'] != "yes":
-#	print "Firewall is not enabled. Exiting."
-#	exit
 
 def main():
 
@@ -90,63 +86,6 @@ def main():
 					else:
 						# Syntax error, log error message
 						pass
-
-# 		# Welcome to the complex part, filtering out
-# 		if key.startswith('security/firewall/services/'):
-# 			key = key.replace('security/firewall/services/', '')
-# 			(service, data) = key.split('/', 1)
-			
-# 			enable = baseConfig['security/firewall/services/'+service+'/enable']
-# 			if baseConfig['security/firewall/services/'+service+'/enable'] != "yes":
-# 				print "Firewall service disabled: " + service + '. Going on.'
-# 				continue
-
-# 			# We got a new service. Means, we can write the old, as we got all data
-# 			# You can be sure about this, because sort() was called on baseConfig
-# 			if oldservice and oldservice != service:
-				
-# 				write_service(oldservice, fh, enable, policy, log, tcp, udp, allowip, denyip)
-				
-# 				# Reset our values
-# 				udp = {}
-# 				tcp = {}
-# 				allowip = []
-# 				denyip = []
-# 				log = ''
-# 				policy = ''
-# 				enable = ''
-			
-# 			# Else we need to collect more data
-# 			else:
-# 				# Logging rule
-# 				if (data == 'logging' and value == 'yes'):
-# 					log = 1
-				
-# 				if data == 'policy' and (value == 'accept' or value == 'deny'):
-# 					policy = value
-
-# 				if data.startswith('tcp/'):
-# 					(tmp, port) = data.split('/', 1)
-# 					tcp[port] = value
-
-# 				if data.startswith('udp/'):
-# 					(tmp, port) = data.split('/', 1)
-# 					udp[port] = value
-				
-# 				if data.startswith('allowed/'):
-# 					allowip.append(value)
-				
-# 				if data.startswith('denied/'):
-# 					denyip.append(value)
-			
-# 			#print "Service: %s " % service
-# 			#print "data: %s => %s " % (data, value)
-
-# 			oldservice = service
-		
-		
-#	write_service(oldservice, fh, enable, policy, log, tcp, udp, allowip, denyip)
-
 	# Close the main script
 	fh_accept.write('\n')
 	fh_deny.write('\n')
@@ -178,87 +117,3 @@ def add_iptables_entry(proto, port, action):
 		jump = 'ACCEPT'
 
 	return ('$iptables -A INPUT -p %s --dport %s -j %s\n' % (proto, port, jump))
-
-
-# Add a new iptables chain
-# iptables -N univention-thinclient
-# call: add_iptables_chain('thinclient')
-def add_iptables_chain(name):
-	return ('$iptables -N univention-%s\n' % name)
-
-# Adds a allow/reject rule to a service
-# iptables -A univention-thinclient -s 10.200.32.128/25 -j ACCEPT
-# call: add_iptables_service_entry('thinclient', '10.200.32.128/25', 'accept')
-def add_iptables_service_entry(name, source, policy):
-	return ('$iptables -A univention-%s -s %s -j %s\n' % (name, source, policy))
-
-# Sets a default policy for a service (should be called last)
-# iptables -A univention-thinclient -j REJECT --reject-with tcp-reset [-p tcp]
-# call: set_iptables_default_policy('univention', 'reject')
-def set_iptables_service_defaultpolicy(name, policy, tcp, udp):
-	str = ''
-	
-	if policy == "deny":
-		policy = "drop"
-	
-	if policy == "drop" and len(tcp) > 0:
-		str += ('$iptables -A univention-%s -p tcp -j REJECT --reject-with tcp-reset\n' % name)
-	
-	return (str+'$iptables -A univention-%s -j %s\n' % (name, policy.upper()))
-
-# This enables logging of packets which end ip in the default policy after
-# traversing the chain
-# iptables -A univention-thinclient -j LOG
-# call: set_iptables_service_logging('thinclient')
-def set_iptables_service_logging(name):
-	return ('$iptables -A univention-%s -j LOG\n' % name)
-
-# Write our service entry...
-# Arguments are:
-# 1. service       The name of the service, i.e. 'thinclient'
-# 2. filehandle    The filehandle of the main init script
-# 3. enable        The enable/disable status of the rule - won't be activated until explicitely set to 'yes'
-# 4. policy        The default policy, should be 'deny' or 'accept'
-# 5. log           Should this service be logged?
-# 6. tcp [dict]    TCP ports this service affects (may be integer or common port name)
-# 7. udp [dict]    UDP ports this service affects (may be integer or common port name)
-# 8. allowip [arr] List of IPs (including netmask) which are allowed for this service
-# 9. denyip [arr]  List of IPs (including netmask) which are denied for this service
-def write_service(service, fh, enable, policy, log, tcp, udp, allowip, denyip):
-	#print "write_service(%s, %s, %s, %s, %s, %s, %s, %s, %s)" % (service, fh, enable, policy, log, tcp, udp, allowip, denyip)
-	script = '/etc/univention-firewall/'+service+'.sh'
-	servicefh = open_iptables_script(script);
-	
-	# If enable is not set to yes, we only write the init scripts, but commented out
-	if enable != "yes":
-		cchar = '# '
-	else:
-		cchar = ''
-	
-	fh.write(cchar+add_iptables_chain(service));
-	
-	for elem in tcp:
-		fh.write(cchar+add_iptables_entry('tcp', elem, service))
-	
-	for elem in udp:
-		fh.write(cchar+add_iptables_entry('udp', elem, service))
-	
-	for elem in allowip:
-		servicefh.write(cchar+add_iptables_service_entry(service, elem, 'ACCEPT'))
-
-	for elem in denyip:
-		servicefh.write(cchar+add_iptables_service_entry(service, elem, 'REJECT'))
-	
-	if log == 1:
-		servicefh.write(cchar+set_iptables_service_logging(service))
-	
-	servicefh.write(cchar+set_iptables_service_defaultpolicy(service, policy, tcp, udp)+'\n')
-	os.chmod(script, 0755);
-
-	servicefh.close()
-
-	# Schedule start of the service in the init.sh Script
-	fh.write(cchar+'/etc/univention-firewall/'+service+'.sh\n');
-
-# Set it off
-main()
