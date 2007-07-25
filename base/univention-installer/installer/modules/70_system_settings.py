2dca60fd6c651c059cc0bd73878ba3d4b9a38edb
Moritz Mühlenhoff <muehlenhoff@univention.de>
2007-07-25 13:57:55 +0000

    Allow selection of security level in installer
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@888 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-installer/installer/modules/70_system_settings.py
+++ base/univention-installer/installer/modules/70_system_settings.py
@@ -4,7 +4,7 @@
 # Univention Installer
 #  installer module: system configuration
 #
-# Copyright (C) 2004, 2005, 2006 Univention GmbH
+# Copyright (C) 2004, 2005, 2006, 2007 Univention GmbH
 #
 # http://www.univention.de/
 #
@@ -41,11 +41,11 @@ import string
 
 class object(content):
 	def checkname(self):
-		return ['local_repository','create_home_share']
+		return ['local_repository','create_home_share', 'security_profile']
 
 
 	def modvars(self):
-		return ['local_repository','create_home_share']
+		return ['local_repository','create_home_share', 'security_profile']
 
 	def mod_depends(self):
 		return {'system_role': ['domaincontroller_master', 'domaincontroller_backup','domaincontroller_slave','memberserver','basesystem'] }
@@ -54,7 +54,7 @@ class object(content):
 		return {}
 
 	def profile_complete(self):
-		if self.check('local_repository') | self.check('create_home_share'):
+		if self.check('local_repository') | self.check('create_home_share') | self.check('security_profile'):
 			return False
 		return True
 
@@ -68,13 +68,27 @@ class object(content):
 		elif not (self.all_results.has_key('system_role') and self.all_results['system_role'] in ['basesystem']):
 			self.elements.append(checkbox({_('Create home share'): 'create_home_share'}, self.minY+3, self.minX+2,30,1,[ ]))#3
 
+		self.add_elem('security_profile_label', textline(_('Security profile:'), self.minY, self.minX+2))
+
+		dict={}
+		dict['Normal']=['normal',0]
+		dict['Strict']=['strict',1]
+		dict['Open']=['open',2]
+
+		list=['normal','strict','open']
+		select=0
+		if self.all_results.has_key('security_profile'):
+			select=list.index(self.mapping(self.all_results['security_profile']))
+		self.add_elem('security_profile_radio', radiobutton(dict,self.minY+1,self.minX+2,40,10,[select]))
+		self.get_elem('security_profile_radio').current = select
+
 	def input(self,key):
 		if key in [ 10, 32 ] and self.btn_next():
 			return 'next'
 		elif key in [ 10, 32 ] and self.btn_back():
 			return 'prev'
 		else:
-			return self.elements[self.current].key_event(key)
+			return self.get_elem_by_id(self.current).key_event(key)
 
 	def incomplete(self):
 		return 0
@@ -98,4 +112,7 @@ class object(content):
 				result['create_home_share']='false'
 		else:
 			result['create_home_share']='false'
+
+		result['security_profile'] = 'foo'
+
 		return result
