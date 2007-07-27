aed69519f7258ca232c89ea111d4e5e239962f5b
Moritz Mühlenhoff <muehlenhoff@univention.de>
2007-07-27 11:06:57 +0000

    fix traceback
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@922 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-installer/installer/modules/70_system_settings.py
+++ base/univention-installer/installer/modules/70_system_settings.py
@@ -85,8 +85,8 @@ class object(content):
 
 		list=['normal','strict','open']
 		select=0
-		if self.all_results.has_key('security_profile'):
-			select=list.index(self.mapping(self.all_results['security_profile']))
+#		if self.all_results.has_key('security_profile'):
+#			select=list.index(self.mapping(self.all_results['security_profile']))
 		self.add_elem('security_profile_radio', radiobutton(dict,self.minY+1,self.minX+2,40,10,[select]))
 		self.get_elem('security_profile_radio').current = select
 
