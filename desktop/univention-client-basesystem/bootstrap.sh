b926f0b2adec707326a92e98d73cbb5844afb3c8
Stefan Gohmann <gohmann@univention.de>
2007-08-08 14:35:00 +0000

    force portmap update-rc.d
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@1259 1b283449-9f2f-0410-b571-8eb228e1a901

--- desktop/univention-client-basesystem/bootstrap.sh
+++ desktop/univention-client-basesystem/bootstrap.sh
@@ -32,7 +32,7 @@ export suite=univention
 mirror=http://192.168.0.10/build2/ucs_2.0-0/
 
 # fixme: univention-python
-export packages="univention-config-registry python-support python-univention libnss-ldap libpam-ldap nfs-common heimdal-clients openssh-client gdm samba samba-common univention-x-core univention-fonts python2.4 bind9-host pam-runasroot ntpdate ntp less rdesktop fuse-utils univention-automount arts kcontrol dhcp3-client libpam-heimdal heimdal-clients cupsys univention-policy-tools libunivention-policy0 libldap2 ldap-utils python-dns kudzu-2.6 univention-gdm lynx locales univention-xnumlock univention-home-mounter rdate netcat fuse-utils smbfs univention-automount univention-client-devices-client univention-bootsplash univention-kde-setdirs console-data portmap"
+export packages="univention-config-registry python-support python-univention libnss-ldap libpam-ldap nfs-common heimdal-clients openssh-client gdm samba samba-common univention-x-core univention-fonts python2.4 bind9-host pam-runasroot ntpdate ntp less rdesktop fuse-utils univention-automount arts kcontrol dhcp3-client libpam-heimdal heimdal-clients cupsys univention-policy-tools libunivention-policy0 libldap2 ldap-utils python-dns kudzu-2.6 univention-gdm lynx locales univention-xnumlock univention-home-mounter rdate netcat fuse-utils smbfs univention-automount univention-client-devices-client univention-bootsplash univention-kde-setdirs console-data"
 export reinstall_packages="univention-client-session univention-gdm univention-gdm-sessions"
 # adduser, e2fsprogs
 export exclude="lilo,pcmcia-cs,base-config,exim,mailx,at,logrotate,cpio,ipchains,dhcp-client,manpages,modconf,mbr,man-db,makedev,telnet,libdb4.0,info,diff,whiptail,syslinux,libsepol1"
@@ -221,7 +221,7 @@ chmod 755 "/sbin/start-stop-daemon"
 	update-rc.d diskless.sh start 02 S .
 	update-rc.d disklesshardware.sh start 12 2 .
 	update-rc.d disklessfinish.sh start 98 2 .
-	update-rc.d portmap start 80 2 .
+	update-rc.d -f portmap start 80 2 .
 
 
 	mv "/sbin/start-stop-daemon.REAL" "/sbin/start-stop-daemon"
