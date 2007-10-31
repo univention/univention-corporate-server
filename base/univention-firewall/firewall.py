ed34565dd580d79a36c3fbea67e58b1bb0fc4cee
Moritz Mühlenhoff <muehlenhoff@univention.de>
2007-10-31 15:12:01 +0000

    Fix rules generation
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@2276 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-firewall/firewall.py
+++ base/univention-firewall/firewall.py
@@ -29,76 +29,17 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-import univention_baseconfig, time, os
+import univention_baseconfig, time, os, sys
 
 baseConfig=univention_baseconfig.baseConfig()
 baseConfig.load()
-
-def main():
-
-	initscript = '/etc/univention-firewall/init.sh'
-	
-	# Important initialize
-	# This is the data one firewall-service record consists of
-	# After all data has been collected out of the baseconfig the
-	# entry is written as iptables script
-	oldservice = ''
-	udp = {}
-	tcp = {}
-	allowip = []
-	denyip = []
-	log = ''
-	policy = ''
-	enable = ''
-
-	# Open the main file
-	fh_accept = open_iptables_script("/etc/security/netfilter.d/10accept")
-	fh_deny = open_iptables_script("/etc/security/netfilter.d/15accept")
-
-	base = baseConfig.keys()
-	base.sort()
-
-	for key in base:
-		value = baseConfig[key]
-		#print "[%s]\t\t%s" % (key, value)
-
-		if key.startswith('security/packetfilter/tcp/') or key.startswith('security/packetfilter/udp/'):
-			tmp = key.split('/')
-			proto = tmp[2]
-			action = tmp[3]
-			
-			if action == 'accept' or action == 'deny':
-				port = value
-				port_chunks = port.split(",")
-				for i in port_chunks:
-					port_range = i.split("-")
-					if len(port_range) == 1:
-						if action == 'deny':
-							fh_deny.write(add_iptables_entry(proto, port_range[0], action))
-						elif action == 'accept':
-							fh_accept.write(add_iptables_entry(proto, port_range[0], action))
-					elif len(port_range) == 2:
-						if action == 'deny':
-							fh_deny.write(add_iptables_entry(proto, (port_range[0] + ":" + port_range[1]), action))
-						elif action == 'accept':
-							fh_accept.write(add_iptables_entry(proto, (port_range[0] + ":" + port_range[1]), action))
-							
-					else:
-						# Syntax error, log error message
-						pass
-	# Close the main script
-	fh_accept.write('\n')
-	fh_deny.write('\n')
-	fh_accept.close()
-	fh_deny.close()
-	os.chmod(initscript, 0755);
 
 ### functions^Wmethods
 def open_iptables_script (file):
 	fh = open(file, 'w')
 	fh.write('#!/bin/sh\n\n')
 	fh.write('#\n# Written by univention-baseconfig at '+time.strftime('%H:%M   %d %b %Y')+' \n#\n\n')
-	fh.write('iptables=$(which iptables)\n\n')
+	fh.write('iptables=/sbin/iptables\n\n')
 	return fh
 
 # Add a IPTables entry to the INPUT chain
@@ -117,3 +58,64 @@ def add_iptables_entry(proto, port, action):
 		jump = 'ACCEPT'
 
 	return ('$iptables -A INPUT -p %s --dport %s -j %s\n' % (proto, port, jump))
+
+
+print "Hallo"
+	
+# Important initialize
+# This is the data one firewall-service record consists of
+# After all data has been collected out of the baseconfig the
+# entry is written as iptables script
+oldservice = ''
+udp = {}
+tcp = {}
+allowip = []
+denyip = []
+log = ''
+policy = ''
+enable = ''
+
+# Open the main file
+fh_accept = open_iptables_script("/etc/security/netfilter.d/10accept")
+fh_deny = open_iptables_script("/etc/security/netfilter.d/15deny")
+
+base = baseConfig.keys()
+base.sort()
+
+for key in base:
+	value = baseConfig[key]
+	#print "[%s]\t\t%s" % (key, value)
+
+
+	if key.startswith('security/packetfilter/tcp/') or key.startswith('security/packetfilter/udp/'):
+		tmp = key.split('/')
+		proto = tmp[2]
+		action = tmp[3]
+
+		print "X1", tmp
+
+		if action == 'accept' or action == 'deny':
+			port = value
+			port_chunks = port.split(",")
+			for i in port_chunks:
+				port_range = i.split("-")
+				if len(port_range) == 1:
+					if action == 'deny':
+						fh_deny.write(add_iptables_entry(proto, port_range[0], action))
+					elif action == 'accept':
+						fh_accept.write(add_iptables_entry(proto, port_range[0], action))
+				elif len(port_range) == 2:
+					if action == 'deny':
+						fh_deny.write(add_iptables_entry(proto, (port_range[0] + ":" + port_range[1]), action))
+					elif action == 'accept':
+						fh_accept.write(add_iptables_entry(proto, (port_range[0] + ":" + port_range[1]), action))
+							
+				else:
+					# Syntax error, log error message
+					pass
+# Close the main script
+fh_accept.write('\n')
+fh_deny.write('\n')
+fh_accept.close()
+fh_deny.close()
+
