fbef41c0f84b5606a86df51db6b6e085d5d71816
Moritz Mühlenhoff <muehlenhoff@univention.de>
2007-10-31 13:54:49 +0000

    import univention-log-collector (which has been modified for UCS 2.0 policies)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@2273 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ services/univention-log-collector/log-collector-client.py
@@ -0,0 +1,388 @@
+#!/usr/bin/python2.4
+#
+# Univention Log Collector Client
+#   send log files to central loghost
+#
+# Copyright (C) 2007 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# Binary versions of this file provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+# python packages
+import socket, fcntl, os, sys, random, errno, stat, struct, cPickle, time
+
+# external packages
+from optparse import OptionParser
+import univention_baseconfig as ub
+import inspect
+import notifier
+from OpenSSL import *
+
+MAX_PICKLE_ID = 999999
+MIN_PICKLE_ID = 1
+MAX_LENGTH_RESEND_QUEUE = 20
+
+LOGERROR = 0
+LOGWARN = 1
+LOGINFO = 2
+LOGDEBUG = 4
+
+loglevel = 0
+logobj = None
+
+def debug( level, msg ):
+	global logobj, loglevel
+	if level <= loglevel:
+		if not logobj:
+			logobj = open('/var/log/univention/log-collector-client.log','a')
+
+		info = inspect.getframeinfo(inspect.currentframe().f_back)[0:3]
+		printInfo=[]
+		if len(info[0])>30:
+			printInfo.append('...'+info[0][-27:])
+		else:
+			printInfo.append(info[0])
+		printInfo.extend(info[1:3])
+		logobj.write( "%s [L%s]: %s\n" % (time.asctime( time.localtime()), printInfo[1], msg) )
+		logobj.flush()
+
+baseconfig = ub.baseConfig()
+baseconfig.load()
+
+class LogCollectorClient( object ):
+	def __init__( self, server, port = 7450 ):
+		'''Initialize a socket-connection to the server.'''
+		self._port = port
+		self._server = server
+		self._nextId = random.randint( 100000, 999999)
+		self._resendQueue = {}
+		self._inbuffer = ''
+		self._config = {}
+		self._socket_ready = False
+		self._outbuffer = ''
+
+		self._read_config()
+
+		self._crypto_context = SSL.Context(SSL.SSLv23_METHOD)
+#		self._crypto_context.set_info_callback(self._info_callback)
+		self._crypto_context.set_cipher_list('DEFAULT')
+		self._crypto_context.set_verify( SSL.VERIFY_PEER | SSL.VERIFY_FAIL_IF_NO_PEER_CERT, self._verify_cert_cb )
+#		dir = '/etc/univention/ssl/%s' % umc.baseconfig[ 'hostname' ]
+#		self._crypto_context.use_privatekey_file( os.path.join( dir, 'private.key' ) )
+#		self._crypto_context.use_certificate_file( os.path.join( dir, 'cert.pem' ) )
+		self._crypto_context.load_verify_locations( os.path.join( dir, '/etc/univention/ssl/udsCA', 'CAcert.pem' ) )
+
+		self._init_socket()
+		self.connect()
+
+
+
+	def _verify_cert_cb( self, conn, cert, errnum, depth, ok ):
+		debug( LOGDEBUG, 'Got certificate subject: %s' % cert.get_subject() )
+		debug( LOGDEBUG, 'Got certificate issuer: %s' % cert.get_issuer() )
+		debug( LOGDEBUG, 'errnum=%d  depth=%d  ok=%d' % (errnum, depth, ok) )
+		if depth == 0 and ok == 0:
+			debug( LOGERROR, 'got invalid certificate from server - dying')
+			sys.exit(1)
+		return ok
+
+
+	def _info_callback( self, conn, fkt, status ):
+		debug( LOGDEBUG, 'fkt=%s  status=%s' % (fkt, status) )
+		return ok
+
+
+	def _init_socket( self ):
+		self._realsocket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+		self._realsocket.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
+		fcntl.fcntl(self._realsocket.fileno(), fcntl.F_SETFD, 1)
+		self._socket = SSL.Connection( self._crypto_context, self._realsocket )
+
+
+	def reconnect( self ):
+		debug( LOGDEBUG, 'RECONNECT')
+		self.connect()
+		return False
+
+
+	def connect( self ):
+		if not self._realsocket or not self._socket:
+			self._init_socket()
+		try:
+			self._socket.connect( ( self._server, self._port ) )
+			self._socket.setblocking( 0 )
+			notifier.socket_add( self._socket, self._receive_data )
+			debug( LOGERROR, 'SSL connection established' )
+
+			self._send_setup()
+		except:
+			debug( LOGERROR, 'SSL connection failed' )
+			self._socket = None
+			notifier.timer_add( 5000, notifier.Callback( self.reconnect ) )
+			return False
+
+		return True
+
+	def _send_setup( self ):
+		filelist = []
+		for fobj,fdict in self._config.items():
+			filelist.append( ( fdict['filename'], fdict['serverdir'] ) )
+
+		packet = { 'id': 0,
+				   'action': 'SETUP',
+				   'data': filelist,
+				   'rotate': (fdict['pos'] == -1) }     # rotate on startup, not on reconnect
+		debug( LOGDEBUG, 'Sending SETUP')
+		self._send_pickled( packet, resendQueue = False )
+
+	def _receive_data( self, sock ):
+		debug( LOGDEBUG, 'GOT NEW DATA' )
+
+		try:
+			recv = sock.recv( 16384 )
+		except SSL.SysCallError, e:
+			# lost connection or any other unfixable error
+			recv = None
+		except SSL.Error:
+			error = sock.getsockopt( socket.SOL_SOCKET, socket.SO_ERROR )
+			if error == errno.EPIPE:
+				# lost connection: server died probably
+				debug( LOGDEBUG, 'EPIPE' )
+				recv = None
+			else:
+				return True
+
+		if not recv:
+			sock.close()
+			notifier.socket_remove( sock )
+			notifier.socket_remove( sock, condition = notifier.IO_WRITE )
+			self._socket = None
+			notifier.timer_add( 1000, notifier.Callback( self.reconnect ) )
+			return False
+
+		self._inbuffer += recv
+
+		debug( LOGDEBUG, 'BUFFER: len=%d    got %d bytes' % (len(self._inbuffer), len(recv)))
+
+		# repeat while enough data is present
+		while len(self._inbuffer) > 4:
+			# get length of pickle object
+			plen = struct.unpack('!I', self._inbuffer[0:4])[0]
+			if plen+4 <= len(self._inbuffer):
+				# unpickle data
+				packet = cPickle.loads( self._inbuffer[4:4+plen] )
+				# remove data from buffer
+				self._inbuffer = self._inbuffer[4+plen:]
+
+				# handle packet
+				if type(packet) == type({}):
+					if packet.has_key('action'):
+						if packet['action'] == 'SETUP':
+							self._handle_packet_setup( packet )
+						elif packet['action'] == 'ACK':
+							self._handle_packet_ack( packet )
+			else:
+				# not enough data
+				break
+
+		return True
+
+
+	def _handle_packet_setup( self, packet ):
+		debug( LOGDEBUG, 'Got SETUP' )
+		if packet['data'] == 'OK':
+			self._socket_ready = True
+			if len(self._resendQueue):
+				self._resend_unacked_packets()
+		else:
+			debug( LOGERROR, 'ERROR: got negative SETUP msg' )
+
+
+	def _handle_packet_ack( self, packet ):
+		debug( LOGDEBUG, 'Got ACK')
+		for ackid in packet['data']:
+			if self._resendQueue.has_key(ackid):
+				del self._resendQueue[ackid]
+
+
+# config files in /etc/logcollector.d/
+# config file format:
+# LOGFILE [TAB] SUBDIR
+	def _read_config(self):
+		# iterate over config dir
+		configdir = '/etc/logcollector.d/'
+		if os.path.exists(configdir):
+			fnlist = os.listdir(configdir)
+			for f in fnlist:
+				fn = os.path.join(configdir, f)
+				mode = os.stat(fn)[stat.ST_MODE]
+				# fetch all files
+				if not stat.S_ISDIR(mode):
+					# read file
+					fd = open(fn, 'r')
+					content = fd.read()
+					fd.close()
+					# iterate over lines
+					for line in content.splitlines():
+						items = line.split('\t')
+						if len(items) < 2:
+							items.append('')
+
+						# if file exists open file
+						if items[0] and os.path.exists(items[0]):
+							try:
+								fd = open( items[0], 'rb' )
+								self._config[ fd ] = { 'filename': items[0], 'serverdir': items[1], 'pos': -1 }
+							except:
+								debug( LOGERROR, 'ERROR: cannot open %s - ignoring file' % items[0])
+		if len(self._config) == 0:
+			debug( LOGERROR, 'ERROR: there is no file to read - please update config')
+			sys.exit(1)
+		else:
+			notifier.timer_add( 50, notifier.Callback( self._check_new_data ) )
+
+
+	def _check_new_data( self ):
+		if self._socket and self._socket_ready:
+			for fobj in self._config.keys():
+				pos = fobj.tell()
+				fobj.seek(0,2)  # seek to end of file
+				length_fd = fobj.tell()
+				self._config[fobj]['pos'] = length_fd
+				fobj.seek(pos)  # seek back to current position
+				length_fn = os.stat( self._config[fobj]['filename'] )[6]
+
+				if pos < length_fd:
+					# still missing data to read
+					self._new_data_in_file( fobj )
+				if ( length_fn < length_fd and pos == length_fd ) or ( length_fd < pos ):
+					# file has been read completely and file seems to be rotated or truncated
+
+					newfd = open( self._config[fobj]['filename'], 'rb' )
+					self._config[newfd] = self._config[fobj]
+					del self._config[fobj]
+					fobj.close()
+					debug( LOGERROR, 'file seems to be rotated - using new one')
+
+		return True
+
+
+	def _new_data_in_file( self, sock ):
+		if len(self._resendQueue) < MAX_LENGTH_RESEND_QUEUE:
+			# read data from file
+			data = sock.read(65535)
+			if data:
+				debug( LOGDEBUG, 'new data in file: %s' % self._config[ sock ][ 'filename' ])
+				self._config[ sock ][ 'pos' ] = sock.tell()
+
+				# create packet
+				packet = { 'id': 0,
+						   'action': 'DATA',
+						   'filename': self._config[ sock ][ 'filename' ],
+						   'data': data }
+
+				self._send_pickled( packet )
+
+		return True
+
+
+	def _send_outbuffer( self, foo = None ):
+		if len(self._outbuffer):
+			try:
+				length = self._socket.send( self._outbuffer )
+				self._outbuffer = self._outbuffer[length:]
+			except ( SSL.WantReadError, SSL.WantWriteError, SSL.WantX509LookupError ), e:
+				pass
+			except:
+				debug( LOGWARN, 'ERROR: sending send_queue failed')
+		if len(self._outbuffer):
+			return True
+		return False
+
+
+	def _add_to_outbuffer( self, data ):
+		self._outbuffer += data
+		if self._socket:
+			notifier.socket_add( self._socket, self._send_outbuffer, condition = notifier.IO_WRITE )
+
+
+	def _resend_unacked_packets( self ):
+		for packetid, buf in self._resendQueue.items():
+			self._add_to_outbuffer( buf )
+
+
+	def _send_pickled( self, data, resendQueue = True ):
+		if resendQueue:
+			# set valid id
+			data['id'] = self._nextId
+
+		# create network data
+		buf = cPickle.dumps(data,-1)
+		buflen = struct.pack('!I', len(buf))
+		buf = buflen + buf
+
+		if resendQueue:
+			# put data into resend queue
+			self._resendQueue[ self._nextId ] = buf
+
+			# bump id
+			self._nextId += 1
+			if self._nextId > MAX_PICKLE_ID:
+				self._nextId = MIN_PICKLE_ID
+
+		self._add_to_outbuffer( buf )
+
+
+
+if __name__ == '__main__':
+
+	if baseconfig.has_key('logcollector/debug/level'):
+		loglevel = baseconfig['logcollector/debug/level']
+
+	loghost = ''
+	if baseconfig.has_key('logcollector/loghost'):
+		loghost = baseconfig['logcollector/loghost']
+	elif baseconfig.has_key('ldap/master'):
+		loghost = baseconfig['ldap/master']
+
+	parser = OptionParser()
+	parser.add_option( '-d', '--debug', action = 'store', type = 'int',
+					   dest = 'debug', default = 0,
+					   help = 'if given then debugging is activated and set to the specified level' )
+	parser.add_option( '-l', '--loghost', action = 'store', type = 'string',
+					   dest = 'host', default = '',
+					   help = 'if given then the specified loghost is used' )
+
+	( options, args ) = parser.parse_args()
+	if options.host:
+		loghost = options.host
+	if options.debug > 0:
+		loglevel = options.debug
+
+	debug(LOGERROR, 'using %s as loghost' % loghost )
+
+	notifier.init()
+
+	logserver = LogCollectorClient( loghost )
+	notifier.loop()
+
---
+++ services/univention-log-collector/log-collector-server.py
@@ -0,0 +1,363 @@
+#!/usr/bin/python2.4
+#
+# Univention Log Collector Server
+#   collects log file from other hosts
+#
+# Copyright (C) 2007 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# Binary versions of this file provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+# python packages
+import socket, fcntl, os, struct, cPickle, time
+
+# external packages
+from optparse import OptionParser
+import univention_baseconfig as ub
+import inspect
+import notifier
+from OpenSSL import *
+
+MAX_PICKLE_ID = 999999
+MIN_PICKLE_ID = 1
+
+LOGERROR = 0
+LOGWARN = 1
+LOGINFO = 2
+LOGDEBUG = 4
+
+loglevel = 0
+logobj = None
+
+def debug( level, msg ):
+	global logobj, loglevel
+	if level <= loglevel:
+		if not logobj:
+			logobj = open('/var/log/univention/log-collector-server.log','a')
+
+		info = inspect.getframeinfo(inspect.currentframe().f_back)[0:3]
+		printInfo=[]
+		if len(info[0])>30:
+			printInfo.append('...'+info[0][-27:])
+		else:
+			printInfo.append(info[0])
+		printInfo.extend(info[1:3])
+		logobj.write( "%s [L%s]: %s\n" % (time.asctime( time.localtime()), printInfo[1], msg) )
+		logobj.flush()
+
+baseconfig = ub.baseConfig()
+baseconfig.load()
+
+class LogCollectorServer( object ):
+	def __init__( self, port = 7450 ):
+		self._port = port
+		self._connectionstates = {}
+		self._ack_queue = []
+
+		self._targetdir = '/root/log/'
+		if baseconfig.has_key('logcollector/targetdir'):
+			self._targetdir = baseconfig[ 'logcollector/targetdir' ]
+		else:
+			debug( LOGERROR, 'WARNING: baseconfig variable "logcollector/targetdir" is not set' )
+			debug( LOGERROR, 'WARNING: using "logcollector/targetdir=%s" as default' % self._targetdir )
+
+		self._logrot_keepcnt = 99
+		if baseconfig.has_key('logcollector/logrotation/keepcount'):
+			try:
+				self._logrot_keepcnt = int(baseconfig[ 'logcollector/logrotation/keepcount' ])
+			except:
+				debug( LOGERROR, 'WARNING: baseconfig variable "logcollector/logrotation/keepcount" contains invalid value' )
+				sys.exit(1)
+		else:
+			debug( LOGERROR, 'WARNING: baseconfig variable "logcollector/logrotation/keepcount" is not set' )
+			debug( LOGERROR, 'WARNING: using "logcollector/logrotation/keepcount=%s" as default' % self._logrot_keepcnt )
+
+		self._logrot_maxsize = ''
+		if baseconfig.has_key('logcollector/logrotation/maxsize'):
+			try:
+				self._logrot_maxsize = baseconfig[ 'logcollector/logrotation/maxsize' ]
+			except:
+				pass
+		if not self._logrot_maxsize:
+			self._logrot_maxsize = '10M'
+			debug( LOGERROR, 'WARNING: baseconfig variable "logcollector/logrotation/maxsize" is not set' )
+			debug( LOGERROR, 'WARNING: using "logcollector/logrotation/maxsize=%s" as default' % self._logrot_maxsize )
+
+
+		multi = ''
+		if self._logrot_maxsize[-1].upper() in 'KMG':
+			multi = self._logrot_maxsize[-1].upper()
+			self._logrot_maxsize = self._logrot_maxsize[:-1]
+
+		try:
+			val = int(self._logrot_maxsize[:-1])
+		except:
+			val = 10
+			multi = 'M'
+		if multi == 'K':
+			val *= 1024
+		elif multi == 'M':
+			val *= 1024 * 1024
+		elif multi == 'G':
+			val *= 1024 * 1024 * 1024
+		self._logrot_maxsize = val
+
+		self._realsocket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+		self._realsocket.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
+		self._realsocket.setblocking( 0 )
+		fcntl.fcntl(self._realsocket.fileno(), fcntl.F_SETFD, 1)
+
+		self.crypto_context = SSL.Context( SSL.SSLv23_METHOD )
+		self.crypto_context.set_cipher_list('DEFAULT')
+		self.crypto_context.set_options( SSL.OP_NO_SSLv2 )
+		self.crypto_context.set_verify( SSL.VERIFY_PEER, self._verify_cert_cb )
+		dir = '/etc/univention/ssl/%s' % baseconfig[ 'hostname' ]
+		self.crypto_context.use_privatekey_file( os.path.join( dir, 'private.key' ) )
+		self.crypto_context.use_certificate_file( os.path.join( dir, 'cert.pem' ) )
+		self.crypto_context.load_verify_locations( os.path.join( dir, '/etc/univention/ssl/udsCA', 'CAcert.pem' ) )
+
+		self.connection = SSL.Connection( self.crypto_context , self._realsocket )
+		self.connection.setblocking(0)
+		self.connection.bind( ( '', self._port ) )
+		debug( LOGDEBUG, 'Server listening to SSL connects' )
+		self.connection.listen( 20 )
+
+		notifier.socket_add( self.connection, self._incoming_connection )
+
+
+	def _verify_cert_cb( self, conn, cert, errnum, depth, ok ):
+		debug( LOGDEBUG, 'Got certificate: %s' % cert.get_subject() )
+		debug( LOGDEBUG, 'Got certificate issuer: %s' % cert.get_issuer() )
+		debug( LOGDEBUG, 'errnum=%d  depth=%d  ok=%d' % (errnum, depth, ok) )
+		return ok
+
+
+	def _incoming_connection( self, socket ):
+		socket, addr = socket.accept()
+		socket.setblocking( 0 )
+		if addr:
+			client = '%s:%d' % ( addr[ 0 ], addr[ 1 ] )
+		else:
+			client = ''
+
+		debug( LOGERROR, 'incoming connection: %s' % client )
+
+		# create new state
+		state = { 'clientaddr': client,
+				  'nextId': 1,
+				  'inbuffer': '',
+				  'outbuffer': '',
+				  'targetdir': '',
+				  'filelist': {} }
+		self._connectionstates[ socket ] = state
+		notifier.socket_add( socket , self._receive_data )
+
+		return True
+
+
+	def _receive_data( self, sock ):
+		debug( LOGDEBUG, 'GOT NEW DATA' )
+
+		if not self._connectionstates.has_key( sock ):
+			debug( LOGERROR, 'unknown socket' )
+			return True
+
+		state = self._connectionstates[ sock ]
+		data = ''
+
+		try:
+			data = sock.recv( 16384 )
+		except SSL.WantReadError:
+			# this error can be ignored (SSL need to do something)
+			debug(LOGDEBUG, 'SSL.WantReadError')
+			return True
+		except ( SSL.SysCallError, SSL.Error ), error:
+			debug( LOGINFO, 'SSL error: %s. Probably the socket was closed by the client.' % str( error ) )
+			notifier.socket_remove( sock )
+			del self._connectionstates[ sock ]
+			sock.close()
+			return False
+
+		if not len( data ):
+			notifier.socket_remove( sock )
+			del self._connectionstates[ sock ]
+			sock.close()
+			return False
+
+		state[ 'inbuffer' ] += data
+
+		debug( LOGDEBUG, 'BUFFER: len=%d    got %d bytes' % (len(state['inbuffer']), len(data)))
+
+		# repeat while enough data is present
+		while len(state['inbuffer']) > 4:
+			# get length of pickle object
+			plen = struct.unpack('!I', state['inbuffer'][0:4])[0]
+			if plen+4 <= len(state['inbuffer']):
+				# unpickle data
+				packet = cPickle.loads( state['inbuffer'][4:4+plen] )
+				# remove data from buffer
+				state['inbuffer'] = state['inbuffer'][4+plen:]
+
+				# handle packet
+				if type(packet) == type({}):
+					if packet.has_key('action'):
+						if packet['action'] == 'SETUP':
+							self._handle_packet_setup( sock, state, packet )
+						elif packet['action'] == 'DATA':
+							self._handle_packet_data( sock, state, packet )
+			else:
+				# not enough data
+				break
+
+		# send ACKs
+		if self._ack_queue:
+			packet = { 'id': 0,
+					   'action': 'ACK',
+					   'data': self._ack_queue }
+			self._send_pickled( sock, packet )
+			self._ack_queue = []
+
+		return True
+
+	def _handle_packet_data( self, sock, state, packet ):
+		debug(LOGDEBUG, 'PACKET_DATA: id=%s' % packet['id'])
+		debug(LOGDEBUG, 'PACKET_DATA: fn=%s' % packet['filename'])
+		debug(LOGDEBUG, 'PACKET_DATA: len=%s' % len(packet['data']))
+		state = self._connectionstates[ sock ]
+		basename = os.path.basename(packet['filename'])
+		fn = os.path.join( state['targetdir'], state['filelist'][ packet['filename'] ], basename )
+		logfd = open( fn, 'ab' )
+		logfd.write( packet['data'] )
+		logfd.seek(0,2)  # seek to end of file
+		length = logfd.tell()
+		logfd.close()
+		debug(LOGDEBUG, 'LENGTH=%d   MAXSIZE=%d' % (length, self._logrot_maxsize))
+		if length > self._logrot_maxsize:
+			self._rotate_file( fn )
+
+		self._ack_queue.append( packet['id'] )
+
+
+	def _handle_packet_setup( self, sock, state, packet ):
+		debug(LOGDEBUG, 'Got SETUP')
+		addr = state['clientaddr']
+		addr = addr[ : addr.rfind(':') ]
+		targetdir = os.path.join(self._targetdir, addr)
+		if not os.path.exists( targetdir ):
+			os.makedirs( targetdir, 0700 )
+			debug( LOGWARN, 'INFO: created %s' % targetdir )
+
+		debug( LOGERROR, 'Client %s:' % addr )
+
+		state['targetdir'] = targetdir
+		for fn,fdir in packet['data']:
+			state['filelist'][ fn ] = fdir
+			absfdir = os.path.join( targetdir, fdir )
+			if not os.path.exists( absfdir ):
+				os.makedirs( absfdir, 0700 )
+				debug( LOGINFO, 'INFO: created %s' % absfdir )
+
+			basename = os.path.basename(fn)
+			logfn = os.path.join( absfdir, basename )
+			if os.path.exists( logfn ) and packet['rotate']:
+				self._rotate_file( logfn )
+			debug( LOGERROR, '  added %s' % logfn )
+
+
+		response = { 'id': 0,
+					 'action': 'SETUP',
+					 'data': 'OK' }
+		debug( LOGDEBUG, 'Sending SETUP:OK')
+		self._send_pickled( sock, response )
+
+
+	def _rotate_file( self, fn ):
+		if os.path.exists( '%s.%d.gz' % (fn, self._logrot_keepcnt) ):
+			debug( LOGDEBUG, ' REMOVE %s.%d.gz' % (fn, self._logrot_keepcnt) )
+			os.remove( '%s.%d.gz' % (fn, self._logrot_keepcnt) )
+		for i in range(self._logrot_keepcnt-1,-1,-1):
+			if os.path.exists( '%s.%d.gz' % (fn,i) ):
+				debug( LOGDEBUG, ' RENAME %s.%d.gz ==> %s.%d.gz' % (fn,i,fn,i+1) )
+				os.rename( '%s.%d.gz' % (fn,i), '%s.%d.gz' % (fn,i+1) )
+		if os.path.exists( fn ) and self._logrot_keepcnt > 0:
+			debug( LOGDEBUG, ' RENAME %s ==> %s.%d' % (fn,fn,1) )
+			os.rename( fn, '%s.1' % fn )
+			os.system( 'gzip %s.1' % fn )
+
+
+	def _send_outbuffer( self, sock ):
+		state = self._connectionstates[ sock ]
+		if len(state['outbuffer']):
+			try:
+				length = sock.send( state['outbuffer'] )
+				debug( LOGDEBUG, 'SEND SEND: %s <> %s' % (length, len(state['outbuffer'])))
+				state['outbuffer'] = state['outbuffer'][length:]
+			except ( SSL.WantReadError, SSL.WantWriteError, SSL.WantX509LookupError ), e:
+				pass
+			except:
+				debug( LOGWARN, 'ERROR: sending send_queue failed')
+		if len(state['outbuffer']):
+			return True
+		return False
+
+
+	def _add_to_outbuffer( self, sock, data ):
+		self._connectionstates[ sock ][ 'outbuffer' ] += data
+		notifier.socket_add( sock, self._send_outbuffer, condition = notifier.IO_WRITE )
+
+
+	def _send_pickled( self, sock, data ):
+		# set correct id
+		state = self._connectionstates[ sock ]
+		data['id'] = state['nextId']
+
+		# bump id
+		state['nextId'] += 1
+		if state['nextId'] > MAX_PICKLE_ID:
+			state['nextId'] = MIN_PICKLE_ID
+
+		# create network data
+		buf = cPickle.dumps(data,-1)
+		buflen = struct.pack('!I', len(buf))
+		buf = buflen + buf
+
+		self._add_to_outbuffer( sock, buf )
+
+
+
+if __name__ == '__main__':
+	if baseconfig.has_key('logcollector/debug/level'):
+		loglevel = baseconfig['logcollector/debug/level']
+
+	parser = OptionParser()
+	parser.add_option( '-d', '--debug', action = 'store', type = 'int',
+					   dest = 'debug', default = 0,
+					   help = 'if given then debugging is activated and set to the specified level' )
+
+	( options, args ) = parser.parse_args()
+	if options.debug > 0:
+		loglevel = options.debug
+
+	notifier.init()
+	logserver = LogCollectorServer()
+	notifier.loop()
