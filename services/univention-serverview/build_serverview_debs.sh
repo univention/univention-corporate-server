ad159f3b5afad8d254daa21bb7b0cbd8f04f2c1f
Stefan Gohmann <gohmann@univention.de>
2008-01-14 12:45:32 +0000

    copy from trunk
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-2.0/ucs@2870 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ services/univention-serverview/build_serverview_debs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+SERVER_URL="http://download.fujitsu-siemens.com/prim_supportcd/Software/ServerView/Linux/Agents/"
+AGENTS="srvmagt-mods_src srvmagt-eecd srvmagt-agents srvmagt-scs"
+VERSION="4.60-08"
+EXTENSION=".suse.rpm"
+
+if [ "$(whoami)" != root ]; then
+	echo "ERROR: Please run as root"
+	exit 1
+fi
+
+for a in $AGENTS; do
+	if [ ! -e "${a}-${VERSION}${EXTENSION}" ]; then
+		wget "${SERVER_URL}/${a}-${VERSION}${EXTENSION}"
+	fi
+done
+
+for a in $AGENTS; do
+	alien ${a}-${VERSION}${EXTENSION}
+done
+
+exit 0
