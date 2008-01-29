98a546fcc125fd54de74e98ca2ff5614930b0ee3
Arvid Requate <requate@univention.de>
2008-01-29 12:27:41 +0000

    set DOMAIN_NAME to $m, allowing e.g. MX for local zone to point to the mailhost
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@3009 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ services/univention-scalix/conffiles/sendmail-domain.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+#
+# Univention Baseconfig
+#  set DOMAIN_NAME in sendmail.cf
+#
+# Copyright (C) 2008 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# Binary versions of this file provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import os, re
+
+mcfile = '/etc/mail/sendmail.mc'
+cffile = '/etc/mail/sendmail.cf'
+
+def handler(baseConfig, changes):
+	patDomain = re.compile("define\(`confDOMAIN_NAME', `[^']*'\)dnl")
+	strDomain = "define(`confDOMAIN_NAME', `$m')dnl"
+
+	fh=open(mcfile, 'r')
+	lines = fh.read().splitlines()
+	fh.close()
+
+	sendmail_domain = False
+	if baseConfig.has_key('mail/sendmail/domain'):
+		sendmail_domain = True
+
+	changed = False
+	mailer_index = 0
+
+	strDomain_found = False
+
+	for idx in range(len(lines)):
+		if patDomain.match(lines[idx]):
+			lines.remove(lines[idx])
+			changed = True
+
+	for idx in range(len(lines)):
+		# save the index of the first mailer entry
+		if mailer_index == 0 and lines[idx].startswith('MAILER'):
+			mailer_index = idx
+			break
+
+	if sendmail_domain and mailer_index > 0:
+		lines.insert(mailer_index,strDomain)
+		changed = True
+		#
+	if changed:
+		fh=open(mcfile, 'w')
+		fh.write( '\n'.join(lines) )
+		fh.close()
+
+		os.system('m4 %s > %s' % (mcfile, cffile))
+		os.system('/etc/init.d/sendmail restart')
