56dde9ab984f946ded2bd70bd3619a8052d9497e
Sönke Schwardt-Krummrich <schwardt@univention.de>
2008-02-04 15:26:16 +0000

    Bug #10435
    * domain masquerading now customizable via univention-config-registry
      variable mail/sendmail/masqueradeas
    
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@3063 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ services/univention-scalix/conffiles/sendmail-masqueradeas.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+#
+# Univention Baseconfig
+#  set MASQUERADEAS in sendmail.cf
+#
+# Copyright (C) 2008 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# Binary versions of this file provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import os, re
+
+mcfile = '/etc/mail/sendmail.mc'
+cffile = '/etc/mail/sendmail.cf'
+
+def handler(baseConfig, changes):
+	patMasqueradeAs = re.compile("MASQUERADE_AS\(`[^']*'\)dnl")
+
+	fh=open(mcfile, 'r')
+	lines = fh.read().splitlines()
+	fh.close()
+
+	sendmail_masqueradeas = False
+	if baseConfig.get('mail/sendmail/masqueradeas',''):
+		strMasqueradeAs = "MASQUERADE_AS(`%s')dnl" % baseConfig['mail/sendmail/masqueradeas']
+		sendmail_masqueradeas = True
+
+	changed = False
+	strMasqueradeAs_found = False
+	mailer_index = 0
+
+	for idx in range(len(lines)):
+		if patMasqueradeAs.match(lines[idx]):
+			match_idx=idx
+			strMasqueradeAs_found = True
+
+	if strMasqueradeAs_found:
+		lines.remove(lines[match_idx])
+		changed = True
+
+	for idx in range(len(lines)):
+		# save the index of the first mailer entry
+		if mailer_index == 0 and lines[idx].startswith('MAILER'):
+			mailer_index = idx
+			break
+
+	if sendmail_masqueradeas and mailer_index > 0:
+		lines.insert(mailer_index,strMasqueradeAs)
+		changed = True
+		#
+	if changed:
+		fh=open(mcfile, 'w')
+		fh.write( '\n'.join(lines) )
+		fh.close()
+
+		os.system('m4 %s > %s' % (mcfile, cffile))
+		os.system('/etc/init.d/sendmail restart')
