f8087c69cb6e021b441ec9591f4cda8630ae5404
Andreas Büsching <buesching@univention.de>
2008-06-16 07:08:50 +0000

    merge support for CSV reports from trunk/component/udm/
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-2.1/ucs@4532 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-directory-reports/modules/univention/directory/reports/document.py
+++ management/univention-directory-reports/modules/univention/directory/reports/document.py
@@ -38,14 +38,28 @@ from interpreter import *
 import admin
 
 class Document( object ):
+	( TYPE_LATEX, TYPE_CSV, TYPE_UNKNOWN ) = range( 3 )
+
 	def __init__( self, template, header = None, footer = None ):
 		self._template = template
 		self._header = header
 		self._footer = footer
+		if self._template.endswith( '.tex' ):
+			self._type = Document.TYPE_LATEX
+		elif self._template.endswith( '.csv' ):
+			self._type = Document.TYPE_CSV
+		else:
+			self._type = Document.TYPE_UNKNOWN
 		self.__check_files()
 
 	def __check_files( self ):
-		for filename in ( self._header, self._footer, self._template ):
+		if self._type == Document.TYPE_LATEX:
+			files = ( self._header, self._footer, self._template )
+		elif self._type == Document.TYPE_CSV:
+			files = ( self._template, )
+		else:
+			files = tuple()
+		for filename in files:
 			if not os.path.isfile( filename ):
 				raise Exception( "error: required file '%s' does not exist or is not readable" % \
 								 filename )
@@ -54,7 +68,7 @@ class Document( object ):
 		umask = os.umask( 0077 )
 		unique = base64.encodestring( os.urandom( 12 ) )[ : -1 ]
 		unique = unique.replace( '/', '-' )
-		filename = os.path.join( '/tmp', 'univention-directory-reports-%d-%s.tex' % ( os.getpid(), unique ) )
+		filename = os.path.join( '/tmp', 'univention-directory-reports-%d-%s.src' % ( os.getpid(), unique ) )
 		fd = open( filename, 'w' )
 		fd.close()
 		os.umask( umask )
@@ -66,23 +80,32 @@ class Document( object ):
 		fd.write( tmpfd.read() )
 		tmpfd.close()
 
-	def create_latex( self, objects = [] ):
+	def create_source( self, objects = [] ):
 		tmpfile = self.__create_tempfile()
+		admin.set_format( self._type == Document.TYPE_LATEX )
+		parser = Parser( filename = self._template )
+		parser.tokenize()
+		tokens = parser._tokens
 		fd = codecs.open( tmpfile, 'wb+', encoding = 'utf8' )
-		self.__append_file( fd, self._header )
+		if parser._header:
+			fd.write( parser._header.data )
+		elif self._header:
+			self.__append_file( fd, self._header )
+
 		for dn in objects:
 			if isinstance( dn, basestring ):
 				obj = admin.get_object( None, dn )
 			else:
 				obj = admin.cache_object( dn )
-			parser = Parser( filename = self._template )
-			parser.tokenize()
-			tokens = parser._tokens
-			interpret = Interpreter( obj, tokens )
+			tks = copy.deepcopy( tokens )
+			interpret = Interpreter( obj, tks )
 			interpret.run()
-			output = Output( tokens, fd = fd )
+			output = Output( tks, fd = fd )
 			output.write()
-		self.__append_file( fd, self._footer )
+		if parser._footer:
+			fd.write( parser._footer.data )
+		elif self._footer:
+			self.__append_file( fd, self._footer )
 		fd.close()
 
 		return tmpfile
