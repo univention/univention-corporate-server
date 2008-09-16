1a2fb2473d74c2900cae7c5fa02821aa1143a5f6
Felix Botner <botner@univention.de>
2008-09-16 12:55:02 +0000

    do not start oosplash.bin in /usr/lib/openoffice/program/soffice and set locking to yes in /etc/openoffice/soffice.sh (Bug #11200 #11195)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-2.1/ucs@6065 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ desktop/univention-ooffice2/soffice.sh
@@ -0,0 +1,21 @@
+# configuration file to set up some environment variables for OOo
+
+# File locking; possible values are:
+# - yes:  enable file locking unconditionally
+# - no:   disable file locking
+# - auto: enable file locking, when the document is found on a nfs share
+# If the environment variable SAL_ENABLE_FILE_LOCKING is set,
+# the setting if ENABLE_FILE_LOCKING has no effect.
+
+FILE_LOCKING=yes
+
+# OpenGL support; may cause trouble with the restricted nvidia and fglrx
+# drivers; possible values are:
+# - yes:  enable OpenGL support unconditionally
+# - no:   disable OpenGL support.
+# - auto: only enable OpenGL support, if not running with the restricted
+#   nvidia and fglrx drivers.
+# If the environment variable SAL_NOOPENGL is set,
+# the setting if OPENGL_SUPPORT has no effect.
+
+OPENGL_SUPPORT=no
