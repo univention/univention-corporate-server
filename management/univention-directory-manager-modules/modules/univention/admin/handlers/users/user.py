f62b1229a084d202b47b387b9ba6d3d7230ed92c
Jan Christoph Ebersbach <ebersbach@univention.de>
2009-01-12 10:21:10 +0000

    * the user's firstname can be changed to a required ingredient via UCR   variable udm/modules/users/user/properties/firstname/required. Bug #12876
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@8026 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-directory-manager-modules/modules/univention/admin/handlers/users/user.py
+++ management/univention-directory-manager-modules/modules/univention/admin/handlers/users/user.py
@@ -1046,6 +1046,10 @@ property_descriptions={
 for key, value in mungeddial.properties.items():
 	property_descriptions[ key ] = value
 
+# overwrite properties by UCR variables
+ucr_properties = ['required']
+univention.admin.ucr_overwrite_properties (module, ucr_properties, property_descriptions)
+
 default_property_descriptions=copy.deepcopy(property_descriptions) # for later reset of descriptions
 
 layout=[
