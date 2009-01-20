38e59af0be8e66d7867fa5100022760b1ab9646c
Sönke Schwardt-Krummrich <schwardt@univention.de>
2009-01-20 17:03:21 +0000

    Bug #8736 * added updated version of AD connector UMC module
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@8154 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-ad-connector/modules/univention/management/console/handlers/adconnector/__init__.py
+++ services/univention-ad-connector/modules/univention/management/console/handlers/adconnector/__init__.py
@@ -43,7 +43,7 @@ import univention.config_registry
 import notifier.popen
 import os, stat, shutil
 
-import subprocess, time
+import subprocess, time, grp
 
 FN_BINDPWD = '/etc/univention/connector/ad/bindpwd'
 DIR_WEB_AD = '/var/www/univention-ad-connector'
@@ -54,6 +54,14 @@ icon = 'adconnector/module'
 short_description = _('AD Connector')
 long_description = _('Configure AD Connector')
 categories = ['all', 'system']
+
+class UMC_AD_StaticSelection( umc.StaticSelection ):
+	def __init__( self, required = True, title = '', choices = () ):
+		umc.StaticSelection.__init__( self, unicode( title ), required = required )
+		self.default_choices_list = choices
+	def choices( self ):
+		return self.default_choices_list
+umcd.copy( umc.StaticSelection, UMC_AD_StaticSelection )
 
 
 command_description = {
@@ -75,12 +83,42 @@ command_description = {
 			'ad_ldap_host': umc.String( _('Hostname of Active Directory server'), regex = '^([a-z]([a-z0-9-]*[a-z0-9])*[.])+[a-z]([a-z0-9-]*[a-z0-9])*$' ),
 			'ad_ldap_base': umc.String( _('BaseDN of Active Directory') ),
 			'ad_ldap_binddn': umc.String( _('DN of replication user') ),
-			'ad_ldap_bindpw': umc.String( _('Password of replication user') ),
+			'ad_ldap_bindpw': umc.Password( _('Password of replication user'), required = False ),
 			'ad_poll_sleep': umc.String( _('Poll Interval (seconds)'), regex = '^[0-9]+$' ),
-			'ad_windows_version': umc.String( _('Version of Windows server') ),
+			'ad_windows_version': UMC_AD_StaticSelection( title=_('Version of Windows server'),
+													choices = ( ( 'win2000', _( 'Windows 2000' ) ), ( '', _( 'Windows 2003' ) ) ) ),
 			'ad_retry_rejected': umc.String( _('Retry interval for rejected objects'), regex = '^[0-9]+$' ),
-			'debug_level': umc.String( _('Debug level') ),
-			'debug_functions': umc.String( _('Debug functions') ),
+			# Workaround for Bug #13139: '_0_' up to '_4_' is a workaround
+			'debug_level': UMC_AD_StaticSelection( title=_('Debug level of AD Connector'),
+													choices = ( ( '_0_', _( '0' ) ), ( '_1_', _( '1' ) ), ( '_2_', _( '2' ) ), ( '_3_', _( '3' ) ), ( '_4_', _( '4' ) ) ) ),
+			# Workaround for Bug #13139: '_0_' and '_1_' is a workaround
+			'debug_function': UMC_AD_StaticSelection( title=_('Add debug output for functions'),
+													choices = ( ( '_0_', _( 'no' ) ), ( '_1_', _( 'yes' ) ) ) ),
+			'ad_mapping_sync_mode': UMC_AD_StaticSelection( title=_('AD Connector sync mode'),
+															choices = (	( 'read', _( 'read (AD --> UCS)' ) ),
+																		( 'write', _( 'write (UCS --> AD)' ) ),
+																		( 'sync', _( 'sync (AD <--> UCS)' ) ) ) ),
+			'ad_mapping_group_language': umc.LanguageSelection( _('AD Connector group mapping language') ),
+			},
+		caching = False
+	),
+
+	'adconnector/uploadcert': umch.command(
+		short_description = _('Upload AD Connector Certificate'),
+		long_description = _('Upload AD Connector Certificate'),
+		method = 'upload_cert',
+		values = {
+			'certfile': umc.FileUploader( _('Please upload Active Directory certificate'), maxfiles=1 ),
+			},
+		caching = False
+	),
+
+	'adconnector/switchstate': umch.command(
+		short_description = _('Start/Stop AD Connector'),
+		long_description = _('Start/Stop AD Connector'),
+		method = 'start_stop',
+		values = {
+			'action': umc.String( 'action' ),
 			},
 		caching = False
 	),
@@ -160,10 +198,29 @@ class handler(umch.simpleHandler):
 									( 'ad_ldap_base', 'connector/ad/ldap/base' ),
 									( 'ad_ldap_binddn', 'connector/ad/ldap/binddn' ),
 									( 'ad_poll_sleep', 'connector/ad/poll/sleep' ),
+									( 'ad_retry_rejected', 'connector/ad/retryrejected' ),
+									( 'debug_level', 'connector/debug/level' ),
+									( 'debug_function', 'connector/debug/function' ),
+									( 'ad_mapping_sync_mode', 'connector/ad/mapping/syncmode' ),
+									( 'ad_mapping_group_language', 'connector/ad/mapping/language' ),
 									):
-				if obj.options.get( umckey ):
-					debugmsg( ud.ADMIN, ud.ERROR, 'DEBUG: umckey=%s' % umckey )
-					univention.config_registry.handler_set( [ u'%s=%s' % (ucrkey, obj.options.get(umckey)) ] )
+				val = obj.options.get(umckey)
+				if val:
+					# Workaround for Bug #13139 START
+					if umckey in [ 'debug_level', 'debug_function' ]:
+						val = val.strip('_')
+					# Workaroung for Bug #13139 END
+					debugmsg( ud.ADMIN, ud.INFO, 'setting %s=%s' % (umckey, val) )
+					univention.config_registry.handler_set( [ u'%s=%s' % (ucrkey, val) ] )
+
+			# special handling for connector/ad/windows_version
+			val = obj.options.get('ad_windows_version')
+			if obj.options.get( val ):
+					debugmsg( ud.ADMIN, ud.INFO, 'setting %s=%s' % (umckey, val) )
+					univention.config_registry.handler_set( [ u'%s=%s' % (ucrkey, val) ] )
+			else:
+					debugmsg( ud.ADMIN, ud.INFO, 'unsetting %ss' % (umckey) )
+					univention.config_registry.handler_unset( [ u'%s' % ucrkey ] )
 
 			debugmsg( ud.ADMIN, ud.ERROR, 'DEBUG: 11' )
 
@@ -214,12 +271,20 @@ class handler(umch.simpleHandler):
 		debugmsg( ud.ADMIN, ud.ERROR, 'DEBUG: 4' )
 
 
+
 	def _copy_certificate(self, obj, error_if_missing = False):
 		ssldir = '/etc/univention/ssl/%s' % obj.options.get('ad_ldap_host')
+		try:
+			gid_wwwdata = grp.getgrnam('www-data')[2]
+		except:
+			gid_wwwdata = 0
 		if os.path.exists( ssldir ):
 			for fn in ( 'private.key', 'cert.pem' ):
+				dst = '%s/%s' % (DIR_WEB_AD, fn)
 				try:
-					shutil.copy2( '%s/%s' % (ssldir, fn), '%s/%s' % (DIR_WEB_AD, fn) )
+					shutil.copy2( '%s/%s' % (ssldir, fn), dst )
+					os.chmod( dst, stat.S_IRUSR | stat.S_IRGRP )
+					os.chown( dst, 0, gid_wwwdata )
 				except Exception, e:
 					self.msg['error'].append( _('copy of %s/%s to %s/%s failed (exception=%s)') % (ssldir, fn, DIR_WEB_AD, fn, str(e.__class__)) )
 					debugmsg( ud.ADMIN, ud.ERROR, 'copy of %s/%s to %s/%s failed (exception=%s)' % (ssldir, fn, DIR_WEB_AD, fn, str(e.__class__)) )
@@ -260,6 +325,39 @@ class handler(umch.simpleHandler):
 			debugmsg( ud.ADMIN, ud.ERROR, 'Could not determine baseDN of given ldap server. Maybe FQDN is wrong or unresolvable! FQDN=%s' % obj.options.get('ad_ldap_host') )
 
 		debugmsg( ud.ADMIN, ud.INFO, 'guessed baseDN: %s' % self.guessed_baseDN )
+
+		self.finished(obj.id(), None)
+
+	def upload_cert(self, obj):
+		_d = ud.function('adconnector.handler.upload_cert')
+		debugmsg( ud.ADMIN, ud.INFO, 'upload_cert: options=%s' % obj.options )
+
+		files = umcobject.options.get('fileupload',[])
+		if len(files) == 1:
+			fileitem = files[0]
+			now = time.strftime( '%Y%m%d_%H%M%S', time.localtime() )
+			fn = '/etc/univention/connector/ad/ad_cert_%s.pem' % now
+			cmd = 'openssl x509 -inform der -outform pem -in %s -out %s' % (fileitem['tmpfname'], fn)
+
+			debugmsg( ud.ADMIN, ud.INFO, 'determine baseDN of specified system: %s' % cmd )
+			proc = notifier.popen.Shell( cmd, stdout = True )
+			cb = notifier.Callback( self._upload_cert_return, obj, fn )
+			proc.signal_connect( 'finished', cb )
+			proc.start()
+		else:
+			debugmsg( ud.ADMIN, ud.ERROR, 'ERROR: len(files)=%s  files=%s' % (len(files), files) )
+			self.finished(obj.id(), None)
+
+
+	def _upload_cert_return( self, pid, status, buffer, obj, fn ):
+		_d = ud.function('adconnector.handler._upload_cert_return')
+		if status == 0:
+			univention.config_registry.handler_set( [ u'connector/ad/ldap/certificate=%s' % fn ] )
+			self.msg['hint'].append( _('Certificate has been uploaded successfully.') )
+			debugmsg( ud.ADMIN, ud.INFO, _('Certificate has been uploaded successfully.') )
+		else:
+			self.msg['error'].append( _('Certificate upload and conversion failed.') )
+			debugmsg( ud.ADMIN, ud.ERROR, _('Certificate upload and conversion failed. status=%s  output=%s' % (status, buffer)) )
 
 		self.finished(obj.id(), None)
 
@@ -363,7 +461,6 @@ class handler(umch.simpleHandler):
 
 		self.__update_status()
 
-		list_items = umcd.List()
 		list_id = []
 
 		debugmsg( ud.ADMIN, ud.ERROR, 'DEBUG: 20' )
@@ -374,7 +471,6 @@ class handler(umch.simpleHandler):
 		else:
 			ldaphost = self.configRegistry.get('connector/ad/ldap/host', '')
 		inp_ldap_host = umcd.make( self['adconnector/configure']['ad_ldap_host'], default = ldaphost )
-		list_items.add_row( [ inp_ldap_host ] )
 		list_id.append( inp_ldap_host.id() )
 
 		# create guess basedn button
@@ -388,10 +484,12 @@ class handler(umch.simpleHandler):
 		# ask for ldap_base and/or display a first guess based on ldapsearch call
 		if obj.options.get('action','') == 'guess_basedn' and self.guessed_baseDN:
 			basedn = self.guessed_baseDN
+			if basedn:
+				self.msg['hint'].append( _('BaseDN of %s has been determined successfully. Suggestion for replication user has been set.') % obj.options.get('ad_ldap_host') )
+				debugmsg( ud.ADMIN, ud.ERROR, 'BaseDN of %s has been determined successfully. Suggestion for replication user has been set.' % obj.options.get('ad_ldap_host') )
 		else:
 			basedn = self.configRegistry.get('connector/ad/ldap/base', '')
 		inp_ldap_base = umcd.make( self['adconnector/configure']['ad_ldap_base'], default = basedn )
-		list_items.add_row( [ inp_ldap_base, btn_guess ] )
 		list_id.append( inp_ldap_base.id() )
 
 		# ask for ldap_binddn and/or display a first guess based on ldapsearch call
@@ -400,38 +498,70 @@ class handler(umch.simpleHandler):
 		else:
 			binddn = self.configRegistry.get('connector/ad/ldap/binddn', '')
 		inp_ldap_binddn = umcd.make( self['adconnector/configure']['ad_ldap_binddn'], default = binddn )
-		list_items.add_row( [ inp_ldap_binddn ] )
 		list_id.append( inp_ldap_binddn.id() )
 
 		debugmsg( ud.ADMIN, ud.ERROR, 'DEBUG: 22' )
 
 		# get bind password if UCR variable is already set and file exists
 		pwd = ''
-		fn = self.configRegistry.get('connector/ad/ldap/bindpwd', FN_BINDPWD)
-		if os.path.exists( fn ):
-			try:
-				pwd = open( fn, 'r' ).read().strip('\n')
-			except Exception, e:
-				self.msg['error'].append( _('reading bind password failed (filename=%(fn)s ; exception=%(exception)s)') % { 'fn': fn, 'exception': str(e.__class__)})
-				debugmsg( ud.ADMIN, ud.ERROR, 'reading bind password failed (filename=%(fn)s ; exception=%(exception)s)' % { 'fn': fn, 'exception': str(e.__class__)} )
+		fn = self.configRegistry.get('connector/ad/ldap/bindpwd')
+		if not fn or not os.path.exists( fn ):
+			self.msg['hint'].append( _('Password of replication user has not been set yet!') )
+			debugmsg( ud.ADMIN, ud.WARN, 'Password of replication user has not been set yet' )
 
 		# ask for ldap_bindpwd
-		inp_ldap_bindpwd = umcd.make( self['adconnector/configure']['ad_ldap_bindpw'], default = pwd )
-		list_items.add_row( [ inp_ldap_bindpwd ] )
+		inp_ldap_bindpwd = umcd.make( self['adconnector/configure']['ad_ldap_bindpw'], default = '' )
 		list_id.append( inp_ldap_bindpwd.id() )
+
+		# ask for windows version
+		inp_windows_version = umcd.make( self['adconnector/configure']['ad_windows_version'], default = self.configRegistry.get('connector/ad/windows_version', '') )
+		list_id.append( inp_windows_version.id() )
+
+		# ask for mapping syncmode
+		inp_sync_mode = umcd.make( self['adconnector/configure']['ad_mapping_sync_mode'], default = self.configRegistry.get('connector/ad/mapping/syncmode', 'sync') )
+		list_id.append( inp_sync_mode.id() )
+
+		# ask for mapping language
+		inp_mapping_language = umcd.make( self['adconnector/configure']['ad_mapping_group_language'], default = self.configRegistry.get('connector/ad/mapping/group/language', 'de') )
+		list_id.append( inp_mapping_language.id() )
+
+		# ask for retry rejected
+		inp_retry_rejected = umcd.make( self['adconnector/configure']['ad_retry_rejected'], default = self.configRegistry.get('connector/ad/retryrejected', '10') )
+		list_id.append( inp_retry_rejected.id() )
 
 		# ask for poll_sleep
 		inp_poll_sleep = umcd.make( self['adconnector/configure']['ad_poll_sleep'], default = self.configRegistry.get('connector/ad/poll/sleep', '5') )
-		list_items.add_row( [ inp_poll_sleep ] )
 		list_id.append( inp_poll_sleep.id() )
 		debugmsg( ud.ADMIN, ud.ERROR, 'DEBUG: 23' )
 
+		# ask for debug level
+		inp_debug_level = umcd.make( self['adconnector/configure']['debug_level'], default = self.configRegistry.get('connector/debug/level', '_1_') )
+		list_id.append( inp_debug_level.id() )
+
+		# ask for debug level
+		inp_debug_function = umcd.make( self['adconnector/configure']['debug_function'], default = self.configRegistry.get('connector/debug/function', '_0_') )
+		list_id.append( inp_debug_function.id() )
+
+
+		# create save button
 		opts = { 'action': 'save' }
 		req = umcp.Command( args = [ 'adconnector/configure' ], opts = opts )
 		actions = ( umcd.Action( req, list_id ), )
 		btn_set = umcd.Button( _('Save Changes'), 'actions/ok', actions = actions, close_dialog = False )
-		list_items.add_row( [ btn_set ] )
 
+		btn_close = umcd.CloseButton()
+
+		# create layout
+		list_items = umcd.List()
+
+		list_items.add_row( [ inp_ldap_host, inp_ldap_base ] )
+		list_items.add_row( [ inp_ldap_binddn, inp_ldap_bindpwd ] )
+		list_items.add_row( [ inp_windows_version, inp_sync_mode ] )
+		list_items.add_row( [ inp_mapping_language, inp_retry_rejected ] )
+		list_items.add_row( [ inp_debug_level, inp_poll_sleep ] )
+		list_items.add_row( [ inp_debug_function ] )
+		list_items.add_row( [ btn_guess ] )
+		list_items.add_row( [ btn_set, btn_close ] )
 		frame = umcd.Frame( [list_items], _('AD Connector Configuration'))
 
 		res.dialog = []
@@ -439,8 +569,10 @@ class handler(umch.simpleHandler):
 			lst = umcd.List()
 			for key in ( 'error', 'warn', 'hint' ):
 				for msg in self.msg[key]:
-					lst.add_row( [ msg ] )
-			res.dialog.append( umcd.Frame( [lst] ) )
+					img = umcd.Image( 'adconnector/%s' % key )
+					txt = umcd.Text( msg )
+					lst.add_row( [ ( img, txt ) ] )
+			res.dialog.append( umcd.Frame( [lst], _('Messages') ) )
 
 		res.dialog.append(frame)
 
