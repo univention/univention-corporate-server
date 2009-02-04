0e6cdd638880d84d2805deb1b392bff0ef06ed48
Jan Christoph Ebersbach <ebersbach@univention.de>
2009-02-04 10:50:06 +0000

    * fixed problem with default the timeout UCR variable which is not set in   every case. Bug #13306
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@8370 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console/conffiles/usr/share/univention-management-console/www/config.php
+++ management/univention-management-console/conffiles/usr/share/univention-management-console/www/config.php
@@ -40,15 +40,18 @@ tail='";'
 if baseConfig.has_key('umc/web/timeout'):
 	try:
 		time = int(baseConfig['umc/web/timeout'])
+		if time  > 2147483647:
+			time = 2147483647
+		elif time <= 30:
+			# minimum timeout is 30 seconds
+			time = 900
 	except:
 		time = 900
 else:
 	time = 900
 
-if time  > 2147483647:
-	timeout='-t 2147483647 '
-else:
-	timeout='-t %s '%baseConfig['umc/web/timeout']
+timeout = '-t %d ' % time
+
 
 run=run+" -e $https "
 run=run+debug
