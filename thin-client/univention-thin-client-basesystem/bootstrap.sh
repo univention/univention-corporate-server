a8c3b10ba289d889f43a9b265d9ce68a2f5f64df
Stefan Gohmann <gohmann@univention.de>
2009-02-13 13:40:22 +0000

    removed some boot error messages. Bug #11634
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@8540 1b283449-9f2f-0410-b571-8eb228e1a901

--- thin-client/univention-thin-client-basesystem/bootstrap.sh
+++ thin-client/univention-thin-client-basesystem/bootstrap.sh
@@ -181,6 +181,7 @@ chmod 755 "/sbin/start-stop-daemon"
 	if test -e /etc/network/run; then rm -rf /etc/network/run; fi
 	ln -fs /ramdisk/etc/network/run /etc/network/run
 	ln -fs /ramdisk/etc/udev/rules.d/z25_persistent-net.rules /etc/udev/rules.d/z25_persistent-net.rules
+	ln -fs /ramdisk/etc/udev/rules.d/z25_persistent-cd.rules /etc/udev/rules.d/z25_persistent-cd.rules
 
 	rm -f /etc/univention/disable_baseconfig
 
