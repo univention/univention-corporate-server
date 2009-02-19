62ba61c98488d58c1db513e4a475126ce26a826f
Stefan Gohmann <gohmann@univention.de>
2009-02-19 08:10:21 +0000

    updated the copyright. Bug #6875
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@8706 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-kolab2-framework/kolabdomain.py
+++ services/univention-kolab2-framework/kolabdomain.py
@@ -3,7 +3,7 @@
 # Univention Kolab2 Framework
 #  listener module: mail domain configuration
 #
-# Copyright (C) 2005, 2006 Univention GmbH
+# Copyright (C) 2005-2009 Univention GmbH
 #
 # http://www.univention.de/
 #
