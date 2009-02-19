b7aaf32890e343c0bb1e94afc139a79efc7f9b72
Stefan Gohmann <gohmann@univention.de>
2009-02-19 08:30:21 +0000

    updated the copyright. Bug #6875
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@8723 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-system-setup/usr/lib/univention-system-setup/scripts/ldap_available.sh
+++ base/univention-system-setup/usr/lib/univention-system-setup/scripts/ldap_available.sh
@@ -1,7 +1,7 @@
 # Univention Setup
 #  ldap available check script
 #
-# Copyright (C) 2004, 2005, 2006, 2007 Univention GmbH
+# Copyright (C) 2004-2009 Univention GmbH
 #
 # http://www.univention.de/
 #
