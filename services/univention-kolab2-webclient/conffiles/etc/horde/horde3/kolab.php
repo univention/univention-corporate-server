8966681b205ac4e715f1ba1940a66c358221c543
Stefan Gohmann <gohmann@univention.de>
2009-04-09 13:34:05 +0000

    fixed another UCR typo in kolab.php. Bug #14152
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@9517 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-kolab2-webclient/conffiles/etc/horde/horde3/kolab.php
+++ services/univention-kolab2-webclient/conffiles/etc/horde/horde3/kolab.php
@@ -3,23 +3,23 @@
 @%@BCWARNING=// @%@
 
 @!@
-	if baseConfig.has_key("horde/hosteddomain"):
-		domains = baseConfig["horde/hosteddomain"]
-	elif baseConfig.has_key("mail/hosteddomains"):
-		domains = baseConfig["mail/hosteddomains"]
-	else:
-		domains=baseConfig['domainname']
+if baseConfig.has_key("horde/hosteddomain"):
+	domains = baseConfig["horde/hosteddomain"]
+elif baseConfig.has_key("mail/hosteddomains"):
+	domains = baseConfig["mail/hosteddomains"]
+else:
+	domains=baseConfig['domainname']
 
-	if domains.find( ' ' ) != -1:
-		d = domains[ : domains.find( ' ' ) ]
-	else:
-		d = domains
+if domains.find( ' ' ) != -1:
+	d = domains[ : domains.find( ' ' ) ]
+else:
+	d = domains
 
-	print "$conf['cookie']['domain'] = '%s.%s';" % (baseConfig['hostname'],d)
-	print "$conf['problems']['email'] = '%s';" % baseConfig['mail/alias/webmaster']
-	print "$conf['problems']['maildomain'] = '%s';" % d
-	print "$conf['kolab']['ldap']['basedn'] = '%s';" % baseconfig['ldap/base']
-	print "$conf['kolab']['imap']['server'] = '%s';" % baseconfig['horde/imapserver']
-	print "$conf['kolab']['imap']['maildomain'] = '%s';" % d
+print "$conf['cookie']['domain'] = '%s.%s';" % (baseConfig['hostname'],d)
+print "$conf['problems']['email'] = '%s';" % baseConfig['mail/alias/webmaster']
+print "$conf['problems']['maildomain'] = '%s';" % d
+print "$conf['kolab']['ldap']['basedn'] = '%s';" % baseConfig['ldap/base']
+print "$conf['kolab']['imap']['server'] = '%s';" % baseConfig['horde/imapserver']
+print "$conf['kolab']['imap']['maildomain'] = '%s';" % d
 @!@
 ?>
