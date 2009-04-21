c3d31de8cc9a5db09a908deda03c6c6f012d57bf
Stefan Gohmann <gohmann@univention.de>
2009-04-21 14:02:23 +0000

    * updated the copyright. Bug #6875 * added more UCR variables for the apache php ini file:    php/memory/executiontime for max_execution_time    php/limit/inputtime for max_input_time    php/limit/sockettimeout for default_socket_timeout   Part of Bug #12454
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-2.2/ucs@9646 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-apache/conffiles/extension.py
+++ services/univention-apache/conffiles/extension.py
@@ -4,7 +4,7 @@
 #  baseconfig module: modifies the memory limit for PHP applications run
 #  by apache
 #
-# Copyright (C) 2004, 2005, 2006 Univention GmbH
+# Copyright (C) 2004-2009 Univention GmbH
 #
 # http://www.univention.de/
 #
@@ -32,7 +32,8 @@
 import os, sys
 
 def handler(bc,changes):
-	if bc.get('php/memory/limit') or bc.get('php/limit/filesize') or bc.get('php/limit/postsize'):
+	if bc.get('php/memory/limit') or bc.get('php/limit/filesize') or bc.get('php/limit/postsize') \
+	or bc.get('php/memory/executiontime') or bc.get('php/limit/inputtime') or bc.get('php/limit/sockettimeout'):
 		memlimit = bc.get('php/memory/limit')
 		if memlimit and memlimit[-1:].lower() == 'm':
 			memlimit = memlimit[:-1]
@@ -44,6 +45,18 @@ def handler(bc,changes):
 		filesize = bc.get('php/limit/filesize')
 		if filesize and filesize[-1:].lower() == 'm':
 			filesize = filesize[:-1]
+
+		executiontime = bc.get('php/memory/executiontime')
+		if executiontime and executiontime[-1:].lower() == 's':
+			executiontime = executiontime[:-1]
+
+		inputtime = bc.get('php/limit/inputtime') 
+		if inputtime and inputtime[-1:].lower() == 's':
+			inputtime = inputtime[:-1]
+
+		sockettimeout = bc.get('php/limit/sockettimeout'):
+		if sockettimeout and sockettimeout[-1:].lower() == 's':
+			sockettimeout = sockettimeout[:-1]
 
 		try:
 			f = open('/etc/php5/apache2/php.ini', 'r')
@@ -62,6 +75,15 @@ def handler(bc,changes):
 
 			if filesize and line[:22] == 'upload_max_filesize = ':
 				line = 'upload_max_filesize = %sM  ; Maximum allowed size for uploaded files. (2MB)\n' % str(filesize)
+
+			if executiontime and line[:21] == 'max_execution_time = ':
+				line = 'max_execution_time = %s  ; Maximum execution time of each script, in seconds\n' % str(executiontime)
+
+			if inputtime and line[:17] == 'max_input_time = ':
+				line = 'max_input_time = %s ; Maximum amount of time each script may spend parsing request data \n' % str(inputtime)
+
+			if sockettimeout and line[:25] == 'default_socket_timeout = ':
+				line = 'default_socket_timeout = %s\n ' % str(sockettimeout)
 
 			tmp.append(line)
 			line = f.readline()
