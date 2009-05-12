8e2a6da0a130678eca09efa5492fce6a84fd6123
Andreas Büsching <buesching@univention.de>
2009-05-12 08:48:56 +0000

    merge preup.sh: reduced required disk space estimation for the update to 2.2-1. Bug #14433
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-2.2/ucs@9878 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-updater/script/preup.sh
+++ base/univention-updater/script/preup.sh
@@ -52,9 +52,9 @@ check_space(){
 if [ "$update22_checkfilesystems" != "no" ]
 then
 
-	check_space "/var/cache/apt/archives" "800000" "800 MB"
+	check_space "/var/cache/apt/archives" "400000" "400 MB"
 	check_space "/boot" "20000" "20 MB"
-	check_space "/" "1200000" "1200 MB"
+	check_space "/" "600000" "600 MB"
 
 else
     echo "WARNING: skipped disk-usage-test as you requested"
