8f46c01ddfa126d552d9f02256a212ef172a2386
Andreas Büsching <buesching@univention.de>
2009-06-11 11:42:51 +0000

    use univention.debug instead of univention.debug2 (Bug #14736)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@10221 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console/var/www/ajax.py
+++ management/univention-management-console/var/www/ajax.py
@@ -1,7 +1,36 @@
 #!/usr/bin/python2.4
+# -*- coding: utf-8 -*-
 #
+# Univention Management Console
+#  web interface daemon
+#
+# Copyright (C) 2009 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# Binary versions of this file provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 import cgi, re, sys, socket, cPickle, struct
-import univention.debug2 as ud
+import univention.debug as ud
 
 import univention.config_registry
 
