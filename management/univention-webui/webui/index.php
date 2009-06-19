ffe7eff867ab9bd37f69c6a0ce9aaa561babb997
Andreas Büsching <buesching@univention.de>
2009-06-19 14:47:29 +0000

    first step in enhancing the performance of the PHP frontend (Bug #8994)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@10390 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-webui/webui/index.php
+++ management/univention-webui/webui/index.php
@@ -90,90 +90,76 @@ if(is_array($_FILES['userfile'])){
 }
 
 
-if(!isset($logout))
-{
-  if(!isset($session_id))
-    {
+if(!isset($logout))	{
+	$glob_tabindex=1;
+	$container = new webui_container($config, $glob_tabindex);
+	$trans = new webui_translator($container, $config);
+	$parser = new webui_in($config, $trans);
+	if(!isset($session_id)) {
+		#################### For orig. Python Backend   ############
+		// spawn off daemon
+		$fp = popen($config->run." -s -", 'w');
+		fwrite($fp, $config->socket_filename);
+		fclose($fp);
 
-#################### For orig. Python Backend   ############
-  // spawn off daemon
-	$fp = popen($config->run." -s -", 'w');
-	fwrite($fp, $config->socket_filename);
-	fclose($fp);
+		$pipe=fsockopen("unix://".$config->socket_filename, 0);
+        if(isset($module)) {
+			fwrite($pipe, "jumpurl: ".$module."\n");
+		}
+		if(isset($opts)) {
+			fwrite($pipe, "opts: ".$opts."\n");
+		}
+// send a notification when the session was caught by the timeout
+		if ($sessioninvalid == '1') {
+			fwrite($pipe, "Sessioninvalid: 1\n");
+		}
+		fwrite($pipe, "SessionId: ".$config->session_id."\n");
+		fwrite($pipe, "Number: -1\n\n\0");
 
-	$pipe=fsockopen("unix://".$config->socket_filename, 0);
-        if(isset($module)){
-		fwrite($pipe, "jumpurl: ".$module."\n");
-	}
-	if(isset($opts)){
-		fwrite($pipe, "opts: ".$opts."\n");
-	}
-	# send a notification when the session was caught by the timeout
-	if ($sessioninvalid == '1') {
-		fwrite($pipe, "Sessioninvalid: 1\n");
-	}
-	fwrite($pipe, "SessionId: ".$config->session_id."\n");
-	fwrite($pipe, "Number: -1\n\n\0");
-
-	$fp=fopen($config->session_dir.$config->number, "w");
-	while (($buf = fread($pipe, 1024))) {
-		fwrite($fp, $buf);
-		if (strpos($buf, '\0'))
+		$fp=fopen($config->session_dir.$config->number, "w");
+		while (($buf = fread($pipe, 1024))) {
+			fwrite($fp, $buf);
+			$parser->parse( $buf );
+			if (strpos($buf, '\0'))
+				break;
+		}
+		fclose($fp);
+		fclose($pipe);
+		############################################################
+    } if(@current($long_table)) {
+		switch ($long_table[key($long_table)]) {
+		case "<<":
+			$long_table[key($long_table)] = $visible*($previous-2);
 			break;
-	}
-	fclose($fp);
-	fclose($pipe);
-############################################################
+		case ">>":
+			$long_table[key($long_table)] = $visible*($next);
+			break;
+		case "<":
+			$long_table[key($long_table)] = $visible*($current - 2);
+			break;
+		case ">":
+			$long_table[key($long_table)] = $visible*($current);
+			break;
+		default:
+			$long_table[key($long_table)] = ($long_table[key($long_table)]-1)*$visible;
+		} if ($usrinput) {
+			$usrinput[key($long_table)] = $long_table[key($long_table)];
+			$output = new webui_out($config, $usrinput, $session_data, $parser );
+		} else {
+			$output = new webui_out($config, $long_table, $session_data, $parser );
+		}
+    } elseif ($usrinput) {
+		if (isset($current_position))
+			$config->set_position($current_position);
+		$output = new webui_out($config, $usrinput, $session_data, $parser );
     }
-  if(@current($long_table))
-    {
-      switch ($long_table[key($long_table)])
-	{
-	case "<<":
-	  $long_table[key($long_table)] = $visible*($previous-2);
-	  break;
-	case ">>":
-	  $long_table[key($long_table)] = $visible*($next);
-	  break;
-	case "<":
-	  $long_table[key($long_table)] = $visible*($current - 2);
-	  break;
-	case ">":
-	  $long_table[key($long_table)] = $visible*($current);
-	  break;
-	default:
-	  $long_table[key($long_table)] = ($long_table[key($long_table)]-1)*$visible;
-	}
-      if ($usrinput)
-	{
-	  $usrinput[key($long_table)] = $long_table[key($long_table)];
-	  $output = new webui_out($config, $usrinput, $session_data);
-	}
-      else
-	{
-	  $output = new webui_out($config, $long_table, $session_data);
-	}
-
-    }
-  elseif ($usrinput)
-    {
-      if (isset($current_position))
-	$config->set_position($current_position);
-      $output = new webui_out($config, $usrinput, $session_data);
-    }
-  $glob_tabindex=1;
-  $container = new webui_container($config, $glob_tabindex);
-  $trans = new webui_translator($container, $config);
-  $parser = new webui_in($config, $trans);
-  $config->del_old();
-  $container->show();
-}
-else
-{
-  $container = new webui_container($config, $glob_tabindex);
-  #$this->config = $config;
-  $container->config = $config;
-  $container->logout($logout, $session_id);
+	$container->show();
+	$config->del_old();
+} else {
+	$container = new webui_container($config, $glob_tabindex);
+	#$this->config = $config;
+	$container->config = $config;
+	$container->logout($logout, $session_id);
 }
 
 ?>
