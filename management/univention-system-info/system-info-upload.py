c1bd75fa246b3777092a195c52d520d15fe5e7a2
Andreas Büsching <buesching@univention.de>
2009-10-05 11:15:38 +0000

    * system-info-upload: send a mail to umc/sysinfo/upload/address of a new   archive has been uploaded (Bug #8969)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@12683 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-system-info/system-info-upload.py
+++ management/univention-system-info/system-info-upload.py
@@ -31,7 +31,10 @@
 import univention.config_registry as ucr
 
 import cgi
+from email.MIMEText import MIMEText
 import os
+import subprocess
+import smtplib
 import sys
 import time
 
@@ -41,13 +44,13 @@ configRegistry.load()
 def save_uploaded_file( form_field, upload_dir, max_size ):
 	form = cgi.FieldStorage()
 	if not form.has_key( form_field ):
-		return
+		return ( False, None )
 	fileitem = form[ form_field ]
 	if not fileitem.filename.endswith( '.tar.gz' ):
-		return False
+		return ( False, fileitem.filename )
 	filename = os.path.join( upload_dir, fileitem.filename )
 	if os.path.exists( filename ):
-		filename += '%s' % str( time.time() * 100 )
+		filename += '.%s' % str( time.time() * 100 )
 	fout = file( filename , 'wb' )
 	size = 0
 	while True:
@@ -58,17 +61,37 @@ def save_uploaded_file( form_field, upload_dir, max_size ):
 		if size > max_size:
 			fout.close()
 			os.unlink( fout.name )
-			return False
+			return ( False, fileitem.filename )
 		fout.write( chunk )
 	fout.close()
 
-	return True
+	return ( True, fileitem.filename )
 
 # make HTTP happy
 print 'Content-Type: text/plain\n'
 
-if not save_uploaded_file( 'filename', configRegistry.get( 'umc/sysinfo/upload/path', '/var/lib/univention-system-info/archives/' ), configRegistry.get( 'umc/sysinfo/upload/size', '2000000' ) ):
+path = configRegistry.get( 'umc/sysinfo/upload/path', '/var/lib/univention-system-info/archives/' )
+
+ok, filename = save_uploaded_file( 'filename', path, configRegistry.get( 'umc/sysinfo/upload/size', '2000000' ) )
+
+if not ok:
 	print 'ERROR: wrong file type or file to big'
 else:
+	msg = MIMEText('''
+A new Univention system info archive has been uploaded.
+
+Archive: %s
+''' % os.path.join( path, filename ) )
+	msg[ 'Subject' ] = 'Univention System Info Upload'
+	sender = 'root'
+	recipient = configRegistry.get( 'umc/sysinfo/upload/address', sender )
+	
+	msg[ 'From' ] = sender
+	msg[ 'To' ] = recipient
+	
+	s = smtplib.SMTP()
+	s.connect()
+	s.sendmail( sender, [ recipient ], msg.as_string() )
+	s.close()
 	print 'OK: file saved successfully'
 
