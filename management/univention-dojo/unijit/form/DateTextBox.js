88eb8e7613bcdf04ea0ee719331a2bc8c967ea7f
Andreas Büsching <buesching@univention.de>
2009-10-20 13:52:59 +0000

    add calendar icon to DateTextBox widget (Bug #12039)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@13094 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ management/univention-dojo/unijit/form/DateTextBox.js
@@ -0,0 +1,8 @@
+dojo.provide("unijit.form.DateTextBox");
+dojo.require("dijit.form.DateTextBox");
+dojo.declare('unijit.form.DateTextBox', dijit.form.DateTextBox,
+{
+templatePath : dojo.moduleUrl("unijit", "form/template/DateTextBox.html"),
+templateString : ''
+});
+
