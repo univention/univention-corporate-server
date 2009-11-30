9b375d3359b4f47fc53afd149cbe30f74a5e6f77
Andreas Büsching <buesching@univention.de>
2009-11-30 15:46:06 +0000

    75_join.sh: fixed typo (Bug #16318)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@14020 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-installer/scripts/75_join.sh
+++ base/univention-installer/scripts/75_join.sh
@@ -46,7 +46,7 @@ if [ -d /var/lib/univention-ldap/ldap ]; then
 fi
 
 if [ "$server_role" != "domaincontroller_master" ] && [ -n "$domain_controller_account" -a -n "$domain_controller_password" ]; then
-	if [ -n "$interfaces_eth0_type" ] && [ "$interfaces_eth0_type" = "dynamic" -o "$interfaces_eth0_type" = "dhcp" ]; then
+	if [ -n "$eth0_type" ] && [ "$eth0_type" = "dynamic" -o "$eth0_type" = "dhcp" ]; then
 		dhclient eth0
 	fi
 	if [ -z "$auto_join" ] || [ "$auto_join" != "FALSE" -a "$auto_join" != "false" -a "$auto_join" != "False" ]; then
