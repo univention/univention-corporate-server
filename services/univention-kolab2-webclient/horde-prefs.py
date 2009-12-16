a1ddd5b2553477a14fec293bf10b191b56cb7176
Daniel Hofmann <hofmann@univention.de>
2009-12-16 15:25:19 +0000

    * avoid exceptions in horde-prefs.py by checking, whether db-entries already exist (Bug #16787)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-2.3/ucs@14248 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-kolab2-webclient/horde-prefs.py
+++ services/univention-kolab2-webclient/horde-prefs.py
@@ -44,61 +44,65 @@ def __create_db_identity( mail, fullname ):
 	return 'a:1:{i:0;a:4:{s:2:\"id\";s:%d:\"%s\";s:8:\"fullname\";s:%d:\"%s\";s:9:\"from_addr\";s:%d:\"%s\";s:16:\"default_identity\";s:1:\"1\";}}' % \
 			( len( name ), name, len( fullname ), fullname, len( mail ), mail )
 
+def __tuples_exist ( db, mail, scope ):
+	return 0 < db.query( "select * from horde_prefs where pref_uid='%s' and pref_scope='%s';" % (mail, scope) ).ntuples()
 
 def __turba_settings ( db, mail ):
-	db.query( "insert into horde_prefs values('%s', 'turba', 'default_dir', '%s');" % (mail, mail))
+	if not __tuples_exist ( db, mail, 'turba' ):
+		db.query( "insert into horde_prefs values('%s', 'turba', 'default_dir', '%s');" % (mail, mail))
 
 def __kronolith_settings ( db, mail ):
-	db.query( "insert into horde_prefs values('%s', 'kronolith', 'show_shared_side_by_side', '0');" % mail)
-	db.query( "insert into horde_prefs values('%s', 'kronolith', 'show_fb_legend', '1');" % mail)
-	db.query( "insert into horde_prefs values('%s', 'kronolith', 'show_panel', '1');" % mail)
-	db.query( "insert into horde_prefs values('%s', 'kronolith', 'show_legend', '1');" % mail)
-	db.query( "insert into horde_prefs values('%s', 'kronolith', 'show_icons', '1');" % mail)
-	db.query( "insert into horde_prefs values('%s', 'kronolith', 'slots_per_hour', '1');" % mail)
-	db.query( "insert into horde_prefs values('%s', 'kronolith', 'day_hour_force', '0');" % mail)
-	db.query( "insert into horde_prefs values('%s', 'kronolith', 'day_hour_end', '44');" % mail)
-	db.query( "insert into horde_prefs values('%s', 'kronolith', 'day_hour_start', '16');" % mail)
-	db.query( "insert into horde_prefs values('%s', 'kronolith', 'week_start_monday', '1');" % mail)
-	db.query( "insert into horde_prefs values('%s', 'kronolith', 'time_between_days', '0');" % mail)
-	db.query( "insert into horde_prefs values('%s', 'kronolith', 'defaultview', 'week');" % mail)
-	db.query( "insert into horde_prefs values('%s', 'kronolith', 'confirm_delete', '1');" % mail)
-	db.query( "insert into horde_prefs values('%s', 'kronolith', 'default_share', '%s');" % (mail, mail))
+	if not __tuples_exist ( db, mail, 'kronolith' ):
+		db.query( "insert into horde_prefs values('%s', 'kronolith', 'show_shared_side_by_side', '0');" % mail)
+		db.query( "insert into horde_prefs values('%s', 'kronolith', 'show_fb_legend', '1');" % mail)
+		db.query( "insert into horde_prefs values('%s', 'kronolith', 'show_panel', '1');" % mail)
+		db.query( "insert into horde_prefs values('%s', 'kronolith', 'show_legend', '1');" % mail)
+		db.query( "insert into horde_prefs values('%s', 'kronolith', 'show_icons', '1');" % mail)
+		db.query( "insert into horde_prefs values('%s', 'kronolith', 'slots_per_hour', '1');" % mail)
+		db.query( "insert into horde_prefs values('%s', 'kronolith', 'day_hour_force', '0');" % mail)
+		db.query( "insert into horde_prefs values('%s', 'kronolith', 'day_hour_end', '44');" % mail)
+		db.query( "insert into horde_prefs values('%s', 'kronolith', 'day_hour_start', '16');" % mail)
+		db.query( "insert into horde_prefs values('%s', 'kronolith', 'week_start_monday', '1');" % mail)
+		db.query( "insert into horde_prefs values('%s', 'kronolith', 'time_between_days', '0');" % mail)
+		db.query( "insert into horde_prefs values('%s', 'kronolith', 'defaultview', 'week');" % mail)
+		db.query( "insert into horde_prefs values('%s', 'kronolith', 'confirm_delete', '1');" % mail)
+		db.query( "insert into horde_prefs values('%s', 'kronolith', 'default_share', '%s');" % (mail, mail))
 
 def __horde_settings ( db, mail, fullname ):
-	baseConfig = univention_baseconfig.baseConfig()
-	baseConfig.load()
+	if not __tuples_exist ( db, mail, 'horde' ):
+		baseConfig = univention_baseconfig.baseConfig()
+		baseConfig.load()
 
-	menu_width = baseConfig.get('horde/menu/width', '200')
-	categories = {}
-	for k in baseConfig.keys():
-		if k.startswith('horde/calendar/category/'):
-			categories[k.split('/')[-1]] = '#%s' % baseConfig[k]
+		menu_width = baseConfig.get('horde/menu/width', '200')
+		categories = {}
+		for k in baseConfig.keys():
+			if k.startswith('horde/calendar/category/'):
+				categories[k.split('/')[-1]] = '#%s' % baseConfig[k]
 
-	db.query( "insert into horde_prefs values('%s','horde', 'identities','%s');" % (mail, __create_db_identity( mail, fullname )) )
-	db.query( "insert into horde_prefs values('%s', 'horde', 'confirm_maintenance', '0');" % mail )
-	db.query( "insert into horde_prefs values('%s', 'horde', 'do_maintenance', '0');" % mail )
-	db.query( "insert into horde_prefs values('%s', 'horde', 'show_last_login', '1');" % mail )
-	db.query( "insert into horde_prefs values('%s', 'horde', 'widget_accesskey', '1');" % mail )
-	db.query( "insert into horde_prefs values('%s', 'horde', 'initial_application', 'horde');" % mail )
-	db.query( "insert into horde_prefs values('%s', 'horde', 'menu_refresh_time', '300');" % mail )
-	db.query( "insert into horde_prefs values('%s', 'horde', 'menu_view', 'both');" % mail )
-	db.query( "insert into horde_prefs values('%s', 'horde', 'sidebar_width', '%s');" % (mail, menu_width ))
-	db.query( "insert into horde_prefs values('%s', 'horde', 'show_sidebar', '1');" % mail )
-	db.query( "insert into horde_prefs values('%s', 'horde', 'summary_refresh_time', '300');" % mail )
-	db.query( "insert into horde_prefs values('%s', 'horde', 'theme', 'univention');" % mail )
-	db.query( "insert into horde_prefs values('%s', 'horde', 'date_format', '%%A, %%d. %%B %%Y');" % mail )
-	db.query( "insert into horde_prefs values('%s', 'horde', 'twentyFour', '1');" % mail )
-	db.query( "insert into horde_prefs values('%s', 'horde', 'timezone', '0');" % mail )
-	db.query( "insert into horde_prefs values('%s', 'horde', 'language', '0');" % mail )
+		db.query( "insert into horde_prefs values('%s','horde', 'identities','%s');" % (mail, __create_db_identity( mail, fullname )) )
+		db.query( "insert into horde_prefs values('%s', 'horde', 'confirm_maintenance', '0');" % mail )
+		db.query( "insert into horde_prefs values('%s', 'horde', 'do_maintenance', '0');" % mail )
+		db.query( "insert into horde_prefs values('%s', 'horde', 'show_last_login', '1');" % mail )
+		db.query( "insert into horde_prefs values('%s', 'horde', 'widget_accesskey', '1');" % mail )
+		db.query( "insert into horde_prefs values('%s', 'horde', 'initial_application', 'horde');" % mail )
+		db.query( "insert into horde_prefs values('%s', 'horde', 'menu_refresh_time', '300');" % mail )
+		db.query( "insert into horde_prefs values('%s', 'horde', 'menu_view', 'both');" % mail )
+		db.query( "insert into horde_prefs values('%s', 'horde', 'sidebar_width', '%s');" % (mail, menu_width ))
+		db.query( "insert into horde_prefs values('%s', 'horde', 'show_sidebar', '1');" % mail )
+		db.query( "insert into horde_prefs values('%s', 'horde', 'summary_refresh_time', '300');" % mail )
+		db.query( "insert into horde_prefs values('%s', 'horde', 'theme', 'univention');" % mail )
+		db.query( "insert into horde_prefs values('%s', 'horde', 'date_format', '%%A, %%d. %%B %%Y');" % mail )
+		db.query( "insert into horde_prefs values('%s', 'horde', 'twentyFour', '1');" % mail )
+		db.query( "insert into horde_prefs values('%s', 'horde', 'timezone', '0');" % mail )
+		db.query( "insert into horde_prefs values('%s', 'horde', 'language', '0');" % mail )
 
-	if len(categories) > 0:
-		db.query( "insert into horde_prefs values('%s', 'horde', 'categories', '%s');" % (mail,string.join(categories.keys(),'|') ) )
-		category_colors = '_default_:#FFFFFF|_unfiled_:#DDDDDD'
-		for k in categories.keys():
-			category_colors = '%s:%s|%s' % (k, categories[k],category_colors)
-		db.query( "insert into horde_prefs values('%s', 'horde', 'category_colors', '%s');" % (mail,category_colors))
-		#category_colors          | bla:#FFFFFF|foobar:#112233|_default_:#FFFFFF|_unfiled_:#DDDDDD
-		
+		if len(categories) > 0:
+			db.query( "insert into horde_prefs values('%s', 'horde', 'categories', '%s');" % (mail,string.join(categories.keys(),'|') ) )
+			category_colors = '_default_:#FFFFFF|_unfiled_:#DDDDDD'
+			for k in categories.keys():
+				category_colors = '%s:%s|%s' % (k, categories[k],category_colors)
+			db.query( "insert into horde_prefs values('%s', 'horde', 'category_colors', '%s');" % (mail,category_colors))
+			#category_colors          | bla:#FFFFFF|foobar:#112233|_default_:#FFFFFF|_unfiled_:#DDDDDD
 
 def handler(dn, new, old):
 	if new and new.has_key( 'mailPrimaryAddress' ):
@@ -108,12 +112,12 @@ def handler(dn, new, old):
 			password = secret.readlines()[ 0 ][ : -1 ]
 			secret.close()
 			db = pg.connect( dbname = 'horde', user = 'horde', passwd = password )
-
-			__horde_settings( db, mail = new[ 'mailPrimaryAddress' ][0], fullname = new[ 'displayName' ][0] )
-			__kronolith_settings( db, mail = new[ 'mailPrimaryAddress' ][0] )
-			__turba_settings( db, mail = new[ 'mailPrimaryAddress' ][0] )
-
-			db.close( )
+			try:
+				__horde_settings( db, mail = new['mailPrimaryAddress'][0], fullname = new[ 'displayName' ][0] )
+				__kronolith_settings( db, mail = new['mailPrimaryAddress'][0] )
+				__turba_settings( db, mail = new['mailPrimaryAddress'][0] )
+			finally:
+				db.close()
 		finally:
 			listener.unsetuid()
 
