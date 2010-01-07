82b20dfc4235a67948f465a7ad75c064710e41ad
Felix Botner <botner@univention.de>
2010-01-07 14:04:37 +0000

    updated copyright Bug #17222
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-2.3/ucs@14673 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-printserver/modules/univention/management/console/handlers/cups/_job.py
+++ services/univention-printserver/modules/univention/management/console/handlers/cups/_job.py
@@ -4,7 +4,7 @@
 # Univention Management Console
 #  cups module: helper functions for print job management
 #
-# Copyright (C) 2006-2009 Univention GmbH
+# Copyright (C) 2006-2010 Univention GmbH
 #
 # http://www.univention.de/
 #
