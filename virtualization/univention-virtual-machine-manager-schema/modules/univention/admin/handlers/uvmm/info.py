50690cc34f88ef713491ed05baccca562446231d
Stefan Gohmann <gohmann@univention.de>
2010-05-25 12:09:13 +0000

    added a UDM module for setting the OS for a virtual machine. Bug #18160
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@17181 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ virtualization/univention-virtual-machine-manager-schema/modules/univention/admin/handlers/uvmm/info.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python2.4
+# -*- coding: utf-8 -*-
+#
+# Univention Virtual Machine Manager
+#  UDM Virtual Machine Manager Information
+#
+# Copyright (C) 2010 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# Binary versions of this file provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import re, sys, string
+import univention.admin.filter
+import univention.admin.handlers
+import univention.admin.syntax
+import univention.admin.localization
+import univention.admin.uexceptions
+
+translation=univention.admin.localization.translation('univention.admin.handlers.uvmm')
+_=translation.translate
+
+module = 'uvmm/info'
+
+childs = 0
+short_description = _('Virtual machine information')
+long_description = ''
+operations = [ 'search', 'edit', 'add', 'remove' ]
+
+property_descriptions={
+	'uuid': univention.admin.property(
+			short_description= _('UUID'),
+			long_description= _('UUID'),
+			syntax=univention.admin.syntax.string,
+			multivalue=0,
+			options=[],
+			required=1,
+			may_change=1,
+			identifies=1
+		),
+	'description': univention.admin.property(
+			short_description= _('Description'),
+			long_description= _('Description of virtual machine'),
+			syntax=univention.admin.syntax.string,
+			multivalue=0,
+			options=[],
+			required=0,
+			may_change=1,
+			identifies=0
+		),
+	'os': univention.admin.property(
+			short_description= _('Operating system'),
+			long_description= _('Name of the operation system'),
+			syntax=univention.admin.syntax.string,
+			multivalue=0,
+			options=[],
+			required=0,
+			may_change=1,
+			identifies=0
+		),
+}
+
+
+layout=[
+	univention.admin.tab( _('General'), _('Virtual machine information'),
+	      [
+			[ univention.admin.field( "uuid" ), univention.admin.field( "description" ) ],
+			[ univention.admin.field( "os" ) ],
+		  ] )
+	]
+
+
+
+mapping=univention.admin.mapping.mapping()
+mapping.register('uuid', 'univentionVirtualMachineUUID', None, univention.admin.mapping.ListToString)
+mapping.register('description', 'description', None, univention.admin.mapping.ListToString)
+mapping.register('os', 'univentionVirtualMachineOS', None, univention.admin.mapping.ListToString)
+
+
+class object(univention.admin.handlers.simpleLdap):
+	module=module
+
+	def __init__(self, co, lo, position, dn='', superordinate=None, arg=None):
+		global mapping
+		global property_descriptions
+
+		self.co=co
+		self.lo=lo
+		self.dn=dn
+		self.position=position
+		self._exists=0
+		self.mapping=mapping
+		self.descriptions=property_descriptions
+
+		univention.admin.handlers.simpleLdap.__init__(self, co, lo, position, dn, superordinate)
+
+	def exists(self):
+		return self._exists
+
+	def _ldap_pre_create(self):
+		self.dn='%s=%s,%s' % (mapping.mapName('uuid'), mapping.mapValue('uuid', self.info['uuid']), self.position.getDn())
+
+	def _ldap_addlist(self):
+		return [ ('objectClass', [ 'univentionVirtualMachine' ] ) ]
+
+def lookup(co, lo, filter_s, base='', superordinate=None, scope='sub', unique=0, required=0, timeout=-1, sizelimit=0):
+	filter=univention.admin.filter.conjunction('&', [
+				univention.admin.filter.expression('objectClass', 'univentionVirtualMachine'),
+				])
+
+	if filter_s:
+		filter_p=univention.admin.filter.parse(filter_s)
+		univention.admin.filter.walk(filter_p, univention.admin.mapping.mapRewrite, arg=mapping)
+		filter.expressions.append(filter_p)
+
+	res=[]
+	for dn in lo.searchDn(unicode(filter), base, scope, unique, required, timeout, sizelimit):
+		res.append(object(co, lo, None, dn))
+	return res
+
+
+def identify(dn, attr, canonical=0):
+	return 'univentionVirtualMachine' in attr.get('objectClass', [])
