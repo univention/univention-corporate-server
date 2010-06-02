281feac33b39836f02288442461bae1e3930a328
Andreas Büsching <buesching@univention.de>
2010-06-02 09:04:30 +0000

    basic functionality for UMC modul; Bug #18531
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@17406 1b283449-9f2f-0410-b571-8eb228e1a901

--- virtualization/univention-opendvdi-sessionbroker/umc/modules/univention/management/console/handlers/opendvdi/__init__.py
+++ virtualization/univention-opendvdi-sessionbroker/umc/modules/univention/management/console/handlers/opendvdi/__init__.py
@@ -44,23 +44,66 @@ import socket
 
 import notifier.popen
 
+from uvmmd import *
+from broker import *
+
 _ = umc.Translation('univention.management.console.handlers.opendvdi').translate
 
 name = 'opendvdi'
 icon = 'opendvdi/module'
 short_description = _('OpenDVDI')
-long_description = _('Management of OpenDVDI instances')
+long_description = _('Management of OpenDVDI connections')
 categories = [ 'system', 'all' ]
 # hide_tabs = True
 
+class SearchKeys( umc.StaticSelection ):
+	def __init__( self, required = True ):
+		umc.StaticSelection.__init__( self, unicode( _( 'Search key' ) ), required = required )
+
+	def choices( self ):
+		return ( ( 'username', _( 'Username' ) ), ( 'client', _( 'Client' ) ) )
+
+umcd.copy( umc.StaticSelection, SearchKeys )
 
 command_description = {
-	'opendvdi/overview': umch.command(
-		short_description = _('Overview'),
-		long_description = _('Overview'),
-		method = 'uvmm_overview',
-		values = {},
+	'opendvdi/session/search': umch.command(
+		short_description = _('Sessions'),
+		long_description = _('Sessions'),
+		method = 'uvmm_session_search',
+		values = {
+			'key' : SearchKeys(),
+			'filter' : umcd.String( _( 'Filter' ) ),
+			},
 		startup = True,
+		),
+	'opendvdi/instance/search': umch.command(
+		short_description = _('Templates'),
+		long_description = _('Templates'),
+		method = 'uvmm_instance_search',
+		values = {
+			'instance' : umcd.String( _( 'Virtual instance' ) ),
+			},
+		startup = True,
+		),
+	'opendvdi/session/view': umch.command(
+		short_description = _('Session details'),
+		long_description = _('Session details'),
+		method = 'uvmm_session_view',
+		values = {
+			'session' : umcd.String( _( 'Session ID' ) ),
+			},
+		),
+	'opendvdi/template/create': umch.command(
+		short_description = _('Create Template'),
+		long_description = _('Create Template'),
+		method = 'uvmm_template_create',
+		values = {
+			'username' : umcd.String( _( 'Name' ) ),
+			'creation_time' : umcd.String( _( 'Creation time' ) ),
+			'name' : umcd.String( _( 'Name' ) ),
+			'os' : umcd.String( _( 'Operating System' ) ),
+			'description' : umcd.String( _( 'Description' ) ),
+			},
 		),
 }
 
@@ -68,8 +111,70 @@ class handler( umch.simpleHandler ):
 	def __init__( self ):
 		global command_description
 		umch.simpleHandler.__init__( self, command_description )
-		# self.uvmm = uvmmd.Client( auto_connect = False )
+		self.broker = SessionBroker()
+		self.uvmm = UVMM_Client( auto_connect = False )
 
-	def opendvdi_overview( self, object ):
+	def opendvdi_session_search( self, object ):
+		res = umcp.Response( object )
+
+		select = umcd.make( self[ 'opendvdi/session/search' ][ 'key' ], default = 'username', attributes = { 'width' : '200' } )
+		text = umcd.make( self[ 'opendvdi/session/search' ][ 'filter' ], default = object.options.get( 'filter', '*' ), attributes = { 'width' : '250' } )
+		form = umcd.SearchForm( 'opendvdi/session/search', [ [ ( select, 'username' ), ( text, '*' ) ] ] )
+
+		if object.incomplete:
+			res.dialog = form
+			self.finished( object.id(), res )
+			return
+
+		# FIXME: retrieve list from session broker
+		key = object.options[ 'key' ]
+		filter = object.options[ 'filter' ]
+		if key == 'username':
+			sessions = self.broker.get_sessions( username = filter )
+		else:
+			sessions = self.broker.get_sessions( client = filter )
+
+		result = umcd.List()
+		result.set_header( [ _( 'User' ), _( 'Client' ), _( 'Server' ), _( 'Status' ) ] )
+		for session in sessions:
+			result.add_row( [] )
+		
+		res.dialog = [ form, result ]
+		self.finished( object.id(), res )
+
+	def opendvdi_session_view( self, object ):
+		res = umcp.Response( object )
+		info = self.broker.get_session( object.options[ 'id' ] )
+
+		lst = umcd.List()
+		lst.add_row( [ _( 'Username' ), '' ] )
+		lst.add_row( [ _( 'Client' ), '' ] )
+		lst.add_row( [ _( 'Server' ), '' ] )
+		session = umcd.List()
+		session.add_row( [ _( 'Status' ), '' ] )
+		session.add_row( [ _( 'Type' ), '' ] )
+		session.add_row( [ _( 'Started' ), '' ] )
+		session.add_row( [ _( 'Virtual instance' ), '' ] )
+		session.add_row( [ _( 'Update intervall' ), '' ] )
+		session.add_row( [ _( 'Last update' ), '' ] )
+
+		res.dialog = [ lst, umcd.Section( _( 'Session' ), session, hidable = False ) ]
+		self.finished( object.id(), res )
+
+	def opendvdi_instance_search( self, object ):
+		res = umcp.Response( object )
+		text = umcd.make( self[ 'opendvdi/instance/search' ][ 'filter' ], default = object.options.get( 'filter', '*' ), attributes = { 'width' : '250' } )
+		form = umcd.SearchForm( 'opendvdi/instance/search', [ [ ( text, '*' ) ] ] )
+
+		if object.incomplete:
+			res.dialog = form
+			self.finished( object.id(), res )
+			return
+
+		
+		self.finished( object.id(), res )
+
+	def opendvdi_template_create( self, object ):
+		res = umcp.Response( object )
 		self.finished( object.id(), res )
 
