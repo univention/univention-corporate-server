387dad4c902ad4c4fb76b5d2f3e32eb1511da183
Arvid Requate <requate@univention.de>
2010-06-15 13:26:59 +0000

    * remove ucs_2.3-0 package, to be repalced by the new version     from svn/dev/branches/ucs-2.3/component/horde-experimental 	(Bug #18667)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@17694 1b283449-9f2f-0410-b571-8eb228e1a901

--- kolab2/univention-kolab2-webclient/scripts/create.database_and_user.sql
+++
@@ -1,9 +0,0 @@
--- $ psql -d template1 -f create.database_and_user.sql
-
-CREATE DATABASE horde;
-
-CREATE USER horde;
--- ALTER USER horde WITH PASSWORD 'pass';
-
-GRANT CREATE on DATABASE horde to horde;
-
