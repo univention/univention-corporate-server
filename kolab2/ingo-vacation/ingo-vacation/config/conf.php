c3f16acba5cc8fd9eeaa431f800eff3c62b76e5e
Arvid Requate <requate@univention.de>
2010-06-15 13:51:29 +0000

    * packages moved to new location (Bug #18667)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@17698 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ kolab2/ingo-vacation/ingo-vacation/config/conf.php
@@ -0,0 +1,10 @@
+<?php
+$conf['menu']['apps'] = array();
+$conf['storage']['driver'] = 'prefs';
+$conf['storage']['maxblacklist'] = 0;
+$conf['storage']['maxwhitelist'] = 0;
+$conf['rules']['userheader'] = true;
+$conf['rules']['usefolderapi'] = true;
+$conf['hooks']['vacation_addresses'] = false;
+$conf['hooks']['vacation_only'] = true;
+
---
+++ kolab2/ingo-vacation/ingo-vacation/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * $Horde: ingo/index.php,v 1.14.10.6 2008/01/02 11:32:08 jan Exp $
+ *
+ * Copyright 2002-2008 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (ASL).  If you
+ * did not receive this file, see http://www.horde.org/licenses/asl.php.
+ *
+ * @author Mike Cochrane <mike@graftonhall.co.nz>
+ */
+
+@define('INGOVACATION_BASE', dirname(__FILE__));
+
+require INGOVACATION_BASE . '/vacation.php';
