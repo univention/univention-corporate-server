3d0e2cc1c6d3576308864231a6c6fd13efca57f7
Janek Walkenhorst <walkenhorst@univention.de>
2010-06-23 12:03:31 +0000

    univention-fetchmail (4.0.0-1) unstable; urgency=low
      * convert to GNU APGL V3 (Bug #17543)
      * bump version for UCS 2.4
    
    
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@17956 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-fetchmail/fetchmailExtension.py
+++ services/univention-fetchmail/fetchmailExtension.py
@@ -1,28 +1,29 @@
 #
-# Copyright (C) 2004-2010 Univention GmbH
+# Copyright 2004-2010 Univention GmbH
 #
 # http://www.univention.de/
 #
 # All rights reserved.
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 as
-# published by the Free Software Foundation.
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
 #
-# Binary versions of this file provided by Univention to you as
+# Binary versions of this program provided by Univention to you as
 # well as other copyrighted, protected or trademarked materials like
 # Logos, graphics, fonts, specific documentations and configurations,
 # cryptographic keys etc. are subject to a license agreement between
 # you and Univention.
 #
-# This program is distributed in the hope that it will be useful,
+# This program is provided in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# should
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
 #
 name = 'fetchmailExtension'
 description = 'write user-configuration to fetchmailrc'
