bdf2ccedd2f02294414b72e3834c5837b8a2d09e
Felix Botner <botner@univention.de>
2010-08-20 08:49:34 +0000

    redirect stderr to stdout in /etc/cron.d/univention-ssl Bug #19538
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@19372 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-ssl/debian/cron.d
+++ base/univention-ssl/debian/cron.d
@@ -1,4 +1,4 @@
 SHELL=/bin/sh
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
-*/20 * * * * root /usr/share/univention-ssl/ssl-sync >>/var/log/univention/ssl-sync.log
+*/20 * * * * root /usr/share/univention-ssl/ssl-sync >>/var/log/univention/ssl-sync.log 2>&1
