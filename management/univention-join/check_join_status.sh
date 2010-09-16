b0b68ebed24c9fc0e48a48b75cfbc7afbccb244a
Philipp Hahn <hahn@univention.de>
2010-09-16 07:26:44 +0000

    Improve check for join status (Bug #19361,#13495,#13497,#18120)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@19719 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-join/check_join_status.sh
+++ management/univention-join/check_join_status.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Univention Join
 #  helper script: checks the join status of the local system
@@ -32,55 +32,66 @@
 
 LOG_FILE=/var/log/univention/check_join_status.log
 
-log_error ()
-{
-	local message="Error: $1"
+log_error () { # Log error message and exit
+	local message="Error: $@"
 	echo $message
-	echo $message >> $LOG_FILE
+	echo $message >>"$LOG_FILE"
 	exit 1
 }
-log_warn ()
-{
-	local message="Warning: $1"
+log_warn () { # Log warning message
+	local message="Warning: $@"
 	echo $message
-	echo $message >> $LOG_FILE
+	echo $message >>"$LOG_FILE"
 }
 
-echo "Start $0 at $(date)" >>$LOG_FILE
-eval `univention-config-registry shell`
+echo "Start $0 at $(date)" >>"$LOG_FILE"
+eval "$(univention-config-registry shell)"
 
 if [ ! -e /etc/machine.secret ]; then
 	log_error "/etc/machine.secret not found"
 fi
 
-ldapsearch -x -h "$ldap_master" -D "$ldap_hostdn" -w `cat /etc/machine.secret` -b $ldap_base -s base >>$LOG_FILE 2>&1
-if [ $? != 0 ]; then
+if ! ldapsearch -x -h "$ldap_master" -D "$ldap_hostdn" -w "$(</etc/machine.secret)" -b "$ldap_base" -s base >>"$LOG_FILE" 2>&1
+then
 	log_error "ldapsearch -x failed"
 fi
 
 
-ldapsearch -x -ZZ -h "$ldap_master" -D "$ldap_hostdn" -w `cat /etc/machine.secret` -b $ldap_base -s base >>$LOG_FILE 2>&1
-if [ $? != 0 ]; then
+if ! ldapsearch -x -ZZ -h "$ldap_master" -D "$ldap_hostdn" -w "$(</etc/machine.secret)" -b "$ldap_base" -s base >>"$LOG_FILE" 2>&1
+then
 	log_error "ldapsearch -x -ZZ failed"
 fi
 
-if [ ! -e /var/univention-join/joined ]; then
+if [ ! -e /var/univention-join/joined ]
+then
 	log_error "The system isn't joined yet"
 fi
 
-ldapsearch -x -ZZ -D "$ldap_hostdn" -w `cat /etc/machine.secret` -b $ldap_base -s base >>$LOG_FILE 2>&1
-if [ $? != 0 ]; then
+if ! ldapsearch -x -ZZ -D "$ldap_hostdn" -w "$(</etc/machine.secret)" -b "$ldap_base" -s base >>"$LOG_FILE" 2>&1
+then
 	log_error "localhost ldapsearch failed"
 fi
 
-inst_files=`ls -l /usr/lib/univention-install/*.inst | wc -l`
-configured=`wc -l /usr/lib/univention-install/.index.txt | awk '{print $1}'`
+declare -i MISSING=0
+for i in /usr/lib/univention-install/*.inst
+do
+	unset VERSION
+	eval "$(grep -h ^VERSION= "$i")"
+	n="${i##*/[0-9][0-9]}"
+	n="${n%.inst}"
+	if ! grep -Fxq "$n v${VERSION} successful" /var/univention-join/status
+	then
+		log_warn "'$n' is not configured."
+		MISSING+=1
+	fi
+done
 
-if [ $configured -lt $inst_files ]; then
-	log_error "Not all install files configured"
+if [ 0 -ne "$MISSING" ]
+then
+	log_error "Not all install files configured: $MISSING missing"
 fi
 
 echo "Joined successful"
-echo "Joined successfully" >> $LOG_FILE
+echo "Joined successfully" >>"$LOG_FILE"
 
 exit 0
