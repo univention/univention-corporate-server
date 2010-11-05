f14724f1b92d04b3c5b2478d391f5c184e3d509a
Sönke Schwardt-Krummrich <schwardt@univention.de>
2010-11-05 10:35:11 +0000

    * fixed wrong UCR comparing values for true and false (Bug #20186)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@20704 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-directory-manager/uniconf/modedit.py
+++ management/univention-directory-manager/uniconf/modedit.py
@@ -4322,8 +4322,8 @@ class modedit(unimodule.unimodule):
 								filter=fixedFilter
 
 						# display default search results
-						_off = ('0', 'false', 'off', 'yes')
-						_on = ('1', 'true', 'on', 'no')
+						_off = ('0', 'false', 'off', 'no')
+						_on = ('1', 'true', 'on', 'yes')
 						autosearch = True
 						if ucr.get ('directory/manager/web/modules/autosearch', '0').lower () in _on:
 							# default value MUST be 1 because autosearch is already switched on
