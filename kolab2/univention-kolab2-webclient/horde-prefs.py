79b523c6e6e93895daadeacf6e4725261d23ae5b
Arvid Requate <requate@univention.de>
2010-11-15 15:37:16 +0000

    univention-kolab2-webclient (6.0.3-9) unstable; urgency=low
      * update the from_addr if the user object changed (Bug #20554)
    
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@20925 1b283449-9f2f-0410-b571-8eb228e1a901

--- kolab2/univention-kolab2-webclient/horde-prefs.py
+++ kolab2/univention-kolab2-webclient/horde-prefs.py
@@ -123,8 +123,11 @@ def __horde_settings ( db, mail, fullname, from_addr ):
 				category_colors = '%s:%s|%s' % (k, categories[k],category_colors)
 			db.query( "insert into horde_prefs values('%s', 'horde', 'category_colors', '%s');" % (mail,category_colors))
 			#category_colors          | bla:#FFFFFF|foobar:#112233|_default_:#FFFFFF|_unfiled_:#DDDDDD
-	elif __triple_exist(db, mail, 'horde', 'identities') and not __quadruple_exist(db, mail, 'horde', 'identities', __create_db_identity( mail, fullname, from_addr )):
-		db.query( "update horde_prefs set pref_value='%s' where pref_uid='%s' and pref_scope='%s' and pref_name='%s';" % (__create_db_identity( mail, fullname, from_addr ), mail, 'horde', 'identities'))
+	else:
+		if __triple_exist(db, mail, 'horde', 'identities') and not __quadruple_exist(db, mail, 'horde', 'identities', __create_db_identity( mail, fullname, from_addr )):
+			db.query( "update horde_prefs set pref_value='%s' where pref_uid='%s' and pref_scope='%s' and pref_name='%s';" % (__create_db_identity( mail, fullname, from_addr ), mail, 'horde', 'identities'))
+		if __triple_exist(db, mail, 'horde', 'from_addr'):
+			db.query( "update horde_prefs set pref_value='%s' where pref_uid='%s' and pref_scope='%s' and pref_name='%s';" % (from_addr, mail, 'horde', 'from_addr'))
 
 def handler(dn, new, old):
 	if new and new.has_key( 'mailPrimaryAddress' ):
