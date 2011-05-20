e68cd8ed0758e968edc260e608b4b6b9acdc12f4
Felix Botner <botner@univention.de>
2011-05-20 13:37:25 +0000

    * UCS 3.0-0 release Bug #22575 * updated python2.4 to pyton2.6 * fixed copyright header * reorganized debian dir
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@24369 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-printquota/tests/druckertest.sh
+++ services/univention-printquota/tests/druckertest.sh
@@ -3,7 +3,7 @@
 # Univention Print Quota
 #  test script
 #
-# Copyright 2004-2010 Univention GmbH
+# Copyright 2004-2011 Univention GmbH
 #
 # http://www.univention.de/
 #
@@ -51,36 +51,36 @@ if [  "$1" != "start" ]; then
 	echo "--> Please enter password for 1st host"
 	ssh $1 /tmp/druckertest.sh start $remotehost $remoteip
 else
-	eval `univention-baseconfig shell`
+	eval "$(univention-config-registry shell)"
 	remotehost=$2
 	remoteip=$3
 
 	# eine Rundablage
 
-	univention-admin shares/printer create --set name="rundablage" \
+	univention-directory-manager shares/printer create --set name="rundablage" \
 	--set uri="parallel:///dev/lp0" --set spoolHost=$remotehost \
 	--set model=None --position="cn=printers,$ldap_base"
 
 	# Benutzer, Benutzergruppen
 
 	for num in `seq 1 2`; do
-		univention-admin users/user create --set username="dtestuser$num" --set \
+		univention-directory-manager users/user create --set username="dtestuser$num" --set \
 		lastname="dtestuser$num" --set password="univention" --position="cn=users,$ldap_base"
 	done
-	univention-admin groups/group create --set name="dtestgroup" \
+	univention-directory-manager groups/group create --set name="dtestgroup" \
 	--position="cn=groups,$ldap_base" --append users="uid=dtestuser1,cn=users,$ldap_base" \
 	--append users="uid=dtestuser2,cn=users,$ldap_base"
 
 	# Drucker, Druckergruppen
 
 	for num in `seq 1 8`; do
-		univention-admin shares/printer create --set name="dtestprinter$num" \
+		univention-directory-manager shares/printer create --set name="dtestprinter$num" \
 		--set uri="ipp://$remoteip/printers/rundablage" --set spoolHost=`hostname -f` \
 		--set setQuota=1 --set model=None --position="cn=printers,$ldap_base"
 	done
 
 	for num in `seq 1 16`; do
-		univention-admin shares/printer create --set name="dgruppentestprinter$num" \
+		univention-directory-manager shares/printer create --set name="dgruppentestprinter$num" \
 		--set uri="ipp://$remoteip/printers/rundablage" --set spoolHost=`hostname -f` \
 		--set setQuota=1 --set model=None --position="cn=printers,$ldap_base"
 	done
@@ -88,12 +88,12 @@ else
 	for num in `seq 1 8`; do
 		if [ "$num" = "1" ]; then
 			# Drucker 2 gehört auch Druckergruppe 1 an
-			univention-admin shares/printergroup create --set name="dtestprintergroup$num" \
+			univention-directory-manager shares/printergroup create --set name="dtestprintergroup$num" \
 			--set spoolHost=`hostname -f` --set setQuota=1 --append groupMember="dgruppentestprinter$num" \
 			--append groupMember="dgruppentestprinter$(($num+8))" --append groupMember="dgruppentestprinter2"\
 			--position="cn=printers,$ldap_base"
 		else
-			univention-admin shares/printergroup create --set name="dtestprintergroup$num" \
+			univention-directory-manager shares/printergroup create --set name="dtestprintergroup$num" \
 			--set spoolHost=`hostname -f` --set setQuota=1 --append groupMember="dgruppentestprinter$num" \
 			--append groupMember="dgruppentestprinter$(($num+8))" --position="cn=printers,$ldap_base"
 		fi
@@ -102,22 +102,22 @@ else
 	# Quotas für Drucker 1-8 und Druckergruppe 1-8 festlegen
 
 	for num in `seq 1 7`; do
-		univention-admin policies/print_quota create --position "cn=printquota,cn=shares,cn=policies,$ldap_base" --set name="printerpolicy$num"
+		univention-directory-manager policies/print_quota create --position "cn=printquota,cn=shares,cn=policies,$ldap_base" --set name="printerpolicy$num"
 
 		if [ "$(( $num & 1 ))" = "1" ]; then
-			univention-admin policies/print_quota modify --dn="cn=printerpolicy$num,cn=printquota,cn=shares,cn=policies,$ldap_base" --set quotaUsers="6 12 dtestuser1"
+			univention-directory-manager policies/print_quota modify --dn="cn=printerpolicy$num,cn=printquota,cn=shares,cn=policies,$ldap_base" --set quotaUsers="6 12 dtestuser1"
 		fi
 
 		if [ "$(( $num & 2 ))" = "2" ]; then
-			univention-admin policies/print_quota modify --dn="cn=printerpolicy$num,cn=printquota,cn=shares,cn=policies,$ldap_base" --set quotaGroupsPerUsers="6 12 dtestgroup"
+			univention-directory-manager policies/print_quota modify --dn="cn=printerpolicy$num,cn=printquota,cn=shares,cn=policies,$ldap_base" --set quotaGroupsPerUsers="6 12 dtestgroup"
 		fi
 
 		if [ "$(( $num & 4 ))" = "4" ]; then
-			univention-admin policies/print_quota modify --dn="cn=printerpolicy$num,cn=printquota,cn=shares,cn=policies,$ldap_base" --set quotaGroups="6 12 dtestgroup"
+			univention-directory-manager policies/print_quota modify --dn="cn=printerpolicy$num,cn=printquota,cn=shares,cn=policies,$ldap_base" --set quotaGroups="6 12 dtestgroup"
 		fi
 
-		univention-admin shares/printer modify --dn="cn=dtestprinter$num,cn=printers,$ldap_base" --policy-reference "cn=printerpolicy$num,cn=printquota,cn=shares,cn=policies,$ldap_base"
-		univention-admin shares/printergroup modify --dn="cn=dtestprintergroup$num,cn=printers,$ldap_base"  --policy-reference "cn=printerpolicy$num,cn=printquota,cn=shares,cn=policies,$ldap_base"
+		univention-directory-manager shares/printer modify --dn="cn=dtestprinter$num,cn=printers,$ldap_base" --policy-reference "cn=printerpolicy$num,cn=printquota,cn=shares,cn=policies,$ldap_base"
+		univention-directory-manager shares/printergroup modify --dn="cn=dtestprintergroup$num,cn=printers,$ldap_base"  --policy-reference "cn=printerpolicy$num,cn=printquota,cn=shares,cn=policies,$ldap_base"
 	done
 
 fi
