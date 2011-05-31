a6be8c4911914450694f796f83c20b4df6c8c486
Alexander Kläser <klaeser@univention.de>
2011-05-31 07:46:43 +0000

    * adapted for i18n, added translations (Bug #22460)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@24589 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console-module-ucr/umc/js/ucr.js
+++ management/univention-management-console-module-ucr/umc/js/ucr.js
@@ -25,45 +25,199 @@ dojo.require("umc.widgets.Grid");
 dojo.require("umc.widgets.ContainerWidget");
 dojo.require("umc.widgets.ContainerForm");
 dojo.require("umc.widgets.StandbyMixin");
+dojo.require("umc.i18n");
 
-dojo.declare("umc.modules._ucrDetailDialog", [ dijit.Dialog, umc.widgets.StandbyMixin ], {
+dojo.declare("umc.modules.ucr", [ umc.widgets.Module, umc.i18n.Mixin ], {
+	// summary:
+	//		Module for modifying and displaying UCR variables on the system.
+
+	_grid: null,
+	_store: null,
+	_searchWidget: null,
+	_detailDialog: null,
+	_contextVariable: null,
+
+	buildRendering: function() {
+		// call superclass method
+		this.inherited(arguments);
+
+		//
+		// add data grid
+		//
+
+		// define actions
+		var actions = [{
+			name: 'add',
+			label: this._( 'Add' ),
+			description: this._( 'Adding a new UCR variable' ),
+			iconClass: 'dijitIconNewTask',
+			isContextAction: false,
+			isStandardAction: true,
+			callback: dojo.hitch(this, function() {
+				this._detailDialog.newVariable();
+			})
+		}, {
+			name: 'edit',
+			label: this._( 'Edit' ),
+			description: this._( 'Setting the UCR variable, editing the categories and/or description' ),
+			iconClass: 'dijitIconEdit',
+			isStandardAction: true,
+			isMultiAction: false,
+			callback: dojo.hitch(this, function(vars) {
+				if (vars.length) {
+					this._detailDialog.loadVariable(vars[0].variable);
+				}
+			})
+		}, {
+			name: 'delete',
+			label: this._( 'Delete' ),
+			description: this._( 'Deleting the selected UCR variables' ),
+			iconClass: 'dijitIconDelete'
+		}];
+
+		// define grid columns
+		var columns = [{
+			name: 'variable',
+			label: this._( 'UCR variable' ),
+			description: this._( 'Unique name of the UCR variable' ),
+			editable: false
+		}, {
+			name: 'value',
+			label: 'Wert',
+			description: this._( 'Value of the UCR variable' )
+		}];
+
+		// generate the data grid
+		this._grid = new umc.widgets.Grid({
+			region: 'center',
+			actions: actions,
+			columns: columns,
+			idField: 'variable',
+			umcpSearchCommand: 'ucr/search',
+			umcpSetCommand: 'ucr/set'
+		});
+		this.addChild(this._grid);
+
+		//
+		// add search widget
+		//
+
+		// define the different search widgets
+		var widgets = [{
+			type: 'ComboBox',
+			name: 'category',
+			value: 'all',
+			description: this._( 'Category the UCR variable should associated with' ),
+			label: this._('Category'),
+			staticValues: {
+				all: this._('All')
+			},
+			umcpValues: 'ucr/categories'
+		}, {
+			type: 'ComboBox',
+			name: 'key',
+			value: 'all',
+			description: this._( 'Select the attribute of a UCR variable that should be search for the given keyword' ),
+			label: this._( 'Search attribute' ),
+			staticValues: {
+				all: this._( 'All' ),
+				key: this._( 'Variable' ),
+				value: this._( 'Value' ),
+				description: this._( 'Description' )
+			}
+		}, {
+			type: 'TextBox',
+			name: 'filter',
+			value: '*',
+			description: this._( 'Keyword that should be searched for in the selected attribute' ),
+			label: this._( 'Keyword' )
+		}];
+
+		// define all buttons
+		var buttons = [{
+			name: 'submit',
+			label: this._( 'Search' ),
+			callback: dojo.hitch(this._grid, 'umcpSearch')
+		}, {
+			name: 'reset',
+			label: this._( 'Reset' )
+		}];
+
+		// define the search form layout
+		var layout = [
+			[ 'category', '' ],
+			[ 'key', 'filter' ]
+		];
+
+		// generate the search widget
+		this._searchWidget = new umc.widgets.Form({
+			region: 'top',
+			widgets: widgets,
+			buttons: buttons,
+			layout: layout
+		});
+		this.addChild(this._searchWidget);
+
+		// simple handler to enable/disable standby mode
+		dojo.connect(this._grid, 'umcpSearch', this, function() {
+			this.standby(true);
+		});
+		dojo.connect(this._grid, 'onUmcpSearchDone', this, function() {
+			this.standby(false);
+		});
+
+		//
+		// create dialog for UCR variable details
+		//
+
+		this._detailDialog = new umc.modules.ucr._DetailDialog({});
+		this._detailDialog.startup();
+
+	}
+
+});
+
+dojo.declare("umc.modules.ucr._DetailDialog", [ dijit.Dialog, umc.widgets.StandbyMixin, umc.i18n.Mixin ], {
 	_form: null,
 
+	// use i18n information from umc.modules.ucr
+	i18nClass: 'umc.modules.ucr',
+
 	postMixInProperties: function() {
-		// call superclass' method
+		// call superclass method
 		this.inherited(arguments);
 
 		dojo.mixin(this, {
-			title: _( 'Edit UCR variable' ),
+			title: this._( 'Edit UCR variable' ),
 			style: 'width: 450px'
 		});
 	},
 
 	buildRendering: function() {
-		// call superclass' method
+		// call superclass method
 		this.inherited(arguments);
 
 		var widgets = [{
 			type: 'TextBox',
 			name: 'variable',
-			description: _( 'Name of UCR variable' ),
-			label: _( 'UCR variable' )
+			description: this._( 'Name of UCR variable' ),
+			label: this._( 'UCR variable' )
 		}, {
 			type: 'TextBox',
 			name: 'value',
-			description: _( 'Value of UCR variable' ),
-			label: _( 'Value' )
+			description: this._( 'Value of UCR variable' ),
+			label: this._( 'Value' )
 		}, {
 			type: 'MultiSelect',
 			name: 'categories',
-			description: _( 'Categories that the UCR variable is assoziated with' ),
-			label: _( 'Categories' ),
+			description: this._( 'Categories that the UCR variable is assoziated with' ),
+			label: this._( 'Categories' ),
 			umcpValues: 'ucr/categories'
 		}];
 
 		var buttons = [{
 			name: 'submit',
-			label: _( 'Save' ),
+			label: this._( 'Save' ),
 			callback: dojo.hitch(this, function() {
 				this.standby(true);
 				this._form.umcpSet();
@@ -71,7 +225,7 @@ dojo.declare("umc.modules._ucrDetailDialog", [ dijit.Dialog, umc.widgets.Standby
 		}, {
 			//FIXME: Should be much simpled. The key name should be enough
 			name: 'cancel',
-			label: _( 'Cancel' ),
+			label: this._( 'Cancel' ),
 			callback: dojo.hitch(this, function() {
 				this.hide();
 			})
@@ -131,156 +285,6 @@ dojo.declare("umc.modules._ucrDetailDialog", [ dijit.Dialog, umc.widgets.Standby
 	onSubmit: function(values) {
 		// stub for event handling
 	}
-});
-
-dojo.declare("umc.modules.ucr", umc.widgets.Module, {
-	// summary:
-	//		Module for modifying and displaying UCR variables on the system.
-
-	_grid: null,
-	_store: null,
-	_searchWidget: null,
-	_detailDialog: null,
-	_contextVariable: null,
-
-	buildRendering: function() {
-		// call superclass' method
-		this.inherited(arguments);
-
-		//
-		// add data grid
-		//
-
-		// define actions
-		var actions = [{
-			name: 'add',
-			label: _( 'Add' ),
-			description: _( 'Adding a new UCR variable' ),
-			iconClass: 'dijitIconNewTask',
-			isContextAction: false,
-			isStandardAction: true,
-			callback: dojo.hitch(this, function() {
-				this._detailDialog.newVariable();
-			})
-		}, {
-			name: 'edit',
-			label: _( 'Edit' ),
-			description: _( 'Setting the UCR variable, editing the categories and/or description' ),
-			iconClass: 'dijitIconEdit',
-			isStandardAction: true,
-			isMultiAction: false,
-			callback: dojo.hitch(this, function(vars) {
-				if (vars.length) {
-					this._detailDialog.loadVariable(vars[0].variable);
-				}
-			})
-		}, {
-			name: 'delete',
-			label: _( 'Delete' ),
-			description: _( 'Deleting the selected UCR variables' ),
-			iconClass: 'dijitIconDelete'
-		}];
-
-		// define grid columns
-		var columns = [{
-			name: 'variable',
-			label: _( 'UCR variable' ),
-			description: _( 'Unique name of the UCR variable' ),
-			editable: false
-		}, {
-			name: 'value',
-			label: 'Wert',
-			description: _( 'Value of the UCR variable' )
-		}];
-
-		// generate the data grid
-		this._grid = new umc.widgets.Grid({
-			region: 'center',
-			actions: actions,
-			columns: columns,
-			idField: 'variable',
-			umcpSearchCommand: 'ucr/search',
-			umcpSetCommand: 'ucr/set'
-		});
-		this.addChild(this._grid);
-
-		//
-		// add search widget
-		//
-
-		// define the different search widgets
-		var widgets = [{
-			type: 'ComboBox',
-			name: 'category',
-			value: 'all',
-			description: _( 'Category the UCR vriable should assoziated with' ),
-			label: 'Kategorie',
-			staticValues: {
-				all: 'Alle'
-			},
-			umcpValues: 'ucr/categories'
-		}, {
-			type: 'ComboBox',
-			name: 'key',
-			value: 'all',
-			description: _( 'Select the attribute of a UCR variable that should be search for the given keyword' ),
-			label: _( 'Search attribute' ),
-			staticValues: {
-				all: _( 'All' ),
-				key: _( 'Variable' ),
-				value: _( 'Value' ),
-				description: _( 'Description' )
-			}
-		}, {
-			type: 'TextBox',
-			name: 'filter',
-			value: '*',
-			description: _( 'Keyword that should be searched for in the selected attribute' ),
-			label: _( 'Keyword' )
-		}];
-
-		// define all buttons
-		var buttons = [{
-			name: 'submit',
-			label: _( 'Search' ),
-			callback: dojo.hitch(this._grid, 'umcpSearch')
-		}, {
-			name: 'reset',
-			label: _( 'Reset' )
-		}];
-
-		// define the search form layout
-		var layout = [
-			[ 'category', '' ],
-			[ 'key', 'filter' ]
-		];
-
-		// generate the search widget
-		this._searchWidget = new umc.widgets.Form({
-			region: 'top',
-			widgets: widgets,
-			buttons: buttons,
-			layout: layout
-		});
-		this.addChild(this._searchWidget);
-
-		// simple handler to enable/disable standby mode
-		dojo.connect(this._grid, 'umcpSearch', this, function() {
-			this.standby(true);
-		});
-		dojo.connect(this._grid, 'onUmcpSearchDone', this, function() {
-			this.standby(false);
-		});
-
-		//
-		// create dialog for UCR variable details
-		//
-
-		this._detailDialog = new umc.modules._ucrDetailDialog({});
-		this._detailDialog.startup();
-
-	}
-
 });
 
 
