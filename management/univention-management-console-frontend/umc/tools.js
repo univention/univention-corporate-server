d6cc2f47a3e8e4409e793c056b5c2c9ab09721a6
Alexander Kläser <klaeser@univention.de>
2011-06-10 08:05:07 +0000

    * added translations (Bug #22460)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@24828 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console-frontend/umc/tools.js
+++ management/univention-management-console-frontend/umc/tools.js
@@ -116,40 +116,40 @@ dojo.mixin(umc.tools, {
 	// _statusMessages:
 	//		A dictionary that translates a status to an error message
 
-	// Status( 'SUCCESS'                           , 200, _( 'OK, operation successful' ) ),
-	// Status( 'SUCCESS_MESSAGE'                   , 204, _( 'OK, containing report message' ) ),
-	// Status( 'SUCCESS_PARTIAL'                   , 206, _( 'OK, partial response' ) ),
-	// Status( 'SUCCESS_SHUTDOWN'                  , 250, _( 'OK, operation successful ask for shutdown of connection' ) ),
+	// Status( 'SUCCESS'                           , 200, ( 'OK, operation successful' ) ),
+	// Status( 'SUCCESS_MESSAGE'                   , 204, ( 'OK, containing report message' ) ),
+	// Status( 'SUCCESS_PARTIAL'                   , 206, ( 'OK, partial response' ) ),
+	// Status( 'SUCCESS_SHUTDOWN'                  , 250, ( 'OK, operation successful ask for shutdown of connection' ) ),
 	// 
-	// Status( 'CLIENT_ERR_NONFATAL'               , 301, _( 'A non-fatal error has occured processing may continue' ) ),
+	// Status( 'CLIENT_ERR_NONFATAL'               , 301, ( 'A non-fatal error has occured processing may continue' ) ),
 	// 
-	// Status( 'BAD_REQUEST'                       , 400, _( 'Bad request' ) ),
-	// Status( 'BAD_REQUEST_UNAUTH'                , 401, _( 'Unauthorized' ) ),
-	// Status( 'BAD_REQUEST_FORBIDDEN'             , 403, _( 'Forbidden' ) ),
-	// Status( 'BAD_REQUEST_NOT_FOUND'             , 404, _( 'Not found' ) ),
-	// Status( 'BAD_REQUEST_NOT_ALLOWED'           , 405, _( 'Command not allowed' ) ),
-	// Status( 'BAD_REQUEST_INVALID_ARGS'          , 406, _( 'Invalid command arguments' ) ),
-	// Status( 'BAD_REQUEST_INVALID_OPTS'          , 407, _( 'Invalid or missing command options' ) ),
-	// Status( 'BAD_REQUEST_AUTH_FAILED'           , 411, _( 'The authentication has failed' ) ),
-	// Status( 'BAD_REQUEST_ACCOUNT_EXPIRED'       , 412, _( 'The account is expired and can not be used anymore' ) ),
-	// Status( 'BAD_REQUEST_ACCOUNT_DISABLED'      , 413, _( 'The account as been disabled' ) ),
-	// Status( 'BAD_REQUEST_UNAVAILABLE_LOCALE'    , 414, _( 'Specified locale is not available' ) ),
+	// Status( 'BAD_REQUEST'                       , 400, ( 'Bad request' ) ),
+	// Status( 'BAD_REQUEST_UNAUTH'                , 401, ( 'Unauthorized' ) ),
+	// Status( 'BAD_REQUEST_FORBIDDEN'             , 403, ( 'Forbidden' ) ),
+	// Status( 'BAD_REQUEST_NOT_FOUND'             , 404, ( 'Not found' ) ),
+	// Status( 'BAD_REQUEST_NOT_ALLOWED'           , 405, ( 'Command not allowed' ) ),
+	// Status( 'BAD_REQUEST_INVALID_ARGS'          , 406, ( 'Invalid command arguments' ) ),
+	// Status( 'BAD_REQUEST_INVALID_OPTS'          , 407, ( 'Invalid or missing command options' ) ),
+	// Status( 'BAD_REQUEST_AUTH_FAILED'           , 411, ( 'The authentication has failed' ) ),
+	// Status( 'BAD_REQUEST_ACCOUNT_EXPIRED'       , 412, ( 'The account is expired and can not be used anymore' ) ),
+	// Status( 'BAD_REQUEST_ACCOUNT_DISABLED'      , 413, ( 'The account as been disabled' ) ),
+	// Status( 'BAD_REQUEST_UNAVAILABLE_LOCALE'    , 414, ( 'Specified locale is not available' ) ),
 	//
-	// Status( 'SERVER_ERR'                        , 500, _( 'Internal error' ) ),
-	// Status( 'SERVER_ERR_MODULE_DIED'            , 510, _( 'Module process died unexpectedly' ) ),
-	// Status( 'SERVER_ERR_MODULE_FAILED'          , 511, _( 'Connection to module process failed' ) ),
-	// Status( 'SERVER_ERR_CERT_NOT_TRUSTWORTHY'   , 512, _( 'SSL server certificate is not trustworthy' ) ),
+	// Status( 'SERVER_ERR'                        , 500, ( 'Internal error' ) ),
+	// Status( 'SERVER_ERR_MODULE_DIED'            , 510, ( 'Module process died unexpectedly' ) ),
+	// Status( 'SERVER_ERR_MODULE_FAILED'          , 511, ( 'Connection to module process failed' ) ),
+	// Status( 'SERVER_ERR_CERT_NOT_TRUSTWORTHY'   , 512, ( 'SSL server certificate is not trustworthy' ) ),
 	//
-	// Status( 'UMCP_ERR_UNPARSABLE_HEADER'        , 551, _( 'Unparsable message header' ) ),
-	// Status( 'UMCP_ERR_UNKNOWN_COMMAND'          , 552, _( 'Unknown command' ) ),
-	// Status( 'UMCP_ERR_INVALID_NUM_ARGS'         , 553, _( 'Invalid number of arguments' ) ),
-	// Status( 'UMCP_ERR_UNPARSABLE_BODY'          , 554, _( 'Unparsable message body' ) ),
+	// Status( 'UMCP_ERR_UNPARSABLE_HEADER'        , 551, ( 'Unparsable message header' ) ),
+	// Status( 'UMCP_ERR_UNKNOWN_COMMAND'          , 552, ( 'Unknown command' ) ),
+	// Status( 'UMCP_ERR_INVALID_NUM_ARGS'         , 553, ( 'Invalid number of arguments' ) ),
+	// Status( 'UMCP_ERR_UNPARSABLE_BODY'          , 554, ( 'Unparsable message body' ) ),
 	//
-	// Status( 'MODULE_ERR'                        , 600, _( 'Error occuried during command processing' ) ),
-	// Status( 'MODULE_ERR_COMMAND_FAILED'         , 601, _( 'The execution of a command caused an fatal error' ) )
+	// Status( 'MODULE_ERR'                        , 600, ( 'Error occuried during command processing' ) ),
+	// Status( 'MODULE_ERR_COMMAND_FAILED'         , 601, ( 'The execution of a command caused an fatal error' ) )
 
 	_statusMessages: {
-		400: umc.tools._( 'Could not fullfill the request.' ),
+		400: umc.tools._( 'Could not fulfill the request.' ),
 		401: umc.tools._( 'Your session has expired, please login again.' ), // error occurrs only when user is not authenticated and a request is sent
 		403: umc.tools._( 'You are not authorized to perform this action.' ),
 		
