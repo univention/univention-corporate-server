ce65a8c15c6be4ce4b361c10fc98bbfadc2ce0f7
Andreas Büsching <buesching@univention.de>
2011-06-23 07:00:39 +0000

    add missing file; Bug #22439
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@25064 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ management/univention-directory-manager-modules/modules/univention/admin/layout.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+#
+# Univention Directory manager Modules
+#  classes to dsefine layouts
+#
+# Copyright 2011 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
+class ILayoutElement( dict ):
+	"""Describes the layout information for a tab or a groupbox"""
+	def __init__( self, label, description = '', layout = [] ):
+		dict.__init__( self )
+		self.__setitem__( 'label', label )
+		self.__setitem__( 'description', description )
+		self.__setitem__( 'layout', layout )
+
+	@property
+	def label( self ):
+		return self[ 'label' ]
+
+	@property
+	def description( self ):
+		return self[ 'description' ]
+
+	@property
+	def layout( self ):
+		return self[ 'layout' ]
+
+	@layout.setter
+	def layout( self, value ):
+		 self[ 'layout' ] = value
+
+class Tab( ILayoutElement ):
+	def __init__( self, label, description = '', advanced = False, layout = [] ):
+		ILayoutElement.__init__( self, label, description, layout )
+		self.__setitem__( 'advanced', advanced )
+
+	@property
+	def advanced( self ):
+		return self[ 'advanced' ]
+
+class Group( ILayoutElement ):
+	pass
+
