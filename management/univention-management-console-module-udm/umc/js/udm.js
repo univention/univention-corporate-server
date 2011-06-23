61c4c01a2b5e688199e10b6c62fac415326f905f
Alexander Kläser <klaeser@univention.de>
2011-06-23 14:10:43 +0000

    univention-management-console-module-udm (1.0.23-1) unstable; urgency=low  .    * dynamically load details page (Bug #22439)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@25071 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console-module-udm/umc/js/udm.js
+++ management/univention-management-console-module-udm/umc/js/udm.js
@@ -2,15 +2,17 @@
 
 dojo.provide("umc.modules.udm");
 
+dojo.require("dojo.DeferredList");
+dojo.require("dijit.layout.BorderContainer");
+dojo.require("dijit.layout.TabContainer");
 dojo.require("umc.widgets.Module");
 dojo.require("umc.tools");
-/*dojo.require("umc.widgets.Form");
-dojo.require("umc.widgets.ContainerWidget");
-dojo.require("umc.widgets.ContainerForm");
-dojo.require("umc.widgets.StandbyMixin");*/
 dojo.require("umc.widgets.Grid");
+dojo.require("umc.widgets.Page");
 dojo.require("umc.i18n");
 dojo.require("umc.widgets.SearchForm");
+dojo.require("umc.widgets.GroupBox");
+dojo.require("umc.widgets.ContainerWidget");
 
 dojo.declare("umc.modules.udm", [ umc.widgets.Module, umc.i18n.Mixin ], {
 	// summary:
@@ -19,9 +21,33 @@ dojo.declare("umc.modules.udm", [ umc.widgets.Module, umc.i18n.Mixin ], {
 	// the property field that acts as unique identifier
 	idProperty: 'ldap-dn',
 
+	_searchPage: null,
+	_detailPage: null,
+	_detailForm: null,
+
 	buildRendering: function() {
 		// call superclass method
 		this.inherited(arguments);
+
+		// render search page directly
+		this._renderSearchPage();
+
+		// for the detail page, we first need to query property data from the server
+		var deferredList = [];
+		deferredList.push(this.umcpCommand('udm/properties'));
+		deferredList.push(this.umcpCommand('udm/layout'));
+		var deferreds = new dojo.DeferredList(deferredList);
+		deferreds.then(dojo.hitch(this, function(results) {
+			var properties = results[0][1];
+			var layout = results[1][1];
+			this._renderDetailPage(properties.result, layout.result);
+		}));
+	},
+
+	_renderSearchPage: function() {
+		// setup search page
+		this._searchPage = new dijit.layout.BorderContainer({});
+		this.addChild(this._searchPage);
 
 		//
 		// add data grid
@@ -44,12 +70,12 @@ dojo.declare("umc.modules.udm", [ umc.widgets.Module, umc.i18n.Mixin ], {
 			description: this._( 'Edit the LDAP object.' ),
 			iconClass: 'dijitIconEdit',
 			isStandardAction: true,
-			isMultiAction: false
-			//callback: dojo.hitch(this, function(vars) {
-			//	if (vars.length) {
-			//		this._detailDialog.loadVariable(vars[0]);
-			//	}
-			//})
+			isMultiAction: false,
+			callback: dojo.hitch(this, function(vars) {
+				if (vars.length && this._detailForm) {
+					this.selectChild(this._detailForm);
+				}
+			})
 		}, {
 			name: 'delete',
 			label: this._( 'Delete' ),
@@ -79,8 +105,7 @@ dojo.declare("umc.modules.udm", [ umc.widgets.Module, umc.i18n.Mixin ], {
 			columns: columns,
 			moduleStore: this.moduleStore
 		});
-		this.addChild(this._grid);
-		this.layout();
+		this._searchPage.addChild(this._grid);
 
 		//
 		// add search widget
@@ -114,10 +139,6 @@ dojo.declare("umc.modules.udm", [ umc.widgets.Module, umc.i18n.Mixin ], {
 			name: 'objectProperty',
 			description: this._( 'The object property on which the query is filtered.' ),
 			label: this._( 'Object property' ),
-			value: 'any',
-			staticValues: [
-				{ id: 'any', label: this._( 'Any property' ) }
-			],
 			dynamicValues: 'udm/properties',
 			dynamicOptions: { searchable: true },
 			umcpCommand: thisUmcpCommand,
@@ -130,14 +151,83 @@ dojo.declare("umc.modules.udm", [ umc.widgets.Module, umc.i18n.Mixin ], {
 			dynamicValues: 'udm/values',
 			depends: [ 'objectProperty', 'objectType' ]
 		}];
+		var layout = [[ 'container', 'objectType', 'objectProperty', 'objectPropertyValue' ]];
 
 		// generate the search widget
 		this._searchWidget = new umc.widgets.SearchForm({
-			region: 'top',
 			widgets: widgets,
+			layout: layout,
 			onSearch: dojo.hitch(this._grid, 'filter')
 		});
-		this.addChild(this._searchWidget);
+		var group = new umc.widgets.GroupBox({
+			legend: this._('Filter results'),
+			region: 'top',
+			toggleable: false,
+			content: this._searchWidget
+		});
+		this._searchPage.addChild(group);
+	},
+
+	_renderDetailPage: function(properties, layoutSubTabs) {
+		// create detail page
+		this._detailTabs = new dijit.layout.TabContainer({
+			nested: true,
+			region: 'center'
+		});
+
+		// render all widgets
+		var widgets = umc.tools.renderWidgets(properties);
+
+		// render the layout for each subtab
+		dojo.forEach(layoutSubTabs, function(ilayout) {
+			var subTab = new umc.widgets.Page({
+				title: ilayout.label || ilayout.name //TODO: 'name' should not be necessary
+			});
+			subTab.addChild(umc.tools.renderLayout(ilayout.layout, widgets));
+			this._detailTabs.addChild(subTab);
+		}, this);
+		this._detailTabs.startup();
+
+		// setup detail page, needs to be wrapped by a form (for managing the
+		// form entries) and a BorderContainer (for the footer with buttons)
+		var layout = new dijit.layout.BorderContainer({});
+		layout.addChild(this._detailTabs);
+
+		// buttons
+		var buttons = umc.tools.renderButtons([{
+			name: 'save',
+			label: this._('Save changes')
+		}, {
+			name: 'reset',
+			label: this._('Reset changes')
+		}]);
+		var footer = new umc.widgets.ContainerWidget({
+			region: 'bottom',
+			'class': 'umcNoBorder'
+		});
+		dojo.forEach(buttons._order, function(i) { 
+			footer.addChild(i);
+		});
+		layout.addChild(footer);
+
+		// create the form containing the whole BorderContainer as content and add 
+		// the form as new 'page'
+		this._detailForm = umc.widgets.Form({
+			widgets: widgets,
+			content: layout,
+			moduleStore: this.moduleStore
+		});
+		this.addChild(this._detailForm);
+	},
+
+	postCreate: function() {
+		// only show the objectType combobox when there are different values to choose from
+		var objTypeWidget = this._searchWidget._widgets.objectType;
+		this.connect(objTypeWidget, 'onDynamicValuesLoaded', function(values) {
+			if (values.length) {
+				//dojo.style(objTypeWidget.
+			}
+		});
 	}
 
 });
