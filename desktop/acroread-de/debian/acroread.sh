7497c39d3b2310a0cc6114f83a897032ce0c80f5
Felix Botner <botner@univention.de>
2011-07-11 11:48:09 +0000

    Depend on Acrobat Reader 9.4.2 Package cleanup for UCS 3.0 Bug #22432
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@25315 1b283449-9f2f-0410-b571-8eb228e1a901

--- desktop/acroread-de/debian/acroread.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# ensure that firefox finds the GTK+ printer backends on amd64
-if dpkg-architecture -eamd64; then
-	export GTK_PATH=/usr/lib32/gtk-2.0
-fi
-
-exec /opt/Adobe/Reader9/bin/acroread "$@"
