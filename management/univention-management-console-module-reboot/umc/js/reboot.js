249ba37b664a2e65884c60e37bfc1d2245c0c6b1
Jascha Geerds <geerds@univention.de>
2011-07-18 12:51:15 +0000

    univention-management-console-module-reboot (1.0.4-1) unstable; urgency=low
      * Change helpText (Bug #22848)
    
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@25459 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console-module-reboot/umc/js/reboot.js
+++ management/univention-management-console-module-reboot/umc/js/reboot.js
@@ -19,7 +19,7 @@ dojo.declare("umc.modules.reboot", [ umc.widgets.Module, umc.i18n.Mixin ], {
 		this.inherited(arguments);
 
         this._page = new umc.widgets.Page({
-            helpText: this._("System reboot or shutdown")
+            helpText: this._("This module can be used to restart or shut down the system remotely. The optionally given message will be displayed on the console and written to the syslog.")
         });
         this.addChild(this._page);
 
