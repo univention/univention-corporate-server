00bcc91efb7d064b287d5a943a5cb107949aa223
Felix Botner <botner@univention.de>
2011-07-21 10:57:32 +0000

    added plymouth theme and lsb-base-logging.sh (Bug #20138)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@25527 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-bootsplash/conffiles/bootsplash.py
+++ base/univention-bootsplash/conffiles/bootsplash.py
@@ -1,5 +1,9 @@
 import os
 
-
 def postinst(baseConfig, changes):
-	os.system("update-initramfs -u")
+	theme = changes.get("bootsplash/theme", False)
+	if theme and type(()) == type(theme):
+		old, new = theme
+		if new:
+			os.system("plymouth-set-default-theme %s" % new)
+			os.system("update-initramfs -u")
