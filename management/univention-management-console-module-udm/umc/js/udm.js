4aa72d8626cd5a97e9024524856d801875990d5a
Alexander Kläser <klaeser@univention.de>
2011-07-25 14:26:02 +0000

    univention-management-console-module-udm (1.0.68-1) unstable; urgency=low  .    * added support for user templates (Bug #22883)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@25584 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console-module-udm/umc/js/udm.js
+++ management/univention-management-console-module-udm/umc/js/udm.js
@@ -35,6 +35,7 @@ dojo.declare("umc.modules.udm", [ umc.widgets.Module, umc.i18n.Mixin ], {
 	_propertySubTabMap: null,
 	_detailPages: null,
 	_tree: null,
+	_template: null,
 
 	buildRendering: function() {
 		// call superclass method
@@ -289,25 +290,34 @@ dojo.declare("umc.modules.udm", [ umc.widgets.Module, umc.i18n.Mixin ], {
 	},
 
 	_createDetailPage: function(objectType, ldapName) {
-		console.log('#_createDetailPage: ' + objectType);
 		// remember the objectType of the object we are going to edit
 		this._editedObjType = objectType;
 
 		// for the detail page, we first need to query property data from the server
 		// for the layout of the selected object type, then we can render the page
 		var params = { objectType: objectType };
-		(new dojo.DeferredList([
+		var commands = [
 			this.umcpCommand('udm/properties', params),
 			this.umcpCommand('udm/layout', params)
-		])).then(dojo.hitch(this, function(results) {
+		];
+
+		// in case an object template has been chosen, add the umcp request for the template
+		var objTemplate = dojo.getObject('objectTemplate', false, this._newObjOptions);
+		if (objTemplate && 'None' != objTemplate) {
+			commands.push(this.umcpCommand('udm/get', [objTemplate], true, 'settings/usertemplate'));
+		}
+
+		// when the commands have been finished, create the detail page
+		(new dojo.DeferredList(commands)).then(dojo.hitch(this, function(results) {
 			var properties = results[0][1];
 			var layout = results[1][1];
-			this._renderDetailPage(properties.result, layout.result);
+			var template = results.length > 2 ? results[2][1].result : null;
+			this._renderDetailPage(properties.result, layout.result, template);
 			this.showDetailPage(ldapName);
 		}));
 	},
 
-	_renderDetailPage: function(_properties, _layout) {
+	_renderDetailPage: function(_properties, _layout, _template) {
 		// create detail page
 		this._detailTabs = new dijit.layout.TabContainer({
 			nested: true,
@@ -418,6 +428,15 @@ dojo.declare("umc.modules.udm", [ umc.widgets.Module, umc.i18n.Mixin ], {
 			onSubmit: dojo.hitch(this, 'validateChanges')
 		});
 		this.addChild(this._detailForm);
+
+		// in case we have a template, create a new template object that takes care
+		// of updating the elements in the form
+		if (_template && _template.length > 0) {
+			this._template = new umc.modules.udm._Template({
+				widgets: widgets,
+				template: _template[0]
+			});
+		}
 	},
 
 	showDetailPage: function(ldapName) {
@@ -445,6 +464,10 @@ dojo.declare("umc.modules.udm", [ umc.widgets.Module, umc.i18n.Mixin ], {
 			this._editedObjType = null;
 			this._propertySubTabMap = null;
 			this._detailPages = null;
+			if (this._template) {
+				this._template.destroy();
+			}
+			this._template = null;
 			this.closeChild(oldDetailForm);
 		}
 	},
@@ -623,6 +646,13 @@ dojo.declare("umc.modules.udm", [ umc.widgets.Module, umc.i18n.Mixin ], {
 				this._createDetailPage(options.objectType);
 			})
 		});
+	},
+
+	destroy: function() {
+		if (this._template) {
+			this._template.destroy();
+			this._template = null;
+		}
 	}
 });
 
@@ -853,5 +883,265 @@ dojo.declare('umc.modules.udm._TreeModel', null, {
 		}));
 	}
 });
+
+
+dojo.declare('umc.modules.udm._Template', null, {
+	widgets: null,
+
+	template: null,
+
+	_inverseReferences: null,
+
+	_userChanges: null,
+
+	_eventHandles: null,
+
+	_focusedWidget: '',
+
+	// mappings to convert umlauts and special characters to standard ones
+	_umlauts: { 'ä' :'ae', 'Ä' : 'Ae', 'ö' : 'oe', 'Ö' : 'Oe', 'ü' : 'ue', 'Ü' : 'Ue', 'ß' : 'ss', 'Á' : 'A', 'Â' : 'A', 'Ã' : 'A', 'Ä' : 'A', 'Å' : 'A', 'Æ' : 'AE', 'Ç' : 'C', 'È' : 'E', 'É' : 'E', 'Ê' : 'E', 'Ë' : 'E', 'Ì' : 'I', 'Í' : 'I', 'Î' : 'I', 'Ï' : 'I', 'Ð' : 'D', 'Ñ' : 'N', 'Ò' : 'O', 'Ó' : 'O', 'Ô' : 'O', 'Õ' : 'O', 'Ö' : 'O', 'Ù' : 'U', 'Ú' : 'U', 'Û' : 'U', 'à' : 'a', 'â' : 'a', 'á' : 'a', 'ã' : 'a', 'æ' : 'ae', 'ç' : 'c', 'è' : 'e', 'é' : 'e', 'ê' : 'e', 'ë' : 'e', 'ì' : 'i', 'í' : 'i', 'î' : 'i', 'ï' : 'i', 'ñ' : 'n', 'ò' : 'o', 'ó' : 'o', 'ô' : 'o', 'ù' : 'u', 'ú' : 'u', 'û' : 'u', 'ý' : 'y', 'ÿ' : 'y', 'Ĉ' : 'C', 'ĉ' : 'c' },
+
+	// regular expressio for matching variables in the template, this includes 
+	// following possible variants with var='Univention'
+	//
+	// simple: 
+	//   <var>               -> 'Univention'
+	// with modifiers: 
+	//   <var:lower>         -> 'univention'
+	//   <var:upper>         -> 'UNIVENTION'
+	//   <var:umlauts,upper> -> 'UNIVENTION'
+	// with pythonic index operator: 
+	//   <var>[0]   -> 'U'
+	//   <var>[-2]  -> 'o'
+	//   <var>[0:2] -> 'Un'
+	//   <var>[1:]  -> 'nivention'
+	//   <var>[:3]  -> 'Uni'
+	//   <var>[:-3] -> 'Univent'
+	//
+	_regVar: /<(\w+)(:([\w,]*))?>(\[(-?\d*)(:(-?\d*))?\])?/g,
+
+	_getModifiers: function(modifierString, startIdx, endIdx) {
+		// get the correct string modifiers (can be a list of modifiers)
+		var modifierNames = dojo.isString(modifierString) ? modifierString.toLowerCase().split(',') : [''];
+		var modifiers = [];
+		dojo.forEach(modifierNames, function(iname) {
+			switch(dojo.trim(iname)) {
+			case 'lower': 
+				modifiers.push(function(str) {
+					return dojo.isString(str) ? str.toLowerCase() : str;
+				});
+				break;
+			case 'upper': 
+				modifiers.push(function(str) {
+					return dojo.isString(str) ? str.toUpperCase() : str;
+				});
+				break;
+			case 'umlaut':
+			case 'umlauts':
+				modifiers.push(dojo.hitch(this, function(str) {
+					if (!dojo.isString(str)) {
+						return str;
+					}
+					var newStr = '';
+					for (var i = 0; i < str.length; ++i) {
+						newStr += this._umlauts[str[i]] || str[i];
+					}
+					return newStr;
+				}));
+				break;
+			default:
+				// default modifier is a dummy function that does nothing
+				modifiers.push(function(str) { return str; });
+			}
+		}, this);
+
+		// add index operator as last modifier
+		modifiers.push(function(str) {
+			return str.slice(startIdx, endIdx);
+		});
+
+		// return function that applies all modifiers
+		return function(str) {
+			dojo.forEach(modifiers, function(imod) {
+				str = imod(str);
+			});
+			return str;
+		};
+	},
+
+	constructor: function(props) {
+		// mixin the props
+		dojo.mixin(this, props);
+
+		// iterate over all template values
+		// * set static values directly to the form
+		// * register dynamic values to react on user input
+		var updaters = [];
+		umc.tools.forIn(this.template, function(ikey, ival) {
+			// ignore values that do not have a widget
+			if (!(ikey in this.widgets)) {
+				console.log('WARNING: The property "' + ikey + '" as specified by the template does not exist. Ignoring error.');
+				return true;
+			}
+
+			// object for updating the field
+			var updater = {
+				key: ikey,
+				selfReference: this.widgets[ikey],
+				templateString: dojo.clone(ival),
+				references: [], // ordered list of widgets that are referenced
+				modifiers: [], // ordered list of string modifiers per reference
+				update: function() {
+					// collect all necessary values
+					var vals = [];
+					dojo.forEach(this.references, function(iwidget, i) {
+						vals.push(this.modifiers[i](iwidget.get('value')));
+					}, this);
+
+					// the value might be a simple string or an array of strings
+					var newVal;
+					if (dojo.isString(this.templateString)) {
+						newVal = dojo.replace(this.templateString, vals);
+					}
+					else if (dojo.isArray(this.templateString)) {
+						newVal = [];
+						dojo.forEach(this.templateString, function(istr) {
+							newVal.push(dojo.replace(istr, vals));
+						});
+					}
+
+					// block onChange events (so we do not register the values as changes by
+					// the user) and set the value
+					//this.selfReference.set('blockOnChange', true);
+					this.selfReference.set('value', newVal);
+					//this.selfReference.set('blockOnChange', false);
+				}
+			};
+
+			// try to match all variable references... the template value might be a string
+			// or an array of strings
+			var nRefs = 0;
+			var tmpVals = dojo.isArray(ival) ? ival : [ival];
+			dojo.forEach(tmpVals, function(jval, j) {
+				var matches = jval.match(this._regVar);
+				dojo.forEach(matches, function(imatch) {
+					// parse the matched reference
+					this._regVar.lastIndex = 0; // start matching in any case from the string beginning
+					var match = this._regVar.exec(imatch);
+
+					// we have a value with variable reference... 
+					// parse the variable reference and get the correct indeces
+					var refKey = match[1];
+					var modifier = match[3];
+					var startIdx = 0;
+					var endIdx = Infinity;
+					try {
+						startIdx = !match[5] ? 0 : parseInt(match[5], 10);
+					}
+					catch (err1) { }
+
+					// check whether the user specified an end index
+					if (!match[6] && dojo.isString(match[5])) {
+						// nope... index points to one single character
+						endIdx = startIdx + 1;
+						if (0 === endIdx) {
+							// startIdx == -1
+							endIdx = Infinity;
+						}
+					}
+					else if (match[6]) {
+						try {
+							endIdx = !match[7] && match[7] !== '0' ? Infinity : parseInt(match[7], 10);
+						}
+						catch (err2) { }
+					}
+
+					// register the reference
+					if (!(refKey in this.widgets)) {
+						// reference does not exist
+						return true;
+					}
+					updater.references.push(this.widgets[refKey]);
+
+					// update the template string
+					if (dojo.isArray(ival)) {
+						updater.templateString[j] = updater.templateString[j].replace(imatch, '{' + nRefs + '}');
+					}
+					else {
+						updater.templateString = updater.templateString.replace(imatch, '{' + nRefs + '}');
+					}
+
+					// register the modifier
+					updater.modifiers.push(this._getModifiers(modifier, startIdx, endIdx));
+
+					// count the matched references
+					++nRefs;
+				}, this);
+			}, this);
+			if (nRefs) {
+				// we have a dynamic value with variable references
+				updaters.push(updater);
+			}
+			else {
+				// we have a static value, try to set the given key
+				if (ikey in this.widgets) {
+					this.widgets[ikey].set('value', ival);
+				}
+			}
+		}, this);
+
+		// build an inverse map to the reference... i.e., we want to know for a field
+		// that is being changed, which other templated fields depend on its value
+		this._inverseReferences = {};
+		dojo.forEach(updaters, function(iupdater) {
+			// get inverse references
+			dojo.forEach(iupdater.references, function(iref) {
+				// when we have the first entry for this reference, initiate with an empty dict
+				if (!(iref.name in this._inverseReferences)) {
+					this._inverseReferences[iref.name] = {};
+				}
+
+				// register the reference
+				this._inverseReferences[iref.name][iupdater.key] = iupdater;
+			}, this);
+
+			// update field for the first time
+			iupdater.update();
+		}, this);
+
+		// register user changes
+		this._userChanges = {};
+		this._eventHandles = [];
+		umc.tools.forIn(this.widgets, function(ikey, iwidget) {
+			// monitor value changes... onChange for changes made automatically and
+			// onKeyUp for changes made by the user
+			this._eventHandles.push(dojo.connect(iwidget, 'onKeyUp', dojo.hitch(this, 'onChange', iwidget)));
+			this._eventHandles.push(dojo.connect(iwidget, 'onChange', dojo.hitch(this, 'onChange', iwidget)));
+		}, this);
+	},
+
+	onChange: function(widget) {
+		// register that the user has changed this field manually in case the
+		// focus was on this field
+		if (widget.get('focused')) {
+			this._userChanges[widget.name] = true;
+		}
+
+		// see whether we can update other fields that have not been changed manually
+		var references = this._inverseReferences[widget.name] || {};
+		umc.tools.forIn(references, function(iRefKey, iUpdater) {
+			if (!this._userChanges[iRefKey]) {
+				iUpdater.update();
+			}
+		}, this);
+	},
+
+	destroy: function() {
+		dojo.forEach(this._eventHandles, dojo.disconnect);	
+	}
+});
+
+
+
 
 
