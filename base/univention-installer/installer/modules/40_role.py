bac067928c70f5deb705fa5c369ba4e1ad6cc9b7
Stefan Gohmann <gohmann@univention.de>
2011-08-03 15:05:13 +0000

    * Removed Managed and Mobile Clients from server role selection   (Bug #23156)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@25871 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-installer/installer/modules/40_role.py
+++ base/univention-installer/installer/modules/40_role.py
@@ -65,10 +65,6 @@ class object(content):
 			return 'domaincontroller_slave'
 		elif value in ['memberserver','MemberServer']:
 			return 'memberserver'
-		elif value in ['managed_client','FatClient']:
-			return 'managed_client'
-		elif value in ['mobile_client','MobileClient']:
-			return 'mobile_client'
 		elif value in ['basesystem','Base']:
 			return 'basesystem'
 
@@ -84,11 +80,9 @@ class object(content):
 			dict['Domain Controller Backup']=['domaincontroller_backup',1]
 			dict['Domain Controller Slave']=['domaincontroller_slave',2]
 			dict['Memberserver']=['memberserver',3]
-			dict['Managed Client']=['managed_client',4]
-			dict['Mobile Client']=['mobile_client',5]
-			dict['Basissystem']=['basesystem',6]
+			dict['Basissystem']=['basesystem',4]
 
-			list=['domaincontroller_master','domaincontroller_backup','domaincontroller_slave','memberserver','managed_client','mobile_client','basesystem']
+			list=['domaincontroller_master','domaincontroller_backup','domaincontroller_slave','memberserver','basesystem']
 			select=0
 			if self.all_results.has_key('system_role'):
 				select=list.index(self.mapping(self.all_results['system_role']))
@@ -102,9 +96,7 @@ class object(content):
 			self.elements.append(textline('[ ] %s' % _('Domain controller Backup'),self.minY+2,self.minX+2,40,40))#4
 			self.elements.append(textline('[ ] %s' % _('Domain controller Slave'),self.minY+3,self.minX+2,40,40))#5
 			self.elements.append(textline('[ ] %s' % _('Member server'),self.minY+4,self.minX+2,40,40))#6
-			self.elements.append(textline('[ ] %s' % _('Managed client'),self.minY+5,self.minX+2,40,40))#7
-			self.elements.append(textline('[ ] %s' % _('Mobile client'),self.minY+6,self.minX+2,40,40))#8
-			self.elements.append(textline('[ ] %s' % _('Base system'),self.minY+7,self.minX+2,40,40))#9
+			self.elements.append(textline('[ ] %s' % _('Base system'),self.minY+7,self.minX+2,40,40))#7
 
 
 	def input(self,key):
@@ -121,7 +113,7 @@ class object(content):
 		return 0
 
 	def helptext(self):
-		return _('System role \n \n Select a system role. Depending on the system role different components will be installed. \n \n Domain Controller Master: \n This system keeps the whole LDAP tree and is the core of your UCS domain. \n \n Domain Controller Backup: \n This system keeps a copy of the complete LDAP structure, which cannot be changed manually. \n \n Domain Controller Slave: \n This system includes required LDAP data for a special purpose (i.e. location based). \n \n Memberserver: \n Member of a domain offering specified domainwide services like printing or backup. No LDAP data is stored on such a system. \n \n Managed Client: \n A Linux desktop system. \n \n Mobile Client: \n A managed client with notebook optimizations. \n \n Basesystem: \n A stand-alone server solution for web-server or firewall for example. This system is not a member of any domain.')
+		return _('System role \n \n Select a system role. Depending on the system role different components will be installed. \n \n Domain Controller Master: \n This system keeps the whole LDAP tree and is the core of your UCS domain. \n \n Domain Controller Backup: \n This system keeps a copy of the complete LDAP structure, which cannot be changed manually. \n \n Domain Controller Slave: \n This system includes required LDAP data for a special purpose (i.e. location based). \n \n Memberserver: \n Member of a domain offering specified domainwide services like printing or backup. No LDAP data is stored on such a system. \n \n Basesystem: \n A stand-alone server solution for web-server or firewall for example. This system is not a member of any domain.')
 
 	def modheader(self):
 		return _('System role')
