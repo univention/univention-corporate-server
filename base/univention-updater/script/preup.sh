2d454e17b7224c232ff62450f345c8b3e66f390c
Philipp Hahn <hahn@univention.de>
2011-08-09 09:40:35 +0000

    Reload ucr Python module after upgrade (Bug #23202) Install Python2.6 before upgrade (Bug #23054) Force upgrade of slapd before libdb4.7 upgrade (Bug #23054)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@26007 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-updater/script/preup.sh
+++ base/univention-updater/script/preup.sh
@@ -28,24 +28,25 @@
 # <http://www.gnu.org/licenses/>.
 
 UPDATER_LOG="/var/log/univention/updater.log"
+exec 3>>"$UPDATER_LOG"
 UPDATE_LAST_VERSION="$1"
 UPDATE_NEXT_VERSION="$2"
 
-echo "Running preup.sh script" >> "$UPDATER_LOG"
-date >>"$UPDATER_LOG" 2>&1
+echo "Running preup.sh script" >&3
+date >&3
 
-eval "$(univention-config-registry shell)" >>"$UPDATER_LOG" 2>&1
+eval "$(univention-config-registry shell)" >&3 2>&3
 
 # Bug #16454: Workaround to remove source.list on failed upgrades
 cleanup () {
 	# remove statoverride for UMC and apache in case of error during preup script
 	if [ -e /usr/sbin/univention-management-console-server ]; then
 		dpkg-statoverride --remove /usr/sbin/univention-management-console-server >/dev/null 2>&1
-		chmod +x /usr/sbin/univention-management-console-server 2>> "$UPDATER_LOG"  >> "$UPDATER_LOG"
+		chmod +x /usr/sbin/univention-management-console-server 2>&3
 	fi
 	if [ -e /usr/sbin/apache2 ]; then
 		dpkg-statoverride --remove /usr/sbin/apache2 >/dev/null 2>&1
-		chmod +x /usr/sbin/apache2 2>> "$UPDATER_LOG"  >> "$UPDATER_LOG"
+		chmod +x /usr/sbin/apache2 2>&3
 	fi
 }
 trap cleanup EXIT
@@ -158,7 +159,7 @@ fi
 # first, test if univention-thin-client-basesystem is installed (UCS TCS or UCS with thin-client packages)
 # second, activate tcs component (thin client services are now only available via component tcs)
 # test if component is available (with univention.updater)
-checkTcsComponent=$(ucr get update/check/component/tcs)
+checkTcsComponent=$(univention-config-registry get update/check/component/tcs)
 tcsInstalled=false
 if [ -n "$checkTcsComponent" -a "$checkTcsComponent" = "no" ]; then
 	continue
@@ -204,19 +205,19 @@ if configRegistry.get("repository/online/component/%s" % scope, "").lower() in y
 	if not available:
 		sys.exit(1)
 sys.exit(0)
-' 2>$updateError)
+' 2>"$updateError")
 	
 	# component tcs in 3.0 not found, -> abort the update
 	if [ ! $? -eq 0 ]; then
 		if [ -s $updateError ]; then
 			echo "WARNING: Traceback in UniventionUpdater() python module:"
-			cat $updateError
+			cat "$updateError"
 		fi
 		echo "WARNING: An update to UCS 3.0 without the component 'tcs' is"
 		echo "         not possible because the component 'tcs' is required."
 		exit 1
 	fi
-	rm $updateError
+	rm "$updateError"
 fi
 # END -- update to 3.0-0 Bug #22878
 
@@ -226,13 +227,13 @@ if [ ! -z "$update_custom_preup" ]; then
 	if [ -f "$update_custom_preup" ]; then
 		if [ -x "$update_custom_preup" ]; then
 			echo "Running custom preupdate script $update_custom_preup"
-			"$update_custom_preup" "$UPDATE_LAST_VERSION" "$UPDATE_NEXT_VERSION" >>"$UPDATER_LOG" 2>&1
-			echo "Custom preupdate script $update_custom_preup exited with exitcode: $?" >>"$UPDATER_LOG" 2>&1
+			"$update_custom_preup" "$UPDATE_LAST_VERSION" "$UPDATE_NEXT_VERSION" >&3 2>&3
+			echo "Custom preupdate script $update_custom_preup exited with exitcode: $?" >&3
 		else
-			echo "Custom preupdate script $update_custom_preup is not executable" >>"$UPDATER_LOG" 2>&1
+			echo "Custom preupdate script $update_custom_preup is not executable" >&3
 		fi
 	else
-		echo "Custom preupdate script $update_custom_preup not found" >>"$UPDATER_LOG" 2>&1
+		echo "Custom preupdate script $update_custom_preup not found" >&3
 	fi
 fi
 
@@ -240,7 +241,7 @@ check_space(){
 	partition=$1
 	size=$2
 	usersize=$3
-	if [ `df -P $partition|tail -n1 | awk '{print $4}'` -gt "$size" ]
+	if [ `df -P "$partition" | tail -n1 | awk '{print $4}'` -gt "$size" ]
 		then
 		printf "Space on $partition:\t OK\n"
 	else
@@ -257,8 +258,8 @@ check_space(){
 
 # move old initrd files in /boot
 initrd_backup=/var/backups/univention-initrd.bak/
-if [ ! -d $initrd_backup ]; then
-	mkdir $initrd_backup
+if [ ! -d "$initrd_backup" ]; then
+	mkdir "$initrd_backup"
 fi
 mv /boot/*.bak /var/backups/univention-initrd.bak/ >/dev/null 2>&1
 
@@ -276,7 +277,7 @@ fi
 
 
 echo "Checking for the package status"
-dpkg -l 2>&1 | grep "^[a-zA-Z][A-Z] " >>"$UPDATER_LOG" 2>&1
+dpkg -l 2>&1 | grep "^[a-zA-Z][A-Z] " >&3 2>&3
 if [ $? = 0 ]; then
 	echo "ERROR: The package state on this system is inconsistent."
 	echo "       Please run 'dpkg --configure -a' manually"
@@ -295,33 +296,47 @@ if [ -e /usr/sbin/apache2 ]; then
 fi
 
 # Disable usplash during update (Bug #16363)
-if dpkg -l lilo 2>> "$UPDATER_LOG" >> "$UPDATER_LOG" ; then
-	dpkg-divert --rename --divert /usr/share/initramfs-tools/bootsplash.debian --add /usr/share/initramfs-tools/hooks/bootsplash 2>> "$UPDATER_LOG" >> "$UPDATER_LOG"
+if dpkg -l lilo >&3 2>&3 ; then
+	dpkg-divert --rename --divert /usr/share/initramfs-tools/bootsplash.debian --add /usr/share/initramfs-tools/hooks/bootsplash >&3 2>&3
 fi
 
 # remove old packages that causes conflicts
 olddebs="python2.4-dns alsa-headers nagios2 nagios2-common nagios2-doc"
 for deb in $olddebs; do
-	if dpkg -l $deb >>"$UPDATER_LOG" 2>&1; then
-		dpkg -P $deb >>"$UPDATER_LOG" 2>&1
+	if dpkg -l "$deb" >&3 2>&3; then
+		dpkg -P "$deb" >&3 2>&3
 	fi
 done
 
 # Update package lists
-apt-get update >>"$UPDATER_LOG" 2>&1
-#
+apt-get update >&3 2>&3
+
+# BEGIN -- update to 3.0-0 Bug #23054
+# 1. Install Python2.6-minimal before Python-minimal gets installed to workaround broken /usr/bin/python being used by python-support
+$update_commands_install libssl0.9.8 python2.6-minimal python-central
+# 2. Upgrade slapd before new libdb4.7 4.7.25-9.3.201105022022 gets installed, which used a different signature than old 4.7.25-6.7.201101311721
+case "$(dpkg-query -W -f '${Status}' slapd)" in
+install*)
+	if dpkg --compare-versions "$(dpkg-query -W -f '${Version}' libdb4.7)" lt 4.7.25-9.3.201105022022
+	then
+		$update_commands_install slapd db4.8-util libdb4.7=4.7.25-6\*
+	fi
+	;;
+esac
+# END -- update to 3.0-0 Bug #23054
+
 for pkg in univention-ssl univention-thin-client-basesystem univention-thin-client-x-base usplash ; do
 	# pre-update $pkg to avoid pre-dependency-problems
-	if dpkg -l $pkg 2>> "$UPDATER_LOG" | grep ^ii  >>"$UPDATER_LOG" ; then
+	if dpkg -l "$pkg" 2>&3 | grep ^ii  >&3 ; then
 	    echo -n "Starting preupdate of $pkg..."
-	    $update_commands_install $pkg >>"$UPDATER_LOG" 2>> "$UPDATER_LOG"
+	    $update_commands_install "$pkg" >&3 2>&3
 	    if [ ! $? = 0 ]; then
 			echo "failed."
 	        echo "ERROR: pre-update of $pkg failed!"
 	        echo "       Please run 'dpkg --configure -a' manually."
 	        exit 1
 	    fi
-	    dpkg -l 2>&1  | grep "  " | grep -v "^|" | grep "^[a-z]*[A-Z]" >>"$UPDATER_LOG" 2>&1
+	    dpkg -l 2>&1 | grep "  " | grep -v "^|" | grep "^[a-z]*[A-Z]" >&3 2>&3
 	    if [ $? = 0 ]; then
 			echo "failed."
 	        echo "ERROR: pre-update of $pkg failed!"
@@ -334,7 +349,7 @@ done
 
 echo "Starting update process, this may take a while."
 echo "Check /var/log/univention/updater.log for more information."
-date >> "$UPDATER_LOG"
+date >&3
 trap - EXIT
 
 exit 0
