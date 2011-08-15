9c1668493fa15234f036ccb263c5d2180ce60cbd
Alexander Kläser <klaeser@univention.de>
2011-08-15 15:22:09 +0000

    univention-dojo (4.0.4-1) unstable; urgency=low  .    * removed template code for DateTextBox due incompatibility with recent Dojo      version; the calendar icon is now set via CSS (Bug #21934)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-2.4/ucs@26121 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-dojo/unijit/form/DateTextBox.js
+++ management/univention-dojo/unijit/form/DateTextBox.js
@@ -1,13 +1,10 @@
 dojo.provide("unijit.form.DateTextBox");
 
-dojo.require("dijit._Calendar");
 dojo.require("dijit.form.DateTextBox");
 
 dojo.declare(
 	"unijit.form.DateTextBox",
 	dijit.form.DateTextBox,
-	{
-		templatePath : dojo.moduleUrl("unijit", "form/template/DateTextBox.html")
-	}
+	{ }
 );
 
