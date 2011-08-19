e38c46a235e601046e032c2deb5aabce99183e37
Stefan Gohmann <gohmann@univention.de>
2011-08-19 10:23:29 +0000

    * Move policy_result.py from univention-lib to univention-policy   (Bug #22521)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@26215 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-lib/python/__init__.py
+++ base/univention-lib/python/__init__.py
@@ -33,6 +33,5 @@
 
 from univention.lib.error import *
 from univention.lib.shell import *
-from univention.lib.policy_result import *
 from univention.lib.locking import *
 from univention.lib.i18n import *
