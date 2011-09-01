c7e335256d189c4d9cb4dd8347f99ab3adfcbc33
Moritz Mühlenhoff <muehlenhoff@univention.de>
2011-09-01 10:09:20 +0000

    Add bandaid for removal directory (Bug #22611)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@26562 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-lib/shell/ucr.sh
+++ base/univention-lib/shell/ucr.sh
@@ -15,6 +15,13 @@ is_ucr_true () { # test if UCS variable is "true" or "false"
 # e.g. remove_ucr_template /etc/obsolete-software.conf
 #
 remove_ucr_template () {
+
+	# /etc/univention/templates/removed/ is created through univention-config-registry, but depending on update
+	# order it may not yet exist. This can be removed after UCS 3.0
+	if [ ! -d /etc/univention/templates/removed/ ] ; then
+	    mkdir -p /etc/univention/templates/removed/
+	fi
+
 	if [ -e "$1" ] ; then
 	    mv "$1" /etc/univention/templates/removed/
 	fi
