623d526a8dcb3319d1c8ea83a6e07bbfdfaa7f3c
Sönke Schwardt-Krummrich <schwardt@univention.de>
2011-09-07 16:31:46 +0000

    * added listener module hosteddomains.py (Bug #22876)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@26658 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ mail/univention-mail-postfix/hosteddomains.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+#
+# Univention Mail Stack
+#  listener module: mail domain configuration
+#
+# Copyright 2005-2011 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
+import listener
+import univention.config_registry
+import os, re
+import univention.debug
+
+name='hosteddomains'
+description='update mail/hosteddomains'
+filter='(objectClass=univentionMailDomainname)'
+attributes=[]
+
+restart=False
+
+def initialize():
+	pass
+
+def handler(dn, new, old):
+	global restart
+	configRegistry = univention.config_registry.ConfigRegistry()
+	configRegistry.load()
+
+	old_hosteddomains = set(re.split('[ ]+', configRegistry.get('mail/hosteddomains','')))
+	hosteddomains = old_hosteddomains.copy()
+
+	# remove old add new
+	if old.get('cn'):
+		hosteddomains.discard(old.get('cn')[0])
+		univention.debug.debug(univention.debug.LISTENER, univention.debug.INFO, "hosteddomains: removed %s" % old.get('cn')[0])
+	if new.get('cn'):
+		hosteddomains.add(new.get('cn')[0])
+		univention.debug.debug(univention.debug.LISTENER, univention.debug.INFO, "hosteddomains: added %s" % new.get('cn')[0])
+
+	# if something changed then set UCR variable
+	if old_hosteddomains != hosteddomains:
+		try:
+			listener.setuid(0)
+			univention.debug.debug(univention.debug.LISTENER, univention.debug.INFO, "hosteddomains: %s" % u'mail/hosteddomains=%s' % ' '.join(hosteddomains) )
+			univention.config_registry.handler_set( [ u'mail/hosteddomains=%s' % ' '.join(hosteddomains) ] )
+			restart=True
+		finally:
+			listener.unsetuid()
