3ac3cca5573099fcab74e40b5c9461b27ffdfa29
Alexander Kläser <klaeser@univention.de>
2011-09-15 13:19:12 +0000

    univention-management-console-frontend (1.0.46-1) unstable; urgency=low  .    * fixed jslint warnings    * fixed error handling for tracebacks in web server and umc.tools    * changed error logging in web server from CORE.error to CORE.process;      since CORE.error writes as well to /var/log/syslog, this seems to have      caused problems with cherrypy    (Bug #22634)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@26834 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console-frontend/umc/tools.js
+++ management/univention-management-console-frontend/umc/tools.js
@@ -196,7 +196,7 @@ dojo.mixin(umc.tools, {
 			}
 			// all other cases
 			else {
-				umc.dialog.alert('<p>' + this._statusMessages[status] + '<p>' + (message ? this._('<p>Server error message:<p><p>%s</p>', message) : ''));
+				umc.dialog.alert('<p>' + this._statusMessages[status] + '</p>' + (message ? '<p>' + this._('Server error message:') + '</p><p class="umcServerErrorMessage">' + message + '</p>' : ''));
 			}
 		}
 		else if (undefined !== status) {
