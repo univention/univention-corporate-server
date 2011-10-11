0532e902c45ed3cc11a2d6a97f47ab3cab7ebd9a
Alexander Kläser <klaeser@univention.de>
2011-10-11 15:41:18 +0000

    univention-management-console-module-join (1.0.3-1) unstable; urgency=low  .    * added _join.Form (Bug #23701)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@27631 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ management/univention-management-console-module-join/umc/js/_join/Form.js
@@ -0,0 +1,30 @@
+/*global console MyError dojo dojox dijit umc */
+
+dojo.provide("umc.modules._join.Form");
+
+dojo.require("umc.widgets.Form");
+dojo.require("umc.widgets.StandbyMixin");
+
+dojo.declare("umc.modules._join.Form", [
+    umc.widgets.Form,
+	umc.widgets.StandbyMixin
+	],
+{
+	// that's all.
+});
+
+/*global console MyError dojo dojox dijit umc */
+
+dojo.provide("umc.modules._join.Form");
+
+dojo.require("umc.widgets.Form");
+dojo.require("umc.widgets.StandbyMixin");
+
+dojo.declare("umc.modules._join.Form", [
+    umc.widgets.Form,
+	umc.widgets.StandbyMixin
+	],
+{
+	// that's all.
+});
+
