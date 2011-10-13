0370be9c989f1704080a863598a3e1595fe0d688
Alexander Kläser <klaeser@univention.de>
2011-10-13 10:30:23 +0000

    univention-join (4.0.22-1) unstable; urgency=low  .    * removed duplicated code in umc.widgets._join.Form (Bug #23701)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@27766 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-join/umc/js/_join/Form.js
+++ management/univention-join/umc/js/_join/Form.js
@@ -12,19 +12,3 @@ dojo.declare("umc.modules._join.Form", [
 {
 	// that's all.
 });
-
-/*global console MyError dojo dojox dijit umc */
-
-dojo.provide("umc.modules._join.Form");
-
-dojo.require("umc.widgets.Form");
-dojo.require("umc.widgets.StandbyMixin");
-
-dojo.declare("umc.modules._join.Form", [
-    umc.widgets.Form,
-	umc.widgets.StandbyMixin
-	],
-{
-	// that's all.
-});
-
