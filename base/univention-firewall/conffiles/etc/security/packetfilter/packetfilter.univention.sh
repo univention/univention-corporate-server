8e5b53542bd7de4e6ad057ea7bc6627782a56050
Sönke Schwardt-Krummrich <schwardt@univention.de>
2011-10-18 14:17:33 +0000

    * renamed init.d script to univention-firewall (Bug #23577) * added user specific script packetfilter.local.sh * new UCR variable concept for univention-firewall
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@27929 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ base/univention-firewall/conffiles/etc/security/packetfilter/packetfilter.univention.sh
@@ -0,0 +1,142 @@
+#!/bin/sh
+@%@UCRWARNING=# @%@
+#
+# Copyright 2004-2011 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
+# initialise IPv4
+/sbin/iptables -F
+/sbin/iptables -F -t nat
+/sbin/iptables -F -t mangle
+
+# accept IPv4 connections from localhost
+/sbin/iptables -A INPUT -i lo -j ACCEPT
+/sbin/iptables -A OUTPUT -o lo -j ACCEPT
+
+# accept established IPv4 connections
+/sbin/iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+# accept all ICMP messages
+/sbin/iptables -A INPUT -p icmp -j ACCEPT
+
+# initialise IPv6
+/sbin/ip6tables -F
+/sbin/ip6tables -F -t mangle
+
+# accept IPv6 connections from localhost
+/sbin/ip6tables -A INPUT -i lo -j ACCEPT
+/sbin/ip6tables -A OUTPUT -o lo -j ACCEPT
+
+# accept established IPv6 connections
+/sbin/ip6tables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+# accept all ICMPv6 messages
+/sbin/ip6tables -A INPUT -p icmpv6 -j ACCEPT
+
+
+@!@
+def print_packetfilter(key, value):
+	items = key.split('/')
+	addrv6 = items[-1]
+	addrv4 = items[-1]
+	# security/packetfilter/package/univention-samba/tcp/139/all=ACCEPT
+	if items[-1].lower() == 'all':
+		addrv4 = ''
+		addrv6 = ''
+	elif items[-1].lower() == 'ipv4':
+		addrv6 = None
+		addrv4 = ''
+	elif items[-1].lower() == 'ipv6':
+		addrv4 = None
+		addrv6 = ''
+	elif ':' in items[-1].lower():
+		addrv4 = None
+	else:
+		addrv6 = None
+
+	if addrv4 is not None:
+		if addrv4:
+			addrv4 = '-d ' + ''.join([ x for x in addrv4 if x in set('0123456789.')])
+		print '/sbin/iptables -A INPUT -p "%(protocol)s" %(addr_args)s --dport %(port)s -j %(action)s' % {
+			'protocol': items[-3],
+			'addr_args': addrv4,
+			'port': items[-2],
+			'action': value,
+			}
+
+	if addrv6 is not None:
+		if addrv6:
+			addrv6 = '-d ' + ''.join([ x for x in addrv6 if x in set('abcdefABCDEF0123456789:.')])
+		print '/sbin/ip6tables -A INPUT -p "%(protocol)s" %(addr_args)s --dport %(port)s -j %(action)s' % {
+			'protocol': items[-3],
+			'addr_args': addrv6,
+			'port': items[-2],
+			'action': value,
+			}
+
+
+def print_descriptions(var):
+	print
+	for key in [ x for x in configRegistry.keys() if x.startswith('%s/' % var) ]:
+		items = key.split('/')
+		pkg = 'user'
+		if key.startswith('security/packetfilter/package/'):
+			pkg = items[3]
+		print '# %s[%s]: %s' % (pkg, items[-1], configRegistry.get(key))
+
+
+filterlist = {}
+
+# get package settings
+if configRegistry.is_true('security/packetfilter/use_packages', True):
+	for key in [ x for x in configRegistry.keys() if x.startswith('security/packetfilter/package/') ]:
+		items = key.split('/')
+		# check if UCR variable is valid: security/packetfilter/package/univention-samba/tcp/139/all=ACCEPT
+		if items[-3] in ['tcp', 'udp'] and items[-2].isdigit():
+			filterlist[ '/'.join(items[-3:]) ] = key
+
+# get user settings
+for key in [ x for x in configRegistry.keys() if x.startswith('security/packetfilter/') and not x.startswith('security/packetfilter/package/') ]:
+	items = key.split('/')
+	# check if UCR variable is valid: security/packetfilter/package/univention-samba/tcp/139/all=ACCEPT
+	if items[-3] in ['tcp', 'udp'] and items[-2].isdigit():
+		filterlist[ '/'.join(items[-3:]) ] = key
+
+# print values
+for ucrkey in filterlist.values():
+	print_descriptions(ucrkey)
+	print_packetfilter(ucrkey, configRegistry[ucrkey])
+
+
+print
+print '# set default policy for incoming traffic'
+policy = configRegistry.get('security/packetfilter/defaultpolicy','ACCEPT').upper()
+print '/sbin/iptables -P INPUT %s' % policy
+print '/sbin/iptables -P OUTPUT ACCEPT'
+print '/sbin/ip6tables -P INPUT %s' % policy
+print '/sbin/ip6tables -P OUTPUT ACCEPT'
+@!@
---
+++ base/univention-firewall/etc/security/packetfilter/packetfilter.local.sh
@@ -0,0 +1,11 @@
+# User defined iptables/ip6tables rules can be placed here and
+# will not be overwritten during system updates.
+# This file will be called automatically by /etc/init.d/univention-firewall
+# during system boot after default rules (defined by UCR) have been set.
+#
+# In dieser Datei können benutzerdefinierte iptables/ip6tables-Regeln definiert 
+# werden. Sie werden während eines Systemupdates nicht überschrieben.
+# Diese Datei wird automatisch durch /etc/init.d/univention-firewall während 
+# des Systemstarts ausgeführt, nachdem die Standard-iptables-Regeln (definiert 
+# durch UCR) gesetzt wurden.
+#
