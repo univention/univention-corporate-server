e30ecbe7ea33432f3ec4bc824893917bf1e61e6c
Stefan Gohmann <gohmann@univention.de>
2011-10-24 20:00:36 +0000

    * Exit if a pre upgrade command failed (Bug #24175)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@28229 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-updater/script/preup.sh
+++ base/univention-updater/script/preup.sh
@@ -371,12 +371,22 @@ apt-get update >&3 2>&3
 # BEGIN -- update to 3.0-0 Bug #23054
 # 1. Install Python2.6-minimal before Python-minimal gets installed to workaround broken /usr/bin/python being used by python-support
 $update_commands_install libssl0.9.8 python2.6-minimal python-central univention-config-wrapper >&3 2>&3
+res=$?
+if [ $res != 0 ]; then
+	echo "ERROR: Failed to upgrade python2.6."
+	exit $res
+fi
 # 2. Upgrade slapd before new libdb4.7 4.7.25-9.3.201105022022 gets installed, which used a different signature than old 4.7.25-6.7.201101311721
 case "$(dpkg-query -W -f '${Status}' slapd)" in
 install*)
 	if dpkg --compare-versions "$(dpkg-query -W -f '${Version}' libdb4.7)" lt 4.7.25-9.3.201105022022
 	then
 		$update_commands_install slapd db4.8-util libdb4.7=4.7.25-6\* >&3 2>&3
+		res=$?
+		if [ $res != 0 ]; then
+			echo "ERROR: Failed to upgrade slapd."
+			exit $res
+		fi
 	fi
 	;;
 esac
