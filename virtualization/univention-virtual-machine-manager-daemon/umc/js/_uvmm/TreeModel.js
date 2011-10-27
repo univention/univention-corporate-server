039307003453e1ecebe43a5473baf8791540f1b3
Alexander Kläser <klaeser@univention.de>
2011-10-27 20:14:37 +0000

    univention-virtual-machine-manager-daemon (1.0.15-1) unstable; urgency=low  .    * added rudimentary GUI-Layout with tree view and icon handling (Bug #22893)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@28431 1b283449-9f2f-0410-b571-8eb228e1a901

--- virtualization/univention-virtual-machine-manager-daemon/umc/js/_uvmm/TreeModel.js
+++ virtualization/univention-virtual-machine-manager-daemon/umc/js/_uvmm/TreeModel.js
@@ -1,35 +1,33 @@
 /*global console MyError dojo dojox dijit umc */
 
-dojo.provide("umc.modules._udm.TreeModel");
+dojo.provide("umc.modules._uvmm.TreeModel");
 
 dojo.require("umc.tools");
 
-dojo.declare('umc.modules._udm.TreeModel', null, {
+dojo.declare('umc.modules._uvmm.TreeModel', null, {
 	// summary:
-	//		Class that implements the tree model for the UDM container hierarchy.
-	// summary:
-	//		This class that implements a tree model for the container hierarchy which
-	//		is use in the UDM navigation module in combination with the diji.Tree widget.
-	//		More details about this model can be found in dijit.tree.model.
+	//		Class that implements the tree model for the UVMM navigation hierarchy.
 
 	// umcpCommand: Function
 	//		Reference to the module specific umcpCommand function.
 	umcpCommand: null,
 
+	// root: Object
+	//		A dummy root node.
+	root: null,
+
 	constructor: function(args) {
 		dojo.mixin(this, args);
+	
+		this.root = {
+			id: '$root$',
+			label: 'Univention Virtual Machine Manager',
+			type: 'root'
+		};
 	},
 
 	getRoot: function(onItem) {
-		this.umcpCommand('udm/nav/container/query').then(dojo.hitch(this, function(data) {
-			var results = dojo.isArray(data.result) ? data.result : [];
-			if (results.length) {
-				onItem(results[0]);
-			}
-			else {
-				console.log('WARNING: No top container could be queried for LDAP navigation! Ignoring error.');
-			}
-		}));
+		onItem(this.root);
 	},
 
 	getLabel: function(item) {
@@ -37,6 +35,9 @@ dojo.declare('umc.modules._udm.TreeModel', null, {
 	},
 
 	mayHaveChildren: function(item) {
+		if (!item.type || item.type == 'node') {
+			return false;
+		}
 		return true;
 	},
 
@@ -45,7 +46,14 @@ dojo.declare('umc.modules._udm.TreeModel', null, {
 	},
 
 	getChildren: function(parentItem, onComplete) {
-		this.umcpCommand('udm/nav/container/query', { container: parentItem.id }).then(dojo.hitch(this, function(data) {
+		if (!parentItem.type || parentItem.type == 'node') {
+			onComplete([]);
+			return
+		}
+
+		this.umcpCommand('uvmm/nav/query', { 
+			parent: parentItem.type == 'root' ? null : parentItem
+		}).then(dojo.hitch(this, function(data) {
 			// sort items alphabetically
 			var results = dojo.isArray(data.result) ? data.result : [];
 			results.sort(umc.tools.cmpObjects('label'));
