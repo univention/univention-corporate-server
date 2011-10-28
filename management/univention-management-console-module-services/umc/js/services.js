b2539cde1aae4545a8de98612e9600440268c501
Jascha Geerds <geerds@univention.de>
2011-10-28 17:26:56 +0000

    univention-management-console-module-services (1.0.0-1) unstable; urgency=low
      * Initial release (Bug #22900)
    
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@28480 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ management/univention-management-console-module-services/umc/js/services.js
@@ -0,0 +1,212 @@
+/*global console MyError dojo dojox dijit umc */
+
+dojo.provide("umc.modules.services");
+
+dojo.require("umc.i18n");
+dojo.require("umc.dialog");
+dojo.require("umc.widgets.ExpandingTitlePane");
+dojo.require("umc.widgets.Grid");
+dojo.require("umc.widgets.Module");
+dojo.require("umc.widgets.Page");
+dojo.require("umc.widgets.SearchForm");
+
+dojo.declare("umc.modules.services", [ umc.widgets.Module, umc.i18n.Mixin ], {
+
+	moduleStore: null,
+	_grid: null,
+	_page: null,
+	_searchWidget: null,
+
+	i18nClass: 'umc.modules.services',
+	idProperty: 'service',
+
+	buildRendering: function() {
+		this.inherited(arguments);
+
+		this._page = new umc.widgets.Page({
+			headerText: this._('System services')
+		});
+		this.addChild(this._page);
+
+		var titlePane = new umc.widgets.ExpandingTitlePane({
+			title: this._('List of services')
+		});
+		this._page.addChild(titlePane);
+
+		var actions = [{
+			name: 'start',
+			label: this._('Start services'),
+			iconClass: 'dijitIconDelete',
+			callback: dojo.hitch(this, function(data) {
+				if (data.length) {
+					var command = 'services/start';
+					var confirmMessage = this._('Please confirm to start the following services: ');
+					var errorMessage = this._('Starting the following services failed: ');
+					this._changeState(data, command, confirmMessage, errorMessage);
+				}
+			}),
+			isStandardAction: false,
+			isMultiAction: true
+		}, {
+			name: 'stop',
+			label: this._('Stop services'),
+			iconClass: 'dijitIconDelete',
+			callback: dojo.hitch(this, function(data) {
+				if (data.length) {
+					var command = 'services/stop';
+					var confirmMessage = this._('Please confirm to stop the following services: ');
+					var errorMessage = this._('Stopping the following services failed: ');
+					this._changeState(data, command, confirmMessage, errorMessage);
+				}
+			}),
+			isStandardAction: false,
+			isMultiAction: true
+		}, {
+			name: 'restart',
+			label: this._('Restart services'),
+			iconClass: 'dijitIconDelete',
+			callback: dojo.hitch(this, function(data) {
+				if (data.length) {
+					var command = 'services/restart';
+					var confirmMessage = this._('Please confirm to restart the following services: ');
+					var errorMessage = this._('Restarting the following services failed: ');
+					this._changeState(data, command, confirmMessage, errorMessage);
+				}
+			}),
+			isStandardAction: false,
+			isMultiAction: true
+		}, {
+			name: 'startAutomatically',
+			label: this._('Start automatically'),
+			iconClass: 'dijitIconDelete',
+			callback: dojo.hitch(this, function(data) {
+				var command = 'services/start_auto';
+				var confirmMessage = this._('Please confirm to automatically start the following services: ');
+				var errorMessage = '';
+				this._changeState(data, command, confirmMessage, errorMessage);
+			}),
+			isStandardAction: false,
+			isMultiAction: true
+		}, {
+			name: 'startManually',
+			label: this._('Start manually'),
+			iconClass: 'dijitIconDelete',
+			callback: dojo.hitch(this, function(data) {
+				var command = 'services/start_manual';
+				var confirmMessage = this._('Please confirm to manually start the following services: ');
+				var errorMessage = '';
+				this._changeState(data, command, confirmMessage, errorMessage);
+			}),
+			isStandardAction: false,
+			isMultiAction: true
+		}, {
+			name: 'startNever',
+			label: this._('Start never'),
+			iconClass: 'dijitIconDelete',
+			callback: dojo.hitch(this, function(data) {
+				var command = 'services/start_never';
+				var confirmMessage = this._('Please confirm to never start the following services: ');
+				var errorMessage = '';
+				this._changeState(data, command, confirmMessage, errorMessage);
+			}),
+			isStandardAction: false,
+			isMultiAction: true
+		}];
+
+		var columns = [{
+			name: 'service',
+			label: this._('Name'),
+            width: '200px'
+		}, {
+			name: 'isRunning',
+			label: this._('Status'),
+            width: 'adjust',
+			formatter: dojo.hitch(this, function(value) {
+				if (value === true) {
+					return this._('running');
+				} else {
+					return this._('stopped');
+				}
+			})
+		}, {
+			name: 'autostart',
+			label: this._('Start type'),
+            width: '100px',
+			formatter: dojo.hitch(this, function(value) {
+				if (value == 'no') {
+					return this._('Never');
+				} else if (value == 'manually') {
+					return this._('Manually');
+				} else {
+					return this._('Automatically');
+				}
+			})
+		}, {
+			name: 'description',
+			label: this._('Description'),
+            width: 'auto',
+			formatter: dojo.hitch(this, function(value) {
+				if (value === null) {
+					return '-';
+				} else {
+					return value;
+				}
+			})
+		}];
+
+		this._grid = new umc.widgets.Grid({
+			region: 'center',
+			actions: actions,
+			columns: columns,
+			moduleStore: this.moduleStore,
+			query: {
+                filter: '*'
+            }
+		});
+		titlePane.addChild(this._grid);
+
+		var widgets = [{
+			type: 'TextBox',
+			name: 'filter',
+			value: '*',
+			label: this._('Keyword')
+		}];
+
+		this._searchWidget = new umc.widgets.SearchForm({
+			region: 'top',
+			widgets: widgets,
+			layout: [[ 'filter', 'submit', 'reset' ]],
+			onSearch: dojo.hitch(this._grid, 'filter')
+		});
+
+		titlePane.addChild(this._searchWidget);
+
+		this._page.startup();
+    },
+
+	_changeState: function(data, command, confirmMessage, errorMessage) {
+		confirmMessage += '<ul>';
+		dojo.forEach(data, function(idata) {
+			confirmMessage += '<li>' + idata + '</li>';
+		});
+		confirmMessage += '</ul>';
+
+		umc.dialog.confirm(confirmMessage, [{
+			label: this._('OK'),
+			callback: dojo.hitch(this, function() {
+				this.standby(true);
+				umc.tools.umcpCommand(command, data).then(
+					dojo.hitch(this, function(response) {
+						this.standby(false);
+						if (response.success === false) {
+							umc.dialog.alert(errorMessage);
+						}
+						var data = this._searchWidget.gatherFormValues();
+						this._grid.filter(data);
+					}));
+			})
+		}, {
+			label: this._('Cancel')
+		}]);
+	}
+});
---
+++ management/univention-management-console-module-services/umc/python/services/__init__.py
@@ -0,0 +1,175 @@
+#!/usr/bin/python2.6
+# -*- coding: utf-8 -*-
+#
+# Univention Management Console
+#  module: manages system services
+#
+# Copyright 2011 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
+import subprocess
+from fnmatch import fnmatch
+import notifier
+import notifier.threads
+
+import univention.info_tools as uit
+import univention.management.console as umc
+import univention.management.console.modules as umcm
+from univention.management.console.log import MODULE
+from univention.management.console.protocol.definitions import *
+
+import univention.service_info as usi
+import univention.config_registry as ucr
+
+_ = umc.Translation('univention-management-console-module-services').translate
+
+class Instance(umcm.Base):
+	def _run_it(self, services, action):
+		failed = []
+		for srv in services:
+			if subprocess.call(('invoke-rc.d', srv, action)):
+				failed.append(srv)
+		return failed
+
+	def query(self, request):
+		srvs = usi.ServiceInfo()
+		ucr_reg = ucr.ConfigRegistry()
+		ucr_reg.load()
+
+		result = []
+		for name, srv in srvs.services.items():
+			entry = {}
+			entry['service'] = name
+			if 'description' in srv:
+				entry['description'] = srv['description']
+			else:
+				entry['description'] = None
+			key = '%s/autostart' % name
+			# default: autostart=yes
+			if 'start_type' in srv:
+				key = srv['start_type']
+			if not ucr_reg.get(key):
+				entry['autostart'] = 'yes'
+			elif ucr_reg.get(key).lower() in ('no'):
+				entry['autostart']  = 'no'
+			elif ucr_reg.get(key).lower() in ('manually'):
+				entry['autostart']  = 'manually'
+			else:
+				entry['autostart'] = 'yes'
+			# Check if service is running
+			if srv.running:
+				entry['isRunning'] = True
+			else:
+				entry['isRunning'] = False
+			# Check filter options
+			filter_ = request.options.get('filter', '*')
+			for value in entry.items():
+				if fnmatch(str(value), filter_):
+					result.append(entry)
+					break
+
+		request.status = SUCCESS
+		self.finished(request.id, result)
+
+	def start(self, request):
+		if self.permitted('services/start', request.options):
+			message = {}
+			message['success'] = _('Successfully started')
+			message['failed'] = _('Starting the following services failed:') + '<ul>'
+			cb = notifier.Callback(self._service_changed, request, message)
+			func = notifier.Callback(self._run_it, request.options, 'start')
+			thread = notifier.threads.Simple('services', func, cb)
+			thread.run()
+		else:
+			message = _('You are not permitted to run this command.')
+			request.status = MODULE_ERR
+			self.finished(request.id, None, message)
+
+	def stop(self, request):
+		if self.permitted('services/stop', request.options):
+			message = {}
+			message['success'] = _('Successfully stopped')
+			message['failed'] = _('Stopping the following services failed:') + '<ul>'
+			cb = notifier.Callback(self._service_changed, request, message)
+			func = notifier.Callback(self._run_it, request.options, 'stop')
+			thread = notifier.threads.Simple('services', func, cb)
+			thread.run()
+		else:
+			message = _('You are not permitted to run this command.')
+			request.status = MODULE_ERR
+			self.finished(request.id, None, message)
+
+	def restart(self, request):
+		MODULE.error(str(request.arguments))
+		if self.permitted('services/restart', request.options):
+			message = {}
+			message['success'] = _('Successfully restarted')
+			message['failed'] = _('Restarting the following services failed:') + '<ul>'
+			cb = notifier.Callback(self._service_changed, request, message)
+			func = notifier.Callback(self._run_it, request.options, 'restart')
+			thread = notifier.threads.Simple('services', func, cb)
+			thread.run()
+		else:
+			message = _('You are not permitted to run this command.')
+			request.status = MODULE_ERR
+			self.finished(request.id, None, message)
+
+	def _service_changed(self, thread, result, request, message):
+		if result:
+			if len(request.options) == 1:
+				error_message = '%s %s' % (message['failed'], result)
+				request.status = MODULE_ERR
+				self.finished(request.id, {'success': False}, error_message)
+			else:
+				request.status = SUCCESS
+				self.finished(request.id, {'result': result, 'success': False})
+		else:
+			request.status = SUCCESS
+			self.finished(request.id, {'success': True}, message['success'])
+
+	def start_type(self, request):
+		srvs = usi.ServiceInfo()
+
+		for name in request.options:
+			srv = srvs.services.get(name, None)
+			if srv:
+				key = '%s/autostart' % name
+				if 'start_type' in srv:
+					key = srv['start_type']
+
+				value = 'yes'
+				if request.arguments[0] == 'services/start_auto':
+					value = 'yes'
+				if request.arguments[0] == 'services/start_manual':
+					value = 'manually'
+				if request.arguments[0] == 'services/start_never':
+					value = 'no'
+				ucr.handler_set(['%s=%s' % (key, value)])
+
+		message = _('Successfully changed start type')
+		self.finished(request.id, None, message)
+
