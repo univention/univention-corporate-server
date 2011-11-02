4df19892477c245710c017744075f05bbe4bf1a5
Alexander Kläser <klaeser@univention.de>
2011-11-02 16:16:22 +0000

    univention-lib (1.0.36-1) unstable; urgency=low  .    * sheel/umc.sh: added check whether UMC frontend is installed for      umc_frontend_new_hash (Bug #22881)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@28659 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-lib/shell/umc.sh
+++ base/univention-lib/shell/umc.sh
@@ -36,6 +36,9 @@ eval "$(ucr shell ldap/base)"
 BIND_ARGS="$@"
 
 umc_frontend_new_hash () {
+	# make sure that the UMC frontend is installed
+	[ -d /usr/share/univention-management-console-frontend ] || return 
+
 	# create new timestamps for index.html and debug.html in order to
 	# avoid caching problems in browsers
 	timestamp=$(date +'%Y%d%m%H%M%S')
