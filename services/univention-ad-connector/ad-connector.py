8071f2f4ff2efb120325df87181c33223a5d6cac
Stefan Gohmann <gohmann@univention.de>
2011-11-02 14:53:25 +0000

    * Use cPickle.Pickler an clear_memo, otherwise the internal python   refcount will not be decreased  and the memeory is lost (Bug #24273)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@28655 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-ad-connector/ad-connector.py
+++ services/univention-ad-connector/ad-connector.py
@@ -67,31 +67,38 @@ def handler(dn, new, old, command):
 	try:
 		for directory in dirs:
 			if not os.path.exists(os.path.join(directory, 'tmp')):
-				os.mkdir(os.path.join(directory, 'tmp'))
+				os.makedirs(os.path.join(directory, 'tmp'))
 
 			old_dn=None
 			if os.path.exists(os.path.join(directory, 'tmp','old_dn')):
 				f=open(os.path.join(directory, 'tmp','old_dn'),'r')
-				old_dn=cPickle.load(f)
+				p=cPickle.Pickler(f)
+				old_dn=p.load()
+				p.clear_memo()
 				f.close()
 			if command == 'r':
 				filename=os.path.join(directory, 'tmp','old_dn')
 
 				f=open(filename, 'w+')
 				os.chmod(filename, 0600)
-				cPickle.dump(dn, f)
+				p=cPickle.Pickler(f)
+				old_dn=p.dump(dn)
+				p.clear_memo()
+				f.close()
 				f.close()
 			else:
-				object=(dn, new, old, old_dn)
+				ob=(dn, new, old, old_dn)
 
 				filename=os.path.join(directory,"%f"%time.time())
 	
 				if new and 'univentionGroup' in new.get('objectClass', []):
-					group_objects.append(object)
+					group_objects.append(ob)
 
 				f=open(filename, 'w+')
 				os.chmod(filename, 0600)
-				cPickle.dump(object, f)
+				p=cPickle.Pickler(f)
+				p.dump(ob)
+				p.clear_memo()
 				f.close()
 
 				if os.path.exists(os.path.join(directory, 'tmp','old_dn')):
@@ -121,12 +128,14 @@ def postrun():
 		listener.setuid(0)
 		try:
 			init_mode = False
-			for object in group_objects:
+			for ob in group_objects:
 				for directory in dirs:
 					filename=os.path.join(directory,"%f"%time.time())
 					f=open(filename, 'w+')
 					os.chmod(filename, 0600)
-					cPickle.dump(object, f)
+					p=cPickle.Pickler(f)
+					p.dump(ob)
+					p.clear_memo()
 					f.close()
 		finally:
 			listener.unsetuid()
