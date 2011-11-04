dbcb561a3047fb538970c44a63a79dc5bed6e350
Andreas Büsching <buesching@univention.de>
2011-11-04 13:53:24 +0000

    enhance upload functionality and fix minor bugs; Bug #23333 ; add upload test web page ; TextArea widget
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@28748 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ management/univention-management-console-frontend/umc/widgets/TextArea.js
@@ -0,0 +1,52 @@
+/*global dojo dijit dojox umc console */
+
+dojo.provide("umc.widgets.TextArea");
+
+dojo.require("dijit.form.SimpleTextarea");
+dojo.require("umc.widgets._FormWidgetMixin");
+dojo.require("umc.tools");
+
+dojo.declare("umc.widgets.TextArea", [ dijit.form.SimpleTextarea, umc.widgets._FormWidgetMixin ], {
+	// the widget's class name as CSS class
+	'class': 'umcTextArea',
+
+	// dynamicValue: String|Function
+	//		Either an UMCP command to query a value from or a javascript function.
+	//		The javascript function may return a String or a dojo.Deferred object.
+	dynamicValue: null,
+
+	// depends: String?|String[]?
+	//		Specifies that values need to be loaded dynamically depending on
+	//		other form fields.
+	depends: null,
+
+	// umcpCommand:
+	//		Reference to the umcpCommand the widget should use.
+	//		In order to make the widget send information such as module flavor
+	//		etc., it can be necessary to specify a module specific umcpCommand
+	//		method.
+	umcpCommand: umc.tools.umcpCommand,
+
+	//FIXME: the name should be different from _loadValues, e.g., _dependencyUpdate,
+	//       and the check for all met dependencies should be done in the Form
+	_loadValues: function(/*Object?*/ params) {
+		// mixin additional options for the UMCP command
+		if (this.dynamicOptions && dojo.isObject(this.dynamicOptions)) {
+			dojo.mixin(params, this.dynamicOptions);
+		}
+
+		// get the dynamic values, block concurrent events for value loading
+		var func = umc.tools.stringOrFunction(this.dynamicValue, this.umcpCommand);
+		var deferredOrValues = func(params);
+
+		// make sure we have an array or a dojo.Deferred object
+		if (deferredOrValues) {
+			dojo.when(deferredOrValues, dojo.hitch(this, function(res) {
+				this.set('value', res);
+			}));
+		}
+	}
+});
+
+
+
