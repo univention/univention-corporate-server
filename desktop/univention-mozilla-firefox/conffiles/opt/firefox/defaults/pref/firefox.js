e656a4f1db28bd363cd826c4fecdf1423dd3bb0c
Moritz Mühlenhoff <muehlenhoff@univention.de>
2011-11-04 07:44:46 +0000

    Default to session restore (Bug #22116)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@28722 1b283449-9f2f-0410-b571-8eb228e1a901

--- desktop/univention-mozilla-firefox/conffiles/opt/firefox/defaults/pref/firefox.js
+++ desktop/univention-mozilla-firefox/conffiles/opt/firefox/defaults/pref/firefox.js
@@ -189,7 +189,7 @@ else:
 
 // 0 = blank, 1 = home (browser.startup.homepage), 2 = last visited page, 3 = resume previous browser session
 // The behavior of option 3 is detailed at: http://wiki.mozilla.org/Session_Restore
-pref("browser.startup.page",                1);
+pref("browser.startup.page",                3);
 
 
 @!@
