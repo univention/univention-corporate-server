543f97478b32dcf48cdafe23b87200b4418352e6
Andreas Büsching <buesching@univention.de>
2011-11-15 18:08:25 +0000

    help: depending on the current language link to the german or english (fallback) contact formular; Bug #24634
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@29193 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console-frontend/umc/help.js
+++ management/univention-management-console-frontend/umc/help.js
@@ -15,6 +15,7 @@ dojo.mixin(umc.help, new umc.i18n.Mixin({
 	show: function() {
 		var introduction = this._( 'Univention Management Console (UMC) is a modularly designed, web-based application to administrate local computers. The application allows the user to control services, check system utilisation, and check or adjust system parameters.  The web-based interface of UMC is described in detail the in current manual of Univention Corporate Server (UCS). Besides the manual, the homepage of Univention provides additional documentation for Univention Corporate Server. These documents assist in different topics like installation or administration.' );
 
+		var lang = dojo.locale.slice( 0, 2 ).toLowerCase();
 		var keys = {
 			introduction : introduction,
 			manual : this._( 'Current manual (PDF)' ),
@@ -23,6 +24,7 @@ dojo.mixin(umc.help, new umc.i18n.Mixin({
 			wiki : this._( 'Univention wiki site' ),
 			forum : this._( 'Univention forum' ),
 			support : this._( 'Univention support' ),
+			supportURL : lang == 'de' ? 'http://www.univention.de/univention/kontakt/kontaktformular/' : 'http://www.univention.de/en/about-univention/contact/',
 			titleDoc : this._( 'Manual and documentation' ),
 			titleSup : this._( 'Supplementary information' ),
 			titleAss : this._( 'Support and assistance' )
