8bfbc9593444a44b587b7b186c3a825b89e9fe43
Andreas Büsching <buesching@univention.de>
2012-01-05 08:17:14 +0000

    because of the new umask of the UMC process the execute rights has not been removed completely; this is fixed by using chmod a-x instead of chmod -x (Bug #25271)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@30154 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-updater/umc/python/updater/__init__.py
+++ base/univention-updater/umc/python/updater/__init__.py
@@ -1382,15 +1382,15 @@ class Instance(umcm.Base):
 #:logfile: %s
 #:lines: %s
 #:command: %s
-dpkg-statoverride --add root root 0644 /usr/sbin/univention-management-console-web-server
-dpkg-statoverride --add root root 0644 /usr/sbin/univention-management-console-server
-dpkg-statoverride --add root root 0644 /usr/sbin/apache2
-chmod -x /usr/sbin/univention-management-console-server /usr/sbin/univention-management-console-web-server /usr/sbin/apache2
+/usr/sbin/dpkg-statoverride --add root root 0644 /usr/sbin/univention-management-console-web-server
+/usr/sbin/dpkg-statoverride --add root root 0644 /usr/sbin/univention-management-console-server
+/usr/sbin/dpkg-statoverride --add root root 0644 /usr/sbin/apache2
+/bin/chmod a-x /usr/sbin/univention-management-console-server /usr/sbin/univention-management-console-web-server /usr/sbin/apache2
 %s < /dev/null
-dpkg-statoverride --remove /usr/sbin/univention-management-console-web-server
-dpkg-statoverride --remove /usr/sbin/univention-management-console-server
-dpkg-statoverride --remove /usr/sbin/apache2
-chmod +x /usr/sbin/univention-management-console-server /usr/sbin/univention-management-console-web-server /usr/sbin/apache2
+/usr/sbin/dpkg-statoverride --remove /usr/sbin/univention-management-console-web-server
+/usr/sbin/dpkg-statoverride --remove /usr/sbin/univention-management-console-server
+/usr/sbin/dpkg-statoverride --remove /usr/sbin/apache2
+/bin/chmod a+x /usr/sbin/univention-management-console-server /usr/sbin/univention-management-console-web-server /usr/sbin/apache2
 ''' % (started,detail,logfile,lines,command,command)
 		p1 = subprocess.Popen( [ 'LC_ALL=C at now', ], stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True )
 		(stdout,stderr) = p1.communicate( script )
@@ -1413,7 +1413,7 @@ chmod +x /usr/sbin/univention-management-console-server /usr/sbin/univention-man
 		for line in atqout.splitlines():
 			job = line.split('\t',1)[0]
 			if job.isdigit():
-				p2 = subprocess.Popen(['usr/bin/at','-c',job], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+				p2 = subprocess.Popen(['/usr/bin/at','-c',job], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 				(atout,stderr) = p2.communicate()
 				for inst in INSTALLERS:
 					if 'command' in INSTALLERS[inst]:
