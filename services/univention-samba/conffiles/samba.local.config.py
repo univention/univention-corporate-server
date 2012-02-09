657eaf14f25d47ede4e01dbdcba14c6b8d4513e1
Felix Botner <botner@univention.de>
2012-02-09 08:20:07 +0000

    * uses univention.lib.listenerSharePath.createOrRename()   to create or rename share path in samba-shares.py * added modrdn support in samba-shares.py   (Bug #26025)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@30802 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-samba/conffiles/samba.local.config.py
+++ services/univention-samba/conffiles/samba.local.config.py
@@ -4,7 +4,7 @@
 # Univention Samba 
 #  this script creates samba configurations from ucr values
 #
-# Copyright 2004-2011 Univention GmbH
+# Copyright 2004-2012 Univention GmbH
 #
 # http://www.univention.de/
 #
