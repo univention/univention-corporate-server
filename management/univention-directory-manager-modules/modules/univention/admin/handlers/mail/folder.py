2b814075a48923ce4310efa48aa07839433b85d8
Janek Walkenhorst <walkenhorst@univention.de>
2012-02-14 12:04:58 +0000

    univention-directory-manager-modules (7.0.220-1) unstable; urgency=low
      * mail/folder: ignore empty string where list is required (Bug #25407)
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@30908 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-directory-manager-modules/modules/univention/admin/handlers/mail/folder.py
+++ management/univention-directory-manager-modules/modules/univention/admin/handlers/mail/folder.py
@@ -278,10 +278,14 @@ class object(univention.admin.handlers.simpleLdap):
 				else:
 					_sre = univention.admin.syntax.GroupName.regex
 				for acl in self[ attr ]:
+					if acl == '':
+						continue
 					if _sre.match( acl[ 0 ] ):
 						new_acls_tmp.append( ' '.join( acl ) )
 			else:
 				for acl in self[attr]:
+					if acl == '':
+						continue
 					new_acls_tmp.append( ' '.join( acl ) )
 
 		if rewrite_acl:
