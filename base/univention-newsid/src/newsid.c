165e11b690ddf0e6201f5bc68b10a8d7d92ee443
Tobias Peters <peters@univention.de>
2012-05-04 07:39:19 +0000

    * added help option  (Bug #25627)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@32629 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-newsid/src/newsid.c
+++ base/univention-newsid/src/newsid.c
@@ -73,7 +73,35 @@ char *sid_to_string(fstring sidstr_out, const DOM_SID *sid)
 int main ( int argc, char * argv[] ) {
   DOM_SID sid;
   fstring sid_str;
-
+  int index_options;
+  int help_flag=0;
+  char help_text[] = "univention-newsid: Generates a new SID\n\n"
+  "Syntax:\n"
+  "\t/usr/sbin/univention-newsid [options]\n\n"
+  "Options:\n"
+    "\t-h | --help:\n"
+    "\t  print this usage message and exit program\n\n";
+ 
+  
+  if(argc >= 1){
+    for(index_options=1;index_options<argc;index_options++){
+      
+      if (strcmp(argv[index_options],"--help")==0 || strcmp(argv[index_options],"-h")==0) {
+      help_flag=1;
+      }
+      else{
+        fprintf(stdout, "illegal option: %s\n",argv[index_options]);
+	help_flag=1;
+      }
+    }
+  }
+  
+ if(help_flag==1){
+   fprintf(stdout,"%s",help_text);
+   exit(0);
+ }
+  
+ 
   generate_random_sid( &sid );
   sid_to_string ( sid_str, &sid );
 
