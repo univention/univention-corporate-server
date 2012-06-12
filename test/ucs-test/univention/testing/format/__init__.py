f1983145059fb39425ec1880ebdfcab93d360513
Philipp Hahn <hahn@univention.de>
2012-06-12 08:13:11 +0000

    Bug #23527: Add JUnit4 format
    This is based on
    <https://svn.jenkins-ci.org/trunk/hudson/dtkit/dtkit-format/dtkit-junit-model/src/main/resources/com/thalesgroup/dtkit/junit/model/xsd/junit-4.xsd>
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@33519 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/univention/testing/format/__init__.py
+++ test/ucs-test/univention/testing/format/__init__.py
@@ -2,4 +2,5 @@ from tap import TAP as format_tap
 from text import Text as format_text
 from html import HTML as format_html
 from jenkins import Jenkins as format_jenkins
+from junit import Junit as format_junit
 FORMATS = [f[7:] for f in dir() if f.startswith('format_')]
