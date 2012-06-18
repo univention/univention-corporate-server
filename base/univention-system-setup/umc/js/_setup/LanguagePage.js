56940569187f1392466a3ba90885b033fb277964
Dirk Wiesenthal <wiesenthal@univention.de>
2012-06-18 16:18:02 +0000

    univention-system-setup (5.0.165-1) unstable; urgency=low  .    * Fix problems with asynchronous calls. Problem may still    occur. (Bug #26732)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@33636 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-system-setup/umc/js/_setup/LanguagePage.js
+++ base/univention-system-setup/umc/js/_setup/LanguagePage.js
@@ -127,12 +127,12 @@ dojo.declare("umc.modules._setup.LanguagePage", [ umc.widgets.Page, umc.i18n.Mix
 				default_locale_default = parts[0] + '_' + parts[1] + '.UTF-8:UTF-8';
 			}
 			var default_locale_keymap = countrycode;
-			this._form.getWidget('locale/keymap').set('value', default_locale_keymap);
-			this._form.getWidget('locale/default').set('value', default_locale_default);
 			this.umcpCommand('setup/lang/default_timezone', {
 				'countrycode': countrycode
 			}).then(dojo.hitch(this, function(data) { 
 				var default_timezone = data.result;
+				this._form.getWidget('locale/keymap').set('value', default_locale_keymap);
+				this._form.getWidget('locale/default').set('value', default_locale_default);
 				this._form.getWidget('timezone').set('value', default_timezone);
 			}));
 		}
