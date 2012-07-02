cdad3377e661986a8e101a71795bec463055a0f1
Philipp Hahn <hahn@univention.de>
2012-07-02 07:22:08 +0000

    Bug #25868: Fix haning rndc proceses
    Using os.spwan(os.P_WAIT, ) blocks the listener including all other
    modules. Use os.P_NOWAIT to fork the rndc processes in the background
    and explicitly wait 15s for their normal termination. After that kill
    all remaining child processes first by SIGTERM, and 5s after that by
    SIGKILL.
    
    Only restart bind9{,-proxy} once.
    
    Consistently use invoke-rc.d to restart the bind9 daemons instead of
    direct call to /etc/init.d/univention-bind*, which breaks chroot
    installations.
    
    Fix quoting errors in join scripts.
    
    Add missing ${python:Depends} in debian/control.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@33916 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-bind/bind.py
+++ services/univention-bind/bind.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python2.6
 # -*- coding: utf-8 -*-
-#
-# Univention Bind
-#  listener script
-#
+# pylint: disable-msg=C0103,W0622,W0312
+"""
+Univention Bind listener script
+
+During the update period, only create the configuration snippets for named and
+the proxy.
+During the quiet period check the cache directory (is-state) against the
+configuration directory (should-state) and reload/restart as appropriate.
+"""
 # Copyright 2001-2012 Univention GmbH
 #
 # http://www.univention.de/
@@ -31,140 +36,257 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
-__package__='' 	# workaround for PEP 366
+__package__ = '' 	# workaround for PEP 366
 import listener
-import univention_baseconfig
-import os, grp
-import univention.debug
+import os
+import univention.debug as ud  # pylint: disable-msg=E0611
+import time
+import errno
+import signal
 
-name='bind'
-description='Update BIND zones'
-filter='(&(objectClass=dNSZone)(relativeDomainName=@)(zoneName=*))'
-attributes=[]
+name = 'bind'
+description = 'Update BIND zones'
+filter = '(&(objectClass=dNSZone)(relativeDomainName=@)(zoneName=*))'
+attributes = []
 
-named_conf_file   = "/etc/bind/univention.conf"
-named_conf_dir    = "/etc/bind/univention.conf.d"
-proxy_conf_file   = "/etc/bind/univention.conf.proxy"
+NAMED_CONF_FILE = "/etc/bind/univention.conf"
+NAMED_CONF_DIR  = "/etc/bind/univention.conf.d"
+PROXY_CONF_FILE = "/etc/bind/univention.conf.proxy"
+NAMED_CACHE_DIR = "/var/cache/bind"
+PROXY_CACHE_DIR = "/var/cache/univention-bind-proxy"
+RNDC_BIN = "/usr/sbin/rndc"
+
+SIGNAL = dict([(getattr(signal, _), _) for _ in dir(signal) if
+	_.startswith('SIG') and not _.startswith('SIG_')])
 
 def initialize():
+	"""Initialize module on first run."""
 	pass
 
+
+def prerun():
+	"""Called before busy period."""
+	listener.configRegistry.load()
+
+
 def handler(dn, new, old):
-	baseConfig = univention_baseconfig.baseConfig()
-	baseConfig.load()
+	"""Handle LDAP changes."""
+	base = listener.configRegistry.get('dns/ldap/base')
+	if base and not dn.endswith(base):
+		return
+
 	listener.setuid(0)
 	try:
-		if (baseConfig.has_key('dns/ldap/base') and baseConfig['dns/ldap/base'] and dn.endswith(baseConfig['dns/ldap/base'])) or not (baseConfig.has_key('dns/ldap/base') and baseConfig['dns/ldap/base']):
-			if new and not old:
-				new_zone(baseConfig, new['zoneName'][0], dn)
-			elif old and not new:
-				remove_zone(old['zoneName'][0])
-			if new.has_key('zoneName'):
-				zonefile=os.path.join('/var/cache/univention-bind-proxy', new['zoneName'][0]) + ".zone"
-				f=open(zonefile, 'w')
-				f.close()
-				os.chmod(zonefile, 0640)
+		if new and not old:
+			# Add
+			_new_zone(listener.configRegistry, new['zoneName'][0], dn)
+		elif old and not new:
+			# Remove
+			_remove_zone(old['zoneName'][0])
+		if new.get('zoneName'):
+			# Change
+			# Create an empty file to trigger the postrun()
+			zonefile = os.path.join(PROXY_CACHE_DIR, "%s.zone" % (new['zoneName'][0],))
+			proxy_cache = open(zonefile, 'w')
+			proxy_cache.close()
+			os.chmod(zonefile, 0640)
 	finally:
 		listener.unsetuid()
 
-def ldap_auth_string(baseConfig):
 
-	account=baseConfig.get('bind/binddn', baseConfig.get('ldap/hostdn')).replace(',', '%2c')
+def _ldap_auth_string(ucr):
+	"""Build extended LDAP query URI part containing bind credentials."""
+	account = ucr.get('bind/binddn', ucr.get('ldap/hostdn')).replace(',', '%2c')
 
-	pwdfile=baseConfig.get('bind/bindpw', '/etc/machine.secret')
-	pwd=open(pwdfile).readlines()
-
+	pwdfile = ucr.get('bind/bindpw', '/etc/machine.secret')
+	pwd = open(pwdfile).readlines()
 
 	return '????!bindname=%s,!x-bindpw=%s,x-tls' % (account, pwd[0])
 
-def new_zone(baseConfig, zonename, dn):
 
-	univention.debug.debug(univention.debug.LISTENER, univention.debug.INFO, 'DNS: Creating zone %s' % zonename)
-	if not os.path.exists(named_conf_dir):
-		os.mkdir(named_conf_dir)
+def _new_zone(ucr, zonename, dn):
+	"""Handle addition of zone."""
+	ud.debug(ud.LISTENER, ud.INFO, 'DNS: Creating zone %s' % (zonename,))
+	if not os.path.exists(NAMED_CONF_DIR):
+		os.mkdir(NAMED_CONF_DIR)
 
+	zonefile = os.path.join(NAMED_CONF_DIR, zonename)
 
-	zonefile=os.path.join(named_conf_dir, zonename)
-	f=open(zonefile, 'w').close()
+	# Create empty file and restrict permission
+	named_zone = open(zonefile, 'w')
+	named_zone.close()
 	os.chmod(zonefile, 0640)
 
-	f=open(zonefile, 'w+')
+	# Now fill zone file
+	ldap_uri = "ldap://%s:%s/%s%s" % (
+			ucr.get('bind/ldap/server/ip', '127.0.0.1'),
+			ucr.get('ldap/server/port', '7389'),
+			dn,
+			_ldap_auth_string(ucr)
+			)
+	named_zone = open(zonefile, 'w+')
+	named_zone.write('zone "%s" {\n' % (zonename,))
+	named_zone.write('\ttype master;\n')
+	named_zone.write('\tnotify yes;\n')
+	named_zone.write('\tdatabase "ldap %s 172800";\n' % (ldap_uri,))
+	named_zone.write('};\n')
+	named_zone.close()
 
-	f.write('zone "%s" {\n' % zonename)
-	f.write('\ttype master;\n')
-	f.write('\tnotify yes;\n')
-	f.write('\tdatabase "ldap ldap://%s:%s/%s%s 172800";\n' % (baseConfig.get('bind/ldap/server/ip', '127.0.0.1'), baseConfig.get('ldap/server/port', '7389'), dn, ldap_auth_string(baseConfig)))
-	f.write('};\n')
-	f.close()
+	# Create proxy configuration file
+	proxy_zone = open(os.path.join(NAMED_CONF_DIR, zonename+'.proxy'), 'w')
+	proxy_zone.write('zone "%s" {\n' % (zonename,))
+	proxy_zone.write('\ttype slave;\n')
+	proxy_zone.write('\tfile "%s.zone";\n' % (zonename,))
+	proxy_zone.write('\tmasters port 7777 { 127.0.0.1; };\n')
+	proxy_zone.write('};\n')
+	proxy_zone.close()
 
-	f=open(os.path.join(named_conf_dir, zonename+'.proxy'), 'w')
-	f.write('zone "%s" {\n' % zonename)
-	f.write('\ttype slave;\n')
-	f.write('\tfile "%s.zone";\n' % zonename)
-	f.write('\tmasters port 7777 { 127.0.0.1; };\n')
-	f.write('};\n')
-	f.close()
 
-def remove_zone(zonename):
-	univention.debug.debug(univention.debug.LISTENER, univention.debug.INFO, 'DNS: Removing zone %s' % zonename)
-	zfile=os.path.join(named_conf_dir, zonename)
-	if os.path.exists(zfile):
-		os.unlink(zfile)
-	if os.path.exists(zfile+'.proxy'):
-		os.unlink(zfile+'.proxy')
+def _remove_zone(zonename):
+	"""Handle removal of zone."""
+	ud.debug(ud.LISTENER, ud.INFO, 'DNS: Removing zone %s' % (zonename,))
+	zonefile = os.path.join(NAMED_CONF_DIR, zonename)
+	# Remove zone file
+	if os.path.exists(zonefile):
+		os.unlink(zonefile)
+	# Remove proxy configuration file
+	if os.path.exists(zonefile + '.proxy'):
+		os.unlink(zonefile + '.proxy')
+
 
 def clean():
+	"""Reset listener state."""
 	listener.setuid(0)
 	try:
-		if os.path.exists(named_conf_file):
-			os.unlink(named_conf_file)
-		open(named_conf_file, 'w').close()
+		if os.path.exists(NAMED_CONF_FILE):
+			os.unlink(NAMED_CONF_FILE)
+		open(NAMED_CONF_FILE, 'w').close()
 
-		if os.path.isdir(named_conf_dir):
-			for f in os.listdir(named_conf_dir):
-				os.unlink(os.path.join(named_conf_dir, f))
-			os.rmdir(named_conf_dir)
+		if os.path.isdir(NAMED_CONF_DIR):
+			for f in os.listdir(NAMED_CONF_DIR):
+				os.unlink(os.path.join(NAMED_CONF_DIR, f))
+			os.rmdir(NAMED_CONF_DIR)
 	finally:
 		listener.unsetuid()
 
-def postrun():
-	baseConfig = univention_baseconfig.baseConfig()
-	baseConfig.load()
 
+def _reload(zones, restart=False):
+	"""Force reload of zones; might restart daemon; returns pids."""
+	pids = {}
+	if zones:
+		# Try to only reload the zones if rndc is available
+		if os.path.exists(RNDC_BIN):
+			for zone in zones:
+				ud.debug(ud.LISTENER, ud.INFO, 'DNS: Reloading zone %s' % (zone,))
+				cmd = ['rndc', '-p 55555', 'reload', zone]
+				pid = os.spawnv(os.P_NOWAIT, RNDC_BIN, cmd)
+				pids[pid] = cmd
+				cmd = ['rndc', '-p 953', 'reload', zone]
+				pid = os.spawnv(os.P_NOWAIT, RNDC_BIN, cmd)
+				pids[pid] = cmd
+		else:
+			restart = True
+	# Fall back to restart, which will temporarily interrupt the service
+	if restart:
+		ud.debug(ud.LISTENER, ud.INFO, 'DNS: Restarting BIND')
+		cmd = ['invoke-rc.d', 'bind9', 'restart']
+		pid = os.spawnv(os.P_NOWAIT, '/usr/sbin/invoke-rc.d', cmd)
+		pids[pid] = cmd
+	return pids
+
+
+def _wait_children(pids, timeout=15):
+	"""Wait for child termination."""
+	# Wait max 15 seconds for forked children
+	timeout += time.time()
+	while pids:
+		try:
+			pid, status = os.waitpid(0, os.WNOHANG)  # non-blocking
+		except OSError, ex:
+			if ex.errno == errno.ECHILD:
+				break  # no more own children
+			else:
+				ud.debug(ud.LISTENER, ud.WARN, 'DNS: Unexpected error: %s' % (ex,))
+		else:
+			if pid:  # only when waitpid() found one child
+				# Ignore unexpected child from other listener modules (Bug #21363)
+				cmd = pids.pop(pid, '')
+				if os.WIFSIGNALED(status):
+					sig = os.WTERMSIG(status)
+					sig = SIGNAL.get(sig, sig)
+					ud.debug(ud.LISTENER, ud.WARN, 'DNS: %d="%s" exited by signal %s' % \
+							(pid, ' '.join(cmd), sig))
+				elif os.WIFEXITED(status):
+					ret = os.WEXITSTATUS(status)
+					if ret:
+						ud.debug(ud.LISTENER, ud.WARN, 'DNS: %d="%s" exited with %d' % \
+								(pid, ' '.join(cmd), ret))
+				else:
+					ud.debug(ud.LISTENER, ud.WARN, 'DNS: %d="%s" exited status %d' % \
+							(pid, ' '.join(cmd), status))
+				continue
+
+		if time.time() > timeout:
+			ud.debug(ud.LISTENER, ud.WARN, 'DNS: Pending children: %s' % \
+					(' '.join(pids),))
+			break
+		time.sleep(1)
+
+
+def _kill_children(pids, timeout=5):
+	"""Kill children."""
+	for pid in pids:
+		try:
+			os.kill(pid, signal.SIGTERM)
+		except OSError, ex:
+			if ex.errno != errno.ESRCH:
+				ud.debug(ud.LISTENER, ud.WARN, 'DNS: Unexpected error: %s' % (ex,))
+	_wait_children(pids, timeout)
+	for pid in pids:
+		try:
+			os.kill(pid, signal.SIGKILL)
+		except OSError, ex:
+			if ex.errno != errno.ESRCH:
+				ud.debug(ud.LISTENER, ud.WARN, 'DNS: Unexpected error: %s' % (ex,))
+	_wait_children(pids, timeout)
+
+
+def postrun():
+	"""Run pending updates."""
 	listener.setuid(0)
 	try:
-		fp = open(named_conf_file, 'w')
-		if os.path.isdir(named_conf_dir):
-			for f in os.listdir(named_conf_dir):
+		# Re-create named and proxy inclusion file
+		named_conf = open(NAMED_CONF_FILE, 'w')
+		proxy_conf = open(PROXY_CONF_FILE, 'w')
+		if os.path.isdir(NAMED_CONF_DIR):
+			for f in os.listdir(NAMED_CONF_DIR):
 				if not f.endswith('.proxy'):
-					fp.write('include "%s";\n' % os.path.join(named_conf_dir, f))
-		fp.close()
-		fp = open(proxy_conf_file, 'w')
-		if os.path.isdir(named_conf_dir):
-			for f in os.listdir(named_conf_dir):
-				if f.endswith('.proxy'):
-					fp.write('include "%s";\n' % os.path.join(named_conf_dir, f))
-		fp.close()
+					named_conf.write('include "%s";\n' % os.path.join(NAMED_CONF_DIR, f))
+				else:
+					proxy_conf.write('include "%s";\n' % os.path.join(NAMED_CONF_DIR, f))
+		named_conf.close()
+		proxy_conf.close()
 
-		if baseConfig.get('dns/backend') in ['samba4', 'none']:
-			univention.debug.debug(univention.debug.LISTENER, univention.debug.INFO, 'DNS: Skip zone reload')
-			listener.unsetuid()
+		ud.debug(ud.LISTENER, ud.INFO, 'DNS: Doing reload')
+
+		if listener.configRegistry.get('dns/backend') in ('samba4', 'none'):
+			ud.debug(ud.LISTENER, ud.INFO, 'DNS: Skip zone reload')
 			return
 
-		univention.debug.debug(univention.debug.LISTENER, univention.debug.INFO, 'DNS: Reloading BIND')
-		restart=False
-		for file in os.listdir('/var/cache/univention-bind-proxy'):
-			if not os.path.exists(os.path.join('/var/cache/bind', file)):
-				restart=True
+		# Restart is needed when new zones are added or old zones removed.
+		restart = False
+		zones = []
+		for filename in os.listdir(PROXY_CACHE_DIR):
+			os.remove(os.path.join(PROXY_CACHE_DIR, filename))
+			if not os.path.exists(os.path.join(NAMED_CACHE_DIR, filename)):
+				restart = True
 			else:
-				zone = file.replace(".zone", "")
-				if os.path.exists('/usr/sbin/rndc'):
-					os.spawnv(os.P_WAIT, '/usr/sbin/rndc', ['rndc', '-p 55555', 'reload', zone])
-					os.spawnv(os.P_WAIT, '/usr/sbin/rndc', ['rndc', '-p 953', 'reload', zone])
-			os.remove(os.path.join('/var/cache/univention-bind-proxy', file))
-		if restart:
-			os.spawnv(os.P_WAIT, '/etc/init.d/univention-bind-proxy', ['univention-bind-proxy', 'restart'])
-			os.spawnv(os.P_WAIT, '/etc/init.d/univention-bind', ['univention-bind', 'restart'])
+				zone = filename.replace(".zone", "")
+				zones.append(zone)
 
+		ud.debug(ud.LISTENER, ud.INFO, 'DNS: Zones: %s' % (zones,))
+		pids = _reload(zones, restart)
+		_wait_children(pids)
+		_kill_children(pids)
 	finally:
 		listener.unsetuid()
