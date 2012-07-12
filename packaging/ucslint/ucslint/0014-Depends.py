9c4347c192281b7c4a29c0f1a42df8697b9471e8
Philipp Hahn <hahn@univention.de>
2012-07-12 07:59:18 +0000

    Bug #24156: Add dependency tests
    Add tests for checking for missing build- and install-time dependencies.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@34143 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ packaging/ucslint/ucslint/0014-Depends.py
@@ -0,0 +1,201 @@
+#!/usr/bin/python
+# vim:set fileencoding=utf-8 sw=4 ts=4 et:
+import os
+import re
+import sys
+from glob import glob
+try:
+    import univention.ucslint.base as uub
+except ImportError:
+    import ucslint.base as uub
+
+
+def debug(lvl, msg):
+    """Print debug message."""
+    if lvl <= debug.level:
+        print >>sys.stderr, '%s: %s' % ('>' * lvl, msg)
+debug.level = 0
+
+
+class UniventionPackageCheck(uub.UniventionPackageCheckBase):
+    RE_FIELD = re.compile("([a-z0-9_]+)[ \t]*(?:(<<|<=|=|>=|>>)[ \t]*([-a-zA-Z0-9.+~]+))?")
+    RE_INIT = re.compile("^(?:File|Subfile): (etc/init.d/.+)$")
+    DEPS = {
+            'uicr': (re.compile("(?:/usr/bin/)?univention-install-(?:config-registry(?:-info)?|service-info)"), set(('univention-config-dev',))),
+            'umcb': (re.compile("(?:/usr/bin/)?dh-umc-module-build"), set(('univention-management-console-dev',))),
+            'ucr': (re.compile("""(?:^|(?<=['";& \t]))(?:/usr/sbin/)?(?:univention-config-registry|ucr)(?:(?=['";& \t])|$)"""), set(('univention-config', '${misc:Depends}'))),
+            'ial': (re.compile("/usr/share/univention-config-registry/init-autostart\.lib"), set(('univention-base-files',))),
+            }
+
+    def __init__(self):
+        uub.UniventionPackageCheckBase.__init__(self)
+        self.name = '0014-Depends'
+
+    def getMsgIds(self):
+        return {
+                '0014-0': [uub.RESULT_WARN, 'failed to open/read file'],
+                '0014-1': [uub.RESULT_ERROR, 'parsing error in debian/control'],
+                '0014-2': [uub.RESULT_ERROR, 'univention-install-... is used in debian/rules, but debian/control lacks a build-dependency on univention-config-dev.'],
+                '0014-3': [uub.RESULT_ERROR, 'dh-umc-module-build is used in debian/rules, but debian/control lacks a build-dependency on univention-management-console-dev.'],
+                '0014-4': [uub.RESULT_ERROR, 'univention-config-registry is used in a .preinst script, but the package lacks a pre-dependency on univention-config.'],
+                '0014-5': [uub.RESULT_ERROR, 'univention-config-registry is used in a maintainer script, but the package lacks a dependency on univention-config.'],
+                '0014-6': [uub.RESULT_WARN, 'init-autostart.lib is sourced by a script, but the package lacks an explicit dependency on univention-base-files.'],
+                '0014-7': [uub.RESULT_WARN, 'The source package contains debian/*.univention- files, but the package is not found in debian/control.'],
+                }
+
+    def postinit(self, path):
+        """Checks to be run before real check or to create pre-calculated data for several runs. Only called once!"""
+        pass
+
+    def _split_field(self, s):
+        """Split control field into parts. Returns generator."""
+        for con in s.split(','):
+            con = con.strip()
+            for dis in con.split('|'):
+                i = dis.find('(')
+                if i >= 0:
+                    dis = dis[:i]
+                pkg = dis.strip()
+                if pkg:
+                    yield pkg
+
+    def _scan_script(self, fn):
+        """find calls to 'univention-install-', 'ucr' and use of 'init-autostart.lib' in file 'fn'."""
+        need = set()
+        debug(1, 'Reading %s' % (fn,))
+        try:
+            f = open(fn, 'r')
+        except (OSError, IOError), e:
+            self.addmsg('0014-0', 'failed to open and read file', filename=fn)
+            return need
+        try:
+            for l in f:
+                for (key, (regexp, pkgs)) in UniventionPackageCheck.DEPS.items():
+                    if regexp.search(l):
+                        debug(2, 'Found %s in %s' % (key.upper(), fn))
+                        need.add(key)
+        finally:
+            f.close()
+        return need
+
+    def check_source(self, source_section):
+        """Check source package for dependencies."""
+        build_arch = source_section.get('Build-Depends', '')
+        build_arch = self._split_field(build_arch)
+        build_arch = set(build_arch)
+        debug(2, 'Build-Depends: %s' % (build_arch,))
+        build_indep = source_section.get('Build-Depends-Indep', '')
+        build_indep = self._split_field(build_indep)
+        build_indep = set(build_indep)
+        debug(2, 'Build-Depends-Indep: %s' % (build_indep,))
+        build_deps = build_arch | build_indep
+
+        fn = os.path.join(self.path, 'debian', 'rules')
+        need = self._scan_script(fn)
+        uses_uicr = 'uicr' in need
+        uses_umcb = 'umcb' in need
+
+        # Assert packages using "univention-install-" build-depens on "univention-config-dev" and depend on "univention-config"
+        if uses_uicr and not build_deps & UniventionPackageCheck.DEPS['uicr'][1]:
+            self.addmsg('0014-2', 'Missing Build-Depends: univention-config-dev', filename=fn)
+        if uses_umcb and not build_deps & UniventionPackageCheck.DEPS['umcb'][1]:
+            self.addmsg('0014-3', 'Missing Build-Depends: univention-management-console-dev', filename=fn)
+
+    def check_package(self, section):
+        """Check binary package for dependencies."""
+        pkg = section['Package']
+        debug(1, 'Package: %s' % (pkg,))
+
+        pre = section.get('Pre-Depends', '')
+        pre = self._split_field(pre)
+        pre = set(pre)
+        debug(2, 'Pre-Depends: %s' % (pre,))
+        dep = section.get('Depends', '')
+        dep = self._split_field(dep)
+        dep = set(dep)
+        debug(2, 'Depends: %s' % (dep,))
+        all = pre | dep
+
+        # Assert packages using "ucr" in preinst pre-depend on "univention-config"
+        for ms in ('preinst',):
+            fn = os.path.join(self.path, 'debian', '%s.%s' % (pkg, ms))
+            if not os.path.exists(fn):
+                continue
+            need = self._scan_script(fn)
+            if 'ucr' in need and not pre & UniventionPackageCheck.DEPS['ucr'][1]:
+                self.addmsg('0014-4', 'Missing Pre-Depends: univention-config', filename=fn)
+
+        # Assert packages using "ucr" depend on "univention-config"
+        for ms in ('postinst', 'prerm', 'postrm'):
+            fn = os.path.join(self.path, 'debian', '%s.%s' % (pkg, ms))
+            if not os.path.exists(fn):
+                continue
+            need = self._scan_script(fn)
+            if 'ucr' in need and not all & UniventionPackageCheck.DEPS['ucr'][1]:
+                self.addmsg('0014-5', 'Missing Depends: univention-config | ${misc:Depends}', filename=fn)
+        p = os.path.join(self.path, '[0-9][0-9]%s.inst' % (pkg,))
+        for fn in glob(p):
+            need = self._scan_script(fn)
+            if 'ucr' in need and not all & UniventionPackageCheck.DEPS['ucr'][1]:
+                self.addmsg('0014-4', 'Missing Depends: univention-config | ${misc:Depends}', filename=fn)
+        # FIXME: scan all other files for ucr as well?
+
+        # Assert packages using "init-autostart.lib" depends on "univention-base-files"
+        init_files = set()
+        init_files.add(os.path.join(self.path, 'debian', '%s.init' % (pkg,)))
+        init_files.add(os.path.join(self.path, 'debian', '%s.init.d' % (pkg,)))
+        try:
+            fn = os.path.join(self.path, 'debian', '%s.univention-config-registry' % (pkg,))
+            if os.path.exists(fn):
+                f = open(fn, 'r')
+                try:
+                    for l in f:
+                        m = UniventionPackageCheck.RE_INIT.match(l)
+                        if m:
+                            fn = os.path.join(self.path, 'conffiles', m.group(1))
+                            init_files.add(fn)
+                finally:
+                    f.close()
+        except IOError, e:
+            self.addmsg('0014-0', 'failed to open and read file', filename=fn)
+        for fn in init_files:
+            if not os.path.exists(fn):
+                continue
+            need = self._scan_script(fn)
+            if 'ial' in need and not all & UniventionPackageCheck.DEPS['ial'][1]:
+                self.addmsg('0014-6', 'Missing Depends: univention-base-files', filename=fn)
+
+    def check(self, path):
+        """ the real check """
+        fn = os.path.join(path, 'debian', 'control')
+        debug(1, 'Reading %s' % (fn,))
+        try:
+            parser = uub.ParserDebianControl(fn)
+            self.path = path
+        except uub.FailedToReadFile, e:
+            self.addmsg('0014-0', 'failed to open and read file', filename=fn)
+            return
+        except uub.UCSLintException, e:
+            self.addmsg('0014-1', 'parsing error', filename=fn)
+            return
+
+        self.check_source(parser.source_section)
+        for section in parser.binary_sections:
+            self.check_package(section)
+
+        # Assert all files debian/$pkg.$suffix belong to a package $pkg declared in debian/control
+        exists = set()
+        for suffix in ('.univention-config-registry', '.univention-config-registry-variables', '.univention-config-registry-categories', '.univention-service'):
+            pat = os.path.join(path, 'debian', '*%s' % (suffix,))
+            exists |= set((os.path.basename(fn)[:-len(suffix)] for fn in glob(pat)))
+        known = set((section['Package'] for section in parser.binary_sections))
+        for fn in exists - known:
+            self.addmsg('0014-7', 'Undeclared package has UCR files', filename=fn)
+
+
+if __name__ == '__main__':
+    upc = UniventionPackageCheck()
+    upc.check(os.path.curdir)
+    msglist = upc.result()
+    for msg in msglist:
+        print str(msg)
