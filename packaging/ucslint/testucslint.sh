abf97daa520153910f6699f5783256f048ba07ac
Philipp Hahn <hahn@univention.de>
2012-07-20 17:57:38 +0000

    Bug #27889: Beautify testucslint
    Add color support.
    Output failed diff.
    Optionally remove correct test results.
    Optionally supress output.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs@34329 1b283449-9f2f-0410-b571-8eb228e1a901

--- packaging/ucslint/testucslint.sh
+++ packaging/ucslint/testucslint.sh
@@ -1,40 +1,60 @@
 #!/bin/bash
 #
+# Run test suite for ucslint
+#
 declare -i RETVAL=0
 
-if [ "$1" = "--update" ] ; then
-	update="1"
-fi
+while [ $# -ge 1 ]
+do
+    case "$1" in
+    --update) update=1 ;;
+    --verbose) verbose=1 ;;
+    --clean) clean=1 ;;
+    --quiet) quiet=1 ;;
+    --color) red=$(tput setaf 1) green=$(tput setaf 2) norm=$(tput op) ;;
+    --) shift ; break ;;
+    -*) exit 1 ;;
+    esac
+    shift
+done
 
-TMPFN=$(mktemp)
-trap "rm -f '$TMPFN'" EXIT
+tmpdir=$(mktemp -d)
+trap "rm -rf '$tmpdir'" EXIT
+tmpresult="$tmpdir/result"
+tmpdiff="$tmpdir/diff"
 
-export PYTHONPATH="$(pwd):$PYTHONPATH"
-UCSLINTPATH="$(pwd)/ucslint"
+export PYTHONPATH="$PWD:$PYTHONPATH"
+BINPATH="$PWD/bin/ucslint"
+UCSLINTPATH="$PWD/ucslint"
 
-for dir in testframework/* ; do
-  if [ -d "$dir" ] ; then
-	  echo -n "Testing $dir "
+for dir in testframework/*
+do
+    if [ -d "$dir" ]
+    then
+        [ -z "$quiet" ] && echo -n "Testing $dir "
 
-	  DIRNAME=$(basename "$dir")
-	  MODULE="${DIRNAME:0:4}"
+        DIRNAME=$(basename "$dir")
+        MODULE="${DIRNAME:0:4}"
 
-	  ( cd "$dir" ; ../../bin/ucslint -p "$UCSLINTPATH" -m "$MODULE" >"$TMPFN" 2>/dev/null)
-	  ./ucslint-sort-output.py "$TMPFN" >"${dir}.test"
+        ( cd "$dir" && "$BINPATH" -p "$UCSLINTPATH" -m "$MODULE" >"$tmpresult" 2>/dev/null )
+        ./ucslint-sort-output.py "$tmpresult" >"${dir}.test"
 
-	  if diff "${dir}.correct" "${dir}.test" >/dev/null 2>&1
-	  then
-		  echo "OK"
-	  else
-		  echo "FAILED"
-		  RETVAL+=1
-      fi
+        if diff -u "${dir}.correct" "${dir}.test" >"$tmpdiff" 2>&1
+        then
+            [ -z "$quiet" ] && echo "${green}OK{$norm}"
+            [ -n "$clean" ] && rm -f "${dir}.test"
+        else
+            [ -z "$quiet" ] && echo "${red}FAILED${norm}"
+            RETVAL+=1
+            [ -n "$verbose" ] && sed "s/^+/${red}&/;s/^-/${green}&/;s/$/${norm}/" "$tmpdiff"
 
-	  if [ -n "$update" ] ; then
-		  echo "USING TESTRESULT AS NEW TEST TEMPLATE"
-		  cp "${dir}.test" "${dir}.correct"
-	  fi
-  fi
+            if [ -n "$update" ]
+            then
+                echo "USING TESTRESULT AS NEW TEST TEMPLATE"
+                cp "${dir}.test" "${dir}.correct"
+            fi
+        fi
+    fi
 done
 
 exit $RETVAL
