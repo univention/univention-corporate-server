777dbe9bbb55546a890d8ea77cfa8ee3d5ffe666
Philipp Hahn <hahn@univention.de>
2012-08-03 18:27:29 +0000

    Bug #18443: 0017: Fix test regression
    Fix passing the directory via the command line argument.
    
    Use base.Tester for correct line counting and extra information on
    column.
    
    Change message to show correct quoting.
    
    Remove testframework/0001-10, because the test moved to 0017.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs@34603 1b283449-9f2f-0410-b571-8eb228e1a901

--- packaging/ucslint/ucslint/0017-Shell.py
+++ packaging/ucslint/ucslint/0017-Shell.py
@@ -1,68 +1,51 @@
 # -*- coding: iso-8859-15 -*-
+"""Find unquoted usage of eval "$(ucr shell)"."""
 
 try:
 	import univention.ucslint.base as uub
 except ImportError:
 	import ucslint.base as uub
 import re
-import os
 
 
 def containsHashBang(path):
 	try:
 		fp = open(path, 'r')
-	except IOError, e:
+	except IOError:
 		return False
-	for line in fp:
-		if '#!/bin/sh' in line or '#!/bin/bash' in line:
-			fp.close()
-			return True
-	return False
-
+	try:
+		for line in fp:
+			if '#!/bin/sh' in line or '#!/bin/bash' in line:
+				return True
+		return False
+	finally:
+		fp.close()
 
 
 class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 	def __init__(self):
 		super(UniventionPackageCheck, self).__init__()
 		self.name = '0017-Shell'
+		self.tester = uub.UPCFileTester()
+		self.tester.addTest(re.compile(r'eval\s+(`|[$][(])\s*(/usr/sbin/)?(ucr|univention-baseconfig|univention-config-registry)\s+shell\s*[^`)]*[`)]\s*'),
+				'0017-1', 'unquoted call of eval "$(ucr shell)"', cntmax=0)
 
 	def getMsgIds(self):
-		return { '0017-1': [ uub.RESULT_WARN,   'script contains unquoted calls of eval $(ucr shell)' ]}
+		return { '0017-1': [ uub.RESULT_WARN,   'script contains unquoted calls of eval "$(ucr shell)"' ]}
 
 	def postinit(self, path):
 		""" checks to be run before real check or to create precalculated data for several runs. Only called once! """
 		pass
 
-
 	def check(self, path):
 		""" the real check """
 		super(UniventionPackageCheck, self).check(path)
 
-		REucr_shell = re.compile('eval\s+(`|[$][(])\s*(/usr/sbin/)?(ucr|univention-baseconfig|univention-config-registry)\s+shell\s*[^`)]*[`)]\s*')
-
 		#
-		# search shell scripts
+		# search shell scripts and execute test
 		#
-		scripts = []
-		files = uub.FilteredDirWalkGenerator('.')
-		for file in files:
-			if file.endswith('.sh') or containsHashBang(file):
-				scripts.append(file[2:])
-
-
-
-		#
-		# check scripts for unquoted eval $(ucr shell) calls
-		#
-		for script in scripts:
-			try:
-				fp = open(script, 'r')
-			except IOError, e:
-				self.debug('could not look for unquoted calls of eval $(ucr shell) in %s' % script)
-			else:
-				lines = fp.readlines()
-				fp.close()
-				for i in range(len(lines)):
-					if REucr_shell.search(lines[i]):
-						self.addmsg('0017-1', '%s, line %i: unquoted call of eval $(ucr shell)' % (script, i+1))
-					
+		for fn in uub.FilteredDirWalkGenerator(path):
+			if fn.endswith('.sh') or containsHashBang(fn):
+				self.tester.open(fn)
+				msglist = self.tester.runTests()
+				self.msg.extend(msglist)
