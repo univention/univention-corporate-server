216d911cfeee96cf33b34994c420a284d68be489
Dirk Wiesenthal <wiesenthal@univention.de>
2012-08-09 14:49:33 +0000

    univention-system-setup (6.0.11-1) unstable; urgency=low  .    * Reuse univention.lib.package_manager instead of own one (Bug #28010)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs@34752 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-system-setup/umc/python/setup/setup_script.py
+++ base/univention-system-setup/umc/python/setup/setup_script.py
@@ -267,90 +267,24 @@ class SetupScript(object):
 		'''
 		pass
 
-import os
-import apt_pkg
-import apt
-import apt.progress
-from apt.cache import FetchFailedException, LockFailedException
-
-class MessageWriter(object):
-	'''Mimics a file object (default: stdout)
-	supports flush and write. Writes no '\\r',
-	writes no empty strings, writes not just spaces.
-	If it writes its tweaking output: '__MSG__:%s\\n' '''
-
-	def __init__(self, file_object=None):
-		if file_object is None:
-			file_object = sys.stdout
-		self.file_object = file_object
-
-	def flush(self):
-		self.file_object.flush()
-
-	def write(self, msg):
-		msg = msg.replace('\r', '').strip()
-		if msg:
-			self.file_object.write('__MSG__:%s\n' % msg)
-
-class FetchProgress(apt.progress.text.AcquireProgress):
-	'''Used to handle information about fetching packages.
-	Writes a lot of __MSG__es, as it uses MessageWriter
-	'''
-	def __init__(self, outfile=None):
-		super(FetchProgress, self).__init__()
-		self._file = MessageWriter()
-
-	# dont use _winch
-	def start(self):
-		super(apt.progress.text.AcquireProgress, self).start()
-		import signal
-		self._signal = signal.SIG_IGN
-
-	# force defaults
-	def _write(self, msg, newline=True, maximize=True):
-		super(FetchProgress, self)._write(msg, newline=False, maximize=False)
-
-class DpkgProgress(apt.progress.base.InstallProgress):
-	'''Progress when installing or removing software.
-	Writes messages (and percentage) from apts status file descriptor
-	'''
-	def __init__(self, script):
-		super(DpkgProgress, self).__init__()
-		self.script = script
-
-	# status == pmstatus
-	def status_change(self, pkg, percent, status):
-		self.script.message('%s' % (status))
-		self.script.step(self.script.tasks_finished * 100 + int(percent))
-
-	# status == pmerror
-	def error(self, pkg, errormsg):
-		self.script.log('ERROR!! %s: %s' % (pkg, errormsg))
-
-#	def start_update(self):
-#		self.script.log('SUPDATE')
-#
-#	def finish_update(self):
-#		self.script.log('FUPDATE')
-#
-#	def conffile(self, current, new):
-#		self.script.log('CONFF', current, new)
-#
-#	def dpkg_status_change(self, pkg, status):
-#		self.script.log('DPKG', pkg, status)
-#
-#	def processing(self, pkg, stage):
-#		self.script.log('PROCESS', pkg, status)
-#
+from univention.lib.package_manager import PackageManager
+from contextlib import contextmanager
 
 class AptScript(SetupScript):
+	'''More or less just a wrapper around
+	univention.lib.package_manager.PackageManager
+	with SetupScript capabilities.
+	'''
 	brutal_apt_options = True
 
 	def up(self, *args, **kwargs):
-		apt_pkg.init()
+		self.package_manager = PackageManager(
+			info_handler=self.message,
+			step_handler=self.step,
+			error_handler=self.error,
+			handle_only_frontend_errors=True, # ignore pmerror status
+		)
 
-		self.always_install = []
-		self.tasks_finished = 0
 		self.roles_package_map = {
 			'domaincontroller_master' : 'univention-server-master',
 			'domaincontroller_backup' : 'univention-server-backup',
@@ -362,48 +296,24 @@ class AptScript(SetupScript):
 		self.current_server_role = self.get_ucr_var('server/role')
 		self.wanted_server_role = self.get_profile_var('server/role')
 
-		# dont ever ask for user input (as there is no user...)
-		os.environ['DEBIAN_FRONTEND'] = 'noninteractive'
-		apt_pkg.config['APT::Get::Assume-Yes'] = 'true'
-		apt_pkg.config['APT::Get::force-yes'] = 'true'
-		apt_pkg.config['APT::Get::AllowUnauthenticated'] = '1'
-		apt_pkg.config['DPkg::Options::'] = '--force-confold'
+	def set_always_install(self, *packages):
+		self.package_manager.always_install = packages
 
-		# advanced options
+	@contextmanager
+	def noninteractive(self):
 		if self.brutal_apt_options:
-			apt_pkg.config['DPkg::Options::'] = '--force-overwrite'
-			apt_pkg.config['DPkg::Options::'] = '--force-overwrite-dir'
-			apt_pkg.config['APT::Get::Trivial-Only'] = 'no'
-			apt_pkg.config['quiet'] = '1'
-
-		# initialise the cache as well as progress objects
-		self.cache = apt.Cache()
-		self.fetch_progress = FetchProgress(sys.stdout)
-		self.dpkg_progress = DpkgProgress(self)
-	
-	def update(self):
-		'''apt-get update
-		Returns success
-		'''
-		try:
-			self.cache.update(self.fetch_progress)
-		except FetchFailedException:
-			self.error(_('Fetch failed'))
-			return False
-		except LockFailedException:
-			self.error(_('Failed to lock'))
-			return False
+			with self.package_manager.brutal_noninteractive():
+				yield
 		else:
-			return True
+			with self.package_manager.noninteractive():
+				yield
+
+	def update(self):
+		with self.noninteractive():
+			return self.package_manager.update()
 
 	def get_package(self, pkg_name):
-		'''Get Package-object for package_name
-		Otherwise write an error
-		'''
-		try:
-			return self.cache[pkg_name]
-		except KeyError:
-			self.error('%s: %s' % (pkg_name, _('No such package')))
+		return self.package_manager.get_package(pkg_name)
 
 	def finish_task(self, *log_msgs):
 		'''Task is finished. Increment counter and inform
@@ -411,59 +321,23 @@ class AptScript(SetupScript):
 		but does not slow us down too much).
 		'''
 		# dont log msgs for now
-		self.tasks_finished += 1
-		self.step(self.tasks_finished * 100)
-		self.cache.open()
+		self.package_manager.add_hundred_percent()
+		self.reopen_cache()
+
+	def reopen_cache(self):
+		self.package_manager.reopen_cache()
 
 	def commit(self, install=None, remove=None, msg_if_failed=''):
-		'''Really commit changes (mark_install or mark_delete)
-		or pass Package-objects that shall be commited.
-		Never forgets to pass progress objects, may print error
-		messages, always reopens cache.
-		'''
-		if install is None:
-			install = []
-		if remove is None:
-			remove = []
-		for pkg in install:
-			pkg.mark_install()
-		for pkg in remove:
-			pkg.mark_delete()
-		try:
-			result = self.cache.commit(fetch_progress=self.fetch_progress, install_progress=self.dpkg_progress)
-			if not result:
-				raise SystemError()
-		except SystemError:
-			if msg_if_failed:
-				self.error(msg_if_failed)
-		# better do a:
-		self.cache.open()
+		with self.noninteractive():
+			self.package_manager.commit(install, remove, msg_if_failed)
 
 	def install(self, *pkg_names):
-		'''Instantly installs packages when found'''
-		pkgs = []
-		for pkg_name in pkg_names:
-			pkg = self.get_package(pkg_name)
-			if pkg is not None:
-				pkgs.append(pkg)
-		self.commit(install=pkgs)
-		for pkg in pkgs:
-			pkg = self.cache[pkg.name] # fresh from cache
-			if not pkg.is_installed:
-				self.error('%s: %s' % (pkg.name, _('Failed to install')))
+		with self.noninteractive():
+			self.package_manager.install(*pkg_names)
 
 	def uninstall(self, *pkg_names):
-		'''Instantly deletes packages when found'''
-		pkgs = []
-		for pkg_name in pkg_names:
-			pkg = self.get_package(pkg_name)
-			if pkg is not None:
-				pkgs.append(pkg)
-		self.commit(install=self.always_install, remove=pkgs)
-		for pkg in pkgs:
-			pkg = self.cache[pkg.name] # fresh from cache
-			if pkg.is_installed:
-				self.error('%s: %s' % (pkg.name, _('Failed to uninstall')))
+		with self.noninteractive():
+			self.package_manager.uninstall(*pkg_names)
 
 	def get_package_for_role(self, role_name):
 		'''Searches for the meta-package that belongs
@@ -472,24 +346,17 @@ class AptScript(SetupScript):
 		try:
 			# get "real" package for server/role
 			pkg_name = self.roles_package_map[role_name]
-			return self.cache[pkg_name]
+			return self.package_manager.cache[pkg_name]
 		except KeyError:
 			self.error(_('Failed to get package for Role %s') % role_name)
 			return False
 
 	def autoremove(self):
-		'''It seems that there is nothing like
-		self.cache.autoremove
-		So, we need to implement it by hand
-		acts as apt-get autoremove
-		'''
-		for pkg_name in self.cache.keys():
-			pkg = self.cache[pkg_name]
-			if pkg.is_auto_removable:
-				self.message(_('Deleting unneeded %s') % pkg.name)
-				pkg.mark_delete()
-		if self.cache.get_changes():
-			self.commit(msg_if_failed=_('Autoremove failed'))
+		with self.noninteractive():
+			self.package_manager.autoremove()
+
+	def down(self):
+		self.package_manager.unlock()
 
 def main(setup_script, exit=True):
 	'''Helper function to run the setup_script and evaluate its
