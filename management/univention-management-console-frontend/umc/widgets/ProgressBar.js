060ecb4b30ca9862e548eb9a3de996b21708ce22
Dirk Wiesenthal <wiesenthal@univention.de>
2012-08-14 12:07:15 +0000

    univention-management-console-frontend (2.0.10-1) unstable; urgency=low  .    * Added ProgressBar (Bug #25558). May be merged with ProgressInfo
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs@34818 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ management/univention-management-console-frontend/umc/widgets/ProgressBar.js
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2011-2012 Univention GmbH
+ *
+ * http://www.univention.de/
+ *
+ * All rights reserved.
+ *
+ * The source code of this program is made available
+ * under the terms of the GNU Affero General Public License version 3
+ * (GNU AGPL V3) as published by the Free Software Foundation.
+ *
+ * Binary versions of this program provided by Univention to you as
+ * well as other copyrighted, protected or trademarked materials like
+ * Logos, graphics, fonts, specific documentations and configurations,
+ * cryptographic keys etc. are subject to a license agreement between
+ * you and Univention and not subject to the GNU AGPL V3.
+ *
+ * In the case you use this program under the terms of the GNU AGPL V3,
+ * the program is provided in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License with the Debian GNU/Linux or Univention distribution in file
+ * /usr/share/common-licenses/AGPL-3; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/*global console MyError dojo dojox dijit umc */
+
+dojo.provide("umc.widgets.ProgressBar");
+
+dojo.require("umc.i18n");
+dojo.require("umc.tools");
+dojo.require("umc.widgets.Text");
+dojo.require("umc.widgets.ContainerWidget");
+dojo.require("dijit.ProgressBar");
+
+dojo.declare("umc.widgets.ProgressBar", [ umc.widgets.ContainerWidget, umc.i18n.Mixin ], {
+	// summary:
+	//		This class provides a widget providing detailed progress information
+
+	i18nClass: 'umc.modules.setup',
+
+	style: 'width: 400px',
+
+	_component: null,
+	_message: null,
+	_progressBar: null,
+	_errors: null,
+	_criticalError: null,
+
+	_initialComponent: null,
+
+	buildRendering: function() {
+		this.inherited(arguments);
+
+		this._component = new umc.widgets.Text( { content : '' , style : 'width: 100%' } );
+		this.addChild( this._component );
+		this._progressBar = new dijit.ProgressBar( { style : 'width: 100%' } );
+		this.addChild( this._progressBar );
+		this._message = new umc.widgets.Text( { content : '&nbsp;', style : 'width: 100%' } );
+		this.addChild( this._message );
+
+		this._progressBar.set( 'value', 0 );
+		this._progressBar.set( 'maximum', 100 );
+
+		this.reset();
+		this.startup();
+	},
+
+	reset: function(initialComponent) {
+		if (initialComponent) {
+			this._initialComponent = initialComponent;
+		}
+		this._criticalError = false;
+		this._errors = [];
+
+		this._component.set( 'content', this._initialComponent);
+
+		// make sure that at least a not breakable space is printed
+		// ... this avoids vertical jumping of widgets
+		this._message.set( 'content', '&nbsp;' );
+
+		this._progressBar.set( 'value', 0 );
+	},
+
+	setInfo: function( component, message, percentage, errors, critical ) {
+		if (component) {
+			this._component.set( 'content', component );
+		}
+		if (percentage) {
+			this._progressBar.set( 'value', percentage );
+		}
+		if (message) {
+			this._message.set( 'content', message );
+		}
+		this._addErrors(errors);
+		if (critical) {
+			this._criticalError = true;
+		}
+	},
+
+	_addErrors: function(errors) {
+		dojo.forEach(errors, dojo.hitch(this, function(error) {
+			if (error) {
+				if (dojo.indexOf(this._errors, error) === -1) {
+					this._errors.push(error);
+				}
+			}
+		}));
+	},
+
+	auto: function(umcpCommand, umcpOptions, callback, pollErrorMsg) {
+		if (pollErrorMsg !== undefined) {
+			pollErrorMsg = this._('Fetching information from the server failed!');
+		}
+		umc.tools.umcpCommand(umcpCommand,
+			umcpOptions, undefined, undefined,
+			{
+				messageInterval: 30,
+				message: pollErrorMsg,
+				xhrTimeout: 40
+			}
+		).then(dojo.hitch(this, function(data) {
+			var result = data.result;
+			if (result) {
+				this.setInfo(result.component, result.info, result.steps, result.errors);
+				this.auto(umcpCommand, umcpOptions, callback, pollErrorMsg);
+			} else {
+				this.stop(callback)
+			}
+		}));
+	},
+
+	stop: function(callback) {
+		var errors = this.getErrors().errors;
+		if (errors.length) {
+			msg = '';
+			if (errors.length == 1) {
+				msg = this._('An error occurred: ') + errors[0];
+			} else {
+				msg = dojo.replace(this._('{number} errors occurred: '), {number : errors.length});
+				msg += '<ul><li>' + errors.join('</li><li>') + '</li></ul>';
+			}
+			umc.dialog.confirm(msg, [{
+				label: 'Ok',
+				'default': true,
+				callback: callback
+			}]);
+		} else {
+			callback();
+		}
+	},
+
+	getErrors: function() {
+		return {'errors' : this._errors, 'critical' : this._criticalError}
+	}
+});
+
