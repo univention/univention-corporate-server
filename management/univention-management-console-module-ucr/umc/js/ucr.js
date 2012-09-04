6fb1db7db4b0959f64657926031f6af52b80f7ad
Alexander Kläser <klaeser@univention.de>
2012-09-04 15:31:17 +0000

    univention-management-console-module-ucr (2.0.0-1) unstable; urgency=low  .    * ported module to Dojo 1.8; Bug #26857
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs@35192 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console-module-ucr/umc/js/ucr.js
+++ management/univention-management-console-module-ucr/umc/js/ucr.js
@@ -26,353 +26,357 @@
  * /usr/share/common-licenses/AGPL-3; if not, see
  * <http://www.gnu.org/licenses/>.
  */
-/*global console MyError dojo dojox dijit umc */
+/*global define*/
 
-dojo.provide("umc.modules.ucr");
+define([
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/_base/kernel",
+	"dojo/_base/array",
+	"dojo/on",
+	"dijit/Dialog",
+	"dijit/form/_TextBoxMixin",
+	"umc/tools",
+	"umc/dialog",
+	"umc/widgets/Form",
+	"umc/widgets/Grid",
+	"umc/widgets/Module",
+	"umc/widgets/Page",
+	"umc/widgets/SearchForm",
+	"umc/widgets/StandbyMixin",
+	"umc/widgets/ExpandingTitlePane",
+	"umc/widgets/TextBox",
+	"umc/widgets/Text",
+	"umc/widgets/HiddenInput",
+	"umc/widgets/ComboBox",
+	"umc/i18n!umc/modules/ucr"
+], function(declare, lang, kernel, array, on, Dialog, _TextBoxMixin, tools, dialog, Form, Grid, Module, Page, SearchForm, StandbyMixin, ExpandingTitlePane, TextBox, Text, HiddenInput, ComboBox, _) {
+	var _DetailDialog = declare([ Dialog, StandbyMixin ], {
+		_form: null,
 
-dojo.require("umc.i18n");
-dojo.require("umc.tools");
-dojo.require("umc.dialog");
-dojo.require("umc.widgets.Form");
-dojo.require("umc.widgets.Grid");
-dojo.require("umc.widgets.Module");
-dojo.require("umc.widgets.Page");
-dojo.require("umc.widgets.SearchForm");
-dojo.require("umc.widgets.StandbyMixin");
-dojo.require("umc.widgets.Text");
-dojo.require("umc.widgets.ExpandingTitlePane");
-dojo.require("umc.widgets._WidgetsInWidgetsMixin");
+		_description: null,
 
-dojo.declare("umc.modules.ucr", [ umc.widgets.Module, umc.i18n.Mixin ], {
-	// summary:
-	//		Module for modifying and displaying UCR variables on the system.
+		moduleStore: null,
 
-	_grid: null,
-	_store: null,
-	_searchForm: null,
-	_detailDialog: null,
-	_contextVariable: null,
-	_page: null,
+		postMixInProperties: function() {
+			// call superclass method
+			this.inherited(arguments);
 
-	moduleID: 'ucr',
-	idProperty: 'key',
+			lang.mixin(this, {
+				title: _( 'Edit UCR variable' ),
+				style: 'max-width: 400px'
+			});
+		},
 
-	buildRendering: function() {
-		// call superclass method
-		this.inherited(arguments);
+		buildRendering: function() {
+			// call superclass method
+			this.inherited(arguments);
 
-		// generate border layout and add it to the module
-		this._page = new umc.widgets.Page({
-			headerText: this._('Univention Configuration Registry'),
-			helpText: this._('The Univention Configuration Registry (UCR) is the local database for the configuration of UCS systems to access and edit system-wide properties in a unified manner. Caution: Changing UCR variables directly results in the change of the system configuration. Misconfiguration may cause an usable system!')
-		});
-		this.addChild(this._page);
+			var widgets = [{
+				type: TextBox,
+				name: 'key',
+				description: _( 'Name of UCR variable' ),
+				label: _( 'UCR variable' )
+			}, {
+				type: TextBox,
+				name: 'value',
+				description: _( 'Value of the UCR variable' ),
+				label: _( 'Value' )
+			}, {
+				type: Text,
+				name: 'description',
+				description: _( 'Description of the UCR variable' ),
+				label: _( 'Description:' )
+			}, {
+				type: HiddenInput,
+				name: 'description[' + kernel.locale + ']'
+	//		}, {
+	//			type: 'MultiSelect',
+	//			name: 'categories',
+	//			description: _( 'Categories that the UCR variable is assoziated with' ),
+	//			label: _( 'Categories' ),
+	//			dynamicValues: 'ucr/categories'
+			}];
 
-		var titlePane = new umc.widgets.ExpandingTitlePane({
-			title: this._('Entries')
-		});
-		this._page.addChild(titlePane);
+			var buttons = [{
+				name: 'submit',
+				label: _( 'Save' ),
+				callback: lang.hitch(this, function() {
+					this._form.save();
+					this.hide();
+				})
+			}, {
+				//FIXME: Should be much simpler. The key name should be enough
+				name: 'cancel',
+				label: _( 'Cancel' ),
+				callback: lang.hitch(this, function() {
+					this.hide();
+				})
+			}];
 
-		//
-		// add data grid
-		//
+			var layout = ['key', 'value', 'description'];//, ['categories']];
 
-		// define actions
-		var actions = [{
-			name: 'add',
-			label: this._( 'Add' ),
-			description: this._( 'Adding a new UCR variable' ),
-			iconClass: 'umcIconAdd',
-			isContextAction: false,
-			isStandardAction: true,
-			callback: dojo.hitch(this, function() {
-				this._detailDialog.newVariable();
-			})
-		}, {
-			name: 'edit',
-			label: this._( 'Edit' ),
-			description: this._( 'Setting the UCR variable, editing the categories and/or description' ),
-			iconClass: 'umcIconEdit',
-			isStandardAction: true,
-			isMultiAction: false,
-			callback: dojo.hitch(this, function(ids) {
-				if (ids.length) {
-					this._detailDialog.loadVariable(ids[0]);
+			this._form = this.own(new Form({
+				style: 'width: 100%',
+				widgets: widgets,
+				buttons: buttons,
+				layout: layout,
+				moduleStore: this.moduleStore,
+				cols: 1
+			}))[0];
+			this._form.placeAt(this.containerNode);
+
+			// simple handler to disable standby mode
+			this.own(on(this._form, 'loaded', lang.hitch(this, function() {
+				// display the description text
+				var descWidget = this._form.getWidget('description');
+				var text = this._form.getWidget('description[' + kernel.locale + ']').get('value');
+				if (text) {
+					// we have description, update the description field
+					descWidget.set('visible', true);
+					descWidget.set('content', '<i>' + text + '</i>');
 				}
-			})
-		}, {
-			name: 'delete',
-			label: this._( 'Delete' ),
-			description: this._( 'Deleting the selected UCR variables' ),
-			iconClass: 'umcIconDelete',
-			isStandardAction: true,
-			isMultiAction: true,
-			callback: dojo.hitch(this, function(ids) {
-				umc.dialog.confirm(this._('Are you sure to delete the %d select UCR variable(s)?', ids.length), [{
-					label: this._('Delete'),
-					callback: dojo.hitch(this, function() {
-						// remove the selected elements via a transaction on the module store
-						var transaction = this.moduleStore.transaction();
-						dojo.forEach(ids, dojo.hitch(this.moduleStore, 'remove'));
-						transaction.commit();
-					})
-				}, {
-					label: this._('Cancel'),
-					'default': true
-				}]);
+				else {
+					// no description -> hide widget and label
+					descWidget.set('visible', false);
+					descWidget.set('content', '');
+				}
 
-			})
-		}];
+				// disable the loading animation
+				this._position();
+				this.standby(false);
+			})));
+			this.own(on(this._form, 'saved', lang.hitch(this, function() {
+				this._position();
+				this.standby(false);
+			})));
+		},
 
-		// define grid columns
-		var columns = [{
-			name: 'key',
-			label: this._( 'UCR variable' ),
-			description: this._( 'Unique name of the UCR variable' )
-		}, {
-			name: 'value',
-			label: this._( 'Value' ),
-			description: this._( 'Value of the UCR variable' )
-		}];
-
-		// generate the data grid
-		this._grid = new umc.widgets.Grid({
-			region: 'center',
-			actions: actions,
-			columns: columns,
-			moduleStore: this.moduleStore,
-			query: {
-				category: "all",
-				key: "all",
-				filter:"*"
-			}
-		});
-		titlePane.addChild(this._grid);
-
-		//
-		// add search widget
-		//
-
-		// define the different search widgets
-		var widgets = [{
-			type: 'ComboBox',
-			name: 'category',
-			value: 'all',
-			description: this._( 'Category the UCR variable should be associated with' ),
-			label: this._('Category'),
-			staticValues: [
-				{ id: 'all', label: this._('All') }
-			],
-			dynamicValues: 'ucr/categories',
-			size: 'TwoThirds'
-		}, {
-			type: 'ComboBox',
-			name: 'key',
-			value: 'all',
-			description: this._( 'Select the attribute of a UCR variable that should be searched for the given keyword' ),
-			label: this._( 'Search attribute' ),
-			staticValues: [
-				{ id: 'all', label: this._( 'All' ) },
-				{ id: 'key', label: this._( 'Variable' ) },
-				{ id: 'value', label: this._( 'Value' ) },
-				{ id: 'description', label: this._( 'Description' ) }
-			],
-			size: 'TwoThirds'
-		}, {
-			type: 'TextBox',
-			name: 'filter',
-			value: '*',
-			description: this._( 'Keyword that should be searched for in the selected attribute' ),
-			label: this._( 'Keyword' ),
-			size: 'TwoThirds'
-		}];
-
-		// generate the search widget
-		this._searchForm = new umc.widgets.SearchForm({
-			region: 'top',
-			widgets: widgets,
-			layout: [[ 'category', 'key', 'filter', 'submit' ]],
-			onSearch: dojo.hitch(this._grid, 'filter')
-		});
-		titlePane.addChild(this._searchForm);
-
-		this._page.startup();
-
-		// make sure that the input field is focused
-		this.connect(this._page, 'onShow', '_selectInputText');
-		this.connect(this._grid, 'onFilterDone', '_selectInputText');
-
-		//
-		// create dialog for UCR variable details
-		//
-
-		this._detailDialog = new umc.modules.ucr._DetailDialog({
-			moduleStore: this.moduleStore
-		});
-		this._detailDialog.startup();
-	},
-
-	_selectInputText: function() {
-		// focus on input widget
-		var widget = this._searchForm.getWidget('filter');
-		widget.focus();
-
-		// select the text
-		if (widget.textbox) {
-			try {
-				dijit.selectInputText(widget.textbox);
-			}
-			catch(err) { }
-		}
-	}
-});
-
-dojo.declare("umc.modules.ucr._DetailDialog", [ dijit.Dialog, umc.widgets.StandbyMixin, umc.widgets._WidgetsInWidgetsMixin, umc.i18n.Mixin ], {
-	_form: null,
-
-	_description: null,
-
-	// use i18n information from umc.modules.ucr
-	i18nClass: 'umc.modules.ucr',
-
-	moduleStore: null,
-
-	postMixInProperties: function() {
-		// call superclass method
-		this.inherited(arguments);
-
-		dojo.mixin(this, {
-			title: this._( 'Edit UCR variable' ),
-			style: 'max-width: 400px'
-		});
-	},
-
-	buildRendering: function() {
-		// call superclass method
-		this.inherited(arguments);
-
-		var widgets = [{
-			type: 'TextBox',
-			name: 'key',
-			description: this._( 'Name of UCR variable' ),
-			label: this._( 'UCR variable' )
-		}, {
-			type: 'TextBox',
-			name: 'value',
-			description: this._( 'Value of the UCR variable' ),
-			label: this._( 'Value' )
-		}, {
-			type: 'Text',
-			name: 'description',
-			description: this._( 'Description of the UCR variable' ),
-			label: this._( 'Description:' )
-		}, {
-			type: 'HiddenInput',
-			name: 'description[' + dojo.locale + ']'
-//		}, {
-//			type: 'MultiSelect',
-//			name: 'categories',
-//			description: this._( 'Categories that the UCR variable is assoziated with' ),
-//			label: this._( 'Categories' ),
-//			dynamicValues: 'ucr/categories'
-		}];
-
-		var buttons = [{
-			name: 'submit',
-			label: this._( 'Save' ),
-			callback: dojo.hitch(this, function() {
-				this._form.save();
-				this.hide();
-			})
-		}, {
-			//FIXME: Should be much simpled. The key name should be enough
-			name: 'cancel',
-			label: this._( 'Cancel' ),
-			callback: dojo.hitch(this, function() {
-				this.hide();
-			})
-		}];
-
-		var layout = ['key', 'value', 'description'];//, ['categories']];
-
-		this._form = this.adopt(umc.widgets.Form, {
-			style: 'width: 100%',
-			widgets: widgets,
-			buttons: buttons,
-			layout: layout,
-			moduleStore: this.moduleStore,
-			cols: 1
-		});
-		this._form.placeAt(this.containerNode);
-
-		// simple handler to disable standby mode
-		this.connect(this._form, 'onLoaded', function() {
-			// display the description text
+		clearForm: function() {
+			var emptyValues = {};
+			tools.forIn(this._form.gatherFormValues(), function(ikey) {
+				emptyValues[ikey] = '';
+			});
+			this._form.setFormValues(emptyValues);
 			var descWidget = this._form.getWidget('description');
-			var text = this._form.getWidget('description[' + dojo.locale + ']').get('value');
-			if (text) {
-				// we have description, update the description field
-				descWidget.set('visible', true);
-				descWidget.set('content', '<i>' + text + '</i>');
-			}
-			else {
-				// no description -> hide widget and label
-				descWidget.set('visible', false);
-				descWidget.set('content', '');
-			}
-
-			// disable the loading animation
+			descWidget.set('content', '');
+			descWidget.set('visible', false);
 			this._position();
+		},
+
+		newVariable: function() {
+			this._form._widgets.key.set('disabled', false);
+			this.clearForm();
 			this.standby(false);
-		});
-		this.connect(this._form, 'onSaved', function() {
-			this._position();
-			this.standby(false);
-		});
+			this.show();
+		},
 
-	},
+		loadVariable: function(ucrVariable) {
+			this._form._widgets.key.set('disabled', true);
 
-	clearForm: function() {
-		var emptyValues = {};
-		umc.tools.forIn(this._form.gatherFormValues(), function(ikey) {
-			emptyValues[ikey] = '';
-		});
-		this._form.setFormValues(emptyValues);
-		var descWidget = this._form.getWidget('description');
-		descWidget.set('content', '');
-		descWidget.set('visible', false);
-		this._position();
-	},
+			// start standing-by mode
+			this.standby(true);
+			this.show();
 
-	newVariable: function() {
-		this._form._widgets.key.set('disabled', false);
-		this.clearForm();
-		this.standby(false);
-		this.show();
-	},
+			// clear form and start the query
+			this.clearForm();
+			this._form.load(ucrVariable);
+		},
 
-	loadVariable: function(ucrVariable) {
-		this._form._widgets.key.set('disabled', true);
+		getValues: function() {
+			// description:
+			//		Collect a property map of all currently entered/selected values.
 
-		// start standing-by mode
-		this.standby(true);
-		this.show();
+			return this._form.gatherFormValues();
+		},
 
-		// clear form and start the query
-		this.clearForm();
-		this._form.load(ucrVariable);
-	},
+		onSubmit: function(values) {
+			// stub for event handling
+		}
+	});
+	return declare("umc.modules.ucr", Module, {
+		// summary:
+		//		Module for modifying and displaying UCR variables on the system.
 
-	getValues: function() {
-		// description:
-		//		Collect a property map of all currently entered/selected values.
+		_grid: null,
+		_store: null,
+		_searchForm: null,
+		_detailDialog: null,
+		_contextVariable: null,
+		_page: null,
 
-		return this._form.gatherFormValues();
-	},
+		moduleID: 'ucr',
+		idProperty: 'key',
 
-	onSubmit: function(values) {
-		// stub for event handling
-	}
+		buildRendering: function() {
+			// call superclass method
+			this.inherited(arguments);
+
+			// generate border layout and add it to the module
+			this._page = new Page({
+				headerText: _('Univention Configuration Registry'),
+				helpText: _('The Univention Configuration Registry (UCR) is the local database for the configuration of UCS systems to access and edit system-wide properties in a unified manner. Caution: Changing UCR variables directly results in the change of the system configuration. Misconfiguration may cause an usable system!')
+			});
+			this.addChild(this._page);
+
+			var titlePane = new ExpandingTitlePane({
+				title: _('Entries')
+			});
+			this._page.addChild(titlePane);
+
+			//
+			// add data grid
+			//
+
+			// define actions
+			var actions = [{
+				name: 'add',
+				label: _( 'Add' ),
+				description: _( 'Adding a new UCR variable' ),
+				iconClass: 'umcIconAdd',
+				isContextAction: false,
+				isStandardAction: true,
+				callback: lang.hitch(this, function() {
+					this._detailDialog.newVariable();
+				})
+			}, {
+				name: 'edit',
+				label: _( 'Edit' ),
+				description: _( 'Setting the UCR variable, editing the categories and/or description' ),
+				iconClass: 'umcIconEdit',
+				isStandardAction: true,
+				isMultiAction: false,
+				callback: lang.hitch(this, function(ids) {
+					if (ids.length) {
+						this._detailDialog.loadVariable(ids[0]);
+					}
+				})
+			}, {
+				name: 'delete',
+				label: _( 'Delete' ),
+				description: _( 'Deleting the selected UCR variables' ),
+				iconClass: 'umcIconDelete',
+				isStandardAction: true,
+				isMultiAction: true,
+				callback: lang.hitch(this, function(ids) {
+					dialog.confirm(_('Are you sure to delete the %d select UCR variable(s)?', ids.length), [{
+						label: _('Delete'),
+						callback: lang.hitch(this, function() {
+							// remove the selected elements via a transaction on the module store
+							var transaction = this.moduleStore.transaction();
+							array.forEach(ids, lang.hitch(this.moduleStore, 'remove'));
+							transaction.commit();
+						})
+					}, {
+						label: _('Cancel'),
+						'default': true
+					}]);
+
+				})
+			}];
+
+			// define grid columns
+			var columns = [{
+				name: 'key',
+				label: _( 'UCR variable' ),
+				description: _( 'Unique name of the UCR variable' )
+			}, {
+				name: 'value',
+				label: _( 'Value' ),
+				description: _( 'Value of the UCR variable' )
+			}];
+
+			// generate the data grid
+			this._grid = new Grid({
+				region: 'center',
+				actions: actions,
+				columns: columns,
+				moduleStore: this.moduleStore,
+				query: {
+					category: "all",
+					key: "all",
+					filter:"*"
+				}
+			});
+			titlePane.addChild(this._grid);
+
+			//
+			// add search widget
+			//
+
+			// define the different search widgets
+			var widgets = [{
+				type: ComboBox,
+				name: 'category',
+				value: 'all',
+				description: _( 'Category the UCR variable should be associated with' ),
+				label: _('Category'),
+				staticValues: [
+					{ id: 'all', label: _('All') }
+				],
+				dynamicValues: 'ucr/categories',
+				size: 'TwoThirds'
+			}, {
+				type: ComboBox,
+				name: 'key',
+				value: 'all',
+				description: _( 'Select the attribute of a UCR variable that should be searched for the given keyword' ),
+				label: _( 'Search attribute' ),
+				staticValues: [
+					{ id: 'all', label: _( 'All' ) },
+					{ id: 'key', label: _( 'Variable' ) },
+					{ id: 'value', label: _( 'Value' ) },
+					{ id: 'description', label: _( 'Description' ) }
+				],
+				size: 'TwoThirds'
+			}, {
+				type: TextBox,
+				name: 'filter',
+				value: '*',
+				description: _( 'Keyword that should be searched for in the selected attribute' ),
+				label: _( 'Keyword' ),
+				size: 'TwoThirds'
+			}];
+
+			// generate the search widget
+			this._searchForm = new SearchForm({
+				region: 'top',
+				widgets: widgets,
+				layout: [[ 'category', 'key', 'filter', 'submit' ]]
+			});
+			titlePane.addChild(this._searchForm);
+			this.own(on(this._searchForm, 'search', lang.hitch(this._grid, 'filter')));
+
+			this._page.startup();
+
+			// make sure that the input field is focused
+			this.own(on(this._page, 'show', lang.hitch(this, '_selectInputText')));
+			this.own(on(this._grid, 'filterDone', lang.hitch(this, '_selectInputText')));
+
+			//
+			// create dialog for UCR variable details
+			//
+
+			this._detailDialog = new _DetailDialog({
+				moduleStore: this.moduleStore
+			});
+			this._detailDialog.startup();
+		},
+
+		_selectInputText: function() {
+			// focus on input widget
+			var widget = this._searchForm.getWidget('filter');
+			widget.focus();
+
+			// select the text
+			if (widget.textbox) {
+				try {
+					_TextBoxMixin.selectInputText(widget.textbox);
+				}
+				catch(err) { }
+			}
+		}
+	});
+
 });
-
 
