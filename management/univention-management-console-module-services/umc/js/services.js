d957d6e4ad1281f644fce9ef0fd804bdb91dd7be
Florian Best <best@univention.de>
2012-09-05 05:53:51 +0000

    ported services module to Dojo 1.8; Bug #26857
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs@35201 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console-module-services/umc/js/services.js
+++ management/univention-management-console-module-services/umc/js/services.js
@@ -26,217 +26,220 @@
  * /usr/share/common-licenses/AGPL-3; if not, see
  * <http://www.gnu.org/licenses/>.
  */
-/*global console MyError dojo dojox dijit umc */
+/*global define*/
 
-dojo.provide("umc.modules.services");
+define([
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/_base/array",
+	"umc/dialog",
+	"umc/widgets/Module",
+	"umc/widgets/Page",
+	"umc/widgets/ExpandingTitlePane",
+	"umc/widgets/Grid",
+	"umc/widgets/SearchForm",
+	"umc/widgets/TextBox",
+	"umc/i18n!umc/modules/services"
+], function(declare, lang, array, dialog, Module, Page, ExpandingTitlePane, Grid, SearchForm, TextBox, _) {
+	return declare("umc.modules.services", [ Module ], {
 
-dojo.require("umc.i18n");
-dojo.require("umc.dialog");
-dojo.require("umc.widgets.ExpandingTitlePane");
-dojo.require("umc.widgets.Grid");
-dojo.require("umc.widgets.Module");
-dojo.require("umc.widgets.Page");
-dojo.require("umc.widgets.SearchForm");
+		moduleStore: null,
+		_grid: null,
+		_page: null,
+		_searchWidget: null,
 
-dojo.declare("umc.modules.services", [ umc.widgets.Module, umc.i18n.Mixin ], {
+		idProperty: 'service',
 
-	moduleStore: null,
-	_grid: null,
-	_page: null,
-	_searchWidget: null,
+		buildRendering: function() {
+			this.inherited(arguments);
 
-	i18nClass: 'umc.modules.services',
-	idProperty: 'service',
+			this._page = new Page({
+				headerText: _('System services')
+			});
+			this.addChild(this._page);
 
-	buildRendering: function() {
-		this.inherited(arguments);
+			var titlePane = new ExpandingTitlePane({
+				title: _('List of services')
+			});
+			this._page.addChild(titlePane);
 
-		this._page = new umc.widgets.Page({
-			headerText: this._('System services')
-		});
-		this.addChild(this._page);
-
-		var titlePane = new umc.widgets.ExpandingTitlePane({
-			title: this._('List of services')
-		});
-		this._page.addChild(titlePane);
-
-		var actions = [{
-			name: 'start',
-			label: this._('Start services'),
-			callback: dojo.hitch(this, function(data) {
-				if (data.length) {
-					var command = 'services/start';
-					var confirmMessage = this._('Please confirm to start the following services: ');
-					var errorMessage = this._('Starting the following services failed: ');
+			var actions = [{
+				name: 'start',
+				label: _('Start services'),
+				callback: lang.hitch(this, function(data) {
+					if (data.length) {
+						var command = 'services/start';
+						var confirmMessage = _('Please confirm to start the following services: ');
+						var errorMessage = _('Starting the following services failed: ');
+						this._changeState(data, command, confirmMessage, errorMessage);
+					}
+				}),
+				isStandardAction: false,
+				isMultiAction: true
+			}, {
+				name: 'stop',
+				label: _('Stop services'),
+				callback: lang.hitch(this, function(data) {
+					if (data.length) {
+						var command = 'services/stop';
+						var confirmMessage = _('Please confirm to stop the following services: ');
+						var errorMessage = _('Stopping the following services failed: ');
+						this._changeState(data, command, confirmMessage, errorMessage);
+					}
+				}),
+				isStandardAction: false,
+				isMultiAction: true
+			}, {
+				name: 'restart',
+				label: _('Restart services'),
+				callback: lang.hitch(this, function(data) {
+					if (data.length) {
+						var command = 'services/restart';
+						var confirmMessage = _('Please confirm to restart the following services: ');
+						var errorMessage = _('Restarting the following services failed: ');
+						this._changeState(data, command, confirmMessage, errorMessage);
+					}
+				}),
+				isStandardAction: false,
+				isMultiAction: true
+			}, {
+				name: 'startAutomatically',
+				label: _('Start automatically'),
+				callback: lang.hitch(this, function(data) {
+					var command = 'services/start_auto';
+					var confirmMessage = _('Please confirm to automatically start the following services: ');
+					var errorMessage = _('Could not change start type of the following services: ');
 					this._changeState(data, command, confirmMessage, errorMessage);
-				}
-			}),
-			isStandardAction: false,
-			isMultiAction: true
-		}, {
-			name: 'stop',
-			label: this._('Stop services'),
-			callback: dojo.hitch(this, function(data) {
-				if (data.length) {
-					var command = 'services/stop';
-					var confirmMessage = this._('Please confirm to stop the following services: ');
-					var errorMessage = this._('Stopping the following services failed: ');
+				}),
+				isStandardAction: false,
+				isMultiAction: true
+			}, {
+				name: 'startManually',
+				label: _('Start manually'),
+				callback: lang.hitch(this, function(data) {
+					var command = 'services/start_manual';
+					var confirmMessage = _('Please confirm to manually start the following services: ');
+					var errorMessage = _('Could not change start type of the following services: ');
 					this._changeState(data, command, confirmMessage, errorMessage);
-				}
-			}),
-			isStandardAction: false,
-			isMultiAction: true
-		}, {
-			name: 'restart',
-			label: this._('Restart services'),
-			callback: dojo.hitch(this, function(data) {
-				if (data.length) {
-					var command = 'services/restart';
-					var confirmMessage = this._('Please confirm to restart the following services: ');
-					var errorMessage = this._('Restarting the following services failed: ');
+				}),
+				isStandardAction: false,
+				isMultiAction: true
+			}, {
+				name: 'startNever',
+				label: _('Start never'),
+				callback: lang.hitch(this, function(data) {
+					var command = 'services/start_never';
+					var confirmMessage = _('Please confirm to never start the following services: ');
+					var errorMessage = _('Could not change start type of the following services: ');
 					this._changeState(data, command, confirmMessage, errorMessage);
-				}
-			}),
-			isStandardAction: false,
-			isMultiAction: true
-		}, {
-			name: 'startAutomatically',
-			label: this._('Start automatically'),
-			callback: dojo.hitch(this, function(data) {
-				var command = 'services/start_auto';
-				var confirmMessage = this._('Please confirm to automatically start the following services: ');
-				var errorMessage = this._('Could not change start type of the following services: ');
-				this._changeState(data, command, confirmMessage, errorMessage);
-			}),
-			isStandardAction: false,
-			isMultiAction: true
-		}, {
-			name: 'startManually',
-			label: this._('Start manually'),
-			callback: dojo.hitch(this, function(data) {
-				var command = 'services/start_manual';
-				var confirmMessage = this._('Please confirm to manually start the following services: ');
-				var errorMessage = this._('Could not change start type of the following services: ');
-				this._changeState(data, command, confirmMessage, errorMessage);
-			}),
-			isStandardAction: false,
-			isMultiAction: true
-		}, {
-			name: 'startNever',
-			label: this._('Start never'),
-			callback: dojo.hitch(this, function(data) {
-				var command = 'services/start_never';
-				var confirmMessage = this._('Please confirm to never start the following services: ');
-				var errorMessage = this._('Could not change start type of the following services: ');
-				this._changeState(data, command, confirmMessage, errorMessage);
-			}),
-			isStandardAction: false,
-			isMultiAction: true
-		}];
+				}),
+				isStandardAction: false,
+				isMultiAction: true
+			}];
 
-		var columns = [{
-			name: 'service',
-			label: this._('Name'),
-            width: '200px'
-		}, {
-			name: 'isRunning',
-			label: this._('Status'),
-            width: 'adjust',
-			formatter: dojo.hitch(this, function(value) {
-				if (value === true) {
-					return this._('running');
-				} else {
-					return this._('stopped');
-				}
-			})
-		}, {
-			name: 'autostart',
-			label: this._('Start type'),
-            width: '100px',
-			formatter: dojo.hitch(this, function(value) {
-				if (value == 'no') {
-					return this._('Never');
-				} else if (value == 'manually') {
-					return this._('Manually');
-				} else {
-					return this._('Automatically');
-				}
-			})
-		}, {
-			name: 'description',
-			label: this._('Description'),
-            width: 'auto',
-			formatter: dojo.hitch(this, function(value) {
-				if (value === null) {
-					return '-';
-				} else {
-					return value;
-				}
-			})
-		}];
+			var columns = [{
+				name: 'service',
+				label: _('Name'),
+            	width: '200px'
+			}, {
+				name: 'isRunning',
+				label: _('Status'),
+            	width: 'adjust',
+				formatter: lang.hitch(this, function(value) {
+					if (value === true) {
+						return _('running');
+					} else {
+						return _('stopped');
+					}
+				})
+			}, {
+				name: 'autostart',
+				label: _('Start type'),
+            	width: '100px',
+				formatter: lang.hitch(this, function(value) {
+					if (value == 'no') {
+						return _('Never');
+					} else if (value == 'manually') {
+						return _('Manually');
+					} else {
+						return _('Automatically');
+					}
+				})
+			}, {
+				name: 'description',
+				label: _('Description'),
+            	width: 'auto',
+				formatter: lang.hitch(this, function(value) {
+					if (value === null) {
+						return '-';
+					} else {
+						return value;
+					}
+				})
+			}];
 
-		this._grid = new umc.widgets.Grid({
-			region: 'center',
-			actions: actions,
-			columns: columns,
-			moduleStore: this.moduleStore,
-			query: {
-                filter: '*'
-            }
-		});
-		titlePane.addChild(this._grid);
+			this._grid = new Grid({
+				region: 'center',
+				actions: actions,
+				columns: columns,
+				moduleStore: this.moduleStore,
+				query: {
+                	filter: '*'
+            	}
+			});
+			titlePane.addChild(this._grid);
 
-		var widgets = [{
-			type: 'TextBox',
-			name: 'filter',
-			value: '*',
-			label: this._('Keyword')
-		}];
+			var widgets = [{
+				type: TextBox,
+				name: 'filter',
+				value: '*',
+				label: _('Keyword')
+			}];
 
-		this._searchWidget = new umc.widgets.SearchForm({
-			region: 'top',
-			widgets: widgets,
-			layout: [[ 'filter', 'submit', 'reset' ]],
-			onSearch: dojo.hitch(this._grid, 'filter')
-		});
+			this._searchWidget = new SearchForm({
+				region: 'top',
+				widgets: widgets,
+				layout: [[ 'filter', 'submit', 'reset' ]],
+				onSearch: lang.hitch(this._grid, 'filter')
+			});
 
-		titlePane.addChild(this._searchWidget);
+			titlePane.addChild(this._searchWidget);
 
-		this._page.startup();
-    },
+			this._page.startup();
+    	},
 
-	_changeState: function(data, command, confirmMessage, errorMessage) {
-		confirmMessage += '<ul>';
-		dojo.forEach(data, function(idata) {
-			confirmMessage += '<li>' + idata + '</li>';
-		});
-		confirmMessage += '</ul>';
+		_changeState: function(data, command, confirmMessage, errorMessage) {
+			confirmMessage += '<ul>';
+			array.forEach(data, function(idata) {
+				confirmMessage += '<li>' + idata + '</li>';
+			});
+			confirmMessage += '</ul>';
 
-		umc.dialog.confirm(confirmMessage, [{
-			label: this._('OK'),
-			callback: dojo.hitch(this, function() {
-				this.standby(true);
-				umc.tools.umcpCommand(command, data).then(
-					dojo.hitch(this, function(response) {
-						this.standby(false);
-						if (response.result.success === false) {
-							errorMessage += '<ul>';
-							dojo.forEach(response.result.objects, function(item) {
-								errorMessage += '<li>' + item + '</li>';
-							});
-							errorMessage += '</ul>';
-							umc.dialog.alert(errorMessage);
-						}
-						data = this._searchWidget.gatherFormValues();
-						this._grid.filter(data);
-					}), dojo.hitch(this, function() {
-						this.standby(false);
-					})
-				);
-			})
-		}, {
-			label: this._('Cancel')
-		}]);
-	}
+			dialog.confirm(confirmMessage, [{
+				label: _('OK'),
+				callback: lang.hitch(this, function() {
+					this.standby(true);
+					tools.umcpCommand(command, data).then(
+						lang.hitch(this, function(response) {
+							this.standby(false);
+							if (response.result.success === false) {
+								errorMessage += '<ul>';
+								array.forEach(response.result.objects, function(item) {
+									errorMessage += '<li>' + item + '</li>';
+								});
+								errorMessage += '</ul>';
+								dialog.alert(errorMessage);
+							}
+							data = this._searchWidget.gatherFormValues();
+							this._grid.filter(data);
+						}), lang.hitch(this, function() {
+							this.standby(false);
+						})
+					);
+				})
+			}, {
+				label: _('Cancel')
+			}]);
+		}
+	});
 });
