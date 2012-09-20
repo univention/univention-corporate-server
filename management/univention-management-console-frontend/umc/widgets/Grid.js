7c1ce7852b8d6ee5bc7e4e873a6c09667abce345
Alexander Kläser <klaeser@univention.de>
2012-09-20 07:12:20 +0000

    univention-management-console-frontend (2.0.63-1) unstable; urgency=low  .    * fixed double queries in Grids; Bug #25476
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs@35742 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console-frontend/umc/widgets/Grid.js
+++ management/univention-management-console-frontend/umc/widgets/Grid.js
@@ -26,7 +26,7 @@
  * /usr/share/common-licenses/AGPL-3; if not, see
  * <http://www.gnu.org/licenses/>.
  */
-/*global define console*/
+/*global define require console*/
 
 define([
 	"dojo/_base/declare",
