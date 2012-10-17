13f7964f22969aed5e53e497d7662a734e85521b
Philipp Hahn <hahn@univention.de>
2012-10-17 08:25:52 +0000

    Bug #27889: Fix command line handling
    Add help message.
    Print error on unknown argument.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs@36377 1b283449-9f2f-0410-b571-8eb228e1a901

--- packaging/ucslint/testucslint.sh
+++ packaging/ucslint/testucslint.sh
@@ -4,6 +4,20 @@
 #
 declare -i RETVAL=0
 
+usage () { # Printusage
+    cat <<__USAGE__
+${0##*/}"
+Options:
+  --update   Save current result as expected result
+  --verbose  Show diff of failed tests
+  --clean    Remove current result after run
+  --quiet    Hide verbose output
+  --color    Colorize output
+  --         Separate options from following arguments
+__USAGE__
+    exit ${1:-0}
+}
+
 while [ $# -ge 1 ]
 do
     case "$1" in
@@ -12,8 +26,10 @@ do
     --clean) clean=1 ;;
     --quiet) quiet=1 ;;
     --color) red=$(tput setaf 1) green=$(tput setaf 2) norm=$(tput op) ;;
+    --help) usage 0 ;;
     --) shift ; break ;;
-    -*) exit 1 ;;
+    -*) echo "Unknown option $1; See ${0##*/} --help" >&2 ; exit 2 ;;
+    *) break ;;
     esac
     shift
 done
