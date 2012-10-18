d7a1b3fe1b500ecd4f1abcd9dfc3c290cf9cffe5
Lukas Walter <walter@univention.de>
2012-10-18 15:10:31 +0000

    univention-directory-manager-modules (8.0.62-1) unstable; urgency=low
      * instead of importing every single computer child module in
        computer.py, import every module in the module dir  (Bug #27617)
    
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs@36439 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-directory-manager-modules/modules/univention/admin/handlers/computers/computer.py
+++ management/univention-directory-manager-modules/modules/univention/admin/handlers/computers/computer.py
@@ -38,20 +38,7 @@ import univention.admin.localization
 translation=univention.admin.localization.translation('univention.admin.handlers.computers')
 _=translation.translate
 
-import univention.admin.handlers.computers.thinclient
-import univention.admin.handlers.computers.managedclient
-import univention.admin.handlers.computers.macos
-import univention.admin.handlers.computers.mobileclient
-import univention.admin.handlers.computers.ipmanagedclient
-import univention.admin.handlers.computers.windows
-import univention.admin.handlers.computers.windows_domaincontroller
-import univention.admin.handlers.computers.domaincontroller_master
-import univention.admin.handlers.computers.domaincontroller_backup
-import univention.admin.handlers.computers.domaincontroller_slave
-import univention.admin.handlers.computers.memberserver
-import univention.admin.handlers.computers.trustaccount
-import univention.admin.handlers.computers.ubuntu
-import univention.admin.handlers.computers.linux
+import univention.admin.handlers.computers
 
 module='computers/computer'
 usewizard=1
@@ -190,20 +177,8 @@ def lookup(co, lo, filter_s, base='', superordinate=None, scope='sub', unique=0,
 		if filter_s:
 			res+=lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit)
 	else:
-		return univention.admin.handlers.computers.macos.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit) +\
-				univention.admin.handlers.computers.thinclient.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit) +\
-				univention.admin.handlers.computers.ubuntu.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit) +\
-				univention.admin.handlers.computers.linux.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit) +\
-				univention.admin.handlers.computers.managedclient.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit) +\
-				univention.admin.handlers.computers.mobileclient.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit) +\
-				univention.admin.handlers.computers.windows.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit) +\
-				univention.admin.handlers.computers.domaincontroller_master.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit) +\
-				univention.admin.handlers.computers.domaincontroller_backup.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit) +\
-				univention.admin.handlers.computers.domaincontroller_slave.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit) +\
-				univention.admin.handlers.computers.memberserver.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit) +\
-				univention.admin.handlers.computers.ipmanagedclient.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit) +\
-				univention.admin.handlers.computers.trustaccount.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit) +\
-				univention.admin.handlers.computers.windows_domaincontroller.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit)
+		for computer in univention.admin.handlers.computers.computers:
+			res.extend( computer.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit) )
 	return res
 
 def identify(dn, attr, canonical=0):
