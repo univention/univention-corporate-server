c98c630a18caabdfed2aa31c15885e21f53782d6
Moritz Mühlenhoff <muehlenhoff@univention.de>
2012-10-22 09:04:40 +0000

    Make the UCR policy also apply to UCC systems (Bug #28852)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs@36499 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-directory-manager-modules/modules/univention/admin/handlers/policies/registry.py
+++ management/univention-directory-manager-modules/modules/univention/admin/handlers/policies/registry.py
@@ -52,7 +52,7 @@ module='policies/registry'
 operations=['add','edit','remove','search']
 
 policy_oc='univentionPolicyRegistry'
-policy_apply_to=["computers/domaincontroller_master", "computers/domaincontroller_backup", "computers/domaincontroller_slave", "computers/memberserver", "computers/managedclient", "computers/mobileclient", "computers/thinclient"]
+policy_apply_to=["computers/domaincontroller_master", "computers/domaincontroller_backup", "computers/domaincontroller_slave", "computers/memberserver", "computers/managedclient", "computers/mobileclient", "computers/thinclient", "computers/ucc"]
 policy_position_dn_prefix="cn=config-registry"
 usewizard=1
 childs=0
