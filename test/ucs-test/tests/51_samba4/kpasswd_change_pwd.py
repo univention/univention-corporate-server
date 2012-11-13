611da4e398c7b9b6890c41d33478268a0aeeba64
Tobias Peters <peters@univention.de>
2012-11-13 17:01:51 +0000

    * resolved the issue that some tests in 51_samba4 failed when the password for   Administrator is not the default one (Bug #29060) * temporarily disabled 51_samba4/40password-policies (Bug #29057)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs@37211 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ test/ucs-test/tests/51_samba4/kpasswd_change_pwd.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python 
+import pexpect
+import tempfile
+import sys
+import atexit
+import univention.config_registry
+import random
+import subprocess
+from optparse import OptionParser
+ucr = univention.config_registry.ConfigRegistry()
+ucr.load()
+
+def create_ssh_session(username, password):
+	known_hosts_file = tempfile.NamedTemporaryFile()
+	shell = pexpect.spawn('ssh', ['-o', 'UserKnownHostsFile="%s"' % known_hosts_file.name, "%s@localhost" % adminname,], timeout=10) # logfile=sys.stdout
+	status = shell.expect([pexpect.TIMEOUT, '[Pp]assword: ', 'Are you sure you want to continue connecting',])
+        del known_hosts_file
+	if status == 2: # accept public key
+		shell.sendline('yes')
+		status = shell.expect([pexpect.TIMEOUT, '[Pp]assword: ',])
+	if status == 0: # timeout
+		raise Exception('ssh behaved unexpectedly! Output:\n\t%r' % (shell.before,))
+	assert (status == 1), "password prompt"
+	shell.sendline(password)
+	status = shell.expect([pexpect.TIMEOUT, '\$ ','Last login',])
+	if status == 0: # timeout
+		raise Exception('No shell prompt found! Output:\n\t%r' % (shell.before,))
+	assert (status == 1 or status ==2), "shell prompt"
+	return shell
+
+if __name__ == "__main__":
+	parser = OptionParser()
+	parser.add_option("-u", "--username", dest="username")
+	parser.add_option("-p", "--password", dest="password")
+	parser.add_option("-n", "--newpassword", dest="newpassword")
+ 	parser.add_option("-r", "--adminpassword", dest="adminpassword")
+	parser.add_option("-a", "--adminname", dest="adminname")
+	(optionen, args) = parser.parse_args()
+	username=optionen.username
+	password=optionen.password
+	newpassword=optionen.newpassword
+	adminpassword=optionen.adminpassword
+	adminname=optionen.adminname
+	try:
+		shell = create_ssh_session(adminname, adminpassword)
+	except Exception, e:
+		print e # print error
+		sys.exit(120)
+
+	shell.sendline('kpasswd %s' % username)
+	status = shell.expect([pexpect.TIMEOUT, '[Pp]assword:',])
+	if status == 0: # timeout
+		sys.exit(120)
+	shell.sendline(password)
+	status = shell.expect([pexpect.TIMEOUT, 'New password:',])
+	shell.sendline(newpassword)
+	status = shell.expect([pexpect.TIMEOUT, 'New password:',])
+	shell.sendline(newpassword)
+	status = shell.expect(['(?i)success', '(?i)error', pexpect.TIMEOUT,])
+	kpasswd_reported_success = status == 0
+	print 'changed password for %s to %s' % (username, newpassword)
+	print 'ENDE:%r' % (shell.before,)
