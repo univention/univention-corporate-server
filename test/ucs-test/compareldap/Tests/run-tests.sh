1cf7469a07a1103eb1b1cf98874caa992473954e
Liam Schwez <schwez@univention.de>
2012-11-20 16:28:48 +0000

    Add first version of compareldif (Bug #29337)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs@37383 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ test/ucs-test/compareldap/Tests/run-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PATH_TO_COMPARE_LDIF='../compareldif'
+
+PATH_TO_COMPARE_LDIF=$(readlink -m "$PATH_TO_COMPARE_LDIF")
+for directory in */
+do
+   echo "$directory"
+    cd "$directory" 
+    if cmp --quiet <("$PATH_TO_COMPARE_LDIF" InputA.ldif InputB.ldif) Output
+    then
+       echo "OK"
+    else
+       diff -u Output <("$PATH_TO_COMPARE_LDIF" InputA.ldif InputB.ldif)
+    fi
+   cd ..
+done
