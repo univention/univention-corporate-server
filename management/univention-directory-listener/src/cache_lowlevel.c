031ac204dad8de6a0b1e312d0d1abfb294b03933
Philipp Hahn <hahn@univention.de>
2013-02-05 13:12:50 +0000

    Bug #30227: listener: assert null termination
    Check key_len and data_len >= 1.
    Check key and data are always '\0' terminated, especially data:
    C-strings are naturally '\0' terminated, binary data is artifically '\0'
    terminated by us.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs@38884 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-directory-listener/src/cache_lowlevel.c
+++ management/univention-directory-listener/src/cache_lowlevel.c
@@ -38,6 +38,7 @@
 #include <stdbool.h>
 #include <sys/types.h>
 
+#include <assert.h>
 #include <univention/debug.h>
 
 #include "common.h"
@@ -230,15 +231,20 @@ int parse_entry(void *data, u_int32_t size, CacheEntry *entry)
 			CacheEntryAttribute **attribute, *c_attr;
 
 			univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_ALL, "attribute is \"%s\"", (char*)key_data);
+			/* key name including '\0' */
+			assert(key_size >= 1);
+			assert('\0' == ((char *)key_data)[key_size - 1]);
+			/* value including '\0' */
+			assert(data_size >= 1);
+			assert('\0' == ((char *)data_data)[data_size - 1]);
 
 			for (attribute = entry->attributes, c_attr = NULL;
 					attribute != NULL && *attribute != NULL;
 					attribute++) {
 				univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_ALL, "current attribute is \"%s\"", c_attr->name);
-				if (strcmp(c_attr->name, (char*)key_data) == 0) {
+				if (strncmp(c_attr->name, (char*)key_data, key_size) == 0)
 					c_attr = *attribute;
 					break;
-				}
 			}
 			if (!c_attr) {
 				if (!(entry->attributes = realloc(entry->attributes, (entry->attribute_count + 2) * sizeof(CacheEntryAttribute*)))) {
