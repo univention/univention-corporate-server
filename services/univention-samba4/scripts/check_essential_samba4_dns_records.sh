515e89aa58bbf8af19cfc05da7162e73cf2f4de5
Arvid Requate <requate@univention.de>
2013-02-11 14:36:49 +0000

    * New UCR variable samba/max_log_size with new default 0 (Bug #29542) * Add UCR variable description for saba/interfaces (Bug #28014) * Duplicate UCRWARNING header removed (Bug #28426) * fixes for check_essential_samba4_dns_records (Bug #28891) * add purge_s4_computer.py (Bug #29460) * improve resilience of s4search-decode against broken   supplementalCredentials (Bug #28931) * Make s4search-decode output AD timestamps in a human parseable format   (Bug #29992)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs@38976 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-samba4/scripts/check_essential_samba4_dns_records.sh
+++ services/univention-samba4/scripts/check_essential_samba4_dns_records.sh
@@ -31,8 +31,6 @@ LDB_MODULES_PATH=/usr/lib/ldb; export LDB_MODULES_PATH;		## currently necessary 
 
 eval "$(univention-config-registry shell windows/domain samba4/ldap/base ldap/hostdn)"
 
-host gc._msdcs
-
 ## retrive DC specific GUID
 samba4servicedcs=$(ldapsearch -ZZ -LLL -D "$ldap_hostdn" -y /etc/machine.secret "(&(univentionService=Samba 4)(objectClass=univentionDomainController))" cn | sed -n 's/^cn: \(.*\)/\1/p')      ## currently there is no u-d-m module computers/dc
 
@@ -41,40 +39,55 @@ for s4dc in $samba4servicedcs; do
 							serverReferenceBL | ldapsearch-wrapper | sed -n 's/^serverReferenceBL: \(.*\)/\1/p')
 	NTDS_objectGUID=$(ldbsearch -H /var/lib/samba/private/sam.ldb -b "$server_object_dn" \
 							"CN=NTDS Settings" objectGUID | sed -n 's/^objectGUID: \(.*\)/\1/p')
-	host -t cname $NTDS_objectGUID._msdcs
 done
 
+## Determine sitename
+sitename=$(echo "$server_object_dn" | sed -n 's/[^,]*,CN=Servers,CN=\([^,]*\),CN=Sites,CN=Configuration,DC=kontor,DC=local/\1/p')
+
+if [ -n "$sitename" ]; then
+	echo "Located local DC in site '$sitename'"
+else
+	sitename="Default-First-Site-Name"
+	echo "Failed to determine site of local DC, using default '$sitename'"
+fi
+	
 ## retrive domain partition GUID
 Partition_GUID="$(ldbsearch -H /var/lib/samba/private/sam.ldb -b "CN=$windows_domain,CN=Partitions,CN=Configuration,$samba4_ldap_base" $ldb_control objectGUID | sed -n 's/^objectGUID: \(.*\)/\1/p')"
 
+Domain_GUID="$(ldbsearch -H /var/lib/samba/private/sam.ldb -s base objectGUID | sed -n 's/^objectGUID: \(.*\)/\1/p')"
+
+
+## Now lookup DNS entries
+host gc._msdcs
+
+host -t cname $NTDS_objectGUID._msdcs
 
 cat << %EOF | while read rec proto; do host -t srv "_$rec._$proto"; done
 gc tcp
-gc._tcp.Default-First-Site-Name sites
+gc._tcp.$sitename sites
 ldap._tcp.gc msdcs
-ldap._tcp.Default-First-Site-Name._sites.gc msdcs
+ldap._tcp.$sitename._sites.gc msdcs
 ldap tcp
 ldap._tcp.dc msdcs
 ldap._tcp.pdc msdcs
-ldap.$Partition_GUID.domains msdcs
-ldap._tcp.Default-First-Site-Name sites
-ldap._tcp.Default-First-Site-Name._sites.dc msdcs
+ldap._tcp.$Domain_GUID.domains msdcs
+ldap._tcp.$sitename sites
+ldap._tcp.$sitename._sites.dc msdcs
 kerberos._tcp.dc msdcs
-kerberos._tcp.Default-First-Site-Name sites
-kerberos._tcp.Default-First-Site-Name._sites.dc msdcs
+kerberos._tcp.$sitename sites
+kerberos._tcp.$sitename._sites.dc msdcs
 kerberos tcp
 kerberos udp
-kerberos-master tcp
-kerberos-master udp
 kpasswd tcp
 kpasswd udp
 %EOF
-host -t txt _kerberos
+
+output=$(host -t txt _kerberos 2>&1)
+if [ $? = 0 ]; then
+	echo "$output"
+else
+	echo "No _kerberos TXT record (ok)"
+fi
 
 exit 0
-
-_kerberos._tcp.Default-First-Site-Name._sites.dc._msdcs
-
-for srvrec in
-host -t srv _kerberos._tcp.Default-First-Site-Name._sites.dc._msdcs
 
