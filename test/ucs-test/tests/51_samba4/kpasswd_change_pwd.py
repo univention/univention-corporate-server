f09ea7f34ca39c6efc3cee2f640f5a91e6df3845
Tobias Peters <peters@univention.de>
2013-03-27 16:01:14 +0000

    * applied patch to 51_samba4/41password_change (Bug #29058) * removed ssh connection from 51_samba4/35smbclient_auth (Bug #28348) * removed ssh connection from 50_samba/35smbclient_auth (Bug #26807)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs-3.1-2@39788 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/51_samba4/kpasswd_change_pwd.py
+++ test/ucs-test/tests/51_samba4/kpasswd_change_pwd.py
@@ -1,63 +1,41 @@
 #!/usr/bin/python 
 import pexpect
-import tempfile
 import sys
-import atexit
 import univention.config_registry
-import random
-import subprocess
 from optparse import OptionParser
-ucr = univention.config_registry.ConfigRegistry()
-ucr.load()
-
-def create_ssh_session(username, password):
-	known_hosts_file = tempfile.NamedTemporaryFile()
-	shell = pexpect.spawn('ssh', ['-o', 'UserKnownHostsFile="%s"' % known_hosts_file.name, "%s@localhost" % adminname,], timeout=10) # logfile=sys.stdout
-	status = shell.expect([pexpect.TIMEOUT, '[Pp]assword: ', 'Are you sure you want to continue connecting',])
-        del known_hosts_file
-	if status == 2: # accept public key
-		shell.sendline('yes')
-		status = shell.expect([pexpect.TIMEOUT, '[Pp]assword: ',])
-	if status == 0: # timeout
-		raise Exception('ssh behaved unexpectedly! Output:\n\t%r' % (shell.before,))
-	assert (status == 1), "password prompt"
-	shell.sendline(password)
-	status = shell.expect([pexpect.TIMEOUT, '\$ ','Last login',])
-	if status == 0: # timeout
-		raise Exception('No shell prompt found! Output:\n\t%r' % (shell.before,))
-	assert (status == 1 or status ==2), "shell prompt"
-	return shell
 
 if __name__ == "__main__":
 	parser = OptionParser()
 	parser.add_option("-u", "--username", dest="username")
 	parser.add_option("-p", "--password", dest="password")
 	parser.add_option("-n", "--newpassword", dest="newpassword")
- 	parser.add_option("-r", "--adminpassword", dest="adminpassword")
-	parser.add_option("-a", "--adminname", dest="adminname")
 	(optionen, args) = parser.parse_args()
 	username=optionen.username
 	password=optionen.password
 	newpassword=optionen.newpassword
-	adminpassword=optionen.adminpassword
-	adminname=optionen.adminname
-	try:
-		shell = create_ssh_session(adminname, adminpassword)
-	except Exception, e:
-		print e # print error
-		sys.exit(120)
 
-	shell.sendline('kpasswd %s' % username)
-	status = shell.expect([pexpect.TIMEOUT, '[Pp]assword:',])
+	ucr = univention.config_registry.ConfigRegistry()
+	ucr.load()
+
+	kpasswd = pexpect.spawn('kpasswd', [username], timeout=20) # logfile=sys.stdout
+	status = kpasswd.expect([pexpect.TIMEOUT, "%s@%s's Password: " % (username, ucr['kerberos/realm']),])
 	if status == 0: # timeout
+		print 'kpasswd behaved unexpectedly! Output:\n\t%r' % (kpasswd.before,)
 		sys.exit(120)
-	shell.sendline(password)
-	status = shell.expect([pexpect.TIMEOUT, 'New password:',])
-	shell.sendline(newpassword)
-	status = shell.expect([pexpect.TIMEOUT, 'New password:',])
-	shell.sendline(newpassword)
-	status = shell.expect(['(?i)success', '(?i)error', pexpect.TIMEOUT,])
-	kpasswd_reported_success = status == 0
-	print 'changed password for %s to %s' % (username, newpassword)
-	print 'ENDEbefore:%s' % (shell.before,)
-	print 'ENDEafter:%s' % (shell.after,)
+	assert (status == 1), "password prompt"
+	kpasswd.sendline(password)
+	status = kpasswd.expect([pexpect.TIMEOUT, 'New password for %s@%s:' % (username, ucr['kerberos/realm']), "kpasswd: krb5_get_init_creds: Preauthentication failed", ])
+	if status == 0: # timeout
+		print 'kpasswd behaved unexpectedly! Output:\n\t%r' % (kpasswd.before,)
+		sys.exit(120)
+	elif status == 2: # timeout
+		print 'Preauthentication failed!'
+		sys.exit(120)
+	kpasswd.sendline(newpassword)
+	status = kpasswd.expect([pexpect.TIMEOUT, 'Verify password - New password for %s@%s:' % (username, ucr['kerberos/realm']),])
+	kpasswd.sendline(newpassword)
+	status = kpasswd.expect(['Success : Password changed', pexpect.TIMEOUT,])
+	if status != 0:
+		sys.exit(1)
+	else:
+		print 'Password changed for %s to %s' % (username, newpassword)
