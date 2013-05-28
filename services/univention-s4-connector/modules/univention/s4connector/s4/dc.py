4815b920793349eced6144646629100a693db774
Stefan Gohmann <gohmann@univention.de>
2013-05-28 11:15:39 +0000

    * In OpenLDAP the attribute sambaMaxPwdAge can be set to -1. This will   be mapped in Samba 4 to 0 since -1 is not allowed for the attribute   maxPwdAge (Bug #29775)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs-3.1-1@40957 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-s4-connector/modules/univention/s4connector/s4/dc.py
+++ services/univention-s4-connector/modules/univention/s4connector/s4/dc.py
@@ -49,6 +49,9 @@ def _unixTimeInverval2seconds(unixTime):
 		ud.debug(ud.LDAP, ud.WARN, 'dc _unixTimeInverval2seconds: Not a valid time format: %s' % unixTime)
 		return 0
 
+	if unixTime[0] < 0:
+		return 0
+
 	if unixTime[1] == 'seconds':
 		return unixTime[0]
 	elif unixTime[1] == 'minutes':
@@ -100,7 +103,11 @@ def ucs2con (s4connector, key, object):
 			ud.debug(ud.LDAP, ud.INFO, 'dc ucs2con: s4-time (%s): %s' % (s4_attr, s4_time))
 
 			if ucs_time != s4_time:
-				ml.append( (ldap.MOD_REPLACE, s4_attr, [ str(_s2nano(ucs_time) * -1) ] ) )
+				if ucs_time > 0:
+					s4_time = str(_s2nano(ucs_time) * -1)
+				else:
+					s4_time = "0"
+				ml.append( (ldap.MOD_REPLACE, s4_attr, [ s4_time ] ) )
 		
 		sync_integers = [ ('sambaPwdHistoryLength', 'pwdHistoryLength'), ('sambaMinPwdLength', 'minPwdLength') ]
 		for (ucs_attr, s4_attr) in sync_integers:
