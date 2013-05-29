f9a61fc25bc556ebbaf9aad9089c63559d9b946c
Lukas Walter <walter@univention.de>
2013-05-29 14:33:32 +0000

    ucs-test (3.0.56-1) unstable; urgency=low
      * skip certain udm dhcp test which currently fail due to known Bugs (Bug
      	#31518) (Bug #31519)
      * added ucr variables to configure domain admin credentials to be used in
      	ucs-test (Bug #31250)
      * ported certain udm-dns tests from ucs-3.0 branchs (Bug #27664)
      * reverted changes to udm-users/35_create_user_with_umlaut_in_uid from last
      	version (Bug #27664)
    
    
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs-3.1-2@41051 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ test/ucs-test/univention/testing/ldap.py
@@ -0,0 +1,31 @@
+import ldap
+import univention.uldap as uldap
+import univention.config_registry
+
+def getLdapConnection(pwdfile = False, start_tls = 2, decode_ignorelist = []):
+	ucr = univention.config_registry.ConfigRegistry()
+	ucr.load()
+
+	port = int(ucr.get('ldap/server/port', 7389))
+	binddn = ucr.get('tests/domainadmin/account', 'uid=Administrator,cn=users,%s' % ucr['ldap/base'])
+	bindpw = None
+	ldapServers = []
+	if ucr['ldap/server/name']:
+		ldapServers.append(ucr['ldap/server/name'])
+	if ucr['ldap/servers/addition']:
+		ldapServers.extend(ucr['ldap/server/addition'].split())
+
+	if pwdfile:
+		with open(ucr['tests/domainadmin/pwdfile']) as f:
+			bindpw = f.read()[:-1]
+	else:
+		bindpw = ucr['tests/domainadmin/pwd']
+
+	
+	for ldapServer in ldapServers:
+		try:
+			lo = uldap.access(host=ldapServer, port=port, base=ucr['ldap/base'], binddn=binddn, bindpw=bindpw, start_tls=start_tls, decode_ignorelist=decode_ignorelist)
+			return lo
+		except ldap.SERVER_DOWN:
+			pass
+	raise ldap.SERVER_DOWN
