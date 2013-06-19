eaaa94636d70bc73c0ee41b127577f0e458de43a
Tobias Peters <peters@univention.de>
2013-06-19 12:00:47 +0000

    * Changed some tests in 51_samba to use the Administrator credentials   provided by the ucs-test framework (Bug #28262)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-0@41516 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/51_samba4/kpasswd_change_pwd.py
+++ test/ucs-test/tests/51_samba4/kpasswd_change_pwd.py
@@ -10,21 +10,30 @@ if __name__ == "__main__":
 	parser.add_option("-u", "--username", dest="username")
 	parser.add_option("-p", "--password", dest="password")
 	parser.add_option("-n", "--newpassword", dest="newpassword")
+	parser.add_option("-a", "--adminname", dest="adminname")
+        parser.add_option("-m", "--adminpassword", dest="adminpassword")
+
 	(optionen, args) = parser.parse_args()
 	username=optionen.username
 	password=optionen.password
 	newpassword=optionen.newpassword
+	adminpassword=optionen.adminpassword
+        adminname=optionen.adminname
+
 
 	ucr = univention.config_registry.ConfigRegistry()
 	ucr.load()
 
-	kpasswd = pexpect.spawn('kpasswd', [username], timeout=20) # logfile=sys.stdout
-	status = kpasswd.expect([pexpect.TIMEOUT, "%s@%s's Password: " % (username, ucr['kerberos/realm']),])
-	if status == 0: # timeout
-		print 'kpasswd behaved unexpectedly! Output:\n\t%r' % (kpasswd.before,)
-		sys.exit(120)
-	assert (status == 1), "password prompt"
-	kpasswd.sendline(password)
+	kpasswd = pexpect.spawn('kpasswd --admin-principal=%s %s' % (adminname, username), timeout=20) # logfile=sys.stdout                                                                  
+        status = kpasswd.expect([pexpect.TIMEOUT, "%s@%s's Password: " % (adminname, ucr['kerberos/realm']),])
+        if status == 0: # timeout                                                                                                                                                            
+                print 'kpasswd behaved unexpectedly! Output:\n\t%r' % (kpasswd.before,)
+                sys.exit(120)
+        assert (status == 1), "password prompt"
+
+        kpasswd.sendline(adminpassword)
+
+
 	status = kpasswd.expect([pexpect.TIMEOUT, 'New password for %s@%s:' % (username, ucr['kerberos/realm']), "kpasswd: krb5_get_init_creds: Preauthentication failed", ])
 	if status == 0: # timeout
 		print 'kpasswd behaved unexpectedly! Output:\n\t%r' % (kpasswd.before,)
