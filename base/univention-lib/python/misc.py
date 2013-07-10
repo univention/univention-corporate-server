0774da1545ebc173413cac23f3aa8376c26bbc31
Stefan Gohmann <gohmann@univention.de>
2013-07-10 05:19:08 +0000

    * python/misc.py: added getLDAPURIs function (Bug #19753)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-0@42215 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-lib/python/misc.py
+++ base/univention-lib/python/misc.py
@@ -16,3 +16,33 @@ def createMachinePassword():
 	p = subprocess.Popen(["pwgen", "-1", "-" + compl, length], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	(stdout, stderr) = p.communicate()
 	return stdout.strip()
+
+def getLDAPURIs(configRegistryInstance = None):
+	"""
+	Returns a string with all configured LDAP servers,
+	 ldap/server/name and ldap/server/additional
+	Optional a UCR instance ca be given as parameter, for example
+	if the function is used in a UCR template
+	"""
+	if configRegistryInstance:
+		ucr = configRegistryInstance
+	else:
+		ucr = univention.config_registry.ConfigRegistry()
+		ucr.load()
+
+	uri_string = ''
+	ldaphosts=[]
+	port = ucr.get('ldap/server/port', '7389')
+	ldap_server_name = ucr.get('ldap/server/name')
+	ldap_server_addition = ucr.get('ldap/server/addition')
+
+	if ldap_server_name:
+		ldaphosts.append(ldap_server_name)
+	if ldap_server_addition:
+		ldaphosts.extend(ldap_server_addition.split())
+	if ldaphosts:
+		urilist=[ "ldap://%s:%s" % (host, port) for host in ldaphosts ]
+		uri_string = ' '.join(urilist)
+
+	return uri_string
+
