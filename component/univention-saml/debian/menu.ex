c67a8bf545c3895498742873a1a31755e62e869a
Jacek Groth <groth@univention.de>
2013-07-10 12:50:39 +0000

    univention-saml (Bug #25408)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-0@42255 1b283449-9f2f-0410-b571-8eb228e1a901

--- component/univention-saml/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(univention-identity-provider):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="univention-identity-provider" command="/usr/bin/univention-identity-provider"
--- component/univention-saml/debian/univention-identity-provider.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the univention-identity-provider package
-#
-0 4	* * *	root	[ -x /usr/bin/univention-identity-provider_maintenance ] && /usr/bin/univention-identity-provider_maintenance
