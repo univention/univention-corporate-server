8ae3bd61e3c648f201c9e5dfc8dfa63c0c5b9378
Florian Best <best@univention.de>
2013-09-12 06:28:56 +0000

    * Bug #30816: fix detection of non physical interfaces
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-0@43921 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-system-setup/umc/python/setup/util.py
+++ base/univention-system-setup/umc/python/setup/util.py
@@ -482,17 +482,17 @@ def detect_interfaces():
 
 	dirnames = [dirname for dirname in os.listdir(PATH_SYS_CLASS_NET) if os.path.isdir(os.path.join(PATH_SYS_CLASS_NET, dirname))]
 	for dirname in dirnames:
+		# filter out lo, etc. interfaces
+		if open(os.path.join(PATH_SYS_CLASS_NET, dirname, 'type'),'r').read().strip() not in ('1', '2', '3', '4', '5', '6', '7', '8', '15', '19'):
+			continue
+		# filter out bridge, bond devices
+		if any(os.path.exists(os.path.join(PATH_SYS_CLASS_NET, dirname, path)) for path in ('bridge', 'bonding')):
+			continue
+		# filter out vlan devices
+		if '.' in dirname:
+			continue
 		mac = None
 		try:
-			# filter out lo, etc. interfaces
-			if open(os.path.join(PATH_SYS_CLASS_NET, dirname, 'type'),'r').read().strip() not in ('1', '2', '3', '4', '5', '6', '7', '8', '15', '19'):
-				continue
-			# filter out bridge, bond devices
-			if any(os.path.exists(os.path.join(PATH_SYS_CLASS_NET, path)) for path in ('bridge', 'bonding')):
-				continue
-			# filter out vlan devices
-			if '.' in dirname:
-				continue
 			# try to read mac address
 			mac = open(os.path.join(PATH_SYS_CLASS_NET, dirname, 'address'),'r').read().strip()
 		except (OSError, IOError):
