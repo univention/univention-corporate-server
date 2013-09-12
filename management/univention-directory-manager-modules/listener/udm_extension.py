5043d56185964e3e106731d7c7336709f51f82b7
Arvid Requate <requate@univention.de>
2013-09-12 19:20:10 +0000

    * Create __init__.py files in the udm_extension listener   (Bug #32403, Bug #32406, Bug #32409)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-0@43971 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-directory-manager-modules/listener/udm_extension.py
+++ management/univention-directory-manager-modules/listener/udm_extension.py
@@ -44,6 +44,7 @@ import zlib
 import tempfile
 import datetime
 import apt
+import exceptions
 
 name = 'udm_extension'
 description = 'Handle UDM module, hook an syntax extensions'
@@ -53,44 +54,101 @@ attributes = []
 PYSHARED_DIR = '/usr/share/pyshared/'
 PYMODULES_DIR = '/usr/lib/pymodules/'
 
-def install_python_file(data, relative_filename):
-	"""Install and link a public python module file"""
+class moduleCreationFailed(exceptions.Exception):
+	message='Module creation failed.'
+
+def create_pyshared_moduledir(target_subdir, module_directory):
+	if not module_directory:
+		return
+
+	if module_directory.startswith('/'):
+		raise moduleCreationFailed, 'Module directory must not be absolute: %s' % (module_directory, )
+
+	parent_dir = os.path.dirname(module_directory)
+	if parent_dir and not os.path.exists(os.path.join(PYSHARED_DIR, target_subdir, parent_dir)):
+		create_pyshared_moduledir(target_subdir, parent_dir)
+	else:
+		targetpath = os.path.join(PYSHARED_DIR, target_subdir, module_directory)
+		if not os.path.isdir(targetpath):
+			try:
+				os.mkdir(targetpath)
+			except OSError as e:
+				raise moduleCreationFailed, 'Directory creation of %s failed: %s.' % (targetpath, e)
+
+		python_init_filename = os.path.join(targetpath,'__init__.py')
+		if not os.path.exists(python_init_filename):
+			open(python_init_filename, 'a').close()
+
+def install_pyshared_file(target_subdir, target_filename, data):
+	"""Install a public python module file"""
+	relative_filename = os.path.join(target_subdir, target_filename)
 	if not relative_filename:
 		ud.debug(ud.LISTENER, ud.ERROR, '%s: No python file to install.' % (name, relative_filename))
 		return False
 
-	filename = os.path.join(PYSHARED_DIR, relative_filename)
-	relative_basedir = os.path.dirname(relative_filename)
+	if relative_filename.startswith('/'):
+		ud.debug(ud.LISTENER, ud.ERROR, '%s: Module path must not be absolute: %s.' % (name, relative_filename))
+		return False
+
+	try:
+		create_pyshared_moduledir(target_subdir, os.path.dirname(target_filename))
+	except moduleCreationFailed as e:
+		ud.debug(ud.LISTENER, ud.ERROR, '%s: %s' % (name, e))
+		return False
+
+	filename = os.path.join(PYSHARED_DIR, target_subdir, relative_filename)
 	try:
 		with open(filename, 'r') as f:
 			file_hash = hashlib.sha256(f.read()).hexdigest()
 	except IOError:
 		file_hash = None
 
-	data_ok = False
 	if file_hash and file_hash == hashlib.sha256(data).hexdigest():
 		ud.debug(ud.LISTENER, ud.INFO, '%s: data %s unchanged.' % (name, filename))
-		data_ok = True
+		return True
 	else:
-		## prepare file target
-		pyshared_target_dir = os.path.join(PYSHARED_DIR, relative_basedir)
-		if not os.path.exists(pyshared_target_dir):
-			try:
-				os.makedirs(pyshared_target_dir)
-			except OSError, e:
-				ud.debug(ud.LISTENER, ud.ERROR, '%s: Directory creation of %s failed: %s.' % (name, pymodules_target_dir, e))
-
 		try:
 			with open(filename, 'w') as f:
 				f.write(data)
-			data_ok = True
 			ud.debug(ud.LISTENER, ud.INFO, '%s: %s installed.' % (name, relative_filename))
+			return True
 		except Exception, e:
 			ud.debug(ud.LISTENER, ud.ERROR, '%s: Writing new data to %s failed: %s.' % (name, filename, e))
 			return False
 
+def create_pymodules_moduledir(pymodules_version_path, target_subdir, module_directory):
+	if not module_directory:
+		return
+
+	if module_directory.startswith('/'):
+		raise moduleCreationFailed, 'Module directory must not be absolute: %s' % (module_directory, )
+
+	parent_dir = os.path.dirname(module_directory)
+	if parent_dir and not os.path.exists(os.path.join(pymodules_version_path, target_subdir, parent_dir)):
+		create_pymodules_moduledir(pymodules_version_path, target_subdir, parent_dir)
+	else:
+		targetpath = os.path.join(pymodules_version_path, target_subdir, module_directory)
+		if not os.path.isdir(targetpath):
+			try:
+				os.mkdir(targetpath)
+			except OSError as e:
+				raise moduleCreationFailed, 'Directory creation of %s failed: %s.' % (targetpath, e)
+
+		linkname = os.path.join(targetpath, '__init__.py')
+		filename = os.path.join(PYSHARED_DIR, target_subdir, module_directory, '__init__.py')
+		if os.path.lexists(linkname):
+			if os.path.exists(linkname) and os.path.realpath(linkname) == filename:
+					return
+			else:
+				os.unlink(linkname)
+		try:
+			os.symlink(filename, linkname)
+		except OSError, e:
+			raise moduleCreationFailed, 'Symlink creation of %s failed: %s.' % (linkname, e)
+
+def create_pymodules_links(target_subdir, target_filename):
 	links_created = 0
-	default_link_present = False
+	is_default_link_present = False
 
 	p = subprocess.Popen(['/usr/bin/pyversions', '-d'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	(stdout, stderr) = p.communicate()
@@ -98,48 +156,45 @@ def install_python_file(data, relative_filename):
 
 	p = subprocess.Popen(['/usr/bin/pyversions', '-i'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	(stdout, stderr) = p.communicate()
-	pyversion_path_list = [ os.path.join(PYMODULES_DIR, version) for version in stdout.strip().split(' ') ]
+	pyversions_list = stdout.strip().split(' ')
 
-	for pyversion_path in pyversion_path_list:
-		if os.path.isdir(pyversion_path):
-			pymodules_target_dir = os.path.join(pyversion_path, relative_basedir)
-			linkname = os.path.join(pyversion_path, relative_filename)
-			
-			## prepare link target
-			if os.path.lexists(linkname):
-				if os.path.exists(linkname) and os.path.realpath(linkname) == filename:
-						if version == default_python_version:
-							default_link_present = True
-						continue
-				else:
-					os.unlink(linkname)
-			else:
-				if not os.path.exists(pymodules_target_dir):
-					try:
-						os.makedirs(pymodules_target_dir)
-					except OSError, e:
-						ud.debug(ud.LISTENER, ud.ERROR, '%s: Directory creation of %s failed: %s.' % (name, pymodules_target_dir, e))
+	relative_filename = os.path.join(target_subdir, target_filename)
+	for pyversion in pyversions_list:
+		pymodules_version_path = os.path.join(PYMODULES_DIR, pyversion)
+		if os.path.isdir(pymodules_version_path):
+			try:
+				create_pymodules_moduledir(pymodules_version_path, target_subdir, os.path.dirname(target_filename))
+			except moduleCreationFailed as e:
+				ud.debug(ud.LISTENER, ud.ERROR, '%s: %s' % (name, e))
 
+			linkname = os.path.join(pymodules_version_path, relative_filename)
+			filename = os.path.join(PYSHARED_DIR, relative_filename)
 			try:
 				os.symlink(filename, linkname)
 				links_created += 1
-				if version == default_python_version:
-					default_link_present = True
+				if pyversion == default_python_version:
+					is_default_link_present = True
 			except OSError, e:
 				ud.debug(ud.LISTENER, ud.ERROR, '%s: Symlink creation of %s failed: %s.' % (name, linkname, e))
 	if links_created:
 		ud.debug(ud.LISTENER, ud.INFO, '%s: symlinks to %s created.' % (name, relative_filename))
 
-	if data_ok and default_link_present:
+	return is_default_link_present
+
+
+def install_python_file(target_subdir, relative_filename, data):
+	"""Install and link a public python module file"""
+
+	if install_pyshared_file(target_subdir, relative_filename, data) and create_pymodules_links(target_subdir, relative_filename):
 		return True
 	else:
 		return False
 
-
-def remove_python_file(relative_filename):
+def remove_python_file(target_subdir, target_filename):
 	"""Unlink and remove a public python module file"""
-	if not relative_filename:
-		ud.debug(ud.LISTENER, ud.ERROR, '%s: No python file to remove.' % (name, relative_filename))
+	relative_filename = os.path.join(target_subdir, target_filename)
+	if not target_filename:
+		ud.debug(ud.LISTENER, ud.ERROR, '%s: No python file to remove.' % (name, ))
 		return False
 
 	filename = os.path.join(PYSHARED_DIR, relative_filename)
@@ -181,6 +236,8 @@ def handler(dn, new, old):
 	"""Handle UDM extension modules"""
 
 	if new:
+		ocs = new.get('objectClass', [])
+
 		univentionUCSVersionStart = new.get('univentionUCSVersionStart', [None])[0]
 		univentionUCSVersionEnd = new.get('univentionUCSVersionEnd', [None])[0]
 		current_UCR_version = "%s-%s" % ( listener.configRegistry.get('version/version'), listener.configRegistry.get('version/patchlevel') )
@@ -190,6 +247,23 @@ def handler(dn, new, old):
 		elif univentionUCSVersionEnd and current_UCR_version >= univentionUCSVersionEnd:
 			ud.debug(ud.LISTENER, ud.INFO, '%s: extension %s specifies compatibility only up to UCR version %s.' % (name, new['cn'][0], univentionUCSVersionEnd))
 			new=None
+	elif old:
+		ocs = old.get('objectClass', [])
+
+	if 'univentionUDMModule' in ocs:
+		objectclass = 'univentionUDMModule'
+		udm_module_name = 'settings/udm_module'
+		target_subdir = 'univention/admin/handlers'
+	elif 'univentionUDMHook' in ocs:
+		objectclass = 'univentionUDMHook'
+		udm_module_name = 'settings/udm_hook'
+		target_subdir = 'univention/admin/hooks.d'
+	elif 'univentionUDMSyntax' in ocs:
+		objectclass = 'univentionUDMSyntax'
+		udm_module_name = 'settings/udm_syntax'
+		target_subdir = 'univention/admin/syntax.d'
+	else:
+		ud.debug(ud.LISTENER, ud.ERROR, '%s: Undetermined error: unknown objectclass: %s.' % (name, ocs))
 
 	if new:
 		new_version = new.get('univentionOwnedByPackageVersion', [None])[0]
@@ -202,7 +276,7 @@ def handler(dn, new, old):
 
 		if old:	## check for trivial changes
 			diff_keys = [ key for key in new.keys() if new.get(key) != old.get(key)  and key not in ('entryCSN', 'modifyTimestamp')]
-			if diff_keys == ['univentionUDMModuleActive']:
+			if diff_keys == ['%sActive' % objectclass]:
 				ud.debug(ud.LISTENER, ud.INFO, '%s: %s: activation status changed.' % (name, new['cn'][0]))
 				return
 			elif diff_keys == ['univentionAppIdentifier']:
@@ -219,55 +293,29 @@ def handler(dn, new, old):
 						ud.debug(ud.LISTENER, ud.WARN, '%s: New version is not higher than version of old object (%s), skipping update.' % (name, old_version))
 					return
 		
-		ocs = new.get('objectClass', [])
-		if 'univentionUDMModule' in ocs:
-			udm_module_name = 'settings/udm_module'
-			target_subdir = 'univention/admin/handlers'
-		elif 'univentionUDMHook' in ocs:
-			udm_module_name = 'settings/udm_hook'
-			target_subdir = 'univention/admin/hooks.d'
-		elif 'univentionUDMSyntax' in ocs:
-			udm_module_name = 'settings/udm_syntax'
-			target_subdir = 'univention/admin/syntax.d'
-		else:
-			ud.debug(ud.LISTENER, ud.ERROR, '%s: Undetermined error: unknown objectclass: %s.' % (name, ocs))
-
 
 		## ok, basic checks passed, handle the data
 		try:
-			new_object_data = zlib.decompress(new.get('univentionUDMModuleData')[0], 16+zlib.MAX_WBITS)
+			new_object_data = zlib.decompress(new.get('%sData' % objectclass)[0], 16+zlib.MAX_WBITS)
 		except TypeError:
 			ud.debug(ud.LISTENER, ud.ERROR, '%s: Error uncompressing data of object %s.' % (name, dn))
 			return
 
-		new_relative_filename = os.path.join(target_subdir, new.get('univentionUDMModuleFilename')[0])
+		new_relative_filename = new.get('%sFilename' % objectclass)[0]
 		listener.setuid(0)
 		try:
-			if not install_python_file(new_object_data, new_relative_filename):
+			if not install_python_file(target_subdir, new_relative_filename, new_object_data):
 				return
 		finally:
 			listener.unsetuid()
 
 	elif old:
-		ocs = old.get('objectClass', [])
-		if 'univentionUDMModule' in ocs:
-			udm_module_name = 'settings/udm_module'
-			target_subdir = 'univention/admin/handlers'
-		elif 'univentionUDMHook' in ocs:
-			udm_module_name = 'settings/udm_hook'
-			target_subdir = 'univention/admin/hooks.d'
-		elif 'univentionUDMSyntax' in ocs:
-			udm_module_name = 'settings/udm_syntax'
-			target_subdir = 'univention/admin/syntax.d'
-		else:
-			ud.debug(ud.LISTENER, ud.ERROR, '%s: Undetermined error: unknown objectclass: %s.' % (name, ocs))
-
 
 		## ok, basic checks passed, handle the change
-		old_relative_filename = os.path.join(target_subdir, old.get('univentionUDMModuleFilename')[0])
+		old_relative_filename = old.get('%sFilename' % objectclass)[0]
 		listener.setuid(0)
 		try:
-			remove_python_file(old_relative_filename)
+			remove_python_file(target_subdir, old_relative_filename)
 		finally:
 			listener.unsetuid()
 
