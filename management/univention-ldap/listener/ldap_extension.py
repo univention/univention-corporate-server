836fea4fdab55a99db79764300b747bb80474cfb
Arvid Requate <requate@univention.de>
2013-09-16 12:54:45 +0000

    * merge settings_ldap* listeners to ldap_extension   to avoid repeated slapd restart + waiting for activation * use new python library module univention.lib.ldap_extension   (Bug #32411, Bug #31801)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-0@44078 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ management/univention-ldap/listener/ldap_extension.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+#
+# Univention LDAP
+"""listener script for ldap schema extensions."""
+#
+# Copyright 2013-2014 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
+__package__ = ''	# workaround for PEP 366
+import listener
+import univention.debug as ud
+import univention.lib.ldap_extension as ldap_extension
+import os
+import subprocess
+
+name = 'ldap_extension'
+description = 'Configure LDAP schema and ACL extensions'
+filter = '(|(objectClass=univentionLDAPExtensionSchema)(objectClass=univentionLDAPExtensionACL))'
+attributes = []
+
+schema_handler = ldap_extension.UniventionLDAPSchema(listener.configRegistry)
+acl_handler = ldap_extension.UniventionLDAPACL(listener.configRegistry)
+
+def handler(dn, new, old):
+	"""Handle LDAP schema extensions on Master and Backup"""
+	global schema_handler, acl_handler
+
+	if new:
+		ocs = new.get('objectClass', [])
+	elif old:
+		ocs = old.get('objectClass', [])
+
+	if 'univentionLDAPExtensionSchema' in ocs:
+		schema_handler.handler(dn, new, old, name=name)
+	elif 'univentionLDAPExtensionACL' in ocs:
+		acl_handler.handler(dn, new, old, name=name)
+	else:
+		ud.debug(ud.LISTENER, ud.ERROR, '%s: Undetermined error: unknown objectclass: %s.' % (name, ocs))
+
+def postrun():
+	"""Restart LDAP server Master and mark new extension objects active"""
+	global schema_handler, acl_handler
+
+	if not listener.configRegistry.get('server/role') == 'domaincontroller_master':
+		if acl_handler._todo_list:
+			## Only set active flag on Master
+			acl_handler._todo_list = []
+		else:
+			## Only restart slapd on Master
+			return
+
+	initscript='/etc/init.d/slapd'
+	if os.path.exists(initscript):
+		listener.setuid(0)
+		try:
+			if schema_handler._do_reload or acl_handler._do_reload:
+				ud.debug(ud.LISTENER, ud.INFO, '%s: Reloading LDAP server.' % (name,) )
+				p = subprocess.Popen([initscript, 'graceful-restart'], close_fds=True)
+				p.wait()
+				for handler_object in (schema_handler, acl_handler,):
+					handler_object._do_reload = False
+				if p.returncode != 0:
+					ud.debug(ud.LISTENER, ud.ERROR, '%s: LDAP server restart returned %s.' % (name, p.returncode))
+					return
+
+			for handler_object in (schema_handler, acl_handler,):
+				handler_object.mark_active()
+
+		finally:
+			listener.unsetuid()
+
