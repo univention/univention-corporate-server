72b0d79275ad8b7a1ed1a631588a986f1ca64560
Florian Best <best@univention.de>
2013-09-30 07:59:26 +0000

    * Bug #24727: use app.linkToModule for links to other UMC modules
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-0@44565 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-join/umc/js/join.js
+++ management/univention-join/umc/js/join.js
@@ -272,7 +272,7 @@ define([
 				}
 				else if (!joined) {
 					if (this._serverRole == 'domaincontroller_master') {
-						dialog.alert(_('A DC master should be joined by the %s.', app.linkToModule('setup')));
+						dialog.alert(_('A DC master should be joined by the %s.', app.linkToModule('setup', undefined, undefined, _('Basic settings module'))));
 						return;
 					}
 					this._switchView('join_form');
