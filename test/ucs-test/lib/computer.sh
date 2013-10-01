663574a26a1372131b1fee0f625e853993b36d62
Stefan Gohmann <gohmann@univention.de>
2013-10-01 12:52:40 +0000

    * lib/computer.sh: use windows as default computer role instead of   managedclient (Bug #32780)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-0@44639 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/lib/computer.sh
+++ test/ucs-test/lib/computer.sh
@@ -8,7 +8,7 @@ computer_randomname () { # Generate a name for a Computer. E.g. COMPUTERNAME=$(c
 
 computer_create () { # Creates a computer. E.g. computer_create "$COMPUTERNAME"
 	local COMPUTERNAME=${1?:missing parameter: computer name}
-	local role=${2:-managedclient}
+	local role=${2:-windows}
 	shift
 	shift
 	log_and_execute udm-test "computers/$role" create \
@@ -19,12 +19,12 @@ computer_create () { # Creates a computer. E.g. computer_create "$COMPUTERNAME"
 
 computer_dn () { #echos the DN of a Computer. E.g. computer_dn $GROUPNAME
 	local name="$1"
-	udm-test computers/managedclient list --filter cn="$name" | sed -ne 's/^DN: //p'
+	udm-test computers/windows list --filter cn="$name" | sed -ne 's/^DN: //p'
 }
 
 computer_remove () { # Removes a computer. E.g. computer_remove "$COMPUTERNAME"
 	local COMPUTERNAME=${1?:missing parameter: name}
-	log_and_execute udm-test computers/managedclient remove --dn "cn=$COMPUTERNAME,cn=computers,$ldap_base"
+	log_and_execute udm-test computers/windows remove --dn "cn=$COMPUTERNAME,cn=computers,$ldap_base"
 }
 
 # vim:set filetype=sh ts=4:
