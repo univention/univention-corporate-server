aa24ed0c5a0ad140ec1def87b40213db92563775
Philipp Hahn <hahn@univention.de>
2013-10-28 19:11:14 +0000

    Bug #28670: USS: Rewrite complete pxe configuration
    Replace all occurrences of the old IP addresses by the new addresses.
    
    Add unit test.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-0@45651 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-system-setup/umc/python/setup/netconf/modules/RewritePxe.py
+++ base/univention-system-setup/umc/python/setup/netconf/modules/RewritePxe.py
@@ -1,6 +1,30 @@
 from univention.management.console.modules.setup.netconf import SkipPhase
 from univention.management.console.modules.setup.netconf.common import AddressMap
 import os
+import re
+
+
+class Mapping(object):
+	def __init__(self, ipv4_changes):
+		self.mapping = dict((
+			(str(old_ip.ip), str(new_ip.ip))
+			for (old_ip, new_ip) in ipv4_changes.items()
+		))
+		pattern = '''\
+		(?:^|(?<![0-9]))
+		(%s)
+		(?:$|(?![0-9]))
+		''' % (
+			'|'.join((re.escape(_) for _ in self.mapping.keys())),
+		)
+		self.regexp = re.compile(pattern, re.VERBOSE)
+
+	def apply(self, string):
+		return self.regexp.sub(self._subst, string)
+
+	def _subst(self, match):
+		pattern = match.group()
+		return self.mapping[pattern]
 
 
 class PhaseRewritePxe(AddressMap):
@@ -16,12 +40,7 @@ class PhaseRewritePxe(AddressMap):
 			raise SkipPhase("No '%s'" % (self.dirname,))
 
 	def pre(self):
-		mapping = dict((
-			(
-				"%s:" % (old_ip.ip,),
-				"%s:" % (new_ip.ip,),
-			) for (old_ip, new_ip) in self.ipv4_changes().items()
-		))
+		mapping = Mapping(self.ipv4_changes())
 		for filename in os.listdir(self.dirname):
 			pathname = os.path.join(self.dirname, filename)
 			self._rewrite_pxe(pathname, mapping)
@@ -30,8 +49,7 @@ class PhaseRewritePxe(AddressMap):
 		self.logger.debug("Processing '%s'...", pathname)
 		with open(pathname, "r") as read_pxe:
 			orig = config = read_pxe.read()
-		for (old_ip, new_ip) in mapping.items():
-			config = config.replace(old_ip, new_ip)
+		config = mapping.apply(orig)
 		if orig == config:
 			self.logger.debug("No change in %s", pathname)
 			return
