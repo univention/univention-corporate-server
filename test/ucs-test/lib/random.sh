22fdb84982853edb83b21fda0d0b7562917ace57
Philipp Hahn <hahn@univention.de>
2013-10-30 23:01:18 +0000

    Bug #32793: test: Fix random generator and remove unused code
    Insert sequence number derived from "seconds since 1970-01-01" and
    "nanoseconds" to make host-, user-, group- and mail-domain-names unique.
    
    Also tried:
    per-process-counter does not work with $(...) in shell.
    pwgen does not support custom character sets.
    makepasswd does not support Unicode characters.
    dd if=/dev/random | tr -d [^] does not work with Unicode characters.
    
    Remove several functions since they're unused (and subtly broken):
     random_lowercasedigit
     getlowercasedigit
     random_maildigit
     getmaildigit
     getletter
     random_letter
     getuppercasedigit
     random_upercasedigit
     getstringstartinguppercase
     random_stringstartinguppercase
     getoctet
     random_octet
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-0@45736 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/lib/random.sh
+++ test/ucs-test/lib/random.sh
@@ -7,40 +7,35 @@ _upperletters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 _ciphers="0123456789"
 
 random_chars () { # [length [characters]]
-	local stringlength=${1:-$STRINGLENGTH}
-	local charset=${2:-${_lowerletters}}
-	local string=
-	while ((${#string} < $stringlength))
+	local prefix= suffix=
+	while [ $# -ge 2 ]
 	do
-		local pos=$((RANDOM % ${#charset}))
-		string+=${charset:$pos:1}
+		case "$1" in
+		--prefix) prefix+="${2:$((RANDOM % ${#2})):1}" ; shift 2 ;;
+		--suffix) suffix+="${2:$((RANDOM % ${#2})):1}" ; shift 2 ;;
+		*) break ;;
+		esac
 	done
-	echo -n "$string"
-}
+	local chars="${2:-${_lowerletters}}"
 
-random_lowercasedigit () { # Generates a random lowercase character (a-z0-9äöü)
-	random_chars 1 "${_lowerletters}${_ciphers}äöü"
-}
+	declare -i now=$(($(date +%s%N) / 1000000 % (24*60*60))) modulo=${#chars}
+	while [ $now -gt 0 ]
+	do
+		suffix="${chars:$((now % modulo)):1}${suffix}"
+		now=$((now / modulo))
+	done
 
-random_uppercasedigit () { # Generates a random uppercase character (A-Z0-9ÄÖÜ)
-	random_chars 1 "${_upperletters}${_ciphers}ÄÖÜ"
-}
-
-random_maildigit () { # Generates a random lowercase character (a-z0-9)
-	random_chars 1 "${_lowerletters}${_ciphers}"
-}
-
-random_letter () { # Generates a random lowercase letter (a-z)
-	random_chars 1 "${_lowerletters}"
+	declare -i length=$((${1:-$STRINGLENGTH}-${#prefix}-${#suffix}))
+	while [ $length -ge 1 ]
+	do
+		prefix+="${chars:$((RANDOM % ${#chars})):1}"
+		length+=-1
+	done
+	echo -n "${prefix}${suffix}"
 }
 
 random_string () { # Generates a random string with length $STRINGLENGTH. The String is lowercase
 	random_chars "$STRINGLENGTH" "${_lowerletters}${_ciphers}äöü"
-}
-
-random_stringstartinguppercase () { # Generates a random string with length $STRINGLENGTH. The String starts uppercase
-	random_chars 1 "${_upperletters}${_ciphers}ÄÖÜ"
-	random_chars "$(($STRINGLENGTH -1))" "${_lowerletters}${_ciphers}äöü"
 }
 
 random_mailaddress () { # Generates a random string with a length of 20 characters
@@ -48,18 +43,11 @@ random_mailaddress () { # Generates a random string with a length of 20 characte
 }
 
 random_hostname () { # Generates a random string with a length of 20
-	random_chars 1 "${_lowerletters}"
-	random_chars 19 "${_lowerletters}${_ciphers}"
+	random_chars --prefix "${_lowerletters}" 20 "${_lowerletters}${_ciphers}"
 }
 
 random_share () { # Generates a random string with length $STRINGLENGTH. string_numbers_letters_dots_spaces
-	random_chars 1 "${_upperletters}${_lowerletters}${_ciphers}"
-	random_chars "$(($STRINGLENGTH -2))" "${_upperletters}${_lowerletters}${_ciphers}._ -"
-	random_chars 1 "${_upperletters}${_lowerletters}${_ciphers}"
-}
-
-random_octet () { # Generates an octet for an ip-adress and echos it.
-	echo -n $((RANDOM % 255))
+	random_chars --prefix "${_upperletters}${_lowerletters}${_ciphers}" --suffix "${_upperletters}${_lowerletters}${_ciphers}" "$STRINGLENGTH" "${_upperletters}${_lowerletters}${_ciphers}._ -"
 }
 
 random_ipv4 () { # Generate a random IPv4 address
