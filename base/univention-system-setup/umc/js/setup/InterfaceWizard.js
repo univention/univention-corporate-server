c06fdc6c413f3b2d04ac08408e081a1421cfb671
Florian Best <best@univention.de>
2014-01-23 11:20:29 +0000

    * Bug #33407: revert changes, don't set maximum IP address possibilities in the frontend
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-0@47372 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-system-setup/umc/js/setup/InterfaceWizard.js
+++ base/univention-system-setup/umc/js/setup/InterfaceWizard.js
@@ -130,10 +130,6 @@ define([
 							// require devicename
 							this.getWidget('name_b').set('required', interfaceType == 'Bridge' || interfaceType == 'Bond');
 
-							// a restriction in UCR enforces that Bridge, VLAN and Bond interfaces can not have multiple IP addresses (Bug #31767)
-							// FIXME: the + button hides on Ethernet page when switch to != Ethernet and switch back
-							this.getWidget('ip4').set('max', (interfaceType !== 'Ethernet') ? 1 : Infinity);
-
 							// adapt visibility of DHCP button
 							this._pages.network._form._buttons.dhcpquery.set('visible', interfaceType === 'Ethernet');
 
