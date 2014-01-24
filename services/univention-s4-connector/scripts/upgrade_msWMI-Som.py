164635258bfc77f78562d1df018e4cf8aa945f8b
Arvid Requate <requate@univention.de>
2014-01-24 16:04:18 +0000

    * add --write2samba4 code to upgrade_msWMI-Som.py (Bug #33956)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs-3.1-1@47435 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-s4-connector/scripts/upgrade_msWMI-Som.py
+++ services/univention-s4-connector/scripts/upgrade_msWMI-Som.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Univention S4 Connector
-#  Add msWMI-Som objects to rejected table
+#  Trigger Synchronization of msWMI-Som objects
 #
 # Copyright 2013-2014 Univention GmbH
 #
@@ -34,7 +34,35 @@
 import sqlite3 as lite
 import subprocess
 import sys
+import ldap
+from optparse import OptionParser
 import univention.config_registry
+import univention.admin.uldap
+import univention.admin.uexceptions
+
+def _connect_ucs(configRegistry, binddn, bindpwd):
+	''' Connect to OpenLDAP '''
+
+	if binddn and bindpwd:
+		bindpw = bindpwd
+	else:
+		bindpw_file = configRegistry.get('connector/ldap/bindpw', '/etc/ldap.secret')
+		binddn = configRegistry.get('connector/ldap/binddn', 'cn=admin,'+configRegistry['ldap/base'])
+		bindpw=open(bindpw_file).read()
+		if bindpw[-1] == '\n':
+			bindpw=bindpw[0:-1]
+
+	host = configRegistry.get('connector/ldap/server', configRegistry.get('ldap/master'))
+
+	try:
+		port = int(configRegistry.get('connector/ldap/port', configRegistry.get('ldap/master/port')))
+	except:
+		port = 7389
+
+	lo = univention.admin.uldap.access(host=host, port=port, base=configRegistry['ldap/base'], binddn=binddn, bindpw=bindpw, start_tls=2, follow_referral=True)
+
+	return lo
+
 
 
 def search_s4(filter, attribute):
@@ -76,12 +104,27 @@ def add_to_sqlite(result):
 	cur.close()
 	dbcon.close()
 
+def trigger_ldap2sd(configRegistry, binddn, bindpwd):
+	''' Touch all UCS objects with objectClass=msWMISom.
+	'''
 
-if __name__ == '__main__':
+	lo = _connect_ucs(configRegistry, binddn, bindpwd)
 
-	configRegistry = univention.config_registry.ConfigRegistry()
-	configRegistry.load()
+	result = {}
+	ldap_result = lo.search('(objectClass=msWMISom)')
+	for dn, attributes in ldap_result:
+		msWMIID = attributes.get('msWMIID', [])[0]
+		ml = [(ldap.MOD_REPLACE, 'msWMIID', msWMIID)]
+		try:
+			print 'Touch UCS object %s' % (dn)
+			lo.lo.modify_s(dn, ml)
+		except ldap.NO_SUCH_OBJECT, ex:
+			pass
+		except ldap.LDAPError, ex:
+			print 'Failure touching UCS object %s (%s)' % (dn, str(ex))
+	pass
 
+def trigger_sd2ldap(configRegistry):
 	# Add CN=System to the reject list
 	result = search_s4('-s base -b CN=System,%s' % configRegistry.get('connector/s4/ldap/base'), 'uSNChanged')
 	add_to_sqlite(result)
@@ -98,4 +141,26 @@ if __name__ == '__main__':
 	result = search_s4('objectClass=msWMI-Som', 'uSNChanged')
 	add_to_sqlite(result)
 
+
+if __name__ == '__main__':
+
+	parser = OptionParser(usage='msGPOWQLFilter.py (--write2ucs|--write2samba4)')
+	parser.add_option("--write2ucs", dest="write2ucs", action="store_true", help="Trigger synchronization of WMI filter objects from Samba 4 to UCS", default=False)
+	parser.add_option("--write2samba4", dest="write2samba4", action="store_true", help="Trigger synchronization of WMI filter objects from UCS to Samba 4", default=False)
+	parser.add_option("--binddn", dest="binddn", action="store", help="Binddn for UCS LDAP connection")
+	parser.add_option("--bindpwd", dest="bindpwd", action="store", help="Password for UCS LDAP connection")
+	(options, args) = parser.parse_args()
+
+	configRegistry = univention.config_registry.ConfigRegistry()
+	configRegistry.load()
+
+	if options.write2ucs:
+		trigger_sd2ldap(configRegistry)
+	elif options.write2samba4:
+		trigger_ldap2sd(configRegistry, options.binddn, options.bindpwd)
+	else:
+		parser.print_help()
+		sys.exit(1)
+
 	sys.exit(0)
+
