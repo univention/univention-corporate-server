07a4c3875efb9fddb670b99ad7a9c561c4601b02
Felix Botner <botner@univention.de>
2014-03-11 11:23:16 +0000

    use user listfilter for listfilter postfix policy (Bug #26910)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-1@48439 1b283449-9f2f-0410-b571-8eb228e1a901

--- mail/univention-mail-postfix/share/listfilter.py
+++ mail/univention-mail-postfix/share/listfilter.py
@@ -55,7 +55,7 @@ def listfilter(attr):
 	if options.ldap_base and sender and recipient:
 
 		# reuse secret file of univention-mail-cyrus
-		ldap = univention.uldap.getMachineConnection(ldap_master=False, secret_file = "/etc/cyrus-ldap.secret")
+		ldap = univention.uldap.getMachineConnection(ldap_master=False, secret_file = "/etc/postfix/listfilter.secret")
 
 		userDn = ""
 		userGroups = []
