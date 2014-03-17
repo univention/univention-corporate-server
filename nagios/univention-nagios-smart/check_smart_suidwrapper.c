b46dec3526f93aed9a1a2541ddc424f1bc9d8a04
Felix Botner <botner@univention.de>
2014-03-17 13:20:39 +0000

    fixed check_smart_suidwrapper.c (Bug #34041)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-1@48585 1b283449-9f2f-0410-b571-8eb228e1a901

--- nagios/univention-nagios-smart/check_smart_suidwrapper.c
+++ nagios/univention-nagios-smart/check_smart_suidwrapper.c
@@ -1,34 +1,55 @@
-#include <signal.h>
-#include <sys/param.h>
-#include <pwd.h>
 #include <unistd.h>
-#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdio.h>
+#include <getopt.h>
 
 #define COMMAND "/usr/lib/nagios/plugins/check_smart.pl"
+#define SIZE 4096
 
-main( int argc, char ** argv, char ** envp )
-{
-    int status = 0;
-    int i = 0;
-    uid_t uid = getuid();
-    char device[50] = "";
-    char interface[50] = "";
+static void usage(void) {
+	fprintf(stderr, "Usage: check_smart_suidwrapper -i INTERFACE -d DEVICE\n");
+}
 
+main(int argc, char ** argv, char ** envp) {
 
-    if( setgid(getegid()) ) perror( "setgid" );
-    if( setuid(geteuid()) ) perror( "setuid" );
-	for(i=0; i<argc; i++) {
-	  if (( strcmp("-d", argv[i]) == 0 ) && (i+1 < argc)) {
-		strcat(device, argv[i+1]);
-	  }
-	  if (( strcmp("-i", argv[i]) == 0 ) && (i+1 < argc)) {
-		strcat(interface, argv[i+1]);
-	  }
+	int i = 0;
+	uid_t uid = getuid();
+	char device[SIZE];
+	char interface[SIZE];
+
+	while ((i = getopt(argc, argv, "::hd:i:")) != -1) {
+		switch (i) {
+			case 'd':
+				strncpy(device, optarg, sizeof(device));
+				device[sizeof(device) -1] = '\0';
+				break;
+			case 'i':
+				strncpy(interface, optarg, sizeof(interface));
+				device[sizeof(interface) -1] = '\0';
+				break;
+			case 'h':
+				usage();
+				exit(0);
+			default:
+				fprintf(stderr, "option %c is undefined\n", optopt);
+				usage();
+				exit(1);	
+		}
 	}
+
+	if (strlen(device) == 0 || strlen(interface) == 0) {
+		usage();
+		exit(1);	
+	}
+
+	if(setgid(getegid()))
+		perror("setgid");
+	if(setuid(geteuid()))
+		perror("setuid");
 	execle(COMMAND, COMMAND, "-d", device, "-i", interface, (char *)0, (char *)0);
 	setuid(uid);
+
 	exit(1);
 }
+
