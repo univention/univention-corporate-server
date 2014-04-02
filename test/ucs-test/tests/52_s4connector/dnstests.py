10af7a11c5d7646e10aa41ec260138f0a499730f
Drees Dormann <dormann@univention.de>
2014-04-02 15:20:29 +0000

    * Renamed tests:   52/400sync_rename_* > 176sync_rename_* (Bug #29211)   52/200sync_create_dns_* > 175sync_create_dns_* (Bug #31324) * Replaced 'raise Exception' with 'sys.exit' in all above tests * Fixed bug in 52/175sync_create_dns_serial (Bug #29211)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-1@48991 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/52_s4connector/dnstests.py
+++ test/ucs-test/tests/52_s4connector/dnstests.py
@@ -8,99 +8,92 @@ import univention.config_registry
 ucr = univention.config_registry.ConfigRegistry()
 ucr.load()
 import os
+import sys
 
-def wait_for_sync(multiplier=1):
+def wait_for_sync(multiplier = 1):
 	synctime = int(ucr.get("connector/s4/poll/sleep",7))
-	synctime = (synctime +3)*multiplier
+	synctime = (synctime + 3) * multiplier
 	print ("Waiting {0} seconds for sync...".format(synctime))
 	sleep (synctime)
 
-def check_ldap_object(item,item_name,item_attribute=None,name_string=None):
+def check_ldap_object(item, item_name, item_attribute = None, name_string = None):
 	print (" Testing Ldap object : {0}			".format(item_name)),
-	if os.isatty:
-		(col,nocol) = ("\033[31m","\033[0m")
-	else:
-		(col,nocol) = ("","")
 	try:
 		if item_attribute:
 			utils.verify_ldap_object(item, {item_attribute:[name_string]})
 		else:
 			utils.verify_ldap_object(item)
 	except:
-		print ('{0} Failed {1}'.format(col,nocol))
-		raise Exception('{0} Verification of Ldap object failed {1}'.format(col,nocol))
+		print (' Failed')
+		sys.exit('Verification of Ldap object failed ')
 	else:
-		if os.isatty:
-			col = ("\033[32m")
-		print('{0} Success {1}'.format(col,nocol))
+		
+		print(' Success ')
 
 
-def test_dns_forward_zone(zone_name,test_object,debug=False):
-	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+NS\s+\"*{1}\"*'.format(zone_name,test_object))
-	match(re_test_object,zone_name,'NS',debug)
+def test_dns_forward_zone(zone_name, test_object):
+	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+NS\s+\"*{1}\"*'.format(zone_name, test_object))
+	match(re_test_object, zone_name, 'NS')
 
-def test_dns_txt(zone_name,test_object,debug=False):
-	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+TXT\s+\"*{1}\"*'.format(zone_name,test_object))
-	match(re_test_object,zone_name,'TXT',debug)
+def test_dns_txt(zone_name, test_object):
+	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+TXT\s+\"*{1}\"*'.format(zone_name, test_object))
+	match(re_test_object, zone_name, 'TXT')
 
 
-def test_dns_ttl(zone_name,test_object,debug=False):
-	re_test_object=re.compile(r"{0}\.*\s+{1}\s+IN\s+SOA".format(zone_name,test_object))
-	match(re_test_object,zone_name,'SOA',debug)
+def test_dns_ttl(zone_name, test_object):
+	re_test_object=re.compile(r"{0}\.*\s+{1}\s+IN\s+SOA".format(zone_name, test_object))
+	match(re_test_object, zone_name, 'SOA')
 
-def test_dns_reverse_zone(zone_name,test_object,debug=False):
+def test_dns_reverse_zone(zone_name, test_object):
 	temp = zone_name.split('.')
-	zone_namereverse = temp[2]+'.'+temp[1]+'.'+temp[0]
-	re_test_object=re.compile(r"{0}.in-addr.arpa.\s+\d+\s+IN\s+NS\s+{1}".format(zone_namereverse,test_object))
-	match(re_test_object,zone_name,'NS',debug,'-x')
+	zone_namereverse = temp[2] + '.' + temp[1] + '.' + temp[0]
+	re_test_object=re.compile(r"{0}.in-addr.arpa.\s+\d+\s+IN\s+NS\s+{1}".format(zone_namereverse, test_object))
+	match(re_test_object, zone_name, 'NS', '-x')
 
-def test_dns_serial(zone_name,test_object,debug=False):
+def test_dns_serial(zone_name, test_object):
 	if debug:
 		print ("Ldap serial : %s"%test_object)
 	re_test_object=re.compile(r"{0}\.*\s+\d+\s+IN\s+SOA\s+.+\s+.+\s+{1}\s+".format(zone_name, test_object))
-	match(re_test_object,zone_name,'SOA',debug)
+	match(re_test_object, zone_name, 'SOA')
 
-def test_dns_a_record(zone_name,test_object,debug=False):
-	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+A\s+\"*{1}\"*'.format(zone_name,test_object))
-	match(re_test_object,zone_name,'A',debug)
+def test_dns_a_record(zone_name, test_object):
+	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+A\s+\"*{1}\"*'.format(zone_name, test_object))
+	match(re_test_object, zone_name, 'A')
 
-def test_dns_aaaa_record(zone_name,test_object,debug=False):
-	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+AAAA\s+\"*{1}\"*'.format(zone_name,test_object))
-	match(re_test_object,zone_name,'AAAA',debug)
+def test_dns_aaaa_record(zone_name, test_object):
+	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+AAAA\s+\"*{1}\"*'.format(zone_name, test_object))
+	match(re_test_object, zone_name ,'AAAA')
 
-def test_dns_alias(zone_name,test_object,debug=False):
-	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+CNAME\s+\"*{1}\"*'.format(zone_name,test_object))
-	match(re_test_object,zone_name,'CNAME',debug)
+def test_dns_alias(zone_name, test_object):
+	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+CNAME\s+\"*{1}\"*'.format(zone_name, test_object))
+	match(re_test_object, zone_name, 'CNAME')
 
-def test_dns_service_record(zone_name,test_object,debug=False):
-	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+SRV\s+\"*{1}\"*'.format(zone_name,test_object))
-	match(re_test_object,zone_name,'SRV',debug)
-
-
+def test_dns_service_record(zone_name, test_object):
+	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+SRV\s+\"*{1}\"*'.format(zone_name, test_object))
+	match(re_test_object, zone_name, 'SRV')
 
 
-def match(re_test_object,zone_name,typ,debug,param=None):
+
+
+def match(re_test_object, zone_name, typ, param=None):
 
 	if not param:
-		dig_subprocess = subprocess.Popen(["dig", zone_name, typ,'+noall','+answer'],shell=False, stdout=subprocess.PIPE).communicate()
+		dig_subprocess = subprocess.Popen(["dig", zone_name, typ, '+noall', '+answer'], shell = False, stdout = subprocess.PIPE).communicate()
 	else:
-		dig_subprocess = subprocess.Popen(["dig", param, zone_name, typ,'+noall','+answer'],shell=False, stdout=subprocess.PIPE).communicate()
+		dig_subprocess = subprocess.Popen(["dig", param, zone_name, typ, '+noall', '+answer'], shell = False, stdout = subprocess.PIPE).communicate()
 	dig_answer = dig_subprocess[0].splitlines()
 
 	found_test_object_match = False
+	print ("Dig Output :")
 	for line in dig_answer:
-		if debug:
-			print line
-		if re.match(re_test_object,line):
+		print line
+		if re.match(re_test_object, line):
 			found_test_object_match = True
-	if os.isatty:
-		(col,nocol) = ("\033[32m","\033[0m")
 	if found_test_object_match:
-		print("{0}  DNS synced {1}".format(col,nocol))
+		print
+		print("  DNS synced ")
 	else:
-		if os.isatty:
-			col = col = ("\033[31m")
-		raise Exception ("{0}  DNS not synced {1}".format(col,nocol))
+		Sys.exit(" DNS not synced ")
 
 
 def get_hostname():
@@ -108,7 +101,7 @@ def get_hostname():
 	return host
 
 def random_name():
-	random_name = '{0} tcp {1}'.format(uts.random_name(),uts.random_name())
+	random_name = '{0} tcp {1}'.format(uts.random_name(), uts.random_name())
 	return random_name
 
 def random_zone():
@@ -121,7 +114,7 @@ def location():
 
 def random_reverse_zone():
 	while True:
-		random_reverse_zone = '{0}.{1}.{2}'.format(random.randrange(1,254), random.randrange(1,254) ,random.randrange(1,254))
+		random_reverse_zone = '{0}.{1}.{2}'.format(random.randrange(1, 254), random.randrange(1, 254), random.randrange(1, 254))
 		try:
 			utils.verify_ldap_object(random_reverse_zone)
 		except:
@@ -132,11 +125,11 @@ def random_reverse_zone():
 
 def make_random_ip():
 	while True:
-		part1 = random.randrange(1,254)
-		part2 = random.randrange(1,254)
-		part3 = random.randrange(1,254)
-		part4 = random.randrange(1,254)
-		randomIP = '{0}.{1}.{2}.{3}'.format(part1,part2,part3,part4)
+		part1 = random.randrange(1, 254)
+		part2 = random.randrange(1, 254)
+		part3 = random.randrange(1, 254)
+		part4 = random.randrange(1, 254)
+		randomIP = '{0}.{1}.{2}.{3}'.format(part1, part2, part3, part4)
 		command = os.system('ping -c 1 {0} >/dev/null'.format(randomIP))
 		if command == 0:
 			pass
@@ -154,4 +147,39 @@ def random_hex():
 	result = []
 	result = ''.join([random.choice('0123456789abcdef') for i in range(4)])
 	return result
+
+def check_group(groupname):
+	samba_tool_subprocess = subprocess.Popen(["samba-tool", 'group', 'list'], shell = False, stdout = subprocess.PIPE).communicate()
+	subprocess_output = samba_tool_subprocess[0].splitlines()
+	re_groupname = re.compile(r"%s"%groupname)
+	group_found = False
+	for line in subprocess_output:
+		if re.match(re_groupname, line):
+			group_found = True
+	if group_found:
+		print ("Group synced to Samba")
+	else:
+		sys.exit("Groupname not synced")
+
+def check_user(username):
+	samba_tool_subprocess = subprocess.Popen(["samba-tool", 'user', 'list'], shell = False, stdout = subprocess.PIPE).communicate()
+	subprocess_output = samba_tool_subprocess[0].splitlines()
+	re_username = re.compile(r"%s"%username)
+	user_found = False
+	for line in subprocess_output:
+		if re.match(re_username, line):
+			user_found = True
+	if user_found:
+		print ("User synced to Samba")
+	else:
+		sys.exit("Username not synced")
+
+def correct_cleanup(group_dn, groupname2, udm_test_instance, return_new_dn = False):
+	tmp = group_dn.split(',')
+	modified_group_dn = 'cn={0},{1},{2},{3}'.format(groupname2, tmp[1], tmp[2], tmp[3])
+	udm_test_instance._cleanup['groups/group'].append(modified_group_dn)
+	if return_new_dn:
+		return modified_group_dn
+
+
 
