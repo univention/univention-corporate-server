4fcda37595aed4792b87517c51de15fa26d29212
Philipp Hahn <hahn@univention.de>
2014-04-14 16:07:54 +0000

    Bug #34315 PAM: Add unit test for /etc/pam.d/common-*
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-1@49335 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ base/univention-pam/tests/common.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e -u
+setup_var () {
+	SELF="$(readlink -f "${0%/*}")"
+	PAMD="$(readlink -f "${0%/*}/../conffiles/etc/pam.d")"
+}
+setup_ucr () {
+	tmp="$(mktemp -d)"
+	trap "rm -rf '$tmp'" EXIT
+	export UNIVENTION_BASECONF="$tmp/base.conf"
+	ucr set dummy='42'
+	echo >>"$UNIVENTION_BASECONF"
+	local IFS=' '
+	echo "auth/methods: $*" >>"$UNIVENTION_BASECONF"
+}
+gen_common () {
+	ucr filter <"$PAMD/common-account" >"$tmp/common-account"
+	cat "$PAMD/common-auth.d/"* | ucr filter >"$tmp/common-auth"
+	cat "$PAMD/common-session.d/"* | ucr filter >"$tmp/common-session"
+	ucr filter <"$PAMD/common-password" >"$tmp/common-password"
+	sed -i -r -e 's/[ \t]+/ /g;/^ *#/d' "$tmp/common-"*
+}
+check () {
+	local service="$1" action="$2" module="$3" arg="${4:-}"
+	local file="$tmp/common-$service" search="${service} ${action} ${module}${arg:+ ${arg}}"
+	fgrep "$search" "$file" && return 0
+	echo "*** MISSING:"
+	echo "$search"
+	echo "*** IN:"
+	grep "^${service} .* ${module}" "$file" ||
+		grep "^${service} " "$file" ||
+		egrep -vx ' *(#.*)?' "$file" ||
+		cat "$file"
+	return 1
+}
+main
