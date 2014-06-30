ba29fd8be56e1ca81cae644184a238ad0a0fadc0
Stefan Gohmann <gohmann@univention.de>
2014-06-30 06:33:01 +0000

    * Update to UCS 4.0 (Bug #35138)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-0@51329 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-licence/internal-tools/make_license.sh
+++ base/univention-licence/internal-tools/make_license.sh
@@ -3,7 +3,7 @@
 # Univention License
 #  Shell Script to generate Univention License Keys
 #
-# Copyright 2004-2012 Univention GmbH
+# Copyright 2004-2014 Univention GmbH
 #
 # http://www.univention.de/
 #
