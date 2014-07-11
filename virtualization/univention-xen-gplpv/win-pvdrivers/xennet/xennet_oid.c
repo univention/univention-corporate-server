a03411e03f660a770aad1376fe9fabf16428bc77
Felix Botner <botner@univention.de>
2014-07-11 10:40:45 +0000

    update to  1.0.1089 Bug #35167
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-2@51734 1b283449-9f2f-0410-b571-8eb228e1a901

--- virtualization/univention-xen-gplpv/win-pvdrivers/xennet/xennet_oid.c
+++
@@ -1,5 +0,0 @@
-#ifdef _GPLPV_NDIS5
-#include "xennet5_oid.c"
-#else
-#include "xennet6_oid.c"
-#endif
--- virtualization/univention-xen-gplpv/win-pvdrivers/xennet/xennet_rx.c
+++
@@ -1,5 +0,0 @@
-#ifdef _GPLPV_NDIS5
-#include "xennet5_rx.c"
-#else
-#include "xennet6_rx.c"
-#endif
