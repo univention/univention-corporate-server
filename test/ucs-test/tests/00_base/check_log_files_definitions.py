e72c52b5c68a118f329dff8df85633bad198aa0f
Dmitry Galkin <galkin@univention.de>
2014-08-01 09:04:13 +0000

    * 00_base/check_log_files_definitions.py: extended and modified patterns,     added separate case for tracebacks;     00_base/99check_log_files: a test to extract warnings, errors and     tracebacks from the logs (Bug #30751)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-2@52452 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/00_base/check_log_files_definitions.py
+++ test/ucs-test/tests/00_base/check_log_files_definitions.py
@@ -1,65 +1,76 @@
 # -*- coding: utf-8 -*.
 import re
 
-# possible erros
-class errors(object):
-	wanted = map(lambda p: re.compile(p, re.IGNORECASE), [
-		".*(traceback).*",
-		".*(error).*",
-		".*(failed).*",
-		".*(usage).*",
-		"^E:",
-	])
-	ignore = map(re.compile, [
-		'ERROR: Site univention-admin does not exist',
-		'.*failedmirror=.*',
-		'All done, no errors.',
-		'I: .* libgpg-error0',
-		'Installation finished. No error reported',
-		'/dev.*/instmnt.*,errors=',
-		'/dev/.*open failed: Read-only file system',
-		'ERROR: Site univention-admin does not exist',
-		'account_policy_get: tdb_fetch_uint32 failed for field .*, returning 0',
-		'psql:/var/lib/postgres/pkgdb.indexes-.*: ERROR:  relation ".*_index" already exists',
-		'.*Not/Inst/Cfg-files/Unpacked/Failed-cfg/Half-inst/trig-aWait/Trig-pend.*',
-		'Not updating .*',
-		'Error: There are no (services|hosts|host groups|contacts|contact groups) defined!',
-		'Total Errors:\s+\d+',
-		'account_policy_get: tdb_fetch_uint32 failed for ',
-		'Cannot find nagios object .*',
-		'ldapError: Type or value exists',									# Bug 19227
-		'sed: read error on stdin: Is a directory',							# Bug 19227
-		'invoke-rc.d: initscript udev, action "reload" failed.',			# Bug 19227
-		'yes: write error',
-	])
 
-# possible warnings
-class warnings(object):
-	wanted = map(lambda p: re.compile(p, re.IGNORECASE), [
-		".*(warning).*",
-	])
-	ignore = map(re.compile, [
-		'.*LibClamAV Warning: \*\*\*.*',
-		'WARNING: The following packages cannot be authenticated!',
-		'Authentication warning overridden.',
-		'^Create .*/warning',
-		'WARNING: You are logged in using SSH -- this may interrupt the update and result in an inconsistent system!',
-		'dpkg - warning: ignoring request to remove .* which isn.t installed.',
-		'dpkg: warning - unable to delete old directory .*: Directory not empty',
-		'dpkg - warning, overriding problem because --force enabled',
-		'WARNING: cannot append .* to .*, value exists',
-		'.*WARNING: ucf was run from a maintainer script that uses debconf, but',
-		'Warning: The config registry variable .*? does not exist',
-		'Total Warnings:\s+\d+',
-		'sys:1: DeprecationWarning: Non-ASCII character.*but no encoding declared; see http://www.python.org/peps/pep-0263.html for details',
-		'warning: commands will be executed using /bin/.*',
-		'Not updating .*',
-		'Warning: The home dir .* you specified already exists.',
-		'dpkg: serious warning: files list file for package .* missing, assuming package has no files currently installed.',
-		'WARNING!',
-		'.* Restarting Squid HTTP proxy squid .* WARNING: .* is a subnetwork of .*',
-		'.* WARNING: because of this .* is ignored to keep splay tree searching predictable',
-		'.* WARNING: You should probably remove .* from the ACL named \'localnet0\'',
-		'.*WARNING: All config files need \.conf: /etc/modprobe\.d/.+, it will be ignored in a future release\.',
-		'update-rc\.d: warning: .* (start|stop) runlevel arguments \([^)]+\) do not match LSB Default-(Start|Stop) values ([^)]+)',
-	])
+class Errors(object):
+    wanted = map(lambda p: re.compile(p, re.IGNORECASE),
+                 [".*(error).*",
+                  ".*(failed).*",
+                  ".*(usage).*",
+                  "^E:"])
+
+    # possible (ignored) errors:
+    ignore_list = [
+        '.*failedmirror=.*',
+        'All done, no errors.',
+        'I: .* libgpg-error0',
+        'Installation finished. No error.* reported',
+        'Not updating .*',
+        'Error: There are no (services|hosts|host groups|contacts|contact groups) defined!',
+        'Total Errors:\s+\d+',
+        'Cannot find nagios object .*',
+        'invoke-rc.d: initscript udev, action "reload" failed.',			# Bug 19227
+        'yes: write error',
+        '.*Update aborted by pre-update script of release.*',
+        '.*update failed. Please check /var/log/univention/.*',
+        '.*failed to convert the username listfilter to the uid.*',
+        '.*Can not write log, .* failed.*',
+        '.*Starting Univention Directory Policy:.*',
+        '.*LISTENER .* : failed to connect to any notifier.*',
+        '.*liberror-perl.*',
+        '.*CONSISTENCY CHECK FAILED: cyls is too large .* setting to possible max .*',
+        '.*error adding .*.pem',
+        '.*failed .*VM used: java-6-cacao.*',
+        '.*/etc/ca-certificates/update.d/.* exited with code 1']
+
+    ignore = map(re.compile, ignore_list)
+
+
+class Tracebacks(object):
+    wanted = map(lambda p: re.compile(p, re.IGNORECASE),
+                 [".*(traceback).*"])
+
+
+class Warnings(object):
+    wanted = map(lambda p: re.compile(p, re.IGNORECASE),
+                 [".*(warning).*"])
+
+    # possible (ignored) warnings:
+    ignore_list = [
+        'WARNING: The following packages cannot be authenticated!',
+        'Authentication warning overridden.',
+        '^Create .*/warning',
+        'WARNING: You are logged in using SSH -- this may interrupt the update and result in an inconsistent system!',
+        'dpkg - warning: ignoring request to remove .* which isn.t installed.',
+        'dpkg: warning - unable to delete old directory .*: Directory not empty',
+        'dpkg - warning, overriding problem because --force enabled',
+        'dpkg: serious warning: files list file for package .* missing, assuming package has no files currently installed.',
+        '.*dpkg: warning: unable to delete old directory .* Directory not empty.*',
+        'WARNING: cannot append .* to .*, value exists',
+        'Warning: The config registry variable .*? does not exist',
+        'Total Warnings:\s+\d+',
+        'sys:1: DeprecationWarning: Non-ASCII character.*but no encoding declared; see http://www.python.org/peps/pep-0263.html for details',
+        'warning: commands will be executed using /bin/.*',
+        'Not updating .*',
+        'Warning: The home dir .* you specified already exists.',
+        'WARNING!',
+        '.*WARNING: All config files need \.conf: /etc/modprobe\.d/.+, it will be ignored in a future release\.',
+        'update-rc\.d: warning: .* (start|stop) runlevel arguments \([^)]+\) do not match LSB Default-(Start|Stop) values ([^)]+)',
+        '.*warning: rule .* already exists.*',
+        '.*Not starting .*: no services enabled.*',
+        '.*Running /etc/init.d/.* is deprecated.*',
+        '.*The resulting partition is not properly aligned for best performance.*',
+        '.*Updating certificates in /etc/ssl/certs.* WARNING: Skipping duplicate certificate ca-certificates.crt.*',
+        '.*Permanently added .* to the list of known hosts.*']
+
+    ignore = map(re.compile, ignore_list)
