e9c63177fc369fe2308b84c111ba88c7aad72a19
Felix Botner <botner@univention.de>
2014-08-06 15:17:38 +0000

    added create_ad_groups etc to lib (Bug #35096)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-3@52580 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-windows-tools/lib/winexe.py
+++ test/ucs-windows-tools/lib/winexe.py
@@ -10,85 +10,135 @@ import ConfigParser
 
 COMMAND_DIR = "/usr/share/ucs-windows-tools/windows-scripts/"
 
+def default_options():
+	usage = "usage: %prog [OPTIONS]"
+	parser = optparse.OptionParser(usage=usage)
+	group = optparse.OptionGroup(parser, "General options")
+	group.add_option("--domain", dest="domain", help="the AD domain name")
+	group.add_option("--domain-admin", dest="domain_admin", help="the domain administrator account")
+	group.add_option("--domain-password", dest="domain_password", help="the domain administrator password")
+	group.add_option("--local-admin", dest="local_admin", help="the local administrator account")
+	group.add_option("--local-password", dest="local_password", help="the local administrator password")
+	group.add_option("--port", dest="port", type="int", default=445, help="winexe port (445)")
+	group.add_option("--client", dest="client", help="the windows client")
+	group.add_option("--debug", dest="debug", action="store_true", default=False, help="verbose output")
+	parser.add_option_group(group)
+	return parser
+
+def check_default_options(self):
+	if not self.domain or not self.domain_admin or not self.domain_password \
+		or not self.local_admin or not self.local_password or not self.port \
+		or not self.client:
+		raise WinExeFailed("Not all mandatory options are defined!")
+
+class WinExeFailed(Exception):
+    '''ucs_addServiceToLocalhost failed'''
+
 class WinExe:
 
-	def __init__(self) :
+	def __init__(self,
+		domain=None,
+		domain_admin=None,
+		domain_password=None,
+		local_admin=None,
+		local_password=None,
+		port=445,
+		client=None,
+		debug=None,
+	):
 
-		self.usage = "usage: %prog [OPTIONS]"
-		self.parser = optparse.OptionParser(usage=self.usage)
-		group = optparse.OptionGroup(self.parser, "General options")
-
-		group.add_option("--domain", dest="domain", help="the AD domain name")
-		group.add_option("--no-domain", dest="no_domain", action="store_true", default=False, help="the AD domain name")
-		group.add_option("--domain-admin", dest="domain_admin", help="the domain administrator account")
-		group.add_option("--domain-password", dest="domain_password", help="the domain administrator password")
-		group.add_option("--local-admin", dest="local_admin", help="the local administrator account")
-		group.add_option("--local-password", dest="local_password", help="the local administrator password")
-		group.add_option("--port", dest="port", type="int", default=445, help="winexe port (445)")
-		group.add_option("--client", dest="client", help="the windows client")
-		group.add_option("--debug", dest="debug", action="store_true", default=False, help="verbose output")
-		
-		self.parser.add_option_group(group)
-
-		self.options = optparse.OptionGroup(self.parser, "More options")
-		self.parser.add_option_group(self.options)
+		#config = ConfigParser.ConfigParser()
+		#config.read(os.path.join(os.environ['HOME'], ".ucs-windows-tools.ini"))
+		#for i in ["domain", "domain_admin", "local_admin", "local_password", "client", "domain_password"]:
+		#	if config.has_section("default") and config.has_option("default", i):
+		#		self.opts.ensure_value(i, config.get("default", i))
 
 		self.command_dir = COMMAND_DIR
+		self.domain = domain
+		self.domain_admin = domain_admin
+		self.domain_password = domain_password
+		self.local_admin = local_admin
+		self.local_password = local_password
+		self.port = port
+		self.client = client
+		self.debug = debug
+
+		check_default_options(self)
 
 		return
 
-	def __error(self, msg):
-		print >> sys.stderr, "%s" % msg
-
 	# TODO better check if IPC$ is reachable for client
 	def __client_reachable(self, timeout=1):
-
 		for i in range(timeout):
 			try:
 				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 				s.settimeout(1)
-				s.connect((self.opts.client, self.opts.port))
+				s.connect((self.client, self.port))
 				return True
 			except socket.error, e:
 				time.sleep(1)
+				self.__debug("checking if client %s:%s is reachable" % (self.client, self.port))
 		return False
 
-	def __build_winexe_cmd(self, domain=True, runas_user=None, runas_password=None):
 
+	def __build_winexe_cmd(self, domain_mode=True, runas_user=None, runas_password=None):
 		cmd = []
 		cmd.append("winexe")
 		cmd.append("--interactive=0")
-		if domain and runas_user and runas_password:
+		if domain_mode and runas_user and runas_password:
 			cmd.append("-U")
-			cmd.append(self.opts.domain + "\\" + self.opts.domain_admin + "%" + self.opts.domain_password)
+			cmd.append(self.domain + "\\" + self.domain_admin + "%" + self.domain_password)
 			cmd.append("--runas")
-			cmd.append(self.opts.domain + "\\" + runas_user + "%" + runas_password)
+			cmd.append(self.domain + "\\" + runas_user + "%" + runas_password)
 		else:
 			cmd.append("-U")
-			cmd.append(self.opts.local_admin + "%" + self.opts.local_password)
+			cmd.append(self.local_admin + "%" + self.local_password)
 		
-		cmd.append("//" + self.opts.client)
+		cmd.append("//" + self.client)
 		return cmd
 
 
-	def __copy_script(self, script=None, domain=True, debug=None):
+	def __debug(self, msg):
+		if self.debug:
+			sys.stdout.write("%s\n" % msg)
+
+	def __trim_windows_stdout(self, msg):
+		new_msg = []
+		for i in msg.split("\r"):
+			i = i.strip()
+			if i.startswith("Microsoft (R) Windows Script Host"):
+				continue
+			if i.startswith("Copyright (C) Microsoft Corporation"):
+				continue
+			if i:
+				new_msg.append(i)
+		return "\n".join(new_msg)
+
+	def __run_command(self, cmd, dont_fail=False):
+		self.__debug("running %s" % " ".join(cmd))
+		p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False)
+		stdout, stderr = p.communicate()
+		self.__debug(stdout)
+		self.__debug(stderr)
+		if p.returncode and not dont_fail:
+		    raise WinExeFailed("command '%s' failed with: %s %s" % (" ".join(cmd), stdout, stderr))
+		stdout = self.__trim_windows_stdout(stdout)
+		return p.returncode, stdout, stderr
+
+
+	def __copy_script(self, script=None, domain_mode=True, runas_user=None, runas_password=None):
 
 		extension = script.split(".")[-1]
-		cmd = self.__build_winexe_cmd(domain=domain)
+		cmd = self.__build_winexe_cmd(domain_mode=domain_mode, runas_user=runas_user, runas_password=runas_password)
 
 		# check certutil
 		# TODO
 		# certutil is required on the client as we need it to decode the
 		# base64 encoded scripts, is there a better way?
 		certutil = cmd + ['certutil']
-		p = subprocess.Popen(certutil, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-		so, se = p.communicate()
-		if debug:
-			print certutil
-			print so
-			print se
-		if p.returncode:
-			self.error_and_exit("certutil not found on client %s!" % self.opts.client)
+		ret, stdout, stderr = self.__run_command(certutil, dont_fail=True)
+		if ret:
+			raise WinExeFailed("certutil not found on client %s! (%s %s)" % (self.client, stderr, stdout))
 
 		# copy file to client in chunks of 4000 chars
 		base64 = open(script, "r").read().encode("base64").replace("\n", "")
@@ -97,66 +147,28 @@ class WinExe:
 		for i in range(0, len(base64), 4000):
 			copy = cmd + ["cmd /C echo %s %s c:\\%s.tmp" % (base64[i:i+4000], overwrite, command)]
 			overwrite = ">>"
-			p = subprocess.Popen(copy, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-			so, se = p.communicate()
-			if debug:
-				print copy
-				print so
-				print se
-			if p.returncode:
-				self.error_and_exit("failed to copy %s.%s (%s, %s, %s)" % (command, extension, p.returncode, so, se))
+			ret, stdout, stderr = self.__run_command(copy, dont_fail=True)
+			if ret:
+				raise WinExeFailed("failed to copy %s.%s (%s, %s, %s)" % (command, extension, ret, stdout, stderr))
 
 		# decode script
 		decode = cmd + ["certutil -f -decode c:\\%s.tmp c:\\%s.%s" % (command, command, extension)]
-		if debug:
-			print decode
-		p = subprocess.Popen(decode, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-		so, se = p.communicate()
-		if p.returncode:
-			self.error_and_exit("failed to copy %s.%s (%s, %s, %s)" % (command, extension, p.returncode, so, se))
+		ret, stdout, stderr = self.__run_command(decode, dont_fail=True)
+		if ret:
+			raise WinExeFailed("failed to decode %s.%s (%s, %s, %s)" % (command, extension, ret, stdout, stderr))
 
 		return 0
 
-	def error_and_exit(self, msg):
-		self.__error(msg)
-		sys.exit(1)
-
-	def check_options(self):
-
-		self.opts, self.args = self.parser.parse_args()
-		config = ConfigParser.ConfigParser()
-		config.read(os.path.join(os.environ['HOME'], ".ucs-windows-tools.ini"))
-		for i in ["domain", "domain_admin", "local_admin", "local_password", "client", "domain_password"]:
-			if config.has_section("default") and config.has_option("default", i):
-				self.opts.ensure_value(i, config.get("default", i))
-		if not self.opts.domain:
-			self.error_and_exit("option --domain is required")
-		if not self.opts.domain_admin:
-			self.error_and_exit("option --domain-admin is required")
-		if not self.opts.domain_password:
-			self.error_and_exit("option --domain-password is required")
-		if not self.opts.local_admin:
-			self.error_and_exit("option --local-admin is required")
-		if not self.opts.local_password:
-			self.error_and_exit("option --local-password is required")
-		if not self.opts.client:
-			self.error_and_exit("option --client is required")
-
-		return
-
 	def winexec(self, *args, **kwarg):
-
-		domain = kwarg.get("domain", True)
-		if self.opts.no_domain:
-			domain = False
-		quiet = kwarg.get("quiet", False)
+		''' run args via winexe on self.client '''
+ 
+		domain_mode = kwarg.get("domain_mode", True)
 		dont_fail = kwarg.get("dont_fail", False)
-		debug = kwarg.get("debug", self.opts.debug)
-		runas_user = kwarg.get("runas_user", self.opts.domain_admin)
-		runas_password = kwarg.get("runas_password", self.opts.domain_password)
+		runas_user = kwarg.get("runas_user", self.domain_admin)
+		runas_password = kwarg.get("runas_password", self.domain_password)
 
 		if len(args) < 1:
-			self.error_and_exit("no command for winexec")
+			raise WinExeFailed("no command for winexec")
 		command = args[0]
 		command_args = []
 		if len(args) > 1:
@@ -165,13 +177,13 @@ class WinExe:
 					command_args.append(str(i))
 
 		if not self.__client_reachable():
-			self.error_and_exit("client %s is not reachable!" % self.opts.client)
+			raise WinExeFailed("client %s is not reachable!" % self.client)
 
-		cmd = self.__build_winexe_cmd(domain=domain, runas_user=runas_user, runas_password=runas_password)
+		cmd = self.__build_winexe_cmd(domain_mode=domain_mode, runas_user=runas_user, runas_password=runas_password)
 
 		script = glob.glob(self.command_dir + command + ".*")
 		if script and len(script) == 1:
-			self.__copy_script(script=script[0], domain=domain, debug=debug)
+			self.__copy_script(script=script[0], domain_mode=domain_mode, runas_user=runas_user, runas_password=runas_password)
 			if script[0].endswith(".bat"):
 				cmd.append("cmd /C call c:\\%s.bat %s" % (command, " ".join(command_args)))
 			elif script[0].endswith(".vbs"):
@@ -179,81 +191,100 @@ class WinExe:
 			elif script[0].endswith(".ps1"):
 				cmd.append("PowerShell.exe -Noninteractive -ExecutionPolicy Bypass -Command c:\\%s.ps1 %s" % (command, " ".join(command_args)))
 			else:
-				self.error_and_exit("script has an unknown file extension: %s"  % script[0])
+				raise WinExeFailed("script has an unknown file extension: %s"  % script[0])
 		else:
 			if command_args:
 				command = "%s %s" % (command, " ".join(command_args))
 			cmd.append(command)
 
-		if debug:
-			print cmd
-
-		# run the command
-		process = subprocess.Popen(
-			cmd,
-			stdout=subprocess.PIPE,
-			stderr=subprocess.PIPE,
-			shell=False)
-		(stdout, stderr) = process.communicate()
-
-		# print command output
-		mystdout = []
-		mystderr = []
-		for i in stdout.split("\r"):
-			i = i.strip()
-			if i.startswith("Microsoft (R) Windows Script Host"):
-				continue
-			if i.startswith("Copyright (C) Microsoft Corporation"):
-				continue
-			if i:
-				mystdout.append(i)
-		for i in stderr.split("\r"):
-			i = i.strip()
-			if i:
-				mystderr.append(i) 
-
-		if not quiet:
-			for i in mystdout:
-				print i
-			for i in mystderr:
-				print i
-
-		# check if command was susccessfull
-		if process.returncode != 0 and not dont_fail:
-			self.error_and_exit("command %s failed with %s" % (command, process.returncode))
-
-		return process.returncode, mystdout, mystderr
+		return self.__run_command(cmd, dont_fail=dont_fail)
 
 	def wait_for_client(self, timeout=1):
+		''' wait timeout seconds until client is reachable and winexe works '''
 
 		# check if client is reachable
 		if not self.__client_reachable(timeout=timeout):
-			self.error_and_exit("wait_for_client failed (client %s is not reachable)" % self.opts.client)
-
+			raise WinExeFailed("waiting for client (%s) failed with timeout %s" % (self.client, timeout))
 		# check winexe
 		for i in range(timeout):
-			retval, stdout, stderr = self.winexec("cmd /C dir", quiet=True, dont_fail=True)
+			retval, stdout, stderr = self.winexec("cmd /C dir", dont_fail=True)
 			if retval == 0:
 				return 0
 			time.sleep(1)
+			self.__debug("wating for client %s" % self.client)
+		# failed to connect
+		raise WinExeFailed("waiting for client (%s) failed with timeout %s (can't run winexe)" % (self.client, timeout))
 
-		self.error_and_exit("wait_for_client failed (winexe to %s failed)" % self.opts.client)
+		return True
+
 
 	def wait_until_client_is_gone(self, timeout=1):
+		''' wait until self.client is no longer reachable '''
 
 		for i in range(timeout):
 			if not self.__client_reachable(timeout=1):
 				return 0
 			time.sleep(1)
+		raise WinExeFailed("waiting until client (%s) is gone failed with timeout %s" % (self.client, timeout))
 
-		self.error_and_exit("wait_until_client_is_gone failed, client %s still reachable after %s attempts" % (self.opts.client, timeout))
+		return True
+
 
 	def check_user_login(self, runas_user, runas_password):
+		''' run klist with user runas_user '''
+
 		ret, stdout, stderr = self.winexec(
 			"klist",
 			runas_user=runas_user,
 			runas_password=runas_password,
-			quiet=True,
-			dont_fail=True)
+			dont_fail=True,
+		)
 		if ret != 0:
-			self.error_and_exit("check_user_login for %s failed with %s (%s)" % (runas_user, ret, stdout + stderr))
+			raise WinExeFailed("check_user_login for %s failed with %s: %s" % (runas_user, ret, stdout + stderr))
+
+		return True
+
+
+	def check_name_server(self, dns_server):
+		''' check (dig) if dns_server is a DNS server '''
+		self.__run_command(["dig", "@%s" % dns_server])
+
+
+	def domain_join(self, dns_server=None):
+		''' join self.client into self.domain '''
+
+		self.check_name_server(dns_server)
+		self.winexec("set-dns-server", dns_server, domain_mode=False)
+		self.winexec("domain-join", self.domain, self.domain_admin, self.domain_password, domain_mode=False)
+		self.winexec("firewall-turn-off")
+		self.winexec("reboot")
+		self.wait_until_client_is_gone(timeout=120)
+		self.wait_for_client(timeout=120)
+		self.winexec("check-domain", self.domain)
+		self.winexec("activate-ts-service")
+		self.check_user_login(self.domain_admin, self.domain_password)
+
+		return True
+
+
+	def list_domain_users(self):
+		''' list all the domain users '''
+
+		return self.winexec("ad-users")
+
+
+	def create_ad_users(self, username, password, users):
+		''' creates users users with prefix username and password password '''
+		
+		return self.winexe.winexec("create-ad-users", username, password, users)
+
+
+	def create_ad_groups(self, groupname, groups):
+		''' creates groups groups with prefix groupname '''
+
+		return self.winexe.winexec("create-ad-groups", groupname, groups)
+
+	def add_users_to_group(self, username, users, groupname, groups):
+		''' adds users with prefix username to groups with prefix groupname '''
+
+		return self.winexe.winexec("add-users-to-group", username, users, groupname, groups)
