9e76307c4d430fa1d815815563c0c4126f651b91
Arvid Requate <requate@univention.de>
2014-08-06 16:13:16 +0000

    * Add log message (Bug #34776) * Catch a sqlite3 exception in case the S4 table is not yet created   (Bug #35346) * Cleanup conflicting GPO directories before the robocopy phase (Bug #34776) * reduce and simplify the patch (Bug #34776) * Fix logic change introduced in last commit (Bug #34776) * Fix two log messages for (Bug #34776)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-3@52587 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console-module-adtakeover/umc/python/adtakeover/takeover.py
+++ management/univention-management-console-module-adtakeover/umc/python/adtakeover/takeover.py
@@ -1063,6 +1063,19 @@ class AD_Takeover():
 		for obj in msgs:
 			name = obj["cn"][0]
 			run_and_output_to_log(["/usr/sbin/univention-directory-manager", "container/msgpo", "delete", "--filter", "name=%s" % name], log.debug)
+			gpo_path = '/var/lib/samba/sysvol/%s/Policies/%s' % (ucr["domainname"], name,)
+			if os.path.exists(gpo_path):
+				log.info("Removing associated conflicting GPO directory %s." % (gpo_path,))
+				shutil.rmtree(gpo_path, ignore_errors=True)
+
+			if name.upper() == name:
+				continue
+
+			run_and_output_to_log(["/usr/sbin/univention-directory-manager", "container/msgpo", "delete", "--filter", "name=%s" % name.upper()], log.debug)
+			gpo_path = '/var/lib/samba/sysvol/%s/Policies/%s' % (ucr["domainname"], name.upper(),)
+			if os.path.exists(gpo_path):
+				log.info("Removing associated conflicting GPO directory %s." % (gpo_path,))
+				shutil.rmtree(gpo_path, ignore_errors=True)
 
 	def rewrite_sambaSIDs_in_OpenLDAP(self):
 		### Phase I.b: Pre-Map SIDs (locale adjustment etc.)
@@ -1763,54 +1776,7 @@ def check_gpo_presence():
 
 	samdb = SamDB(os.path.join(SAMBA_PRIVATE_DIR, "sam.ldb"), session_info=system_session(lp), lp=lp)
 
-	## first try to fix AD case issue "6AC1786C-016F-11D2-945F-00C04fB984F9".
-	msgs = samdb.search(base=samdb.domain_dn(), scope=samba.ldb.SCOPE_SUBTREE,
-						expression="(objectClass=groupPolicyContainer)",
-						attrs=["cn", "gPCFileSysPath"])
-	for obj in msgs:
-		name = obj["cn"][0]
-		if name.upper() == name:
-			continue
-
-		change = ldb.Message()
-		change.dn = ldb.Dn(samdb, dn=str(obj.dn))
-		if "gPCFileSysPath" in obj:
-			basedirname = '/var/lib/samba/sysvol/%s/Policies' % ucr["domainname"]
-			subdirname = obj["gPCFileSysPath"][0].split("\\")[-1]
-			if not os.path.exists(os.path.join(basedirname, subdirname)) and \
-				os.path.exists(os.path.join(basedirname, subdirname.replace(name, name.upper()))):
-				new_gPCFileSysPath = obj["gPCFileSysPath"][0].replace(name, name.upper())
-				change["gPCFileSysPath"] = ldb.MessageElement(new_gPCFileSysPath, ldb.FLAG_MOD_REPLACE, "gPCFileSysPath")
-				samdb.modify(change)
-			else:
-				log.warn("GPO %s path %s not found" % (obj.dn, obj["gPCFileSysPath"][0],))
-
-			## check for similar object of different case:
-			msgs2 = samdb.search(base=samdb.domain_dn(), scope=samba.ldb.SCOPE_SUBTREE,
-								expression="(&(objectClass=groupPolicyContainer)(cn=%s))" % (name,),
-								attrs=["cn", "gPCFileSysPath"])
-			if len(msgs2) > 1:
-				for obj2 in msgs2:
-					name2 = obj2["cn"][0]
-					if name2.upper() != name2:
-						## only consider upper case objects here.
-						continue
-					if name2 == name:
-						## obsolete, but better safe than sorry
-						continue
-					if "gPCFileSysPath" in obj2 and obj2["gPCFileSysPath"][0] != new_gPCFileSysPath:
-						log.warn("GPO object %s and %s look similar, but their gPCFileSysPath differ, leaving them untouched." % (obj.dn, obj2.dn))
-						log.warn("Probably %s should be obsolete?" % (obj2.dn,))
-						continue
-					try:
-						log.info("Removing %s from SAM database." % (obj2.dn,))
-						log.info("Keeping similar object %s." % (obj.dn,))
-						samdb.delete(obj2.dn)
-					except:
-						log.debug("Removal of object %s from Samba4 SAM database failed. See %s for details." % (obj2.dn, LOGFILE_NAME,))
-						log.debug(traceback.format_exc())
-						
-	## next check versions
+	## check versions
 	msgs = samdb.search(base=samdb.domain_dn(), scope=samba.ldb.SCOPE_SUBTREE,
 						expression="(objectClass=groupPolicyContainer)",
 						attrs=["cn", "gPCFileSysPath", "versionNumber"])
@@ -2080,9 +2046,13 @@ def wait_for_s4_connector_replication(ucr, lp, progress = None, max_time=None):
 		highestCommittedUSN = msgs[0]["highestCommittedUSN"][0]
 
 		previous_lastUSN = lastUSN
-		c.execute('select value from S4 where key=="lastUSN"')
-		conn.commit()
-		lastUSN = c.fetchone()[0]
+		try:
+			c.execute('select value from S4 where key=="lastUSN"')
+		except sqlite3.OperationalError as ex:
+			log.debug(str(ex))
+		else:
+			conn.commit()
+			lastUSN = c.fetchone()[0]
 
 		if not ( lastUSN == highestCommittedUSN and lastUSN == previous_lastUSN and highestCommittedUSN == previous_highestCommittedUSN ):
 			static_count = 0
