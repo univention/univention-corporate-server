69819a357c0499e57a31a207afa8513b21ba680a
Janek Walkenhorst <walkenhorst@univention.de>
2014-08-22 17:24:38 +0000

    univention-mail-cyrus (7.0.0-1) unstable; urgency=medium
      * Update to UCS 4.0 (Bug #35264)
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-0@52980 1b283449-9f2f-0410-b571-8eb228e1a901

--- mail/univention-mail-cyrus/univention-mail-valid-mboxname.py
+++ mail/univention-mail-cyrus/univention-mail-valid-mboxname.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python2.6
+#!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 #
 # Univention Mail Cyrus
 #  check for invalid chars in imap folder names
 #
-# Copyright (C) 2011-2013 Univention GmbH
+# Copyright (C) 2011-2014 Univention GmbH
 #
 # http://www.univention.de/
 #
