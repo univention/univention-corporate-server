606b827216ba5804b4dea051b9dcb3c8803369df
Dmitry Galkin <galkin@univention.de>
2014-08-25 15:17:45 +0000

    * 60_umc-system/samba_repl.sh: checkpkg output > /dev/null     60_umc-system/31_umc-service-user-authentication: wait bit more     after user is being deleted (Bug #34369)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-3@53010 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/60_umc-system/samba_repl.sh
+++ test/ucs-test/tests/60_umc-system/samba_repl.sh
@@ -3,7 +3,7 @@
 
 username="$1"
 
-if checkpkg "univention-samba4"; then
+if checkpkg "univention-samba4" 2>/dev/null; then
 	wait_for_drs_replication "(sAMAccountName=$username)" objectSid
 else
         echo 'No need to wait for replication in S3 environment...'
