1c8c91eb075acca18d1abb8fb431fdb7b5418cc7
Dmitry Galkin <galkin@univention.de>
2014-09-05 08:55:06 +0000

    * 00_base/99check_log_files and 00_base/check_log_files_definitions.py:    applied patch by Philipp: Join individual regular expression patterns    into one regexp (Bug #30751).
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-3@53396 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/00_base/check_log_files_definitions.py
+++ test/ucs-test/tests/00_base/check_log_files_definitions.py
@@ -2,77 +2,90 @@
 import re
 
 
-class Errors(object):
-    wanted = map(lambda p: re.compile(p, re.IGNORECASE),
-                 [".*(error).*",
-                  ".*(failed).*",
-                  ".*(usage).*",
-                  "^E:"])
+class LogMessage(object):
+	def __init__(self, wanted=None, ignore=None):
+		self.wanted = self.recomp(self.wanted_list + (wanted or [])).match
+		self.ignore = self.recomp(self.ignore_list + (ignore or [])).match
 
-    # possible (ignored) errors:
-    ignore_list = [
-        '.*failedmirror=.*',
-        'All done, no errors.',
-        'I: .* libgpg-error0',
-        'Installation finished. No error.* reported',
-        'Not updating .*',
-        'Error: There are no (services|hosts|host groups|contacts|contact groups) defined!',
-        'Total Errors:\s+\d+',
-        'Cannot find nagios object .*',
-        'invoke-rc.d: initscript udev, action "reload" failed.',			# Bug 19227
-        'yes: write error',
-        '.*Update aborted by pre-update script of release.*',
-        '.*update failed. Please check /var/log/univention/.*',
-        '.*failed to convert the username listfilter to the uid.*',
-        '.*Can not write log, .* failed.*',
-        '.*Starting Univention Directory Policy:.*',
-        '.*LISTENER .* : failed to connect to any notifier.*',
-        '.*liberror-perl.*',
-        '.*CONSISTENCY CHECK FAILED: cyls is too large .* setting to possible max .*',
-        '.*error adding .*.pem',
-        '.*failed .*VM used: java-6-cacao.*',
-        '.*/etc/ca-certificates/update.d/.* exited with code 1']
-
-    ignore = map(re.compile, ignore_list)
+	@staticmethod
+	def recomp(patterns, ignore_case=True):
+		pattern = '|'.join('(?:%s)' % _ for _ in patterns)
+		return re.compile(pattern, re.IGNORECASE if ignore_case else 0)
 
 
-class Tracebacks(object):
-    wanted = map(lambda p: re.compile(p, re.IGNORECASE),
-                 [".*(traceback).*"])
+class Errors(LogMessage):
+	wanted_list = [
+		".*error.*",
+		".*failed.*",
+		".*usage.*",
+		"^E:",
+	]
+
+	# possible (ignored) errors:
+	ignore_list = [
+		'.*failedmirror=.*',
+		'All done, no errors.',
+		'I: .* libgpg-error0',
+		'Installation finished. No error.* reported',
+		'Not updating .*',
+		'Error: There are no (?:services|hosts|host groups|contacts|contact groups) defined!',
+		'Total Errors:\s+\d+',
+		'Cannot find nagios object .*',
+		'invoke-rc.d: initscript udev, action "reload" failed.',			# Bug 19227
+		'yes: write error',
+		'.*Update aborted by pre-update script of release.*',
+		'.*update failed. Please check /var/log/univention/.*',
+		'.*failed to convert the username listfilter to the uid.*',
+		'.*Can not write log, .* failed.*',
+		'.*Starting Univention Directory Policy:.*',
+		'.*LISTENER .* : failed to connect to any notifier.*',
+		'.*liberror-perl.*',
+		'.*CONSISTENCY CHECK FAILED: cyls is too large .* setting to possible max .*',
+		'.*error adding .*.pem',
+		'.*failed .*VM used: java-6-cacao.*',
+		'.*/etc/ca-certificates/update.d/.* exited with code 1',
+	]
 
 
-class Warnings(object):
-    wanted = map(lambda p: re.compile(p, re.IGNORECASE),
-                 [".*(warning).*"])
+class Tracebacks(LogMessage):
+	wanted_list = [
+		".*traceback.*",
+	]
 
-    # possible (ignored) warnings:
-    ignore_list = [
-        'WARNING: The following packages cannot be authenticated!',
-        'Authentication warning overridden.',
-        '^Create .*/warning',
-        'WARNING: You are logged in using SSH -- this may interrupt the update and result in an inconsistent system!',
-        'dpkg - warning: ignoring request to remove .* which isn.t installed.',
-        'dpkg: warning - unable to delete old directory .*: Directory not empty',
-        'dpkg - warning, overriding problem because --force enabled',
-        'dpkg: serious warning: files list file for package .* missing, assuming package has no files currently installed.',
-        '.*dpkg: warning: unable to delete old directory .* Directory not empty.*',
-        'WARNING: cannot append .* to .*, value exists',
-        'Warning: The config registry variable .*? does not exist',
-        'Total Warnings:\s+\d+',
-        'sys:1: DeprecationWarning: Non-ASCII character.*but no encoding declared; see http://www.python.org/peps/pep-0263.html for details',
-        'warning: commands will be executed using /bin/.*',
-        'Not updating .*',
-        'Warning: The home dir .* you specified already exists.',
-        'WARNING!',
-        '.*WARNING: All config files need \.conf: /etc/modprobe\.d/.+, it will be ignored in a future release\.',
-        'update-rc\.d: warning: .* (start|stop) runlevel arguments \([^)]+\) do not match LSB Default-(Start|Stop) values ([^)]+)',
-        '.*warning: rule .* already exists.*',
-        '.*Not starting .*: no services enabled.*',
-        '.*Running /etc/init.d/.* is deprecated.*',
-        '.*The resulting partition is not properly aligned for best performance.*',
-        '.*Updating certificates in /etc/ssl/certs.* WARNING: Skipping duplicate certificate ca-certificates.crt.*',
-        '.*Permanently added .* to the list of known hosts.*',
-        '.*usr/sbin/grub-probe: warning: disk does not exist, so falling back to partition device.*',
-        '.*WARNING: cannot read /sys/block/vda.* (No such file or directory|Datei oder Verzeichnis nicht gefunden).*']
+	ignore_list = []
 
-    ignore = map(re.compile, ignore_list)
+
+class Warnings(LogMessage):
+	wanted_list = [
+		".*warning.*",
+	]
+
+	# possible (ignored) warnings:
+	ignore_list = [
+		'WARNING: The following packages cannot be authenticated!',
+		'Authentication warning overridden.',
+		'^Create .*/warning',
+		'WARNING: You are logged in using SSH -- this may interrupt the update and result in an inconsistent system!',
+		'dpkg - warning: ignoring request to remove .* which isn.t installed.',
+		'dpkg: warning - unable to delete old directory .*: Directory not empty',
+		'dpkg - warning, overriding problem because --force enabled',
+		'dpkg: serious warning: files list file for package .* missing, assuming package has no files currently installed.',
+		'.*dpkg: warning: unable to delete old directory .* Directory not empty.*',
+		'WARNING: cannot append .* to .*, value exists',
+		'Warning: The config registry variable .*? does not exist',
+		'Total Warnings:\s+\d+',
+		'sys:1: DeprecationWarning: Non-ASCII character.*but no encoding declared; see http://www.python.org/peps/pep-0263.html for details',
+		'warning: commands will be executed using /bin/.*',
+		'Not updating .*',
+		'Warning: The home dir .* you specified already exists.',
+		'WARNING!',
+		'.*WARNING: All config files need \.conf: /etc/modprobe\.d/.+, it will be ignored in a future release\.',
+		'update-rc\.d: warning: .* (?:start|stop) runlevel arguments \([^)]+\) do not match LSB Default-(?:Start|Stop) values [^)]+',
+		'.*warning: rule .* already exists.*',
+		'.*Not starting .*: no services enabled.*',
+		'.*Running /etc/init.d/.* is deprecated.*',
+		'.*The resulting partition is not properly aligned for best performance.*',
+		'.*Updating certificates in /etc/ssl/certs.* WARNING: Skipping duplicate certificate ca-certificates.crt.*',
+		'.*Permanently added .* to the list of known hosts.*',
+		'.*usr/sbin/grub-probe: warning: disk does not exist, so falling back to partition device.*',
+		'.*WARNING: cannot read /sys/block/vda.* (?:No such file or directory|Datei oder Verzeichnis nicht gefunden).*']
