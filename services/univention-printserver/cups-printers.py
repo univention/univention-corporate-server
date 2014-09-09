1d59d0e13357d731615ce311c3e535f5364e061d
Arvid Requate <requate@univention.de>
2014-09-09 13:06:33 +0000

    * New UCR Variable samba/force_printername (Bug #33505)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-0@53486 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-printserver/cups-printers.py
+++ services/univention-printserver/cups-printers.py
@@ -351,7 +351,8 @@ def handler(dn, new, old):
 				print >>fp, 'path = /tmp'
 				print >>fp, 'guest ok = yes'
 				print >>fp, 'printable = yes'
-				print >>fp, 'force printername = yes'
+				if listener.baseConfig.is_true('samba/force_printername', True):
+					print >>fp, 'force printername = yes'
 				if perm:
 					if new['univentionPrinterACLtype'][0] == 'allow':
 						print >>fp, 'valid users = %s' % perm
