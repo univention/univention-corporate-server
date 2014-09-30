7b9eb9fe638ed1f1839dd2bae1a920d0f3489b92
Florian Best <best@univention.de>
2014-09-30 10:45:27 +0000

    * Bug #35654: UMC design adaptions
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-0@54053 1b283449-9f2f-0410-b571-8eb228e1a901

--- virtualization/univention-virtual-machine-manager-daemon/umc/js/uvmm/CloudConnectionWizard.js
+++ virtualization/univention-virtual-machine-manager-daemon/umc/js/uvmm/CloudConnectionWizard.js
@@ -26,30 +26,20 @@
  * /usr/share/common-licenses/AGPL-3; if not, see
  * <http://www.gnu.org/licenses/>.
  */
+/*global define*/
 
 define([
 	"dojo/_base/declare",
 	"dojo/_base/lang",
 	"dojo/_base/array",
-	"dojo/store/Memory",
-	"dojo/store/Observable",
-	"dijit/form/MappedTextBox",
-	"umc/tools",
-	"umc/dialog",
-	"umc/widgets/TitlePane",
-	"umc/widgets/TextArea",
 	"umc/widgets/TextBox",
-	"umc/widgets/Text",
 	"umc/widgets/ComboBox",
 	"umc/widgets/CheckBox",
 	"umc/widgets/HiddenInput",
 	"umc/widgets/Wizard",
 	"umc/widgets/Form",
-	"umc/widgets/ContainerWidget",
-	"umc/modules/uvmm/DriveGrid",
-	"umc/modules/uvmm/types",
 	"umc/i18n!umc/modules/uvmm"
-], function(declare, lang, array, Memory, Observable, MappedTextBox, tools, dialog, TitlePane, TextArea, TextBox, Text, ComboBox, CheckBox, HiddenInput, Wizard, Form, ContainerWidget, DriveGrid, types, _) {
+], function(declare, lang, array, TextBox, ComboBox, CheckBox, HiddenInput, Wizard, Form, _) {
 
 	return declare("umc.modules.uvmm.CloudConnectionWizard", [ Wizard ], {
 		autoValidate: true,
@@ -78,80 +68,79 @@ define([
 						'service_region',
 						'service_type',
 						'service_name',
-						'base_url',
+						'base_url'
 					],
-					widgets: 
-				[{
-					name: 'username',
-					type: TextBox,
-					label: _('Username'),
-					required: true
-				}, {
-					name: 'auth_version',
-					type: ComboBox,
-					label: _('Use the following authentication type'),
-					staticValues: [
-						{ id: '2.0_password', label: _('Password') },
-						{ id: '2.0_apikey', label: _('API Key') },
-					],
-					onChange: lang.hitch(this, function(value){
-						var widget = this.getWidget('credentials', 'parameter').getWidget('password');
-						widget.set('disabled', value.indexOf('2.0_password') < 0);
-						var widget = this.getWidget('credentials', 'parameter').getWidget('auth_token');
-						widget.set('disabled', value.indexOf('2.0_apikey') < 0);
-					}),
-					required: true
-				}, {
-					name: 'password',
-					type: TextBox,
-					label: _('Password'),
-					depends: 'auth_version',
-					required: true
-				}, {
-					name: 'auth_token',
-					type: TextBox,
-					label: _('API Key'),
-					depends: 'auth_version',
-					required: true
-				}, {
-					name: 'auth_url',
-					type: TextBox,
-					label: _('Authentication URL endpoint'),
-					required: true,
-					validator: this._validateUrl,
-					invalidMessage: this._invalidUrlMessage,
-				}, {
-					name: 'tenant',
-					type: TextBox,
-					label: _('Tenant'),
-					required: false
-				}, {
-					name: 'service_region',
-					type: TextBox,
-					label: _('Service region'),
-					required: false
-				}, {
-					name: 'service_type',
-					type: TextBox,
-					label: _('Service type'),
-					value: 'compute',
-					required: false
-				}, {
-					name: 'service_name',
-					type: TextBox,
-					label: _('Service name'),
-					value: 'nova',
-					required: false
-				}, {
-					name: 'base_url',
-					type: TextBox,
-					label: _('Service URL endpoint'),
-					required: false,
-					validator: this._validateUrl,
-					invalidMessage: this._invalidUrlMessage,
-				}]
+					widgets: [{
+						name: 'username',
+						type: TextBox,
+						label: _('Username'),
+						required: true
+					}, {
+						name: 'auth_version',
+						type: ComboBox,
+						label: _('Use the following authentication type'),
+						staticValues: [
+							{ id: '2.0_password', label: _('Password') },
+							{ id: '2.0_apikey', label: _('API Key') }
+						],
+						onChange: lang.hitch(this, function(value){
+							var password = this.getWidget('credentials', 'parameter').getWidget('password');
+							password.set('disabled', value.indexOf('2.0_password') < 0);
+							var auth_token = this.getWidget('credentials', 'parameter').getWidget('auth_token');
+							auth_token.set('disabled', value.indexOf('2.0_apikey') < 0);
+						}),
+						required: true
+					}, {
+						name: 'password',
+						type: TextBox,
+						label: _('Password'),
+						depends: 'auth_version',
+						required: true
+					}, {
+						name: 'auth_token',
+						type: TextBox,
+						label: _('API Key'),
+						depends: 'auth_version',
+						required: true
+					}, {
+						name: 'auth_url',
+						type: TextBox,
+						label: _('Authentication URL endpoint'),
+						required: true,
+						validator: this._validateUrl,
+						invalidMessage: this._invalidUrlMessage
+					}, {
+						name: 'tenant',
+						type: TextBox,
+						label: _('Tenant'),
+						required: false
+					}, {
+						name: 'service_region',
+						type: TextBox,
+						label: _('Service region'),
+						required: false
+					}, {
+						name: 'service_type',
+						type: TextBox,
+						label: _('Service type'),
+						value: 'compute',
+						required: false
+					}, {
+						name: 'service_name',
+						type: TextBox,
+						label: _('Service name'),
+						value: 'nova',
+						required: false
+					}, {
+						name: 'base_url',
+						type: TextBox,
+						label: _('Service URL endpoint'),
+						required: false,
+						validator: this._validateUrl,
+						invalidMessage: this._invalidUrlMessage
+					}]
 				}];
-			};
+			}
 			if (cloudtype == 'EC2') {
 				return [{
 					name: 'parameter',
@@ -161,42 +150,42 @@ define([
 						'region',
 						'access_id',
 						'password',
-						'secure',
+						'secure'
 					],
-					widgets: 
-				[{
-					name: 'access_id',
-					type: TextBox,
-					label: _('Access Key ID'),
-					required: true
-				}, {
-					name: 'password',
-					type: TextBox,
-					label: _('Secret Access Key'),
-					required: true
-				}, {
-					name: 'region',
-					type: ComboBox,
-					staticValues: [
-						{ id: 'EC2_EU_WEST', label: 'EU (Ireland)' },
-						{ id: 'EC2_US_EAST', label: 'US East (N. Virginia)' },
-						{ id: 'EC2_US_WEST', label: 'US West (N. California)' },
-						{ id: 'EC2_US_WEST_OREGON', label: 'US West (Oregon)' },
-						{ id: 'EC2_AP_SOUTHEAST', label: 'Asia Pacific (Sydney)' },
-						{ id: 'EC2_AP_NORTHEAST', label: 'Asia Pacific (Tokyo)' },
-						{ id: 'EC2_AP_SOUTHEAST2', label: 'Asia Pacific (Singapore)' },
-						{ id: 'EC2_SA_EAST', label: 'South America (São Paulo)' },
-					],
-					label: _('EC2 Region'),
-					required: true
-				}, {
-					name: 'secure',
-					type: CheckBox,
-					label: _('Secure connection'),
-					value: true,
-					required: false
-				}
-				]}]};
+					widgets: [{
+						name: 'access_id',
+						type: TextBox,
+						label: _('Access Key ID'),
+						required: true
+					}, {
+						name: 'password',
+						type: TextBox,
+						label: _('Secret Access Key'),
+						required: true
+					}, {
+						name: 'region',
+						type: ComboBox,
+						staticValues: [
+							{ id: 'EC2_EU_WEST', label: 'EU (Ireland)' },
+							{ id: 'EC2_US_EAST', label: 'US East (N. Virginia)' },
+							{ id: 'EC2_US_WEST', label: 'US West (N. California)' },
+							{ id: 'EC2_US_WEST_OREGON', label: 'US West (Oregon)' },
+							{ id: 'EC2_AP_SOUTHEAST', label: 'Asia Pacific (Sydney)' },
+							{ id: 'EC2_AP_NORTHEAST', label: 'Asia Pacific (Tokyo)' },
+							{ id: 'EC2_AP_SOUTHEAST2', label: 'Asia Pacific (Singapore)' },
+							{ id: 'EC2_SA_EAST', label: 'South America (São Paulo)' }
+						],
+						label: _('EC2 Region'),
+						required: true
+					}, {
+						name: 'secure',
+						type: CheckBox,
+						label: _('Secure connection'),
+						value: true,
+						required: false
+					} ]
+				}];
+			}
 			return [{}];
 		},
 
