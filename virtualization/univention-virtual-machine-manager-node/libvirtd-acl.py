919e1d368f02e873dd1c3ca9ceea0a98d5ce80e8
Philipp Hahn <hahn@univention.de>
2014-10-20 14:26:35 +0000

    Bug #33694 KVM: Disable bridge creation by default
    Update to rename from libvirt-bin to libvirtd.
    Update template file from Debian-Wheezy.
    debian/control: Remove Andreas Büsching.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-0@54669 1b283449-9f2f-0410-b571-8eb228e1a901

--- virtualization/univention-virtual-machine-manager-node/libvirtd-acl.py
+++ virtualization/univention-virtual-machine-manager-node/libvirtd-acl.py
@@ -101,7 +101,7 @@ def postrun():
 		listener.setuid(0)
 		try:
 			# "libvirtd reload" only reloads the driver state, not the config file!
-			ret = subprocess.call(['invoke-rc.d', 'libvirt-bin', 'restart'])
+			ret = subprocess.call(['invoke-rc.d', 'libvirtd', 'restart'])
 			need_restart = False
 		finally:
 			listener.unsetuid()
