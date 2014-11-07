b1f19138196d2046ddea590d6f7045424ca477de
Stefan Gohmann <gohmann@univention.de>
2014-11-07 12:49:16 +0000

    * Adjust pre- and postup.sh for UCS 3.2-4 (Bug #36403)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-4@55503 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-updater/script/postup.sh
+++ base/univention-updater/script/postup.sh
@@ -107,14 +107,14 @@ fi
 
 # Move to mirror mode for previous errata component
 ucr set \
-	repository/online/component/3.2-2-errata=false \
-	repository/online/component/3.2-2-errata/localmirror=true >>"$UPDATER_LOG" 2>&1
+	repository/online/component/3.2-3-errata=false \
+	repository/online/component/3.2-3-errata/localmirror=true >>"$UPDATER_LOG" 2>&1
 
-# Set errata component for UCS 3.2-3
+# Set errata component for UCS 3.2-4
 ucr set \
-	repository/online/component/3.2-3-errata=enabled \
-	repository/online/component/3.2-3-errata/description="Errata updates for UCS 3.2-3" \
-	repository/online/component/3.2-3-errata/version="3.2" >>"$UPDATER_LOG" 2>&1
+	repository/online/component/3.2-4-errata=enabled \
+	repository/online/component/3.2-4-errata/description="Errata updates for UCS 3.2-4" \
+	repository/online/component/3.2-4-errata/version="3.2" >>"$UPDATER_LOG" 2>&1
 
 # make sure that UMC server is restarted (Bug #33426)
 echo "
