b866641da9f6d3c2655bc0eb5751803995af260e
Sönke Schwardt-Krummrich <schwardt@univention.de>
2014-11-07 16:53:33 +0000

    Bug #35537: several bugfixes
    - new defaults for pxe/installer/kernel and pxe/installer/initrd
    - fixes for the listener module
    - removed unused UCR variables
    - update UCR variable descriptions
    - ...
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-0@55531 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-net-installer/serverinstallerpxe.py
+++ services/univention-net-installer/serverinstallerpxe.py
@@ -29,26 +29,29 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
-__package__='' 	# workaround for PEP 366
+__package__ = '' 	# workaround for PEP 366
 import univention.config_registry
 
-name='serverinstallerpxe'
-description='PXE configuration for the Server installer'
-filter='(|(objectClass=univentionDomainController)(objectClass=univentionMemberServer)(objectClass=univentionClient)(objectClass=univentionMobileClient))'
-attributes=['univentionServerReinstall', 'aRecord', 'univentionServerInstallationProfile', 'univentionServerInstallationOption']
+name = 'serverinstallerpxe'
+description = 'PXE configuration for the Server installer'
+filter = '(|(objectClass=univentionDomainController)(objectClass=univentionMemberServer)(objectClass=univentionClient)(objectClass=univentionMobileClient))'
+attributes = ['univentionServerReinstall', 'aRecord', 'univentionServerInstallationProfile', 'univentionServerInstallationOption']
 
 import listener
-import os, univention.debug
+import os
+import univention.debug
 
 ucr = listener.configRegistry
 
 pxebase = '/var/lib/univention-client-boot/pxelinux.cfg'
+
 
 def ip_to_hex(ip):
 	if ip.count('.') != 3:
 		return ''
 	o = ip.split('.')
 	return '%02X%02X%02X%02X' % (int(o[0]), int(o[1]), int(o[2]), int(o[3]))
+
 
 def handler(dn, new, old):
 	ucr.load()
@@ -58,12 +61,12 @@ def handler(dn, new, old):
 	else:
 		append = 'auto=true priority=critical video=vesa:ywrap,mtrr '
 		append += 'vga=%s ' % ucr.get("pxe/installer/vga", "788")
-		append += 'initrd=%s ' % (ucr.get('pxe/installer/initrd', 'linux.bin'),)
+		append += 'initrd=%s ' % (ucr.get('pxe/installer/initrd', 'initrd.gz'),)
 		if ucr.is_true('pxe/installer/quiet', False):
 			append += 'quiet '
 		append += 'loglevel=%s ' % ucr.get('pxe/installer/loglevel', '0')
 		if new.get('univentionServerInstallationProfile'):
-			append += 'url=http://%s.%s/installer/./%s ' % (ucr.get('hostname'), ucr.get('domainname'), new.get('univentionServerInstallationProfile'))
+			append += 'url=http://%s.%s/installer/./%s ' % (ucr.get('hostname'), ucr.get('domainname'), new.get('univentionServerInstallationProfile')[0])
 
 	pxeconfig = '''# Perform a profile installation by default
 PROMPT 0
@@ -73,9 +76,10 @@ DEFAULT linux
 LABEL linux
         kernel %(kernel)s
         append %(append)s
-''' % { 'kernel': ucr.get('pxe/installer/kernel', 'linux-server'),
+''' % {
+		'kernel': ucr.get('pxe/installer/kernel', 'linux'),
 		'append': append,
-		'additional_options': new.get('univentionServerInstallationOption', '')
+		'additional_options': new.get('univentionServerInstallationOption', ''),
 		}
 
 	# remove pxe host file
@@ -103,7 +107,7 @@ LABEL linux
 			return
 		filename = os.path.join(pxebase, basename)
 
-		if new.get('univentionServerReinstall', ['']) == '1':
+		if new.get('univentionServerReinstall', [''])[0] == '1':
 			listener.setuid(0)
 			try:
 				with open(filename, 'w') as fd:
