c8bf22db32dfd3495144173a6ddf2cd01a440cdb
Felix Botner <botner@univention.de>
2014-11-07 09:34:02 +0000

    fixed TLS Forward Secrecy (Bug #35923)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-3@55480 1b283449-9f2f-0410-b571-8eb228e1a901

--- mail/univention-mail-postfix/share/create-dh-parameter-files.sh
+++ mail/univention-mail-postfix/share/create-dh-parameter-files.sh
@@ -30,9 +30,11 @@
 # <http://www.gnu.org/licenses/>.
 
 # (re)create DH keys
+umask 022
 openssl gendh -out /etc/postfix/dh_512.pem.tmp -2 512 && mv /etc/postfix/dh_512.pem.tmp /etc/postfix/dh_512.pem
-openssl gendh -out /etc/postfix/dh_1024.pem.tmp -2 1024 && mv /etc/postfix/dh_1024.pem.tmp /etc/postfix/dh_1024.pem
+#openssl gendh -out /etc/postfix/dh_1024.pem.tmp -2 1024 && mv /etc/postfix/dh_1024.pem.tmp /etc/postfix/dh_1024.pem
 openssl gendh -out /etc/postfix/dh_2048.pem.tmp -2 2048 && mv /etc/postfix/dh_2048.pem.tmp /etc/postfix/dh_2048.pem
+chmod 644 /etc/postfix/dh_2048.pem /etc/postfix/dh_512.pem
 
 invoke-rc.d postfix reload || true
 
