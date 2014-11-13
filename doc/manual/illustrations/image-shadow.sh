196f67c02b929710437a44dd7fb33bf1cba12bd2
Johannes Keiser <keiser@univention.de>
2014-11-13 13:49:58 +0000

    Bug #36424: * remade appcenter screenshots due to missing ucs logo * remade some screenshots to show more details * added image-shadow.sh script for adding shadow to images
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-0@55769 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ doc/manual/illustrations/image-shadow.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#schleife: 
+# for i in *.png; do ~/image-shadow.sh $i; done
+in=$1
+out=$2
+if [[ $out == "" ]] ; then
+	out=${in%.*}-shadow.${in##*.}
+fi
+echo "Converting file : $in -> $in with shadow"
+convert $in \( +clone -background black -shadow 50x5+0+0 \) +swap -background transparent -layers merge +repage $in
