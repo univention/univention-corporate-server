6eaf2ec9c20263b58ba7004ff1f8df432d5b2831
Alexander Kläser <klaeser@univention.de>
2014-11-18 11:49:26 +0000

    Bug #36724: post-process installer screenshots
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-0@55925 1b283449-9f2f-0410-b571-8eb228e1a901

--- doc/manual/illustrations/image-shadow.sh
+++ doc/manual/illustrations/image-shadow.sh
@@ -2,10 +2,11 @@
 
 #schleife: 
 # for i in *.png; do ~/image-shadow.sh $i; done
+
 in=$1
 out=$2
-if [[ $out == "" ]] ; then
+if [ -z "$out" ]; then
 	out=${in%.*}-shadow.${in##*.}
 fi
-echo "Converting file : $in -> $in with shadow"
+echo "Converting file : $in -> $out with shadow"
 convert $in \( +clone -background black -shadow 40x5+0+0 \) +swap -background transparent -layers merge +repage $in
