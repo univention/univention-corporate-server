56eb6bf1fb2e57f7afd5470355faa980207c904e
Arvid Requate <requate@univention.de>
2014-11-25 15:12:41 +0000

    * Bug #33768: On updates trigger synchronization of GPO NTSDs * Add --filter option to resync_object_from_ucs and resync_object_from_s4
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-4@56138 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-s4-connector/scripts/resync_object_from_s4.py
+++ services/univention-s4-connector/scripts/resync_object_from_s4.py
@@ -31,19 +31,13 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
-import base64
-import ldap
-import subprocess
 import sys
-import univention.uldap
 from optparse import OptionParser
-import univention.config_registry
+from univention.config_registry import ConfigRegistry
 
 import sqlite3
 
-import samba
 import ldb
-from samba import dsdb
 from samba.samdb import SamDB
 from samba.param import LoadParm
 from samba.auth import system_session
@@ -55,32 +49,19 @@ class GuidNotFound(BaseException): pass
 
 class S4Resync:
 	def __init__(self):
-		self.configRegistry = univention.config_registry.ConfigRegistry()
+		self.configRegistry = ConfigRegistry()
 		self.configRegistry.load()
-
-	def _get_guid(self):
-		self.guid = None
 
 		lp = LoadParm()
 		creds = Credentials()
 		creds.guess(lp) 
-		samdb = SamDB(url='/var/lib/samba/private/sam.ldb', session_info=system_session(), credentials=creds, lp=lp)
+		self.samdb = SamDB(url='/var/lib/samba/private/sam.ldb', session_info=system_session(), credentials=creds, lp=lp)
+		
 
-		domain_dn = samdb.domain_dn()
-		res = samdb.search(self.s4_dn, scope=ldb.SCOPE_BASE, attrs=["objectGuid", "uSNChanged"])
-
-		for msg in res:
-			guid_blob = msg.get("objectGuid", idx=0)
-			self.guid = ndr_unpack(misc.GUID, guid_blob)
-			self.usn = msg.get("uSNChanged", idx=0)
-
-		if not self.guid:
-			raise GuidNotFound
-				
-	def _remove_cache_entries(self):
+	def _remove_cache_entries(self, guid):
 		cache_db = sqlite3.connect('/etc/univention/connector/s4cache.sqlite')
 		c = cache_db.cursor()
-		c.execute("SELECT id FROM GUIDS WHERE guid='%s'" % self.guid)
+		c.execute("SELECT id FROM GUIDS WHERE guid='%s'" % guid)
 		guid_ids = c.fetchone()
 		if guid_ids:
 			guid_id = guid_ids[0]
@@ -89,50 +70,115 @@ class S4Resync:
 			cache_db.commit()
 		cache_db.close()
 
-	def _add_object_to_rejected(self):
+	def _add_object_to_rejected(self, s4_dn, usn):
 		db = sqlite3.connect('/etc/univention/connector/s4internal.sqlite')
 		c = db.cursor()
-		c.execute("INSERT OR REPLACE INTO 'S4 rejected' (key,value) VALUES ('%(key)s', '%(value)s');" % {'key': self.usn, 'value': self.s4_dn})
+		c.execute("INSERT OR REPLACE INTO 'S4 rejected' (key,value) VALUES ('%(key)s', '%(value)s');" % {'key': usn, 'value': s4_dn})
 		db.commit()
 		db.close()
 		
 
-	def resync(self, s4_dn):
-		self.s4_dn = s4_dn
-		self._get_guid()
-		self._remove_cache_entries()
-		self._add_object_to_rejected()
+	def resync(self, s4_dns=None, ldapfilter=None):
+		if s4_dns and not type(s4_dns) in (type(()), type([])):
+			raise ValueError("'s4_dns' is of type %s, must be list or tuple" % type(s4_dns))
+
+		treated_dns = []
+		for s4_dn, guid, usn in self.search_samdb(s4_dns, ldapfilter):
+			self._remove_cache_entries(guid)
+			self._add_object_to_rejected(s4_dn, usn)
+			treated_dns.append(s4_dn)
+
+		return treated_dns
+
+	def search_samdb(self, s4_dns=None, ldapfilter=None):
+
+		search_result = []
+		if s4_dns:
+			if not type(s4_dns) in (type(()), type([])):
+				raise ValueError("'s4_dns' is of type %s, must be list or tuple" % type(s4_dns))
+			if not ldapfilter:
+				ldapfilter='(objectClass=*)'
+
+			error_dns = []
+			missing_dns = []
+			for targetdn in s4_dns:
+				guid = None
+				try:
+					res = self.samdb.search(targetdn, scope=ldb.SCOPE_BASE, expression=ldapfilter, attrs=["objectGuid", "uSNChanged"])
+
+					for msg in res:
+						guid_blob = msg.get("objectGuid", idx=0)
+						guid = ndr_unpack(misc.GUID, guid_blob)
+						usn = msg.get("uSNChanged", idx=0)
+						search_result.append((targetdn, guid, usn))
+					if not guid:
+						missing_dns.append(targetdn)
+				except ldb.LdbError as ex:
+					error_dns.append((targetdn, ex.args[1]))
+			if error_dns:
+				raise ldb.LdbError(1, error_dns, [r[0] for r in search_result])
+			if missing_dns:
+				raise GuidNotFound(1, missing_dns, [r[0] for r in search_result])
+		else:
+			guid = None
+			res = self.samdb.search(expression=ldapfilter, attrs=["objectGuid", "uSNChanged"])
+
+			for msg in res:
+				guid_blob = msg.get("objectGuid", idx=0)
+				guid = ndr_unpack(misc.GUID, guid_blob)
+				usn = msg.get("uSNChanged", idx=0)
+				search_result.append((str(msg.dn), guid, usn))
+
+			if not guid:
+				raise GuidNotFound(2, "No match")
+
+		return search_result
+
 
 
 if __name__ == '__main__':
 
-	parser = OptionParser(usage='resync_object_from_s4.py dn')
+	parser = OptionParser(usage='resync_object_from_s4.py [--filter <LDAP filter>] [dn]')
+	parser.add_option("--filter", dest="ldapfilter", help="LDAP Filter")
 	(options, args) = parser.parse_args()
 	
-	if len(args) != 1:
+	if len(args) != 1 and not options.ldapfilter:
 		parser.print_help()
 		sys.exit(2)
-		
 
-	s4_dn = args[0]
+	s4_dns = []
+	if len(args) == 1:
+		s4_dns.append(args[0])
 
+	treated_dns = []
 	try:
 		resync = S4Resync()
-		resync.resync(s4_dn)
-	except ldb.LdbError:
-		print 'ERROR: The S4 object %s was not found.' % s4_dn
+		treated_dns = resync.resync(s4_dns, options.ldapfilter)
+	except ldb.LdbError as ex:
+		print 'ERROR: The S4 object was not found: %s' % (ex.args[1],)
+		if len(ex.args) == 3:
+			treated_dns = ex.args[2]
 		sys.exit(1)
-	except GuidNotFound:
-		print 'ERROR: The S4 search failed (objectGUID was not found).'
+	except GuidNotFound as ex:
+		print 'ERROR: The S4 search for objectGUID failed: %s' % (ex.args[1],)
+		if len(ex.args) == 3:
+			treated_dns = ex.args[2]
 		sys.exit(1)
+	finally:
+		for dn in treated_dns:
+			print 'resync triggered for %s' % dn
+
+
+	if treated_dns:
+		estimated_delay = 60
+		try:
+			estimated_delay = int(resync.configRegistry.get('connector/s4/retryrejected', 10)) * int(resync.configRegistry.get('connector/s4/poll/sleep', 5))
+		except ValueError:
+			pass
+
+		print 'Estimated sync in %s seconds.' % (estimated_delay,)
+	else:
+		print 'No matching objects.'
 	
-	estimated_delay = 60
-	try:
-		estimated_delay = int(resync.configRegistry.get('connector/s4/retryrejected', 10)) * int(resync.configRegistry.get('connector/s4/poll/sleep', 5))
-	except ValueError:
-		pass
-
-	print 'The resync of %s has been initialized (estimated sync in %s seconds).' % (s4_dn, estimated_delay)
-
 	sys.exit(0)
 
