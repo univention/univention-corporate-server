3b15738b4746c26c08bc63ba4ce6bc94d230f399
Ammar Najjar <najjar@univention.de>
2015-02-04 09:16:10 +0000

    Bug #35909: improve the code, use essential/simplesquid
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-0@57747 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ test/ucs-test/tests/43_proxy/essential/simplesquid.py
@@ -0,0 +1,75 @@
+"""
+.. module:: simplesquid
+	:platform: Unix
+
+.. moduleauthor:: Ammar Najjar <najjar@univention.de>
+"""
+import os
+import subprocess
+import time
+import univention.testing.ucr as ucr_test
+
+def returncode(cmd):
+	pop = subprocess.Popen(cmd)
+	pop.communicate()
+	return pop.returncode
+
+def get_lines_containing(filename, string):
+	with open(filename) as input_file:
+		return [line for line in input_file if string in line]
+
+class SimpleSquid(object):
+	"""
+	:param path: path for the executable squid
+	:type path: str
+	"""
+	def __init__(self, path=None):
+		self.path = path if path else "/etc/init.d/squid3"
+		self.basename  = os.path.basename(self.path)
+		self.conf = "/etc/%s/squid.conf" % self.basename
+
+	def restart(self):
+		"""Trying to restart"""
+		return returncode([self.path, "restart"])
+
+	def is_not_running(self):
+		"""Check the current running status\n
+		:return boolean : True if not running, Flase if running
+		"""
+		return returncode([self.basename, "-k", "check"])
+
+	def is_running(self, tolerance=5):
+		"""Check if it is running within the given tolerance of time\n
+		Use when waiting for squid to be running.\n
+		:param tolerance: time duration
+		:type tolerance: int for seconds
+		:return boolean:True if running, False if not running
+		"""
+		result = False
+		# Give 5 seconds max for squid to be ready
+		for interval in xrange(tolerance):
+			if self.is_not_running():
+				result = False
+				time.sleep(1)
+			else:
+				result = True
+				break
+		return result
+
+	def redirector_is(self, expected_redirector):
+		"""Check if the redirector is the same as the passed parameter\n
+		:param expected_redirector: in the config file
+		:type expected_redirector: string
+		:return boolean:True if match, False if not match
+		"""
+		result = False
+		with ucr_test.UCSTestConfigRegistry() as ucr:
+			current_redirector = ucr.get('squid/redirect')
+			config_lines = get_lines_containing(self.conf, 'url_rewrite_program')
+			if config_lines:
+				# in config file the first line setting the redirector is activated
+				config_line = config_lines[0]
+				result = ('url_rewrite_program %s\n' % (expected_redirector,) == config_line)
+			else:
+				utils.fail("Squid config redirector line does not exist")
+		return result
