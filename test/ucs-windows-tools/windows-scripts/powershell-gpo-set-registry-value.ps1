a8e89e98252c7fce13a9d5d4f954edc006c32f3e
Dmitry Galkin <galkin@univention.de>
2015-02-17 11:33:06 +0000

    * windows-scripts/powershell-gpo-set-registry-value.ps1: script to edit GPO;     windows-scripts/powershell-get-gpo-report.ps1: script to get report on GPO.     (Bug #37568).
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-1@58142 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ test/ucs-windows-tools/windows-scripts/powershell-gpo-set-registry-value.ps1
@@ -0,0 +1,57 @@
+Param(
+# arguments:
+        [string]$domain,
+        [string]$gpo_name,  # GpoName (aka DisplayName)
+        [string]$reg_key,  # for ex: HKLM\Software\Policies\Microsoft\Windows NT\DNSClien
+        [string]$value_name,  # a value name or array of value names
+        [string]$value,  # a value(-s)
+        [string]$type,  # Unknown|String|ExpandString|Binary|DWord|MultiString|QWord
+        [string]$server  # Domain controller
+)
+
+##################
+# error handling #
+##################
+
+$ErrorActionPreference = "Stop"
+
+# for external commands
+trap {
+	Write-Error $_
+	exit 1
+}
+
+#############
+# functions #
+#############
+
+function error ($msg) {
+	Write-Host "[$Now] $msg" -ForegroundColor Red
+	exit(1)
+}
+
+########
+# MAIN #
+########
+
+$global:Now = Set-PSBreakpoint -Variable Now -Mode Read -Action { $global:Now = Get-Date }
+
+if (!(gwmi win32_computersystem).partofdomain -eq $true) {
+	error("I am not joined")
+}
+
+    try {
+            Set-GPRegistryValue `
+                -Name "$gpo_name" `
+                -Key "$reg_key" `
+                -ValueName "$value_name" `
+                -Value "$value" `
+                -Type "$type" `
+                -Domain "$domain" `
+                -Server "$server" `
+}
+    Catch {
+            error("$_")
+    }
+
+exit(0)
