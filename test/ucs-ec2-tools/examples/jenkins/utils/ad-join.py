87e29605c5121b0de0558f3ac8fb403fbfc613ab
Philipp Hahn <hahn@univention.de>
2015-02-19 13:31:35 +0000

    Bug #37807 Jenkins: Copyright update
    2015
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-5@58266 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-ec2-tools/examples/jenkins/utils/ad-join.py
+++ test/ucs-ec2-tools/examples/jenkins/utils/ad-join.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 #
-# Copyright 2013 Univention GmbH
+# Copyright 2013-2015 Univention GmbH
 #
 # http://www.univention.de/
 #
--- test/ucs-ec2-tools/examples/jenkins/utils/utils-local.sh
+++ test/ucs-ec2-tools/examples/jenkins/utils/utils-local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2014 Univention GmbH
+# Copyright 2014-2015 Univention GmbH
 #
 # http://www.univention.de/
 #
