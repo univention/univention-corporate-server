5b535515380fb420cdbb43f2f712128cb3132d56
Dirk Wiesenthal <wiesenthal@univention.de>
2015-02-26 22:39:34 +0000

    * Bug #35456: Support WebInterfacePort
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-1@58499 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console-module-appcenter/udm/handlers/appcenter/app.py
+++ management/univention-management-console-module-appcenter/udm/handlers/appcenter/app.py
@@ -3,7 +3,7 @@
 # Univention Directory Manager Modules
 #  direcory manager module for App Metadata
 #
-# Copyright 2013-2014 Univention GmbH
+# Copyright 2013-2015 Univention GmbH
 #
 # http://www.univention.de/
 #
--- management/univention-management-console-module-appcenter/umc/js/appcenter/SearchForm.js
+++ management/univention-management-console-module-appcenter/umc/js/appcenter/SearchForm.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2014 Univention GmbH
+ * Copyright 2011-2015 Univention GmbH
  *
  * http://www.univention.de/
  *
