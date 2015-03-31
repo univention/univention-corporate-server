13d31a9fcfb519d9173efc4f82647597a94eec6d
Philipp Hahn <hahn@univention.de>
2015-03-31 07:27:22 +0000

    Bug #31561 EC2: Speed up retry of locked updater
    Retry the release update more often if it only fails because cron is
    running "univention-upgrade --check" in the back-ground.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-1@59506 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-ec2-tools/examples/jenkins/utils/utils.sh
+++ test/ucs-ec2-tools/examples/jenkins/utils/utils.sh
@@ -80,15 +80,24 @@ upgrade_to_testing ()
 	upgrade_to_latest
 }
 
-upgrade_to_latest ()
-{
-	univention-upgrade --noninteractive --ignoreterm --ignoressh "$@" && return 0
-	(echo "ERROR: univention-upgrade failed in attempt 1 with exitcode $?"; ps faxwww ; ucr search update/check)
-	# Workaround for Bug #31561
-	sleep 300
-	univention-upgrade --noninteractive --ignoreterm --ignoressh "$@" && return 0
-	(echo "ERROR: univention-upgrade failed in attempt 2 with exitcode $?"; ps faxwww ; ucr search update/check)
-	return 1
+upgrade_to_latest () {
+	declare -i remain=300 rv delay=30
+	while true
+	do
+		univention-upgrade --noninteractive --ignoreterm --ignoressh "$@"
+		rv="$?"
+		case "$rv" in
+		0) return 0 ;;  # success
+		5) delay=30 ;;  # /var/lock/univention-updater exists
+		*) delay=$max ;;  # all other errors
+		esac
+		echo "ERROR: univention-upgrade failed exitcode $rv"
+		ps faxwww
+		ucr search --brief --non-empty update/check
+		[ $remain -gt 0 ] || return "$rv"
+		remain+=-$delay
+		sleep "$delay"  # Workaround for Bug #31561
+	done
 }
 
 run_setup_join ()
@@ -134,7 +143,6 @@ install_apps ()
 
 uninstall_apps ()
 {
-
 	local app
 	for app in "$@"; do echo "$app" >>/var/cache/appcenter-uninstalled.txt; done
 	for app in "$@"; do /root/uninstall-app.py -a "$app"; done
