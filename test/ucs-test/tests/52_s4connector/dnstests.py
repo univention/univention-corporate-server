7f431f4baf44e2efc5dd2dacb16831fab5e681aa
Dmitry Galkin <galkin@univention.de>
2015-05-04 12:53:24 +0000

    * 52_s4connector/dnstests.py: perform up to 10 dig attempts (Bug #38288).
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-5@60356 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/52_s4connector/dnstests.py
+++ test/ucs-test/tests/52_s4connector/dnstests.py
@@ -8,7 +8,10 @@ ucr = univention.config_registry.ConfigRegistry()
 ucr.load()
 import os
 import sys
+from time import sleep
 
+
+MATCH_ATTEMPTS = 10  # number of 'dig' attempts to be done, see Bug #38288
 
 
 def check_ldap_object(item, item_name, item_attribute = None, name_string = None):
@@ -30,6 +33,7 @@ def test_dns_forward_zone(zone_name, test_object):
 	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+NS\s+\"*{1}\"*'.format(zone_name, test_object))
 	match(re_test_object, zone_name, 'NS')
 
+
 def test_dns_txt(zone_name, test_object):
 	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+TXT\s+\"*{1}\"*'.format(zone_name, test_object))
 	match(re_test_object, zone_name, 'TXT')
@@ -39,19 +43,23 @@ def test_dns_ttl(zone_name, test_object):
 	re_test_object=re.compile(r"{0}\.*\s+{1}\s+IN\s+SOA".format(zone_name, test_object))
 	match(re_test_object, zone_name, 'SOA')
 
+
 def test_dns_reverse_zone(zone_name, test_object):
 	temp = zone_name.split('.')
 	zone_namereverse = temp[2] + '.' + temp[1] + '.' + temp[0]
 	re_test_object=re.compile(r"{0}.in-addr.arpa.\s+\d+\s+IN\s+NS\s+{1}".format(zone_namereverse, test_object))
 	match(re_test_object, zone_name, 'NS', '-x')
 
+
 def test_dns_serial(zone_name, test_object):
 	re_test_object=re.compile(r"{0}\.*\s+\d+\s+IN\s+SOA\s+.+\s+.+\s+{1}\s+".format(zone_name, test_object))
 	match(re_test_object, zone_name, 'SOA')
 
+
 def test_dns_a_record(zone_name, test_object):
 	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+A\s+\"*{1}\"*'.format(zone_name, test_object))
 	match(re_test_object, zone_name, 'A')
+
 
 def test_dns_aaaa_record(zone_name, test_object):
 	#leading zeros will not be displayed in dig output so test_object has to be
@@ -67,57 +75,67 @@ def test_dns_aaaa_record(zone_name, test_object):
 	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+AAAA\s+\"*{1}\"*'.format(zone_name, test_object))
 	match(re_test_object, zone_name ,'AAAA')
 
+
 def test_dns_alias(zone_name, test_object):
 	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+CNAME\s+\"*{1}\"*'.format(zone_name, test_object))
 	match(re_test_object, zone_name, 'CNAME')
 
+
 def test_dns_service_record(zone_name, test_object):
 	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+SRV\s+\"*{1}\"*'.format(zone_name, test_object))
 	match(re_test_object, zone_name, 'SRV')
+
 
 def test_dns_pointer_record(reverse_zone, ip, test_object):
 	reverse_address = str(ip) + '.' + reverse_zone
 	re_test_object=re.compile(r'{0}\.*\s+\d+\s+IN\s+PTR\s+\"*{1}\"*'.format(reverse_address, test_object))
 	match(re_test_object, reverse_address, 'PTR')
 
+
 def match(re_test_object, zone_name, typ, param=None):
 
 	if not param:
-		dig_subprocess = subprocess.Popen(["dig", zone_name, typ, '+noall', '+answer'], shell = False, stdout = subprocess.PIPE).communicate()
+		dig_cmd = ("dig", zone_name, typ, '+noall', '+answer')
 	else:
-		dig_subprocess = subprocess.Popen(["dig", param, zone_name, typ, '+noall', '+answer'], shell = False, stdout = subprocess.PIPE).communicate()
-	dig_answer = dig_subprocess[0].splitlines()
+		dig_cmd = ("dig", param, zone_name, typ, '+noall', '+answer')
 
-	found_test_object_match = False
-	print ("Dig Output :")
-	for line in dig_answer:
-		print line
-		if re.match(re_test_object, line):
-			found_test_object_match = True
-	if found_test_object_match:
-		print
-		print("  DNS synced ")
-	else:
-		print
-		print(" DNS not synced ")
-		sys.exit(1)
+	for attempt in range(MATCH_ATTEMPTS):
+		dig_subprocess = subprocess.Popen(dig_cmd, shell = False, stdout = subprocess.PIPE).communicate()
+		dig_answer = dig_subprocess[0].splitlines()
+
+		print ("\nDig Output :")
+		for line in dig_answer:
+			print line
+			if re.match(re_test_object, line):
+				print("\nOK: DNS synced\n")
+				return
+
+		print("\n  DNS not synced yet, making another dig attempt in 1 sec.")
+		sleep(1)
+
+	print("\nFAIL: DNS still not synced, made %s dig attempts " % MATCH_ATTEMPTS)
+	sys.exit(1)
 
 
 def get_hostname():
 	host = ucr.get("ldap/master")
 	return host
 
+
 def random_name():
 	random_name = '{0} tcp {1}'.format(uts.random_name(), uts.random_name())
 	return random_name
+
 
 def random_zone():
 	random_zone = '{0}.{1}'.format(uts.random_string(), uts.random_string())
 	return random_zone
 
+
 def location():
 	location = '0 1 2 {0}.{1}'.format(uts.random_name(), uts.random_name())
 	return location
+
 
 def random_reverse_zone():
 	while True:
@@ -131,6 +149,7 @@ def random_reverse_zone():
 			pass
 	return random_reverse_zone
 
+
 def make_random_ip():
 	while True:
 		ip_parts = [str(random.randrange(1, 254)) for i in range(4)]
@@ -142,14 +161,15 @@ def make_random_ip():
 			break
 	return randomIP
 
+
 def make_random_ipv6():
 	ipv6 = random_hex()
 	for i in range(7):
 		ipv6 +=':'+(random_hex())
 	return ipv6
 
+
 def random_hex():
 	result = []
 	result = ''.join([random.choice('0123456789abcdef') for i in range(4)])
 	return result
-
