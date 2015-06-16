d76eecbe874f23bfe28888261134422a46923582
Florian Best <best@univention.de>
2015-06-16 15:49:11 +0000

    Bug #38701: Initial release.
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-2@61287 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-appreport/pyshared/univention/appreport/__init__.py
+++ management/univention-appreport/pyshared/univention/appreport/__init__.py
@@ -1,18 +1,47 @@
 # -*- coding: utf-8 -*-
+#
+# Univention Application Reporting
+#
+# Copyright (C) 2015 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of the software contained in this package
+# as well as the source package itself are made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this package provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use the software under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
+__version__ = '1.0'
 
 import urllib2
 from socket import error as SocketError
 
-from univention.appreport.api import ucr
-from univention.appreport import provider
-# TODO: import univention.debug
-
-PROVIDER_IDENTIFIER = 'appreport/recipient'
+from univention.appreport.api import UniventionServer
+import univention.debug as ud
 
 
 class ConnectionManager(object):
 
-	user_agent = 'univention.appreport'
+	user_agent = 'univention.appreport/%s' % (__version__,)
 	timeout = 60
 
 	def __init__(self):
@@ -30,31 +59,42 @@ class ConnectionManager(object):
 
 	def request(self, request):
 		try:
-			self.opener.open(request.request, timeout=self.timeout)
-		except SocketError:
-			raise  # TODO
-		except (urllib2.HTTPError, urllib2.URLError):
-			raise  # TODO
+			response = self._request(request)
+		except (SocketError,) as exc:
+			ud.debug(ud.MAIN, ud.ERROR, 'Connection failed (%r): %s' % (request, self.http_error_string(exc)))
+		except (urllib2.HTTPError, urllib2.URLError) as exc:
+			ud.debug(ud.MAIN, ud.ERROR, 'Request failed (%r): %s' % (request, self.http_error_string(exc)))
+		else:
+			ud.debug(ud.MAIN, ud.INFO, 'Request successful: %s' % (response.getcode(),))
+
+	def http_error_string(self, exc):
+		return '%s: %s: errno=%r, reason=%r, strerror=%r' % (type(exc), exc, getattr(exc, 'errno', ''), getattr(exc, 'reason', ''), getattr(exc, 'strerror', ''))
+
+	def _request(self, request):
+		return self.opener.open(request.request, timeout=self.timeout)
 
 
 class AppReporting(object):
 
 	def __init__(self):
 		self.connection = ConnectionManager()
-		provider.load()
 
 	def main(self):
-		for provider_ in self.providers():
-			try:
-				for request in provider_.requests():
-					self.connection.request(request)
-			except:
-				raise  # TODO
+		self.log_init()
+		self.make_requests()
 
-	def providers(self):
-		yield provider.get('univention')
-		for name in ucr.get(PROVIDER_IDENTIFIER, '').split():
-			yield provider.get(name)
+	def log_init(self):
+		ud.init('stderr', ud.FLUSH, ud.NO_FUNCTION)
+
+	def make_requests(self):
+		for service in self.services():
+			ud.debug(ud.MAIN, ud.PROCESS, 'Processing requests for service %s' % (type(service).__name__,))
+			for request in service.requests():
+				ud.debug(ud.MAIN, ud.PROCESS, 'Requesting %s' % (request,))
+				self.connection.request(request)
+
+	def services(self):
+		yield UniventionServer()
 
 
 if __name__ == '__main__':
--- management/univention-appreport/pyshared/univention/appreport/api.py
+++ management/univention-appreport/pyshared/univention/appreport/api.py
@@ -1,9 +1,40 @@
 # -*- coding: utf-8 -*-
+#
+# Univention Application Reporting
+#
+# Copyright (C) 2015 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of the software contained in this package
+# as well as the source package itself are made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this package provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use the software under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
 
 import urllib2
 import ldap
 from json import dumps as json_encode
 
+import univention.debug as ud
 import univention.admin.uldap
 import univention.admin.modules
 import univention.config_registry
@@ -15,8 +46,9 @@ ldap_connection, ldap_position = univention.admin.uldap.getMachineConnection()
 ucr = univention.config_registry.ConfigRegistry()
 ucr.load()
 
-UUID_SYSTEM = ucr.get('uuid/system')
-UUID_LICENSE = ucr.get('uuid/license')
+UUID_SYSTEM = ucr.get('uuid/system', '')
+UUID_LICENSE = ucr.get('uuid/license', '')
+LDAP_BASE = ucr.get('ldap/base', '')
 
 from univention.management.console.modules.appcenter import app_center
 APPS = dict((app.id.lower(), app) for app in app_center.Application.all_installed(PackageManager(), only_local=True, localize=False))
@@ -34,7 +66,7 @@ class Request(object):
 		return '<Request(%s, %r)>' % (self.url, self.headers)
 
 
-class ServerInterface(object):
+class ServiceInterface(object):
 
 	apps = ()
 
@@ -42,15 +74,19 @@ class ServerInterface(object):
 		raise NotImplementedError('url')
 
 	def body(self, app):
-		return app.body()
+		body = {
+			'uuid/system': UUID_SYSTEM,
+			'uuid/license': UUID_LICENSE,
+			'ldap/base': LDAP_BASE,
+		}
+		body.update(app.body())
+		return body
 
 	def encode_body(self, body):
 		return json_encode(body)
 
 	def headers(self, app):
 		return {
-			'X-System-Uuid': UUID_SYSTEM,
-			'X-License-Uuid': UUID_LICENSE,
 			'Content-Type': 'application/json'
 		}
 
@@ -73,38 +109,37 @@ class ServerInterface(object):
 		return Request(self.url(app), self.encode_body(self.body(app)), self.headers(app))
 
 
-class UniventionServer(ServerInterface):
+class UniventionServer(ServiceInterface):
 
 	apps = ('*',)
 
 	def url(self, app):
-		return 'http://localhost:8090/application-report/%s' % urllib2.quote(app.id)  # FIXME: remove
-		return 'https://license.univention.de/application-report/%s' % urllib2.quote(app.id)
-
-
-class CloudServiceProvider(ServerInterface):
-	pass
+		return 'https://license.univention.de/appreport/%s' % urllib2.quote(app.id)
 
 
 class Application(object):
 
-	id = None
-	user_filter = '(!(disabled=all))'
-
 	def __init__(self, app):
 		self.app = app
 		self.id = app.id
+		self.object_type = ucr.get('appreport/%s/object_type' % (self.id,), 'users/user')
+		self.object_filter = ucr.get('appreport/%s/object_filter' % (self.id,), '')
+		self.ldap_filter = ucr.get('appreport/%s/ldap_filter' % (self.id,), '(&((!(disabled=all)%s))')
+		if '%s' in self.ldap_filter:
+			self.ldap_filter = self.ldap_filter % self.object_filter
 
-	def user_count(self):
-		users_user = univention.admin.modules.get('users/user')
+	def object_count(self):
+		object_ = univention.admin.modules.get(self.object_type)
 		try:
-			return len(users_user.lookup(None, ldap_connection, self.user_filter))
-		except AttributeError: # users_user is None
+			return len(object_.lookup(None, ldap_connection, self.ldap_filter))
+		except AttributeError as exc: # object_ is None
+			ud.debug(ud.MAIN, ud.ERROR, 'Unknown object type? %r=%r: %s' % (self.object_type, object_, exc))
 			raise
-		except (ldap.LDAPError, udm_error):
+		except (ldap.LDAPError, udm_error) as exc:
+			ud.debug(ud.MAIN, ud.ERROR, 'LDAP lookup failure: %s' % (exc,))
 			raise
 
 	def body(self):
 		return {
-			'user_count': self.user_count()
+			'count': self.object_count()
 		}
