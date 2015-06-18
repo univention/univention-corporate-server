ae76f51f445c7502ff98c9de0f5c996885efbae2
Ammar Najjar <najjar@univention.de>
2015-06-18 07:14:16 +0000

    Bug #38600: Include dovecot case when creating shared mail folder.
     + small improvements.
     + skeleton for acl evaluation.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-2@61356 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/40_mail/essential/mailclient.py
+++ test/ucs-test/tests/40_mail/essential/mailclient.py
@@ -3,7 +3,9 @@
 .. moduleauthor:: Ammar Najjar <najjar@univention.de>
 """
 from itertools import izip
+import email
 import imaplib
+import time
 import univention.testing.strings as uts
 import univention.testing.ucr as ucr_test
 
@@ -44,7 +46,7 @@ class MailClient(imaplib.IMAP4, imaplib.IMAP4_SSL):
 		:returns: list of strings, list of existing mailboxes
 		"""
 		mBoxes = self.list()[1]
-		return [x.split()[2] for x in mBoxes]
+		return [x.split()[-1] for x in mBoxes if 'Noselect' not in x.split()[0]]
 
 	def get_acl(self, mailbox):
 		"""get the exact acls from getacl
@@ -84,6 +86,7 @@ class MailClient(imaplib.IMAP4, imaplib.IMAP4_SSL):
 		}
 		for mailbox in expected_acls:
 			current = self.get_acl(mailbox)
+			print 'Current = ', current
 			for who in expected_acls.get(mailbox):
 				permissions = expected_acls.get(mailbox).get(who)
 				set1 = set(''.join([permissions_map[x] for x in permissions]))
@@ -93,12 +96,73 @@ class MailClient(imaplib.IMAP4, imaplib.IMAP4_SSL):
 					raise WrongAcls('\nExpected = %s\nCurrent = %s\n' % (
 						expected_acls.get(mailbox).get(who), current.get(mailbox).get(who)))
 
+	def check_read(self, expected_result):
+		"""Checks the read access of a certain mailbox
 
-def create_shared_mailfolder(udm, mailHomeServer, user_permission=None, group_permission=None):
+		:expected_result: dict{mailbox : bool}
+		"""
+		for mailbox in expected_result:
+			self.select(mailbox)
+			print self.status(mailbox, '(MESSAGES RECENT UIDNEXT UIDVALIDITY UNSEEN)')
+			typ, data = self.search(None, 'ALL')
+			print 'Response code:', typ
+			for num in data[0].split():
+				typ, data = self.fetch(num, '(RFC822)')
+				print 'Message %s\n%s\n' % (num, data[0][1])
+			self.close()
+
+	def check_post(self, expected_result):
+		"""Checks the post access of a certain mailbox
+
+		:expected_result: dict{mailbox : bool}
+		"""
+		for mailbox in expected_result:
+			self.select(mailbox)
+			print self.status(mailbox, '(MESSAGES RECENT UIDNEXT UIDVALIDITY UNSEEN)')
+			typ, data = self.search(None, 'ALL')
+			print 'Response code:', typ
+			self.close()
+
+	def check_append(self, expected_result):
+		"""Checks the append access of a certain mailbox
+
+		:expected_result: dict{mailbox : bool}
+		"""
+		for mailbox in expected_result:
+			self.select(mailbox)
+			print self.get_acl(mailbox)
+			typ, data =  self.append(
+				mailbox, '',
+				imaplib.Time2Internaldate(time.time()),
+				str(email.message_from_string('TEST %s' % mailbox))
+			)
+			print 'Append = ', typ
+			self.close()
+
+	def check_write(self, expected_result):
+		"""Checks the write access of a certain mailbox
+
+		:expected_result: dict{mailbox : bool}
+		"""
+		pass
+
+	def check_all(self, expected_result):
+		"""Checks all access of a certain mailbox
+
+		:expected_result: dict{mailbox : bool}
+		"""
+		pass
+
+def create_shared_mailfolder(udm, mailHomeServer, mail=None, user_permission=None, group_permission=None):
 	with ucr_test.UCSTestConfigRegistry() as ucr:
 		domain = ucr.get('domainname')
 		basedn = ucr.get('ldap/base')
+		dovecat = ucr.is_true('mail/dovecot')
 	name = uts.random_name()
+	if mail:
+		shared_mail = '%s@%s' % (name, domain)
+	else:
+		shared_mail = ''
 	udm.create_object(
 		'mail/folder',
 		position = 'cn=folder,cn=mail,%s' % basedn,
@@ -106,12 +170,21 @@ def create_shared_mailfolder(udm, mailHomeServer, user_permission=None, group_pe
 			'name'                 : name,
 			'mailHomeServer'       : mailHomeServer,
 			'mailDomain'           : domain,
+			'mailPrimaryAddress'   : shared_mail
 		},
 		append = {
 			'sharedFolderUserACL'  : user_permission or [],
 			'sharedFolderGroupACL' : group_permission or [],
 		}
 	)
-	return 'shared/%s' % name
+	if dovecat:
+		if mail:
+			ret_value = 'shared/%s' % shared_mail
+			# ret_value = '"/"'
+		else:
+			ret_value = '%s@%s/INBOX' % (name, domain)
+	else:
+		ret_value = 'shared/%s' % name
+	return ret_value
 
 # vim: set ft=python ts=4 sw=4 noet ai :
