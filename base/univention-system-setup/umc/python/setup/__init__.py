1ec990411490c589fea74aec3d694ab97864bc3f
Alexander Kramer <kramer@univention.de>
2015-06-23 10:15:44 +0000

    * Bug #37816: Adapted the messages as requested
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-2@61411 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-system-setup/umc/python/setup/__init__.py
+++ base/univention-system-setup/umc/python/setup/__init__.py
@@ -388,9 +388,9 @@ class Instance(Base, ProgressMixin):
 
 		# host and domain name
 		packages = set(values.get('components', []))
-		_check('hostname', util.is_hostname, _('The hostname is not a valid fully qualified domain name (e.g. host.example.com).'))
+		_check('hostname', util.is_hostname, _('The hostname or the hostname part of the fully qualified domain name is invalid. Please go back to the host setting and make sure, that the hostname only contains letter (a-zA-Z) and digits (0-9).'))
 		_check('hostname', lambda x: len(x) <= 13, _('A valid NetBIOS name can not be longer than 13 characters. If Samba is installed, the hostname should be shortened.'), critical=('univention-samba' in packages or 'univention-samba4' in packages))
-		_check('domainname', util.is_domainname, _("Please enter a valid fully qualified domain name in lowercase (e.g. host.example.com)."))
+		_check('domainname', util.is_domainname, _("Please enter a valid fully qualified domain name (e.g. host.example.com)."))
 		hostname = allValues.get('hostname', '')
 		domainname = allValues.get('domainname', '')
 		if len('%s%s' % (hostname, domainname)) >= 63:
