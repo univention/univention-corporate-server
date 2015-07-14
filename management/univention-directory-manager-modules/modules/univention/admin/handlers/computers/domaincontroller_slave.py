47ea1845713d88384684c03f278864a0a1be72b5
Florian Best <best@univention.de>
2015-07-14 09:21:50 +0000

    Bug #38364: disallow name change of DCs and memberservers
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-2@62090 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-directory-manager-modules/modules/univention/admin/handlers/computers/domaincontroller_slave.py
+++ management/univention-directory-manager-modules/modules/univention/admin/handlers/computers/domaincontroller_slave.py
@@ -77,7 +77,7 @@ property_descriptions={
 			include_in_default_search=1,
 			options=[],
 			required=1,
-			may_change=1,
+			may_change=False,
 			identifies=1
 		),
 	'description': univention.admin.property(
