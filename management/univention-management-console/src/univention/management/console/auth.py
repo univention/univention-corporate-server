499fd101278b935e589ffdd0684ecf000edd42af
Florian Best <best@univention.de>
2015-07-15 12:09:58 +0000

    Bug #38826: catch all exceptions
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-0@62124 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console/src/univention/management/console/auth.py
+++ management/univention-management-console/src/univention/management/console/auth.py
@@ -31,6 +31,8 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
+import traceback
+
 import ldap
 from ldap.filter import escape_filter_chars
 
@@ -118,8 +120,12 @@ class AuthHandler(signals.Provider):
 				AUTH.info('Canonicalized username; %r' % (username,))
 			lo.lo.unbind()
 		except (ldap.LDAPError, IOError) as exc:
-			AUTH.info('Canonicalization of username failed: %s' % (exc,))
-		return username
+			# /etc/machine.secret missing or LDAP server not reachable
+			AUTH.warn('Canonicalization of username was not possible: %s' % (exc,))
+		except:
+			AUTH.error('Canonicalization of username failed: %s' % (traceback.format_exc(),))
+		finally:  # ignore all exceptions, even  in except blocks
+			return username
 
 	def __authentication_result(self, thread, result):
 		if isinstance(result, BaseException) and not isinstance(result, (AuthenticationFailed, PasswordExpired, PasswordChangeFailed)):
