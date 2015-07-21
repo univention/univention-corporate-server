e0460d13e4e8c51d8e3a1cb72e54d57e1f3a702c
Philipp Hahn <hahn@univention.de>
2015-07-21 06:47:38 +0000

    Bug #38673 test: Return error code
    Collect error codes and return them to help debug Jenkins regressions.
    
    "dash -n utils.sh" clean.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-2@62275 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-ec2-tools/examples/jenkins/utils/utils.sh
+++ test/ucs-ec2-tools/examples/jenkins/utils/utils.sh
@@ -140,31 +140,42 @@ upgrade_to_latest () {
 
 run_setup_join ()
 {
-	local srv
-	/usr/lib/univention-system-setup/scripts/setup-join.sh
+	local srv rv=0
+	/usr/lib/univention-system-setup/scripts/setup-join.sh || rv=$?
 	ucr set apache2/startsite='ucs-overview/' # Bug #31682
 	for srv in univention-management-console-server univention-management-console-web-server apache2
-	do invoke-rc.d "$srv" restart; done
+	do
+		invoke-rc.d "$srv" restart
+	done
 	ucr unset --forced update/available
+	return $rv
 }
 
 run_setup_join_on_non_master ()
 {
 	local admin_password="${1:-univention}"
-	local srv
+	local srv rv=0
 	ucr set nameserver1="$(sed -ne 's|^nameserver=||p' /var/cache/univention-system-setup/profile)"
 	echo -n "$admin_password" >/tmp/univention
-	/usr/lib/univention-system-setup/scripts/setup-join.sh --dcaccount Administrator --password_file /tmp/univention
+	/usr/lib/univention-system-setup/scripts/setup-join.sh --dcaccount Administrator --password_file /tmp/univention || rv=$?
 	ucr set apache2/startsite='ucs-overview/' # Bug #31682
 	for srv in univention-management-console-server univention-management-console-web-server apache2
-	do invoke-rc.d "$srv" restart; done
+	do
+		invoke-rc.d "$srv" restart
+	done
 	ucr unset --forced update/available
+	return $rv
 }
 
 wait_for_reboot ()
 {
-	local i
-	for ((i=0;i<100;i++)); do pidof apache2 && break; sleep 1; done
+	local i=0
+	while [ $i -lt 100 ]
+	do
+		pidof apache2 && break
+		sleep 1
+		i=$((i + 1))
+	done
 }
 
 switch_to_test_app_center ()
@@ -174,51 +185,69 @@ switch_to_test_app_center ()
 
 install_apps ()
 {
-	local app
+	local app rv=0
 	for app in "$@"; do echo "$app" >>/var/cache/appcenter-installed.txt; done
-	for app in "$@"; do univention-add-app -a --latest "$app"; done
+	for app in "$@"
+	do
+		univention-add-app -a --latest "$app" || rv=$?
+	done
+	return $rv
 }
 
 uninstall_apps ()
 {
-	local app
+	local app rv=0
 	for app in "$@"; do echo "$app" >>/var/cache/appcenter-uninstalled.txt; done
-	for app in "$@"; do /root/uninstall-app.py -a "$app"; done
+	for app in "$@"
+	do
+		/root/uninstall-app.py -a "$app" || rv=$?
+	done
+	return $rv
 }
 
 install_apps_master_packages ()
 {
-	local app
-	for app in "$@"; do univention-add-app -m --latest "$app"; done
+	local app rv=0
+	for app in "$@"
+	do
+		univention-add-app -m --latest "$app" || rv=$?
+	done
+	return $rv
+}
+
+install_with_unmaintained () {
+	local rv=0
+	ucr set repository/online/unmaintained=yes
+	univention-install --yes "$@" || rv=$?
+	ucr set repository/online/unmaintained=no
+	return $rv
 }
 
 install_ucs_test ()
 {
-	ucr set repository/online/unmaintained=yes
-	univention-install --yes ucs-test
-	ucr set repository/online/unmaintained=no
+	install_with_unmaintained ucs-test
 }
 
 install_apps_test_packages ()
 {
-	local app
+	local app rv=0
 	ucr set repository/online/unmaintained=yes
-	for app in "$@"; do univention-install --yes "ucs-test-$app" || true; done
+	for app in "$@"
+	do
+		univention-install --yes "ucs-test-$app" || rv=$?
+	done
 	ucr set repository/online/unmaintained=no
+	return $rv
 }
 
 install_ucs_test_appcenter_uninstall ()
 {
-	ucr set repository/online/unmaintained=yes
-	univention-install --yes ucs-test-appcenter-uninstall
-	ucr set repository/online/unmaintained=no
+	install_with_unmaintained ucs-test
 }
 
 install_ucs_windows_tools ()
 {
-	ucr set repository/online/unmaintained=yes
-	univention-install --yes ucs-windows-tools
-	ucr set repository/online/unmaintained=no
+	install_with_unmaintained ucs-windows-tools
 }
 
 run_apptests ()
@@ -254,7 +283,7 @@ run_win_member_gpo_tests ()
 run_windows_native_client_tests ()
 {
 	# tests that require a native windows client in the domain
-        run_tests -r native_win_client "$@"
+	run_tests -r native_win_client "$@"
 }
 
 run_tests ()
@@ -280,54 +309,40 @@ do_reboot () {
 
 install_gpmc_windows ()
 {
-	local HOST="$1"
-	local DOMAIN="$2"
+	local HOST="${1:?Missing host address}"
+	local DOMAIN="${2:?Missing domain name}"
 	local ADMIN_ACCOUNT="${3:-administrator}"
-	
-        if [[ ! -z "$HOST" ]] && [[ ! -z "$DOMAIN" ]]; then
 	python -c "
 import univention.winexe
 win=univention.winexe.WinExe('$DOMAIN', '$ADMIN_ACCOUNT', 'Univention@99', 'Administrator', 'Univention@99', 445, '$HOST')
 win.add_gpo_management_console()
 "
-	else
-		echo "You must specify a host address, domain name, dns server address."
-	fi
 }
 
 join_windows_memberserver ()
 {
-	local HOST="$1"
-	local DOMAIN="$2"
-	local DNS_SERVER="$3"
+	local HOST="${1:?Missing host address}"
+	local DOMAIN="${2:?Missing domain name}"
+	local DNS_SERVER="${3:?Missing DNS server address}"
 	local ADMIN_ACCOUNT="${4:-administrator}"
-	
-        if [[ ! -z "$HOST" ]] && [[ ! -z "$DOMAIN" ]] && [[ ! -z "$DNS_SERVER" ]]; then
 	python -c "
 import univention.winexe
 win=univention.winexe.WinExe('$DOMAIN', '$ADMIN_ACCOUNT', 'Univention@99', 'testadmin', 'Univention@99', 445, '$HOST')
 win.domain_join('$DNS_SERVER')
 "
-	else
-		echo "You must specify a host address, domain name, dns server address."
-	fi
 }
 
 _promote_ad ()
 {
-	local HOST="$1"
-	local DOMAIN="$2"
-	local MODE="$3"
+	local HOST="${1:?Missing host address}"
+	local DOMAIN="${2:?Missing domain name}"
+	local MODE="${3:?Missing mode}"
 	local ADMIN_ACCOUNT="${4:-administrator}"
-	if [[ ! -z "$HOST" ]] && [[ ! -z "$DOMAIN" ]]; then
 	python -c "
 import univention.winexe
 win=univention.winexe.WinExe('$DOMAIN', '$ADMIN_ACCOUNT', 'Univention@99', 'testadmin', 'Univention@99', 445, '$HOST')
 win.promote_ad('$MODE', '$MODE')
 "
-	else
-		echo "You must specify an host address domain name."
-	fi
 }
 
 promote_ad_w2k12r2 ()
@@ -357,7 +372,7 @@ promote_ad_w2k3r2 ()
 
 reboot_windows_host ()
 {
-	local HOST="$1"
+	local HOST="${1:?Missing host address}"
 	local ADMIN_ACCOUNT="${2:-administrator}"
 	python -c "
 import univention.winexe
@@ -368,7 +383,7 @@ win.reboot_remote_win_host()
 
 shutdown_windows_host ()
 {
-	local HOST="$1"
+	local HOST="${1:?Missing host address}"
 	local DOMAIN_MODE="${2:-False}"
 	local ADMIN_ACCOUNT="${3:-administrator}"
 	python -c "
@@ -380,38 +395,30 @@ win.shutdown_remote_win_host($DOMAIN_MODE)
 
 set_windows_gateway ()
 {
-	local HOST="$1"
-	local DOMAIN="$2"
-	local GATEWAY="$3"
+	local HOST="${1:?Missing host address}"
+	local DOMAIN="${2:?Missing domain name}"
+	local GATEWAY="${3:?Missing gateway address}"
 	local ADMIN_ACCOUNT="${4:-administrator}"
-	if [[ ! -z "$HOST" ]] && [[ ! -z "$GATEWAY" ]]; then
 	python -c "
 import univention.winexe
 win=univention.winexe.WinExe('$DOMAIN', '$ADMIN_ACCOUNT', 'Univention@99', 'testadmin', 'Univention@99', 445, '$HOST')
 win.set_gateway('$GATEWAY')
 "
-	else
-		echo "You must specify an host address domain name and a gateway."
-	fi
 }
 
 create_ad_user_and_add_the_user_to_the_group ()
 {
-	local HOST="$1"
-	local DOMAIN="$2"
-	local NEW_ADMIN_USERNAME="$3"
-	local NEW_ADMIN_PASSWORD="$4"
-	local NEW_ADMIN_GROUP="$5"
+	local HOST="${1:?Missing host address}"
+	local DOMAIN="${2:?Missing domain name}"
+	local NEW_ADMIN_USERNAME="${3:?Missing admin user name}"
+	local NEW_ADMIN_PASSWORD="${4:?Missing admin password}"
+	local NEW_ADMIN_GROUP="${5:?Missing group name}"
 	local ADMIN_ACCOUNT="${6:-administrator}"
-	if [ -n "$HOST" -a -n "$NEW_ADMIN_USERNAME" -a -n "$NEW_ADMIN_PASSWORD" -a -n "$NEW_ADMIN_GROUP" ]; then
 	python -c "
 import univention.winexe
 win=univention.winexe.WinExe('$DOMAIN', '$ADMIN_ACCOUNT', 'Univention@99', 'testadmin', 'Univention@99', 445, '$HOST')
 win.create_user_and_add_to_group('$NEW_ADMIN_USERNAME', '$NEW_ADMIN_PASSWORD', '$NEW_ADMIN_GROUP')
 "
-	else
-		echo "Please specify an host address, a domain name, an admin username, an admin password and a group name."
-	fi
 }
 
 set_administrator_dn_for_ucs_test ()
