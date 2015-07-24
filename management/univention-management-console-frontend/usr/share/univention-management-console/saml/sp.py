514a19a90437dc166b9c3678a4692e54b1f79d7b
Florian Best <best@univention.de>
2015-07-24 09:51:37 +0000

    Bug #31943: Implement single sign on (SSO) via SAML2.0 using pysaml2
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-0@62394 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ management/univention-management-console-frontend/usr/share/univention-management-console/saml/sp.py
@@ -0,0 +1,39 @@
+from saml2 import BINDING_HTTP_REDIRECT, BINDING_HTTP_POST
+from saml2.saml import NAME_FORMAT_URI
+
+from univention.config_registry.interfaces import Interfaces
+from univention.config_registry import ConfigRegistry
+ucr = ConfigRegistry()
+ucr.load()
+
+i = Interfaces()
+try:
+	fqdn = '%s.%s' % (ucr['hostname'], ucr['domainname'])
+except KeyError:
+	fqdn = ''
+addresses = [fqdn]
+addresses.extend([y['address'] for x, y in i.all_interfaces])
+
+bases = ['%s://%s/umcp/saml' % (scheme, addr) for addr in addresses for scheme in ('https', 'http')]
+CONFIG = {
+	"entityid": "https://%s/umcp/saml/metadata" % (fqdn,),
+	"name_form": NAME_FORMAT_URI,
+	"description": "Univention Management Console SAML2.0 Service Provider",
+	"service": {
+		"sp": {
+			"authn_requests_signed": True,
+			"logout_requests_signed": True,
+			"endpoints": {
+				"assertion_consumer_service": [('%s/acs/' % (url,), binding) for url in bases for binding in (BINDING_HTTP_POST, BINDING_HTTP_REDIRECT)],
+				"single_logout_service": [('%s/slo/' % (url,), binding) for url in bases for binding in (BINDING_HTTP_POST, BINDING_HTTP_REDIRECT)],
+			}
+		},
+	},
+	"key_file": "/usr/share/univention-management-console/pki/mykey.pem",
+	"cert_file": "/usr/share/univention-management-console/pki/mycert.pem",
+	"xmlsec_binary": "/usr/bin/xmlsec1",
+	"metadata": {
+		"local": ["/usr/share/univention-management-console/saml/idp.xml"],
+#		"remote": ["https://%s/simplesamlphp/saml2/idp/metadata.php" % (fqdn,)],
+	},
+}
