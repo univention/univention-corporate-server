5bac2d5685e0b3a6e9d732eb637a86c440f48e73
Felix Botner <botner@univention.de>
2015-09-03 09:11:04 +0000

    Bug #38785: Fixed key expansion for des encryption in pyMsChapV2.py
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-0@63427 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-radius/usr/share/pyshared/univention/pyMsChapV2.py
+++ services/univention-radius/usr/share/pyshared/univention/pyMsChapV2.py
@@ -41,18 +41,50 @@ def md4(data):
 	return md.digest()
 
 def DesEncrypt(data, key):
-	def expandDesKey(key):
-		key = list(key)
-		number = 0
-		while key:
-			number *= 256
-			number += ord(key.pop(0))
-		key = []
-		while number:
-			key.append(chr(number % 128*2))
-			number /= 128
-		return ''.join(reversed(key))
-	return pyDes.des(expandDesKey(key), pyDes.ECB).encrypt(data)
+
+
+	def convertKey (key):
+		"""
+		Converts a 7-bytes key to an 8-bytes key based on an algorithm.
+		"""
+		assert len(key) == 7, "NTLM convertKey needs 7-byte key"
+		bytes = [key[0],
+			chr(((ord(key[0]) << 7) & 0xFF) | (ord(key[1]) >> 1)),
+			chr(((ord(key[1]) << 6) & 0xFF) | (ord(key[2]) >> 2)),
+			chr(((ord(key[2]) << 5) & 0xFF) | (ord(key[3]) >> 3)),
+			chr(((ord(key[3]) << 4) & 0xFF) | (ord(key[4]) >> 4)),
+			chr(((ord(key[4]) << 3) & 0xFF) | (ord(key[5]) >> 5)),
+			chr(((ord(key[5]) << 2) & 0xFF) | (ord(key[6]) >> 6)),
+			chr( (ord(key[6]) << 1) & 0xFF),
+		]
+		return "".join([ setOddParity(b) for b in bytes ])
+
+	def setOddParity (byte):
+		"""
+		Turns one-byte into odd parity. Odd parity means that a number in
+		binary has odd number of 1's.
+		"""
+		assert len(byte) == 1
+		parity = 0
+		ordbyte = ord(byte)
+		for dummy in range(8):
+			if (ordbyte & 0x01) != 0:
+				parity += 1
+			ordbyte >>= 1
+		ordbyte = ord(byte)
+		if parity % 2 == 0:
+			if (ordbyte & 0x01) != 0:
+				ordbyte &= 0xFE
+			else:
+				ordbyte |= 0x01
+		return chr(ordbyte)
+
+	return pyDes.des(convertKey(key), pyDes.ECB).encrypt(data)
+
+	# as alternative, this seems to work too, but
+	# the package python-passlib has to be installed
+	#import passlib.utils.des
+	#return passlib.utils.des.des_encrypt_block(key, data)
 
 def GenerateNtResponse(AuthenticatorChallenge, PeerChallenge, UserName, Password):
 	Challenge = ChallengeHash(PeerChallenge, AuthenticatorChallenge, UserName)
@@ -114,6 +146,27 @@ def executeTestVectors():
 	assert HashNtPasswordHash(PasswordHash) == PasswordHashHash
 	AuthenticatorResponse = 'S=407A5589115FD0D6209F510FE9C04566932CDA56'
 	assert GenerateAuthenticatorResponse(Password, NtResponse, PeerChallenge, AuthenticatorChallenge, UserName) == AuthenticatorResponse
+
+	test_ntlm = [
+		# key, data, resp
+		('CAA1239D44DA7EDF926BCE39F5C65D0F', '4c29654e436e7844', '1cffa87d8b48ce73a71e3e6c9a9dd80f112d48dfeea8792c'),
+		('3b1b47e42e0463276e3ded6cef349f93', 'b019d38bad875c9d', 'e6285df3287c5d194f84df1a94817c7282d09754b6f9e02a'),
+		('624aac413795cdc1ff17365faf1ffe89', '6da297169f7aa9c2', '2e17884ea16177e2b751d53b5cc756c3cd57cdfd6e3bf8b9'),
+		('3b1b47e42e0463276e3ded6cef349f93', 'eacf7d5a2a6fa7d4', 'd2025bc5d6c201af7472550a677ca9904245a16ebb542a8e'),
+		('ae33a32dca8c9821844f740d5b3f4d6c', '677f1c557a5ee96c', '1bb250184772028e54394762ded81de1f608e6f37e7de5b0'),
+		('c4ea95cb148df11bf9d7c3611ad6d722', '514246973ea892c1', '497e9072282f5d33529e7359177d42ac9e106600630d3a6d'),
+		('cd06ca7c7e10c99b1d33b7485a2ed808', '0123456789abcdef', '25a98c1c31e81847466b29b2df4680f39958fb8c213a9cc6'),
+		('ff3750bcc2b22412c2265b23734e0dac', '0123456789abcdef', 'c337cd5cbd44fc9782a667af6d427c6de67c20c2d3e77c56'),
+		('04b8e0ba74289cc540826bab1dee63ae', 'ffffff0011223344', 'c951c8b1ddf71b2f8ec0be33f21ad93b7cd5fb2cd6cf51c5'),
+		('00563126f04f3875c417f789b00e72d2', '5355f4fc60c8888a', '9681672b365655d0592c3e4009547b9e11bc751b6e97943b'),
+	]
+
+	for key, data, resp in test_ntlm:
+		assert resp == ChallengeResponse(data.decode('hex'), key.decode('hex')).encode('hex')
+
+	# see https://forge.univention.org/bugzilla/show_bug.cgi?id=38785
+	res = ChallengeResponse('5355f4fc60c8888a'.decode('hex'), '00563126f04f3875c417f789b00e72d2'.decode('hex'))
+	assert res.encode('hex') == '9681672b365655d0592c3e4009547b9e11bc751b6e97943b'
 
 if __name__ == "__main__":
 	executeTestVectors()
