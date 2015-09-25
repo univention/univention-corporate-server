d7549c1f5a308723d68b8818550b06bcd03f88d5
Dirk Wiesenthal <wiesenthal@univention.de>
2015-09-25 21:15:22 +0000

    * Bug #39374: Copy repository/online/server into the container (useful esp while developing)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-0@64003 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console-module-appcenter/python/appcenter-docker/actions/docker_base.py
+++ management/univention-management-console-module-appcenter/python/appcenter-docker/actions/docker_base.py
@@ -110,7 +110,7 @@ class DockerActionMixin(object):
 		set_vars['ldap/hostdn'] = hostdn
 		set_vars['server/role'] = app.docker_server_role
 		set_vars['update/warning/releasenotes'] = 'no'
-		for var in ['nameserver.*', 'repository/app_center/server', 'ldap/master.*', 'locale.*', 'domainname', 'update/secure_apt']:
+		for var in ['nameserver.*', 'repository/online/server', 'repository/app_center/server', 'ldap/master.*', 'locale.*', 'domainname', 'update/secure_apt']:
 			for key in ucr.iterkeys():
 				if re.match(var, key):
 					set_vars[key] = ucr.get(key)
