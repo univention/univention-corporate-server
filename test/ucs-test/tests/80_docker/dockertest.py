99e971d8985824e239eb336dcfc6da472e01d009
Stefan Gohmann <gohmann@univention.de>
2015-09-26 19:41:54 +0000

    * First draft for more docker app tests (Bug #38887)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-0@64009 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/80_docker/dockertest.py
+++ test/ucs-test/tests/80_docker/dockertest.py
@@ -29,11 +29,18 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
+from univention.testing.strings import random_name, random_version
+from univention.config_registry import ConfigRegistry
+from univention.testing.debian_package import DebianPackage
+import os
+import shutil
 import subprocess
 
 class UCSTest_Docker_Exception(Exception): pass
 class UCSTest_Docker_LoginFailed(Exception): pass
 class UCSTest_Docker_PullFailed(Exception): pass
+class AppcenterMetainfAlreadyExists(Exception): pass
+class AppcenterRepositoryAlreadyExists(Exception): pass
 
 def docker_login(server='docker.software-univention.de'):
 	ret = subprocess.call(['docker','login','-e','foo@bar','-u','ucs','-p','readonly',server])
@@ -45,3 +52,78 @@ def docker_pull(image, server='docker.software-univention.de'):
 	if ret != 0:
 		raise UCSTest_Docker_PullFailed()
 
+def get_app_name():
+	""" returns a valid app name """
+	return random_name()
+
+def get_app_version():
+	return random_version()
+
+class App:
+	def __init__(self, name, version):
+		self.app_name = name
+		self.app_version = version
+
+		self.package_name = get_app_name()
+		self.package_version = get_app_version()
+
+		self.package = DebianPackage(name=self.package_name, version=self.package_version)
+		self.package.build()
+
+	def build(self):
+		pass
+
+	def add_to_local_appcenter(self):
+		pass
+
+	def install(self):
+		pass
+
+	def verify(self):
+		pass
+
+	def uninstall(self):
+		pass
+
+	def remove(self):
+		self.package.remove()
+
+class Appcenter:
+	def __init__(self):
+		self.meta_inf_created = False
+		self.univention_repository_created = False
+		
+		ucr = ConfigRegistry()
+		ucr.load()
+
+		version = ucr.get('version/version')
+
+		if os.path.exists('/var/www/meta-inf'):
+			print 'ERROR: /var/www/meta-inf already exists'
+			raise AppcenterMetainfAlreadyExists
+		if os.path.exists('/var/www/univention-repository'):
+			print 'ERROR: /var/www/univention-repository already exists'
+			raise AppcenterRepositoryAlreadyExists
+
+		os.makedirs('/var/www/meta-inf', 0755)
+		self.meta_inf_created = True
+
+		os.makedirs('/var/www/univention-repository', 0755)
+		self.univention_repository_created = True
+
+		os.makedirs('/var/www/univention-repository/%s/maintained/component' % version)
+		os.makedirs('/var/www/meta-inf/%s' % version)
+
+	def cleanup(self):
+		if self.meta_inf_created:
+			shutil.rmtree('/var/www/meta-inf')
+		if self.univention_repository_created:
+			shutil.rmtree('/var/www/univention-repository')
+
+	def __enter__(self):
+		return self
+
+	def __exit__(self, exc_type, exc_value, traceback):
+		if exc_type:
+			print 'Cleanup after exception: %s %s' % (exc_type, exc_value)
+		self.cleanup()
