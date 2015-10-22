350fbda4cbe31d7ae5b58f2aa57931ac0ae0bc6c
Felix Botner <botner@univention.de>
2015-10-22 12:17:33 +0000

    added some dsl groovy helpers Bug #39478
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-0@64749 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-ec2-tools/jenkins-dsl/app_autotest_multienv.groovy
+++ test/ucs-ec2-tools/jenkins-dsl/app_autotest_multienv.groovy
@@ -1,4 +1,5 @@
 import univention.Apps
+import univention.Jobs
 
 // Build parameters are exposed as environment variables in Jenkins.
 // A seed job build parameter named FOO is available as FOO variable
@@ -9,6 +10,7 @@ def loc = new File(JOB_NAME)
 def workdir = loc.getParent()
 
 // better get version from JOB_NAME
+println JOB_NAME
 def version = '4.1'
 def patch_level = '0'
 
@@ -26,70 +28,72 @@ test_apps['zarafa']['roles'] = ['master', 'backup', 'slave', 'memberserver']
 test_apps['dudle'] = [:]
 test_apps['dudle']['roles'] = ['master', 'backup']
 
-// create matrix job for every app
-test_apps.keySet()each { app ->
-  
-  // create folders
-  folder(workdir + '/apps')
-  folder(workdir + '/apps/' + app)
-  
-  // create job
-  job_name = workdir + '/apps/' + app + '/App Autotest MultiEnv'
-  matrixJob(job_name) {
-    
-    // config
-    quietPeriod(60)
-    logRotator(-1, 5, -1, -1)
-    description("run job for ${app}")
-    concurrentBuild()
+new Jobs().appAutotestMultiEnv(workdir + '/apps/', version, patch_level, 'app_name', ['master'])
 
-    // build parameters
-    parameters {
-      booleanParam('HALT', true, 'uncheck to disable shutdown of ec2 instances')
-    }
-    
-    // svn
-    scm {
-      svn {
-        checkoutStrategy(SvnCheckoutStrategy.CHECKOUT)
-        location("svn+ssh://svnsync@billy/var/svn/dev/branches/ucs-${version}/ucs-${version}-${patch_level}/test/ucs-ec2-tools") {
-          credentials('50021505-442b-438a-8ceb-55ea76d905d3')    
-        }
-        configure { scmNode ->
-          scmNode / browser(class: 'hudson.plugins.websvn2.WebSVN2RepositoryBrowser') {
-            url('https://billy.knut.univention.de/websvn/listing.php/?repname=dev')
-            baseUrl('https://billy.knut.univention.de/websvn/')
-            repname('repname=dev')      
-          }
-        }
-      }
-    }
-    
-    // axies
-    axes {
-      text('Systemrolle', test_apps[app].get('roles'))
-      text('SambaVersion', 's3', 's4')
-    }
-    
-    // wrappers
-    wrappers {
-      preBuildCleanup()
-    }
-    
-    // build step
-    steps {
-      cmd = """
-cfg="examples/jenkins/autotest-10*-app-\${Systemrolle}-\${SambaVersion}.cfg"
-sed -i "s|APP_ID|${app}|g" \$cfg
-sed -i "s|%PARAM_HALT%|\$HALT|g" \$cfg
-exec ./ucs-ec2-create -c \$cfg"""
-      shell(cmd)
-    }
-    
-    // post build
-    publishers {
-      archiveArtifacts('**/autotest-*.log,**/ucs-test.log')
-      archiveJunit('**/test-reports/**/*.xml')
-    }
-  }
-}
+//test_apps.keySet()each { app ->
+//  
+//  // create folders
+//  folder(workdir + '/apps')
+//  folder(workdir + '/apps/' + app)
+//
+//  
+//  // create matrix job App Autotest MultiEnv
+//  job_name = workdir + '/apps/' + app + '/App Autotest MultiEnv'
+//  matrixJob(job_name) {
+//    
+//    // config
+//    quietPeriod(60)
+//    logRotator(-1, 5, -1, -1)
+//    description("run job for ${app}")
+//    concurrentBuild()
+//
+//    // build parameters
+//    parameters {
+//      booleanParam('HALT', true, 'uncheck to disable shutdown of ec2 instances')
+//    }
+//    
+//    // svn
+//    scm {
+//      svn {
+//        checkoutStrategy(SvnCheckoutStrategy.CHECKOUT)
+//        location("svn+ssh://svnsync@billy/var/svn/dev/branches/ucs-${version}/ucs-${version}-${patch_level}/test/ucs-ec2-tools") {
+//          credentials('50021505-442b-438a-8ceb-55ea76d905d3')    
+//        }
+//        configure { scmNode ->
+//          scmNode / browser(class: 'hudson.plugins.websvn2.WebSVN2RepositoryBrowser') {
+//            url('https://billy.knut.univention.de/websvn/listing.php/?repname=dev')
+//            baseUrl('https://billy.knut.univention.de/websvn/')
+//            repname('repname=dev')      
+//          }
+//        }
+//      }
+//    }
+//    
+//    // axies
+//    axes {
+//      text('Systemrolle', test_apps[app].get('roles'))
+//      text('SambaVersion', 's3', 's4')
+//    }
+//    
+//    // wrappers
+//    wrappers {
+//      preBuildCleanup()
+//    }
+//    
+//    // build step
+//    steps {
+//      cmd = """
+//cfg="examples/jenkins/autotest-10*-app-\${Systemrolle}-\${SambaVersion}.cfg"
+//sed -i "s|APP_ID|${app}|g" \$cfg
+//sed -i "s|%PARAM_HALT%|\$HALT|g" \$cfg
+//exec ./ucs-ec2-create -c \$cfg"""
+//      shell(cmd)
+//    }
+//    
+//    // post build
+//    publishers {
+//      archiveArtifacts('**/autotest-*.log,**/ucs-test.log')
+//      archiveJunit('**/test-reports/**/*.xml')
+//    }
+//  }
+//}
