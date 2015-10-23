7b92b721dc2cede209e472cba5c9fcc9ffc9bb90
Daniel Tröder <troeder@univention.de>
2015-10-23 14:03:55 +0000

    cleanup after uninstalling (Bug #37890)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-0@64805 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-self-service/scp_app_to_omar.sh
+++ management/univention-self-service/scp_app_to_omar.sh
@@ -34,7 +34,7 @@ if ! ls app/*.ini > /dev/null; then
 	exit 1
 fi
 
-scp app/*.meta app/*_screenshot.* app/*_$DATE.{ini,png} omar:/mnt/omar/vmwares/mirror/appcenter.test/meta-inf/4.1/
+scp app/*.meta app/*_screenshot.* app/*_$DATE.{ini,png} omar:/mnt/omar/vmwares/mirror/appcenter.test/meta-inf/4.1/self-service/
 ssh omar mkdir       /mnt/omar/vmwares/mirror/appcenter.test/univention-repository/4.1/maintained/component/self-service_$DATE
 scp app/README* omar:/mnt/omar/vmwares/mirror/appcenter.test/univention-repository/4.1/maintained/component/self-service_$DATE/
 
