6e50a40ee21e3f91a401ab9c327a4e39aa64eaa0
Florian Best <best@univention.de>
2015-10-28 14:28:56 +0000

    Bug #39605: make it possible to detect if the IDP is reachable
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-0@64940 1b283449-9f2f-0410-b571-8eb228e1a901

--- saml/univention-saml/simplesamlphp-modules/univentiontheme/themes/univention/core/frontpage_welcome.tpl.php
+++ saml/univention-saml/simplesamlphp-modules/univentiontheme/themes/univention/core/frontpage_welcome.tpl.php
@@ -1,10 +1,8 @@
-<?php 
+<?php
 
-$this->includeAtTemplateBase('includes/header.php'); 
-
+$this->includeAtTemplateBase('includes/header.php');
 $this->data['header'] = $this->t('{core:frontpage:page_title}');
 
-SimpleSAML_Utilities::redirect('/ucs-overview');
-		
-$this->includeAtTemplateBase('includes/footer.php'); 
-?>
+#SimpleSAML_Utilities::redirect('/ucs-overview');
+
+$this->includeAtTemplateBase('includes/footer.php');
