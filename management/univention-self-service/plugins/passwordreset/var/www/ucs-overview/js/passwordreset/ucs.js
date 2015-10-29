7097e416fab2c124aed347e2c7ee25848d70847b
Alexander Kramer <kramer@univention.de>
2015-10-29 06:38:33 +0000

    Restructuring self-service - step 1 Bug #39597
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-0@64958 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-self-service/plugins/passwordreset/var/www/ucs-overview/js/passwordreset/ucs.js
+++
@@ -1,9 +0,0 @@
-var _callback = dojoConfig.callback;
-dojoConfig.callback = function() {
-	try {
-		_callback();
-	} catch(err) {};
-	require(["passwordreset/passwordreset", "dojo/domReady!"], function(passwordreset) {
-		passwordreset.start();
-	});
-}
--- management/univention-self-service/plugins/setpassword/var/www/ucs-overview/js/setpassword/ucs.js
+++
@@ -1,9 +0,0 @@
-var _callback = dojoConfig.callback;
-dojoConfig.callback = function() {
-	try {
-		_callback();
-	} catch(err) {};
-	require(["setpassword/setpassword", "dojo/domReady!"], function(setpassword) {
-		setpassword.start();
-	});
-}
