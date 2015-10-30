6c5de134ebe4426de7a15d68deaba563c933706e
Alexander Kramer <kramer@univention.de>
2015-10-30 09:21:40 +0000

    * Bug #39597: Improved UI for passwordchange
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-0@65003 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-self-service/js/ucs/passwordchange.js
+++ management/univention-self-service/js/ucs/passwordchange.js
@@ -31,16 +31,17 @@
 define([
 	"dojo/_base/lang",
 	"dojo/on",
+	"dojo/keys",
 	"dojo/dom",
 	"dojo/json",
 	"dojo/request/xhr",
 	"dijit/form/Button",
 	"put-selector/put",
 	"./TextBox",
-	"./PasswordBox",
-	"./PasswordInputBox",
 	"./i18n!"
-], function(lang, on, dom, json, xhr, Button, put, TextBox, PasswordBox, PasswordInputBox, _) {
+], function(lang, on, keys, dom, json, xhr, Button, put, TextBox, _) {
+	
+
 	return {
 		_createTitle: function() {
 			var title = _('Change Password');
@@ -52,49 +53,91 @@ define([
 
 		_createForm: function() {
 			var contentNode = dom.byId('content');
-			var tabNode = put(contentNode, 'div');
-			put(tabNode, 'p > b', _('Change your password'));
-			put(tabNode, 'p', _('Please follow this description I still have to write'));
+			var formNode = put(contentNode, 'div[id=form]');
+			put(formNode, 'p > b', _('Change your password'));
+			put(formNode, 'p', _('Please provide the necessary data to set a new password.'));
 
 			// create input field for username
 			this._username = new TextBox({
 				inlineLabel: _('Username'),
+				isValid: function() {
+					return !!this.get('value');
+				},
+				style: 'margin-top: 6px',
 				required: true
 			});
-			put(tabNode, '>', this._username.domNode);
+			put(formNode, '>', this._username.domNode);
 			this._username.startup();
 
 			// create input field for old password
-			this._oldPassword = new PasswordBox({
+			this._oldPassword = new TextBox({
 				inlineLabel: _('Old password'),
+				type: 'password',
+				isValid: function() {
+					return !!this.get('value');
+				},
+				style: 'margin-top: 6px',
 				required: true
 			});
-			put(tabNode, '>', this._oldPassword.domNode);
+			put(formNode, '>', this._oldPassword.domNode);
 			this._oldPassword.startup();
 
 			// create input fields for new password
-			this._newPassword = new PasswordInputBox({
+			this._newPassword = new TextBox({
 				inlineLabel: _('New password'),
 				twoRows: true,
+				type: 'password',
+				isValid: function() {
+					return !!this.get('value');
+				},
+				style: 'margin-top: 12px',
 				required: true
 			});
-			put(tabNode, '>', this._newPassword.domNode);
+			put(formNode, '>', this._newPassword.domNode);
 			this._newPassword.startup();
+
+			// create input fields for new password
+			this._verifyPassword = new TextBox({
+				inlineLabel: _('New password (retype)'),
+				twoRows: true,
+				type: 'password',
+				isValid: lang.hitch(this, function() {
+					return this._newPassword.get('value') ===
+						this._verifyPassword.get('value');
+				}),
+				invalidMessage: _('The passwords do not match, please retype again.'),
+				style: 'margin-top: 6px',
+				required: true
+			});
+			put(formNode, '>', this._verifyPassword.domNode);
+			this._verifyPassword.startup();
 
 			// create submit button
 			this._submitButton = new Button({
 				label: _('Submit'),
+				style: 'margin-top: 12px',
 				onClick: lang.hitch(this, '_submit')
 			});
-			put(tabNode, '>', this._submitButton.domNode);
+			put(formNode, '>', this._submitButton.domNode);
+
+			// let the user submit form by pressing ENTER
+			on(document, "keyup", lang.hitch(this, function(evt) {
+				if (evt.keyCode === keys.ENTER && !this._submitButton.get('disabled')) {
+					this._submit();
+				}
+			}));
 		},
 
 		_submit: function() {
-			var allInputFieldsValid = this._username.isValid() &&
+			this._removeMessage();
+			this._showValidStatusOfInputFields();
+			this._submitButton.set('disabled', true);
+			var allInputFieldsAreValid = this._username.isValid() &&
 				this._oldPassword.isValid() &&
-				this._newPassword.isValid();
-			
-			if (allInputFieldsValid) {
+				this._newPassword.isValid() &&
+				this._verifyPassword.isValid();
+
+			if (allInputFieldsAreValid) {
 				data = json.stringify({
 					'username': this._username.get('value'),
 					'password': this._oldPassword.get('value'),
@@ -107,11 +150,47 @@ define([
 						'Content-Type': 'application/json'
 					},
 					data: data
-				}).then(function() {
+				}).then(lang.hitch(this, function(data) {
 					// TODO show msg
-				}, lang.hitch(this, function(err) {
-					// TODO show msg
+					this._showMessage(data, '.success');
+					// TODO implement redirect by query string
+				}), lang.hitch(this, function(err) {
+					this._showMessage(err.name + ": " + err.message, '.error');
+				})).always(lang.hitch(this, function(){
+					this._submitButton.set('disabled', false);
 				}));
+			} else {
+				this._submitButton.set('disabled', false);
+			}
+		},
+
+		_showValidStatusOfInputFields: function() {
+			this._username.setValid(this._username.isValid());
+			this._oldPassword.setValid(this._oldPassword.isValid());
+			this._newPassword.setValid(this._newPassword.isValid());
+			//this._verifyPassword.setValid(this._verifyPassword.isValid());
+		},
+
+		_showMessage: function(msg, msgClass) {
+			var formNode = dom.byId('form');
+			var msgNode = dom.byId('msg');
+			if (!msgNode) {
+				msgNode = put('div[id=msg]');
+				put(formNode, 'div', msgNode);
+			}
+
+			if (msgClass) {
+				put(msgNode, "." + msgClass);
+			}
+			// replace newlines with BR tags
+			msg = msg.replace(/\n/g, '<br/>');
+			msgNode.innerHTML = msg;
+		},
+
+		_removeMessage: function() {
+			var msgNode = dom.byId('msg');
+			if (msgNode) {
+				put(msgNode, "!");
 			}
 		},
 
