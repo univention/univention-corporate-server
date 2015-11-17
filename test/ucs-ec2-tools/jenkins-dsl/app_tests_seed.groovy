9458faa0568aa589ba3382d9e7de218445cac4dd
Felix Botner <botner@univention.de>
2015-11-17 15:26:00 +0000

    added app-autotest-multienv.groovy Bug #39478
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-0@65672 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-ec2-tools/jenkins-dsl/app_tests_seed.groovy
+++ test/ucs-ec2-tools/jenkins-dsl/app_tests_seed.groovy
@@ -7,60 +7,27 @@ import univention.Jobs
 // in the DSL scripts. See the section about environment variables above.
 
 // get location (and UCS version) from job name 
-def loc = new File(JOB_NAME)
-def workdir = loc.getParent()
-
-// better get version from JOB_NAME
-//def version = JOB_NAME.split('/')[0].replace('UCS-', '')
-//def last_version = univention.Constants.LAST_VERSION.get(version)
-//def patch_level = univention.Constants.LASTEST_PATCHLEVEL.get(version)
-
-//if (last_version == null) {
-//	throw new RuntimeException("last version for version ${version} not found")
-//}
-
-//if (patch_level == null) {
-//    throw new RuntimeException("lastest patch_level for version ${version} not found")
-//}
+//def loc = new File(JOB_NAME)
+//def workdir = loc.getParent()
 
 univention.Constants.VERSIONS.each {
 
     version = it.getKey()
     patch_level = it.getValue()['patch_level']
     last_version = it.getValue()['last_version']
-
     path = 'UCS-' + version + '/Apps'
 
-    println version
-    println patch_level
-    println last_version
-    println path
+    // create folder, generic app jobs and views
+    folder(path)
+    Jobs.createAppStatusViews(this, path)
 
+    // get apps for version and create folder and jobs
     apps = Apps.getApps(version, test=true, ucs_components=false)
     apps.keySet().sort().each { app ->
         app_path = path + '/' + app
-        println "${app_path} -> ${app}"
+        folder(app_path)
+        Jobs.createAppAutotestUpdateMultiEnv(this, app_path, version, patch_level, apps[app])
+        Jobs.createAppAutotestMultiEnv(this, app_path, version, patch_level, apps[app])
+        Jobs.createAppAutotestMultiEnvUpdateFrom(this, app_path, version, patch_level, last_version, apps[app])
     }
 }
-
-//// create folder, generic app jobs and views
-//folder(path)
-//Jobs.createAppStatusViews(this, path)
-//
-//// get apps from testing, without ucs components
-//apps = Apps.getApps(version, test=true, ucs_components=false)
-//
-//// create jobs for every app
-//apps.keySet().sort().each { app ->
-//
-//  path = workdir + '/Apps/' + app
-//
-//  // create app folder
-//  folder(path)
-//  
-//  // create  jobs
-//  Jobs.createAppAutotestUpdateMultiEnv(this, path, version, patch_level, apps[app])
-//  Jobs.createAppAutotestMultiEnv(this, path, version, patch_level, apps[app])
-//  Jobs.createAppAutotestMultiEnvUpdateFrom(this, path, version, patch_level, last_version, apps[app])
-//
-//}
