1d6afc1769543b829df928cc73e1b4ffb8ba13e3
Felix Botner <botner@univention.de>
2015-12-03 16:46:24 +0000

    delete obsolte univention-welcome-screen source package (Bug #39241)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-0@66097 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-welcome-screen/var/www/ucs-overview/js/welcome/ucs.js
+++
@@ -1,9 +0,0 @@
-var _callback = dojoConfig.callback;
-dojoConfig.callback = function() {
-	try {
-		_callback();
-	} catch(err) {};
-	require(["welcome/welcome", "dojo/domReady!"], function(welcome) {
-		welcome.start();
-	});
-}
