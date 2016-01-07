bb72928fced580eaec85d233e77471a409661a2b
Florian Best <best@univention.de>
2016-01-07 10:31:00 +0000

    Bug #40354: autopep8
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-0@66619 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-lib/python/atjobs.py
+++ base/univention-lib/python/atjobs.py
@@ -41,7 +41,7 @@ import subprocess
 import re
 import locale
 
-__all__ = [ 'add', 'list', 'load', 'remove', 'reschedule', 'AtJob' ]
+__all__ = ['add', 'list', 'load', 'remove', 'reschedule', 'AtJob']
 
 # internal formatting strings and regexps
 _regWhiteSpace = re.compile(r'\s+')
@@ -54,32 +54,33 @@ _dateFormatWrite = '%Y-%m-%d'
 SCRIPT_PREFIX = '# --- Univention-Lib at job  ---'
 COMMENT_PREFIX = '# Comment: '
 
-def add( cmd, execTime = None, comments = {} ):
+
+def add(cmd, execTime=None, comments={}):
 	'''Add a new command to the job queue given a time (in seconds since
 	the epoch or as a datetime object) at which the job will be
 	executed. Optionally comments can be provided as a dictionary. The
 	data is assoziated with the job.'''
 
-	if isinstance( execTime, ( int, float ) ):
-		start = datetime.datetime.fromtimestamp( execTime )
+	if isinstance(execTime, (int, float)):
+		start = datetime.datetime.fromtimestamp(execTime)
 	else:
 		start = execTime
 
 	# launch the at job directly
 	atCmd = ['/usr/bin/at']
 	if start:
-		jobTime = start.strftime( _timeFormatWrite )
-		jobDate = start.strftime( _dateFormatWrite )
+		jobTime = start.strftime(_timeFormatWrite)
+		jobDate = start.strftime(_dateFormatWrite)
 		atCmd.extend([jobTime, jobDate])
 	else:
 		atCmd.append('now')
 
 	# add comments
 	if comments:
-		cmd =  '\n'.join( map( lambda c: '%s%s:%s' % ( COMMENT_PREFIX, c[ 0 ], c[ 1 ] ), comments.items() ) ) + '\n' + SCRIPT_PREFIX + '\n' + cmd
+		cmd = '\n'.join(map(lambda c: '%s%s:%s' % (COMMENT_PREFIX, c[0], c[1]), comments.items())) + '\n' + SCRIPT_PREFIX + '\n' + cmd
 
 	# add job
-	p = subprocess.Popen(atCmd, stdout = subprocess.PIPE, stdin = subprocess.PIPE, stderr = subprocess.PIPE)
+	p = subprocess.Popen(atCmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 
 	# send the job to stdin
 	out = p.communicate(cmd)
@@ -90,22 +91,24 @@ def add( cmd, execTime = None, comments = {} ):
 		return load(int(matches[0]))
 	return None
 
-def reschedule( nr, execTime = None ):
+
+def reschedule(nr, execTime=None):
 	"""Re-schedules the at job with the given number for the specified time"""
-	atjob = load( nr, extended = True )
+	atjob = load(nr, extended=True)
 	if atjob is None:
-		raise AttributeError( 'Could not find at job %s' % nr )
+		raise AttributeError('Could not find at job %s' % nr)
 	if atjob.command is None:
-		raise AttributeError( 'The command of the at job is not available' )
+		raise AttributeError('The command of the at job is not available')
 	atjob.rm()
 
-	return add( atjob.command, execTime, atjob.comments )
+	return add(atjob.command, execTime, atjob.comments)
 
-def list( extended = False ):
+
+def list(extended=False):
 	'''Returns a list of all registered jobs as instances of AtJob. If
 	extended is set to True the parser also extras the comments and the
 	command to execute. This can be used to re-schedule a job.'''
-	p = subprocess.Popen('/usr/bin/atq', stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+	p = subprocess.Popen('/usr/bin/atq', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out = p.communicate()[0].splitlines()
 	jobs = []
 	for line in out:
@@ -115,26 +118,29 @@ def list( extended = False ):
 
 	if extended:
 		for job in jobs:
-			_parseScript( job )
+			_parseScript(job)
 
 	return jobs
 
-def load( nr, extended = False ):
+
+def load(nr, extended=False):
 	'''Load a job given a particular ID. Returns None if job does not exist,
 	otherwise an instance of AtJob is returned.'''
-	result = [ p for p in list( extended ) if p.nr == nr ]
+	result = [p for p in list(extended) if p.nr == nr]
 	if len(result):
 		return result[0]
 	return None
 
-def remove( nr ):
-	"""Removes the at job with the given number"""
-	result = [ p for p in list() if p.nr == nr ]
-	if len(result):
-		return result[ 0 ].rm()
 
-def _parseScript( job ):
-	p = subprocess.Popen( [ '/usr/bin/at', '-c', str( job.nr ) ], stdout = subprocess.PIPE, stderr = subprocess.PIPE )
+def remove(nr):
+	"""Removes the at job with the given number"""
+	result = [p for p in list() if p.nr == nr]
+	if len(result):
+		return result[0].rm()
+
+
+def _parseScript(job):
+	p = subprocess.Popen(['/usr/bin/at', '-c', str(job.nr)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out = p.communicate()[0].splitlines()
 	job.comments = {}
 	script = False
@@ -150,6 +156,7 @@ def _parseScript( job ):
 		elif line.startswith(SCRIPT_PREFIX):
 			script = True
 
+
 def _parseJob(string):
 	'''Internal method to parse output of at-command.'''
 	timeLocale = locale.getlocale(locale.LC_TIME)
@@ -160,23 +167,22 @@ def _parseJob(string):
 
 		# parse string
 		tmp = _regWhiteSpace.split(string)
-		execTime = datetime.datetime.strptime( ' '.join( tmp[ 1 : 6 ] ), _dateTimeFormatRead )
+		execTime = datetime.datetime.strptime(' '.join(tmp[1: 6]), _dateTimeFormatRead)
 		isRunning = tmp[6] == '='
 		owner = tmp[7]
 		nr = int(tmp[0])
-	except (IndexError, ValueError), e:
-		# reset locale to default
-		locale.setlocale(locale.LC_TIME, timeLocale)
+	except (IndexError, ValueError):
 		# parsing failed
 		return None
-
-	# reset locale to default
-	locale.setlocale(locale.LC_TIME, timeLocale)
+	finally:
+		# reset locale to default
+		locale.setlocale(locale.LC_TIME, timeLocale)
 
 	return AtJob(nr, owner, execTime, isRunning)
 
 
 class AtJob(object):
+
 	'''This class is an abstract representation of an at-job. Do not initiate
 	the class directly, but use the methods provided in this module.'''
 
@@ -189,7 +195,7 @@ class AtJob(object):
 		self.comments = {}
 
 	def __str__(self):
-		t = self.execTime.strftime( _dateTimeFormatWrite )
+		t = self.execTime.strftime(_dateTimeFormatWrite)
 		if self.isRunning:
 			t = 'running'
 		return 'Job #%d (%s)' % (self.nr, t)
@@ -199,6 +205,5 @@ class AtJob(object):
 
 	def rm(self):
 		'''Remove the job from the queue.'''
-		p = subprocess.Popen(['/usr/bin/atrm', str(self.nr)], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+		p = subprocess.Popen(['/usr/bin/atrm', str(self.nr)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		p.communicate()
-
