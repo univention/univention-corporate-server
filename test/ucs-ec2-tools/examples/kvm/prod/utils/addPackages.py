1661edae1bc395efbfe299410fad31fd68ef617b
Julian Hupertz <hupertz@univention.de>
2016-02-03 11:59:09 +0000

    Bug #39359 changed jenkins project Autotest MultiEnv (Ipv6) Generic and Update from 4.0 so the packages can be given to the install process over a text field. the script will now check if no packages are used and foramt the kvm-files as suspected
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-0@67140 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-ec2-tools/examples/kvm/prod/utils/addPackages.py
+++ test/ucs-ec2-tools/examples/kvm/prod/utils/addPackages.py
@@ -9,38 +9,36 @@ def main():
 	parser = argparse.ArgumentParser(description=desc)
 	parser.add_argument('--url','-u',help='the url to the corresponding file',required=True)
 	
-	group = parser.add_mutually_exclusive_group(required=True)
-	group.add_argument('--packages','-p', nargs='*')
-	group.add_argument('--empty','-e',help='this is used if no additional packages should be installed', action="store_true")
-	
+	parser.add_argument('--packages','-p', nargs='*', default=['empty'])
+
 	args = vars(parser.parse_args())
 
 	path = args['url']
 
+	print(len(args['packages']))
+
 	with open(path, 'r') as f:
 		listFile = f.readlines()
 
-	if args['packages']:
-		packages = args['packages']
-		#print("packages übergeben")
-		#print(len(packages))
-		newListFile = [checkPackages(item, packages, empty=False) for item in listFile]
-	elif args['empty']:
-		#print("empty übergeben")
-		newListFile = [checkPackages(item, empty=True) for item in listFile]
+	#if args['packages'] or len(args['packages']) == 0:
+	packages = args['packages']
+	newListFile = [checkPackages(item, packages) for item in listFile]
+
 
 	with open(path, 'w') as f:
 		for elem in newListFile:
 			f.write(elem)
 
-	
-
-
-
-def checkPackages(item, packagesList=None, empty=False):
-	if empty==False and packagesList and "packages_install=" in item:
+"""
+Check if item which represents a line from the imported kvm-file represents
+the line with the additional packages that have to be installed.
+In case that no packages where given as arguments the list of packages
+will be empty
+"""
+def checkPackages(item, packagesList=None):
+	#print(packagesList)
+	if packagesList and (packagesList[0] != "empty" and "packages_install=" in item):
 		packages = packagesList
-		#print(len(packages))
 		packagesStr = " packages_install=\""
 
 		for package in packages[:-1]:
@@ -49,8 +47,9 @@ def checkPackages(item, packagesList=None, empty=False):
 		packagesStr = packagesStr + packages[-1] + "\"\n"
 
 		return packagesStr
-	elif empty==True and not packagesList and "packages_install=" in item:
-		#print("angekommen")
+	elif packagesList and (packagesList[0] == "empty" and "packages_install=" in item):
+		return " packages_install=\"\"\n"
+	elif not packagesList and "packages_install=" in item:
 		return " packages_install=\"\"\n"
 	else:
 		return item
