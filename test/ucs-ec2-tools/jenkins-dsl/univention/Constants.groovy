520804cc171def307b361c95735c59951a9eee26
Felix Botner <botner@univention.de>
2016-02-17 09:28:56 +0000

    App Tests new UCS patch level Bug #40616
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-1@67494 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-ec2-tools/jenkins-dsl/univention/Constants.groovy
+++ test/ucs-ec2-tools/jenkins-dsl/univention/Constants.groovy
@@ -8,18 +8,6 @@ class Constants {
 		memberserver : 'member',
 	]
 
-	static LAST_VERSION = [
-		'4.0' : '3.2',
-		'3.2' : '3.1',
-		'4.1' : '4.0',
-	]
-
-    static LASTEST_PATCHLEVEL = [
-        '4.0' : '4',
-        '4.1' : '0',
-        '3.2' : '7',
-    ]
-
     static VERSIONS =  [
         '4.0' : ['patch_level' : '4', 'last_version' : '3.2'],
         '4.1' : ['patch_level' : '1', 'last_version' : '4.0'],
