b5863f932f7c964b4b7f48f0f8be9c93fd6b1307
Florian Best <best@univention.de>
2016-03-04 11:27:37 +0000

    Bug #23804: prevent to search for name=* in dns/dns as it has no results
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-1@67901 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-directory-manager-modules/modules/univention/admin/handlers/dns/dns.py
+++ management/univention-directory-manager-modules/modules/univention/admin/handlers/dns/dns.py
@@ -30,7 +30,6 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
-from univention.admin.layout import Tab, Group
 import univention.admin.filter
 import univention.admin.localization
 
@@ -41,6 +40,7 @@ import univention.admin.handlers.dns.alias
 import univention.admin.handlers.dns.host_record
 import univention.admin.handlers.dns.srv_record
 import univention.admin.handlers.dns.ptr_record
+import univention.admin.handlers.dns.txt_record
 
 
 translation=univention.admin.localization.translation('univention.admin.handlers.dns')
@@ -69,21 +69,7 @@ childmodules = [ 'dns/forward_zone', 'dns/reverse_zone', 'dns/alias', 'dns/host_
 virtual=1
 options={
 }
-property_descriptions={
-	'name': univention.admin.property(
-			short_description=_('Name'),
-			long_description='',
-			syntax=univention.admin.syntax.string,
-			multivalue=0,
-			include_in_default_search=1,
-			options=[],
-			required=1,
-			may_change=1,
-			identifies=1
-		)
-}
-layout = [ Tab(_('General'),_('Basic settings'), layout = [ "name" ] ) ]
-
+property_descriptions={}
 mapping=univention.admin.mapping.mapping()
 
 
@@ -118,5 +104,3 @@ def lookup(co, lo, filter_s, base='', superordinate=None, scope='sub', unique=0,
 
 def identify(dn, attr, canonical=0):
 	pass
-
-
