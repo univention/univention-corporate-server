d80d0400b1a98b3cf001d34a922bc2abe668542d
Stefan Gohmann <gohmann@univention.de>
2016-03-14 07:34:50 +0000

    * script/postup.sh: Fix shell escaping (Bug #40692)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.3/ucs-3.3-0@68057 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-updater/script/postup.sh
+++ base/univention-updater/script/postup.sh
@@ -59,7 +59,7 @@ check_and_reinstall ()
 is_ucr_true () {
     local value
     value="$(/usr/sbin/univention-config-registry get "$1")"
-    case "$(echo -n "$value" | tr [:upper:] [:lower:])" in
+    case "$(echo -n "$value" | tr '[:upper:]' '[:lower:]')" in
         1|yes|on|true|enable|enabled) return 0 ;;
         0|no|off|false|disable|disabled) return 1 ;;
         *) return 2 ;;
