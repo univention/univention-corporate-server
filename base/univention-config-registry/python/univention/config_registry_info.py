b28e9093e4e9318a1d840814c85de85e7c25430b
Arvid Requate <requate@univention.de>
2016-03-23 14:53:13 +0000

    * For Bug #40383: Improve code readability
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-1@68258 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-config-registry/python/univention/config_registry_info.py
+++ base/univention-config-registry/python/univention/config_registry_info.py
@@ -267,7 +267,7 @@ class ConfigRegistryInfo( object ):
 		if os.path.exists ( path ):
 			for entry in os.listdir( path ):
 				cfgfile = os.path.join( path, entry )
-				if os.path.isfile( cfgfile ) and cfgfile[-len(ConfigRegistryInfo.FILE_SUFFIX):] == ConfigRegistryInfo.FILE_SUFFIX and entry != ConfigRegistryInfo.CUSTOMIZED:
+				if os.path.isfile( cfgfile ) and cfgfile.endswith(ConfigRegistryInfo.FILE_SUFFIX) and entry != ConfigRegistryInfo.CUSTOMIZED:
 					self.read_variables( cfgfile )
 			self.check_patterns()
 			if not registered_only:
