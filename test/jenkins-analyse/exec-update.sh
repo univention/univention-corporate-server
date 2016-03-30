a02a16556852e93dc9bff3502f83f27ef8dedbf8
Julian Hupertz <hupertz@univention.de>
2016-03-30 13:06:29 +0000

    Bug #39359 changed pathes in kvm-scripts and added a new shell-script for calling jenkins-analyse
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-1@68304 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ test/jenkins-analyse/exec-update.sh
@@ -0,0 +1,1 @@
+python ./jenkins-analyse/jenkins_analyze_html.py -u http://jenkins.knut.univention.de:8080/job/UCS-4.1/job/UCS-4.1-1/job/Autotest%20MultiEnv%20%28IPv6%29%20Update%20from%204.0/ -l -o build.html
