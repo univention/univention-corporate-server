univention/univention-corporate-server

commit 49600af38deaae35af3e4c0380a5ed3644886ad0

Bug #41261 UDL: Restore schema/id writing
Revert part of r69907, which removed
"/var/lib/univention-ldap/schema/id/id". It is still required by the
Notifier on DC Backups when chaining the notifications.

As the Notifier still uses the deprecated "dnotify" which triggers only
on DN_MODIFY (but not DM_RENAME), we need to modify the existing file.
As luckily the ID is monotonic and thus its string length is monotonic
as well, we can just overwrite the exiting file.
But the stdio C library's stream interface does not support opening an
existing file without overwriting, which also works when the files does
not yet exist. As such use the lower-level FD interface.

Drop useless 'key' parameter for cache_[sg]et_schema_id()

Add warning message is writing files fails.

git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-2@69980 1b283449-9f2f-0410-b571-8eb228e1a901

management/univention-directory-listener/src/notifier.c
@@ -187,7 +187,8 @@ int notifier_listen(univention_ldap_parameters_t *lp,
 
 		cache_master_entry.id = id;
 		cache_update_master_entry(&cache_master_entry, NULL);
-		cache_set_int("notifier_id", id);
+		if (cache_set_int("notifier_id", id))
+			univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN, "failed to write notifier ID");
 		change_free_transaction_op(&trans.cur);
 	}
 

management/univention-directory-listener/debian/univention-directory-listener.postinst
@@ -55,15 +55,10 @@ univention-config-registry set listener/debug/level?2 \
 univention-config-registry set	listener/memberuid/skip?no \
 							listener/uniquemember/skip?no
 
-mkdir -p /var/lib/univention-ldap/schema/id/
-
-if [ ! -e /var/lib/univention-ldap/schema/id/id ]; then
-	touch /var/lib/univention-ldap/schema/id/id
-fi
-
-if [ -e /var/lib/univention-ldap/schema/id/id ]; then
-	chown listener /var/lib/univention-ldap/schema/id/id
-fi
+install -d -o listener -g root -m 0755 /var/lib/univention-ldap/schema
+install -d -o listener -g root -m 0755 /var/lib/univention-ldap/schema/id
+touch /var/lib/univention-ldap/schema/id/id
+chown listener:root /var/lib/univention-ldap/schema/id/id
 
 if [ -e /var/lib/univention-ldap/replog/replog ]; then
 	chmod 600 /var/lib/univention-ldap/replog/replog

management/univention-directory-listener/src/cache.c
@@ -214,7 +214,37 @@ void cache_sync(void) {
 	}
 }
 
-int cache_get_schema_id(char *key, NotifierID *value, const long def)
+int cache_set_schema_id(const NotifierID value)
+{
+	int rv, fd, len;
+	char file[PATH_MAX], buf[15];
+
+	univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN, "Set Schema ID to %ld", value);
+	len = snprintf(buf, sizeof buf, "%ld", value);
+	if (len < 0 || len >= sizeof buf)
+		return len;
+
+	rv = snprintf(file, PATH_MAX, "%s/schema/id/id", ldap_dir);
+	if (rv < 0 || rv >= PATH_MAX)
+		return rv;
+	fd = open(file, O_WRONLY | O_CREAT, 0644);
+	if (fd < 0)
+		return fd;
+	rv = write(fd, buf, len);
+	if (rv != len) {
+		close(fd);
+		return 1;
+	}
+	rv = ftruncate(fd, len);
+	if (rv != 0)
+		return rv;
+	rv = close(fd);
+	if (rv != 0)
+		return rv;
+	return 0;
+}
+
+int cache_get_schema_id(NotifierID *value, const long def)
 {
 	FILE *fp;
 	char file[PATH_MAX];
@@ -234,7 +264,9 @@ int cache_set_int(char *key, const NotifierID value)
 	FILE *fp;
 	char file[PATH_MAX], tmpfile[PATH_MAX];
 
-	snprintf(tmpfile, PATH_MAX, "%s/%s.tmp", cache_dir, key);
+	rv = snprintf(tmpfile, PATH_MAX, "%s/%s.tmp", cache_dir, key);
+	if (rv < 0 || rv >= PATH_MAX)
+		return rv;
 	if ((fp = fopen(tmpfile, "w")) == NULL)
 		return 1;
 	fprintf(fp, "%ld", value);
@@ -242,7 +274,9 @@ int cache_set_int(char *key, const NotifierID value)
 	if (rv != 0)
 		return rv;
 
-	snprintf(file, PATH_MAX, "%s/%s", cache_dir, key);
+	rv = snprintf(file, PATH_MAX, "%s/%s", cache_dir, key);
+	if (rv < 0 || rv >= PATH_MAX)
+		return rv;
 	rv = rename(tmpfile, file);
 	return rv;
 }

management/univention-directory-listener/src/cache.h
@@ -89,6 +89,7 @@ int	cache_get_int				(char		 *key,
 						 NotifierID			 *value,
 						 const long		  def);
 
-int cache_get_schema_id(char *key, NotifierID *value, const long def);
+int cache_get_schema_id(NotifierID *value, const long def);
+int cache_set_schema_id(const NotifierID value);
 
 #endif /* _CACHE_H_ */

management/univention-directory-listener/src/change.c
@@ -367,6 +367,8 @@ int change_update_schema(univention_ldap_parameters_t *lp)
 		}
 		cache_master_entry.schema_id = new_id;
 		rv = cache_update_master_entry(&cache_master_entry, NULL);
+		if (cache_set_schema_id(new_id))
+			univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN, "failed to write schema ID");
 	}
 
 	return rv;

management/univention-directory-listener/src/main.c
@@ -604,14 +604,15 @@ int main(int argc, char* argv[])
 			}
 		}
 
-		cache_get_schema_id("notifier_schema_id", &cache_master_entry.schema_id, 0);
+		cache_get_schema_id(&cache_master_entry.schema_id, 0);
 
 		rv = cache_update_master_entry(&cache_master_entry, NULL);
 	}
 	if (rv != 0)
 		return rv;
 	/* Legacy file for Nagios et al. */
-	cache_set_int("notifier_id", cache_master_entry.id);
+	if (cache_set_int("notifier_id", cache_master_entry.id))
+		univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN, "failed to write notifier ID");
 
 	/* update schema */
 	if ((rv=change_update_schema(lp)) != LDAP_SUCCESS)

commit c5fa75e163b2e725eb0d443a26ba958cc2776702

Bug #41261 UDL: Fix notifier ID not incrementing
Fix regression introduced by r69980: The notifier ID needs to be
initialized once before the loop is started. Later on the ID gets
updates from the ID returned by the notifier.c:153:
> »···»···id = trans.cur.notify.id;
Otherwise the ID does not increment in case of moves, as the same ID is
queried again and again.

git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-2@69998 1b283449-9f2f-0410-b571-8eb228e1a901

management/univention-directory-listener/src/notifier.c
@@ -91,6 +91,7 @@ int notifier_listen(univention_ldap_parameters_t *lp,
 		univention_ldap_parameters_t *lp_local)
 {
 	int rv = 0;
+	NotifierID id = cache_master_entry.id;
 	struct transaction trans = {
 		.lp = lp,
 		.lp_local = lp_local,
@@ -99,7 +100,6 @@ int notifier_listen(univention_ldap_parameters_t *lp,
 	for (;;) {
 		int		msgid;
 		time_t		timeout = DELAY_LDAP_CLOSE;
-		NotifierID id = cache_master_entry.id;
 
 		if ((msgid = notifier_get_dn(NULL, id + 1)) < 1)
 			break;