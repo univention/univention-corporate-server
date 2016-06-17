1938744506ec29d4aa2e6a1c1fe8c0e7703584e8
Alexander Kläser <klaeser@univention.de>
2016-06-17 14:37:08 +0000

    * Bug #40826: Move branding process from ucr template to /usr/sbin/univention-app-appliance-branding * Add some verbosity to the branding script
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-2@70399 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-app-appliance-branding/conffiles/setup-appliance-branding.py
+++ base/univention-app-appliance-branding/conffiles/setup-appliance-branding.py
@@ -31,12 +31,9 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
-import os
-import requests
-from glob import glob
-from subprocess import call
-from univention.app_appliance import AppManager, get_luminance
-from univention.config_registry.frontend import ucr_update
+import subprocess
+
+from univention.app_appliance import AppManager
 
 
 def handler(config_registry, changes):
@@ -45,107 +42,4 @@ def handler(config_registry, changes):
 	app = AppManager.find(app_id)
 	if not app:
 		return
-
-	# query color information
-	css_background = app.appliance_css_background or '#eeeeee'
-	primary_color = app.appliance_primary_color or '#5a5a5a'
-	secondary_color = app.appliance_secondary_color or '#7db523'
-	if get_luminance(primary_color) < .5:
-		theme = 'dark'
-	else:
-		theme = 'light'
-
-	# adjust colors in SVG images via search and replace
-	for src_path in glob('/usr/share/univention-app-appliance-branding/images/*.svg'):
-		filename = os.path.basename(src_path)
-		dest_path = os.path.join('/usr/share/univention-management-console-frontend/js/umc/modules/setup/', filename)
-		with open(src_path, 'r') as in_file:
-			with open(dest_path, 'w') as out_file:
-				for line in in_file:
-					out_file.write(line.replace('#ff00ff', secondary_color))
-
-	# create background image for plymouth theme
-	call(['/usr/share/univention-app-appliance-branding/render-css-background', '1600x1200', css_background, '/usr/share/plymouth/themes/ucs-appliance-%s/bg.png' % theme])
-
-	def symlink_first_steps():
-		for lang in ['', '_DE']:
-			source = '/var/cache/univention-appcenter/%s.README%s' % (app.component_id, lang)
-			target = '/var/www/univention-management-console/js/umc/modules/appliance/appliance_first_steps.README%s' % (lang,)
-			try:
-				print("Remove old first steps symlink")
-				os.remove(target)
-			except OSError:
-				pass
-			print("Symlink first steps from %s to %s" % (source, target,))
-			os.symlink(source, target)
-
-	def _download(filename, dest_path):
-		url = 'https://{server}/meta-inf/{version}/{app}/{file}'.format(
-			server=config_registry.get('repository/app_center/server'),
-			version=config_registry.get('version/version'),
-			app=app.id,
-			file=filename,
-		)
-		try:
-			req = requests.head(url, timeout=5)
-			if req.status_code < 400:
-				req = requests.get(url)
-				with open(dest_path, 'wb') as handle:
-					for block in req.iter_content(8192):
-						handle.write(block)
-				print 'Successfully downloaded %s' % url
-		except (IOError, requests.HTTPError, requests.ConnectionError, requests.Timeout) as err:
-			print 'WARNING: Failed to download %s' % url
-
-
-	def set_grub_theme():
-		if theme == 'dark':
-			grub_color = 'white/black'
-		elif theme == 'light':
-			grub_color = 'black/white'
-		ucr_update(config_registry, {
-			'grub/backgroundimage':  '/usr/share/plymouth/themes/ucs-appliance-%s/bg.png' % (theme,),
-			'grub/color/highlight': grub_color,
-			'grub/color/normal': grub_color,
-			'grub/menu/color/highlight': grub_color,
-			'grub/menu/color/normal': grub_color,
-			'grub/title': config_registry.get('umc/web/appliance/name', 'App') + ' Appliance'
-		})
-
-	def _svg_to_png(source, dest, params, res='800x600'):
-		css_background = 'url(file:///{source}) {params}'.format(source=source, params=params)
-		call(['/usr/share/univention-app-appliance-branding/render-css-background', res,  css_background, dest])
-
-	symlink_first_steps()
-
-	# download image files for the app appliance
-	if app.appliance_logo:
-		_download(app.appliance_logo, '/usr/share/univention-management-console-frontend/js/umc/modules/setup/welcome.svg')
-
-	if app.appliance_umc_header_logo:
-		_download(app.appliance_umc_header_logo, '/usr/share/univention-management-console-frontend/js/dijit/themes/umc/images/appliance_header_logo.svg')
-		_img = '/usr/share/plymouth/themes/ucs-appliance-%s/logo_header_welcome_screen.svg' % theme
-		_download(app.appliance_umc_header_logo, _img)
-		_params = 'no-repeat; background-size: auto 28px; background-position: left center;'
-		_svg_to_png(_img, _img + '.png', _params, '400x30')
-		_download(app.appliance_umc_header_logo, '/usr/share/univention-system-activation/www/css/icons/appliance_header_logo.svg')
-
-	if app.appliance_welcome_screen_logo:
-		_img = '/usr/share/plymouth/themes/ucs-appliance-%s/logo_welcome_screen.svg' % theme
-		_download(app.appliance_welcome_screen_logo, _img)
-		_params = 'no-repeat; background-size: contain; background-position: center center;'
-		_svg_to_png(_img, _img + '.png', _params, '120x120')
-
-	if app.appliance_bootsplash_logo:
-		_img = '/usr/share/plymouth/themes/ucs-appliance-%s/logo_bootsplash.svg' % theme
-		_download(app.appliance_bootsplash_logo, _img)
-		_params = 'no-repeat; background-size: contain; background-position: center center;'
-		_svg_to_png(_img, _img + '.png', _params)
-
-	category_logo = app.appliance_umc_category_logo or app.appliance_welcome_screen_logo
-	if category_logo:
-		_download(category_logo, '/usr/share/univention-management-console-frontend/js/dijit/themes/umc/icons/scalable/category-appliance.svg')
-
-	# set plymouth appliance theme
-	ucr_update(config_registry, {'bootsplash/theme': 'ucs-appliance-%s' % theme})
-	set_grub_theme()
+	subprocess.check_call(['/usr/sbin/univention-app-appliance-branding', app_id])
