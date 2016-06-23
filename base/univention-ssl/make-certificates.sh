b9ff409b2ee603e94adaf6344d028591c2b13762
Philipp Hahn <hahn@univention.de>
2016-06-23 06:39:08 +0000

    Bug #31369 ssl: Fix certificate not expired
    Not only check that certificate still exists and is not revoked, but
    also check that it hasn't expired yet.
    
    Format of index.txt:
    > #define DB_type         0 /* Status of the certificate */
    > #define DB_exp_date     1 /* Expiry date */
    > #define DB_rev_date     2 /* Revocation date */
    > #define DB_serial       3 /* Serial No., index - unique */
    > #define DB_file         4 /* (unused, always="unknown") */
    > #define DB_name         5 /* DN, index - unique when active and not disabled */
    >
    > #define DB_TYPE_REV    'R' /* Revoked */
    > #define DB_TYPE_EXP    'E' /* Expired (unused) */
    > #define DB_TYPE_VAL    'V' /* Valid */
    >
    > The dates are in ASN1_UTCTIME-format.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-2@70561 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-ssl/make-certificates.sh
+++ base/univention-ssl/make-certificates.sh
@@ -332,11 +332,11 @@ list_cert_names () {
 	}' <"${SSLBASE}/${CA}/index.txt"
 }
 
-has_valid_cert () { # returns 0 if yes, 1 if none found, 2 if expired
+has_valid_cert () { # returns 0 if yes, 1 if not found, 2 if revoked, 3 if expired
 	local cn="${1:?Missing argument: common name}"
 
 	awk -F '\t' -v name="$cn" '
-	BEGIN { ret=1; seq=""; }
+	BEGIN { ret=1; seq=""; now=strftime("%y%m%d%H%M%SZ", systime(), 1); }
 	{
 		split ( $6, X, "/" );
 		for ( i=2; X[i] != ""; i++ ) {
@@ -344,7 +344,7 @@ has_valid_cert () { # returns 0 if yes, 1 if none found, 2 if expired
 				split ( X[i], Y, "=" );
 				if ( name == Y[2] ) {
 					seq = $4;
-					ret = ( $1 == "V" ) ? 0 : 2;
+					ret = ( $1 == "V" ) ? ( $2 >= now ? 0 : 3 ) : 2;
 				}
 			}
 		}
