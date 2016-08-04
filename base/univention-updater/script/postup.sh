9209c4f5c2f034b9c223f11888457188dd9849e7
Philipp Hahn <hahn@univention.de>
2016-08-04 14:13:24 +0000

    Bug #41895 up: Adapt to UCS-4.1-3
    Update links.
    Change UCRV patchlevel
    Enable/disable errata components.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-3@71419 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-updater/script/postup.sh
+++ base/univention-updater/script/postup.sh
@@ -132,14 +132,11 @@ fi
 
 # Move to mirror mode for previous errata component
 ucr set \
-	repository/online/component/4.1-1-errata=false \
-	repository/online/component/4.1-1-errata/localmirror=true >>"$UPDATER_LOG" 2>&1
-
-# Set errata component for UCS 4.1-1
-ucr set \
-	repository/online/component/4.1-2-errata=enabled \
-	repository/online/component/4.1-2-errata/description="Errata updates for UCS 4.1-2" \
-	repository/online/component/4.1-2-errata/version="4.1" >>"$UPDATER_LOG" 2>&1
+	repository/online/component/4.1-2-errata=false \
+	repository/online/component/4.1-2-errata/localmirror=true \
+	repository/online/component/4.1-3-errata=enabled \
+	repository/online/component/4.1-3-errata/description="Errata updates for UCS 4.1-3" \
+	repository/online/component/4.1-3-errata/version="4.1" >>"$UPDATER_LOG" 2>&1
 
 # run remaining joinscripts
 if [ "$server_role" = "domaincontroller_master" ]; then
