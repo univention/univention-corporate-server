dd10547bccd63eed66d4a171118858695d3ac3e4
Richard Ulmer <ulmer@univention.de>
2016-08-31 10:50:52 +0000

    Bug #42077: Corrected some typos.
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-3@72111 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-pkgdb/umc/python/pkgdb/__init__.py
+++ services/univention-pkgdb/umc/python/pkgdb/__init__.py
@@ -171,9 +171,9 @@ LABELS = {
 	'sysversion_lower':			_("UCS Version is lower than"),
 	'all_properties':			_("All properties"),
 	# ----------- server roles --------------------
-	'domaincontroller_master':	_("Domaincontroller Master"),
-	'domaincontroller_backup':	_("Domaincontroller Backup"),
-	'domaincontroller_slave':	_("Domaincontroller Slave"),
+	'domaincontroller_master':	_("Domain controller Master"),
+	'domaincontroller_backup':	_("Domain controller Backup"),
+	'domaincontroller_slave':	_("Domain controller Slave"),
 	'memberserver':				_("Member Server"),
 	# ------------------ selection states --------------
 	'install':					_("Install"),
