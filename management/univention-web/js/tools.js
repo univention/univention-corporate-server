fcbe8694a8608fd4d06ce098ee3e5e114cfc21cf
Florian Best <best@univention.de>
2016-09-14 16:45:39 +0000

    Bug #42384: fix images
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@72604 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-web/js/tools.js
+++ management/univention-web/js/tools.js
@@ -1161,8 +1161,8 @@ define([
 				values.icon = filenameParts[1];
 
 				// search in local icons directory
-				values.url = require.toUrl('dijit/themes');
-				values.url = lang.replace('{url}/umc/icons/{dir}/{icon}.{suffix}', values);
+				values.url = require.toUrl('dijit/themes/umc/icons');
+				values.url = lang.replace('{url}/{dir}/{icon}.{suffix}', values);
 				iconClass = lang.replace('icon{s}-{icon}', values);
 			}
 
