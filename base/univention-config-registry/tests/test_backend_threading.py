3933d176a71f88d9354895baca9c229bd5fd45b3
Philipp Hahn <hahn@univention.de>
2016-09-16 13:03:27 +0000

    Bug #37402 ucr: Improve multi-threaded load
    The UCR instance is cleared at the beginning of load(). If loading the
    old file fails or is taking a long time, a concurrent access will find
    the UCR being empty.
    
    1. Make creating the file atomic; otherwise there is a (small) time-slot
    between the "os.path.exists()" test and the "open()" creating the file.
    
    2. Only read two lines instead of the full file to check for validity,
    which is significantly slower.
    
    3. Just clear() the dictionary before reading the new content.
    
    4. Add a unit test to check concurrent access.
       - UMC doesn't do the locking, but even with it it fails.
       - moving the "assertEqual()" inside the locks makes the problem go
         away, as then a concurrent "clear()" can not get in between.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@72646 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ base/univention-config-registry/tests/test_backend_threading.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+"""Unit test for univention.config_registry.backend."""
+# pylint: disable-msg=C0103,E0611,R0904
+import unittest
+import os
+import sys
+from tempfile import mkdtemp
+from shutil import rmtree
+from threading import Thread, Lock
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.path.pardir, 'python'))
+from univention.config_registry.backend import ConfigRegistry
+
+
+class DummyLock(object):
+	def __enter__(self):
+		pass
+
+	def __exit__(self, exc_type, exc_value, traceback):
+		pass
+
+
+class TestConfigRegistry(unittest.TestCase):
+	"""Unit test for univention.config_registry.backend.ConfigRegistry"""
+	def setUp(self):
+		"""Create object."""
+		self.work_dir = mkdtemp()
+		ConfigRegistry.PREFIX = self.work_dir
+
+	def tearDown(self):
+		"""Destroy object."""
+		# os.kill(os.getpid(), 19)  # signal.SIGSTOP
+		rmtree(self.work_dir)
+
+	def test_threading(self):
+		"""Multiple threads accessing same registry."""
+		DO_LOCKING = True
+		THREADS = 10
+		ITERATIONS = 1000
+		BASE, PRIME = 7, 23
+		KEY = 'x' * PRIME
+
+		SKEY, SVALUE = 'always', 'there'
+		ucr = ConfigRegistry()
+		ucr[SKEY] = SVALUE
+		ucr.save()
+
+		lock = Lock() if DO_LOCKING else DummyLock()
+
+		def run(tid):
+			for iteration in xrange(ITERATIONS):
+				i = tid + iteration
+				random = pow(BASE, i, PRIME)
+				key = KEY[:random + 1]
+
+				with lock:
+					ucr.load()
+				self.assertEqual(ucr[SKEY], SVALUE, 'tid=%d iter=%d %r' % (tid, iteration, ucr.items()))
+
+				try:
+					del ucr[key]
+				except LookupError:
+					ucr[key] = '%d %d' % (tid, iteration)
+				if i % 10 == 0 and tid % 10 == 0:
+					with lock:
+						ucr.save()
+
+		threads = []
+		for tid in xrange(THREADS):
+			thread = Thread(target=run, name='%d' % tid, args=(tid,))
+			threads.append(thread)
+		for thread in threads:
+			thread.start()
+		for thread in threads:
+			thread.join()
+
+
+if __name__ == '__main__':
+	unittest.main()
