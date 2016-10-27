e41b106484a7097e3a1d336b41c3da932e902c78
Florian Best <best@univention.de>
2016-10-27 15:33:36 +0000

    Bug #31771: autopep8: univention-heimdal
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-3@73713 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-heimdal/keytab-member.py
+++ base/univention-heimdal/keytab-member.py
@@ -29,20 +29,24 @@
 # License with the Debian GNU/Linux or Univention distribution in file
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
-__package__=''  # workaround for PEP 366
-import listener, os, pwd, types
+__package__ = ''  # workaround for PEP 366
+import listener
+import os
+import pwd
 import univention.debug
 import univention.config_registry
 
-name='keytab-member'
-description='Kerberos 5 keytab maintainance for memberserver'
-filter='(&(objectClass=krb5Principal)(objectClass=krb5KDCEntry)(krb5KeyVersionNumber=*)(objectClass=univentionMemberServer))'
+name = 'keytab-member'
+description = 'Kerberos 5 keytab maintainance for memberserver'
+filter = '(&(objectClass=krb5Principal)(objectClass=krb5KDCEntry)(krb5KeyVersionNumber=*)(objectClass=univentionMemberServer))'
 
 etypes = ['des-cbc-crc', 'des-cbc-md4', 'des3-cbc-sha1', 'des-cbc-md5', 'arcfour-hmac-md5']
 listener.setuid(0)
 
+
 def clean():
 	pass
+
 
 def handler(dn, new, old):
 	global keytab
@@ -52,7 +56,7 @@ def handler(dn, new, old):
 
 	server_role = configRegistry['server/role']
 	if server_role == 'domaincontroller_master':
-			
+
 		if not new.get('krb5Key'):
 			return
 
@@ -60,19 +64,18 @@ def handler(dn, new, old):
 		try:
 			if old:
 				try:
-					os.unlink('/var/lib/univention-heimdal/%s' %old['cn'][0])
+					os.unlink('/var/lib/univention-heimdal/%s' % old['cn'][0])
 				except:
 					pass
 			if new:
-				#FIXME: otherwise the keytab entry is duplicated
+				# FIXME: otherwise the keytab entry is duplicated
 				os.spawnv(os.P_WAIT, '/usr/sbin/kadmin', ['kadmin', '-l', 'ext', '--keytab=/var/lib/univention-heimdal/%s' % new['cn'][0], new['krb5PrincipalName'][0]])
 				try:
-					userID=pwd.getpwnam('%s$'%new['cn'][0])[2]
-					os.chown('/var/lib/univention-heimdal/%s' %new['cn'][0], userID, 0)
-					os.chmod('/var/lib/univention-heimdal/%s' %new['cn'][0],0660)
+					userID = pwd.getpwnam('%s$' % new['cn'][0])[2]
+					os.chown('/var/lib/univention-heimdal/%s' % new['cn'][0], userID, 0)
+					os.chmod('/var/lib/univention-heimdal/%s' % new['cn'][0], 0o660)
 				except:
 					pass
-
 
 		finally:
 			listener.unsetuid()
