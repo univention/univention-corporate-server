b8d9a1d328d27ce1886c423c730682b21f5a4d39
Florian Best <best@univention.de>
2016-11-01 12:25:16 +0000

    Bug #26133: don't display container widget if no container is selectable when adding objects
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-4@73969 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console-module-udm/umc/js/udm/wizards/FirstPageWizard.js
+++ management/univention-management-console-module-udm/umc/js/udm/wizards/FirstPageWizard.js
@@ -300,7 +300,16 @@ define([
 					label: _('Container'),
 					description: _('The container in which the LDAP object shall be created.'),
 					autoHide: true,
-					staticValues: containers,
+					depends: superordinates.length ? 'superordinate' : undefined,
+					dynamicValues: function(options) {
+						var deferred = new Deferred();
+						var values = [];
+						if (!options.superordinate || options.superordinate == 'None') {
+							values = containers;
+						}
+						deferred.resolve(values);
+						return deferred;
+					},
 					size: 'Two'
 				});
 				layout.push('container');
@@ -331,7 +340,8 @@ define([
 						depends: 'superordinate',
 						size: 'Two'
 					});
-					layout.push('superordinate', 'objectType');
+					layout.unshift('superordinate');
+					layout.push('objectType');
 				} else {
 					// no superordinates
 					// object types
