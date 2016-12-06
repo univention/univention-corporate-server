147207144e9e6c67d05dc6ba79e00ee72d2bb635
Eduard Mai <mai@univention.de>
2016-12-06 13:20:01 +0000

    Bug #42980: fix wrong error pop-up when clicking a link on the overview
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-4@75030 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console-frontend/umc/app.js
+++ management/univention-management-console-frontend/umc/app.js
@@ -2164,7 +2164,9 @@ define([
 					label: _('Open module'),
 					isDefaultAction: true,
 					callback: lang.hitch(this, function(id, item) {
-						this.openModule(item);
+						if (!item.is_link) {
+							this.openModule(item);
+						}
 						//this._tabContainer.transition(id, item);
 					})
 				}, {
