898fec9e631b19566385b5493149b551db0e92b9
Johannes Keiser <keiser@univention.de>
2016-12-21 18:16:46 +0000

    * Bug #43026: Show a warning when current UCS version is out of maintenance
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-4@75496 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-updater/umc/js/updater.js
+++ base/univention-updater/umc/js/updater.js
@@ -62,8 +62,19 @@ define([
 				}
 			});
 		};
+		var checkOutOfMaintenance = function() {
+			tools.umcpCommand('updater/maintenance_information').then(function(data) {
+				var info = data.result;
+				if ((info.maintained === 'extended' && !info.hasExtendedMaintenance) || info.maintained === 'false') {
+					var link = tools.linkToModule({module: 'updater'});
+					var warning = _("<b style='color: #ff7070'>Warning:</b> The currently used UCS version is out of maintenance. Plase visit the %s for more information", link);
+					dialog.warn(warning);
+				}
+			});
+		};
 		checkUpdateIsRunning();
 		checkUpdateAvailable();
+		checkOutOfMaintenance();
 	});
 
 	return declare("umc.modules.updater", Module, {
