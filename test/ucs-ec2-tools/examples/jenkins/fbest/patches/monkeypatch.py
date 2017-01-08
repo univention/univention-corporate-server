0c45c801cf465f583152d9c6e63466046bdc8aac
Florian Best <best@univention.de>
2017-01-08 21:52:16 +0000

    Bug #37102: add monkeypatching
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-4@75631 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ test/ucs-ec2-tools/examples/jenkins/fbest/patches/monkeypatch.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+import ast
+import glob
+from subprocess import call
+
+
+def main():
+	services = []
+	for patch in glob.glob('patches/*.patch'):
+		with open(patch) as fd:
+			try:
+				params = ast.literal_eval(fd.readline().strip())
+				assert isinstance(params, dict)
+			except (SyntaxError, ValueError, AssertionError):
+				params = {}
+		call(['patch', '-p%s' % params.get('p', 0), '-d', params.get('d', '/'), '-i', patch])
+		services.extend(params.get('restart_service', []))
+
+	for service in set(services):
+		call(['invoke-rc.d', service, 'restart'])
+
+
+if __name__ == '__main__':
+	main()
