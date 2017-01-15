5152295d26e95466565647b910362563d394c8e8
Philipp Hahn <hahn@univention.de>
2017-01-15 07:13:21 +0000

    Bug #39269 test/dns: Fix DNS query tests
    Fix busy loop.
    Remove exception wrapping, which only added debug info.
    Use ldap/base directly.
    Remove broken hand-rolled UDP-only resolver.
    Simplify iterative and redundant assignment of zoneName.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@75797 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/67_udm-dns/essential/dns_helper.py
+++ test/ucs-test/tests/67_udm-dns/essential/dns_helper.py
@@ -28,108 +28,30 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
-import dns.resolver as resolver
-import dns.query as query_udp
-import dns.message as message
-import dns.rdatatype
 import time
 
+import dns.resolver as resolver
 from dns.exception import Timeout
-from dns.exception import DNSException
-from dns.exception import FormError
 from dns.resolver import NXDOMAIN
-from dns.resolver import NoAnswer
-from dns.resolver import NoNameservers
 
 
-def resolveDnsEntry(duration, zoneName, resourceRecord, resourceRecord_Number):
-
+def resolveDnsEntry(zoneName, resourceRecord, timeout=120, tries=3):
 	start = time.time()
-	timeout = 3
-	flag = False
 
-	while flag == False:
+	while True:
 		try:
-			wait = time.time()
-			wait_flag = False
-			while wait_flag == False:
-				if(time.time() - wait > 1):
-					wait_flag = True
-
-			answer = resolver.query(zoneName, resourceRecord)
-			flag = True
-			return answer
-
+			answers = resolver.query(zoneName, resourceRecord)
+			return answers
 		except Timeout:
-			timeout = timeout - 1
-			if timeout < 1:
-				raise Timeout(
-					'no answers could be found in the specified lifetime '
-					+ 'NameToResolve: '
-					+ zoneName
-					+ ' ResourceRecord: '
-					+ resourceRecord)
+			tries -= 1
+			if tries < 0:
+				raise
 		except NXDOMAIN:
 			diff = time.time() - start
-			if(diff > duration):
-				raise NXDOMAIN(
-					'the query name does not exist '
-					+ 'NameToResolve: '
-					+ zoneName
-					+ ' ResourceRecord: '
-					+ resourceRecord
-					+ ' Time waited: '
-					+ str(diff))
-		except NoAnswer:
-			query = message.make_query(zoneName, resourceRecord_Number)
-			try:
-				answer = query_udp.udp(query, zoneName)
-				flag = True
-				return answer
-			except DNSException:
-				raise DNSException(
-					'query response comes from unexpected address or port '
-					+ 'NameToResolve: '
-					+ zoneName
-					+ ' ResourceRecord: '
-					+ resourceRecord)
-			except FormError:
-				raise FormError(
-					'query response does not respond to the question asked '
-					+ 'NameToResolve: '
-					+ zoneName
-					+ ' ResourceRecord: ' +
-					resourceRecord)
-			print 'the response did not contain an answer to the question '
-			+ 'NameToResolve: '
-			+ zoneName
-			+ ' ResourceRecord: '
-			+ resourceRecord
-		except NoNameservers:
-			raise NoNameservers(
-				'no non-broken nameservers are available to answer the query '
-				+ 'NameToResolve: '
-				+ zoneName
-				+ ' ResourceRecord: '
-				+ resourceRecord)
-		except Exception as e:
-			if e is dns.resolver.YXDOMAIN:
-				raise dns.resolver.YXDOMAIN(
-					'the query name does not exist ' + 'NameToResolve: '
-					+ zoneName +
-					' ResourceRecord: '
-					+ resourceRecord
-					+ ' Time waited: '
-					+ str(diff))
-			else:
-				raise Exception(
-					'the query name does not exist '
-					+ 'NameToResolve: '
-					+ zoneName
-					+ ' ResourceRecord: '
-					+ resourceRecord
-					+ ' Time waited: '
-					+ str(diff))
+			if diff > timeout:
+				raise
+
+		time.sleep(1)
 
 
 if __name__ == '__main__':
