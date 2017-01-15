5b10075f63f00a05668d044290db72c8e6e8640e
Philipp Hahn <hahn@univention.de>
2017-01-15 07:13:28 +0000

    Bug #43304 udm: Convert space to tabs
    Missed by automatic transformation?
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@75798 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-directory-manager-modules/modules/univention/admin/handlers/dns/__init__.py
+++ management/univention-directory-manager-modules/modules/univention/admin/handlers/dns/__init__.py
@@ -5,72 +5,72 @@ ARPA_IP6 = '.ip6.arpa'
 
 
 def makeContactPerson(obj, arg):
-    """Create contact Email-address for domain."""
-    domain = obj.position.getDomain()
-    return 'root@%s.' % (domain.replace('dc=', '').replace(',', '.'),)
+	"""Create contact Email-address for domain."""
+	domain = obj.position.getDomain()
+	return 'root@%s.' % (domain.replace('dc=', '').replace(',', '.'),)
 
 
 def unescapeSOAemail(email):
-    r"""
-    Un-escape Email-address from DNS SOA record.
-    >>> unescapeSOAemail(r'first\.last.domain.tld')
-    'first.last@domain.tld'
-    """
-    ret = ''
-    i = 0
-    while i < len(email):
-        if email[i] == '\\':
-            i += 1
-            if i >= len(email):
-                raise ValueError()
-        elif email[i] == '.':
-            i += 1
-            if i >= len(email):
-                raise ValueError()
-            ret += '@'
-            ret += email[i:]
-            return ret
-        ret += email[i]
-        i += 1
-    raise ValueError()
+	r"""
+	Un-escape Email-address from DNS SOA record.
+	>>> unescapeSOAemail(r'first\.last.domain.tld')
+	'first.last@domain.tld'
+	"""
+	ret = ''
+	i = 0
+	while i < len(email):
+		if email[i] == '\\':
+			i += 1
+			if i >= len(email):
+				raise ValueError()
+		elif email[i] == '.':
+			i += 1
+			if i >= len(email):
+				raise ValueError()
+			ret += '@'
+			ret += email[i:]
+			return ret
+		ret += email[i]
+		i += 1
+	raise ValueError()
 
 
 def escapeSOAemail(email):
-    r"""
-    Escape Email-address for DNS SOA record.
-    >>> escapeSOAemail('first.last@domain.tld')
-    'first\\.last.domain.tld'
-    """
-    SPECIAL_CHARACTERS = set('"(),.:;<>@[\\]')
-    if '@' not in email:
-        raise ValueError()
-    (local, domain) = email.rsplit('@', 1)
-    tmp = ''
-    for c in local:
-        if c in SPECIAL_CHARACTERS:
-            tmp += '\\'
-        tmp += c
-    local = tmp
-    return local + '.' + domain
+	r"""
+	Escape Email-address for DNS SOA record.
+	>>> escapeSOAemail('first.last@domain.tld')
+	'first\\.last.domain.tld'
+	"""
+	SPECIAL_CHARACTERS = set('"(),.:;<>@[\\]')
+	if '@' not in email:
+		raise ValueError()
+	(local, domain) = email.rsplit('@', 1)
+	tmp = ''
+	for c in local:
+		if c in SPECIAL_CHARACTERS:
+			tmp += '\\'
+		tmp += c
+	local = tmp
+	return local + '.' + domain
 
 
 def stripDot(old):
-    """
-    >>> stripDot(['example.com.', 'example.com'])
-    ['example.com', 'example.com']
-    >>> stripDot('example.com.')
-    'example.com'
-    >>> stripDot([])
-    []
-    >>> stripDot('')
-    ''
-    >>> stripDot(None)
-    """
-    if isinstance(old, list):
-        return [stripDot(_) for _ in old]
-    return old[:-1] if isinstance(old, basestring) and old.endswith('.') else old
+	"""
+	>>> stripDot(['example.com.', 'example.com'])
+	['example.com', 'example.com']
+	>>> stripDot('example.com.')
+	'example.com'
+	>>> stripDot([])
+	[]
+	>>> stripDot('')
+	''
+	>>> stripDot(None)
+	"""
+	if isinstance(old, list):
+		return [stripDot(_) for _ in old]
+	return old[:-1] if isinstance(old, basestring) and old.endswith('.') else old
 
 
 if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
+	import doctest
+	doctest.testmod()
