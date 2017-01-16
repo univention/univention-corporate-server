c1ac58df51d634879951040f910eab4d076bd625
Florian Best <best@univention.de>
2017-01-16 17:55:14 +0000

    Bug #43348: remove own implementation of wait_for_drs_replication
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@75836 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/60_umc-system/TestUMCSystemModule.py
+++ test/ucs-test/tests/60_umc-system/TestUMCSystemModule.py
@@ -5,7 +5,6 @@ import shutil
 import json
 from time import sleep
 from httplib import HTTPException
-from subprocess import Popen, PIPE
 
 from univention.config_registry import ConfigRegistry
 from univention.testing.codes import TestCodes
@@ -231,19 +230,6 @@ class TestUMCSystem(object):
 			"options": {"container": "cn=computers," + self.ldap_base, "objectType": "computers/windows"}
 		}]
 		return self.make_udm_request("add", options, "computers/computer")
-
-	def wait_for_samba_replication(self, username):
-		"""
-		Runs a helper shell script with a given 'username' to wait for
-		DRS replication. (Timeout can be found in lib/samba.sh).
-		"""
-		# to avoid the need for execute permissions
-		proc = Popen((os.getenv('SHELL', '/bin/bash'), "samba_repl.sh", username), stdout=PIPE, stderr=PIPE)
-		stdout, stderr = proc.communicate()
-		if stderr:
-			utils.fail("\nWaiting for Samba replication failed, in stderr:\n'%s'" % stderr)
-		if stdout:
-			print("\nWaiting for Samba replication produced the following output:\n%s" % stdout)
 
 	def get_translation(self, obj_type, obj_name):
 		"""
