c71cd453989541789c68fa1203ef094022323b72
Florian Best <best@univention.de>
2017-01-24 17:54:15 +0000

    Bug #34498: deprecate univention.lib.umc_connection
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@76067 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-lib/python/umc_connection.py
+++ base/univention-lib/python/umc_connection.py
@@ -31,82 +31,61 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
-from httplib import HTTPSConnection, HTTPException
-from json import loads, dumps
-from socket import error as SocketError
+import warnings
+
+from univention.lib.umc import Client, ConnectionError, HTTPError, Forbidden
+from httplib import HTTPException
 
 from univention.config_registry import ConfigRegistry
 ucr = ConfigRegistry()
 ucr.load()
 
+warnings.warn(
+	'univention.lib.umc_connection is deprecated, use univention.lib.umc instead!\n'
+	'- UMCConnection(host, username, password).request("udm/query", {}, flavor)\n'
+	'+ Client(hostname, username, password).umc_command("udm/query", {}, flavor).result\n'
+)
+
 
 class UMCConnection(object):
 
 	def __init__(self, host, username=None, password=None, error_handler=None):
-		self._host = host
-		self._headers = {
-			'Content-Type': 'application/json; charset=UTF-8',
-			'Accept': 'application/json; q=1, text/html; q=0.5; */*; q=0.1',
-			'X-Requested-With': 'XMLHttpRequest',
-		}
-		self._base_uri = '/univention/'
+		self.client = Client(host, username, password)
 		self._error_handler = error_handler
-		if username is not None:
-			self.auth(username, password)
+
+	build_data = Client._Client__build_data
+
+	@property
+	def _headers(self):
+		return self._client._headers
+
+	@property
+	def _host(self):
+		return self.client.hostname
 
 	def get_connection(self):
-		'''Creates a new HTTPSConnection to the host'''
-		# once keep-alive is over, the socket closes
-		#   so create a new connection on every request
-		return HTTPSConnection(self._host)
+		return self._client._get_connection()
 
 	@classmethod
 	def get_machine_connection(cls, error_handler=None):
 		'''Creates a connection with the credentials of the local host
 		to the DC Master'''
-		username = '%s$' % ucr.get('hostname')
-		password = ''
-		try:
-			with open('/etc/machine.secret') as machine_file:
-				password = machine_file.readline().strip()
-		except (OSError, IOError) as e:
-			if error_handler:
-				error_handler('Could not read /etc/machine.secret: %s' % e)
 		try:
 			connection = cls(ucr.get('ldap/master'))
-			connection.auth(username, password)
+			connection.client.authenticate_with_machine_account()
 			return connection
-		except (HTTPException, SocketError) as e:
+		except ConnectionError as exc:
 			if error_handler:
-				error_handler('Could not connect to UMC on %s: %s' % (ucr.get('ldap/master'), e))
-		return None
+				error_handler('Could not connect to UMC on %s: %s' % (ucr.get('ldap/master'), exc.reason))
 
 	def auth(self, username, password, auth_type=None):
 		'''Tries to authenticate against the host and preserves the
 		cookie. Has to be done only once (but keep in mind that the
 		session probably expires after 10 minutes of inactivity)'''
-		data = self.build_data({'username': username, 'password': password, 'auth_type': auth_type})
 		try:
-			response = None
-			response = self.__request('POST', 'auth', data, headers=self._headers)
-			cookie = response.getheader('set-cookie')
-			if cookie is None:
-				raise ValueError('No cookie')
-			self._headers['Cookie'] = cookie  # FIXME: transform Set-Cookie to Cookie
-		except Exception as exc:
-			raise
-			err = (exc if response is None else response.read())
-			if self._error_handler:
-				self._error_handler(str(exc))
-			error_message = '%s: Authentication failed: %s' % (self._host, err)
-			raise HTTPException(error_message)
-
-	def build_data(self, data, flavor=None):
-		'''Returns a dictionary as expected by the UMC Server'''
-		data = {'options': data}
-		if flavor:
-			data['flavor'] = flavor
-		return dumps(data)
+			self.client.umc_auth(username, password, auth_type=auth_type)
+		except HTTPError as exc:
+			raise HTTPException(str(exc))
 
 	def request(self, url, data=None, flavor=None, command='command'):
 		'''Sends a request and returns the data from the response. url
@@ -119,38 +98,17 @@ class UMCConnection(object):
 		'''
 		if data is None:
 			data = {}
-		data = self.build_data(data, flavor)
-		path = command
-		if url:
-			path = '%s/%s' % (command, url)
-		response = self.__request('POST', path, data, headers=self._headers)
-		if response.status != 200:
-			error_message = '%s on %s (%s): %s' % (response.status, self._host, url, response.read())
-			if response.status == 403:
-				# 403 is either command is unknown
-				#   or command is known but forbidden
-				if self._error_handler:
-					self._error_handler(error_message)
-				raise NotImplementedError('command forbidden: %s' % url)
-			raise HTTPException(error_message)
-		content = response.read()
-		content = loads(content)  # FIXME: inspect Content-Type response header
-		if isinstance(content, dict):
-			return content.get('result', content)
-		return content
-
-	def __request(self, method, path, data, headers):
-		uri = '%s%s' % (self._base_uri, path)
-		con = self.get_connection()
-		con.request(method, uri, data, headers=headers)
-		response = con.getresponse()
-		if response.status == 404:
-			if self._base_uri == '/univention/':
-				# UCS 4.0
-				self._base_uri = '/univention-management-console/'
-				return self.__request(method, path, data, headers)
-			elif self._base_uri == '/univention-management-console/':
-				# UCS 3.X
-				self._base_uri = '/umcp/'
-				return self.__request(method, path, data, headers)
-		return response
+		try:
+			if command in ('command', 'upload'):
+				response = self.client.umc_command(url, data, flavor)
+			elif command == 'get':
+				response = self.client.umc_get(url)
+			elif command == 'set':
+				response = self.client.umc_set(data)
+		except Forbidden:
+			raise NotImplementedError('command forbidden: %s' % url)
+		except HTTPError as exc:
+			if self._error_handler:
+				self._error_handler(str(exc))
+			raise HTTPException(str(exc))
+		return response.result
