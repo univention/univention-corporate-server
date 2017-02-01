ac7b35c4211f4facad686c6aa72b1eae5df92757
Janek Walkenhorst <walkenhorst@univention.de>
2017-02-01 15:56:46 +0000

    Bug #43233: Extend the user agent string with number of users and the server role
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@76307 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-updater/debian/univention-updater.cron.d
+++ base/univention-updater/debian/univention-updater.cron.d
@@ -1,3 +1,5 @@
 PATH=/usr/sbin:/usr/bin:/sbin:/bin
 
 @reboot root /usr/share/univention-updater/enable-apache2-umc
+@reboot root /usr/share/univention-updater/updater-statistics > /dev/zero
+@daily  root /usr/share/univention-updater/updater-statistics > /dev/zero
