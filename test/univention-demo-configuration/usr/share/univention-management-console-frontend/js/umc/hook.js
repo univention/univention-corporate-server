c8bbd2ed87407f74fc5ecf789e8d1f0e569bd6ad
Florian Best <best@univention.de>
2017-02-07 20:02:52 +0000

    Bug #42174: adjust login dialog path
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@76511 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/univention-demo-configuration/usr/share/univention-management-console-frontend/js/umc/hook.js
+++ test/univention-demo-configuration/usr/share/univention-management-console-frontend/js/umc/hook.js
@@ -1,10 +1,11 @@
-define(["dojo/dom", "dojo/dom-attr", "umc/dialog/LoginDialog", "dojo/_base/lang"], function (dom,  attr, LoginDialog, lang) {
+/*global define*/
+define(["dojo/dom", "dojo/dom-attr", "login/LoginDialog", "dojo/_base/lang"], function (dom,  attr, LoginDialog, lang) {
 	var _fillLoginForm = function () {
 		var node = dom.byId("umcLoginPassword");
 		attr.set(node, 'value', 'univention');
 		node = dom.byId('umcLoginUsername');
 		attr.set(node, 'value', 'Administrator');
-	}
+	};
 
 	lang.extend(LoginDialog, {
 		_resetFormOld: lang.clone(LoginDialog.prototype._resetForm),
