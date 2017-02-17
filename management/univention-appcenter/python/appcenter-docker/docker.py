7b09ac16ad66685c471e9d171b5af64bbf5958fc
Dirk Wiesenthal <wiesenthal@univention.de>
2017-02-17 17:53:08 +0000

    * Bug #43151: Create temp files in containers from outside
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-4.1-4@76814 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-appcenter/python/appcenter-docker/docker.py
+++ management/univention-appcenter/python/appcenter-docker/docker.py
@@ -37,13 +37,14 @@ import sys
 from subprocess import check_output, call, CalledProcessError
 import os
 import os.path
-import stat
 import shlex
 from json import loads
 from StringIO import StringIO
 from gzip import GzipFile
 import requests
 from hashlib import sha256
+from tempfile import NamedTemporaryFile
+from contextlib import contextmanager
 
 from univention.appcenter.utils import app_ports_with_protocol, call_process, shell_safe, _
 from univention.appcenter.log import get_base_logger
@@ -261,6 +262,21 @@ class Docker(object):
 			self.logger.fatal(str(exc))
 			raise Abort()
 
+	@contextmanager
+	def tmp_file(self):
+		path = self.path()
+		if not path:
+			yield None
+		else:
+			tmp_dir = os.path.join(path, 'tmp')
+			tmp_file = NamedTemporaryFile(dir=tmp_dir)
+			os.chmod(tmp_file.name, 0622)  # world writable for containers not using root as user
+			tmp_file.container_path = tmp_file.name[len(path) - 1:]
+			try:
+				yield tmp_file
+			finally:
+				tmp_file.close()
+
 	def execute_with_output(self, *args, **kwargs):
 		args = list(args)
 		for key, value in kwargs.iteritems():
@@ -291,7 +307,8 @@ class Docker(object):
 		except OSError:
 			pass
 		# create new env file
-		fd = os.open(env_file, os.O_RDWR | os.O_CREAT, stat.S_IRUSR)
+		fd = os.open(env_file, os.O_RDWR | os.O_CREAT)
+		os.chmod(env_file, 0400)
 		with os.fdopen(fd, 'w') as outfile:
 			# appcenter env file
 			if os.path.exists(self.app.get_cache_file('env')):
