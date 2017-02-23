aec47e22452423a3195bedb9671480cbc70d634e
Alexander Kläser <klaeser@univention.de>
2017-02-23 02:49:14 +0000

    Bug #42263: add generic menu entries
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@77023 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ management/univention-web/js/hooks/default_menu_entries.js
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2014-2017 Univention GmbH
+ *
+ * http://www.univention.de/
+ *
+ * All rights reserved.
+ *
+ * The source code of this program is made available
+ * under the terms of the GNU Affero General Public License version 3
+ * (GNU AGPL V3) as published by the Free Software Foundation.
+ *
+ * Binary versions of this program provided by Univention to you as
+ * well as other copyrighted, protected or trademarked materials like
+ * Logos, graphics, fonts, specific documentations and configurations,
+ * cryptographic keys etc. are subject to a license agreement between
+ * you and Univention and not subject to the GNU AGPL V3.
+ *
+ * In the case you use this program under the terms of the GNU AGPL V3,
+ * the program is provided in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License with the Debian GNU/Linux or Univention distribution in file
+ * /usr/share/common-licenses/AGPL-3; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/*global define*/
+
+define([
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/_base/array",
+	"umc/menu",
+	"umc/tools",
+	"umc/dialog",
+	"umc/i18n/tools",
+	"umc/i18n!umc/modules/passwordchange"
+], function(declare, lang, array, menu, tools, dialog, i18nTools, _) {
+	var setupLanguageMenu = function() {
+		menu.addSubMenu({
+			priority: 55,
+			label: _('Switch language'),
+			id: 'umcMenuLanguage',
+		});
+		array.forEach(i18nTools.availableLanguages, function(language) {
+			menu.addEntry({
+				parentMenuId: 'umcMenuLanguage',
+				label: language.label,
+				disabled: language.id === i18nTools.defaultLang(),
+				onClick: function() {
+					if (tools.status('loggedIn')) {
+						dialog.confirm(_('<b>Warning</b>: The current session with all opened modules and unsaved settings gets lost and a page reload is done when switching the language.'), [{
+							name: 'change',
+							label: _('Switch language'),
+							callback: function() {
+								i18nTools.setLanguage(language.id);
+							}
+						}, {
+							name: 'cancel',
+							label: _('Cancel')
+						}], _('Changing language'));
+						return;
+					}
+					i18nTools.setLanguage(language.id);
+				}
+			});
+		});
+	};
+
+	var setupHelpMenu = function() {
+		// the help context menu
+		menu.addSubMenu({
+			priority: 50,
+			label: _('Help'),
+			id: 'umcMenuHelp'
+		});
+
+		menu.addEntry({
+			parentMenuId: 'umcMenuHelp',
+			label: _('UCS start site'),
+			onClick: function() {
+				topic.publish('/umc/actions', 'menu-help', 'ucs-start-site');
+				var w = window.open('/univention?lang=' + kernel.locale, 'ucs-start-site');
+				w.focus();
+			}
+		});
+
+		menu.addEntry({
+			parentMenuId: 'umcMenuHelp',
+			label: _('Univention Website'),
+			onClick: function() {
+				topic.publish('/umc/actions', 'menu-help', 'website');
+				var w = window.open(_('umcUniventionUrl'), 'univention');
+				w.focus();
+			}
+		});
+	};
+
+	var setupMenus = function() {
+		// the settings context menu
+		menu.addSubMenu({
+			priority: 60,
+			label: _('User settings'),
+			id: 'umcMenuUserSettings'
+		});
+
+		// the language switch menu
+		if (i18nTools.availableLanguages.length > 1) {
+			setupLanguageMenu();
+		}
+
+		setupHelpMenu();
+
+		// the logout button
+		menu.addEntry({
+			priority: -1,
+			id: 'umcMenuLogout',
+			label: _('Logout'),
+			onClick: function() { require('login').logout(); }
+		});
+	};
+
+	setupMenus();
+});
