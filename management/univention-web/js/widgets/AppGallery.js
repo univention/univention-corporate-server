59eef3c4d691f2f6aa3f5681e4f22e263124d5ad
Johannes Keiser <keiser@univention.de>
2017-03-02 12:20:29 +0000

    * Bug #43528: Move styling and code for the gallery in the portal and appcenter into univention-web
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@77240 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ management/univention-web/js/widgets/AppGallery.js
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2017 Univention GmbH
+ *
+ * http://www.univention.de/
+ *
+ * All rights reserved.
+ *
+ * The source code of this program is made available
+ * under the terms of the GNU Affero General Public License version 3
+ * (GNU AGPL V3) as published by the Free Software Foundation.
+ *
+ * Binary versions of this program provided by Univention to you as
+ * well as other copyrighted, protected or trademarked materials like
+ * Logos, graphics, fonts, specific documentations and configurations,
+ * cryptographic keys etc. are subject to a license agreement between
+ * you and Univention and not subject to the GNU AGPL V3.
+ *
+ * In the case you use this program under the terms of the GNU AGPL V3,
+ * the program is provided in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License with the Debian GNU/Linux or Univention distribution in file
+ * /usr/share/common-licenses/AGPL-3; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/*global define, require, navigator*/
+
+define([
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/_base/event",
+	"dojo/_base/kernel",
+	"dojo/on",
+	"dojo/dom-class",
+	"dojo/dom-construct",
+	"dojo/dom-geometry",
+	"dojo/dom-style",
+	"dojo/query",
+	"dojo/mouse",
+	"dijit/Tooltip",
+	"./GalleryPane",
+	"../tools"
+], function(declare, lang, dojoEvent, kernel, on, domClass, domConstruct, domGeometry, domStyle, query, mouse, Tooltip, GalleryPane, tools) {
+	return declare("umc.widgets.AppGallery", [ GalleryPane ], {
+		region: 'main',
+
+		bootstrapClasses: '',
+
+		baseClass: 'umcAppGallery',
+
+		iconClassPrefix: 'umcAppGallery',
+
+		getIconClass: function(iconName) {
+			var iconClass = tools.getIconClass(iconName, 'scalable', this.iconClassPrefix);
+			if (iconClass) {
+				//IE specific
+				//on IE some svgs are not shown. this fixes the problem
+				if (navigator.userAgent.indexOf('Trident/') !== -1) {
+					var iconUrl = this.getIconUrl(item.logo_name);
+					domConstruct.create('img', {
+						src: iconUrl
+					});
+				}
+			}
+
+			return iconClass;
+		},
+
+		getIconUrl: function(iconName) {
+			return lang.replace('{url}/umc/icons/scalable/{icon}', {
+				url: require.toUrl('dijit/themes'),
+				icon: iconName || ''
+			});
+		},
+		
+		renderRow: function(item) {
+			var renderInfo = this.getRenderInfo(item);
+			return this.getDomForRenderRow(renderInfo);
+		},
+
+		getRenderInfo: function(item) {
+			return {
+				itemId: item.id,
+				iconClass: this.getIconClass(item.logo_name),
+				itemName: this.getItemName(item),
+				itemSubName: this.getMore(item),
+				itemHoverContent: this.getItemDescription(item),
+				itemStatusIcon: this.getStatusIconClass(item),
+				itemStatusTooltipMessage: this.getItemStatusTooltipMessage(item)
+			};
+		},
+
+		getDomForRenderRow: function(item) {
+			var domString = '' +
+				'<div class="umcGalleryWrapperItem" moduleID={itemId}>' +
+					'<div class="cornerPiece boxShadow bl">' +
+						'<div class="hoverBackground"></div>' +
+					'</div>' +
+					'<div class="cornerPiece boxShadow tr">' +
+						'<div class="hoverBackground"></div>' +
+					'</div>' +
+					'<div class="cornerPiece boxShadowCover bl"></div>' +
+					'<div class="appIcon umcGalleryIcon {iconClass}"></div>' +
+					'<div class="appInnerWrapper umcGalleryItem">' +
+						'<div class="contentWrapper">' +
+							'<div class="appContent">' +
+								'<div class="umcGalleryName">{itemName}</div>' +
+								'<div class="umcGallerySubName">{itemSubName}</div>' +
+							'</div>' +
+							'<div class="appHover">' +
+								'<div>{itemHoverContent}</div>' +
+							'</div>' +
+						'</div>' +
+					'</div>' +
+					'<div class="appStatusIcon {itemStatusIcon}"></div>' +
+					'<div class="appStatusIcon appStatusHoverIcon {itemStatusIcon}"></div>' +
+				'</div>';
+
+			var domNode = domConstruct.toDom(lang.replace(domString, item));
+
+			on(domNode, mouse.enter, function() {
+				domClass.add(this, 'hover');
+			});
+
+			on(domNode, mouse.leave, function() {
+				domClass.remove(this, 'hover');
+			});
+
+			var statusIcon = query('.appStatusHoverIcon', domNode)[0];
+			on(statusIcon, 'click', function(evt) {
+				Tooltip.show(item.itemStatusTooltipMessage, statusIcon);
+				if (evt) {
+					dojoEvent.stop(evt);
+				}
+				on.once(kernel.body(), 'click', function(evt) {
+					Tooltip.hide(statusIcon);
+					dojoEvent.stop(evt);
+				});
+			});
+
+			return domNode;
+		},
+
+		_resizeItemNames: function() {
+			query('.umcGalleryName', this.contentNode).forEach(lang.hitch(this, function(inode) {
+				var fontSize = 1.4;
+				while (domGeometry.position(inode).h > 40) {
+					domStyle.set(inode, 'font-size', fontSize + 'em');
+					fontSize *= 0.95;
+				}
+			}));
+		},
+
+		getMore: function(item) {
+			return item.vendor || item.maintainer || '';
+		},
+
+		getStatusIconClass: function(item) {
+			return 'noStatus';
+		},
+
+		getItemStatusTooltipMessage: function(item) {
+			return '';
+		}
+	});
+});
