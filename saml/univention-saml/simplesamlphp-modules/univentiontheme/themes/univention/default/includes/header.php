42638b683187109d0ae2faad3fc67aab98603e34
Alexander Kläser <klaeser@univention.de>
2017-03-07 14:56:52 +0000

    Bug #42263: Adjust menu structure
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@77435 1b283449-9f2f-0410-b571-8eb228e1a901

--- saml/univention-saml/simplesamlphp-modules/univentiontheme/themes/univention/default/includes/header.php
+++ saml/univention-saml/simplesamlphp-modules/univentiontheme/themes/univention/default/includes/header.php
@@ -102,7 +102,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	<body class="umc">
 		<div class="umcHeader">
 			<div class="umcHeaderRight">
-				<div id="umcMenu" data-dojo-type="umc/widgets/Menu"></div>
+				<div data-dojo-type="umc/menu/Button"></div>
 				<div class="univentionLogo"></div>
 			</div>
 		</div>
