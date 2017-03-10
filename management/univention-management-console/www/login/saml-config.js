87e3145f540752860e1fe023fdc28a5844a3fc2c
Florian Best <best@univention.de>
2017-03-10 12:17:38 +0000

    Bug #42174: make it possible to switch language in simplesamlphp login page
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@77571 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-management-console/www/login/saml-config.js
+++ management/univention-management-console/www/login/saml-config.js
@@ -1,10 +1,18 @@
+/*global availableLocales*/
 var umcConfig = {
 	deps: [
 		"login/dialog",
-		"dojo/dom-class"
+		"dojo/_base/array",
+		"dojo/dom-class",
+		"umc/i18n/tools"
 	],
-	callback: function(dialog, domClass) {
+	callback: function(dialog, array, domClass, i18nTools) {
 		dialog.renderLoginDialog();
 		domClass.remove(document.body, 'umcLoginLoading');
+		i18nTools.availableLanguages = availableLocales;
+		i18nTools.setLanguage = function(locale) {
+			var localelink = array.filter(availableLocales, function(lang) { return lang.id === locale; })[0].href;
+			window.location = localelink;
+		};
 	}
 };
