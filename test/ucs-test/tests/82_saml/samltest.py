905528b22423197d6171d9c2e44135d24dea3ea6
Jürn Brodersen <brodersen@univention.de>
2017-03-13 18:21:54 +0000

    * Bug #42174: adjust SAML tests to use RelayState and Referer
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@77667 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/82_saml/samltest.py
+++ test/ucs-test/tests/82_saml/samltest.py
@@ -75,7 +75,7 @@ class SamlTest(object):
 	def _request(self, method, url, status_code, data=None):
 		"""does POST or GET requests and raises SamlError which encodes the login step
 		through position parameter."""
-		headers = {'Accept-Language': 'en-US;q=0.6,en;q=0.4'}
+		headers = {'Accept-Language': 'en-US;q=0.6,en;q=0.4', 'Referer': ''}
 		_requests = {
 			'GET': self.session.get,
 			'POST': self.session.post}
@@ -91,13 +91,24 @@ class SamlTest(object):
 
 	def _login_at_idp_with_credentials(self):
 		"""Send form with login data"""
-		auth_state = self._extract_auth_state_from()
+		auth_state = self._extract_auth_state()
 		self.position = "posting login form"
 		print("Post SAML login form to: %s" % self.page.url)
 		data = {'username': self.username, 'password': self.password, 'AuthState': auth_state}
 		self._request('POST', self.page.url, 200, data=data)
 
-	def _extract_saml_msg_from(self):
+	def _extract_relay_state(self):
+		print("Extract relay state from SAML response")
+		try:
+			relay_state = re.search('name="RelayState" value="([^"]+)"', bytes(self.page.text)).group(1)
+			relay_state = html.unescape(relay_state)
+			print("The relay state is:\n%s" % relay_state)
+			return relay_state
+		except AttributeError:
+			print("No relay state found")
+			raise SamlLoginError(self.page)
+
+	def _extract_saml_msg(self):
 		print("Extract SAML message from SAML response")
 		try:
 			saml_message = re.search('name="SAMLResponse" value="([^"]+)"', bytes(self.page.text)).group(1)
@@ -108,7 +119,7 @@ class SamlTest(object):
 			print("No SAML message found")
 			raise SamlLoginError(self.page)
 
-	def _extract_sp_url_from(self):
+	def _extract_sp_url(self):
 		print("Extract url to post SAML message to")
 		try:
 			url = re.search('method="post" action="([^"]+)"', bytes(self.page.text)).group(1)
@@ -119,7 +130,7 @@ class SamlTest(object):
 			print("No url to post SAML message to found")
 			raise SamlLoginError(self.page)
 
-	def _extract_auth_state_from(self):
+	def _extract_auth_state(self):
 		print("Extract AuthState")
 		try:
 			auth_state = re.search('name="AuthState" value="([^"]+)"', bytes(self.page.text)).group(1)
@@ -129,11 +140,11 @@ class SamlTest(object):
 		except AttributeError:
 			raise SamlError("No AuthState field found.\nSAML response:\n%s" % self.page.text)
 
-	def _send_saml_response_to_sp(self, url, saml_msg):
+	def _send_saml_response_to_sp(self, url, saml_msg, relay_state):
 		# POST the SAML message to SP, thus logging in.
 		print("POST SAML message to: %s" % url)
 		self.position = "posting SAML message"
-		self._request('POST', url, 200, data={'SAMLResponse': saml_msg})
+		self._request('POST', url, 200, data={'SAMLResponse': saml_msg, 'RelayState': relay_state})
 
 	def test_login(self):
 		"""Test login on umc"""
@@ -174,9 +185,10 @@ class SamlTest(object):
 		self.position = "requesting SAML message"
 		self._request('GET', url, 200)
 		print('SAML message recieved from %s' % self.page.url)
-		url = self._extract_sp_url_from()
-		saml_msg = self._extract_saml_msg_from()
-		self._send_saml_response_to_sp(url, saml_msg)
+		url = self._extract_sp_url()
+		saml_msg = self._extract_saml_msg()
+		relay_state = self._extract_relay_state()
+		self._send_saml_response_to_sp(url, saml_msg, relay_state)
 
 	def login_with_new_session_at_IdP(self):
 		"""Use Identity Provider to log in to a Service Provider.
@@ -191,9 +203,10 @@ class SamlTest(object):
 
 		# Login at IdP. IdP answers with SAML message and url to SP in body
 		self._login_at_idp_with_credentials()
-		url = self._extract_sp_url_from()
-		saml_msg = self._extract_saml_msg_from()
-		self._send_saml_response_to_sp(url, saml_msg)
+		url = self._extract_sp_url()
+		saml_msg = self._extract_saml_msg()
+		relay_state = self._extract_relay_state()
+		self._send_saml_response_to_sp(url, saml_msg, relay_state)
 
 	def logout_at_IdP(self):
 		"""Logout from session"""
