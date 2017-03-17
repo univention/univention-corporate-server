a910f5d57b0c46bd995dc114e539e829830816a1
Richard Ulmer <ulmer@univention.de>
2017-03-17 12:23:00 +0000

    Bug #42232: Migrated EC2 start site
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@77886 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ base/univention-system-setup/usr/share/univention-system-setup-boot/www/main.js
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2017 Univention GmbH
+ *
+ * http://www.univention.de/
+ *
+ * All rights reserved.
+ *
+ * The source code of this program is made available
+ * under the terms of the GNU Affero General Public License version 3
+ * (GNU AGPL V3) as published by the Free Software Foundation.
+ *
+ * Binary versions of this program provided by Univention to you as
+ * well as other copyrighted, protected or trademarked materials like
+ * Logos, graphics, fonts, specific documentations and configurations,
+ * cryptographic keys etc. are subject to a license agreement between
+ * you and Univention and not subject to the GNU AGPL V3.
+ *
+ * In the case you use this program under the terms of the GNU AGPL V3,
+ * the program is provided in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License with the Debian GNU/Linux or Univention distribution in file
+ * /usr/share/common-licenses/AGPL-3; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+/*global define*/
+define([
+	"dojo/_base/declare",
+	"dojo/dom",
+	"dojox/html/entities",
+	"umc/json!/univention/meta.json",
+	"umc/i18n!initialsetup"
+], function(declare, dom, entities, meta, _) {
+	return {
+		start: function() {
+			this.initLabels();
+		},
+
+		initLabels: function() {
+			var title = _("Welcome to UCS initial configuration");
+			window.document.title = title;
+
+			var heading = entities.encode(title);
+			dom.byId('heading').innerHTML = heading;
+
+			var contentP1 = "<p>" + _("Before starting the setup wizard, an initial password for the <i>root</i> user must be set.") + "</p>";
+			var contentP2 = "<p>" +
+				_("Connect to this server instance as <i>root</i> via ssh and your selected private ssh key. ") +
+				_("Set a password with <i>passwd</i>, e.g., by issuing the following command:") +
+				"<pre>" +
+					_("ssh -ti &lt;path/to/privatekey&gt; root@%(serveraddress)s passwd", {
+						serveraddress: meta.serveraddress
+					}) +
+				"</pre></p>";
+			var contentP3 = "<p>" + _("If a password for <i>root</i> has been set, <a href='/univention/setup/?username=root'>start the setup wizard</a>.") + "</p>";
+			var content = contentP1 + contentP2 + contentP3;
+			dom.byId('content').innerHTML = content;
+		}
+	};
+});
