63f3e2d6f056221d785eaf7797037e2b322bb4f1
Florian Best <best@univention.de>
2017-03-17 15:44:33 +0000

    Bug #43862: fix error message strings
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@77916 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/60_umc/103_test_umc_server_down.py
+++ test/ucs-test/tests/60_umc/103_test_umc_server_down.py
@@ -11,35 +11,36 @@ import subprocess
 
 class Test_ServerDown_Messages(object):
 
-	def test_umc_webserver_down(Client, ServiceUnavailable):
+	def test_umc_webserver_down(self, Client, ServiceUnavailable):
 		try:
 			subprocess.call(['service', 'univention-management-console-web-server', 'stop'])
 			with pytest.raises(ServiceUnavailable) as exc:
 				Client().umc_get('modules')
-			assert json.loads(exc.response.body)['message'] == 'The Univention Management Console Web Server could not be reached. Please restart it or try again later.'
+			assert json.loads(exc.value.response.body)['message'] == 'The Univention Management Console Web Server could not be reached. Please restart it or try again later.'
 		finally:
 			subprocess.call(['service', 'univention-management-console-web-server', 'start'])
 
-	def test_umc_server_down(Client, ServiceUnavailable):
+	def test_umc_server_down(self, Client, ServiceUnavailable):
 		try:
 			subprocess.call(['service', 'univention-management-console-server', 'stop'])
 			with pytest.raises(ServiceUnavailable) as exc:
 				Client().umc_get('modules')
-			assert exc.message.splitlines() == [
-				'The connection to the Univention Management Console Server broke up unexpectedly. ',
-				'If you have root permissions on the system you can restart UMC by executing the following commands:',
+			assert exc.value.message.splitlines() == [
+				'The Univention Management Console Server is currently not running. ',
+				'If you have root permissions on the system you can restart it by executing the following command:',
 				' * service univention-management-console-server restart',
-				' * service univention-management-console-web-server restart',
+				'The following logfile may contain information why the server is not running:',
+				' * /var/log/univention/management-console-server.log',
 				'Otherwise please contact an administrator or try again later.',
 			]
 		finally:
 			subprocess.call(['service', 'univention-management-console-server', 'start'])
 
-	def test_apache_down(Client, ConnectionError):
+	def test_apache_down(self, Client, ConnectionError):
 		try:
 			subprocess.call(['service', 'apache2', 'stop'])
 			with pytest.raises(ConnectionError) as exc:
 				Client().umc_get('modules')
-			assert isinstance(exc.reason, socket.error)
+			assert isinstance(exc.value.reason, socket.error)
 		finally:
 			subprocess.call(['service', 'apache2', 'start'])
