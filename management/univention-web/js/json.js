4862cab6124f2ba3d8d070b28920cefb4cb9c90f
Alexander Kläser <klaeser@univention.de>
2017-03-28 12:29:39 +0000

    Bug #43977: Use dojo/request/xhr in umc/json to handle errors gracefully
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@78422 1b283449-9f2f-0410-b571-8eb228e1a901

--- management/univention-web/js/json.js
+++ management/univention-web/js/json.js
@@ -29,16 +29,48 @@
 /*global define*/
 
 define([
-	"dojo/text",
 	"dojo/_base/lang",
+	"dojo/request/xhr",
+	"dojo/io-query",
 	"dojo/json"
-], function(text, lang, json) {
-	return lang.delegate(text, {
+], function(lang, xhr, ioQuery, json) {
+	return {
 		load: function(id, require, load) {
-			text.load(id, require, function(data) {
-				load(json.parse(data));
+			// id: String
+			//		Path to the resource
+			// require: Function
+			//		Object that include the function toUrl with given id returns a valid URL from which to load the text.
+			// load: Function
+			//		Callback function which will be called, when the loading finished.
+
+			// id is something like (path is always absolute):
+			//   "path/to/data.json"
+			//   "path/to/data.json!timeout=500&"
+			// The parameters after "!" are passed over to xhr.get().
+
+			var parts = id.split("!");
+			var url = require.toUrl(parts[0]);
+
+			var _getCustomParams = function() {
+				var hasFlags = parts.length > 1;
+				if (hasFlags) {
+					return ioQuery.queryToObject(parts[1]);
+				}
+				return {};
+			};
+
+			var params = lang.mixin({
+				handleAs: 'json',
+				timeout: 10000
+			}, _getCustomParams());
+
+			xhr.get(url, params).then(function(data) {
+				load(data);
+			}, function(err) {
+				console.error(lang.replace('Could not load JSON data from {0}: {1}', [ id, err ]));
+				load({});
 			});
 		}
-	});
+	};
 });
 
