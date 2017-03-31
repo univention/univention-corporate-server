13c396a2b3c517ac623567770badff4fe14914fb
Alexander Kläser <klaeser@univention.de>
2017-03-31 09:43:57 +0000

    Bug #43520: Remove block for latest erratum of univention-updater
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@78540 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-updater/script/preup.sh
+++ base/univention-updater/script/preup.sh
@@ -476,22 +476,6 @@ check_qemu () {
 }
 check_qemu
 
-check_latest_updater_installed () {
-	local updater_version=$(dpkg -s univention-updater 2>/dev/null | sed -n 's/Version: //p')
-	if ! is_ucr_true update42/skip/updater/check; then
-		if dpkg --compare-versions "$updater_version" lt "11.0.11-23"; then
-			echo
-			echo "ERROR: Please install the latest errata updates on this machine before "
-			echo "       upgrading this system to UCS 4.2-0."
-			echo
-			echo "This check can be skipped by setting the UCR"
-			echo "variable update42/skip/updater/check to yes."
-			exit 1
-		fi
-	fi
-}
-check_latest_updater_installed
-
 # ensure that en_US is included in list of available locales (Bug #44150)
 available_locales="$(/usr/sbin/univention-config-registry get locale)"
 case "$available_locales" in
