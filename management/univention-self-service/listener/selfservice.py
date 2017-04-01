2e719eafd1b0afd6b00178264ef760ff03ffba74
Stefan Gohmann <gohmann@univention.de>
2017-04-01 19:50:06 +0000

    * Added a simple listener module which sets the UCR variable umc/self-   service/passwordreset/email/webserver_address if it is empty   (Bug #44112)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@78586 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ management/univention-self-service/listener/selfservice.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# Univention LDAP
+"""listener script to set umc/self-service/passwordreset/email/webserver_address."""
+#
+# Copyright 2017 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
+__package__ = ''  # workaround for PEP 366
+import listener
+import univention.debug as ud
+import univention.config_registry
+import os
+import subprocess
+
+name = 'selfservice'
+description = 'Set umc/self-service/passwordreset/email/webserver_address.'
+filter = '(univentionService=univention-self-service)'
+attributes = []
+
+UCRV = 'umc/self-service/passwordreset/email/webserver_address'
+
+def handler(dn, new, old):
+	if new and not old:
+		ucr = univention.config_registry.ConfigRegistry()
+		ucr.load()
+		if not ucr.get(UCRV):
+			fqdn = '%s.%s' % (new['cn'][0], new.get('associatedDomain')[0])
+			listener.setuid(0)
+			try:
+				univention.config_registry.handler_set(['%s=%s' % (UCRV, fqdn)])
+			finally:
+				listener.unsetuid()
