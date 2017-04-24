e63588dec8b1dd7d0148541ab7892c7c46d39edd
Arvid Requate <requate@univention.de>
2017-04-24 17:42:24 +0000

    * Bug #31311: Don't append a trailing dot to the cname record value
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@78902 1b283449-9f2f-0410-b571-8eb228e1a901

--- services/univention-s4-connector/modules/univention/s4connector/s4/dns.py
+++ services/univention-s4-connector/modules/univention/s4connector/s4/dns.py
@@ -655,7 +655,7 @@ def __unpack_cName(object):
 		dnsRecord = dnsRecord.encode('latin1')
 		ndrRecord = ndr_unpack(dnsp.DnssrvRpcRecord, dnsRecord)
 		if ndrRecord.wType == dnsp.DNS_TYPE_CNAME:
-			c.append(__append_dot(ndrRecord.data))
+			c.append(ndrRecord.data)
 	return c
 
 
