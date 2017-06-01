d11d67cbbca9a792a84002af569b818dedab3602
Richard Ulmer <ulmer@univention.de>
2017-06-01 08:30:32 +0000

    Bug #44640: Added script for adapting the screenshots to the needs of the manual.
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@79972 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ test/ucs-test/tests/90_umc-screenshots/change_names_and_add_shadows.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+LANGUAGE="en"
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+case $key in
+	-l|--language)
+		LANGUAGE="$2"
+		shift # past argument
+		;;
+	*)
+		# unknown option
+		;;
+esac
+shift # past argument or value
+done
+
+function adapt_for_manual {
+	SHADOW=NO
+	OUTFILE=default_outfile
+
+	while [[ $# -gt 0 ]]
+	do
+	key="$1"
+	case $key in
+		-s|--shadow)
+			SHADOW=YES
+			;;
+		-i|--input-filename)
+			INFILE="$2"
+			shift # past argument
+			;;
+		-o|--output-filename)
+			OUTFILE="$2"
+			shift # past argument
+			;;
+		*)
+			# unknown option
+			;;
+	esac
+	shift # past argument or value
+	done
+
+	if [ "$SHADOW" = "YES" ]; then
+		convert "$INFILE" \( +clone -background black -shadow 40x5+0+0 \) +swap -background white -layers merge +repage "$OUTFILE"
+	else
+		cp "$INFILE" "$OUTFILE"
+	fi
+}
+
+mkdir manual
+adapt_for_manual -i "portal_$LANGUAGE.png" -o "manual/portal_$LANGUAGE.png"
+adapt_for_manual -i "umc-favorites_$LANGUAGE.png" -o "manual/umc-favorites-tab_$LANGUAGE.png" -s
+adapt_for_manual -i "umc-login_cropped_$LANGUAGE.png" -o "manual/umc_login_$LANGUAGE.png" -s
