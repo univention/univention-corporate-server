7abdc45c1c4c907bc77f29f4b8727206349604a0
Florian Best <best@univention.de>
2017-07-21 12:49:34 +0000

    Bug #44382: add temporary debug level of slapd
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-1@81308 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-ec2-tools/examples/jenkins/utils/utils.sh
+++ test/ucs-ec2-tools/examples/jenkins/utils/utils.sh
@@ -421,6 +421,9 @@ run_appcenter_uninstall_tests ()
 
 run_admember_tests ()
 {
+	# temporary debug, Bug #44382
+	ucr set ldap/debug/level='stats'
+	service slapd restart
 	run_tests -p skip_admember -p docker "$@"
 }
 
