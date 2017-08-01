72584dd8d0e9f2d2730fba5031a6a564b1eecfda
Lukas Oyen <oyen@univention.de>
2017-08-01 14:10:36 +0000

    Bug #40605: umc-diagnostic: new check samba_tool_dbcheck.py
    This is Julius Hinrichs version with little cleanup
    (https://forge.univention.org/bugzilla/attachment.cgi?id=8058).
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-1@81631 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ management/univention-management-console-module-diagnostic/umc/python/diagnostic/plugins/samba_tool_dbcheck.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python2.7
+# coding: utf-8
+#
+# Univention Management Console module:
+#  System Diagnosis UMC module
+#
+# Copyright 2016-2017 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
+from subprocess import Popen, PIPE, STDOUT
+
+from univention.management.console.modules.diagnostic import Critical
+
+from univention.lib.i18n import Translation
+_ = Translation('univention-management-console-module-diagnostic').translate
+
+title = _('Check local AD database for errors')
+description = _('No errors found.'),
+
+
+def run():
+	process = Popen(['samba-tool', 'dbcheck'], stdout=PIPE, stderr=STDOUT)
+	stdout, stderr = process.communicate()
+	if process.returncode:
+		raise Critical('\n'.join([description, '', stdout]))
+
+
+if __name__ == '__main__':
+	from univention.management.console.modules.diagnostic import main
+	main()
