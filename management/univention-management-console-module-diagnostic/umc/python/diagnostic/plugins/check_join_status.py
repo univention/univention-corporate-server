36853333342e35f87e76d6254b15952b8c369989
Lukas Oyen <oyen@univention.de>
2017-08-01 14:10:22 +0000

    Bug #36748: umc-diagnostic: new check check_join_status
    This is the original check by Julian Hupertz, written as part of his masters
    thesis.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-1@81602 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ management/univention-management-console-module-diagnostic/umc/python/diagnostic/plugins/check_join_status.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python2.7
+# coding: utf-8
+#
+# Univention Management Console module:
+#  System Diagnosis UMC module
+#
+# Copyright 2016 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
+from subprocess import Popen, PIPE, STDOUT
+from univention.management.console.modules.diagnostic import Critical
+
+from univention.lib.i18n import Translation
+_ = Translation('univention-management-console-module-diagnostic').translate
+
+title = _('Check join status')
+description = _('The check for the join status was succsesful.')
+
+
+def run():
+	try:
+		process = Popen(['univention-check-join-status'], stdout=PIPE, stderr=STDOUT)
+		if process.returncode:
+			description = _('Call of "univention-check-join-status failed')
+			raise Critical('\n'.join([
+				description,
+				"Returncode of process: %s" % (process.returncode)
+			]))
+		stdout, stderr = process.communicate()
+		if "Joined successfully" not in stdout:
+			description = _('"univention-check-join-status" returned a problem with domain join.')
+			raise Critical('\n'.join([
+				description,
+				"stdout: %s" % (stdout),
+				"stderr: %s" % (stderr)
+			]))
+	except Critical:
+		raise
+	except Exception as ex:
+		description = _('Problem with plugin check_join_status.py.')
+		raise Critical('\n'.join([
+			description,
+			"Exception-Type: %s" % (ex.__class__),
+			"Exception-Message: %s" % (ex.message)
+		]))
+
+if __name__ == '__main__':
+	from univention.management.console.modules.diagnostic import main
+	main()
