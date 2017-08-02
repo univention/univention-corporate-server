eda722a49cef01200f90fdb65abdcf973cdacb4f
Lukas Oyen <oyen@univention.de>
2017-08-02 15:56:16 +0000

    Bug #38554: u-lib: add backup.sh: clean_old_backups
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-1@81721 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ base/univention-lib/shell/backup.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+# Univention Common Shell Library
+#
+# Copyright 2017 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
+# Clean old backups in /var/univention-backup/ that are older than
+# backup/clean/max_age, if more than backup/clean/min_backups files exist.
+# 1. parameter: a pattern to match files to delete via `find .. -regex ..`
+# 2. parameter: override backup/clean/max_age (optional)
+#
+# Example to cleanup LDAP-backups:
+# clean_old_backups 'ldap-backup_*.\(log\|ldif\)'
+clean_old_backups () {
+	arg_pattern="$1"
+	arg_max_age="$2"
+	[ -z "$arg_pattern" ] && return 1
+	eval "$(univention-config-registry shell backup/clean/.*)"
+
+	backup_dir="/var/univention-backup"
+	pattern="$backup_dir/$arg_pattern"
+	max_age="${arg_max_age:-$backup_clean_max_age}"
+
+	if [ -n "$max_age" ]; then
+		echo find "$backup_dir" -type f -mtime "+$max_age" -regex "$pattern"
+		count=$(find "$backup_dir" -type f -mtime "+$max_age" -regex "$pattern" | wc -l)
+		if [ "$count" -ge "${backup_clean_min_backups:-10}" ]; then
+				find "$backup_dir" -type f -mtime +"$backup_clean_max_age" -regex "$pattern" -delete
+		fi
+	fi
+}
