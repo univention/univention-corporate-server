501a63b008027fa04c023c431fed33c06802fff8
Lukas Oyen <oyen@univention.de>
2017-08-07 07:01:18 +0000

    Bug #44374: revert "Bug #43598: s4c-test: skip complex groups/users tests"
    This reverts commit r79205.
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-1@81813 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/52_s4connector/501test_group_sync.py
+++ test/ucs-test/tests/52_s4connector/501test_group_sync.py
@@ -185,8 +185,7 @@ def pytest_generate_tests(metafunc):
 				yield (udm_group(), None, "sync")
 				yield (udm_group(), None, "read")
 
-		# XXX Special-Groups are skipped until bug #44374 is fixed.
-		for nested_group in [utf8_group]: #, special_group]:
+		for nested_group in [utf8_group, special_group]:
 			if "udm_to_s4" in metafunc.function.__name__:
 				def udm_member_factory(*args, **kwargs):
 					return udm_group_factory(nested_group(), *args, **kwargs)
@@ -200,8 +199,7 @@ def pytest_generate_tests(metafunc):
 				yield (special_group(), s4_member_factory, "sync")
 				yield (special_group(), s4_member_factory, "read")
 
-		# XXX Special-Groups are skipped until bug #44374 is fixed.
-		for nested_user in [utf8_user]: #, special_user]:
+		for nested_user in [utf8_user, special_user]:
 			if "udm_to_s4" in metafunc.function.__name__:
 				def udm_member_factory(*args, **kwargs):
 					return udm_user_factory(nested_user(), *args, **kwargs)
