7143104541eaf41cf5afc8b7b5b867156d1b6a54
Lukas Oyen <oyen@univention.de>
2017-08-16 15:23:16 +0000

    Bug #43598: s4c-test: rework 500test_user_sync.py (rename/move)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-1@82207 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/52_s4connector/500test_user_sync.py
+++ test/ucs-test/tests/52_s4connector/500test_user_sync.py
@@ -11,177 +11,139 @@ import ldap
 import pytest
 
 from univention.testing.udm import UCSTestUDM
-import univention.testing.strings as tstrings
-import univention.testing.ucr as testing_ucr
-from univention.config_registry import handler_set as ucr_set
 
 import s4connector
-from s4connector import s4_in_sync_mode
-from s4connector import restart_univention_cli_server
-from s4connector import connector_running_on_this_host
+from s4connector import (connector_running_on_this_host, connector_setup,
+	create_udm_user, delete_udm_user, create_s4_user, delete_s4_user)
+from s4_users_groups import (NormalUser, Utf8User, SpecialUser)
 
 
-UTF8_CHARSET = tstrings.STR_UMLAUT + u"КирилицаКириллицаĆirilicaЋирилица" + u"普通話普通话"
-SPECIAL_CHARSET = tstrings.STR_SPECIAL_CHARACTER
-# we exclude '$' as it has special meaning and '#"' as the S4C sync can't
-# handle them (see bug #44373)
-FORBIDDEN_SAMACCOUNTNAME = "\\/[]:;|=,+*?<>@ " + '$' + '#"'
-SPECIAL_CHARSET_USERNAME = "".join(set(SPECIAL_CHARSET) - set(FORBIDDEN_SAMACCOUNTNAME))
+TEST_USERS = [NormalUser, Utf8User, SpecialUser]
 
 
-def random_string(length=10, alpha=False, numeric=False, charset=None, encoding='utf-8'):
-	return tstrings.random_string(length, alpha, numeric, charset, encoding)
-
-
-NORMAL_SIMPLE_USER = {
-	"username": tstrings.random_username(),
-	"firstname": tstrings.random_name(),
-	"lastname": tstrings.random_name(),
-	"description": random_string(alpha=True, numeric=True)}
-
-NORMAL_COMPLEX_USER = {
-	"username": tstrings.random_username(),
-	"firstname": tstrings.random_name(),
-	"lastname": tstrings.random_name(),
-	"description": random_string(alpha=True, numeric=True),
-	"street": random_string(alpha=True, numeric=True),
-	"city": random_string(alpha=True, numeric=True),
-	"postcode": random_string(numeric=True),
-	"profilepath": random_string(alpha=True, numeric=True),
-	"scriptpath": random_string(alpha=True, numeric=True),
-	"homeTelephoneNumber": random_string(numeric=True),
-	"mobileTelephoneNumber": random_string(numeric=True),
-	"pagerTelephoneNumber": random_string(numeric=True),
-	"sambaUserWorkstations": random_string(numeric=True)}
-
-UTF8_SIMPLE_USER = {
-	"username": random_string(charset=UTF8_CHARSET),
-	"firstname": random_string(charset=UTF8_CHARSET),
-	"lastname": random_string(charset=UTF8_CHARSET),
-	"description": random_string(charset=UTF8_CHARSET)}
-
-UTF8_COMPLEX_USER = {
-	"username": random_string(charset=UTF8_CHARSET),
-	"firstname": random_string(charset=UTF8_CHARSET),
-	"lastname": random_string(charset=UTF8_CHARSET),
-	"description": random_string(charset=UTF8_CHARSET),
-	"street": random_string(charset=UTF8_CHARSET),
-	"city": random_string(charset=UTF8_CHARSET),
-	"postcode": random_string(numeric=True),
-	"profilepath": random_string(charset=UTF8_CHARSET),
-	"scriptpath": random_string(charset=UTF8_CHARSET),
-	"homeTelephoneNumber": random_string(numeric=True),
-	"mobileTelephoneNumber": random_string(numeric=True),
-	"pagerTelephoneNumber": random_string(numeric=True),
-	"sambaUserWorkstations": random_string(numeric=True)}
-
-SPECIAL_SIMPLE_USER = {
-	"username": random_string(charset=SPECIAL_CHARSET_USERNAME),
-	"firstname": tstrings.random_name_special_characters(),
-	"lastname": tstrings.random_name_special_characters(),
-	"description": random_string(charset=SPECIAL_CHARSET)}
-
-SPECIAL_COMPLEX_USER = {
-	"username": random_string(charset=SPECIAL_CHARSET_USERNAME),
-	"firstname": tstrings.random_name_special_characters(),
-	"lastname": tstrings.random_name_special_characters(),
-	"description": random_string(charset=SPECIAL_CHARSET),
-	"street": random_string(charset=SPECIAL_CHARSET),
-	"city": random_string(charset=SPECIAL_CHARSET),
-	"postcode": random_string(numeric=True),
-	"profilepath": random_string(charset=SPECIAL_CHARSET),
-	"scriptpath": random_string(charset=SPECIAL_CHARSET),
-	"homeTelephoneNumber": random_string(numeric=True),
-	"mobileTelephoneNumber": random_string(numeric=True),
-	"pagerTelephoneNumber": random_string(numeric=True),
-	"sambaUserWorkstations": random_string(numeric=True)}
-
-
-@pytest.mark.parametrize("udm_user", [
-	NORMAL_SIMPLE_USER, NORMAL_COMPLEX_USER, UTF8_SIMPLE_USER,
-	UTF8_COMPLEX_USER, SPECIAL_SIMPLE_USER, SPECIAL_COMPLEX_USER
-], ids=[
-	"NORMAL_SIMPLE_USER", "NORMAL_COMPLEX_USER", "UTF8_SIMPLE_USER",
-	"UTF8_COMPLEX_USER", "SPECIAL_SIMPLE_USER", "SPECIAL_COMPLEX_USER"
-])
+@pytest.mark.parametrize("user_class", TEST_USERS)
 @pytest.mark.parametrize("sync_mode", ["write", "sync"])
-@pytest.mark.skipif(not connector_running_on_this_host(), reason="Univention S4 Connector not configured.")
-def test_user_sync_from_udm_to_s4(udm_user, sync_mode):
-	print("\n###################")
-	print("running test_user_sync_from_udm_to_s4({}, {})".format(udm_user, sync_mode))
-	print("###################\n")
+@pytest.mark.skipif(not connector_running_on_this_host(),
+	reason="Univention S4 Connector not configured.")
+def test_user_sync_from_udm_to_s4(user_class, sync_mode):
+	with connector_setup(sync_mode) as s4, UCSTestUDM() as udm:
+		udm_user = user_class()
+		(udm_user_dn, s4_user_dn) = create_udm_user(udm, s4, udm_user)
 
-	user_syntax = "directory/manager/web/modules/users/user/properties/username/syntax=string"
-	with testing_ucr.UCSTestConfigRegistry():
-		ucr_set([user_syntax])
-		restart_univention_cli_server()
-		s4_in_sync_mode(sync_mode)
-		with UCSTestUDM() as udm:
-			s4 = s4connector.S4Connection()
-
-			selection = ("username", "firstname", "lastname")
-			basic_udm_user = {k: v for (k, v) in udm_user.iteritems() if k in selection}
-
-			print("\nCreating UDM user {}\n".format(basic_udm_user))
-			(udm_user_dn, username) = udm.create_user(**basic_udm_user)
-			s4_user_dn = ldap.dn.dn2str([
-				[("CN", username, ldap.AVA_STRING)],
-				[("CN", "users", ldap.AVA_STRING)]] + ldap.dn.str2dn(s4.adldapbase))
-			s4connector.wait_for_sync()
-			s4.verify_object(s4_user_dn, s4connector.map_udm_user_to_s4(basic_udm_user))
-
-			print("\nModifying UDM user\n")
-			udm.modify_object('users/user', dn=udm_user_dn, **udm_user)
-			s4connector.wait_for_sync()
-			s4.verify_object(s4_user_dn, s4connector.map_udm_user_to_s4(udm_user))
-
-			print("\nDeleting UDM user\n")
-			udm.remove_object('users/user', dn=udm_user_dn)
-			s4connector.wait_for_sync()
-			s4.verify_object(s4_user_dn, None)
-
-
-@pytest.mark.parametrize("udm_user", [
-	NORMAL_SIMPLE_USER, NORMAL_COMPLEX_USER, UTF8_SIMPLE_USER,
-	UTF8_COMPLEX_USER, SPECIAL_SIMPLE_USER, SPECIAL_COMPLEX_USER
-], ids=[
-	"NORMAL_SIMPLE_USER", "NORMAL_COMPLEX_USER", "UTF8_SIMPLE_USER",
-	"UTF8_COMPLEX_USER", "SPECIAL_SIMPLE_USER", "SPECIAL_COMPLEX_USER"
-])
-@pytest.mark.parametrize("sync_mode", ["read", "sync"])
-@pytest.mark.skipif(not connector_running_on_this_host(), reason="Univention S4 Connector not configured.")
-def test_user_sync_from_s4_to_udm(udm_user, sync_mode):
-	print("\n###################")
-	print("running test_user_sync_from_s4_to_udm({}, {})".format(udm_user, sync_mode))
-	print("###################\n")
-
-	user_syntax = "directory/manager/web/modules/users/user/properties/username/syntax=string"
-	with testing_ucr.UCSTestConfigRegistry():
-		ucr_set([user_syntax])
-		restart_univention_cli_server()
-		s4_in_sync_mode(sync_mode)
-
-		s4 = s4connector.S4Connection()
-
-		selection = ("username", "firstname", "lastname")
-		basic_udm_user = {k: v for (k, v) in udm_user.iteritems() if k in selection}
-		basic_s4_user = s4connector.map_udm_user_to_s4(basic_udm_user)
-
-		print("\nCreating S4 user {}\n".format(basic_s4_user))
-		username = udm_user.get("username")
-		s4_user_dn = s4.createuser(username, **basic_s4_user)
-		udm_user_dn = ldap.dn.dn2str([
-			[("uid", username, ldap.AVA_STRING)],
-			[("CN", "users", ldap.AVA_STRING)]] + ldap.dn.str2dn(UCSTestUDM.LDAP_BASE))
+		print("\nModifying UDM user\n")
+		udm.modify_object('users/user', dn=udm_user_dn, **udm_user.user)
 		s4connector.wait_for_sync()
-		s4connector.verify_udm_object("users/user", udm_user_dn, basic_udm_user)
+		s4.verify_object(s4_user_dn, s4connector.map_udm_user_to_s4(udm_user.user))
+
+		delete_udm_user(udm, s4, udm_user_dn, s4_user_dn)
+
+
+@pytest.mark.parametrize("user_class", TEST_USERS)
+@pytest.mark.parametrize("sync_mode", ["write", "sync"])
+@pytest.mark.skipif(not connector_running_on_this_host(),
+	reason="Univention S4 Connector not configured.")
+def test_user_sync_from_udm_to_s4_with_rename(user_class, sync_mode):
+	with connector_setup(sync_mode) as s4, UCSTestUDM() as udm:
+		udm_user = user_class()
+		(udm_user_dn, s4_user_dn) = create_udm_user(udm, s4, udm_user)
+
+		print("\nRename UDM user\n")
+		udm_user_dn = udm.modify_object('users/user', dn=udm_user_dn, **udm_user.rename)
+		s4connector.wait_for_sync()
+
+		s4.verify_object(s4_user_dn, None)
+		s4_user_dn = ldap.dn.dn2str([
+			[("CN", udm_user.rename.get("username"), ldap.AVA_STRING)],
+			[("CN", "users", ldap.AVA_STRING)]] + ldap.dn.str2dn(s4.adldapbase))
+		s4.verify_object(s4_user_dn, s4connector.map_udm_user_to_s4(udm_user.rename))
+
+		delete_udm_user(udm, s4, udm_user_dn, s4_user_dn)
+
+
+@pytest.mark.parametrize("user_class", TEST_USERS)
+@pytest.mark.parametrize("sync_mode", ["write", "sync"])
+@pytest.mark.skipif(not connector_running_on_this_host(),
+	reason="Univention S4 Connector not configured.")
+def test_user_sync_from_udm_to_s4_with_move(user_class, sync_mode):
+	with connector_setup(sync_mode) as s4, UCSTestUDM() as udm:
+		udm_user = user_class()
+		(udm_user_dn, s4_user_dn) = create_udm_user(udm, s4, udm_user)
+
+		print("\nMove UDM user\n")
+		udm_container_dn = udm.create_object('container/cn', name=udm_user.container)
+		udm_user_dn = udm.move_object('users/user', dn=udm_user_dn,
+			position=udm_container_dn)
+
+		s4connector.wait_for_sync()
+		s4.verify_object(s4_user_dn, None)
+		s4_user_dn = ldap.dn.dn2str([
+			[("CN", udm_user.basic.get("username"), ldap.AVA_STRING)],
+			[("CN", udm_user.container, ldap.AVA_STRING)]] + ldap.dn.str2dn(s4.adldapbase))
+		s4.verify_object(s4_user_dn, s4connector.map_udm_user_to_s4(udm_user.basic))
+
+		delete_udm_user(udm, s4, udm_user_dn, s4_user_dn)
+
+
+@pytest.mark.parametrize("user_class", TEST_USERS)
+@pytest.mark.parametrize("sync_mode", ["read", "sync"])
+@pytest.mark.skipif(not connector_running_on_this_host(),
+	reason="Univention S4 Connector not configured.")
+def test_user_sync_from_s4_to_udm(user_class, sync_mode):
+	with connector_setup(sync_mode) as s4:
+		udm_user = user_class()
+		(basic_s4_user, s4_user_dn, udm_user_dn) = create_s4_user(s4, udm_user)
 
 		print("\nModifying S4 user\n")
-		s4.set_attributes(s4_user_dn, **s4connector.map_udm_user_to_s4(udm_user))
+		s4.set_attributes(s4_user_dn, **s4connector.map_udm_user_to_s4(udm_user.user))
 		s4connector.wait_for_sync()
-		s4connector.verify_udm_object("users/user", udm_user_dn, udm_user)
+		s4connector.verify_udm_object("users/user", udm_user_dn, udm_user.user)
 
-		print("\nDeleting S4 user\n")
-		s4.delete(s4_user_dn)
+		delete_s4_user(s4, s4_user_dn, udm_user_dn)
+
+
+@pytest.mark.parametrize("user_class", TEST_USERS)
+@pytest.mark.parametrize("sync_mode", ["read", "sync"])
+@pytest.mark.skipif(not connector_running_on_this_host(),
+	reason="Univention S4 Connector not configured.")
+def test_user_sync_from_s4_to_udm_with_rename(user_class, sync_mode):
+	with connector_setup(sync_mode) as s4:
+		udm_user = user_class()
+		(basic_s4_user, s4_user_dn, udm_user_dn) = create_s4_user(s4, udm_user)
+
+		print("\nRename S4 user {!r} to {!r}\n".format(s4_user_dn, udm_user.rename.get("username")))
+		s4_user_dn = s4.rename_or_move_user_or_group(s4_user_dn,
+			name=udm_user.rename.get("username"))
+		s4.set_attributes(s4_user_dn, **s4connector.map_udm_user_to_s4(udm_user.rename))
 		s4connector.wait_for_sync()
+
 		s4connector.verify_udm_object("users/user", udm_user_dn, None)
+		udm_user_dn = ldap.dn.dn2str([
+			[("uid", udm_user.rename.get("username"), ldap.AVA_STRING)],
+			[("CN", "users", ldap.AVA_STRING)]] + ldap.dn.str2dn(UCSTestUDM.LDAP_BASE))
+		s4connector.verify_udm_object("users/user", udm_user_dn, udm_user.rename)
+
+		delete_s4_user(s4, s4_user_dn, udm_user_dn)
+
+
+@pytest.mark.parametrize("user_class", TEST_USERS)
+@pytest.mark.parametrize("sync_mode", ["read", "sync"])
+@pytest.mark.skipif(not connector_running_on_this_host(),
+	reason="Univention S4 Connector not configured.")
+def test_user_sync_from_s4_to_udm_with_move(user_class, sync_mode):
+	with connector_setup(sync_mode) as s4:
+		udm_user = user_class()
+		(basic_s4_user, s4_user_dn, udm_user_dn) = create_s4_user(s4, udm_user)
+
+		print("\nMove S4 user {!r} to {!r}\n".format(s4_user_dn, udm_user.container))
+		container_dn = s4.container_create(udm_user.container)
+		s4_user_dn = s4.rename_or_move_user_or_group(s4_user_dn, position=container_dn)
+		s4.set_attributes(s4_user_dn, **s4connector.map_udm_user_to_s4(udm_user.basic))
+		s4connector.wait_for_sync()
+
+		s4connector.verify_udm_object("users/user", udm_user_dn, None)
+		udm_user_dn = ldap.dn.dn2str([
+			[("uid", udm_user.basic.get("username"), ldap.AVA_STRING)],
+			[("CN", udm_user.container, ldap.AVA_STRING)]] + ldap.dn.str2dn(UCSTestUDM.LDAP_BASE))
+		s4connector.verify_udm_object("users/user", udm_user_dn, udm_user.basic)
+
+		delete_s4_user(s4, s4_user_dn, udm_user_dn)
