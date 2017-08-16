bc6a78ae34b5a496413f4974e261f9f48744fc07
Lukas Oyen <oyen@univention.de>
2017-08-16 15:23:19 +0000

    Bug #11658: adc-test: adconnector: use common code from univention.testing.connector_common
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-1@82211 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-test/tests/55_adconnector/adconnector.py
+++ test/ucs-test/tests/55_adconnector/adconnector.py
@@ -9,10 +9,13 @@ import ldap
 import copy
 import time
 import subprocess
+import contextlib
 import ldap_glue
 import ldap.modlist as modlist
 import univention.connector.ad as ad
 import univention.testing.utils as utils
+import univention.testing.ucr as testing_ucr
+import univention.testing.connector_common as tcommon
 
 import univention.admin.modules
 import univention.admin.objects
@@ -192,34 +195,17 @@ class ADConnection(ldap_glue.LDAPConnection):
 		else:
 			ad_object = self.get(dn)
 			for (key, value) in expected_attributes.iteritems():
-				ad_value = set(to_unicode(x).lower() for x in ad_object.get(key, []))
-				expected = set((to_unicode(value).lower(),)) if isinstance(value, basestring) \
-					else set(to_unicode(v).lower() for v in value)
+				ad_value = set(tcommon.to_unicode(x).lower() for x in ad_object.get(key, []))
+				expected = set((tcommon.to_unicode(value).lower(),)) if isinstance(value, basestring) \
+					else set(tcommon.to_unicode(v).lower() for v in value)
 				if not expected.issubset(ad_value):
 					try:
-						ad_value = set(normalize_dn(dn) for dn in ad_value)
-						expected = set(normalize_dn(dn) for dn in expected)
+						ad_value = set(tcommon.normalize_dn(dn) for dn in ad_value)
+						expected = set(tcommon.normalize_dn(dn) for dn in expected)
 					except ldap.DECODING_ERROR:
 						pass
 				error_msg = '{}: {} not in {}, object {}'.format(key, expected, ad_value, ad_object)
 				assert expected.issubset(ad_value), error_msg
-
-
-def to_unicode(string):
-	if isinstance(string, unicode):
-		return string
-	return unicode(string, 'utf-8')
-
-
-def normalize_dn(dn):
-	"""
-	Normalize a given dn. This removes some escaping of special chars in the
-	DNs. Note: The S4-LDAP returns DNs with escaping chars, OpenLDAP does not.
-
-	>>> normalize_dn("cn=peter\#,cn=groups")
-	'cn=peter#,cn=groups'
-	"""
-	return ldap.dn.dn2str(ldap.dn.str2dn(dn))
 
 
 def connector_running_on_this_host():
@@ -229,11 +215,6 @@ def connector_running_on_this_host():
 def restart_adconnector():
 	print("Restarting AD-Connector")
 	subprocess.check_call(["service", "univention-ad-connector", "restart"])
-
-
-def restart_univention_cli_server():
-	print("Restarting Univention-CLI-Server")
-	subprocess.call(["pkill", "-f", "univention-cli-server"])
 
 
 def ad_in_sync_mode(sync_mode, configbase='connector'):
@@ -255,78 +236,12 @@ def wait_for_sync(min_wait_time=0):
 	time.sleep(synctime)
 
 
-def map_udm_user_to_ad(user):
-	"""
-	Map a UDM user given as a dictionary of `property`:`values` mappings to a
-	dictionary of `attributes`:`values` mappings as required by the AD-LDAP.
-	Note: This expects the properties from the UDM users/user modul and not
-	OpenLDAP-attributes!.
-	"""
-	mapping = {"username": "sAMAccountName",
-		"firstname": "givenName",
-		"lastname": "sn",
-		"description": "description",
-		"street": "streetAddress",
-		"city": "l",
-		"postcode": "postalCode",
-		"profilepath": "profilePath",
-		"scriptpath": "scriptPath",
-		"homeTelephoneNumber": "homePhone",
-		"mobileTelephoneNumber": "mobile",
-		"pagerTelephoneNumber": "pager",
-		"sambaUserWorkstations": "userWorkstations"}
-	return {mapping.get(key): value for (key, value) in user.iteritems()
-			if key in mapping}
-
-
-def map_udm_group_to_ad(group):
-	"""
-	Map a UDM group given as a dictionary of `property`:`values` mappings to a
-	dictionary of `attributes`:`values` mappings as required by the AD-LDAP.
-	Note: This expects the properties from the UDM groups/group modul and not
-	OpenLDAP-attributes!.
-	"""
-	mapping = {"name": "sAMAccountName",
-		"description": "description"}
-	return {mapping.get(key): value for (key, value) in group.iteritems()
-			if key in mapping}
-
-
-def verify_udm_object(module, dn, expected_properties):
-	"""
-	Verify an object exists with the given `dn` in the given UDM `module` with
-	some properties. Setting `expected_properties` to `None` requires the
-	object to not exist. `expected_properties` is a dictionary of
-	`property`:`value` pairs.
-
-	This will throw an `AssertionError` in case of a mismatch.
-	"""
-	lo = utils.get_ldap_connection()
-	try:
-		position = univention.admin.uldap.position(lo.base)
-		udm_module = univention.admin.modules.get(module)
-		udm_object = univention.admin.objects.get(udm_module, None, lo, position, dn)
-		udm_object.open()
-	except univention.admin.uexceptions.noObject as e:
-		if expected_properties is None:
-			return
-		raise e
-
-	if expected_properties is None:
-		raise AssertionError("UDM object {} should not exist".format(dn))
-
-	for (key, value) in expected_properties.iteritems():
-		udm_value = udm_object.info.get(key)
-		if isinstance(udm_value, basestring):
-			udm_value = set([udm_value])
-		if not isinstance(value, (tuple, list)):
-			value = set([value])
-		value = set(to_unicode(v).lower() for v in value)
-		udm_value = set(to_unicode(v).lower() for v in udm_value)
-		if udm_value != value:
-			try:
-				value = set(normalize_dn(dn) for dn in value)
-				udm_value = set(normalize_dn(dn) for dn in udm_value)
-			except ldap.DECODING_ERROR:
-				pass
-		assert udm_value == value, '{}: {} != expected {}'.format(key, udm_value, value)
+@contextlib.contextmanager
+def connector_setup(sync_mode):
+	user_syntax = "directory/manager/web/modules/users/user/properties/username/syntax=string"
+	group_syntax = "directory/manager/web/modules/groups/group/properties/name/syntax=string"
+	with testing_ucr.UCSTestConfigRegistry():
+		ucr_set([user_syntax, group_syntax])
+		tcommon.restart_univention_cli_server()
+		ad_in_sync_mode(sync_mode)
+		yield
