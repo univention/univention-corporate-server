e8564f76fc5593c1d915828d03364e97fb368421
Florian Best <best@univention.de>
2017-08-18 16:41:47 +0000

    Bug #43218: Migrate test_master_slave_backup_member
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-1@82378 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-gui-tests/installation/test_master_slave_backup_member.py
+++ test/ucs-gui-tests/installation/test_master_slave_backup_member.py
@@ -1,34 +1,95 @@
-def master_slave_backup_member():
-	pass
-#rm -rf screen_dumps
-#mv univention installer_test
-#echo 10.200.13.60 > IP_MASTER
-#echo 10.200.13.61 > IP_SLAVE
-#echo 10.200.13.62 > IP_BACKUP
-#echo 10.200.13.63 > IP_MEMBER
-#ssh-keygen -R $(cat IP_MASTER)
-#ssh-keygen -R $(cat IP_SLAVE)
-#ssh-keygen -R $(cat IP_BACKUP)
-#ssh-keygen -R $(cat IP_MEMBER)
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
 #
-#mkdir -p screen_dumps/master
-#python installer_test/create-vm --name installer_test-large_master_de --server $KVM_BUILD_SERVER --ucs-iso /mnt/omar/vmwares/kvm/iso/iso-tests/ucs_4.2-0-latest-amd64.iso --resultfile resultfile_master
-#python installer_test/00_master-de.py --ip $(cat IP_MASTER) --dump-dir screen_dumps/master $(python installer_test/resultfile_to_vnc_connection resultfile_master)
+# Python VNC automate
 #
-#mkdir -p screen_dumps/slave
-#python installer_test/create-vm --name installer_test-large_slave_de --server $KVM_BUILD_SERVER --ucs-iso /mnt/omar/vmwares/kvm/iso/iso-tests/ucs_4.2-0-latest-amd64.iso --resultfile resultfile_slave
-#python installer_test/10_slave-de.py --ip $(cat IP_SLAVE) --dns-server $(cat IP_MASTER) --dump-dir screen_dumps/slave $(python installer_test/resultfile_to_vnc_connection resultfile_slave)
+# Copyright 2017 Univention GmbH
 #
-#mkdir -p screen_dumps/backup
-#python installer_test/create-vm --name installer_test-large_backup_de --server $KVM_BUILD_SERVER --ucs-iso /mnt/omar/vmwares/kvm/iso/iso-tests/ucs_4.2-0-latest-amd64.iso --resultfile resultfile_backup
-#python installer_test/20_backup-de.py --ip $(cat IP_BACKUP) --dns-server $(cat IP_MASTER) --dump-dir screen_dumps/backup $(python installer_test/resultfile_to_vnc_connection resultfile_backup)
+# http://www.univention.de/
 #
-#mkdir -p screen_dumps/member
-#python installer_test/create-vm --name installer_test-large_member_de --server $KVM_BUILD_SERVER --ucs-iso /mnt/omar/vmwares/kvm/iso/iso-tests/ucs_4.2-0-latest-amd64.iso --resultfile resultfile_member
-#python installer_test/30_member-de.py --ip $(cat IP_MEMBER) --dns-server $(cat IP_MASTER) --dump-dir screen_dumps/member $(python installer_test/resultfile_to_vnc_connection resultfile_member)
+# All rights reserved.
 #
-#python installer_test/do_simple_vm_test.py $(cat IP_MASTER)
-#python installer_test/do_simple_vm_test.py --reuse-license $(cat IP_SLAVE)
-#python installer_test/do_simple_vm_test.py --reuse-license $(cat IP_BACKUP)
-#python installer_test/do_simple_vm_test.py --reuse-license $(cat IP_MEMBER)
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
 #
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
+import os
+import sys
+import subprocess
+
+from vminstall.installer import Installer
+from vminstall.virtual_machine import VirtualMachine
+
+
+def configure_it(installer):
+	installer.vm_config.update_ucs_after_install = False
+	installer.skip_boot_device_selection()
+	installer.select_language()
+	installer.set_country_and_keyboard_layout()
+	installer.network_setup()
+	installer.account_setup()
+	installer.hdd_setup()
+
+
+def test_installing_all_roles(language, server, ip_address, iso_image):
+	# TODO: move to some more general point
+	if not os.path.exists('screen_dumps'):
+		os.makedirs('screen_dumps')
+
+	# TODO: the name parameter should be automatically generated
+	with VirtualMachine(name='installer_test_multi_roles-master-%s' % (language,), server=server, iso_image=iso_image) as vm, Installer(args=['--ip', ip_address, '--dump-dir', 'screen_dumps', vm.vnc_host], language=language) as installer:
+		configure_it(installer)
+		installer.setup_ucs_master()
+
+		contexts = []
+		i = 20
+
+		def configure(role):
+			global i
+			if not os.path.exists('screen_dumps/%s' % (role,)):
+				os.makedirs('screen_dumps/%s' % (role,))
+
+			role_ip_address = '%s.%s' % (ip_address.rsplit('.', 1)[0], int(ip_address.rsplit('.', 1)[1]) + i)
+			i += 1
+
+			role_vm = VirtualMachine(name='installer_test_multi_roles-%s-%s' % (role, language,), server=server, iso_image=iso_image)
+			contexts.append(role_vm)
+			role_vm.__enter__()
+			role_installer = Installer(args=['--ip', role_ip_address, '--dump-dir', 'screen_dumps/%s' % (role,), '--dns-server', ip_address, role_vm.vnc_host], language=language)
+			contexts.append(role_installer)
+			role_installer.__enter__()
+
+			configure_it(role_installer)
+			setup_system = getattr(role_installer, 'setup_ucs_%s' % (role,))
+			setup_system()
+
+		try:
+			for role in ('slave', 'backup', 'member'):
+				configure(role)
+		finally:
+			exc_info = sys.exc_info()
+			for context in contexts:
+				try:
+					contexts.__exit__(*exc_info)
+				except:
+					pass
+
+	# TODO: move to some more general point
+	subprocess.call(['tar', '--remove-files', '-zcf', 'screen_dumps.tar.gz', 'screen_dumps'])
