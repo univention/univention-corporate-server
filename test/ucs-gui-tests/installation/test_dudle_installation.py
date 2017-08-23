538fd494bc5738087ed6e152a555180516b39848
Richard Ulmer <ulmer@univention.de>
2017-08-23 15:19:25 +0000

    Bug #43218: Test if VMs are functional after the installation
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-1@82455 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ test/ucs-gui-tests/installation/test_dudle_installation.py
@@ -0,0 +1,38 @@
+import subprocess
+
+
+class TestDudleInstallation(object):
+
+	def test_install_dudle(self, role, ip_address, master_ip, password):
+		self.ip = ip_address
+		self.master_ip = master_ip
+		self.password = password
+		if role != 'basesystem':
+			self.import_license_on_vm()
+
+		self.execute_through_ssh('echo %s > pwdfile' % (self.password,))
+		self.execute_through_ssh('univention-app install dudle --noninteractive --pwdfile=pwdfile')
+
+	def import_license_on_vm(self):
+		self.copy_through_ssh('utils/license_client.py', 'root@%s:/root/' % (self.master_ip,))
+		self.execute_through_ssh('python -m ./license_client "$(ucr get ldap/base)" "$(date -d \'+1 year\' \'+%d.%m.%Y\')"', self.master_ip)
+		self.execute_through_ssh('univention-license-import ./ValidTest.license && univention-license-check', self.master_ip)
+
+	def execute_through_ssh(self, command, ip=None):
+		subprocess.check_call((
+			'sshpass',
+			'-p', self.password,
+			'ssh',
+			'-o', 'StrictHostKeyChecking=no',
+			'root@%s' % (ip or self.ip,),
+			command
+		))
+
+	def copy_through_ssh(self, source_file, target_file):
+		subprocess.check_call((
+			'sshpass',
+			'-p', self.password,
+			'scp',
+			'-o', 'StrictHostKeyChecking=no',
+			source_file, target_file
+		))
