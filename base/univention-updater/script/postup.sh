3464a444c2e37c31ce665b42197dd8fcb66004c4
Philipp Hahn <hahn@univention.de>
2017-09-01 13:52:47 +0000

    Bug #45269 up: Adapt links and components for 4.2-2
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-2@82587 1b283449-9f2f-0410-b571-8eb228e1a901

--- base/univention-updater/script/postup.sh
+++ base/univention-updater/script/postup.sh
@@ -145,16 +145,16 @@ if [ -f /var/univention-join/joined -a "$server_role" != basesystem ]; then
 		--bindpwdfile "/etc/machine.secret" \
 		--dn "$ldap_hostdn" \
 		--set operatingSystem="Univention Corporate Server" \
-		--set operatingSystemVersion="4.2-1" >>"$UPDATER_LOG" 2>&1
+		--set operatingSystemVersion="4.2-2" >>"$UPDATER_LOG" 2>&1
 fi
 
 # Move to mirror mode for previous errata component
 ucr set \
-	repository/online/component/4.2-0-errata=false \
-	repository/online/component/4.2-0-errata/localmirror=true \
-	repository/online/component/4.2-1-errata=enabled \
-	repository/online/component/4.2-1-errata/description="Errata updates for UCS 4.2-1" \
-	repository/online/component/4.2-1-errata/version="4.2" >>"$UPDATER_LOG" 2>&1
+	repository/online/component/4.2-1-errata=false \
+	repository/online/component/4.2-1-errata/localmirror=true \
+	repository/online/component/4.2-2-errata=enabled \
+	repository/online/component/4.2-2-errata/description="Errata updates for UCS 4.2-2" \
+	repository/online/component/4.2-2-errata/version="4.2" >>"$UPDATER_LOG" 2>&1
 
 # run remaining joinscripts
 if [ "$server_role" = "domaincontroller_master" ]; then
