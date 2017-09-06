d365fb133cfad3c66eaec9dd41d55ff49a99a420
Richard Ulmer <ulmer@univention.de>
2017-09-06 08:52:39 +0000

    Bug #43218: Fix passing of dns_server to the Installer()
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-1@82710 1b283449-9f2f-0410-b571-8eb228e1a901

--- test/ucs-gui-tests/installation/vminstall/__init__.py
+++ test/ucs-gui-tests/installation/vminstall/__init__.py
@@ -8,22 +8,22 @@ from vminstall.virtual_machine import VirtualMachine
 def create_virtual_machine(language, role, environment, server, iso_image, ip_address, output_directory, dns_server=None):
 	name = 'installer_test_%s-%s-%s' % (language, role, environment)
 	installer_args = ['--ip', ip_address, '--dump-dir', output_directory]
-	if dns_server:
-		installer_args.extend(['--dns-server', dns_server])
 	with VirtualMachine(name=name, server=server, iso_image=iso_image, interfaces=3 if environment == 'multiple_nics' else 1, disks=3 if environment == 'multiple_hdds' else 1) as vm:
 		installer_args.append(vm.vnc_host)
 		with Installer(args=installer_args, role=role, language=language) as installer:
-			adapt_vm_config(installer, environment)
+			adapt_vm_config(installer, environment, dns_server)
 			go_through_setup_process(installer, language, environment)
 			yield (vm, installer)
 
 
-def adapt_vm_config(installer, environment):
+def adapt_vm_config(installer, environment, dns_server):
 	installer.vm_config.update_ucs_after_install = False
 	installer.vm_config.install_all_additional_components = environment == 'additional_software_components'
 	if environment == 'difficult_password':
 		installer.vm_config.password = "@fooBar99Extr4L4rg3Size"  # FIXME: generate a random one!
 	installer.vm_config.use_multiple_partitions = environment == 'multiple_partitions'
+	if dns_server:
+		installer.vm_config.dns_server_ip = dns_server
 
 
 def go_through_setup_process(installer, language, environment):
