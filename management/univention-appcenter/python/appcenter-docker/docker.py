322126f0eb7c656964e4e468a93bfa0423fb159d
Eduard Mai <mai@univention.de>
2017-09-15 18:44:57 +0200

    Revert "Bug #45290: backport cp_from_container"
    This reverts commit 646de8bbd80c4cf620c104b0a6abbf9624cc5b0e.

--- management/univention-appcenter/python/appcenter-docker/docker.py
+++ management/univention-appcenter/python/appcenter-docker/docker.py
@@ -226,13 +226,6 @@ def dockerd_logs(logger=None):
 	ret, out = call_process2(args, logger=logger)
 	return out
 
-def docker_cp(src, dest, logger=None, followlink=False):
-	args = ['docker', 'cp']
-	if followlink is True:
-		args.append('-L')
-	args.append(src)
-	args.append(dest)
-	return call_process2(args, logger=logger)
 
 class Docker(object):
 
@@ -380,6 +373,3 @@ class Docker(object):
 
 	def dockerd_logs(self):
 		return dockerd_logs(logger=self.logger)
-
-	def cp_from_container(self, src, dest, **kwargs):
-		return docker_cp(self.container + ':' + src, dest, logger=self.logger, **kwargs)
