f83e510d9c617b16b319f61438319791f154130d
Dirk Wiesenthal <wiesenthal@univention.de>
2017-10-13 20:19:14 +0200

    Bug #41436: Create an image usable by ucs-kt-get for every App Appliance

--- test/utils/build-for-ucs-kt.sh
+++ test/utils/build-for-ucs-kt.sh
@@ -10,6 +10,16 @@ META="/tmp/appliance_${APP_ID}_amd.xml"
 ARCHIVE="/tmp/appliance_${APP_ID}_amd64.tar"
 MASTER_IMAGE="/tmp/master-image"
 
+function finish {
+	rm -v "$META" || true
+	rm -v "$IMG" || true
+	rm -v "$MASTER_IMAGE" || true
+	rm -v "$ARCHIVE" || true  # should fail if everything went right
+	rm -v "$ARCHIVE".gz || true  # should fail if everything went right
+}
+
+trap finish EXIT
+
 APP="$APP_ID" envsubst </mnt/omar/vmwares/kvm/single/Others/appliance_template.xml >"$META"
 tar -C "$(dirname "$META")" -cf "$ARCHIVE" "$(basename "$META")"
 tar -C "$(dirname "$IMG")" -rf "$ARCHIVE" "$(basename "$IMG")"
@@ -17,5 +27,3 @@ touch "$MASTER_IMAGE"  # what is this for?
 tar -C "$(dirname "$MASTER_IMAGE")" -rf "$ARCHIVE" "$(basename "$MASTER_IMAGE")"
 gzip -9 "$ARCHIVE"
 mv "$ARCHIVE".gz /mnt/omar/vmwares/kvm/single/Others/appliance_"${APP_ID}"_amd64.tar.gz
-
-rm "$META" "$IMG" "$MASTER_IMAGE"
