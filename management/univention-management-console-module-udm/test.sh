64ad7702cbe55911454914b505f341b310f7343c
Florian Best <best@univention.de>
2017-12-08 15:41:32 +0100

    Add script which tests some basic requests

---
+++ management/univention-management-console-module-udm/test.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# shortcut to test some requests
+
+base="http://Administrator:univention@10.200.27.130:8888/udm"
+
+_curl() {
+	echo -- "$@"
+	curl "$@"
+}
+
+_curl -i "$base/"
+_curl -i "$base/users/user/"
+_curl -i "$base/users/object-types/"
+_curl -i "$base/users/user/properties"
+_curl -i "$base/users/user/options"
+_curl -i "$base/users/user/layout"
+_curl -i "$base/users/user/templates"
+_curl -i "$base/users/user/containers"
+_curl -i "$base/users/user/policies"
+_curl -i "$base/users/user/report-types"
+#_curl -i "$base/users/user/report/PDF%20Document/?"
+_curl -i "$base/users/user/properties/mailPrimaryAddress/default"
+#_curl -i "$base/users/user/uid%3DAdministrator%2Ccn%3Dusers%2Cdc%3Dschool%2Cdc%3Dlocal/property"
+_curl -i "$base/users/user/uid%3DAdministrator%2Ccn%3Dusers%2Cdc%3Dschool%2Cdc%3Dlocal/property/primaryGroup/choices"
+_curl -i "$base/users/user/uid%3DAdministrator%2Ccn%3Dusers%2Cdc%3Dschool%2Cdc%3Dlocal/policies/umc"
+_curl -i "$base/users/user/policies/umc/uid%3DAdministrator%2Ccn%3Dusers%2Cdc%3Dschool%2Cdc%3Dlocal/"
+_curl -i "$base/users/user/policies/umc/cn%3Dusers%2Cdc%3Dschool%2Cdc%3Dlocal/?container=yes"
+_curl -i "$base/users/user/policies/umc/uid%3DAdministrator%2Ccn%3Dusers%2Cdc%3Dschool%2Cdc%3Dlocal/?policy=cn%3Ddefault-umc-users%2Ccn%3DUMC%2Ccn%3Dpolicies%2Cdc%3Dschool%2Cdc%3Dlocal"
+_curl -i "$base/users/user/policies/umc/cn%3Dusers%2Cdc%3Dschool%2Cdc%3Dlocal/?container=yes&policy=cn%3Ddefault-umc-users%2Ccn%3DUMC%2Ccn%3Dpolicies%2Cdc%3Dschool%2Cdc%3Dlocal"
+
+# search for users:
+_curl -i "$base/users/user/"
+
+# create a user:
+_curl -XPOST -i "$base/users/user/"
+
+# get a specific user:
+_curl -i "$base/users/user/uid%3DAdministrator%2Ccn%3Dusers%2Cdc%3Dschool%2Cdc%3Dlocal"
+
+# modify a specific user:
+_curl -X PUT -i "$base/users/user/uid%3DAdministrator%2Ccn%3Dusers%2Cdc%3Dschool%2Cdc%3Dlocal"
+_curl -X PATCH -i "$base/users/user/uid%3DAdministrator%2Ccn%3Dusers%2Cdc%3Dschool%2Cdc%3Dlocal"
+
+# remove a specific user:
+_curl -X DELETE -i "$base/users/user/uid%3DAdministrator%2Ccn%3Dusers%2Cdc%3Dschool%2Cdc%3Dlocal"
