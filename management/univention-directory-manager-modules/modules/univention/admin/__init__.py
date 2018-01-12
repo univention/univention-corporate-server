c29aa83a1b1062f5d2c9dd95730d1e4beb0c74d5
Florian Best <best@univention.de>
2018-01-12 18:40:25 +0100

    Bug #45842: object argument in replace_pattern might be a dict

--- management/univention-directory-manager-modules/modules/univention/admin/__init__.py
+++ management/univention-directory-manager-modules/modules/univention/admin/__init__.py
@@ -150,7 +150,7 @@ def pattern_replace(pattern, object):
 				return ''
 
 		# make sure the key value exists
-		if object.has_property(key) and object[key]:
+		if key in object and object[key]:
 			val = modify_text(object[key], strCommands)
 			# try to apply the indexing instructions, indicated through '[...]'
 			if ext:
