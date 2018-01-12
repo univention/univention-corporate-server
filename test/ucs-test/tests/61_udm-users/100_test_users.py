953fbf612f47cee4b422720d76bbf4e47f40f07d
Florian Best <best@univention.de>
2018-01-12 15:38:04 +0100

    Bug #45842: implement test_modlist_samba_privileges

--- test/ucs-test/tests/61_udm-users/100_test_users.py
+++ test/ucs-test/tests/61_udm-users/100_test_users.py
@@ -26,24 +26,6 @@ class TestUsers(object):
 		# TODO: test pre_create() / pre_modify() / pre_ready() method:
 		primaryGroupWithoutSamba
 
-		modlist_samba_privileges
-		modlist_cn
-		modlist_gecos
-		modlist_display_name
-		modlist_krb_principal
-		modlist_password_change
-		modlist_samba_bad_pw_count
-		modlist_sambaAcctFlags
-		modlist_shadowMax
-		modlist_samba_kickoff_time
-		modlist_krb5_valid_end
-		modlist_shadow_expire
-		modlist_mail_forward
-		modlist_univention_person
-		modlist_home_share
-		modlist_samba_mungeddial
-		modlist_samba_sid
-
 		different password expiry interval values!
 
 		is the locking of uidNumber still okay, what if limits are reached?
@@ -151,3 +133,88 @@ class TestUsers(object):
 			'krb5MaxLife': ['86400'],
 			'krb5MaxRenew': ['604800'],
 		})
+
+	@pytest.mark.parametrize('privileges', [
+		['SeMachineAccountPrivilege'],
+		['SeSecurityPrivilege'],
+		['SeTakeOwnershipPrivilege'],
+		['SeBackupPrivilege'],
+		['SeRestorePrivilege'],
+		['SeRemoteShutdownPrivilege'],
+		['SePrintOperatorPrivilege'],
+		['SeAddUsersPrivilege'],
+		['SeDiskOperatorPrivilege'],
+		[
+			'SeMachineAccountPrivilege',
+			'SeSecurityPrivilege',
+			'SeTakeOwnershipPrivilege',
+			'SeBackupPrivilege',
+			'SeRestorePrivilege',
+			'SeRemoteShutdownPrivilege',
+			'SePrintOperatorPrivilege',
+			'SeAddUsersPrivilege',
+			'SeDiskOperatorPrivilege',
+		]
+	])
+	def test_modlist_samba_privileges(self, udm, privileges, verify_ldap_object):
+		self._test_modlist(udm, verify_ldap_object, {'sambaPrivileges': privileges}, {'univentionSambaPrivilegeList': privileges, 'objectClass': ['univentionSambaPrivileges']})
+
+	@pytest.mark.parametrize('privileges', [
+		pytest.mark.xfail(['SeMachineAccountPrivilege', 'foobar'], reason='https://forge.univention.org/bugzilla/show_bug.cgi?id=46020'),
+		['foobar'],
+	])
+	def test_modlist_samba_privileges_invalid(self, udm, privileges):
+		with pytest.raises(Exception):
+			udm.create_user(sambaPrivileges=privileges)
+
+		user = udm.create_user()[0]
+		with pytest.raises(Exception):
+			udm.modify_object('users/user', dn=user, sambaPrivileges=privileges)
+
+	def test_modlist_cn(self, udm):
+		pass
+
+	def test_modlist_gecos(self, udm):
+		pass
+
+	def test_modlist_display_name(self, udm):
+		pass
+
+	def test_modlist_krb_principal(self, udm):
+		pass
+
+	def test_modlist_password_change(self, udm):
+		pass
+
+	def test_modlist_samba_bad_pw_count(self, udm):
+		pass
+
+	def test_modlist_sambaAcctFlags(self, udm):
+		pass
+
+	def test_modlist_shadowMax(self, udm):
+		pass
+
+	def test_modlist_samba_kickoff_time(self, udm):
+		pass
+
+	def test_modlist_krb5_valid_end(self, udm):
+		pass
+
+	def test_modlist_shadow_expire(self, udm):
+		pass
+
+	def test_modlist_mail_forward(self, udm):
+		pass
+
+	def test_modlist_univention_person(self, udm):
+		pass
+
+	def test_modlist_home_share(self, udm):
+		pass
+
+	def test_modlist_samba_mungeddial(self, udm):
+		pass
+
+	def test_modlist_samba_sid(self, udm):
+		pass
