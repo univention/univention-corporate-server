72ee0eb670f7103a78ba1b7e086405eaee328b9f
Fathan Vidjaja <fvidjaja@alvine.knut.univention.de>
2018-01-17 13:10:10 +0100

    Bug #45818: added samba tests to configs and refactor some code bits

--- test/utils/ad-takeover.sh
+++ test/utils/ad-takeover.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 function create_user {
     for value in {1..10}
     do
@@ -7,4 +9,9 @@ function create_user {
     done
 }
 
-create_user
+function check_all_user {
+    for value in {1..10}
+    do
+        python shared-utils/ucs-winrm.py domain_user_validate_password --domain adtakeover.local --domainuser user$value --domainpassword Univention@$value
+    done
+}
