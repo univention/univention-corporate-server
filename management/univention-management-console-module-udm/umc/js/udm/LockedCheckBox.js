def3d94cc3ed3159cd39c6320415d550f6331e20
Florian Best <best@univention.de>
2018-01-31 12:22:41 +0100

    Bug #39817: introduce LockedCheckbox

---
+++ management/univention-management-console-module-udm/umc/js/udm/LockedCheckBox.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2018 Univention GmbH
+ *
+ * http://www.univention.de/
+ *
+ * All rights reserved.
+ *
+ * The source code of this program is made available
+ * under the terms of the GNU Affero General Public License version 3
+ * (GNU AGPL V3) as published by the Free Software Foundation.
+ *
+ * Binary versions of this program provided by Univention to you as
+ * well as other copyrighted, protected or trademarked materials like
+ * Logos, graphics, fonts, specific documentations and configurations,
+ * cryptographic keys etc. are subject to a license agreement between
+ * you and Univention and not subject to the GNU AGPL V3.
+ *
+ * In the case you use this program under the terms of the GNU AGPL V3,
+ * the program is provided in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License with the Debian GNU/Linux or Univention distribution in file
+ * /usr/share/common-licenses/AGPL-3; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/*global define*/
+
+define([
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/_base/array",
+	"dojo/when",
+	"dojo/on",
+	"dojo/keys",
+	"dojo/dom-construct",
+	"dojo/Deferred",
+	"umc/tools",
+	"umc/widgets/CheckBox",
+], function(declare, lang, array, when, on, keys, domConstruct, Deferred, tools, CheckBox) {
+	return declare("umc.modules.udm.LockedCheckBox", [ CheckBox ], {
+		// summary:
+		//		This class extends the normal CheckBox in order to encapsulate
+		//		some UDM specific behaviour.
+
+		setInitialValue: function(value) {
+			this.set('value', value);
+			if (value === '0') {
+				this.set('disabled', true);
+			}
+		}
+
+	});
+});
+
