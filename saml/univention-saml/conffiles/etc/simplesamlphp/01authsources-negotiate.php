14420665cf2600aca591500ea5d33821f7e7f63c
Erik Damrose <damrose@univention.de>
2018-02-07 09:45:13 +0100

    Bug #33214: wip commit

---
+++ saml/univention-saml/conffiles/etc/simplesamlphp/01authsources-negotiate.php
@@ -0,0 +1,27 @@
+
+@!@
+if configRegistry.is_true('saml/idp/negotiate'):
+	print '''
+$config['univention-negotiate'] = array(
+		'negotiate:Negotiate',
+		'keytab' => '/etc/simplesamlphp/krb5.keytab',
+		'fallback' => 'univention-ldap',
+	'''
+	print " 'attributes' => '%s'," % configRegistry.get('saml/idp/ldap/get_attributes', 'uid')
+	from univention.lib.misc import getLDAPURIs
+	hostname = getLDAPURIs()
+	print "	'hostname' => '%s'," % (hostname,)
+	print "	'base' => '%s'," % (configRegistry.get('ldap/base', 'null'),)
+	ldap_user = 'uid=sys-idp-user,cn=users,%s' % configRegistry.get('ldap/base', 'null')
+	if configRegistry.get('saml/idp/ldap/user'):
+		ldap_user = configRegistry.get('saml/idp/ldap/user')
+	print "	'adminUser' => '%s'," % (ldap_user,)
+	password = ''
+	try:
+		password = open('/etc/idp-ldap-user.secret','r').read().strip()
+	except (IOError, OSError):
+		import sys
+		print >> sys.stderr, '/etc/idp-ldap-user.secret could not be read!'
+	print "	'adminPassword' => '%s'," % (password,)
+	print ');'
+@!@
