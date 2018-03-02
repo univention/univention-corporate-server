9d09ec815ee55aa69e69e3162a3c03e7d227657e
Philipp Hahn <hahn@univention.de>
2018-03-02 09:43:33 +0100

    Bug #46120: Copyright 2018

--- base/univention-python-heimdal/creds.h
+++ base/univention-python-heimdal/creds.h
@@ -2,7 +2,7 @@
  * Python Heimdal
  *	Bindings for the credentials API of heimdal
  *
- * Copyright 2003-2017 Univention GmbH
+ * Copyright 2003-2018 Univention GmbH
  *
  * http://www.univention.de/
  *
