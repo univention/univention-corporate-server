9c88ef6a6c9e3bdcc9525a03fbabf9337dcf450b
Philipp Hahn <hahn@univention.de>
2018-03-02 09:43:51 +0100

    Bug #46120: Copyright 2018

--- services/univention-samba4/samba-shares.py
+++ services/univention-samba4/samba-shares.py
@@ -3,7 +3,7 @@
 # Univention Samba
 #  listener module: manages samba share configuration
 #
-# Copyright 2001-2017 Univention GmbH
+# Copyright 2001-2018 Univention GmbH
 #
 # http://www.univention.de/
 #
