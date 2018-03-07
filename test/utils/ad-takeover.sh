d43c6f3e727e253dfa49ddad507cd6e1d7bb4068
Fathan Vidjaja <fvidjaja@alvine.knut.univention.de>
2018-03-07 15:51:18 +0100

    Bug #45818: addition for checking GPOs after AD-Takeover

--- test/utils/ad-takeover.sh
+++ test/utils/ad-takeover.sh
@@ -15,3 +15,24 @@ function check_all_user {
         python shared-utils/ucs-winrm.py domain_user_validate_password --domain adtakeover.local --domainuser user$value --domainpassword Univention@$value
     done
 }
+function check_GPO {
+command=`samba-tool gpo listall`
+if echo "$command" | grep -q "$1"
+then
+    echo "GPO found"
+else
+    echo "GPO not found"
+    exit 1
+fi
+}
+function check_GPO_for_user {
+command=`samba-tool gpo list $2`
+if  echo $command | grep -q "$1"
+then
+    echo "GPO for user found"
+else
+    echo "GPO for user not found"
+    exit 1
+fi
+}
+
