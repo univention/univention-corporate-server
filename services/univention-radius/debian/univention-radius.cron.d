dfa9e430e86338eaa344752b1d87dd8775f363bc
Jürn Brodersen <brodersen@univention.de>
2018-03-07 13:23:21 +0100

    Bug #46528: fix multiple errors and path

--- services/univention-radius/debian/univention-radius.cron.d
+++ services/univention-radius/debian/univention-radius.cron.d
@@ -1,5 +1,5 @@
 SHELL=/bin/sh
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
-55 5 * * * root (ucr commit /etc/freeradius/modules/ldap && /etc/init.d/freeradius restart) > /var/log/univention/univention-freeradius-sync.log
+55 5 * * * root (ucr commit /etc/freeradius/3.0/mods-available/ldap && systemctl restart freeradius) > /var/log/univention/univention-freeradius-sync.log
 
