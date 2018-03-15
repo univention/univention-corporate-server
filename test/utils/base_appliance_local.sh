ccb6e3b026e43df8e1f103eabbb75609b3e1c69d
Felix Botner <botner@univention.de>
2018-03-15 16:38:15 +0100

    Bug #46671: 4.3 appliance

--- test/utils/base_appliance_local.sh
+++ test/utils/base_appliance_local.sh
@@ -1,58 +1,109 @@
-[Global]
-logfile: app-appliance-[ENV:APP_ID].log
-recover: 9
+#!/bin/bash
+#
+# Copyright 2018 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
 
-kvm_server: [ENV:KVM_BUILD_SERVER]
-kvm_user: [ENV:KVM_USER]
-kvm_architecture: amd64
-kvm_ucsversion: empty
-kvm_template: 50GiB
-kvm_operating_system: Others
-kvm_interface: eth0
-kvm_network: appliance-net
-kvm_extra_label: app-appliance-[ENV:APP_ID]
-kvm_connect: false
-kvm_copy_files: false
-kvm_iso: /mnt/omar/vmwares/kvm/iso/ucs/UCS_[ENV:TARGET_VERSION]-amd64.iso
+# ENV VARS:
+# APP_ID
+# KVM_USER
+# UCS_VERSION
+# KVM_BUILD_SERVER
+#
+# KVM_USER needs password less ssh access to APPS_SERVER, KVM_BUILD_SERVER and IMAGE_CONVERT_SERVER
+# KVM_USER needs password less ssh access from IMAGE_CONVERT_SERVER to APPS_SERVER
+# KVM_USER needs password less ssh access from KVM_BUILD_SERVER to APPS_SERVER
 
-[app-appliance]
-command1:
- LOCAL python utils/installation_test/vnc-install-ucs.py --vnc "[app-appliance_VNC_DISPLAY]" --language deu --role applianceLVM --fqdn mytest
- COPY_FILES
- . utils.sh && add_tech_key_authorized_keys
- . utils.sh && upgrade_to_latest_patchlevel
- . utils.sh && do_reboot
- LOCAL sleep 60
- . utils.sh && wait_for_reboot
- ucr set repository/online=true
- ucr set updater/identify="Univention App [ENV:UCS_VERSION] Appliance [ENV:APP_ID] (KVM)"
- ucr set repository/app_center/server="[ENV:APP_CENTER_SERVER]"
- ucr set appcenter/index/verify=no update/secure_apt=no # ?? univention-app test-appcenter
- univention-app update
- . base_appliance.sh && setup_appliance [ENV:APP_ID]
- . base_appliance.sh && prepare_docker_app_container [ENV:APP_ID]
- . base_appliance.sh && backup_current_local_packagecache
- . base_appliance.sh && setup_pre_joined_environment [ENV:APP_ID] [ENV:DOMAINNAME]
- . base_appliance.sh && restore_current_local_packagecache
- . base_appliance.sh && register_apps [ENV:APP_ID]
- . base_appliance.sh && install_pre_packages [ENV:APP_ID]
- . base_appliance.sh && download_packages_and_dependencies [ENV:APP_ID]
- . base_appliance.sh && create_install_script [ENV:APP_ID]
- . base_appliance.sh && install_app_in_prejoined_setup [ENV:APP_ID]
- . base_appliance.sh && appliance_basesettings [ENV:APP_ID]
- . base_appliance.sh && install_activation_packages
- . base_appliance.sh && appliance_dump_memory [ENV:APP_ID] /tmp/ucsmaster-[ENV:APP_ID].memory
- LOCAL echo "univention" >/tmp/univention
- LOCAL /usr/sbin/univention-scp /tmp/univention root@[[app-appliance_IP]]:/tmp/ucsmaster-[ENV:APP_ID].memory /tmp/
- LOCAL scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /tmp/ucsmaster-[ENV:APP_ID].memory [ENV:KVM_USER]@docker.knut.univention.de:/tmp/
- RESET && (. base_appliance.sh; appliance_reset_servers)
- . base_appliance.sh; appliance_cleanup
- . base_appliance.sh; disable_root_login_and_poweroff ROOTLOGIN
- LOCAL sleep 60
-command2:
- LOCAL . utils/base_appliance_local.sh && create_app_images
-#command4:
-# LOCAL sleep 60
+KT_CREATE_IMAGE="/var/lib/libvirt/images/${KVM_USER}_app-appliance-app-appliance-${APP_ID}.qcow2"
+APPS_BASE="/var/univention/buildsystem2/mirror/appcenter.test/univention-apps/${UCS_VERSION}/${APP_ID}"
+APPS_SERVER="omar.knut.univention.de"
+IMAGE_CONVERT_SERVER="docker.knut.univention.de"
+KVM_IMAGE="Univention-App-${APP_ID}-KVM.qcow2"
+VMPLAYER_IMAGE="Univention-App-${APP_ID}-vmware.zip"
+TMPDIR="/tmp/build-${APP_ID}"
+TMP_VMPLAYER_IMAGE="$TMPDIR/$VMPLAYER_IMAGE"
+TMP_KVM_IMAGE="$TMPDIR/master.qcow2"
+
+set -x
+set -e
+
+_ssh () {
+	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -n "$@"
+}
+
+_scp () {
+	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
+}
+
+_cleanup () {
+	_ssh -l "$KVM_USER" "$KVM_BUILD_SERVER" "rm -f $TMP_KVM"
+	_ssh -l "$KVM_USER" "$IMAGE_CONVERT_SERVER" "rm -f $TMP_KVM"
+}
+
+_kvm_image () {
+	# copy KVM image
+	_ssh -l "$KVM_USER" "$APPS_SERVER" "mkdir -p $APPS_BASE"
+	_scp ${KVM_USER}@${KVM_BUILD_SERVER}:${TMP_KVM_IMAGE} ${KVM_USER}@${APPS_SERVER}:"$APPS_BASE/$KVM_IMAGE"
+	_ssh -l "$KVM_USER" "$APPS_SERVER" "
+cd $APPS_BASE;
+md5sum $KVM_IMAGE > $KVM_IMAGE.md5;
+chmod 644 $KVM_IMAGE*;
+"
+}
+
+_vmplayer_image () {
+	_ssh -l "$KVM_USER" "${IMAGE_CONVERT_SERVER}" "
+guestfish add ${TMP_KVM_IMAGE} : run : mount /dev/mapper/vg_ucs-root / : command \"/usr/sbin/ucr set updater/identify=\'Univention App ${UCS_VERSION} Appliance APP_ID \(VMware\)\'\";
+test -e ${TMP_VMPLAYER_IMAGE} && rm ${TMP_VMPLAYER_IMAGE} || true;
+generate_appliance -m '$(< /tmp/ucsmaster-${APP_ID}.memory)' -p UCS -v ${UCS_VERSION}-with-${APP_ID} -o --vmware -s $TMP_KVM_IMAGE -f Univention-App-${APP_ID}
+"
+	_scp ${KVM_USER}@${IMAGE_CONVERT_SERVER}:${VMPLAYER_IMAGE} ${KVM_USER}@${APPS_SERVER}:"$APPS_BASE/${VMPLAYER_IMAGE}"
+	_ssh -l "$KVM_USER" "${IMAGE_CONVERT_SERVER}" "rm -f ${VMPLAYER_IMAGE}"
+	_ssh -l "$KVM_USER" "$APPS_SERVER" "
+cd $APPS_BASE;
+md5sum ${VMPLAYER_IMAGE} > ${VMPLAYER_IMAGE}.md5
+chmod 644 ${VMPLAYER_IMAGE}*
+"
+}
+
+create_app_images () {
+	# convert image
+	_ssh -l "$KVM_USER" "$KVM_BUILD_SERVER" "
+mkdir -p $TMP_KVM
+qemu-img convert -p -c -O qcow2 $KT_CREATE_IMAGE $TMP_KVM_IMAGE
+"
+	# copy to image convert server for later steps
+	_scp ${KVM_USER}@${KVM_BUILD_SERVER}:/${TMP_KVM} ${KVM_USER}@${IMAGE_CONVERT_SERVER}:/tmp
+
+	_kvm_image
+	_vmplayer_image
+	_cleanup
+}
+
+
 # LOCAL ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no build@KVM_BUILD_SERVER mkdir -p /tmp/build-APP_ID/
 # LOCAL ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no build@KVM_BUILD_SERVER qemu-img convert -p -c -O qcow2 /var/lib/libvirt/images/build_ucsmaster-APP_ID.qcow2 /tmp/build-APP_ID/master.qcow2
 # LOCAL ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no build@KVM_BUILD_SERVER ucs-kt-remove build_ucsmaster-APP_ID
@@ -64,6 +115,9 @@ command2:
 # LOCAL ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no build@KVM_BUILD_SERVER rm -f /tmp/build-APP_ID/master.qcow2
 # LOCAL rm -f /tmp/build-APP_ID/master.qcow2
 #command5:
+
+
+
 ## vmware player
 # LOCAL ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no build@docker.knut.univention.de guestfish add /tmp/build-APP_ID/master.qcow2 : run : mount /dev/mapper/vg_ucs-root / : command \"/usr/sbin/ucr set updater/identify=\'Univention App 4.2 Appliance APP_ID \(VMware\)\'\"
 # LOCAL ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no build@docker.knut.univention.de "test -e /tmp/build-APP_ID/Univention-App-APP_ID-vmware.zip && rm /tmp/build-APP_ID/Univention-App-APP_ID-vmware.zip || true"
@@ -74,6 +128,7 @@ command2:
 # LOCAL ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no build@docker.knut.univention.de rm -f Univention-App-APP_ID-vmware.zip
 # LOCAL rm /tmp/build-APP_ID/Univention-App-APP_ID-vmware.zip
 #command6:
+
 ## virtualbox
 # LOCAL ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no build@docker.knut.univention.de guestfish add /tmp/build-APP_ID/master.qcow2 : run : mount /dev/mapper/vg_ucs-root / : command \"/usr/sbin/ucr set updater/identify=\'Univention App 4.2 Appliance APP_ID \(VirtualBox\)\'\"
 # LOCAL ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no build@docker.knut.univention.de "test -e Univention-App-APP_ID-virtualbox.ova && rm Univention-App-APP_ID-virtualbox.ova || true"
