b6094143f4e357e0a05e3b228c5179e7b2139f71
Felix Botner <botner@univention.de>
2018-04-20 16:12:48 +0200

    Bug #46487: appliance setup

---
+++ test/utils/installation_test/appliance-vnc-setup.py
@@ -0,0 +1,205 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+"""
+setup via umc
+"""
+
+from argparse import ArgumentParser
+from vncautomate import init_logger, VNCConnection
+from vncautomate.config import OCRConfig
+from vncdotool.api import VNCDoException
+
+from installation import UCSInstallation
+from components.components import components_with_steps as components
+
+import time
+import sys
+import os
+
+
+class UCSSetup(UCSInstallation):
+
+	def __init__(self, args):
+		init_logger('info')
+		self.args = args
+		self.config = OCRConfig()
+		self.config.update(lang='eng')
+		self.timeout = 40
+		self.connect()
+
+	def click(self, text):
+		self.client.waitForText(text, timeout=self.timeout)
+		self.client.mouseClickOnText(text)
+
+	def screenshot(self, filename):
+		if not os.path.isdir(self.args.screenshot_dir):
+			os.mkdir(self.args.screenshot_dir)
+		screenshot_file = os.path.join(self.args.screenshot_dir, filename)
+		self.client.captureScreen(screenshot_file)
+
+	def next(self):
+		self.client.waitForText('NEXT', timeout=self.timeout)
+		self.client.mouseClickOnText('NEXT')
+
+	def language(self, language):
+		self.client.waitForText('English', timeout=self.timeout)
+		self.screenshot('language-setup.png')
+		self.next()
+		self.client.waitForText('Default system locale', timeout=self.timeout)
+		self.next()
+
+	def network(self):
+		self.client.waitForText('IP address', timeout=self.timeout)
+		self.screenshot('network-setup.png')
+		if self.args.role == 'admember':
+			self.click('Preferred DNS')
+			self.client.enterText(self.args.dns)
+		self.next()
+		time.sleep(60)
+		# check APIPA warning (automatic private address for eth0 if no dhcp answer)
+		try:
+			self.client.waitForText('APIPA', timeout=self.timeout)
+			self.client.keyPress('enter')
+			time.sleep(60)
+		except VNCDoException:
+			self.connect()
+		try:
+			self.client.waitForText('No gateway has been', timeout=self.timeout)
+			self.client.keyPress('enter')
+			time.sleep(60)
+		except VNCDoException:
+			self.connect()
+		time.sleep(120)
+
+	def domain(self, role):
+		text = 'Manage users and permissions'
+		if self.args.ucs is True:
+			text = 'Create a new UCS domain'
+		if role == 'admember':
+			text = 'Join into an existing Microsoft Active'
+		elif role == 'join':
+			text = 'Join into existing UCS domain'
+		elif role == 'fast':
+			text = 'Fast demo'
+		self.client.waitForText(text, timeout=self.timeout)
+		self.client.mouseClickOnText(text, timeout=self.timeout)
+		self.screenshot('domain-setup.png')
+		self.next()
+
+	def orga(self, orga, password):
+		self.client.waitForText('Account information', timeout=self.timeout)
+		self.screenshot('organisation-setup.png')
+		self.client.enterText('home')
+		self.client.keyPress('tab')
+		self.client.keyPress('tab')
+		self.client.keyPress('tab')
+		self.client.enterText(password)
+		self.client.keyPress('tab')
+		self.client.enterText(password)
+		self.next()
+
+	def hostname(self, hostname):
+		self.client.waitForText('Host settings', timeout=self.timeout)
+		self.screenshot('hostname-setup.png')
+		# delete the pre-filled hostname
+		self.client.keyPress('end')
+		for i in range(1, 200):
+			self.client.keyPress('bsp')
+		time.sleep(3)
+		self.client.enterText(hostname)
+		self.client.keyPress('tab')
+		if self.args.role == 'admember':
+			self.client.keyPress('tab')
+			self.client.enterText(self.args.password)
+			self.client.keyPress('tab')
+			self.client.enterText(self.args.password)
+		self.next()
+
+	def start(self):
+		self.client.waitForText('confirm configuration', timeout=self.timeout)
+		self.screenshot('start-setup.png')
+		found = False
+		try:
+			self.client.mouseClickOnText('configuresystem')
+			found = True
+		except VNCDoException:
+			self.connect()
+		if not found:
+			self.client.mouseClickOnText('configure system')
+
+	def finish(self):
+		self.client.waitForText('Setup successful', timeout=3600)
+		self.screenshot('finished-setup.png')
+		self.client.keyPress('tab')
+		self.client.keyPress('enter')
+		# except welcome screen
+		found = False
+		try:
+			self.client.waitForText('www', timeout=self.timeout)
+			found = True
+		except VNCDoException:
+			self.connect()
+		if not found:
+			self.client.waitForText('press any key', timeout=self.timeout)
+		self.screenshot('welcome-screen.png')
+
+	def connect(self):
+		self.conn = VNCConnection(self.args.vnc)
+		self.client = self.conn.__enter__()
+		self.client.updateOCRConfig(self.config)
+
+	def setup(self):
+		try:
+			self.language('English')
+			self.network()
+			self.domain(self.args.role)
+			if self.args.role == 'admember':
+				self.client.waitForText('Active Directory join', timeout=self.timeout)
+				self.click('Username')
+				self.client.enterText(self.args.join_user)
+				self.click('Password')
+				self.client.enterText(self.args.join_password)
+				self.next()
+			else:
+				self.orga(self.args.organisation, self.args.password)
+			if not self.args.role == 'fast':
+				self.hostname(self.args.fqdn)
+			if self.args.ucs is True:
+				self.select_components()
+				self.next()
+			self.start()
+			time.sleep(900)
+			self.finish()
+		except Exception:
+			self.connect()
+			self.screenshot('error.png')
+			raise
+
+
+def main():
+	''' python %prog% --vnc 'utby:1' '''
+	description = sys.modules[__name__].__doc__
+	parser = ArgumentParser(description=description)
+	parser.add_argument('--vnc')
+	parser.add_argument('--fqdn', default='master.ucs.local')
+	parser.add_argument('--dns')
+	parser.add_argument('--join-user')
+	parser.add_argument('--join-password')
+	parser.add_argument('--password', default='univention')
+	parser.add_argument('--organisation', default='ucs')
+	parser.add_argument('--screenshot-dir', default='../screenshots')
+	parser.add_argument('--role', default='master', choices=['master', 'admember', 'fast'])
+	parser.add_argument('--ucs', help='ucs appliance', action='store_true')
+	parser.add_argument('--components', default=[], choices=components.keys() + ['all'], action='append')
+	args = parser.parse_args()
+	if args.role in ['admember']:
+		assert args.dns is not None
+		assert args.join_user is not None
+		assert args.join_password is not None
+	assert args.vnc is not None
+	setup = UCSSetup(args=args)
+	setup.setup()
+
+if __name__ == '__main__':
+	main()
---
+++ test/utils/prepare-ucs-appliance-for-tests.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export KVM_SERVER="$KVM_SERVER"
+export KVM_USER="$KVM_USER"
+export UCS_VERSION="$UCS_VERSION"
+export TEST_ERRATA="true"
+
+_ssh () {
+	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -n -l "$KVM_USER" "$KVM_SERVER" "$@"
+}
+
+# update the image on the kvm server and exit
+ucs_template="/var/univention/buildsystem2/temp/build/appliance/UCS-KVM-Demo-Image.qcow2"
+kvm_template_dir="/var/lib/libvirt/templates/single/Others/appliance_ucsa_amd64/"
+template_name="$(basename $ucs_template)"
+kvm_template="$kvm_template_dir/$template_name"
+xml_template="/mnt/omar/vmwares/kvm/single/Others/ucs_appliance_template.xml"
+kvm_xml="$kvm_template_dir/appliance_ucsa_amd.xml"
+
+# check disk space
+stat=$(_ssh stat -f -c '%a*%S' /var/lib/libvirt)
+if [ "$((${stat}>>30))" -lt 20 ]; then
+	echo "ERORR: Not enough Space on $KVM_SERVER! Aborting..."
+	exit 1
+fi
+
+# check if instances still running
+existing_instances=$(_ssh virsh  list --all| grep appliance-test-ucs || true)
+if [ -n "$existing_instances" ]; then
+	echo "ERROR: existing instances on $KVM_SERVER ($existing_instances)! Aborting ..."
+	exit 1
+fi
+
+# check if template is used as backing file
+_ssh "
+while read image; do
+	if qemu-img info \"\$image\" | grep -i \"backing file\" | grep \"$template_name\"; then
+		echo "ERROR: $template_name is backing file for \$image"
+		exit 1
+	fi
+done < <(ls /var/lib/libvirt/images/*)"
+
+# check if update is necessary
+appliance_md5=$(_ssh cat "$ucs_template.md5" || true)
+kvm_md5=$(_ssh cat "$kvm_template.md5" || true)
+if [ "$appliance_md5" != "$kvm_md5" ]; then
+	# copy image
+	_ssh mkdir -p "$kvm_template_dir"
+	_ssh "chmod g+w -R $kvm_template_dir"
+	_ssh cp "$ucs_template" "$kvm_template"
+	_ssh cp "$ucs_template.md5" "$kvm_template.md5"
+fi
+
+# cp xml
+_ssh cp "$xml_template" "$kvm_xml"
+
+# fake uvmm template
+_ssh touch /mnt/omar/vmwares/kvm/single/Others/appliance_ucsa_amd64.tar.gz
+
+# prepare image
+_ssh "guestfish add $kvm_template : set-network true : run : mount /dev/mapper/vg_ucs-root / : \
+	command 'ucr unset --force auth/sshd/user/root' : \
+	command 'ucr set umc/module/debug/level=4 umc/server/debug/level=4' : \
+	mkdir-p /root/.ssh/ : \
+	write /root/.ssh/authorized_keys 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKxi4dwmF9K7gV4JbQUmQ4ufRHcxYOYUHWoIRuj8jLmP1hMOqEmZ43rSRoe2E3xTNg+RAjwkX1GQmWQzzjRIYRpUfwLo+yEXtER1DCDTupLPAT5ulL6uPd5mK965vbE46g50LHRyTGZTbsh1A/NPD7+LNBvgm5dTo/KtMlvJHWDN0u4Fwix2uQfvCSOpF1n0tDh0b+rr01orITJcjuezIbZsArTszA+VVJpoMyvu/I3VQVDSoHB+7bKTPwPQz6OehBrFNZIp4zl18eAXafDoutTXSOUyiXcrViuKukRmvPAaO8u3+r+OAO82xUSQZgIWQgtsja8vsiQHtN+EtR8mIn tech' : \
+"
+	# TODO Update
+	#command 'ucr set repository/online=yes' : \
+	#write /etc/apt/sources.list.d/test.list 'deb http://192.168.0.10/build2/ ucs_4.3-0-errata4.3-0/all/' : \
+	#write-append /etc/apt/sources.list.d/test.list 'deb http://192.168.0.10/build2/ ucs_4.3-0-errata4.3-0/\$(ARCH)/' : \
+
+
+
+exit 0
