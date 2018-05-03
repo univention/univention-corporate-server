1c790808e8875a67fb229b4404ce803cd39f670b
Fathan Vidjaja <fvidjaja@alvine.knut.univention.de>
2018-05-03 14:12:30 +0200

    Bug #46802 : modify domain-join for non Takeover samba tests

--- test/utils/samba-utils.sh
+++ test/utils/samba-utils.sh
@@ -6,6 +6,10 @@ function create_gpo {
     samba-tool gpo create $1 -U $2 --password=$3
 }
 
+function check_gpos {
+	echo "WIP"
+}
+
 function link_gpo_to_container {
     gpo=$(samba-tool gpo listall | grep $1 -B 1 | grep GPO | grep -oE "[^ ]+$")
     samba-tool gpo setlink $2 $gpo -U $3 --password=$4
