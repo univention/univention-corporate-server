2c3f82de806225ea60af7ea1e7a6b9da9ed6e322
Fathan Vidjaja <fvidjaja@alvine.knut.univention.de>
2018-05-03 14:11:09 +0200

    Bug #45818 : added checks in the UCS system for GPOs and user

--- test/utils/ad-takeover.sh
+++ test/utils/ad-takeover.sh
@@ -90,13 +90,17 @@ check_ad_takeover_product_test () {
 	python shared-utils/ucs-winrm.py change-user-password --domainuser benutzer11 --userpassword "$new_password" --client "$WIN1"
 	python shared-utils/ucs-winrm.py domain-user-validate-password --domainuser benutzer11 --domainpassword "$new_password" --client "$WIN1"
 	# TODO check ucs
-
+	check_user_in_ucs benutzer11 $new_password adtakeover
+	udm groups/group modify \
+		--dn "cn=Domänen-Admins,cn=groups,dc=adtakeover,dc=local" \
+		--append users="uid=benutzer4,cn=users,dc=adtakeover,dc=local"
 	# Gruppenrichtlinienverwaltung (GPMC) starten, neue GPO anlegen.
 	# Name des Windows-Clients ändern, dann reboot
 	python shared-utils/ucs-winrm.py create-gpo --name NewGPO --comment "testing new GPO in domain" --client "$WIN1" --credssp
 	python shared-utils/ucs-winrm.py link-gpo --name NewGPO --target "$LDAP_BASE" --client "$WIN1" --credssp
 	python shared-utils/ucs-winrm.py run-ps --client "$WIN1" --credssp \
 		--cmd 'set-GPPrefRegistryValue -Name NewGPO -Context User -key "HKCU\Environment" -ValueName NewGPO -Type String -value NewGPO -Action Update' --client "$WIN1" --credssp
+ 	. ad-takeover.sh; check_GPO NewGPO
 	python shared-utils/ucs-winrm.py rename-computer --name mycom --domainmode --credssp --client "$WIN1"
 	# AD Benutzer&Computer: Neuen Benutzer anlegen.
 	# Client-Anmeldung als neuer Benutzer: GPO Auswertung OK?
@@ -105,13 +109,20 @@ check_ad_takeover_product_test () {
 		--usergpo 'TestGPOUser' --usergpo 'Default Domain Policy' --usergpo 'NewGPO' \
 		--computergpo 'TestGPOMachine' --computergpo 'Default Domain Policy'
 	# TODO check ucs
+		
+ 	#. ad-takeover.sh; check_user_in_ucs user10
+	#. ad-takeover.sh; check_user_in_ucs user1
+	check_user_in_ucs benutzer1 "Univention@99" adtakeover
+	check_user_in_ucs benutzer1500 "Univention@99" adtakeover
 
 	# Login an UMC als weiteres Mitglied der AD-Gruppe "Domain Admins" / "Domänen Administratoren".
 	# TODO
-
 	# Anlegen eines neuen Benutzers per UMC, Anmeldung mit dem neuen Benutzer am Windows Client
 	# TODO
-
+	udm users/user create --position "cn=users,dc=adtakeover,dc=local" --set username="newuser01" --set firstname="Random" --set lastname="User" --set password="Univention.99"
+	python shared-utils/ucs-winrm.py check-applied-gpos --client "$WIN1" --username 'newuser01' --userpwd "Univention.99" \
+		--usergpo 'TestGPOUser' --usergpo 'Default Domain Policy' --usergpo 'NewGPO' \
+		--computergpo 'TestGPOMachine' --computergpo 'Default Domain Policy'
 	# Joinen eines weiteren Windows Clients in die (UCS-)Domäne
 	python shared-utils/ucs-winrm.py domain-join --dnsserver "$UCS" --client "$WIN2" --user "$WIN2_ADMIN" --password "$WIN2_PASSWORD" --domainpassword "$AD_PASSWORD" --domainuser "$AD_ADMIN"
 
@@ -120,70 +131,51 @@ check_ad_takeover_product_test () {
 		--usergpo 'TestGPOUser' --usergpo 'Default Domain Policy' --usergpo 'NewGPO' \
 		--computergpo 'TestGPOMachine' --computergpo 'Default Domain Policy'
 
- #. ad-takeover.sh; check_GPO TestGPO
-
- #udm users/user modify --dn uid=benutzer1,cn=users,dc=adtakeover,dc=local --set password='univention' --set overridePWHistory=1 --set overridePWLength=1
- #udm users/user create --position "cn=users,dc=adtakeover,dc=local" --set username="newuser01" --set firstname="Random" --set lastname="User" --set password="Univention.99"
- #python shared-utils/ucs-winrm.py reboot
- #python shared-utils/ucs-winrm.py wait_for_client
- #LOCAL sleep 150
-
- #python shared-utils/ucs-winrm.py domain-join --domain adtakeover.local --dnsserver [master231_IP] --domainuser benutzer2 --domainpassword 'Univention@99'
-
- #. ad-takeover.sh; check_user_in_ucs user10
- #. ad-takeover.sh; check_user_in_ucs user1
- #. ad-takeover.sh; check_user_in_ucs benutzer1
- #. ad-takeover.sh; check_user_in_ucs benutzer1500
-
- #. ad-takeover.sh; check_user_in_winclient newuser01 'Univention.99'
-
- #. ad-takeover.sh; check_GPO_for_user TestGPO user1
- #. ad-takeover.sh; check_GPO_for_user TestGPO1 user10
- #. ad-takeover.sh; check_GPO_for_user TestGPO user1
- #. ad-takeover.sh; check_GPO_for_user TestGPO1 user10
-#TODO A better check on client for applied GPOs
 }
 
 function check_all_user {
-    for value in {1..10}
-    do
-        python shared-utils/ucs-winrm.py domain_user_validate_password --domain adtakeover.local --domainuser user$value --domainpassword Univention@$value
-    done
+	for value in {1..10}
+	do
+		python shared-utils/ucs-winrm.py domain_user_validate_password --domain adtakeover.local --domainuser user$value --domainpassword Univention@$value
+	done
 }
 
 function check_user_in_winclient {
- python shared-utils/ucs-winrm.py domain_user_validate_password --domain adtakeover.local --domainuser $1 --domainpassword $2
+	python shared-utils/ucs-winrm.py domain_user_validate_password --domain adtakeover.local --domainuser $1 --domainpassword $2
 }
 
 function check_user_in_ucs {
-command=`udm users/user list --filter uid=$1`
-if echo "$command" | grep -q "username"
-then
-    echo "user in DC found"
-else
-    echo "user in DC not found"
-    exit 1
-fi
+	command=`udm users/user list --filter uid=$1`
+	echo $2 > pwd
+	kinit --password-file=pwd $1
+	ldbsearch --user=$3/$1%$2 -H ldap://127.0.0.1
+	if echo "$command" | grep -q "username"
+	then
+		echo "user in DC found"
+	else
+		echo "user in DC not found"
+		exit 1
+	fi
 }
 
 function check_GPO {
-command=`samba-tool gpo listall`
-if echo "$command" | grep -q "$1"
-then
-    echo "GPO found"
-else
-    echo "GPO not found"
-    exit 1
-fi
+	command=`samba-tool gpo listall`
+	if echo "$command" | grep -q "$1"
+	then
+		echo "GPO found"
+	else
+		echo "GPO not found"
+		exit 1
+	fi
 }
 
 function check_GPO_for_user {
-command=`samba-tool gpo list $2`
-if  echo $command | grep -q "$1"
-then
-    echo "GPO for user found"
-else
-    echo "GPO for user not found"
-    exit 1
-fi
+	command=`samba-tool gpo list $2`
+	if  echo $command | grep -q "$1"
+	then
+		echo "GPO for user found"
+	else
+		echo "GPO for user not found"
+		exit 1
+	fi
 }
