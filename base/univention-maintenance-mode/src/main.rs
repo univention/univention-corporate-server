1be709a2d8d0644881eb5bd89b2a31dfa863f43d
Dirk Wiesenthal <wiesenthal@univention.de>
2018-05-10 00:56:31 +0200

    Bug #37223: Prototype for a univention-maintenance-mode

--- base/univention-maintenance-mode/src/main.rs
+++ base/univention-maintenance-mode/src/main.rs
@@ -1,5 +1,5 @@
 // Univention Maintenance Mode
-//  Minimal web server
+//  Write progress of univention-updater in JSON file
 //
 // Copyright 2018 Univention GmbH
 //
@@ -29,36 +29,49 @@
 // <http://www.gnu.org/licenses/>.
 //
 
-extern crate gotham;
-extern crate hyper;
-extern crate mime;
+use std::io::prelude::*;
+use std::fs::File;
+use std::io;
+use std::io::BufReader;
+use std::time::Duration;
+use std::thread::sleep;
 
-use hyper::{Response, StatusCode};
+pub fn read_progress() -> io::Result<f32> {
+    let mut ret = 0.0;
+    let infile = File::open("/var/lib/univention-updater/univention-updater.status.details")?;
+    for line in BufReader::new(infile).lines() {
+        if let Ok(line) = line {
+            let bits: Vec<&str> = line.split(":").collect();
+            if bits.len() >= 3 {
+                if bits[0] == "pmstatus" {
+                    ret = match bits[2].parse::<f32>() {
+                        Ok(val) => val,
+                        Err(_) => ret
+                    };
+                }
+            }
+        }
+    }
+    Ok(ret)
+}
 
-use gotham::http::response::create_response;
-use gotham::state::State;
-
-pub fn say_hello(state: State) -> (State, Response) {
-    let res = create_response(
-        &state,
-        StatusCode::Ok,
-        Some((String::from("
-<html>
-    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />
-    <title>UCS in Maintenance Mode</title>
-    <body>
-        <h1>UCS in Maintenance Mode</h1>
-        <p>UCS is currently in Maintenance Mode. Important services like Univention Management Console (UMC) or even Apache are stopped or may not work correctly, so instead, this page is shown. New software is being installed and everything is going right. Please be patient while the system is updated.
-    </body>
-</html>").into_bytes(), mime::TEXT_HTML)),
-    );
-
-    (state, res)
+pub fn write_json(percentage: f32) -> io::Result<()> {
+    let mut outfile = File::create("/var/www/univention/maintenance/updater.json")?;
+    write!(&mut outfile, "{{\"v1\":{{\"percentage\":{}}}}}", percentage)?;
+    Ok(())
 }
 
 pub fn main() {
-    let addr = "0.0.0.0:80";
-    println!("Listening for requests at http://{}", addr);
-    gotham::start(addr, || Ok(say_hello))
+    loop {
+        match read_progress() {
+            Ok(percentage) => match write_json(percentage) {
+                Ok(_) => {},
+                Err(err) => println!("univention-maintenance-mode: Error while writing json: {:?}", err)
+            },
+            Err(err) => println!("univention-maintenance-mode: Error while reading progress: {:?}", err)
+        };
+        let duration = Duration::from_millis(2000);
+        sleep(duration);
+    }
 }
 
