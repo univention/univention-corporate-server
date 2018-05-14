67e67db1c81250ace2fa733ce35a06a7d6c0606f
Felix Botner <botner@univention.de>
2018-05-14 17:30:30 +0200

    Bug #45818: check ucs access

--- test/utils/ad-takeover.sh
+++ test/utils/ad-takeover.sh
@@ -79,28 +79,32 @@ check_ad_takeover_product_test () {
 	# Login am übernommenen Windows-Client als "Domänen\Administrator"
 	python shared-utils/ucs-winrm.py check-applied-gpos --client "$WIN1" --username 'Administrator' --userpwd "$AD_PASSWORD" \
 		--usergpo 'TestGPOUser' --usergpo 'Default Domain Policy' --computergpo 'TestGPOMachine' --computergpo 'Default Domain Policy'
-	for user in 150 3 1000 1500; do
+	for user in 1 3 150 1000 1500; do
 		python shared-utils/ucs-winrm.py check-applied-gpos --client "$WIN1" --username "benutzer$user" --userpwd 'Univention@99' \
 		--usergpo 'TestGPOUser' --usergpo 'Default Domain Policy'
+		check_user_in_ucs "benutzer$user" "Univention@99"
 	done
-	# TODO check ucs
+	for i in $(seq 1 1500); do
+		udm users/user list --filter username="uid=benutzer$i" | grep "^DN: "
+	done
+	for i in $(seq 1 40); do
+		udm groups/group list --filter name="gruppe$i" | grep "^DN: "
+	done
+	# TODO check group membership
 
 	# Passwortänderung muss funktionieren
 	local new_password='üUnivention@90809098798'
 	python shared-utils/ucs-winrm.py change-user-password --domainuser benutzer11 --userpassword "$new_password" --client "$WIN1"
 	python shared-utils/ucs-winrm.py domain-user-validate-password --domainuser benutzer11 --domainpassword "$new_password" --client "$WIN1"
-	# TODO check ucs
-	check_user_in_ucs benutzer11 $new_password adtakeover
-	udm groups/group modify \
-		--dn "cn=Domänen-Admins,cn=groups,dc=adtakeover,dc=local" \
-		--append users="uid=benutzer4,cn=users,dc=adtakeover,dc=local"
+	check_user_in_ucs benutzer11 "$new_password"
+
 	# Gruppenrichtlinienverwaltung (GPMC) starten, neue GPO anlegen.
 	# Name des Windows-Clients ändern, dann reboot
 	python shared-utils/ucs-winrm.py create-gpo --name NewGPO --comment "testing new GPO in domain" --client "$WIN1" --credssp
 	python shared-utils/ucs-winrm.py link-gpo --name NewGPO --target "$LDAP_BASE" --client "$WIN1" --credssp
 	python shared-utils/ucs-winrm.py run-ps --client "$WIN1" --credssp \
 		--cmd 'set-GPPrefRegistryValue -Name NewGPO -Context User -key "HKCU\Environment" -ValueName NewGPO -Type String -value NewGPO -Action Update' --client "$WIN1" --credssp
- 	. ad-takeover.sh; check_GPO NewGPO
+	samba-tool gpo listall | grep NewGPO
 	python shared-utils/ucs-winrm.py rename-computer --name mycom --domainmode --credssp --client "$WIN1"
 	# AD Benutzer&Computer: Neuen Benutzer anlegen.
 	# Client-Anmeldung als neuer Benutzer: GPO Auswertung OK?
@@ -108,24 +112,19 @@ check_ad_takeover_product_test () {
 	python shared-utils/ucs-winrm.py check-applied-gpos --client "$WIN1" --username 'new1' --userpwd "Univention@99" \
 		--usergpo 'TestGPOUser' --usergpo 'Default Domain Policy' --usergpo 'NewGPO' \
 		--computergpo 'TestGPOMachine' --computergpo 'Default Domain Policy'
-	# TODO check ucs
-		
- 	#. ad-takeover.sh; check_user_in_ucs user10
-	#. ad-takeover.sh; check_user_in_ucs user1
-	check_user_in_ucs benutzer1 "Univention@99" adtakeover
-	check_user_in_ucs benutzer1500 "Univention@99" adtakeover
 
 	# Login an UMC als weiteres Mitglied der AD-Gruppe "Domain Admins" / "Domänen Administratoren".
-	# TODO
+	check_admin_umc benutzer1 "Univention@99"
+	check_admin_umc benutzer2 "Univention@99"
+
 	# Anlegen eines neuen Benutzers per UMC, Anmeldung mit dem neuen Benutzer am Windows Client
-	# TODO
 	udm users/user create --position "cn=users,dc=adtakeover,dc=local" --set username="newuser01" --set firstname="Random" --set lastname="User" --set password="Univention.99"
 	python shared-utils/ucs-winrm.py check-applied-gpos --client "$WIN1" --username 'newuser01' --userpwd "Univention.99" \
 		--usergpo 'TestGPOUser' --usergpo 'Default Domain Policy' --usergpo 'NewGPO' \
 		--computergpo 'TestGPOMachine' --computergpo 'Default Domain Policy'
+
 	# Joinen eines weiteren Windows Clients in die (UCS-)Domäne
 	python shared-utils/ucs-winrm.py domain-join --dnsserver "$UCS" --client "$WIN2" --user "$WIN2_ADMIN" --password "$WIN2_PASSWORD" --domainpassword "$AD_PASSWORD" --domainuser "$AD_ADMIN"
-
 	# Anmeldung als übernommener Benutzer, GPO's?
 	python shared-utils/ucs-winrm.py check-applied-gpos --client "$WIN2" --username 'benutzer13' --userpwd "Univention@99" \
 		--usergpo 'TestGPOUser' --usergpo 'Default Domain Policy' --usergpo 'NewGPO' \
@@ -133,49 +132,26 @@ check_ad_takeover_product_test () {
 
 }
 
-function check_all_user {
-	for value in {1..10}
-	do
-		python shared-utils/ucs-winrm.py domain_user_validate_password --domain adtakeover.local --domainuser user$value --domainpassword Univention@$value
-	done
+check_user_in_ucs () {
+	local username="$1"
+	local password="$2"
+	local binddn="$(univention-ldapsearch uid="$username" dn | sed -ne 's|dn: ||p')"
+	# nss/pam
+	getent passwd | grep -w "$username"
+	su "$username" -c "exit"
+	# kerberos
+	echo "$password" > /tmp/pwdfile
+	kinit --password-file=/tmp/pwdfile $1
+	# ucs ldap
+	univention-ldapsearch -D "$binddn" -w "$password" "uid=$username"
+	# samba/ldap
+	ldbsearch -U "$username"%"$password" -H ldap://127.0.0.1 "cn=$username"
+	smbclient -U "$username"%"$password" //$(hostname)/sysvol -c exit
 }
 
-function check_user_in_winclient {
-	python shared-utils/ucs-winrm.py domain-user-validate-password --domain adtakeover.local --domainuser $1 --domainpassword $2
-}
-
-function check_user_in_ucs {
-	command=`udm users/user list --filter uid=$1`
-	echo $2 > pwd
-	kinit --password-file=pwd $1
-	ldbsearch --user=$3/$1%$2 -H ldap://127.0.0.1
-	if echo "$command" | grep -q "username"
-	then
-		echo "user in DC found"
-	else
-		echo "user in DC not found"
-		exit 1
-	fi
-}
-
-function check_GPO {
-	command=`samba-tool gpo listall`
-	if echo "$command" | grep -q "$1"
-	then
-		echo "GPO found"
-	else
-		echo "GPO not found"
-		exit 1
-	fi
-}
-
-function check_GPO_for_user {
-	command=`samba-tool gpo list $2`
-	if  echo $command | grep -q "$1"
-	then
-		echo "GPO for user found"
-	else
-		echo "GPO for user not found"
-		exit 1
-	fi
+check_admin_umc () {
+	local username="$1"
+	local password="$2"
+	local binddn="$(univention-ldapsearch uid="$username" dn | sed -ne 's|dn: ||p')"
+	umc-command -U "$username" -P "$password" udm/get -f users/user -l -o "$binddn"
 }
