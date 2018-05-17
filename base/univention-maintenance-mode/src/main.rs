c39a43f965072f1154fc752335de8e09ac290d12
Dirk Wiesenthal <wiesenthal@univention.de>
2018-05-17 11:51:52 +0200

    Bug #37223: Prototype for a univention-maintenance-mode

--- base/univention-maintenance-mode/src/main.rs
+++ base/univention-maintenance-mode/src/main.rs
@@ -36,7 +36,11 @@ use std::io::BufReader;
 use std::time::Duration;
 use std::thread::sleep;
 
-pub fn read_progress() -> io::Result<f32> {
+fn read_progress() -> io::Result<f32> {
+    let updater_status = UpdaterStatus::new()?;
+    if updater_status.status == Some("DONE".to_string()) {
+        return Ok(0.0);
+    }
     let mut ret = 0.0;
     let infile = File::open("/var/lib/univention-updater/univention-updater.status.details")?;
     for line in BufReader::new(infile).lines() {
@@ -55,7 +59,92 @@ pub fn read_progress() -> io::Result<f32> {
     Ok(ret)
 }
 
-pub fn write_json(percentage: f32) -> io::Result<()> {
+#[derive(Debug)]
+struct UpdaterStatus {
+    current_version: Option<String>,
+    next_version: Option<String>,
+    target_version: Option<String>,
+    updatetype: Option<String>,
+    status: Option<String>,
+    errorsource: Option<String>,
+    overall_updates: Vec<String>,
+}
+
+impl UpdaterStatus {
+    pub fn finished_updates(&self) -> usize {
+        let finished_updates_index = self.overall_updates.iter().position(|x| Some(x.to_string()) == self.current_version);
+        match finished_updates_index {
+            Some(index) => index + 1,
+            None => 0
+        }
+    }
+
+    pub fn new() -> io::Result<Self> {
+        let status_file = File::open("/var/lib/univention-updater/univention-updater.status")?;
+        let mut current_version: Option<String> = None;
+        let mut next_version = None;
+        let mut target_version = None;
+        let mut updatetype = None;
+        let mut status = None;
+        let mut errorsource = None;
+        for line in BufReader::new(status_file).lines() {
+            if let Ok(line) = line {
+                let bits: Vec<&str> = line.split("=").collect();
+                if line.starts_with("current_version=") {
+                    current_version = Some(String::from(bits[1]));
+                }
+                if line.starts_with("next_version=") {
+                    next_version = Some(String::from(bits[1]));
+                }
+                if line.starts_with("target_version=") {
+                    target_version = Some(String::from(bits[1]));
+                }
+                if line.starts_with("type=") {
+                    updatetype = Some(String::from(bits[1]));
+                }
+                if line.starts_with("status=") {
+                    status = Some(String::from(bits[1]));
+                }
+                if line.starts_with("errorsource=") {
+                    errorsource = Some(String::from(bits[1]));
+                }
+            }
+        }
+		let mut release_file = File::open("/var/lib/univention-updater/univention-updater.releases")?;
+		let mut contents = String::new();
+		release_file.read_to_string(&mut contents)?;
+        let mut overall_updates = Vec::new();
+        for ver in contents.split(",") {
+            if Some(ver.to_string()) == target_version {
+                break;
+            }
+            overall_updates.push(ver.to_string());
+        }
+
+        let status = UpdaterStatus {
+            current_version: current_version,
+            next_version: next_version,
+            target_version: target_version,
+            updatetype: updatetype,
+            status: status,
+            errorsource: errorsource,
+            overall_updates: overall_updates,
+        };
+        println!("Status: {:?}", status);
+        Ok(status)
+    }
+}
+fn add_updater_context(percentage: f32) -> io::Result<f32> {
+    let updater_status = UpdaterStatus::new()?;
+    if updater_status.target_version == updater_status.current_version {
+        return Ok(100.0);
+    }
+    let finished_updates = updater_status.finished_updates() as f32;
+    let ret = (finished_updates * 100.0 + percentage) / (updater_status.overall_updates.len() as f32);
+    Ok(ret)
+}
+
+fn write_json(percentage: f32) -> io::Result<()> {
     let mut outfile = File::create("/var/www/univention/maintenance/updater.json")?;
     write!(&mut outfile, "{{\"v1\":{{\"percentage\":{}}}}}", percentage)?;
     Ok(())
@@ -64,9 +153,12 @@ pub fn write_json(percentage: f32) -> io::Result<()> {
 pub fn main() {
     loop {
         match read_progress() {
-            Ok(percentage) => match write_json(percentage) {
-                Ok(_) => {},
-                Err(err) => println!("univention-maintenance-mode: Error while writing json: {:?}", err)
+            Ok(percentage) => match add_updater_context(percentage) {
+                Ok(percentage) => match write_json(percentage) {
+                    Ok(_) => {},
+                    Err(err) => println!("univention-maintenance-mode: Error while writing json: {:?}", err)
+                },
+                Err(err) => println!("univention-maintenance-mode: Error while adding context: {:?}", err)
             },
             Err(err) => println!("univention-maintenance-mode: Error while reading progress: {:?}", err)
         };
