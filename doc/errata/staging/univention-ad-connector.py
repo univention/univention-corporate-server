47e3e3f179680c54880314d8cfcccdaefe2071c3
Arvid Requate <requate@univention.de>
2018-07-26 10:25:10 +0200

    Bug #47232: Advisory

--- doc/errata/staging/univention-ad-connector.py
+++ doc/errata/staging/univention-ad-connector.py
@@ -3,10 +3,10 @@ release: "4.3"
 version: [1]
 scope: ucs_4.3-0-errata4.3-1
 src: univention-ad-connector
-fix: 12.0.0-13A~4.3.0.201807251854
+fix: 12.0.0-14A~4.3.0.201807261024
 desc: |-
-  This update addresses the following issue(s):
-  * For new tools have beeen added:
+  This update addresses the following issue:
+  * Four new tools have beeen added:
     - resync_object_from_ad.py
     - resync_object_from_ucs.py
     - remove_ad_rejected.py
