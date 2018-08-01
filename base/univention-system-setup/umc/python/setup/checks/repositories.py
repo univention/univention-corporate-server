73118c28bc3d6a9295c5507384558a131da09aa7
Richard Ulmer <ulmer@univention.de>
2018-08-01 10:30:05 +0200

    Bug #47105: Log returncode if curl fails

--- base/univention-system-setup/umc/python/setup/checks/repositories.py
+++ base/univention-system-setup/umc/python/setup/checks/repositories.py
@@ -1,5 +1,6 @@
 import subprocess
 
+from univention.management.console.log import MODULE
 import univention.config_registry
 
 UCR = univention.config_registry.ConfigRegistry()
@@ -17,6 +18,7 @@ def get_unreachable_repository_servers():
 
 	processes = start_curl_processes(servers)
 	wait_for_processes_to_finish(processes)
+	log_warnings_about_unreachable_repository_servers(zip(servers, processes))
 	return [server for server, process in zip(servers, processes) if process.returncode != 0]
 
 def start_curl_processes(servers):
@@ -25,3 +27,12 @@ def start_curl_processes(servers):
 def wait_for_processes_to_finish(processes):
 	for process in processes:
 		process.wait()
+
+def log_warnings_about_unreachable_repository_servers(servers_with_curl_processes):
+	for server, process in servers_with_curl_processes:
+		if process.returncode != 0:
+			MODULE.warn(
+				# FIXME: When changing to Python 3 use process.args here.
+				"'curl --max-time 10 %s' exited with returncode %s." %
+				(server, process.returncode)
+			)
