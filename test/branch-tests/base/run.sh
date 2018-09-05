828a96e9a0f9acaec0a8a8fa1d6b9abc505b847a
Felix Botner <botner@univention.de>
2018-09-05 15:21:00 +0200

    Bug #47757: added branch tests

---
+++ test/branch-tests/base/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x -e
+export KVM_USER="build"
+
+cd test
+
+# update packages
+#./build-branch-packages -g $GIT_BRANCH
+cfg="branch-tests/base/$CFG_FILE"
+declare -a cmd=("./ucs-ec2-tools/ucs-kvm-create" "-c" "branch-tests/$CFG_FILE")
+"$HALT" && cmd+=("-t")
+"${cmd[@]}"
+test -e "./COMMAND_SUCCESS"
