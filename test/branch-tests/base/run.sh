18ddcca7cbaa10bb9f0dd17304f5934849e9a6ca
Erik Damrose <damrose@univention.de>
2018-09-06 12:08:41 +0200

    Bug #47757: added branch tests

--- test/branch-tests/base/run.sh
+++ test/branch-tests/base/run.sh
@@ -6,8 +6,9 @@ export KVM_USER="build"
 cd test
 
 # update packages
-#./build-branch-packages -g $GIT_BRANCH
-echo "# ignore" > utils/apt-get-branch-repo.list
+#./utils/build-branch-packages -g $GIT_BRANCH
+ssh jenkins@10.200.4.18 python3 build -r "https://gitlab+deplay-token-2:FAd2WHRZQqYFzLMZd5Q1@git.knut.univention.de/univention/ucs.git" -b "$GIT_BRANCH" > utils/apt-get-branch-repo.list
+# echo "# ignore" > utils/apt-get-branch-repo.list
 
 declare -a cmd=("./ucs-ec2-tools/ucs-kvm-create" "-c" "branch-tests/base/$CFG_FILE")
 "$HALT" && cmd+=("-t")
