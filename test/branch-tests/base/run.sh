0d915177b49aa99438297ef823388a60952749a2
Erik Damrose <damrose@univention.de>
2018-09-06 13:09:32 +0200

    Bug #47757: fix run.sh, make building packages work

--- test/branch-tests/base/run.sh
+++ test/branch-tests/base/run.sh
@@ -6,8 +6,7 @@ export KVM_USER="build"
 cd test
 
 # update packages
-#./utils/build-branch-packages -g $GIT_BRANCH
-ssh jenkins@10.200.4.18 python3 build -r "https://gitlab+deplay-token-2:FAd2WHRZQqYFzLMZd5Q1@git.knut.univention.de/univention/ucs.git" -b "$GIT_BRANCH" > utils/apt-get-branch-repo.list
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no jenkins@10.200.18.180 python3 /home/jenkins/build -r "https://gitlab+deplay-token-2:FAd2WHRZQqYFzLMZd5Q1@git.knut.univention.de/univention/ucs.git" -b "$GIT_BRANCH" > utils/apt-get-branch-repo.list
 # echo "# ignore" > utils/apt-get-branch-repo.list
 
 declare -a cmd=("./ucs-ec2-tools/ucs-kvm-create" "-c" "branch-tests/base/$CFG_FILE")
