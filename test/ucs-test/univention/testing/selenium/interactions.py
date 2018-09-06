bdef0c216ee86de3385bdedb7a6af7a6112e3ce0
Ole Schwiegert <schwiegert@univention.de>
2018-09-06 12:21:01 +0200

    Bug #47735: Add scroll down function and test correct process representation

--- test/ucs-test/univention/testing/selenium/interactions.py
+++ test/ucs-test/univention/testing/selenium/interactions.py
@@ -227,3 +227,25 @@ class Interactions(object):
 		except selenium_exceptions.StaleElementReferenceException:
 			pass
 		return False
+
+	def scroll_down(self):
+		"""
+		This function scrolls the page down until the end of the page is reached.
+		"""
+		SCROLL_PAUSE_TIME = 0.5
+
+		# Get scroll height
+		last_height = self.driver.execute_script("return document.body.scrollHeight")
+
+		while True:
+			# Scroll down to bottom
+			self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+			# Wait to load page
+			time.sleep(SCROLL_PAUSE_TIME)
+
+			# Calculate new scroll height and compare with last scroll height
+			new_height = self.driver.execute_script("return document.body.scrollHeight")
+			if new_height == last_height:
+				break
+			last_height = new_height
