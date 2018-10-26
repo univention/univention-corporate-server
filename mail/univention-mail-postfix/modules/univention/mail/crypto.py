8d438f0f7da3aedd77810ebd246bb6c814965356
Daniel Troeder <troeder@univention.de>
2018-10-26 10:51:04 +0200

    Bug #40609: generalize crypto class, add en/decryption

--- mail/univention-mail-postfix/modules/univention/mail/crypto.py
+++ mail/univention-mail-postfix/modules/univention/mail/crypto.py
@@ -28,12 +28,11 @@
 # <http://www.gnu.org/licenses/>.
 
 """
-Create signature of texts. Create, store and load keys.
+* Encrypt, decrypt and sign using RSA.
+* Create, store and load RSA keys.
 """
 
 import os
-import grp
-import pwd
 import stat
 import hashlib
 from cryptography.hazmat.backends import default_backend
@@ -41,19 +40,19 @@ from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding, rsa, utils
 from cryptography.exceptions import InvalidSignature
 
+try:
+	from typing import Optional
+except ImportError:
+	pass
 
-class Signature(object):
+
+class SimpleRSACryptography(object):
 	"""
 	Create signature of texts. Create, store and load keys.
 	"""
 	key_size = 2048
 
-	def __init__(self, key_file):  # type: (str) -> None
-		"""
-		:param key_file: path to a file from which to load or store in the
-			key pair
-		"""
-		self.key_file = key_file
+	def __init__(self):  # type: () -> None
 		self.private_key = None  # type: rsa.RSAPrivateKey
 
 	def create_keys(self):  # type: () -> None
@@ -69,33 +68,42 @@ class Signature(object):
 			backend=default_backend()
 		)
 
-	def store_keys(self):  # type: () -> None
+	def store_keys(self, key_file, uid, gid, mode=None):  # type: (str, int, int, Optional[int]) -> None
 		"""
-		Save :py:attr:`self.private_key` and :py:attr:`self.public_key` in
-		:py:attr:`self.key_file`.
+		Save :py:attr:`self.private_key` in `key_file`. The public key does
+		not need to be saved, it can be generated from the secret key.
 
 		Warning: will overwrite an existing file.
 
+		:param str key_file: file to store secret key in
+		:param int uid: owner of file
+		:param int gid: group of file
+		:param int mode: file permissions, 600 if not set
 		:return: None
-		:raises IOError: if :py:attr:`self.key_file` cannot be written to
+		:raises IOError: if `key_file` cannot be written to
 		"""
 		assert self.private_key is not None
-		uid = pwd.getpwnam('listfilter').pw_uid
-		gid = grp.getgrnam('nogroup').gr_gid
-		with open(self.key_file, 'wb') as fp:
+		assert isinstance(uid, int)
+		assert isinstance(gid, int)
+		assert isinstance(mode, int) or mode is None
+
+		mode = mode or stat.S_IRUSR | stat.S_IWUSR
+		with open(key_file, 'wb') as fp:
 			os.fchown(fp.fileno(), uid, gid)
-			os.fchmod(fp.fileno(), stat.S_IRUSR | stat.S_IWUSR)
+			os.fchmod(fp.fileno(), mode)
 			fp.write(self.private_key_as_str)
 
-	def load_keys(self):  # type: () -> None
+	def load_keys(self, key_file):  # type: (str) -> None
 		"""
-		Load private and public keys from :py:attr:`self.key_file` and store
-			them in :py:attr:`self.private_key` and :py:attr:`self.public_key`.
+		Load private key from `key_file` (public key will be generated from
+		it) and store them in :py:attr:`self.private_key` and
+		:py:attr:`self.public_key`.
 
+		:param str key_file: file to load secret key from
 		:return: None
-		:raises IOError: if :py:attr:`self.key_file` cannot be read from
+		:raises IOError: if `key_file` cannot be read from
 		"""
-		with open(self.key_file, 'rb') as fp:
+		with open(key_file, 'rb') as fp:
 			self.private_key = serialization.load_pem_private_key(
 				fp.read(),
 				password=None,
@@ -104,6 +112,7 @@ class Signature(object):
 
 	@property
 	def private_key_as_str(self):  # type: () -> str
+		assert self.private_key is not None
 		return self.private_key.private_bytes(
 			encoding=serialization.Encoding.PEM,
 			format=serialization.PrivateFormat.PKCS8,
@@ -112,6 +121,7 @@ class Signature(object):
 
 	@property
 	def public_key(self):  # type: () -> rsa.RSAPublicKey
+		assert self.private_key is not None
 		return self.private_key.public_key()
 
 	@property
@@ -119,6 +129,42 @@ class Signature(object):
 		return self.public_key.public_bytes(
 			encoding=serialization.Encoding.PEM,
 			format=serialization.PublicFormat.SubjectPublicKeyInfo
+		)
+
+	def encrypt(self, clear_text):  # type: (str) -> str
+		"""
+		Encrypt `text` with :py:attr:`self.public_key`.
+
+		:param clear_text: the text to encrypt
+		:return: the encrypted text (cyphertext)
+		:rtype: str
+		"""
+		return self.public_key.encrypt(
+			clear_text,
+			padding.OAEP(
+				mgf=padding.MGF1(algorithm=hashes.SHA256()),
+				algorithm=hashes.SHA256(),
+				label=None
+			)
+		)
+
+	def decrypt(self, cyphertext):  # type: (str) -> str
+		"""
+		Decrypt `text` with :py:attr:`self.private_key`.
+
+		:param cyphertext: the text to decrypt
+		:return: the decrypted text (clear text)
+		:rtype: str
+		:raises ValueError: if cyphertext
+		"""
+		assert self.private_key is not None
+		return self.private_key.decrypt(
+			cyphertext,
+			padding.OAEP(
+				mgf=padding.MGF1(algorithm=hashes.SHA256()),
+				algorithm=hashes.SHA256(),
+				label=None
+			)
 		)
 
 	def sign(self, text):  # type: (str) -> None
@@ -129,6 +175,7 @@ class Signature(object):
 		:return: signed hash of `text`
 		:rtype: str
 		"""
+		assert self.private_key is not None
 		hashed_msg = hashlib.sha256(bytes(text)).digest()
 		return self.private_key.sign(
 			hashed_msg,
@@ -139,15 +186,15 @@ class Signature(object):
 			utils.Prehashed(hashes.SHA256())
 		)
 
-	def verify(self, signature, msg):  # type: (str, str) -> bool
+	def verify(self, signature, text):  # type: (str, str) -> bool
 		"""
-		Verify that `signature` is valid for the text in `msg`.
+		Verify that `signature` is valid for `text`.
 
-		:param str signature: the signature for the text in `msg`
-		:param str msg: the message that was signed
-		:return: True if the signature is correct for teh message
+		:param str signature: the signature for `text`
+		:param str text: the message that was signed
+		:return: True if the signature is correct for the message
 		"""
-		hashed_msg = hashlib.sha256(bytes(msg)).digest()
+		hashed_msg = hashlib.sha256(bytes(text)).digest()
 		try:
 			self.public_key.verify(
 				signature,
