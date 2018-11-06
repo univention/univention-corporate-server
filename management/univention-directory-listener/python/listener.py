univention/univention-corporate-server

commit 4a6850f3d6ffee9eeca88e361b2b0a6e4d64c2ec

Bug #27547: docstrings

management/univention-directory-listener/python/listener.py
@@ -41,6 +41,18 @@ baseConfig = configRegistry
 
 
 def setuid(uid):
+	"""
+	Set the current process’s effective user id.
+	Use :py:func:`unsetuid()` to return to the listeners UID.
+
+	When used with any other user than `root`. Be aware that
+	:py:func:`listener.unsetuid()` will *not* be possible afterwards, as that
+	requires root privileges.
+
+	:param uid: UID the process should have
+	:type uid: int or str
+	:return: None
+	"""
 	if isinstance(uid, basestring):
 		uid = pwd.getpwnam(uid)[2]
 	os.seteuid(uid)
@@ -50,6 +62,12 @@ __listener_uid = -1
 
 
 def unsetuid():
+	"""
+	Return the current process’s effective user id to the listeners UID.
+	Only possible if the current effective user id is `root`.
+
+	:return: None
+	"""
 	global __listener_uid
 	if __listener_uid == -1:
 		try:
@@ -60,6 +78,22 @@ def unsetuid():
 
 
 def run(file, argv, uid=-1, wait=1):
+	"""
+	Execute a the program `file` with arguments `argv` and effective user id
+	`uid`.
+
+	:param str file: path to executable
+	:param argv: arguments to pass to executable
+	:type argv: list(str)
+	:param int uid: effective user id the process should be started with
+	:param bool wait: if true will block until the process has finished and
+	return either its exit code or the signal that lead to its stop (a negativ
+	number), see :py:const:`os.P_WAIT`. If false will return as soon as the
+	new process has been created, with the process id as the return value (see
+	:py:const:`os.P_NOWAIT`).
+	:return: exit code or signal number or process id
+	:rtype: int
+	"""
 	if uid > -1:
 		olduid = os.getuid()
 		setuid(uid)

management/univention-directory-listener/python/univention/listener/api_adapter.py
@@ -40,7 +40,7 @@ class ListenerModuleAdapter(object):
 	"""
 	def __init__(self, module_configuration, *args, **kwargs):
 		"""
-		:param module_configuration: ListenerModuleConfiguration object
+		:param ListenerModuleConfiguration module_configuration: configuration object
 		"""
 		self.config = module_configuration
 		self._ldap_cred = dict()
@@ -59,8 +59,10 @@ class ListenerModuleAdapter(object):
 		Returns the variables to be written to the module namespace, that
 		make up the legacy listener module interface.
 
-		:return: dict(name, description, filter_s, attributes, modrdn,
-		handler, initialize, clean, prerun, postrun, setdata, ..)
+		:return: a mapping with keys: `name`, `description`, `filter_s`,
+		`attributes`, `modrdn`, `handler`, `initialize`, `clean`, `prerun`,
+		`postrun`, `setdata`, ..
+		:rtype: dict
 		"""
 		name = self.config.get_name()
 		description = self.config.get_description()
@@ -88,6 +90,16 @@ class ListenerModuleAdapter(object):
 		)
 
 	def _setdata(self, key, value):
+		"""
+		Store LDAP connection credentials passes by the listener (one by one)
+		to the listener module. Passes them to the handler object once they
+		are complete.
+
+		:param str key: one of `basedn`, `basedn`, `bindpw`, `ldapserver`
+		:param str value: credentials
+		:return: None
+		:rtype: None
+		"""
 		self._ldap_cred[key] = value
 		if all(a in self._ldap_cred for a in ('basedn', 'basedn', 'bindpw', 'ldapserver')):
 			self._module_handler._set_ldap_credentials(
@@ -100,11 +112,23 @@ class ListenerModuleAdapter(object):
 
 	@property
 	def _module_handler(self):
+		"""Make sure to not create more than one instance of a listener module."""
 		if not self._module_handler_obj:
 			self._module_handler_obj = self.config.get_listener_module_instance()
 		return self._module_handler_obj
 
 	def _handler(self, dn, new, old, command):
+		"""
+		Function called by listener when a LDAP object matching the filter is
+		created/modified/moved/deleted.
+
+		:param str dn: the objects DN
+		:param dict new: new LDAP objects attributes
+		:param dict old: previous LDAP objects attributes
+		:param str command: LDAP modification type
+		:return: None
+		:rtype: None
+		"""
 		if command == 'r':
 			self._saved_old = old
 			self._saved_old_dn = dn

management/univention-directory-listener/python/univention/listener/handler.py
@@ -110,9 +110,10 @@ class ListenerModuleHandler(object):
 		"""
 		Called when a new object was created.
 
-		:param dn: str
-		:param new: dict
+		:param str dn: current objects DN
+		:param dict new: new LDAP objects attributes
 		:return: None
+		:rtype: None
 		"""
 		pass
 
@@ -123,11 +124,13 @@ class ListenerModuleHandler(object):
 		A move can be be detected by looking at old_dn. Attributes can be
 		modified during a move.
 
-		:param dn: str
-		:param old: dict
-		:param new: dict
-		:param old_dn: str if object was moved/renamed, None otherwise
+		:param str dn: current objects DN
+		:param dict old: previous LDAP objects attributes
+		:param dict new: new LDAP objects attributes
+		:param old_dn: previous DN if object was moved/renamed, None otherwise
+		:type old_dn: str or None
 		:return: None
+		:rtype: None
 		"""
 		pass
 
@@ -135,9 +138,10 @@ class ListenerModuleHandler(object):
 		"""
 		Called when an object was deleted.
 
-		:param dn: str
-		:param old: dict
+		:param str dn: current objects DN
+		:param dict old: previous LDAP objects attributes
 		:return: None
+		:rtype: None
 		"""
 		pass
 
@@ -147,6 +151,7 @@ class ListenerModuleHandler(object):
 		for the first time or when a resync it triggered.
 
 		:return: None
+		:rtype: None
 		"""
 		pass
 
@@ -156,6 +161,7 @@ class ListenerModuleHandler(object):
 		for the first time or when a resync it triggered.
 
 		:return: None
+		:rtype: None
 		"""
 		pass
 
@@ -167,6 +173,7 @@ class ListenerModuleHandler(object):
 		Use for example to open an LDAP connection.
 
 		:return: None
+		:rtype: None
 		"""
 		pass
 
@@ -178,6 +185,7 @@ class ListenerModuleHandler(object):
 		Use for example to close an LDAP connection.
 
 		:return: None
+		:rtype: None
 		"""
 		pass
 
@@ -185,16 +193,18 @@ class ListenerModuleHandler(object):
 	@contextmanager
 	def as_root():
 		"""
-		Temporarily change the UID of the current process to 0.
+		Contextmanager to temporarily change the effective UID of the current
+		process to 0.
 
 		with self.as_root():
 			do something
 
-		Use listener.setuid(<int|str>) for any other user that root. But be
-		aware that listener.unsetuid() will not be possible afterwards, as
-		that requires root privileges.
+		Use :py:func:`listener.setuid()` for any other user than `root`. But be
+		aware that :py:func:`listener.unsetuid()` will not be possible
+		afterwards, as that requires root privileges.
 
 		:return: None
+		:rtype: None
 		"""
 		old_uid = os.geteuid()
 		try:
@@ -211,11 +221,12 @@ class ListenerModuleHandler(object):
 		Find differences in old and new. Returns dict with keys pointing to old
 		and new values.
 
-		:param old: dict
-		:param new: dict
-		:param keys: list: consider only those keys in comparison
-		:param ignore_metadata: bool: ignore changed metadata attributes (if `keys` is not set)
-		:return: dict: key -> (old[key], new[key])
+		:param dict old: previous LDAP objects attributes
+		:param dict new: new LDAP objects attributes
+		:param list keys: consider only those keys in comparison
+		:param bool ignore_metadata: ignore changed metadata attributes (if `keys` is not set)
+		:return: key -> (old[key], new[key]) mapping
+		:rtype: dict
 		"""
 		res = dict()
 		if keys:
@@ -233,14 +244,15 @@ class ListenerModuleHandler(object):
 		"""
 		Will be called for unhandled exceptions in create/modify/remove.
 
-		:param dn: str
-		:param old: dict
-		:param new: dict
-		:param command: str
-		:param exc_type: exception class
-		:param exc_value: exception object
-		:param exc_traceback: traceback object
+		:param str dn: current objects DN
+		:param dict old: previous LDAP objects attributes
+		:param dict new: new LDAP objects attributes
+		:param str command: LDAP modification type
+		:param Exception exc_type: exception class
+		:param Exception exc_value: exception object
+		:param traceback exc_traceback: traceback object
 		:return: None
+		:rtype: None
 		"""
 		self.logger.exception('dn=%r command=%r\n    old=%r\n    new=%r', dn, command, old, new)
 		raise exc_type, exc_value, exc_traceback
@@ -250,7 +262,8 @@ class ListenerModuleHandler(object):
 		"""
 		LDAP connection object.
 
-		:return: univention.admin.uldap.access object
+		:return: uldap.access object
+		:rtype: univention.admin.uldap.access
 		"""
 		if not self._lo:
 			ldap_credentials = self._get_ldap_credentials()
@@ -266,7 +279,8 @@ class ListenerModuleHandler(object):
 		"""
 		Get a LDAP position object for the base DN (ldap/base).
 
-		:return: univention.admin.uldap.position object
+		:return: uldap.position object
+		:rtype: univention.admin.uldap.position
 		"""
 		if not self._po:
 			self._po = position(self.lo.base)
@@ -276,7 +290,8 @@ class ListenerModuleHandler(object):
 		"""
 		Get the LDAP credentials received through setdata().
 
-		:return: dict: the LDAP credentials
+		:return: the LDAP credentials
+		:rtype: dict(str, str)
 		"""
 		return self._ldap_credentials
 
@@ -286,11 +301,12 @@ class ListenerModuleHandler(object):
 		necessary to manually run this method. The listener will automatically
 		run it at startup.
 
-		:param base: str
-		:param binddn: str
-		:param bindpw: str
-		:param host: str
+		:param str base: base DN
+		:param str binddn: bind DB
+		:param str bindpw: bind password
+		:param str host: LDAP server
 		:return: None
+		:rtype: None
 		"""
 		old_credentials = self._ldap_credentials
 		self._ldap_credentials = dict(
@@ -311,7 +327,8 @@ class ListenerModuleHandler(object):
 		a subclass of ListenerModuleConfiguration to change the returned
 		object type.
 
-		:return: ListenerModuleConfiguration object
+		:return: configuration object
+		:rtype: ListenerModuleConfiguration
 		"""
 		try:
 			conf_class = cls.Configuration
@@ -352,7 +369,8 @@ class ListenerModuleHandler(object):
 		"""
 		Is this a listener module?
 
-		:return: bool: True id the file is in /usr/lib/univention-directory-listener.
+		:return: True id the file is in /usr/lib/univention-directory-listener.
+		:rtype: bool
 		"""
 		try:
 			path = inspect.getfile(cls)

management/univention-directory-listener/python/univention/listener/handler_configuration.py
@@ -97,7 +97,8 @@ class ListenerModuleConfiguration(object):
 		"""
 		Get the configuration of a listener module.
 
-		:return: dict
+		:return: configuration of listener module
+		:rtype: dict
 		"""
 		res = dict()
 		for key in self.get_configuration_keys():
@@ -116,6 +117,13 @@ class ListenerModuleConfiguration(object):
 
 	@classmethod
 	def get_configuration_keys(cls):
+		"""
+		List of known configuration keys. Subclasses can expand this to support
+		additional attributes.
+
+		:return: list of known configuration keys
+		:rtype: list(str)
+		"""
 		return [
 			'attributes',
 			'description',
@@ -125,15 +133,32 @@ class ListenerModuleConfiguration(object):
 		]
 
 	def get_name(self):
+		"""
+		:return: name of module
+		:rtype: str
+		"""
 		return self.name
 
 	def get_description(self):
+		"""
+		:return: description string of module
+		:rtype: str
+		"""
 		return self.description
 
 	def get_ldap_filter(self):
+		"""
+		:return: LDAP filter of module
+		:rtype: str
+		"""
 		return self.ldap_filter
 
 	def get_attributes(self):
+		"""
+		:return: attributes of matching LDAP objects the module will be
+		notified about if changed
+		:rtype: list(str)
+		"""
 		assert isinstance(self.attributes, list)
 		return self.attributes
 
@@ -141,9 +166,10 @@ class ListenerModuleConfiguration(object):
 		"""
 		Get an instance of the listener module.
 
-		:param args: tuple: passed to __init__ of ListenerModuleHandler
-		:param kwargs: dict: : passed to __init__ of ListenerModuleHandler
+		:param tuple args: passed to __init__ of ListenerModuleHandler
+		:param dict kwargs: : passed to __init__ of ListenerModuleHandler
 		:return: instance of ListenerModuleHandler
+		:rtype: ListenerModuleHandler
 		"""
 		return self.get_listener_module_class()(self, *args, **kwargs)
 
@@ -151,15 +177,17 @@ class ListenerModuleConfiguration(object):
 		"""
 		Get the class to instantiate for a listener module.
 
-		:return: type: subclass of univention.listener.ListenerModuleHandler
+		:return: subclass of univention.listener.ListenerModuleHandler
+		:rtype: ListenerModuleHandler
 		"""
 		return self.listener_module_class
 
 	def get_active(self):
 		"""
-		If this listener module should run. Detemined by the value of
+		If this listener module should run. Determined by the value of
 		listener/module/<name>/deactivate.
 
-		:return: bool
+		:return: whether the listener module should be activated
+		:rtype: bool
 		"""
 		return not listener.configRegistry.is_true('listener/module/{}/deactivate'.format(self.get_name()), False)

management/univention-directory-listener/python/univention/listener/handler_logging.py
@@ -56,6 +56,12 @@ __syslog_opened = False
 
 
 class UniFileHandler(TimedRotatingFileHandler):
+	"""
+	Used by listener modules using the :py:mod:`univention.listener` API to
+	write log files below `/var/log/univention/listener_log/`.
+	Configuration can be done through the `handler_kwargs` argument of
+	:py:func:`get_listener_logger`.
+	"""
 	_listener_uid = pwd.getpwnam('listener').pw_uid
 	_adm_gid = grp.getgrnam('adm').gr_gid
 
@@ -76,6 +82,11 @@ class UniFileHandler(TimedRotatingFileHandler):
 
 
 class ModuleHandler(logging.Handler):
+	"""
+	Used by listener modules using the :py:mod:`univention.listener` API to
+	write log messages through :py:mod:`univention.debug` to
+	`/var/log/univention/listener.log`
+	"""
 	LOGGING_TO_UDEBUG = dict(
 		CRITICAL=ud.ERROR,
 		ERROR=ud.ERROR,
@@ -147,6 +158,16 @@ listener_module_root_logger.setLevel(getattr(logging, _listener_debug_level_str)
 
 
 def get_logger(name, path=None):
+	"""
+	Get a logging instance. Caching wrapper for
+	:py:func:`get_listener_logger()`.
+
+	:param str name: name of the logger instance will be <root loggers name>.name
+	:param str path: path to log file to create. If unset will be
+	`/var/log/univention/listener_modules/<name>.log`.
+	:return: a python logging object
+	:rtype: logging.Logger
+	"""
 	if name not in _logger_cache:
 		file_name = name.replace('/', '_')
 		logger_name = name.replace('.', '_')
@@ -173,8 +194,14 @@ def get_logger(name, path=None):
 
 
 def calculate_loglevel(name):
-	# get the higher of listener/debug/level and listener/module/<name>/debug/level
-	# which is the lower log level
+	"""
+	Returns the higher of listener/debug/level and listener/module/<name>/debug/level
+	which is the lower log level.
+
+	:param str name: name of logger instance
+	:return: log level
+	:rtype: int
+	"""
 	listener_module_debug_level = _get_ucr_int('listener/module/{}/debug/level'.format(name), 2)
 	# 0 <= ucr level <= 4
 	return UCR_DEBUG_LEVEL_TO_LOGGING_LEVEL[min(4, max(0, _listener_debug_level, listener_module_debug_level))]
@@ -200,18 +227,19 @@ def get_listener_logger(name, filename, level=None, handler_kwargs=None, formatt
 	* Using custom handler and formatter classes is possible by configuring
 	the 'cls' key of handler_kwargs and formatter_kwargs.
 
-	:param name: str: name of the logger instance will be <root loggers name>.name
-	:param level: str: loglevel (DEBUG, INFO etc) or if not set it will be chosen
+	:param str name: name of the logger instance will be <root loggers name>.name
+	:param str level: loglevel (DEBUG, INFO etc) or if not set it will be chosen
 	automatically (see above)
-	:param target: str (file path)
-	:param handler_kwargs: dict: will be passed to the handlers constructor.
+	:param str target: (file path)
+	:param dict handler_kwargs: will be passed to the handlers constructor.
 	It cannot be used to modify a handler, as it is only used at creation time.
 	If it has a key 'cls' it will be used as handler instead of UniFileHandler
 	or UniStreamHandler. It should be a subclass of one of those!
-	:param formatter_kwargs: dict: will be passed to the formatters constructor,
+	:param dict formatter_kwargs: will be passed to the formatters constructor,
 	if it has a key 'cls' it will be used to create a formatter instead of
 	logging.Formatter.
 	:return: a python logging object
+	:rtype: logging.Logger
 	"""
 	assert isinstance(filename, basestring)
 	assert isinstance(name, basestring)
@@ -267,6 +295,7 @@ def get_listener_logger(name, filename, level=None, handler_kwargs=None, formatt
 	if _listener_module_handler not in listener_module_root_logger.handlers:
 		listener_module_root_logger.addHandler(_listener_module_handler)
 	return _logger
+
 
 def _log_to_syslog(level, msg):
 	global __syslog_opened

commit 67f0ca32a97f7ca0112c0def4ba588f23076d646

Bug #27547: Fix UDL docstrings
Fixes: 4a6850f3d6f

management/univention-directory-listener/python/listener.py
@@ -86,11 +86,7 @@ def run(file, argv, uid=-1, wait=1):
 	:param argv: arguments to pass to executable
 	:type argv: list(str)
 	:param int uid: effective user id the process should be started with
-	:param bool wait: if true will block until the process has finished and
-	return either its exit code or the signal that lead to its stop (a negativ
-	number), see :py:const:`os.P_WAIT`. If false will return as soon as the
-	new process has been created, with the process id as the return value (see
-	:py:const:`os.P_NOWAIT`).
+	:param bool wait: if true will block until the process has finished and return either its exit code or the signal that lead to its stop (a negativ number), see :py:const:`os.P_WAIT`. If false will return as soon as the new process has been created, with the process id as the return value (see :py:const:`os.P_NOWAIT`).
 	:return: exit code or signal number or process id
 	:rtype: int
 	"""