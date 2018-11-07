aff090c0f0dd5b2832a044e7890cfcf8913f9ccc
Fathan Vidjaja <vidjaja@univention.de>
2018-11-07 14:55:36 +0100

    Bug #47228: added timeouts between tests

--- test/product-tests/samba/multi-server.sh
+++ test/product-tests/samba/multi-server.sh
@@ -15,10 +15,12 @@ create_share_memberslave () {
 	#create gpo on Backup to check if change of DC is possible
 	#    Per Gruppenrichtlineinverwaltung (GPMC) vom Client aus auf den DC Salve wechseln (Rechts-click auf Domäne, anderen DC auswählen). DONE: simulated by creating a new GPO with Slave as DC
 	python shared-utils/ucs-winrm.py create-gpo-server --credssp --name NewGPOinSlave --comment "testing new GPO in domain" --server $SLAVE
+	sleep 30
 	python shared-utils/ucs-winrm.py link-gpo --name NewGPOinSlave --target "dc=sambatest,dc=local" --credssp
 	# Per Gruppenrichtlineinverwaltung (GPMC) vom Client aus auf den DC Backup wechseln (Rechts-click auf Domäne, anderen DC auswählen)
 	# und dort z.B. die Benutzer-Richtlinie anpassen (z.B. einfach Lautstärkesymbol entfernen -> deaktivieren/Ok). Es sollte keine Fehlermeldung kommen. DONE : simulated by creating GPO with Backup as DC
 	python shared-utils/ucs-winrm.py create-gpo-server --credssp --name NewGPOinBackup --comment "testing new GPO in domain" --server $BACKUP
+	sleep 30
 	python shared-utils/ucs-winrm.py link-gpo --name NewGPOinBackup --target "dc=sambatest,dc=local" --credssp
 }
 
