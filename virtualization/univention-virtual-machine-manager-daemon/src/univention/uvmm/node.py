9b050226cea83149dfd4a7a4d2f7d03ca2156482
Philipp Hahn <hahn@univention.de>
2018-11-08 13:29:47 +0100

    Bug #47617 uvmm: Fix migration of snapshots
    The libvirt API does not support migrating domains with snapshots.
    Previously UCS carries a patch to work-around that by putting the
    snapshot XML on a shared storage, only deleting the references on the
    source host and adopting them on the destination.
    
    That patch carries some bugs, which leads to the whole domain XML
    getting lost during some migration scenarios, mainly offline migration.
    
    Drop those patches and implement the following logic:
    - backup the snapshot XML for disaster recovery.
    - fetch the snapshot XML data into memory.
    - delete only the snapshot meta data.
    - migrate the domain, which now has no snapshots.
    - Re-add the snapshot XML to the migrated domain.
    
    Care must be taken as the snapshots have a hierarchy and build a tree:
    - snapshots must be removed leaves to root.
    - snapshots must be restored root to leaves.

--- virtualization/univention-virtual-machine-manager-daemon/src/univention/uvmm/node.py
+++ virtualization/univention-virtual-machine-manager-daemon/src/univention/uvmm/node.py
@@ -1980,6 +1980,7 @@ def domain_migrate(source_uri, domain, target_uri, mode=0):
 	:param int mode: Migration mode: 0 to start normal migration, -1 to abort any running migration, 1..99 to set auto-convergence increment, 100 to force post-copy now.
 	:raises NodeError: if migration failes.
 	"""
+	snapshots = []
 	try:
 		source_node = node_query(source_uri)
 		source_conn = source_node.conn
@@ -2038,9 +2039,33 @@ def domain_migrate(source_uri, domain, target_uri, mode=0):
 		else:
 			raise NodeError(_('Domain "%(domain)s" in state "%(state)s" can not be migrated'), domain=domain, state=STATES[source_state])
 
+		_domain_backup(source_dom)
+
+		while source_dom.snapshotNum() > 0:
+			for snapshot in source_dom.listAllSnapshots(libvirt.VIR_DOMAIN_SNAPSHOT_LIST_LEAVES):
+				snap_xml = snapshot.getXMLDesc(libvirt.VIR_DOMAIN_XML_SECURE)
+				snapshots.append(snap_xml)
+				logger.info('Deleting snapshot %s of domain %s', snapshot.getName(), domain)
+				snapshot.delete(libvirt.VIR_DOMAIN_SNAPSHOT_DELETE_METADATA_ONLY)
+		snapshots.reverse()
+
+		logger.info('Starting migration of domain %s to host %s with flags %x', domain, target_uri, flags)
 		domStat.pd.migration['msg'] = _('Migration started')
-		source_dom.migrate3(target_conn, params, flags)
+		dest_dom = source_dom.migrate3(target_conn, params, flags)
+		logger.info('Finished migration of domain %s to host %s with flags %x', domain, target_uri, flags)
+		source_node.domains.pop(domain, None)
+
+		for snap_xml in snapshots:
+			snapshot = dest_dom.snapshotCreateXML(snap_xml, libvirt.VIR_DOMAIN_SNAPSHOT_CREATE_REDEFINE)
+			logger.info('Added snapshot %s of domain %s', snapshot.getName(), domain)
 	except libvirt.libvirtError as ex:
+		for snap_xml in snapshots:
+			try:
+				snapshot = source_dom.snapshotCreateXML(snap_xml, libvirt.VIR_DOMAIN_SNAPSHOT_CREATE_REDEFINE)
+				logger.info('Restored snapshot %s of domain %s', snapshot.getName(), domain)
+			except libvirt.libvirtError as ex2:
+				logger.error(_('Failed to restore snapshot after failed migration of domain "%(domain)s": %(error)s'), domain=domain, error=ex2.get_error_message())
+
 		if ex.get_error_code() == libvirt.VIR_ERR_CPU_INCOMPATIBLE:
 			raise NodeError(_('The target host has an incompatible CPU; select a different host or try an offline migration. (%(details)s)') % {'details': ex.get_str2()})
 		logger.error(ex)
