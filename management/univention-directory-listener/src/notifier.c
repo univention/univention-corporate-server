univention/univention-corporate-server

commit 083de5b0ab0d894a5d3ff7a70801af7c635f8e55

Bug #48030 UDL: Remove wrong select_server()
when connecting the LDAP server fails as that will just reconnect any
LDAP server, but the Notifier connections is not updated.

If either the connection got suspended because of 15 seconds of
inactivity, or the connections gets closed while change_update_dn() is
running (too long), connect_to_ldap() called select_server() if the
original chosen server was not reachable.
On a slave that might select a different LDAP Backup server, but the
connection to the Notifier is *not* updated and still connects the old
host.
If the newly selected Backup is behind the previous host, the UDL will
see references to entries not yet replicated and will do arbitrary
things.

Drop the call to select_server().
Add error handling with 5 retries and return with an error instead of
looping infinitely.

management/univention-directory-listener/src/notifier.c
@@ -57,7 +57,6 @@
 #include "change.h"
 #include "network.h"
 #include "transfile.h"
-#include "select_server.h"
 
 #define DELAY_LDAP_CLOSE 15               /* 15 seconds */
 #define DELAY_ALIVE 5 * 60                /* 5 minutes */
@@ -65,16 +64,22 @@
 
 
 static int connect_to_ldap(univention_ldap_parameters_t *lp) {
-	while (univention_ldap_open(lp) != LDAP_SUCCESS) {
-		univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN, "can not connect to ldap server (%s)", lp->host);
+	int i, rv;
 
-			univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN, "can not connect to any ldap server, retrying in 30 seconds");
+	for (i = 0; i < 6; i++) {
+		if (i)
 			sleep(30);
 
-		select_server(lp);
+		univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_INFO, "connecting to LDAP server %s:%d", lp->uri ? lp->uri : lp->host ? lp->host : "NULL", lp->port);
+		rv = univention_ldap_open(lp);
+		if (rv == LDAP_SUCCESS)
+			break;
 	}
-
-	return LDAP_SUCCESS;
+	if (rv == LDAP_SUCCESS)
+		univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN, "connection to LDAP server %s:%d succeeded", lp->uri ? lp->uri : lp->host ? lp->host : "NULL", lp->port);
+	else
+		univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_ERROR, "connection to LDAP server %s:%d failed", lp->uri ? lp->uri : lp->host ? lp->host : "NULL", lp->port);
+	return rv;
 }
 
 static void check_free_space() {
@@ -171,10 +176,8 @@ int notifier_listen(univention_ldap_parameters_t *lp, bool write_transaction_fil
 
 		/* ensure that LDAP connection is open */
 		if (trans.lp->ld == NULL) {
-			if ((rv = connect_to_ldap(trans.lp)) != 0) {
-				univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_ERROR, "failed to connect to LDAP");
+			if ((rv = connect_to_ldap(trans.lp)) != 0)
 				goto out;
-			}
 		}
 
 		/* Try to do the change. If the LDAP server is down, try
@@ -182,9 +185,8 @@ int notifier_listen(univention_ldap_parameters_t *lp, bool write_transaction_fil
 		while ((rv = change_update_dn(&trans)) != LDAP_SUCCESS) {
 			univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_ERROR, "change_update_dn failed: %d", rv);
 			if (rv == LDAP_SERVER_DOWN)
-				if ((rv = connect_to_ldap(trans.lp)) == 0)
-					continue;
-			goto out;
+				if ((rv = connect_to_ldap(trans.lp)) != 0)
+					goto out;
 		}
 
 		/* rv had better be LDAP_SUCCESS if we get here */

management/univention-directory-listener/src/select_server.c
@@ -51,6 +51,12 @@ extern int backup_notifier;
  * 2. @Master|Backup: ldap/master : ldap/master/port
  * 2. @*: ldap/backup[?] : ldap/backup/port, fallback: ldap/master : ldap/master/port
  * @param lp LDAP configuration object.
+ *
+ * .. warning::
+ *
+ *    notifier and LDAP server must belong to the same host. Otherwise the
+ *    notifier might reference entries, which are not yet correctly replicated
+ *    to the LDAP server. This leads to inconsistencies.
  */
 void select_server(univention_ldap_parameters_t *lp) {
 	static unsigned seed = 0;

commit 9464dfbe0586ee9932e783e6412fc582a2bba4b3

Bug #48030: Fix retry logic
As every ldap_search() call is already wrapped by LDAP_RETRY, just drop
the external retry loop completely.

Fixes: 083de5b0ab0d894a5d3ff7a70801af7c635f8e55

management/univention-directory-listener/src/notifier.c
@@ -57,30 +57,12 @@
 #include "change.h"
 #include "network.h"
 #include "transfile.h"
+#include "utils.h"
 
 #define DELAY_LDAP_CLOSE 15               /* 15 seconds */
 #define DELAY_ALIVE 5 * 60                /* 5 minutes */
 #define TIMEOUT_NOTIFIER_RECONNECT 5 * 60 /* 5 minutes */
 
-
-static int connect_to_ldap(univention_ldap_parameters_t *lp) {
-	int i, rv;
-
-	for (i = 0; i < 6; i++) {
-		if (i)
-			sleep(30);
-
-		univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_INFO, "connecting to LDAP server %s:%d", lp->uri ? lp->uri : lp->host ? lp->host : "NULL", lp->port);
-		rv = univention_ldap_open(lp);
-		if (rv == LDAP_SUCCESS)
-			break;
-	}
-	if (rv == LDAP_SUCCESS)
-		univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN, "connection to LDAP server %s:%d succeeded", lp->uri ? lp->uri : lp->host ? lp->host : "NULL", lp->port);
-	else
-		univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_ERROR, "connection to LDAP server %s:%d failed", lp->uri ? lp->uri : lp->host ? lp->host : "NULL", lp->port);
-	return rv;
-}
 
 static void check_free_space() {
 	static int64_t min_mib = -2;
@@ -176,17 +158,13 @@ int notifier_listen(univention_ldap_parameters_t *lp, bool write_transaction_fil
 
 		/* ensure that LDAP connection is open */
 		if (trans.lp->ld == NULL) {
-			if ((rv = connect_to_ldap(trans.lp)) != 0)
+			if ((rv = LDAP_RETRY(trans.lp, univention_ldap_open(trans.lp))) != LDAP_SUCCESS)
 				goto out;
 		}
 
-		/* Try to do the change. If the LDAP server is down, try
-		   to reconnect */
-		while ((rv = change_update_dn(&trans)) != LDAP_SUCCESS) {
+		if ((rv = change_update_dn(&trans)) != LDAP_SUCCESS) {
 			univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_ERROR, "change_update_dn failed: %d", rv);
-			if (rv == LDAP_SERVER_DOWN)
-				if ((rv = connect_to_ldap(trans.lp)) != 0)
-					goto out;
+			goto out;
 		}
 
 		/* rv had better be LDAP_SUCCESS if we get here */