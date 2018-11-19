71e354488174c38fdc407a4824660916d661ca23
Jürn Brodersen <brodersen@univention.de>
2018-11-19 15:55:56 +0100

    Bug #35196,Bug #47137 uvmm: Fix CPU usage

--- virtualization/univention-virtual-machine-manager-daemon/umc/js/uvmm.js
+++ virtualization/univention-virtual-machine-manager-daemon/umc/js/uvmm.js
@@ -1620,7 +1620,7 @@ define([
 				return '';
 			}
 			if (item.type === 'domain') {
-				if (!isRunning(item)) {
+				if (!isRunning(item) || isPaused(item)) {
 					// only show CPU info, if the machine is running
 					return '';
 				}
@@ -1629,7 +1629,7 @@ define([
 					return '';
 				}
 			}
-			var percentage = Math.round(item.cpuUsage);
+			var percentage = Math.round(item.cpuUsage * 100);
 			var progressBar = new ProgressBar({
 				value: percentage + '%'
 			});
