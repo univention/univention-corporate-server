f7c63ba033b3a0a8142fdaa30e641b7e55830625
Erik Damrose <damrose@univention.de>
2018-11-21 09:51:57 +0100

    Bug #48159: Install app before updating packages.

--- test/utils/install-vbox-guesttools.sh
+++ test/utils/install-vbox-guesttools.sh
@@ -2,9 +2,13 @@
 
 # Install virtualbox guest-tools and delete script afterwards
 
+# secure_apt=no because otherwise installation fails when app is installed from test appcenter
+# E: The repository https://appcenter-test ... does not have a Release file.
+ucr set update/secure_apt=no
 ucr set repository/online/unmaintained=yes repository/online=yes
 univention-install -y virtualbox-guest-utils
 apt-get clean
+ucr set update/secure_apt=yes
 ucr set repository/online/unmaintained=no repository/online=no
 apt-get update
 
