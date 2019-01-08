5e71dbe666916baa4b8f0fadbe57223e24086848
Dirk Wiesenthal <wiesenthal@univention.de>
2019-01-08 10:35:34 +0100

    Bug #48334: Add 4.4 to the App Center

--- test/ucs-test/tests/80_docker/dockertest.py
+++ test/ucs-test/tests/80_docker/dockertest.py
@@ -562,8 +562,9 @@ class Appcenter(object):
 			self.add_ucs_version_to_appcenter('4.1')
 			self.add_ucs_version_to_appcenter('4.2')
 			self.add_ucs_version_to_appcenter('4.3')
-			self.versions = ['4.1', '4.2', '4.3']
-			self._write_ucs_ini('[4.3]\nSupportedUCSVersions=4.3, 4.2, 4.1\n')
+			self.add_ucs_version_to_appcenter('4.4')
+			self.versions = ['4.1', '4.2', '4.3', '4.4']
+			self._write_ucs_ini('[4.4]\nSupportedUCSVersions=4.4, 4.3, 4.2, 4.1\n')
 		else:
 			self.add_ucs_version_to_appcenter(version)
 			self.versions = [version]
