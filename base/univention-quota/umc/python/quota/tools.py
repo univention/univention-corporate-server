f93da9cd5bfd28c999fd3ff9851c9e720031589f
Jannik Ahlers <ahlers@univention.de>
2019-01-10 16:43:11 +0100

    Bug #48315: fix typo

--- base/univention-quota/umc/python/quota/tools.py
+++ base/univention-quota/umc/python/quota/tools.py
@@ -83,7 +83,7 @@ def repquota(partition, callback, user=None):
 	fs = fstab.File()
 	part = fs.find(spec=partition)
 	if part.type == 'xfs':
-		args += '--version xfs '
+		args += '--format xfs '
 
 	# grep a single user
 	if user:
