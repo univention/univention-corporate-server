6461416e22213dbc5c97c71f33c4ac18416c73fb
Richard Ulmer <ulmer@univention.de>
2019-01-16 11:35:46 +0100

    Bug #48449: Add icons to sidebar headings in Apps details page

--- management/univention-appcenter/umc/js/appcenter/AppDetailsPage.js
+++ management/univention-appcenter/umc/js/appcenter/AppDetailsPage.js
@@ -608,7 +608,7 @@ define([
 
 			domConstruct.create('span', {
 				innerHTML: _('Buy in App Center'),
-				'class': 'mainHeader'
+				'class': 'mainHeader iconHeaderBuy'
 			}, appBuyContainer.domNode);
 
 			var buy_button = new Button({
@@ -644,7 +644,7 @@ define([
 
 			domConstruct.create('span', {
 				innerHTML: _('More information'),
-				'class': 'mainHeader'
+				'class': 'mainHeader iconHeaderInfo'
 			}, appDetailsContainer.domNode);
 			domConstruct.place(this._detailsTable, appDetailsContainer.domNode);
 		},
@@ -658,7 +658,7 @@ define([
 
 			domConstruct.create('span', {
 				innerHTML: _('App Center Badges'),
-				'class': 'mainHeader'
+				'class': 'mainHeader iconHeaderBadges',
 			}, appBadgeContainer.domNode);
 
 			array.forEach(this.app.rating, function(rating) {
