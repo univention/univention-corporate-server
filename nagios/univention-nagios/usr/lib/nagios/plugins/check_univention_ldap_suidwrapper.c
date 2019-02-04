7beb74af8fc0aa32910129b89f32a03e0d28acad
Philipp Hahn <hahn@univention.de>
2019-02-04 10:15:03 +0100

    Bug #48616 nagios: Fix local root exploit
    Use execle() to sanitize process environment before calling Python,
    which can be tricked to execute local code from PYTHONPATH as "root".
    
    Backport of Bug #48603:
        035e7b73930c69c9b4b340f4baab3cc4f863aa1e
        0654f211c33c30bd5bb8db470e5c78f02d5e6fdf

--- nagios/univention-nagios/usr/lib/nagios/plugins/check_univention_ldap_suidwrapper.c
+++ nagios/univention-nagios/usr/lib/nagios/plugins/check_univention_ldap_suidwrapper.c
@@ -2,21 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define COMMAND "/usr/lib/nagios/plugins/check_univention_ldap"
+static const char COMMAND[] = "/usr/lib/nagios/plugins/check_univention_ldap";
 
-main(int argc, char ** argv, char ** envp)
+static char *suid_envp[] = {
+	NULL
+};
+
+int main(int argc, char **argv, char **envp)
 {
-	uid_t uid = getuid();
-	char* args[2];
 	if (setgid(getegid())) {
 		perror("setgid");
+		return EXIT_FAILURE;
 	}
 	if (setuid(geteuid())) {
 		perror("setuid");
+		return EXIT_FAILURE;
 	}
-	args[0] = COMMAND;
-	args[1] = NULL;
-	execv(COMMAND, args);
-	setuid(uid);
-	exit(1);
+	execle(COMMAND, COMMAND, NULL, suid_envp);
+	perror("execle");
+	return EXIT_FAILURE;
 }
