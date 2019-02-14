1fabe84923f7f0998c714db6c986671fb7865e8e
Florian Best <best@univention.de>
2019-02-14 15:46:55 +0100

    Bug #48098 UVMM: Add missing URL encoding

--- virtualization/univention-virtual-machine-manager-daemon/umc/js/uvmm.js
+++ virtualization/univention-virtual-machine-manager-daemon/umc/js/uvmm.js
@@ -445,7 +445,7 @@ define([
 		vncLink: function( ids, items ) {
 			array.forEach(items, function(item) {
 				var id = item.id;
-				var uuid = id.slice(id.indexOf('#') + 1);
+				var uuid = encodeURIComponent(id.slice(id.indexOf('#') + 1));
 				var port = window.location.port ? ':' + window.location.port : '';
 				var title = encodeURIComponent(item.label + '@' + item.nodeName);
 				var url = window.location.protocol + '//' + window.location.host + port + '/univention-novnc/vnc_auto.html?port=6080&path=?token=' + uuid + '&title=' + title;
