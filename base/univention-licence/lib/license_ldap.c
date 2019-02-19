bd3b50ca9d06ad36974dbdb1008cc6abe7bb5ed5
Philipp Hahn <hahn@univention.de>
2019-02-19 10:08:24 +0100

    Bug #48698 spell: Recieved -> Received

--- base/univention-licence/lib/license_ldap.c
+++ base/univention-licence/lib/license_ldap.c
@@ -103,7 +103,7 @@ char* univention_license_ldap_get_basedn(void)
 
 /******************************************************************************/
 /*!
-	@brief	open a connection to the ldap server, with the connection data recived from UCR
+	@brief	open a connection to the ldap server, with the connection data received from UCR
 	@retval	1	if the connection was already open or was opened successfull
 	@retval	0	if an error has occurred
 */
