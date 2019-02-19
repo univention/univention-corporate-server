2267d43cfd01259d440b3bdc244ab0d4e637aa34
Philipp Hahn <hahn@univention.de>
2019-02-19 10:07:46 +0100

    Bug #48698 spell: candiate -> candidate

--- base/univention-lib/python/admember.py
+++ base/univention-lib/python/admember.py
@@ -139,7 +139,7 @@ class notDomainAdminInAD(Exception):
 
 class univentionSambaWrongVersion(Exception):
 
-	'''univention-samba candiate has wrong version'''
+	'''univention-samba candidate has wrong version'''
 
 
 class timeSyncronizationFailed(Exception):
