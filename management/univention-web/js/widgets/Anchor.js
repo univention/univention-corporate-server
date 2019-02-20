0ebb2c19a1036de57876def561265528ef132e1f
Florian Best <best@univention.de>
2019-02-20 21:13:43 +0100

    Bug #48621: add Anchor widget

---
+++ management/univention-web/js/widgets/Anchor.js
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2019 Univention GmbH
+ *
+ * http://www.univention.de/
+ *
+ * All rights reserved.
+ *
+ * The source code of this program is made available
+ * under the terms of the GNU Affero General Public License version 3
+ * (GNU AGPL V3) as published by the Free Software Foundation.
+ *
+ * Binary versions of this program provided by Univention to you as
+ * well as other copyrighted, protected or trademarked materials like
+ * Logos, graphics, fonts, specific documentations and configurations,
+ * cryptographic keys etc. are subject to a license agreement between
+ * you and Univention and not subject to the GNU AGPL V3.
+ *
+ * In the case you use this program under the terms of the GNU AGPL V3,
+ * the program is provided in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License with the Debian GNU/Linux or Univention distribution in file
+ * /usr/share/common-licenses/AGPL-3; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/*global define*/
+
+define([
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/dom-class",
+	"dojox/html/entities",
+	"dijit/_WidgetBase",
+	"dijit/_TemplatedMixin"
+], function(declare, lang, domClass, entities, _WidgetBase, _TemplatedMixin) {
+	return declare("umc.widgets.Anchor", [_WidgetBase, _TemplatedMixin], {
+		// summary:
+		//		Simple widget that displays a link.
+
+		templateString: '<a href="${href}" title="${title}" dojoAttachPoint="contentNode">${content}</a>',
+
+		labelPosition: 'top',
+
+		// content: String
+		//		String which contains the text (or HTML code) to be rendered.
+		content: '',
+
+		title: '',
+		href: '#',
+
+		// the widget's class name as CSS class
+		baseClass: 'umcText',
+
+		postCreate: function() {
+			this.inherited(arguments);
+
+			if (typeof this.callback === "function") {
+				this.on('click', lang.hitch(this, 'callback'));
+			}
+		},
+
+		_setContentAttr: function(content) {
+			this.contentNode.innerHTML = entities.encode(content);
+			this._set('content', content);
+		},
+
+		_setVisibleAttr: function(visible) {
+			this._set('visible', visible);
+			domClass.toggle(this.domNode, 'dijitDisplayNone', !visible);
+		},
+
+		isValid: function() {
+			// text is always valid
+			return true;
+		}
+	});
+});
