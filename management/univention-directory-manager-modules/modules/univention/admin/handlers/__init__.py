univention/univention-corporate-server

commit 8c644509c7be271cafb9949cbd33fdf222d83dd1

Bug #45842: use generic unmapped_lookup_filter()

management/univention-directory-manager-modules/modules/univention/admin/handlers/users/user.py
@@ -2623,8 +2623,8 @@ class object(univention.admin.handlers.simpleLdap, mungeddial.Support):
 		return struct.pack("%iB" % len(bytes), *bytes)
 
 	@classmethod
-	def lookup_filter(cls, filter_s=None, lo=None):
-		lookup_filter_obj = univention.admin.filter.conjunction('&', [
+	def unmapped_lookup_filter(cls):
+		return univention.admin.filter.conjunction('&', [
 			univention.admin.filter.expression('objectClass', 'posixAccount'),
 			univention.admin.filter.expression('objectClass', 'shadowAccount'),
 			univention.admin.filter.expression('objectClass', 'univentionMail'),
@@ -2636,72 +2636,69 @@ class object(univention.admin.handlers.simpleLdap, mungeddial.Support):
 			univention.admin.filter.conjunction('!', [univention.admin.filter.expression('uid', '*$')]),
 			univention.admin.filter.conjunction('!', [univention.admin.filter.expression('univentionObjectFlag', 'functional')]),
 		])
-		# ATTENTION: has its own rewrite function.
-		lookup_filter_obj.append_unmapped_filter_string(filter_s, rewrite, mapping)
-		return lookup_filter_obj
 
-
-def rewrite(filter, mapping):
-	if filter.variable == 'username':
-		filter.variable = 'uid'
-	elif filter.variable == 'firstname':
-		filter.variable = 'givenName'
-	elif filter.variable == 'lastname':
-		filter.variable = 'sn'
-	elif filter.variable == 'primaryGroup':
-		filter.variable = 'gidNumber'
-
-	elif filter.variable == 'disabled':
-		if filter.value == 'none':
-			filter.variable = '&(!(shadowExpire=1))(!(krb5KDCFlags=254))(!(|(sambaAcctFlags=[UD       ])(sambaAcctFlags'
-			filter.value = '[ULD       ])))'
-		elif filter.value == 'all':
-			filter.variable = '&(shadowExpire=1)(krb5KDCFlags=254)(|(sambaAcctFlags=[UD       ])(sambaAcctFlags'
-			filter.value = '[ULD       ]))'
-		elif filter.value == 'posix':
-			filter.variable = 'shadowExpire'
-			filter.value = '1'
-		elif filter.value == 'kerberos':
-			filter.variable = 'krb5KDCFlags'
-			filter.value = '254'
-		elif filter.value == 'windows':
-			filter.variable = '|(sambaAcctFlags=[UD       ])(sambaAcctFlags'
-			filter.value = '=[ULD       ])'
-		elif filter.value == 'windows_kerberos':
-			filter.variable = '&(krb5KDCFlags=254)(|(sambaAcctFlags=[UD       ])(sambaAcctFlags'
-			filter.value = '=[ULD       ]))'
-		elif filter.value == 'windows_posix':
-			filter.variable = '&(shadowExpire=1)(|(sambaAcctFlags=[UD       ])(sambaAcctFlags'
-			filter.value = '=[ULD       ]))'
-		elif filter.value == 'posix_kerberos':
-			filter.variable = '&(shadowExpire=1)(krb5KDCFlags'
-			filter.value = '254)'
-		elif filter.value == '*':
+	@classmethod
+	def rewrite(cls, filter, mapping):
+		if filter.variable == 'username':
 			filter.variable = 'uid'
+		elif filter.variable == 'firstname':
+			filter.variable = 'givenName'
+		elif filter.variable == 'lastname':
+			filter.variable = 'sn'
+		elif filter.variable == 'primaryGroup':
+			filter.variable = 'gidNumber'
 
-	elif filter.variable == 'locked':
-		# substring match for userPassword is not possible
-		if filter.value in ['posix', 'windows', 'all', 'none']:
-			if filter.value == 'all':
-				filter.variable = '|(sambaAcctFlags=[UL       ])(sambaAcctFlags'
-				filter.value = '[ULD       ])'
-				# filter.variable='|(sambaAcctFlags=[UL       ])(sambaAcctFlags=[ULD       ])(userPassword'
-				# filter.value = '{crypt}!*)'
-			if filter.value == 'windows':
-				filter.variable = '|(sambaAcctFlags=[UL       ])(sambaAcctFlags'
-				filter.value = '[ULD       ])'
-			# if filter.value == 'posix':
-			#	filter.variable='userPassword'
-			#	filter.value = '{crypt}!*'
+		elif filter.variable == 'disabled':
 			if filter.value == 'none':
-				# filter.variable='&(!(sambaAcctFlags=[UL       ]))(!(sambaAcctFlags=[ULD       ]))(!(userPassword'
-				# filter.value = '{crypt}!*))'
-				filter.variable = '&(!(sambaAcctFlags=[UL       ]))(!(sambaAcctFlags'
+				filter.variable = '&(!(shadowExpire=1))(!(krb5KDCFlags=254))(!(|(sambaAcctFlags=[UD       ])(sambaAcctFlags'
+				filter.value = '[ULD       ])))'
+			elif filter.value == 'all':
+				filter.variable = '&(shadowExpire=1)(krb5KDCFlags=254)(|(sambaAcctFlags=[UD       ])(sambaAcctFlags'
 				filter.value = '[ULD       ]))'
-		elif filter.value == '*':
-			filter.variable = 'uid'
-	else:
-		univention.admin.mapping.mapRewrite(filter, mapping)
+			elif filter.value == 'posix':
+				filter.variable = 'shadowExpire'
+				filter.value = '1'
+			elif filter.value == 'kerberos':
+				filter.variable = 'krb5KDCFlags'
+				filter.value = '254'
+			elif filter.value == 'windows':
+				filter.variable = '|(sambaAcctFlags=[UD       ])(sambaAcctFlags'
+				filter.value = '=[ULD       ])'
+			elif filter.value == 'windows_kerberos':
+				filter.variable = '&(krb5KDCFlags=254)(|(sambaAcctFlags=[UD       ])(sambaAcctFlags'
+				filter.value = '=[ULD       ]))'
+			elif filter.value == 'windows_posix':
+				filter.variable = '&(shadowExpire=1)(|(sambaAcctFlags=[UD       ])(sambaAcctFlags'
+				filter.value = '=[ULD       ]))'
+			elif filter.value == 'posix_kerberos':
+				filter.variable = '&(shadowExpire=1)(krb5KDCFlags'
+				filter.value = '254)'
+			elif filter.value == '*':
+				filter.variable = 'uid'
+
+		elif filter.variable == 'locked':
+			# substring match for userPassword is not possible
+			if filter.value in ['posix', 'windows', 'all', 'none']:
+				if filter.value == 'all':
+					filter.variable = '|(sambaAcctFlags=[UL       ])(sambaAcctFlags'
+					filter.value = '[ULD       ])'
+					# filter.variable='|(sambaAcctFlags=[UL       ])(sambaAcctFlags=[ULD       ])(userPassword'
+					# filter.value = '{crypt}!*)'
+				if filter.value == 'windows':
+					filter.variable = '|(sambaAcctFlags=[UL       ])(sambaAcctFlags'
+					filter.value = '[ULD       ])'
+				# if filter.value == 'posix':
+				#	filter.variable='userPassword'
+				#	filter.value = '{crypt}!*'
+				if filter.value == 'none':
+					# filter.variable='&(!(sambaAcctFlags=[UL       ]))(!(sambaAcctFlags=[ULD       ]))(!(userPassword'
+					# filter.value = '{crypt}!*))'
+					filter.variable = '&(!(sambaAcctFlags=[UL       ]))(!(sambaAcctFlags'
+					filter.value = '[ULD       ]))'
+			elif filter.value == '*':
+				filter.variable = 'uid'
+		else:
+			univention.admin.mapping.mapRewrite(filter, mapping)
 
 
 lookup = object.lookup

management/univention-directory-manager-modules/modules/univention/admin/handlers/users/ldap.py
@@ -223,8 +223,8 @@ class object(univention.admin.handlers.simpleLdap):
 			univention.admin.allocators.release(self.lo, self.position, i, j)
 
 	@classmethod
-	def lookup_filter(cls, filter_s=None, lo=None):
-		lookup_filter_obj = univention.admin.filter.conjunction('&', [
+	def unmapped_lookup_filter(cls):
+		return univention.admin.filter.conjunction('&', [
 			univention.admin.filter.expression('objectClass', 'simpleSecurityObject'),
 			univention.admin.filter.expression('objectClass', 'uidObject'),
 			univention.admin.filter.expression('objectClass', 'person'),
@@ -233,8 +233,6 @@ class object(univention.admin.handlers.simpleLdap):
 			univention.admin.filter.conjunction('!', [univention.admin.filter.expression('uid', '*$')]),
 			univention.admin.filter.conjunction('!', [univention.admin.filter.expression('univentionObjectFlag', 'functional')]),
 		])
-		lookup_filter_obj.append_unmapped_filter_string(filter_s, univention.admin.mapping.mapRewrite, mapping)
-		return lookup_filter_obj
 
 
 lookup = object.lookup

commit 24d02d3f722a9898678ada8cb6f6c1164c423645

Bug #48621: add possibility to filter for options
This fixes also that the custom users/user filter is evaluated again,
which was broken since 8c644509c7be271cafb9949cbd33fdf222d83dd1.

This also revert the function signature change of
univention.admin.mapping.mapRewrite() of
1587325369a81ae311ef195e054d1c4907b62815.

management/univention-directory-manager-modules/modules/univention/admin/handlers/__init__.py
@@ -454,7 +454,7 @@ class simpleLdap(object):
 
 		.. warning:: this method changes the set value to the default if it is unset. For a side effect free retrieval of the value use :func:`univention.admin.handlers.simpleLdap.get`.
 		"""
-		_d = univention.debug.function('admin.handlers.base.__getitem__ key = %s' % key)
+		_d = univention.debug.function('admin.handlers.base.__getitem__ key = %s' % key)  # noqa
 		if not key:
 			return None
 
@@ -1538,7 +1538,7 @@ class simpleLdap(object):
 					policy.mapping.unregister(pname, False)
 
 	def _update_policies(self):  # type: () -> None
-		_d = univention.debug.function('admin.handlers.simpleLdap._update_policies')
+		_d = univention.debug.function('admin.handlers.simpleLdap._update_policies')  # noqa
 		for policy_type, policy_object in self.policyObjects.items():
 			univention.debug.debug(univention.debug.ADMIN, univention.debug.INFO, "simpleLdap._update_policies: processing policy of type: %s" % policy_type)
 			if policy_object.changes:
@@ -1676,7 +1676,9 @@ class simpleLdap(object):
 
 	@classmethod
 	def rewrite_filter(cls, filter, mapping):
-		property_ = univention.admin.modules.get(cls.module).property_descriptions.get(filter.variable)
+		key = filter.variable
+		mod = univention.admin.modules.get(cls.module)
+		property_ = mod.property_descriptions.get(key)
 		if property_ and not isinstance(filter.value, (list, tuple)):
 			if property_.multivalue:
 				# special case: mutlivalue properties need to be a list when map()-ing
@@ -1684,19 +1686,34 @@ class simpleLdap(object):
 			if issubclass(property_.syntax if inspect.isclass(property_.syntax) else type(property_.syntax), univention.admin.syntax.complex):
 				# special case: complex syntax properties need to be a list (of lists, if multivalue)
 				filter.value = [filter.value]
-		# special case: properties that are represented as Checkboxes in the
-		# frontend should include '(!(propertyName=*))' in the ldap filter
-		# if the Checkboxe is set to False to also find objects where the property
-		# is not set. In that case we don't want to map the '*' to a different value.
-		dont_map_value = (
-				property_ 
-				and issubclass(
-					property_.syntax if inspect.isclass(property_.syntax) else type(property_.syntax),
-					(univention.admin.syntax.IStates, univention.admin.syntax.boolean)
-				)
-				and filter.value == '*'
-		)
-		univention.admin.mapping.mapRewrite(filter, mapping, dont_map_value=dont_map_value)
+		elif not property_ and key == 'options' and filter.value in getattr(mod, 'options', {}):
+			ocs = mod.options[filter.value]
+			filter.variable = 'objectClass'
+			if len(ocs.objectClasses) > 1:
+				con = univention.admin.filter.conjunction('&', [univention.admin.filter.expression('objectClass', oc, escape=True) for oc in ocs.objectClasses])
+				filter.__dict__.clear()
+				filter.__dict__.update(con.__dict__.copy())
+				filter.__class__ = univention.admin.filter.conjunction
+			elif ocs.objectClasses:
+				filter.value = list(ocs.objectClasses)[0]
+			return
+
+		try:
+			if not mapping.shouldMap(filter.variable):
+				return
+		except KeyError:
+			return
+
+		filter.variable = mapping.mapName(key)
+		if filter.value == '*' and property_ and issubclass(property_.syntax if inspect.isclass(property_.syntax) else type(property_.syntax), (univention.admin.syntax.IStates, univention.admin.syntax.boolean)):
+			# special case: properties that are represented as Checkboxes in the
+			# frontend should include '(!(propertyName=*))' in the ldap filter
+			# if the Checkboxe is set to False to also find objects where the property
+			# is not set. In that case we don't want to map the '*' to a different value.
+			pass
+		else:
+			filter.value = mapping.mapValue(key, filter.value)
+
 		if isinstance(filter.value, (list, tuple)) and filter.value:
 			# complex syntax
 			filter.value = filter.value[0]
@@ -3292,7 +3309,7 @@ class simplePolicy(simpleLdap):
 				return key
 
 	def __makeUnique(self):
-		_d = univention.debug.function('admin.handlers.simplePolicy.__makeUnique')
+		_d = univention.debug.function('admin.handlers.simplePolicy.__makeUnique')  # noqa
 		identifier = self.getIdentifier()
 		components = self.info[identifier].split("_uv")
 		if len(components) > 1:

management/univention-directory-manager-modules/modules/univention/admin/handlers/users/user.py
@@ -2538,16 +2538,9 @@ class object(univention.admin.handlers.simpleLdap):
 		return ['*', 'pwdAccountLockedTime']
 
 	@classmethod
-	def rewrite(cls, filter, mapping):
-		if filter.variable == 'username':
-			filter.variable = 'uid'
-		elif filter.variable == 'firstname':
-			filter.variable = 'givenName'
-		elif filter.variable == 'lastname':
-			filter.variable = 'sn'
-		elif filter.variable == 'primaryGroup':
+	def rewrite_filter(cls, filter, mapping):
+		if filter.variable == 'primaryGroup':
 			filter.variable = 'gidNumber'
-
 		elif filter.variable == 'disabled':
 			# substring match for userPassword is not possible
 			if filter.value == '1':
@@ -2582,7 +2575,6 @@ class object(univention.admin.handlers.simpleLdap):
 				filter.value = '254)'
 			elif filter.value == '*':
 				filter.variable = 'uid'
-
 		elif filter.variable == 'locked':
 			if filter.value == '1':
 				filter.variable = '|(krb5KDCFlags:1.2.840.113556.1.4.803:=131072)(sambaAcctFlags=[UL       ])(sambaAcctFlags'
@@ -2610,7 +2602,7 @@ class object(univention.admin.handlers.simpleLdap):
 			elif filter.value == '*':
 				filter.variable = 'uid'
 		else:
-			univention.admin.mapping.mapRewrite(filter, mapping)
+			super(object, cls).rewrite_filter(filter, mapping)
 
 
 lookup = object.lookup

management/univention-directory-manager-modules/modules/univention/admin/mapping.py
@@ -465,7 +465,7 @@ def mapDiffAl(mapping, diff):  # UNUSED
 	return ml
 
 
-def mapRewrite(filter, mapping, dont_map_value=False):
+def mapRewrite(filter, mapping):
 	"""
 	Re-write UDM property name/value in UDM filter expression to LDAP attribute name/value.
 
@@ -482,7 +482,7 @@ def mapRewrite(filter, mapping, dont_map_value=False):
 		if not mapping.shouldMap(key):
 			return
 		k = mapping.mapName(key)
-		v = filter.value if dont_map_value else mapping.mapValue(key, filter.value)
+		v = mapping.mapValue(key, filter.value)
 	except KeyError:
 		return
 	if k: