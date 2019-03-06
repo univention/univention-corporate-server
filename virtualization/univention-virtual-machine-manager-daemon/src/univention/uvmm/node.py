univention/univention-corporate-server

commit 75bf8e557274cfef37bf0ea578d0c9fd64e113fc

Bug #22695 uvmm: Report IO errors

virtualization/univention-virtual-machine-manager-daemon/src/univention/uvmm/node.py
@@ -794,6 +794,7 @@ class Node(PersistentCached):
 		self.domainCB = [
 			self.conn.domainEventRegisterAny(None, libvirt.VIR_DOMAIN_EVENT_ID_LIFECYCLE, self.livecycle_event, None),
 			self.conn.domainEventRegisterAny(None, libvirt.VIR_DOMAIN_EVENT_ID_REBOOT, self.reboot_event, None),
+			self.conn.domainEventRegisterAny(None, libvirt.VIR_DOMAIN_EVENT_ID_IO_ERROR_REASON, self.error_event, None),
 		]
 
 	def livecycle_event(self, conn, dom, event, detail, opaque):
@@ -821,6 +822,8 @@ class Node(PersistentCached):
 				domStat.update(dom, redefined=event == libvirt.VIR_DOMAIN_EVENT_DEFINED)
 			if event in (libvirt.VIR_DOMAIN_EVENT_STARTED, libvirt.VIR_DOMAIN_EVENT_RESUMED):
 				self.write_novnc_tokens()
+			if event != libvirt.VIR_DOMAIN_EVENT_SUSPENDED and detail != libvirt.VIR_DOMAIN_EVENT_SUSPENDED_IOERROR:
+				domStat.pd.error = {}
 		except KeyError:
 			# during migration events are not ordered causal
 			pass
@@ -853,6 +856,38 @@ class Node(PersistentCached):
 							raise
 			finally:
 				domStat._restart = 0
+		except Exception:
+			log.error('%s: Exception handling callback', self.pd.uri, exc_info=True)
+			# don't crash the event handler
+
+	def error_event(self, conn, dom, srcpath, devalias, action, reason, opaque):
+		"""
+		Handle IO errors.
+		"""
+		log = logger.getChild('io')
+		try:
+			log.debug(
+				"Node %s Domain %s(%s) dev=%s[%s] action=%d reason=%s",
+				self.pd.name,
+				dom.name(),
+				dom.ID(),
+				devalias,
+				srcpath,
+				action,
+				reason,
+			)
+			uuid = dom.UUIDString()
+			try:
+				domStat = self.domains[uuid]
+			except LookupError:
+				return
+			error = {
+				'reason': reason,
+				'device': devalias,
+				'srcpath': srcpath,
+			}
+			error['msg'] = _('IO error "%(reason)s" on device "%(device)s[%(srcpath)s]"') % error
+			domStat.pd.error.update(error)
 		except Exception:
 			log.error('%s: Exception handling callback', self.pd.uri, exc_info=True)
 			# don't crash the event handler
@@ -1017,7 +1052,8 @@ class Node(PersistentCached):
 					'vnc_port': vnc[1] if vnc else -1,
 					'suspended': pd.suspended,
 					'description': descr,
-					'node_available': self.pd.last_try == self.pd.last_update
+					'node_available': self.pd.last_try == self.pd.last_update,
+					'error': pd.error,
 				})
 
 		return domains

doc/errata/staging/univention-virtual-machine-manager-daemon.yaml
@@ -5,7 +5,8 @@ scope: ucs_4.3-0-errata4.3-2
 src: univention-virtual-machine-manager-daemon
 fix: 7.0.0-14
 desc: |
- This update addresses the following issue:
+ This update addresses the following issues:
  * Fix assignment of target device name for newly added disks.
  * Drop corrupt cache files on errors.
-bug: [47956,35761]
+ * Report IO errors in UMC frontend.
+bug: [47956,22695]

virtualization/univention-virtual-machine-manager-daemon/src/univention/uvmm/protocol.py
@@ -593,6 +593,7 @@ class Data_Domain(object):
 		self.suspended = None  # True|False
 		self.available = None  # None: not set, (True|False) -> node availability
 		self.targethosts = None  # List of node hostnames
+		self.error = {}
 
 
 class Data_Node(object):

commit 19f5ffdb3a417ce8247c63a4b75caac7afad0022

Bug #22695 UVMM: Fix Traceback
> uvmmd.node.livecycle - DEBUG - Node xen16.phahn.dev Domain ucs43c(-1) Event Stopped Details Migrated
> uvmmd.node.livecycle - ERROR - qemu://xen16.phahn.dev/system: Exception handling callback
> Traceback (most recent call last):
>   File "/usr/lib/pymodules/python2.7/univention/uvmm/node.py", line 926, in livecycle_event
>     domStat.pd.error = {}
> UnboundLocalError: local variable 'domStat' referenced before assignment

Move code to reset domStat to section, where domStat gets defined.

Fixes: 75bf8e557274cfef37bf0ea578d0c9fd64e113fc

virtualization/univention-virtual-machine-manager-daemon/src/univention/uvmm/node.py
@@ -920,10 +920,10 @@ class Node(PersistentCached):
 					self.domains[uuid] = domStat
 
 				domStat.update(dom, redefined=event == libvirt.VIR_DOMAIN_EVENT_DEFINED)
+				if event != libvirt.VIR_DOMAIN_EVENT_SUSPENDED and detail != libvirt.VIR_DOMAIN_EVENT_SUSPENDED_IOERROR:
+					domStat.pd.error = {}
 			if event in (libvirt.VIR_DOMAIN_EVENT_STARTED, libvirt.VIR_DOMAIN_EVENT_RESUMED):
 				self.write_novnc_tokens()
-			if event != libvirt.VIR_DOMAIN_EVENT_SUSPENDED and detail != libvirt.VIR_DOMAIN_EVENT_SUSPENDED_IOERROR:
-				domStat.pd.error = {}
 		except KeyError:
 			# during migration events are not ordered causal
 			pass