8227b5277167cad00c2bd936d13e7cc30e537ad2
Johannes Keiser <keiser@univention.de>
2019-03-27 14:23:29 +0100

    Bug #49119: UMC producttest

---
+++ test/product-tests/umc/single-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# export UCS=10.200.7.160
+# export ADMIN_PASSWORD=univention
+# export WINRM_DOMAIN="four.three"
+# export WINRM_CLIENT=10.200.7.65
+# export WINRM_USER=Administrator
+# export WINRM_PASSWORD=univention
+
+set -x
+set -e
+
+
+echo "Success"
+
+exit 0
