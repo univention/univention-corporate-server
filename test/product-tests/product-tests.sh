a1f537c893a6ec67966338c0f834c15004135f04
Philipp Hahn <hahn@univention.de>
2019-03-27 11:28:50 +0100

    Bug #45818 PT: shell police
    Get UCS release version from git tag (instead of hard-coding 4.4-0)
    
    Make the script usable in environments where ucs-ec2-tools are only
    available on $PATH

--- test/product-tests/product-tests.sh
+++ test/product-tests/product-tests.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
+#
+# Execute UCS product tests in EC2 or KVM environment
+#
 
 set -x
 
+if command -v git >/dev/null 2>&1
+then
+	head="$(git describe --tags --match 'release-[1-9].[0-9]-[0-9]')" && release="${head:8:5}"
+fi
+[ -n "${release:-}" ] || release='4.4-0'
+
 export RELEASE_UPDATE="${release_update:-public}"
 export ERRATA_UPDATE="${errata_update:-testing}"
-export TARGET_VERSION=${TARGET_VERSION:-4.4-0}
-export UCS_VERSION=${UCS_VERSION:-4.4-0}
-export TEMPLATE_VERSION=${TEMPLATE_VERSION:-$UCS_VERSION}
-export UCSSCHOOL_RELEASE=${UCSSCHOOL_RELEASE:-scope}
-export KVM_BUILD_SERVER=${KVM_BUILD_SERVER:-lattjo.knut.univention.de}
-export KVM_USER=${KVM_USER:-$USER}
+export TARGET_VERSION="${TARGET_VERSION:=$release}"
+export UCS_VERSION="${UCS_VERSION:=$release}"
+export TEMPLATE_VERSION="${TEMPLATE_VERSION:=$UCS_VERSION}"
+export UCSSCHOOL_RELEASE=${UCSSCHOOL_RELEASE:=scope}
+export KVM_BUILD_SERVER="${KVM_BUILD_SERVER:=lattjo.knut.univention.de}"
+export KVM_USER="${KVM_USER:=$USER}"
 if [ "$KVM_USER" = "jenkins" ]; then
 	KVM_USER="build"
 fi
-export HALT=${HALT:-true}
-export CFG=$1
+export HALT="${HALT:=true}"
+export CFG="$1"
 
 # create the command and run in ec2 or kvm depending on cfg
-grep -q kvm_template "$CFG"
-if [ "$?" -eq 1 ]; then
-	declare -a cmd=("./ucs-ec2-tools/ucs-ec2-create" "-c")
+if ! grep -Fq kvm_template "$CFG"
+then
+	exe='ucs-ec2-create'
 else
-	declare -a cmd=("./ucs-ec2-tools/ucs-kvm-create" "-c")
+	exe='ucs-kvm-create'
 fi
-cmd+=($CFG)
+declare -a cmd=("$exe" -c "$CFG")
 "$HALT" && cmd+=("-t")
-"${cmd[@]}"
-test -e "./COMMAND_SUCCESS"
+# shellcheck disable=SC2123
+PATH="${PATH:+$PATH:}./ucs-ec2-tools"
+"${cmd[@]}" &&
+[ -e "./COMMAND_SUCCESS" ]
