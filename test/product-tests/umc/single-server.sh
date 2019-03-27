4c85b0eacadbb03ad9ad56060bb699f5232ae4ba
Florian Best <best@univention.de>
2019-03-27 16:28:01 +0100

    Bug #49119: check for large environment

--- test/product-tests/umc/single-server.sh
+++ test/product-tests/umc/single-server.sh
@@ -10,6 +10,11 @@
 set -x
 set -e
 
+# make sure we have a large environment
+[ "$(udm users/user list | grep ^DN | wc -l)" -gt 5000 ]
+[ "$(udm groups/group list | grep ^DN | wc -l)" -gt 1000 ]
+[ "$(udm shares/share list | grep ^DN | wc -l)" -gt 2000 ]
+[ "$(udm shares/printer list | grep ^DN | wc -l)" -gt 1000 ]
 
 echo "Success"
 
