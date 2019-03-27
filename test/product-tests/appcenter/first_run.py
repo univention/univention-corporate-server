186ae7ba53ac052a6e5ec9f386984ca892103417
Dirk Wiesenthal <wiesenthal@univention.de>
2019-03-27 17:28:16 +0100

    Bug #49124: KVM support for product-tests.sh

---
+++ test/product-tests/appcenter/first_run.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+import sys
+
+print 'HALLO stdout'
+print >>sys.stderr, 'HALLO stderr'
--- test/product-tests/product-tests.sh
+++ test/product-tests/product-tests.sh
@@ -6,11 +6,19 @@ export RELEASE_UPDATE="${release_update:-public}"
 export ERRATA_UPDATE="${errata_update:-testing}"
 export TARGET_VERSION=${TARGET_VERSION:-4.4-0}
 export UCS_VERSION=${UCS_VERSION:-4.4-0}
+export TEMPLATE_VERSION=${TEMPLATE_VERSION:-$TARGET_VERSION}
+export KVM_BUILD_SERVER=${KVM_BUILD_SERVER:-lattjo.knut.univention.de}
+export KVM_USER=${KVM_USER:-$USER}
 export HALT=${HALT:-true}
 export CFG=$1
 
-# create the command and run ucs ec2
-declare -a cmd=("./ucs-ec2-tools/ucs-ec2-create" "-c")
+# create the command and run in ec2 or kvm depending on cfg
+grep -q kvm_template "$CFG"
+if [ "$?" -eq 1 ]; then
+	declare -a cmd=("./ucs-ec2-tools/ucs-ec2-create" "-c")
+else
+	declare -a cmd=("./ucs-ec2-tools/ucs-kvm-create" "-c")
+fi
 cmd+=($CFG)
 "$HALT" && cmd+=("-t")
 "${cmd[@]}"
