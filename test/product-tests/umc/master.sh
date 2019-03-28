871444ddebb78267dcd43b872e31be714bdb37ad
Florian Best <best@univention.de>
2019-03-28 11:22:30 +0100

    Bug #49119: use multi server environment

--- test/product-tests/umc/master.sh
+++ test/product-tests/umc/master.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# export UCS=10.200.7.160
-# export ADMIN_PASSWORD=univention
-# export WINRM_DOMAIN="four.three"
-# export WINRM_CLIENT=10.200.7.65
-# export WINRM_USER=Administrator
-# export WINRM_PASSWORD=univention
-
 set -x
 set -e
+
+. lib.sh
 
 # make sure we have a large environment
 [ "$(udm users/user list | grep ^DN | wc -l)" -gt 5000 ]
---
+++ test/product-tests/umc/member.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x
+set -e
+
+. lib.sh
+
