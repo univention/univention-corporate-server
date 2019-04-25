univention/univention-corporate-server

commit e251b588ad07f579ed6eccd2aeab9c2db6437a14

Bug #49145 - Migrate to dh_python2

management/univention-appcenter/setup.py
@@ -1,12 +1,12 @@
-from distutils.core import setup
+from packaging import setup
 
-setup(name='univention-appcenter',
-    packages=[
-	'univention.appcenter',
-	'univention.appcenter.actions',
+setup(name='python-univention-appcenter',
+	packages=[
+		'univention.appcenter',
+		'univention.appcenter.actions',
 	],
-    package_dir={
-	'univention.appcenter': 'python/appcenter',
-	'univention.appcenter.actions': 'python/appcenter/actions',
+	package_dir={
+		'univention.appcenter': 'python/appcenter',
+		'univention.appcenter.actions': 'python/appcenter/actions',
 	},
 )

management/univention-appcenter/debian/control
@@ -8,6 +8,7 @@ Build-Depends: debhelper (>= 7.0.50~),
  univention-join,
  univention-config-dev,
  univention-management-console-dev,
+ python-debian,
  python-all,
  ucslint,
  stylus

management/univention-appcenter/debian/python-univention-appcenter.postinst
@@ -54,6 +54,4 @@ create_portal_entries.handler(ucr_instance(), changes)
 '
 fi
 
-univention-app info
-
 exit 0

management/univention-appcenter/debian/rules
@@ -70,6 +70,9 @@ override_dh_auto_install:
 	python setup_umc.py build --force -b build/univention-appcenter-umc install --force --root=debian/univention-management-console-module-appcenter --install-layout=deb
 	dh_auto_install
 
+	( cd debian/univention-management-console-module-appcenter && install -m755 -o root -g root -d usr/share/python-support && cp ../../python-legacy/univention-management-console-module-appcenter.public usr/share/python-support/univention-management-console-module-appcenter.public )
+	( cd debian/univention-management-console-module-apps && install -m755 -o root -g root -d usr/share/python-support && cp ../../python-legacy/univention-management-console-module-apps.public usr/share/python-support/univention-management-console-module-apps.public )
+
 	@set -e;\
 	for i in $(UDM_MO_FILES); do\
 		lang=`basename "$$i" .mo`;\

management/univention-appcenter/debian/univention-appcenter-docker.postinst
@@ -30,9 +30,6 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
-# this is for the files to be available when the ucr templates are
-# registered for the very first time
-
 #DEBHELPER#
 
 ucr set appcenter/docker?enabled

management/univention-appcenter/debian/univention-management-console-module-appcenter.postinst
@@ -34,18 +34,9 @@
 # Needs to be done because the source package uses dh_python2, but UMC modules
 # are still installed the old way
 # TODO: Remove once we migrated all UMC modules (namely dh-umc-module-install)
-if [ -e "/usr/share/pyshared/univention/admin/handlers/appcenter/__init__.py" ] ; then
-cat > /usr/share/python-support/univention-management-console-module-appcenter.public <<-EOF
-/usr/share/pyshared/univention/admin/handlers/appcenter/__init__.py
-/usr/share/pyshared/univention/admin/handlers/appcenter/app.py
-/usr/share/pyshared/univention/management/console/modules/appcenter/__init__.py
-/usr/share/pyshared/univention/management/console/modules/appcenter/app_center.py
-/usr/share/pyshared/univention/management/console/modules/appcenter/constants.py
-/usr/share/pyshared/univention/management/console/modules/appcenter/sanitizers.py
-/usr/share/pyshared/univention/management/console/modules/appcenter/util.py
-EOF
-update-python-modules univention-management-console-module-appcenter.public
-update-python-modules --post-install
+if which update-python-modules >/dev/null 2>&1; then
+	update-python-modules univention-management-console-module-appcenter.public
+	update-python-modules --post-install
 fi
 
 #DEBHELPER#

management/univention-appcenter/debian/univention-management-console-module-appcenter.postrm
@@ -1,37 +1,0 @@
-#!/bin/bash
-#
-# Univention Management Console Module Appcenter
-#  postrm file for the debian package
-#
-# Copyright 2019 Univention GmbH
-#
-# http://www.univention.de/
-#
-# All rights reserved.
-#
-# The source code of this program is made available
-# under the terms of the GNU Affero General Public License version 3
-# (GNU AGPL V3) as published by the Free Software Foundation.
-#
-# Binary versions of this program provided by Univention to you as
-# well as other copyrighted, protected or trademarked materials like
-# Logos, graphics, fonts, specific documentations and configurations,
-# cryptographic keys etc. are subject to a license agreement between
-# you and Univention and not subject to the GNU AGPL V3.
-#
-# In the case you use this program under the terms of the GNU AGPL V3,
-# the program is provided in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public
-# License with the Debian GNU/Linux or Univention distribution in file
-# /usr/share/common-licenses/AGPL-3; if not, see
-# <http://www.gnu.org/licenses/>.
-
-rm -f /usr/share/python-support/univention-management-console-module-appcenter.public
-
-#DEBHELPER#
-
-exit 0

management/univention-appcenter/debian/univention-management-console-module-apps.postinst
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Univention Management Console Module Apps
 #  postinst file for the debian package
@@ -34,12 +34,9 @@
 # Needs to be done because the source package uses dh_python2, but UMC modules
 # are still installed the old way
 # TODO: Remove once we migrated all UMC modules (namely dh-umc-module-install)
-if [ -e "/usr/share/pyshared/univention/management/console/modules/apps/__init__.py" ] ; then
-cat > /usr/share/python-support/univention-management-console-module-apps.public <<-EOF
-/usr/share/pyshared/univention/management/console/modules/apps/__init__.py
-EOF
-update-python-modules univention-management-console-module-apps.public
-update-python-modules --post-install
+if which update-python-modules >/dev/null 2>&1; then
+	update-python-modules univention-management-console-module-apps.public
+	update-python-modules --post-install
 fi
 
 #DEBHELPER#

commit d6df225d44e867fd64bfb304a980d86f28e4d9e5

Bug #49145 AppC: Fix dh_python2 migration
No need to specify separate package directories for sub-modules.
It's important to only list all (sub-)packages in "packages".

It breaks the Python API build.

Fixes: e251b588ad07f579ed6eccd2aeab9c2db6437a14

management/univention-appcenter/setup.py
@@ -5,8 +5,5 @@ setup(name='python-univention-appcenter',
 		'univention.appcenter',
 		'univention.appcenter.actions',
 	],
-	package_dir={
-		'univention.appcenter': 'python/appcenter',
-		'univention.appcenter.actions': 'python/appcenter/actions',
-	},
+	package_dir={'univention.appcenter': 'python/appcenter'},
 )