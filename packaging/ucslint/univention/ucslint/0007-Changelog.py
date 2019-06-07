7d459daf6f3609bd428bad4d4eebd0aa5c4b7e13
Philipp Hahn <hahn@univention.de>
2019-06-07 17:45:11 +0200

    Bug #49620 ucslint: Check debian/changelog is strict-monotonical

--- packaging/ucslint/univention/ucslint/0007-Changelog.py
+++ packaging/ucslint/univention/ucslint/0007-Changelog.py
@@ -30,6 +30,8 @@
 import univention.ucslint.base as uub
 import re
 import os
+from email.utils import mktime_tz, parsedate_tz
+from debian.changelog import Changelog, ChangelogParseError
 
 REticket = re.compile(r'''
 	(Bug:?[ ]\#[0-9]{1,6} # Bugzilla
@@ -37,6 +39,7 @@ REticket = re.compile(r'''
 	|Ticket(\#:[ ]|:?[ ]\#)2[0-9]{3}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])(?:1[0-9]{7}|21[0-9]{6})) # OTRS
 	(?![0-9]) # not followed by additional digits
 	''', re.VERBOSE)
+RECENT_ENTRIES = 5
 
 
 class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
@@ -45,6 +48,10 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 		return {
 			'0007-1': (uub.RESULT_WARN, 'failed to open file'),
 			'0007-2': (uub.RESULT_WARN, 'changelog does not contain ticket/bug/issue number'),
+			'0007-3': (uub.RESULT_ERROR, 'debian/changelog entries are not strict-monotonically increasing by time'),
+			'0007-4': (uub.RESULT_ERROR, 'debian/changelog entries are not strict-monotonically increasing by version'),
+			'0007-5': (uub.RESULT_WARN, 'old debian/changelog entries are not strict-monotonically increasing by time'),
+			'0007-6': (uub.RESULT_WARN, 'old debian/changelog entries are not strict-monotonically increasing by version'),
 		}
 
 	def check(self, path):
@@ -53,14 +60,31 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 
 		fn = os.path.join(path, 'debian', 'changelog')
 		try:
-			content = open(fn, 'r').read()
-		except EnvironmentError:
+			with open(fn, 'r') as stream:
+				changelog = Changelog(stream, strict=True)
+		except (EnvironmentError, ChangelogParseError):
 			self.addmsg('0007-1', 'failed to open and read file', fn)
 			return
 
-		REchangelog = re.compile('^ -- [^<]+ <[^>]+>', re.M)
-
-		firstEntry = REchangelog.split(content)[0]
-		match = REticket.search(firstEntry)
-		if not match:
+		for change in changelog[0].changes():
+			if REticket.search(change):
+				break
+		else:
 			self.addmsg('0007-2', 'latest changelog entry does not contain bug/ticket/issue number', fn)
+
+		last = None
+		for nr, block in enumerate(changelog):
+			if last:
+				if mktime_tz(parsedate_tz(last.date)) <= mktime_tz(parsedate_tz(block.date)):
+					if nr < RECENT_ENTRIES:
+						self.addmsg('0007-3', 'not strict-monotonically increasing by time: %s <= %s' % (last.date, block.date), fn)
+					else:
+						self.addmsg('0007-5', 'old not strict-monotonically increasing by time: %s <= %s' % (last.date, block.date), fn)
+
+				if last.version <= block.version:
+					if nr < RECENT_ENTRIES:
+						self.addmsg('0007-4', 'not strict-monotonically increasing by version: %s <= %s' % (last.version, block.version), fn)
+					else:
+						self.addmsg('0007-6', 'old not strict-monotonically increasing by version: %s <= %s' % (last.version, block.version), fn)
+
+			last = block
