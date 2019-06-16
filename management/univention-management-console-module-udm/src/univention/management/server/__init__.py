032f21478f3370bf60e0608a8db5532869a86959
Florian Best <best@univention.de>
2019-06-16 14:17:53 +0200

    Bug #27816: add UDM HTTP service proxy

---
+++ management/univention-management-console-module-udm/src/univention/management/server/__init__.py
@@ -0,0 +1,167 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+#
+# Univention Management Console
+#  Univention Directory Manager Module
+#
+# Copyright 2019 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+import sys
+import signal
+import argparse
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.iostream
+import tornado.web
+import tornado.httpclient
+import tornado.httputil
+import tornado.process
+
+import pycurl
+
+from univention.management.console.config import ucr
+
+
+class Server(tornado.web.RequestHandler):
+	"""A server which acts as proxy to multiple processes in different languages
+
+	TODO: Implement authentication via PAM
+	TODO: Implement ACL handling (restriction on certain pathes for certain users/groups)
+	TODO: Implement a SAML service provider
+	TODO: Implement management of modules
+	"""
+
+	LANGUAGE_SERVICE_MAPPING = {
+		'de': '/var/run/univention-management-module-udm-de-de.socket',
+		'de_DE': '/var/run/univention-management-module-udm-de-de.socket',
+		'en': '/var/run/univention-management-module-udm-en-us.socket',
+		'en_US': '/var/run/univention-management-module-udm-en-us.socket',
+	}
+	PROCESSES = {}
+
+	@tornado.gen.coroutine
+	def get(self):
+		accepted_language = self.get_browser_locale().code
+		socket = self.LANGUAGE_SERVICE_MAPPING.get(accepted_language, self.LANGUAGE_SERVICE_MAPPING['en'])
+		request = tornado.httpclient.HTTPRequest(
+			self.request.full_url(),
+			method=self.request.method,
+			body=self.request.body or None,
+			headers=self.request.headers,
+			allow_nonstandard_methods=True,
+			follow_redirects=False,
+			connect_timeout=20.0,  # TODO: raise value?
+			request_timeout=int(ucr.get('umc/http/response-timeout', '310')) + 1,
+			prepare_curl_callback=lambda curl: curl.setopt(pycurl.UNIX_SOCKET_PATH, socket),
+		)
+		client = tornado.httpclient.AsyncHTTPClient()
+		response = yield client.fetch(request, raise_error=False)
+		self.set_status(response.code, response.reason)
+		self._headers = tornado.httputil.HTTPHeaders()
+
+		self.add_header('Content-Language', accepted_language.replace('_', '-'))
+		for header, v in response.headers.get_all():
+			if header not in ('Content-Length', 'Transfer-Encoding', 'Content-Encoding', 'Connection', 'X-Http-Reason'):
+				self.add_header(header, v)
+
+		if response.body:
+			self.set_header('Content-Length', len(response.body))
+			self.write(response.body)
+		self.finish()
+
+	@tornado.web.asynchronous
+	def post(self):
+		return self.get()
+
+	@tornado.web.asynchronous
+	def put(self):
+		return self.get()
+
+	@tornado.web.asynchronous
+	def delete(self):
+		return self.get()
+
+	@tornado.web.asynchronous
+	def patch(self):
+		return self.get()
+
+	@tornado.web.asynchronous
+	def options(self):
+		return self.get()
+
+	@classmethod
+	def main(cls):
+		parser = argparse.ArgumentParser(prog='python -m univention.management.server')
+		parser.parse_args()
+		tornado.httpclient.AsyncHTTPClient.configure('tornado.curl_httpclient.CurlAsyncHTTPClient')
+		tornado.locale.load_gettext_translations('/usr/share/locale', 'univention-management-console-module-udm')
+		cls.start_processes()
+		cls.register_signal_handlers()
+		app = tornado.web.Application([
+			(r'.*', cls),
+		], debug=False, serve_traceback=True,
+		)
+		app.listen(int(ucr.get('umc/server/port', 8888)))
+		ioloop = tornado.ioloop.IOLoop.instance()
+		ioloop.start()
+
+	@classmethod
+	def start_processes(cls):
+		for language in ('de_DE', 'en_US'):
+			socket = cls.LANGUAGE_SERVICE_MAPPING[language]
+			cls.PROCESSES[language] = tornado.process.Subprocess(['/usr/bin/python2.7', '-m', 'univention.management.modules.udm', '-s', socket, '-l', language, 'run'], stdout=sys.stdout, stderr=sys.stderr)
+
+	@classmethod
+	def register_signal_handlers(cls):
+		signal.signal(signal.SIGTERM, cls.signal_handler_stop)
+		signal.signal(signal.SIGINT, cls.signal_handler_stop)
+		signal.signal(signal.SIGHUP, cls.signal_handler_reload)
+
+	@classmethod
+	def signal_handler_reload(cls, sig, frame):
+		for process in cls.PROCESSES.values():
+			os.kill(process.pid, sig)
+
+	@classmethod
+	def signal_handler_stop(cls, sig, frame):
+		for process in cls.PROCESSES.values():
+			os.kill(process.pid, sig)
+
+		io_loop = tornado.ioloop.IOLoop.instance()
+
+		def shutdown():
+			io_loop.stop()
+
+		io_loop.add_callback_from_signal(shutdown)
