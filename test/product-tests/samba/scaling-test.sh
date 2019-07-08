f9d2acb7ff488b32d04a2d7d31a7bcac9385799a
Felix Botner <botner@univention.de>
2019-07-08 18:14:18 +0200

    Bug #48090: qa

--- test/product-tests/samba/scaling-test.sh
+++ test/product-tests/samba/scaling-test.sh
@@ -4,6 +4,10 @@ set -x
 set -e
 
 scaling_tests () {
+
+	set -x
+	set -e
+
 	. product-tests/samba/utils.sh
 	eval "$(ucr shell ldap/base windows/domain)"
 	#5000 benutzer 1050 gruppen je 50 zufällig benutzer
@@ -13,7 +17,7 @@ scaling_tests () {
 	for i in $(seq 1 5000); do
 		newindex="$i"
 		udm users/user create --position "cn=users,$ldap_base" --set username="benutzer$newindex" \
-						--set lastname="newuser" --set password="Univention.99"
+			--set lastname="newuser" --set password="Univention.99"
 	done
 
 	for i in $(seq 1 1050); do
@@ -21,7 +25,18 @@ scaling_tests () {
 		for j in $(seq 1 50); do
 			newindex=$(shuf -i 1-5000 -n 1)
 			udm groups/group modify --dn "cn=gruppe$i,cn=groups,$ldap_base" \
-							--append users="uid=benutzer$newindex,cn=users,$ldap_base"
+				--append users="uid=benutzer$newindex,cn=users,$ldap_base"
 		done
 	done
+
+	# wait for replication
+	for i in $(seq 1 1080); do
+		if univention-s4search "cn=gruppe1050" | grep -q -i "cn: gruppe1050"; then
+			echo "found gruppe1050"
+			break
+		fi
+		echo "waiting for samba object cn=gruppe1050"
+		sleep 10
+	done
+
 }
