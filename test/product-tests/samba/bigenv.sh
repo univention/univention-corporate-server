8ef5cb03a9587e665c668c5372d5e2d4412623d5
Felix Botner <botner@univention.de>
2019-07-09 13:03:06 +0200

    Bug #45818: added update test with big env

--- test/product-tests/samba/bigenv.sh
+++ test/product-tests/samba/bigenv.sh
@@ -53,3 +53,66 @@ add: lDAPAdminLimits
 lDAPAdminLimits: MaxConnIdleTime=9000" | ldbmodify -H /var/lib/samba/private/sam.ldb --cross-ncs
 	/etc/init.d/samba restart
 }
+
+clone_copy_samdb () {
+	cp /usr/lib/python2.7/dist-packages/samba/join.py /usr/lib/python2.7/dist-packages/samba/join.py.bak
+	sed -i 's/    ctx.do_join()/    ctx.plaintext_secrets=True\n    ctx.do_join()/' /usr/lib/python2.7/dist-packages/samba/join.py
+	samba-tool drs clone-dc-database "$(dnsdomainname)" --server=master -UAdministrator%univention --targetdir /var/tmp/master --include-secrets
+	sed -i 's/    ctx.plaintext_secrets=True//' /usr/lib/python2.7/dist-packages/samba/join.py
+	echo univention > /tmp/pw
+	univention-ssh-rsync /tmp/pw -a /var/tmp/master/ master:/var/tmp/master/
+}
+
+replace_samdb_and_upgrade () {
+	ucr set dns/backend='ldap'
+	/etc/init.d/bind9 restart
+	/etc/init.d/samba stop
+
+	local ridsetldif=$(ldbsearch -H /var/lib/samba/private/sam.ldb -b  "CN=RID Set,CN=$(hostname),OU=Domain Controllers,$(ucr get connector/s4/ldap/base)")
+	local nextrid=$(sed -n 's/^rIDNextRID: //p' <<<"$ridsetldif")
+	local prevpool=$(sed -n 's/^rIDPreviousAllocationPool: //p' <<<"$ridsetldif")
+
+	# Important: Backup the Samba private data directory:
+	cp -a /var/lib/samba/private/ /var/lib/samba/private.backup
+
+	rsync -a /var/tmp/master/private/sam.ldb /var/lib/samba/private/
+	rsync -a /var/tmp/master/private/sam.ldb.d/*.ldb /var/lib/samba/private/sam.ldb.d/
+
+	if [ -f /var/tmp/master/private/encrypted_secrets.key ]; then
+		rsync -a /var/tmp/master/private/encrypted_secrets.key /var/lib/samba/private/
+	fi
+
+	# Next update samba to make ldbedit work against the new Database format:
+	univention-upgrade --noninteractive --updateto=4.3-4 --disable-app-updates
+	/etc/init.d/samba stop
+
+   #  Finally restore the local RID Set state:
+echo -e "dn: CN=RID Set,CN=$(hostname),OU=Domain Controllers,$(ucr get connector/s4/ldap/base)
+changetype: modify
+replace: rIDNextRID
+rIDNextRID: $nextrid
+-
+replace: rIDPreviousAllocationPool
+rIDPreviousAllocationPool: $prevpool" | ldbmodify -H /var/lib/samba/private/sam.ldb
+
+	/etc/init.d/samba start
+	# give samba some time to repair drs
+	sleep 180
+}
+
+restart_connector_and_bind () {
+	/etc/init.d/univention-s4-connector start
+	ucr set dns/backend='samba4'
+	/etc/init.d/bind9 restart
+}
+
+bigenv_settings () {
+	# some settings for setups with a big database
+	ucr set directory/manager/user/primarygroup/update=false
+	ucr set connector/s4/mapping/group/syncmode=read
+	ucr set nss/group/cachefile/invalidate_interval=disabled
+	ucr set ldap/database/mdb/maxsize='4294967296'
+	ucr set listener/cache/mdb/maxsize='4294967296'
+	ucr set slapd/backup=disbaled
+	ucr unset samba4/backup/cron
+}
