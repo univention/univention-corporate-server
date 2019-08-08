754b0a1f62382c95a9fefead722ad29b153cb1da
Philipp Hahn <hahn@univention.de>
2019-08-08 13:51:41 +0200

    Bug #49978 l10n: Move files
    to make the programs useable from the source directory.

--- packaging/univention-ucs-translation-template/univention/translationhelper/__init__.py
+++ packaging/univention-ucs-translation-template/univention/translationhelper/__init__.py
@@ -27,4 +27,4 @@
 # License with the Debian GNU/Linux or Univention distribution in file
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
-from . translationhelper import *
+from . translationhelper import *  # noqa
