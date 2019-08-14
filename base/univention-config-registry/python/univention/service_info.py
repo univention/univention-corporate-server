univention/univention-corporate-server

commit df4b966c50b5441ea497c8bcc6e57a6b9bdc7b76

Bug #42241: move start/stop/restart logic into service_info.py

base/univention-config-registry/python/univention/service_info.py
@@ -33,9 +33,14 @@
 
 import os
 import shlex
+import subprocess
 from logging import getLogger
 
 import univention.info_tools as uit
+
+
+class ServiceError(Exception):
+	"""Error when starting, stopping or restarting a service."""
 
 
 class Service(uit.LocalizedDictionary):
@@ -71,6 +76,50 @@ class Service(uit.LocalizedDictionary):
 				break
 		else:
 			self.running = True
+
+	def start(self):
+		"""Start the service."""
+		return self._change_state('start')
+
+	def stop(self):
+		"""Stop the service."""
+		return self._change_state('stop')
+
+	def restart(self):
+		"""Restart the service."""
+		return self._change_state('restart')
+
+	def status(self):
+		"""Get status of the service."""
+		try:
+			return self.__change_state('status')[1]
+		except EnvironmentError:
+			return u''
+
+	def _change_state(self, action):
+		rc, output = self.__change_state(action)
+		if rc:
+			raise ServiceError(self.status() or output)
+		return True
+
+	def __change_state(self, action):
+		if self.get('init_script'):
+			# samba currently must not be started via systemd
+			return self._exec(('/etc/init.d/%s' % (self['init_script'],), action))
+
+		service_name = self._service_name()
+		return self._exec(('/usr/sbin/service', service_name, action))
+
+	def _service_name(self):
+		service_name = self.get('systemd', self.get('name'))
+		if service_name.endswith('.service'):
+			service_name = service_name.rsplit('.', 1)[0]
+		return service_name
+
+	def _exec(self, args):
+		process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
+		output = process.communicate()[0]
+		return process.returncode, output.decode('utf-8', 'replace').strip()
 
 
 def pidof(name, docker='/var/run/docker.pid'):
@@ -213,6 +262,7 @@ class ServiceInfo(object):
 			if not override and sec in self.services:
 				continue
 			srv = Service()
+			srv['name'] = sec
 			for name, value in cfg.items(sec):
 				srv[name] = value
 			for path in srv.get('programs', '').split(','):

commit 7352ba95120b34c88a7cd91e1f392e0ebc610964

Bug #42241 ucr: Silence u-install-service-info warning
Every invocation of "univention-install-service-info" (which is called by "univention-install-config-registry") now prints a warning message:

> univention-install-config-registry
> Incomplete entries in services definition univention-dhcp.univention-service
>   [univention-dhcp]
>     name

Add "name" to the list of optional properties.
Update manual page.

Fixes: df4b966c50b5441ea497c8bcc6e57a6b9bdc7b76

base/univention-config-registry/doc/univention-install-service-info.1
@@ -42,10 +42,18 @@ Name for an icon relative to \fI???\fP.
 .B Unused!
 .IP \fBprograms=\fP\fIname\fP[\fI,...\fP]
 Names of the executables, which must be running to flag this service as running.
+.IP \fBname=\fP\fIservice_name\fP
+The name of the service.
+Defaults to \fIservice_name\fP as used in the section header of the entry.
+.IP \fBsystemd=\fP\fIunit\fP[\fB.service\fP]
+Name of the systemd unit used to start, stop, and restart the service.
+Defaults to \fIname\fP.
+.IP \fBinit_script=\fP\fIname\fP
+Name of the SystemV init script \fB/etc/init.d/\fP\fIname\fP to use instead of the systemd unit.
 
 .SH ENVIRONMENT
 .IP "\s-1DH_VERBOSE\s0" 4
-Set to 1 to enable verbose mode.
+Set to \fB1\fP to enable verbose mode.
 Debhelper will output every command it runs that modifies files on the build system.
 
 .SH SEE ALSO

base/univention-config-registry/python/univention/service_info.py
@@ -49,7 +49,7 @@ class Service(uit.LocalizedDictionary):
 	"""
 
 	REQUIRED = frozenset(('description', 'programs'))
-	OPTIONAL = frozenset(('start_type', 'systemd', 'icon'))
+	OPTIONAL = frozenset(('start_type', 'systemd', 'icon', 'name', 'init_script'))
 	KNOWN = REQUIRED | OPTIONAL
 
 	def __init__(self, *args, **kwargs):

doc/developer-reference/ucr/ucr.xml
@@ -748,14 +748,18 @@ description[<language>]=<description>
 start_type=<service-name>/autostart
 systemd=<service-name>.service
 icon=<service/icon_name>
-programs=<executable>]]></programlisting>
+programs=<executable>
+name=<service-name>
+init_scipt=<init.name>]]></programlisting>
 			<variablelist>
 				<varlistentry>
 					<term><literal>[</literal><replaceable>service-name</replaceable><literal>]</literal></term>
+					<term><literal>name=</literal><replaceable>service-name</replaceable> (optional)</term>
 					<listitem>
 						<simpara>
 							For each daemon one section needs to be created.
 							The service-name should match the name of the init-script in <filename class="directory">/etc/init.d/</filename>.
+							If the name differs, it can be overwritten by the <literal>name=</literal> property.
 						</simpara>
 					</listitem>
 				</varlistentry>
@@ -785,6 +789,16 @@ programs=<executable>]]></programlisting>
 						<simpara>
 							A comma separated list of <package>systemd</package> service names, which are enabled/disabled/masked when <literal>start_type</literal> is used.
 							This defaults to the name of the service plus the suffix <literal>.service</literal>.
+						</simpara>
+					</listitem>
+				</varlistentry>
+				<varlistentry>
+					<term><literal>init_script</literal> (optional)</term>
+					<listitem>
+						<simpara>
+							The name of the legacy init script below <filename class="directory">/etc/init.d/</filename>.
+							This defaults to the name of the service.
+							This option should not be used any more in favor of <application>systemd</application>.
 						</simpara>
 					</listitem>
 				</varlistentry>