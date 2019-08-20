235449f05be15b9b133951197e3d03035ea050f1
Soenke Schwardt-Krummrich <schwardt@univention.de>
2019-08-20 10:18:45 +0200

    Bug #50016: fix missing read permissions for source.list file

--- test/utils/utils.sh
+++ test/utils/utils.sh
@@ -1013,6 +1013,7 @@ add_branch_repository () {
 	local extra_list="/root/apt-get-branch-repo.list"
 	if [ -s "$extra_list" ]; then
 		cp "$extra_list" /etc/apt/sources.list.d/
+		chmod a+r "/etc/apt/sources.list.d/$extra_list"
 		apt-get update
 		return $?
 	fi
