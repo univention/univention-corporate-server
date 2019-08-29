0ef0f6d258aed3d4f2688c7c4e6cd9ca5ec5a564
Felix Botner <botner@univention.de>
2019-08-29 12:24:11 +0200

    Bug #50055: stabalize samba pt

--- test/product-tests/samba/multi-server.sh
+++ test/product-tests/samba/multi-server.sh
@@ -150,8 +150,9 @@ test_master () {
 	run_on_ucs_hosts $MEMBER "stat /home/testshare/test-admin01.txt"
 	run_on_ucs_hosts $SLAVE "stat /home/testshare/test-admin01.txt"
 	run_on_ucs_hosts $SLAVE "stat /home/testshare/test-newuser01.txt"
-	python shared-utils/ucs-winrm.py create-share-file --server ucs-slave --filename newuser03-test.txt \
-		--username 'newuser03' --userpwd "Univention.99" --share testshareSlave --debug 2>&1 | grep -i PermissionDenied
+	# check invalid write access
+	! python shared-utils/ucs-winrm.py create-share-file --server ucs-slave --filename newuser03-test.txt --username 'newuser03' --userpwd "Univention.99" --share testshareSlave --debug
+	! run_on_ucs_hosts $SLAVE "stat /home/testshare/newuser03-test.txt"
 	python shared-utils/ucs-winrm.py create-share-file --server $MASTER --share "testshare" --filename "testfile.txt" --username 'administrator' --userpwd "$ADMIN_PASSWORD"
 	python shared-utils/ucs-winrm.py create-share-file --server $MASTER --filename test-admin.txt --username 'Administrator' --userpwd "$ADMIN_PASSWORD" --share Administrator
 	stat /home/Administrator/test-admin.txt
