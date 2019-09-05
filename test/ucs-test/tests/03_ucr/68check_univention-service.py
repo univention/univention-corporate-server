97c0761a384ca7630b5c5202baf65386f167ded8
Jürn Brodersen <brodersen@univention.de>
2019-09-05 14:36:33 +0200

    Bug #50098: Add 03_ucr/68check_univention-service.py

---
+++ test/ucs-test/tests/03_ucr/68check_univention-service.py
@@ -0,0 +1,19 @@
+#!/usr/share/ucs-test/runner /usr/bin/py.test
+# -*- coding: utf-8 -*-
+## desc: Check the service info files for ucr and the umc services module
+## exposure: careful
+## tags: [apptest]
+## bugs: [50098]
+
+from univention.service_info import ServiceInfo
+
+
+def test_check_univention_service():
+	services_info = ServiceInfo()
+	service_problems = services_info.check_services()
+	print('Services with problems:\n{}'.format(service_problems))
+	assert len(service_problems) == 0
+
+
+if __name__ == '__main__':
+	test_check_univention_service()
