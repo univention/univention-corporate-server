acb2b0437bc3c6f7b5148c35691cbf8c2b1633f6
Felix Botner <botner@univention.de>
2019-09-24 10:04:13 +0200

    Bug #50166: Stop/Start docker only on joined systems

--- base/univention-updater/script/postup.sh
+++ base/univention-updater/script/postup.sh
@@ -147,17 +147,19 @@ fi
 service univention-firewall restart >>"$UPDATER_LOG" 2>&1
 
 # Bug #50166: Docker update only worked smoothly with stopped containers...
-echo "Starting all Docker Apps again..."
-python -c "
-from univention.appcenter.log import log_to_stream
-from univention.appcenter.app_cache import Apps
-from univention.appcenter.actions import get_action
-log_to_stream()
-apps = [app for app in Apps().get_all_locally_installed_apps() if app.docker]  # single container apps and multi container apps alike
-start = get_action('start')
-for app in apps:
-	start.call(app=app)"
-echo "... Docker Apps started."
+if [ -f /var/univention-join/joined ]; then
+	echo "Starting all Docker Apps again..."
+	python -c "
+	from univention.appcenter.log import log_to_stream
+	from univention.appcenter.app_cache import Apps
+	from univention.appcenter.actions import get_action
+	log_to_stream()
+	apps = [app for app in Apps().get_all_locally_installed_apps() if app.docker]  # single container apps and multi container apps alike
+	start = get_action('start')
+	for app in apps:
+		start.call(app=app)"
+	echo "... Docker Apps started."
+fi
 
 echo "
 
