univention/univention-corporate-server

commit 23187ded67d7ac71e6de589759c0bf00926e6a5a

Bug #50319 nagios: Set PATH in SUID wrappers
exec?e(envp=NULL) only sets `PATH=/usr/bin:/bin` (or whatever
`confstr(_CS_PATH)` returns. This is not enough as we need to execute
programs from `/sbin:/usr/sbin` as well.

As it is the job of the SUID wrapper to provide a clean and sane
environment, explicitly configure PATH.

Also fix debian/control to fix several warning:
    dpkg-gencontrol: Warnung: Feld Provides von Paket univention-nagios-server: unbekannte Substitutionsvariable ${python:Provides}
    dpkg-gencontrol: Warnung: Paket univention-nagios-client: unbenutzte Substitutionsvariable ${shlibs:Depends}
    dpkg-gencontrol: Warnung: Paket univention-nagios-client: unbenutzte Substitutionsvariable ${shlibs:Depends}
    dpkg-gencontrol: Warnung: Feld Depends von Paket univention-nagios-common: unbekannte Substitutionsvariable ${python:Depends}

Turn on build hardening.

Cherry-pick: 849d7357abb4d47ac6609fc9dbb6f7c9da6cd426

nagios/univention-nagios/usr/lib/nagios/plugins/check_univention_joinstatus_suidwrapper.c
@@ -7,20 +7,28 @@
 
 #define COMMAND "/usr/lib/nagios/plugins/check_univention_joinstatus"
 
+static char *const suid_envp[] = {
+	"PATH=/usr/sbin:/usr/bin:/sbin:/bin",
+	NULL
+};
 
 main( int argc, char ** argv, char ** envp )
 {
-    int status = 0;
 	int i = 0;
-	uid_t uid = getuid();
-    if( setgid(getegid()) ) perror( "setgid" );
-    if( setuid(geteuid()) ) perror( "setuid" );
+	if (setgid(getegid())) {
+		perror("setgid");
+		return EXIT_FAILURE;
+	}
+	if (setuid(geteuid())) {
+		perror("setuid");
+		return EXIT_FAILURE;
+	}
 	for(i=0; i<argc; i++) {
 	  if (( strcmp("-L", argv[i]) == 0 ) && (i+1 < argc)) {
-		execle(COMMAND, COMMAND, "-L", argv[i+1], (char *)0, (char *)0);
+		execle(COMMAND, COMMAND, "-L", argv[i+1], NULL, &suid_envp);
 	  }
 	}
-	execle(COMMAND, COMMAND, (char *)0, (char *)0);
-	setuid(uid);
-	exit(1);
+	execle(COMMAND, COMMAND, NULL, &suid_envp);
+	perror("execle");
+	return EXIT_FAILURE;
 }

nagios/univention-nagios/usr/lib/nagios/plugins/check_univention_nscd_suidwrapper.c
@@ -7,20 +7,28 @@
 
 #define COMMAND "/usr/lib/nagios/plugins/check_univention_nscd"
 
+static char *const suid_envp[] = {
+	"PATH=/usr/sbin:/usr/bin:/sbin:/bin",
+	NULL
+};
 
 main( int argc, char ** argv, char ** envp )
 {
-    int status = 0;
 	int i = 0;
-	uid_t uid = getuid();
-    if( setgid(getegid()) ) perror( "setgid" );
-    if( setuid(geteuid()) ) perror( "setuid" );
+	if (setgid(getegid())) {
+		perror("setgid");
+		return EXIT_FAILURE;
+	}
+	if (setuid(geteuid())) {
+		perror("setuid");
+		return EXIT_FAILURE;
+	}
 	for(i=0; i<argc; i++) {
 	  if (( strcmp("-L", argv[i]) == 0 ) && (i+1 < argc)) {
-		execle(COMMAND, COMMAND, "-L", argv[i+1], (char *)0, (char *)0);
+		execle(COMMAND, COMMAND, "-L", argv[i+1], NULL, &suid_envp);
 	  }
 	}
-	execle(COMMAND, COMMAND, (char *)0, (char *)0);
-	setuid(uid);
-	exit(1);
+	execle(COMMAND, COMMAND, NULL, &suid_envp);
+	perror("execle");
+	return EXIT_FAILURE;
 }

nagios/univention-nagios/usr/lib/nagios/plugins/check_univention_slapd_mdb_maxsize_suidwrapper.c
@@ -38,7 +38,12 @@
 
 #define COMMAND "/usr/lib/nagios/plugins/check_univention_slapd_mdb_maxsize"
 
-main(int argc, char ** argv, char ** envp) {
+static char *const suid_envp[] = {
+	"PATH=/usr/sbin:/usr/bin:/sbin:/bin",
+	NULL
+};
+
+int main(int argc, char ** argv, char ** envp) {
 	int i = 0;
 	char warning[] = "75";
 	char critical[] = "90";
@@ -54,13 +59,20 @@ main(int argc, char ** argv, char ** envp) {
 				exit(EXIT_FAILURE);
 		}
 	}
-	uid_t uid = getuid();
-	if (setgid(getegid()))
+	if (setgid(getegid())) {
 		perror("setgid");
-	if (setuid(geteuid()))
+		return EXIT_FAILURE;
+	}
+	if (setuid(geteuid())) {
 		perror("setuid");
-	execle(COMMAND, COMMAND, "-w", warning, "-c", critical, (char *)0, (char *)0);
+		return EXIT_FAILURE;
+	}
+
+	if (listener) {
+		execle(COMMAND, COMMAND, "-l", "-w", warning, "-c", critical, NULL, &suid_envp);
+	} else {
+		execle(COMMAND, COMMAND, "-w", warning, "-c", critical, NULL, &suid_envp);
+	}
 	perror("execle");
-	setuid(uid);
-	exit(0);
+	return EXIT_FAILURE;
 }

nagios/univention-nagios/Makefile
@@ -1,11 +1,8 @@
 #!/usr/bin/make -f
 #
-# Univention Print Server
-#  rules file for the debian package
-#
 # Copyright 2004-2019 Univention GmbH
 #
-# http://www.univention.de/
+# https://www.univention.de/
 #
 # All rights reserved.
 #
@@ -28,31 +25,12 @@
 # You should have received a copy of the GNU Affero General Public
 # License with the Debian GNU/Linux or Univention distribution in file
 # /usr/share/common-licenses/AGPL-3; if not, see
-# <http://www.gnu.org/licenses/>.
-#
-DPKG_EXPORT_BUILDFLAGS = 1
-include /usr/share/dpkg/buildflags.mk
+# <https://www.gnu.org/licenses/>.
 
-DS=$(CURDIR)/debian/univention-nagios-server
-DC=$(CURDIR)/debian/univention-nagios-client
+SRC := $(wildcard usr/lib/nagios/plugins/check_univention_*_suidwrapper.c)
+BIN := $(patsubst %.c,%,$(SRC))
 
-override_dh_fixperms:
-	dh_fixperms
-	chmod u+s ${DC}/usr/lib/nagios/plugins/check_univention_joinstatus_suidwrapper
-	chmod u+s ${DC}/usr/lib/nagios/plugins/check_univention_ldap_suidwrapper
-	chmod u+s ${DC}/usr/lib/nagios/plugins/check_univention_winbind_suidwrapper
-	chmod u+s ${DC}/usr/lib/nagios/plugins/check_univention_nscd_suidwrapper
-	chmod u+s ${DC}/usr/lib/nagios/plugins/check_univention_slapd_mdb_maxsize_suidwrapper
+all: $(BIN)
 
-override_dh_auto_install:
-	univention-install-config-registry
-	univention-install-config-registry-info
-	univention-install-service-info
-	dh_auto_install
-
-override_dh_auto_test:
-	ucslint
-	dh_auto_test
-
-%:
-	dh $@ --with python_support
+clean:
+	$(RM) $(BIN)

nagios/univention-nagios/debian/control
@@ -2,7 +2,7 @@ Source: univention-nagios
 Section: univention
 Priority: optional
 Maintainer: Univention GmbH <packages@univention.de>
-Build-Depends: debhelper (>= 7.0.50~),
+Build-Depends: debhelper (>= 9),
  univention-config-dev,
  python-all,
  python-support,
@@ -30,7 +30,7 @@ Depends: ${misc:Depends}, ${python:Depends},
  univention-nagios-dansguardian,
  python-univention-lib (>= 3.0.26-14),
  shell-univention-lib,
-Provides: ${python:Provides},
+Provides:
  univention-nagios-group-access
 Conflicts: nagios2 (<< 3.0),
  univention-nagios-group-access
@@ -47,6 +47,7 @@ Description: UCS: nagios server support
 Package: univention-nagios-client
 Architecture: any
 Depends: ${misc:Depends}, ${python:Depends},
+ ${shlibs:Depends},
  univention-nagios-common (= ${binary:Version}),
  univention-directory-manager-tools (>> 7.0.147),
  monitoring-plugins,
@@ -72,7 +73,7 @@ Description: UCS: nagios client support
 
 Package: univention-nagios-common
 Architecture: any
-Depends: ${misc:Depends}, ${python:Depends},
+Depends: ${misc:Depends},
  univention-directory-manager-tools (>> 7.0.147),
  univention-config,
  univention-directory-listener,

nagios/univention-nagios/usr/lib/nagios/plugins/check_univention_ldap_suidwrapper.c
@@ -4,7 +4,8 @@
 
 static const char COMMAND[] = "/usr/lib/nagios/plugins/check_univention_ldap";
 
-static char *suid_envp[] = {
+static char *const suid_envp[] = {
+	"PATH=/usr/sbin:/usr/bin:/sbin:/bin",
 	NULL
 };
 

nagios/univention-nagios/usr/lib/nagios/plugins/check_univention_winbind_suidwrapper.c
@@ -8,12 +8,22 @@
 
 #define COMMAND "/usr/lib/nagios/plugins/check_univention_winbind"
 
+static char *const suid_envp[] = {
+	"PATH=/usr/sbin:/usr/bin:/sbin:/bin",
+	NULL
+};
 
 int main ( int argc, char ** argv, char ** envp )
 {
-	uid_t uid = getuid();
-    if( setuid(geteuid()) ) perror( "setuid" );
-	execle(COMMAND, COMMAND, (char *)0, (char *)0);
-	setuid(uid);
-	exit(1);
+	if (setgid(getegid())) {
+		perror("setgid");
+		return EXIT_FAILURE;
+	}
+	if (setuid(geteuid())) {
+		perror("setuid");
+		return EXIT_FAILURE;
+	}
+	execle(COMMAND, COMMAND, NULL, &suid_envp);
+	perror("execle");
+	return EXIT_FAILURE;
 }

commit eeefb922c73a0ab5035721817a01a7a6570547e3

Bug #50319 nagios: Fix SUID wrapper
UCS-4.3 does not (yet) contains a back-port for Bug #48617 and
Bug #48422 to also check the LMDB size of UDL, which was added with

> [4.4-2] 43767b4c2b Bug #48422: add slapd mdb nagios check in postinst
> [4.4-2] 92e9a55591 Bug #48422: fix postinst
> [4.4-2] 893e83c683 Bug #48422: fix postinst
> [4.4-2] 374fa67c43 Bug #48422: ucslint
> [4.4-2] 55ec68a52d Bug #48422: create objects in postinst
> [4.4-2] fe848f867b Bug #48617: enhance output / error handling
> [4.4-2] f8abb2c8e5 Bug #48422: Bug #48617: add mdb check for UDL and translog database

Fixes: 23187ded67d7ac71e6de589759c0bf00926e6a5a
Cherry-pick: f8abb2c8e5b63aa7d72bad0cfd0f012576aa422e

nagios/univention-nagios/usr/lib/nagios/plugins/check_univention_joinstatus_suidwrapper.c
@@ -3,6 +3,8 @@
 #include <pwd.h>
 #include <unistd.h>
 #include <errno.h>
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 #define COMMAND "/usr/lib/nagios/plugins/check_univention_joinstatus"
@@ -12,7 +14,7 @@ static char *const suid_envp[] = {
 	NULL
 };
 
-main( int argc, char ** argv, char ** envp )
+int main( int argc, char ** argv, char ** envp )
 {
 	int i = 0;
 	if (setgid(getegid())) {

nagios/univention-nagios/usr/lib/nagios/plugins/check_univention_nscd_suidwrapper.c
@@ -3,6 +3,8 @@
 #include <pwd.h>
 #include <unistd.h>
 #include <errno.h>
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 #define COMMAND "/usr/lib/nagios/plugins/check_univention_nscd"
@@ -12,7 +14,7 @@ static char *const suid_envp[] = {
 	NULL
 };
 
-main( int argc, char ** argv, char ** envp )
+int main( int argc, char ** argv, char ** envp )
 {
 	int i = 0;
 	if (setgid(getegid())) {

nagios/univention-nagios/usr/lib/nagios/plugins/check_univention_slapd_mdb_maxsize_suidwrapper.c
@@ -45,10 +45,14 @@ static char *const suid_envp[] = {
 
 int main(int argc, char ** argv, char ** envp) {
 	int i = 0;
+	int listener = 0;
 	char warning[] = "75";
 	char critical[] = "90";
-	while ((i = getopt(argc, argv, "c:w:")) != -1) {
+	while ((i = getopt(argc, argv, "lc:w:")) != -1) {
 		switch (i) {
+			case 'l':
+				listener = 1;
+				break;
 			case 'w':
 				strncpy(warning, optarg, 2);
 				break;