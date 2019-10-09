3fd394f78e81d1d3b4ce756e11b772aaddef2c6e
Florian Best <best@univention.de>
2019-10-09 10:19:56 +0200

    Bug #50342: modernize UDM modules

--- services/univention-s4-connector/modules/univention/admin/handlers/container/msgpo.py
+++ services/univention-s4-connector/modules/univention/admin/handlers/container/msgpo.py
@@ -31,12 +31,9 @@
 # <https://www.gnu.org/licenses/>.
 
 from univention.admin.layout import Tab, Group
-import univention.admin.uldap
 import univention.admin.syntax
-import univention.admin.filter
 import univention.admin.handlers
 import univention.admin.localization
-import string
 
 translation = univention.admin.localization.translation('univention.admin.handlers.container.msgpo')
 _ = translation.translate
@@ -47,127 +44,74 @@ childs = 1
 short_description = _('Container: MS Group Policy')
 long_description = ''
 options = {
+	'default': univention.admin.option(
+		short_description='',
+		default=True,
+		objectClasses=['msGPOContainer', 'top']
+	),
 }
 property_descriptions = {
 	'name': univention.admin.property(
 		short_description=_('Name'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
 		required=True,
-		may_change=True,
 		identifies=True
 	),
 	'description': univention.admin.property(
 		short_description=_('Description'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'displayName': univention.admin.property(
 		short_description=_('Display name'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'msGPOFlags': univention.admin.property(
 		short_description=_('MS Group Policy Flags'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'msGPOVersionNumber': univention.admin.property(
 		short_description=_('MS Group Policy Version Number'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'msGPOSystemFlags': univention.admin.property(
 		short_description=_('MS Group Policy System Flags'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'msGPOFunctionalityVersion': univention.admin.property(
 		short_description=_('MS Group Policy Functionality Version'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'msGPOFileSysPath': univention.admin.property(
 		short_description=_('MS Group Policy File Sys Path'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'msGPOUserExtensionNames': univention.admin.property(
 		short_description=_('MS Group Policy User Extension Names'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'msGPOMachineExtensionNames': univention.admin.property(
 		short_description=_('MS Group Policy Machine Extension Names'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'msGPOWQLFilter': univention.admin.property(
 		short_description=_('MS Group Policy WQL Filter'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'msNTSecurityDescriptor': univention.admin.property(
 		short_description=_('MS NT Security Descriptor'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 }
 
@@ -210,49 +154,10 @@ mapping.register('msNTSecurityDescriptor', 'msNTSecurityDescriptor', None, unive
 class object(univention.admin.handlers.simpleLdap):
 	module = module
 
-	def __init__(self, co, lo, position, dn='', superordinate=None, attributes=[]):
-		global mapping
-		global property_descriptions
-
-		self.mapping = mapping
-		self.descriptions = property_descriptions
-		self.default_dn = ''
-
-		univention.admin.handlers.simpleLdap.__init__(self, co, lo, position, dn, superordinate, attributes=attributes)
-
-		self.save()
-
-	def _ldap_pre_create(self):
-		self.dn = '%s=%s,%s' % (mapping.mapName('name'), mapping.mapValue('name', self.info['name']), self.position.getDn())
-
 	def _ldap_pre_modify(self):
 		if self.hasChanged('name'):
-			newdn = string.replace(self.dn, 'cn=%s,' % self.oldinfo['name'], 'cn=%s,' % self.info['name'], 1)
-			self.move(newdn)
-
-	def _ldap_addlist(self):
-		return [
-			('objectClass', ['top', 'msGPOContainer'])
-		]
+			self.move(self._ldap_dn())
 
 
-def lookup(co, lo, filter_s, base='', superordinate=None, scope='sub', unique=False, required=False, timeout=-1, sizelimit=0):
-
-	filter = univention.admin.filter.conjunction('&', [
-		univention.admin.filter.expression('objectClass', 'msGPOContainer'),
-	])
-
-	if filter_s:
-		filter_p = univention.admin.filter.parse(filter_s)
-		univention.admin.filter.walk(filter_p, univention.admin.mapping.mapRewrite, arg=mapping)
-		filter.expressions.append(filter_p)
-
-	res = []
-	for dn, attrs in lo.search(unicode(filter), base, scope, [], unique, required, timeout, sizelimit):
-		res.append(object(co, lo, None, dn, attributes=attrs))
-	return res
-
-
-def identify(dn, attr, canonical=0):
-
-	return 'msGPOContainer' in attr.get('objectClass', [])
+identify = object.identify
+lookup = object.lookup
--- services/univention-s4-connector/modules/univention/admin/handlers/settings/mswmifilter.py
+++ services/univention-s4-connector/modules/univention/admin/handlers/settings/mswmifilter.py
@@ -31,183 +31,108 @@
 # <https://www.gnu.org/licenses/>.
 
 from univention.admin.layout import Tab, Group
-import univention.admin.uldap
+
 import univention.admin.syntax
-import univention.admin.filter
 import univention.admin.handlers
 import univention.admin.localization
-import string
 
 translation = univention.admin.localization.translation('univention.admin.handlers.settings.mswmifilter')
 _ = translation.translate
 
 module = 'settings/mswmifilter'
 operations = ['add', 'edit', 'remove', 'search', 'move', 'subtree_move']
-childs = 1
+childs = True
 short_description = _('Settings: MS WMI Filter')
 long_description = ''
 options = {
+	'default': univention.admin.option(
+		short_description='',
+		default=True,
+		objectClasses=['msWMISom', 'top']
+	),
 }
 property_descriptions = {
 	'name': univention.admin.property(
 		short_description=_('Name'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
 		required=True,
-		may_change=True,
-		identifies=True
 	),
 	'description': univention.admin.property(
 		short_description=_('Description'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'displayName': univention.admin.property(
 		short_description=_('Display name'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'id': univention.admin.property(
 		short_description=_('MS WMI ID'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
 		identifies=True
 	),
 	'author': univention.admin.property(
 		short_description=_('MS WMI Author'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'creationDate': univention.admin.property(
 		short_description=_('MS WMI Creation Date'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'changeDate': univention.admin.property(
 		short_description=_('MS WMI Change Date'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'parm1': univention.admin.property(
 		short_description=_('MS WMI Parameter1'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'parm2': univention.admin.property(
 		short_description=_('MS WMI Parameter2'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'parm3': univention.admin.property(
 		short_description=_('MS WMI Parameter3'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'parm4': univention.admin.property(
 		short_description=_('MS WMI Parameter4'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'flags1': univention.admin.property(
 		short_description=_('MS WMI Flags1'),
 		long_description='',
 		syntax=univention.admin.syntax.integer,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'flags2': univention.admin.property(
 		short_description=_('MS WMI Flags2'),
 		long_description='',
 		syntax=univention.admin.syntax.integer,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'flags3': univention.admin.property(
 		short_description=_('MS WMI Flags3'),
 		long_description='',
 		syntax=univention.admin.syntax.integer,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'flags4': univention.admin.property(
 		short_description=_('MS WMI Flags4'),
 		long_description='',
 		syntax=univention.admin.syntax.integer,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 	'sourceOrganization': univention.admin.property(
 		short_description=_('MS WMI Source Organization'),
 		long_description='',
 		syntax=univention.admin.syntax.string,
-		multivalue=False,
-		options=[],
-		required=False,
-		may_change=True,
-		identifies=False
 	),
 }
 
@@ -228,11 +153,11 @@ layout = [
 			['parm2'],
 			['parm3'],
 			['parm4'],
-			#			[ 'flags1' ],
-			#			[ 'flags2' ],
-			#			[ 'flags3' ],
-			#			[ 'flags4' ],
-			#			[ 'sourceOrganization' ],
+			#['flags1'],
+			#['flags2'],
+			#['flags3'],
+			#['flags4'],
+			#['sourceOrganization'],
 		]),
 	])
 ]
@@ -259,49 +184,10 @@ mapping.register('sourceOrganization', 'msWMISourceOrganization', None, univenti
 class object(univention.admin.handlers.simpleLdap):
 	module = module
 
-	def __init__(self, co, lo, position, dn='', superordinate=None, attributes=[]):
-		global mapping
-		global property_descriptions
-
-		self.mapping = mapping
-		self.descriptions = property_descriptions
-		self.default_dn = ''
-
-		univention.admin.handlers.simpleLdap.__init__(self, co, lo, position, dn, superordinate, attributes=attributes)
-
-		self.save()
-
-	def _ldap_pre_create(self):
-		self.dn = 'cn=%s,%s' % (mapping.mapValue('id', self.info['id']), self.position.getDn())
-
 	def _ldap_pre_modify(self):
 		if self.hasChanged('id'):
-			newdn = string.replace(self.dn, 'cn=%s,' % self.oldinfo['id'], 'cn=%s,' % self.info['id'], 1)
-			self.move(newdn)
-
-	def _ldap_addlist(self):
-		return [
-			('objectClass', ['top', 'msWMISom'])
-		]
+			self.move(self._ldap_dn())
 
 
-def lookup(co, lo, filter_s, base='', superordinate=None, scope='sub', unique=False, required=False, timeout=-1, sizelimit=0):
-
-	filter = univention.admin.filter.conjunction('&', [
-		univention.admin.filter.expression('objectClass', 'msWMISom'),
-	])
-
-	if filter_s:
-		filter_p = univention.admin.filter.parse(filter_s)
-		univention.admin.filter.walk(filter_p, univention.admin.mapping.mapRewrite, arg=mapping)
-		filter.expressions.append(filter_p)
-
-	res = []
-	for dn, attrs in lo.search(unicode(filter), base, scope, [], unique, required, timeout, sizelimit):
-		res.append(object(co, lo, None, dn, attributes=attrs))
-	return res
-
-
-def identify(dn, attr, canonical=0):
-
-	return 'msWMISom' in attr.get('objectClass', [])
+identify = object.identify
+lookup = object.lookup
