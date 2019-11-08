35b7619f95605ef62f8ac83bd8aa8d8848d920bb
Florian Best <best@univention.de>
2019-11-08 17:46:04 +0100

    Bug #49060: outsource into file

---
+++ test/utils/python3-utils.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Copyright 2019 Univention GmbH
+#
+# https://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <https://www.gnu.org/licenses/>.
+
+set -x
+
+. utils.sh
+
+fakepackage() { equivs-control "$1" && sed "s/^Package: .*/Package: $1/g; s/^# Version:.*/Version: ${2:1.0}/g" -i "$1" && equivs-build "$1" && dpkg -i "$1"_*.deb; }
+
+fake_package_install () {
+	install_with_unmaintained equivs
+	fakepackage python3-ldb
+	fakepackage python3-samba
+	fakepackage python3-trml2pdf
+	fakepackage python3-univention-heimdal 4.2
+	fakepackage python3-m2crypto
+	fakepackage python3-smbpasswd
+	install_with_unmaintained python3-univention python3-univention-config-registry python3-univention-connector-s4 python3-univention-debug python3-univention-directory-manager python3-univention-directory-manager-cli python3-univention-directory-manager-rest python3-univention-directory-reports python3-univention-heimdal python3-univention-ipcalc python3-univention-lib python3-univention-license python3-univention-management-console python3-univention-updater
+}
