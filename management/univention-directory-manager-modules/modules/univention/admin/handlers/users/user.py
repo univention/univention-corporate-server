31e876732df156b067016832db157f3b5c10bcbf
Owen Synge <synge@univention.de>
2019-11-11 01:02:19 +0100

    Bug #50648 UDM users/user: concatenate pwlist
    Warning this looks like a bug but keeping old functionality.
    
    Traceback (most recent call last):
      File "/usr/share/univention-directory-manager-tools/univention-cli-server", line 236, in doit
        output = univention.admincli.admin.doit(arglist)
      File "/usr/lib/python3/dist-packages/univention/admincli/admin.py", line 410, in doit
        out = _doit(arglist)
      File "/usr/lib/python3/dist-packages/univention/admincli/admin.py", line 761, in _doit
        dn = object.create()
      File "/usr/lib/python3/dist-packages/univention/admin/handlers/__init__.py", line 558, in create
        dn = self._create(response=response, serverctrls=serverctrls)
      File "/usr/lib/python3/dist-packages/univention/admin/handlers/__init__.py", line 1244, in _create
        al.extend(self._ldap_modlist())
      File "/usr/lib/python3/dist-packages/univention/admin/handlers/users/user.py", line 1723, in _ldap_modlist
        ml = self._modlist_samba_password(ml, pwhistoryPolicy)
      File "/usr/lib/python3/dist-packages/univention/admin/handlers/users/user.py", line 1830, in _modlist_samba_password
        newsmbPWHistory = self.__getsmbPWHistory(password_nt, smbpwhistory, pwhistoryPolicy.pwhistoryLength)
      File "/usr/lib/python3/dist-packages/univention/admin/handlers/users/user.py", line 2249, in __getsmbPWHistory
        res = string.join(pwlist, '')
    AttributeError: module 'string' has no attribute 'join'

--- management/univention-directory-manager-modules/modules/univention/admin/handlers/users/user.py
+++ management/univention-directory-manager-modules/modules/univention/admin/handlers/users/user.py
@@ -2238,7 +2238,7 @@ class object(univention.admin.handlers.simpleLdap):
 					pwlist.append(smbpwhash)
 
 		# and build the new history
-		res = string.join(pwlist, '')
+		res = ''.join(pwlist)
 		return res
 
 	def __allocate_rid(self, rid):
