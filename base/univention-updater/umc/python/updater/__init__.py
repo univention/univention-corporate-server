univention/univention-corporate-server

commit 167e25f87583f4945f7459278263401f0cfdd4ae

Bug #49143: upgrade hook directory

base/univention-updater/debian/univention-management-console-module-updater.postinst
@@ -1,0 +1,48 @@
+#!/bin/sh
+#
+# Univention Updater
+#  postinst script
+#
+# Copyright 2019 Univention GmbH
+#
+# https://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <https://www.gnu.org/licenses/>.
+
+#DEBHELPER#
+
+if [ "$1" = "configure" -a -n "$2" ] && dpkg --compare-versions "$2" lt 14.0.2-6; then
+	if [ -e /usr/share/pyshared/univention/management/console/modules/updater/hooks/* ]; then
+		mkdir /usr/lib/python2.7/dist-packages/univention/management/console/modules/hooks/
+		for file in /usr/share/pyshared/univention/management/console/modules/updater/hooks/*; do
+			if dpkg -S "$file"; then
+				echo "WARNING: Need to migrate Updater hooks from /usr/share/pyshared/univention/management/console/modules/updater/hooks/ to /usr/lib/python2.7/dist-packages/univention/management/console/modules/hooks/!"
+				continue
+			fi
+			mv "$file" /usr/lib/python2.7/dist-packages/univention/management/console/modules/hooks/
+		done
+	fi
+fi
+
+exit 0

base/univention-updater/umc/python/updater/__init__.py
@@ -71,7 +71,7 @@ UPDATE_SERIAL_FILES = [
 	'/etc/apt/sources.list.d/20_ucs-online-component.list'
 ]
 
-HOOK_DIRECTORY = '/usr/share/pyshared/univention/management/console/modules/updater/hooks'
+HOOK_DIRECTORY = '/usr/lib/python2.7/dist-packages/univention/updater/hooks'
 
 INSTALLERS = {
 	'release': {
@@ -365,12 +365,12 @@ class Instance(Base):
 				else:
 					install.append([pkg, ver])
 			elif line.startswith('Remv '):
-				l = line.split(' ')
-				pkg = l[1]
+				ll = line.split(' ')
+				pkg = ll[1]
 				# i18n: The package version is unknown.
 				ver = _('unknown')
-				if len(l) > 2:
-					ver = l[2].replace('[', '').replace(']', '')
+				if len(ll) > 2:
+					ver = ll[2].replace('[', '').replace(']', '')
 				remove.append([pkg, ver])
 
 		return dict(

base/univention-updater/debian/univention-updater.postinst
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 #
 # Univention Updater
 #  postinst script

commit 9b67df10aa0a7be2e7ec1840a41cc79a74831ff1

Bug #49143 up: Support legacy hook directory
Support the legacy directory for now; code can be removed post UCS-5.

Fix traceback:
$ umc-command -U Administrator -P univention updater/hooks/call
...
>   ERROR    : {
> u'traceback': u'  File "/usr/lib/pymodules/python2.7/notifier/threads.py", line 78, in _run
>     tmp = self._function()
>   File "/usr/lib/pymodules/python2.7/notifier/__init__.py", line 104, in __call__
>     return self._function( *tmp, **self._kwargs )
>   File "/usr/lib/python2.7/dist-packages/univention/management/console/modules/updater/__init__.py", line 314, in _thread
>     for hookname in hooknames:
> TypeError: \'NoneType\' object is not iterable
> ', u'command': u'updater/hooks/call'}

$ umc-command -U Administrator -P univention updater/hooks/call -e -o '{"hooks":["updater_show_message","updater_prohibit_update"]}'

Fixes: 167e25f87583f4945f7459278263401f0cfdd4ae

base/univention-updater/debian/control
@@ -33,7 +33,7 @@ Depends: ${python:Depends}, ${python3:Depends},
  univention-errata-level,
  apt-transport-https,
  ca-certificates,
- python-univention-updater (>= 14.0.2-6),
+ python-univention-updater (= ${source:Version}),
 Recommends:
  screen,
 Suggests:

base/univention-updater/debian/rules
@@ -82,5 +82,11 @@ override_dh_auto_test:
 	PYTHONPATH=modules python2.7 tests/test_mirror.py -v
 	ucslint
 
+override_dh_python2:
+	dh_python2 --skip-private
+
+override_dh_python3:
+	dh_python3 --skip-private
+
 %:
 	dh $@ --with 'python2,python3'

base/univention-updater/debian/univention-management-console-module-updater.postinst
@@ -1,48 +1,0 @@
-#!/bin/sh
-#
-# Univention Updater
-#  postinst script
-#
-# Copyright 2019 Univention GmbH
-#
-# https://www.univention.de/
-#
-# All rights reserved.
-#
-# The source code of this program is made available
-# under the terms of the GNU Affero General Public License version 3
-# (GNU AGPL V3) as published by the Free Software Foundation.
-#
-# Binary versions of this program provided by Univention to you as
-# well as other copyrighted, protected or trademarked materials like
-# Logos, graphics, fonts, specific documentations and configurations,
-# cryptographic keys etc. are subject to a license agreement between
-# you and Univention and not subject to the GNU AGPL V3.
-#
-# In the case you use this program under the terms of the GNU AGPL V3,
-# the program is provided in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public
-# License with the Debian GNU/Linux or Univention distribution in file
-# /usr/share/common-licenses/AGPL-3; if not, see
-# <https://www.gnu.org/licenses/>.
-
-#DEBHELPER#
-
-if [ "$1" = "configure" -a -n "$2" ] && dpkg --compare-versions "$2" lt 14.0.2-6; then
-	if [ -e /usr/share/pyshared/univention/management/console/modules/updater/hooks/* ]; then
-		mkdir /usr/lib/python2.7/dist-packages/univention/management/console/modules/hooks/
-		for file in /usr/share/pyshared/univention/management/console/modules/updater/hooks/*; do
-			if dpkg -S "$file"; then
-				echo "WARNING: Need to migrate Updater hooks from /usr/share/pyshared/univention/management/console/modules/updater/hooks/ to /usr/lib/python2.7/dist-packages/univention/management/console/modules/hooks/!"
-				continue
-			fi
-			mv "$file" /usr/lib/python2.7/dist-packages/univention/management/console/modules/hooks/
-		done
-	fi
-fi
-
-exit 0

base/univention-updater/debian/univention-updater.dirs
@@ -1,0 +1,1 @@
+usr/share/univention-updater/hooks

base/univention-updater/umc/python/updater/__init__.py
@@ -51,7 +51,7 @@ from univention.management.console.log import MODULE
 from univention.management.console.config import ucr
 from univention.management.console.modules import Base, UMC_Error
 from univention.management.console.modules.decorators import simple_response, sanitize
-from univention.management.console.modules.sanitizers import ChoicesSanitizer, StringSanitizer, IntegerSanitizer
+from univention.management.console.modules.sanitizers import ChoicesSanitizer, StringSanitizer, IntegerSanitizer, ListSanitizer
 
 from univention.updater.tools import UniventionUpdater
 from univention.updater.errors import RequiredComponentError, UpdaterException
@@ -71,7 +71,8 @@ UPDATE_SERIAL_FILES = [
 	'/etc/apt/sources.list.d/20_ucs-online-component.list'
 ]
 
-HOOK_DIRECTORY = '/usr/lib/python2.7/dist-packages/univention/updater/hooks'
+HOOK_DIRECTORY_LEGACY = '/usr/share/pyshared/univention/management/console/modules/updater/hooks'
+HOOK_DIRECTORY = '/usr/share/univention-updater/hooks'
 
 INSTALLERS = {
 	'release': {
@@ -298,6 +299,9 @@ class Instance(Base):
 
 		return result
 
+	@sanitize(
+		hooks=ListSanitizer(StringSanitizer(minimum=1), required=True)
+	)
 	def call_hooks(self, request):
 		"""
 		Calls the specified hooks and returns data given back by each hook
@@ -305,15 +309,16 @@ class Instance(Base):
 
 		def _thread(request):
 			result = {}
+			hookmanager_legacy = univention.hooks.HookManager(HOOK_DIRECTORY_LEGACY)  # , raise_exceptions=False
 			hookmanager = univention.hooks.HookManager(HOOK_DIRECTORY)  # , raise_exceptions=False
 
 			hooknames = request.options.get('hooks')
 			MODULE.info('requested hooks: %s' % hooknames)
 			for hookname in hooknames:
 				MODULE.info('calling hook %s' % hookname)
-				result[hookname] = hookmanager.call_hook(hookname)
-			MODULE.info('result: %r' % (result,))
+				result[hookname] = hookmanager.call_hook(hookname) + hookmanager_legacy.call_hook(hookname)
 
+			MODULE.info('result: %r' % (result,))
 			return result
 
 		thread = notifier.threads.Simple('call_hooks', notifier.Callback(_thread, request), notifier.Callback(self.thread_finished_callback, request))