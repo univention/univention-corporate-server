univention/univention-corporate-server

commit 105808fdc7b8af3d682ce96dd08557c91aaf6241

Bug #49179: futurize univention-virtual-machine-manager-daemon

virtualization/univention-virtual-machine-manager-daemon/scripts-dev/check-vms.py
@@ -8,6 +8,8 @@ Check if VMs are still valid
 3. snapshot XML exists for volume snapshots
 4. unreferenced volumes
 """
+from __future__ import print_function
+
 import sys
 import os
 import re
@@ -48,11 +50,11 @@ class Dotter(object):
         if self.dot_out:
             text = fmt % (data or {})
             try:  # FIXME: Fix Unicode
-                print >> self.dot_out, ''.join(_ for _ in text if
-                        32 <= ord(_))
+                print(''.join(_ for _ in text if
+                        32 <= ord(_)), file=self.dot_out)
             except UnicodeEncodeError:
-                print >> self.dot_out, ''.join(_ for _ in text if
-                        32 <= ord(_) < 128)
+                print(''.join(_ for _ in text if
+                        32 <= ord(_) < 128), file=self.dot_out)
 
 
 class Resource(object):
@@ -736,9 +738,9 @@ def main():
             filtered.add(res)
             text = '// %s' % (res.console(),)
             try:  # FIXME: Fix Unicode
-                print ''.join(_ for _ in text if 32 <= ord(_))
+                print(''.join(_ for _ in text if 32 <= ord(_)))
             except UnicodeEncodeError:
-                print ''.join(_ for _ in text if 32 <= ord(_) < 128)
+                print(''.join(_ for _ in text if 32 <= ord(_) < 128))
 
     if options.dot:
         if not options.show_all:

virtualization/univention-virtual-machine-manager-daemon/scripts-dev/watch_vm
@@ -1,5 +1,7 @@
 #!/usr/bin/python2.4
 
+from __future__ import print_function
+
 import sys
 import time
 import libvirt
@@ -17,7 +19,7 @@ c = libvirt.open(sys.argv[1])
 
 
 def cb(*a):
-	print '|'.join(map(str, a))
+	print('|'.join(map(str, a)))
 
 
 c.domainEventRegister(cb, 42)
@@ -28,7 +30,7 @@ try:
 	while True:
 		s = d.info()[0]
 		if s != state:
-			print "%s\t%s" % (time.time(), STATE[s])
+			print("%s\t%s" % (time.time(), STATE[s]))
 			state = s
 		time.sleep(1)
 except KeyboardInterrupt:

virtualization/univention-virtual-machine-manager-daemon/scripts-dev/lv-pool-crash.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
+from __future__ import print_function
 import libvirt
 c = libvirt.open('qemu:///system')
-print c.listDefinedStoragePools()
-print c.listStoragePools()
+print(c.listDefinedStoragePools())
+print(c.listStoragePools())
 p = c.storagePoolLookupByName('default')
 p.isActive()
 p.isPersistent()

virtualization/univention-virtual-machine-manager-daemon/scripts-dev/uvmm-memleak
@@ -2,6 +2,7 @@
 """
 Test python-maelie
 """
+from __future__ import print_function
 # <http://jam-bazaar.blogspot.de/2009/11/memory-debugging-with-meliae.html>
 # <file:../BUG.DONE/35977_testlibcloud.py>
 
@@ -90,7 +91,7 @@ def analyze():
 		diff(prev_sum, sm.type_summaries)
 		prev_sum = sm.type_summaries
 
-		print
+		print()
 
 
 def compare(old_filename, new_filename):
@@ -100,7 +101,7 @@ def compare(old_filename, new_filename):
 
 
 def load(filename):
-	print filename
+	print(filename)
 	om = loader.load(filename, show_prog=False, collapse=True)
 	om.remove_expensive_references()
 	om.compute_parents()
@@ -125,11 +126,11 @@ def diff(old, new):
 		))
 		if any(data):
 			sign = '+' if not old else '-' if not new else '!'
-			print '{sign} {typ:_<30} {data[0]:_>+12n} {data[1]:_>+12n} {data[2]:_>+12n}'.format(**locals())
+			print('{sign} {typ:_<30} {data[0]:_>+12n} {data[1]:_>+12n} {data[2]:_>+12n}'.format(**locals()))
 
 
 def usage(ret):
-	print >> stderr, 'Usage: %s <d|f|a|c> ...' % (argv[0],)
+	print('Usage: %s <d|f|a|c> ...' % (argv[0],), file=stderr)
 	exit(ret)
 
 

virtualization/univention-virtual-machine-manager-daemon/src/univention-virtual-machine-manager
@@ -3,6 +3,7 @@
 """
 UCS Virtual Machine Manager Daemon command line interface
 """
+from __future__ import print_function
 # Copyright 2010-2019 Univention GmbH
 #
 # https://www.univention.de/
@@ -50,7 +51,7 @@ def create_socket(options):
 	UNIX socket only."""
 	sock = client.UVMM_ClientUnixSocket(options.socket, timeout=options.timeout)
 	if options.verbose:
-		print >> sys.stderr, "sock=%s" % (sock,)
+		print("sock=%s" % (sock,), file=sys.stderr)
 	return sock
 
 
@@ -59,33 +60,33 @@ def request(options, req):
 	sock = create_socket(options)
 	try:
 		if options.verbose:
-			print >> sys.stderr, "request=%s" % (req,)
+			print("request=%s" % (req,), file=sys.stderr)
 		res = sock.send(req)
 		if isinstance(res, protocol.Response_DUMP):
-			print "DATA:"
+			print("DATA:")
 			try:
 				pprint(dump(res.data))
 			except AttributeError:
-				print str(res)
+				print(str(res))
 			except Exception as ex:
 				import traceback
 				traceback.print_exc()
-				print 'Exception %s\n%s' % (ex, res)
+				print('Exception %s\n%s' % (ex, res))
 		elif isinstance(res, protocol.Response_ERROR):
 			raise TranslatableException(res.translatable_text, res.values)
 		else:
-			print "OK."
+			print("OK.")
 	finally:
 		sock.close()
 
 
 def dump(obj):
 	if isinstance(obj, list):
-		return map(dump, obj)
+		return list(map(dump, obj))
 	elif isinstance(obj, dict):
-		return dict(map(lambda k_v: (k_v[0], dump(k_v[1])), obj.items()))
+		return dict([(k_v[0], dump(k_v[1])) for k_v in list(obj.items())])
 	elif hasattr(obj, '__dict__'):
-		return dict(map(lambda k_v1: (k_v1[0], dump(k_v1[1])), obj.__dict__.items()))
+		return dict([(k_v1[0], dump(k_v1[1])) for k_v1 in list(obj.__dict__.items())])
 	else:
 		return obj
 
@@ -97,12 +98,12 @@ def from_ldap(options):
 		uris = ldap_uris()
 	except LdapError as ex:
 		(translatable_text, fillin) = ex
-		print >> sys.stderr, "ERROR:"
-		print >> sys.stderr, _(translatable_text) % fillin
+		print("ERROR:", file=sys.stderr)
+		print(_(translatable_text) % fillin, file=sys.stderr)
 		return False
 
 	if len(uris) == 0:
-		print >> sys.stderr, _("No nodes found.")
+		print(_("No nodes found."), file=sys.stderr)
 		return False
 
 	result = True
@@ -115,7 +116,7 @@ def from_ldap(options):
 				raise client.ClientError(res.translatable_text, res.values)
 		except client.ClientError as ex:
 			(translatable_text, fillin) = ex
-			print >> sys.stderr, _(translatable_text) % fillin
+			print(_(translatable_text) % fillin, file=sys.stderr)
 			result = False
 	sock.close()
 
@@ -174,7 +175,7 @@ class ParserGenerator(object):
 			del lines[0]
 		while lines and not lines[-1].strip():
 			del lines[-1]
-		indent = min([len(l[0:-len(l.lstrip())]) for l in filter(None, lines)])
+		indent = min([len(l[0:-len(l.lstrip())]) for l in [_f for _f in lines if _f]])
 		desc = [l[indent:] for l in lines]
 		return '\n'.join(desc)
 
@@ -1025,7 +1026,7 @@ def main():
 		func = args.func
 	except AttributeError:
 		if args.verbose:
-			print >> sys.stderr, "I: No function for mode '{0.mode}'".format(args)
+			print("I: No function for mode '{0.mode}'".format(args), file=sys.stderr)
 		func = do_request
 
 	ret = func(args)
@@ -1037,7 +1038,7 @@ def do_request(args):
 		pre = args.pre
 	except AttributeError:
 		if args.verbose:
-			print >> sys.stderr, "I: No pre-function for mode '{0.mode}'".format(args)
+			print("I: No pre-function for mode '{0.mode}'".format(args), file=sys.stderr)
 	else:
 		pre(args)
 
@@ -1051,7 +1052,7 @@ def do_request(args):
 		sys.exit(ret)
 	except TranslatableException as ex:
 		(translatable_text, fillin) = ex
-		print >> sys.stderr, _(translatable_text) % fillin
+		print(_(translatable_text) % fillin, file=sys.stderr)
 		sys.exit(1)
 
 

virtualization/univention-virtual-machine-manager-daemon/src/univention-virtual-machine-manager-daemon
@@ -31,6 +31,7 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 """UVMM Daemon."""
+from __future__ import print_function
 
 import sys
 import locale
@@ -115,7 +116,7 @@ def createDaemon(options):
 
 				sys.exit(0)
 			else:
-				print >>sys.stderr, _("Failed to fork daemon.")
+				print(_("Failed to fork daemon."), file=sys.stderr)
 				sys.exit(1)
 		except EnvironmentError as ex:
 			raise TranslatableException(N_("%(strerror)s [%(errno)d]"), stderror=ex.strerror, errno=ex.errno)
@@ -138,7 +139,7 @@ def install_signal_handlers(options):
 		logger = logging.getLogger("uvmmd")
 		logger.info('Closing logfile.')
 		# Inspired by <http://www.cherrypy.org/ticket/679>
-		for x in logger.manager.loggerDict.itervalues():
+		for x in logger.manager.loggerDict.values():
 			try:
 				for h in x.handlers:
 					if isinstance(h, logging.FileHandler):

virtualization/univention-virtual-machine-manager-daemon/src/univention/uvmm/client.py
@@ -32,6 +32,8 @@
 """UVMM client using a unix-socket."""
 
 from __future__ import absolute_import
+from __future__ import print_function
+
 import socket
 from . import protocol
 from .helpers import TranslatableException, FQDN, N_ as _
@@ -149,7 +151,7 @@ def __debug(msg):
 	try:
 		if int(__ucr['dvs/uvmm/debug']) > 0:
 			import sys
-			print >>sys.stderr, msg
+			print(msg, file=sys.stderr)
 	except:
 		pass
 

virtualization/univention-virtual-machine-manager-daemon/src/univention/uvmm/cloudconnection.py
@@ -39,7 +39,7 @@ import threading
 import time
 import re
 import fnmatch
-import cPickle as pickle
+import pickle
 
 from .protocol import Cloud_Data_Connection, Cloud_Data_Image, Cloud_Data_Keypair
 from .helpers import TranslatableException, ms, uri_encode

commit e54ad5e26898605cc5358981e2b2c557859ec31f

Bug #49179 UVMM: Fix development scripts
Make scripts-dev/ usable again.

Fixes: 105808fdc7b8af3d682ce96dd08557c91aaf6241

virtualization/univention-virtual-machine-manager-daemon/scripts-dev/check-vms.py
@@ -24,10 +24,9 @@ from xml.parsers.expat import ExpatError
 try:
     import curses
     curses.setupterm()
-    NORMAL = curses.tigetstr('sgr0')
+    NORMAL = curses.tigetstr('sgr0').decode('ascii')
     SET_AF = curses.tigetstr('setaf')
-    BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = \
-        [curses.tparm(SET_AF, i) for i in range(8)]
+    BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = [curses.tparm(SET_AF, i).decode('ascii') for i in range(8)]
 except (ImportError, curses.error):
     NORMAL = BLACK = RED = GREEN = YELLOW = BLUE = MAGENTA = CYAN = WHITE = ''
 
@@ -49,10 +48,7 @@ class Dotter(object):
         """Print dot graph."""
         if self.dot_out:
             text = fmt % (data or {})
-            try:  # FIXME: Fix Unicode
-                print(''.join(_ for _ in text if 32 <= ord(_)), file=self.dot_out)
-            except UnicodeEncodeError:
-                print(''.join(_ for _ in text if 32 <= ord(_) < 128), file=self.dot_out)
+            print(text, file=self.dot_out)
 
 
 class Resource(object):
@@ -541,10 +537,8 @@ virsh # vol-list temp
 def check_storage_pools(conn):
     """Check active and defined storage pools."""
     logger = logging.getLogger('root')
-    pool_names = conn.listStoragePools()
-    for pool_name in pool_names:
-        logger.debug('POOL %s', pool_name)
-        pool = conn.storagePoolLookupByName(pool_name)
+    for pool in conn.listAllStoragePools():
+        logger.debug('POOL %s', pool)
         try:
             pool.refresh(0)
         except libvirt.libvirtError as ex:
@@ -562,31 +556,16 @@ def check_storage_pools(conn):
         target_path = target.findtext('path')
         StorageVolume.pools.add(target_path)
 
-        volume_names = pool.listVolumes()
-        for volume_name in volume_names:
-            volume = pool.storageVolLookupByName(volume_name)
+        for volume in pool.listAllVolumes():
             StorageVolume.libvirt(volume)
     # TODO: conn.listDefinedStoragePools() are inactive
 
 
-def check_virtual_machines_running(conn):
+def check_virtual_machines(conn):
     """Check running domains."""
     logger = logging.getLogger('root')
-    dom_ids = conn.listDomainsID()
-    for dom_id in dom_ids:
-        logger.debug('running domain %s', dom_id)
-        dom = conn.lookupByID(dom_id)
-        virtual_machine = VirtualMachine.libvirt(dom)
-        virtual_machine.mark_used()
-
-
-def check_virtual_machines_defined(conn):
-    """Check defined domain."""
-    logger = logging.getLogger('root')
-    dom_names = conn.listDefinedDomains()
-    for dom_name in dom_names:
-        logger.debug('persistent domain %s', dom_name)
-        dom = conn.lookupByName(dom_name)
+    for dom in conn.listAllDomains():
+        logger.debug('domain %s', dom)
         virtual_machine = VirtualMachine.libvirt(dom)
         virtual_machine.mark_used()
 
@@ -704,14 +683,13 @@ def main():
     try:
         # volumes first because this is more detailed
         check_storage_pools(conn)
-        check_virtual_machines_running(conn)
-        check_virtual_machines_defined(conn)
+        check_virtual_machines(conn)
         check_storage_volumes(conn)
     finally:
         conn.close()
 
     # Validate all resources
-    for res in Resource.all.values():
+    for res in list(Resource.all.values()):
         res.check_valid()
 
     # Print all resources
@@ -722,10 +700,7 @@ def main():
                 not res.valid:
             filtered.add(res)
             text = '// %s' % (res.console(),)
-            try:  # FIXME: Fix Unicode
-                print(''.join(_ for _ in text if 32 <= ord(_)))
-            except UnicodeEncodeError:
-                print(''.join(_ for _ in text if 32 <= ord(_) < 128))
+            print(text)
 
     if options.dot:
         if not options.show_all:

virtualization/univention-virtual-machine-manager-daemon/scripts-dev/watch_vm
@@ -1,15 +1,23 @@
-#!/usr/bin/python2.4
+#!/usr/bin/python2.7
 
 from __future__ import print_function
 
 import sys
 import time
 import libvirt
+from threading import Thread
 
-from univention.uvmm.eventloop import virEventLoopPureStart, virEventLoopPureRegister
 
-virEventLoopPureStart()
-virEventLoopPureRegister()
+def runEventLoop():
+	while True:
+		libvirt.virEventRunDefaultImpl()
+
+
+libvirt.virEventRegisterDefaultImpl()
+loop = Thread(target=runEventLoop, name="libvirtEventLoop")
+loop.setDaemon(True)
+loop.start()
+
 
 STATE = ['no state', 'running', 'idle', 'paused', 'shut down', 'shut off', 'crashed']
 