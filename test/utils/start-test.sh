eeb7bf3143ec09e86b33f96125a5e13f52aa0160
Felix Botner <botner@univention.de>
2019-12-02 12:07:34 +0100

    Bug #50563: switch to ami-0152c75b08efb2c99 ami

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -16,7 +16,7 @@ release='4.4-3'
 old_release='4.3-5'
 kvm_template_version='4.4-2'
 
-export CURRENT_AMI=ami-076be15cd58709a0c # AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 4 - ami-076be15cd58709a0c
+export CURRENT_AMI=ami-0152c75b08efb2c99 # AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 5 - ami-0152c75b08efb2c99
 export OLD_AMI=ami-0dd235a67a0eb9bdc # AMI: Univention Corporate Server (UCS) 4.3 (official image) rev. 6 - ami-0dd235a67a0eb9bdc
 export UCS_MINORRELEASE="${release%%-*}"
 export TARGET_VERSION="${TARGET_VERSION:=$release}"
