univention/univention-corporate-server

commit d4459d8bd7323c7f6212e7df4d3059afba91baec

Bug #50554: Remove UCS 4.4-2 docker check from preup.sh

base/univention-updater/script/preup.sh
@@ -306,35 +306,6 @@ fail_if_role_package_will_be_removed () {
 	fi
 }
 
-block_update_with_docker() {
-	storage_driver=$(docker info 2> /dev/null | awk -F ': ' '/Storage Driver/ {print $2}')
-	# if docker is not running fall back to ucr variable
-	if [ -z "$storage_driver" ]; then
-		storage_driver=$(/usr/sbin/univention-config-registry get docker/daemon/default/opts/storage-driver)
-	fi
-	if [ "$storage_driver" != "overlay" ]; then
-		echo "ERROR: The current docker storage driver is \"$storage_driver\"."
-		echo "       An update to UCS 4.2-2 is only possible if docker uses"
-		echo "       \"overlay\" as storage driver. To perform the update"
-		echo "       the docker storage driver has to be changed to \"overlay\"."
-		echo ""
-		echo "       Please visit https://help.univention.com/t/upgrade-to-ucs-4-4-2-only-possible-with-overlay-as-dockers-storage-driver/13368"
-		echo "       for more information."
-		echo ""
-		if is_ucr_true update44/ignore_docker_issue; then
-			echo "WARNING: update44/ignore_docker_issue is set to true. Skipped as requested."
-		else
-			exit 1
-		fi
-	fi
-
-	[ -d /var/lib/docker/aufs ] && find /var/lib/docker/aufs -type d -empty -delete
-	[ -d /var/lib/docker/aufs ] && mv /var/lib/docker/aufs /var/lib/docker/aufs.moved-by-ucs4.4-2-upgrade
-	[ -d /var/lib/docker/devicemapper ] && find /var/lib/docker/devicemapper -type d -empty -delete
-	[ -d /var/lib/docker/devicemapper ] && mv /var/lib/docker/devicemapper /var/lib/docker/devicemapper.moved-by-ucs4.4-2-upgrade
-}
-block_update_with_docker
-
 
 # begin bug 46562
 block_update_if_system_date_is_too_old() {

commit edcd43d4c16493c49e4a2fb3709ab06aeaddf98e

Bug #50586 up: Remove docker check from p*up.sh scripts
Fixes: d4459d8bd7323c7f6212e7df4d3059afba91baec

base/univention-updater/script/postup.sh
@@ -146,21 +146,6 @@ fi
 # Bug #44188: recreate and reload packetfilter rules to make sure the system is accessible
 service univention-firewall restart >>"$UPDATER_LOG" 2>&1
 
-# Bug #50166: Docker update only worked smoothly with stopped containers...
-if [ -f /var/univention-join/joined ]; then
-	echo "Starting all Docker Apps again..."
-	python -c "
-from univention.appcenter.log import log_to_stream
-from univention.appcenter.app_cache import Apps
-from univention.appcenter.actions import get_action
-log_to_stream()
-apps = [app for app in Apps().get_all_locally_installed_apps() if app.docker]  # single container apps and multi container apps alike
-start = get_action('start')
-for app in apps:
-	start.call(app=app)"
-	echo "... Docker Apps started."
-fi
-
 echo "
 
 

base/univention-updater/script/preup.sh
@@ -479,27 +479,6 @@ apt-get -s -o Debug::pkgProblemResolver=yes dist-upgrade >&3 2>&3
 
 fail_if_role_package_will_be_removed
 
-# Bug #50200: New docker version needs other dir permissions for App "prometheus"
-[ -e /var/lib/univention-appcenter/apps/prometheus/data/ ] && chgrp 0 /var/lib/univention-appcenter/apps/prometheus/data/
-[ -e /var/lib/univention-appcenter/apps/prometheus/conf/ ] && chgrp 0 /var/lib/univention-appcenter/apps/prometheus/conf/
-
-# Bug #50166: Docker update only works smoothly with stopped containers...
-if [ -f /var/univention-join/joined ]; then
-	echo "Stopping all Docker Apps and Docker itself in preparation for the Docker engine update..."
-	python -c "
-from univention.appcenter.log import log_to_stream
-from univention.appcenter.app_cache import Apps
-from univention.appcenter.actions import get_action
-log_to_stream()
-apps = [app for app in Apps().get_all_locally_installed_apps() if app.docker]  # single container apps and multi container apps alike
-stop = get_action('stop')
-for app in apps:
-	stop.call(app=app)"
-	service docker stop
-	echo "... Docker stopped. Apps will be started after the successful update"
-	echo "If stopping / starting of the Apps fails, one may see issues with the Apps. In this case, a reboot should help"
-fi
-
 echo ""
 echo "Starting update process, this may take a while."
 echo "Check /var/log/univention/updater.log for more information."