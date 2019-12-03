ef96e168ee87b7f6ebb299e82e4b4b0474cd2e02
Felix Botner <botner@univention.de>
2019-12-03 15:01:21 +0100

    Bug #50563: switch 4.4-3 kvm template

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -14,7 +14,7 @@ test -f "$1" || die "Missing test config file!"
 
 release='4.4-3'
 old_release='4.3-5'
-kvm_template_version='4.4-2'
+kvm_template_version='4.4-3'
 
 export CURRENT_AMI=ami-0152c75b08efb2c99 # AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 5 - ami-0152c75b08efb2c99
 export OLD_AMI=ami-0dd235a67a0eb9bdc # AMI: Univention Corporate Server (UCS) 4.3 (official image) rev. 6 - ami-0dd235a67a0eb9bdc
