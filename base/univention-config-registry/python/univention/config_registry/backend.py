a51e25bcb4d89c7b8d0eadcff55f9f3201a1aae4
Owen Synge <synge@univention.de>
2019-12-06 16:35:50 +0100

    import of MutableMapping conditional on python version

--- base/univention-config-registry/python/univention/config_registry/backend.py
+++ base/univention-config-registry/python/univention/config_registry/backend.py
@@ -37,7 +37,10 @@ import fcntl
 import re
 import errno
 import time
-from collections import MutableMapping
+if sys.version_info >= (3,):
+	from collections.abc import MutableMapping
+else:
+	from collections import MutableMapping
 import six
 try:
 	from typing import overload, Any, Dict, IO, Iterator, List, NoReturn, Optional, Set, Tuple, Type, TypeVar, Union  # noqa F401
