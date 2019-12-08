5dd939120681f3cc845016ff355231589175b65a
Philipp Hahn <hahn@univention.de>
2019-12-08 14:30:35 +0100

    Bug #28497: Unify univention/__init__.py
    Consistently use pkgutil.extend_path() to make modules usable on
    development machine.

--- base/univention-updater/modules/univention/__init__.py
+++ base/univention-updater/modules/univention/__init__.py
@@ -1,7 +1,1 @@
-from pkgutil import extend_path
-try:
-	from typing import Iterable  # noqa
-except ImportError:
-	pass
-__path__ = extend_path(__path__, __name__)  # type: Iterable[str]
-del extend_path
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
