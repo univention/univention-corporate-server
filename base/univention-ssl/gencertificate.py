univention/univention-corporate-server

commit 06f7d45481dfa85e1c29b333b7af6776d7a077a3

Bug #45115: based on dwiesent/bug45115, add support for wildcard certificates

base/univention-ssl/gencertificate.py
@@ -63,6 +63,12 @@ def domain(info):
 	except LookupError:
 		return configRegistry['domainname']
 
+def wildcard_certificate(info):
+	if 'univentionService' in info:
+		if 'wildcard-certificate' in info['univentionService']:
+			return True
+	return False
+
 
 def handler(dn, new, old, command=''):
 	"""Handle changes to 'dn'."""
@@ -82,12 +88,15 @@ def handler(dn, new, old, command=''):
 		if new and not old:
 			# changeType: add
 			create_certificate(new['cn'][0], domain(new))
+			if wildcard_certificate(new):
+				create_certificate('*.%s' % new['cn'][0], domain(new))
 		elif old and not new:
 			# changeType: delete
 			if 'r' == command:
 				_delay = (dn, old)
 			else:
 				remove_certificate(old['cn'][0], domainname=domain(old))
+				remove_certificate('*.%s' % old['cn'][0], domainname=domain(old))
 		elif old and new:
 			# changeType: modify
 			old_domain = domain(old)
@@ -96,11 +105,24 @@ def handler(dn, new, old, command=''):
 			if new_domain != old_domain:
 				remove_certificate(old['cn'][0], old_domain)
 				create_certificate(new['cn'][0], new_domain)
+				remove_certificate('*.%s' % old['cn'][0], old_domain)
+				if wildcard_certificate(new):
+					create_certificate('*.%s' % new['cn'][0], new_domain)
+			else:
+				if wildcard_certificate(new) and not wildcard_certificate(old):
+					create_certificate('*.%s' % new['cn'][0], domain(new))
+				if not wildcard_certificate(new) and wildcard_certificate(old):
+					remove_certificate('*.%s' % old['cn'][0], domainname=domain(old))
+
 		if new:
 			# Reset permissions
 			fqdn = "%s.%s" % (new['cn'][0], domain(new))
 			certpath = os.path.join(SSLDIR, fqdn)
 			os.path.walk(certpath, set_permissions, (dn, new))
+			if wildcard_certificate(new):
+				fqdn = "*.%s.%s" % (new['cn'][0], domain(new))
+				certpath = os.path.join(SSLDIR, fqdn)
+				os.path.walk(certpath, set_permissions, (dn, new))
 	finally:
 		unsetuid()
 

base/univention-ssl/debian/control
@@ -17,6 +17,7 @@ Depends: ${misc:Depends},
  openssl,
  univention-directory-listener,
  openssl-blacklist,
+ univention-ssh,
  shell-univention-lib (>= 3.0.1-1),
  python-univention-lib (>= 3.0.1-1),
  python-m2crypto

base/univention-ssl/make-certificates.sh
@@ -373,7 +373,7 @@ is_valid () {
 
 has_cert () {
 	local cn="${1:?Missing argument: common name}"
-
+	cn="${cn//'*'/'\*'}"
 	awk -F '\t' -v cn="$cn" '
 	BEGIN { ret=1; seq=""; }
 	$6 ~ cn {
@@ -393,7 +393,7 @@ has_cert () {
 
 has_valid_cert () {
 	local cn="${1:?Missing argument: common name}"
-
+	cn="${cn//'*'/'\*'}"
 	tac "${SSLBASE}/${CA}/index.txt" | awk -F '\t' -v cn="$cn" -v now="$(TZ=UTC date +%y%m%d%H%M%S)" '
 	BEGIN { ret=1; seq=""; }
 	$6 ~ cn {
@@ -509,8 +509,13 @@ gencert () {
 		# Add DNS alias names
 		local san
 		san="$(univention-ldapsearch -LLLo ldif-wrap=no "(cNAMERecord=${fqdn%.}.)" 1.1 | sed -rne 's/^dn: relativeDomainName=([^,]+),zoneName=([^,]+),.*/\1 \1.\2/p' | tr '\n' ' ')"
+		# generate config, ignore san for wildcard certs
+		if [ "$hostname" = '*' ]; then
+			mk_config "$name/openssl.cnf" "" "$days" "$cn" ""
+		else
+			mk_config "$name/openssl.cnf" "" "$days" "$cn" "$fqdn $hostname $san"
+		fi
 		# generate a key pair
-		mk_config "$name/openssl.cnf" "" "$days" "$cn" "$fqdn $hostname $san"
 		openssl genrsa -out "$name/private.key" "$DEFAULT_BITS"
 		openssl req -batch -config "$name/openssl.cnf" -new -key "$name/private.key" -out "$name/req.pem"
 	fi

base/univention-ssl/tests/test_host_wildcard_valid
@@ -1,0 +1,21 @@
+#!/bin/bash
+set -e -u
+. ${0%/*}/common.sh
+
+name="*.${R2}.${R2}"
+init
+gencert "${name}" "${name}"
+list_cert_names | grep -F -e "${name}"
+[ 01 = "$(has_valid_cert "${name}")" ]
+
+has_cert "$name"
+
+renew_cert "${name}"
+list_cert_names | grep -F -e "${name}"
+[ 02 = "$(has_valid_cert "${name}")" ]
+
+revoke_cert "${name}"
+has_valid_cert "${name}" && exit 1
+list_cert_names | grep -F -e "${name}" && exit 1
+
+:

commit 2f101a037be63271f7bd376b019f979e6e2e6da7

Bug #45115: Add wildcard documentation
Fixes: 06f7d45481d

base/univention-ssl/gencertificate.py
@@ -76,7 +76,15 @@ def domain(info):
 	except LookupError:
 		return configRegistry['domainname']
 
+
 def wildcard_certificate(info):
+	# type: (Dict[str, List[str]]) -> bool
+	"""
+	Check if a wildcard certificate should be created for the host.
+
+	:param info: LDAP attribute values.
+	:returns: `True` for a wildcard name, `False` otherwise.
+	"""
 	if 'univentionService' in info:
 		if 'Wildcard Certificate' in info['univentionService']:
 			return True