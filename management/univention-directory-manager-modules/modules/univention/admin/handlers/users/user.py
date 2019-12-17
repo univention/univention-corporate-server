8629c4d1d51879f7d1122afc5d6bb5043d648bb2
Florian Best <best@univention.de>
2019-12-17 15:29:59 +0100

    Bug #50648: users/user:_ldap_attributes() use unicode strings

--- management/univention-directory-manager-modules/modules/univention/admin/handlers/users/user.py
+++ management/univention-directory-manager-modules/modules/univention/admin/handlers/users/user.py
@@ -2289,7 +2289,7 @@ class object(univention.admin.handlers.simpleLdap):
 
 	@classmethod
 	def _ldap_attributes(cls):
-		return ['*', 'pwdAccountLockedTime']
+		return [u'*', u'pwdAccountLockedTime']
 
 	@classmethod
 	def rewrite_filter(cls, filter, mapping):
