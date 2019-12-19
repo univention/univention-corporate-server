0e1401d2fec693deb2879e5987cc3813075ce605
Ole Schwiegert <schwiegert@univention.de>
2019-12-19 13:23:47 +0100

    Implement get and search

--- management/univention-config-registry-api/univention/config_registry_api/main.py
+++ management/univention-config-registry-api/univention/config_registry_api/main.py
@@ -1,16 +1,16 @@
 from typing import List
 
-from fastapi import FastAPI, Depends, Query
+from fastapi import FastAPI, Depends, Query, HTTPException
 from pydantic import BaseModel
 from starlette.status import HTTP_201_CREATED
-from univention.config_registry import ConfigRegistry
+from univention.config_registry import ConfigRegistry, handler_set, handler_search
 
 app = FastAPI()
 
 
 class UCRVar(BaseModel):
     identifier: str
-    value: str
+    value: str = None
 
 
 def get_ucr():
@@ -21,19 +21,28 @@ def get_ucr():
 
 @app.get("/", response_model=List[UCRVar])
 async def search(
-    pattern_filter: str = Query(
+    pattern: str = Query(
         None,
         title="Pattern to match searched UCR variables to. List all if empty",
         min_length=2,
-    ),
-    ucr: ConfigRegistry = Depends(get_ucr),
+    )
 ):
-    pass
+    try:
+        ucr_vars = handler_search(pattern)
+    except SystemExit as exc:
+        raise HTTPException(status_code=400, detail="Invalid pattern provided")
+    result = list()
+    for ucr_var in ucr_vars:
+        identifier, value = ucr_var.split(":", 1)
+        value = value[1:] if value != " <empty>" else None
+        result.append(UCRVar(identifier=identifier, value=value))
+    return result
 
 
-@app.get("/{var_name}", response_model=List[UCRVar])
-async def get(var_name: str, ucr: ConfigRegistry = Depends(get_ucr)) -> List[UCRVar]:
-    pass
+@app.get("/{var_name:path}", response_model=UCRVar)
+async def get(var_name: str, ucr: ConfigRegistry = Depends(get_ucr)) -> UCRVar:
+    ucr_value = ucr.get(var_name)
+    return UCRVar(identifier=var_name, value=ucr_value)
 
 
 @app.post("/", status_code=HTTP_201_CREATED, response_model=List[UCRVar])
