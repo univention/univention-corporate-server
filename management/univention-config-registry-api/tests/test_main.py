43b54b55155d9f9f06ed814fb2d2719ab0984454
Daniel Troeder <troeder@univention.de>
2019-12-19 13:48:51 +0100

    add documentation

--- management/univention-config-registry-api/tests/test_main.py
+++ management/univention-config-registry-api/tests/test_main.py
@@ -1,2 +1,2 @@
 def test_true():
-	assert True
+    assert True
--- management/univention-config-registry-api/univention/config_registry_api/main.py
+++ management/univention-config-registry-api/univention/config_registry_api/main.py
@@ -1,9 +1,10 @@
 from typing import List
 
-from fastapi import FastAPI, Depends, Query, HTTPException
+from fastapi import Body, Depends, FastAPI, HTTPException, Query
 from pydantic import BaseModel
 from starlette.status import HTTP_201_CREATED
-from univention.config_registry import ConfigRegistry, handler_set, handler_search
+
+from univention.config_registry import ConfigRegistry, handler_search, handler_set
 
 app = FastAPI()
 
@@ -13,7 +14,7 @@ class UCRVar(BaseModel):
     value: str = None
 
 
-def get_ucr():
+def get_ucr() -> ConfigRegistry:
     ucr = ConfigRegistry()
     ucr.load()
     return ucr
@@ -23,7 +24,10 @@ def get_ucr():
 async def search(
     pattern: str = Query(
         None,
-        title="Pattern to match searched UCR variables to. List all if empty",
+        description="Pattern to search UCR variables with. If empty, all UCR "
+        "variables will be returned. '.*' will be prepended / "
+        "appended, if the pattern does not start / end with '^' / "
+        "'$'.",
         min_length=2,
     )
 ):
@@ -41,6 +45,9 @@ async def search(
 
 @app.get("/{var_name:path}", response_model=UCRVar)
 async def get(var_name: str, ucr: ConfigRegistry = Depends(get_ucr)) -> UCRVar:
+    """
+    Retrieve the value of a UCR variable.
+    """
     ucr_value = ucr.get(var_name)
     return UCRVar(key=var_name, value=ucr_value)
 
@@ -49,14 +56,28 @@ async def get(var_name: str, ucr: ConfigRegistry = Depends(get_ucr)) -> UCRVar:
 async def post(
     ucr_vars: List[UCRVar], ucr: ConfigRegistry = Depends(get_ucr)
 ) -> List[UCRVar]:
+    """
+    Create / change one or more UCR variables.
+
+    **ucr_vars**: A **list of UCRVar** objects to create or update. If the
+        value is null, the UCR variable will be unset (deleted).
+    """
     pass
 
 
-@app.put("/{var_name}", response_model=List[UCRVar])
-async def put(var_name: str, ucr_var: UCRVar, ucr: ConfigRegistry = Depends(get_ucr)):
+@app.put("/{key}", response_model=List[UCRVar])
+async def put(key: str, value: str = Body(...), ucr: ConfigRegistry = Depends(get_ucr)):
+    """
+    Set or unset a UCR variable.
+
+    **value**: The new value, null will unset (delete) the UCR variable
+    """
     pass
 
 
-@app.delete("/{var_name}")
-async def delete(var_name: str, ucr: ConfigRegistry = Depends(get_ucr)):
+@app.delete("/{key}")
+async def delete(key: str, ucr: ConfigRegistry = Depends(get_ucr)):
+    """
+    Unset (delete) a UCR variable.
+    """
     pass
