c2fc128505e470193f8df315e861bceea9aa0dd6
Florian Best <best@univention.de>
2019-12-19 14:22:29 +0100

    Bug #50648 UDM modules: make EA_LAyout comparable in Python 3
    Note: this looks broken in python 2: cmp() or cmp()
    
    Thanks to Philipp:
    Fix logic errors in comparison and always use rich comparison, which is
    available since Python 2.6
    
    Traceback (most recent call last):
      File "/usr/share/univention-directory-manager-tools/univention-cli-server", line 223, in doit
        output = univention.admincli.admin.doit(arglist)
      File "/usr/lib/python3/dist-packages/univention/admincli/admin.py", line 412, in doit
        out = _doit(arglist)
      File "/usr/lib/python3/dist-packages/univention/admincli/admin.py", line 625, in _doit
        univention.admin.modules.init(lo, position, module)
      File "/usr/lib/python3/dist-packages/univention/admin/modules.py", line 203, in init
        update_extended_attributes(lo, module, position)
      File "/usr/lib/python3/dist-packages/univention/admin/modules.py", line 553, in update_extended_attributes
        priofields = sorted(priofields)
    TypeError: unorderable types: EA_Layout() < EA_Layout()

--- management/univention-directory-manager-modules/modules/univention/admin/modules.py
+++ management/univention-directory-manager-modules/modules/univention/admin/modules.py
@@ -338,8 +338,26 @@ class EA_Layout(dict):
 		# type: () -> bool
 		return self.get('is_app_tab', False)
 
-	def __cmp__(self, other):
-		return cmp(self.groupName, other.groupName) or cmp(self.position, other.position)
+	def __lt__(self, other):
+		return (self.groupName, self.position) < (other.groupName, other.position)
+
+	def __gt__(self, other):
+		return (self.groupName, self.position) > (other.groupName, other.position)
+
+	def __eq__(self, other):
+		return (self.groupName, self.position) == (other.groupName, other.position)
+
+	def __le__(self, other):
+		return (self.groupName, self.position) <= (other.groupName, other.position)
+
+	def __ge__(self, other):
+		return (self.groupName, self.position) >= (other.groupName, other.position)
+
+	def __ne__(self, other):
+		return (self.groupName, self.position) != (other.groupName, other.position)
+
+	def __hash__(self):
+		return hash((self.groupName, self.position))
 
 
 def update_extended_attributes(lo, module, position):
