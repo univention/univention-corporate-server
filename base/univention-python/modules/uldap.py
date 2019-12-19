c2ea0f454076af16d9a01275bd057a06adcd9365
Florian Best <best@univention.de>
2019-12-19 13:41:59 +0100

    Bug #50648 python uldap: force python 3 to use correct string types
    Traceback (most recent call last):
      File "/usr/share/univention-directory-manager-tools/univention-cli-server", line 223, in doit
        output = univention.admincli.admin.doit(arglist)
      File "/usr/lib/python3/dist-packages/univention/admincli/admin.py", line 412, in doit
        out = _doit(arglist)
      File "/usr/lib/python3/dist-packages/univention/admincli/admin.py", line 591, in _doit
        lo = univention.admin.uldap.access(host=configRegistry['ldap/master'], port=int(configRegistry.get('ldap/master/port', '7389')), base=baseDN, binddn=binddn, bindpw=pwd, start_tls=tls)
      File "/usr/lib/python3/dist-packages/univention/admin/uldap.py", line 555, in __init__
        self.lo = univention.uldap.access(host, port, base, binddn, bindpw, start_tls, follow_referral=follow_referral)
      File "/usr/lib/python3/dist-packages/univention/uldap.py", line 275, in __init__
        self.__open(ca_certfile)
      File "/usr/lib/python3/dist-packages/univention/uldap.py", line 368, in __open
        self.bind(self.binddn, self.bindpw)
      File "/usr/lib/python3/dist-packages/univention/uldap.py", line 199, in _decorated
        return func(self, *args, **kwargs)
      File "/usr/lib/python3/dist-packages/univention/uldap.py", line 295, in bind
        self.lo.simple_bind_s(self.binddn, self.__encode_pwd(self.bindpw))
      File "/usr/lib/python3/dist-packages/univention/uldap.py", line 278, in __encode_pwd
        if isinstance(pwd, unicode):
    NameError: name 'unicode' is not defined

--- base/univention-python/modules/uldap.py
+++ base/univention-python/modules/uldap.py
@@ -34,6 +34,7 @@ import re
 from functools import wraps
 import random
 
+import six
 import ldap
 import ldap.schema
 import ldap.sasl
@@ -283,7 +284,9 @@ class access(object):
 		self.__open(ca_certfile)
 
 	def __encode_pwd(self, pwd):
-		if isinstance(pwd, unicode):
+		if six.PY3:
+			return pwd
+		if isinstance(pwd, unicode):  # noqa: F821
 			return str(pwd)
 		else:
 			return pwd
@@ -389,7 +392,7 @@ class access(object):
 	def __encode(self, value):
 		if value is None:
 			return value
-		elif isinstance(value, unicode):
+		elif six.PY2 and isinstance(value, unicode):  # noqa: F821
 			return str(value)
 		elif isinstance(value, (list, tuple)):
 			return map(self.__encode, value)
