d7c283676ac06e8e6f58f3d667804e2537d0f2c7
Philipp Hahn <hahn@univention.de>
2020-03-12 21:28:52 +0100

    Bug #50943 licence: Fix warnings
    dpkg-gencontrol: Warnung: Paket univention-license-import: unbenutzte Substitutionsvariable ${shlibs:Depends}
    
    dpkg-gensymbols: Warnung: einige neue Symbole sind in der Symboldatei aufgetaucht: lesen Sie die folgende Diff-Ausgabe
    dpkg-gensymbols: Warnung: debian/libunivention-license0/DEBIAN/symbols passt nicht komplett zu debian/libunivention-license0.symbols
    --- debian/libunivention-license0.symbols (libunivention-license0_10.0.0-4_amd64)
    +++ dpkg-gensymbols4VtJeb       2020-03-12 21:35:22.345134112 +0100
    @@ -1,4 +1,5 @@
    + lp@Base 10.0.0-4
    
    aclocal: warning: autoconf input should be named 'configure.ac', not 'configure.in'
    automake: warning: autoconf input should be named 'configure.ac', not 'configure.in'
    
    license_ldap.c: In function ‘univention_ldap_set_machine_connection’:
    license_ldap.c:17:2: warning: implicit declaration of function ‘asprintf’ [-Wimplicit-function-declaration]
      asprintf(&lp->binddn, univention_config_get_string("ldap/hostdn"));
      ^~~~~~~~

--- base/univention-licence/lib/license_ldap.c
+++ base/univention-licence/lib/license_ldap.c
@@ -6,7 +6,7 @@
 
 #include <univention/ldap.h>
 
-univention_ldap_parameters_t* lp = NULL;
+static univention_ldap_parameters_t* lp = NULL;
 
 #define _UNIVENTION_LDAP_MACHINE_SECRET_LEN_MAX 60
 int univention_ldap_set_machine_connection( univention_ldap_parameters_t *lp )
@@ -14,7 +14,7 @@ int univention_ldap_set_machine_connection( univention_ldap_parameters_t *lp )
 	FILE *secret;
 	size_t len;
 
-	asprintf(&lp->binddn, univention_config_get_string("ldap/hostdn"));
+	lp->binddn = univention_config_get_string("ldap/hostdn");
 	if (!lp->binddn) {
 		goto err;
 	}
