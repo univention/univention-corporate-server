5c40561b300c3c438dfe838835156f84aca939f0
Philipp Hahn <hahn@univention.de>
2020-03-16 16:39:45 +0100

    Bug #48334: branch tests 2
    $TEST_SECTIONS expected each section to be prefixed by '-s' for
    `ucs-test` to consume. If it was not given the run fails without any
    error message.
    
    Extend the code to automatically prefix '-s' for each work in
    $TEST_SECTIONS.
    
    Output errors to STDERR.
    
    Replace `egrep` with `grep -E`.
    
    Fix shellcheck issues.

--- test/branch-tests/run.sh
+++ test/branch-tests/run.sh
@@ -5,36 +5,48 @@ set -x
 
 env
 
+# LOCAL_UCS='/var/lib/jenkins/LOCAL/ucs.git'
+LOCAL_UAS='/var/lib/jenkins/LOCAL/ucsschool.git'
+
+die () {
+	echo "${0##*/} ERROR: $*" >&2
+	exit 1
+}
+
 getGitlabURL () {
-	local fn
 	local product="$1"
-	fn="${HOME}/secrets/gitlab_${product}.url"
-	if [ ! -f "$fn" ] ; then
-		echo "ERROR: the gitlab secret file for $product ($fn) does not exist or is no file!"
-		exit 1
-	fi
+	local fn="${HOME}/secrets/gitlab_${product}.url"
+	[ -f "$fn" ] ||
+		die "the gitlab secret file for $product ($fn) does not exist or is no file!"
 	cat "$fn"
 }
 
 get_ucsschool_git_checkout () {
-	if [ -z "$WORKSPACE" ] ; then
-		echo "ERROR: \$WORKSPACE is empty"
-		exit 1
-	fi
+	[ -n "${UCSSCHOOL_BRANCH}" ] ||
+		return 0
+	[ -n "${WORKSPACE:-}" ] ||
+		die "\$WORKSPACE is empty"
+	local UAS="${WORKSPACE}/${GIT_DIR_UCSSCHOOL}"
 	echo ">>> performing cleanup"
-	[ -n "$WORKSPACE" ] && rm -Rf "${WORKSPACE}/${GIT_DIR_UCSSCHOOL}"
+	[ -n "$WORKSPACE" ] && rm -rf "$UAS"
 	echo ">>> create repo"
-	git init "${WORKSPACE}/${GIT_DIR_UCSSCHOOL}"
-	echo ">>> set referenced repo / reuse local repo"
-	echo "/var/lib/jenkins/LOCAL/ucsschool.git/objects" > "${WORKSPACE}/${GIT_DIR_UCSSCHOOL}/.git/objects/info/alternates"
+	git clone \
+		--progress \
+		--branch "$UCSSCHOOL_BRANCH" \
+		--reference "$LOCAL_UAS" \
+		--single-branch \
+		--no-checkout \
+		--depth 1 \
+		--config remote.origin.tagOpt=--no-tags \
+		--config core.sparsecheckout=true \
+		"$REPO_UCSSCHOOL" "$UAS"
+	#	--no-tags \
 	echo ">>> fetch UCS@school (feature) branch"
 	(
-		cd "${WORKSPACE}/${GIT_DIR_UCSSCHOOL}"
-		git fetch --no-tags --progress "$REPO_UCSSCHOOL" "+refs/heads/${UCSSCHOOL_BRANCH}:refs/remotes/origin/${UCSSCHOOL_BRANCH}" --depth=1
+		cd "$UAS"
 		echo ">>> perform a sparse checkout"
-		git config core.sparsecheckout true
 		echo "test/*" >> .git/info/sparse-checkout
-		git checkout -b "$UCSSCHOOL_BRANCH" "origin/$UCSSCHOOL_BRANCH"
+		git checkout "$UCSSCHOOL_BRANCH"
 		echo ">>> UCS@school checkout done"
 	)
 }
@@ -47,13 +59,16 @@ GIT_DIR_UCSSCHOOL="ucsschool.git"
 BUILD_HOST="10.200.18.180"
 
 # if the default branch of UCS@school is given, then build UCS else build UCS@school
-if echo "$UCSSCHOOL_BRANCH" | egrep -q "^[0-9].[0-9]$" ; then
+case "$UCSSCHOOL_BRANCH" in
+''|[0-9].[0-9])
 	BUILD_BRANCH="$UCS_BRANCH"
 	BUILD_REPO="$REPO_UCS"
-else
+	;;
+*)
 	BUILD_BRANCH="$UCSSCHOOL_BRANCH"
 	BUILD_REPO="$REPO_UCSSCHOOL"
-fi
+	;;
+esac
 
 get_ucsschool_git_checkout
 
@@ -63,12 +78,29 @@ get_ucsschool_git_checkout
 cd test
 
 # update packages
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "jenkins@${BUILD_HOST}" python3 /home/jenkins/build -r "${BUILD_REPO}" -b "${BUILD_BRANCH}" > utils/apt-get-branch-repo.list || exit 1
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "jenkins@${BUILD_HOST}" \
+	python3 /home/jenkins/build -r "${BUILD_REPO}" -b "${BUILD_BRANCH}" > utils/apt-get-branch-repo.list ||
+	die "Failed building packages"
 
 # replace non deb lines
 sed -i '/^deb /!d' utils/apt-get-branch-repo.list
 
 release=4.4-3
+
+sections=''
+parse_sections () {
+	local arg
+	local IFS=' ,'
+	# shellcheck disable=SC2048
+	for arg in $*  # IFS
+	do
+		case "$arg" in
+			-s|--section) continue ;;
+			*) sections="${sections:+$sections }-s $arg" ;;
+		esac
+  done
+}
+parse_sections "${TEST_SECTIONS:-}"
 
 export TEMPLATE_VERSION="${TEMPLATE_VERSION:=$release}"
 export TEMPLATE_NAME="${TEMPLATE_NAME:=generic-unsafe}"
@@ -76,6 +108,7 @@ export UCS_VERSION="${UCS_VERSION:=$release}"
 export TARGET_VERSION="${UCS_VERSION}"
 export RELEASE_UPDATE="${release_update:=public}"
 export ERRATA_UPDATE="${errata_update:=testing}"
+export TEST_SECTIONS="${sections}"
 
 declare -a cmd=("./ucs-ec2-tools/ucs-kvm-create" "-c" "$CFG_FILE")
 "$HALT" && cmd+=("-t")
