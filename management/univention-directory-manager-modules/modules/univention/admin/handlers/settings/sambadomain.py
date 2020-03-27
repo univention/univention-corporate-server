dc6e4d9c30c6e5ba80cb3bf573da9adc50c0b865
Florian Best <best@univention.de>
2020-03-27 09:36:15 +0100

    Bug #50648 UDM settings/sambadomain: fix mapping
    python3 /usr/lib/python3/dist-packages/univention/admincli/admin.py settings/sambadomain modify --dn sambaDomainName=SCHOOL,cn=samba,l=school,l=dev --set logonToChangePW=1
    Traceback (most recent call last):
      File "/usr/lib/python3/dist-packages/univention/admincli/admin.py", line 1129, in <module>
        print('\n'.join(doit(sys.argv)))
      File "/usr/lib/python3/dist-packages/univention/admincli/admin.py", line 411, in doit
        out = _doit(arglist)
      File "/usr/lib/python3/dist-packages/univention/admincli/admin.py", line 910, in _doit
        dn = object.modify()
      File "/usr/lib/python3/dist-packages/univention/admin/handlers/__init__.py", line 665, in modify
        dn = self._modify(modify_childs, ignore_license=ignore_license, response=response)
      File "/usr/lib/python3/dist-packages/univention/admin/handlers/__init__.py", line 1344, in _modify
        self.dn = self.lo.modify(self.dn, ml, ignore_license=ignore_license, serverctrls=serverctrls, response=response)
      File "/usr/lib/python3/dist-packages/univention/admin/uldap.py", line 888, in modify
        return self.lo.modify(dn, changes, serverctrls=serverctrls, response=response)
      File "/usr/lib/python3/dist-packages/univention/uldap.py", line 200, in _decorated
        return func(self, *args, **kwargs)
      File "/usr/lib/python3/dist-packages/univention/uldap.py", line 766, in modify
        self.modify_ext_s(dn, ml, serverctrls=serverctrls, response=response)
      File "/usr/lib/python3/dist-packages/univention/uldap.py", line 200, in _decorated
        return func(self, *args, **kwargs)
      File "/usr/lib/python3/dist-packages/univention/uldap.py", line 825, in modify_ext_s
        rtype, rdata, rmsgid, resp_ctrls = self.lo.modify_ext_s(dn, ml, serverctrls=serverctrls)
      File "/usr/lib/python3/dist-packages/ldap/ldapobject.py", line 1253, in modify_ext_s
        return self._apply_method_s(SimpleLDAPObject.modify_ext_s,*args,**kwargs)
      File "/usr/lib/python3/dist-packages/ldap/ldapobject.py", line 1197, in _apply_method_s
        return func(self,*args,**kwargs)
      File "/usr/lib/python3/dist-packages/ldap/ldapobject.py", line 601, in modify_ext_s
        msgid = self.modify_ext(dn,modlist,serverctrls,clientctrls)
      File "/usr/lib/python3/dist-packages/ldap/ldapobject.py", line 598, in modify_ext
        return self._ldap_call(self._l.modify_ext,dn,modlist,RequestControlTuples(serverctrls),RequestControlTuples(clientctrls))
      File "/usr/lib/python3/dist-packages/ldap/ldapobject.py", line 313, in _ldap_call
        result = func(*args,**kwargs)
    TypeError: ('Tuple_to_LDAPMod(): expected a byte string in the list', '2')

--- management/univention-directory-manager-modules/modules/univention/admin/handlers/settings/sambadomain.py
+++ management/univention-directory-manager-modules/modules/univention/admin/handlers/settings/sambadomain.py
@@ -50,19 +50,18 @@ def logonToChangePWMap(val):
 	"""
 
 	if (val == "1"):
-		return "2"
+		return b"2"
 	else:
-		return "0"
+		return b"0"
 
 # see also container/dc.py
 
 
 def logonToChangePWUnmap(val):
-
-	if (val[0] == "2"):
-		return "1"
+	if (val[0] == b"2"):
+		return u"1"
 	else:
-		return "2"
+		return u"2"
 
 
 module = 'settings/sambadomain'
