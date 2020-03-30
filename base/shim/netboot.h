1d9efaea22941ae7f60a28bef4b2775e9c66963e
Philipp Hahn <hahn@univention.de>
2020-03-30 18:07:14 +0200

    Bug #99999: Remove shim - newer version in Debian 10 Buster

--- base/shim/netboot.h
+++
@@ -1,9 +0,0 @@
-#ifndef _NETBOOT_H_
-#define _NETBOOT_H_
-
-extern BOOLEAN findNetboot(EFI_HANDLE image_handle);
-
-extern EFI_STATUS parseNetbootinfo(EFI_HANDLE image_handle);
-
-extern EFI_STATUS FetchNetbootimage(EFI_HANDLE image_handle, VOID **buffer, UINTN *bufsiz);
-#endif
