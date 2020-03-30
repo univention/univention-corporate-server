592896ebe6fa784c435510722732778dc64ce9bf
Philipp Hahn <hahn@univention.de>
2020-03-30 18:07:14 +0200

    Bug #51984: Remove shim - newer version in Debian 10 Buster

--- base/shim/include/errors.h
+++
@@ -1,9 +0,0 @@
-#include <efierr.h>
-
-#ifndef EFI_INCOMPATIBLE_VERSION
-#define EFI_INCOMPATIBLE_VERSION	EFIERR(25)
-#endif
-#ifndef EFI_SECURITY_VIOLATION
-#define EFI_SECURITY_VIOLATION		EFIERR(26)
-#endif
-
