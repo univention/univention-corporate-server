08cea9bed8c10152442a797db3c8231505531f23
Arvid Requate <requate@univention.de>
2020-04-03 14:28:33 +0200

    Bug #49060 UDM dhcp: migrate ipaddr → ipaddress [CHECK]
    Move ipaddress logic into dhcp module.
    Move it to __common as the same code should be used for shared-subnet,
    too.
    
    TODO: syntax.IPv4_AddressRange allows 1-tuple ranges - does this raise a
    ValueError?
    
    fixup! Bug #49060 UDM dhcp: migrate ipaddr → ipaddress [CHECK]

--- management/univention-directory-manager-modules/modules/univention/admin/handlers/dhcp/subnet.py
+++ management/univention-directory-manager-modules/modules/univention/admin/handlers/dhcp/subnet.py
@@ -30,15 +30,14 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
-import ipaddr
+import ipaddress
 
 from univention.admin.layout import Tab, Group
 import univention.admin.filter
 import univention.admin.handlers
-import univention.admin.ipaddress
 import univention.admin.localization
 
-from .__common import DHCPBase, add_dhcp_options, rangeUnmap, rangeMap
+from .__common import DHCPBaseSubnet, add_dhcp_options, rangeUnmap, rangeMap
 
 translation = univention.admin.localization.translation('univention.admin.handlers.dhcp')
 _ = translation.translate
@@ -106,32 +105,8 @@ mapping.register('range', 'dhcpRange', rangeMap, rangeUnmap)
 add_dhcp_options(__name__)
 
 
-class object(DHCPBase):
+class object(DHCPBaseSubnet):
 	module = module
-
-	def ready(self):
-		super(object, self).ready()
-
-		if ipaddr.IPv4Network('%(subnet)s/%(subnetmask)s' % self.info).network != ipaddr.IPv4Address('%(subnet)s' % self.info):
-			raise univention.admin.uexceptions.valueError(_('The subnet mask does not match the subnet.'), property='subnetmask')
-
-		subnet = ipaddr.IPNetwork('%(subnet)s/%(subnetmask)s' % self.info)
-		if self.hasChanged('range') or not self.exists():
-			# TODO: replace univention.admin.ipaddress.*() with ipaddr module
-			for addresses in self['range']:
-				for addresses2 in self['range']:
-					if addresses != addresses2 and univention.admin.ipaddress.is_range_overlapping(addresses, addresses2):
-						raise univention.admin.uexceptions.rangesOverlapping('%s-%s; %s-%s' % (addresses[0], addresses[1], addresses2[0], addresses2[1]))
-
-				for addr in addresses:
-					if univention.admin.ipaddress.ip_is_network_address(self['subnet'], self['subnetmask'], addr):
-						raise univention.admin.uexceptions.rangeInNetworkAddress('%s-%s' % (addresses[0], addresses[1]))
-
-					if univention.admin.ipaddress.ip_is_broadcast_address(self['subnet'], self['subnetmask'], addr):
-						raise univention.admin.uexceptions.rangeInBroadcastAddress('%s-%s' % (addresses[0], addresses[1]))
-
-					if ipaddr.IPAddress(addr) not in subnet:
-						raise univention.admin.uexceptions.rangeNotInNetwork(addr)
 
 	@staticmethod
 	def unmapped_lookup_filter():
