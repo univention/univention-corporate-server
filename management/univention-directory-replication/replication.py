univention/univention-corporate-server

commit 5d0a78710c63768ed893767b8b38b8442aac9a9c

Bug #48545 repl: Do incremental updates for group.uniqueMember
A LDAP_MOD_REPLACE on group.uniqueMember trigger slapd-memberof to touch
each users with a sub-transaction, which is very slow as it forces slapd
to to a fdatasync() for each sub-transaction.

Do an incremental update for attribute uniqueMember only for now.

management/univention-directory-replication/replication.py
@@ -574,15 +574,35 @@ def modlist(old, new):
 	for key, values in new.items():
 		if key in EXCLUDE_ATTRIBUTES:
 			continue
+
 		if key not in old:
 			ml.append((ldap.MOD_ADD, key, values))
-		elif values != old[key]:
-			ml.append((ldap.MOD_REPLACE, key, values))
+			continue
+
+		set_old = set(old[key])
+		set_new = set(values)
+		if set_old == set_new:
+			continue
+
+		if key == 'uniqueMember':
+			# trigger slapd-memberof, where REPLACE is inefficient (Bug #48545)
+			added_items = set_new - set_old
+			removed_items = set_old - set_new
+			if len(added_items) + len(removed_items) < len(values):
+				if added_items:
+					ml.append((ldap.MOD_ADD, key, list(added_items)))
+				if removed_items:
+					ml.append((ldap.MOD_DELETE, key, list(removed_items)))
+				continue
+
+		ml.append((ldap.MOD_REPLACE, key, values))
+
 	for key in old:
 		if key in EXCLUDE_ATTRIBUTES:
 			continue
 		if key not in new:
 			ml.append((ldap.MOD_DELETE, key, []))
+
 	return ml
 
 

commit 929762b894ece8aa4b1643ec95898b49575ce4a0

Bug #48545: Fix incremental updates for group.uniqueMember
delete before add; otherwise we can a duplicate attribute value and UDL
switches to failed.ldif mode.

The name of the `memberOf` is configurable via UCRV.

Fixes: 5d0a78710c63768ed893767b8b38b8442aac9a9c

management/univention-directory-replication/replication.py
@@ -584,15 +584,15 @@ def modlist(old, new):
 		if set_old == set_new:
 			continue
 
-		if key == 'uniqueMember':
+		if key == listener.baseConfig.get('ldap/overlay/memberof/member', 'uniqueMember'):
 			# trigger slapd-memberof, where REPLACE is inefficient (Bug #48545)
 			added_items = set_new - set_old
 			removed_items = set_old - set_new
 			if len(added_items) + len(removed_items) < len(values):
-				if added_items:
-					ml.append((ldap.MOD_ADD, key, list(added_items)))
 				if removed_items:
 					ml.append((ldap.MOD_DELETE, key, list(removed_items)))
+				if added_items:
+					ml.append((ldap.MOD_ADD, key, list(added_items)))
 				continue
 
 		ml.append((ldap.MOD_REPLACE, key, values))