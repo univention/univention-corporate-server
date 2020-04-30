1ad5c9bf56ac31200b1b34976a89c3f29eccf5f5
Philipp Hahn <hahn@univention.de>
2020-04-30 07:24:02 +0200

    Bug #49704 ucslint: Convert 0002 to UPCFileTeste
    - reduces code complexity
    - allows easier extension
    - adds column nuumbers

--- packaging/ucslint/univention/ucslint/0002-CopyPasteErrors.py
+++ packaging/ucslint/univention/ucslint/0002-CopyPasteErrors.py
@@ -28,7 +28,9 @@
 # <https://www.gnu.org/licenses/>.
 
 import univention.ucslint.base as uub
-import os
+from itertools import chain
+from os.path import join
+import re
 
 # 1) check if strings like "dc=univention,dc=qa" appear in debian/* and conffiles/*
 # 2) check if strings like "univention.qa" appear in debian/* and conffiles/*
@@ -47,43 +49,17 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 		""" the real check """
 		super(UniventionPackageCheck, self).check(path)
 
-		files = []
-		# scan directory only
-		for dir in ['debian']:
-			for fn in os.listdir(os.path.join(path, dir)):
-				self.debug(os.path.join(path, dir, fn))
-				if not os.path.isdir(os.path.join(path, dir, fn)):
-					files.append(os.path.join(path, dir, fn))
+		tester = uub.UPCFileTester()
+		tester.addTest(re.compile(r'dc=univention,dc=(?:local|qa|test)'), '0002-2', 'contains invalid basedn', cntmax=0)
+		tester.addTest(re.compile(r'univention\.(?:local|qa|test)'), '0002-3', 'contains invalid domainname', cntmax=0)
 
-		# scan directory recursively
-		for dir in ['conffiles']:
-			for fn in uub.FilteredDirWalkGenerator(os.path.join(path, dir)):
-				files.append(fn)
-
-		for fn in files:
+		for fn in chain(
+			uub.FilteredDirWalkGenerator(join(path, 'conffiles')),
+			uub.FilteredDirWalkGenerator(join(path, 'debian')),
+		):
 			try:
-				content = open(fn, 'r').read()
+				tester.open(fn)
 			except EnvironmentError:
 				self.addmsg('0002-1', 'failed to open and read file', fn)
-				continue
-
-			for txt in ['dc=univention,dc=local', 'dc=univention,dc=qa', 'dc=univention,dc=test']:
-				for line in self._searchString(content, txt):
-					self.addmsg('0002-2', 'contains invalid basedn', fn, line)
-
-			for txt in ['univention.local', 'univention.qa', 'univention.test']:
-				for line in self._searchString(content, txt):
-					self.addmsg('0002-3', 'contains invalid domainname', fn, line)
-
-	def _searchString(self, content, txt):
-		result = []
-		pos = 0
-		while True:
-			fpos = content.find(txt, pos)
-			if fpos < 0:
-				break
 			else:
-				line = content.count('\n', 0, fpos) + 1
-				pos = fpos + len(txt) - 1
-				result.append(line)
-		return result
+				self.msg += tester.runTests()
