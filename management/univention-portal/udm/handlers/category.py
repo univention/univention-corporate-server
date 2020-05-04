4fdeb34d8cc71b3f9600f063da68fee870e36c7a
Dirk Wiesenthal <wiesenthal@univention.de>
2020-05-04 22:01:44 +0200

    Bug #51157: i18n typo fix

--- management/univention-portal/udm/handlers/category.py
+++ management/univention-portal/udm/handlers/category.py
@@ -33,7 +33,7 @@ from univention.admin.layout import Tab, Group
 import univention.admin.localization
 import univention.admin.handlers
 
-translation = univention.admin.localization.translation('univention.admin.handlers.portals')
+translation = univention.admin.localization.translation('univention.admin.handlers.portals.portal')
 _ = translation.translate
 
 module = 'portals/category'
