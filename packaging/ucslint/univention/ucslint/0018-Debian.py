a668865b1970b1a26e50b70f2a7178d5e0c5634a
Philipp Hahn <hahn@univention.de>
2020-05-20 06:46:00 +0200

    Bug #43981 ucslint: Check Debian maintainer script actions
    Detect wrong action handling in {pre,post}{inst,rm} scripts.

--- packaging/ucslint/univention/ucslint/0018-Debian.py
+++ packaging/ucslint/univention/ucslint/0018-Debian.py
@@ -32,10 +32,22 @@ import univention.ucslint.base as uub
 from os import walk
 from os.path import basename, dirname, isdir, join, normpath, relpath, splitext
 from glob import glob
+from shlex import split
 from typing import Callable, Dict, Iterator, List, Set, Tuple  # noqa F401
+import re
 
 
 class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
+
+	# https://www.debian.org/doc/debian-policy/ch-maintainerscripts.html#summary-of-ways-maintainer-scripts-are-called
+	# https://wiki.debian.org/DpkgTriggers
+	ACTIONS = {
+		'preinst': {'install', 'upgrade', 'abort-upgrade'},
+		'postinst': {'configure', 'abort-upgrade', 'abort-remove', 'abort-deconfigure', 'triggered'},
+		'prerm': {'remove', 'upgrade', 'deconfigure', 'failed-upgrade'},
+		'postrm': {'remove', 'purge', 'upgrade', 'disappear', 'failed-upgrade', 'abort-install', 'abort-upgrade'},
+	}
+	SCRIPTS = frozenset(ACTIONS)
 
 	def getMsgIds(self):
 		return {
@@ -51,22 +63,130 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 
 	def check_scripts(self, path):
 		# type: (str) -> None
-		SCRIPTS = frozenset(('preinst', 'postinst', 'prerm', 'postrm'))
 		debianpath = join(path, 'debian')
-		for script_path in uub.FilteredDirWalkGenerator(debianpath, suffixes=SCRIPTS):
+		for script_path in uub.FilteredDirWalkGenerator(debianpath, suffixes=self.SCRIPTS):
 			package, suffix = self.split_pkg(script_path)
 
+			other_scripts = self.SCRIPTS - {suffix}
+			other_actions = set(action for actions in self.ACTIONS.values() for action in actions) - self.ACTIONS[suffix]
+			self.debug('script=%s' % suffix)
+			self.debug('actions=%s' % ' '.join(sorted(self.ACTIONS[suffix])))
+			self.debug('other_script=%s' % ' '.join(sorted(other_scripts)))
+			self.debug('other_actions=%s' % ' '.join(sorted(other_actions)))
+
 			with open(script_path, 'r') as script_file:
-				for nr, line in enumerate(script_file, start=1):
+				content = script_file.read()
+
+			for nr, line in enumerate(content.splitlines(), start=1):
 					if not line.startswith('#'):
 						break
-					for script_name in SCRIPTS - set((suffix,)):
+					for script_name in other_scripts:
 						if script_name in line:
 							self.addmsg(
 								'0018-1',
 								'wrong script name: %r' % (line.strip(),),
 								filename=script_path,
 								line=nr)
+
+			for nr, line in enumerate(content.splitlines(), start=1):
+				if line.startswith('#'):
+					continue
+				for match in self.RE_TEST.finditer(line):
+					try:
+						actions = self.parse_test(split(match.group('cond'))) & other_actions
+					except ValueError as ex:
+						self.debug('Failed %s:%d: %s in %s' % (script_path, nr, ex, line))
+						continue
+					if actions:
+						self.addmsg(
+							'0018-3',
+							'Invalid actions "%s" in Debian maintainer script' % (','.join(actions),),
+							filename=script_path,
+							line=nr)
+
+			nr = 1
+			col = 1
+			pos = 0
+			for match in self.RE_CASE.finditer(content):
+				for cases in match.group('cases').split(';;'):
+					cases = cases.lstrip('\t\n\r (')
+					cases = cases.split(')', 1)[0]
+					actions = set(action for case in cases.split('|') for action in split(case)) & other_actions
+					if actions:
+						start, end = match.span()
+						while pos < start:
+							if match.string[pos] == "\n":
+								col = 1
+								nr += 1
+							else:
+								col += 1
+							pos += 1
+
+						self.addmsg(
+							'0018-3',
+							'Invalid actions "%s" in Debian maintainer script' % (','.join(actions),),
+							filename=script_path, line=nr, pos=col)
+
+	@classmethod
+	def parse_test(cls, tokens):
+		# type: (List[str]) -> Set[str]
+		"""
+		Parse test string and return action names
+
+		:param tokens: `test` string tokens.
+		:returns: Set containing the action names
+
+		>>> UniventionPackageCheck.parse_test(['$1', '=', 'upgrade'])
+		{'upgrade'}
+		>>> UniventionPackageCheck.parse_test(['upgrade', '!=', '${1}'])
+		{'upgrade'}
+		>>> UniventionPackageCheck.parse_test(['(', '-n', '$1', ')'])
+		set()
+		>>> UniventionPackageCheck.parse_test(['-n', '$1'])
+		set()
+		>>> UniventionPackageCheck.parse_test(['-n', '$1', '-a', '-z', '$1'])
+		set()
+		>>> UniventionPackageCheck.parse_test(['1', '-le', '2'])
+		set()
+		>>> UniventionPackageCheck.parse_test(['$1'])
+		set()
+		"""
+		COND = {'-a', '-o'}
+		UNARY = {'-b', '-c', '-d', '-e', '-f', '-g', '-G', '-h', '-k', '-L', '-n', '-O', '-p', '-r', '-s', '-S', '-t', '-u', '-w', '-x', '-z'}
+		COMP1 = {'-ot', '-nt', '-ef', '-ne', '-lt', '-le', '-gt', '-ge', '-eq'}
+		COMP2 = {'=', '!=', '==', '=~'}
+		result = set()
+		while tokens:
+			t = tokens.pop(0)
+			if t == ')':
+				break
+			elif t == '(':
+				result |= cls.parse_test(tokens)
+			elif t == '!':
+				pass
+			elif t in COND:
+				pass
+			elif t in UNARY:
+				tokens.pop(0)
+			elif t.startswith('-'):
+				raise ValueError(t)
+			elif tokens:
+				op = tokens.pop(0)
+				if op in COMP1 | COMP2:
+					arg = tokens.pop(0)
+					if op in COMP2:
+						if cls.RE_ARG1.match(t):
+							result.add(arg)
+						elif cls.RE_ARG1.match(arg):
+							result.add(t)
+				elif op in COND:
+					pass  # [-n] t
+				else:
+					raise ValueError(op)
+			else:
+				pass  # [-n] t
+
+		return result
 
 	def check_dirs(self, path):
 		# type: (str) -> None
@@ -171,3 +291,34 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 							yield (src_path, dst_path)
 				else:
 					yield (fn, join(dst, basename(fn)))
+
+	RE_HASHBANG = re.compile(r'^#!\s*/bin/(?:[bd]?a)?sh\b')
+	RE_TEST = re.compile(
+		r'''
+		(?:(?P<test>\[{1,2}) | \b test)
+		\s+
+		(?P<cond>.+?)
+		(?(test)\s+\]{1,2} | (?:\s*(?:; | && | \|\| | $)))
+		''', re.VERBOSE)
+	RE_CASE = re.compile(
+		r'''
+		\b
+		case
+		\s+
+		(?P<quot>"?)
+		(?:\$(?:1|\{1[#%:?+=/-[^}]*\}))
+		(?P=quot)
+		\s+
+		in
+		\s+
+		(?P<cases>.+?)
+		\b
+		esac
+		\b
+		''', re.VERBOSE | re.DOTALL)
+	RE_ARG1 = re.compile(r'\$(?:1|\{1[#%:?+=/-[^}]*\})')
+
+
+if __name__ == '__main__':
+	import doctest
+	doctest.testmod()
