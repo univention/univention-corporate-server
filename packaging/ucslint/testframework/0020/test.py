univention/univention-corporate-server

commit 6399d1150517a183d26b68852aefe5712de19058

Bug #51107 ucslint: Extend testsuite for 0020
TODO: Needs more work

packaging/ucslint/testframework/0020.correct
@@ -1,0 +1,2 @@
+W:0020-E999: conffiles/python3:2:4: SyntaxError: invalid syntax
+W:0020-E999: conffiles/python2:2:8: SyntaxError: invalid syntax

packaging/ucslint/testframework/0020/test.py
@@ -1,0 +1,2 @@
+var: str = ""
+print var

packaging/ucslint/testframework/0020/test2
@@ -1,0 +1,2 @@
+#!/usr/bin/python2
+var: str = ""

packaging/ucslint/testframework/0020/conffiles/python2
@@ -1,0 +1,3 @@
+@!@
+print ""
+@!@

packaging/ucslint/testframework/0020/conffiles/python3
@@ -1,0 +1,3 @@
+@!@
+var: str = ""
+@!@

packaging/ucslint/testframework/0020/debian/compat
@@ -1,0 +1,1 @@
+9

packaging/ucslint/testframework/0020/debian/control
@@ -1,0 +1,12 @@
+Source: ucslint
+Section: univention
+Priority: optional
+Maintainer: Univention GmbH <packages@univention.de>
+Build-Depends:
+ debhelper (>> 9),
+ dh-python,
+Standards-Version: 3.6.1
+
+Package: ucslint
+Architecture: all
+Description: test

packaging/ucslint/testframework/0020/debian/copyright
@@ -1,0 +1,29 @@
+Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: Univention GmbH
+Upstream-Contact: <package@univention.de>
+Source: https://updates.software-univention.de/
+
+Files: *
+Copyright: 2008-2020 Univention GmbH
+License: AGPL-3.0-only
+ The source code of the software contained in this package
+ as well as the source package itself are made available
+ under the terms of the GNU Affero General Public License version 3
+ (GNU AGPL V3) as published by the Free Software Foundation.
+ .
+ Binary versions of this software provided by Univention to you as
+ well as other copyrighted, protected or trademarked materials like
+ Logos, graphics, fonts, specific documentations and configurations,
+ cryptographic keys etc. are subject to a license agreement between
+ you and Univention and not subject to the GNU AGPL V3.
+ .
+ In the case you use the software under the terms of the GNU AGPL V3,
+ the program is provided in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Affero General Public License for more details.
+ .
+ You should have received a copy of the GNU Affero General Public
+ License with the Debian GNU/Linux or Univention distribution in file
+ /usr/share/common-licenses/AGPL-3; if not, see
+ <https://www.gnu.org/licenses/>.

commit 84d8d9c2cce93d24da429b793bcbb847a9b9e66a

Bug #51107 ucslint: Fix testsuite for 0020 2
Use print() function as type annotations only work since 3.5, but
UCS-4.4 only has 3.3.

Handle the additional 2 lines when `flake8 --show-source´ is enabled by
--debug.

Extends ucslint-sort-output.py to sort flake8 output too to make the
result stable: listdir() returns the path names in no stable order,
which leads to different output ordering and fails the test suite.

Fixes: 6399d1150517a183d26b68852aefe5712de19058

packaging/ucslint/testframework/0020.correct
@@ -1,2 +1,6 @@
-W:0020-E999: conffiles/python3:2:14: SyntaxError: invalid syntax
 W:0020-E999: conffiles/python2:2:8: SyntaxError: invalid syntax
+W:0020-E999: conffiles/python3:2:14: SyntaxError: invalid syntax
+W:0020-E999: test.py:1:14: SyntaxError: invalid syntax
+W:0020-E999: test.py:2:9: SyntaxError: invalid syntax
+W:0020-E999: test2:2:14: SyntaxError: invalid syntax
+W:0020-E999: test3:2:8: SyntaxError: invalid syntax

packaging/ucslint/testframework/0020/test.py
@@ -1,2 +1,2 @@
-var: str = ""
+print("", end="")
 print var

packaging/ucslint/testframework/0020/test2
@@ -1,2 +1,2 @@
 #!/usr/bin/python2
-var: str = ""
+print("", end="")

packaging/ucslint/ucslint-sort-output.py
@@ -5,7 +5,7 @@
 import sys
 import re
 
-RE_ID = re.compile(r'^[UWEIS]:\d\d\d\d-\d+: ')
+RE_ID = re.compile(r'^[UWEIS]:\d\d\d\d-[BEFNW]?\d+: ')
 
 
 def main():

packaging/ucslint/univention/ucslint/0020-flake8.py
@@ -63,7 +63,6 @@ class UniventionPackageCheck(uub.UniventionPackageCheckBase):
 		re.compile(r'conffiles/[^/]+/'),  # UCR templates with markers contain syntax errors
 		re.compile(r'python-notifier/'),  # external code
 		re.compile(r'univention-ldb-modules/buildtools/'),  # external code
-		re.compile(r'ucslint/testframework/'),  # don't care about tests for ucslint
 		re.compile(r'services/univention-printserver/modules/univention/management/console/handlers/cups'),  # UCS 2.4 code
 		re.compile(r'univention-directory-manager-modules/test/'),  # unrelevant, should be removed imho
 	]
@@ -332,8 +331,13 @@ class UniventionPackageCheck(uub.UniventionPackageCheckBase):
 				leading_lines = len(text[:text.index(match)].splitlines())
 				match = match.rstrip() + '\n'  # prevent "blank line at end of file" and "blank line contains whitespace" false positives
 				for error in self.flake8(python, ['-'], self.DEFAULT_IGNORE, UCR_HEADER + match):
-					errno, filename, lineno, position, descr = error.split(' ', 4)
-					lineno = str(int(lineno) - header_length + leading_lines)
+					try:
+						errno, filename, lineno, position, descr = error.split(' ', 4)
+						lineno = str(int(lineno) - header_length + leading_lines)
+					except ValueError as ex:
+						# flake8 --show-source provides 2 extra lines
+						self.debug('%s: %s' % (ex, error))
+						continue
 					errors.append(' '.join((errno, conffile, lineno, position, descr)))
 		return errors
 