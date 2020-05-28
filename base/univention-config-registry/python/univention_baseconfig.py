dd3b33bff1e0d68d8aa151c07ce6954d751be8d3
Philipp Hahn <hahn@univention.de>
2020-05-28 17:55:10 +0200

    Bug #52016 UCR: Drop baseconfig

--- base/univention-config-registry/python/univention_baseconfig.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import warnings
-warnings.warn('univention_baseconfig is deprecated by univention.config_registry', PendingDeprecationWarning)
-del warnings
-
-from univention.config_registry import *
-baseConfig = ConfigRegistry
