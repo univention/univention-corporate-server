univention/univention-corporate-server

commit 9442c8cc53847f457d5c65f0e202ed921e8ca098

Bug #50648: univention.uldap: fix decoding of policy dn's
Traceback (most recent call last):
  File "/usr/share/univention-directory-manager-tools/univention-cli-server", line 223, in doit
    output = univention.admincli.admin.doit(arglist)
  File "/usr/lib/python3/dist-packages/univention/admincli/admin.py", line 412, in doit
    out = _doit(arglist)
  File "/usr/lib/python3/dist-packages/univention/admincli/admin.py", line 760, in _doit
    dn = object.create()
  File "/usr/lib/python3/dist-packages/univention/admin/handlers/__init__.py", line 584, in create
    dn = self._create(response=response, serverctrls=serverctrls)
  File "/usr/lib/python3/dist-packages/univention/admin/handlers/__init__.py", line 1268, in _create
    al.extend(self._ldap_modlist())
  File "/usr/lib/python3/dist-packages/univention/admin/handlers/users/user.py", line 1722, in _ldap_modlist
    pwhistoryPolicy = univention.admin.password.PasswortHistoryPolicy(self.loadPolicyObject('policies/pwhistory'))
  File "/usr/lib/python3/dist-packages/univention/admin/password.py", line 298, in __init__
    self.pwhistoryLength = max(0, int(pwhistoryPolicy['length'] or 0))
  File "/usr/lib/python3/dist-packages/univention/admin/handlers/__init__.py", line 3530, in __getitem__
    self.policy_result()
  File "/usr/lib/python3/dist-packages/univention/admin/handlers/__init__.py", line 3502, in policy_result
    result = self.lo.getPolicies(self.lo.parentDn(self.referring_object_dn), policies=policies)
  File "/usr/lib/python3/dist-packages/univention/admin/uldap.py", line 823, in getPolicies
    return self.lo.getPolicies(dn, policies, attrs, result, fixedattrs)
  File "/usr/lib/python3/dist-packages/univention/uldap.py", line 200, in _decorated
    return func(self, *args, **kwargs)
  File "/usr/lib/python3/dist-packages/univention/uldap.py", line 592, in getPolicies
    self._merge_policy(policy_dn, obj_dn, object_classes, result)

base/univention-python/modules/uldap.py
@@ -578,9 +578,9 @@ class access(object):
 		else:
 			oattrs = self.get(dn, ['univentionPolicyReference', 'objectClass'])
 		if attrs and 'univentionPolicyReference' in attrs:
-			policies = attrs['univentionPolicyReference']
+			policies = [x.decode('utf-8') for x in attrs['univentionPolicyReference']]
 		elif not policies and not attrs:
-			policies = oattrs.get('univentionPolicyReference', [])
+			policies = [x.decode('utf-8') for x in oattrs.get('univentionPolicyReference', [])]
 
 		object_classes = set(oc.lower() for oc in oattrs.get('objectClass', []))
 
@@ -597,7 +597,7 @@ class access(object):
 					parent = self.get(dn, attr=['univentionPolicyReference'], required=True)
 				except ldap.NO_SUCH_OBJECT:
 					break
-				policies = parent.get('univentionPolicyReference', [])
+				policies = [x.decode('utf-8') for x in parent.get('univentionPolicyReference', [])]
 
 		univention.debug.debug(
 			univention.debug.LDAP, univention.debug.INFO,
@@ -638,7 +638,7 @@ class access(object):
 		fixed = set(pattrs.get('fixedAttributes', ()))
 		empty = set(pattrs.get('emptyAttributes', ()))
 		values = result.setdefault(ptype, {})
-		for key in list(empty) + pattrs.keys() + list(fixed):
+		for key in list(empty) + list(pattrs.keys()) + list(fixed):
 			if key in ('requiredObjectClasses', 'prohibitedObjectClasses', 'fixedAttributes', 'emptyAttributes', 'objectClass', 'cn', 'univentionObjectType', 'ldapFilter'):
 				continue
 

commit cd5ecfb27316fdaf94f457aad892e41037d50b34

Bug #51029 python uldap: Fix Python 3 compatibility
Python 2: dict.keys() -> List[str]
Python 3: dict.keys() -> Iterator[str]
    List[str] + Iterator[str] -> TypeError

Fixes: 9442c8cc53847f457d5c65f0e202ed921e8ca098

base/univention-python/modules/uldap.py
@@ -648,10 +648,8 @@ class access(object):
 		fixed = set(pattrs.get('fixedAttributes', ()))
 		empty = set(pattrs.get('emptyAttributes', ()))
 		values = result.setdefault(ptype, {})
-		for key in list(empty) + list(pattrs.keys()) + list(fixed):
-			if key in ('requiredObjectClasses', 'prohibitedObjectClasses', 'fixedAttributes', 'emptyAttributes', 'objectClass', 'cn', 'univentionObjectType', 'ldapFilter'):
-				continue
-
+		SKIP = {'requiredObjectClasses', 'prohibitedObjectClasses', 'fixedAttributes', 'emptyAttributes', 'objectClass', 'cn', 'univentionObjectType', 'ldapFilter'}
+		for key in (empty | set(pattrs) | fixed) - SKIP:
 			if key not in values or key in fixed:
 				value = [] if key in empty else pattrs.get(key, [])
 				# value = [x.decode('UTF-8') for x in value]   # TODO: This would be ugly here