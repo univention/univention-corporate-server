022aa7df5aa18c0f700b699c491ad074bcfe7820
Jürn Brodersen <brodersen@univention.de>
2020-06-11 21:16:52 +0200

    Bug #51472: Update upstream version to 1.1.3

--- saml/php-krb5/src/examples/kadm/ex2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = new KADM5('test2/admin', 'test.keytab', true);
-$princ = $conn->getPrincipal('test');
-var_dump($princ->getPropertyArray());
-$princ->setExpiryTime(time() + 60*60*24*100);
-$princ->save();
-echo "\n";
-?>
--- saml/php-krb5/src/examples/kadm/ex5.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-$princ = new KRB5CCache();
-$princ->initPassword('test1', 'foo123');
-var_dump($princ->getEntries());
-?>
