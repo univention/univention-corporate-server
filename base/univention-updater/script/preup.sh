c0248247230c09ea77b045afbd4ddc26a13d4a40
Florian Best <best@univention.de>
2020-06-15 15:12:52 +0200

    51497: WIP: block upgrade if univention-samba4wins is installed

--- base/univention-updater/script/preup.sh
+++ base/univention-updater/script/preup.sh
@@ -339,6 +339,19 @@ block_update_if_failed_ldif_exists() {
 }
 block_update_if_failed_ldif_exists
 
+# Bug #51497
+block_update_if_univention_samba4wins_exists() {
+	if dpkg -l univention-samba4wins 2>/dev/null | grep -q ^rc; then
+		echo "WARNING: The package univention-samba4wins is deprecated and must be purged"
+		exit 1
+	fi
+	if ! univention-ldapsearch -LLL 'objectClass=univentionSamba4WinsHost' | grep -q '^dn:'; then
+		echo "WARNING: The package univention-samba4wins is deprecated and must be purged"
+		exit 1
+	fi
+
+}
+block_update_if_univention_samba4wins_exists
 
 # move old initrd files in /boot
 initrd_backup=/var/backups/univention-initrd.bak/
