univention/univention-corporate-server

commit fccf19b8e538a928902cfd30380839114bb44529

Bug #51505: remove wtmp and btmp from logrotate.conf

base/univention-base-files/conffiles/etc/logrotate.conf
@@ -18,19 +18,3 @@ create
 
 # packages drop log rotation information into this directory
 include /etc/logrotate.d
-
-# no packages own wtmp, or btmp -- we'll rotate them here
-/var/log/wtmp {
-    missingok
-    monthly
-    create 0664 root utmp
-    rotate @%@logrotate/rotate/count@%@
-}
-
-/var/log/btmp {
-    missingok
-    monthly
-    create 0660 root utmp
-    rotate @%@logrotate/rotate/count@%@
-}
-

commit 2bae7a2df2cb01bbefa14fcead299391ce00f4f0

Bug #51505,Bug #32509 base: Update UCR template [WIP]
Remove SysV init scripts shadows by systemd.service files.

Update templates from Debian 10 Buster.

TODO: Check rsyslog.conf syntax change!

Fixes: fccf19b8e538a928902cfd30380839114bb44529

base/univention-base-files/conffiles/etc/bash.bashrc
@@ -18,7 +18,10 @@ if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
 fi
 
 # set a fancy prompt (non-color, overwrite the one in /etc/profile)
-PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+# but only if not SUDOing and have SUDO_PS1 set; then assume smart user.
+if ! [ -n "${SUDO_USER}" -a -n "${SUDO_PS1}" ]; then
+  PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
 
 # Commented out, don't overwrite xterm -T "title" -n "icontitle" by default.
 # If this is an xterm set the title to user@host:dir
@@ -48,10 +51,10 @@ if [ -x /usr/lib/command-not-found -o -x /usr/share/command-not-found/command-no
 	function command_not_found_handle {
 	        # check because c-n-f could've been removed in the meantime
                 if [ -x /usr/lib/command-not-found ]; then
-		   /usr/bin/python /usr/lib/command-not-found -- "$1"
+		   /usr/lib/command-not-found -- "$1"
                    return $?
                 elif [ -x /usr/share/command-not-found/command-not-found ]; then
-		   /usr/bin/python /usr/share/command-not-found/command-not-found -- "$1"
+		   /usr/share/command-not-found/command-not-found -- "$1"
                    return $?
 		else
 		   printf "%s: command not found\n" "$1" >&2

base/univention-base-files/conffiles/etc/default/ssh
@@ -3,11 +3,5 @@
 # Default settings for openssh-server. This file is sourced by /bin/sh from
 # /etc/init.d/ssh.
 
-# check ucr autostart setting
-if [ -f "/usr/share/univention-config-registry/init-autostart.lib" ]; then
-    . "/usr/share/univention-config-registry/init-autostart.lib"
-    check_autostart sshd sshd/autostart
-fi
-
 # Options to pass to sshd
 SSHD_OPTS=

base/univention-base-files/conffiles/etc/init.d/networking
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/sh -e
 @%@UCRWARNING=# @%@
 
 ### BEGIN INIT INFO
@@ -11,9 +11,10 @@
 # Description:       Prepare /run/network directory, ifstate file and raise network interfaces, or take them down.
 ### END INIT INFO
 
-PATH="/sbin:/bin:/usr/sbin:/usr/bin"
+PATH="/sbin:/bin"
 RUN_DIR="/run/network"
 IFSTATE="$RUN_DIR/ifstate"
+STATEDIR="$RUN_DIR/state"
 
 [ -x /sbin/ifup ] || exit 0
 [ -x /sbin/ifdown ] || exit 0
@@ -26,6 +27,7 @@ VERBOSE=no
 
 [ -f /etc/default/networking ] && . /etc/default/networking
 
+verbose=""
 [ "$VERBOSE" = yes ] && verbose=-v
 
 process_exclusions() {
@@ -49,6 +51,9 @@ check_ifstate() {
 	    log_failure_msg "can't create $RUN_DIR"
 	    exit 1
 	fi
+	if ! chown root:netdev "$RUN_DIR" ; then
+	    log_warning_msg "can't chown $RUN_DIR"
+	fi
     fi
     if [ ! -r "$IFSTATE" ] ; then
 	if ! :> "$IFSTATE" ; then
@@ -68,7 +73,7 @@ check_network_file_systems() {
 
     while read DEV MTPT FSTYPE REST; do
 	case $DEV in
-	/dev/nbd*|/dev/nd[a-z]*|/dev/etherd/e*)
+	/dev/nbd*|/dev/nd[a-z]*|/dev/etherd/e*|curlftpfs*)
 	    log_warning_msg "not deconfiguring network interfaces: network devices still mounted."
 	    exit 0
 	    ;;
@@ -102,9 +107,9 @@ ifup_hotplug () {
 			    do
 				    link=${iface##:*}
 				    link=${link##.*}
-				    if [ -e "/sys/class/net/$link" ] && [ "$(cat /sys/class/net/$link/operstate)" = up ]
+				    if [ -e "/sys/class/net/$link" ]
 				    then
-					    echo "$iface"
+					echo "$iface"
 				    fi
 			    done)
 	    if [ -n "$ifaces" ]
@@ -115,22 +120,17 @@ ifup_hotplug () {
 }
 
 domac () { # change MAC-address
-    eval "$(univention-config-registry shell)"
-
-    for i in 0 1 2 3; do
-        var=interfaces_eth${i}_mac
-        if [ -n "${!var}" ]; then
-            #iprout: ip link set "eth${i}" address "${!var}"
-            ifconfig "eth${i}" hw ether "${!var}"
-        fi
+    /usr/sbin/ucr search --non-empty --brief '^interfaces/[^/]*/mac$' |
+    while IFS=': ' read -r k v
+    do
+        k="${k#*/}"
+        k="${k%/*}"
+        ip link set "$k" address "$v"
     done
 }
 
 case "$1" in
 start)
-	if init_is_upstart; then
-		exit 1
-	fi
 	process_options
 	check_ifstate
 	domac
@@ -143,6 +143,11 @@ start)
 	set -f
 	exclusions=$(process_exclusions)
 	log_action_begin_msg "Configuring network interfaces"
+	if [ -x /bin/udevadm ]; then
+		if [ -n "$(ifquery --list --exclude=lo)" ] || [ -n "$(ifquery --list --allow=hotplug)" ]; then
+			/bin/udevadm settle || true
+		fi
+	fi
 	if ifup -a $exclusions $verbose && ifup_hotplug $exclusions $verbose
 	then
 	    log_action_end_msg $?
@@ -152,9 +157,6 @@ start)
 	;;
 
 stop)
-	if init_is_upstart; then
-		exit 0
-	fi
 	check_network_file_systems
 	check_network_swap
 
@@ -170,7 +172,7 @@ reload)
 	process_options
 
 	log_action_begin_msg "Reloading network interfaces configuration"
-	state=$(cat /run/network/ifstate)
+	state=$(ifquery --state)
 	ifdown -a --exclude=lo $verbose || true
 	if ifup --exclude=lo $state $verbose ; then
 	    log_action_end_msg $?
@@ -180,9 +182,6 @@ reload)
 	;;
 
 force-reload|restart)
-	if init_is_upstart; then
-		exit 1
-	fi
 	process_options
 
 	log_warning_msg "Running $0 $1 is deprecated because it may not re-enable some interfaces"

base/univention-base-files/conffiles/etc/init.d/nscd
@@ -1,143 +1,0 @@
-#!/bin/sh
-@%@UCRWARNING=# @%@
-### BEGIN INIT INFO
-# Provides:          nscd
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Starts the Name Service Cache Daemon
-### END INIT INFO
-# pidfile: /var/run/nscd/nscd.pid
-
-#
-# nscd:		Starts the Name Service Cache Daemon
-#
-# description:  This is a daemon which handles passwd and group lookups
-#		for running programs and caches the results for the next
-#		query.  You should start this daemon only if you use
-#		slow Services like NIS or NIS+
-
-PATH="/sbin:/usr/sbin:/bin:/usr/bin"
-NAME="nscd"
-DESC="Name Service Cache Daemon"
-DAEMON="/usr/sbin/nscd"
-PIDFILE="/var/run/nscd/nscd.pid"
-
-# Sanity checks.
-umask 022
-[ -f /etc/nscd.conf ] || exit 0
-[ -x "$DAEMON" ] || exit 0
-[ -d /var/run/nscd ] || mkdir -p /var/run/nscd
-. /lib/lsb/init-functions
-
-start_nscd()
-{
-	# Return
-	#   0 if daemon has been started or was already running
-	#   2 if daemon could not be started
-
-	start-stop-daemon --start --quiet --pidfile "$PIDFILE" --exec "$DAEMON" --test > /dev/null || return 0
-	start-stop-daemon --start --quiet --pidfile "$PIDFILE" --exec "$DAEMON" || return 2
-}
-
-stop_nscd()
-{
-	# Return
-	#   0 if daemon has been stopped
-	#   1 if daemon was already stopped
-	#   2 if daemon could not be stopped
-
-	# we try to stop using nscd --shutdown, that fails also if nscd is not present.
-	# in that case, fallback to "good old methods"
-	RETVAL=0
-
-	if [ -r "$PIDFILE" ]; then
-		PID=$(cat "$PIDFILE")
-	fi
-	if ! $DAEMON --shutdown; then
-		start-stop-daemon --stop --quiet --pidfile "$PIDFILE" --name "$NAME" --test > /dev/null
-		RETVAL="$?"
-		[ "$?" -ne 0  -a  "$?" -ne 1 ] && return 2
-	fi
-
-	# Wait for children to finish too
-	if [ -n "$PID" ]; then
-		pkill -HUP -P "$PID"
-		if [ -n "$(pgrep -P "$PID")" ]; then
-			sleep 1
-			pkill -KILL -P "$PID"
-		fi
-	fi
-
-	rm -f "$PIDFILE"
-	return "$RETVAL"
-}
-
-status()
-{
-	# Return
-	#   0 if daemon is stopped
-	#   1 if daemon is running
-	start-stop-daemon --start --quiet --pidfile "$PIDFILE" --exec "$DAEMON" --test > /dev/null || return 1
-	return 0
-}
-
-invalidate_cache()
-{
-	for table in passwd group hosts ; do
-		$DAEMON --invalidate $table
-	done
-}
-
-case "$1" in
-start)
-	log_daemon_msg "Starting $DESC" "$NAME"
-	start_nscd
-	case "$?" in
-		0) log_end_msg 0 ; exit 0 ;;
-		1) log_warning_msg " (already running)." ; exit 0 ;;
-		*) log_end_msg 1 ; exit 1 ;;
-	esac
-	;;
-stop)
-	log_daemon_msg "Stopping $DESC" "$NAME"
-	stop_nscd
-	case "$?" in
-		0) log_end_msg 0 ; exit 0 ;;
-		1) log_warning_msg " (not running)." ; exit 0 ;;
-		*) log_end_msg 1 ; exit 1 ;;
-	esac
-	;;
-restart|force-reload|reload)
-	log_daemon_msg "Restarting $DESC" "$NAME"
-	stop_nscd
-	case "$?" in
-	0|1)
-		start_nscd
-		case "$?" in
-			0) invalidate_cache; log_end_msg 0 ; exit 0 ;;
-			1) log_failure_msg " (failed -- old process is still running)." ; exit 1 ;;
-			*) log_failure_msg " (failed to start)." ; exit 1 ;;
-		esac
-		;;
-	*)
-		log_failure_msg " (failed to stop)."
-		exit 1
-		;;
-	esac
-	;;
-status)
-	log_daemon_msg "Status of $DESC service: "
-	status
-	case "$?" in
-		0) log_failure_msg "not running." ; exit 3 ;;
-		1) log_success_msg "running." ; exit 0 ;;
-	esac
-	;;
-*)
-	echo "Usage: /etc/init.d/$NAME {start|stop|reload|force-reload|restart|status}" >&2
-	exit 1
-	;;
-esac
-

base/univention-base-files/conffiles/etc/init.d/rdate
@@ -1,53 +1,0 @@
-#!/bin/sh
-@%@UCRWARNING=# @%@
-
-### BEGIN INIT INFO
-# Provides:        rdate
-# Required-Start:  $network $remote_fs
-# Required-Stop:   $network $remote_fs
-# Default-Start:   2 3 4 5
-# Default-Stop:
-# X-Start-Before: ntp
-# Short-Description: Fetch NTP time
-### END INIT INFO
-
-PATH=/sbin:/bin
-
-test -f /usr/bin/rdate || exit 0
-
-if test -f /etc/default/ntpdate ; then
-	. /etc/default/ntpdate
-else
-	NTPSERVERS="pool.ntp.org"
-fi
-
-test -n "$NTPSERVERS" || exit 0
-
-case "$1" in
-	start|restart)
-		echo -n "Synchronize clock:"
-		for srv in $NTPSERVERS; do
-			if /bin/netcat -q0 -w1 $srv time </dev/null >/dev/null 2>&1; then
-				if /usr/bin/rdate $srv >/dev/null 2>&1; then
-					echo " done."
-					exit 0
-				fi
-			fi
-		done
-		for srv in $NTPSERVERS; do
-			if /usr/sbin/ntpdate -b -t 0.2 -u 54321 $srv >/dev/null 2>&1; then
-				echo " done."
-				exit 0
-			fi
-		done
-		echo " failed!"
-		;;
-	stop)
-		;;
-	*)
-		echo "Usage: /etc/init.d/rdate {start|stop|restart}"
-		exit 1
-		;;
-esac
-
-exit 0

base/univention-base-files/conffiles/etc/init.d/rpcbind
@@ -1,109 +1,0 @@
-#!/bin/sh
-@%@UCRWARNING=# @%@
-#
-# start/stop rpcbind daemon.
-
-### BEGIN INIT INFO
-# Provides:          rpcbind
-# Required-Start:    $network $local_fs
-# Required-Stop:     $network $local_fs
-# Default-Start:     S
-# Default-Stop:      0 1 6
-# Short-Description: RPC portmapper replacement
-# Description:       rpcbind is a server that converts RPC (Remote
-#                    Procedure Call) program numbers into DARPA
-#                    protocol port numbers. It must be running in
-#                    order to make RPC calls. Services that use
-#                    RPC include NFS and NIS.
-### END INIT INFO
-# pidfile: /run/rpcbind.pid
-
-test -f /sbin/rpcbind || exit 0
-
-. /lib/lsb/init-functions
-
-OPTIONS="-w"
-STATEDIR=/run/rpcbind
-PIDFILE=/run/rpcbind.pid
-
-if [ -f /etc/default/rpcbind ]
-then
-    . /etc/default/rpcbind
-elif [ -f /etc/rpcbind.conf ]
-then
-    . /etc/rpcbind.conf
-fi
-
-start ()
-{
-    if [ ! -d $STATEDIR ] ; then
-        mkdir $STATEDIR
-    fi
-    if [ ! -O $STATEDIR ] ; then
-        log_begin_msg "$STATEDIR not owned by root"
-        log_end_msg 1
-        exit 1
-    fi
-    if [ ! -f $STATEDIR/rpcbind.xdr ]
-    then
-        touch $STATEDIR/rpcbind.xdr
-    fi
-    if [ ! -f $STATEDIR/portmap.xdr ]
-    then
-        touch $STATEDIR/portmap.xdr
-    fi
-    [ -x /sbin/restorecon ] && /sbin/restorecon $STATEDIR/*.xdr
-    pid=$( pidofproc /sbin/rpcbind )
-    if [ -n "$pid" ]
-    then
-        log_action_msg "Already running: rcpbind"
-        exit 0
-    fi
-    log_begin_msg "Starting rpcbind daemon..."
-    start-stop-daemon --start --quiet --oknodo --exec /sbin/rpcbind -- "$@"
-    pid=$( pidofproc /sbin/rpcbind )
-    echo -n "$pid" >"$PIDFILE"
-    # /run/sendsigs.omit.d is created by /etc/init.d/mountkernfs.sh
-    ln -sf "$PIDFILE" /run/sendsigs.omit.d/rpcbind
-    log_end_msg $?
-
-}
-
-stop ()
-{
-    log_begin_msg "Stopping rpcbind daemon..."
-    start-stop-daemon --stop --quiet --oknodo --exec /sbin/rpcbind
-    rm -f "$PIDFILE"
-    log_end_msg $?
-}
-
-case "$1" in
-    start)
-        if init_is_upstart; then
-            exit 1
-        fi
-        start $OPTIONS
-        ;;
-    stop)
-        if init_is_upstart; then
-            exit 0
-        fi
-        stop
-        ;;
-    restart|force-reload)
-        if init_is_upstart; then
-            exit 1
-        fi
-        stop
-        start $OPTIONS
-        ;;
-    status)
-        status_of_proc /sbin/rpcbind rpcbind && exit 0 || exit $?
-        ;;
-    *)
-        log_success_msg "Usage: /etc/init.d/rpcbind {start|stop|force-reload|restart|status}"
-        exit 1
-        ;;
-esac
-
-exit 0

base/univention-base-files/conffiles/etc/inputrc
@@ -7,7 +7,7 @@
 set input-meta on
 set output-meta on
 
-# To allow the use of 8bit-characters like the german umlauts, comment out
+# To allow the use of 8bit-characters like the german umlauts, uncomment
 # the line below. However this makes the meta key not work as a meta key,
 # which is annoying to those which don't need to type in 8-bit characters.
 
@@ -52,6 +52,7 @@ $if mode=emacs
 "\e\e[D": backward-word
 
 $if term=rxvt
+"\e[7~": beginning-of-line
 "\e[8~": end-of-line
 "\eOc": forward-word
 "\eOd": backward-word
@@ -66,4 +67,3 @@ $endif
 # "\e[F": end-of-line
 
 $endif
-

base/univention-base-files/conffiles/etc/issue
@@ -1,27 +1,20 @@
 @!@
+ROLES = {
+	"domaincontroller_master": "Univention DC Master",
+	"domaincontroller_slave": "Univention DC Backup",
+	"domaincontroller_backup": "Univention DC Slave",
+	"memberserver": "Univention Memberserver",
+}
 
-if configRegistry.get('server/role') == 'fatclient':
-	menutitle="Univention Managed Client"
-elif configRegistry.get('server/role') == 'mobileclient':
-	menutitle="Univention Mobile Client"
-elif configRegistry.get('server/role') == 'domaincontroller_master':
-	menutitle="Univention DC Master"
-elif configRegistry.get('server/role') == 'domaincontroller_backup':
-	menutitle="Univention DC Backup"
-elif configRegistry.get('server/role') == 'domaincontroller_slave':
-	menutitle="Univention DC Slave"
-elif configRegistry.get('server/role') == 'memberserver':
-	menutitle="Univention Memberserver"
-else:
-	menutitle="Univention Corporate Server"
+role = configRegistry.get('server/role')
+menutitle = ROLES.get(role, "Univention Corporate Server")
 
 if configRegistry.get('version/version'):
 	print('%s %s-%s:' % (menutitle, configRegistry.get('version/version'), configRegistry.get('version/patchlevel')))
 else:
 	print(menutitle)
 
-
-if configRegistry.get('server/role') in ['domaincontroller_master', 'domaincontroller_backup', 'domaincontroller_slave', 'memberserver']:
+if role in ROLES:
 	print('')
 	startsite = ""
 	host = configRegistry.get('hostname')
@@ -37,7 +30,7 @@ if configRegistry.get('server/role') in ['domaincontroller_master', 'domaincontr
 		if ip:
 			startsite = "%s (%s)" % (startsite, ip)
 
-	if configRegistry.get('server/role') == 'domaincontroller_master':
+	if role == 'domaincontroller_master':
 		print('The UCS management system is available at https://' + startsite)
 	else:
 		print('The UCS management system can be reached by following the link to the domain controller')

base/univention-base-files/conffiles/etc/issue.net
@@ -1,19 +1,13 @@
 @!@
+ROLES = {
+	"domaincontroller_master": "Univention DC Master",
+	"domaincontroller_slave": "Univention DC Backup",
+	"domaincontroller_backup": "Univention DC Slave",
+	"memberserver": "Univention Memberserver",
+}
 
-if configRegistry.get('server/role') == 'fatclient':
-	menutitle="Univention Managed Client"
-elif configRegistry.get('server/role') == 'mobileclient':
-	menutitle="Univention Mobile Client"
-elif configRegistry.get('server/role') == 'domaincontroller_master':
-	menutitle="Univention DC Master"
-elif configRegistry.get('server/role') == 'domaincontroller_backup':
-	menutitle="Univention DC Backup"
-elif configRegistry.get('server/role') == 'domaincontroller_slave':
-	menutitle="Univention DC Slave"
-elif configRegistry.get('server/role') == 'memberserver':
-	menutitle="Univention Memberserver"
-else:
-	menutitle="Univention Corporate Server"
+role = configRegistry.get('server/role')
+menutitle = ROLES.get(role, "Univention Corporate Server")
 
 if configRegistry.get('version/version'):
 	print('%s %s-%s:' % (menutitle, configRegistry.get('version/version'), configRegistry.get('version/patchlevel')))

base/univention-base-files/conffiles/etc/logrotate.conf
@@ -13,8 +13,13 @@ tabooext + .debian
 # create new (empty) log files after rotating old ones
 create
 
+# use date as a suffix of the rotated file
+#dateext
+
 # uncomment this if you want your log files compressed
 #compress
 
 # packages drop log rotation information into this directory
 include /etc/logrotate.d
+
+# system-specific logs may be also be configured here.

base/univention-base-files/conffiles/etc/logrotate.d/btmp
@@ -1,0 +1,9 @@
+@%@UCRWARNING=# @%@
+
+# no packages own btmp -- we'll rotate it here
+/var/log/btmp {
+    missingok
+    monthly
+    create 0660 root utmp
+    rotate @%@logrotate/rotate/count@%@
+}

base/univention-base-files/conffiles/etc/logrotate.d/rsyslog
@@ -46,7 +46,7 @@ for (cat, (paths, settings)) in FILES.items():
 	delaycompress
 	sharedscripts
 	postrotate
-		invoke-rc.d rsyslog rotate > /dev/null
+		/usr/lib/rsyslog/rsyslog-rotate
 	endscript
 }""")
 @!@

base/univention-base-files/conffiles/etc/logrotate.d/wtmp
@@ -1,0 +1,10 @@
+@%@UCRWARNING=# @%@
+
+# no packages own wtmp -- we'll rotate it here
+/var/log/wtmp {
+    missingok
+    monthly
+    create 0664 root utmp
+    minsize 1M
+    rotate @%@logrotate/rotate/count@%@
+}

base/univention-base-files/conffiles/etc/modules
@@ -1,4 +1,8 @@
 @%@UCRWARNING=# @%@
+# /etc/modules: kernel modules to load at boot time.
+#
+# This file contains the names of kernel modules that should be loaded
+# at boot time, one per line. Lines beginning with "#" are ignored.
 
 @!@
 if configRegistry['kernel/modules']:

base/univention-base-files/conffiles/etc/network/interfaces.d/00-base
@@ -1,5 +1,13 @@
 @%@UCRWARNING=# @%@
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
 
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
 auto lo
 iface lo inet loopback
 
+# The primary network interface
+#allow-hotplug enp0s31f6
+#iface enp0s31f6 inet dhcp

base/univention-base-files/conffiles/etc/networks
@@ -1,6 +1,6 @@
 @%@UCRWARNING=# @%@
 
-default			0.0.0.0
-loopback		127.0.0.0
-link-local		169.254.0.0
+default		0.0.0.0
+loopback	127.0.0.0
+link-local	169.254.0.0
 

base/univention-base-files/conffiles/etc/profile
@@ -4,13 +4,14 @@
 # and Bourne compatible shells (bash(1), ksh(1), ash(1), ...).
 
 if [ "`id -u`" -eq 0 ]; then
-  PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/X11"
+  PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 else
-  PATH="/usr/local/bin:/usr/bin:/bin:/usr/bin/X11:/usr/games"
+  PATH="/usr/local/bin:/usr/bin:/bin:/usr/games"
 fi
+export PATH
 
-if [ "$PS1" ]; then
-  if [ "$BASH" ] && [ "$BASH" != "/bin/sh" ]; then
+if [ "${PS1-}" ]; then
+  if [ "${BASH-}" ] && [ "$BASH" != "/bin/sh" ]; then
     # The file bash.bashrc already sets the default PS1.
     # PS1='\h:\w\$ '
     if [ -f /etc/bash.bashrc ]; then

base/univention-base-files/conffiles/etc/rsyslog.conf
@@ -1,8 +1,8 @@
 @%@UCRWARNING=# @%@
-#  /etc/rsyslog.conf	Configuration file for rsyslog.
+# /etc/rsyslog.conf configuration file for rsyslog
 #
-#			For more information see
-#			/usr/share/doc/rsyslog-doc/html/rsyslog_conf.html
+# For more information install rsyslog-doc and see
+# /usr/share/doc/rsyslog-doc/html/configuration/index.html
 
 ######################
 #### Message size ####
@@ -20,13 +20,13 @@ print('$MaxMessageSize %s' % (configRegistry.get("syslog/maxmessagesize", '4k'),
 #### MODULES ####
 #################
 
-$ModLoad imuxsock # provides support for local system logging
+module(load="imuxsock") # provides support for local system logging
 @!@
 print('$SystemLogRateLimitInterval %s' % (configRegistry.get('syslog/limit/interval', '5'),))
 print('$SystemLogRateLimitBurst %s' % (configRegistry.get('syslog/limit/burst', '200'),))
 @!@
-$ModLoad imklog   # provides kernel logging support
-#$ModLoad immark  # provides --MARK-- message capability
+module(load="imklog")   # provides kernel logging support
+#module(load="immark")  # provides --MARK-- message capability
 
 @!@
 from os.path import exists
@@ -40,8 +40,8 @@ for (typ, cfg) in [('udp', 'UDP'), ('tcp', 'InputTCP'), ('relp', 'InputRELP')]:
 	mod = '%s/im%s.so' % (mod_path, typ,)
 	port = configRegistry.get('syslog/input/%s' % (typ,))
 	pfx = '' if port and exists(mod) else '#'
-	print('%s$ModLoad im%s' % (pfx, typ))
-	print('%s$%sServerRun %s' % (pfx, cfg, port or 514))
+	print('%smodule(load="im%s")' % (pfx, typ))
+	print('%sinput(type="im%s" port="%d")' % (pfx, cfg, port or 514))
 	print("")
 @!@
 
@@ -175,24 +175,6 @@ print(r"""
 # Emergencies are sent to everybody logged in.
 #
 *.emerg				:omusrmsg:*
-
-#
-# I like to have messages displayed on the console, but only on a virtual
-# console I usually leave idle.
-#
-#daemon,mail.*;\
-#	news.=crit;news.=err;news.=notice;\
-#	*.=debug;*.=info;\
-#	*.=notice;*.=warn	/dev/tty8
-
-# The named pipe /dev/xconsole is for the `xconsole' utility.  To use it,
-# you must invoke `xconsole' with the `-file' option:
-# 
-#    $ xconsole -file /dev/xconsole [...]
-#
-# NOTE: adjust the list below, or you'll go crazy if you have a reasonably
-#      busy site..
-#
 """)
 syslog('xconsole', r"""
 daemon.*;mail.*;\

base/univention-base-files/conffiles/etc/ssh/ssh_config
@@ -21,30 +21,36 @@ Host *
 #   ForwardAgent no
 #   ForwardX11 no
 #   ForwardX11Trusted yes
-#   RhostsRSAAuthentication no
-#   RSAAuthentication yes
 #   PasswordAuthentication yes
 #   HostbasedAuthentication no
+#   GSSAPIAuthentication no
+#   GSSAPIDelegateCredentials no
+#   GSSAPIKeyExchange no
+#   GSSAPITrustDNS no
 #   BatchMode no
 #   CheckHostIP yes
 #   AddressFamily any
 #   ConnectTimeout 0
 #   StrictHostKeyChecking ask
-#   IdentityFile ~/.ssh/identity
 #   IdentityFile ~/.ssh/id_rsa
 #   IdentityFile ~/.ssh/id_dsa
+#   IdentityFile ~/.ssh/id_ecdsa
+#   IdentityFile ~/.ssh/id_ed25519
 #   Port 22
-#   Protocol 2,1
-#   Cipher 3des
-#   Ciphers aes128-cbc,3des-cbc,blowfish-cbc,cast128-cbc,arcfour,aes192-cbc,aes256-cbc
+#   Protocol 2
+#   Ciphers aes128-ctr,aes192-ctr,aes256-ctr,aes128-cbc,3des-cbc
+#   MACs hmac-md5,hmac-sha1,umac-64@openssh.com
 #   EscapeChar ~
 #   Tunnel no
 #   TunnelDevice any:any
 #   PermitLocalCommand no
+#   VisualHostKey no
+#   ProxyCommand ssh -q -W %h:%p gateway.example.com
+#   RekeyLimit 1G 1h
     SendEnv LANG LC_*
     HashKnownHosts yes
 @!@
-if configRegistry.get( 'auth/methods', '' ).find( 'krb5' ) >= 0:
+if 'krb5' in configRegistry.get('auth/methods', ''):
    print('    GSSAPIAuthentication yes')
 @!@
     GSSAPIDelegateCredentials no

base/univention-base-files/conffiles/etc/ssh/sshd_config
@@ -1,5 +1,5 @@
 @%@UCRWARNING=# @%@
-#	$OpenBSD: sshd_config,v 1.100 2016/08/15 12:32:04 naddy Exp $
+#	$OpenBSD: sshd_config,v 1.103 2018/04/09 20:41:22 tj Exp $
 
 # This is the sshd server system-wide configuration file.  See
 # sshd_config(5) for more information.

base/univention-base-files/conffiles/etc/systemd/journald.conf
@@ -1,3 +1,4 @@
+@%@UCRWARNING=# @%@
 #  This file is part of systemd.
 #
 #  systemd is free software; you can redistribute it and/or modify it
@@ -5,8 +6,11 @@
 #  the Free Software Foundation; either version 2.1 of the License, or
 #  (at your option) any later version.
 #
-# See journald.conf(5) for details
-@%@UCRWARNING=# @%@
+# Entries in this file show the compile time defaults.
+# You can change settings by editing this file.
+# Defaults can be restored by simply deleting this file.
+#
+# See journald.conf(5) for details.
 
 [Journal]
 @!@
@@ -24,8 +28,8 @@ if value is None:
 else:
 	print("SplitMode=%s" % (value, ))
 @!@#SyncIntervalSec=5m
-#RateLimitInterval=30s
-#RateLimitBurst=1000
+#RateLimitIntervalSec=30s
+#RateLimitBurst=10000
 @!@
 value = configRegistry.get('systemd/journald/SystemMaxUse')
 if value is None:
@@ -42,9 +46,12 @@ if value is None:
 	print("#SystemMaxFileSize=")
 else:
 	print("SystemMaxFileSize=%s" % (value, ))
-@!@#RuntimeMaxUse=
+@!@
+#SystemMaxFiles=100
+#RuntimeMaxUse=
 #RuntimeKeepFree=
 #RuntimeMaxFileSize=
+#RuntimeMaxFiles=100
 @!@
 value = configRegistry.get('systemd/journald/MaxRetentionSec')
 if value is None:
@@ -67,4 +74,5 @@ else:
 #MaxLevelKMsg=notice
 #MaxLevelConsole=info
 #MaxLevelWall=emerg
-
+#LineMax=48K
+#ReadKMsg=yes

base/univention-base-files/debian/ucslint.overrides
@@ -15,7 +15,6 @@
 0004-56: conffiles/etc/init.d/nscd
 0004-56: conffiles/etc/init.d/openbsd-inetd
 0004-56: conffiles/etc/init.d/rpcbind
-0004-56: conffiles/etc/init.d/rdate
 0004-56: conffiles/etc/inputrc
 0004-56: conffiles/etc/networks
 0004-56: conffiles/etc/python2.7/sitecustomize.py.d/00header.py
@@ -33,7 +32,6 @@
 0004-16: conffiles/etc/welcome.msg
 # No copyright claim for copied files
 0010-2: conffiles/etc/init.d/nscd
-0010-2: conffiles/etc/init.d/rdate
 0010-2: conffiles/etc/init.d/openbsd-inetd
 0010-2: conffiles/etc/init.d/rpcbind
 0010-2: conffiles/etc/init.d/cron

base/univention-base-files/debian/univention-base-files.maintscript
@@ -2,6 +2,9 @@ rm_conffile /etc/univention/templates/files/etc/init.d/portmap 6.0.0-3~
 rm_conffile /etc/univention/templates/files/etc/init.d/klogd 6.0.0-4~
 rm_conffile /etc/univention/templates/files/etc/init.d/cron 6.0.0-10~
 rm_conffile /etc/univention/templates/files/etc/init.d/openbsd-inetd 6.0.0-10~
+rm_conffile /etc/univention/templates/files/etc/init.d/nscd 9.0.0-3~
+rm_conffile /etc/univention/templates/files/etc/init.d/rdate 9.0.0-3~
+rm_conffile /etc/univention/templates/files/etc/init.d/rpcbind 9.0.0-3~
 rm_conffile /etc/univention/templates/files/etc/python2.6/sitecustomize.py.d/00header.py 9.0.0-3~
 rm_conffile /etc/univention/templates/files/etc/python2.6/sitecustomize.py.d/10apport.py 9.0.0-3~
 rm_conffile /etc/univention/templates/files/etc/python2.6/sitecustomize.py.d/20utf8.py 9.0.0-3~

base/univention-base-files/debian/univention-base-files.postinst
@@ -123,10 +123,6 @@ univention-config-registry set sshd/port?22 \
 	sshd/challengeresponse?yes \
 	sshd/passwordauthentication?no
 
-if [ -e /etc/init.d/rdate ]; then
-	chmod +x /etc/init.d/rdate
-fi
-
 if [ -n "$locale" ]; then
 	locale-gen || true
 fi

base/univention-base-files/debian/univention-base-files.univention-config-registry
@@ -103,18 +103,6 @@ File: etc/init.d/networking
 Mode: 0755
 
 Type: file
-File: etc/init.d/rdate
-Mode: 0755
-
-Type: file
-File: etc/init.d/rpcbind
-Mode: 0755
-
-Type: file
-File: etc/init.d/nscd
-Mode: 0755
-
-Type: file
 File: etc/networks
 
 Type: file
@@ -221,6 +209,12 @@ Subfile: etc/hosts.d/90-ipv6defaults
 Type: file
 File: etc/logrotate.conf
 Variables: logrotate/.*
+
+Type: file
+File: etc/logrotate.d/btmp
+
+Type: file
+File: etc/logrotate.d/wtmp
 
 Type: file
 File: etc/resolv.conf