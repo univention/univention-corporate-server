9ca64e939bc9c8d07d1d314a066349b00409d64f
Florian Best <best@univention.de>
2020-06-27 18:22:16 +0200

    Bug #51525: selenium+chrome: ignore unknown certificates

--- test/ucs-test/univention/testing/selenium/base.py
+++ test/ucs-test/univention/testing/selenium/base.py
@@ -101,6 +101,7 @@ class UMCSeleniumTest(ChecksAndWaits, Interactions):
 			if self.browser == 'chrome':
 				chrome_options = webdriver.ChromeOptions()
 				chrome_options.add_argument('--no-sandbox')  # chrome complains about being executed as root
+				chrome_options.add_argument('ignore-certificate-errors')
 				self.driver = webdriver.Chrome(chrome_options=chrome_options)
 			else:
 				self.driver = webdriver.Firefox()
