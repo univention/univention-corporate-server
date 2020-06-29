63e997c6800d49d3d5dc945c361d533aa170f7c8
Florian Best <best@univention.de>
2020-06-29 11:42:20 +0200

    squash! Try to get initial join running
    fixup! Bug #51268: use ldap.initialize

--- management/univention-directory-replication/replication.py
+++ management/univention-directory-replication/replication.py
@@ -553,21 +553,9 @@ def connect(ldif=0):
 				pw = new_password()
 				init_slapd('restart')
 
-		local_ip = '127.0.0.1'
-		local_port = listener.baseConfig.get('slapd/port', '7389').split(',')[0]
-
-		try:
-			connection = ldap.initialize(local_ip, int(local_port))
-			connection.simple_bind_s('cn=update,' + listener.baseConfig['ldap/base'], pw)
-		except ldap.LDAPError as exc:
-			listener.setuid(0)
-			try:
-				import univention.uldap
-				connection = univention.uldap.getRootDnConnection().lo
-			except ldap.LDAPError:
-				raise exc
-			finally:
-				listener.unsetuid()
+		local_port = int(listener.baseConfig.get('slapd/port', '7389').split(',')[0])
+		connection = ldap.initialize('ldap://127.0.0.1:%d' % (local_port,))
+		connection.simple_bind_s('cn=update,' + listener.baseConfig['ldap/base'], pw)
 	else:
 		connection = LDIFObject(LDIF_FILE)
 
