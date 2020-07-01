10b44e0e1058aca43dd2dc2c9e71ca231f74525a
Florian Best <best@univention.de>
2020-07-01 16:32:21 +0200

    Bug #51593: add unit tests

---
+++ base/univention-lib/unittests/test_locking.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python2.7
+#
+# Copyright 2020 Univention GmbH
+#
+# https://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <https://www.gnu.org/licenses/>.
+
+import os
+import sys
+import pytest
+import subprocess
+
+from univention.unittests import import_module
+locking = import_module('locking', 'python/', 'univention.lib.locking')
+
+
+@pytest.mark.xfail()
+@pytest.mark.parametrize('nonblocking', [True, False])
+def test_locking(nonblocking):
+	lock = locking.get_lock('foo', nonblocking)
+	try:
+		assert os.path.exists('/var/run/foo.pid')
+		assert int(open('/var/run/foo.pid').read().strip()) == os.getpid()
+		assert subprocess.check_output([sys.executable, '-c', "from univention.lib import locking; print(locking.get_lock('foo', %r))" % (nonblocking,)], shell=True) == b'False'
+		locking.release_lock(lock)
+	finally:
+		os.unlink('/var/run/foo.pid')
