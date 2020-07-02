a43ff6981ab53cf8584a03aaf5962d1213d7b7e7
Florian Best <best@univention.de>
2020-07-02 11:45:36 +0200

    USS Python 3: with_metaclass

--- base/univention-system-setup/umc/python/setup/netconf/__init__.py
+++ base/univention-system-setup/umc/python/setup/netconf/__init__.py
@@ -32,6 +32,7 @@ from abc import ABCMeta
 import logging
 import subprocess
 from univention.config_registry.interfaces import Interfaces
+from six import with_metaclass
 
 
 class ChangeSet(object):
@@ -93,12 +94,11 @@ class SkipPhase(Exception):
 	pass
 
 
-class Phase(object):
+class Phase(with_metaclass(ABCMeta, object)):
 
 	"""
 	Base-class for all phases.
 	"""
-	__metaclass__ = ABCMeta
 	priority = 0
 
 	def __init__(self, changeset):
