588c623b9e93769ae03601ec1e8ab294f4963b6b
Philipp Hahn <hahn@univention.de>
2020-07-08 18:01:33 +0200

    Bug #51973: Remove mobileclient
    git grep mobileclient -- ':!**/debian/changelog'

--- management/univention-directory-manager-modules/modules/univention/admin/handlers/policies/release.py
+++ management/univention-directory-manager-modules/modules/univention/admin/handlers/policies/release.py
@@ -58,7 +58,7 @@ module = 'policies/release'
 operations = ['add', 'edit', 'remove', 'search']
 
 policy_oc = 'univentionPolicyUpdate'
-policy_apply_to = ["computers/domaincontroller_master", "computers/domaincontroller_backup", "computers/domaincontroller_slave", "computers/memberserver", "computers/managedclient", "computers/mobileclient"]
+policy_apply_to = ["computers/domaincontroller_master", "computers/domaincontroller_backup", "computers/domaincontroller_slave", "computers/memberserver", "computers/managedclient"]
 policy_position_dn_prefix = "cn=update"
 
 childs = 0
