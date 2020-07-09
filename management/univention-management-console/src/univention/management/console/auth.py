8e92e22b44d8d902043deae53c1ce14758d0fe20
Florian Best <best@univention.de>
2020-07-09 09:42:23 +0200

    UMC-Server: enhance debug logging during authentication

--- management/univention-management-console/src/univention/management/console/auth.py
+++ management/univention-management-console/src/univention/management/console/auth.py
@@ -95,7 +95,6 @@ class AuthHandler(signals.Provider):
 
 		args = msg.body.copy()
 		locale = args.pop('locale', None)
-		args.pop('auth_type', None)
 		args.pop('pam', None)
 		args.setdefault('new_password', None)
 		args.setdefault('username', '')
@@ -105,8 +104,8 @@ class AuthHandler(signals.Provider):
 		thread = threads.Simple('pam', notifier.Callback(self.__authenticate_thread, pam, **args), notifier.Callback(self.__authentication_result, pam, msg))
 		thread.run()
 
-	def __authenticate_thread(self, pam, username, password, new_password, **custom_prompts):
-		AUTH.info('Trying to authenticate user %r' % (username,))
+	def __authenticate_thread(self, pam, username, password, new_password, auth_type=None, **custom_prompts):
+		AUTH.info('Trying to authenticate user %r (auth_type: %r)' % (username, auth_type))
 		username = self.__canonicalize_username(username)
 		try:
 			pam.authenticate(username, password, **custom_prompts)
