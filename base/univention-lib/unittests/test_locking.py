390300eff8d663959dd515e0752e5318f2a6b71b
Arvid Requate <requate@univention.de>
2020-07-13 12:13:06 +0200

    fixup! Bug #51593: add unit tests

--- base/univention-lib/unittests/test_locking.py
+++ base/univention-lib/unittests/test_locking.py
@@ -37,14 +37,13 @@ from .conftest import import_lib_module
 locking = import_lib_module('locking')
 
 
-@pytest.mark.skip
 @pytest.mark.parametrize('nonblocking', [True, False])
 def test_locking(nonblocking):
 	lock = locking.get_lock('foo', nonblocking)
 	try:
 		assert os.path.exists('/var/run/foo.pid')
 		assert int(open('/var/run/foo.pid').read().strip()) == os.getpid()
-		assert subprocess.check_output([sys.executable, '-c', "from univention.lib import locking; print(locking.get_lock('foo', %r))" % (nonblocking,)]) == b'False'
+		assert subprocess.check_output([sys.executable, '-c', "from univention.lib import locking; print(locking.get_lock('foo', %r))" % (nonblocking,)], shell=True) == b'False'
 		locking.release_lock(lock)
 	finally:
 		os.unlink('/var/run/foo.pid')
