63d139f3d3dd113154ba64c83466687c94ef2a5d
Johannes Keiser <keiser@univention.de>
2020-07-23 09:33:57 +0200

    Bug #51718: make the title of the login page configurable

--- management/univention-management-console/www/login/config.js
+++ management/univention-management-console/www/login/config.js
@@ -6,11 +6,24 @@ var umcConfig = {
 		"login",
 		"login/dialog",
 		"umc/tools",
+		"umc/i18n/tools",
+		"umc/json!/univention/login/meta.json",
 		"umc/i18n!login",
 		"dojo/NodeList-html"
 	],
-	callback: function(query, login, dialog, tools, _) {
-		query('h1').html(_('Login at %(domainname)s', tools.status()));
+	callback: function(query, login, dialog, tools, i18nTools, meta, _) {
+		var _getText = function(name, fallback) {
+			var loginTexts = meta.login_texts || {};
+			var locale = i18nTools.defaultLang().substring(0, 2);
+
+			var text = loginTexts[name + '/' + locale];
+			text = text || loginTexts[name];
+			text = text || fallback;
+			text = text || '';
+			return text;
+		};
+
+		query('h1').html(_getText('title', _('Login at %(domainname)s', tools.status())));
 		tools.status('username', getQuery('username') || tools.status('username'));
 		tools.status('password', getQuery('password'));
 		login.renderLoginDialog();
