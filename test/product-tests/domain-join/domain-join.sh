a905efd0791c82bcb53d17b00e273f8ac209cf7d
Christian Castens <castens@univention.de>
2020-08-05 15:51:05 +0200

    Bug #51163: new domain-join-tests

--- test/product-tests/domain-join/domain-join.sh
+++ test/product-tests/domain-join/domain-join.sh
@@ -1,9 +1,18 @@
 set -e
 set -x
 
+install_domain_join() {
+	local branch=${2:?missing branch}
+	if [ "$1" == "testing" ]; then
+		install_testing_version $branch
+	else
+		install_released_version
+	fi
+
+}
 install_testing_version () {
 	local branch=${1:?missing branch}
-	DEBIAN_FRONTEND=noninteractive apt install -y git curl build-essential dpkg-dev debhelper python3 dh-python python3-all python-setuptools
+	DEBIAN_FRONTEND=noninteractive apt install -y git curl build-essential dpkg-dev debhelper python3 dh-python python3-all python-setuptools expect
 	curl -k  https://billy.knut.univention.de/ucs-root-ca.crt > /usr/local/share/ca-certificates/billy.crt
 	update-ca-certificates -f
 	git clone https://git.knut.univention.de/univention/univention-domain-join.git
@@ -15,9 +24,23 @@ install_testing_version () {
 	DEBIAN_FRONTEND=noninteractive apt-get -y -f install
 }
 
-install_released_version_ubuntu () {
+
+install_released_version() {
 	add-apt-repository -y ppa:univention-dev/ppa
-	DEBIAN_FRONTEND=noninteractive apt-get install univention-domain-join univention-domain-join-cli
+	DEBIAN_FRONTEND=noninteractive apt-get install univention-domain-join univention-domain-join-cli expect
+}
+
+
+create_user () {
+	local user=${1:?missing username}
+	local lastname=${2:?missing lastname}
+	local password=${3:?missing password}
+
+	univention-directory-manager users/user create \
+	--position "cn=users,$(ucr get ldap/base)" \
+    --set username="$user" \
+    --set lastname="$lastname" \
+    --set password="$password"
 }
 
 test_univention_domain_join_cli () {
@@ -38,16 +61,55 @@ test_user () {
 	kinit --password-file=$password_file $username
 	ldapsearch uid=$username | grep "^dn: uid=$username"
 	sshpass -p "$password" ssh -o StrictHostKeyChecking=no "$username@$(hostname)" "whoami"
+	kdestroy
+}
+
+test_login () {
+	local user=${1:?missing user}
+	local password=${2:?missing password}
+	echo "Login as User: $user"
+	expect ~/product-tests/domain-join/login "$user" "$password"
+}
+
+test_change_password () {
+	local user=${1:?missing user}
+	local old_password=${2:?missing old password}
+	local new_password=${3:?missing new password}
+	echo "Changing password of User: $user"
+	expect ~/product-tests/domain-join/kpasswd "$user" "$old_password" "$new_password" 
+}
+
+test_home_directory () {
+	local user=${1:?missing user}
+	echo "Checking directory /home/$user/"
+	[ -d "/home/$user/" ] && echo "Directory /home/$user/ found"
+}
+
+test_login_with_old_pw () {
+	local user=${1:?missing user}
+	local password=${2:?missing password}
+	local password_file=$(mktemp)
+	echo -n $password > $password_file
+	! kinit --password-file=$password_file $user
 }
 
 run_tests () {
 	local run_tests=${1:?missing run tests parameter}
 	local dc_ip=${2:?missing dc ip}
 	local admin=${3:?missing admin account}
-	local password=${4:?missing admin account password}
-
+	local admin_password=${4:?missing admin account password}
+	local user=${5:?missing user account}
+	local user_lastname=${6:?missing user lastname}
+	local user_password=${7:?missing user password}
+	local new_user_password=${8:?missing new user password}
+	
 	if $run_tests; then
-		test_univention_domain_join_cli "$dc_ip" "$admin" "$password"
-		test_user "$admin" "$password"
+		test_univention_domain_join_cli "$dc_ip" "$admin" "$admin_password"
+		test_user "$admin" "$admin_password"
+		test_login "$user" "$user_password"
+		test_home_directory "$user"
+		test_change_password "$user" "$user_password" "$new_user_password"
+		test_login "$user" "$new_user_password"
+	   	test_login_with_old_pw "$user" "$user_password"
 	fi
 }
