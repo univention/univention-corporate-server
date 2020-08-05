35ee5070367846d3c28a1ddf9350ddac042534e4
Philipp Hahn <hahn@univention.de>
2020-08-05 16:30:39 +0200

    Bug #51588 up: Fix backing up files
    Preserve file permissions by using shutil.copy2()
    
    Use atomic os.rename() to rename backup file back; otherwise everything
    would be for void if the disk is full.

--- base/univention-updater/conffiles/apt-mirror.py
+++ base/univention-updater/conffiles/apt-mirror.py
@@ -28,25 +28,30 @@
 import os
 import shutil
 
-FILE_NAME = '/etc/apt/mirror.list'
+CUR = '/etc/apt/mirror.list'
+BAK = CUR + '.old'
 
 
-def preinst(baseConfig, changes):
-    if os.path.exists('%s.old' % FILE_NAME):
-        os.remove('%s.old' % FILE_NAME)
-    if os.path.exists(FILE_NAME):
-        shutil.copyfile('%s' % FILE_NAME, '%s.old' % FILE_NAME)
-    if 'local/repository' in set(changes):
+def preinst(ucr, changes):
+    if os.path.exists(BAK):
+        os.remove(BAK)
+
+    if os.path.exists(CUR):
+        shutil.copy2(CUR, BAK)
+
+    if 'local/repository' in changes:
         """ Immediately resolve pending policy changes if local/repository is changed (Bug #16646) """
         os.system('/usr/lib/univention-directory-policy/univention-policy-set-repository-server >>/var/log/univention/repository.log')
 
 
-def postinst(baseConfig, changes):
-    if os.path.exists(FILE_NAME):
-        res = open(FILE_NAME, 'r').readlines()
-        if len(res) <= 1:
-            os.remove(FILE_NAME)
-            if os.path.exists('%s.old' % FILE_NAME):
-                shutil.copyfile('%s.old' % FILE_NAME, '%s' % FILE_NAME)
-        if os.path.exists('%s.old' % FILE_NAME):
-            os.remove('%s.old' % FILE_NAME)
+def postinst(ucr, changes):
+    if not os.path.exists(CUR):
+        return
+
+    res = open(CUR, 'r').readlines()
+    if len(res) <= 1:
+        if os.path.exists(BAK):
+            os.rename(BAK, CUR)
+
+    if os.path.exists(BAK):
+        os.remove(BAK)
