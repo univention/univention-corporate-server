1705175bd8fd81f509fe066746c14ba5704d1d27
Dirk Wiesenthal <wiesenthal@univention.de>
2020-08-10 12:05:18 +0200

    Bug #51566: WIP: Unittests for UMC module

---
+++ management/univention-management-console-module-lib/unittests/test_umc.py
@@ -0,0 +1,117 @@
+#!/usr/bin/py.test
+# -*- coding: utf-8 -*-
+# Copyright 2020 Univention GmbH
+#
+# https://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <https://www.gnu.org/licenses/>.
+
+from univention.management.console.error import ServerError
+
+from univention.unittests.umc import import_umc_module, umc_requests
+
+import pytest
+import six
+
+umc_lib = import_umc_module('lib')
+
+
+class TestUMCModule(object):
+	def test_message_sanitizer(self):
+		sanitizer = umc_lib.server.MessageSanitizer(default='')
+		assert sanitizer.sanitize('message', {}) == ''
+		assert sanitizer.sanitize('message', {"message": "Echt gut"}) == "Echt gut"
+		if six.PY2:
+			assert sanitizer.sanitize('message', {"message": u"Ächt gut"}) == b"\xc3\x84cht gut"
+
+	def test_restart_isNeeded(self, instance, umc_request):
+		instance.restart_isNeeded(umc_request)
+		umc_request.expected_response(True)
+
+	def test_ping(self, instance, umc_request):
+		instance.ping(umc_request)
+		umc_request.expected_response({'success': True})
+
+	def test_restart(self, instance, mocker, umc_request):
+		mocked_subprocess = mocker.patch.object(umc_lib.server, 'subprocess')
+		out = b'''[ ok ] Restarting univention-management-console-server (via systemctl): univention-management-console-server.service.
+		[ ok ] Restarting univention-management-console-web-server (via systemctl): univention-management-console-web-server.service.
+		[ ok ] Restarting apache2 (via systemctl): apache2.service.
+		'''
+		popen_mock = mocker.Mock(**{'communicate.return_value': (out, '')})
+		mocked_subprocess.PIPE = -1
+		mocked_subprocess.STDOUT = 1
+		mocked_subprocess.Popen.return_value = popen_mock
+		instance.restart(umc_request)
+		mocked_subprocess.call.assert_called_once_with('/usr/share/univention-updater/disable-apache2-umc')
+		mocked_subprocess.Popen.assert_called_once_with('/usr/share/univention-updater/enable-apache2-umc', stderr=1, stdout=-1)
+		umc_request.expected_response(True)
+
+	@umc_requests([{}, {"message": "my message"}])
+	def test_shutdown(self, instance, mocker, umc_request):
+		mocked_subprocess = mocker.patch.object(umc_lib.server, 'subprocess')
+		mocked_subprocess.call.side_effect = [0, 0]
+		if umc_request.options.get("message"):
+			message = umc_request.options.get("message")
+			reason = 'The system will now be shut down ({})'.format(message)
+		else:
+			reason = 'The system will now be shut down'
+		instance.shutdown(umc_request)
+		assert mocked_subprocess.call.call_count == 2
+		args, kwargs = mocked_subprocess.call.call_args_list[0]
+		assert args[0] == ('/usr/bin/logger', '-f', '/var/log/syslog', '-t', 'UMC', reason)
+		args, kwargs = mocked_subprocess.call.call_args_list[1]
+		assert args[0] == ('/sbin/shutdown', '-h', 'now', reason)
+		umc_request.expected_response(None)
+
+	def test_failed_shutdown_failing(self, instance, mocker, umc_request):
+		mocked_subprocess = mocker.patch.object(umc_lib.server, 'subprocess')
+		mocked_subprocess.call.side_effect = [OSError, 1]
+		with pytest.raises(ServerError):
+			instance.shutdown(umc_request)
+		assert mocked_subprocess.call.call_count == 2
+
+	@umc_requests([{}, {"message": "my message"}])
+	def test_reboot(self, instance, mocker, umc_request):
+		mocked_subprocess = mocker.patch.object(umc_lib.server, 'subprocess')
+		mocked_subprocess.call.side_effect = [0, 0]
+		if umc_request.options.get("message"):
+			message = umc_request.options.get("message")
+			reason = 'The system will now be restarted ({})'.format(message)
+		else:
+			reason = 'The system will now be restarted'
+		instance.reboot(umc_request)
+		assert mocked_subprocess.call.call_count == 2
+		args, kwargs = mocked_subprocess.call.call_args_list[0]
+		assert args[0] == ('/usr/bin/logger', '-f', '/var/log/syslog', '-t', 'UMC', reason)
+		args, kwargs = mocked_subprocess.call.call_args_list[1]
+		assert args[0] == ('/sbin/shutdown', '-r', 'now', reason)
+		umc_request.expected_response(None)
+
+	def test_failed_reboot_failing(self, instance, mocker, umc_request):
+		mocked_subprocess = mocker.patch.object(umc_lib.server, 'subprocess')
+		mocked_subprocess.call.side_effect = [OSError, 1]
+		with pytest.raises(ServerError):
+			instance.reboot(umc_request)
+		assert mocked_subprocess.call.call_count == 2
---
+++ management/univention-management-console-module-lib/unittests/units.py
@@ -0,0 +1,108 @@
+#!/usr/bin/py.test
+# -*- coding: utf-8 -*-
+# Copyright 2020 Univention GmbH
+#
+# https://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <https://www.gnu.org/licenses/>.
+
+import sys
+import importlib
+
+import pytest
+
+
+class UMCTestRequest(object):
+	_requests = {}
+
+	def __init__(self, options):
+		self.id = id(self)
+		self.options = options
+		self._requests[self.id] = self
+
+	def save_result(self, result):
+		if hasattr(self, 'result'):
+			raise RuntimeError('Already saved result {!r}. Cannot be called twice ({!r})'.format(self.result, result))
+		self.result = result
+
+	def expected_response(self, result):
+		if not hasattr(self, 'result'):
+			raise RuntimeError('No result for this request.')
+		assert self.result == result
+		del self._requests[self.id]
+
+	def __repr__(self):
+		return '<UMCTestRequest id={!r} options={!r}>'.format(self.id, self.options)
+
+
+def save_result_on_request(request_id, result, *args, **kwargs):
+	umc_request = UMCTestRequest._requests[request_id]
+	umc_request.save_result(result)
+
+
+@pytest.fixture
+def umc_request(request):
+	if hasattr(request, "param"):
+		return UMCTestRequest(request.param)
+	else:
+		return UMCTestRequest({})
+
+
+@pytest.fixture
+def instance(umc_module_class, mocker):
+	mocker.patch.object(umc_module_class, 'finished', side_effect=save_result_on_request)
+	mod = umc_module_class()
+	mod.init()
+	return mod
+
+
+@pytest.fixture(scope='session')
+def umc_module_class():
+	return umc_module_class.umc_module.Instance
+
+
+def pytest_addoption(parser):
+	parser.addoption("--installed-umc", action="store_true", help="Test against installed UMC module (not src)")
+
+
+def umc_requests(params):
+	return pytest.mark.parametrize('umc_request', params, indirect=['umc_request'])
+
+
+def import_umc_module(module_id, umc_src_path=None):
+	umc_module_python_path = 'univention.management.console.modules.{}'.format(module_id)
+	if pytest.config.getoption('--installed-umc'):
+		module_name = umc_module_python_path
+		# print('Testing against installed {}'.format(module_name))
+	else:
+		if umc_src_path is None:
+			umc_src_path = 'umc/python/'
+		if umc_src_path not in sys.path:
+			sys.path.insert(1, umc_src_path)
+		module_name = module_id
+		# print('Testing against src {}'.format(module_name))
+	module = importlib.import_module(module_name)
+	umc_module_class.umc_module = module
+	sys.modules[umc_module_python_path] = module
+	return module
