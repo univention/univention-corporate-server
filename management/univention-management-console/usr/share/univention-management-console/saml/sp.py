f7ed333486f544e19842edfa89ad906ef39d0335
Florian Best <best@univention.de>
2020-08-10 12:30:37 +0200

    Bug #52297: explicitly add all possible configuration variables to SAML config

--- management/univention-management-console/usr/share/univention-management-console/saml/sp.py
+++ management/univention-management-console/usr/share/univention-management-console/saml/sp.py
@@ -7,8 +7,8 @@ from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from cryptography.hazmat.primitives.serialization import Encoding
 from cryptography.hazmat.backends import default_backend
 
-from saml2 import BINDING_HTTP_REDIRECT, BINDING_HTTP_POST
-from saml2.saml import NAME_FORMAT_URI
+from saml2 import SamlBase, BINDING_HTTP_REDIRECT, BINDING_HTTP_POST
+from saml2.saml import NAME_FORMAT_URI, NAMEID_FORMAT_TRANSIENT, NAMEID_FORMAT_PERSISTENT
 import saml2.attributemaps
 
 from univention.config_registry.interfaces import Interfaces
@@ -61,18 +61,21 @@ bases = ['%s://%s/univention/saml' % (scheme, addr) for addr in addresses for sc
 CONFIG = {
 	"entityid": "https://%s/univention/saml/metadata" % (fqdn,),
 	"name_form": NAME_FORMAT_URI,
-	"description": "Univention Management Console SAML2.0 Service Provider",
+	"description": "Univention Management Console at %s" % (fqdn,),
 	"service": {
 		"sp": {
 			"allow_unsolicited": True,
 			"want_assertions_signed": True,
+			"want_response_signed": True,
 			"authn_requests_signed": True,
 			"logout_requests_signed": True,
 			"endpoints": {
 				"assertion_consumer_service": [('%s/' % (url,), binding) for url in bases for binding in (BINDING_HTTP_POST,)],
 				"single_logout_service": [('%s/slo/' % (url,), binding) for url in bases for binding in (BINDING_HTTP_POST, BINDING_HTTP_REDIRECT)],
 			},
-			"required_attributes": ["uid"],
+			"name_id_format": [NAMEID_FORMAT_TRANSIENT, NAMEID_FORMAT_PERSISTENT],
+			"required_attributes": [x.strip() for x in ucr.get('umc/saml/required_attributes', 'uid').split(',') if x.strip()],
+			"optional_attributes": [x.strip() for x in ucr.get('umc/saml/optional_attributes', '').split(',') if x.strip()]
 		},
 	},
 	"attribute_map_dir": os.path.dirname(saml2.attributemaps.__file__),
@@ -82,10 +85,42 @@ CONFIG = {
 	"metadata": {
 		"local": glob.glob('/usr/share/univention-management-console/saml/idp/*.xml'),
 	},
-	# TODO: add contact_person?
+	"debug": ucr.is_true('umc/saml/debug', False),
+	"contact_person": [
+		{
+			"givenname": ucr.get('umc/saml/contact-person/%s/givenname' % (type_,), ''),
+			"surname": ucr.get('umc/saml/contact-person/%s/surname' % (type_,), ''),
+			"company": ucr.get('umc/saml/contact-person/%s/company' % (type_,), ''),
+			"mail": [x.strip() for x in ucr.get('umc/saml/contact-person/%s/mail' % (type_,), '').split(',') if x.strip()],
+			"type": type_,
+		} for type_ in ('technical', 'administrative') if ucr.get('umc/saml/contact-person/%s/mail' % (type_,))
+	],
+	"organization": {
+		"name": [
+			(ucr.get('umc/saml/organization/name', 'Univention Management Console %s' % (fqdn,)), "en"),
+		],
+		"display_name": [ucr.get('umc/saml/organization/display-name', 'Univention Management Console %s' % (fqdn,))],
+		"url": [
+			(ucr.get('umc/saml/organization/url', 'https://%s/univention/management/' % (fqdn,)), "en"),
+		],
+	},
 }
 
 tmpfile = NamedTemporaryFile()  # workaround for broken PEM parsing in pysaml2
 tmpfile.write(get_cert())
 tmpfile.flush()
 CONFIG['cert_file'] = tmpfile.name
+
+OASIS_DEFAULT_NS_PREFIXES = {
+	'ds': 'http://www.w3.org/2000/09/xmldsig#',
+	'md': 'urn:oasis:names:tc:SAML:2.0:metadata',
+	'mdui': 'urn:oasis:names:tc:SAML:metadata:ui',
+	'saml': 'urn:oasis:names:tc:SAML:2.0:assertion',
+	'samlp': 'urn:oasis:names:tc:SAML:2.0:protocol',
+	'xenc': 'http://www.w3.org/2001/04/xmlenc#',
+	'xs': 'http://www.w3.org/2001/XMLSchema',
+	'xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+	'alg': "urn:oasis:names:tc:SAML:metadata:algsupport",
+	'mdattr': "urn:oasis:names:tc:SAML:metadata:attribute"
+}
+SamlBase().register_prefix(OASIS_DEFAULT_NS_PREFIXES)
