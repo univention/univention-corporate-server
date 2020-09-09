8520c2e039f201bb317824366ee2998b0a528111
Florian Best <best@univention.de>
2020-09-09 10:31:14 +0200

    Bug #51656: rename univention.translationhelper into univention-l10n

--- packaging/univention-l10n/univention/l10n/__init__.py
+++ packaging/univention-l10n/univention/l10n/__init__.py
@@ -27,4 +27,5 @@
 # License with the Debian GNU/Linux or Univention distribution in file
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
-from . translationhelper import *  # noqa
+
+from . l10n import *  # noqa
