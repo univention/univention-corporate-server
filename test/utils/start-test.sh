16c5a84fb735ebc66219137e40dbc8f38075c8b3
Erik Damrose <damrose@univention.de>
2020-09-22 09:59:54 +0200

    Bug #52032: 4.4-6 kvm image

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -6,7 +6,7 @@
 # defaults for release
 release='4.4-6'
 old_release='4.3-5'
-kvm_template_version='4.4-5+e652'
+kvm_template_version='4.4-6+e750'
 # AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 8 - ami-0d544b4cebfcb5f48
 current_ami=ami-0d544b4cebfcb5f48
 # AMI: Univention Corporate Server (UCS) 4.3 (official image) rev. 6 - ami-0dd235a67a0eb9bdc
