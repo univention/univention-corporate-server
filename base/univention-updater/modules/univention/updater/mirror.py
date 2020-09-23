univention/univention-corporate-server

commit 8f8ee41b55eeb2a8ea4f6b71cc9bae98c92ae5e4

Bug #51588: Filter releases.json for mirror
Copying and subsequent filtering the releases.json from the upstream
repository instead of recreating it, allows us to add new keys to it
without needing to modify the mirror code.

base/univention-updater/modules/univention/updater/mirror.py
@@ -38,10 +38,9 @@ import subprocess
 import itertools
 import logging
 import json
-from itertools import groupby
 from operator import itemgetter
 
-from .tools import UniventionUpdater
+from .tools import UniventionUpdater, UCSHttpServer
 from .repo_url import UcsRepoUrl
 from univention.lib.ucs import UCS_Version
 try:
@@ -68,27 +67,52 @@ def makedirs(dirname, mode=0o755):
             raise
 
 
-def gen_releases(releases):  # type: (Iterator[Tuple[int, int, int]]) -> str
-    """Generate a releases.json string from a list of given releases"""
-    data = dict(
-        releases=[
-            dict(
-                major=major,
-                minors=[
-                    dict(
-                        minor=minor,
-                        patchlevels=[
-                            dict(
-                                patchlevel=patchlevel,
-                                status="maintained",  # FIXME: development | end-of-live
-                            ) for major, minor, patchlevel in patchelevels
-                        ]
-                    ) for minor, patchelevels in groupby(minors, key=itemgetter(1))
+def filter_releases_json(releases, start=None, end=None):
+    # type: (str, Optional[UCS_Version], Optional[UCS_Version]) -> None
+    """
+    Filter releases that are not mirrored from the upstream repository
+    Filtering is done inplace!
+
+    :param str releases: The upstream releases object from releases.json.
+    :param UCS_Version start: First UCS version that is being mirrored.
+    :param UCS_Version end: Last UCS version that is being mirrored.
+    """
+
+    max_minor = 99
+    max_patchlevel = 99
+
+    def _include_version(major, minor=None, patchlevel=None):
+        bigger_as_start = start is None or start <= UCS_Version((
+            major,
+            max_minor if minor is None else minor,
+            max_patchlevel if patchlevel is None else patchlevel
+        ))
+        smaller_as_end = end is None or end >= UCS_Version((
+            major,
+            0 if minor is None else minor,
+            0 if patchlevel is None else patchlevel
+        ))
+        return bigger_as_start and smaller_as_end
+
+    def _filter_majors():
+        releases['releases'] = [major for major in releases['releases'] if _include_version(major['major'])]
+
+    def _filter_minors():
+        for major in releases['releases']:
+            major['minors'] = [minor for minor in major['minors'] if _include_version(major['major'], minor['minor'])]
+
+    def _filter_patchlevels():
+        for major in releases['releases']:
+            for minor in major['minors']:
+                minor['patchlevels'] = [
+                    patchlevel for patchlevel in minor['patchlevels']
+                    if _include_version(major['major'], minor['minor'], patchlevel['patchlevel'])
                 ]
-            ) for major, minors in groupby(releases, key=itemgetter(0))
-        ]
-    )
-    return json.dumps(data)
+
+    _filter_majors()
+    _filter_minors()
+    _filter_patchlevels()
+    return None
 
 
 class UniventionMirror(UniventionUpdater):
@@ -237,13 +261,24 @@ class UniventionMirror(UniventionUpdater):
         return result
 
     def write_releases_json(self):
+        """
+        Write a releases.json including only the mirrored releases.
+        """
         start = self.version_start
         end = self.version_end
+        _code, _size, data = self.server.access(None, 'releases.json', get=True)
+        try:
+            releases = json.loads(data)
+        except ValueError as exc:
+            ud.debug(ud.NETWORK, ud.ERROR, 'Querying maintenance information failed: %s' % (exc,))
+            if isinstance(self.server, UCSHttpServer) and self.server.proxy_handler.proxies:
+                ud.debug(ud.NETWORK, ud.WARN, 'Maintenance information malformed, blocked by proxy?')
+            raise
+        filter_releases_json(releases, start=start, end=end)
         releases_json_path = os.path.join(self.repository_path, 'mirror', 'releases.json')
         makedirs(os.path.dirname(releases_json_path))
-        releases = sorted(r for r in self.get_releases(self.server) if r >= start and r <= end)
         with open(releases_json_path, 'w') as releases_json:
-            releases_json.write(gen_releases((r['major'], r['minor'], r['patchlevel']) for r in releases))
+            json.dump(releases, releases_json)
 
     def run(self):
         """

base/univention-updater/tests/mockups.py
@@ -8,6 +8,9 @@ import sys
 import os.path
 import errno
 import httplib
+import json
+from itertools import groupby
+from operator import itemgetter
 import univention
 univention.__path__.insert(0, os.path.abspath('modules/univention'))  # type: ignore
 import univention.updater.tools as U  # noqa: E402
@@ -175,6 +178,29 @@ class MockFile(object):
             M.makedirs(dirname)
             filename = os.path.join(dirname, tail)
             return MockFile._ORIG(filename, mode, *args, **kwargs)
+
+
+def gen_releases(releases):  # type: (Iterator[Tuple[int, int, int]]) -> str
+    """Generate a releases.json string from a list of given releases"""
+    data = dict(
+        releases=[
+            dict(
+                major=major,
+                minors=[
+                    dict(
+                        minor=minor,
+                        patchlevels=[
+                            dict(
+                                patchlevel=patchlevel,
+                                status="maintained",
+                            ) for major, minor, patchlevel in patchelevels
+                        ]
+                    ) for minor, patchelevels in groupby(minors, key=itemgetter(1))
+                ]
+            ) for major, minors in groupby(releases, key=itemgetter(0))
+        ]
+    )
+    return json.dumps(data)
 
 
 def verbose(verbose_mode=True):

base/univention-updater/tests/test_mirror.py
@@ -6,16 +6,19 @@ import os
 import unittest
 from tempfile import mkdtemp
 from shutil import rmtree
+import json
+from copy import deepcopy
+from univention.lib.ucs import UCS_Version
 from mockups import (
     U, M, MAJOR, MINOR, PATCH, ARCH,
     MockFile, MockConfigRegistry, MockUCSHttpServer, MockPopen,
+    gen_releases,
 )
 
 UM = M.UniventionMirror
 DATA = 'x' * U.MIN_GZIP
 
 
-@unittest.skip("Not yet implemented for the new pool structure")
 class TestUniventionMirror(unittest.TestCase):
 
     """Unit test for univention.updater.tools"""
@@ -114,7 +117,7 @@ class TestUniventionMirror(unittest.TestCase):
             '%d.%d/maintained/component/%s/Packages.gz' % (MAJOR, 0, 'b'): DATA,
             '%d.%d/maintained/component/%s/preup.sh' % (MAJOR, 0, 'b'): '#!b_pre',
             '%d.%d/maintained/component/%s/postup.sh' % (MAJOR, 0, 'b'): '#!b_post',
-            'releases.json': M.gen_releases([(MAJOR, MINOR, 0), ])
+            'releases.json': gen_releases([(MAJOR, MINOR, 0), ])
         }
         self._uri(uris)
         del uris['releases.json']
@@ -134,12 +137,22 @@ class TestUniventionMirror(unittest.TestCase):
             self.assertEqual(script, value)
 
     def test_write_releases_json(self):
-        self._uri({'releases.json': M.gen_releases([(MAJOR, MINOR, 0), (MAJOR, MINOR, 1)])})
-        expected = '{"releases": [{"major": 3, "minors": [{"patchlevels": [{"patchlevel": 0}, {"patchlevel": 1}], "minor": 0}]}]}'
+        self._uri({'releases.json': gen_releases([(MAJOR, MINOR, 0), (MAJOR, MINOR, 1)])})
+        expected = '{"releases": [{"major": 3, "minors": [{"patchlevels": [{"status": "maintained", "patchlevel": 0}, {"status": "maintained", "patchlevel": 1}], "minor": 0}]}]}'
         self.m.write_releases_json()
         releases_json_path = os.path.join(self.base_dir, 'mock', self.m.repository_path.lstrip('/'), 'mirror', 'releases.json')
         with open(releases_json_path, 'r') as releases_json:
             self.assertEqual(expected, releases_json.read())
+
+    def test_filter_releases_json(self):
+        self.maxDiff = None
+        with open(os.path.join(os.path.dirname(__file__), 'mockup_upstream_releases.json'), 'r') as upstream_releases_fp:
+            upstream_releases = json.load(upstream_releases_fp)
+        with open(os.path.join(os.path.dirname(__file__), 'mockup_mirror_releases.json'), 'r') as mirror_releases_fp:
+            expected_mirror_releases = json.load(mirror_releases_fp)
+        mirrored_releases = deepcopy(upstream_releases)
+        M.filter_releases_json(mirrored_releases, start=UCS_Version((3, 1, 1)), end=UCS_Version((4, 1, 0)))
+        self.assertEqual(mirrored_releases, expected_mirror_releases)
 
 
     def test_run(self):

base/univention-updater/tests/test_updater.py
@@ -5,8 +5,9 @@
 import unittest
 from tempfile import NamedTemporaryFile
 from mockups import (
-    U, M, MAJOR, MINOR, PATCH, ARCH, ERRAT, PART,
+    U, MAJOR, MINOR, PATCH, ARCH, ERRAT, PART,
     MockConfigRegistry, MockUCSHttpServer, MockPopen,
+    gen_releases,
 )
 
 UU = U.UniventionUpdater
@@ -75,7 +76,7 @@ class TestUniventionUpdater(unittest.TestCase):
     def test_get_next_version_PATCH(self):
         """Test next patch version."""
         self._uri({
-            'releases.json': M.gen_releases([(MAJOR, MINOR, PATCH), (MAJOR, MINOR, PATCH + 1)])
+            'releases.json': gen_releases([(MAJOR, MINOR, PATCH), (MAJOR, MINOR, PATCH + 1)])
         })
         ver = self.u.get_next_version(version=U.UCS_Version((MAJOR, MINOR, PATCH)))
         self.assertEqual('%d.%d-%d' % (MAJOR, MINOR, PATCH + 1), ver)
@@ -83,7 +84,7 @@ class TestUniventionUpdater(unittest.TestCase):
     def test_get_next_version_PATCH99(self):
         """Test next patch version after 99."""
         self._uri({
-            'releases.json': M.gen_releases([(MAJOR, MINOR + 1, 0), (MAJOR, MINOR, 100)])
+            'releases.json': gen_releases([(MAJOR, MINOR + 1, 0), (MAJOR, MINOR, 100)])
         })
         ver = self.u.get_next_version(version=U.UCS_Version((MAJOR, MINOR, 99)))
         self.assertEqual('%d.%d-%d' % (MAJOR, MINOR + 1, 0), ver)
@@ -91,7 +92,7 @@ class TestUniventionUpdater(unittest.TestCase):
     def test_get_next_version_MINOR(self):
         """Test next minor version."""
         self._uri({
-            'releases.json': M.gen_releases([(MAJOR, MINOR, PATCH), (MAJOR, MINOR + 1, 0)])
+            'releases.json': gen_releases([(MAJOR, MINOR, PATCH), (MAJOR, MINOR + 1, 0)])
         })
         ver = self.u.get_next_version(version=U.UCS_Version((MAJOR, MINOR, PATCH)))
         self.assertEqual('%d.%d-%d' % (MAJOR, MINOR + 1, 0), ver)
@@ -99,7 +100,7 @@ class TestUniventionUpdater(unittest.TestCase):
     def test_get_next_version_MINOR99(self):
         """Test next minor version after 99."""
         self._uri({
-            'releases.json': M.gen_releases([(MAJOR + 1, 0, 0), (MAJOR, 100, 0)])
+            'releases.json': gen_releases([(MAJOR + 1, 0, 0), (MAJOR, 100, 0)])
         })
         ver = self.u.get_next_version(version=U.UCS_Version((MAJOR, 99, 0)))
         self.assertEqual('%d.%d-%d' % (MAJOR + 1, 0, 0), ver)
@@ -107,7 +108,7 @@ class TestUniventionUpdater(unittest.TestCase):
     def test_get_next_version_MAJOR(self):
         """Test next major version."""
         self._uri({
-            'releases.json': M.gen_releases([(MAJOR, MINOR, PATCH), (MAJOR + 1, 0, 0)])
+            'releases.json': gen_releases([(MAJOR, MINOR, PATCH), (MAJOR + 1, 0, 0)])
         })
         ver = self.u.get_next_version(version=U.UCS_Version((MAJOR, MINOR, PATCH)))
         self.assertEqual('%d.%d-%d' % (MAJOR + 1, 0, 0), ver)
@@ -115,7 +116,7 @@ class TestUniventionUpdater(unittest.TestCase):
     def test_get_next_version_MAJOR99(self):
         """Test next major version after 99."""
         self._uri({
-            'releases.json': M.gen_releases([(99, 0, 0), (100, 0, 0)])
+            'releases.json': gen_releases([(99, 0, 0), (100, 0, 0)])
         })
         ver = self.u.get_next_version(version=U.UCS_Version((99, MINOR, PATCH)))
         self.assertEqual(None, ver)
@@ -127,7 +128,7 @@ class TestUniventionUpdater(unittest.TestCase):
             'repository/online/component/a/version': 'current',
         })
         self._uri({
-            'releases.json': M.gen_releases([(MAJOR, MINOR + 1, 0), (MAJOR + 1, 0, 0)])
+            'releases.json': gen_releases([(MAJOR, MINOR + 1, 0), (MAJOR + 1, 0, 0)])
         })
         self._uri({
             '%d.%d/maintained/component/%s/all/Packages.gz' % (MAJOR, MINOR + 1, 'a'): DATA,
@@ -139,7 +140,7 @@ class TestUniventionUpdater(unittest.TestCase):
     def test_release_update_available_NO(self):
         """Test no update available."""
         self._uri({
-            'releases.json': M.gen_releases([(MAJOR, MINOR, PATCH), (MAJOR - 1, 0, 0)])
+            'releases.json': gen_releases([(MAJOR, MINOR, PATCH), (MAJOR - 1, 0, 0)])
         })
         next = self.u.release_update_available()
         self.assertEqual(None, next)
@@ -148,7 +149,7 @@ class TestUniventionUpdater(unittest.TestCase):
         """Test next patch-level update."""
         NEXT = '%d.%d-%d' % (MAJOR, MINOR, PATCH + 1)
         self._uri({
-            'releases.json': M.gen_releases([(MAJOR, MINOR, PATCH), (MAJOR, MINOR, PATCH + 1)])
+            'releases.json': gen_releases([(MAJOR, MINOR, PATCH), (MAJOR, MINOR, PATCH + 1)])
         })
         next = self.u.release_update_available()
         self.assertEqual(NEXT, next)
@@ -157,7 +158,7 @@ class TestUniventionUpdater(unittest.TestCase):
         """Test next minor update."""
         NEXT = '%d.%d-%d' % (MAJOR, MINOR + 1, 0)
         self._uri({
-            'releases.json': M.gen_releases([(MAJOR, MINOR, PATCH), (MAJOR, MINOR + 1, 0)])
+            'releases.json': gen_releases([(MAJOR, MINOR, PATCH), (MAJOR, MINOR + 1, 0)])
         })
         next = self.u.release_update_available()
         self.assertEqual(NEXT, next)
@@ -166,7 +167,7 @@ class TestUniventionUpdater(unittest.TestCase):
         """Test next major update."""
         NEXT = '%d.%d-%d' % (MAJOR + 1, 0, 0)
         self._uri({
-            'releases.json': M.gen_releases([(MAJOR, MINOR, PATCH), (MAJOR + 1, 0, 0)])
+            'releases.json': gen_releases([(MAJOR, MINOR, PATCH), (MAJOR + 1, 0, 0)])
         })
         next = self.u.release_update_available()
         self.assertEqual(NEXT, next)
@@ -178,7 +179,7 @@ class TestUniventionUpdater(unittest.TestCase):
             'repository/online/component/a/version': 'current',
         })
         self._uri({
-            'releases.json': M.gen_releases([(MAJOR, MINOR, PATCH), (MAJOR, MINOR + 1, 0)])
+            'releases.json': gen_releases([(MAJOR, MINOR, PATCH), (MAJOR, MINOR + 1, 0)])
         })
         self.assertRaises(U.RequiredComponentError, self.u.release_update_available, errorsto='exception')
 
@@ -605,7 +606,7 @@ class TestUniventionUpdater(unittest.TestCase):
         self._ucr({'repository/online/component/a/version': 'current'})
         self._uri({
             '%d.%d/maintained/%d.%d-%d/' % (MAJOR, MINOR, MAJOR, MINOR, PATCH): '',
-            'releases.json': M.gen_releases([(MAJOR, MINOR, PATCH)]),
+            'releases.json': gen_releases([(MAJOR, MINOR, PATCH)]),
         })
         ver = U.UCS_Version((MAJOR, MINOR, PATCH))  # component.erratalevel!
         comp_ver = self.u._get_component_versions('a', start=ver, end=ver)
@@ -616,7 +617,7 @@ class TestUniventionUpdater(unittest.TestCase):
         self._ucr({'repository/online/component/a/version': ''})
         self._uri({
             '%d.%d/maintained/%d.%d-%d/' % (MAJOR, MINOR, MAJOR, MINOR, PATCH): '',
-            'releases.json': M.gen_releases([(MAJOR, MINOR, PATCH)]),
+            'releases.json': gen_releases([(MAJOR, MINOR, PATCH)]),
         })
         ver = U.UCS_Version((MAJOR, MINOR, PATCH))  # component.erratalevel!
         comp_ver = self.u._get_component_versions('a', start=ver, end=ver)
@@ -655,7 +656,7 @@ class TestUniventionUpdater(unittest.TestCase):
     def test__releases_in_range_current(self):
         """Test getting releases in range."""
         self._uri({
-            'releases.json': M.gen_releases([(MAJOR, MINOR, PATCH)]),
+            'releases.json': gen_releases([(MAJOR, MINOR, PATCH)]),
         })
         ver = U.UCS_Version((MAJOR, MINOR, PATCH))
         versions = self.u._releases_in_range()
@@ -664,7 +665,7 @@ class TestUniventionUpdater(unittest.TestCase):
     def test__releases_in_range_multi(self):
         """Test getting multiple releases in range."""
         self._uri({
-            'releases.json': M.gen_releases([(MAJOR, 0, 0), (MAJOR, 1, 0)]),
+            'releases.json': gen_releases([(MAJOR, 0, 0), (MAJOR, 1, 0)]),
         })
         start = U.UCS_Version((MAJOR, 0, 0))
         end = U.UCS_Version((MAJOR, 1, 0))
@@ -674,7 +675,7 @@ class TestUniventionUpdater(unittest.TestCase):
     def test__releases_in_range_first(self):
         """Test getting first releases in range."""
         self._uri({
-            'releases.json': M.gen_releases([(MAJOR, 1, 0)]),
+            'releases.json': gen_releases([(MAJOR, 1, 0)]),
         })
         start = U.UCS_Version((MAJOR, 0, 0))
         end = U.UCS_Version((MAJOR, 1, 0))
@@ -689,7 +690,7 @@ class TestUniventionUpdater(unittest.TestCase):
         self._uri({
             '%d.%d/maintained/component/%s/%s/Packages.gz' % (MAJOR, MINOR, 'a', 'all'): DATA,
             '%d.%d/maintained/component/%s/%s/Packages.gz' % (MAJOR, MINOR, 'a', ARCH): DATA,
-            'releases.json': M.gen_releases([(MAJOR, MINOR, PATCH)]),
+            'releases.json': gen_releases([(MAJOR, MINOR, PATCH)]),
         })
         tmp = self.u.print_component_repositories()
         self.assertEqual(set((

commit f0f4a02c5951d62ca6021eea8579baa65ad51b77

fixup! Bug #51588: Filter releases.json for mirror
Fixes: 8f8ee41b55e

base/univention-updater/modules/univention/updater/mirror.py
@@ -48,7 +48,7 @@ try:
 except ImportError:
     import univention.debug2 as ud
 try:
-    from typing import Iterator, Tuple  # noqa F401
+    from typing import Iterator, Optional, Tuple  # noqa F401
 except ImportError:
     pass
 