univention/univention-corporate-server

commit 8d35eb4f4abd2812c0dd3be6d2a81c7c3a18dafb

Bug #51588 up: Also mirror main/debian-installer
which is required for PXE network installations.

base/univention-updater/modules/univention/updater/tools.py
@@ -257,8 +257,8 @@ class UCSRepoPool5(_UCSRepo):
         """
         return "{1}{0.major}{0.minor}{0.patchlevel}".format(self, "errata" if self.errata else "ucs")
 
-    def deb(self, server, type="deb"):
-        # type: (_UCSServer, str) -> str
+    def deb(self, server, type="deb", mirror=False):
+        # type: (_UCSServer, str, bool) -> str
         """
         Format for :file:`/etc/apt/sources.list`.
 
@@ -270,14 +270,17 @@ class UCSRepoPool5(_UCSRepo):
         >>> r=UCSRepoPool5(major=5, minor=1, patchlevel=0)
         >>> r.deb('https://updates.software-univention.de/')
         'deb https://updates.software-univention.de/ ucs510 main'
+        >>> r.deb('https://updates.software-univention.de/', mirror=True)
+        'deb https://updates.software-univention.de/ ucs510 main main/debian-installer'
         >>> r=UCSRepoPool5(major=5, minor=1, patchlevel=0, errata=True)
         >>> r.deb('https://updates.software-univention.de/')
         'deb https://updates.software-univention.de/ errata510 main'
         """
-        return "%s %s %s main" % (
+        return "%s %s %s %s" % (
             type,
             server,
             self._suite,
+            "main main/debian-installer" if mirror else "main",
         )
 
     def path(self, filename=None):

base/univention-updater/conffiles/etc/apt/mirror.list
@@ -36,7 +36,7 @@ try:
 				pool = UCSRepoPool5(rel)
 				for pool.errata in (False, True):
 					for typ in ["deb", "clean"] + ["deb-src"][:mirror.sources]:
-						print(pool.deb(mirror.server, typ))
+						print(pool.deb(mirror.server, typ, mirror=True))
 
 		print(mirror.print_component_repositories(clean=True, start=mirror.version_start, end=mirror.version_end, for_mirror_list=True))
 except Exception:

commit 63093bb0f250495d6c79a49420e7fa4d84d087ab

Bug #51588 up: Fix mirroring main/debian-installer
Only for mirror.
Not for source.
Not for errata.

Fixes: 8d35eb4f4abd2812c0dd3be6d2a81c7c3a18dafb

base/univention-updater/modules/univention/updater/tools.py
@@ -264,6 +264,7 @@ class UCSRepoPool5(_UCSRepo):
 
         :param str server: The URL of the repository server.
         :param str type: The repository type, e.g. `deb` for a binary and `deb-src` for source package repository.
+        :param bool mirror: Also mirror files for Debian installer.
         :returns: The APT repository stanza.
         :rtype: str
 
@@ -276,12 +277,8 @@ class UCSRepoPool5(_UCSRepo):
         >>> r.deb('https://updates.software-univention.de/')
         'deb https://updates.software-univention.de/ errata510 main'
         """
-        return "%s %s %s %s" % (
-            type,
-            server,
-            self._suite,
-            "main main/debian-installer" if mirror else "main",
-        )
+        components = "main main/debian-installer" if mirror and not self.errata and type != "deb-src" else "main"
+        return "%s %s %s %s" % (type, server, self._suite, components)
 
     def path(self, filename=None):
         # type: (str) -> str