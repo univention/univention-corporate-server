univention/univention-corporate-server

commit 0f37d183f0f057e14f091c29c25987a81bde862e

Bug #51588 up: Add `clean` statements for `apt-mirror`
Always add `clean` statements instead of making it conditional on UCRV
`online/repository/clean`: The script must be executed manually anyway.

base/univention-updater/conffiles/etc/apt/mirror.list
@@ -35,7 +35,7 @@ try:
 			if mirror.version_start <= rel <= mirror.version_end:
 				pool = UCSRepoPool5(rel)
 				for pool.errata in (False, True):
-					for typ in ["deb"] + ["deb-src"][:mirror.sources]:
+					for typ in ["deb", "clean"] + ["deb-src"][:mirror.sources]:
 						print(pool.deb(mirror.server, typ))
 
 		print(mirror.print_component_repositories(clean=True, start=mirror.version_start, end=mirror.version_end, for_mirror_list=True))

commit a9a3291666fe9a691537c600709dcb01c424eda4

Bug #51588 up: Fix apt-mirror clean
The apt-mirror `clean` statement expects only a single argument: an URL
which is internally converted to a path by stripping the schema, host
and port prefix and by normalizing the path. This is then used as a
starting point to crawl that directory locally and to compare is with
the list of expected files `ALL`.

Before UCS-5 we used the "flat layout", e.g.
> deb https://updates.software-univention.de/4.0/maintained/ 4.0-0/all/
so for each $major.$minor release and per [un]maintained we had multiple
starting points.

With UCS-5 we use the "pool" layout, where both "dists/" and "pool/" are
directly below the base:
> deb https://updates.software-univention.de/ ucs500 main
Therefore there its sufficient to give "clean" a single URL as the
starting point.

Fixes: 0f37d183f0f057e14f091c29c25987a81bde862e

base/univention-updater/conffiles/etc/apt/mirror.list
@@ -35,8 +35,10 @@ try:
 			if mirror.version_start <= rel <= mirror.version_end:
 				pool = UCSRepoPool5(rel)
 				for pool.errata in (False, True):
-					for typ in ["deb", "clean"] + ["deb-src"][:mirror.sources]:
+					for typ in ["deb"] + ["deb-src"][:mirror.sources]:
 						print(pool.deb(mirror.server, typ, mirror=True))
+
+		print("clean %s" % (mirror.server.baseurl.public(),))
 
 		print(mirror.print_component_repositories(clean=True, start=mirror.version_start, end=mirror.version_end, for_mirror_list=True))
 except Exception:

base/univention-updater/modules/univention/updater/tools.py
@@ -277,7 +277,7 @@ class UCSRepoPool5(_UCSRepo):
         >>> r.deb('https://updates.software-univention.de/')
         'deb https://updates.software-univention.de/ errata510 main'
         """
-        components = "main main/debian-installer" if mirror and not self.errata and type != "deb-src" else "main"
+        components = "main main/debian-installer" if mirror and not self.errata and type == "deb" else "main"
         return "%s %s %s %s" % (type, server, self._suite, components)
 
     def path(self, filename=None):