3117302afe6be83c6640f647f23bf54f9d24e73d
Florian Best <best@univention.de>
2020-10-14 22:31:41 +0200

    Bug #52043 s4c: migrate listener to Python 3

--- services/univention-s4-connector/s4-connector.py
+++ services/univention-s4-connector/s4-connector.py
@@ -67,19 +67,17 @@ if 'connector/listener/additionalbasenames' in listener.configRegistry and liste
 def _save_old_object(directory, dn, old):
 	filename = os.path.join(directory, 'tmp', 'old_dn')
 
-	f = open(filename, 'w+')
-	os.chmod(filename, 0o600)
-	p = pickle.Pickler(f)
-	p.dump((dn, old))
-	p.clear_memo()
-	f.close()
+	with open(filename, 'wb+') as fd:
+		os.chmod(filename, 0o600)
+		p = pickle.Pickler(fd)
+		p.dump((dn, old))
+		p.clear_memo()
 
 
 def _load_old_object(directory):
-	f = open(os.path.join(directory, 'tmp', 'old_dn'), 'r')
-	p = pickle.Unpickler(f)
-	(old_dn, old_object) = p.load()
-	f.close()
+	with open(os.path.join(directory, 'tmp', 'old_dn'), 'rb') as fd:
+		p = pickle.Unpickler(fd)
+		(old_dn, old_object) = p.load()
 
 	return (old_dn, old_object)
 
@@ -91,7 +89,7 @@ def _dump_changes_to_file_and_check_file(directory, dn, new, old, old_dn):
 	filename = '%f' % (time.time(),)
 	filepath = os.path.join(tmpdir, filename)
 
-	with open(filepath, 'w+') as fd:
+	with open(filepath, 'wb+') as fd:
 		os.chmod(filepath, 0o600)
 		p = pickle.Pickler(fd)
 		p.dump(ob)
@@ -128,7 +126,7 @@ def handler(dn, new, old, command):
 		return
 
 	# restart connector on extended attribute changes
-	if 'univentionUDMProperty' in new.get('objectClass', []) or 'univentionUDMProperty' in old.get('objectClass', []):
+	if b'univentionUDMProperty' in new.get('objectClass', []) or b'univentionUDMProperty' in old.get('objectClass', []):
 		connector_needs_restart = True
 	else:
 		if connector_needs_restart is True:
@@ -158,7 +156,7 @@ def handler(dn, new, old, command):
 					old_dn = None
 
 				if s4_init_mode:
-					if new and 'univentionGroup' in new.get('objectClass', []):
+					if new and b'univentionGroup' in new.get('objectClass', []):
 						group_objects.append((dn, new, old, old_dn))
 
 				_dump_changes_to_file_and_check_file(directory, dn, new, old, old_dn)
@@ -198,12 +196,11 @@ def postrun():
 			for ob in group_objects:
 				for directory in dirs:
 					filename = os.path.join(directory, "%f" % time.time())
-					f = open(filename, 'w+')
-					os.chmod(filename, 0o600)
-					p = pickle.Pickler(f)
-					p.dump(ob)
-					p.clear_memo()
-					f.close()
+					with open(filename, 'wb+') as fd:
+						os.chmod(filename, 0o600)
+						p = pickle.Pickler(fd)
+						p.dump(ob)
+						p.clear_memo()
 			del group_objects
 			group_objects = []
 		finally:
