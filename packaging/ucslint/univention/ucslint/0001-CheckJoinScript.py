univention/univention-corporate-server

commit 08ed24bd0a3bdd2cecfd720afaed816cccc3170a

Bug #52211 ucslint: Test for univention-join dh sequence
Fix deb822 parser, see <man:deb822(5)> and <man:deb-src-control(5)>
Move common code to base for re-use.
Implement test for using `dh --with univention-join`.

packaging/ucslint/univention/ucslint/0001-CheckJoinScript.py
@@ -29,7 +29,7 @@
 
 import os
 import re
-from typing import Dict  # noqa F401
+from typing import Dict, Optional  # noqa F401
 
 import univention.ucslint.base as uub
 
@@ -61,12 +61,14 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 			'0001-19': (uub.RESULT_ERROR, 'unjoin script must not call "joinscript_save_current_version"'),
 			'0001-20': (uub.RESULT_ERROR, 'unjoin script does not call "joinscript_remove_script_from_status_file"'),
 			'0001-21': (uub.RESULT_WARN, 'unjoin script does not call "joinscript_init"'),
+			'0001-22': (uub.RESULT_ERROR, '"dh --with univention-join" is used in "debian/rules" without "Build-Depends: univention-join-dev" in "debian/rules"'),
+			'0001-23': (uub.RESULT_STYLE, 'Consider switchting to "univention-join" debhelper sequence'),
 		}
 
 	RE_LINE_ENDS_WITH_TRUE = re.compile(r'\|\|[ \t]+true[ \t]*$')
 	RE_LINE_CONTAINS_SET_E = re.compile(r'\n[\t ]*set -e', re.M)
 	RE_DH_UMC = re.compile(r'\bdh-umc-module-install\b|\bdh\b.*--with\b.*\bumc\b')
-	RE_DH_JOIN = re.compile(r'\bunivention-install-joinscript\b')
+	RE_DH_JOIN = re.compile(r'(?P<old>\bunivention-install-joinscript\b)|\bdh\b.*--with\b.*\bunivention-join\b')
 
 	def check_join_script(self, filename: str) -> None:
 		"""Check a single join script."""
@@ -194,19 +196,24 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 
 		# Look for dh-umc-modules-install
 		try:
-			content = open(fn_rules, 'r').read()
+			fn_control = os.path.join(path, 'debian', 'control')
+			ctrl = uub.ParserDebianControl(fn_control)  # type: Optional[uub.ParserDebianControl]
+		except uub.UCSLintException:
+			self.debug('Errors in debian/control. Skipping here')
+			ctrl = None
+
+		try:
+			c_rules = open(fn_rules, 'r').read()
 		except EnvironmentError:
 			self.addmsg('0001-9', 'failed to open and read file', fn_rules)
 		else:
-			if self.RE_DH_JOIN.search(content):
-				self.debug('Detected use of univention-install-joinscript')
-				try:
-					fn_control = os.path.join(path, 'debian', 'control')
-					parser = uub.ParserDebianControl(fn_control)
-				except uub.UCSLintException:
-					self.debug('Errors in debian/control. Skipping here')
-				else:
-					for binary_package in parser.binary_sections:
+			match = self.RE_DH_JOIN.search(c_rules)
+			if match:
+				self.debug('Detected use of %s' % (match.group(0),))
+				if match.group(1):
+						self.addmsg('0001-23', 'Consider switchting to "univention-join" debhelper sequence', fn_rules)
+				if ctrl:
+					for binary_package in ctrl.binary_sections:
 						package = binary_package['Package']
 						for js in fnlist_joinscripts:
 							if re.match(r'^\./\d\d%s\.u?inst$' % re.escape(package), js):
@@ -214,7 +221,10 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 								fnlist_joinscripts[js] = 0
 								found[js] = found.get(js, 0) + 1
 
-			if self.RE_DH_UMC.search(content):
+					if 'univention-join-dev' not in ctrl.source_section.dep:
+						self.addmsg('0001-22', '"dh --with univention-join" is used in "debian/rules" without "Build-Depends: univention-join-dev" in "debian/rules"', fn_control)
+
+			if self.RE_DH_UMC.search(c_rules):
 				self.debug('Detected use of dh-umc-module-install')
 				for fn in uub.FilteredDirWalkGenerator(debianpath, suffixes=['.umc-modules']):
 					package = os.path.basename(fn)[:-len('.umc-modules')]

packaging/ucslint/testframework/0001-22.correct
@@ -1,0 +1,1 @@
+E:0001-22: debian/control: "dh --with univention-join" is used in "debian/rules" without "Build-Depends: univention-join-dev" in "debian/rules"

packaging/ucslint/testframework/0001-22/00bin.inst
@@ -1,0 +1,7 @@
+#!/bin/sh
+## joinscript api: bindpwdfile
+VERSION="1"
+. /usr/share/univention-join/joinscripthelper.lib
+joinscript_init
+joinscript_save_current_version
+exit 0

packaging/ucslint/testframework/0001-22/debian/compat
@@ -1,0 +1,1 @@
+9

packaging/ucslint/testframework/0001-22/debian/control
@@ -1,0 +1,13 @@
+Source: src
+Section: univention
+Priority: optional
+Maintainer: Univention GmbH <packages@univention.de>
+Build-Depends:
+ debhelper,
+Standards-Version: 3.7.2
+
+Package: bin
+Architecture: all
+Depends:
+ ${misc:Depends},
+Description: desc

packaging/ucslint/testframework/0001-22/debian/copyright
@@ -1,0 +1,29 @@
+Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: Univention GmbH
+Upstream-Contact: <package@univention.de>
+Source: https://updates.software-univention.de/
+
+Files: *
+Copyright: 2012-2020 Univention GmbH
+License: AGPL-3.0-only
+ The source code of the software contained in this package
+ as well as the source package itself are made available
+ under the terms of the GNU Affero General Public License version 3
+ (GNU AGPL V3) as published by the Free Software Foundation.
+ .
+ Binary versions of this program provided by Univention to you as
+ well as other copyrighted, protected or trademarked materials like
+ Logos, graphics, fonts, specific documentations and configurations,
+ cryptographic keys etc. are subject to a license agreement between
+ you and Univention and not subject to the GNU AGPL V3.
+ .
+ In the case you use this program under the terms of the GNU AGPL V3,
+ the program is provided in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Affero General Public License for more details.
+ .
+ You should have received a copy of the GNU Affero General Public
+ License with the Debian GNU/Linux or Univention distribution in file
+ /usr/share/common-licenses/AGPL-3; if not, see
+ <https://www.gnu.org/licenses/>.

packaging/ucslint/testframework/0001-22/debian/rules
@@ -1,0 +1,3 @@
+#!/usr/bin/make -f
+%:
+	dh $@ --with univention-join

packaging/ucslint/testframework/0001-23.correct
@@ -1,0 +1,1 @@
+S:0001-23: debian/rules: Consider switchting to "univention-join" debhelper sequence

commit 2803ef9129c1979a5571480c729ef7ce29b598d6

Bug #52211 ucslint: Fix univention-join dh sequence test
Only error out if the new sequence is actually used.

Fixes: 08ed24bd0a3bdd2cecfd720afaed816cccc3170a

packaging/ucslint/univention/ucslint/0001-CheckJoinScript.py
@@ -210,8 +210,9 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 			match = self.RE_DH_JOIN.search(c_rules)
 			if match:
 				self.debug('Detected use of %s' % (match.group(0),))
-				if match.group(1):
-						self.addmsg('0001-23', 'Consider switchting to "univention-join" debhelper sequence', fn_rules)
+				is_old = bool(match.group(1))
+				if is_old:
+					self.addmsg('0001-23', 'Consider switchting to "univention-join" debhelper sequence', fn_rules)
 				if ctrl:
 					for binary_package in ctrl.binary_sections:
 						package = binary_package['Package']
@@ -221,7 +222,7 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 								fnlist_joinscripts[js] = 0
 								found[js] = found.get(js, 0) + 1
 
-					if 'univention-join-dev' not in ctrl.source_section.dep:
+					if 'univention-join-dev' not in ctrl.source_section.dep and not is_old:
 						self.addmsg('0001-22', '"dh --with univention-join" is used in "debian/rules" without "Build-Depends: univention-join-dev" in "debian/rules"', fn_control)
 
 			if self.RE_DH_UMC.search(c_rules):