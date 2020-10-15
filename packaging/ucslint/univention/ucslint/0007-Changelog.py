univention/univention-corporate-server

commit 7d459daf6f3609bd428bad4d4eebd0aa5c4b7e13

Bug #49620 ucslint: Check debian/changelog is strict-monotonical

packaging/ucslint/univention/ucslint/0007-Changelog.py
@@ -30,6 +30,8 @@
 import univention.ucslint.base as uub
 import re
 import os
+from email.utils import mktime_tz, parsedate_tz
+from debian.changelog import Changelog, ChangelogParseError
 
 REticket = re.compile(r'''
 	(Bug:?[ ]\#[0-9]{1,6} # Bugzilla
@@ -37,6 +39,7 @@ REticket = re.compile(r'''
 	|Ticket(\#:[ ]|:?[ ]\#)2[0-9]{3}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])(?:1[0-9]{7}|21[0-9]{6})) # OTRS
 	(?![0-9]) # not followed by additional digits
 	''', re.VERBOSE)
+RECENT_ENTRIES = 5
 
 
 class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
@@ -45,6 +48,10 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 		return {
 			'0007-1': (uub.RESULT_WARN, 'failed to open file'),
 			'0007-2': (uub.RESULT_WARN, 'changelog does not contain ticket/bug/issue number'),
+			'0007-3': (uub.RESULT_ERROR, 'debian/changelog entries are not strict-monotonically increasing by time'),
+			'0007-4': (uub.RESULT_ERROR, 'debian/changelog entries are not strict-monotonically increasing by version'),
+			'0007-5': (uub.RESULT_WARN, 'old debian/changelog entries are not strict-monotonically increasing by time'),
+			'0007-6': (uub.RESULT_WARN, 'old debian/changelog entries are not strict-monotonically increasing by version'),
 		}
 
 	def check(self, path):
@@ -53,14 +60,31 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 
 		fn = os.path.join(path, 'debian', 'changelog')
 		try:
-			content = open(fn, 'r').read()
-		except EnvironmentError:
+			with open(fn, 'r') as stream:
+				changelog = Changelog(stream, strict=True)
+		except (EnvironmentError, ChangelogParseError):
 			self.addmsg('0007-1', 'failed to open and read file', fn)
 			return
 
-		REchangelog = re.compile('^ -- [^<]+ <[^>]+>', re.M)
-
-		firstEntry = REchangelog.split(content)[0]
-		match = REticket.search(firstEntry)
-		if not match:
+		for change in changelog[0].changes():
+			if REticket.search(change):
+				break
+		else:
 			self.addmsg('0007-2', 'latest changelog entry does not contain bug/ticket/issue number', fn)
+
+		last = None
+		for nr, block in enumerate(changelog):
+			if last:
+				if mktime_tz(parsedate_tz(last.date)) <= mktime_tz(parsedate_tz(block.date)):
+					if nr < RECENT_ENTRIES:
+						self.addmsg('0007-3', 'not strict-monotonically increasing by time: %s <= %s' % (last.date, block.date), fn)
+					else:
+						self.addmsg('0007-5', 'old not strict-monotonically increasing by time: %s <= %s' % (last.date, block.date), fn)
+
+				if last.version <= block.version:
+					if nr < RECENT_ENTRIES:
+						self.addmsg('0007-4', 'not strict-monotonically increasing by version: %s <= %s' % (last.version, block.version), fn)
+					else:
+						self.addmsg('0007-6', 'old not strict-monotonically increasing by version: %s <= %s' % (last.version, block.version), fn)
+
+			last = block

packaging/ucslint/testframework/0007-2a.correct
@@ -1,1 +1,2 @@
 W:0007-2: debian/changelog: latest changelog entry does not contain bug/ticket/issue number
+W:0007-5: debian/changelog: old not strict-monotonically increasing by time: Mon, 25 Aug 2008 16:14:55 +0200 <= Fri, 17 Oct 2008 18:11:02 +0200

packaging/ucslint/testframework/0007-2b.correct
@@ -1,0 +1,1 @@
+W:0007-5: debian/changelog: old not strict-monotonically increasing by time: Mon, 25 Aug 2008 16:14:55 +0200 <= Fri, 17 Oct 2008 18:11:02 +0200

packaging/ucslint/testframework/0007-2c.correct
@@ -1,0 +1,1 @@
+W:0007-5: debian/changelog: old not strict-monotonically increasing by time: Mon, 25 Aug 2008 16:14:55 +0200 <= Fri, 17 Oct 2008 18:11:02 +0200

packaging/ucslint/testframework/0007-3.correct
@@ -1,0 +1,1 @@
+E:0007-3: debian/changelog: not strict-monotonically increasing by time: Wed, 27 Feb 2019 07:16:59 +0100 <= Wed, 27 Feb 2019 08:16:59 +0100

packaging/ucslint/testframework/0007-3/debian/compat
@@ -1,0 +1,1 @@
+9

packaging/ucslint/testframework/0007-3/debian/control
@@ -1,0 +1,11 @@
+Source: ucslint
+Section: univention
+Priority: optional
+Maintainer: Univention GmbH <packages@univention.de>
+Build-Depends: ucslint,
+Build-Depends-Indep: debhelper,
+Standards-Version: 4.3.0.2
+
+Package: ucslint
+Architecture: all
+Description: ucslint

packaging/ucslint/testframework/0007-3/debian/copyright
@@ -1,0 +1,29 @@
+Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: Univention GmbH
+Upstream-Contact: <package@univention.de>
+Source: https://updates.software-univention.de/
+
+Files: *
+Copyright: 2008-2020 Univention GmbH
+License: AGPL-3.0-only
+ The source code of the software contained in this package
+ as well as the source package itself are made available
+ under the terms of the GNU Affero General Public License version 3
+ (GNU AGPL V3) as published by the Free Software Foundation.
+ .
+ Binary versions of this software provided by Univention to you as
+ well as other copyrighted, protected or trademarked materials like
+ Logos, graphics, fonts, specific documentations and configurations,
+ cryptographic keys etc. are subject to a license agreement between
+ you and Univention and not subject to the GNU AGPL V3.
+ .
+ In the case you use the software under the terms of the GNU AGPL V3,
+ the program is provided in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Affero General Public License for more details.
+ .
+ You should have received a copy of the GNU Affero General Public
+ License with the Debian GNU/Linux or Univention distribution in file
+ /usr/share/common-licenses/AGPL-3; if not, see
+ <http://www.gnu.org/licenses/>.

commit bb56ca5b761ff00c3f580c8b985c4d108ae19cb9

Bug #49620 ucslint: Change debian/changelog monoton check
to only check the first two entries as this breaks regularly when
changes from UCS 4.4-x are merged to UCS 5.0-0: The original version and
timestamp are kept and inserted before the UCS 5.0-0 version bump, which
makes the sequence of entries non-monotonically.

Fixes: 7d459daf6f3

packaging/ucslint/univention/ucslint/0007-Changelog.py
@@ -42,7 +42,7 @@ REticket = re.compile(
 	|Ticket(\#:[ ]|:?[ ]\#)2[0-9]{3}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])(?:1[0-9]{7}|21[0-9]{6})) # OTRS
 	(?![0-9]) # not followed by additional digits
 	''', re.VERBOSE)
-RECENT_ENTRIES = 5
+RECENT_ENTRIES = 2
 
 
 class UniventionPackageCheck(uub.UniventionPackageCheckDebian):