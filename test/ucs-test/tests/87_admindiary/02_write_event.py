55bddd287c5da9c4bcb20884a087e5624bebcae3
Florian Best <best@univention.de>
2020-10-15 12:40:53 +0200

    fixup! Bug #51334: add 87_admindiary/02_write_event.py

--- test/ucs-test/tests/87_admindiary/02_write_event.py
+++ test/ucs-test/tests/87_admindiary/02_write_event.py
@@ -1,4 +1,4 @@
-#!/usr/share/ucs-test/runner /usr/bin/py.test-3 -s
+#!/usr/share/ucs-test/runner /usr/bin/py.test-3 -s -vvv
 ## desc: Tests the Univention Admin Diary
 ## tags: [apptest]
 ## roles: [domaincontroller_master]
@@ -10,15 +10,23 @@ import datetime
 import univention.admindiary.backend
 import univention.testing.ucr as ucr_test
 import univention.testing.udm as udm_test
-from univention.testing import utils
 
 
 def test_write_event():
 	with udm_test.UCSTestUDM() as udm, ucr_test.UCSTestConfigRegistry() as ucr, univention.admindiary.backend.get_client(version=1) as client:
+		d = (datetime.datetime.now() - datetime.timedelta(seconds=5)).isoformat()
 		dn, username = udm.create_user()
-		expected = {'id': 33, 'date': datetime.datetime.now().strftime('%y-%m-%d %H:%M:%S'), 'event_name': 'UDM_USERS_USER_CREATED', 'hostname': ucr['hostname'], 'username': 'cn=admin', 'message': None, 'args': {'username': username, 'module': 'users/user'}, 'comments': False}
-		utils.wait_for_replication()
-		entries = [_ for _ in client.query() if _['event_name'] == 'UDM_USERS_USER_CREATED']
+		expected = {
+			'id': 33,
+			'date': datetime.datetime.now().strftime('%y-%m-%d %H:%M:%S'),
+			'event_name': 'UDM_USERS_USER_CREATED',
+			'hostname': ucr['hostname'],
+			'username': 'cn=admin',
+			'message': None,
+			'args': {'username': username, 'module': 'users/user'},
+			'comments': False
+		}
+		entries = client.query(d, event='UDM_USERS_USER_CREATED', username=username)
 		x = entries[-1]
 		expected['id'] = x['id']
 		expected['date'] = x['date']
