univention/univention-corporate-server

commit ecf2c5368f806b761dfd95c776e8527c0eb5fab9

Bug #33029: remove obsolete USS scripts paths

base/univention-system-setup/debian/univention-system-setup.dirs
@@ -14,7 +14,3 @@ var/lib/univention-system-setup/gateway6.post
 var/lib/univention-system-setup/nameserver.post
 var/lib/univention-system-setup/dnsforwarder.post
 var/lib/univention-system-setup/httpproxy.post
-usr/lib/univention-system-setup/scripts/25_defaultlocale
-usr/lib/univention-system-setup/scripts/45_modules
-usr/lib/univention-system-setup/scripts/55_system_settings
-usr/lib/univention-system-setup/scripts/60_join

base/univention-system-setup/usr/lib/univention-system-setup/scripts/setup-join.sh
@@ -84,7 +84,6 @@ echo "no-ldap" > /var/run/univention-system-setup.ldap
 echo "Starting re-configuration of locales"
 run-parts -v /usr/lib/univention-system-setup/scripts/15_keyboard
 run-parts -v /usr/lib/univention-system-setup/scripts/20_language
-run-parts -v /usr/lib/univention-system-setup/scripts/25_defaultlocale
 
 # Do not allow the UMC to be restarted, webserver is OK, but
 # make sure the webserver uses the same certificates during setup.
@@ -154,9 +153,6 @@ else
 fi
 # set root password
 /usr/lib/univention-system-setup/scripts/10_basis/18root_password
-
-# set init-script configuration
-/usr/lib/univention-system-setup/scripts/10_basis/20initscripts
 
 if [ "$server_role" = "domaincontroller_master" ]; then
 	realm="$(echo $domainname | tr "[:lower:]" "[:upper:]")"
@@ -241,8 +237,6 @@ else
 	fi
 	ln -snf "$fqdn" "/etc/univention/ssl/$hostname"
 fi
-
-run-parts -v /usr/lib/univention-system-setup/scripts/45_modules
 
 # Re-create sources.list files
 ucr commit /etc/apt/sources.list.d/*

commit 4ddd9c0268b83be3c1d994188bb2adc4a07bfecc

Bug #33029 USS: remove obsolete USS scripts paths
The empty directories must be provided for appliance images, as they
might add scripts there.
They were already removed and re-added several times in the past:

> commit b160266245e9b698f3245f6a578409bf00761116
> Author: Philipp Hahn <hahn@univention.de>
> Date:   Thu Aug 21 07:59:47 2014 +0000
>     Bug #35675: Re-add empty scripts directories
>     Referenced by setup-join.sh
>     git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-4.0-0@52883 1b283449-9f2f-0410-b571-8eb228e1a901

> commit 226ffd32b09d03cefe7caed2afd2d0f5124c7fce
> Author: Philipp Hahn <hahn@univention.de>
> Date:   Tue Oct 22 00:28:25 2013 +0000
>     Bug #28670: USS: Ship hook directories
>     Create and ship empty hook directories.
>     git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-3.2-0@45396 1b283449-9f2f-0410-b571-8eb228e1a901

The culprits here are `dh_python[23]` which delete empty directories.
Use `--skip-private` there to prevent that misbehavior.

Fixes: ecf2c5368f806b761dfd95c776e8527c0eb5fab9.

base/univention-system-setup/debian/rules
@@ -43,5 +43,11 @@ endif
 override_dh_auto_test:
 	run-parts -a -v tests
 
+override_dh_python2:
+	dh_python2 --skip-private
+
+override_dh_python3:
+	dh_python3 --skip-private
+
 %:
 	dh $@ --with python2,python3,univention-l10n,umc,ucr

base/univention-system-setup/debian/univention-system-setup.dirs
@@ -14,3 +14,5 @@ var/lib/univention-system-setup/gateway6.post
 var/lib/univention-system-setup/nameserver.post
 var/lib/univention-system-setup/dnsforwarder.post
 var/lib/univention-system-setup/httpproxy.post
+usr/lib/univention-system-setup/scripts/25_defaultlocale
+usr/lib/univention-system-setup/scripts/45_modules

base/univention-system-setup/usr/lib/univention-system-setup/scripts/setup-join.sh
@@ -84,6 +84,7 @@ echo "no-ldap" > /var/run/univention-system-setup.ldap
 echo "Starting re-configuration of locales"
 run-parts -v /usr/lib/univention-system-setup/scripts/15_keyboard
 run-parts -v /usr/lib/univention-system-setup/scripts/20_language
+run-parts -v /usr/lib/univention-system-setup/scripts/25_defaultlocale
 
 # Do not allow the UMC to be restarted, webserver is OK, but
 # make sure the webserver uses the same certificates during setup.
@@ -238,6 +239,8 @@ else
 	ln -snf "$fqdn" "/etc/univention/ssl/$hostname"
 fi
 
+run-parts -v /usr/lib/univention-system-setup/scripts/45_modules
+
 # Re-create sources.list files
 ucr commit /etc/apt/sources.list.d/*
 
@@ -366,4 +369,3 @@ else
 fi
 
 exit 0
-