f139e57f5c451ef4e03b1c8fa0ebc44301c84bab
Florian Best <best@univention.de>
2020-10-22 16:27:54 +0200

    Bug #50631: basic 52_s4connector/dnstests.py Python3 compatibility

--- test/ucs-test/tests/52_s4connector/dnstests.py
+++ test/ucs-test/tests/52_s4connector/dnstests.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import os
 import sys
 import time
@@ -12,13 +14,13 @@ ucr = univention.config_registry.ConfigRegistry()
 ucr.load()
 
 
-## Adding a DNS zone causes bind restart in postrun, so we may have to
-## retry a couple of times:
+# Adding a DNS zone causes bind restart in postrun, so we may have to
+# retry a couple of times:
 MATCH_ATTEMPTS = 17  # number of 'dig' attempts to be done, see Bug #38288
 
 
 def check_ldap_object(item, item_name, item_attribute=None, expected_values=None, should_exist=True):
-	print (" Testing Ldap object : {0}			".format(item_name)),
+	print((" Testing Ldap object : {0}".format(item_name)), end=' ')
 	if not isinstance(expected_values, list):
 		expected_values = [expected_values]
 	try:
@@ -27,8 +29,8 @@ def check_ldap_object(item, item_name, item_attribute=None, expected_values=None
 		else:
 			utils.verify_ldap_object(item, should_exist=should_exist)
 	except utils.LDAPError as exc:
-		print (' Failed')
-		print ('Verification of Ldap object failed: %s' % exc)
+		print(' Failed')
+		print('Verification of Ldap object failed: %s' % exc)
 		sys.exit(1)
 	else:
 		print(' Success ')
@@ -76,7 +78,7 @@ def test_dns_aaaa_record(dns_name, test_object, should_exist=True):
 			part = part[1:]
 		new_test_object_parts.append(part)
 	test_object = (':').join(new_test_object_parts)
-	print test_object
+	print(test_object)
 	re_test_object = re.compile(r'{0}\.*\s+\d+\s+IN\s+AAAA\s+\"*{1}\"*'.format(*map(re.escape, (dns_name, test_object))))
 	match(re_test_object, dns_name, 'AAAA', should_exist=should_exist)
 
@@ -104,18 +106,18 @@ def match(re_test_object, dns_name, typ, param=None, should_exist=True):
 	else:
 		dig_cmd = ("dig", param, dns_name, typ, '+noall', '+answer')
 
-	## Adding a DNS zone causes bind restart in postrun, so we may have to
-	## retry a couple of times
+	# Adding a DNS zone causes bind restart in postrun, so we may have to
+	# retry a couple of times
 	for attempt in range(MATCH_ATTEMPTS):
 		dig_subprocess = subprocess.Popen(dig_cmd, shell=False, stdout=subprocess.PIPE).communicate()
-		dig_answer = dig_subprocess[0].splitlines()
+		dig_answer = dig_subprocess[0].decode('UTF-8', 'replace').splitlines()
 
 		if not re_test_object and dig_answer:
-			print("\nFAIL: Received DNS answer, expected none:\n%s", dig_answer)
+			print(("\nFAIL: Received DNS answer, expected none:\n%s", dig_answer))
 
-		print ("\nDig Output :")
+		print("\nDig Output :")
 		for line in dig_answer:
-			print line
+			print(line)
 			if should_exist and re.match(re_test_object, line):
 				print("\nOK: DNS synced after %s seconds\n" % attempt)
 				return
@@ -155,10 +157,8 @@ def random_reverse_zone():
 		random_reverse_zone = '.'.join(ip_parts)
 		try:
 			utils.verify_ldap_object(random_reverse_zone)
-		except:
+		except Exception:
 			break
-		else:
-			pass
 	return random_reverse_zone
 
 
@@ -188,43 +188,36 @@ def random_hex():
 
 
 def fail_if_cant_resolve_own_hostname(max_attempts=17, delta_t_seconds=1):
-		p = subprocess.Popen(["host", "%(hostname)s.%(domainname)s" % ucr])
-		rc = p.wait()
-		attempt = 1
-		while rc != 0:
-			if attempt > max_attempts:
-				utils.fail("Cannot resolve own hostname after %s seconds" % max_attempts)
-			sys.stdout.flush()
-			time.sleep(delta_t_seconds)
-			p = subprocess.Popen(["host", "%(hostname)s.%(domainname)s" % ucr])
-			rc = p.wait()
-			attempt += 1
-		print "Resolved own hostname after %s seconds" % attempt
+	rc = subprocess.call(["host", "%(hostname)s.%(domainname)s" % ucr])
+	attempt = 1
+	while rc != 0:
+		if attempt > max_attempts:
+			utils.fail("Cannot resolve own hostname after %s seconds" % max_attempts)
+		sys.stdout.flush()
+		time.sleep(delta_t_seconds)
+		rc = subprocess.call(["host", "%(hostname)s.%(domainname)s" % ucr])
+		attempt += 1
+	print("Resolved own hostname after %s seconds" % attempt)
 
 
 def udm_remove_dns_record_object(module, object_dn):
 	superordinate = ",".join(ldap.explode_dn(object_dn)[1:])
 	cmd = ['/usr/sbin/udm-test', module, 'remove', '--dn', object_dn, '--superordinate', superordinate]
-	p = subprocess.Popen(cmd)
-	rc = p.wait()
-
-	return rc
+	return subprocess.call(cmd)
 
 
 def get_kerberos_ticket_for_machine():
 	sys.stdout.flush()
-	p = subprocess.Popen(["kdestroy"])
-	rc = p.wait()
+	rc = subprocess.call(["kdestroy"])
 
 	principal_for_nsupdate = "%s$" % ucr["hostname"].upper()
-	p = subprocess.Popen(["kinit", "-t", "/etc/krb5.keytab", principal_for_nsupdate])
-	rc = p.wait()
+	rc = subprocess.call(["kinit", "-t", "/etc/krb5.keytab", principal_for_nsupdate])
 	if rc != 0:
 		utils.fail("kinit for %s failed" % principal_for_nsupdate)
 
 
 def nsupdate(nsupdate_request):
-	p = subprocess.Popen(["nsupdate", "-v", "-g"], stdin=subprocess.PIPE)
-	(stdout, stderr) = p.communicate(input=nsupdate_request)
+	p = subprocess.Popen(["nsupdate", "-v", "-g"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	(stdout, stderr) = p.communicate(nsupdate_request.encode('UTF-8'))
 	if p.returncode != 0:
-		utils.fail("nsupdate failed")
+		utils.fail("nsupdate failed: %s" % (stdout.decode('UTF-8', 'replace'),))
