univention/univention-corporate-server

commit 1eb0ce960cfb99da77cdfa437ec5b2d809b48105

Bug #52318: add HTTP reason phrase for 502+503 errors if UMC-Web-Server is unreachable

management/univention-management-console/usr/share/univention-management-console/error.py
@@ -1,0 +1,41 @@
+#!/usr/bin/python3
+import json
+import cgitb
+cgitb.enable()
+
+
+def application(environ, start_response):
+	status, _, reason = environ.get('REDIRECT_STATUS', '200 OK').partition(' ')
+	status = int(status)
+	reason = reason or {502: 'Proxy Error', 503: 'Service Unavailable'}.get(status, reason)
+	url = environ.get('REDIRECT_URL', '')
+	service = 'it'
+	service_name = 'Service'
+	if url.startswith('/univention/udm'):
+		service_name = 'Univention Directory Manager REST API'
+		service = 'univention-directory-manager-rest'
+		if status == 503:
+			reason = 'UDM REST Unavailable'
+	elif url.startswith('/univention/portal'):
+		service = 'univention-portal-server'
+		service_name = 'Portal Server'
+		if status == 503:
+			reason = 'Portal Service Unavailable'
+	elif url.startswith('/univention/'):
+		service_name = 'Univention Management Console Web Server'
+		service = 'univention-management-console-web-server'
+		if status == 503:
+			reason = 'UMC-Web-Server Unavailable'
+
+	message = "The %s could not be reached. Please restart %s or try again later." % (service_name, service)
+	if status == 502:
+		message += ' %s' % (environ.get('REDIRECT_ERROR_NOTES', ''),)
+		message = message.rstrip()
+	data = {
+		"status": status,
+		"message": message,
+		# DEBUG: "environ": dict((key, val) for key, val in environ.items() if not key.startswith('wsgi.') and not key.startswith('mod_wsgi.')),
+	}
+	response_header = [('Content-type', 'application/json')]  # TODO: give HTML when json is not accepable
+	start_response('%d %s' % (status, reason), response_header)
+	return [json.dumps(data).encode('UTF-8')]

management/univention-management-console/conffiles/etc/apache2/sites-available/univention.conf
@@ -68,7 +68,8 @@ print('\tHeader always setifempty "Content-Security-Policy" "%s"' % (csp,))
 
 <LocationMatch "^/(univention|univention-management-console)/(.*)$">
 	RequestHeader set X-UMC-HTTPS %{HTTPS}s
-	ErrorDocument 503 "{\"status\": 503, \"message\": \"The Univention Management Console Web Server could not be reached. Please restart it or try again later.\"}"
+	ErrorDocument 502 /error-univention
+	ErrorDocument 503 /error-univention
 	# fix for bug in cherrypy returning 408/409 errors out of the blue:
 	#   http://cherrypy.org/ticket/853
 	SetEnv proxy-nokeepalive 1
@@ -84,6 +85,8 @@ print('	ProxyPassMatch http://%s:%s/$2 retry=0 timeout=%d' % (interface, port, i
 print('	ProxyPassReverse http://%s:%s/' % (interface, port))
 @!@
 </LocationMatch>
+
+WSGIScriptAlias "/error-univention" "/usr/share/univention-management-console/error.py"
 
 # do not proxy forward certain requests
 ProxyPassMatch "^/univention/js(/|_.*)" "!"

management/univention-management-console/debian/control
@@ -179,6 +179,7 @@ Package: univention-management-console-web-server
 Architecture: all
 Depends:
  curl,
+ libapache2-mod-wsgi,
  openssl,
  python-cherrypy3,
  python-daemon,

management/univention-management-console/debian/univention-management-console-web-server.postinst
@@ -35,6 +35,7 @@
 . /usr/share/univention-lib/base.sh
 
 # add apache modules for redirect to UMC web server
+a2enmod wsgi
 a2enmod proxy
 a2enmod proxy_connect
 a2enmod proxy_http

commit 52fe07436547f426f1ff1e6e668f3574ba6c0144

squash! Bug #52318: add HTTP reason phrase for 502+503 errors if UMC-Web-Server is unreachable
> $ ucslint
> 0010-2: management/univention-management-console/usr/share/univention-management-console/error.py: file contains no copyright text block

Fixes: 1eb0ce960cf

management/univention-management-console/usr/share/univention-management-console/error.py
@@ -1,4 +1,32 @@
 #!/usr/bin/python3
+#
+# Copyright 2020 Univention GmbH
+#
+# https://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <https://www.gnu.org/licenses/>.
+
 import json
 import cgitb
 cgitb.enable()