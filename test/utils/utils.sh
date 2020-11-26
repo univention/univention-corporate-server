033a852546d6d4aa4d6ed47f0b715f2c90288873
Felix Botner <botner@univention.de>
2020-11-26 12:11:21 +0100

    Bug #52435: update additional apps,
    but do not expect that an update is available

--- test/utils/utils.sh
+++ test/utils/utils.sh
@@ -808,25 +808,25 @@ install_apps_via_umc () {
 }
 
 update_apps_via_umc () {
-	local username=${1:?missing username} password=${2:?missing password} rv=0 app
-	shift 2 || return $?
-	# check if any update is available - if not, fail the test
-	local update_available=false
+	local username=${1:?missing username} password=${2:?missing password} main_app=${3:?missing main_app} rv=0 app
+	shift 3 || return $?
+
+	# update the main app
+	python -m shared-utils/apps -U "$username" -p "$password" -a "$main_app" -u || rv=$?
+
+	# TODO, do we want to update additional apps?
+	# In app tests we want to check the new version of the main app.
+	# Normally we do not want to update additional apps to the
+	# the test version, but if that becomes necessary, we can re-add
+	# the update
 	for app in "$@"; do
+		test "$app" = "$main_app" && continue
 		if ! assert_app_is_installed_and_latest "${app}"; then
-			update_available=true
-			break
+			# try update, but do not except that an update is available
+			python -m shared-utils/apps -U "$username" -p "$password" -a $app -u -i || rv=$?
 		fi
 	done
 
-	if ! $update_available; then
-		echo "Apps should be updated: $@; but no app has any update available"
-		exit 1
-	fi
-
-	for app in "$@"; do
-		python -m shared-utils/apps -U "$username" -p "$password" -a $app -u || rv=$?
-	done
 	return $rv
 }
 
