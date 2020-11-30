b12dc65e47447b4385ef4a0574db271a13856a4a
Philipp Hahn <hahn@univention.de>
2020-11-30 15:00:30 +0100

    Bug #52447 UDL: Add new SetUID API
    Add SetUID() SUID decorator and context manager.

--- management/univention-directory-listener/python/listener.py
+++ management/univention-directory-listener/python/listener.py
@@ -31,17 +31,26 @@
 # <https://www.gnu.org/licenses/>.
 
 import os
-import pwd
+from contextlib import contextmanager
+from functools import wraps
+from pwd import getpwnam
 from six import string_types
-import univention.config_registry
+from univention.config_registry import ConfigRegistry
+try:
+	from typing import Any, Callable, List, Optional, Text, Type, TypeVar, Union  # noqa F401
+	from types import TracebackType  # noqa F401
 
-configRegistry = univention.config_registry.ConfigRegistry()
+	_F = TypeVar("_F", bound=Callable[..., Any])
+except ImportError:
+	pass
+
+configRegistry = ConfigRegistry()
 configRegistry.load()
 
 baseConfig = configRegistry
 
 
-def setuid(uid):
+def setuid(uid):  # type: (Union[int, Text]) -> None
 	"""
 	Set the current process’s effective user id.
 	Use :py:func:`unsetuid()` to return to the listeners UID.
@@ -55,14 +64,14 @@ def setuid(uid):
 	:return: None
 	"""
 	if isinstance(uid, string_types):
-		uid = pwd.getpwnam(uid)[2]
+		uid = getpwnam(uid)[2]
 	os.seteuid(uid)
 
 
 __listener_uid = -1
 
 
-def unsetuid():
+def unsetuid():  # type: () -> None
 	"""
 	Return the current process’s effective user id to the listeners UID.
 	Only possible if the current effective user id is `root`.
@@ -72,38 +81,65 @@ def unsetuid():
 	global __listener_uid
 	if __listener_uid == -1:
 		try:
-			__listener_uid = pwd.getpwnam('listener')[2]
+			__listener_uid = getpwnam('listener')[2]
 		except KeyError:
 			__listener_uid = 0
 	os.seteuid(__listener_uid)
 
 
-def run(file, argv, uid=-1, wait=1):
+def run(exe, argv, uid=-1, wait=True):  # type: (str, List[Union[str, Text]], int, bool) -> int
 	"""
-	Execute a the program `file` with arguments `argv` and effective user id
+	Execute a the program `exe` with arguments `argv` and effective user id
 	`uid`.
 
-	:param str file: path to executable
+	:param str exe: path to executable
 	:param argv: arguments to pass to executable
 	:type argv: list(str)
 	:param int uid: effective user id the process should be started with
 	:param bool wait: if true will block until the process has finished and return either its exit code or the signal that lead to its stop (a negative number), see :py:const:`os.P_WAIT`. If false will return as soon as the new process has been created, with the process id as the return value (see :py:const:`os.P_NOWAIT`).
 	:return: exit code or signal number or process id
 	:rtype: int
+
+	.. warning::
+	   Not waiting for the sub-process leads to zombie processes.
 	"""
 	if uid > -1:
 		olduid = os.getuid()
 		setuid(uid)
+
+	waitp = os.P_WAIT if wait else os.P_NOWAIT
 	try:
-		if wait:
-			waitp = os.P_WAIT
-		else:
-			waitp = os.P_NOWAIT
-		rc = os.spawnv(waitp, file, argv)
+		rc = os.spawnv(waitp, exe, argv)
 	except:
 		rc = 100
+	finally:
 		if uid > -1:
 			setuid(olduid)
-	if uid > -1:
-		setuid(olduid)
+
 	return rc
+
+
+class SetUID(object):
+	"""
+	Temporarily change effective UID to given user.
+
+	:param int uid: Numeric user ID. Defaults to `root`.
+	"""
+	def __init__(self, uid=0):  # type: (int) -> None
+		self.uid = uid if os.geteuid() != uid else -1
+
+	def __enter__(self):  # type: () -> None
+		if self.uid >= 0:
+			setuid(self.uid)
+
+	def __exit__(self, exc_type, exc_value, traceback):  # type: (Optional[Type[BaseException]], Optional[BaseException], Optional[TracebackType]) -> None
+		if self.uid >= 0:
+			unsetuid()
+
+	def __call__(self, f):  # type: (_F) -> Callable[[_F], _F]
+		@wraps(f)
+		def wrapper(*args, **kwargs):  # type: (*Any, **Any) -> Any
+			with self:
+				return f(*args, **kwargs)
+
+		return wrapper
