7436e44ff78080bfa94a5297aab5bebd93b288d2
Dirk Wiesenthal <wiesenthal@univention.de>
2020-12-02 06:05:01 +0100

    Bug #52302: Remove call to /univention/get/modules for every page load

--- management/univention-portal/www/main.js
+++ management/univention-portal/www/main.js
@@ -1529,12 +1529,16 @@ define([
 
 		_checkEditAuthorization: function() {
 			var deferred = new Deferred();
-			tools.umcpCommand('get/modules').then(function(result) {
-				var isAuthorized = result.modules.some(function(module) {
-					return module.flavor === 'portals/all';
+			if (portalJson.may_edit_portal) {
+				tools.umcpCommand('get/modules').then(function(result) {
+					var isAuthorized = result.modules.some(function(module) {
+						return module.flavor === 'portals/all';
+					});
+					deferred.resolve(isAuthorized);
 				});
-				deferred.resolve(isAuthorized);
-			});
+			} else {
+				deferred.resolve(false);
+			}
 			return deferred;
 		},
 
