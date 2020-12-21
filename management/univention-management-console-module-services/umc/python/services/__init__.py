93aea44db634301bc8a0410623400238a4829d8b
Jan Luttermann <luttermann@univention.de>
2020-12-21 12:23:33 +0100

    Bug #51333: migrated services UMC module to python 3

--- management/univention-management-console-module-services/umc/python/services/__init__.py
+++ management/univention-management-console-module-services/umc/python/services/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2.7
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
 # Univention Management Console
@@ -47,24 +47,27 @@ import univention.config_registry
 _ = Translation('univention-management-console-module-services').translate
 
 
+from typing import Any, Dict, List, Union  # noqa F401
+
+
 class Instance(Base):
 
 	@sanitize(pattern=PatternSanitizer(default='.*'))
 	@simple_response
-	def query(self, pattern):
+	def query(self, pattern: str) -> List[Dict]:
 		ucr.load()
 		srvs = ServiceInfo()
 
-		lang = _.im_self.locale.language
+		lang = _.__self__.locale.language
 		if lang in (None, 'C'):
 			lang = 'en'
 
 		result = []
 		for name, srv in srvs.services.items():
-			key = srv.get('start_type', '%s/autostart' % (name,))
+			key = srv.get('start_type', f'{name}/autostart')
 			entry = {
 				'service': name,
-				'description': srv.get('description[%s]' % (lang,), srv.get('description')),
+				'description': srv.get(f'description[{lang}]', srv.get('description')),
 				'autostart': ucr.get(key, 'yes'),
 				'isRunning': srv.running,
 			}
@@ -77,24 +80,24 @@ class Instance(Base):
 		return result
 
 	@sanitize(StringSanitizer(required=True))
-	def start(self, request):
+	def start(self, request: 'Request') -> None:
 		func = notifier.Callback(self._change_services, request.options, 'start')
 		thread = notifier.threads.Simple('services', func, notifier.Callback(self.thread_finished_callback, request))
 		thread.run()
 
 	@sanitize(StringSanitizer(required=True))
-	def stop(self, request):
+	def stop(self, request: 'Request') -> None:
 		func = notifier.Callback(self._change_services, request.options, 'stop')
 		thread = notifier.threads.Simple('services', func, notifier.Callback(self.thread_finished_callback, request))
 		thread.run()
 
 	@sanitize(StringSanitizer(required=True))
-	def restart(self, request):
+	def restart(self, request: 'Request') -> None:
 		func = notifier.Callback(self._change_services, request.options, 'restart')
 		thread = notifier.threads.Simple('services', func, notifier.Callback(self.thread_finished_callback, request))
 		thread.run()
 
-	def _change_services(self, services, action):
+	def _change_services(self, services: List[str], action: str) -> Dict:
 		error_messages = []
 		srvs = ServiceInfo()
 		for srv in services:
@@ -102,7 +105,7 @@ class Instance(Base):
 			try:
 				getattr(service, action)()
 			except ServiceError as exc:
-				MODULE.warn('Error during %s of %s: %s' % (action, srv, exc))
+				MODULE.warn(f'Error during {action} of {srv}: {exc}')
 				error_messages.append('%s\n%s' % ({
 					'start': _('Starting the service %s failed:'),
 					'stop': _('Stopping the service %s failed:'),
@@ -114,24 +117,24 @@ class Instance(Base):
 		return {'success': True}
 
 	@sanitize(StringSanitizer(required=True))
-	def start_auto(self, request):
+	def start_auto(self, request: 'Request') -> None:
 		self._change_start_type(request.options, 'yes')
 		self.finished(request.id, {'success': True}, _('Successfully changed start type'))
 
 	@sanitize(StringSanitizer(required=True))
-	def start_manual(self, request):
+	def start_manual(self, request: 'Request') -> None:
 		self._change_start_type(request.options, 'manually')
 		self.finished(request.id, {'success': True}, _('Successfully changed start type'))
 
 	@sanitize(StringSanitizer(required=True))
-	def start_never(self, request):
+	def start_never(self, request: 'Request') -> None:
 		self._change_start_type(request.options, 'no')
 		self.finished(request.id, {'success': True}, _('Successfully changed start type'))
 
-	def _change_start_type(self, service_names, start_type):
+	def _change_start_type(self, service_names: str, start_type: Any) -> None:
 		service_info = ServiceInfo()
 		services = [(service_name, service_info.services[service_name]) for service_name in service_names if service_name in service_info.services]
-		values = ['%s=%s' % (service.get('start_type', '%s/autostart' % (service_name,)), start_type) for service_name, service in services]
+		values = ['%s=%s' % (service.get('start_type', f'{service_name}/autostart'), start_type) for service_name, service in services]
 		univention.config_registry.handler_set(values)
 		failed = [x for x in service_names if not service_info.services.get(x)]
 		if failed:
