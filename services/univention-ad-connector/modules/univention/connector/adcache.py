c76a6f9974b8ac6fc4812c3ca6457cd53c6c7f87
Florian Best <best@univention.de>
2021-01-27 19:52:50 +0100

    Bug #52044: hide noisy unrelevant debug messages

--- services/univention-ad-connector/modules/univention/connector/adcache.py
+++ services/univention-ad-connector/modules/univention/connector/adcache.py
@@ -147,17 +147,17 @@ class ADCache(object):
 				cur = self._dbcon.cursor()
 				for sql_command in sql_commands:
 					if isinstance(sql_command, tuple):
-						ud.debug(ud.LDAP, ud.INFO, "ADCache: Execute SQL command: '%s', '%s'" % (sql_command[0], sql_command[1]))
+						ud.debug(ud.LDAP, ud.ALL, "ADCache: Execute SQL command: '%s', '%s'" % (sql_command[0], sql_command[1]))
 						cur.execute(sql_command[0], sql_command[1])
 					else:
-						ud.debug(ud.LDAP, ud.INFO, "ADCache: Execute SQL command: '%s'" % sql_command)
+						ud.debug(ud.LDAP, ud.ALL, "ADCache: Execute SQL command: '%s'" % sql_command)
 						cur.execute(sql_command)
 				self._dbcon.commit()
 				if fetch_result:
 					rows = cur.fetchall()
 				cur.close()
 				if fetch_result:
-					ud.debug(ud.LDAP, ud.INFO, "ADCache: Return SQL result: '%s'" % rows)
+					ud.debug(ud.LDAP, ud.ALL, "ADCache: Return SQL result: '%s'" % rows)
 					return rows
 				return None
 			except sqlite3.Error as exp:
