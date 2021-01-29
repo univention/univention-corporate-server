7315777a85e6493d7dfcac26c3a18da116ce239a
Philipp Hahn <hahn@univention.de>
2021-01-29 13:48:48 +0100

    refactor[ucr]: Extract saving code
    extract code to save file into separate function, which is called in two
    places.
    
    Bug #38938

--- base/univention-config-registry/python/univention/config_registry/backend.py
+++ base/univention-config-registry/python/univention/config_registry/backend.py
@@ -591,14 +591,7 @@ class _ConfigRegistry(dict):
 				file_stat = os.stat_result((0o0644, -1, -1, -1, 0, 0, -1, -1, -1, -1))
 
 			# open temporary file for writing
-			with open(temp_filename, 'w', encoding='utf-8') as reg_file:
-				# write data to file
-				reg_file.write(u'# univention_ base.conf\n\n')
-				reg_file.write(self.__unicode__())
-				# flush (meta)data
-				reg_file.flush()
-				os.fsync(reg_file.fileno())
-
+			self.__save_to(temp_filename)
 			try:
 				os.chmod(temp_filename, file_stat.st_mode)
 				os.chown(temp_filename, file_stat.st_uid, file_stat.st_gid)
@@ -613,13 +606,26 @@ class _ConfigRegistry(dict):
 					# function was already moved by a concurrent UCR
 					# operation. Dump the current state to a backup file
 					temp_filename = '%s.concurrent_%s' % (filename, time.time())
-					with open(temp_filename, 'w', encoding='utf-8') as reg_file:
-						reg_file.write(u'# univention_ base.conf\n\n')
-						reg_file.write(self.__unicode__())
+					self.__save_to(temp_filename)
 		except EnvironmentError as ex:
 			# suppress certain errors
 			if ex.errno != errno.EACCES:
 				raise
+
+	def __save_to(self, filename):
+		# type: (str) -> None
+		"""
+		Serialize sub registry to file.
+
+		:param filename: File name for saving.
+		"""
+		with open(filename, 'w', encoding='utf-8') as fd:
+			# write data to file
+			fd.write(u'# univention_ base.conf\n\n')
+			fd.write(self.__unicode__())
+			# flush (meta)data
+			fd.flush()
+			os.fsync(fd.fileno())
 
 	def save(self):
 		# type: () -> None
