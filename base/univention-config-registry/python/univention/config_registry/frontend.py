dc703113412af5e4039f89f2696c8d500135598e
Philipp Hahn <hahn@univention.de>
2021-01-29 13:48:48 +0100

    refactor[ucr]: Fix utf-8 encoding
    Use io.open(encoding='utf-8') for forward compatibility with Python 3
    
    Bug #51126
    Fixes: 1d891d2d5f4c723cfe2258cd8650e00343c3ba27

--- base/univention-config-registry/python/univention/config_registry/frontend.py
+++ base/univention-config-registry/python/univention/config_registry/frontend.py
@@ -40,6 +40,8 @@ import re
 import time
 
 import six
+if six.PY2:
+	from io import open
 
 from univention.config_registry.backend import exception_occured, SCOPE, ConfigRegistry
 from univention.config_registry.handler import run_filter, ConfigHandlers
@@ -73,10 +75,6 @@ __all__ = [
 REPLOG_FILE = '/var/log/univention/config-registry.replog'
 
 _SHOW_EMPTY, _SHOW_DESCRIPTION, _SHOW_SCOPE, _SHOW_CATEGORIES, _SHOW_DEFAULT = (1 << _ for _ in range(5))
-
-encoding = {}
-if six.PY3:
-	encoding['encoding'] = 'UTF-8'  # make sure we open files in UTF-8, even if the locale is "C"
 
 
 class UnknownKeyException(Exception):
@@ -123,7 +121,7 @@ def replog(ucr, var, old_value, value=None):
 			if not os.path.isfile(REPLOG_FILE):
 				os.close(os.open(REPLOG_FILE, os.O_CREAT, 0o640))
 
-			with open(REPLOG_FILE, "a+", **encoding) as logfile:
+			with open(REPLOG_FILE, "a+", encoding='utf-8') as logfile:
 				logfile.write(log)
 		except EnvironmentError as ex:
 			print(("E: exception occurred while writing to replication log: %s" % (ex,)), file=sys.stderr)
