396797e473152e651e2a3c87aa0fe47a0c44c022
Johannes Keiser <keiser@univention.de>
2021-02-01 11:11:26 +0100

    Revert "Bug #52453: cleanup"
    This reverts commit 59357e55dd386ac20702811623dae358541773d6.

--- management/univention-management-console/www/management/main.js
+++ management/univention-management-console/www/management/main.js
@@ -719,13 +719,13 @@ define([
 
 					this._updateMoreTabsVisibility();
 
-					aspectHandlesMap[module.id] = module.watch('title', lang.hitch(this, function(_name, _oldVal, newVal) {
+					aspectHandlesMap[module.id] = aspect.after(module, '_setTitleAttr', lang.hitch(this, function(label) {
 						var menuItemToUpdate = array.filter(this._moreTabsDropDownButton.dropDown.getChildren(), function(menuItem) {
 							return menuItem.correspondingModuleID === module.id;
 						})[0];
-						menuItemToUpdate.set('label', newVal);
+						menuItemToUpdate.set('label', label);
 						this._updateMoreTabsVisibility();
-					}));
+					}), true);
 				}
 			}));
 			this._tabController.on('removeChild', lang.hitch(this, function(module) {
