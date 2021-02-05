5c96323a37148c28277c90bc37d47127e2bc4132
Philipp Hahn <hahn@univention.de>
2021-02-05 15:45:00 +0100

    chore[test]: Rewrite 03_ucr/37check-ucr-templates
    to allow skipping Python 3 only templates
    
    Use difflib instead of external diff.
    
    Replace shell=True with Python equivalent
    
    Improve performance by doing `dpkg -S` once.
    
    Bug #52740: Updater for UCS-5
    
    chore[test]: Reformat test for readability
    
    Bug #52740: Updater for UCS-5

--- test/ucs-test/tests/03_ucr/37check-ucr-templates-py3-migration-status.py
+++ test/ucs-test/tests/03_ucr/37check-ucr-templates-py3-migration-status.py
@@ -4,13 +4,14 @@ from __future__ import print_function
 
 import os
 import sys
-import pipes
 import tempfile
 import subprocess
 import json
 import glob
+from difflib import unified_diff
 
 import pytest
+import py.path
 
 from univention.config_registry.handler import run_filter, EXECUTE_TOKEN
 from univention.config_registry import ConfigRegistry
@@ -18,11 +19,8 @@ from univention.config_registry import ConfigRegistry
 ucr = ConfigRegistry()
 ucr.load()
 
-SCRIPT = r'''#!/bin/bash
-cat < /dev/stdin > /tmp/py{0}.in
-python{0} -m coverage run /tmp/py{0}.in &>/tmp/py{0}.out
-echo $? >> /tmp/py{0}.out
-'''
+VERSIONS = (2, 3)
+BASE_DIR = "/etc/univention/templates/files/etc"
 
 ALLOWED_DIFFERENCES = [
 	'/etc/univention/templates/files/usr/share/univention-management-console/meta.json',
@@ -35,63 +33,123 @@ ALLOWED_DIFFERENCES = [
 	'/etc/univention/templates/files/var/lib/dovecot/sieve/default.sieve',  # different date if not commited in the same second
 	'/etc/univention/templates/files/usr/share/univention-management-console/i18n/de/apps.mo',  # po-lib adds date
 ] + glob.glob('/etc/univention/templates/files/etc/logrotate.d/*') + glob.glob('/etc/univention/templates/files/etc/security/*.conf')
+IGNORE = {
+	# python-univention-updater is Python 3 only
+	'/etc/univention/templates/files/etc/apt/apt.conf.d/55user_agent': {2},
+	'/etc/univention/templates/files/etc/apt/mirror.list': {2},
+	'/etc/univention/templates/files/etc/apt/sources.list.d/15_ucs-online-version.list': {2},
+	'/etc/univention/templates/files/etc/apt/sources.list.d/20_ucs-online-component.list': {2},
+}
+
+SCRIPT = r'''#!/bin/sh
+cat >{1[tmp]}
+python{0} -m coverage run {1[tmp]} >{1[out]} 2>&1
+exec echo "$?" >{1[ret]}
+'''
+
+
+@pytest.fixture(scope='session')
+def tmpfile(request):
+	tmpdir = py.path.local(tempfile.mkdtemp())
+	request.addfinalizer(lambda: tmpdir.remove(rec=1))
+	return lambda pyver, suffix: tmpdir.join("ucr{0}.{1}".format(pyver, suffix))
 
 
 @pytest.fixture(scope="module")
-def python_versions():
-	try:
-		with tempfile.NamedTemporaryFile(delete=False, suffix='.py') as fd1, tempfile.NamedTemporaryFile(delete=False, suffix='.py') as fd2:
-			for pyversion, fd in (('2', fd1), ('3', fd2)):
-				fd.write(SCRIPT.format(pyversion, os.path.basename(fd.name)).encode('utf-8'))
-				fd.close()
-				os.chmod(fd.name, 0o755)
-			yield (('2', fd1), ('3', fd2))
-	finally:
-		for fd in (fd1, fd2):
-			os.unlink(fd.name)
+def python_versions(tmpfile):
+	result = [
+		(pyver, dict((suf, tmpfile(pyver, suf)) for suf in ("py", "tmp", "out", "ret")))
+		for pyver in VERSIONS
+	]
+	for (pyver, fn) in result:
+		fn["py"].write(SCRIPT.format(pyver, fn))
+		fn["py"].chmod(0o755)
+
+	return result
 
 
 def pytest_generate_tests(metafunc):
-	tempfiles = [os.path.join(path, filename) for path, dirs, files in os.walk('/etc/univention/templates/files/') for filename in files]
-	tempfiles = [pytest.param(path, marks=pytest.mark.xfail) if path in ALLOWED_DIFFERENCES else path for path in tempfiles if EXECUTE_TOKEN.search(open(path, 'rb').read())]
+	tempfiles = [
+		pytest.param(path, marks=pytest.mark.xfail) if path in ALLOWED_DIFFERENCES else path
+		for path in (
+			os.path.join(path, filename)
+			for path, dirs, files in os.walk(BASE_DIR)
+			for filename in files
+		) if EXECUTE_TOKEN.search(open(path, 'rb').read())
+	]
 	metafunc.parametrize('ucr_config_file', tempfiles)
 
 
-def test_configfile_python_compatibility(ucr_config_file, python_versions):
+@pytest.fixture(scope='session')
+def dpkg():
+	etc = {}
+	cmd_dpkg = ["dpkg", "-S", os.path.join(BASE_DIR, '*')]
+	proc = subprocess.Popen(cmd_dpkg, stdout=subprocess.PIPE)
+	assert proc.stdout
+	for line in proc.stdout:
+		pkg, fn = line.decode('UTF-8', 'replace').strip().split(': ')
+		etc[fn] = pkg
+	assert not proc.wait()
+	return etc
+
+
+def test_configfile_python_compatibility(ucr_config_file, python_versions, dpkg):
 	with open(ucr_config_file) as fd:
 		template = fd.read()
 
-	result = {}
-	for pyversion, fd in python_versions:
-		sys.executable = fd.name
+	msg = []
+	python = {}
+	for (pyver, fn) in python_versions:
+		sys.executable = fn["py"]
 		run_filter(template, ucr)
 
-		with open('/tmp/py{0}.out'.format(pyversion), 'rb') as x:
-			compiled, _, rc = x.read().decode('ISO8859-1').rstrip('\n').rpartition('\n')
-			result[pyversion] = {
-				'success': rc == '0',
-				'compiled': compiled,
-				'coverage': subprocess.check_output(r'python{0} -m coverage report | grep /tmp/py{0}.in | egrep -o [0-9]+%\$ || echo 0%'.format(pyversion), shell=True).decode('UTF-8', 'replace')
-			}
-			if ucr_config_file.endswith('.json'):
-				try:
-					result[pyversion]['compiled'] = json.dumps(json.loads(result[pyversion]['compiled']), sort_keys=True)
-				except ValueError:
-					result[pyversion]['json_failed'] = True
-			if os.path.exists('.coverage'):
-				os.unlink('.coverage')
+		data = fn["out"].read_text('ISO8859-1').rstrip('\n')
+		ret = int(fn["ret"].read().strip())
 
-	print('', ucr_config_file, end=':\n', sep='\n')
-	for key, res in result.items():
-		print('Python%s: ' % (key,), end='')
-		print('|%s' % ('✅' if res['success'] else '❎'), end='')
-		print('|%s' % (res['coverage'],), end='')
+		cmd_ucr = ["python{0}".format(pyver), "-m", "coverage", "report"]
+		cov = subprocess.check_output(cmd_ucr).decode('UTF-8', 'replace')
+		try:
+			line, = [line for line in cov.splitlines() if str(fn["tmp"]) in line]
+			_name, _stmts, _miss, coverage = line.split()
+		except ValueError:
+			coverage = "?"
 
-	result = {
-		'python': result,
-		'package': subprocess.check_output('dpkg -S %s 2>/dev/null | cut -d: -f1' % (pipes.quote(ucr_config_file),), shell=True).decode('UTF-8', 'replace'),
-		'diff': subprocess.check_output('diff -u /tmp/py[23].out || true', shell=True).decode('UTF-8', 'replace').replace('/tmp/py', ucr_config_file),
+		python[pyver] = {
+			'success': ret == 0,
+			'compiled': data,
+			'coverage': coverage,
+		}
+		if ucr_config_file.endswith('.json'):
+			try:
+				python[pyver]['compiled'] = json.dumps(json.loads(data), sort_keys=True)
+			except ValueError:
+				python[pyver]['json_failed'] = True
+
+		if os.path.exists('.coverage'):
+			os.unlink('.coverage')
+
+		msg.append('Py%s:|%s|%s' % (
+			pyver,
+			'✅' if ret == 0 else '❎',
+			coverage,
+		))
+
+	print('\t'.join(msg), end='\t')
+
+	try:
+		diff = ''.join(unified_diff(
+			*(python[pyver]["compiled"].splitlines(keepends=True) for pyver in VERSIONS),
+			*(str(pyver) for pyver in VERSIONS),
+		))
+	except LookupError:
+		diff = ""
+
+	details = {
+		'python': python,
+		'package': dpkg.get(ucr_config_file, ""),
+		'diff': diff,
 	}
 
-	assert all(res['success'] for res in result['python'].values()), result
-	assert result['python']['2']['compiled'] == result['python']['3']['compiled'], result
+	ignore = IGNORE.get(ucr_config_file, set())
+	assert all(res['success'] for pyver, res in python.items() if pyver not in ignore), details
+	assert len({res['compiled'] for pyver, res in python.items() if pyver not in ignore}) == 1, details
