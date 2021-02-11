univention/univention-corporate-server

commit 0e037a745344f95a65ffb825b288e3d02c4ff145

Bug #51316 up: Cache releases.json
Download the file once and use the cached data.

Add parameters to specify start and end limit to reduce code
duplication.

base/univention-updater/modules/univention/updater/tools.py
@@ -981,26 +981,33 @@ class UniventionUpdater(object):
         user_agent = self._get_user_agent_string()
         UCSHttpServer.load_credentials(self.configRegistry)
 
-        # Auto-detect prefix
         self.server = UCSHttpServer(
             baseurl=self.repourl,
             user_agent=user_agent,
             timeout=self.timeout,
         )
+        self._get_releases()
+
+    def _get_releases(self):
+        # type: () -> None
+        """
+        Detect server prefix and download `releases.json` file.
+        """
         try:
             if not self.repourl.path:
                 try:
-                    assert self.server.access(None, '/univention-repository/')
+                    _code, _size, data = self.server.access(None, '/univention-repository/releases.json', get=True)
                     self.server += '/univention-repository/'
                     self.log.info('Using detected prefix /univention-repository/')
+                    self.releases = json.loads(data)
                 except DownloadError as e:
                     self.log.info('No prefix /univention-repository/ detected, using /')
                     ud.debug(ud.NETWORK, ud.ALL, "%s" % e)
-                return  # already validated or implicit /
             # Validate server settings
             try:
-                assert self.server.access(None, '')
+                _code, _size, data = self.server.access(None, 'releases.json', get=True)
                 self.log.info('Using configured prefix %s', self.repourl.path)
+                self.releases = json.loads(data)
             except DownloadError as e:
                 self.log.error('Failed configured prefix %s', self.repourl.path, exc_info=True)
                 uri, code = e.args
@@ -1009,30 +1016,33 @@ class UniventionUpdater(object):
             if self.check_access:
                 self.log.fatal('Failed server detection: %s', e, exc_info=True)
                 raise
-
-    @staticmethod
-    def get_releases(server):
-        # type: (_UCSServer) -> Iterator[UCS_Version]
-        try:
-            _code, _size, data = server.access(None, 'releases.json', get=True)
-        except DownloadError as e:
-            ud.debug(ud.NETWORK, ud.ALL, "%s" % e)
-            return
-        try:
-            releases = json.loads(data)['releases']
-        except (ValueError, KeyError) as exc:
+            self.releases = {"error": str(e)}
+        except (ValueError, LookupError) as exc:
             ud.debug(ud.NETWORK, ud.ERROR, 'Querying maintenance information failed: %s' % (exc,))
-            if isinstance(server, UCSHttpServer) and server.proxy_handler.proxies:
-                ud.debug(ud.NETWORK, ud.WARN, 'Maintenance information malformed, blocked by proxy?')
-            return
-        for major_release in releases:
+            self.releases = {"error": str(exc)}
+
+    def get_releases(self, start=None, end=None):
+        # type: (Optional[UCS_Version], Optional[UCS_Version]) -> Iterator[Tuple[UCS_Version, Dict[str, Any]]]
+        """
+        Return UCS releases in range.
+
+        :param start: Minimum requried version.
+        :param end: Maximum allowed version.
+        :returns: Iterator of 2-tuples (UCS_Version, data).
+        """
+        for major_release in self.releases.get('releases', []):
             for minor_release in major_release['minors']:
                 for patchlevel_release in minor_release['patchlevels']:
-                    yield UCS_Version((
+                    ver = UCS_Version((
                         major_release['major'],
                         minor_release['minor'],
                         patchlevel_release['patchlevel']
                     ))
+                    if start and ver < start:
+                        continue
+                    if end and ver > end:
+                        continue
+                    yield (ver, dict(patchlevel_release, major=major_release['major'], minor=minor_release['minor']))
 
     def get_next_version(self, version, components=[], errorsto='stderr'):
         # type: (UCS_Version, Iterable[str], Literal["stderr", "exception", "none"]) -> Optional[UCS_Version]
@@ -1049,7 +1059,7 @@ class UniventionUpdater(object):
         :raises RequiredComponentError: if a required component is missing
         """
         debug = (errorsto == 'stderr')
-        releases = list(self.get_releases(self.server))
+        releases = [ver for ver, _data in self.get_releases() if ver > version)
 
         for ver in (
             UCS_Version((version.major, version.minor, version.patchlevel + 1)),
@@ -1504,7 +1514,7 @@ class UniventionUpdater(object):
         :returns: A iterator returning 2-tuples (server, ver).
         """
         self.log.info('Searching releases [%s..%s), dists=%s', start, end, dists)
-        releases = sorted(r for r in self.get_releases(self.server) if r >= start and r <= end)
+        releases = sorted(ver for ver, _data in self.get_releases(start, end))
         for release in releases:
             yield self.server, UCSRepoPool5(
                 major=release['major'],

base/univention-updater/tests/test_updater.py
@@ -36,6 +36,8 @@ class TestUniventionUpdater(unittest.TestCase):
         """Fill URI mockup."""
         for key, value in uris.items():
             MockUCSHttpServer.mock_add(key, value)
+            if 'releases.json' in key:
+                self.u._get_releases()
 
     def tearDown(self):
         """Clean up Updater mockup."""
@@ -71,6 +73,14 @@ class TestUniventionUpdater(unittest.TestCase):
         self.assertEqual(ERRAT, self.u.erratalevel)
         self.assertEqual(MAJOR, self.u.version_major)
         self.assertEqual(MINOR, self.u.version_minor)
+
+    def test_get_releases(self):
+        self._uri({
+            RJSON: gen_releases([(MAJOR, minor, patch) for minor in range(3) for patch in range(3)])
+        })
+        expected = [(U.UCS_Version((MAJOR, 1, patch)), dict(major=MAJOR, minor=1, patchlevel=patch, status="maintained")) for patch in range(3)]
+        found = list(self.u.get_releases(start=expected[0][0], end=expected[-1][0]))
+        self.assertEqual(expected, found)
 
     def test_get_next_version(self):
         """Test no next version."""

base/univention-updater/conffiles/etc/apt/mirror.list
@@ -31,12 +31,11 @@ try:
 		print('#   univention-config-registry set repository/mirror=true')
 		print('')
 	else:
-		for rel in mirror.get_releases(mirror.server):
-			if mirror.version_start <= rel <= mirror.version_end:
-				pool = UCSRepoPool5(rel)
-				for pool.errata in (False, True):
-					for typ in ["deb"] + ["deb-src"][:mirror.sources]:
-						print(pool.deb(mirror.server, typ, mirror=True))
+		for rel, _data in mirror.get_releases(mirror.version_start, mirror.version_end):
+			pool = UCSRepoPool5(rel)
+			for pool.errata in (False, True):
+				for typ in ["deb"] + ["deb-src"][:mirror.sources]:
+					print(pool.deb(mirror.server, typ, mirror=True))
 
 		print("clean %s" % (mirror.server.baseurl.public(),))
 

base/univention-updater/tests/test_mirror.py
@@ -28,6 +28,7 @@ class TestUniventionMirror(unittest.TestCase):
             # 'univention-repository/': '',
             '': b'',
             '/': b'',
+            'releases.json': b'{"releases":[]}',
         })
         self.base_dir = mkdtemp()
         self.mock_file = MockFile(os.path.join(self.base_dir, 'mock'))
@@ -118,6 +119,7 @@ class TestUniventionMirror(unittest.TestCase):
             'releases.json': gen_releases([(MAJOR, MINOR, 0), ])
         }
         self._uri(uris)
+        self.m._get_releases()
         del uris['releases.json']
         self.m.mirror_update_scripts()
         for key, value in uris.items():
@@ -207,6 +209,7 @@ class TestUniventionMirrorList(unittest.TestCase):
             '': b'',
             '/': b'',
             'univention-repository/': b'',
+            'releases.json': b'{"releases":[]}',
         })
         self.m = M.UniventionMirror()
         repos = (

base/univention-updater/umc/python/updater/__init__.py
@@ -40,10 +40,7 @@ from hashlib import md5
 import subprocess
 import psutil
 import pipes
-import json
-import requests
 from datetime import datetime
-from urlparse import urljoin
 
 import notifier.threads
 
@@ -56,7 +53,6 @@ from univention.management.console.modules.decorators import simple_response, sa
 from univention.management.console.modules.sanitizers import ChoicesSanitizer, StringSanitizer, IntegerSanitizer, ListSanitizer
 
 from univention.updater.tools import UniventionUpdater
-from univention.updater.repo_url import UcsRepoUrl
 from univention.updater.errors import RequiredComponentError, UpdaterException
 
 try:
@@ -239,52 +235,27 @@ class Instance(Base):
 		return ret
 
 	def _maintenance_information(self):  # type: () -> Dict[str, Any]
-		ucr.load()
-		releases = {}
 		default = {'show_warning': False}
-		if ucr.is_true('license/extended_maintenance/disable_warning') or not self.uu:
+		if not self.uu:
+			return default
+
+		ucr.load()
+		if ucr.is_true('license/extended_maintenance/disable_warning'):
 			return default
 
 		version = self.uu.current_version
-		url = urljoin(UcsRepoUrl(ucr, 'repository/online').private(), 'releases.json')
-		try:
-			if url.startswith('file://'):
-				with open(url, 'r') as releases_fd:
-					releases = json.load(releases_fd)
-			else:
-				response = requests.get(url, timeout=10)
-				if not response.ok:
-					response.raise_for_status()
-				releases = response.json()
-		except (requests.exceptions.RequestException, EnvironmentError) as exc:
-			MODULE.error("Querying maintenance information failed: %s" % (exc,))
-		except ValueError as exc:
-			MODULE.error('The JSON format is malformed: %s' % (exc,))
-		try:
-			for majors in releases['releases']:
-				if majors['major'] != version.major:
-					continue
-				for minors in majors["minors"]:
-					if minors['minor'] != version.minor:
-						continue
-					for patchlevel in minors["patchlevels"]:
-						if patchlevel['patchlevel'] != version.patchlevel:
-							continue
+		for _ver, data in self.uu.get_releases(version, version):
+			status = data.get('status', 'unmaintained')
 
-						_maintained_status = patchlevel.get('status', 'unmaintained')
-						maintenance_extended = _maintained_status == 'extended'
-						show_warning = maintenance_extended or _maintained_status != 'maintained'
+			maintenance_extended = status == 'extended'
+			show_warning = maintenance_extended or status != 'maintained'
 
-						return {
-							'ucs_version': str(version),
-							'show_warning': show_warning,
-							'maintenance_extended': maintenance_extended,
-							'base_dn': ucr.get('license/base')
-						}
-					break
-				break
-		except KeyError as exc:
-			MODULE.error('The JSON format is missing keys: %s' % (exc,))
+			return {
+				'ucs_version': str(version),
+				'show_warning': show_warning,
+				'maintenance_extended': maintenance_extended,
+				'base_dn': ucr.get('license/base'),
+			}
 
 		return default
 

commit e7e236013fc64a895627cda5016c6a348a01b102

fix[up] Initialize releases when offline
<https://jenkins.knut.univention.de:8181/job/UCS-5.0/job/UCS-5.0-0/job/ADConnectorMultiEnv/Version=w2k19-english/ws/test/updater.log.1.gz>
> Traceback (most recent call last):
>   File "/usr/lib/python3/dist-packages/univention/updater/scripts/upgrade.py", line 442, in do_update
>     update_available = performUpdate(options, checkForUpdates=options.check, silent=False)
>   File "/usr/lib/python3/dist-packages/univention/updater/scripts/upgrade.py", line 139, in performUpdate
>     if func(options, checkForUpdates, silent):
>   File "/usr/lib/python3/dist-packages/univention/updater/scripts/upgrade.py", line 152, in do_release_update
>     version_next = updater.release_update_available()
>   File "/usr/lib/python3/dist-packages/univention/updater/tools.py", line 1413, in release_update_available
>     return self.get_next_version(UCS_Version(ucs_version), components, errorsto)
>   File "/usr/lib/python3/dist-packages/univention/updater/tools.py", line 1350, in get_next_version
>     ver = min(ver for ver, _data in self.get_releases() if ver > version)
>   File "/usr/lib/python3/dist-packages/univention/updater/tools.py", line 1350, in <genexpr>
>     ver = min(ver for ver, _data in self.get_releases() if ver > version)
>   File "/usr/lib/python3/dist-packages/univention/updater/tools.py", line 1322, in get_releases
>     for major_release in self.releases.get('releases', []):
> AttributeError: 'UniventionUpdater' object has no attribute 'releases'

`updater.releases` is loaded from `releases.json` on instance creation, but is
does not happen when UCRV `repository/online=no`:

modules/univention/updater/scripts/upgrade.py
  148     updater = UniventionUpdater()
modules/univention/updater/tools.py
1206     def __init__(self, check_access=True):
1220         self.ucr_reinit()
1234     def ucr_reinit(self):
1264         if not self.online_repository:
1267             return
1278         self._get_releases()
1280     def _get_releases(self):
1300                 self.releases = json.loads(data)
1309             self.releases = {"error": str(e)}
1312             self.releases = {"error": str(exc)}

Fixes: 0e037a74534

base/univention-updater/modules/univention/updater/tools.py
@@ -1264,6 +1264,7 @@ class UniventionUpdater(object):
         if not self.online_repository:
             self.log.info('Disabled')
             self.server = UCSLocalServer('')  # type: _UCSServer
+            self.releases = {"error": "offline"}
             return
 
         # generate user agent string

base/univention-updater/tests/test_updater.py
@@ -67,6 +67,15 @@ class TestUniventionUpdater(object):
         assert not u.is_repository_server
         assert ERRAT == u.erratalevel
 
+    def test_ucs_reinit_offline(self, ucr, u):
+        ucr({
+            'repository/online': 'no',
+        })
+        u.ucr_reinit()
+        assert not u.online_repository
+        assert isinstance(u.server, U.UCSLocalServer)
+        assert u.releases["error"]
+
     def test_get_releases(self, u, http):
         http({
             RJSON: gen_releases([(MAJOR, minor, patch) for minor in range(3) for patch in range(3)]),