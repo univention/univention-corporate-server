univention/univention-corporate-server

commit 62ce9d387cbfee1f8f6cc06622dae6565070dc78

Bug #51316 up: Generate stable output
Make components sortable by name for stable UCR output.

base/univention-updater/modules/univention/updater/tools.py
@@ -867,6 +867,27 @@ class Component(object):
         self.updater = updater
         self.name = name
 
+    def __lt__(self, other):
+        return self.name < other.name if isinstance(other, Component) else NotImplementedError
+
+    def __le__(self, other):
+        return self.name <= other.name if isinstance(other, Component) else NotImplementedError
+
+    def __eq__(self, other):
+        return isinstance(other, Component) and self.name == other.name
+
+    def __ne__(self, other):
+        return not isinstance(other, Component) or self.name != other.name
+
+    def __ge__(self, other):
+        return self.name >= other.name if isinstance(other, Component) else NotImplementedError
+
+    def __gt__(self, other):
+        return self.name > other.name if isinstance(other, Component) else NotImplementedError
+
+    def __hash__(self):
+        return hash(self.name)
+
     def __str__(self):
         # type: () -> str
         return "Component({0.name})".format(self)
@@ -1116,7 +1137,7 @@ class Component(object):
         """
         for server, struct in self.versions(start, end, for_mirror_list):
             try:
-                for struct.arch in struct.ARCHS:
+                for struct.arch in sorted(struct.ARCHS):
                     assert server.access(struct, "Packages.gz")
                     yield struct.deb(server)
 
@@ -1567,7 +1588,7 @@ class UniventionUpdater(object):
 
         result = []  # type: List[str]
         failed = set()  # type: Set[Tuple[Component, str]]
-        for comp in self.get_components(only_localmirror_enabled=for_mirror_list):
+        for comp in sorted(self.get_components(only_localmirror_enabled=for_mirror_list)):
             result += comp.repositories(start, end, clean=clean, for_mirror_list=for_mirror_list, failed=failed)
         result += ["# Component %s: %s" % (comp.name, ex) for comp, ex in failed]
 

base/univention-updater/tests/test_updater.py
@@ -664,6 +664,31 @@ class TestComponents(object):
             'deb https://updates.software-univention.de/%d.%d/maintained/component/a/ ./' % (MAJOR, MINOR),
         } == set(a_repo)
 
+    def test_cmp(u):
+        a, b, bb = (U.Component(u, c) for c in "abb")
+        assert a < b
+        assert a <= b
+        assert b <= b
+        assert b <= bb
+        assert b == b
+        assert b == bb
+        assert a != b
+        assert b >= bb
+        assert b >= b
+        assert b >= a
+        assert b > a
+        assert a.__lt__(None) is NotImplementedError
+        assert a.__le__(None) is NotImplementedError
+        assert a.__eq__(None) is False
+        assert a.__ne__(None) is True
+        assert a.__ge__(None) is NotImplementedError
+        assert a.__gt__(None) is NotImplementedError
+
+    def test_hash(u):
+        a, b, bb = (U.Component(u, c) for c in "abb")
+        assert hash(a) != hash(b)
+        assert hash(b) == hash(bb)
+
 
 @pytest.mark.parametrize('prefix', ["", "/p"])
 def test_UCSHttpServer(prefix):

base/univention-updater/conffiles/etc/apt/sources.list.d/20_ucs-online-component.list
@@ -10,7 +10,7 @@ try:
 
 	if updater.online_repository:
 		ver = updater.current_version
-		for comp in updater.get_components():
+		for comp in sorted(updater.get_components()):
 			try:
 				for repo in comp.repositories(ver, ver):
 					print(repo)

commit 0c70d6bfe7b8d953f4901da3e7ca10ec9c9bfdb3

Bug #51316 up: Change NotImplemented{Error() -> }
Fixes: 62ce9d387cb

base/univention-updater/modules/univention/updater/tools.py
@@ -868,10 +868,10 @@ class Component(object):
         self.name = name
 
     def __lt__(self, other):
-        return self.name < other.name if isinstance(other, Component) else NotImplementedError
+        return self.name < other.name if isinstance(other, Component) else NotImplemented
 
     def __le__(self, other):
-        return self.name <= other.name if isinstance(other, Component) else NotImplementedError
+        return self.name <= other.name if isinstance(other, Component) else NotImplemented
 
     def __eq__(self, other):
         return isinstance(other, Component) and self.name == other.name
@@ -880,10 +880,10 @@ class Component(object):
         return not isinstance(other, Component) or self.name != other.name
 
     def __ge__(self, other):
-        return self.name >= other.name if isinstance(other, Component) else NotImplementedError
+        return self.name >= other.name if isinstance(other, Component) else NotImplemented
 
     def __gt__(self, other):
-        return self.name > other.name if isinstance(other, Component) else NotImplementedError
+        return self.name > other.name if isinstance(other, Component) else NotImplemented
 
     def __hash__(self):
         return hash(self.name)

base/univention-updater/tests/test_updater.py
@@ -677,12 +677,12 @@ class TestComponents(object):
         assert b >= b
         assert b >= a
         assert b > a
-        assert a.__lt__(None) is NotImplementedError
-        assert a.__le__(None) is NotImplementedError
+        assert a.__lt__(None) is NotImplemented
+        assert a.__le__(None) is NotImplemented
         assert a.__eq__(None) is False
         assert a.__ne__(None) is True
-        assert a.__ge__(None) is NotImplementedError
-        assert a.__gt__(None) is NotImplementedError
+        assert a.__ge__(None) is NotImplemented
+        assert a.__gt__(None) is NotImplemented
 
     def test_hash(u):
         a, b, bb = (U.Component(u, c) for c in "abb")