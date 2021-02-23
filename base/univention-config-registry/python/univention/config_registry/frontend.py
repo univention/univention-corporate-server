univention/univention-corporate-server

commit 2363f423c47798bcdcbe9e0643d31321a8639e23

Bug #38938 UCR: add layer for default UCR variables
Introduce a new layer /etc/univention/base-defaults.conf which contains
all UCR default variables from the variable definitions in
/etc/univention/registry.info/variables/*.cfg.

The values are written on ucr register and ucr unregister:
aka: in the postinst of every package via univention-install-config-registry

base/univention-config-registry/python/univention/config_registry/frontend.py
@@ -37,7 +37,7 @@ import os
 import sys
 import re
 import time
-from univention.config_registry.backend import exception_occured, SCOPE, ConfigRegistry
+from univention.config_registry.backend import exception_occured, SCOPE, ConfigRegistry, _DefaultConfigRegistry
 from univention.config_registry.handler import run_filter, ConfigHandlers
 from univention.config_registry.misc import validate_key, escape_value
 from univention.config_registry.filters import filter_shell, filter_keys_only, filter_sort
@@ -306,6 +306,7 @@ def handler_register(args, opts=dict()):
 	# diversion is (re-)done when >= 1.
 	handlers.register(args[0], ucr)
 	# handlers.commit((ucr, {}))
+	_register_variable_default_values(ucr)
 
 
 def handler_unregister(args, opts=dict()):
@@ -323,6 +324,7 @@ def handler_unregister(args, opts=dict()):
 	cur = handlers.update()  # cache must be current
 	obsolete = handlers.unregister(args[0], ucr)
 	handlers.update_divert(cur - obsolete)
+	_register_variable_default_values(ucr)
 
 
 def handler_filter(args, opts=dict()):
@@ -362,11 +364,7 @@ def handler_search(args, opts=dict()):
 			print('E: invalid regular expression: %s' % (ex,), file=sys.stderr)
 			sys.exit(1)
 
-	# Import located here, because on module level, a circular import would be
-	# created
-	import univention.config_registry_info as cri  # pylint: disable-msg=W0403
-	cri.set_language('en')
-	info = cri.ConfigRegistryInfo(install_mode=False)
+	info = _get_config_registry_info()
 
 	category = opts.get('category', None)
 	if category and not info.get_category(category):
@@ -492,11 +490,7 @@ def handler_info(args, opts=dict()):
 	"""
 	ucr = ConfigRegistry()
 	ucr.load()
-	# Import located here, because on module level, a circular import would be
-	# created
-	import univention.config_registry_info as cri  # pylint: disable-msg=W0403
-	cri.set_language('en')
-	info = cri.ConfigRegistryInfo(install_mode=False)
+	info = _get_config_registry_info()
 
 	for arg in args:
 		try:
@@ -608,6 +602,25 @@ def missing_parameter(action):
 	print('E: too few arguments for command [%s]' % (action,), file=sys.stderr)
 	print('try `univention-config-registry --help` for more information', file=sys.stderr)
 	sys.exit(1)
+
+
+def _get_config_registry_info():
+	# Import located here, because on module level, a circular import would be
+	# created
+	import univention.config_registry_info as cri  # pylint: disable-msg=W0403
+	cri.set_language('en')
+	return cri.ConfigRegistryInfo(install_mode=False)
+
+
+def _register_variable_default_values(ucr):
+	"""Create base-default.conf layer containig all default values"""
+	info = _get_config_registry_info()
+	defaults = _DefaultConfigRegistry(ucr, '/etc/univention/base-defaults.conf')
+	for key, variable in info.get_variables().items():
+		value = variable.get('Default')
+		if value:
+			defaults[key] = value
+	defaults.save()
 
 
 HANDLERS = {

base/univention-config-registry/python/univention/config_registry/backend.py
@@ -70,7 +70,7 @@ def exception_occured(out=sys.stderr):
 	sys.exit(1)
 
 
-SCOPE = ['normal', 'ldap', 'schedule', 'forced', 'custom']
+SCOPE = ['normal', 'ldap', 'schedule', 'forced', 'custom', 'default']
 
 
 if MYPY:
@@ -87,14 +87,15 @@ class ConfigRegistry(MM):
 	:param filename: File name for text database file.
 	:param write_registry: The UCR level used for writing.
 	"""
-	NORMAL, LDAP, SCHEDULE, FORCED, CUSTOM = range(5)
-	LAYER_PRIORITIES = (FORCED, SCHEDULE, LDAP, NORMAL, CUSTOM)
+	NORMAL, LDAP, SCHEDULE, FORCED, CUSTOM, DEFAULTS = range(6)
+	LAYER_PRIORITIES = (FORCED, SCHEDULE, LDAP, NORMAL, CUSTOM, DEFAULTS)
 	PREFIX = '/etc/univention'
 	BASES = {
 		NORMAL: 'base.conf',
 		LDAP: 'base-ldap.conf',
 		SCHEDULE: 'base-schedule.conf',
 		FORCED: 'base-forced.conf',
+		DEFAULTS: 'base-defaults.conf',
 	}
 
 	def __init__(self, filename=None, write_registry=NORMAL):
@@ -127,6 +128,8 @@ class ConfigRegistry(MM):
 			filename = self.file
 		else:
 			filename = os.path.join(ConfigRegistry.PREFIX, ConfigRegistry.BASES[reg])
+		if reg == ConfigRegistry.DEFAULTS:
+			return _DefaultConfigRegistry(self, filename=filename)
 		return _ConfigRegistry(filename=filename)
 
 	def load(self):
@@ -675,5 +678,12 @@ class _ConfigRegistry(dict):
 		if isinstance(data, bytes):
 			data = data.decode('UTF-8')
 		return data
+
+
+class _DefaultConfigRegistry(_ConfigRegistry):
+
+	def __init__(self, parent, filename=None):
+		super(_DefaultConfigRegistry, self).__init__(filename)
+		self.parent = parent
 
 # vim:set sw=4 ts=4 noet:

commit b0bfee07a746ee6e21828ea119719a4ca4569d96

fix[ucr] Re-load default on update
`ucr update` is used to force re-loading all UCR info files.
It should also re-build the default layer.

Bug #38938
Fixes: 2363f423c477

base/univention-config-registry/python/univention/config_registry/frontend.py
@@ -278,6 +278,9 @@ def handler_update(args, opts=dict()):
 	cur = handlers.update()
 	handlers.update_divert(cur)
 
+	ucr = ConfigRegistry()
+	_register_variable_default_values(ucr)
+
 
 def handler_commit(args, opts=dict()):
 	# type: (List[str], Dict[str, Any]) -> None