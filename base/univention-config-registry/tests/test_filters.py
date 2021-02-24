c8185727f5f30c042c54216c50e20b33edf1dd91
Philipp Hahn <hahn@univention.de>
2021-02-24 18:50:11 +0100

    test[ucr] Test filters
    Bug #38938

---
+++ base/univention-config-registry/tests/test_filters.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+"""Unit test for univention.config_registry.filters."""
+# vim:set fileencoding=utf-8:
+
+import pytest
+
+import univention.config_registry.filters as ucrf
+
+
+@pytest.mark.parametrize("text,ret", [
+	([], []),
+	(["key: val"], ["key=val"]),
+	(["other"], ["other=''"]),
+])
+def test_shell(text, ret):
+	assert ucrf.filter_shell(None, text) == ret
+
+
+@pytest.mark.parametrize("text,ret", [
+	([], []),
+	(["key: val"], ["key"]),
+])
+def test_keys_only(text, ret):
+	assert ucrf.filter_keys_only(None, text) == ret
+
+
+@pytest.mark.parametrize("text,ret", [
+	([], []),
+	(["b: b", "a: a"], ["a: a", "b: b"]),
+])
+def test_sort(text, ret):
+	assert ucrf.filter_sort(None, text) == ret
