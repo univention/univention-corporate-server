07005c84f0e9128b43f1795fd9d00d68e4d372bf
Philipp Hahn <hahn@univention.de>
2021-02-24 18:50:11 +0100

    test[ucr]: Test main
    Pass arguments as string to improve test output.
    Use custom UCR.
    
    Bug #38938

--- base/univention-config-registry/tests/test_cli.py
+++ base/univention-config-registry/tests/test_cli.py
@@ -2,74 +2,90 @@
 """
 Run (uninstalled) univention.config_registry to make sure all commands work
 """
+from copy import deepcopy
+
 import pytest
 
 from univention.config_registry import main  # noqa E402
+import univention.config_registry.frontend as ucrfe
+
+
+@pytest.fixture(autouse=True)
+def global_state():
+    """Save and restore global state for each test."""
+    opt_actions = deepcopy(ucrfe.OPT_ACTIONS)
+    opt_filters = deepcopy(ucrfe.OPT_FILTERS)
+    opt_commands = deepcopy(ucrfe.OPT_COMMANDS)
+    yield
+    ucrfe.OPT_ACTIONS = opt_actions
+    ucrfe.OPT_FILTERS = opt_filters
+    ucrfe.OPT_COMMANDS = opt_commands
 
 
 @pytest.mark.parametrize(
     "args",
     [
-        ("-?",),
-        ("-h",),
-        ("--help",),
-        ("-v",),
-        ("--version",),
-        ("set", "key?value"),
-        ("set", "key=value"),
-        ("set", "--force", "key?value"),
-        ("set", "--force", "key=value"),
-        ("set", "--schedule", "key?value"),
-        ("set", "--schedule", "key=value"),
-        ("set", "--ldap-policy", "key?value"),
-        ("set", "--ldap-policy", "key=value"),
-        ("get", "key"),
-        ("unset", "key"),
-        ("unset", "--force", "key"),
-        ("unset", "--schedule", "key"),
-        ("unset", "--ldap-policy", "key"),
-        ("dump",),
-        ("--shell", "dump"),
-        ("--keys-only", "dump"),
-        ("search",),
-        ("search", "hostname"),
-        ("search", "--key", "hostname"),
-        ("search", "--value", "hostname"),
-        ("search", "--all", "hostname"),
-        ("search", "--category", "system-network", "hostname"),
-        ("search", "--brief", "hostname"),
-        ("search", "--non-empty", "hostname"),
-        ("--shell", "search"),
-        ("--shell", "search", "hostname"),
-        ("--shell", "search", "--key", "hostname"),
-        ("--shell", "search", "--value", "hostname"),
-        ("--shell", "search", "--all", "hostname"),
-        ("--shell", "search", "--category", "system-network", "hostname"),
-        ("--shell", "search", "--brief", "hostname"),
-        ("--shell", "search", "--non-empty", "hostname"),
-        ("--shell", "search"),
-        ("--shell", "search", "hostname"),
-        ("--keys-only", "search"),
-        ("--keys-only", "search", "hostname"),
-        ("--keys-only", "search", "--key", "hostname"),
-        ("--keys-only", "search", "--value", "hostname"),
-        ("--keys-only", "search", "--all", "hostname"),
-        ("--keys-only", "search", "--category", "system-network", "hostname"),
-        ("--keys-only", "search", "--brief", "hostname"),
-        ("--keys-only", "search", "--non-empty", "hostname"),
-        ("info", "hostname"),
-        ("shell",),
-        ("shell", "hostname"),
-        # ("commit",),
-        ("commit", "/etc/hostname"),
-        ("filter",),  # </dev/null
-        # ("register", "..."),
-        # ("unregister", "..."),
-        ("update",),
-    ],
-)
-def test_cmd(args):
+        "",
+        "-?",
+        "-h",
+        "--help",
+        "-v",
+        "--version",
+    ])
+def test_exit(args, tmpucr):
     with pytest.raises(SystemExit) as exc_info:
-        main(list(args))
+        main(args.split() if args else [])
 
     assert exc_info.value.code == 0
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        "set key?value",
+        "set key=value",
+        "set --force key?value",
+        "set --force key=value",
+        "set --forced key=value",
+        "set --schedule key?value",
+        "set --schedule key=value",
+        "set --ldap-policy key?value",
+        "set --ldap-policy key=value",
+        "get key",
+        "--shell get key",
+        "unset key",
+        "unset --force key",
+        "unset --forced key",
+        "unset --schedule key",
+        "unset --ldap-policy key",
+        "dump",
+        pytest.param("--shell dump", marks=pytest.mark.xfail(reason="Fails on empty output")),
+        "--sort dump",
+        "--keys-only dump",
+        "search",
+        "search hostname",
+        "search --key hostname",
+        "search --value hostname",
+        "search --all hostname",
+        pytest.param("search --category system-network hostname", marks=pytest.mark.xfail(reason="Category not defined")),
+        "search --brief hostname",
+        "search --non-empty hostname",
+        "search --verbose hostname",
+        "--shell search",
+        "--sort search",
+        "--keys-only search",
+        "info hostname",
+        "--sort info hostname",
+        "shell",
+        "shell hostname",
+        # "commit",
+        "commit /etc/hostname",
+        pytest.param("filter", marks=pytest.mark.skip(reason="Requires input")),
+        pytest.param("filter --encode-utf8", marks=pytest.mark.skip(reason="Requires input")),
+        pytest.param("filter disallow-execution", marks=pytest.mark.skip(reason="Requires input")),
+        pytest.param("register ...", marks=pytest.mark.skip("Requires file")),
+        pytest.param("unregister ...", marks=pytest.mark.skip("Requires file")),
+        "update",
+    ])
+def test_cmd(args, tmpucr):
+    main(args.split())
