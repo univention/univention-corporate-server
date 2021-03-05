univention/univention-corporate-server

commit dc703113412af5e4039f89f2696c8d500135598e

refactor[ucr]: Fix utf-8 encoding
Use io.open(encoding='utf-8') for forward compatibility with Python 3

Bug #51126
Fixes: 1d891d2d5f4c723cfe2258cd8650e00343c3ba27

base/univention-config-registry/python/univention/config_registry/frontend.py
@@ -40,6 +40,8 @@ import re
 import time
 
 import six
+if six.PY2:
+	from io import open
 
 from univention.config_registry.backend import exception_occured, SCOPE, ConfigRegistry
 from univention.config_registry.handler import run_filter, ConfigHandlers
@@ -73,10 +75,6 @@ __all__ = [
 REPLOG_FILE = '/var/log/univention/config-registry.replog'
 
 _SHOW_EMPTY, _SHOW_DESCRIPTION, _SHOW_SCOPE, _SHOW_CATEGORIES, _SHOW_DEFAULT = (1 << _ for _ in range(5))
-
-encoding = {}
-if six.PY3:
-	encoding['encoding'] = 'UTF-8'  # make sure we open files in UTF-8, even if the locale is "C"
 
 
 class UnknownKeyException(Exception):
@@ -123,7 +121,7 @@ def replog(ucr, var, old_value, value=None):
 			if not os.path.isfile(REPLOG_FILE):
 				os.close(os.open(REPLOG_FILE, os.O_CREAT, 0o640))
 
-			with open(REPLOG_FILE, "a+", **encoding) as logfile:
+			with open(REPLOG_FILE, "a+", encoding='utf-8') as logfile:
 				logfile.write(log)
 		except EnvironmentError as ex:
 			print(("E: exception occurred while writing to replication log: %s" % (ex,)), file=sys.stderr)

commit 2e73fe219706e1a753aef3a46967b5eef454084e

fix[ucr]: Fix logging non-unicode changes
USS running with Python 2.7 still want's to log non-unicode data:

>  File "/usr/lib/python2.7/dist-packages/univention/management/console/modules/setup/setup_script.py", line 312, in run
>    success = self.inner_run()
>  File "/usr/lib/univention-system-setup/scripts/05_role/10role", line 80, in inner_run
>    self.ucr.commit()
>  File "/usr/lib/python2.7/dist-packages/univention/management/console/modules/setup/setup_script.py", line 133, in commit
>    ucr_update(self.ucr, self.changes)
>  File "/usr/lib/python2.7/dist-packages/univention/config_registry/frontend.py", line 211, in ucr_update
>    _run_changed(ucr, changed)
>  File "/usr/lib/python2.7/dist-packages/univention/config_registry/frontend.py", line 225, in _run_changed
>    replog(ucr, key, old_value, new_value)
>  File "/usr/lib/python2.7/dist-packages/univention/config_registry/frontend.py", line 125, in replog
>    logfile.write(log)
> TpeError: write() argument 1 must be unicode, not str

Fixes: dc703113412af5e4039f89f2696c8d500135598e
Bug #51126

base/univention-config-registry/python/univention/config_registry/frontend.py
@@ -116,7 +116,7 @@ def replog(ucr, var, old_value, value=None):
 		if old_value is None:
 			old_value = "[Previously undefined]"
 
-		log = '%s: %s %s%s old:%s\n' % (time.strftime("%Y-%m-%d %H:%M:%S"), method, scope_arg, varvalue, old_value)
+		log = u'%s: %s %s%s old:%s\n' % (time.strftime("%Y-%m-%d %H:%M:%S"), method, scope_arg, varvalue, old_value)
 		try:
 			if not os.path.isfile(REPLOG_FILE):
 				os.close(os.open(REPLOG_FILE, os.O_CREAT, 0o640))
@@ -124,7 +124,7 @@ def replog(ucr, var, old_value, value=None):
 			with open(REPLOG_FILE, "a+", encoding='utf-8') as logfile:
 				logfile.write(log)
 		except EnvironmentError as ex:
-			print(("E: exception occurred while writing to replication log: %s" % (ex,)), file=sys.stderr)
+			print("E: exception occurred while writing to replication log: %s" % (ex,), file=sys.stderr)
 			exception_occured()
 
 

base/univention-config-registry/tests/test_frontend.py
@@ -105,6 +105,10 @@ class TestReplog(object):
 
 		assert exc_info.value.code != 0
 
+	def test_replog_bytes(self, replog, ucrr):
+		ucrfe.replog(ucrr, "key", "old", "new")
+		assert u": set key=new old:old\n" in replog.read()
+
 
 @pytest.mark.parametrize("opt,scope", [
 	("ldap-policy", "LDAP"),