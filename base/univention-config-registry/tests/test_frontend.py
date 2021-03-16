univention/univention-corporate-server

commit e589e6b7b519038cfd5d2cae9e19f36032511fbb

fix[ucr]: Call handlers only for changed variables
BREAKING CHANGE:
When UCR variables are changed the list of changed variables (from the
command line or to `update_ucr()`) were passed unfiltered to the
internal function  `_run_changed()`, which does the grunt work of
calling the registered handlers.
This even happened for UCR variables shadowed by layers of higher
priority, leading to handlers being called for variables, which did not
change visibly. Even worse if the used the `new` valued passed via
`changed` they would use the wrong "shadowed" value:

   ucr set --forced key=forced  # changed={"key", (None, "forced")}
   ucr set          key=normal  # changed={"key", (None, "normal")}

Only a message was printed, that the change was shadowed.
A handler would need to use `ucr.get(key)` to get the real value instead
of suing `new` directly.
This strange behavior is at best unexpected and all UCR scripts /
handlers in UCR either don't care about the values at all or expect the
new value.

The new behavior filters the list of changes and only passes on those,
which are *not* shadowed, e.g where the change happens in the layer with
the current highest priority (in the example from above the 2nd `set`
would *not* trigger any handler.)

Doing a null-change (setting a UCR variable to its current value again)
will still trigger the handlers as this work-around is often used to
re-commit files like `/etc/apt/sources.list.d/`.

Bug #52847

base/univention-config-registry/python/univention/config_registry/frontend.py
@@ -211,7 +211,7 @@ def ucr_update(ucr, changes):
 	_run_changed(ucr, changed)
 
 
-def _run_changed(ucr, changed, msg=None):
+def _run_changed(ucr, changed, msg=""):
 	# type: (ConfigRegistry, Dict[str, Tuple[Optional[str], Optional[str]]], str) -> None
 	"""
 	Run handlers for changed UCR variables.
@@ -220,16 +220,19 @@ def _run_changed(ucr, changed, msg=None):
 	:param changed: Mapping from UCR variable name to 2-tuple (old-value, new-value).
 	:param msg: Message to be printed when change is shadowed by higher layer. Must contain 2 `%s` placeholders for `key` and `scope-name`.
 	"""
+	visible = {}  # type: Dict[str, Tuple[Optional[str], Optional[str]]]
 	for key, (old_value, new_value) in changed.items():
 		replog(ucr, key, old_value, new_value)
-		if msg:
-			scope, _value = ucr.get(key, (0, None), getscope=True)
-			if scope > ucr.scope:
+		scope, value = ucr.get(key, (0, None), getscope=True)
+		if scope > ucr.scope:
+			if msg:
 				print(msg % (key, SCOPE[scope]), file=sys.stderr)
+		else:
+			visible[key] = (old_value, new_value)
 
 	handlers = ConfigHandlers()
 	handlers.load()
-	handlers(list(changed.keys()), (ucr, changed))
+	handlers(list(visible), (ucr, visible))
 
 
 def _ucr_from_opts(opts):

base/univention-config-registry/tests/test_frontend.py
@@ -127,6 +127,10 @@ class TestHandler(object):
 		"bar": ("NORMAL", "bar"),
 		"baz": ("NORMAL", None),
 	}
+	VISIBLE = {
+		"new": (None, "val"),
+		"baz": ("NORMAL", None),
+	}
 
 	@pytest.fixture(autouse=True)
 	def ucr1(self, ucrf, mocker):
@@ -179,12 +183,12 @@ class TestHandler(object):
 
 		ucrfe._run_changed(ucrf, self.CHANGED, "%s %s")
 
-		for (key, (old, new)) in self.CHANGED.items():
+		for (key, (old, new)) in self.VISIBLE.items():
 			replog.assert_any_call(mocker.ANY, key, old, new)
 
 		handlers.assert_called_once_with()
 		handlers.return_value.load.assert_called_once_with()
-		handlers.return_value.assert_called_once_with(list(self.CHANGED), (mocker.ANY, self.CHANGED))
+		handlers.return_value.assert_called_once_with(list(self.VISIBLE), (mocker.ANY, self.VISIBLE))
 
 	def test_handler_dump(self):
 		assert set(ucrfe.handler_dump([])) == {"foo: FORCED", "bar: FORCED", "baz: NORMAL"}

commit 0f613f600c57ff0378a2cefc1dd45c4812a919c5

fix[ucr]: Call handlers with old&new visible value
We must also get the *old* value of the previously highest layer. This
is complicated by the fact, that `_run_changed()` is called *after* the
change was already performed, so we manually search the layers *below*
for the first one having a value. (Searching the layers *above* cam be
skipped as then the higher layer is still in place and the following
test with `if reg > ucr.scope` will trigger.)

So we have these cases:

1. Adding a new overwrite needs to get the old value from the layer
below and the new value from the current change, which will be the
newest highest layer.

2. Change the value on the highest layer will use the old and new value
from this layer.

3. Removing an overwrite will use the old value from the change and the
new value from the then highest new layer.

Bug #52847
Fixes: e589e6b7b519038cfd5d2cae9e19f36032511fbb
Fixes: 6535f1408969b8b071244c769f48ef05ffbac177

base/univention-config-registry/python/univention/config_registry/frontend.py
@@ -223,12 +223,18 @@ def _run_changed(ucr, changed, msg=""):
 	visible = {}  # type: Dict[str, Tuple[Optional[str], Optional[str]]]
 	for key, (old_value, new_value) in changed.items():
 		replog(ucr, key, old_value, new_value)
-		scope, vis_value = ucr.get(key, (0, None), getscope=True)
-		if scope > ucr.scope:
+
+		reg = ucr.scope
+		while old_value is None and reg > 0:
+			reg -= 1
+			old_value = ucr._registry[reg].get(key)
+
+		reg, new_value = ucr.get(key, (0, None), getscope=True)
+		if reg > ucr.scope:
 			if msg:
-				print(msg % (key, SCOPE[scope]), file=sys.stderr)
+				print(msg % (key, SCOPE[reg]), file=sys.stderr)
 		else:
-			visible[key] = (vis_value, new_value)
+			visible[key] = (old_value, new_value)
 
 	handlers = ConfigHandlers()
 	handlers.load()

base/univention-config-registry/tests/test_frontend.py
@@ -187,6 +187,9 @@ class TestHandler(object):
 	def test_run_changed(self, ucrf, mocker, capsys, handlers):
 		replog = mocker.patch("univention.config_registry.frontend.replog")
 
+		changed = ucrf.update(self.CHANGES)
+		assert changed == self.CHANGED
+
 		ucrfe._run_changed(ucrf, self.CHANGED, "%s %s")
 
 		for (key, (old, new)) in self.VISIBLE.items():
@@ -205,7 +208,8 @@ class TestHandler(object):
 		("baz", None, {"baz": ("NORMAL", None)}),
 	])
 	def test_run_changed_normal(self, key, val, visible, ucrf, mocker, handlers):
-		ucrfe._run_changed(ucrf, {key: ("OLD", val)})
+		changed = ucrf.update({key: val})
+		ucrfe._run_changed(ucrf, changed)
 		handlers.return_value.assert_called_once_with(list(visible), (mocker.ANY, visible))
 
 	@pytest.mark.ucr_layer(ucrfe.ConfigRegistry.LDAP)
@@ -213,25 +217,27 @@ class TestHandler(object):
 		("foo", "val", {"foo": ("LDAP", "val")}),
 		("foo", None, {"foo": ("LDAP", None)}),
 		("bar", "val", {"bar": ("LDAP", "val")}),
-		("bar", None, {"bar": ("LDAP", None)}),
+		("bar", None, {"bar": ("LDAP", "NORMAL")}),
 		("baz", "val", {"baz": ("NORMAL", "val")}),
-		("baz", None, {"baz": ("NORMAL", None)}),
+		("baz", None, {}),
 	])
 	def test_run_changed_ldap(self, key, val, visible, ucrf, mocker, handlers):
-		ucrfe._run_changed(ucrf, {key: ("OLD", val)})
+		changed = ucrf.update({key: val})
+		ucrfe._run_changed(ucrf, changed)
 		handlers.return_value.assert_called_once_with(list(visible), (mocker.ANY, visible))
 
 	@pytest.mark.ucr_layer(ucrfe.ConfigRegistry.FORCED)
 	@pytest.mark.parametrize("key,val,visible", [
 		("foo", "val", {"foo": ("LDAP", "val")}),
-		("foo", None, {"foo": ("LDAP", None)}),
+		("foo", None, {}),
 		("bar", "val", {"bar": ("LDAP", "val")}),
-		("bar", None, {"bar": ("LDAP", None)}),
+		("bar", None, {}),
 		("baz", "val", {"baz": ("NORMAL", "val")}),
-		("baz", None, {"baz": ("NORMAL", None)}),
+		("baz", None, {}),
 	])
 	def test_run_changed_forced(self, key, val, visible, ucrf, mocker, handlers):
-		ucrfe._run_changed(ucrf, {key: ("OLD", val)})
+		changed = ucrf.update({key: val})
+		ucrfe._run_changed(ucrf, changed)
 		handlers.return_value.assert_called_once_with(list(visible), (mocker.ANY, visible))
 
 	def test_handler_dump(self):