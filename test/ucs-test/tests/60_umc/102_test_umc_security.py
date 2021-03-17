4e789affb49cb24a8df41cc2fc14c01adb4b825e
Florian Best <best@univention.de>
2021-03-17 14:52:16 +0100

    Bug #52935: allow localhost (::1, 127.0.0.1) to use sessions from any IP

--- test/ucs-test/tests/60_umc/102_test_umc_security.py
+++ test/ucs-test/tests/60_umc/102_test_umc_security.py
@@ -80,16 +80,12 @@ class TestSecurityHeaders(object):
 		c2 = Client('localhost')
 		c2.ConnectionType = HTTPConnection  # workaround TLS hostname mismatch
 		c2.cookies = copy.deepcopy(client.cookies)
-		with pytest.raises(Unauthorized) as exc:
-			c2.umc_get('modules')
-		assert 'The current session is not valid with your IP address for security reasons.' in exc.value.message
+		assert any(x['id'] == 'top' for x in c2.umc_get('modules').data['modules'])
 
 		# check if the session is still bound after the internal connection to the UMC-Server was lost
 		restart_umc_server()
 		c2.cookies = copy.deepcopy(client.cookies)
-		with pytest.raises(Unauthorized) as exc:
-			c2.umc_get('modules')
-		assert 'The current session is not valid with your IP address for security reasons.' in exc.value.message
+		assert any(x['id'] == 'top' for x in c2.umc_get('modules').data['modules'])
 
 		# make sure any UMC module is present (the session is not dropped to anonymous)
 		assert any(x['id'] == 'top' for x in client.umc_get('modules').data['modules'])
