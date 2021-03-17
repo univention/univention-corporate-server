univention/univention-corporate-server

commit 04e8105a5744a7d2151624771cf7bcc765b2094b

refactor[ucr]: Extract common layer walking code
into method _walk().
There is a small semantic change in _merge() where the (empty) dict()s
will also get merged now.

Bug #51126

base/univention-config-registry/python/univention/config_registry/backend.py
@@ -122,6 +122,17 @@ class ConfigRegistry(MM):
 			else:
 				self._registry[reg] = _ConfigRegistry(os.devnull if custom else os.path.join(self.PREFIX, self.BASES[reg]))
 
+	def _walk(self):
+		# type: () -> Iterator[Tuple[int, Union[_ConfigRegistry, Dict[str, str]]]]
+		"""
+		Iterator over layers.
+
+		:returns: Iterator of 2-tuple (layers-mumber, layer)
+		"""
+		for reg in self.LAYER_PRIORITIES:
+			registry = self._registry[reg]
+			yield (reg, registry)
+
 	def load(self):
 		# type: () -> None
 		"""Load registry from file."""
@@ -224,11 +235,7 @@ class ConfigRegistry(MM):
 		:param key: UCR variable name.
 		:returns: `True` is set, `False` otherwise.
 		"""
-		for reg in self.LAYER_PRIORITIES:
-			registry = self._registry[reg]
-			if key in registry:
-				return True
-		return False
+		return any(key in registry for _reg, registry in self._walk())
 
 	def __iter__(self):
 		# type: () -> Iterator[str]
@@ -271,9 +278,8 @@ class ConfigRegistry(MM):
 		:param getscope: `True` makes the method return the scope level in addition to the value itself.
 		:returns: the value or a 2-tuple (level, value) or the default.
 		"""
-		for reg in self.LAYER_PRIORITIES:
+		for reg, registry in self._walk():
 			try:
-				registry = self._registry[reg]
 				value = registry[key]  # type: str
 			except KeyError:
 				continue
@@ -315,8 +321,7 @@ class ConfigRegistry(MM):
 		:returns: A mapping from varibal ename to eiter the value (if `getscope` is False) or a 2-tuple (level, value).
 		"""
 		merge = {}  # type: Dict[str, Union[str, Tuple[int, str]]]
-		for reg in self.LAYER_PRIORITIES:
-			registry = self._registry[reg]
+		for reg, registry in self._walk():
 			for key, value in registry.items():
 				if key not in merge:
 					if reg == ConfigRegistry.DEFAULTS:

base/univention-config-registry/python/univention/config_registry/interfaces.py
@@ -225,9 +225,8 @@ class VengefulConfigRegistry(ConfigRegistry):
 		:returns: the value.
 		:raises: KeyError when the value is not found.
 		"""
-		for reg in ConfigRegistry.LAYER_PRIORITIES:
+		for _reg, registry in self._walk():
 			try:
-				registry = self._registry[reg]
 				value = registry[key]
 				return value
 			except KeyError:

commit a8abf0cabadf87ecec5c39dfb43596625b8d97f9

doc[ucr]: Fix PEP 484 signature
Bug #51126
Fixes: 04e8105a5744a7d2151624771cf7bcc765b2094b
Augments: 2ced107e413e072e95926403e16d19aa697127c5

base/univention-config-registry/python/univention/config_registry/backend.py
@@ -217,7 +217,7 @@ class ReadOnlyConfigRegistry(_M, BooleanConfigRegistry):
 				self._registry[reg] = _ConfigRegistry(os.devnull if custom else os.path.join(self.PREFIX, self.BASES[reg]))
 
 	def _walk(self):
-		# type: () -> Iterator[Tuple[int, Union[_ConfigRegistry, Dict[str, str]]]]
+		# type: () -> Iterator[Tuple[int, _ConfigRegistry]]
 		"""
 		Iterator over layers.
 