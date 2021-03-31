a08abac1257ceb1c3785a539ca8c1d6c3211effe
Florian Best <best@univention.de>
2021-03-31 19:00:10 +0200

    fixup! Bug #51655 up: Unify preup.sh and pre-check.sh

--- base/univention-updater/script/preup.sh
+++ base/univention-updater/script/preup.sh
@@ -217,8 +217,6 @@ done
 echo "** Starting: apt-get -s -o Debug::pkgProblemResolver=yes dist-upgrade" >&3 2>&3
 apt-get -s -o Debug::pkgProblemResolver=yes dist-upgrade >&3 2>&3
 
-fail_if_role_package_will_be_removed
-
 echo ""
 echo "Starting update process, this may take a while."
 echo "Check /var/log/univention/updater.log for more information."
