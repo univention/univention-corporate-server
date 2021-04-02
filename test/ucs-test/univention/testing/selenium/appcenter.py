d4c5a1e517b44a9ae77098a100f7f328f9dafdf1
Jan Luttermann <luttermann@univention.de>
2021-04-02 14:48:28 +0200

    Bug #52953: increased timeout for install_app in selenium appcenter

--- test/ucs-test/univention/testing/selenium/appcenter.py
+++ test/ucs-test/univention/testing/selenium/appcenter.py
@@ -55,7 +55,7 @@ class AppCenter(object):
 
 		# ChooseHostWizard
 		try:
-			self.selenium.wait_for_text(_('In order to proceed with the installation'), timeout=1)
+			self.selenium.wait_for_text(_('In order to proceed with the installation'))
 		except TimeoutException:
 			pass
 		else:
