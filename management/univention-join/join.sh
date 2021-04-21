univention/univention-corporate-server

commit fce82c16e0bc0689bc5f9cb916c378d3cdbb9305

Bug #52211 join: Move join function to separate lib
Move the code calling univention-join related functions into a separate
shell library included with univention-join itself.

For backward compatibility provide include the new `join.sh` from the
old `base.sh`.

management/univention-join/join.sh
@@ -1,0 +1,119 @@
+# shellcheck shell=bash
+# Univention Common Shell Library
+#
+# Copyright 2011-2020 Univention GmbH
+#
+# https://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <https://www.gnu.org/licenses/>.
+
+UCS_JS_DIR='/usr/lib/univention-install'
+UCS_JS_STATUS='/var/univention-join/status'
+
+is_primary () {
+	[ "$(/usr/sbin/univention-config-registry get server/role)" = domaincontroller_master ]
+}
+
+is_backup () {
+	[ "$(/usr/sbin/univention-config-registry get server/role)" = domaincontroller_backup ]
+}
+
+is_replica () {
+	[ "$(/usr/sbin/univention-config-registry get server/role)" = domaincontroller_slave ]
+}
+
+is_primary_or_backup () {
+	case "$(/usr/sbin/univention-config-registry get server/role)" in
+	domaincontroller_master) return 0 ;;
+	domaincontroller_backup) return 0 ;;
+	*) return 1 ;;
+	esac
+}
+
+_join_call () {  # <role-check> <joinscript> [<args>...]
+	local role_check="${1:?}" js_path="${UCS_JS_DIR}/${2:?}" js_name="${2:?}" rv=0
+	[ -x "$js_path" ] ||
+		return 0
+
+	"$role_check"
+
+	shift 2 ||
+		return $?
+
+	echo "Calling joinscript $js_name ..."
+	"$js_path" "$@" ||
+		rv="$?"
+	echo "Joinscript $js_name finished with exitcode $rv"
+	return "$rv"
+}
+
+call_joinscript () {  # <joinscipt> [<args>...]
+	# calls the given joinscript
+	# e.g. call_joinscript 99my-custom-joinscript.inst --binddn ... --bindpwd ...
+	_join_call is_primary_or_backup "$@"
+}
+
+delete_unjoinscript () {  # <joinscipt>
+	# deletes the given unjoinscript if it does not belong to any package
+	# e.g. delete_unjoinscript 99my-custom-joinscript.uinst
+	local js_path="${UCS_JS_DIR}/${1:?}"
+
+	# Nothing to do if it does not exist
+	[ -e "$js_path" ] ||
+		return 1
+
+	# Does the script end with uinst?
+	[ "${1%.uinst}" != "$1" ] ||
+		return 1
+
+	# Remove the script only if it is no longer part of a package
+	dpkg -S "$js_path" >/dev/null 2>&1 &&
+		return 1
+
+	# Do it
+	rm -f "$js_path"
+}
+
+remove_joinscript_status () {  # <name>
+	# removes the given joinscript from the join script status file
+	# e.g. remove_joinscript_status univention-pkgdb-tools
+	local js_name="${1:?}"
+
+	sed -i "/^${js_name} /d" "$UCS_JS_STATUS"
+}
+
+call_unjoinscript () {  # <joinscript> [<args>...]
+	# calls the given unjoinscript
+	# e.g. call_unjoinscript 99my-custom-joinscript.uinst --binddn ... --bindpwd ...
+	_join_call is_primary_or_backup "$@" &&
+		delete_unjoinscript "${1:?}"
+}
+
+call_joinscript_on_dcmaster () {  # <joinscript> [<args>...]
+	# calls the given joinscript ONLY on Primary Directory Node
+	# e.g. call_joinscript_on_dcmaster 99my-custom-joinscript.inst --binddn ... --bindpwd ...
+	_join_call is_primary "$@"
+}
+
+# vim:set sw=4 ts=4 noet:

base/univention-lib/debian/control
@@ -86,6 +86,8 @@ Depends:
  python3-univention-lib (= ${binary:Version}),
  univention-config (>= 15),
  ${misc:Depends},
+Recommends:
+ univention-join,
 Description: UCS - common scripting functions for shell scripts
  This package contains shell functions used by various
  components of UCS.

base/univention-lib/shell/base.sh
@@ -28,6 +28,9 @@
 # <https://www.gnu.org/licenses/>.
 
 . /usr/share/univention-lib/ucr.sh
+# shellcheck source=/dev/null
+[ -e /usr/share/univention-lib/join.sh ] &&
+. /usr/share/univention-lib/join.sh
 
 #
 # creates an empty file with given owner/group and permissions
@@ -48,103 +51,6 @@ create_logfile () {
 create_logfile_if_missing () {
 	if [ ! -e "$1" ] ; then
 		create_logfile "$@"
-	fi
-}
-
-#
-# calls the given joinscript
-# call_joinscript <joinscript>
-# e.g. call_joinscript 99my-custom-joinscript.inst
-# e.g. call_joinscript 99my-custom-joinscript.inst --binddn ... --bindpwd ...
-#
-call_joinscript () {
-	local joinscript
-	joinscript="/usr/lib/univention-install/$1"
-	if [ -x "$joinscript" ] ; then
-		local namejoinscript
-		namejoinscript="$1"
-		shift
-		local role="$(/usr/sbin/univention-config-registry get server/role)"
-		if [ "$role" = "domaincontroller_master" -o "$role" = "domaincontroller_backup" ] ; then
-			echo "Calling joinscript $namejoinscript ..."
-			"$joinscript" "$@"
-			echo "Joinscript $namejoinscript finished with exitcode $?"
-		fi
-	fi
-}
-
-#
-# deletes the given unjoinscript if it does not belong to any package
-# delete_unjoinscript <joinscript>
-# e.g. call_unjoinscript 99my-custom-joinscript.uinst
-#
-delete_unjoinscript ()
-{
-	local joinscript
-	joinscript="/usr/lib/univention-install/$1"
-
-	# Nothing to do if it does not exist
-	test -e "$joinscript" || return 1
-
-	# Does the script ends with uinst?
-	echo "$joinscript" | grep -q ".uinst$" || return 1
-
-	# Remove the script only if it is not part of a package
-	dpkg -S "$joinscript" >/dev/null 2>&1 && return 1
-
-	# Do it
-	rm -f "$joinscript"
-
-	return 0
-}
-
-# removes the given joinscript from the join script status file
-# remove_joinscript_status <name>
-# e.g. remove_joinscript_status univention-pkgdb-tools
-#
-remove_joinscript_status ()
-{
-	local name="$1"
-
-	sed -i "/^${name} /d" /var/univention-join/status
-}
-
-#
-# calls the given unjoinscript
-# call_unjoinscript <joinscript>
-# e.g. call_unjoinscript 99my-custom-joinscript.uinst
-# e.g. call_unjoinscript 99my-custom-joinscript.uinst --binddn ... --bindpwd ...
-#
-call_unjoinscript () {
-	local joinscript
-	local joinscript_name
-
-	joinscript_name="$1"
-	joinscript="/usr/lib/univention-install/${joinscript_name}"
-
-	if [ -x "$joinscript" ] ; then
-		shift
-		local role="$(/usr/sbin/univention-config-registry get server/role)"
-		if [ "$role" = "domaincontroller_master" -o "$role" = "domaincontroller_backup" ] ; then
-			"$joinscript" "$@" && delete_unjoinscript "${joinscript_name}"
-		fi
-	fi
-}
-
-#
-# calls the given joinscript ONLY on Primary Directory Node
-# call_joinscript_on_dcmaster <joinscript>
-# e.g. call_joinscript_on_dcmaster 99my-custom-joinscript.inst
-# e.g. call_joinscript_on_dcmaster 99my-custom-joinscript.inst --binddn ... --bindpwd ...
-#
-call_joinscript_on_dcmaster () {
-	local joinscript
-	joinscript="/usr/lib/univention-install/$1"
-	if [ -x "$joinscript" ] ; then
-		shift
-		if [ "$(/usr/sbin/univention-config-registry get server/role)" = "domaincontroller_master" ] ; then
-			"$joinscript" "$@"
-		fi
 	fi
 }
 

management/univention-join/debian/univention-join.install
@@ -1,8 +1,9 @@
-univention-server-join usr/share/univention-join/
-univention-join usr/share/univention-join/
-univention-run-join-scripts usr/share/univention-join/
-joinscripthelper.lib usr/share/univention-join/
-check_join_status.sh usr/share/univention-join/
-20univention-join.inst usr/lib/univention-install/
-univention-join-hooks usr/share/univention-join/
-ensure-minmum-ucs-version usr/share/univention-join/
+20univention-join.inst	usr/lib/univention-install/
+check_join_status.sh	usr/share/univention-join/
+ensure-minmum-ucs-version	usr/share/univention-join/
+joinscripthelper.lib	usr/share/univention-join/
+join.sh	usr/share/univention-lib/
+univention-join-hooks	usr/share/univention-join/
+univention-join	usr/share/univention-join/
+univention-run-join-scripts	usr/share/univention-join/
+univention-server-join	usr/share/univention-join/

commit 66856b5362e3fe152c7932aea6f3d6d42cff2974

fix[join]: evaluate role check
Actually evaluate the role check and return early from the function if
it does not match.

Bug #52211
Fixes: fce82c16e0bc0689bc5f9cb916c378d3cdbb9305

management/univention-join/join.sh
@@ -58,7 +58,8 @@ _join_call () {  # <role-check> <joinscript> [<args>...]
 	[ -f "$UCS_JS_STATUS" ] ||
 		return 0
 
-	"$role_check"
+	"$role_check" ||
+		return $?
 
 	shift 2 ||
 		return $?