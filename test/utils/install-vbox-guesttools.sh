a5a05eeab5222f41cead0d3b31ee9793025ff695
Erik Damrose <damrose@univention.de>
2021-04-27 14:08:42 +0200

    Bug #51967: add xorg config for virtualbox
    Without the config xorg and system setup cannot be started

--- test/utils/install-vbox-guesttools.sh
+++ test/utils/install-vbox-guesttools.sh
@@ -12,4 +12,20 @@ ucr set update/secure_apt=yes
 ucr set repository/online/unmaintained=no repository/online=no
 apt-get update
 
+mkdir -p /etc/X11/xorg.conf.d
+cat >/etc/X11/xorg.conf.d/use-fbdev-driver.conf <<__EOF__
+Section "Device"
+    Identifier  "Card0"
+    Driver      "fbdev"
+EndSection
+__EOF__
+
+cat >/usr/lib/univention-system-setup/appliance-hooks.d/20_remove_xorg_config <<__EOF__
+#!/bin/sh
+rm /etc/X11/xorg.conf.d/use-fbdev-driver.conf
+exit 0
+__EOF__
+
+chmod +x /usr/lib/univention-system-setup/appliance-hooks.d/20_remove_xorg_config
+
 rm -- "$0"
