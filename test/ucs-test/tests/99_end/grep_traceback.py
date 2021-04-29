a6d36db1c95545084c50e6114abda502a94d710c
Florian Best <best@univention.de>
2021-04-29 10:55:30 +0200

    Bug #50300: ignore ldap.NO_SUCH_OBJECT in quota listener

--- test/ucs-test/tests/99_end/grep_traceback.py
+++ test/ucs-test/tests/99_end/grep_traceback.py
@@ -53,7 +53,7 @@ RE_APPCENTER = re.compile(r'^(\s+\d+ .*[\d \-:]+ \[(    INFO| WARNING|   DEBUG| 
  17954 packages                         21-02-14 04:25:09 [ WARNING]:   File "/usr/lib/python2.7/dist-packages/pgdb.py", line 1619, in connect
  17954 packages                         21-02-14 04:25:09 [ WARNING]:     cnx = _connect(dbname, dbhost, dbport, dbopt, dbuser, dbpasswd)
  17954 packages                         21-02-14 04:25:09 [ WARNING]: Exception: foo
-"""
+"""  # noqa: E101
 
 
 class Tracebacks(set):
@@ -206,13 +206,8 @@ COMMON_EXCEPTIONS = dict((re.compile(x), [re.compile(z) if isinstance(z, str) el
 	("TypeError: argument of type 'NoneType' is not iterable", ['disable_user_from_ucs', 'primary_group_sync_from_ucs']),  # Bug #52788, Bug #51809
 	(r"FileNotFoundError\: \[Errno 2\] No such file or directory\: \'\/etc\/machine\.secret\'", [r'bind\.py.*_ldap_auth_string']),  # Bug #52789
 	('dbm.error: db type could not be determined', ['univention-management-console-web-server']),  # Bug #52764
-
-	## temporary
-	#("univention.udm.exceptions.UnknownModuleType: UDM module 'portals/portal' does not exist.", ['refresh']),  # Bug #52512
-	#("univention.udm.exceptions.ConnectionError: Could not read secret file", ['reloader.py.* in refresh']),  # Bug #52512
-	#("AttributeError: 'NoneType' object has no attribute 'module'", ['create_portal_entries.py']),  # Bug #52512
-	#('Exception: Creating blog entry failed: 3: unknown module portals/entry.', ['license_uuid.py']),  # Bug # 51197
-	#(r"FileNotFoundError: \[Errno 2\] No such file or directory: '/etc/machine.secret'", ['reloader.py.* in refresh']),  # Bug #52512
+	('at least one delete handler failed', ['_add_all_shares_below_this_container_to_dn_list']),  # Bug #43171
+	('ldap.NO_SUCH_OBJECT', ['_add_all_shares_below_this_container_to_dn_list']),  # Bug #43171
 ])
 
 
