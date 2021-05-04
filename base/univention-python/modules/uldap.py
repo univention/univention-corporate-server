univention/univention-corporate-server

commit 6b7fca1a1cb247cc4f51442c680d9fcee6b3f75d

Bug #49638: Implement servercontrols for search()
res = {}; c = ldap.controls.SimplePagedResultsControl(True, size=10, cookie=''); import univention.uldap, ldap; lo = univention.uldap.getMachineConnection(); lo.searchDn(filter='univentionObjectType=users/user', response=res, serverctrls=[c]); c.cookie = res['ctrls'][0].cookie; print lo.searchDn(filter='univentionObjectType=users/user', response=res, serverctrls=[c]);
res = {}
c = ldap.controls.SimplePagedResultsControl(True, size=10, cookie='')
import univention.uldap, ldap
lo = univention.uldap.getMachineConnection()
lo.searchDn(filter='univentionObjectType=users/user', response=res, serverctrls=[c])
c.cookie = res['ctrls'][0].cookie
print lo.searchDn(filter='univentionObjectType=users/user', response=res, serverctrls=[c]);

base/univention-python/modules/uldap.py
@@ -184,7 +184,7 @@ def getMachineConnection(start_tls=2, decode_ignorelist=[], ldap_master=True, se
 			raise exc
 
 
-class access:
+class access(object):
 	"""
 	The low-level class to access a LDAP server.
 
@@ -417,8 +417,8 @@ class access:
 			raise ldap.NO_SUCH_OBJECT({'desc': 'no object'})
 		return []
 
-	def search(self, filter='(objectClass=*)', base='', scope='sub', attr=[], unique=False, required=False, timeout=-1, sizelimit=0, serverctrls=None):
-		# type: (str, str, str, List[str], bool, bool, int, int, Optional[List[ldap.controls.LDAPControl]]) -> List[Tuple[str, Dict[str, List[str]]]]
+	def search(self, filter='(objectClass=*)', base='', scope='sub', attr=[], unique=False, required=False, timeout=-1, sizelimit=0, serverctrls=None, response=None):
+		# type: (str, str, str, List[str], bool, bool, int, int, Optional[List[ldap.controls.LDAPControl]], Optional[Dict]) -> List[Tuple[str, Dict[str, List[str]]]]
 		"""
 		Perform LDAP search and return values.
 
@@ -445,8 +445,8 @@ class access:
 			base = self.base
 
 		if scope == 'base+one':
-			res = self.lo.search_ext_s(base, ldap.SCOPE_BASE, filter, attr, serverctrls=serverctrls, clientctrls=None, timeout=timeout, sizelimit=sizelimit) + \
-				self.lo.search_ext_s(base, ldap.SCOPE_ONELEVEL, filter, attr, serverctrls=serverctrls, clientctrls=None, timeout=timeout, sizelimit=sizelimit)
+			res = self.__search(base, ldap.SCOPE_BASE, filter, attr, serverctrls=serverctrls, response=response, clientctrls=None, timeout=timeout, sizelimit=sizelimit) + \
+				self.__search(base, ldap.SCOPE_ONELEVEL, filter, attr, serverctrls=serverctrls, response=response, clientctrls=None, timeout=timeout, sizelimit=sizelimit)
 		else:
 			if scope == 'sub' or scope == 'domain':
 				ldap_scope = ldap.SCOPE_SUBTREE
@@ -454,7 +454,7 @@ class access:
 				ldap_scope = ldap.SCOPE_ONELEVEL
 			else:
 				ldap_scope = ldap.SCOPE_BASE
-			res = self.lo.search_ext_s(base, ldap_scope, filter, attr, serverctrls=serverctrls, clientctrls=None, timeout=timeout, sizelimit=sizelimit)
+			res = self.__search(base, ldap_scope, filter, attr, serverctrls=serverctrls, response=response, clientctrls=None, timeout=timeout, sizelimit=sizelimit)
 
 		if unique and len(res) > 1:
 			raise ldap.INAPPROPRIATE_MATCHING({'desc': 'more than one object'})
@@ -462,7 +462,21 @@ class access:
 			raise ldap.NO_SUCH_OBJECT({'desc': 'no object'})
 		return res
 
-	def searchDn(self, filter='(objectClass=*)', base='', scope='sub', unique=False, required=False, timeout=-1, sizelimit=0, serverctrls=None):
+	def __search(self, *args, **kwargs):
+		response = kwargs.pop('response', None)
+		try:
+			rtype, rdata, rmsgid, resp_ctrls = self.lo.result3(self.lo.search_ext(*args, **kwargs))
+		except ldap.REFERRAL as exc:
+			if not self.follow_referral:
+				raise
+			lo_ref = self._handle_referral(exc)
+			rtype, rdata, rmsgid, resp_ctrls = lo_ref.result3(lo_ref.search_ext(*args, **kwargs))
+
+		if kwargs.get('serverctrls') and isinstance(response, dict):
+			response['ctrls'] = resp_ctrls
+		return rdata
+
+	def searchDn(self, filter='(objectClass=*)', base='', scope='sub', unique=False, required=False, timeout=-1, sizelimit=0, serverctrls=None, response=None):
 		# type: (str, str, str, bool, bool, int, int, Optional[List[ldap.controls.LDAPControl]]) -> List[str]
 		"""
 		Perform LDAP search and return distinguished names only.
@@ -482,7 +496,7 @@ class access:
 		:raises ldap.INAPPROPRIATE_MATCHING: Indicates that the matching rule specified in the search filter does not match a rule defined for the attribute's syntax.
 		"""
 		_d = univention.debug.function('uldap.searchDn filter=%s base=%s scope=%s unique=%d required=%d' % (filter, base, scope, unique, required))  # noqa F841
-		return [x[0] for x in self.search(filter, base, scope, ['dn'], unique, required, timeout, sizelimit, serverctrls)]
+		return [x[0] for x in self.search(filter, base, scope, ['dn'], unique, required, timeout, sizelimit, serverctrls, response)]
 
 	def getPolicies(self, dn, policies=None, attrs=None, result=None, fixedattrs=None):
 		# type: (str, List[str], Dict[str, List[Any]], Any, Any) -> Dict[str, Dict[str, Any]]

management/univention-directory-manager-modules/modules/univention/admin/uldap.py
@@ -722,7 +722,7 @@ class access:
 		"""
 		return self.lo.getAttr(dn, attr, required)
 
-	def search(self, filter='(objectClass=*)', base='', scope='sub', attr=[], unique=False, required=False, timeout=-1, sizelimit=0):
+	def search(self, filter='(objectClass=*)', base='', scope='sub', attr=[], unique=False, required=False, timeout=-1, sizelimit=0, serverctrls=None, response=None):
 		# type: (str, str, str, List[str], bool, bool, int, int) -> List[Tuple[str, Dict[str, List[str]]]]
 		"""
 		Perform LDAP search and return values.
@@ -747,7 +747,7 @@ class access:
 		:raises univention.admin.uexceptions.ldapError: on any other LDAP error.
 		"""
 		try:
-			return self.lo.search(filter, base, scope, attr, unique, required, timeout, sizelimit)
+			return self.lo.search(filter, base, scope, attr, unique, required, timeout, sizelimit, serverctrls=serverctrls, response=response)
 		except ldap.NO_SUCH_OBJECT as msg:
 			raise univention.admin.uexceptions.noObject(_err2str(msg))
 		except ldap.INAPPROPRIATE_MATCHING as msg:
@@ -763,7 +763,7 @@ class access:
 		except ldap.LDAPError as msg:
 			raise univention.admin.uexceptions.ldapError(_err2str(msg), original_exception=msg)
 
-	def searchDn(self, filter='(objectClass=*)', base='', scope='sub', unique=False, required=False, timeout=-1, sizelimit=0):
+	def searchDn(self, filter='(objectClass=*)', base='', scope='sub', unique=False, required=False, timeout=-1, sizelimit=0, serverctrls=None, response=None):
 		# type: (str, str, str, bool, bool, int, int) -> List[str]
 		"""
 		Perform LDAP search and return distinguished names only.
@@ -788,7 +788,7 @@ class access:
 		:raises univention.admin.uexceptions.ldapError: on any other LDAP error.
 		"""
 		try:
-			return self.lo.searchDn(filter, base, scope, unique, required, timeout, sizelimit)
+			return self.lo.searchDn(filter, base, scope, unique, required, timeout, sizelimit, serverctrls=serverctrls, response=response)
 		except ldap.NO_SUCH_OBJECT as msg:
 			raise univention.admin.uexceptions.noObject(_err2str(msg))
 		except ldap.INAPPROPRIATE_MATCHING as msg:

doc/errata/staging/univention-directory-manager-modules.yaml
@@ -1,0 +1,11 @@
+product: ucs
+release: "4.4"
+version: [0]
+scope: ucs_4.4-0-errata4.4-0
+src: univention-directory-manager-modules
+fix: 
+desc: |
+ This update addresses the following issue(s):
+ * It is now possible to specifiy ldap server controls
+   in some UDM handlers lookup() method.
+bug: [49638]

doc/errata/staging/univention-python.yaml
@@ -1,0 +1,11 @@
+product: ucs
+release: "4.4"
+version: [0]
+scope: ucs_4.4-0-errata4.4-0
+src: univention-python
+fix: 
+desc: |
+ This update addresses the following issue(s):
+ * It is now possible to specifiy ldap server controls
+   in the univention.uldap.access.search() methods.
+bug: [49638]

management/univention-directory-manager-modules/debian/control
@@ -14,7 +14,7 @@ Architecture: all
 Depends: ${misc:Depends}, ${python:Depends},
  python (>= 2.7),
  python-pyasn1-modules,
- python-univention (>= 10.0.4-2),
+ python-univention (>= 12.0.0-12),
  python-univention-heimdal (>= 4.0.1-1),
  univention-admin-diary-client (>= 1.0.0-29),
  makepasswd,

management/univention-directory-manager-modules/modules/univention/admin/handlers/__init__.py
@@ -1691,7 +1691,7 @@ class simpleLdap(object):
 		return containers
 
 	@classmethod
-	def lookup(cls, co, lo, filter_s, base='', superordinate=None, scope='sub', unique=False, required=False, timeout=-1, sizelimit=0):  # type: (univention.admin.uldap.config, univention.admin.uldap.access, str, str, Optional[str], str, bool, bool, int, int) -> list[simpleLdap]
+	def lookup(cls, co, lo, filter_s, base='', superordinate=None, scope='sub', unique=False, required=False, timeout=-1, sizelimit=0, serverctrls=None, response=None):  # type: (univention.admin.uldap.config, univention.admin.uldap.access, str, str, Optional[str], str, bool, bool, int, int, Optional[list], Optional[Dict]) -> list[simpleLdap]
 		"""
 		Perform a LDAP search and return a list of instances.
 
@@ -1714,7 +1714,7 @@ class simpleLdap(object):
 		filter_str = unicode(filter_s or '')
 		attr = cls._ldap_attributes()
 		result = []
-		for dn, attrs in lo.search(filter_str, base, scope, attr, unique, required, timeout, sizelimit):
+		for dn, attrs in lo.search(filter_str, base, scope, attr, unique, required, timeout, sizelimit, serverctrls=serverctrls, response=response):
 			try:
 				result.append(cls(co, lo, None, dn=dn, superordinate=superordinate, attributes=attrs))
 			except univention.admin.uexceptions.base as exc:
@@ -3121,7 +3121,7 @@ class simpleComputer(simpleLdap):
 
 		for group in set(self.oldinfo.get('groups', [])) - set(self.info.get('groups', [])):
 			univention.debug.debug(univention.debug.ADMIN, univention.debug.INFO, '__update_groups_after_namechange: removing from grp=%s' % (group,))
-			self.lo.modify(group, [('uniqueMember', olddn, ''), ('memberUid', oldUid, ''),])
+			self.lo.modify(group, [('uniqueMember', olddn, ''), ('memberUid', oldUid, ''), ])
 
 	def update_groups(self):  # type: () -> None
 		if not self.hasChanged('groups') and not self.oldPrimaryGroupDn and not self.newPrimaryGroupDn:

management/univention-directory-manager-modules/modules/univention/admin/handlers/computers/ipmanagedclient.py
@@ -247,7 +247,7 @@ def rewrite(filter, mapping):
 		univention.admin.mapping.mapRewrite(filter, mapping)
 
 
-def lookup(co, lo, filter_s, base='', superordinate=None, scope='sub', unique=False, required=False, timeout=-1, sizelimit=0):
+def lookup(co, lo, filter_s, base='', superordinate=None, scope='sub', unique=False, required=False, timeout=-1, sizelimit=0, serverctrls=None, response=None):
 	res = []
 	filter_s = univention.admin.filter.replace_fqdn_filter(filter_s)
 	filter_s = univention.admin.handlers.dns.alias.lookup_alias_filter(lo, filter_s)
@@ -263,7 +263,7 @@ def lookup(co, lo, filter_s, base='', superordinate=None, scope='sub', unique=Fa
 		univention.admin.filter.walk(filter_p, rewrite, arg=mapping)
 		filter.expressions.append(filter_p)
 
-	for dn, attrs in lo.search(unicode(filter), base, scope, [], unique, required, timeout, sizelimit):
+	for dn, attrs in lo.search(unicode(filter), base, scope, [], unique, required, timeout, sizelimit, serverctrls, response):
 		res.append(object(co, lo, None, dn, attributes=attrs))
 	return res
 

management/univention-directory-manager-modules/modules/univention/admin/handlers/computers/trustaccount.py
@@ -186,7 +186,7 @@ class object(univention.admin.handlers.simpleLdap):
 			univention.admin.allocators.release(self.lo, self.position, i, j)
 
 
-def lookup(co, lo, filter_s, base='', superordinate=None, scope='sub', unique=False, required=False, timeout=-1, sizelimit=0):
+def lookup(co, lo, filter_s, base='', superordinate=None, scope='sub', unique=False, required=False, timeout=-1, sizelimit=0, serverctrls=None, response=None):
 	filter = univention.admin.filter.conjunction('&', [
 		univention.admin.filter.expression('objectClass', 'sambaSamAccount'),
 		univention.admin.filter.expression('sambaAcctFlags', '[I          ]'),
@@ -198,7 +198,7 @@ def lookup(co, lo, filter_s, base='', superordinate=None, scope='sub', unique=Fa
 		filter.expressions.append(filter_p)
 
 	res = []
-	for dn, attrs in lo.search(unicode(filter), base, scope, [], unique, required, timeout, sizelimit):
+	for dn, attrs in lo.search(unicode(filter), base, scope, [], unique, required, timeout, sizelimit, serverctrls, response):
 		res.append(object(co, lo, None, dn, attributes=attrs))
 	return res
 

commit f9faf49b643bb781c61ab99ae4912eb59e18c19a

doc[udm]: PEP 484 type annotations
uldap.search(..., response) seems to be unused as
63ba30a20400c8d498e686285e77db57c03c16b5 reverted the change.

Fixes: 6b7fca1a1cb247cc4f51442c680d9fcee6b3f75d

base/univention-python/modules/uldap.py
@@ -476,7 +476,7 @@ class access(object):
 
 	@_fix_reconnect_handling
 	def search(self, filter='(objectClass=*)', base='', scope='sub', attr=[], unique=False, required=False, timeout=-1, sizelimit=0, serverctrls=None, response=None):
-		# type: (str, str, str, List[str], bool, bool, int, int, Optional[List[ldap.controls.LDAPControl]], Optional[Dict]) -> List[Tuple[str, Dict[str, List[str]]]]
+		# type: (str, str, str, List[str], bool, bool, int, int, Optional[List[ldap.controls.LDAPControl]], Optional[Dict[str, ldap.controls.LDAPControl]]) -> List[Tuple[str, Dict[str, List[str]]]]
 		"""
 		Perform LDAP search and return values.
 
@@ -522,7 +522,7 @@ class access(object):
 		return res
 
 	def searchDn(self, filter='(objectClass=*)', base='', scope='sub', unique=False, required=False, timeout=-1, sizelimit=0, serverctrls=None, response=None):
-		# type: (str, str, str, bool, bool, int, int, Optional[List[ldap.controls.LDAPControl]]) -> List[str]
+		# type: (str, str, str, bool, bool, int, int, Optional[List[ldap.controls.LDAPControl]], Optional[Dict[str, ldap.controls.LDAPControl]]) -> List[str]
 		"""
 		Perform LDAP search and return distinguished names only.
 
@@ -576,7 +576,7 @@ class access(object):
 
 		object_classes = set(oc.lower() for oc in oattrs.get('objectClass', []))
 
-		merged = {}
+		merged = {}  # type: Dict[str, Dict[str, Any]]
 		if dn:
 			obj_dn = dn
 			while True: