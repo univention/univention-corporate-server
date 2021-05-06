f6ea45990f1b468aba0acb8a0b91f9ee326acb13
Florian Best <best@univention.de>
2021-05-06 12:54:03 +0200

    Bug #50300: fix execution of 03_dc_master_tracebacks.py

--- test/ucs-test/tests/99_end/03_dc_master_tracebacks.py
+++ test/ucs-test/tests/99_end/03_dc_master_tracebacks.py
@@ -1,4 +1,4 @@
-#!/usr/share/ucs-test/runner /usr/bin/py.test-3 -s
+#!/usr/share/ucs-test/runner /usr/bin/py.test-3 -s --tb=native
 ## desc: "Find traceback on the DC Master"
 ## exposure: dangerous
 ## roles-not: [domaincontroller_master]
@@ -16,17 +16,32 @@ import pytest
 ])
 def test_fetch_logfiles_on_dc_master(testfile, ucr):
 	password = univention.testing.utils.UCSTestDomainAdminCredentials().bindpw
+	testpath = '/usr/share/ucs-test/99_end/%s' % (testfile,)
 	with tempfile.NamedTemporaryFile() as fd:
-		fd.write(password.encode())
+		fd.write(password.encode('UTF-8'))
 		fd.flush()
 		try:
-			subprocess.check_output("""univention-ssh %s root@%s 'univention-install -y ucs-test-end'""" % (pipes.quote(fd.name), pipes.quote(ucr['ldap/master'])), shell=True)
+			cmd_install = """univention-ssh %s root@%s '[ -e %s ] || univention-install -y ucs-test-end'""" % (pipes.quote(fd.name), pipes.quote(ucr['ldap/master']), pipes.quote(pipes.quote(testpath)))
+			subprocess.check_output(cmd_install, shell=True)
 		except subprocess.CalledProcessError as exc:
+			print(cmd_install)
+			if exc.output:
+				print(exc.output.decode('UTF-8', 'replace'))
 			raise Exception(exc.output)
 
 		try:
-			subprocess.check_call("""univention-ssh %s root@%s '/usr/share/ucs-test/99_end/%s -f'""" % (pipes.quote(fd.name), pipes.quote(ucr['ldap/master']), pipes.quote(testfile)), shell=True)
+			cmd_test = """univention-ssh %s root@%s '%s -f'""" % (pipes.quote(fd.name), pipes.quote(ucr['ldap/master']), pipes.quote(pipes.quote(testpath)))  # somehow does not work, because no TTY?!
+			cmd_test = """univention-ssh %s root@%s 'ucs-test -s end -r %s -E dangerous -l /tmp/%s -H; rc=$?; cat /tmp/%s; exit "$rc"'""" % (
+				pipes.quote(fd.name),
+				pipes.quote(ucr['ldap/master']),
+				pipes.quote(pipes.quote(testfile)),
+				pipes.quote(pipes.quote(testfile)),
+				pipes.quote(pipes.quote(testfile)),
+			)
+			subprocess.check_call(cmd_test, shell=True)
 		except subprocess.CalledProcessError as exc:
-			print(exc.output.decode('UTF-8', 'replace'))
-			assert not exc.returncode, exc.returncode
+			print(cmd_test)
+			if exc.output:
+				print(exc.output.decode('UTF-8', 'replace'))
+			assert not exc.returncode, (cmd_test, exc.returncode)
 		# TODO: detect skipped exit code
