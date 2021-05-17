univention/univention-corporate-server

commit 100b63d3d2be60ea986550cdc6c5d252929554b1

Bug #51364: filter inetOrgPerson objectclass in settings/usertemplate

management/univention-directory-manager-modules/modules/univention/admin/handlers/settings/usertemplate.py
@@ -30,6 +30,8 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
+import six
+
 from univention.admin.layout import Tab, Group
 import univention.admin.filter
 import univention.admin.handlers
@@ -348,6 +350,8 @@ mapping.register('mailPrimaryAddress', 'mailPrimaryAddress', None, univention.ad
 mapping.register('mailAlternativeAddress', 'mailAlternativeAddress')
 mapping.register('_options', 'userOptionsPreset', encoding='ASCII')
 
+BLACKLISTED_OBJECT_CLASSES = {b'inetOrgPerson'}
+
 
 class object(univention.admin.handlers.simpleLdap):
 	module = module
@@ -356,6 +360,25 @@ class object(univention.admin.handlers.simpleLdap):
 		super(object, self).__init__(co, lo, position, dn, superordinate, attributes=attributes)
 		univention.admin.syntax.optionsUsersUser.update_choices()  # woraround: somehow init() didn't do it
 		self.options.extend(self['_options'])
+
+	def _ldap_object_classes(self, ml):
+		ml = super(object, self)._ldap_object_classes(ml)
+		return self.filter_object_classes(ml)
+
+	def _ldap_object_classes_add(self, al):
+		al = super(object, self)._ldap_object_classes_add(al)
+		return self.filter_object_classes(al)
+
+	@classmethod
+	def filter_object_classes(cls, ml):
+		"""Remove blacklisted object classes
+
+		>>> object.filter_object_classes([('objectClass', 'inetOrgPerson'), ('objectClass', 'foo', ['inetOrgPerson'])])
+		[['objectClass', 'foo', []]]
+		"""
+		ml = [x for x in ml if x[0] != 'objectClass' or not isinstance(x[-1], six.string_types) or x[-1] not in BLACKLISTED_OBJECT_CLASSES]
+		ml = [x if x[0] != 'objectClass' and not isinstance(x[-1], (list, tuple)) else list(x[:-1]) + [list(set(x[-1]) - BLACKLISTED_OBJECT_CLASSES)] for x in ml]
+		return ml
 
 	def _ldap_pre_modify(self):
 		super(object, self)._ldap_pre_modify()

management/univention-directory-manager-modules/modules/univention/admin/handlers/__init__.py
@@ -1226,37 +1226,7 @@ class simpleLdap(object):
 
 		al = self._ldap_addlist()
 		al.extend(self._ldap_modlist())
-		m = univention.admin.modules.get(self.module)
-
-		# evaluate extended attributes
-		ocs = set()  # type: Set[unicode]
-		for prop in getattr(m, 'extended_udm_attributes', []):
-			ud.debug(ud.ADMIN, ud.INFO, 'simpleLdap._create: info[%s]:%r = %r' % (prop.name, self.has_property(prop.name), self.info.get(prop.name)))
-			if prop.syntax == 'boolean' and self.info.get(prop.name) == u'0':
-				continue
-			if self.has_property(prop.name) and self.info.get(prop.name):
-				ocs.add(prop.objClass)
-
-		module_options = univention.admin.modules.options(self.module)
-		# add object classes of (especially extended) options
-		for option in ['default'] + self.options:
-			try:
-				opt = module_options[option]
-			except KeyError:
-				ud.debug(ud.ADMIN, ud.INFO, '%r does not specify option %r' % (m.module, option))
-				continue
-			ocs |= set(opt.objectClasses)
-
-		# remove duplicated object classes
-		for i in al:
-			key, val = i[0], i[-1]  # might be a triple
-			if val and key.lower() == 'objectclass':
-				val_list = [val] if not isinstance(val, (tuple, list)) else val
-				val_unicode = [x.decode('UTF-8') if isinstance(x, bytes) else x for x in val_list]
-				ocs -= set(val_unicode)  # TODO: check six.string_types vs bytes everywhere for ocs calculations
-		if ocs:
-			al.append(('objectClass', [x.encode('UTF-8') for x in ocs]))
-
+		al = self._ldap_object_classes_add(al)
 		al = self.call_udm_property_hook('hook_ldap_addlist', self, al)
 
 		# ensure univentionObject is set
@@ -1292,6 +1262,39 @@ class simpleLdap(object):
 
 		self.save()
 		return self.dn
+
+	def _ldap_object_classes_add(self, al):
+		m = univention.admin.modules.get(self.module)
+		# evaluate extended attributes
+		ocs = set()  # type: Set[unicode]
+		for prop in getattr(m, 'extended_udm_attributes', []):
+			ud.debug(ud.ADMIN, ud.INFO, 'simpleLdap._create: info[%s]:%r = %r' % (prop.name, self.has_property(prop.name), self.info.get(prop.name)))
+			if prop.syntax == 'boolean' and self.info.get(prop.name) == u'0':
+				continue
+			if self.has_property(prop.name) and self.info.get(prop.name):
+				ocs.add(prop.objClass)
+
+		module_options = univention.admin.modules.options(self.module)
+		# add object classes of (especially extended) options
+		for option in ['default'] + self.options:
+			try:
+				opt = module_options[option]
+			except KeyError:
+				ud.debug(ud.ADMIN, ud.INFO, '%r does not specify option %r' % (m.module, option))
+				continue
+			ocs |= set(opt.objectClasses)
+
+		# remove duplicated object classes
+		for i in al:
+			key, val = i[0], i[-1]  # might be a triple
+			if val and key.lower() == 'objectclass':
+				val_list = [val] if not isinstance(val, (tuple, list)) else val
+				val_unicode = [x.decode('UTF-8') if isinstance(x, bytes) else x for x in val_list]
+				ocs -= set(val_unicode)  # TODO: check six.string_types vs bytes everywhere for ocs calculations
+		if ocs:
+			al.append(('objectClass', [x.encode('UTF-8') for x in ocs]))
+
+		return al
 
 	def _modify(self, modify_childs=1, ignore_license=0, response=None, serverctrls=None):
 		"""Modify the object. Should only be called by :func:`univention.admin.handlers.simpleLdap.modify`."""

commit 6c7120f1b1b589902404e2982c0c20b55bf581de

fixup! Bug #51364: filter inetOrgPerson objectclass in settings/usertemplate
Fixes: 100b63d3d2be60ea986550cdc6c5d252929554b1

management/univention-directory-manager-modules/modules/univention/admin/handlers/settings/usertemplate.py
@@ -30,8 +30,6 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
-import six
-
 from univention.admin.layout import Tab, Group
 import univention.admin.filter
 import univention.admin.handlers
@@ -374,12 +372,24 @@ class object(univention.admin.handlers.simpleLdap):
 	def filter_object_classes(cls, ml):
 		"""Remove blacklisted object classes
 
-		>>> object.filter_object_classes([('objectClass', 'inetOrgPerson'), ('objectClass', 'foo', ['inetOrgPerson'])])
-		[['objectClass', 'foo', []]]
+		>>> object.filter_object_classes([('objectClass', b'bar', b'inetOrgPerson'), ('objectClass', b'foo', [b'inetOrgPerson', b'baz'])])
+		[('objectClass', b'bar', None), ('objectClass', b'foo', [b'baz'])]
 		"""
-		ml = [x for x in ml if x[0] != 'objectClass' or not isinstance(x[-1], six.string_types) or x[-1] not in BLACKLISTED_OBJECT_CLASSES]
-		ml = [x if x[0] != 'objectClass' and not isinstance(x[-1], (list, tuple)) else list(x[:-1]) + [list(set(x[-1]) - BLACKLISTED_OBJECT_CLASSES)] for x in ml]
-		return ml
+		def _iter_ml():
+			for x in ml:
+				if x[0].lower() != 'objectClass'.lower():
+					yield x
+				elif isinstance(x[-1], (bytes, str, type(u''))):
+					if x[-1] not in BLACKLISTED_OBJECT_CLASSES:
+						yield x
+					elif len(x) == 3:
+						yield (x[0], x[1], None)
+				elif isinstance(x[-1], (list, tuple)):
+					yield tuple(list(x[:-1]) + [[z for z in x[-1] if z not in BLACKLISTED_OBJECT_CLASSES]])
+				else:
+					yield x
+
+		return list(_iter_ml())
 
 	def _ldap_pre_modify(self):
 		super(object, self)._ldap_pre_modify()