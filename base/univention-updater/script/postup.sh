univention/univention-corporate-server

commit 93fb357345df31a28b8d8357f27498afeff228bb

Bug #52993: recreate initramfs for all available kernels

base/univention-updater/script/postup.sh
@@ -102,6 +102,11 @@ if [ -e "/etc/systemd/system/atd.service.d/update500.conf" ]; then
 	systemctl daemon-reload
 fi
 
+# Bug #52993: recreate initramfs for all available kernels due to removed initramfs/init
+echo "recreate initramfs for all available kernels due to changes in unvenition-initrd..." >&3 2>&3
+/usr/sbin/update-initramfs -k all -c >&3 2>&3
+echo "done" >&3 2>&3
+
 # executes custom postup script (always required)
 if [ -n "${update_custom_postup:-}" ]; then
 	if [ -f "$update_custom_postup" ]; then

commit a3d23606346a4cc88ac11fdc65b1537a939d58fa

spell[initrd]: recreate initramfs for all available kernels
Bug #52993
Fixes: 93fb357345d

base/univention-initrd/debian/univention-initrd.postinst
@@ -34,7 +34,7 @@
 
 if [ "$1" = configure ] && dpkg --compare-versions "$2" lt-nl 12.0.0-4; then
 	# Bug #52993: recreate initramfs for all available kernels due to removed initramfs/init
-	echo "recreate initramfs for all available kernels due to changes in unvenition-initrd..."
+	echo "recreate initramfs for all available kernels due to changes in univention-initrd..."
 	update-initramfs -k all -c
 	echo "done"
 fi

base/univention-updater/script/postup.sh
@@ -103,7 +103,7 @@ if [ -e "/etc/systemd/system/atd.service.d/update500.conf" ]; then
 fi
 
 # Bug #52993: recreate initramfs for all available kernels due to removed initramfs/init
-echo "recreate initramfs for all available kernels due to changes in unvenition-initrd..." >&3 2>&3
+echo "recreate initramfs for all available kernels due to changes in univention-initrd..." >&3 2>&3
 /usr/sbin/update-initramfs -k all -c >&3 2>&3
 echo "done" >&3 2>&3
 