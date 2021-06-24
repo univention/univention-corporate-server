univention/univention-corporate-server

commit a3e434bf99b644eade4031f2625798b80afaa481

Bug #52453: adjust user wizard styling

management/univention-management-console-module-udm/umc/js/udm/wizards/users/user.js
@@ -62,7 +62,6 @@ define([
 			var node;
 			if (widget) {
 				node = widget.domNode.parentNode.parentNode.parentNode;
-				node.classList.add('wizardInvitationBox');
 			}
 			var inviteWidget = this.getWidget('page1', '_invite');
 			if (inviteWidget) {
@@ -72,7 +71,6 @@ define([
 
 			widget = this.getWidget('page1', 'password');
 			node = widget.domNode.parentNode.parentNode.parentNode;
-			node.classList.add('wizardInvitationBox');
 		},
 
 		postMixInProperties: function() {

management/univention-management-console-module-udm/umc/js/udm.css
@@ -13,10 +13,6 @@
 /* umcSearchFormChangeViewButton END */
 
 
-.umc .wizardInvitationBox {
-	max-height: 80px;
-}
-
 .umc .umcUDMUsersModule__jpegPhoto {
 	margin-top: 1rem;
 }

commit 5eab381b92b5e282b8dee67635cb5be52fa5b8b8

Bug #53456: remove dead code
Fixes: a3e434bf99b644eade4031f2625798b80afaa481

management/univention-management-console-module-udm/umc/js/udm/wizards/users/user.js
@@ -58,11 +58,6 @@ define([
 
 		startup: function() {
 			this.inherited(arguments);
-			var widget = this.getWidget('page1', 'PasswordRecoveryEmail');
-			var node;
-			if (widget) {
-				node = widget.domNode.parentNode.parentNode.parentNode;
-			}
 			array.forEach(this.pages, function(page) {
 				array.forEach(this.getPage(page.name)._form.widgets, function(widget) {
 					var ucrkey = {'_invite': 'invite'}[widget.id] || widget.id;
@@ -81,9 +76,6 @@ define([
 					}
 				}, this);
 			}, this);
-
-			widget = this.getWidget('page1', 'password');
-			node = widget.domNode.parentNode.parentNode.parentNode;
 		},
 
 		postMixInProperties: function() {