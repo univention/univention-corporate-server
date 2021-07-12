21ca3ffd9a0cc01964936dbd5f13dfa7011655cb
Florian Best <best@univention.de>
2021-07-12 13:49:10 +0200

    Bug #50300: merge tests into one pytest

---
+++ test/ucs-test/tests/99_end/01_var_log_tracebacks.py
@@ -0,0 +1,59 @@
+#!/usr/share/ucs-test/runner /usr/bin/pytest-3 -s --tb=native
+## desc: "Find traceback in /var/log/univention/ logfiles"
+## exposure: safe
+## tags: [apptest]
+
+from __future__ import print_function
+
+import grep_traceback
+import glob
+import univention.testing.utils
+import subprocess
+import tempfile
+import pipes
+import pytest
+
+
+@pytest.mark.exposure('safe')
+def test_var_log_tracebacks():
+	not_found = grep_traceback.main(glob.glob('/var/log/univention/*.log'), ignore_exceptions=grep_traceback.COMMON_EXCEPTIONS)
+	assert not_found, 'logfiles contain tracebacks'
+
+
+@pytest.mark.exposure('safe')
+def test_var_log_tracebacks_gz():
+	not_found = grep_traceback.main(glob.glob('/var/log/univention/*.log.*.gz'), ignore_exceptions=grep_traceback.COMMON_EXCEPTIONS)
+	assert not_found, 'logfiles *.gz contain tracebacks'
+
+
+@pytest.mark.roles_not('domaincontroller_master')
+@pytest.mark.exposure('dangerous')
+# @pytest.mark.parametrize('testcase', [
+# 	'test_var_log_tracebacks',
+# 	'test_var_log_tracebacks_gz',
+# ])
+def test_fetch_logfiles_on_dc_master(ucr, testcase=None):
+	"""Find traceback on the DC Master"""
+	password = univention.testing.utils.UCSTestDomainAdminCredentials().bindpw
+	testpath = '/usr/share/ucs-test/99_end/01_var_log_tracebacks.py'
+	with tempfile.NamedTemporaryFile() as fd:
+		fd.write(password.encode('UTF-8'))
+		fd.flush()
+		try:
+			cmd_install = """univention-ssh %s root@%s '[ -e %s ] || univention-install -y ucs-test-end'""" % (pipes.quote(fd.name), pipes.quote(ucr['ldap/master']), pipes.quote(pipes.quote(testpath)))
+			subprocess.check_output(cmd_install, shell=True)
+		except subprocess.CalledProcessError as exc:
+			print(cmd_install)
+			if exc.output:
+				print(exc.output.decode('UTF-8', 'replace'))
+			assert False, (cmd_install, exc.output.decode('UTF-8', 'replace'))
+
+		try:
+			cmd_test = """univention-ssh %s root@%s '%s -i -f'""" % (pipes.quote(fd.name), pipes.quote(ucr['ldap/master']), pipes.quote(pipes.quote(testpath)))
+			subprocess.check_call(cmd_test, shell=True)
+		except subprocess.CalledProcessError as exc:
+			print(cmd_test)
+			if exc.output:
+				print(exc.output.decode('UTF-8', 'replace'))
+			assert not exc.returncode, (cmd_test, exc.returncode)
+		# TODO: detect skipped exit code
