60a292adde1ff96574fab051603e60b221a6fcbf
Mika Westphal <westphal@univention.de>
2021-07-13 05:09:23 +0200

    pytest migration of test 02_group_creation_with_single_letter_name

--- test/ucs-test/tests/62_udm-groups/02_group_creation_with_single_letter_name.py
+++ test/ucs-test/tests/62_udm-groups/02_group_creation_with_single_letter_name.py
@@ -8,11 +8,18 @@
 ##   - univention-directory-manager-tools
 
 
+import pytest
 import univention.testing.strings as uts
 import univention.testing.utils as utils
-import univention.testing.udm as udm_test
 
-if __name__ == '__main__':
-	with udm_test.UCSTestUDM() as udm:
-		group = udm.create_group(name=uts.random_groupname(1))[0]
-		utils.verify_ldap_object(group)
+
+@pytest.mark.tags('udm', 'apptest')
+@pytest.mark.roles('domaincontroller_master')
+@pytest.mark.exposure('careful')
+def test_group_creation_with_single_letter_name(udm):
+	"""Create groups/group with single letter name"""
+	# packages:
+	#   - univention-config
+	#   - univention-directory-manager-tools
+	group = udm.create_group(name=uts.random_groupname(1))[0]
+	utils.verify_ldap_object(group)
