univention/univention-corporate-server

commit 69923edc57208ab4f8a77f9880338f01be307d7b

Bug #51804: call unset_listener_callback before unlocking the listener file. Check listener file on restart, even if listener.priv exists

management/univention-directory-notifier/src/network.c
@@ -63,7 +63,6 @@ extern void set_listener_callback ( int sig, siginfo_t *si, void *data);
 extern int get_schema_callback ();
 extern int get_listener_callback ();
 extern void unset_schema_callback ();
-extern void unset_listener_callback ();
 
 extern NotifyId_t notify_last_id;
 
@@ -294,7 +293,6 @@ void check_callbacks()
 	}
 	if ( get_listener_callback () ) {
 		notify_listener_change_callback ( 0, NULL, NULL);
-		unset_listener_callback();
 	}
 }
 int network_client_main_loop ( )

management/univention-directory-notifier/src/notify.c
@@ -56,6 +56,7 @@ extern NotifyId_t notify_last_id;
 extern Notify_t notify;
 extern long long notifier_lock_count;
 extern long long notifier_lock_time;
+extern void unset_listener_callback ();
 
 extern unsigned long SCHEMA_ID;
 
@@ -664,7 +665,10 @@ void notify_listener_change_callback(int sig, siginfo_t *si, void *data)
 			close(fd);
 		}
 
+               unset_listener_callback();
 		fclose_lock(&l_file);
+	} else {
+		set_listener_callback();
 	}
 
 	if ( ( file = fopen_lock (FILE_NAME_NOTIFIER_PRIV, "r+") ) == NULL ) {

commit e11cca73e91dd374f3dce21b7935f6ba91b7962e

fix[udn]: Fix compiler warning
> notify.c: In function ‘notify_listener_change_callback’:
> notify.c:688:3: warning: implicit declaration of function ‘set_listener_callback’; did you mean ‘unset_listener_callback’? [-Wimplici
> t-function-declaration]
>    set_listener_callback();
>    ^~~~~~~~~~~~~~~~~~~~~
>    unset_listener_callback

Fixes: 69923edc57208ab4f8a77f9880338f01be307d7b
Bug #51804: call unset_listener_callback before unlocking the listener file.

management/univention-directory-notifier/src/network.c
@@ -58,8 +58,6 @@ static NetworkClient_t *network_client_first = NULL;
 static int server_socketfd_listener;
 fd_set readfds;
 
-extern void set_schema_callback ( int sig, siginfo_t *si, void *data);
-extern void set_listener_callback ( int sig, siginfo_t *si, void *data);
 extern int get_schema_callback ();
 extern int get_listener_callback ();
 extern void unset_schema_callback ();

management/univention-directory-notifier/src/notify.c
@@ -56,6 +56,7 @@ extern NotifyId_t notify_last_id;
 extern Notify_t notify;
 extern long long notifier_lock_count;
 extern long long notifier_lock_time;
+extern void set_listener_callback(int sig, siginfo_t *si, void *data);
 extern void unset_listener_callback ();
 
 extern unsigned long SCHEMA_ID;
@@ -685,7 +686,7 @@ void notify_listener_change_callback(int sig, siginfo_t *si, void *data)
                unset_listener_callback();
 		fclose_lock(&l_file);
 	} else {
-		set_listener_callback();
+		set_listener_callback(0, NULL, NULL);
 	}
 
 	if ( ( file = fopen_lock (FILE_NAME_NOTIFIER_PRIV, "r+") ) == NULL ) {