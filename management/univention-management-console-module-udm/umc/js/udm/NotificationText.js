3c1de8054f92192ec4acaa14a7abbf79a4eeb8dd
Johannes Keiser <keiser@univention.de>
2021-09-10 10:54:13 +0200

    Bug #53665: adjust success notification in udm NewObjectDialog

--- management/univention-management-console-module-udm/umc/js/udm/NotificationText.js
+++ management/univention-management-console-module-udm/umc/js/udm/NotificationText.js
@@ -31,106 +31,29 @@
 define([
 	"dojo/_base/declare",
 	"dojo/_base/lang",
-	"dojo/_base/array",
-	"dojo/Deferred",
-	"dojo/when",
+	"dojo/dom-class",
 	"dojo/on",
-	"dojo/dom-style",
-	"dojo/dom-geometry",
 	"umc/widgets/Text",
-	"umc/widgets/ContainerWidget"
-], function(declare, lang, array, Deferred, when, on, domStyle, domGeom, Text, Container) {
-	return declare("umc.modules.udm.NotificationText", [Container], {
+], function(declare, lang, domClass, on, Text) {
+	return declare("umc.modules.udm.NotificationText", [Text], {
 		// summary:
-		//		This class extends the normal Container widget in order to encapsulate
+		//		This class extends the normal Text widget in order to encapsulate
 		//		some UDM specific notification behavior.
-
-		_text: null,
-
-		_currentAnimation: null, // use to avoid overlapping animations
 
 		buildRendering: function() {
 			this.inherited(arguments);
-
-			this._text = new Text({
-				labelPosition: 'middle'
-			});
-			this.own(this._text);
-			this.addChild(this._text);
-			
+			domClass.add(this.domNode, 'udmNewObjectDialog__successNotification');
+			this.set('visible', false);
 			on(this.domNode, 'click', lang.hitch(this, '_hideMessage'));
 		},
 
-		postCreate: function() {
-			this.inherited(arguments);
-
-			domStyle.set(this.domNode, {
-				height: "0",
-				margin: "0 -24px",
-				position: "relative",
-				"-webkit-transition": "height 1s",
-				"transition": "height 1s",
-				cursor: "pointer"
-			});
-
-			domStyle.set(this._text.domNode, {
-				overflow: "hidden",
-				height: "100%",
-				width: "100%"
-			});
-		},
-
 		showSuccess: function(message) {
-			domStyle.set(this.domNode, {
-				color: "rgb(60, 118, 61)",
-				backgroundColor: "rgb(223, 240, 216)"
-			});
-			this._showMessage(message);
-		},
-
-		_showMessage: function(message) {
-			var current_height = domGeom.getContentBox(this.domNode).h;
-			var stopDeferred = new Deferred();
-			if (current_height > 0) {
-				this._hideMessage(stopDeferred);
-			} else {
-				stopDeferred.resolve();
-			}
-
-			when(stopDeferred, lang.hitch(this, function() {
-				this.set('message', message);
-				domStyle.set(this.domNode, {
-					padding: "0.3em 1.6em",
-					"margin-bottom": "8px",
-					height: "auto"
-				});
-			}));
+			this.set('visible', true);
+			this.set('content', message);
 		},
 
 		_hideMessage: function(stopDeferred) {
-			domStyle.set(this.domNode, {
-				padding: "0",
-				"margin-bottom": "",
-				height: "0"
-			});
-
-			if (stopDeferred) {
-				setTimeout(stopDeferred.resolve, 1000);
-			}
+			this.set('visible', false);
 		},
-
-		_setMessageAttr: function(value) {
-			if (this._text.domNode) {
-				this._text.domNode.textContent = value;
-			}
-		},
-
-		_getMessageAttr: function() {
-			if (this._text.domNode) {
-				return this.domNode.textContent;
-			} else {
-				return null;
-			}
-		}
 	});
 });
