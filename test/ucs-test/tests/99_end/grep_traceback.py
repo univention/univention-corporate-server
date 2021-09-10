8396dc0b3aa9a10cf50651bcbe10cb9b205a25ad
Florian Best <best@univention.de>
2021-09-10 18:01:25 +0200

    Bug #53769: [grep_traceback]: ignore traceback

--- test/ucs-test/tests/99_end/grep_traceback.py
+++ test/ucs-test/tests/99_end/grep_traceback.py
@@ -270,6 +270,7 @@ COMMON_EXCEPTIONS = dict((re.compile(x), [re.compile(z) if isinstance(z, str) el
 	('INSUFFICIENT_ACCESS:.*', ['in password_sync_s4_to_ucs']),  # Bug #53721
 	("ModuleNotFoundError: No module named 'univention.config_registry'", ['/usr/sbin/univention-config-registry']),  # Bug #53765
 	("AttributeError: module 'univention.admin.syntax' has no attribute 'UMCMessageCatalogFilename_and_GNUMessageCatalog'", ['_unregister_app', 'import_hook_files', 'pupilgroups.py']),  # Bug #53754
+	('univention.admin.uexceptions.noObject: uid=.*', ['connector/ad/.*set_userPrincipalName_from_ucr']),  # Bug #53769
 ])
 
 
