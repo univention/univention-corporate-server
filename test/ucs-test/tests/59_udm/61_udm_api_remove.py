1245d0413e7c521e289e0062c76d99b26d4e7ed3
Florian Best <best@univention.de>
2021-09-23 09:20:26 +0200

    Bug #40709: [pytest] share one common conftest.py

--- test/ucs-test/tests/59_udm/61_udm_api_remove.py
+++ test/ucs-test/tests/59_udm/61_udm_api_remove.py
@@ -12,11 +12,49 @@ import pytest
 
 import univention.debug as ud
 from univention.testing.strings import random_username
+from univention.testing import utils
 from univention.udm.exceptions import DeleteError, NoObject
+from univention.udm import NotYetSavedError, UDM
 
+from typing import List, Tuple  # noqa: F401
 
 ud.init('/var/log/univention/directory-manager-cmd.log', ud.FLUSH, ud.NO_FUNCTION)
 ud.set_level(ud.ADMIN, ud.ALL)
+
+
+@pytest.fixture
+def simple_udm(ucr):  # type: () -> UDM
+	account = utils.UCSTestDomainAdminCredentials()
+	return UDM.credentials(
+		account.binddn,
+		account.bindpw,
+		ucr["ldap/base"],
+		ucr["ldap/master"],
+		ucr["ldap/master/port"],
+	).version(1)
+
+
+@pytest.fixture
+def schedule_delete_udm_obj(simple_udm):
+	objs = []  # type: List[Tuple[str, str]]
+
+	def _func(dn, udm_mod):  # type: (str, str) -> None
+		objs.append((dn, udm_mod))
+
+	yield _func
+
+	for dn, udm_mod_name in objs:
+		mod = simple_udm.get(udm_mod_name)
+		try:
+			udm_obj = mod.get(dn)
+		except NoObject:
+			print("UDM {!r} object {!r} does not exist (anymore).".format(udm_mod_name, dn))
+			continue
+		try:
+			udm_obj.delete(remove_childs=True)
+			print("Deleted UDM {!r} object {!r} through UDM.".format(udm_mod_name, dn))
+		except NotYetSavedError:
+			print("UDM {!r} object {!r} not deleted, it had not been saved.".format(udm_mod_name, dn))
 
 
 def test_remove_children(ldap_base, schedule_delete_udm_obj, simple_udm):
