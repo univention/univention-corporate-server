5d7457924a85c696ae51ff313b8a15252409c4f6
Philipp Hahn <hahn@univention.de>
2021-09-24 15:08:28 +0200

    style[udm/syntax] all_required: bool
    Bug #31857

--- management/univention-directory-manager-modules/modules/univention/admin/syntax.py
+++ management/univention-directory-manager-modules/modules/univention/admin/syntax.py
@@ -2172,7 +2172,7 @@ class keyAndValue(complex):
 	delimiter = ' = '
 	subsyntaxes = [(_('Key'), string), (_('Value'), string)]
 	subsyntax_key_value = True
-	all_required = 1
+	all_required = True
 
 
 class dnsMX(complex):
@@ -4622,7 +4622,7 @@ class translationTuple(complex):
 	delimiter = ': '
 	subsyntaxes = [(_('Language code (e.g. en_US)'), languageCode), (_('Text'), string)]
 	subsyntax_key_value = True
-	all_required = 1
+	all_required = True
 
 
 class translationTupleShortDescription(translationTuple):
