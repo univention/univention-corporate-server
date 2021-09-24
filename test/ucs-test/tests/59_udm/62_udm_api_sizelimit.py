afe63166e621c4e77004bc284c227a37780802d3
Ole Schwiegert <ole.schwiegert@oxur.de>
2021-09-24 13:38:20 +0200

    Bug #53832: Expose sizelimit parameter in simple UDM API

---
+++ test/ucs-test/tests/59_udm/62_udm_api_sizelimit.py
@@ -0,0 +1,29 @@
+#!/usr/share/ucs-test/runner /usr/bin/py.test
+# -*- coding: utf-8 -*-
+## desc: Test sizelimit for UDM API
+## exposure: safe
+## roles: [domaincontroller_master]
+## tags: [udm_api]
+## packages: [python-univention-directory-manager]
+## bugs: [53823]
+import pytest
+from univention.udm.exceptions import SearchLimitReached
+
+from univention.udm import UDM
+
+
+def test_sizelimit():
+    # No need to create any objects since there is at least two groups on any UCS
+    udm = UDM.admin().version(1)
+    groups_module = udm.get("groups/group")
+    with pytest.raises(SearchLimitReached):
+        list(groups_module.search("", sizelimit=1))
+
+
+def test_sizelimit_force_lookup():
+    # No need to create any objects since there is at least two groups on any UCS
+    udm = UDM.admin().version(1)
+    groups_module = udm.get("groups/group")
+    del groups_module._orig_udm_module.lookup_filter
+    with pytest.raises(SearchLimitReached):
+        list(groups_module.search("", sizelimit=1))
