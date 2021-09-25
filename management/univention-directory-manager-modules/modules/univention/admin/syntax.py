univention/univention-corporate-server

commit 89cacade6e08f9da71780d8f97e1f18d62cfa5a2

Bug #16923 udm: Handle optional complex values
Both parse() and tostring() abort on the first empty value, which breaks
parsing and printing the DHCP entry.

Also note:
- parsing the values again to tostring() is pointless, as the values
  already have been parsed and parse(string)->string is a no-op.
- I found no user, where "delimiters" is a list().
- The optional argument 'minn' will get used with the next patch.

git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-4.2-0@74205 1b283449-9f2f-0410-b571-8eb228e1a901

management/univention-directory-manager-modules/modules/univention/admin/syntax.py
@@ -220,64 +220,38 @@ class complex(ISyntax):
 	all_required = True
 
 	@classmethod
-	def parse(self, texts):
-		parsed = []
-
-		if self.min_elements is not None:
-			count = self.min_elements
-		else:
-			count = len(self.subsyntaxes)
-
-		if len(texts) < count:
+	def parse(self, texts, minn=None):
+		if minn is None:
+			minn = self.min_elements
+		if minn is None:
+			minn = len(self.subsyntaxes)
+		if len(texts) < minn:
 			raise univention.admin.uexceptions.valueInvalidSyntax(_("not enough arguments"))
-
-		if len(texts) > len(self.subsyntaxes):
+		elif len(texts) > len(self.subsyntaxes):
 			raise univention.admin.uexceptions.valueInvalidSyntax(_("too many arguments"))
 
-		for i in range(len(texts)):
-			univention.debug.debug(univention.debug.ADMIN, univention.debug.INFO, 'syntax.py: self.subsyntax[%s] is %s, texts is %s' % (i, self.subsyntaxes[i], texts))
-			if not inspect.isclass(self.subsyntaxes[i][1]):
-				s = self.subsyntaxes[i][1]
-			else:
-				s = self.subsyntaxes[i][1]()
-			if texts[i] is None:
-				if self.min_elements is None or (i + 1) < self.min_elements:
-					raise univention.admin.uexceptions.valueInvalidSyntax(_("Invalid syntax"))
-			p = s.parse(texts[i])
-			if not p:
-				break
+		parsed = []
+		for i, (text, (desc, syn)) in enumerate(zip(texts, self.subsyntaxes)):
+			univention.debug.debug(
+				univention.debug.ADMIN, univention.debug.INFO,
+				'syntax.py: subsyntax[%s]=%s, texts=%s' % (i, syn, text))
+			if text is None and i + 1 < minn:
+				raise univention.admin.uexceptions.valueInvalidSyntax(_("Missing argument"))
+			s = syn() if inspect.isclass(syn) else syn
+			p = s.parse(text)
 			parsed.append(p)
 		return parsed
 
 	@classmethod
 	def tostring(self, texts):
-		newTexts = []
-		if len(self.subsyntaxes) != len(texts):
-			return ''
-		else:
-			for i in range(0, len(texts)):
-				if texts[i]:
-					if not inspect.isclass(self.subsyntaxes[i][1]):
-						res = self.subsyntaxes[i][1].parse(texts[i])
-					else:
-						res = self.subsyntaxes[i][1]().parse(texts[i])
-					if res:
-						newTexts.append(res)
-				elif self.all_required:
-					return ''
+		if self.all_required:
+			if len(self.subsyntaxes) != len(texts) or not all(texts):
+				return ''
 
-		if not newTexts:
-			return ''
+		if isinstance(self.delimiter, basestring):
+			return self.delimiter.join(texts)
 
-		txt = ''
-		if isinstance(self.delimiter, type([])):
-			for i in range(0, len(newTexts)):
-				txt += self.delimiter[i] + newTexts[i]
-			txt += self.delimiter[-1]
-		elif isinstance(self.delimiter, type('')):
-			txt = self.delimiter.join(newTexts)
-
-		return txt
+		return ''.join([s for sub in zip(self.delimiters, texts) for s in sub] + [self.delimiter[-1]])
 
 	@classmethod
 	def new(self):

commit a03e7ace25000a5ca3c36b41dba333df87247db2

fix[udm/syntax] Remove broken delimiters
89cacade6e08f9da71780d8f97e1f18d62cfa5a2 broke it for UCS-4.2 and no one
complained since then.

Remove broken code.

Bug #31857

management/univention-directory-manager-modules/modules/univention/admin/syntax.py
@@ -313,7 +313,7 @@ class complex(ISyntax):
 	Base class for complex syntax classes consisting of multiple sub-items.
 	"""
 
-	delimiter = ' '  # type: Union[str, Sequence[str]]
+	delimiter = ' '  # type: str
 	"""
 	Delimiter to separate the sub-items. Two possibilities:
 
@@ -407,11 +407,7 @@ class complex(ISyntax):
 			if len(self.subsyntaxes) != len(texts) or not all(texts):
 				return ''
 
-		if isinstance(self.delimiter, basestring):
-			return self.delimiter.join(texts)
-
-		# FIXME: s/(delimiter)s/\1/
-		return ''.join([s for sub in zip(self.delimiters, texts) for s in sub] + [self.delimiter[-1]])
+		return self.delimiter.join(texts)
 
 	@classmethod
 	def new(self):