48178f65fd9c75da7e417e744327d6438ccdfcf0
Philipp Hahn <hahn@univention.de>
2021-09-25 12:05:13 +0200

    style[ipcalc] Convert to setuptool
    Bug #53345

---
+++ base/univention-ipcalc/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# SPDX-License-Identifier: AGPL-3.0
+# Copyright 2021 Univention GmbH
+
+from setuptools import setup
+
+version = open("debian/changelog", "r").readline().split()[1][1:-1]
+
+setup(
+    version=version,
+)
