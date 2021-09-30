univention/univention-corporate-server

commit b2feeeb830c319f2486236b8d5076d1cfa0b8a53

style[udm/syntax] Make choices a list of tuple
for consistency and add explicit type annotations where required.

Some syntax classes like `DNS_ForwardZoneList` do not declare their own
`choices` as they `depend` on some other parent class. For them
`choices` remains undefined, for which `parse` must return the value
unmodified.

Bug #31857

management/univention-directory-manager-modules/modules/univention/admin/syntax.py
@@ -274,13 +274,16 @@ class select(ISyntax):
 	def parse(self, text):
 		# type: (Any) -> Optional[str]
 		# for the UDM CLI
-		if not hasattr(self, 'choices'):
+		if not text and select.empty_value:
+			return text
+		try:
+			choices = getattr(self, "choices")
+			if any(text == c[0] for c in choices):
+				return text
+			return None
+		except AttributeError:
 			return text
 
-		if text in map(lambda x: x[0], self.choices) or (not text and select.empty_value):
-			return text
-
-		return None  # FIXME
 
 class combobox(select):
 	"""
@@ -298,7 +301,7 @@ class MultiSelect(ISyntax):
 	"""
 	Select multiple items from a list of choices.
 	"""
-	choices = ()  # type: Sequence[Tuple[str, Any]]
+	choices = []  # type: Sequence[Tuple[Any, str]]
 	"""The list of choices."""
 	empty_value = True
 	"""Allow the empty value."""
@@ -2629,12 +2632,12 @@ class TimeUnits(select):
 	Syntax to select a time unit.
 	"""
 	size = 'Half'
-	choices = (
+	choices = [
 		('seconds', _('seconds')),
 		('minutes', _('minutes')),
 		('hours', _('hours')),
 		('days', _('days'))
-	)
+	]
 
 
 class TimeString(simple):
@@ -2764,7 +2767,11 @@ class NetworkType(select):
 	"""
 	Syntax to select network technology type.
 	"""
-	choices = (('ethernet', _('Ethernet')), ('fddi', _('FDDI')), ('token-ring', _('Token-Ring')))
+	choices = [
+		('ethernet', _('Ethernet')),
+		('fddi', _('FDDI')),
+		('token-ring', _('Token-Ring')),
+	]
 
 
 class MAC_Address(simple):
@@ -2914,24 +2921,24 @@ class IMAP_POP3(select):
 	"""
 	Syntax to select between |IMAP| and POP3.
 	"""
-	choices = (
+	choices = [
 		('IMAP', _('IMAP')),
 		('POP3', _('POP3')),
-	)
+	]
 
 
 class IMAP_Right(select):
 	"""
 	Syntax to select an |IMAP| access control permission.
 	"""
-	choices = (
+	choices = [
 		('none', _('No access')),
 		('read', _('Read')),
 		('post', _('Post')),
 		('append', _('Append')),
 		('write', _('Write')),
 		('all', _('All'))
-	)
+	]
 
 
 class UserMailAddress(UDM_Objects):
@@ -3698,7 +3705,7 @@ class IP_AddressList(ipAddress, select):
 	"""
 	Syntax to select an IP address from the lists of addresses stored with the machine account.
 	"""
-	choices = ()
+	choices = []  # type: Sequence[Tuple[str, str]]
 	depends = 'ip'
 
 
@@ -3718,7 +3725,7 @@ class MAC_AddressList(MAC_Address, select):
 	"""
 	Syntax to select a MAC address from the lists of addresses stored with the machine account.
 	"""
-	choices = ()
+	choices = []  # type: Sequence[Tuple[str, str]]
 	depends = 'mac'
 
 
@@ -4653,7 +4660,18 @@ class SambaLogonHours(MultiSelect):
 	...
 	valueError:
 	"""
-	choices = [(idx * 24 + hour, '%s %d-%d' % (day, hour, hour + 1)) for idx, day in ((0, _('Sun')), (1, _('Mon')), (2, _('Tue')), (3, _('Wed')), (4, _('Thu')), (5, _('Fri')), (6, _('Sat'))) for hour in range(24)]
+	choices = [
+		(idx * 24 + hour, '%s %d-%d' % (day, hour, hour + 1))
+		for idx, day in (
+			(0, _('Sun')),
+			(1, _('Mon')),
+			(2, _('Tue')),
+			(3, _('Wed')),
+			(4, _('Thu')),
+			(5, _('Fri')),
+			(6, _('Sat'))
+		) for hour in range(24)
+	]
 
 	type_class = univention.admin.types.SambaLogonHours
 
@@ -5726,7 +5744,7 @@ class Country(select):
 	"""
 	empty_value = True
 
-	choices = []
+	choices = []  # type: List[Tuple[str, str]]
 
 	@classmethod
 	def update_choices(cls):

commit 110c6b0874369b4a38f57bd341630b92d8ab897f

style[udm/syntax] Harmoize UCS-4.4-9 and UCS-5.0-0 code for select.parse
Apply missing part from refactored a46cbdb708ad7601fcf03b994e88ccfa526b9c70

Bug #31857
Fixes: b2feeeb830c319f2486236b8d5076d1cfa0b8a53

management/univention-directory-manager-modules/modules/univention/admin/syntax.py
@@ -274,16 +274,15 @@ class select(ISyntax):
 	def parse(self, text):
 		# type: (Any) -> Optional[str]
 		# for the UDM CLI
-		if not text and select.empty_value:
-			return text
 		try:
 			choices = getattr(self, "choices")
-			if any(text == c[0] for c in choices):
-				return text
-			return None
 		except AttributeError:
 			return text
-
+		if not text and select.empty_value:
+			return text
+		if any(text == c[0] for c in choices):
+			return text
+		return None
 
 class combobox(select):
 	"""