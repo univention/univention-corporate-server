9d04198238815d1482318764722a26fc8afd3c6e
Florian Best <best@univention.de>
2021-10-06 11:48:26 +0200

    Bug #50050: shared_memory_dict.py: Add Python 2 compatibility

--- management/univention-directory-manager-rest/shared_memory_dict/dict.py
+++ management/univention-directory-manager-rest/shared_memory_dict/dict.py
@@ -9,18 +9,8 @@ from contextlib import contextmanager
 from functools import wraps
 from multiprocessing import Lock
 from multiprocessing_shared_memory import SharedMemory
-from typing import (
-    Any,
-    Dict,
-    Generator,
-    ItemsView,
-    Iterator,
-    KeysView,
-    Optional,
-    ValuesView,
-)
 
-from .serializers import PickleSerializer, SharedMemoryDictSerializer
+from .serializers import PickleSerializer
 
 MEMORY_NAME = 'sm_{name}'
 NOT_GIVEN = object()
@@ -44,26 +34,20 @@ def lock(func):
     return wrapper
 
 
-class SharedMemoryDict:
-    def __init__(
-        self,
-        name: str,
-        size: int,
-        *,
-        serializer: SharedMemoryDictSerializer = DEFAULT_SERIALIZER,
-    ) -> None:
-        super().__init__()
+class SharedMemoryDict(object):
+    def __init__(self, name, size, serializer=DEFAULT_SERIALIZER):
+        super(SharedMemoryDict, self).__init__()
         self._serializer = serializer
         self._memory_block = self._get_or_create_memory_block(
             MEMORY_NAME.format(name=name), size
         )
 
-    def cleanup(self) -> None:
+    def cleanup(self):
         if not hasattr(self, '_memory_block'):
             return
         self._memory_block.close()
 
-    def move_to_end(self, key: str, last: Optional[bool] = True) -> None:
+    def move_to_end(self, key, last=True):
         warnings.warn(
             'The \'move_to_end\' method will be removed in future versions. '
             'Use pop and reassignment instead.',
@@ -74,10 +58,10 @@ class SharedMemoryDict:
             db[key] = db.pop(key)
 
     @lock
-    def clear(self) -> None:
+    def clear(self):
         self._save_memory({})
 
-    def popitem(self, last: Optional[bool] = None) -> Any:
+    def popitem(self, last=None):
         if last is not None:
             warnings.warn(
                 'The \'last\' parameter will be removed in future versions. '
@@ -90,91 +74,91 @@ class SharedMemoryDict:
 
     @contextmanager
     @lock
-    def _modify_db(self) -> Generator:
+    def _modify_db(self):
         db = self._read_memory()
         yield db
         self._save_memory(db)
 
-    def __getitem__(self, key: str) -> Any:
+    def __getitem__(self, key):
         return self._read_memory()[key]
 
-    def __setitem__(self, key: str, value: Any) -> None:
+    def __setitem__(self, key, value):
         with self._modify_db() as db:
             db[key] = value
 
-    def __len__(self) -> int:
+    def __len__(self):
         return len(self._read_memory())
 
-    def __delitem__(self, key: str) -> None:
+    def __delitem__(self, key):
         with self._modify_db() as db:
             del db[key]
 
-    def __iter__(self) -> Iterator:
+    def __iter__(self):
         return iter(self._read_memory())
 
     def __reversed__(self):
         return reversed(self._read_memory())
 
-    def __del__(self) -> None:
+    def __del__(self):
         self.cleanup()
 
-    def __contains__(self, key: str) -> bool:
+    def __contains__(self, key):
         return key in self._read_memory()
 
-    def __eq__(self, other: Any) -> bool:
+    def __eq__(self, other):
         return self._read_memory() == other
 
-    def __ne__(self, other: Any) -> bool:
+    def __ne__(self, other):
         return self._read_memory() != other
 
     if sys.version_info > (3, 8):
 
-        def __or__(self, other: Any):
+        def __or__(self, other):
             return self._read_memory() | other
 
-        def __ror__(self, other: Any):
+        def __ror__(self, other):
             return other | self._read_memory()
 
-        def __ior__(self, other: Any):
+        def __ior__(self, other):
             with self._modify_db() as db:
                 db |= other
                 return db
 
-    def __str__(self) -> str:
+    def __str__(self):
         return str(self._read_memory())
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return repr(self._read_memory())
 
-    def get(self, key: str, default: Optional[Any] = None) -> Any:
+    def get(self, key, default=None):
         return self._read_memory().get(key, default)
 
-    def keys(self) -> KeysView[Any]:
+    def keys(self):
         return self._read_memory().keys()
 
-    def values(self) -> ValuesView[Any]:
+    def values(self):
         return self._read_memory().values()
 
-    def items(self) -> ItemsView:
+    def items(self):
         return self._read_memory().items()
 
-    def pop(self, key: str, default: Optional[Any] = NOT_GIVEN):
+    def pop(self, key, default=NOT_GIVEN):
         with self._modify_db() as db:
             if default is NOT_GIVEN:
                 return db.pop(key)
             return db.pop(key, default)
 
-    def update(self, other=(), /, **kwds):
+    def update(self, other=(), **kwds):
         with self._modify_db() as db:
             db.update(other, **kwds)
 
-    def setdefault(self, key: str, default: Optional[Any] = None):
+    def setdefault(self, key, default=None):
         with self._modify_db() as db:
             return db.setdefault(key, default)
 
     def _get_or_create_memory_block(
-        self, name: str, size: int
-    ) -> SharedMemory:
+        self, name, size
+    ):
         try:
             return SharedMemory(name=name)
         except FileNotFoundError:
@@ -183,16 +167,16 @@ class SharedMemoryDict:
             shm.buf[: len(data)] = data
             return shm
 
-    def _save_memory(self, db: Dict[str, Any]) -> None:
+    def _save_memory(self, db):
         data = self._serializer.dumps(db)
         try:
             self._memory_block.buf[: len(data)] = data
-        except ValueError as exc:
-            raise ValueError("exceeds available storage") from exc
+        except ValueError:
+            raise ValueError("exceeds available storage")
 
-    def _read_memory(self) -> Dict[str, Any]:
+    def _read_memory(self):
         return self._serializer.loads(self._memory_block.buf.tobytes())
 
     @property
-    def shm(self) -> SharedMemory:
+    def shm(self):
         return self._memory_block
