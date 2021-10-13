univention/univention-corporate-server

commit 08ed24bd0a3bdd2cecfd720afaed816cccc3170a

Bug #52211 ucslint: Test for univention-join dh sequence
Fix deb822 parser, see <man:deb822(5)> and <man:deb-src-control(5)>
Move common code to base for re-use.
Implement test for using `dh --with univention-join`.

packaging/ucslint/univention/ucslint/base.py
@@ -29,7 +29,7 @@
 
 import os
 import re
-from typing import Callable, Dict, Iterable, Iterator, List, Match, Optional, Pattern, Tuple  # noqa F401
+from typing import Any, Callable, Dict, Iterable, Iterator, List, Mapping, Match, Optional, Pattern, Set, Tuple  # noqa F401
 
 try:
 	from junit_xml import TestCase  # type: ignore
@@ -291,28 +291,67 @@ class DebianControlEntry(Dict[str, str]):
 	:param content: String content of paragraph.
 	"""
 
+	RE_MULTILINE = re.compile(r'$\n\s', re.MULTILINE)
+
 	def __init__(self, content: str) -> None:
 		dict.__init__(self)
 
-		lines = content.splitlines()
-		i = 0
-		# handle multiline entries and merge them into one line
-		while i < len(lines):
-			if not lines[i] or lines[i].startswith('#'):
-				del lines[i]
-				continue
-			if lines[i].startswith(' ') or lines[i].startswith('\t'):
-				lines[i - 1] += ' %s' % lines[i].lstrip(' \t')
-				del lines[i]
-				continue
-			i += 1
-
-		# split lines into dictionary
-		for line in lines:
-			if ':' not in line:
+		content = self.RE_MULTILINE.sub(' ', content)
+		for line in content.splitlines():
+			try:
+				key, val = line.split(':', 1)
+			except ValueError:
 				raise DebianControlParsingError(line)
-			key, val = line.split(': ', 1)
-			self[key] = val
+			self[key.strip()] = val.strip()
+
+	def _split_field(self, s: str) -> Iterator[str]:
+		"""Split control field into parts. Returns generator."""
+		for con in s.split(','):
+			con = con.strip()
+			for dis in con.split('|'):
+				i = dis.find('(')
+				if i >= 0:
+					dis = dis[:i]
+
+				pkg = dis.strip()
+				if pkg:
+					yield pkg
+
+	def _pkgs(self, key: str) -> Set[str]:
+		"""
+		Return package list.
+		"""
+		return set(self._split_field(self.get(key, "")))
+
+
+class DebianControlSource(DebianControlEntry):
+	"""
+	Source package entry from :file:`debian/control`.
+	"""
+	dep = property(lambda self: self._pkgs('Build-Depends'))
+	dep_indep = property(lambda self: self._pkgs('Build-Depends-Indep'))
+	dep_arch = property(lambda self: self._pkgs('Build-Depends-Arch'))
+	dep_all = property(lambda self: self.dep | self.dep_indep | self.dep_arch)
+	conf = property(lambda self: self._pkgs('Build-Conflicts'))
+	conf_indep = property(lambda self: self._pkgs('Build-Conflicts-Indep'))
+	conf_arch = property(lambda self: self._pkgs('Build-Conflicts-Arch'))
+	conf_all = property(lambda self: self.conf | self.conf_indep | self.conf_arch)
+
+
+class DebianControlBinary(DebianControlEntry):
+	"""
+	Binary package entry from :file:`debian/control`.
+	"""
+	pre = property(lambda self: self._pkgs('Pre-Depends'))
+	dep = property(lambda self: self._pkgs('Depends'))
+	rec = property(lambda self: self._pkgs('Recommends'))
+	sug = property(lambda self: self._pkgs('Suggests'))
+	all = property(lambda self: self.pre | self.dep | self.rec | self.sug)
+	bre = property(lambda self: self._pkgs('Breaks'))
+	enh = property(lambda self: self._pkgs('Enhances'))
+	repl = property(lambda self: self._pkgs('Replaces'))
+	conf = property(lambda self: self._pkgs('Conflicts'))
+	pro = property(lambda self: self._pkgs('Provides'))
 
 
 class ParserDebianControl(object):
@@ -322,24 +361,25 @@ class ParserDebianControl(object):
 	:param filename: Full path.
 	"""
 
+	RE_COMMENT = re.compile(r'^#.*$', re.MULTILINE)
+	RE_SECTION = re.compile(r'\n{2,}', re.MULTILINE)
+
 	def __init__(self, filename: str) -> None:
 		self.filename = filename
-		self.binary_sections = []  # type: List[DebianControlEntry]
 
 		try:
 			content = open(self.filename, 'r').read()
 		except EnvironmentError:
 			raise FailedToReadFile(self.filename)
 
-		parts = content.split('\n\n')
-		if len(parts) < 2:
-			raise DebianControlNotEnoughSections()
+		content = self.RE_COMMENT.sub('', content)
 
-		self.source_section = DebianControlEntry(parts[0])
-		for part in parts[1:]:
-			package = DebianControlEntry(part)
-			if package:
-				self.binary_sections.append(package)
+		parts = [part for part in self.RE_SECTION.split(content) if part]
+		try:
+			self.source_section = DebianControlSource(parts.pop(0))
+			self.binary_sections = [DebianControlBinary(part) for part in parts]
+		except IndexError:
+			raise DebianControlNotEnoughSections()
 
 
 class RegExTest(object):

packaging/ucslint/testframework/0001-22/debian/control
@@ -1,0 +1,13 @@
+Source: src
+Section: univention
+Priority: optional
+Maintainer: Univention GmbH <packages@univention.de>
+Build-Depends:
+ debhelper,
+Standards-Version: 3.7.2
+
+Package: bin
+Architecture: all
+Depends:
+ ${misc:Depends},
+Description: desc

packaging/ucslint/testframework/0001-23/debian/control
@@ -1,0 +1,14 @@
+Source: src
+Section: univention
+Priority: optional
+Maintainer: Univention GmbH <packages@univention.de>
+Build-Depends:
+ debhelper,
+ univention-join-dev,
+Standards-Version: 3.7.2
+
+Package: bin
+Architecture: all
+Depends:
+ ${misc:Depends},
+Description: desc

packaging/ucslint/testframework/0001-22.correct
@@ -1,0 +1,1 @@
+E:0001-22: debian/control: "dh --with univention-join" is used in "debian/rules" without "Build-Depends: univention-join-dev" in "debian/rules"

packaging/ucslint/testframework/0001-22/00bin.inst
@@ -1,0 +1,7 @@
+#!/bin/sh
+## joinscript api: bindpwdfile
+VERSION="1"
+. /usr/share/univention-join/joinscripthelper.lib
+joinscript_init
+joinscript_save_current_version
+exit 0

packaging/ucslint/testframework/0001-22/debian/compat
@@ -1,0 +1,1 @@
+9

packaging/ucslint/testframework/0001-22/debian/copyright
@@ -1,0 +1,29 @@
+Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: Univention GmbH
+Upstream-Contact: <package@univention.de>
+Source: https://updates.software-univention.de/
+
+Files: *
+Copyright: 2012-2020 Univention GmbH
+License: AGPL-3.0-only
+ The source code of the software contained in this package
+ as well as the source package itself are made available
+ under the terms of the GNU Affero General Public License version 3
+ (GNU AGPL V3) as published by the Free Software Foundation.
+ .
+ Binary versions of this program provided by Univention to you as
+ well as other copyrighted, protected or trademarked materials like
+ Logos, graphics, fonts, specific documentations and configurations,
+ cryptographic keys etc. are subject to a license agreement between
+ you and Univention and not subject to the GNU AGPL V3.
+ .
+ In the case you use this program under the terms of the GNU AGPL V3,
+ the program is provided in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Affero General Public License for more details.
+ .
+ You should have received a copy of the GNU Affero General Public
+ License with the Debian GNU/Linux or Univention distribution in file
+ /usr/share/common-licenses/AGPL-3; if not, see
+ <https://www.gnu.org/licenses/>.

packaging/ucslint/testframework/0001-22/debian/rules
@@ -1,0 +1,3 @@
+#!/usr/bin/make -f
+%:
+	dh $@ --with univention-join

commit eadf448305ecfb12b47233b8b80b974fc6e0836a

fix[ucslint] Strip newline after comment
otherwise the newlines is interpreted as a paragraph separator.

Fixes: 08ed24bd0a3bdd2cecfd720afaed816cccc3170a
Bug #52211

packaging/ucslint/testframework/0014-run2/debian/control
@@ -1,6 +1,7 @@
 Source: ucslint
 Build-Depends: foo,
  univention-config-dev,
+# comment
  univention-management-console-dev
 Build-Depends-Indep: bar
 

packaging/ucslint/univention/ucslint/base.py
@@ -361,7 +361,7 @@ class ParserDebianControl(object):
 	:param filename: Full path.
 	"""
 
-	RE_COMMENT = re.compile(r'^#.*$', re.MULTILINE)
+	RE_COMMENT = re.compile(r'^#.*$\n?', re.MULTILINE)
 	RE_SECTION = re.compile(r'\n{2,}', re.MULTILINE)
 
 	def __init__(self, filename: str) -> None: