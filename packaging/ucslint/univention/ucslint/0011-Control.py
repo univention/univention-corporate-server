univention/univention-corporate-server

commit 0c005a03f0c0063c1f68c7d7fcc52b1a4c4cc504

Bug #51246 ucslint: Ignore more DH files
<man:dh_systemd_enable(1)>
<man:dh_python2(1)>
<man:dh_python3(1)>

packaging/ucslint/univention/ucslint/0011-Control.py
@@ -190,8 +190,11 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 		'compat',  # dh
 		'control',
 		'copyright',  # dh_installdocs default
+		'debhelper-build-stamp',  # dh
 		'files',  # dh_builddeb
 		'NEWS',  # dh_installchangelogs default
+		'pydist-overrides',  # dh_python2
+		'py3dist-overrides',  # dh_python3
 		'rules',
 		'source.lintian-overrides',  # dh_lintian
 		'ucslint.overrides',
@@ -199,6 +202,7 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 
 	KNOWN_DH_FILES = set((
 		'bash-completion',  # dh_bash-completion
+		'bcep',  # dh_python3
 		'bug-control',  # dh_bugfiles
 		'bug-presubj',  # dh_bugfiles
 		'bug-script',  # dh_bugfiles
@@ -237,13 +241,23 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 		'prerm',  # dh_installdeb
 		'prerm.debhelper',  # dh_installdeb
 		'README.Debian',  # dh_installdocs
+		'mount',  # dh_systemd_enable
+		'path',  # dh_systemd_enable
+		'pydist',  # dh_python2 dh_python3
+		'pyinstall',  # dh_python2 dh_python3
+		'pyremove',  # dh_python2 dh_python3
+		'service',  # dh_systemd_enable
 		'sgmlcatalogs',  # dh_installcatalogs
 		'sharedmimeinfo',  # dh_installmime
 		'shlibs',  # dh_installdeb
+		'socket',  # dh_systemd_enable
 		'substvars',  # dh_gencontrol
 		'symbols',  # dh_makeshlibs
 		'symbols.i386',  # dh_makeshlibs
+		'target',  # dh_systemd_enable
 		'templates',  # dh_installdebconf
+		'timer',  # dh_systemd_enable
+		'tmpfile',  # dh_systemd_enable
 		'TODO',  # dh_installdocs
 		'triggers',  # dh_installdeb
 		'umc-modules',  # dh-umc-modules-install
@@ -251,9 +265,9 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 		'univention-config-registry-mapping',  # univention-install-config-registry-info
 		'univention-config-registry',  # univention-install-config-registry
 		'univention-config-registry-variables',  # univention-install-config-registry-info
+		'univention-l10n',  # univention-l10n-build / univention-l10n-install
 		'univention-service',  # univention-install-service-info
 		'wm',  # dh_installwm
-		'univention-l10n',  # univention-l10n-build / univention-l10n-install
 	))
 
 	NAMED_DH_FILES = set((

commit cc91cad52f6d7d6623d9abda43d0854cef8f5191

fix[ucslint] sh_systemd-enable --name
<man:dh_systemd_enable(1)> has option `--name` to use alternative names.

Fixes: 0c005a03f0c0063c1f68c7d7fcc52b1a4c4cc504
Bug #51246

packaging/ucslint/univention/ucslint/0011-Control.py
@@ -245,23 +245,16 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 		'prerm',  # dh_installdeb
 		'prerm.debhelper',  # dh_installdeb
 		'README.Debian',  # dh_installdocs
-		'mount',  # dh_systemd_enable
-		'path',  # dh_systemd_enable
 		'pydist',  # dh_python2 dh_python3
 		'pyinstall',  # dh_python2 dh_python3
 		'pyremove',  # dh_python2 dh_python3
-		'service',  # dh_systemd_enable
 		'sgmlcatalogs',  # dh_installcatalogs
 		'sharedmimeinfo',  # dh_installmime
 		'shlibs',  # dh_installdeb
-		'socket',  # dh_systemd_enable
 		'substvars',  # dh_gencontrol
 		'symbols',  # dh_makeshlibs
 		'symbols.i386',  # dh_makeshlibs
-		'target',  # dh_systemd_enable
 		'templates',  # dh_installdebconf
-		'timer',  # dh_systemd_enable
-		'tmpfile',  # dh_systemd_enable
 		'TODO',  # dh_installdocs
 		'triggers',  # dh_installdeb
 		'umc-modules',  # dh-umc-modules-install
@@ -297,9 +290,16 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 		'logrotate',  # dh_installlogrotate
 		'modprobe',  # dh_installmodules
 		'modules',  # dh_installmodules
+		'mount',  # dh_systemd_enable
 		'pam',  # dh_installpam
+		'path',  # dh_systemd_enable
 		'ppp.ip-down',  # dh_installppp
 		'ppp.ip-up',  # dh_installppp
+		'service',  # dh_systemd_enable
+		'socket',  # dh_systemd_enable
+		'target',  # dh_systemd_enable
+		'timer',  # dh_systemd_enable
+		'tmpfile',  # dh_systemd_enable
 		'udev',  # dh_installudev
 		'upstart',  # dh_installinit
 	}