3e3eb90f27a3aceb289b17fd7f7323af5e278651
Arvid Requate <requate@univention.de>
2021-11-10 10:58:52 +0100

    fixup! Bug #54014: Configure username map script for nss_ldap

--- services/univention-samba/scripts/mapusers.sh
+++ services/univention-samba/scripts/mapusers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-eval "$(ucr shell windows/domain)"
-winbind_separator=$(testparm -sv -d0 --parameter-name="winbind separator" 2>/dev/null)
+testparm_output=$(testparm -sv -d0 2>/dev/null)
+windows_domain=$(sed -n 's/^\tworkgroup = //p' <<<"$testparm_output")
+winbind_separator=$(sed -n 's/^\twinbind separator = //p' <<<"$testparm_output")
 echo -nE "${1#$windows_domain$winbind_separator}"
