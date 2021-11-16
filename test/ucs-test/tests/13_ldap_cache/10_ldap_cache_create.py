f9d6422d4bce3f4126b95377ea0d32a83e842c46
Esteban Martinena Guerrero <martinena@univention.de>
2021-11-16 18:51:24 +0100

    Added some more tests to LDAP cache fixed test

--- test/ucs-test/tests/13_ldap_cache/10_ldap_cache_create.py
+++ test/ucs-test/tests/13_ldap_cache/10_ldap_cache_create.py
@@ -5,23 +5,17 @@
 ## packages:
 ##   - univention-group-membership-cache
 
-
-import re
-
 import pytest
 from ldap import explode_dn
 
+from univention.testing.strings import random_name
 from univention.testing.utils import stop_listener, start_listener, verify_ldap_object
 from univention.ldap_cache.cache import get_cache
 
 
-def test_cache_creation(udm, group1, group2, user1, user2):
+def test_cache_user_add_remove_from_group(udm, group1, user1, user2):
 	"""
 	Create new users and groups and check if the cache is updated.
-	1) create user1
-	2) create user2
-	3) add user1 and user2 to group
-	4) check if user1 and user2 are in the changed group
 	"""
 	udm.modify_object('groups/group', dn=group1, users=[user1], wait_for_replication=True)
 	cache = get_cache()
@@ -38,12 +32,56 @@ def test_cache_creation(udm, group1, group2, user1, user2):
 	assert len(data_members[group1]) == 2
 
 	udm.modify_object('groups/group', dn=group1, remove={'users': [user1]}, wait_for_replication=True)
+	cache = get_cache()
 	data_uids = cache.get_sub_cache('memberUids').load()
 	data_members = cache.get_sub_cache('uniqueMembers').load()
 	assert data_uids[group1] == [explode_dn(user2, 1)[0]]
 	assert data_members[group1] == [user2]
 
 	udm.modify_object('groups/group', dn=group1, remove={'users': [user2]}, wait_for_replication=True)
+	cache = get_cache()
+	data_uids = cache.get_sub_cache('memberUids').load()
+	data_members = cache.get_sub_cache('uniqueMembers').load()
+	assert group1 not in data_uids
+	assert group1 not in data_members
+
+
+def test_cache_group_rename(udm, group1, user1):
+	cache = get_cache()
+	udm.modify_object('groups/group', dn=group1, users=[user1], wait_for_replication=True)
+	data_uids = cache.get_sub_cache('memberUids').load()
+	data_members = cache.get_sub_cache('uniqueMembers').load()
+	assert data_uids[group1] == [explode_dn(user1, 1)[0]]
+	assert data_members[group1] == [user1]
+	new_group_dn = udm.modify_object('groups/group', dn=group1, name=random_name(), wait_for_replication=True)
+
+	data_uids = cache.get_sub_cache('memberUids').load()
+	data_members = cache.get_sub_cache('uniqueMembers').load()
+	assert group1 not in data_uids
+	assert group1 not in data_members
+	assert data_uids[new_group_dn] == [explode_dn(user1, 1)[0]]
+	assert data_members[new_group_dn] == [user1]
+
+
+def test_cache_user_rename(udm, group1, user1):
+	udm.modify_object('groups/group', dn=group1, users=[user1], wait_for_replication=False)
+	new_user_dn = udm.modify_object('users/user', dn=user1, set={'username': '%scopy' % ("other_"+random_name())}, wait_for_replication=True)
+
+	cache = get_cache()
+	data_uids = cache.get_sub_cache('memberUids').load()
+	data_members = cache.get_sub_cache('uniqueMembers').load()
+	# old user not in cache
+	assert all(user1 not in members for members in data_members.values())
+	assert all(explode_dn(user1, 1)[0] not in members for members in data_uids.values())
+	# new user in cache
+	assert data_uids[group1] == [explode_dn(new_user_dn, 1)[0]]
+	assert data_members[group1] == [new_user_dn]
+
+
+def test_cache_group_remove(udm, group1, user1):
+	udm.modify_object('groups/group', dn=group1, users=[user1], wait_for_replication=True)
+	udm.remove_object('groups/group', dn=group1, wait_for_replication=True)
+	cache = get_cache()
 	data_uids = cache.get_sub_cache('memberUids').load()
 	data_members = cache.get_sub_cache('uniqueMembers').load()
 	assert group1 not in data_uids
