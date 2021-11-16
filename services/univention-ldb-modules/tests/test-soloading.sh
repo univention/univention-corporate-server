316f5992770fe432ce834b3c148d09e105f0a6eb
Philipp Hahn <hahn@univention.de>
2021-11-16 11:25:55 +0100

    style[ldb-modules] shellcheck issues
    Bug #48823

--- services/univention-ldb-modules/tests/test-soloading.sh
+++ services/univention-ldb-modules/tests/test-soloading.sh
@@ -1,36 +1,29 @@
 #!/bin/sh
+set -e
 
-if [ -n "$TEST_DATA_PREFIX" ]; then
-	LDB_URL="$TEST_DATA_PREFIX/tdbtest.ldb"
-else
-	LDB_URL="tdbtest.ldb"
-fi
-export LDB_URL
+test="$(dirname "$0")"
+base="${test}/.."
 
-PATH=bin:$PATH
-export PATH
+export PATH="${base}/bin${PATH:+:$PATH}"
+export LDBDIR="${LDBDIR:-${base}}"
+export LDB_MODULES_PATH="${base}/bin/modules/ldb/"
+export LDB_URL="${TEST_DATA_PREFIX:-.}/tdbtest.ldb"
 
-rm -f $LDB_URL*
+rm -f "$LDB_URL"*
 
-if [ -z "$LDBDIR" ]; then
-    LDBDIR=`dirname $0`/..
-    export LDBDIR
-fi
-
-LDB_MODULES_PATH=$(pwd)/bin/modules/ldb/
-export LDB_MODULES_PATH
-
-cat <<EOF | $VALGRIND ldbadd || exit 1
+${VALGRIND:+$VALGRIND} ldbadd <<EOF
 dn: @MODULES
 @LIST: univention_samaccountname_ldap_check
 EOF
 
 testaccount=Administrator
 
-cat <<EOF | $VALGRIND ldbadd --controls="bypass_samaccountname_ldap_check:0" --trace || exit 1
+${VALGRIND:+$VALGRIND} ldbadd --controls="bypass_samaccountname_ldap_check:0" --trace <<EOF
 dn: dc=bar
 dc: bar
 sAMAccountName: $testaccount
 EOF
 
-$VALGRIND ldbsearch "(sAMAccountName=$testaccount)" | grep "sAMAccountName: $testaccount" || exit 1
+${VALGRIND:+$VALGRIND} ldbsearch "(sAMAccountName=$testaccount)" | grep "sAMAccountName: $testaccount"
+
+echo "SUCCESS"
