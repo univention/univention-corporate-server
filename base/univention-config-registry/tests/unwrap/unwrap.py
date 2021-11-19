504aead52efc7a87fde57c5587cfcdf7e3039f93
Philipp Hahn <hahn@univention.de>
2021-11-19 12:25:53 +0100

    style[UCR] isort
    Bug #54208

--- base/univention-config-registry/tests/unwrap/unwrap.py
+++ base/univention-config-registry/tests/unwrap/unwrap.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 #
 # Univention Configuration Registry
-"""ldapsearch results without line break."""
 #
 # Copyright 2004-2021 Univention GmbH
 #
@@ -30,6 +29,8 @@
 # License with the Debian GNU/Linux or Univention distribution in file
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
+
+"""ldapsearch results without line break."""
 
 import sys
 
