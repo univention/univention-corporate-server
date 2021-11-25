526f19ca69b5da33950e37318792b51db0e10737
Jürn Brodersen <brodersen@univention.de>
2021-11-25 22:46:13 +0100

    fixup! Bug #53941: fixing join log truncation

--- management/univention-join/umc/python/join/__init__.py
+++ management/univention-join/umc/python/join/__init__.py
@@ -400,7 +400,7 @@ class Instance(Base):
 			fd.seek(max(os.stat(fd.name).st_size - size, 0))
 			return fd.read(size).decode('utf-8', 'replace')
 
-@sanitize(
+	@sanitize(
 		username=StringSanitizer(required=True, minimum=1),
 		password=StringSanitizer(required=True, minimum=1),
 		hostname=HostSanitizer(required=True, regex_pattern=RE_HOSTNAME),
