1b4a136fbdc63423e3655384c35d799d7cf29cbf
Philipp Hahn <hahn@univention.de>
2021-12-07 10:26:18 +0100

    test[UCR]: Switch to Python3
    Bug #54208

--- base/univention-config-registry/tests/test_frontend.py
+++ base/univention-config-registry/tests/test_frontend.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """Unit test for univention.config_registry.frontend."""
 # vim:set fileencoding=utf-8:
 
--- base/univention-config-registry/tests/test_handler.py
+++ base/univention-config-registry/tests/test_handler.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """Unit test for univention.config_registry.backend."""
 # vim:set fileencoding=utf-8:
 # pylint: disable-msg=C0103,E0611,R0904
