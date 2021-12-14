cb39f838362b1025561f7f480bc9d24e274ae6ef
Florian Best <best@univention.de>
2021-12-14 11:48:09 +0100

    fixup! fixup! Expose authinfo in frontend

--- management/univention-portal/frontend/src/jsHelper/createCategories.ts
+++ management/univention-portal/frontend/src/jsHelper/createCategories.ts
@@ -98,6 +98,13 @@ function makeEntry(
       dn: folder.dn,
       title: folder.name,
       isFolder: true,
+      auth_info: {
+        roles: [],
+        idps: [],
+        loa: 'low',
+        disallow_anonymous: true,
+        allow_global_search: false,
+      },
       tiles,
     };
   }
@@ -131,6 +138,13 @@ export default function createCategories(
         title: category.display_name,
         dn: category.dn,
         virtual: category.virtual,
+        /*auth_info: {
+          roles: [],
+          idps: [],
+          loa: 'low',
+          disallow_anonymous: true,
+          allow_global_search: false,
+        },*/
         tiles,
       });
     } else {
