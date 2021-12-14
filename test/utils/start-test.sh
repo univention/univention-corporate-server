f5a11e96d3926d448392a697537d2152f18afb94
Philipp Hahn <hahn@univention.de>
2021-12-14 18:59:20 +0100

    fix[test]: UCS 5.0-1+e174 images
    Bug #54225

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -6,9 +6,9 @@
 # defaults for release
 release='5.0-1'
 old_release='4.4-8'
-kvm_template_version='5.0-0+e0'
-# AMI: Univention Corporate Server (UCS) 5.0 (official image) rev.1
-current_ami=ami-0be22e1965991d8b9
+kvm_template_version='5.0-1+e174'
+# AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 2
+current_ami=ami-0514b932904f7aa57
 # AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 9
 old_ami=ami-0810f599d8e0444b1
 
