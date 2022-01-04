00b0e1caab24ed66be05ff53a95afb5c0005d109
Ildefonso González Sánchez <isanchez.extern@univention.de>
2022-01-04 17:23:31 +0100

    Bug #53269: extension examples update with subject alternative names

--- base/univention-ssl/extensions-example.sh
+++ base/univention-ssl/extensions-example.sh
@@ -2,7 +2,7 @@ createHostExtensionsFile () {
 	local fqdn="$1"
 	local hostname=${fqdn%%.*}
 	local extFile=$(mktemp)
-
+  local ipaddr=`ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`
 	cat <<EOF >>"$extFile"
 extensions = myx509v3
 [ myx509v3 ]
@@ -13,8 +13,10 @@ subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid,issuer:always
 
 # alternative name
-subjectAltName = DNS:$fqdn, DNS:$hostname
+subjectAltName = DNS:$fqdn, DNS:$hostname, IP:127.0.0.1, DNS:localhost, IP:$ipaddr
 EOF
 
 	echo "$extFile"
 }
+
+
