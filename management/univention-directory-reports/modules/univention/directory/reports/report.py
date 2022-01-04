031f4c15324f7b4ac1a92b04e1862244c7cdf9ee
Ildeofnso González Sánchez <isanchez.extern@univention.de>
2022-01-04 01:46:19 +0100

    Revert "Bug #54153 include changes for use a custom file name output report"
    This reverts commit 2458a3d8912eb57ea541a012ec479b9ede847f34.

--- management/univention-directory-reports/modules/univention/directory/reports/report.py
+++ management/univention-directory-reports/modules/univention/directory/reports/report.py
@@ -46,10 +46,11 @@ class Report(object):
 		self.lo = lo
 		self.config = config or Config()
 
-	def create(self, module, report, objects, output=None, output_name=None):
+	def create(self, module, report, objects,  output=None):
 		"""Create a report of objects for the specified module in the specified report type format"""
 		connect(access=self.lo)
 		clear_cache()
+		print("output:  ", output)
 		template = self.config.get_report(module, report)
 		if template is None:
 			if not module:
@@ -66,7 +67,7 @@ class Report(object):
 		doc = Document(template, header=header, footer=footer)
 		if output is None:
 			output = self.config.get_output_path()
-		tmpfile = doc.create_source(objects, output, output_name)
+		tmpfile = doc.create_source(objects, output)
 		pdffile = tmpfile
 		func = {Document.TYPE_RML: doc.create_rml_pdf, Document.TYPE_LATEX: doc.create_pdf}.get(doc._type)
 		if func:
