67055d2bbc43198b9aa7453f63ad7b4b3d027c79
Nico Gulden <gulden@univention.de>
2022-01-20 12:30:34 +0100

    Bug #54353: Setup Sphinx structure
    Supress warnings from sphinx_last_updated_by_git:
    
    * The build pipeline causes warnings with this extension in connection
      with git.
    * Suppress git shallow copy warnings during spell check, because the
      pipeline interprets them as errors. The warnings remain in other build
      targets
    * For configuration of sphinx_last_updated_by_git see
      https://github.com/mgeier/sphinx-last-updated-by-git

---
+++ doc/architecture/conf.py
@@ -0,0 +1,97 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+from datetime import date
+from sphinx.locale import _
+
+# -- Project information -----------------------------------------------------
+
+release = "0.0.1"
+project = "Univention Corporate Server architecture {}".format(release)
+copyright = '{}, Univention GmbH'.format(date.today().year)
+author = 'Univention GmbH'
+language = 'en'
+
+html_title = project
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinxcontrib.spelling",
+    "univention_sphinx_extension",
+    "sphinx_last_updated_by_git",
+]
+
+root_doc = "contents"
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+pdf_doc_base = "ucs-architecture"
+
+html_theme = 'univention_sphinx_book_theme'
+html_theme_options = {
+    "extra_navbar": "Status: DRAFT. Work in Progress.",
+}
+
+html_context = {
+    "pdf_download_filename" : f"{pdf_doc_base}.pdf",
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+
+html_last_updated_fmt = "%d. %b %Y at %H:%M (UTC%z)"
+
+# https://github.com/mgeier/sphinx-last-updated-by-git
+git_last_updated_timezone = 'Europe/Berlin'
+
+
+numfig = True
+
+if "spelling" in sys.argv:
+    spelling_lang = "en_US"
+    spelling_show_suggestions = True
+    spelling_word_list_filename = list()
+    # Warnings may come up by sphinx-last-updated-by-git. Shall be suppressed in spelling job
+    suppress_warnings = ['git.too_shallow']
+
+latex_engine = 'lualatex'
+latex_show_pagerefs = True
+latex_show_urls = "footnote"
+latex_logo = "_static/univention_logo.pdf"
+latex_documents = [(root_doc, f'{pdf_doc_base}.tex', project, author, "manual", False)]
+latex_elements = {
+    "papersize": "a4paper",
+}
+
