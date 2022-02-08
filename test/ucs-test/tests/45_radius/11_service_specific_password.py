ffd54aa03bbe5cd286d61982404411e48a8ce6d6
Julia Bremer <bremer@univention.de>
2022-02-08 14:03:44 +0100

    Bug #54438: Test Radius auth also with a password generated by the udm-rest api

--- test/ucs-test/tests/45_radius/11_service_specific_password.py
+++ test/ucs-test/tests/45_radius/11_service_specific_password.py
@@ -6,8 +6,11 @@
 ## join: true
 ## exposure: dangerous
 
+import json
 import ldap
+import urllib
 import pytest
+import requests
 import subprocess
 
 from univention.config_registry import handler_set as ucr_set
@@ -24,6 +27,34 @@ def radius_auth(username, password):
 		'0',
 		'testing123',
 	])
+
+
+def request_new_password(ucr, user_dn, service={"service": "radius"}):
+	dn = urllib.parse.quote_plus(user_dn)
+	headers = {"Accept": "application/json", "Content-Type": "application/json"}
+	#TODO: not do it via Administrator
+	auth = (ucr.get("users/default/administrator", "Administrator"), ucr.get("tests/domainadmin/pwd"))
+	resp = requests.post("https://localhost/univention/udm/users/user/%s/service-specific-password" % (dn,), auth=auth, json=service, headers=headers, verify=False)
+	if not resp.ok:
+		print(resp.text)
+		raise AssertionError("Received Error %s" % (resp.text,))
+	return json.loads(resp.text).get('password')
+
+
+def test_udm_rest(ucr, udm_session, rad_user):
+	dn, name, password = rad_user
+	new_password = request_new_password(ucr, dn)
+	ucr_set(['radius/use-service-specific-password=true'])
+	with pytest.raises(subprocess.CalledProcessError):
+		radius_auth(name, password)
+	radius_auth(name, new_password)
+
+
+def test_udm_rest_invalid_service(ucr, udm_session, rad_user):
+	dn, name, password = rad_user
+	with pytest.raises(AssertionError):
+		request_new_password(ucr, dn, service={"service": "testtest"})
+
 
 # Check if radius auth works with userpassword, when ssp is disabled
 def test_radius_auth_without_ssp(udm_session, rad_user):
