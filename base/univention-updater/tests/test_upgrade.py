54fdf6cf4bee3c44ad087f77f228ffdf41f44d59
Siavash Sefid Rodi <sefid-rodi.extern@univention.de>
2022-02-25 15:07:04 +0100

    Bug #49061: spaces to tabs + string in one line

--- base/univention-updater/tests/test_upgrade.py
+++ base/univention-updater/tests/test_upgrade.py
@@ -8,21 +8,21 @@ import sys
 import pytest
 
 if sys.version_info < (3,):
-    pytest.skip("unsupported python version, upgrade.py package from scripts folder is not available for python2,"
-                " skipping test.", allow_module_level=True)
+	pytest.skip("unsupported python version, upgrade.py package from scripts folder is not available for python2 skipping test.",
+		allow_module_level=True)
 
 from univention.updater.scripts.upgrade import parse_args
 
 
 def test_parse_args_rejects_invalid_update_to_bug49061(mocker):
-    with pytest.raises(SystemExit) as pytest_wrapped_e:
-        mocker.patch('sys.argv', ['/usr/sbin/univention-upgrade', '--updateto=4.0.0'])
-        parse_args()
-    assert pytest_wrapped_e.type == SystemExit
-    assert pytest_wrapped_e.value.code == 1
+	with pytest.raises(SystemExit) as pytest_wrapped_e:
+		mocker.patch('sys.argv', ['/usr/sbin/univention-upgrade', '--updateto=4.0.0'])
+		parse_args()
+	assert pytest_wrapped_e.type == SystemExit
+	assert pytest_wrapped_e.value.code == 1
 
 
 def test_parse_args_accepts_valid_update_to_bug49061(mocker):
-    mocker.patch('sys.argv', ['/usr/sbin/univention-upgrade', '--updateto=4.0-0'])
-    options = parse_args()
-    assert options.updateto == '4.0-0'
+	mocker.patch('sys.argv', ['/usr/sbin/univention-upgrade', '--updateto=4.0-0'])
+	options = parse_args()
+	assert options.updateto == '4.0-0'
