1d674e03d6b2326986fa465d34055df41c6143cd
Johannes Lohmer <lohmer@univention.de>
2022-03-04 14:34:02 +0100

    Bug #53538: [ucs-test] 60_umc migrated to pytest

--- test/ucs-test/tests/60_umc/75_ipchange_basic.py
+++ test/ucs-test/tests/60_umc/75_ipchange_basic.py
@@ -1,36 +1,37 @@
-#!/usr/share/ucs-test/runner python3
+#!/usr/share/ucs-test/runner pytest-3 -s -l -vv
 ## desc: Check basic ip_change functionality
 ## roles-not: [basesystem]
 ## exposure: dangerous
 
+import pytest
+
 import univention.testing.strings as uts
-import univention.testing.udm as udm_test
 import univention.testing.utils as utils
-from univention.config_registry import ConfigRegistry
 from univention.testing.umc import Client
 
-if __name__ == '__main__':
-	ucr = ConfigRegistry()
-	ucr.load()
 
-	with udm_test.UCSTestUDM() as udm:
-		role = ucr.get('server/role')
-		if role == 'domaincontroller_master':
-			role = 'domaincontroller_backup'
-		computerName = uts.random_string()
-		computer = udm.create_object('computers/%s' % role, name=computerName, password='univention', network='cn=default,cn=networks,%s' % ucr.get('ldap/base'))
+@pytest.mark.tags('udm')  # Correct?
+@pytest.mark.roles_not('basesystem')
+@pytest.mark.exposure('dangerous')
+def test_ipchange_basic(udm, ucr):
+	"""Check basic ip_change functionality"""
+	role = ucr.get('server/role')
+	if role == 'domaincontroller_master':
+		role = 'domaincontroller_backup'
+	computerName = uts.random_string()
+	computer = udm.create_object('computers/%s' % role, name=computerName, password='univention', network='cn=default,cn=networks,%s' % ucr.get('ldap/base'))
 
-		lo = utils.get_ldap_connection()
-		computer_object = lo.get(computer)
-		print(computer_object)
-		ip = computer_object['aRecord']
-		utils.verify_ldap_object(computer, {'aRecord': ip})
+	lo = utils.get_ldap_connection()
+	computer_object = lo.get(computer)
+	print(computer_object)
+	ip = computer_object['aRecord']
+	utils.verify_ldap_object(computer, {'aRecord': ip})
 
-		new_ip = '1.2.3.4'
+	new_ip = '1.2.3.4'
 
-		iface = ucr.get('interfaces/primary', 'eth0')
-		client = Client(ucr.get('ldap/master'), '%s$' % computerName, 'univention')
-		client.umc_command('ip/change', {'ip': new_ip, 'oldip': ip[0].decode('UTF-8'), 'netmask': ucr.get('interfaces/%s/netmask' % iface), 'role': role})
+	iface = ucr.get('interfaces/primary', 'eth0')
+	client = Client(ucr.get('ldap/master'), '%s$' % computerName, 'univention')
+	client.umc_command('ip/change', {'ip': new_ip, 'oldip': ip[0].decode('UTF-8'), 'netmask': ucr.get('interfaces/%s/netmask' % iface), 'role': role})
 
-		utils.wait_for_replication()
-		utils.verify_ldap_object(computer, {'aRecord': [new_ip]}, strict=True)
+	utils.wait_for_replication()
+	utils.verify_ldap_object(computer, {'aRecord': [new_ip]}, strict=True)
