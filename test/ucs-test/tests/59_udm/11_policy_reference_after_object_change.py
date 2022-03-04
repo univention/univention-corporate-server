e92d8774f3a01e5dd64b820dbeb4f302f5cf9912
Johannes Lohmer <lohmer@univention.de>
2022-03-04 12:30:32 +0100

    Bug #53538: [ucs-test] 59_udm migrated to pytest

--- test/ucs-test/tests/59_udm/11_policy_reference_after_object_change.py
+++ test/ucs-test/tests/59_udm/11_policy_reference_after_object_change.py
@@ -1,4 +1,4 @@
-#!/usr/share/ucs-test/runner python3
+#!/usr/share/ucs-test/runner pytest-3 -s -l -vv
 ## desc: policy reference works after changing the objects dn
 ## tags: [udm,apptest,SKIP]
 ## roles: [domaincontroller_master]
@@ -9,19 +9,23 @@
 ##   - univention-directory-manager-tools
 
 
-import univention.testing.udm as udm_test
+import pytest
+
 import univention.testing.utils as utils
 from univention.testing.strings import random_string
 
-if __name__ == '__main__':
-	with udm_test.UCSTestUDM() as udm:
-		for object_type in ('container/cn', 'mail/domain'):
-			print('testing', object_type)
-			policy = udm.create_object('policies/pwhistory', **{'name': random_string()})
-			old_dn = udm.create_object(object_type, **{'name': random_string()})
 
-			new_dn = udm.modify_object(object_type, **{'name': random_string(), 'dn': old_dn, 'policy_reference': policy})
-			print('new_dn', new_dn)
-			utils.verify_ldap_object(new_dn, {'univentionPolicyReference': [policy]})
-			print('old_dn', old_dn)
-			utils.verify_ldap_object(old_dn, should_exist=False)
+@pytest.mark.tags('udm')
+@pytest.mark.roles('domaincontroller_master')
+@pytest.mark.exposure('dangerous')
+@pytest.mark.parametrize('object_type', ['container/cn', 'mail/domain'])
+def test_policy_reference_after_object_change(udm, object_type):
+	print('testing', object_type)
+	policy = udm.create_object('policies/pwhistory', **{'name': random_string()})
+	old_dn = udm.create_object(object_type, **{'name': random_string()})
+
+	new_dn = udm.modify_object(object_type, **{'name': random_string(), 'dn': old_dn, 'policy_reference': policy})
+	print('new_dn', new_dn)
+	utils.verify_ldap_object(new_dn, {'univentionPolicyReference': [policy]})
+	print('old_dn', old_dn)
+	utils.verify_ldap_object(old_dn, should_exist=False)
