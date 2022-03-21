8ec42b09ce8dea26dc229e1119df3ba545a9f456
Julia Bremer <bremer@univention.de>
2022-03-21 17:32:10 +0100

    Bug #54561: Return an empty list instead of None if key doesn't exist in cache
    Traceback (most recent call last):
      File "/usr/lib/univention-directory-listener/system/office365-group.py", line 133, in handler
        ol.create_groups(dn, new)
      File "/usr/lib/python2.7/dist-packages/univention/office365/listener.py", line 389, in create_groups
        if self.udm.group_in_azure(dn):
      File "/usr/lib/python2.7/dist-packages/univention/office365/udm_helper.py", line 146, in group_in_azure
        alias_users = set(x.lower() for x in univentionOffice365ADConnectionAlias.get(self.adconnection_alias))
    TypeError: 'NoneType' object is not iterable

--- base/univention-group-membership-cache/src/univention/ldap_cache/cache/backend/gdbm_cache.py
+++ base/univention-group-membership-cache/src/univention/ldap_cache/cache/backend/gdbm_cache.py
@@ -147,7 +147,9 @@ class GdbmCache(LdapCache):
 			try:
 				value = reader[key]
 			except KeyError:
-				return None
+				if self.single_value:
+					return None
+				return []
 			if self.single_value:
 				return _s(value)
 			elif value:
