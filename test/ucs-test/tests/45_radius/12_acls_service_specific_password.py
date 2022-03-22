e4e3f4b8c9d65029a7f2408c206bdc0e63da2261
Florian Best <best@univention.de>
2022-03-22 16:20:44 +0100

    Bug #54409: migrate test case to pytest
    Revert "Bug #54409: user ldap replace for univentionRadiusPassword"
    
    This reverts commit b33d75fd0f0268069d3b1d87a5ffa4877f7b80e0.
    
    Bug #54409: fix uldap usage
    
    Bug #54409: use pytest builtins
    
    Bug #54409: use pytest parametrization
    
    Bug #54409: replace subprocess with python-ldap

--- test/ucs-test/tests/45_radius/12_acls_service_specific_password.py
+++ test/ucs-test/tests/45_radius/12_acls_service_specific_password.py
@@ -6,70 +6,49 @@
 ## join: true
 ## exposure: dangerous
 
-import ldap
 import pytest
-import subprocess
-import univention.uldap as uldap
+import univention.admin
 
 
-def radius_auth(username, password):
-	subprocess.check_call([
-		'radtest',
-		'-t',
-		'mschap',
-		username,
-		password,
-		'127.0.0.1:18120',
-		'0',
-		'testing123',
-	])
+@pytest.fixture
+def credentials(user_type, rad_user, ucr):
+	if user_type == 'user':
+		return (rad_user[0], rad_user[2])
+	elif user_type == 'computer':
+		return (ucr.get('ldap/hostdn'), open('/etc/machine.secret').read())
+	elif user_type == 'admin':
+		return (ucr.get('tests/domainadmin/account'), ucr.get('tests/domainadmin/pwd'))
+	assert False
 
 
-def test_acl_user_may_not_read(rad_user, lo, ssp):
+@pytest.mark.parametrize('allowed,user_type', [
+	(False, 'user'),
+	(True, 'computer'),
+	(True, 'admin'),
+])
+def test_acl_read_access(rad_user, lo, ssp, ucr, allowed, credentials):
 	dn, name, password = rad_user
-	lo.modify_ext_s(dn, ((ldap.MOD_REPLACE, 'univentionRadiusPassword', ssp[1]),))
-	output = subprocess.check_output(['univention-ldapsearch', '-D', dn, '-w', password, '-b', dn, 'univentionRadiusPassword'])
-	output = output.decode('utf-8')
-	if 'univentionRadiusPassword:' in output:
-		print(output)
-		raise RuntimeError('%s should not be able to see univentionRadiusPassword' % dn)
+	binddn, bindpw = credentials
+	lo.modify(dn, [('univentionRadiusPassword', [b'old'], [ssp[1]])])
+	lo = univention.admin.uldap.access(host=ucr.get('ldap/master'), port=ucr.get('ldap/server/port'), base=ucr.get('ldap/base'), binddn=binddn, bindpw=bindpw, start_tls=2, follow_referral=True)
+	passwords = [(dn, attr) for dn, attr in lo.search(attr=['univentionRadiusPassword']) if 'univentionRadiusPassword' in attr]
+	if allowed:
+		assert passwords
+	else:
+		assert not passwords
 
 
-def test_acl_user_may_not_write(rad_user, ssp, ucr):
+@pytest.mark.parametrize('allowed,user_type', [
+	(False, 'user'),
+	(False, 'computer'),
+	(True, 'admin'),
+])
+def test_acl_write_access(rad_user, ssp, ucr, allowed, credentials):
 	dn, name, password = rad_user
-	lo = uldap.access(host=ucr.get('ldap/master'), port=ucr.get('ldap/server/port'), base=ucr.get('ldap/base'), binddn=dn, bindpw=password, start_tls=2, follow_referral=True)
-	with pytest.raises(ldap.INSUFFICIENT_ACCESS):
-		lo.modify_ext_s(dn, ((ldap.MOD_REPLACE, 'univentionRadiusPassword', ssp[1]),))
-
-
-def test_acl_computer_may_read(rad_user, lo, ssp, ucr_session):
-	dn, name, password = rad_user
-	lo.modify_ext_s(dn, ((ldap.MOD_REPLACE, 'univentionRadiusPassword', ssp[1]),))
-	output = subprocess.check_output(['univention-ldapsearch', '-D', ucr_session.get('ldap/hostdn'), '-y', '/etc/machine.secret', '-b', dn, 'univentionRadiusPassword'])
-	output = output.decode('utf-8')
-	if 'univentionRadiusPassword:' not in output:
-		print(output)
-		raise RuntimeError('%s needs to be able to see univentionRadiusPassword' % dn)
-
-
-def test_acl_computer_may_not_write(rad_user, ssp, ucr):
-	dn, name, password = rad_user
-	bindpw = open('/etc/machine.secret').read()
-	lo = uldap.access(host=ucr.get('ldap/master'), port=ucr.get('ldap/server/port'), base=ucr.get('ldap/base'), binddn=ucr.get('ldap/host'), bindpw=bindpw, start_tls=2, follow_referral=True)
-	with pytest.raises(ldap.INSUFFICIENT_ACCESS):
-		lo.modify_ext_s(dn, ((ldap.MOD_REPLACE, 'univentionRadiusPassword', ssp[1]),))
-
-
-def test_acl_admin_may_read(rad_user, lo, ssp, ucr):
-	dn, name, password = rad_user
-	output = subprocess.check_output(['univention-ldapsearch', '-D', ucr.get('tests/domainadmin/account'), '-w', ucr.get('tests/domainadmin/pwd'), '-b', dn, 'univentionRadiusPassword'])
-	output = output.decode('utf-8')
-	if 'univentionRadiusPassword:' not in output:
-		print(output)
-		raise RuntimeError('Admin should be able to see univentionRadiusPassword')
-
-
-def test_acl_admin_may_write(rad_user, ssp, ucr):
-	dn, name, password = rad_user
-	lo = uldap.access(host=ucr.get('ldap/master'), port=ucr.get('ldap/server/port'), base=ucr.get('ldap/base'), binddn=ucr.get('tests/domainadmin/account'), bindpw=ucr.get('tests/domainadmin/pwd'), start_tls=2, follow_referral=True)
-	lo.modify_ext_s(dn, ((ldap.MOD_REPLACE, 'univentionRadiusPassword', ssp[1]),))
+	binddn, bindpw = credentials
+	lo = univention.admin.uldap.access(host=ucr.get('ldap/master'), port=ucr.get('ldap/server/port'), base=ucr.get('ldap/base'), binddn=binddn, bindpw=bindpw, start_tls=2, follow_referral=True)
+	if allowed:
+		lo.modify(dn, (('univentionRadiusPassword', [b''], [ssp[1]]),))
+	else:
+		with pytest.raises(univention.admin.uexceptions.permissionDenied):
+			lo.modify(dn, (('univentionRadiusPassword', [b''], [ssp[1]]),))
