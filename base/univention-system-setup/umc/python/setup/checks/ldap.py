dafb09bf90abd81461d2e21f09512323082db591
Florian Best <best@univention.de>
2022-03-24 15:25:07 +0100

    Bug #51318: replace python with dash

--- base/univention-system-setup/umc/python/setup/checks/ldap.py
+++ base/univention-system-setup/umc/python/setup/checks/ldap.py
@@ -1,31 +1,29 @@
-import subprocess
+from pipes import quote
+from subprocess import PIPE, Popen
 
 from ldap.filter import filter_format
 
 from univention.management.console.modules.setup.util import _temporary_password_file
-
-# do we need to keep this backwards compatible with UCS 4.4?
-_SCRIPT = '''#!/usr/bin/python2.7
-from univention.uldap import getAdminConnection
-connection = getAdminConnection()
-results = connection.search(filter="(&(objectClass=person)(uid=%%s))")
-if len(results) == 0:
-	exit(0)
-elif len(results) == 1:
-	role = results[0][1]["univentionServerRole"][0].decode("UTF-8")
-	if role in "%s":
-		exit(0)
-exit(1)
-'''
+from univention.management.console.log import MODULE
 
 
 def check_if_uid_is_available(uid, role, address, username, password):
+	"""check if either the UID it not yet taken at all
+		or it is already taken (by our previous self) and still matches the server role
+	"""
+	# type: (str, str, str, str, str) -> bool
+	filter_s = filter_format("(&(objectClass=person)(uid=%s)(!(univentionServerRole=%s)))", [uid, role])
+	rcmd = 'univention-ldapsearch -LLL %s 1.1' % (quote(filter_s),)
 	with _temporary_password_file(password) as password_file:
-		process = subprocess.Popen([
-			'univention-ssh', password_file, '%s@%s' % (username, address),
-			'/usr/bin/python2.7', '-'
-		], stdin=subprocess.PIPE)
-		process.communicate((filter_format(_SCRIPT % (role,), [uid])).encode('UTF-8'))
-		if process.wait() != 0:
-			return False
-	return True
+		cmd = [
+			"univention-ssh", "--no-split",
+			password_file,
+			'%s@%s' % (username, address),
+			rcmd
+		]
+		MODULE.info("Running %s" % " ".join(quote(arg) for arg in cmd))
+		process = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+		stdout, stderr = process.communicate()
+		if process.wait() or stderr:
+			MODULE.error("Failed checking uid=%s role=%s: %s" % (uid, role, stderr))
+	return not stdout.strip()
