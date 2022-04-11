d7092a7fad76ae44651ae3a16e22cc1cd8a8b85b
Erik Damrose <damrose@univention.de>
2022-04-11 21:37:36 +0200

    Bug #53922: Use latest AMI

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -8,7 +8,7 @@ release='4.4-9'
 old_release='4.3-5'
 kvm_template_version='4.4-8+e958'
 # AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 10 - ami-0b2e9a1fd907dce95
-current_ami=ami-0b2e9a1fd907dce95
+current_ami=ami-0b5559a5fb0684c95
 # AMI: Univention Corporate Server (UCS) 4.3 (official image) rev. 6 - ami-0dd235a67a0eb9bdc
 old_ami=ami-0dd235a67a0eb9bdc
 
