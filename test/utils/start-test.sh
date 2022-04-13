d5d45b4ed410961495d8d6c996a3ed03949c15e6
Felix Botner <botner@univention.de>
2022-04-13 09:34:31 +0200

    Bug #54293: update traeger to 4.4-9

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -5,7 +5,7 @@
 
 # defaults for release
 release='5.0-1'
-old_release='4.4-8'
+old_release='4.4-9'
 kvm_template_version='5.0-1+e174'
 # AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 2
 current_ami=ami-0514b932904f7aa57
