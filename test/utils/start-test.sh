a8adeedd04e8a82f7580987cc51b32c45800b1de
Erik Damrose <damrose@univention.de>
2022-04-13 18:30:34 +0200

    Bug #53924: use ami-02ad9aab36aadf18a

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -7,8 +7,8 @@
 release='4.4-9'
 old_release='4.3-5'
 kvm_template_version='4.4-9+e1216'
-# AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 10 - ami-0b2e9a1fd907dce95
-current_ami=ami-001d99c1aef94b566
+# AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 11 - ami-0b2e9a1fd907dce95
+current_ami=ami-02ad9aab36aadf18a
 # AMI: Univention Corporate Server (UCS) 4.3 (official image) rev. 6 - ami-0dd235a67a0eb9bdc
 old_ami=ami-0dd235a67a0eb9bdc
 
