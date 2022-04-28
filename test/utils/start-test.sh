59e46e1fa13b12363601466d244d365e4929127e
Erik Damrose <damrose@univention.de>
2022-04-28 11:44:39 +0200

    Bug #53925: Use 4.4 rev11 aws ami ami-02ad9aab36aadf18a

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -9,8 +9,8 @@ old_release='4.4-9'
 kvm_template_version='5.0-1+e174'
 # AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 2
 current_ami=ami-0514b932904f7aa57
-# AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 10
-old_ami=ami-0b2e9a1fd907dce95
+# AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 11
+old_ami=ami-02ad9aab36aadf18a
 
 # defaults
 kvm_template='generic-unsafe'
