464155bc8b90aa8b6d6ef2c19862085cf6f14e5b
Julia Bremer <bremer@univention.de>
2022-05-23 16:50:33 +0200

    Bug #TODO: schoolserver join often fails in big environments
    The s4connector join script waits for the synchronization of the dns objects,
    as well as some kerberos principals. If a replica server is joined, all replicated objects
    were added as pickle files before. This means that the necessary objects for the join are only
    added to samba4 last, which can take several hours in big environments.
    Resyncing them first, makes the join faster, and avoids customers having to restart the join
    if they ran into the timeout.

--- services/univention-s4-connector/scripts/resync_object_from_ucs.py
+++ services/univention-s4-connector/scripts/resync_object_from_ucs.py
@@ -52,12 +52,14 @@ class UCSResync:
 	def _get_listener_dir(self):
 		return self.configRegistry.get('connector/s4/listener/dir', '/var/lib/univention-connector/s4')
 
-	def _generate_filename(self):
+	def _generate_filename(self, first):
 		directory = self._get_listener_dir()
-		return os.path.join(directory, "%f" % time.time())
+		if not first:
+			return os.path.join(directory, "%f" % time.time())
+		return os.path.join(directory, "-%f" % time.time())
 
-	def _dump_object_to_file(self, object_data):
-		filename = self._generate_filename()
+	def _dump_object_to_file(self, object_data, first):
+		filename = self._generate_filename(first)
 		f = open(filename, 'w+')
 		os.chmod(filename, 0600)
 		p = cPickle.Pickler(f)
@@ -68,7 +70,7 @@ class UCSResync:
 	def _search_ldap_object_orig(self, ucs_dn):
 		return self.lo.get(ucs_dn, attr=['*', '+'], required=True)
 
-	def resync(self, ucs_dns=None, ldapfilter=None):
+	def resync(self,first, ucs_dns=None, ldapfilter=None):
 
 		if ucs_dns and not type(ucs_dns) in (type(()), type([])):
 			raise ValueError("'ucs_dns' is of type %s, must be list or tuple" % type(ucs_dns))
@@ -76,7 +78,7 @@ class UCSResync:
 		treated_dns = []
 		for dn, new in self.search_ldap(ucs_dns, ldapfilter):
 			object_data = (dn, new, {}, None)
-			self._dump_object_to_file(object_data)
+			self._dump_object_to_file(object_data, first)
 			treated_dns.append(dn)
 
 		return treated_dns
@@ -117,6 +119,7 @@ if __name__ == '__main__':
 
 	parser = OptionParser(usage='resync_object_from_ucs.py [--filter <LDAP filter>] [dn]')
 	parser.add_option("--filter", dest="ldapfilter", help="LDAP Filter")
+	parser.add_option("--first", dest="first", action="store_true")
 	(options, args) = parser.parse_args()
 
 	if len(args) != 1 and not options.ldapfilter:
@@ -130,7 +133,7 @@ if __name__ == '__main__':
 	treated_dns = []
 	try:
 		resync = UCSResync()
-		treated_dns = resync.resync(ucs_dns, options.ldapfilter)
+		treated_dns = resync.resync(options.first, ucs_dns, options.ldapfilter)
 	except ldap.NO_SUCH_OBJECT as ex:
 		print 'ERROR: The LDAP object not found : %s' % ex.args[1]
 		if len(ex.args) == 3:
