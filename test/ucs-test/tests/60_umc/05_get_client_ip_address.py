8f2b915d99809593eca0596e14b2d3be655927e2
Johannes Lohmer <lohmer@univention.de>
2022-05-27 15:22:13 +0200

    Bug #53538: [ucs-test] 60_umc/05_udm_non_ucr_policies refactored and migrated to pytest

--- test/ucs-test/tests/60_umc/05_get_client_ip_address.py
+++ test/ucs-test/tests/60_umc/05_get_client_ip_address.py
@@ -1,48 +1,43 @@
-#!/usr/share/ucs-test/runner python3
+#!/usr/share/ucs-test/runner pytest-3 -s -l -vv
 ## desc: Check if UMC is able to return correct IP address
 ## exposure: dangerous
 ## packages: [univention-management-console-web-server]
 
 from six.moves.http_client import HTTPConnection
+import pytest
 
-from univention.config_registry import ConfigRegistry
 from univention.testing import network, utils
 from univention.testing.umc import Client
 
 
-class Client(Client):
+class HTTPConnectionClient(Client):
 	# workaround ssl.CertificateError: hostname '1.2.3.4' doesn't match either of 'master091.$domainname', 'master091'
 	ConnectionType = HTTPConnection
 
 
-def get_ip_address(host, username, password):
-	client = Client(host, username, password)
-	return client.umc_get('ipaddress').data
-
-
-def main():
-	ucr = ConfigRegistry()
-	ucr.load()
-
+@pytest.mark.exposure('dangerous')
+@pytest.mark.parametrize('addr2', ['4.3.2.1', '1.1.1.1', '2.2.2.2'])
+def test_get_client_ip_address(addr2):
+	"""Check if UMC is able to return correct IP address"""
 	account = utils.UCSTestDomainAdminCredentials()
+	print('*** Check with remote addresses:', addr2)
 
 	with network.NetworkRedirector() as nethelper:
-		print('*** Check with different remote addresses')
-		for addr2 in ('4.3.2.1', '1.1.1.1', '2.2.2.2'):
-			nethelper.add_loop('1.2.3.4', addr2)
+		nethelper.add_loop('1.2.3.4', addr2)
 
-			result = get_ip_address('1.2.3.4', account.username, account.bindpw)
-			print('Result: %r' % result)
-			if addr2 not in result:
-				utils.fail('UMC webserver is unable to determine correct UMCP client address (expected=%r result=%r)' % (addr2, result))
+		client = HTTPConnectionClient('1.2.3.4', account.username, account.bindpw)
+		result = client.umc_get('ipaddress').data
+		assert addr2 in result, 'UMC webserver is unable to determine correct UMCP client address (expected=%r result=%r)' % (addr2, result)
 
-			nethelper.remove_loop('1.2.3.4', addr2)
-
-		print('*** Check with localhost')
-		result = get_ip_address('localhost', account.username, account.bindpw)
-		if result:
-			utils.fails('Response is expected to be empty')
+		nethelper.remove_loop('1.2.3.4', addr2)
 
 
-if __name__ == '__main__':
-	main()
+@pytest.mark.exposure('dangerous')
+def test_get_client_ip_address_localhost():
+	"""Check if UMC is able to return correct IP address"""
+	print('*** Check with localhost')
+	account = utils.UCSTestDomainAdminCredentials()
+
+	client = HTTPConnectionClient('localhost', account.username, account.bindpw)
+	result = client.umc_get('ipaddress').data
+	assert not result, 'Response is expected to be empty'
