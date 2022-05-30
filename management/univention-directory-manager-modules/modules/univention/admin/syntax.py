univention/univention-corporate-server

commit 802fceb7d1f5342e18a856e5cc096ff59bbfb060

Bug #38762: add additional handling for MultiObjectSelect
MultiObjectSelects adds a "objectProperty": "None" as string.
Transform it into a real python None just like search() would do.

MultiObjectSelects adds a static "container": "all".
It's not required to handle it here because it was unused, but make it explicit.
Maybe in the future one wants to use it.

Fixes: d283eac48af496e0cd159ff4d9c392c3e1f1a174

management/univention-management-console-module-udm/umc/python/udm/udm_ldap.py
@@ -1335,8 +1335,10 @@ def read_syntax_choices(syn, options=None, ldap_connection=None, ldap_position=N
 	if '$dn$' in options:
 		options['dn'] = options.pop('$dn$')
 
+	if 'container' in options:
+		options['base'] = options.pop('container') if options['container'] != 'all' else None
 	if 'objectProperty' in options:
-		options['property'] = options.pop('objectProperty')
+		options['property'] = options.pop('objectProperty') if options['objectProperty'] != 'None' else None
 	if 'objectPropertyValue' in options:
 		options['value'] = options.pop('objectPropertyValue')
 	# only for syntax_choices_key

commit fd835042be5e07d96e159dca979199799af1e40e

fixup! Bug #38762: add additional handling for MultiObjectSelect
with property being "None" no search for the default properties was done
anymore leading to the search result matched everything.

Now the filter is adjusted so that the default properties are used.

Fixes: 802fceb7d1f5342e18a856e5cc096ff59bbfb060

management/univention-directory-manager-modules/modules/univention/admin/syntax.py
@@ -697,13 +697,11 @@ class _UDMObjectOrAttribute(object):
 			return ret
 		hidden_filter = '!(objectFlag=hidden)'
 		if ret:
-			if not ret.startswith('('):
-				ret = '(%s)' % ret
-			return '(&(%s)%s)' % (hidden_filter, ret)
+			return '(&(%s)%s)' % (hidden_filter, cls.wrap_filter(ret))
 		return hidden_filter
 
 	@classmethod
-	def _create_ldap_filter(cls, options, module=None):
+	def _create_ldap_filter(cls, options, module):
 		if cls.depends and cls.depends not in options:
 			return None
 
@@ -712,21 +710,38 @@ class _UDMObjectOrAttribute(object):
 		else:
 			filter_s = cls.udm_filter % options  # FIXME: missing LDAP filter escaping
 
-		object_property = options.get('property')
+		object_properties = []
+		if 'property' in options:
+			object_properties = [options['property']] if options['property'] else [
+				key for key, prop in module.property_descriptions.items()
+				if prop.include_in_default_search
+			]
 		object_property_value = options.get('value')
-		if object_property and object_property_value:
-			prop = module.property_descriptions.get(object_property)
-			syn = prop.syntax if prop else cls
-			property_filter_s = syn.get_object_property_filter(object_property, object_property_value, allow_asterisks=options.get('allow_asterisks', True))
+		if object_properties and object_property_value:
+			property_filters = []
+			for object_property in object_properties:
+				prop = module.property_descriptions.get(object_property)
+				syn = prop.syntax if prop else cls
+				property_filter = syn.get_object_property_filter(object_property, object_property_value, allow_asterisks=options.get('allow_asterisks', True))
+				if property_filter:
+					property_filters.append(property_filter)
+
+			if not property_filters:
+				property_filter_s = ''
+			else:
+				property_filter_s = cls.wrap_filter(property_filters[0]) if len(property_filters) == 1 else '(|%s)' % ''.join(cls.wrap_filter(x) for x in property_filters)
+
 			if not options.get('hidden', True):
 				property_filter_s = cls._append_hidden_filter(module, property_filter_s)
-			if property_filter_s and not property_filter_s.startswith('('):
-				# make sure that the LDAP filter is wrapped in brackets
-				property_filter_s = '(%s)' % property_filter_s
-			if filter_s and not filter_s.startswith('('):
-				# make sure that the LDAP filter is wrapped in brackets
-				filter_s = '(%s)' % filter_s
-			filter_s = '(&%s%s)' % (property_filter_s, filter_s)
+
+			filter_s = '(&%s%s)' % (cls.wrap_filter(property_filter_s), cls.wrap_filter(filter_s))
+		return filter_s
+
+	@classmethod
+	def wrap_filter(cls, filter_s):
+		if filter_s and not filter_s.startswith('('):
+			# make sure that the LDAP filter is wrapped in brackets
+			filter_s = '(%s)' % filter_s
 		return filter_s
 
 

management/univention-management-console-module-udm/umc/python/udm/udm_ldap.py
@@ -1339,7 +1339,7 @@ def read_syntax_choices(syn, options=None, ldap_connection=None, ldap_position=N
 		options['base'] = options.pop('container') if options['container'] != 'all' else None
 	if 'objectProperty' in options:
 		options['property'] = options.pop('objectProperty') if options['objectProperty'] != 'None' else None
-	if 'objectPropertyValue' in options:
+	if 'objectPropertyValue' in options and 'objectProperty' in options:
 		options['value'] = options.pop('objectPropertyValue')
 	# only for syntax_choices_key
 	options['allow_asterisks'] = options.pop('allow_asterisks', True)