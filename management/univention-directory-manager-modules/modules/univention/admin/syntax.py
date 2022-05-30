univention/univention-corporate-server

commit ed1d263e87e9b2721605d2ee91b1685c1f4415fc

Bug #38762: move LDAP filter escaping into syntax classes
where the actual filter escaping needs to be done

management/univention-directory-manager-modules/modules/univention/admin/syntax.py
@@ -194,6 +194,12 @@ def _default_widget_options(syntax):
 	}
 
 
+def _replace_asterisks_in_filter(filter_s, allow_asterisks=True):
+	if allow_asterisks:
+		return filter_s.replace(escape_filter_chars('*'), '*')
+	return filter_s
+
+
 def is_syntax(syntax_obj, syntax_type):
 	# type: (Any, Type) -> bool
 	"""
@@ -360,7 +366,7 @@ class ISyntax(object):
 		return None
 
 	@classmethod
-	def get_object_property_filter(cls, object_property, object_property_value):
+	def get_object_property_filter(cls, object_property, object_property_value, allow_asterisks=True):
 		"""Get a LDAP filter for a certain property
 
 		>>> ISyntax.get_object_property_filter('foo', 'bar')
@@ -368,11 +374,10 @@ class ISyntax(object):
 		>>> ISyntax.get_object_property_filter('foo', 'bar*')
 		'(|(foo=bar*)(foo=bar))'
 		"""
-		# TODO: do LDAP escaping here, instead of in layer above
-		ret = '%s=%s' % (object_property, object_property_value)
+		ret = _replace_asterisks_in_filter(filter_format('%s=%s', (object_property, object_property_value)), allow_asterisks)
 		no_substring_value = object_property_value.strip('*')
 		if no_substring_value and no_substring_value != object_property_value:
-			ret = '(|(%s)(%s=%s))' % (ret, object_property, no_substring_value)
+			ret = '(|(%s)(%s))' % (ret, _replace_asterisks_in_filter(filter_format('%s=%s', (object_property, no_substring_value)), allow_asterisks))
 		return ret
 
 
@@ -675,14 +680,14 @@ class _UDMObjectOrAttribute(object):
 		if callable(cls.udm_filter):
 			filter_s = cls.udm_filter(options)
 		else:
-			filter_s = cls.udm_filter % options
+			filter_s = cls.udm_filter % options  # FIXME: missing LDAP filter escaping
 
 		object_property = options.get('property')
 		object_property_value = options.get('value')
 		if object_property and object_property_value:
 			prop = module.property_descriptions.get(object_property)
 			syn = prop.syntax if prop else cls
-			property_filter_s = syn.get_object_property_filter(object_property, object_property_value)
+			property_filter_s = syn.get_object_property_filter(object_property, object_property_value, allow_asterisks=options.get('allow_asterisks', True))
 			if not options.get('hidden', True):
 				property_filter_s = cls._append_hidden_filter(module, property_filter_s)
 			if property_filter_s and not property_filter_s.startswith('('):
@@ -1711,9 +1716,9 @@ class boolean(simple):
 		return super(boolean, self).parse(text)
 
 	@classmethod
-	def get_object_property_filter(cls, object_property, object_property_value):
-		not_set_filter = '(!(%s=*))' % object_property
-		compare_filter = '%s=%s' % (object_property, object_property_value)
+	def get_object_property_filter(cls, object_property, object_property_value, allow_asterisks=True):
+		not_set_filter = filter_format('(!(%s=*))', (object_property,))
+		compare_filter = _replace_asterisks_in_filter(filter_format('%s=%s', (object_property, object_property_value)), allow_asterisks)
 		if object_property_value == '0':
 			return '(|(%s)%s)' % (compare_filter, not_set_filter)
 		elif object_property_value == '1':
@@ -4468,7 +4473,7 @@ class IStates(select):
 		raise univention.admin.uexceptions.valueInvalidSyntax(_('Invalid choice.'))
 
 	@classmethod
-	def get_object_property_filter(cls, object_property, object_property_value):
+	def get_object_property_filter(cls, object_property, object_property_value, allow_asterisks=True):
 		try:
 			state_of_object_property_value = [state for state, (ldap_value, _) in cls.values if ldap_value == object_property_value][0]
 			if state_of_object_property_value not in (None, True, False):
@@ -4477,8 +4482,8 @@ class IStates(select):
 			return ''
 
 		states_of_this_syntax = [state for state, _ in cls.values]
-		not_set_filter = '(!(%s=*))' % object_property
-		compare_filter = '%s=%s' % (object_property, object_property_value)
+		not_set_filter = filter_format('(!(%s=*))', (object_property,))
+		compare_filter = _replace_asterisks_in_filter(filter_format('%s=%s', (object_property, object_property_value)), allow_asterisks)
 		if state_of_object_property_value is None:
 			return not_set_filter
 		elif state_of_object_property_value is False and None not in states_of_this_syntax:

management/univention-management-console-module-udm/umc/python/udm/__init__.py
@@ -53,7 +53,7 @@ from univention.management.console.config import ucr
 from univention.management.console.modules import Base, UMC_Error
 from univention.management.console.modules.decorators import simple_response, sanitize, multi_response, prevent_xsrf_check, allow_get_request
 from univention.management.console.modules.sanitizers import (
-	Sanitizer, LDAPSearchSanitizer, EmailSanitizer, ChoicesSanitizer,
+	Sanitizer, SearchSanitizer, EmailSanitizer, ChoicesSanitizer,
 	ListSanitizer, StringSanitizer, DictSanitizer, BooleanSanitizer,
 	DNSanitizer
 )
@@ -141,7 +141,7 @@ class ObjectPropertySanitizer(StringSanitizer):
 		StringSanitizer.__init__(self, **args)
 
 
-class PropertySearchSanitizer(LDAPSearchSanitizer):
+class PropertySearchSanitizer(SearchSanitizer):
 
 	def _sanitize(self, value, name, further_arguments):
 		object_type = further_arguments.get('objectType')
@@ -606,7 +606,7 @@ class Instance(Base, ProgressMixin):
 			scope = request.options.get('scope', 'sub')
 			hidden = request.options.get('hidden')
 			fields = (set(request.options.get('fields', []) or []) | set([objectProperty])) - set(['name', 'None'])
-			result = module.search(container, objectProperty, objectPropertyValue, superordinate, scope=scope, hidden=hidden)
+			result = module.search(container, objectProperty, objectPropertyValue, superordinate, scope=scope, hidden=hidden, allow_asterisks=USE_ASTERISKS)
 			if result is None:
 				return []
 
@@ -938,7 +938,7 @@ class Instance(Base, ProgressMixin):
 
 	@sanitize(
 		syntax=StringSanitizer(required=True),
-		key=LDAPSearchSanitizer(use_asterisks=False),
+		key=SearchSanitizer(use_asterisks=False),
 	)
 	@simple_response
 	def syntax_choices_key(self, syntax, key):
@@ -973,7 +973,7 @@ class Instance(Base, ProgressMixin):
 		return info_syntax_choices(syntax, ldap_connection=lo, ldap_position=po)
 
 	@sanitize(
-		objectPropertyValue=LDAPSearchSanitizer(),
+		objectPropertyValue=SearchSanitizer(),
 		objectProperty=ObjectPropertySanitizer(),
 		syntax=StringSanitizer(required=True)
 	)
@@ -991,7 +991,9 @@ class Instance(Base, ProgressMixin):
 			syntax = _get_syntax(request.options['syntax'])
 			if syntax is None:
 				return
-			return read_syntax_choices(syntax, request.options, ldap_connection=ldap_connection, ldap_position=ldap_position)
+			options = request.options
+			options.pop('allow_asterisks', None)  # internal option
+			return read_syntax_choices(syntax, options, ldap_connection=ldap_connection, ldap_position=ldap_position)
 
 		thread = notifier.threads.Simple('SyntaxChoice', notifier.Callback(_thread, request), notifier.Callback(self.thread_finished_callback, request))
 		thread.run()

management/univention-management-console-module-udm/umc/python/udm/udm_ldap.py
@@ -60,7 +60,7 @@ import univention.admin.uexceptions as udm_errors
 import univention.admin.mapping as udm_mapping
 
 from ldap import LDAPError, NO_SUCH_OBJECT
-from ldap.filter import filter_format
+from ldap.filter import filter_format, escape_filter_chars
 from ldap.dn import explode_dn
 from functools import reduce
 
@@ -649,7 +649,7 @@ class UDM_Module(object):
 			MODULE.warn('Failed to modify LDAP object %s: %s: %s' % (obj.dn, e.__class__.__name__, str(e)))
 			UDM_Error(e).reraise()
 
-	def search(self, container=None, attribute=None, value=None, superordinate=None, scope='sub', filter='', simple=False, simple_attrs=None, hidden=True, serverctrls=None, response=None):
+	def search(self, container=None, attribute=None, value=None, superordinate=None, scope='sub', filter='', simple=False, simple_attrs=None, hidden=True, serverctrls=None, response=None, allow_asterisks=True):
 		"""Searches for LDAP objects based on a search pattern"""
 		ldap_connection, ldap_position = self.get_ldap_connection()
 		if container == 'all':
@@ -659,7 +659,7 @@ class UDM_Module(object):
 		if attribute in [None, 'None'] and filter:
 			filter_s = str(filter)
 		else:
-			filter_s = self._object_property_filter(attribute, value, hidden)
+			filter_s = self._object_property_filter(attribute, value, hidden, allow_asterisks)
 
 		MODULE.info('Searching for LDAP objects: container = %s, filter = %s, superordinate = %s' % (container, filter_s, superordinate))
 		result = None
@@ -1146,16 +1146,17 @@ class UDM_Module(object):
 			return self.module.mapping
 		return udm_mapping.mapping()
 
-	def _object_property_filter(self, object_property, object_property_value, show_hidden=True):
+	def _object_property_filter(self, object_property, object_property_value, show_hidden=True, allow_asterisks=True):
 		if object_property in [None, 'None']:
 			ret = ''
 			if object_property_value not in [None, '*']:
-				# TODO: filter_format() here (but allow "foo*bar") instead of in layer above
-				ret = '(|%s)' % ''.join('(%s=%s)' % (attr, object_property_value) for attr in self.default_search_attrs)
+				ret = '(|%s)' % ''.join(filter_format('(%s=%s)', (attr, object_property_value)) for attr in self.default_search_attrs)
+				if allow_asterisks:
+					ret = ret.replace(escape_filter_chars('*'), '*')
 		else:
 			prop = self.module.property_descriptions.get(object_property)
 			syn = prop.syntax if prop else udm_syntax.ISyntax
-			ret = syn.get_object_property_filter(object_property, object_property_value)
+			ret = syn.get_object_property_filter(object_property, object_property_value, allow_asterisks=allow_asterisks)
 
 		if not show_hidden:
 			ret = self._append_hidden_filter(ret)
@@ -1297,7 +1298,7 @@ def search_syntax_choices_by_key(syn, key, ldap_connection, ldap_position):
 			match = LDAP_ATTR_RE.match(syn.key)
 			if match:
 				attr = match.groups()[0]
-				options = {'objectProperty': attr, 'objectPropertyValue': key}
+				options = {'objectProperty': attr, 'objectPropertyValue': key, 'allow_asterisks': False}
 				return read_syntax_choices(syn, options, ldap_connection=ldap_connection, ldap_position=ldap_position)
 
 	MODULE.warn('Syntax %r: No fast search function' % syn.name)
@@ -1337,6 +1338,8 @@ def read_syntax_choices(syn, options=None, ldap_connection=None, ldap_position=N
 		options['property'] = options.pop('objectProperty')
 	if 'objectPropertyValue' in options:
 		options['value'] = options.pop('objectPropertyValue')
+	# only for syntax_choices_key
+	options['allow_asterisks'] = options.pop('allow_asterisks', True)
 
 	try:
 		if isinstance(syn, udm_syntax.LDAP_Search):

commit 5f3bda89c63f51526c13a5963b0c174cfc419bde

Bug #38762: escape string interpolated LDAP filters
Fixes: ed1d263e87e9b2721605d2ee91b1685c1f4415fc

management/univention-directory-manager-modules/modules/univention/admin/syntax.py
@@ -708,7 +708,7 @@ class _UDMObjectOrAttribute(object):
 		if callable(cls.udm_filter):
 			filter_s = cls.udm_filter(options)
 		else:
-			filter_s = cls.udm_filter % options  # FIXME: missing LDAP filter escaping
+			filter_s = cls.udm_filter % _EscapedDict(options)
 
 		object_properties = []
 		if 'property' in options:
@@ -5872,7 +5872,9 @@ class LDAP_Search(select):
 			filter_mod = univention.admin.modules.identifyOne(options['dn'], lo.get(options['dn']))
 			if filter_mod:
 				obj = univention.admin.objects.get(filter_mod, None, lo, None, options['dn'])
-				filter_s = univention.admin.pattern_replace(filter_s, obj)  # FIXME: LDAP filter is not escaped
+				eobj = _EscapedDict(obj)
+				eobj.dn = escape_filter_chars(obj.dn)
+				filter_s = univention.admin.pattern_replace(filter_s, eobj)
 
 		choices = []
 		for dn in lo.searchDn(filter=filter_s, base=cls.base):
@@ -6799,3 +6801,16 @@ class UDM_Syntax(combobox):
 
 
 __register_choice_update_function(UDM_Syntax.update_choices)
+
+
+class _EscapedDict(dict):
+
+	def __init__(self, _dict):
+		self._dict = _dict
+		super(_EscapedDict, self).__init__()
+
+	def __contains__(self, key):
+		return key in self._dict
+
+	def __missing__(self, key):
+		return escape_filter_chars(self._dict[key], 0)