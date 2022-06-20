00096f5bfb45113f014e1ae3a031347db061903c
Felix Botner <botner@univention.de>
2022-06-20 12:58:59 +0200

    Bug #54779: user specific kvm instances

--- test/scenarios/veyon/start-kvm.sh
+++ test/scenarios/veyon/start-kvm.sh
@@ -8,6 +8,14 @@ export UCS_ENV_VEYON_WINDOWS_HOST="${UCS_ENV_VEYON_WINDOWS_HOST:=3}"
 export KVM_BUILD_SERVER="${KVM_BUILD_SERVER:=ranarp.knut.univention.de}"
 export HALT=false
 
+# extra label for instances names so that the instances
+# are user specific
+if [ -n "$BUILD_URL" ]; then
+	# -> if started via jenkins KVM_LABEL_SUFFIX="username_"
+	KVM_LABEL_SUFFIX="$(curl -k -s "$BUILD_URL/api/json" | awk -F '"userId":"' '{print $2}'| awk -F '"' '{print $1}')_"
+	export KVM_LABEL_SUFFIX
+fi
+
 ./scenarios/veyon/create_veyon_cfg.py  \
 	-w "$UCS_ENV_VEYON_WINDOWS_HOST" \
 	-v kvm > "$cfg_file"
