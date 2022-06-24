40f54fee742353e3ca9f9d094a267708835f91fe
Felix Botner <botner@univention.de>
2022-06-24 09:16:10 +0200

    !fixup Bug #54779: user specific kvm instances

--- test/utils/id-broker/start-kvm.sh
+++ test/utils/id-broker/start-kvm.sh
@@ -3,22 +3,23 @@
 set -x
 set -e
 
-cfg_file="$(mktemp)"
-export UCS_ENV_VEYON_WINDOWS_HOST="${UCS_ENV_VEYON_WINDOWS_HOST:=3}"
 export KVM_BUILD_SERVER="${KVM_BUILD_SERVER:=ranarp.knut.univention.de}"
 export HALT=false
+export DOCKER=true
+export UCS_ENV_IDBROKER_DOMAIN=broker.local
+export UCS_ENV_TRAEGER1_DOMAIN=traeger1.local
+export UCS_ENV_TRAEGER2_DOMAIN=traeger2.local
+export KVM_KEYPAIR_PASSPHRASE=univention
+export UCS_ENV_PASSWORD=univention
 
+# TODO remove
 export DIMAGE="docker-registry.knut.univention.de/ucs-ec2-tools:branch-fbotner-issue-13"
 
 # extra label for instances names so that the instances
 # are user specific
 if [ -n "$BUILD_URL" ]; then
-	# -> if started via jenkins "...-username"
 	my_name="$(curl -k -s "$BUILD_URL/api/json" | awk -F '"userId":"' '{print $2}'| awk -F '"' '{print $1}')"
 	export UCS_ENV_UCS_KT_GET_USERNAME="${my_name}"
 fi
 
-./scenarios/veyon/create_veyon_cfg.py  \
-	-w "$UCS_ENV_VEYON_WINDOWS_HOST" \
-	-v kvm > "$cfg_file"
-exec ./utils/start-test.sh "$cfg_file"
+exec ./utils/start-test.sh scenarios/autotest-247-ucsschool-id-broker.cfg
