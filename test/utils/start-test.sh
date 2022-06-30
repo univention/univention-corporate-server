397ef3ed04d400138585a6b943fa81c4df3e0644
Julia Bremer <bremer@univention.de>
2022-06-30 17:57:08 +0200

    fixup! Bug #54870: Update ami

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -7,10 +7,10 @@
 release='5.0-2'
 old_release='4.4-9'
 kvm_template_version='5.0-1+e174'
-# AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 2
+# AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 3
 current_ami=ami-0536dfa9892ba1f17
 # AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 11
-old_ami=ami-0514b932904f7aa57
+old_ami=ami-02ad9aab36aadf18a
 
 # defaults
 kvm_template='generic-unsafe'
