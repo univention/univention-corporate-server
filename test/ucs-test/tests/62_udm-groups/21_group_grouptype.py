25b906d0bd8e9fad1cd11d842bd4f8dea131fc5a
Florian Best <best@univention.de>
2022-07-03 15:34:42 +0200

    Bug #53538: simplify AD group type tests

--- test/ucs-test/tests/62_udm-groups/21_group_grouptype.py
+++ test/ucs-test/tests/62_udm-groups/21_group_grouptype.py
@@ -7,6 +7,8 @@
 ##   - univention-config
 ##   - univention-directory-manager-tools
 
+import pytest
+
 import univention.testing.utils as utils
 
 
@@ -28,11 +30,19 @@ def _get_samba_sid(dn):
 	return res[0][1].get('sambaSID')[0].decode('ASCII')
 
 
-def _test_group_type(group_type, use_create_group_parameter, builtin_sid_expected, _udm):
+@pytest.mark.parametrize('group_type,use_create_group_parameter,builtin_sid_expected', [
+	('-2147483646', False, False),  # default value
+	('-2147483646', True, False),  # global group
+	('-2147483643', True, True),  # local group
+	('-2147483644', True, False),  # domain local group
+	('-2147483640', True, False),  # universal group
+])
+def test_group_type(group_type, use_create_group_parameter, builtin_sid_expected, udm):
+	"""Test the groupType attribute while creating groups"""
 	if use_create_group_parameter:
-		group = _udm.create_group(adGroupType=group_type)[0]
+		group = udm.create_group(adGroupType=group_type)[0]
 	else:
-		group = _udm.create_group()[0]
+		group = udm.create_group()[0]
 	utils.verify_ldap_object(group, {'univentionGroupType': [group_type]})
 
 	utils.wait_for_connector_replication()
@@ -43,28 +53,3 @@ def _test_group_type(group_type, use_create_group_parameter, builtin_sid_expecte
 		assert group_type == '-2147483643', 'New generated group has builtin sid: %s' % sid
 	else:
 		assert group_type != '-2147483643', 'New generated builtin group has no builtin sid: %s' % sid
-
-
-def test_default_value(udm):
-	print('Test default value')
-	_test_group_type(group_type='-2147483646', use_create_group_parameter=False, builtin_sid_expected=False, _udm=udm)
-
-
-def test_global_group(udm):
-	print('Test global group')
-	_test_group_type(group_type='-2147483646', use_create_group_parameter=True, builtin_sid_expected=False, _udm=udm)
-
-
-def test_local_group(udm):
-	print('Test local group')
-	_test_group_type(group_type='-2147483643', use_create_group_parameter=True, builtin_sid_expected=True, _udm=udm)
-
-
-def test_domain_local_group(udm):
-	print('Test domain local group')
-	_test_group_type(group_type='-2147483644', use_create_group_parameter=True, builtin_sid_expected=False, _udm=udm)
-
-
-def test_universal_group(udm):
-	print('Test universal group')
-	_test_group_type(group_type='-2147483640', use_create_group_parameter=True, builtin_sid_expected=False, _udm=udm)
--- test/ucs-test/tests/62_udm-groups/24_group_modify_uniquemember.py
+++ test/ucs-test/tests/62_udm-groups/24_group_modify_uniquemember.py
@@ -1,4 +1,4 @@
-#!/usr/share/ucs-test/runner pytest-3
+#!/usr/share/ucs-test/runner pytest-3 -s -l -vv
 ## desc: Test the allowed groupType changes
 ## tags: [udm,apptest, SKIP-UCSSCHOOL]
 ## roles: [domaincontroller_master]
@@ -6,44 +6,24 @@
 ##   - univention-config
 ##   - univention-directory-manager-tools
 
-from six.moves import range
-
 import univention.testing.strings as uts
-import univention.uldap
 
 
-def _print(capsys, val):
-	with capsys.disabled():
-		print("\t %s " % (val,))
+def test_umd_memberUid(udm, ucr, lo):
+	"""Test the allowed groupType changes"""
+	# create
+	group1 = udm.create_group(adGroupType="")[0]
 
-
-def _search(lo, val, capsys):
-	res = lo.get(str(val)).get('memberUid')
-	res1 = lo.get(str(val)).get('uniqueMember')
-	strings = ["Group memberuid = ", str(res), " uniquemember  ", str(res1)]
-	_print(capsys, ''.join([_f for _f in strings if _f]))
-	assert res and res1
-
-
-def _update_loop(capsys, ucr, udm, group1):
-	lo = univention.uldap.getAdminConnection()
+	# validate memberuid after update users in group
 	username = uts.random_name()
 	for i in range(5):
 		user = 'uid=%s,cn=temp_%s,%s' % (username, i, ucr.get('ldap/base'),)
-		strings = ["set user = ", user, " to group ", group1]
-		_print(capsys, ''.join([_f for _f in strings if _f]))
 		group1 = udm.modify_object('groups/group', dn=group1, users=user)
-		_search(lo, group1, capsys)
+		res = lo.get(group1).get('memberUid')
+		res1 = lo.get(group1).get('uniqueMember')
+		assert res and res1
 
-
-def test_umd_memberUid(udm, ucr, capsys):
-	# create
-	group1 = udm.create_group(adGroupType="")[0]
-	strings = ["create group = ", group1]
-	_print(capsys, ''.join([_f for _f in strings if _f]))
-	# validate memberuid after update users in group
-	_update_loop(capsys, ucr, udm, group1)
 	# remove group
 	group1 = udm.remove_object('groups/group', dn=group1)
 	if not group1:
-		_print(capsys, "Remove group successfully  ")
+		print("Remove group successfully  ")
