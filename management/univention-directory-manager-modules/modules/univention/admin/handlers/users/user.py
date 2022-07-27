ef6e1101a46b0d26bedd66e604de00a7a6707151
Florian Best <best@univention.de>
2022-07-27 15:20:15 +0200

    Bug #53807: fix search for users/user:sambaLogonHours

--- management/univention-directory-manager-modules/modules/univention/admin/handlers/users/user.py
+++ management/univention-directory-manager-modules/modules/univention/admin/handlers/users/user.py
@@ -2073,6 +2073,12 @@ class object(univention.admin.handlers.simpleLdap, PKIIntegration, GuardianBase)
     @classmethod
     def rewrite_filter(cls, filter, mapping):
         # type: (univention.admin.filter.expression, univention.admin.mapping.mapping) -> None
+        if filter.variable == 'sambaLogonHours' and filter.value:
+            try:
+                filter.value = property_descriptions['sambaLogonHours'].syntax.parse(filter.value)
+            except univention.admin.uexceptions.valueError:
+                pass
+
         if filter.variable == 'primaryGroup':
             filter.variable = 'gidNumber'
         elif filter.variable == 'groups':
