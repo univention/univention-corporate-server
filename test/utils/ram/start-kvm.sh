53191284b619ccbd7a843d51256bf022471a2f87
Tobias Wenzel <wenzel@univention.de>
2022-08-22 17:03:40 +0200

    Bug #55127: add rankine jenkins job (univention/internal/research-library/issues/14)

---
+++ test/utils/ram/start-kvm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export KVM_BUILD_SERVER="${KVM_BUILD_SERVER:=ranarp.knut.univention.de}"
+export HALT=false
+export DOCKER=true
+export REPLACE=true
+export UCS_ENV_PASSWORD=univention
+export KVM_KEYPAIR_PASSPHRASE=univention # Initial appliance password
+export UCS_TEST_RUN=false
+
+# user specific instances "username_..."
+export KVM_OWNER="$BUILD_USER_ID"
+
+exec ./utils/start-test.sh scenarios/autotest-248-ram-rankine.cfg
