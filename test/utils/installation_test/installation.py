d0fad2abe8db941f392d8a8b5bd99d55a172c12e
Philipp Hahn <hahn@univention.de>
2022-08-26 14:01:22 +0200

    feat[vnc]: Add debug output
    Bug #55107

--- test/utils/installation_test/installation.py
+++ test/utils/installation_test/installation.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python2.7
+#!/usr/bin/python2.7 -u
 # -*- coding: utf-8 -*-
 
 """
-UCS installation via vnc
+UCS installation via VNC
 """
 
 from vncautomate import init_logger, VNCConnection
@@ -18,8 +18,29 @@ import sys
 import os
 import logging
 from argparse import Namespace  # noqa: F401
+from functools import wraps
 
 KVM_INTERFACE = 'ens8'
+
+
+def verbose(msg, fmt="", skip=1):  # type (str, str, int) -> Callabble[[T], T]
+	def decorator(f):  # type (Callable) -> Callable
+		@wraps(f)
+		def wrapper(*args, **kwargs):  # type (*Any, **Any) -> Any
+			print(("%s:BEGIN " + fmt) % ((msg,) + args[skip:][:fmt.count("%")]))
+			sys.stdout.flush()
+			try:
+				return f(*args, **kwargs)
+			finally:
+				print("%s:END" % msg)
+				sys.stdout.flush()
+		return wrapper
+	return decorator
+
+
+@verbose("sleep", "%.1f", skip=0)
+def sleep(seconds):  # type: (float) -> None
+	time.sleep(seconds)
 
 
 class UCSInstallation(object):
@@ -48,6 +69,7 @@ class UCSInstallation(object):
 		screenshot_file = os.path.join(self.args.screenshot_dir, filename)
 		self.client.captureScreen(screenshot_file)
 
+	@verbose("click", "%r")
 	def click(self, text):  # type: (str) -> None
 		self.client.waitForText(text, timeout=self.timeout)
 		self.client.mouseClickOnText(text)
@@ -65,13 +87,15 @@ class UCSInstallation(object):
 			self.connect()
 			return False
 
+	@verbose("NEXT")
 	def move_to_next_and_click(self):  # type: () -> None
-		time.sleep(1)
+		sleep(1)
 		self.client.mouseMove(910, 700)
-		time.sleep(1)
+		sleep(1)
 		logging.info('clicking next')
 		self.client.mousePress(1)
 
+	@verbose("INSTALLER")
 	def installer(self):  # type: () -> None
 		# language
 		for i in range(3):
@@ -93,7 +117,7 @@ class UCSInstallation(object):
 		if not self.network_setup():
 			self.client.mouseMove(100, 320)
 			self.client.mousePress(1)
-			time.sleep(1)
+			sleep(1)
 
 		# root
 		self.client.waitForText(self._['user_and_password'], timeout=self.timeout)
@@ -105,20 +129,20 @@ class UCSInstallation(object):
 		if self.args.language == 'eng':
 			self.client.waitForText(self._['configure_clock'], timeout=self.timeout)
 			#self.client.enterText(self._['clock'])
-			time.sleep(1)
+			sleep(1)
 			self.client.keyPress('enter')
 		# hd
-		time.sleep(60)
+		sleep(60)
 		self.client.waitForText(self._['partition_disks'], timeout=self.timeout)
 		if self.args.role == 'applianceLVM':
 			#self.click(self._['entire_disk_with_lvm'])
 			# LVM is the default so just press enter
 			self.client.keyPress('enter')
-			time.sleep(3)
+			sleep(3)
 			self.client.keyPress('enter')
 			self.click(self._['all_files_on_partition'])
 			self.client.keyPress('enter')
-			time.sleep(3)
+			sleep(3)
 			self.client.keyPress('down')
 			self.client.keyPress('enter')
 			self.client.waitForText(self._['continue_partition'], timeout=self.timeout)
@@ -128,38 +152,38 @@ class UCSInstallation(object):
 			# Manuel
 			self.click(self._['manual'])
 			self.client.keyPress('enter')
-			time.sleep(3)
+			sleep(3)
 			# Virtuelle Festplatte 1
 			self.click(self._['virtual_disk_1'])
-			time.sleep(3)
+			sleep(3)
 			self.client.keyPress('enter')
-			time.sleep(3)
+			sleep(3)
 			self.client.keyPress('down')
-			time.sleep(3)
+			sleep(3)
 			self.client.keyPress('enter')
-			time.sleep(3)
+			sleep(3)
 			self.click(self._['free_space'])
 			self.client.keyPress('enter')
-			time.sleep(3)
+			sleep(3)
 			# neue partition erstellen
 			self.client.keyPress('enter')
-			time.sleep(3)
+			sleep(3)
 			# enter: ganze festplattengröße ist eingetragen
 			self.client.keyPress('enter')
-			time.sleep(3)
+			sleep(3)
 			# enter: primär
 			self.client.keyPress('enter')
-			time.sleep(3)
+			sleep(3)
 			self.click(self._['boot_flag'])
 			# enter: boot-flag aktivieren
 			self.client.keyPress('enter')
-			time.sleep(3)
+			sleep(3)
 			self.click(self._['finish_create_partition'])
 			self.client.keyPress('enter')
-			time.sleep(3)
+			sleep(3)
 			self.click(self._['finish_partition'])
 			self.client.keyPress('enter')
-			time.sleep(3)
+			sleep(3)
 			# Nein (kein swap speicher)
 			self.click(self._['no'])
 			self.client.keyPress('enter')
@@ -170,22 +194,23 @@ class UCSInstallation(object):
 		else:
 			self.click(self._['entire_disk'])
 			self.client.keyPress('enter')
-			time.sleep(3)
+			sleep(3)
 			self.client.keyPress('enter')
-			time.sleep(3)
+			sleep(3)
 			self.client.keyPress('enter')
 			self.click(self._['finish_partition'])
 			self.client.keyPress('enter')
 			self.client.waitForText(self._['continue_partition'], timeout=self.timeout)
 			self.client.keyPress('down')
 			self.client.keyPress('enter')
-		time.sleep(600)
+		sleep(600)
 		self.client.waitForText(self._['finish_installation'], timeout=1300)
 		self.client.keyPress('enter')
-		time.sleep(30)
+		sleep(30)
 
+	@verbose("NETWORK")
 	def network_setup(self):  # type: () -> bool
-		time.sleep(60)
+		sleep(60)
 		# we may not see this because the only interface is configured via dhcp
 		if not self.text_is_visible(self._['configure_network'], timeout=120):
 			return False
@@ -193,7 +218,7 @@ class UCSInstallation(object):
 		if not self.text_is_visible(self._['ip_address'], timeout=self.timeout):
 			# always use first interface
 			self.click(self._['continue'])
-			time.sleep(60)
+			sleep(60)
 		if self.args.ip:
 			if self.text_is_visible(self._['not_using_dhcp'], timeout=self.timeout):
 				self.client.waitForText(self._['not_using_dhcp'], timeout=self.timeout)
@@ -220,26 +245,26 @@ class UCSInstallation(object):
 
 	def configure_kvm_network(self):  # type: () -> None
 		if 'all' in self.args.components or 'kde' in self.args.components:
-			time.sleep(10)
+			sleep(10)
 			self.client.keyDown('alt')
 			self.client.keyDown('ctrl')
 			self.client.keyPress('f1')
 			self.client.keyUp('alt')
 			self.client.keyUp('ctrl')
 		elif self.args.role == 'basesystem':
-			time.sleep(3)
+			sleep(3)
 		else:
 			self.client.waitForText('corporate server')
 			self.client.keyPress('enter')
-		time.sleep(3)
+		sleep(3)
 		self.client.enterText('root')
 		self.client.keyPress('enter')
-		time.sleep(5)
+		sleep(5)
 		self.client.enterText(self.args.password)
 		self.client.keyPress('enter')
 		self.client.enterText('ucr set interfaces-%s-tzpe`manual' % KVM_INTERFACE)
 		self.client.keyPress('enter')
-		time.sleep(30)
+		sleep(30)
 		self.client.enterText('ip link set %s up' % KVM_INTERFACE)
 		self.client.keyPress('enter')
 		self.client.enterText('echo ')
@@ -253,6 +278,7 @@ class UCSInstallation(object):
 		self.client.enterText(' crontab')
 		self.client.keyPress('enter')
 
+	@verbose("SETUP")
 	def setup(self):  # type: () -> None
 		self.client.waitForText(self._['domain_setup'], timeout=self.timeout + 900)
 		if self.args.role == 'master':
@@ -269,10 +295,10 @@ class UCSInstallation(object):
 				self.click(self._['preferred_dns'])
 				self.client.enterText(self.args.dns)
 				self.client.keyPress('enter')
-				time.sleep(120)
+				sleep(120)
 				if self.text_is_visible(self._['repositories_not_reachable']):
 					self.client.keyPress('enter')
-					time.sleep(30)
+					sleep(30)
 				self.click(self._['join_domain'])
 				self.move_to_next_and_click()
 			self.client.waitForText(self._['role'])
@@ -291,16 +317,16 @@ class UCSInstallation(object):
 			self.client.waitForText(self._['no_dc_dns'], timeout=self.timeout)
 			self.client.keyPress('enter')
 			self.click(self._['preferred_dns'])
-			time.sleep(1)
+			sleep(1)
 			self.client.enterText(self.args.dns)
 			self.client.keyPress('enter')
-			time.sleep(120)
+			sleep(120)
 			if self.text_is_visible(self._['repositories_not_reachable']):
 				self.client.keyPress('enter')
-				time.sleep(30)
+				sleep(30)
 			if self.text_is_visible('APIPA', timeout=self.timeout):
 				self.client.keyPress('enter')
-				time.sleep(60)
+				sleep(60)
 			self.move_to_next_and_click()
 			self.move_to_next_and_click()
 		elif self.args.role == 'basesystem':
@@ -312,18 +338,19 @@ class UCSInstallation(object):
 			self.client.keyDown('ctrl')
 			self.client.keyPress('q')
 			self.client.keyUp('ctrl')
-			time.sleep(60)
+			sleep(60)
 			sys.exit(0)
 		else:
 			raise NotImplementedError
 
+	@verbose("JOIN")
 	def joinpass(self):  # type: () -> None
 		if self.args.role not in ['slave', 'backup', 'member']:
 			return
 		self.client.waitForText(self._['start_join'], timeout=self.timeout)
 		for i in range(2):
 			self.click(self._['hostname_primary'])
-			time.sleep(5)
+			sleep(5)
 			self.client.keyPress('tab')
 			self._clear_input()
 			self.client.enterText(self.args.join_user)
@@ -373,14 +400,15 @@ class UCSInstallation(object):
 			self.client.keyPress('tab')
 		self.move_to_next_and_click()
 
+	@verbose("FINISH")
 	def finish(self):  # type: () -> None
 		self.client.waitForText(self._['confirm_config'], timeout=self.timeout)
 		self.client.keyPress('enter')
-		time.sleep(self.setup_finish_sleep)
+		sleep(self.setup_finish_sleep)
 		self.client.waitForText(self._['setup_successful'], timeout=2100)
 		self.client.keyPress('tab')
 		self.client.keyPress('enter')
-		time.sleep(10)
+		sleep(10)
 		self.client.waitForText('univention', timeout=self.timeout)
 
 	def ucsschool(self):  # type: () -> None
