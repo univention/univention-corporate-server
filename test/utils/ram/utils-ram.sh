276dcf70f536423fc097b384ff776089dc9cd17e
Felix Botner <botner@univention.de>
2022-09-02 15:25:33 +0200

    Bug #55127: added frontend packages

--- test/utils/ram/utils-ram.sh
+++ test/utils/ram/utils-ram.sh
@@ -1,20 +1,31 @@
 set_udm_properties_for_kelvin () {
-  echo '{ "user": ["displayName", "e-mail", "accountActivationDate", "pwdChangeNextLogin", "serviceprovider", "ucsschoolPurgeTimestamp"]}' > /etc/ucsschool/kelvin/mapped_udm_properties.json
-  univention-app restart ucsschool-kelvin-rest-api
+	echo '{ "user": ["displayName", "e-mail", "accountActivationDate", "pwdChangeNextLogin", "serviceprovider", "ucsschoolPurgeTimestamp"]}' > /etc/ucsschool/kelvin/mapped_udm_properties.json
+	univention-app restart ucsschool-kelvin-rest-api
 }
 
-install_ucschool_bff_users () {
-  # do not rename function: used as install_[ENV:TEST_API]_api in autotest-241-ucsschool-HTTP-API.cfg
-  . utils.sh && switch_to_test_app_center || true
-  echo -n univention > /tmp/univention
-  # use brach image if given
-  if [ -n "$UCS_ENV_RANKINE_USERS_IMAGE" ]; then
-    if [[ $UCS_ENV_RANKINE_USERS_IMAGE =~ ^gitregistry.knut.univention.de.* ]]; then
-        docker login -u "$GITLAB_REGISTRY_TOKEN" -p "$GITLAB_REGISTRY_TOKEN_SECRET" gitregistry.knut.univention.de
-    fi
-    univention-app dev-set ucsschool-bff-users "DockerImage=$UCS_ENV_RANKINE_USERS_IMAGE"
-  fi
-  univention-app install --noninteractive --username Administrator --pwdfile /tmp/univention ucsschool-bff-users
-  docker images
-  docker ps -a
+install_frontend_apps () {
+	local kelvin_host=${1:?missing kelvin_host}
+
+	echo -n univention > /tmp/univention
+
+	# use brach image if given
+	if [ -n "$UCS_ENV_RANKINE_USERS_IMAGE" ]; then
+		univention-app dev-set ucsschool-bff-users "DockerImage=$UCS_ENV_RANKINE_USERS_IMAGE"
+	fi
+	univention-app install --noninteractive --username Administrator --pwdfile /tmp/univention ucsschool-bff-users
+	univention-app configure ucsschool-bff-users --set \
+		ucsschool/ui_users/kelvin/username=Administrator \
+		ucsschool/ui_users/kelvin/password=univention \
+		ucsschool/ui_users/kelvin/host="$kelvin_host" \
+		ucsschool/ui_users/kelvin/ssl_verify=0
+
+	docker images
+	docker ps -a
+}
+
+install_frontend_packages () {
+	univention-install -y ucs-school-ui-users-frontend
+
+	# create dev clients for easier testing
+	/usr/share/ucs-school-ui-common/scripts/univention-create-keycloak-clients --admin-password univention --client-id school-ui-users-dev --direct-access
 }
