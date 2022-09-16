e4c78930b76e81667085f88749ee40ede291a215
Carlos García-Mauriño <garcia-maurino.extern@univention.de>
2022-09-16 13:13:58 +0200

    Bug #55127: enable all RAM attributes in Jenkins

--- test/utils/ram/utils-ram.sh
+++ test/utils/ram/utils-ram.sh
@@ -22,3 +22,49 @@ install_frontend_packages () {
 	# create dev clients for easier testing
 	/usr/share/ucs-school-ui-common/scripts/univention-create-keycloak-clients --admin-password univention --client-id school-ui-users-dev --direct-access
 }
+
+install_all_attributes_primary () {
+    # get APT customer repo $username and @password from the RAM secrets
+    export $(grep -v '^#' /etc/ram.secrets| xargs)
+	echo -n univention > /tmp/univention
+
+    /usr/sbin/univention-config-registry set \
+      repository/online/component/fhh-bsb-iam=yes \
+      repository/online/component/fhh-bsb-iam/server='service.knut.univention.de' \
+      repository/online/component/fhh-bsb-iam/prefix="apt/$username" \
+      repository/online/component/fhh-bsb-iam/parts='maintained' \
+      repository/online/component/fhh-bsb-iam/username="$username" \
+      repository/online/component/fhh-bsb-iam/password="$password"
+
+    univention-install -y ucsschool-divis-custom-ldap-extension ucsschool-iserv-custom-ldap-extension ucsschool-moodle-custom-ldap-extension univention-saml
+    systemctl restart univention-directory-manager-rest.service
+    univention-install -y univention-appcenter-dev
+    univention-app dev-use-test-appcenter
+    univention-app install --noninteractive --username Administrator --pwdfile /tmp/univention \
+        radius \
+        self-service-backend
+}
+
+enable_all_attributes_kelvin () {
+    cat <<EOT > /etc/ucsschool/kelvin/mapped_udm_properties.json
+{
+        "user": [
+                "accountActivationDate",
+                "displayName",
+                "divisNameAffix",
+                "divisNickname",
+                "e-mail",
+                "networkAccess",
+                "PasswordRecoveryEmail",
+                "PasswordRecoveryEmailVerified",
+                "pwdChangeNextLogin",
+                "serviceprovider",
+                "ucsschoolPurgeTimestamp"
+        ]
+}
+
+EOT
+    univention-app shell ucsschool-kelvin-rest-api /var/lib/univention-appcenter/apps/ucsschool-kelvin-rest-api/data/update_openapi_client
+    univention-app shell ucsschool-kelvin-rest-api /etc/init.d/ucsschool-kelvin-rest-api restart
+
+}
