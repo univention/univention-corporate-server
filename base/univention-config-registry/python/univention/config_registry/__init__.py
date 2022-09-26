univention/univention-corporate-server

commit e4778c95ce151fd5588efb42ac2a18fc2f15a613

feat[ucr]: Singleton UCR instance
Provide a lazy loaded UCR instance to be shared by everyone only needing
read-only access.

Bug #51126

base/univention-config-registry/python/univention/config_registry/__init__.py
@@ -31,7 +31,9 @@
 
 """Univention Configuration Registry module."""
 
-from univention.config_registry.backend import StrictModeException, SCOPE, ConfigRegistry  # noqa F401
+from lazy_object_proxy import Proxy
+
+from univention.config_registry.backend import StrictModeException, SCOPE, ConfigRegistry, ReadConfigRegistry as _RCR  # noqa F401
 from univention.config_registry.handler import run_filter as filter, ConfigHandlers as configHandlers  # noqa F401
 from univention.config_registry.misc import key_shell_escape, validate_key, INVALID_KEY_CHARS as invalid_key_chars  # noqa F401
 from univention.config_registry.filters import filter_shell, filter_keys_only, filter_sort  # noqa F401
@@ -42,6 +44,8 @@ from univention.config_registry.frontend import (  # noqa F401
 	handler_register, handler_unregister, handler_update,
 )
 from univention.debhelper import parseRfc822  # noqa F401
+
+ucr = Proxy(lambda: _RCR().load())
 
 
 def ucr_factory():  # type: () -> ConfigRegistry

base/univention-config-registry/debian/control
@@ -11,6 +11,7 @@ Build-Depends:
  libunivention-debug-dev (>= 5.0.6-1),
  python-all,
  python-ipaddress,
+ python-lazy-object-proxy,
  python-mock,
  python-pytest,
  python-pytest-cov,
@@ -20,6 +21,7 @@ Build-Depends:
  python-univention-debhelper,
  python3 (>= 3.6) | python3-mock,
  python3-all,
+ python3-lazy-object-proxy,
  python3-mock,
  python3-pytest,
  python3-pytest-cov,
@@ -122,6 +124,7 @@ Architecture: all
 Depends:
  ifupdown,
  python-ipaddress,
+ python-lazy-object-proxy,
  python-six,
  python-univention-debhelper,
  python-univention-namespace,
@@ -142,6 +145,7 @@ Package: python3-univention-config-registry
 Architecture: all
 Depends:
  ifupdown,
+ python3-lazy-object-proxy,
  python3-six,
  python3-univention-debhelper,
  ${misc:Depends},

base/univention-config-registry/setup.cfg
@@ -61,6 +61,7 @@ addopts =
 	--cov-report xml:coverage.xml
 	--junit-xml pytest.xml
 	--doctest-modules
+	--ignore python/univention/config_registry/__init__.py
 marksers =
 	slow: mark test as slow
 

base/univention-config-registry/tests/test_ucr.py
@@ -2,8 +2,36 @@
 """Unit test for univention.config_registry."""
 # pylint: disable-msg=C0103,E0611,R0904
 
+import sys
+
+import pytest
+
 import univention.config_registry as UCR
+
+if sys.version_info >= (3,):
+	from importlib import reload
 
 
 def test_private(tmpucr):
 	assert UCR.ucr_factory() is not UCR.ucr_factory()
+
+
+def test_ro(ucrf):
+	reload(UCR)
+	assert not isinstance(UCR.ucr, UCR.ConfigRegistry)
+	assert UCR.ucr["foo"] == "LDAP"
+	assert UCR.ucr["bam"] is None
+	with pytest.raises(TypeError):
+		UCR.ucr["foo"] = "42"
+	with pytest.raises(TypeError):
+		del UCR.ucr["foo"]
+
+
+def test_ro_stale(ucr0):
+	reload(UCR)
+	ucr0["baz"] = "BEFORE"
+	ucr0.save()
+	assert UCR.ucr["baz"] == "BEFORE"
+	ucr0["baz"] = "AFTER"
+	ucr0.save()
+	assert UCR.ucr["baz"] == "BEFORE"

commit ff4c6005cd981d52562076a87f1172574eeda65e

doc[ucr]: PEP-484 types for Singleton UCR instance
Declare as ReadOnlyConfigRegistry instead of Any.

Bug #51126
Fixes: e4778c95ce151fd5588efb42ac2a18fc2f15a613

base/univention-config-registry/python/univention/config_registry/__init__.py
@@ -48,8 +48,8 @@ from univention.config_registry.frontend import (  # noqa: F401
 )
 from univention.debhelper import parseRfc822  # noqa: F401
 
-ucr = Proxy(lambda: _RCR().load(autoload=Load.ONCE))
-ucr_live = Proxy(lambda: _RCR().load(autoload=Load.ALWAYS))
+ucr = Proxy(lambda: _RCR().load(autoload=Load.ONCE))  # type: _RCR
+ucr_live = Proxy(lambda: _RCR().load(autoload=Load.ALWAYS))  # type: _RCR
 
 
 def ucr_factory():  # type: () -> ConfigRegistry