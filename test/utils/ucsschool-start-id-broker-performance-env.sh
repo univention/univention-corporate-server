56b5ff4241b33bb053d648a8d9a3d691a3eafcda
Felix Botner <botner@univention.de>
2022-10-10 15:07:58 +0200

    Issue #9: update template
    univention/components/ucsschool-api-plugins/id-broker-plugin#9

---
+++ test/utils/ucsschool-start-id-broker-performance-env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# docker can't hanlde newline in env file, so replace
+# newline witch DELIM
+export UCS_ENV_LOCUST_VARS="${UCS_ENV_LOCUST_VARS//$'\n'/:DELIM:}"
+export KVM_BUILD_SERVER="${KVM_BUILD_SERVER:=ranarp.knut.univention.de}"
+export HALT=false
+export DOCKER=true
+export REPLACE=true
+export UCS_TEST_RUN=false
+
+# user specific instances "username_..."
+if [ -n "$BUILD_USER_ID" ]; then
+	export KVM_OWNER="$BUILD_USER_ID"
+fi
+
+exec ./utils/start-test.sh ./scenarios/autotest-247-ucsschool-id-broker-perf-kvm.cfg
