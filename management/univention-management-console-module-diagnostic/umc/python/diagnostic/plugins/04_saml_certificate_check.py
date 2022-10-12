90616a9e34649630915bc4293543fb05c9bf7a8b
Philipp Hahn <hahn@univention.de>
2022-10-12 13:57:30 +0200

    fix[diag]: SAML check
    The old code never worked because Python 2.7.9 resp. 3.4 added SNI and
    downloading the certificate always failed:
    > Failed to get certificate
    > 'https://10.200.17.38/simplesamlphp/saml2/idp/certificate':
    > HTTPSConnectionPool(host='10.200.17.38', port=443): Max retries
    > exceeded with url: /simplesamlphp/saml2/idp/certificate (Caused by
    > SSLError(SSLCertVerificationError("hostname '10.200.17.38' doesn't
    > match either of 'm38.phahn.dev', 'm38'")))
    
    To work-around this disable verification.
    
    But still the downloaded certificate from
    https://…/simplesamlphp/saml2/idp/certificate is PEM formatted, line
    wrapped and includes the `openssl x509 -text` prefix.
    In comparison to that the files
    /usr/share/univention-management-console/saml/idp/*.xml just contain the
    raw date base64 encoded and the `in` check never found them.
    
    Read certificate once as it is compared multiple times.
    
    Improve error messagesm e.g. name the failing certificates.
    
    Collect multiple errors and show them all instead of only the first.
    
    Bug #49417

--- management/univention-management-console-module-diagnostic/umc/python/diagnostic/plugins/04_saml_certificate_check.py
+++ management/univention-management-console-module-diagnostic/umc/python/diagnostic/plugins/04_saml_certificate_check.py
@@ -34,93 +34,285 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
-from __future__ import print_function
 import glob
-import shutil
 import socket
-import tempfile
-import contextlib
+from base64 import b64decode
+from subprocess import call
+from typing import Any, Callable, Dict, Iterator, List, Tuple
 
 import requests
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
 from ldap.filter import filter_format
-from defusedxml.ElementTree import fromstring
+from defusedxml.ElementTree import fromstring, parse
+
 import univention.uldap
-import univention.config_registry
-from univention.management.console.modules.diagnostic import Critical, MODULE
 from univention.management.console.config import ucr
+from univention.management.console.modules.diagnostic import MODULE, Critical, Instance, Problem
 
 from univention.lib.i18n import Translation
 _ = Translation('univention-management-console-module-diagnostic').translate
 
+XML = "/usr/share/univention-management-console/saml/idp/*.xml"
+X509CERT = ".//{http://www.w3.org/2000/09/xmldsig#}X509Certificate"
 
 title = _('SAML certificate verification failed!')
 run_descr = ['Checks SAML certificates']
 
 
-@contextlib.contextmanager
-def download_tempfile(url, headers=None):
-	with tempfile.NamedTemporaryFile() as fob:
-		response = requests.get(url, headers=headers, stream=True)
-		shutil.copyfileobj(response.raw, fob)
-		fob.flush()
-		yield fob
+def run(_umc_instance: Instance, rerun: bool = False) -> None:
+	problems: List[str] = []
+	buttons: List[Dict[str, str]] = []
+	links: List[Dict[str, str]] = []
+	umc_modules: List[Dict[str, str]] = []
+
+	idp = False
+	for problem in test_identity_provider_certificate():
+		idp = True
+		kwargs = problem.kwargs
+		problems.append(kwargs["description"])
+		buttons += kwargs.get("buttons", [])
+		links += kwargs.get("links", [])
+		umc_modules += kwargs.get("umc_modules", [])
+
+	if idp and not rerun:
+		problems.append(_(
+			"Re-execute the join-script <tt>92univention-management-console-web-server</tt> via {join} "
+			"or execute <tt>univention-run-join-scripts --force --run-scripts 92univention-management-console-web-server</tt> on the command line as user <i>root</i>."
+		))
+		buttons.append({
+			"action": "fix_idp",
+			"label": _("Re-run join script"),
+		})
+		umc_modules.append({
+			"module": "join",
+			# /univention/command/join/run
+			# {"options":{"scripts":["92univention-management-console-web-server"],"force":true}}
+		})
+
+	sp = False
+	for problem in test_service_provider_certificate():
+		sp = True
+		kwargs = problem.kwargs
+		problems.append(kwargs["description"])
+		buttons += kwargs.get("buttons", [])
+		links += kwargs.get("links", [])
+		umc_modules += kwargs.get("umc_modules", [])
+
+	if sp:
+		problems.append(_(
+			"If you renewed your certificates please see {sdb-one} and {sdb-all} for more instructions. "
+			"In that case the new certificate must be re-uploaded into LDAP."
+		))
+		links += [
+			{
+				"name": "sdb-one",
+				"href": "https://help.univention.com/t/renewing-the-ssl-certificates/37",
+				"label": _("Univention Support Database - Renewing the TLS/SSL certificates"),
+			},
+			{
+				"name": "sdb-all",
+				"href": "https://help.univention.com/t/renewing-the-complete-ssl-certificate-chain/36",
+				"label": _("Univention Support Database - Renewing the complete TLS/SSL certificate chain"),
+			},
+		]
+		if not rerun:
+			buttons.append({
+				"action": "fix_sp",
+				"label": _("Re-upload certificate"),
+			})
+
+	if problems:
+		raise Critical(
+			description="\n".join(problems),
+			buttons=buttons,
+			links=links,
+			umc_modules=umc_modules,
+		)
 
 
-def find_node(node, element):
-	elem = node.find(element)
-	if elem is not None:
-		return elem
-	for child in node:
-		x = find_node(child, element)
-		if x is not None:
-			return x
+def test_identity_provider_certificate() -> Iterator[Problem]:
+	"""
+	Check that all IDP certificates from :file:`/usr/share/univention-management-console/saml/idp/*.xml`
+	are included in :url:`https:/$(ucr get ucs/server/sso/fqdn)/simplesamlphp/saml2/idp/certificate`.
 
-
-def run(_umc_instance):
-	test_identity_provider_certificate()
-	test_service_provider_certificate()
-
-
-def test_identity_provider_certificate():
-	# download from all ip addresses of ucs-sso. the IDP certificate (/etc/simplesamlphp/*-idp-certificate.crt)
-	# compare this with /usr/share/univention-management-console/saml/idp/*.xml
-	# If it fails: univention-run-joinscripts --force --run-scripts 92univention-management-console-web-server
-
+	Fix: ``univention-run-join-scripts --force --run-scripts 92univention-management-console-web-server``
+	"""
 	sso_fqdn = ucr.get('ucs/server/sso/fqdn')
-	MODULE.process("Checks ucs-sso by comparing 'ucr get ucs/server/sso/fqdn' with the Location field in /usr/share/univention-management-console/saml/idp/*.xml")
+	MODULE.process("Checks ucs-sso by comparing 'ucr get ucs/server/sso/fqdn' with the Location field in %s" % (XML,))
 	if not sso_fqdn:
 		return
-	for host in socket.gethostbyname_ex(sso_fqdn)[2]:
+
+	backend = default_backend()
+
+	# download from all ip addresses of ucs-sso the IDP certificate (/etc/simplesamlphp/*-idp-certificate.crt)
+	_name, _aliaslist, addresslist = socket.gethostbyname_ex(sso_fqdn)
+	for i, host in enumerate(addresslist):
+		url = "https://%s/simplesamlphp/saml2/idp/certificate" % (host,)
+		link = "addr%d" % (i,)
+		links = {
+			"name": link,
+			"href": url,
+			"label": url,
+		}
 		try:
-			with download_tempfile('https://%s/simplesamlphp/saml2/idp/certificate' % (host,), {'host': sso_fqdn}) as certificate:
-				certificate = certificate.read()
-				for idp in glob.glob('/usr/share/univention-management-console/saml/idp/*.xml'):
-					with open(idp) as fd:
-						cert = find_node(fromstring(fd.read()), '{http://www.w3.org/2000/09/xmldsig#}X509Certificate')
-						if cert.text.strip() not in certificate:
-							MODULE.error('The certificate of the SAML identity provider does not match.')
-							raise Critical(_('The certificate of the SAML identity provider does not match.'))
+			res = requests.get(url, headers={'host': sso_fqdn}, verify=False)  # required for SNI since Python 2.7.9 / 3.4
+			data = res.content
+		except requests.exceptions.ConnectionError as exc:
+			yield Critical(
+				description=_("Failed to load certificate {{{link}}}: {exc}").format(link=link, exc=exc),
+				links=[links],
+			)
+			continue
 
-		except requests.exceptions.ConnectionError:
-			print('error, connecting')
-			pass
+		try:
+			certificate = x509.load_pem_x509_certificate(data, backend)
+			MODULE.process("Looking for certificate %s" % (certificate.subject,))
+		except ValueError as exc:
+			yield Critical(
+				description=_("Failed to load certificate {{{link}}}: {exc}").format(link=link, exc=exc),
+				links=[links],
+			)
+			continue
+
+		# compare this with /usr/share/univention-management-console/saml/idp/*.xml
+		for idp in glob.glob(XML):
+			try:
+				tree = parse(idp)
+			except OSError as exc:
+				yield Critical(
+					description=_("Failed to load certificate {cert!r}: {exc}").format(cert=idp, exc=exc),
+				)
+				continue
+
+			root = tree.getroot()
+			nodes = root.findall(X509CERT)
+			if not nodes:
+				yield Critical(
+					description=_("Failed to find any certificate in {cert!r}").format(cert=idp),
+				)
+				continue
+
+			for node in nodes:  # FIXME: currently only KeyDescriptor/@use="signing" relevant
+				text = node.text
+				der = b64decode(text)
+				try:
+					cert = x509.load_der_x509_certificate(der, backend)
+					MODULE.process("Found certificate %s in %s" % (cert.subject, idp))
+				except ValueError as exc:
+					yield Critical(
+						description=_("Failed to load certificate {cert!r}: {exc}").format(cert=idp, exc=exc),
+					)
+					continue
+
+				if cert == certificate:
+					break
+			else:
+				yield Critical(
+					description=_("The SAML identity provider certificate {cert!r} is missing in {{{link}}}.").format(cert=idp, link=link),
+					links=[links],
+				)
 
 
-def test_service_provider_certificate():
-	# compare /etc/univention/ssl/$(hostname -f)/cert.pem with
-	# univention-ldapsearch -LLL "(&(serviceProviderMetadata=*)(univentionObjectType=saml/serviceprovider)(SAMLServiceProviderIdentifier=https://$(hostname -f)/univention/saml/metadata))" serviceProviderMetadata  | ldapsearch-wrapper | ldapsearch-decode64
-	# If it fails: /usr/share/univention-management-console/saml/update_metadata
-	#
-	# fails because https://help.univention.com/t/renewing-the-ssl-certificates/37 was not used. https://help.univention.com/t/renewing-the-complete-ssl-certificate-chain/36
+def fix_idp(umc: Instance) -> None:
+	MODULE.process("Re-running join-script 92univention-management-console-web-server")
+	call(["univention-run-join-scripts", "--force", "--run-scripts", "92univention-management-console-web-server"])
+	return run(umc, rerun=True)
+
+
+def test_service_provider_certificate() -> Iterator[Problem]:
+	"""
+	Check that local certificate :file:`/etc/univention/ssl/$FQHN/cert.pem` matches the certificate in LDAP
+	`(&(serviceProviderMetadata=*)(univentionObjectType=saml/serviceprovider)(SAMLServiceProviderIdentifier=https://$FQHN/univention/saml/metadata))`
+
+	Fix: ``/usr/share/univention-management-console/saml/update_metadata``
+	"""
+	backend = default_backend()
+
+	path = '/etc/univention/ssl/%(hostname)s.%(domainname)s/cert.pem' % ucr
+	MODULE.process("Checking certificates of %s" % (path,))
+	try:
+		with open(path, "rb") as fd:
+			data = fd.read()
+	except OSError as exc:
+		yield Critical(
+			description=_("Failed to load certificate {cert!r}: {exc}").format(cert=path, exc=exc),
+		)
+		return
+
+	try:
+		certificate = x509.load_pem_x509_certificate(data, backend)
+		MODULE.process("Looking for certificate %s" % (certificate.subject,))
+	except ValueError as exc:
+		yield Critical(
+			description=_("Failed to load certificate {cert!r}: {exc}").format(cert=path, exc=exc),
+		)
+		return
+
 	lo = univention.uldap.getMachineConnection()
-	certs = lo.search(filter_format('(&(serviceProviderMetadata=*)(univentionObjectType=saml/serviceprovider)(SAMLServiceProviderIdentifier=https://%s/univention/saml/metadata))', ['%s.%s' % (ucr.get('hostname'), ucr.get('domainname'))]), attr=['serviceProviderMetadata'])
-	MODULE.process("Checking certificates of /etc/univention/ssl/%s.%s/cert.pem" % (ucr.get('hostname'), ucr.get('domainname')))
-	with open('/etc/univention/ssl/%s.%s/cert.pem' % (ucr.get('hostname'), ucr.get('domainname'))) as fd:
-		for cert in certs:
-			cert = find_node(fromstring(cert[1]['serviceProviderMetadata'][0].decode('UTF-8')), '{http://www.w3.org/2000/09/xmldsig#}X509Certificate')
-			if cert.text.strip() not in fd.read():
-				MODULE.error('The certificate of the SAML service provider does not match.')
-				raise Critical(_('The certificate of the SAML service provider does not match.'))
+	url = "https://%(hostname)s.%(domainname)s/univention/saml/metadata" % ucr
+	search = filter_format("(&(serviceProviderMetadata=*)(univentionObjectType=saml/serviceprovider)(SAMLServiceProviderIdentifier=%s))", [url])
+	certs = lo.search(search, attr=['serviceProviderMetadata'])
+	for dn, attrs in certs:
+		link, umcm = dns_link(dn)
+		xml = attrs['serviceProviderMetadata'][0].decode('UTF-8')
+		root = fromstring(xml)
+		nodes = root.findall(X509CERT)
+		if not nodes:
+			yield Critical(
+				description=_("Failed to find any certificate in {{{link}}}").format(link=link),
+				umc_modules=[umcm],
+			)
+			continue
+
+		for node in nodes:
+			text = node.text
+			der = b64decode(text)
+			try:
+				cert = x509.load_der_x509_certificate(der, backend)
+				MODULE.process("Found certificate %s in %s" % (cert.subject, dn))
+			except ValueError as exc:
+				yield Critical(
+					description=_("Failed to load certificate {{{link}}}: {exc}").format(link=link, exc=exc),
+					umc_modules=[umcm],
+				)
+				continue
+
+			if cert == certificate:
+				break
+		else:
+			yield Critical(
+				description=_("The SAML identity provider certificate {{{link}}} does not match the local certificate {loc!r}.").format(link=link, loc=path),
+				umc_modules=[umcm],
+			)
+
+
+def dns_link(dn: str) -> Tuple[str, Dict[str, Any]]:
+	"""Create UMC UDM link for DN."""
+	link = "udm:saml/serviceprovider"
+	umcm: Dict[str, Any] = {
+		"module": "udm",
+		"flavor": "saml/serviceprovider",
+		"props": {
+			"openObject": {
+				"objectDN": dn,
+				"objectType": "saml/serviceprovider",
+			},
+		},
+	}
+	return (link, umcm)
+
+
+def fix_sp(umc: Instance) -> None:
+	MODULE.process("Re-running update_metadata")
+	call(["/usr/share/univention-management-console/saml/update_metadata"])
+	return run(umc, rerun=True)
+
+
+actions: Dict[str, Callable[[Instance], None]] = {
+	"fix_idp": fix_idp,
+	"fix_sp": fix_sp,
+}
 
 
 if __name__ == '__main__':
