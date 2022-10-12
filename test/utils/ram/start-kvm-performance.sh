a76125991561f2109bd927b082ff94cfc968bcf6
Felix Botner <botner@univention.de>
2022-10-12 17:26:30 +0200

    Issue #1284: add RAM performance env
    univention/ucs#1284

---
+++ test/utils/ram/start-kvm-performance.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export KVM_BUILD_SERVER="${KVM_BUILD_SERVER:=tross.knut.univention.de}"
+export HALT=false
+export DOCKER=true
+export REPLACE=true
+export UCS_TEST_RUN=false
+
+# user specific instances "username_..."
+export KVM_OWNER="$BUILD_USER_ID"
+
+exec ./utils/start-test.sh scenarios/autotest-248-ram-rankine-performance.cfg
