univention/univention-corporate-server

commit 5d3da2a0e701a082ef12072e186cd5ce5fce696d

if primaryGroup does not exist, set primaryGroup to defaultGroup (Bug #12621)
git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/ucs@7663 1b283449-9f2f-0410-b571-8eb228e1a901

management/univention-directory-manager-modules/modules/univention/admin/handlers/users/user.py
@@ -1533,9 +1533,20 @@ class object( univention.admin.handlers.simpleLdap, mungeddial.Support ):
 						if primaryGroupResult:
 							self['primaryGroup']=primaryGroupResult[0]
 						else:
-							self['primaryGroup']=None
+							result=None
+							primaryGroup=None
+							try:
+								result=self.lo.search( filter='(objectClass=univentionDefault)', base='cn=univention,'+self.position.getDomain(), attr=['univentionDefaultGroup'])
+							except:
+								pass
+							if result and result[0] and result[0][1] and result[0][1].has_key("univentionDefaultGroup"):
+								univention.debug.debug(univention.debug.ADMIN, univention.debug.INFO, 'user: could not find primaryGroup, setting primaryGroup to univentionDefaultGroup')
+								primaryGroup=result[0][1]["univentionDefaultGroup"][0]
+
+							self['primaryGroup']=primaryGroup
+							self.newPrimaryGroupDn=primaryGroup
+							self.__primary_group()
 							self.save()
-							raise univention.admin.uexceptions.primaryGroup
 					else:
 						self['primaryGroup']=None
 						self.save()

commit cd4333da2714d12bb3af639a0710916bcf5bc628

Bug #42080: don't modify primary group for users/user during open()
* the primary group might not be replicated yet
* the user might have no permission to read his primary group

If we don't find a primary group for the gidNumber opening a UDM module should just be possible, while `primaryGroup` is unset then.
As `primaryGroup` is required we can set it to the default value before modify() so that the old behavior is delayed until a modification:
it still raises `permissionDenied` and prevents modification of users if permission or replication problems exists.
`permissionDenied` is of course then not raised for administrator accounts with write access to e.g. the default primary group "Domain Users" and in that case the primary group would still be changed/reset.
This way the change is at least not so invasive as an complete API change would be.

Fixes: Bug #12621
Fixes: 5d3da2a0e701a082ef12072e186cd5ce5fce696d
Fixes: 372ce7317e507ed05dcebce0c7736746428ed7b5

doc/errata/staging/univention-directory-manager-modules.yaml
@@ -8,4 +8,9 @@ desc: |
  This update addresses the following issues:
  * It is now possible to create extended attributes for LDAP operational
    attributes.
-bug: [20235]
+ * The `primaryGroup` of `users/user` was unexpectedly reset to the default
+   primary group when the primary group could not be read in LDAP. This was
+   the case when the LDAP replication was not yet done or when the user had
+   no permission to read it. The behaior is now postponed to actual
+   modifications of the object.
+bug: [20235, 42080]

management/univention-directory-manager-modules/modules/univention/admin/handlers/users/user.py
@@ -1167,59 +1167,41 @@ class object(univention.admin.handlers.simpleLdap, PKIIntegration):
 			self._unmap_automount_information()
 			self._unmapUnlockTime()
 			self._load_groups(loadGroups)
-		self.save()
-		if not self.exists():  # TODO: move this block into _ldap_pre_create!
+			self._unmap_gid_number()
+		else:
 			self._set_default_group()
+		self.save()
 
 	def _load_groups(self, loadGroups):
-		if self.exists():
-			if loadGroups:  # this is optional because it can take much time on larger installations, default is true
-				self['groups'] = [x.decode('UTF-8') if six.PY2 else x for x in self.lo.searchDn(filter=filter_format(u'(&(|(objectClass=univentionGroup)(objectClass=sambaGroupMapping))(uniqueMember=%s))', [self.dn]))]
-			else:
-				ud.debug(ud.ADMIN, ud.INFO, 'user: open with loadGroups=false for user %s' % self['username'])
-			self.groupsLoaded = loadGroups
-			primaryGroupNumber = self.oldattr.get('gidNumber', [b''])[0].decode('ASCII')
-			if primaryGroupNumber:
-				primary_group = get_primary_group_dn(self.lo, primaryGroupNumber)
-				if primary_group:
-					self['primaryGroup'] = primary_group
-				else:
-					try:
-						primaryGroup = self.lo.search(filter='(objectClass=univentionDefault)', base='cn=univention,' + self.position.getDomain(), attr=['univentionDefaultGroup'])
-						try:
-							primaryGroup = primaryGroup[0][1]["univentionDefaultGroup"][0].decode('UTF-8')
-						except Exception:
-							primaryGroup = None
-					except Exception:
-						primaryGroup = None
+		if loadGroups:  # this is optional because it can take much time on larger installations, default is true
+			self['groups'] = [x.decode('UTF-8') if six.PY2 else x for x in self.lo.searchDn(filter=filter_format(u'(&(|(objectClass=univentionGroup)(objectClass=sambaGroupMapping))(uniqueMember=%s))', [self.dn]))]
+		else:  # TODO: document where it is needed and used
+			ud.debug(ud.ADMIN, ud.INFO, 'user: open with loadGroups=false for user %s' % (self['username'],))
+		self.groupsLoaded = loadGroups
 
-					ud.debug(ud.ADMIN, ud.INFO, 'user: could not find primaryGroup, setting primaryGroup to %s' % primaryGroup)
+	def _unmap_gid_number(self):
+		primaryGroupNumber = self.oldattr.get('gidNumber', [b''])[0].decode('ASCII')
+		if not primaryGroupNumber:
+			self.info['primaryGroup'] = None
+			self.save()
+			raise univention.admin.uexceptions.primaryGroup(self.dn)
 
-					if not primaryGroup:
-						raise univention.admin.uexceptions.primaryGroup(self.dn)
-					self.info['primaryGroup'] = primaryGroup
-					self.__primary_group()
-			else:
-				self.info['primaryGroup'] = None
-				self.save()
-				raise univention.admin.uexceptions.primaryGroup(self.dn)
+		primary_group = get_primary_group_dn(self.lo, primaryGroupNumber)
+		if primary_group:
+			self['primaryGroup'] = primary_group
+
+		ud.debug(ud.ADMIN, ud.ERROR, 'No primary group was found with gidNumber=%s for %s as %s' % (primaryGroupNumber, self.dn, self.lo.binddn))
 
 	def _set_default_group(self):
-		primary_group_from_template = self['primaryGroup']
-		if not primary_group_from_template:
-			searchResult = self.lo.search(filter=u'(objectClass=univentionDefault)', base=u'cn=univention,' + self.position.getDomain(), attr=['univentionDefaultGroup'])
-			if not searchResult or not searchResult[0][1]:
-				self.info['primaryGroup'] = None
-				self.save()
-				raise univention.admin.uexceptions.primaryGroup(self.dn)
+		if not self['primaryGroup']:
+			for _dn, attrs in self.lo.search(filter='(objectClass=univentionDefault)', base=u'cn=univention,' + self.position.getDomain(), attr=['univentionDefaultGroup']):
+				primary_group = attrs['univentionDefaultGroup'][0].decode('UTF-8')
+				ud.debug(ud.ADMIN, ud.INFO, 'user: setting primaryGroup to %s' % (primary_group,))
+				if self.lo.get(primary_group):
+					self['primaryGroup'] = primary_group
 
-			for tmp, number in searchResult:
-				primaryGroupResult = self.lo.searchDn(filter=filter_format(u'(&(objectClass=posixGroup)(cn=%s))', (univention.admin.uldap.explodeDn(number['univentionDefaultGroup'][0].decode('UTF-8'), 1)[0],)), base=self.position.getDomain(), scope='domain')
-				if primaryGroupResult:
-					self['primaryGroup'] = primaryGroupResult[0]
-					# self.save() must not be called after this point in self.open()
-					# otherwise self.__primary_group doesn't add a new user to the
-					# univentionDefaultGroup because "not self.hasChanged('primaryGroup')"
+		if not self['primaryGroup']:
+			raise univention.admin.uexceptions.primaryGroup(self.dn)
 
 	def _unmap_pwd_change_next_login(self):
 		self['pwdChangeNextLogin'] = '0'
@@ -1465,6 +1447,7 @@ class object(univention.admin.handlers.simpleLdap, PKIIntegration):
 			self.info['password'] = self.oldinfo['password'] = u'*'
 			self.info['disabled'] = self.oldinfo['disabled']
 
+		self._set_default_group()
 		if not self.exists() or self.hasChanged('primaryGroup'):
 			# Ensure the primary Group has the samba option enabled
 			if self['primaryGroup'] and not self.lo.getAttr(self['primaryGroup'], 'sambaSID'):