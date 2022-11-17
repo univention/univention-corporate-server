f733a0b6b51b3407d81e7da30e6e991f52377cd2
Felix Botner <botner@univention.de>
2022-11-17 14:29:03 +0100

    Bug #55184: added school performance env job

---
+++ test/utils/start-ucs-school-performance-env1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export KVM_BUILD_SERVER="${KVM_BUILD_SERVER:=tross.knut.univention.de}"
+export HALT=false
+export DOCKER=true
+export REPLACE=true
+export UCS_TEST_RUN=false
+
+# user specific instances "username_..."
+export KVM_OWNER="${BUILD_USER_ID:=$USER}"
+export JOB_BASE_NAME="${JOB_BASE_NAME:=ucs-school-performance-env1}"
+
+exec ./utils/start-test.sh scenarios/base/ucs-school-performance-env1.cfg
