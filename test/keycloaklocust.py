9e3eca86ed3a7af6e98f8db78371a60adcfc9119
Julia Bremer <bremer@univention.de>
2022-11-29 09:23:07 +0100

    add performance job

--- test/keycloaklocust.py
+++ test/keycloaklocust.py
@@ -2,9 +2,10 @@
 
 from html.parser import HTMLParser
 
+import random
 from bs4 import BeautifulSoup
 import univention.testing.utils as utils
-from locust import HttpUser, task, between
+from locust import HttpUser, task, constant_throughput
 
 html = HTMLParser()
 
@@ -12,19 +13,21 @@ account = utils.UCSTestDomainAdminCredentials()
 
 WAIT_MIN = 1
 WAIT_MAX = 1
+hosts = ["https://master.ucs.test", "https://backup.ucs.test"]
+#hosts = ["https://backup.ucs.test"]
 
 
-def logout_at_idp(self, client):
-	"""Logout from session"""
-	with client.get("/univention/logout", allow_redirects=True, timeout=10, catch_response=True) as req3:
+def logout_at_idp(client, host):
+	logout = "/univention/logout"
+	uri = host + logout
+	with client.get(uri, name="/univention/logout/", allow_redirects=True, timeout=10, catch_response=True) as req3:
 		if not (200 <= req3.status_code <= 399):
 			return None
 
 
-def login_at_idp_with_credentials(self, client):
-	"""Send form with login data"""
+def login_at_idp_with_credentials(client, login_link):
 	data = {'username': "Administrator", 'password': "univention"}
-	with client.post(self.login_link, name="/realms/ucs/login-actions/authenticate", allow_redirects=True, timeout=10, catch_response=True, data=data) as req2:
+	with client.post(login_link, name="/realms/ucs/login-actions/authenticate", allow_redirects=True, timeout=10, catch_response=True, data=data) as req2:
 		soup = BeautifulSoup(req2.content, features="lxml")
 		saml_response = soup.find("input", {"name": "SAMLResponse"}).get("value")
 		if not saml_response:
@@ -33,28 +36,26 @@ def login_at_idp_with_credentials(self, client):
 			return None
 
 
-def entry(self, client):
-	"""Use Identity Provider to log in to a Service Provider.
-	The IdP doesn't know the session and has to ask for username and password"""
-
-	# Open login prompt. Redirects to IdP. IdP answers with login prompt
+def entry(client, host):
 	entry = "/univention/saml/"
-	with client.get(entry, allow_redirects=True, timeout=10, catch_response=True) as req1:
+	uri = host + entry
+	with client.get(uri, name="/univention/saml/", allow_redirects=True, timeout=10, catch_response=True) as req1:
 		if not (200 <= req1.status_code <= 399):
 			return None
 		if req1.content is None or len(req1.content) == 0:
 			return None
 	soup = BeautifulSoup(req1.content, features="lxml")
 	login_link = soup.find("form", {"id": "kc-form-login"}).get("action")
-	self.login_link = html.unescape(login_link)
-	self.login_at_idp_with_credentials(client)
-	self.logout_at_idp(client)
+	login_link = html.unescape(login_link)
+	login_at_idp_with_credentials(client, login_link)
+	logout_at_idp(client, host)
 	client.cookies.clear()
 
 
 class QuickstartUser(HttpUser):
-	wait_time = between(WAIT_MIN, WAIT_MAX)
+	wait_time = constant_throughput(0.1)
 
 	@task
 	def get_samlSession(self):
-		entry(self.client)
+		host = random.choice(hosts)
+		entry(self.client, host)
