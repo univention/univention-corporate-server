f6f9b968892762cbfe8de9854b4bbeebe7c9c8f2
Florian Best <best@univention.de>
2022-12-15 10:20:07 +0100

    test: use lowercased LDAP base
    The LDAP base is lowercased in the response.
    
    Bug #50249

--- test/ucs-test/tests/73_udm-rest/00_general_tests.py
+++ test/ucs-test/tests/73_udm-rest/00_general_tests.py
@@ -153,7 +153,7 @@ def test_error_handling(udm, ldap_base):
 	assert sorted(exc.value.error_details['error'], key=itemgetter('location')) == sorted([
 		{'location': ['query', 'scope'], 'message': "Value has to be one of ['sub', 'one', 'base', 'base+one']", 'type': 'value_error'},
 		{'location': ['query', 'filter'], 'message': 'Not a valid LDAP search filter', 'type': 'value_error'},
-		{'location': ['query', 'position'], 'message': f'The ldap base is invalid. Use {ldap_base}.', 'type': 'value_error'},
+		{'location': ['query', 'position'], 'message': f'The ldap base is invalid. Use {ldap_base.lower()}.', 'type': 'value_error'},
 	], key=itemgetter('location'))
 
 	# not existing search base underneath of the real LDAP base
