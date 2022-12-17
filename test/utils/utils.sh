398ff65a8c4a332f274a4516608c3d0e6bcc3990
Florian Best <best@univention.de>
2022-12-17 12:39:05 +0100

    Bug #54481: allow to specify a git-scope in add_extra_apt_scope

--- test/utils/utils.sh
+++ test/utils/utils.sh
@@ -1211,8 +1211,13 @@ ucs-winrm () {
 
 add_extra_apt_scope () {
 	if [ -n "$SCOPE" ]; then
-		echo "deb [trusted=yes] http://192.168.0.10/build2/ ucs_$(ucr get version/version)-0-$SCOPE/all/" > /etc/apt/sources.list.d/99_extra_scope.list
-		echo "deb [trusted=yes] http://192.168.0.10/build2/ ucs_$(ucr get version/version)-0-$SCOPE/\$(ARCH)/" >> /etc/apt/sources.list.d/99_extra_scope.list
+		if [ "$(echo "$SCOPE" | cut -c1-5)" = "http:" ] || [ "$(echo "$SCOPE" | cut -c1-6)" = "https:" ]; then
+			# support: deb [trusted=yes] http://192.168.0.10/build2/git/fbest-12345-foo/ git main
+			echo "deb [trusted=yes] $SCOPE" > /etc/apt/sources.list.d/99_extra_scope.list
+		else
+			echo "deb [trusted=yes] http://192.168.0.10/build2/ ucs_$(ucr get version/version)-0-$SCOPE/all/" > /etc/apt/sources.list.d/99_extra_scope.list
+			echo "deb [trusted=yes] http://192.168.0.10/build2/ ucs_$(ucr get version/version)-0-$SCOPE/\$(ARCH)/" >> /etc/apt/sources.list.d/99_extra_scope.list
+		fi
 		apt-get update -q || true  # ignore failure, univention-upgrade will do this as well
 	fi
 }
