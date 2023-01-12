a68cf87642ea4e0e3a2cfb0be327ce9bad657e86
Philipp Hahn <hahn@univention.de>
2023-01-12 17:53:12 +0100

    fix: proxy/https defaulting
    to proxy/http unless explicitly set to the empty string.
    
    A http-proxy must implement GET and HEAD
    A https-proxy must implement CONNECT instead, which proxy.py does not
    do.
    
    Instead of implementing the fallback for https to http in many locations
    set it in the default layer of UCR, which allows to disable proxy/https
    by explicitly setting it to the empty string.
    
    Bug #55561

--- base/univention-updater/modules/univention/updater/tools.py
+++ base/univention-updater/modules/univention/updater/tools.py
@@ -1265,14 +1265,15 @@ class UniventionUpdater(object):
         self.is_repository_server = self.configRegistry.is_true('local/repository', False)
 
         reinitUCSHttpServer = False
-        if 'proxy/http' in self.configRegistry and self.configRegistry['proxy/http']:
-            os.environ['http_proxy'] = self.configRegistry['proxy/http']
-            os.environ['https_proxy'] = self.configRegistry['proxy/http']
+        proxy = self.configRegistry.get('proxy/http')
+        if proxy:
+            os.environ['http_proxy'] = os.environ['https_proxy'] = proxy
             reinitUCSHttpServer = True
-        if 'proxy/https' in self.configRegistry and self.configRegistry['proxy/https']:
-            os.environ['https_proxy'] = self.configRegistry['proxy/https']
+        proxy = self.configRegistry.get('proxy/https')
+        if proxy:
+            os.environ['https_proxy'] = proxy
             reinitUCSHttpServer = True
-        if 'proxy/no_proxy' in self.configRegistry and self.configRegistry['proxy/no_proxy']:
+        if self.configRegistry.get('proxy/no_proxy'):
             os.environ['no_proxy'] = self.configRegistry['proxy/no_proxy']
             reinitUCSHttpServer = True
         if reinitUCSHttpServer:
