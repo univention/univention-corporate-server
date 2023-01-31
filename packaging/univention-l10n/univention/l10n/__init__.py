dadc54fcfcb9174d407df04f68270d3eef1cd481
Florian Best <best@univention.de>
2023-01-31 20:36:12 +0100

    style: manually fix F401,F403
    Bug #31771

--- packaging/univention-l10n/univention/l10n/__init__.py
+++ packaging/univention-l10n/univention/l10n/__init__.py
@@ -31,4 +31,12 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
-from . l10n import *  # noqa: F401,F403
+from .l10n import (
+    DIR_BLACKLIST, MODULE_BLACKLIST, REFERENCE_LANG, UMC_MODULES, Error, MIMEChecker, NoMatchingFiles,
+    NoSpecialCaseDefintionsFound, SpecialCase, UMCModuleTranslation, create_new_package,
+    find_base_translation_modules, get_special_cases_from_checkout, get_special_cases_from_srcpkg,
+    message_catalogs, read_special_case_definition, sourcefileprocessing, template_file,
+    translate_special_case, update_package_translation_files, write_makefile,
+)
+
+__all__ = ['DIR_BLACKLIST', 'Error', 'MIMEChecker', 'MODULE_BLACKLIST', 'NoMatchingFiles', 'NoSpecialCaseDefintionsFound', 'REFERENCE_LANG', 'SpecialCase', 'UMCModuleTranslation', 'UMC_MODULES', 'create_new_package', 'find_base_translation_modules', 'get_special_cases_from_checkout', 'get_special_cases_from_srcpkg', 'message_catalogs', 'read_special_case_definition', 'sourcefileprocessing', 'template_file', 'translate_special_case', 'update_package_translation_files', 'write_makefile']
