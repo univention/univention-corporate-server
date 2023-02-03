univention/univention-corporate-server

commit 2fa3e199686919a7f2cfa404a4c37f69b3dc40ce

style: fix PLR0402
ruff #2199
import sys
import subprocess

lines = [line.split(':') for line in subprocess.check_output(['ruff', '-e', '--select', 'PLR0402', *sys.argv[1:]]).decode().splitlines()]
lines = [line for line in lines if len(line) == 4]
for filename, lineo, row, rest in lines:
    replacement = rest.split('`')[1]
    module = replacement.split(' ')[-1]
    package = replacement.split(' ')[1]
    print(filename, lineo, replacement, module, package)
    subprocess.call(['sed', '-i', f's/import {package}.{module} as {module}/{replacement}/g', filename])

plus `isort`
Bug #31771

management/univention-ldap/listener/ldap_extension.py
@@ -42,7 +42,7 @@ from typing import Dict, List
 
 import listener
 import univention.debug as ud
-import univention.lib.ldap_extension as ldap_extension
+from univention.lib.ldap_extension import ldap_extension
 
 name = 'ldap_extension'
 description = 'Configure LDAP schema and ACL extensions'

commit 64d9e9ebf432896a42628a26446424ce0a104ab9

fix: fix import statement
LISTENER    ( ERROR   ) : import of filename=/usr/lib/univention-directory-listener/system/ldap_extension.py failed
Traceback (most recent call last):
  File "/usr/lib/univention-directory-listener/system/ldap_extension.py", line 44, in <module>
    from univention.lib.ldap_extension import ldap_extension
ImportError: cannot import name 'ldap_extension' from 'univention.lib.ldap_extension' (/usr/lib/python3/dist-packages/univention/lib/ldap_extension.py)

Fixes: 2fa3e199686919a7f2cfa404a4c37f69b3dc40ce

Bug #31771

management/univention-ldap/listener/ldap_extension.py
@@ -41,7 +41,7 @@ import subprocess
 from typing import Dict, List
 
 import univention.debug as ud
-from univention.lib.ldap_extension import ldap_extension
+from univention.lib import ldap_extension
 
 import listener
 