5a5d78dac1fc058cb5d41b4f11358841e5a497d8
Philipp Hahn <hahn@univention.de>
2023-02-04 08:12:52 +0100

    test(spamassassin): fix invalid SSL path
    Bug #53751
    Fixes: 622a990a40d952051c9ca3d4c46598c1ac2e8b4b

--- test/utils/utils.sh
+++ test/utils/utils.sh
@@ -1218,8 +1218,9 @@ run_workarounds_before_starting_the_tests () {
 sa_bug53751 () {
 	# https://forge.univention.org/bugzilla/show_bug.cgi?id=47030
 	# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=922499
-	[ -f /etc/ca-certificates.conf/etc/ca-certificates.conf ] &&
-		sed -i -e 's=^mozilla/DST_Root_CA_X3.crt=!&=' /etc/ca-certificates.conf &&
+	local cfg='/etc/ca-certificates.conf'
+	[ -f "$cfg" ] &&
+		sed -i -e 's=^mozilla/DST_Root_CA_X3.crt=!&=' "$cfg" &&
 		update-ca-certificates ||
 		true
 
