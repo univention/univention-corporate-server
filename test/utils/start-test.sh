80a4d3e27769c324841eca1526f951efcdc6de08
Philipp Hahn <hahn@univention.de>
2023-02-08 18:31:47 +0100

    churn(ec2): Use 5.0-3 AMI
    Bug #55649

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -7,8 +7,8 @@
 release='5.0-3'
 old_release='4.4-9'
 kvm_template_version='5.0-3+e572'
-# AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 3
-current_ami=ami-0536dfa9892ba1f17
+# AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 4
+current_ami=ami-051aaf4d1cd9dedfb
 # AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 11
 old_ami=ami-02ad9aab36aadf18a
 
