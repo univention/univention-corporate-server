4686031e98804c82f86b967f91d0c44a10d92552
Felix Botner <botner@univention.de>
2023-02-13 09:36:38 +0100

    Issue #1456: added keycloak env

---
+++ test/utils/start-ucs-keycloak.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export KVM_BUILD_SERVER="${KVM_BUILD_SERVER:=tross.knut.univention.de}"
+export HALT=false
+export DOCKER=true
+export REPLACE=true
+export UCS_TEST_RUN=false
+
+# user specific instances "username_..."
+export KVM_OWNER="${BUILD_USER_ID:=$USER}"
+export JOB_BASE_NAME="${JOB_BASE_NAME:=ucs-keycloak}"
+
+exec ./utils/start-test.sh scenarios/keycloak/keycloak-env.cfg
