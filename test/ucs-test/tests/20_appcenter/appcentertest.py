ef6f142d2e4877951c587e950bb4eeb4bc7905d0
Florian Best <best@univention.de>
2023-02-24 10:42:52 +0100

    fix(debian/compat): upgrade to latest debhelper compat #1481
    WIP: TODO: check for the real changes
    
    squash! fix(debian/compat): upgrade to latest debhelper compat #1481
    Bug #53127

--- test/ucs-test/tests/20_appcenter/appcentertest.py
+++ test/ucs-test/tests/20_appcenter/appcentertest.py
@@ -259,7 +259,7 @@ Source: {package_name}
 Section: univention
 Priority: optional
 Maintainer: Univention GmbH <packages@univention.de>
-Build-Depends: debhelper-compat (=12)
+Build-Depends: debhelper-compat (=13)
 Standards-Version: 3.5.2
 
 Package: {package_name}
