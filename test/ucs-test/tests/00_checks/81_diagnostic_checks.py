257fad933bcb20e0d1072663f3c7e652ccc8b163
Philipp Hahn <hahn@univention.de>
2023-03-01 11:50:16 +0100

    fix(test): Skip notifier/protocol/version diagnostics
    for old upgrade tests.
    
    Bug #54264

--- test/ucs-test/tests/00_checks/81_diagnostic_checks.py
+++ test/ucs-test/tests/00_checks/81_diagnostic_checks.py
@@ -7,22 +7,41 @@ from __future__ import print_function
 
 import subprocess
 import tempfile
+from typing import Set
+
+from univention.config_registry import ConfigRegistry
 from univention.testing.umc import Client
 from univention.testing import utils
 
 # One would neeed a strong argument to skip any tests here, as it masks reals problems (See bug #50021)
-SKIPPED_TESTS = []
+ucr = ConfigRegistry()
+ucr.load()
+PREFIX = "diagnostic/check/disable/"
+SKIPPED_TESTS = {
+	k[len(PREFIX):]
+	for k, v in ucr.items()
+	if k.startswith(PREFIX) and ucr.is_true(value=v)
+}  # type: Set[str]
 
 
 def test_run_diagnostic_checks():
 	client = Client.get_test_connection()
-	plugins = [plugin['id'] for plugin in client.umc_command('diagnostic/query').result if plugin['id'] not in SKIPPED_TESTS]
+	plugins = [
+		plugin['id']
+		for plugin in client.umc_command('diagnostic/query').result
+		if plugin['id'] not in SKIPPED_TESTS
+	]
 
 	account = utils.UCSTestDomainAdminCredentials()
 	with tempfile.NamedTemporaryFile() as fd:
 		fd.write(account.bindpw)
 		fd.flush()
-		args = ['/usr/bin/univention-run-diagnostic-checks', '--username', account.username, '--bindpwdfile', fd.name, '-t'] + plugins
+		args = [
+			'/usr/bin/univention-run-diagnostic-checks',
+			'--username', account.username,
+			'--bindpwdfile', fd.name,
+			'-t',
+		] + plugins
 		print(args)
 		returncode = subprocess.call(args)
 		assert 0 == returncode, 'Exit code != 0: %d' % (returncode,)
