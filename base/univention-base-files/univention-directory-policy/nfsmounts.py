univention/univention-corporate-server

commit 919380fd7b9d752164e7ebdad89e89684908feb9

fix(base/nfsmount): Forbid overlapping paths
Not same path (only on server itself):
    s: /mnt
    d: /mnt
No over-mounting of source path
    s: /mnt/sub
    d: /mnt
No recursive mounting:
    s: /mnt
    d: /mnt/sub
No symbolic link trickery:
    s: /mnt/{src -> dst}
    d: /mnt/dst
No trailing slash:
    s: /mnt/
    d: /mnt

    s: /mnt
    d: /mnt/
Allow different paths:
    s: /mnt/src
    d: /mnt/dst

    s: /mnt/dir
    d: /mnt/dir2

Bug #50193

base/univention-base-files/univention-directory-policy/nfsmounts.py
@@ -39,6 +39,7 @@ import argparse
 import os
 import subprocess
 import sys
+from os.path import abspath, sep
 from typing import IO, List, Optional, Set, Tuple
 
 import ldap
@@ -171,7 +172,7 @@ def get_nfs_data(nfs_mount: str, entries: List[fstab.Entry]) -> Optional[Tuple[s
 
     # skip share if to self
     fqdn = "%(hostname)s.%(domainname)s" % ucr
-    if share_host == fqdn and share_path == mp:
+    if share_host == fqdn and not overlap(share_path, mp):
         debug('is self, skipping\n')
         return None
 
@@ -200,6 +201,26 @@ def get_nfs_data(nfs_mount: str, entries: List[fstab.Entry]) -> Optional[Tuple[s
         return None
 
     return dn, nfs_path_ip, mp
+
+
+def overlap(path_a: str, path_b: str) -> bool:
+    """
+    Check if two paths overlap.
+
+    >>> overlap("/mnt/a", "/mnt/b")
+    False
+    >>> overlap("/mnt/a", "/mnt/aa")
+    False
+    >>> overlap("/mnt/a", "/mnt")
+    True
+    """
+    return all(
+        a == b
+        for a, b in zip(
+            abspath(path_a).split(sep),
+            abspath(path_b).split(sep),
+        )
+    )
 
 
 def mount(to_mount: Set[str]) -> None:

commit f131de469ceea5e2ff392b17c8f16e42892e3a80

fix(base/nfsmount): Invert overlap logic
SKIP mounting from self IF src and dst OVERLAP:

Bug #50193
Fixes: 919380fd7b9d752164e7ebdad89e89684908feb9

base/univention-base-files/univention-directory-policy/nfsmounts.py
@@ -172,8 +172,8 @@ def get_nfs_data(nfs_mount: str, entries: List[fstab.Entry]) -> Optional[Tuple[s
 
     # skip share if to self
     fqdn = "%(hostname)s.%(domainname)s" % ucr
-    if share_host == fqdn and not overlap(share_path, mp):
-        debug('is self, skipping\n')
+    if share_host == fqdn and overlap(share_path, mp):
+        debug('is self and %s overlaps %s, skipping\n' % (share_path, mp))
         return None
 
     nfs_path_fqdn = "%s:%s" % (share_host, share_path)

doc/errata/staging/univention-base-files.yaml
@@ -3,7 +3,7 @@ release: '5.0'
 version: [3]
 scope: ucs_5.0-0-errata5.0-3
 src: univention-base-files
-fix: 9.0.5-3A~5.0.0.202303081504
+fix: 9.0.5-4
 desc: |
  This update addresses the following issue:
  * Allow NFS shares to be mounted on exporting host itself to prevent