univention/univention-corporate-server

commit 5447d3e981aba9ff0f8f0f53ab4f3129e2c0c491

refactor(base): Move code and update descriptions
Bug #50193

base/univention-base-files/univention-directory-policy/nfsmounts.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
-# Univention Configuration Registry
-#  add and remove nfs shares from the LDAP directory to /etc/fstab
-#
 # Like what you see? Join us!
 # https://www.univention.com/about-us/careers/vacancies/
 #
@@ -33,6 +30,8 @@
 # License with the Debian GNU/Linux or Univention distribution in file
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
+
+"Add/remove NFS mount points from LDAP to/from /etc/fstab and mount them"
 
 from __future__ import print_function
 
@@ -85,8 +84,7 @@ def query_policy(host_dn: str, server: Optional[str] = None, password_file: str 
 
 def main() -> None:
     # parse command line
-    description = "Add NFS mount points from LDAP to /etc/fstab and mount them"
-    parser = argparse.ArgumentParser(description=description)
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--dn', default=ucr['ldap/hostdn'], help=argparse.SUPPRESS)
     parser.add_argument('-s', '--simulate', action='store_true', help='simulate and show values to be set')
     parser.add_argument('-v', '--verbose', action='store_true', help='print verbose information')
@@ -141,10 +139,11 @@ def update_fstab(args: argparse.Namespace, simulate: bool) -> Set[str]:
 def get_nfs_data(nfs_mount: str, entries: List[fstab.Entry]) -> Optional[Tuple[str, str, str]]:
     fields = nfs_mount.split(' ')  # dn_univentionShareNFS mount_point
     dn = fields[0]
-    lo = univention.uldap.getMachineConnection()
     if not dn:
         debug('no dn, skipping\n')
         return None
+
+    lo = univention.uldap.getMachineConnection()
     # get univention share host and path for dn
     try:
         result = lo.lo.search_s(

base/univention-base-files/conffiles/autostart.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 #
-"""config registry module for autostart handling."""
-#
 # Like what you see? Join us!
 # https://www.univention.com/about-us/careers/vacancies/
 #
@@ -31,6 +29,8 @@
 # License with the Debian GNU/Linux or Univention distribution in file
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
+
+"""UCR module for autostart handling."""
 
 from logging import getLogger
 from subprocess import PIPE, Popen

base/univention-base-files/conffiles/interfaces.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 #
-# Univention Configuration Registry
-"""config registry module for the network interfaces."""
-#
 # Like what you see? Join us!
 # https://www.univention.com/about-us/careers/vacancies/
 #
@@ -32,6 +29,8 @@
 # License with the Debian GNU/Linux or Univention distribution in file
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
+
+"""config registry module for the network interfaces."""
 
 import re
 from subprocess import call

base/univention-base-files/conffiles/xkbKeyboard.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 #
-# Univention Configuration Registry
-#  config registry module  to trigger udev on changes in /etc/default/keyboard
-#
 # Like what you see? Join us!
 # https://www.univention.com/about-us/careers/vacancies/
 #
@@ -32,6 +29,8 @@
 # License with the Debian GNU/Linux or Univention distribution in file
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
+
+"""UCR module to trigger udev on changes in /etc/default/keyboard."""
 
 import os
 

base/univention-base-files/univention-directory-policy/univention-policy-update-config-registry.py
@@ -5,7 +5,6 @@
 # https://www.univention.com/about-us/careers/vacancies/
 #
 # Copyright 2007-2023 Univention GmbH
-"""Get UCR settings from LDAP policy."""
 #
 # https://www.univention.de/
 #
@@ -32,6 +31,8 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
+"""Apply LDAP UCR policy to local UCR."""
+
 from __future__ import print_function
 
 import argparse
@@ -55,9 +56,8 @@ def get_policy(host_dn, server=None, password_file="/etc/machine.secret", verbos
 
 
 def parse_cmdline() -> argparse.Namespace:
-    """Parse command line and return options and dn."""
-    description = "Set local UCR settings from LDAP policy."
-    parser = argparse.ArgumentParser(description=description)
+    """Parse command line and return options and DN."""
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-a', '--setall', action='store_true', help='write all variables set by policy')
     parser.add_argument('-s', '--simulate', action='store_true', help='simulate update and show values to be set')
     parser.add_argument('-v', '--verbose', action='store_true', help='print verbose information')
@@ -79,7 +79,7 @@ def parse_cmdline() -> argparse.Namespace:
 
 
 def main() -> None:
-    """Get UCR settings from LDAP policy."""
+    """Apply LDAP UCR policy to local UCR."""
     args = parse_cmdline()
 
     confregfn = os.path.join(confreg.ConfigRegistry.PREFIX, confreg.ConfigRegistry.BASES[confreg.ConfigRegistry.LDAP])

commit 7d79269b4e85df48758229928fb4340e6f174c78

style(base): fix docstring
Fixes: 5447d3e981aba9ff0f8f0f53ab4f3129e2c0c491
Bug #50193

base/univention-base-files/univention-directory-policy/nfsmounts.py
@@ -31,7 +31,7 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
-"Add/remove NFS mount points from LDAP to/from /etc/fstab and mount them"
+"""Add/remove NFS mount points from LDAP to/from /etc/fstab and mount them"""
 
 from __future__ import print_function
 