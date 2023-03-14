6b0e1de6e8c1c3e6ae0ac04a51235bd86058eade
Jannik Ahlers <ahlers@univention.de>
2023-03-14 10:17:53 +0100

    Issue #43375: add bsb test jenkins job
    Planio Issue #43375

---
+++ test/utils/ram/start-kvm-bsb-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export KVM_BUILD_SERVER="${KVM_BUILD_SERVER:=tross.knut.univention.de}"
+export HALT=false
+export DOCKER=true
+export REPLACE=true
+export UCS_TEST_RUN=false
+export UCS_TEST_APPCENTER="${UCS_TEST_APPCENTER:=true}"
+export BSB_PACKAGE_SCOPE="${BSB_PACKAGE_SCOPE:=fhh-bsb-iam-dev-intern}"
+
+# user specific instances "username_..."
+export KVM_OWNER="${BUILD_USER_ID:=$USER}"
+export JOB_BASE_NAME="${JOB_BASE_NAME:=bsb-test-env}"
+
+exec ./utils/start-test.sh scenarios/autotest-249-bsb-test.cfg
