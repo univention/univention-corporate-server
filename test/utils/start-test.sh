131cf96a06afe578ef45f7f81a6deadf53ab1b83
Florian Best <best@univention.de>
2023-03-21 15:21:16 +0100

    WIP ci(jenkins): UCS 5.1
    Bug #55715

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -4,13 +4,13 @@
 #
 
 # defaults for release
-release='5.0-5'
-old_release='4.4-9'
+release='5.1-0'
+old_release='5.0-5'
 kvm_template_version='5.0-5+e798'
 # AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 6
 current_ami=ami-0e72981c452ee9b85
-# AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 11
-old_ami=ami-02ad9aab36aadf18a
+# AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 6
+old_ami=ami-0e72981c452ee9b85
 
 # defaults
 kvm_template='generic-unsafe'
@@ -21,7 +21,7 @@ kvm_label_suffix=''
 exact_match='false'
 ucsschool_release='scope'
 shutdown='false'
-openstack_image_name='UCS 5.0-5+4'
+openstack_image_name='UCS 5.1-0'
 source_iso="/var/univention/buildsystem2/isotests/ucs_${release}-latest-amd64.iso"
 
 # some internal stuff
