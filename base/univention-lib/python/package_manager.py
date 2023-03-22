efef1c011e45d49389bddb2f6e57f408cf6ee532
Florian Best <best@univention.de>
2023-03-22 16:28:57 +0100

    Adapt package mangager to python-apt changes
    commit 324b2d49f16b307e7b365645591ebfd9ff71aba4
    Author: Julian Andres Klode <jak@debian.org>
    Date:   Mon Jun 24 13:49:55 2019 +0200
    
        Remove install_protect() methods
    
        They were dropped in apt 1.9, and do nothing.
    
    Traceback (most recent call last):
      File "/usr/lib/python3/dist-packages/univention/management/console/modules/setup/setup_script.py", line 323, in run
        success = self.inner_run()
      File "/usr/lib/univention-system-setup/scripts/05_role/10role", line 89, in inner_run
        success = self.install(wanted_server_role_package)
      File "/usr/lib/python3/dist-packages/univention/management/console/modules/setup/setup_script.py", line 437, in install
        return self.package_manager.install(*pkg_names)
      File "/usr/lib/python3/dist-packages/univention/lib/package_manager.py", line 1118, in install
        return self.commit(install=pkg_names)
      File "/usr/lib/python3/dist-packages/univention/lib/package_manager.py", line 904, in commit
        to_be_installed, to_be_removed, broken = self.mark(install, remove, dry_run=True)
      File "/usr/lib/python3/dist-packages/univention/lib/package_manager.py", line 809, in mark
        fixer.install_protect()
    AttributeError: 'ProblemResolver' object has no attribute 'install_protect'

--- base/univention-lib/python/package_manager.py
+++ base/univention-lib/python/package_manager.py
@@ -823,7 +823,6 @@ class PackageManager(object):
                 fixer.clear(pkg)
                 fixer.protect(pkg)
 
-        fixer.install_protect()  # type: ignore
         try:
             fixer.resolve()
         except SystemError as exc:
