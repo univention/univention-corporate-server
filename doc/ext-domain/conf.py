01662cb204ec3a032c77a6a2b0039647ebad05dd
Nico Gulden <gulden@univention.de>
2023-03-29 20:58:47 +0200

    Bug #55914: Annotate license header to source files for ext-domain

--- doc/ext-domain/conf.py
+++ doc/ext-domain/conf.py
@@ -1,3 +1,35 @@
+# Like what you see? Join us!
+# https://www.univention.com/about-us/careers/vacancies/
+#
+# Copyright (C) 2021-2023 Univention GmbH
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+#
+# https://www.univention.com/
+#
+# All rights reserved.
+#
+# The source code of this program is made available under the terms of
+# the GNU Affero General Public License v3.0 only (AGPL-3.0-only) as
+# published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the AGPL-3.0-only.
+#
+# In the case you use this program under the terms of the AGPL-3.0-only,
+# the program is provided in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <https://www.gnu.org/licenses/agpl-3.0.txt>.
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -13,9 +45,6 @@
 import sys
 from datetime import date
 from os import path
-
-
-# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -77,11 +106,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 pdf_doc_base = path.basename(path.dirname(__file__))
 
 html_theme = 'univention_sphinx_book_theme'
-html_context = {
-    "pdf_download_filename": f"{pdf_doc_base}.pdf",
-}
-
 html_theme_options = {
+    "show_source_license": True,
+    "pdf_download_filename": f"{pdf_doc_base}.pdf",
     "use_download_button": False,
 }
 
