a60520919c67f928de9ca82fee0dadac9ef42525
Florian Best <best@univention.de>
2023-03-29 14:46:52 +0200

    DROPME: ignore validate_email for now

--- management/univention-directory-manager-modules/modules/univention/admin/syntax.py
+++ management/univention-directory-manager-modules/modules/univention/admin/syntax.py
@@ -2594,6 +2594,8 @@ class emailForwardSetting(select):
 
 
 class emailAddress(simple):
+    """foo"""
+
     """
     Syntax class for an e-mail address.
 
@@ -2710,11 +2712,11 @@ class emailAddress(simple):
 
     @classmethod
     def parse(self, text):
-        if self.extra_validation and validate_email and configRegistry.is_true('directory/manager/mail-address/extra-validation', True):
-            try:
-                validate_email(text, allow_smtputf8=False, check_deliverability=False)
-            except EmailNotValidError as exc:
-                raise univention.admin.uexceptions.valueError(_("Not a valid email address!") + " " + str(exc))
+        #if self.extra_validation and validate_email and configRegistry.is_true('directory/manager/mail-address/extra-validation', True):
+        #    try:
+        #        validate_email(text, allow_smtputf8=False, check_deliverability=False)
+        #    except EmailNotValidError as exc:
+        #        raise univention.admin.uexceptions.valueError(_("Not a valid email address!") + " " + str(exc))
         if not text.startswith('@') and \
                 '@' in text and \
                 not text.endswith('@') and \
