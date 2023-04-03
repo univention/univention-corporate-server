c703584de2ca4aa768584162d3edc5fc6ae229f0
Florian Best <best@univention.de>
2023-04-03 13:36:45 +0200

    DROPME: temporarily upgrade system before system setup with the latest packages

--- test/utils/utils.sh
+++ test/utils/utils.sh
@@ -141,6 +141,12 @@ jenkins_updates () {
 	[ -n "${RELEASE_UPDATE:-}" ] && release_update="$RELEASE_UPDATE"
 	[ -n "${ERRATA_UPDATE:-}" ] && errata_update="$ERRATA_UPDATE"
 
+	if [ "$(ucr get version/version)" = "5.0" ] && [ "$target" = "5.1-0" ]; then
+	    echo 'deb [trusted=yes] http://omar.knut.univention.de/build2/ ucs_5.1-0/all/' >>/etc/apt/sources.list
+	    echo 'deb [trusted=yes] http://omar.knut.univention.de/build2/ ucs_5.1-0/$(ARCH)/' >>/etc/apt/sources.list
+	    apt update
+	fi
+
 	eval "$(ucr shell '^version/(version|patchlevel|erratalevel)$')"
 	echo "Starting from ${version_version}-${version_patchlevel}+${version_erratalevel} to ${target}..."
 	echo "release_update=$release_update"
@@ -151,6 +157,12 @@ jenkins_updates () {
 	none|"") ;;
 	*) die "Unknown release_update='$release_update'" ;;
 	esac
+
+	if [ "$(ucr get version/version)" = "5.0" ] && [ "$target" = "5.1-0" ]; then
+	    echo 'deb [trusted=yes] http://omar.knut.univention.de/build2/ ucs_5.1-0/all/' >>/etc/apt/sources.list
+	    echo 'deb [trusted=yes] http://omar.knut.univention.de/build2/ ucs_5.1-0/$(ARCH)/' >>/etc/apt/sources.list
+	    apt update
+	fi
 
 	eval "$(ucr shell '^version/(version|patchlevel|erratalevel)$')"
 	echo "Continuing from ${version_version}-${version_patchlevel}+${version_erratalevel} to ${target}..."
@@ -267,6 +279,11 @@ _fix_ssh47233 () { # Bug #47233: ssh connection stuck on reboot
 }
 
 run_setup_join () {
+	if [ "$(ucr get version/version)" = "5.1" ]; then
+	    echo 'deb [trusted=yes] http://omar.knut.univention.de/build2/ ucs_5.1-0/all/' >>/etc/apt/sources.list
+	    echo 'deb [trusted=yes] http://omar.knut.univention.de/build2/ ucs_5.1-0/$(ARCH)/' >>/etc/apt/sources.list
+	    univention-upgrade --disable-app-updates --noninteractive --ignoreterm --ignoressh --updateto 5.1-0
+	fi
 	local rv=0
 	patch_setup_join # temp. remove me
 	set -o pipefail
