445ba407676c5411a46f78731bc3671e35433b85
Florian Best <best@univention.de>
2023-04-05 19:52:02 +0200

    Bug #55702: update version to UCS 5.0-4

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -4,7 +4,7 @@
 #
 
 # defaults for release
-release='5.0-3'
+release='5.0-4'
 old_release='4.4-9'
 kvm_template_version='5.0-3+e572'
 # AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 4
