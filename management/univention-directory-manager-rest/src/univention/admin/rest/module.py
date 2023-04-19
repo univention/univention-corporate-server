2d9542a0668b22f41c1b9e3237860f83f58fd328
Florian Best <best@univention.de>
2023-04-19 11:13:19 +0200

    fix(udm-rest): fix accessing UDM modules with numbers in its name
    Bug #55551

--- management/univention-directory-manager-rest/src/univention/admin/rest/module.py
+++ management/univention-directory-manager-rest/src/univention/admin/rest/module.py
@@ -4336,8 +4336,8 @@ class Application(tornado.web.Application):
     def __init__(self, **kwargs):
         #module_type = '([a-z]+)'
         module_type = '(%s)' % '|'.join(re.escape(mod) for mod in Modules.mapping)
-        object_type = '([a-z_-]+/[a-z_-]+)'
-        policies_object_type = '(policies/[a-z_-]+)'
+        object_type = '([A-Za-z0-9_-]+/[A-Za-z0-9_-]+)'
+        policies_object_type = '(policies/[A-Za-z0-9_-]+)'
         dn = '((?:[^/]+%s.+%s)?%s)' % (self.multi_regex('='), self.multi_regex(','), self.multi_regex(ucr['ldap/base']))
         # FIXME: with that dn regex, it is not possible to have urls like (/udm/$dn/foo/$dn/) because ldap-base at the end matches the last dn
         # Note: the ldap base is part of the url to support "/" as part of the DN. otherwise we can use: '([^/]+(?:=|%3d|%3D)[^/]+)'
