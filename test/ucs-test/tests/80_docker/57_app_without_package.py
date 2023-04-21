bd28bf5c5949b914107a5a825019fb4fbdaabe8e
Dirk Wiesenthal <wiesenthal@univention.de>
2023-04-21 10:59:24 +0200

    Remove some appbox tests

--- test/ucs-test/tests/80_docker/57_app_without_package.py
+++ test/ucs-test/tests/80_docker/57_app_without_package.py
@@ -7,27 +7,20 @@
 
 from univention.testing.utils import get_ldap_connection
 
-from dockertest import App, Appcenter, get_app_name, get_app_version, get_docker_appbox_image, get_docker_appbox_ucs
+from dockertest import Appcenter, tiny_app
 
 
 if __name__ == '__main__':
 
     with Appcenter() as appcenter:
-        app_name = get_app_name()
-        app_version = get_app_version()
-
-        app = App(name=app_name, version=app_version, container_version=get_docker_appbox_ucs(), build_package=False)
-
         try:
+            app = tiny_app()
             app.set_ini_parameter(
-                DockerImage=get_docker_appbox_image(),
                 WebInterface='/%s' % app.app_name,
                 WebInterfacePortHTTP='80',
                 WebInterfacePortHTTPS='443',
                 AutoModProxy='True',
-                DockerScriptSetup='/usr/sbin/%s-setup' % app_name,
             )
-            app.create_basic_modproxy_settings()
             app.add_to_local_appcenter()
 
             appcenter.update()
@@ -36,10 +29,11 @@ if __name__ == '__main__':
 
             app.verify()
 
-            app.verify_basic_modproxy_settings()
+            app.configure_tinyapp_modproxy()
+            app.verify_basic_modproxy_settings_tinyapp()
 
             lo = get_ldap_connection()
-            print(lo.searchDn(filter='(&(cn=%s-*)(objectClass=univentionMemberServer)(!(aRecord=*))(!(macAddress=*)))' % app_name[:5], unique=True, required=True))
+            print(lo.searchDn(filter='(&(cn=%s-*)(objectClass=univentionMemberServer)(!(aRecord=*))(!(macAddress=*)))' % app.app_name[:5], unique=True, required=True))
         finally:
             app.uninstall()
             app.remove()
