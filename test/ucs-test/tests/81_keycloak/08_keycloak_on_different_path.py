a99e24850b31ed342273a947b60e2668bf76c043
Felix Botner <botner@univention.de>
2023-05-08 16:43:56 +0200

    Bug #56022: Allow keycloak to run under the same fqdn as the host

--- test/ucs-test/tests/81_keycloak/08_keycloak_on_different_path.py
+++ test/ucs-test/tests/81_keycloak/08_keycloak_on_different_path.py
@@ -4,30 +4,25 @@
 ## roles: [domaincontroller_master, domaincontroller_backup]
 ## exposure: dangerous
 
-from urllib.parse import urljoin
-
 import pytest
 import requests
 
 from univention.config_registry import ucr
 
 
-@pytest.mark.parametrize(
-    'kc_fqdn',
-    ['%(hostname)s.%(domainname)s' % ucr, 'ucs-sso-ng.%(domainname)s' % ucr],
-    ids=['same_fqdn', 'different_fqdn'],
-)
-@pytest.mark.parametrize(
-    'kc_path', ['/', '/myauth'],
-    ids=['default_sso_path', 'custom_sso_path'],
-)
-def test_kc_fqdn_path(change_app_setting, kc_fqdn, kc_path):
+@pytest.mark.parametrize('path', ['/', '/myauth'])
+@pytest.mark.parametrize('fqdn', ['%(hostname)s.%(domainname)s' % ucr, 'ucs-sso-ng.%(domainname)s' % ucr])
+def test_kc_fqdn_path(change_app_setting, fqdn, path):
     settings = {
-        'keycloak/server/sso/fqdn': kc_fqdn,
-        'keycloak/server/sso/path': kc_path,
+        'keycloak/server/sso/fqdn': fqdn,
+        'keycloak/server/sso/path': path,
     }
+    if fqdn == f"{ucr['hostname']}.{ucr['domainname']}":
+        # if keycloak uses the UCS hostname, we need to use a global config
+        settings['keycloak/server/sso/virtualhost'] = False
+        if path == "/":
+            pytest.skip("this is not supported")
     change_app_setting('keycloak', settings)
-
-    url = urljoin(f"https://{kc_fqdn}", f"{kc_path}/realms/ucs")
+    url = f"https://{fqdn}{path}/realms/ucs"
     resp = requests.get(url)
     assert resp.status_code == 200
