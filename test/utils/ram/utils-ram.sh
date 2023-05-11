b258e9b97cd3835c2a1dd85b7efabaa772845d30
Jannik Ahlers <ahlers@univention.de>
2023-05-11 16:51:45 +0200

    Issue #43375: create fl school in bsb test environment

--- test/utils/ram/utils-ram.sh
+++ test/utils/ram/utils-ram.sh
@@ -357,10 +357,10 @@ create_bsb_test_users () {
 		"lastname": "Hawkins",
 		"password": "!Univention2023",
 		"source_uid": "Manuell",
-		"school": "https://'"$fqdn"'/ucsschool/kelvin/v1/schools/aa-Schuldock",
+		"school": "https://'"$fqdn"'/ucsschool/kelvin/v1/schools/9990000",
 		"roles": ["https://'"$fqdn"'/ucsschool/kelvin/v1/roles/school_admin"],
 		"record_uid": "1a23bc45-67890-1234-56d7-89012345e6f7g",
-		"ucsschool_roles": ["technical_admin:bsb:*", "teacher:school:aa-Schuldock"]
+		"ucsschool_roles": ["technical_admin:bsb:*", "teacher:school:9990000"]
 	}'
 	# create schuladmin
 	create_user_from_json '{
