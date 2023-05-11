e9c12a3c1df9180acba1eab1e5c3b0e196ee5159
Marius Meschter <marius.meschter@univention.de>
2023-05-11 11:40:07 +0200

    test(browser): migrate 134_side_menu to playwright
    Bug #56054

--- test/ucs-test/tests/86_browser/134_side_menu.py
+++ test/ucs-test/tests/86_browser/134_side_menu.py
@@ -1,4 +1,4 @@
-#!/usr/share/ucs-test/runner /usr/share/ucs-test/selenium
+#!/usr/share/ucs-test/runner /usr/share/ucs-test/playwright
 # -*- coding: utf-8 -*-
 ## desc: Test the Side menu
 ## roles-not:
@@ -8,29 +8,34 @@
 ## join: true
 ## exposure: dangerous
 
+import time
+
+from playwright.sync_api import expect
+
 from univention.lib.i18n import Translation
-from univention.testing import selenium
+from univention.testing.browser.lib import UMCBrowserTest
+from univention.testing.browser.sidemenu import SideMenu
 
 
-_ = Translation('ucs-test-selenium').translate
+_ = Translation("ucs-test-browser").translate
 
 
-class UMCTester(object):
+def test_side_menu_navigation(umc_browser_test: UMCBrowserTest):
+    side_menu = SideMenu(umc_browser_test)
+    side_menu.navigate()
+    side_menu.open_user_settings()
 
-    def test_umc(self):
-        self.selenium.do_login()
-        self.selenium.open_side_menu()
-        self.selenium.click_side_menu_entry(_('User settings'))
-        self.selenium.wait_for_text(_('Change password'))  # check if submenu opened
-        self.selenium.click_side_menu_back()
-        self.selenium.wait_for_text(_('User settings'))  # check if submenu closed
-        self.selenium.close_side_menu()
-        self.selenium.open_side_menu()  # if opening does not work here that means closing did not work
+    change_password_locator = side_menu.page.get_by_text(_("Change password"))
+    expect(change_password_locator, "Expect 'Change password' to be visible when on the side menu user settings").to_be_visible()
 
+    side_menu.back()
 
-if __name__ == '__main__':
-    with selenium.UMCSeleniumTest() as s:
-        umc_tester = UMCTester()
-        umc_tester.selenium = s
+    user_settings_locator = side_menu.page.locator("#umcMenuUserSettings")
+    expect(user_settings_locator, "Expect 'User settings' to be visible after clicking back from User settings").to_be_visible()
 
-        umc_tester.test_umc()
+    side_menu.toggle_menu()
+    # sleep to allow animation
+    time.sleep(2)
+    side_menu.toggle_menu()
+    user_settings_locator = side_menu.page.locator("#umcMenuUserSettings")
+    expect(user_settings_locator, "Expect 'User settings' to be visible after closing and opening the side menu").to_be_visible()
