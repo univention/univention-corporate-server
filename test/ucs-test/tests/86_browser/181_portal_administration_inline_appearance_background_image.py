9dfcf07ad59b264f5f787aad711d136f1c78e272
Marius Meschter <marius.meschter@univention.de>
2023-05-15 14:26:24 +0200

    test(browser): migrate 181_admin.. to playwright
    migrate 181_portal_administration_inline_appearance_background_image.py
    to playwright
    
    Bug #56054

--- test/ucs-test/tests/86_browser/181_portal_administration_inline_appearance_background_image.py
+++ test/ucs-test/tests/86_browser/181_portal_administration_inline_appearance_background_image.py
@@ -1,4 +1,4 @@
-#!/usr/share/ucs-test/runner /usr/bin/python3
+#!/usr/share/ucs-test/runner /usr/share/ucs-test/playwright
 ## desc: Test portal_administration_inline_appearance_background_image
 ## roles:
 ##  - domaincontroller_master
@@ -7,79 +7,44 @@
 ## join: true
 ## exposure: dangerous
 
-import importlib
-import os
-import sys
+import re
+import time
+
+from playwright.sync_api import Page, expect
+
+from univention.testing.browser.lib import UMCBrowserTest
+from univention.testing.browser.portal import UCSPortalEditMode
 
 
-test_lib = os.environ.get('UCS_TEST_LIB', 'univention.testing.apptest')
-try:
-    test_lib = importlib.import_module(test_lib)
-except ImportError:
-    print(f'Could not import {test_lib}. Maybe set $UCS_TEST_LIB')
-    sys.exit(1)
+def ensure_background(page: Page, exists: bool):
+    background = page.locator(".portal__background")
+    if exists:
+        css = re.compile("url\\(.*\\)")
+    else:
+        css = "none"
+
+    expect(background).to_have_css("background-image", css)
 
 
-def _save(chrome):
-    form = chrome.assert_one('form.edit-mode-side-navigation__form')
-    chrome.click_element_below(form, 'button.primary')
-    chrome.wait_until_clickable('#portalCategories')
+def test_portal_change_background_picture(umc_browser_test: UMCBrowserTest):
+    page = umc_browser_test.page
+    edit_mode = UCSPortalEditMode(umc_browser_test)
 
+    edit_mode.navigate()
 
-def _change_background(chrome):
-    chrome.wait_until_clickable_and_click('#header-button-menu')
-    chrome.wait_until_clickable_and_click('button.portal-sidenavigation__edit-mode')
-    chrome.wait_until_clickable_and_click('#header-button-settings')
-    chrome.wait_until_clickable('form.edit-mode-side-navigation__form')
-    chrome.assert_one('form.edit-mode-side-navigation__form')
-    uploaders = chrome.find_all('div.image-upload')
-    assert len(uploaders) == 2
-    uploader = chrome.assert_one_below(uploaders[1], 'input[type=file]')
-    img_path = chrome.save_screenshot('background')
-    uploader.send_keys(img_path)
+    # make sure we start out with no background set
+    ensure_background(page, False)
 
+    # set the background
+    edit_mode.open_edit_side_bar()
+    edit_mode.upload_background_picture()
+    ensure_background(page, True)
 
-def _remove_background(chrome):
-    chrome.wait_until_clickable_and_click('#header-button-menu')
-    chrome.wait_until_clickable_and_click('button.portal-sidenavigation__edit-mode')
-    chrome.wait_until_clickable_and_click('#header-button-settings')
-    chrome.wait_until_clickable('form.edit-mode-side-navigation__form')
-    chrome.assert_one('form.edit-mode-side-navigation__form')
-    uploaders = chrome.find_all('div.image-upload')
-    assert len(uploaders) == 2
-    buttons = chrome.find_all_below(uploaders[1], 'footer button')
-    buttons[1].click()
+    # very random sleep but test sometimes fails without this sleep
+    time.sleep(5)
 
-
-def test_background(chrome, admin_username, admin_password, test_logger):
-    # chrome.goto_portal()
-    chrome.get('/univention/portal')
-    chrome.portal_login(admin_username, admin_password)
-    chrome.wait_until_clickable('#portalCategories')
-    body_background = chrome.assert_one('.portal__background').value_of_css_property('background-image')
-    if body_background != 'none':
-        raise ValueError('Background already set')
-
-    _change_background(chrome)
-    body_background = chrome.assert_one('.portal__background').value_of_css_property('background-image')
-    if 'data:image/png;base64,' not in body_background:
-        raise ValueError('Changing the background image was not hot changed')
-
-    _save(chrome)
-    body_background = chrome.assert_one('.portal__background').value_of_css_property('background-image')
-    if body_background == 'none':
-        raise ValueError('Background not set')
-
-    _remove_background(chrome)
-    body_background = chrome.assert_one('.portal__background').value_of_css_property('background-image')
-    if body_background != 'none':
-        raise ValueError('Background not set')
-
-    _save(chrome)
-    body_background = chrome.assert_one('.portal__background').value_of_css_property('background-image')
-    if body_background != 'none':
-        raise ValueError('Background still set')
-
-
-if __name__ == '__main__':
-    test_lib.run_test_file(__file__)
+    # remove the background
+    edit_mode.navigate()
+    edit_mode.open_edit_side_bar()
+    edit_mode.remove_background_picture()
+    ensure_background(page, False)
