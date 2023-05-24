univention/univention-corporate-server

commit 3a2b66fc9a0e3b28e5e996ae7fc3319902beb61b

feat[diag]: UDN check
Remove dependency on external packaging package.

Add link to UMC UCR module on errors.

Add fixer.

Bug #49417

management/univention-management-console-module-diagnostic/umc/python/diagnostic/plugins/61_notifier_protocol_version.py
@@ -30,58 +30,81 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
-from packaging.version import parse
+from subprocess import call
+from typing import Callable, Dict, List
 
-from univention.config_registry import ucr_live as ucr
+from univention.config_registry import handler_set, ucr_live as ucr
 from univention.lib.i18n import Translation
 from univention.management.console.modules.diagnostic import MODULE, Critical, Instance, Warning
 
 _ = Translation('univention-management-console-module-diagnostic').translate
 
-title = _('Check of the protocol version of the Univention Directory Notifier')
-description = _('Starting with UCS 4.3-3 Errata 428, the minimum protocol version should be set to 3.')
-run_descr = ['This can be checked by running: ucr get notifier/protocol/version']
+UCS = (4, 3, 3, 428)
+UCR = "notifier/protocol/version"
+UDN = 3
+BUTTON = {
+	"label": _("Update protocol version"),
+	"action": "set_protocol_version",
+}
 
-version_err_msg = _('There is no version/version configured.')
-patchlevel_err_msg = _('There is no version/patchlevel configured.')
-erratalevel_err_msg = _('There is no version/erratalevel configured.')
-npversion_err_msg = _('There is no notifier/protocol/version configured.')
-int_cast_err_msg = _('The value notifier/protocol/version has an invalid value.')
+title = _('Check of the protocol version of the Univention Directory Notifier')
+description = _('Starting with UCS {ucs[0]}.{ucs[1]}-{ucs[2]} erratum {ucs[3]}, the minimum protocol version should be set to {udn}.').format(ucs=UCS, udn=UDN)
+run_descr = ['This can be checked by running: ucr get {}'.format(UCR)]
+umc_modules = [{'module': 'ucr'}]
+
+invalid_msg = _('The UCR variable <tt>{ucr}</tt> is not configured or invalid.')
 
 
 def run(_umc_instance: Instance) -> None:
-	np_version = ucr.get('notifier/protocol/version')
-	ucs_version = ucr.get('version/version')
-	ucs_patchlevel = ucr.get('version/patchlevel')
-	ucs_erratalevel = ucr.get('version/erratalevel')
 	server_role = ucr.get('server/role')
-
 	if server_role not in ('domaincontroller_master', 'domaincontroller_backup'):
 		return
 
-	if not ucs_version:
-		MODULE.error(version_err_msg)
-		raise Critical(version_err_msg)
+	problems: List[str] = []
+
+	var = "version/version"
+	ucs_version = ucr.get(var, "")
+	maj_str, _, min_str = ucs_version.partition(".")
+	try:
+		major, minor = int(maj_str), int(min_str)
+	except ValueError:
+		problems.append(invalid_msg.format(ucr=var))
+
+	var = "version/patchlevel"
+	ucs_patchlevel = ucr.get_int(var)
 	if not ucs_patchlevel:
-		MODULE.error(patchlevel_err_msg)
-		raise Critical(patchlevel_err_msg)
+		problems.append(invalid_msg.format(ucr=var))
+
+	var = "version/erratalevel"
+	ucs_erratalevel = ucr.get_int(var)
 	if not ucs_erratalevel:
-		MODULE.error(erratalevel_err_msg)
-		raise Critical(erratalevel_err_msg)
+		problems.append(invalid_msg.format(ucr=var))
 
-	if parse("4.3-3e428") <= parse("%s-%se%s" % (ucs_version, ucs_patchlevel, ucs_erratalevel)):
-		if not np_version:
-			MODULE.error(npversion_err_msg)
-			raise Critical(_(npversion_err_msg))
-		try:
-			np_version = int(np_version)
-		except ValueError:
-			MODULE.error(int_cast_err_msg)
-			raise Critical(int_cast_err_msg)
+	np_version = ucr.get_int(UCR)
+	if not np_version:
+		problems.append(invalid_msg.format(ucr=UCR))
 
-		if np_version < 3:
+	if problems:
+		text = "\n".join(problems)
+		MODULE.error(text)
+		raise Critical(text)
+
+	if (4, 3, 3, 428) <= (major, minor, ucs_patchlevel, ucs_erratalevel):
+		if np_version < UDN:
 			MODULE.error(description)
-			raise Warning(description)
+			raise Warning(description, buttons=[BUTTON])
+
+
+def set_protocol_version(umc: Instance) -> None:
+	MODULE.process("Setting UDN protocol version {}".format(UDN))
+	handler_set(["%s=%d" % (UCR, UDN)])
+	call(["systemctl", "try-restart", "univention-directory-notifier.service"])
+	return run(umc)
+
+
+actions: Dict[str, Callable[[Instance], None]] = {
+	"set_protocol_version": set_protocol_version,
+}
 
 
 if __name__ == '__main__':

doc/errata/staging/nivention-management-console-module-diagnostic.yaml
@@ -7,6 +7,9 @@ fix: 6.0.2-6
 desc: |
  This update addresses the following issue:
  * The diagnostics checks for SAML Identifier and Service Providers has been
-   fixed to work again. It now provides more information in case of errors
-   and provides automatic fixers to correct issues.
+   fixed to work again. It now provides more information in case of errors and
+   provides automatic fixers to correct issues.
+ * The diagnostics check for the Univention Directory Notifier Protocol
+   version has been extended to provide mot information in case of errors and
+   provides an automatic fixer to update the protocol version.
 bug: [49417]

management/univention-management-console-module-diagnostic/debian/control
@@ -17,7 +17,6 @@ Depends:
  python3-dateutil,
  python3-dnspython,
  python3-openssl,
- python3-packaging,
  python3-paramiko,
  python3-pexpect,
  python3-psutil,

management/univention-management-console-module-diagnostic/umc/python/diagnostic/de.po
@@ -760,11 +760,11 @@ msgstr "Einige Festplatten sind beinahe voll:"
 
 #: umc/python/diagnostic/plugins/61_notifier_protocol_version.py:15
 msgid ""
-"Starting with UCS 4.3-3 Errata 428, the minimum protocol version should be "
-"set to 3."
+"Starting with UCS {ucs[0]}.{ucs[1]}-{ucs[2]} erratum {ucs[3]}, the minimum "
+"protocol version should be set to {udn}."
 msgstr ""
-"Ab UCS 4.3-3 Errata 428 sollte die minimale Protokollversion auf 3 angehoben "
-"sein."
+"Ab UCS {ucs[0]}.{ucs[1]}-{ucs[2]} Erratum {ucs[3]} sollte die minimale "
+"Protokollversion auf {udn} angehoben sein."
 
 #: umc/python/diagnostic/__init__.py:297
 msgid "Test again"
@@ -989,21 +989,10 @@ msgstr "Die Variable %(variable)r hat den ungültigen Wert %(value)r."
 msgid "There is no gateway configured."
 msgstr "Es ist kein Gateway eingerichtet."
 
-#: umc/python/diagnostic/plugins/61_notifier_protocol_version.py:21
-msgid "There is no notifier/protocol/version configured."
-msgstr "Die UCR-Variable notifier/protocol/version ist nicht konfiguriert."
-
-#: umc/python/diagnostic/plugins/61_notifier_protocol_version.py:20
-msgid "There is no version/erratalevel configured."
-msgstr "Die UCR-Variable version/erratalevel ist nicht konfiguriert."
-
-#: umc/python/diagnostic/plugins/61_notifier_protocol_version.py:19
-msgid "There is no version/patchlevel configured."
-msgstr "Die UCR-Variable version/patchlevel ist nicht konfiguriert."
-
-#: umc/python/diagnostic/plugins/61_notifier_protocol_version.py:18
-msgid "There is no version/version configured."
-msgstr "Die UCR-Variable version/version ist nicht konfiguriert."
+#: umc/python/diagnostic/plugins/61_notifier_protocol_version.py:45
+#, python-brace-format
+msgid "The UCR variable <tt>{ucr}</tt> is not configured or invalid."
+msgstr "Die UCR-Variable <tt>{ucr}</tt> ist nicht konfiguriert oder ungültig."
 
 #: umc/python/diagnostic/plugins/12_proxy.py:15
 msgid ""
@@ -1146,6 +1135,10 @@ msgstr "Univention Support Database - Listener-/Notifier-Fehlersuche"
 #: umc/python/diagnostic/plugins/50_check_ucr_templates.py:54
 msgid "Updated versions will be named FILENAME.dpkg-*."
 msgstr "Aktualisierte Versionen werden DATEINAME.dpkg-* genannt."
+
+#: umc/python/diagnostic/plugins/61_notifier_protocol_version.py:44
+msgid "Update protocol version"
+msgstr "Protokollversion anpassen"
 
 #: umc/python/diagnostic/plugins/64_check_ucr_types.py:51
 #, python-brace-format

commit 593c11b125b8bf2b44db969f96f4d0696a0d52c8

fixup! feat[diag]: UDN check
Fixes: 3a2b66fc9a0e3b28e5e996ae7fc3319902beb61b
Bug #49417

management/univention-management-console-module-diagnostic/umc/python/diagnostic/plugins/61_notifier_protocol_version.py
@@ -72,17 +72,17 @@ def run(_umc_instance: Instance) -> None:
         problems.append(invalid_msg.format(ucr=var))
 
     var = "version/patchlevel"
-    ucs_patchlevel = ucr.get_int(var)
-    if not ucs_patchlevel:
+    ucs_patchlevel = ucr.get_int(var, -1)
+    if ucs_patchlevel < 0:
         problems.append(invalid_msg.format(ucr=var))
 
     var = "version/erratalevel"
-    ucs_erratalevel = ucr.get_int(var)
-    if not ucs_erratalevel:
+    ucs_erratalevel = ucr.get_int(var, -1)
+    if ucs_erratalevel < 0:
         problems.append(invalid_msg.format(ucr=var))
 
-    np_version = ucr.get_int(UCR)
-    if not np_version:
+    np_version = ucr.get_int(UCR, -1)
+    if np_version < 0:
         problems.append(invalid_msg.format(ucr=UCR))
 
     if problems: