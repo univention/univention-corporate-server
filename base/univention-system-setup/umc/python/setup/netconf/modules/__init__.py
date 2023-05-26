univention/univention-corporate-server

commit d34605ac9ea18112b538782ae65d348d1e742ced

Bug #55632: improved the Python 3.11 compatiblity of univention-system-setup

base/univention-system-setup/umc/python/setup/netconf/modules/__init__.py
@@ -29,7 +29,7 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
-import imp
+import importlib.util
 import logging
 import os
 import sys
@@ -58,20 +58,19 @@ class RunPhases(object):
                         self.logger.debug("Skipping '%s'", filename)
                         continue
                     try:
-                        info = imp.find_module(name, [dirpath])
+                        info = importlib.util.find_spec(name, [dirpath])
                     except ImportError:
                         self.logger.warning("Failed to open '%s'", filename)
                     yield name, info
 
     def load(self):
-        for name, (pfile, filename, desc) in self.find():
+        for name, info in self.find():
             try:
-                module = imp.load_module(name, pfile, filename, desc)
+                module = importlib.util.module_from_spec(info)
+                sys.modules[name] = module
+                info.loader.exec_module(module)
             except SyntaxError as ex:
                 self.logger.warning("Failed to import '%s': %s", name, ex)
-            finally:
-                if pfile:
-                    pfile.close()
             for key, value in vars(module).items():
                 if not key.startswith('_'):
                     self.add(key, value)

doc/errata/staging/univention-system-setup.yaml
@@ -1,0 +1,10 @@
+product: ucs
+release: "5.0"
+version: [3]
+scope: ucs_5.0-0-errata5.0-3
+src: univention-system-setup
+fix: 13.0.6-3
+desc: |
+ This update addresses the following issues:
+ * Future compatibility with Python 3.11 has been added.
+bug: [55632]

base/univention-system-setup/debian/control
@@ -12,6 +12,7 @@ Build-Depends:
  python3-dnspython,
  python3-lxml,
  python3-notifier,
+ python3-packaging,
  python3-pil,
  python3-psutil,
  python3-univention-appcenter,
@@ -126,6 +127,7 @@ Depends:
  python3-ldap,
  python3-lxml,
  python3-notifier,
+ python3-packaging,
  python3-psutil,
  python3-six,
  python3-univention-appcenter,

base/univention-system-setup/umc/python/setup/checks/ldap.py
@@ -36,7 +36,7 @@
 
 from __future__ import absolute_import
 
-from pipes import quote
+from shlex import quote
 from subprocess import PIPE, Popen
 
 from ldap.filter import filter_format

base/univention-system-setup/umc/python/setup/checks/univention_join.py
@@ -1,7 +1,8 @@
-import pipes
+import shlex
 import subprocess
 import traceback
-from distutils.version import LooseVersion
+
+from packaging.version import Version
 
 import univention.config_registry
 from univention.lib.i18n import Translation
@@ -101,7 +102,7 @@ def check_domain_is_higher_or_equal_version(address, username, password):
             MODULE.error('Failed to retrieve UCS version: %s' % (traceback.format_exc(),))
             return
         nonmaster_ucs_version = '{}-{}'.format(UCR.get('version/version'), UCR.get('version/patchlevel'))
-        if LooseVersion(nonmaster_ucs_version) > LooseVersion(master_ucs_version):
+        if Version(nonmaster_ucs_version) > Version(master_ucs_version):
             raise UMC_Error(_('The UCS version of the domain you are trying to join ({}) is lower than the local one ({}). This constellation is not supported.').format(master_ucs_version, nonmaster_ucs_version))
 
 
@@ -153,7 +154,7 @@ def check_is_school_multiserver_domain(address, username, password):
                 'get',
                 'ldap/base',
             ]).strip().decode('UTF-8')
-            remote_cmd = ' '.join(pipes.quote(x) for x in [
+            remote_cmd = ' '.join(shlex.quote(x) for x in [
                 'univention-ldapsearch',
                 '-D',
                 f'cn=admin,{ldap_base}',
@@ -190,7 +191,7 @@ def get_server_school_roles(hostname, address, username, password):
                 'get',
                 'ldap/base',
             ]).strip().decode('UTF-8')
-            remote_cmd = ' '.join(pipes.quote(x) for x in [
+            remote_cmd = ' '.join(shlex.quote(x) for x in [
                 'univention-ldapsearch',
                 '-D',
                 f'cn=admin,{ldap_base}',

base/univention-system-setup/umc/python/setup/util.py
@@ -755,7 +755,7 @@ def dhclient(interface, timeout=None):
 
     stderr = []
     stderr_thread = threading.Thread(target=_readerthread, args=(p.stderr, stderr))
-    stderr_thread.setDaemon(True)
+    stderr_thread.daemon = True
     stderr_thread.start()
     stderr_thread.join(timeout)
 

commit 85baeff0cdad76a937447a39d76681e0518f65b2

fix(USS): Fix netconf module loading
code was switched to `importlib`, but did not work.

Also add better error handling.

Fixes: d34605ac9ea18112b538782ae65d348d1e742ced
Bug #55632

base/univention-system-setup/umc/python/setup/netconf/modules/__init__.py
@@ -34,7 +34,7 @@ import importlib.util
 import logging
 import os
 import sys
-from typing import Any, Iterator, List, Tuple
+from typing import Any, List
 
 from univention.management.console.modules.setup.netconf import ChangeSet, Phase, SkipPhase
 
@@ -50,7 +50,7 @@ class RunPhases(object):
         self.phases: List[Phase] = []
         self.logger = logging.getLogger("uss.network.plug")
 
-    def find(self) -> Iterator[Tuple[str, Any]]:
+    def load(self) -> None:
         for module_dir in sys.modules[__name__].__path__:
             for dirpath, _dirnames, filenames in os.walk(module_dir):
                 self.logger.debug("Processing '%s'...", dirpath)
@@ -60,22 +60,16 @@ class RunPhases(object):
                         self.logger.debug("Skipping '%s'", filename)
                         continue
                     try:
-                        info = importlib.util.find_spec(name, [dirpath])
+                        module = importlib.import_module('%s.%s' % (__name__, name))
                     except ImportError:
                         self.logger.warning("Failed to open '%s'", filename)
-                    yield name, info
-
-    def load(self) -> None:
-        for name, info in self.find():
-            try:
-                module = importlib.util.module_from_spec(info)
-                sys.modules[name] = module
-                info.loader.exec_module(module)
-            except SyntaxError as ex:
-                self.logger.warning("Failed to import '%s': %s", name, ex)
-            for key, value in vars(module).items():
-                if not key.startswith('_'):
-                    self.add(key, value)
+                        continue
+                    except SyntaxError as ex:
+                        self.logger.warning("Failed to import '%s': %s", name, ex)
+                        continue
+                    for key, value in vars(module).items():
+                        if not key.startswith('_'):
+                            self.add(key, value)
         self.logger.info("Finished loading %d modules", len(self.classes))
 
     def add(self, name: str, obj: Any) -> None:

doc/errata/staging/univention-system-setup.yaml
@@ -3,7 +3,7 @@ release: "5.0"
 version: [3]
 scope: ucs_5.0-0-errata5.0-3
 src: univention-system-setup
-fix: 13.0.6-3
+fix: 13.0.6-4
 desc: |
  This update addresses the following issues:
  * Future compatibility with Python 3.11 has been added.