univention/univention-corporate-server

commit 840f2d9ef64bab3486baaa3c87e64ef69e71d69c

univention-system-setup (6.0.13-1) unstable; urgency=low  .    * Use umc.widgets.ProgressBar instead of own ProgressInfo (Bug #25558)
git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs@34866 1b283449-9f2f-0410-b571-8eb228e1a901

base/univention-system-setup/umc/python/setup/util.py
@@ -226,45 +226,15 @@ class ProgressState( object ):
 		self.steps = 1
 		self.step = 0
 		self.max = 100
-		self._join_error = ''
-		self._join_errors = []
-		self._misc_error = ''
-		self._misc_errors = []
-
-	# make sure the error is only send once
-	def _pop_error( self, error_name ):
-		# error attrs are "hidden"
-		error_name = '_%s' % error_name
-		error = getattr(self, error_name)
-		setattr(self, error_name, '')
-		return error
-
-	def pop_misc_error( self ):
-		return self._pop_error('misc_error')
-
-	def pop_join_error( self ):
-		return self._pop_error('join_error')
-
-	# make sure the error is preserved even if it cannot be returned now
-	def _add_error( self, error_name, error ):
-		error_name = '_%s' % error_name
-		# simplify error (to not have to use nested lists)
-		error = '%s: %s' % (self.fractionName, error)
-		setattr(self, error_name, error)
-		getattr(self, error_name + 's').append(error)
-
-	def add_misc_error( self, error ):
-		self._add_error('misc_error', error)
-
-	def add_join_error( self, error ):
-		self._add_error('join_error', error)
+		self.errors = []
+		self.critical = False
 
 	@property
 	def percentage( self ):
 		return ( self._percentage + self.fraction * ( self.step / float( self.steps ) ) ) / self.max * 100
 
 	def __eq__( self, other ):
-		return self.name == other.name and self.message == other.message and self.percentage == other.percentage and self.fraction == other.fraction and self.steps == other.steps and self.step == other.step and self._join_error == self._join_error and self._misc_error == other._misc_error
+		return self.name == other.name and self.message == other.message and self.percentage == other.percentage and self.fraction == other.fraction and self.steps == other.steps and self.step == other.step and self.errors == self.errors and self.critical == self.critical
 
 	def __ne__( self, other ):
 		return not self.__eq__( other )
@@ -364,13 +334,16 @@ class ProgressParser( object ):
 		# error message: why did the join fail?
 		match = ProgressParser.JOINERROR.match( line )
 		if match is not None:
-			self.current.add_join_error( match.groups()[ 0 ] )
+			error = '%s: %s' % (self.current.fractionName, match.groups()[ 0 ])
+			self.current.errors.append( error )
+			self.current.critical = True
 			return True
 
 		# error message: why did the script fail?
 		match = ProgressParser.ERROR.match( line )
 		if match is not None:
-			self.current.add_misc_error( match.groups()[ 0 ] )
+			error = '%s: %s' % (self.current.fractionName, match.groups()[ 0 ])
+			self.current.errors.append( error )
 			return True
 
 		return False

base/univention-system-setup/umc/js/_setup/ProgressInfo.js
@@ -1,94 +1,0 @@
-/*
- * Copyright 2011-2012 Univention GmbH
- *
- * http://www.univention.de/
- *
- * All rights reserved.
- *
- * The source code of this program is made available
- * under the terms of the GNU Affero General Public License version 3
- * (GNU AGPL V3) as published by the Free Software Foundation.
- *
- * Binary versions of this program provided by Univention to you as
- * well as other copyrighted, protected or trademarked materials like
- * Logos, graphics, fonts, specific documentations and configurations,
- * cryptographic keys etc. are subject to a license agreement between
- * you and Univention and not subject to the GNU AGPL V3.
- *
- * In the case you use this program under the terms of the GNU AGPL V3,
- * the program is provided in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public
- * License with the Debian GNU/Linux or Univention distribution in file
- * /usr/share/common-licenses/AGPL-3; if not, see
- * <http://www.gnu.org/licenses/>.
- */
-/*global console MyError dojo dojox dijit umc */
-
-dojo.provide("umc.modules._setup.ProgressInfo");
-
-dojo.require("umc.i18n");
-dojo.require("umc.render");
-dojo.require("umc.tools");
-dojo.require("umc.widgets.Text");
-dojo.require("umc.widgets.ContainerWidget");
-dojo.require("dijit.ProgressBar");
-
-dojo.declare("umc.modules._setup.ProgressInfo", [ umc.widgets.ContainerWidget, umc.i18n.Mixin ], {
-	// summary:
-	//		This class provides a widget providing detailed progress information
-
-	// use i18n information from umc.modules.udm
-	i18nClass: 'umc.modules.setup',
-
-	// system-setup-boot
-	wizard_mode: false,
-
-	// __systemsetup__ user is logged in at local firefox session
-	local_mode: false,
-
-	umcpCommand: umc.tools.umcpCommand,
-
-	style: 'width: 400px',
-
-	_component: null,
-	_message : null,
-	_progressBar: null,
-
-	buildRendering: function() {
-		this.inherited(arguments);
-
-		this._component = new umc.widgets.Text( { content : '' , style : 'width: 100%' } );
-		this.addChild( this._component );
-		this._progressBar = new dijit.ProgressBar( { style : 'width: 100%' } );
-		this.addChild( this._progressBar );
-		this._message = new umc.widgets.Text( { content : '&nbsp;', style : 'width: 100%' } );
-		this.addChild( this._message );
-
-		this._progressBar.set( 'value', 0 );
-		this._progressBar.set( 'maximum', 100 );
-
-		this.reset();
-		this.startup();
-	},
-
-	reset: function() {
-		this._component.set( 'content', this._( 'Initialize the configuration process ...' ) );
-		this._message.set( 'content', '&nbsp;' );
-		this._progressBar.set( 'value', 0 );
-	},
-
-	setInfo: function( component, message, percentage ) {
-		this._component.set( 'content', component );
-		this._progressBar.set( 'value', percentage );
-		// make sure that at least a not breakable space is printed
-		// ... this avoids vertical jumping of widgets
-		this._message.set( 'content', message || '&nbsp;' );
-	}
-});
-
-
-

base/univention-system-setup/umc/js/setup.js
@@ -41,7 +41,7 @@ dojo.require("umc.widgets.Page");
 dojo.require("umc.widgets.TitlePane");
 dojo.require("umc.modules.lib.server");
 
-dojo.require("umc.modules._setup.ProgressInfo");
+dojo.require("umc.widgets.ProgressBar");
 
 dojo.declare("umc.modules._setup.CancelDialogException", null, {
 	// empty class that indicates that the user canceled a dialog
@@ -64,13 +64,10 @@ dojo.declare("umc.modules.setup", [ umc.widgets.Module, umc.i18n.Mixin ], {
 
 	_currentPage: -1,
 
-	_progressInfo: null,
+	_progressBar: null,
 
 	// internal dict to save error messages while polling
 	_saveErrors: null,
-
-	// internal flag to indicate whether a fatal join error occurred
-	_joinError: null,
 
 	// a timer used it in _cleanup
 	// to make sure the session does not expire
@@ -107,8 +104,7 @@ dojo.declare("umc.modules.setup", [ umc.widgets.Module, umc.i18n.Mixin ], {
 	},
 
 	renderPages: function(ucr, values) {
-		this._progressInfo = new umc.modules._setup.ProgressInfo();
-		// this._progressInfo.buildRendering();
+		this._progressBar = new umc.widgets.ProgressBar();
 		this.standby(true);
 
 		// console.log('joined=' + values.joined);
@@ -662,45 +658,11 @@ dojo.declare("umc.modules.setup", [ umc.widgets.Module, umc.i18n.Mixin ], {
 				username = username || null;
 				password = password || null;
 
-				this.resetErrors();
-				var self = this;
-				var _Poller = function( _parent, _deferred ) {
-					return {
-						deferred: _deferred,
-						parent: _parent,
-						check: function() {
-							var message = dojo.replace( this.parent._( 'The connection to the server could not be established after {time} seconds. This problem can occur due to a change of the IP address. In this case, please login to Univention Management Console again at the {linkStart}new address{linkEnd}.' ), { 
-								time: '{time}',
-								linkStart : umc_url ? '<a href="' + umc_url + '">' : '',
-								linkEnd : umc_url ? '</a>' : ''
-							} );
-							this.parent.umcpCommand( 'setup/finished', {}, undefined, undefined, {
-								// long polling options
-								messageInterval: 30,
-								message: message,
-								xhrTimeout: 40
-							} ).then( dojo.hitch( this, function( response ) {
-								self.addError( response.result.error );
-								self.addErrors( response.result.all_errors );
-								self.addError( response.result.join_error, true );
-								self.addErrors( response.result.all_join_errors, true );
-								if ( response.result.finished ) {
-									this.parent._progressInfo.setInfo(this.parent._('Configuration finished'), '', 100);
-									this.deferred.resolve();
-									return;
-								}
-								this.parent._progressInfo.setInfo( response.result.name, response.result.message, response.result.percentage );
-								setTimeout( dojo.hitch( this, 'check' ), 100 );
-							} ) );
-						}
-					};
-				};
-
 				var deferred = new dojo.Deferred();
 
 				// send save command to server
-				this._progressInfo.reset();
-				this.standby( true, this._progressInfo );
+				this._progressBar.reset(this._( 'Initialize the configuration process ...' ));
+				this.standby( true, this._progressBar );
 				var command = null;
 				if (!this.wizard_mode || role == 'basesystem') {
 					command = this.umcpCommand('setup/save', {
@@ -715,8 +677,18 @@ dojo.declare("umc.modules.setup", [ umc.widgets.Module, umc.i18n.Mixin ], {
 				}
 				command.then(dojo.hitch(this, function() {
 					// poll whether script has finished
-					var poller = new _Poller( this, deferred );
-					poller.check();
+					this._progressBar.auto(
+						'setup/finished',
+						{},
+						dojo.hitch(deferred, 'resolve'),
+						dojo.replace( this._( 'The connection to the server could not be established after {time} seconds. This problem can occur due to a change of the IP address. In this case, please login to Univention Management Console again at the {linkStart}new address{linkEnd}.' ), { 
+							time: '{time}',
+							linkStart : umc_url ? '<a href="' + umc_url + '">' : '',
+							linkEnd : umc_url ? '</a>' : ''
+						} ),
+						this._('Configuration finished'),
+						true
+					);
 				}));
 
 				return deferred;
@@ -772,10 +744,11 @@ dojo.declare("umc.modules.setup", [ umc.widgets.Module, umc.i18n.Mixin ], {
 			if (this.wizard_mode) {
 				// kill the browser and restart the UMC services in wizard mode
 				deferred = deferred.then(dojo.hitch(this, function() {
-					var errorHtml = this._buildErrorHtml();
+					var errors = this._progressBar.getErrors();
+					var errorHtml = this._buildErrorHtml(errors.errors);
 					if (!errorHtml) {
 						return _cleanup(this._('The configuration was successful. Please confirm to complete the process.'));
-					} else if (this._joinError) {
+					} else if (errors.critical) {
 						return _cleanup(this._embedErrorHTML(
 							this._('The system join was not successful.'),
 							errorHtml,
@@ -793,7 +766,8 @@ dojo.declare("umc.modules.setup", [ umc.widgets.Module, umc.i18n.Mixin ], {
 			else {
 				// show success/error message and eventually restart UMC server components
 				deferred = deferred.then(dojo.hitch(this, function() {
-					var errorHtml = this._buildErrorHtml();
+					var errors = this._progressBar.getErrors();
+					var errorHtml = this._buildErrorHtml(errors.errors);
 					if (errorHtml) {
 						// errors have occurred
 						return _failure(errorHtml);
@@ -810,40 +784,20 @@ dojo.declare("umc.modules.setup", [ umc.widgets.Module, umc.i18n.Mixin ], {
 			}
 
 			// error case, turn off standby animation
-			var self = this;
-			deferred.then(function() {}, function() {
-				self.standby(false);
-			});
+			deferred.then(
+				function() {},
+				dojo.hitch(this, function() {
+					this.standby(false); 
+				})
+			);
 		}), dojo.hitch(this, function() {
 			this.standby(false);
 		}));
 	},
 
-	resetErrors: function() {
-		this._saveErrors = [];
-		this._joinError = false;
-	},
-
-	addErrors: function(errors, is_join_error) {
-		dojo.forEach(errors, dojo.hitch(this, function(error) {
-			this.addError(error, is_join_error);
-		}));
-	},
-
-	addError: function(error, is_join_error) {
-		if ( error ) {
-			if ( dojo.indexOf( this._saveErrors, error ) === -1 ) {
-				this._saveErrors.push( error );
-			}
-			if (is_join_error) {
-				this._joinError = true;
-			}
-		}
-	},
-
-	_buildErrorHtml: function() {
+	_buildErrorHtml: function(errors) {
 		var errorHtml = '';
-		dojo.forEach(this._saveErrors, function(error) {
+		dojo.forEach(errors, function(error) {
 			errorHtml += '<li>' + error + '</li>';
 		});
 		if (errorHtml) {

base/univention-system-setup/umc/python/setup/__init__.py
@@ -243,6 +243,14 @@ class Instance(umcm.Base):
 		have been executed or due to a timeout. If it returns because of the timeout, a new
 		try can be started.'''
 		def _thread(request, obj):
+			def progress_info(state, **kwargs):
+				info = { 'component' : state.fractionName,
+					 'info' : state.message,
+					 'errors' : state.errors,
+					 'critical' : state.critical,
+					 'steps' : state.percentage }
+				info.update(kwargs)
+				return info
 			# acquire the lock in order to wait for the join/setup scripts to finish
 			# do this for 30 sec and then return anyway
 			SLEEP_TIME = 0.200
@@ -251,12 +259,7 @@ class Instance(umcm.Base):
 			while not obj._finishedLock.acquire(False):
 				if ntries <= 0 or self._progressParser.changed and self._progressParser.current:
 					state = self._progressParser.current
-					return { 'finished' : False,
-						 'name' : state.fractionName,
-						 'message' : state.message,
-						 'join_error' : state.pop_join_error(),
-						 'error' : state.pop_misc_error(),
-						 'percentage' : state.percentage }
+					return progress_info(state, finished=False)
 				time.sleep( SLEEP_TIME )
 				ntries -= 1
 
@@ -265,16 +268,7 @@ class Instance(umcm.Base):
 			# scripts are done, return final result
 			# return all errors that we gathered throughout the setup
 			state = self._progressParser.current
-			return { 
-				'finished' : obj._finishedResult,
-				'name' : state.fractionName,
-				'message' : state.message,
-				'join_error' : state.pop_join_error(),
-				'error' : state.pop_misc_error(),
-				'all_join_errors' : state._join_errors,
-				'all_errors' : state._misc_errors,
-				'percentage' : state.percentage,
-				 }
+			return progress_info(state, finished=obj._finishedResult)
 
 		thread = notifier.threads.Simple( 'check_finished',
 			notifier.Callback( _thread, request, self ),

base/univention-system-setup/umc/python/setup/de.po
@@ -3,7 +3,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: univention-management-console-module-setup\n"
 "Report-Msgid-Bugs-To: packages@univention.de\n"
-"POT-Creation-Date: 2012-07-05 12:45+0200\n"
+"POT-Creation-Date: 2012-06-26 21:04+0200\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
 "Last-Translator: FULL NAME <EMAIL@ADDRESS>\n"
 "Language-Team: LANGUAGE <LL@li.org>\n"
@@ -12,11 +12,11 @@ msgstr ""
 "Content-Type: text/plain; charset=UTF-8\n"
 "Content-Transfer-Encoding: 8bit\n"
 
-#: umc/python/setup/__init__.py:368
+#: umc/python/setup/__init__.py:393
 msgid " (virtual)"
 msgstr " (virtuell)"
 
-#: umc/python/setup/__init__.py:505
+#: umc/python/setup/__init__.py:530
 msgid ""
 "A default entry with the identifier \"default\" needs to be specified for "
 "each network device."
@@ -24,7 +24,7 @@ msgstr ""
 "Ein Standardeintrag mit dem Bezeichner \"default\" muss für jedes "
 "Netzwerkgerät angegeben werden."
 
-#: umc/python/setup/__init__.py:316
+#: umc/python/setup/__init__.py:341
 msgid ""
 "A valid NetBIOS name can not be longer than 13 characters. If Samba is "
 "installed, the hostname should be shortened."
@@ -32,20 +32,20 @@ msgstr ""
 "Ein gültiger NetBIOS Name darf nicht länger als 13 Zeichen sein. Wenn Samba "
 "installiert wird, sollte der Rechnername gekürzt werden."
 
-#: umc/python/setup/__init__.py:492
+#: umc/python/setup/__init__.py:517
 #, python-format
 msgid "A virtual device cannot be specified alone: %s"
 msgstr "Ein virtuelles Netzwerkgerät kann nicht alleine definiert werden: %s"
 
-#: umc/python/setup/__init__.py:507
+#: umc/python/setup/__init__.py:532
 msgid "At least one IPv4 address needs to be specified."
 msgstr "Zumindest eine IPv4-Adresse muss angegeben werden."
 
-#: umc/python/setup/__init__.py:503
+#: umc/python/setup/__init__.py:528
 msgid "At least one IPv6 address needs to be specified."
 msgstr "Zumindest eine IPv6-Adresse muss angegeben werden."
 
-#: umc/python/setup/__init__.py:509
+#: umc/python/setup/__init__.py:534
 msgid ""
 "At least one domain name server needs to be given if DHCP or SLAAC is not "
 "specified."
@@ -53,57 +53,62 @@ msgstr ""
 "Zumindest ein Domänen-DNS-Server muss angegeben werden, wenn DHCP oder SLAAC "
 "nicht ausgewählt sind."
 
-#: umc/python/setup/__init__.py:501
+#: umc/python/setup/__init__.py:526
 msgid ""
 "At least one network device (either IPv4 or IPv6) needs to be configured."
 msgstr ""
 "Zumindest ein Netzwerkgerät (entweder IPv4 oder IPv6) muss konfiguriert sein."
 
-#: umc/python/setup/__init__.py:178
+#: umc/python/setup/__init__.py:193
 msgid "Base systems and already joined systems cannot be joined."
 msgstr "Basissysteme and bereits gejointe Systeme können nicht gejoint werden."
 
-#: umc/python/setup/__init__.py:339
+#: umc/python/setup/__init__.py:364
 msgid "Business unit"
 msgstr "Abteilung"
 
-#: umc/python/setup/__init__.py:406
+#: umc/python/setup/__init__.py:431
 msgid "Domain name server"
 msgstr "Domänen-DNS-Server"
 
-#: umc/python/setup/__init__.py:339
+#: umc/python/setup/__init__.py:364
 msgid "Email address"
 msgstr "E-Mail-Adresse"
 
-#: umc/python/setup/__init__.py:406
+#: umc/python/setup/__init__.py:431
 msgid "External name server"
 msgstr "Externer DNS-Server"
 
-#: umc/python/setup/__init__.py:325
+#: umc/python/setup/setup_script.py:351
+#, python-format
+msgid "Failed to get package for Role %s"
+msgstr "Es konnte kein Paket für die Rolle %s gefunden werden"
+
+#: umc/python/setup/__init__.py:350
 msgid "Hostname is equal to domain name."
 msgstr "Systemname ist gleich dem Domänennamen."
 
-#: umc/python/setup/__init__.py:370
+#: umc/python/setup/__init__.py:395
 #, python-format
 msgid "IPv4 address is not valid [%s%s]: \"%s\""
 msgstr "IPv4-Adresse ist nicht gültig [%s%s]: \"%s\""
 
-#: umc/python/setup/__init__.py:372
+#: umc/python/setup/__init__.py:397
 #, python-format
 msgid "IPv4 netmask is not valid [%s%s]: \"%s\""
 msgstr "IPv4-Netzmaske ist nicht gültig [%s%s]: \"%s\""
 
-#: umc/python/setup/__init__.py:401
+#: umc/python/setup/__init__.py:426
 #, python-format
 msgid "IPv6 address is not valid [%s]: %s"
 msgstr "IPv6-Adresse ist nicht gültig [%s]: %s"
 
-#: umc/python/setup/__init__.py:403
+#: umc/python/setup/__init__.py:428
 #, python-format
 msgid "IPv6 prefix is not valid [%s]: %s"
 msgstr "IPv6-Präfix ist nicht gültig [%s]: %s"
 
-#: umc/python/setup/__init__.py:516
+#: umc/python/setup/__init__.py:538
 msgid ""
 "It is not possible to install KVM and XEN components on one system. Please "
 "select only one of these components."
@@ -111,7 +116,7 @@ msgstr ""
 "Es ist nicht möglich KVM- und XEN-Komponenten auf einem System zu "
 "installieren. Bitte wählen Sie nur eine dieser Komponenten aus."
 
-#: umc/python/setup/__init__.py:518
+#: umc/python/setup/__init__.py:540
 msgid ""
 "It is not possible to install Samba 3 and Samba 4 on one system. Please "
 "select only one of these components."
@@ -119,19 +124,19 @@ msgstr ""
 "Es ist nicht möglich Samba 3 und Samba 4 auf einem System zu installieren. "
 "Bitte wählen Sie nur eine dieser Komponenten aus."
 
-#: umc/python/setup/__init__.py:337
+#: umc/python/setup/__init__.py:362
 msgid "Location"
 msgstr "Ort"
 
-#: umc/python/setup/__init__.py:339
+#: umc/python/setup/__init__.py:364
 msgid "Organization"
 msgstr "Organisation"
 
-#: umc/python/setup/__init__.py:341
+#: umc/python/setup/__init__.py:366
 msgid "Please enter a valid email address"
 msgstr "Bitte geben Sie eine gültige E-Mail-Adresse ein"
 
-#: umc/python/setup/__init__.py:318
+#: umc/python/setup/__init__.py:343
 msgid ""
 "Please enter a valid fully qualified domain name in lowercase (e.g. host."
 "example.com)."
@@ -139,26 +144,26 @@ msgstr ""
 "Bitte geben Sie einen klein-geschriebenen und vollständigen Rechnernamen an "
 "(z.B. host.example.com)."
 
-#: umc/python/setup/__init__.py:337
+#: umc/python/setup/__init__.py:362
 msgid "State"
 msgstr "Land"
 
-#: umc/python/setup/__init__.py:331
+#: umc/python/setup/__init__.py:356
 msgid "The LDAP base may not contain any blanks (e.g., dc=test,dc=net)."
 msgstr ""
 "Die LDAP-Basis darf keine Leerzeichen enthalten (z.B., dc=test,dc=net)."
 
-#: umc/python/setup/__init__.py:338
+#: umc/python/setup/__init__.py:363
 #, python-format
 msgid "The following value is too long, only 128 characters allowed: %s"
 msgstr "Der folgende Wert ist zu lang, es sind nur 128 Zeichen erlaubt: %s"
 
-#: umc/python/setup/__init__.py:340
+#: umc/python/setup/__init__.py:365
 #, python-format
 msgid "The following value is too long, only 64 characters allowed: %s"
 msgstr "Der folgende Wert ist zu lang, es sind nur 64 Zeichen erlaubt: %s"
 
-#: umc/python/setup/__init__.py:315
+#: umc/python/setup/__init__.py:340
 msgid ""
 "The hostname is not a valid fully qualified domain name in lowercase (e.g. "
 "host.example.com)."
@@ -166,17 +171,17 @@ msgstr ""
 "Der Rechnernamen muss vollständig und klein geschrieben sein (z.B. host."
 "example.com)."
 
-#: umc/python/setup/__init__.py:323
+#: umc/python/setup/__init__.py:348
 msgid ""
 "The length of fully qualified domain name is greater than 63 characters."
 msgstr "Die Länge von System- und Domänennamen ist größer als 63 Zeichen."
 
-#: umc/python/setup/__init__.py:328
+#: umc/python/setup/__init__.py:353
 msgid ""
 "The length of the windows domain name needs to be smaller than 14 characters."
 msgstr "Die Länge des Windows Domain Names ist größer als 14 Zeichen."
 
-#: umc/python/setup/__init__.py:333
+#: umc/python/setup/__init__.py:358
 msgid ""
 "The root password is too short. For security reasons, your password must "
 "contain at least 8 characters."
@@ -184,16 +189,16 @@ msgstr ""
 "Das Root-Passwort ist zu kurz, aus Sicherheitsgründen sollte es mindestens 8 "
 "Zeichen lang sein."
 
-#: umc/python/setup/__init__.py:334
+#: umc/python/setup/__init__.py:359
 msgid "The root password may only contain ascii characters."
 msgstr "Für das Root-Passwort sind nur ASCII-Zeichen erlaubt."
 
-#: umc/python/setup/__init__.py:413
+#: umc/python/setup/__init__.py:438
 #, python-format
 msgid "The specified IP address (%s) is not valid: %s"
 msgstr "Die angegebenen IP-Adresse (%s) ist nicht gültig: %s"
 
-#: umc/python/setup/__init__.py:397
+#: umc/python/setup/__init__.py:422
 #, python-format
 msgid ""
 "The specified IPv6 identifier may only consist of letters and numbers: %s"
@@ -201,68 +206,36 @@ msgstr ""
 "Der angegebene IPv6-Bezeichner kann nur aus Buchstaben und Zahlen bestehen: "
 "%s"
 
-#: umc/python/setup/__init__.py:417
+#: umc/python/setup/__init__.py:442
 #, python-format
 msgid "The specified gateway IPv4 address is not valid: %s"
 msgstr "Die angegebene Gateway-IPv4-Adresse ist nicht gültig: %s"
 
-#: umc/python/setup/__init__.py:419
+#: umc/python/setup/__init__.py:444
 #, python-format
 msgid "The specified gateway IPv6 address is not valid: %s"
 msgstr "Die angegebene Gateway-IPv6-Adresse is nicht gültig: %s"
 
-#: umc/python/setup/__init__.py:422
+#: umc/python/setup/__init__.py:447
 #, python-format
 msgid ""
 "The specified proxy address is not valid (e.g., http://10.201.1.1:8080): %s"
 msgstr "Die Proxy-Adresse ist nicht gültig (z.B. http://10.201.1.1:8080): %s"
 
-#: umc/python/setup/__init__.py:312
+#: umc/python/setup/__init__.py:333
 msgid ""
 "The system role may not change on a system that has already joined to domain."
 msgstr ""
 "Die Systemrolle darf sich auf einem bereits gejointen System nicht mehr "
 "ändern."
 
-#: umc/python/setup/__init__.py:327
+#: umc/python/setup/__init__.py:352
 msgid "The windows domain name can only consist of upper case characters."
 msgstr "Die Windows-Domäne kann nur aus Großbuchstaben bestehen."
 
-#: umc/python/setup/__init__.py:329
+#: umc/python/setup/__init__.py:354
 msgid "The windows domain name is not valid."
 msgstr "Die Windows-Domäne ist nicht gültig."
-
-#: usr/lib/univention-system-setup/scripts/software/10software:108
-msgid "Autoremove failed"
-msgstr "Deinstallation unbenutzter Pakete fehlgeschlagen"
-
-#: usr/lib/univention-system-setup/scripts/software/10software:108
-msgid "Deleting unneeded %s"
-msgstr "Deinstalliere unbenutztes Paket %s"
-
-#: usr/lib/univention-system-setup/scripts/software/10software:108
-msgid "Failed to get package for Role %s"
-msgstr "Es konnte kein Paket für die Rolle %s gefunden werden"
-
-#: usr/lib/univention-system-setup/scripts/software/10software:108
-msgid "No such package"
-msgstr "Paket existiert nicht"
-
-#: usr/lib/univention-system-setup/scripts/software/10software:92
-msgid "Failed to uninstall"
-msgstr "Deinstallation fehlgeschlagen"
-
-#: usr/lib/univention-system-setup/scripts/software/10software:108
-msgid "Failed to install"
-msgstr "Installation fehlgeschlagen"
-
-#: usr/lib/univention-system-setup/scripts/software/10software:108
-msgid "Fetch failed"
-msgstr "Herunterladen fehlgeschlagen"
-
-#: usr/lib/univention-system-setup/scripts/software/10software:108
-msgid "Failed to lock"
-msgstr "Sperren des Systems fehlgeschlagen"
 
 #~ msgid "Failed to shut down the web browser."
 #~ msgstr "Der Beendevorgang des Web-Browsers ist fehlgeschlagen."

commit 0256d8c3a00e88982986521a19b9d11ee92c2126

fix(USS): Remove undefined _*_error variables
Instance variables ProgressState._{join,misc}_error were removed, but
`__bool__()` was not updated.

Fixes: 840f2d9ef64bab3486baaa3c87e64ef69e71d69c
Bug #55632

base/univention-system-setup/umc/python/setup/util.py
@@ -369,7 +369,7 @@ class ProgressState(object):
         return not self.__eq__(other)
 
     def __bool__(self) -> bool:
-        return bool(self.name or self.message or self.percentage or self._join_error or self._misc_error)
+        return bool(self.name or self.message or self.percentage)
     __nonzero__ = __bool__
 
 