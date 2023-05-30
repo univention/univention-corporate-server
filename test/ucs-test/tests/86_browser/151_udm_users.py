722740e25062575fc9dadcfd08ed804d7a52bc52
Marius Meschter <marius.meschter@univention.de>
2023-05-30 13:42:02 +0200

    test(browser): migrate 151_udm_users to playwright
    Bug #56054

--- test/ucs-test/tests/86_browser/151_udm_users.py
+++ test/ucs-test/tests/86_browser/151_udm_users.py
@@ -1,4 +1,4 @@
-#!/usr/share/ucs-test/runner /usr/share/ucs-test/selenium
+#!/usr/share/ucs-test/runner /usr/share/ucs-test/playwright
 # -*- coding: utf-8 -*-
 ## desc: Various test for UDM users/user
 ## packages:
@@ -11,130 +11,89 @@
 ## join: true
 ## exposure: dangerous
 
-import univention.testing.selenium.udm as selenium_udm
+import time
+
+import pytest
+from playwright.sync_api import expect
+
 import univention.testing.strings as uts
-import univention.testing.ucr as ucr_test
-import univention.testing.udm as udm_test
 from univention.lib.i18n import Translation
-from univention.testing import selenium
-from univention.testing.utils import get_ldap_connection
+from univention.testing.browser.generic_udm_module import UserModule
+from univention.testing.browser.udm_users import User, create_test_user
 
 
-_ = Translation('ucs-test-selenium').translate
+_ = Translation("ucs-test-browser").translate
 
 
-class UmcUdmError(Exception):
-    pass
+@pytest.fixture()
+def test_user(lo, udm):
+    return create_test_user(udm, lo)
 
 
-class UMCTester(object):
+def test_move_user_into_container_and_out_again(user_module: UserModule, test_user: User, udm, ucr, ldap_base):
+    position = ucr.get("ldap/base")
+    cn_name = uts.random_string()
+    udm.create_object("container/cn", position=position, name=cn_name)
+    ou_name = uts.random_string()
+    udm.create_object("container/ou", position=position, name=ou_name)
 
-    def test_umc(self):
-        self.users = selenium_udm.Users(self.selenium)
-        userdn = self.udm.create_user()[0]
-        lo = get_ldap_connection()
-        user_object = lo.get(userdn)
-        user = {}
-        user['username'] = user_object['uid'][0].decode('utf-8')
-        user['lastname'] = user_object['sn'][0].decode('utf-8')
-
-        self.selenium.do_login()
-        self.selenium.open_module(self.users.name)
-        self.users.wait_for_main_grid_load()
-
-        self.move_user_into_containers_and_out_again(user)
-        self.test_user_templates()
-
-    def move_user_into_containers_and_out_again(self, user):
-        print('*** move user into different container')
-        position = self.ucr.get('ldap/base')
-        cn_name = uts.random_string()
-        self.udm.create_object('container/cn', position=position, name=cn_name)
-        ou_name = uts.random_string()
-        self.udm.create_object('container/ou', position=position, name=ou_name)
-
-        self.move_user(user, cn_name)
-        self.move_user(user, ou_name)
-        self.move_user(user, 'users')
-
-    def test_user_templates(self):
-        print('*** testing user templates')
-        self.test_description_template()
-        self.test_group_template()
-
-    def test_description_template(self):
-        description_template = uts.random_string()
-        self.udm.create_object(
-            'settings/usertemplate',
-            position='cn=templates,cn=univention,%s' % (self.ucr.get('ldap/base'),),
-            name=description_template,
-            # Using description instead of mailPrimaryAddress here, because
-            # mailPrimaryAddress always gets lower-cased.
-            description='<firstname:lower,umlauts>.<lastname>[0:2]@test.com',
-        )
-
-        # The user-template would not be available when adding a user without
-        # this, sometimes.
-        self.selenium.open_module(self.users.name)
-        self.users.wait_for_main_grid_load()
-
-        user_description_template = self.users.add(
-            template=description_template, firstname=u'Bärbel', lastname='Edison',
-        )
-
-        self.users.open_details(user_description_template)
-        expected_description = 'baerbel.Ed@test.com'
-        if self.users.get_description() != expected_description:
-            raise UmcUdmError(
-                'Setting the description via a usertemplate did not work. '
-                'The generated description was %r instead of %r .'
-                % (self.users.get_description(), expected_description),
-            )
-        self.users.close_details()
-
-        self.users.delete(user_description_template)
-
-    def test_group_template(self):
-        secondary_group_template = uts.random_string()
-        self.udm.create_object(
-            'settings/usertemplate',
-            position='cn=templates,cn=univention,%s' % (self.ucr.get('ldap/base'),),
-            name=secondary_group_template,
-            groups='cn=Domain Admins,cn=groups,%s' % (self.ucr.get('ldap/base'),),
-        )
-
-        # The user-template would not be available when adding a user without
-        # this, sometimes.
-        self.selenium.open_module(self.users.name)
-        self.users.wait_for_main_grid_load()
-
-        user_group_template = self.users.add(
-            template=secondary_group_template, firstname='Thomas', lastname='Edison',
-        )
-
-        self.users.open_details(user_group_template)
-        self.selenium.click_tab(_('Groups'))
-        self.selenium.wait_for_text(_('Domain Admins'))
-        self.users.close_details()
-
-        self.users.delete(user_group_template)
-
-    # container_name has to be a direct child of the LDAP-base.
-    def move_user(self, user, container_name):
-        print('*** moving user %s into %s' % (user['username'], container_name))
-        self.selenium.click_checkbox_of_grid_entry(self.users._get_grid_value(user))
-        self.selenium.click_text(_('more'))
-        self.selenium.click_text(_('Move to...'))
-        self.selenium.click_tree_entry(container_name, scroll_into_view=True)
-        self.selenium.click_button(_('Move user'))
-        self.users.wait_for_main_grid_load()
+    user_module.navigate()
+    assert move_user(user_module, test_user, cn_name, udm) == f"uid={test_user.username},cn={cn_name},{ldap_base}"
+    assert move_user(user_module, test_user, ou_name, udm) == f"uid={test_user.username},ou={ou_name},{ldap_base}"
+    assert move_user(user_module, test_user, "users", udm) == f"uid={test_user.username},cn=users,{ldap_base}"
 
 
-if __name__ == '__main__':
-    with ucr_test.UCSTestConfigRegistry() as ucr, udm_test.UCSTestUDM() as udm, selenium.UMCSeleniumTest() as s:
-        umc_tester = UMCTester()
-        umc_tester.ucr = ucr
-        umc_tester.udm = udm
-        umc_tester.selenium = s
+def move_user(user_module: UserModule, test_user: User, container_name: str, udm) -> str:
+    user_module.tester.check_checkbox_in_grid_by_name(test_user.username)
+    user_module.page.get_by_role("button", name=_("More")).click()
+    user_module.page.get_by_role("cell", name=_("Move to...")).click()
+    user_module.page.get_by_role("gridcell", name=container_name, exact=True).click()
+    user_module.page.get_by_role("button", name=_("Move User")).click()
+    time.sleep(3)
+    props = udm.list_objects("users/user", filter=f"username={test_user.username}")[0]
 
-        umc_tester.test_umc()
+    return next(prop for prop in props if prop.startswith("uid="))
+
+
+def test_user_templates_description(user_module: UserModule, udm):
+    description_template = uts.random_string()
+    udm.create_object(
+        "settings/usertemplate",
+        position=f"cn=templates,cn=univention,{user_module.tester.ldap_base}",
+        name=description_template,
+        description="<firstname:lower,umlauts>.<lastname>[0:2]@test.com",
+    )
+    user_module.navigate()
+    created_item = user_module.create_object(name="template_description", first_name="Bärbel", last_name="Edison", template=description_template)
+    details = user_module.open_details(created_item.identifying_name)
+
+    heading = user_module.page.get_by_role("heading", name=_("Basic settings"))
+    expect(heading).to_be_visible()
+
+    description = user_module.page.get_by_label(_("Description")).last.input_value()
+    try:
+        assert description == "baerbel.Ed@test.com"
+    finally:
+        details.save()
+        user_module.delete(created_item)
+
+
+def test_user_templates_group(user_module: UserModule, udm):
+    secondary_group_template = uts.random_string()
+    udm.create_object(
+        "settings/usertemplate",
+        position=f"cn=templates,cn=univention,{user_module.tester.ldap_base}",
+        name=secondary_group_template,
+        groups=f"cn=Domain Admins,cn=groups,{user_module.tester.ldap_base}",
+    )
+
+    user_module.navigate()
+    created_item = user_module.create_object(name="template_group", first_name="Thomas", last_name="Edison", template=secondary_group_template)
+    details_view = user_module.open_details(created_item.identifying_name)
+    details_view.open_tab(_("Groups"))
+    try:
+        loc = user_module.page.get_by_text(_("Domain Admins"))
+        expect(loc).to_be_visible()
+    finally:
+        details_view.save()
+        user_module.delete(created_item)
