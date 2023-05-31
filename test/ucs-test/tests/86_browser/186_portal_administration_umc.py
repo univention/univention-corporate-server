002b06c9a4878128dc305daa1604fedf9605f2f8
Marius Meschter <marius.meschter@univention.de>
2023-05-31 15:25:00 +0200

    test(browser): migrate 186_portal... to playwright
    migrate 186_portal_administration_umc to playwright
    
    Bug #56054

--- test/ucs-test/tests/86_browser/186_portal_administration_umc.py
+++ test/ucs-test/tests/86_browser/186_portal_administration_umc.py
@@ -1,4 +1,4 @@
-#!/usr/share/ucs-test/runner /usr/share/ucs-test/selenium
+#!/usr/share/ucs-test/runner /usr/share/ucs-test/playwright
 # -*- coding: utf-8 -*-
 ## desc: Test creating a portal via UMC
 ## roles:
@@ -8,72 +8,60 @@
 ## join: true
 ## exposure: dangerous
 
-import logging
 import subprocess
 
-from selenium.common.exceptions import TimeoutException
+import pytest
+from playwright.sync_api import expect
 
-import univention.config_registry
-import univention.testing.selenium.udm as selenium_udm
-import univention.testing.ucr as ucr_test
 from univention.lib.i18n import Translation
-from univention.testing import selenium
-from univention.testing.udm import UCSTestUDM
+from univention.testing.browser import logger
+from univention.testing.browser.generic_udm_module import PortalModule
+from univention.testing.browser.lib import UMCBrowserTest
 from univention.udm import UDM
 
 
-logger = logging.getLogger(__name__)
+_ = Translation("ucs-test-browser").translate
 
-_ = Translation('ucs-test-selenium').translate
-
-
-class PortalNotSetException(Exception):
-    pass
+udm = UDM.admin().version(2)
 
 
-class UMCTester(object):
-
-    def test_umc(self):
-        try:
-            self.do_test()
-        finally:
-            self.cleanup()
-
-    def do_test(self):
-        self.udm = UDM.admin().version(1)
-        self.selenium.do_login()
-
-        logger.info('Create portal via UMC and set this host as server')
-        self.portals = selenium_udm.Portals(self.selenium)
-        self.selenium.open_module('Portal')
-        self.portal_name = self.portals.add()
-        portal = list(self.udm.get('portals/portal').search('name={}'.format(self.portal_name)))[0]
-        portal_dname = portal.props.displayName['en_US']
-        univention.config_registry.handler_set(['portal/default-dn=%s' % portal.dn])
-        subprocess.call(['univention-portal', 'update'])
-        logger.info('Visiting portal')
-        self.selenium.driver.get(self.selenium.base_url)
-        try:
-            self.selenium.wait_for_text(portal_dname)
-        except TimeoutException:
-            raise PortalNotSetException('The portal added via UMC is not visible')
-
-    def cleanup(self):
-        logger.info('Cleanup')
-        if hasattr(self, 'portal_name'):
-            logger.info('Delete portal created via UMC')
-            try:
-                self.udm.obj_by_dn('cn=%s,cn=portal,cn=portals,cn=univention,%s' % (self.portal_name, self.ucr.get('ldap/base'))).delete()
-            except Exception:
-                pass
+class PortalContext:
+    def __init__(self) -> None:
+        self.dn = ""
 
 
-if __name__ == '__main__':
-    with ucr_test.UCSTestConfigRegistry() as ucr, UCSTestUDM() as udm_test, selenium.UMCSeleniumTest() as s:
-        umc_tester = UMCTester()
-        umc_tester.ucr = ucr
-        umc_tester.udm_test = udm_test
-        umc_tester.selenium = s
+@pytest.fixture()
+def portal_context(ucr):
+    old_portal_dn = ucr.get("portal/default-dn")
+    portal_context = PortalContext()
 
-        umc_tester.test_umc()
-    subprocess.call(['univention-portal', 'update'])
+    yield portal_context
+    try:
+        udm.obj_by_dn(portal_context.dn).delete()
+    except Exception:
+        logger.exception("failed to delete created portal with ")
+    finally:
+        set_portal(old_portal_dn, ucr)
+
+
+def test_portal(umc_browser_test: UMCBrowserTest, ucr, portal_context):
+    portal_module = PortalModule(umc_browser_test)
+    portal_module.navigate()
+
+    created_item = portal_module.add()
+    portal_module.tester.check_checkbox_in_grid_by_name(created_item.identifying_name)
+
+    portal = next(iter(udm.get("portals/portal").search(f"name={created_item.identifying_name}")))
+    portal_context.dn = portal.dn
+    portal_dname = portal.props.displayName["en_US"]
+
+    set_portal(portal.dn, ucr)
+
+    umc_browser_test.login(location="/univention/portal")
+    portal_name = umc_browser_test.page.get_by_text(portal_dname)
+    expect(portal_name, f"expected new portal name '{portal_name}' to be visible").to_be_visible()
+
+
+def set_portal(dn: str, ucr):
+    ucr.handler_set([f"portal/default-dn={dn}"])
+    subprocess.run(["/usr/sbin/univention-portal", "update"], check=True)
