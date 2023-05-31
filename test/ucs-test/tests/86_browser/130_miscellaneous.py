bbee489e3ad1ac38d0f40ffd88054875212e37fe
Marius Meschter <marius.meschter@univention.de>
2023-05-31 12:58:30 +0200

    test(browser): migrate 130_miscel... to playwright
    Bug #56054

--- test/ucs-test/tests/86_browser/130_miscellaneous.py
+++ test/ucs-test/tests/86_browser/130_miscellaneous.py
@@ -1,4 +1,4 @@
-#!/usr/share/ucs-test/runner /usr/share/ucs-test/selenium
+#!/usr/share/ucs-test/runner /usr/share/ucs-test/playwright
 # -*- coding: utf-8 -*-
 ## desc: test language switch, logout, module visibility, process timeout
 ## packages:
@@ -10,181 +10,151 @@
 ##  - skip_admember
 ## join: true
 ## exposure: dangerous
+from __future__ import annotations
 
+import re
 import time
+from typing import Set
 
 import psutil
-from selenium.webdriver.common.by import By
+import pytest
+from playwright.sync_api import BrowserContext, expect
 
-import univention.testing.ucr as ucr_test
-import univention.testing.udm as udm_test
-from univention.config_registry import handler_set
 from univention.lib.i18n import Translation
-from univention.testing import selenium
+from univention.testing.browser.lib import UCSLanguage, UMCBrowserTest
+from univention.testing.browser.process_overview import ProcessOverview
+from univention.testing.browser.sidemenu import SideMenu
 
 
-translator = Translation('ucs-test-selenium')
+translator = Translation("ucs-test-browser")
 _ = translator.translate
 
 
-class UmcError(Exception):
-    pass
+def test_switch_language(umc_browser_test: UMCBrowserTest):
+    original_language = umc_browser_test.lang
+    target_language = UCSLanguage.DE_DE if umc_browser_test.lang == UCSLanguage.EN_US else UCSLanguage.EN_US
+    side_menu = SideMenu(umc_browser_test)
+    side_menu.navigate()
+    side_menu.switch_to_language(target_language)
+
+    translator.set_language(str(target_language).replace("-", "_"))
+
+    umc_browser_test.end_umc_session()
+    umc_browser_test.login()
+    expect(side_menu.page.get_by_role("button", name=_("Favorites"))).to_be_visible()
+
+    translator.set_language(str(original_language).replace("-", "_"))
 
 
-class UMCTester(object):
+def test_logout_with_side_menu(umc_browser_test: UMCBrowserTest):
+    umc_browser_test.login()
+    favorites_button = umc_browser_test.page.get_by_role("button", name=_("Favorites"))
+    expect(favorites_button).to_be_visible()
 
-    def test_umc(self):
-        self.selenium.do_login()
-        self.test_language_switch()
-        self.test_logout()
-        try:
-            with ucr_test.UCSTestConfigRegistry():
-                self.test_module_process_timeout()
-        finally:
-            self.selenium.restart_umc()
-        self.test_module_visibility_for_regular_user()
+    context = umc_browser_test.page.context
 
-    def test_language_switch(self):
-        switch_to_language = 'de' if (self.selenium.language != 'de') else 'en'
-        self.switch_language(switch_to_language)
-        self.switch_language(self.selenium.language)
+    assert search_for_session_id_cookie(context), "No UMCSessionid cookie found after login"
 
-    def test_logout(self):
-        self.selenium.open_side_menu()
-        self.selenium.click_text(_('Logout'))
-        self.selenium.wait_for_text(_('really want to logout'))
-        self.selenium.click_button(_('Logout'), xpath_prefix="//div[contains(@class, 'dijitDialog')]")
+    umc_browser_test.logout()
 
-        self.selenium.do_login(username=self.selenium.umcLoginUsername)
+    exp = re.compile(".*/univention/portal.*")
+    umc_browser_test.page.wait_for_url(exp)
 
-    def test_module_process_timeout(self, timeout=30):
-        handler_set(['umc/module/timeout=%s' % (timeout,)])
-
-        self.kill_all_module_processes()
-        self.selenium.open_module(_('Univention Configuration Registry'))
-        self.selenium.wait_for_text('apache2')
-        self.selenium.open_module(_('Process overview'))
-        self.selenium.wait_for_text(_('generates an overview'))
-
-        loop_end = time.time() + timeout + 10
-        while time.time() < loop_end:
-            self.selenium.submit_input('pattern')
-            self.selenium.wait_until_all_standby_animations_disappeared()
-
-        if self.module_process_alive('ucr'):
-            raise UmcError(
-                "A module's process still exists after it's timeout.",
-            )
-        if not self.module_process_alive('top'):
-            raise UmcError("A module's process died before it's timeout.")
-
-    def test_module_visibility_for_regular_user(self):
-        username = 'umc_test_user'
-        self.udm.create_user(username=username, password='univention')
-        self.selenium.end_umc_session()
-
-        self.selenium.do_login(username=username, password='univention')
-        self.selenium.end_umc_session()
-
-        self.add_user_policy(username)
-        self.add_group_policy(username)
-
-        self.selenium.do_login(username=username, password='univention')
-        self.check_if_allowed_modules_are_visible()
-
-    def add_user_policy(self, username):
-        self.udm.create_object(
-            'policies/umc',
-            name='username_policy',
-            allow='cn=top-all,cn=operations,cn=UMC,cn=univention,%s' % (self.selenium.ldap_base,),
-            position='cn=policies,%s' % (self.selenium.ldap_base,),
-        )
-        self.udm.modify_object(
-            'users/user',
-            dn='uid=%s,cn=users,%s' % (username, self.selenium.ldap_base),
-            policy_reference='cn=username_policy,cn=policies,%s' % (self.selenium.ldap_base,),
-        )
-
-    def add_group_policy(self, username):
-        self.udm.create_object(
-            'groups/group',
-            name='umc_test_group',
-            position='cn=groups,%s' % (self.selenium.ldap_base,),
-        )
-        self.udm.modify_object(
-            'users/user',
-            dn='uid=%s,cn=users,%s' % (username, self.selenium.ldap_base),
-            append={'groups': ['cn=umc_test_group,cn=groups,%s' % (self.selenium.ldap_base,)]},
-        )
-        self.udm.create_object(
-            'policies/umc',
-            name='umc_test_group_policy',
-            allow='cn=ucr-all,cn=operations,cn=UMC,cn=univention,%s' % (self.selenium.ldap_base,),
-            position='cn=policies,%s' % (self.selenium.ldap_base,),
-        )
-        self.udm.modify_object(
-            'groups/group',
-            dn='cn=umc_test_group,cn=groups,%s' % (self.selenium.ldap_base,),
-            policy_reference='cn=umc_test_group_policy,cn=policies,%s' % (self.selenium.ldap_base,),
-        )
-
-    def check_if_allowed_modules_are_visible(self):
-        available_modules = self.get_available_modules()
-        required_modules = [
-            _('Univention Configuration Registry'),
-            _('Process overview'),
-        ]
-        differing_modules = {
-            module.lower() for module in required_modules
-        }.symmetric_difference({
-            module.lower() for module in available_modules
-        })
-        if len(differing_modules) > 0:
-            raise UmcError(
-                'Applying module-visibility-policies for a regular user did not'
-                ' work.\nThese modules are missing or excess in the UMC: %r'
-                % (differing_modules,),
-            )
-
-    def kill_all_module_processes(self):
-        return  # not necessary, causes Chromium to hang...
-        for process in psutil.process_iter():
-            if '/usr/sbin/univention-management-console-module' in process.cmdline():
-                process.kill()
-
-    def module_process_alive(self, module):
-        return any({'/usr/sbin/univention-management-console-module', '-m', module}.issubset(set(process.cmdline())) for process in psutil.process_iter())
-
-    def switch_language(self, target_language_code):
-        iso_639_1_to_name = {
-            'en': 'English',
-            'de': 'Deutsch',
-            'fr': u'Français',
-        }
-        target_language = iso_639_1_to_name[target_language_code]
-        self.selenium.open_side_menu()
-        self.selenium.click_text(_('Switch language'))
-        self.selenium.click_text(target_language)
-        self.selenium.click_button(_('Switch language'))
-
-        translator.set_language(target_language_code)
-
-        self.selenium.end_umc_session()
-        self.selenium.do_login(language=target_language_code)
-
-    def get_available_modules(self):
-        self.selenium.search_module('*')
-
-        xpath = '//*[contains(concat(" ", normalize-space(@class), " "), " umcGalleryName ")]'
-        tile_headings = self.selenium.driver.find_elements(By.XPATH, xpath)
-
-        return [tile_heading.get_attribute("title") if tile_heading.get_attribute("title") else tile_heading.text for tile_heading in tile_headings]
+    assert not search_for_session_id_cookie(context), "UMCSessionid cookie found after logout"
 
 
-if __name__ == '__main__':
-    with udm_test.UCSTestUDM() as udm, selenium.UMCSeleniumTest() as s:
-        umc_tester = UMCTester()
-        umc_tester.udm = udm
-        umc_tester.selenium = s
+def search_for_session_id_cookie(context: BrowserContext) -> bool:
+    return any("name" in cookie and cookie["name"] == "UMCSessionId" for cookie in context.cookies())
 
-        umc_tester.test_umc()
+
+@pytest.fixture()
+def module_process_timeout(umc_browser_test: UMCBrowserTest, ucr):
+    umc_browser_test.restart_umc()
+    timeout = 30
+    ucr.handler_set([f"umc/module/timeout={timeout}"])
+    yield timeout
+    ucr.revert_to_original_registry()
+    umc_browser_test.restart_umc()
+
+
+def test_module_process_timeout(umc_browser_test: UMCBrowserTest, module_process_timeout: int):
+    umc_browser_test.login()
+    umc_browser_test.open_module("Univention Configuration Registry")
+    expect(umc_browser_test.page.get_by_role("grid")).to_be_visible()
+    time.sleep(5)
+    process_overview = ProcessOverview(umc_browser_test)
+    umc_browser_test.open_module("Process overview")
+
+    timeout_end = time.time() + module_process_timeout + 10
+
+    while time.time() < timeout_end:
+        search_bar = umc_browser_test.page.get_by_text("Search...").last
+        search_bar.click(force=True)
+        with umc_browser_test.page.expect_response(process_overview.grid_load_url):
+            search_bar.press("Enter")
+
+        time.sleep(10)
+
+    assert not module_process_alive("ucr"), "A module's process still exists after its timeout"
+    assert module_process_alive("top"), "A module's process dies before its timeout"
+
+
+def module_process_alive(module) -> bool:
+    return any({"/usr/sbin/univention-management-console-module", "-m", module}.issubset(set(process.cmdline())) for process in psutil.process_iter())
+
+
+def test_module_visibility_for_regular_user(umc_browser_test: UMCBrowserTest, udm):
+    username = udm.create_user()[1]
+    umc_browser_test.login(username, check_for_no_module_available_popup=True)
+
+    add_user_policy(udm, umc_browser_test, username)
+    add_group_policy(udm, umc_browser_test, username)
+
+    umc_browser_test.login(username, skip_xhr_check=True)
+
+    allowed_modules: Set[str] = {
+        _("Univention Configuration Registry"),
+        _("Process overview"),
+    }
+
+    assert (
+        set(umc_browser_test.get_available_modules()) & allowed_modules == allowed_modules
+    ), "Modules 'Process overview' and 'Univention Configuration Registry'  should be visible"
+
+
+def add_user_policy(udm, umc_browser_test: UMCBrowserTest, username: str):
+    udm.create_object(
+        "policies/umc",
+        name="username_policy",
+        allow=f"cn=top-all,cn=operations,cn=UMC,cn=univention,{umc_browser_test.ldap_base}",
+        position=f"cn=policies,{umc_browser_test.ldap_base}",
+    )
+    udm.modify_object(
+        "users/user",
+        dn=f"uid={username},cn=users,{umc_browser_test.ldap_base}",
+        policy_reference=f"cn=username_policy,cn=policies,{umc_browser_test.ldap_base}",
+    )
+
+
+def add_group_policy(udm, umc_browser_test: UMCBrowserTest, username: str):
+    udm.create_object(
+        "groups/group",
+        name="umc_test_group",
+        position=f"cn=groups,{umc_browser_test.ldap_base}",
+    )
+    udm.modify_object(
+        "users/user",
+        dn=f"uid={username},cn=users,{umc_browser_test.ldap_base}",
+        append={"groups": [f"cn=umc_test_group,cn=groups,{umc_browser_test.ldap_base}"]},
+    )
+    udm.create_object(
+        "policies/umc",
+        name="umc_test_group_policy",
+        allow=f"cn=ucr-all,cn=operations,cn=UMC,cn=univention,{umc_browser_test.ldap_base}",
+        position=f"cn=policies,{umc_browser_test.ldap_base}",
+    )
+    udm.modify_object(
+        "groups/group",
+        dn=f"cn=umc_test_group,cn=groups,{umc_browser_test.ldap_base}",
+        policy_reference=f"cn=umc_test_group_policy,cn=policies,{umc_browser_test.ldap_base}",
+    )
