610476aed19c3d1543a6ad9313fe3eca0c3b9572
Philipp Hahn <hahn@univention.de>
2023-06-09 17:31:48 +0200

    test: Use 5.0-4+e709 KVM image
    Bug #55702

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -6,7 +6,7 @@
 # defaults for release
 release='5.0-4'
 old_release='4.4-9'
-kvm_template_version='5.0-3+e572'
+kvm_template_version='5.0-4+e709'
 # AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 4
 current_ami=ami-051aaf4d1cd9dedfb
 # AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 11
