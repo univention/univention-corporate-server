5674f513e9b34bcdd60c3a6bc07d04a4c59d3c2a
Philipp Hahn <hahn@univention.de>
2023-06-16 17:54:32 +0200

    refactor(UDL): Merge fix_cache_permissions.sh
    into univention-directory-listener.service itself.
    
    Bug #56247

--- management/univention-directory-listener/tools/fix_cache_permissions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-#
-# SPDX-FileCopyrightText: 2020-2025 Univention GmbH
-# SPDX-License-Identifier: AGPL-3.0-only
-
-cachedir=/var/lib/univention-directory-listener
-for dir in "$cachedir" /var/lib/univention-ldap/listener; do
-	find "$dir" ! -user listener -exec chown listener {} \;
-done
