5c2414bdb741aefac89de961f111d8aba28ece35
Florian Best <best@univention.de>
2023-06-27 10:27:30 +0200

    Revert "test(mail): partly revert "use systemctl for reloading amavis and postfix""
    This reverts commit 5a2357116063876ba29c95718a7e306d7cf6ef5b.
    
    Issue #1738
    Bug #55715

--- test/ucs-test/tests/40_mail/essential/mail.py
+++ test/ucs-test/tests/40_mail/essential/mail.py
@@ -305,8 +305,8 @@ def reload_postfix():
 def reload_amavis_postfix():
     for cmd in (
             ['newaliases'],
-            ['/etc/init.d/amavis', 'force-reload'],
-            ['/etc/init.d/postfix', 'force-reload'],
+            ['systemctl', 'reload', 'amavis'],
+            ['systemctl', 'reload', 'postfix'],
     ):
         try:
             print(repr(cmd))
