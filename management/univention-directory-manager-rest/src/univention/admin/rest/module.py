7e6b6faf958cf702ef5df0a67b59308a34cc2c17
Florian Best <best@univention.de>
2023-06-27 09:00:47 +0200

    Bug #56189: fix accessing objects with UTF-8 character in its DN

--- management/univention-directory-manager-rest/src/univention/admin/rest/module.py
+++ management/univention-directory-manager-rest/src/univention/admin/rest/module.py
@@ -867,7 +867,7 @@ class ResourceBase:
         def quote_param(s):
             for char in '\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f':  # remove non printable characters
                 s = s.replace(char, '')
-            return s.replace('\\', '\\\\').replace('"', '\\"')
+            return s.encode('ISO8859-1', 'replace').decode('ISO8859-1').replace('\\', '\\\\').replace('"', '\\"')
         kwargs['rel'] = relation
         params = []
         for param in ('rel', 'name', 'title', 'media'):
