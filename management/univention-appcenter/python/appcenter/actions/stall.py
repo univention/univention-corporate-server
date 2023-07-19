d5c6e532448584592760666de8cd35335c271e4a
Marius Meschter <marius.meschter@univention.de>
2023-07-19 15:19:15 +0200

    chore(appcenter): fix typo in univention-app stall
    Bug #56047

--- management/univention-appcenter/python/appcenter/actions/stall.py
+++ management/univention-appcenter/python/appcenter/actions/stall.py
@@ -41,7 +41,7 @@ from univention.appcenter.ucr import ucr_save
 
 class Stall(UniventionAppAction):
     """
-    Disbales updates for this app. Useful for suppressing
+    Disables updates for this app. Useful for suppressing
     warnings when an app reached its end of life but shall still
     be used.
     """
