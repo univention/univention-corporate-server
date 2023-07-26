20fb5ec752de2725c69c59858415713c8c2e3ce2
Philipp Hahn <hahn@univention.de>
2023-07-26 14:43:11 +0200

    style(test): make TMP_ variable local
    Bug #15204

--- test/ucs-test/lib/printer.sh
+++ test/ucs-test/lib/printer.sh
@@ -7,11 +7,11 @@
 . "$TESTLIBPATH/random.sh" || exit 137
 
 getprintername () { # Generate a name for a printer. E.g. PRINTERNAME=$(getprintername)
-    random_chars 10 "${_lowerletters}${_ciphers}"
+	random_chars 10 "${_lowerletters}${_ciphers}"
 }
 
 create_localprinter () { #Creates a printer. E.g. createlocalprinter $PRINTERNAME
-	TMP_PRINTERNAME="${1:?printername, e.g \$(getprintername)}"
+	local TMP_PRINTERNAME="${1:?printername, e.g \$(getprintername)}"
 	shift
 	info "create printer $TMP_PRINTERNAME"
 
@@ -25,16 +25,16 @@ create_localprinter () { #Creates a printer. E.g. createlocalprinter $PRINTERNAM
 }
 
 remove_printer () { # Remove a printer. E.g. removeprinter $PRINTERNAME
-	TMP_PRINTERNAME="${1:?printername}"
+	local TMP_PRINTERNAME="${1:?printername}"
 	info "remove printer $TMP_PRINTERNAME"
 	udm-test shares/printer remove \
 		--dn "cn=$TMP_PRINTERNAME,cn=printers,${ldap_base:?}"
 }
 
 set_printer_sambaname () { # Set the Sambaname for a printer. E.g. setsambaname $PRINTERNAME $PRINTERSAMBANAME
-	PRINTERNAME="${1:?printer name}"
-	SAMBANAME="${2:?samba printer name}"
-	info  "setting the sambaName for printer $PRINTERNAME to the value $SAMBANAME"
+	local PRINTERNAME="${1:?printer name}"
+	local SAMBANAME="${2:?samba printer name}"
+	info "setting the sambaName for printer $PRINTERNAME to the value $SAMBANAME"
 	udm-test shares/printer modify \
 		--dn "cn=$PRINTERNAME,cn=printers,${ldap_base:?}" \
 		--set sambaName="$SAMBANAME"
