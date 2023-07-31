univention/univention-corporate-server

commit cf5a4ecccfdd7f07bfeca05a765ad3134ff24b5a

refactor(umc): replace deprecated API with new UMC Python API
replace `self.{username,password,user_dn,bind_user_connection}` and others
with the provided alternative in `request`

Bug #56201

management/univention-management-console-module-udm/umc/python/udm/__init__.py
@@ -171,12 +171,13 @@ class Instance(Base, ProgressMixin):
         self.__license_checks = set()
         install_opener(ucr)
 
-    def init(self):
-        if not self.user_dn:
-            raise UserWithoutDN(self._username)
+    def prepare(self, request):
+        super(Instance, self).prepare(request)
+        if not request.user_dn:
+            raise UserWithoutDN(request.username)
 
-        MODULE.info('Initializing module as user %r' % (self.user_dn,))
-        set_bind_function(self.bind_user_connection)
+        MODULE.info('Initializing module as user %r' % (request.user_dn,))
+        set_bind_function(request.bind_user_connection)
 
         # read user settings and initial UDR
         self.reports_cfg = udr.Config()
@@ -507,7 +508,7 @@ class Instance(Base, ProgressMixin):
         result = []
         for ldap_dn in request.options:
             if request.flavor == 'users/self':
-                ldap_dn = self._user_dn
+                ldap_dn = request.user_dn
             obj, module = self.get_obj_module(request.flavor, ldap_dn)
             if module is None:
                 raise ObjectDoesNotExist(ldap_dn)

management/univention-appcenter/umc/python/appcenter/__init__.py
@@ -219,7 +219,7 @@ class Instance(umcm.Base, ProgressMixin):
         info = get_action('info')
         return {'compatible': info.is_compatible(version, function=function), 'version': info.get_ucs_version()}
 
-    def _remote_appcenter(self, host, function=None):
+    def _remote_appcenter(self, request, host, function=None):
         if host is None:
             raise ValueError('Cannot connect to None')
         if not host.endswith('.%s' % self.ucr.get('domainname')):
@@ -229,7 +229,7 @@ class Instance(umcm.Base, ProgressMixin):
         if function is not None:
             opts['function'] = function
         try:
-            client = Client(host, self.username, self.password)
+            client = Client(host, request.username, request.password)
             response = client.umc_command('appcenter/version2', opts)
         except (HTTPError) as exc:
             raise umcm.UMC_Error(_('Problems connecting to {0} ({1}). Please update {0}!').format(host, exc.message))
@@ -276,8 +276,8 @@ class Instance(umcm.Base, ProgressMixin):
         settings=DictSanitizer({}, required=True),
         dry_run=BooleanSanitizer(),
     )
-    @simple_response(with_progress=True)
-    def run(self, progress, apps, auto_installed, action, hosts, settings, dry_run):
+    @simple_response(with_progress=True, with_request=True)
+    def run(self, request, progress, apps, auto_installed, action, hosts, settings, dry_run):
         localhost = get_local_fqdn()
         ret = {}
         if dry_run:
@@ -287,7 +287,7 @@ class Instance(umcm.Base, ProgressMixin):
                     ret[host] = self._run_local_dry_run(_apps, action, {}, progress)
                 else:
                     try:
-                        ret[host] = self._run_remote_dry_run(host, _apps, action, auto_installed, {}, progress)
+                        ret[host] = self._run_remote_dry_run(request, host, _apps, action, auto_installed, {}, progress)
                     except umcm.UMC_Error:
                         ret[host] = {'unreachable': [app.id for app in _apps]}
         else:
@@ -301,7 +301,7 @@ class Instance(umcm.Base, ProgressMixin):
                     if host == localhost:
                         host_result[app.id] = self._run_local(app, action, _settings, auto_installed, progress)
                     else:
-                        host_result[app.id] = self._run_remote(host, app, action, auto_installed, _settings, progress)[app.id]
+                        host_result[app.id] = self._run_remote(request, host, app, action, auto_installed, _settings, progress)[app.id]
                     if not host_result[app.id]['success']:
                         break
         return ret
@@ -355,18 +355,18 @@ class Instance(umcm.Base, ProgressMixin):
         finally:
             action.logger.removeHandler(handler)
 
-    def _run_remote_dry_run(self, host, apps, action, auto_installed, settings, progress):
-        return self._run_remote_logic(host, apps, action, auto_installed, settings, progress, dry_run=True)
+    def _run_remote_dry_run(self, request, host, apps, action, auto_installed, settings, progress):
+        return self._run_remote_logic(request, host, apps, action, auto_installed, settings, progress, dry_run=True)
 
-    def _run_remote(self, host, app, action, auto_installed, settings, progress):
-        return self._run_remote_logic(host, [app], action, auto_installed, settings, progress, dry_run=False)
+    def _run_remote(self, request, host, app, action, auto_installed, settings, progress):
+        return self._run_remote_logic(request, host, [app], action, auto_installed, settings, progress, dry_run=False)
 
-    def _run_remote_logic(self, host, apps, action, auto_installed, settings, progress, dry_run):
+    def _run_remote_logic(self, request, host, apps, action, auto_installed, settings, progress, dry_run):
         if len(apps) == 1:
             progress.title = _('%s: Connecting to %s') % (apps[0].name, host)
         else:
             progress.title = _('%d Apps: Connecting to %s') % (len(apps), host)
-        client = self._remote_appcenter(host, function='appcenter/run')
+        client = self._remote_appcenter(request, host, function='appcenter/run')
         opts = {'apps': [str(app) for app in apps], 'auto_installed': auto_installed, 'action': action, 'hosts': {host: [app.id for app in apps]}, 'settings': settings, 'dry_run': dry_run}
         progress_id = client.umc_command('appcenter/run', opts).result['id']
         while True:
@@ -450,10 +450,10 @@ class Instance(umcm.Base, ProgressMixin):
 
     @require_apps_update
     @require_password
-    @simple_response(with_progress=True)
-    def sync_ldap(self):
+    @simple_response(with_progress=True, with_request=True)
+    def sync_ldap(self, request):
         register = get_action('register')
-        register.call(username=self.username, password=self.password)
+        register.call(username=request.username, password=request.password)
 
     # used in updater-umc
     @simple_response
@@ -556,7 +556,7 @@ class Instance(umcm.Base, ProgressMixin):
         except LockError:
             raise umcm.UMC_Error(_('Another package operation is in progress'))
 
-    def _install_master_packages_on_hosts(self, app, function):
+    def _install_master_packages_on_hosts(self, request, app, function):
         if function.startswith('upgrade'):
             remote_function = 'update-schema'
         else:
@@ -575,7 +575,7 @@ class Instance(umcm.Base, ProgressMixin):
         for host, host_is_master in hosts:
             package_manager.progress_state.info(_('Installing LDAP packages on %s') % host)
             try:
-                if not self._install_master_packages_on_host(app, remote_function, host):
+                if not self._install_master_packages_on_host(request, app, remote_function, host):
                     error_message = 'Unable to install %r on %s. Check /var/log/univention/management-console-module-appcenter.log on the host and this server. All errata updates have been installed on %s?' % (master_packages, host, host)
                     raise Exception(error_message)
             except Exception as e:
@@ -591,8 +591,8 @@ class Instance(umcm.Base, ProgressMixin):
             finally:
                 package_manager.add_hundred_percent()
 
-    def _install_master_packages_on_host(self, app, function, host):
-        client = Client(host, self.username, self.password)
+    def _install_master_packages_on_host(self, request, app, function, host):
+        client = Client(host, request.username, request.password)
         result = client.umc_command('appcenter/invoke', {'function': function, 'application': app.id, 'force': True, 'dont_remote_install': True}).result
         if result['can_continue']:
             all_errors = self._query_remote_progress(client)

management/univention-management-console/umc-module-templates/grid_with_detailpage/umc/python/MODULEID/__init__.py
@@ -71,7 +71,7 @@ class Instance(Base):
 
     def init(self):
         # this initialization method is called when the module process is created
-        pass
+        super(Instance, self).init()
 
     def colors(self, request):
         """Returns a list of all existing colors."""

management/univention-management-console/umc-module-templates/simple_form/umc/python/MODULEID/__init__.py
@@ -54,7 +54,7 @@ class Instance(Base):
         # this initialization method is called when the
         # module process is started and the configuration from the
         # UMC server is completed
-        pass
+        super(Instance, self).init()
 
     def configuration(self, request):
         """Returns a directionary of initial values for the form."""

management/univention-self-service/umc/python/passwordreset/__init__.py
@@ -239,20 +239,13 @@ class Instance(Base):
         self._usersmod = None
         self.groupmod = None
 
-        def ucr_try_int(variable, default):
-            try:
-                return int(ucr.get(variable, default))
-            except ValueError:
-                MODULE.error('UCR variables %s is not a number, using default: %s' % (variable, default))
-                return default
-
-        self.token_validity_period = ucr_try_int("umc/self-service/passwordreset/token_validity_period", 3600)
-        limit_total_minute = ucr_try_int("umc/self-service/passwordreset/limit/total/minute", 0)
-        limit_total_hour = ucr_try_int("umc/self-service/passwordreset/limit/total/hour", 0)
-        limit_total_day = ucr_try_int("umc/self-service/passwordreset/limit/total/day", 0)
-        self.limit_user_minute = ucr_try_int("umc/self-service/passwordreset/limit/per_user/minute", 0)
-        self.limit_user_hour = ucr_try_int("umc/self-service/passwordreset/limit/per_user/hour", 0)
-        self.limit_user_day = ucr_try_int("umc/self-service/passwordreset/limit/per_user/day", 0)
+        self.token_validity_period = ucr.get_int("umc/self-service/passwordreset/token_validity_period", 3600)
+        limit_total_minute = ucr.get_int("umc/self-service/passwordreset/limit/total/minute", 0)
+        limit_total_hour = ucr.get_int("umc/self-service/passwordreset/limit/total/hour", 0)
+        limit_total_day = ucr.get_int("umc/self-service/passwordreset/limit/total/day", 0)
+        self.limit_user_minute = ucr.get_int("umc/self-service/passwordreset/limit/per_user/minute", 0)
+        self.limit_user_hour = ucr.get_int("umc/self-service/passwordreset/limit/per_user/hour", 0)
+        self.limit_user_day = ucr.get_int("umc/self-service/passwordreset/limit/per_user/day", 0)
 
         self.total_limits = [
             ("t:c_minute", 60, limit_total_minute),
@@ -549,15 +542,15 @@ class Instance(Base):
     @sanitize(
         username=StringSanitizer(required=DISALLOW_AUTHENTICATION, minimum=1),
         password=StringSanitizer(required=DISALLOW_AUTHENTICATION, minimum=1))
-    @simple_response
-    def set_user_attributes(self, attributes, username=None, password=None):
+    @simple_response(with_request=True)
+    def set_user_attributes(self, request, attributes, username=None, password=None):
         dn, username = self.authenticate_user(username, password)
-        username = username or self.username
+        username = username or request.username
         if password:
             dn, username = self.auth(username, password)
             lo, po = get_user_connection(binddn=dn, bindpw=password)
         else:
-            lo = self.get_user_ldap_connection(write=True)
+            lo = request.get_user_ldap_connection(write=True)
             po = univention.admin.uldap.position(lo.base)
 
         if self.is_blacklisted(username, 'profiledata'):

services/univention-admin-diary/umc/python/admindiary/__init__.py
@@ -141,7 +141,7 @@ class Instance(Base):
     def get_query_limit(self):
         return get_query_limit()
 
-    @simple_response
-    def add_comment(self, context_id, message):
-        add_comment(message, context_id, self.username)
+    @simple_response(with_request=True)
+    def add_comment(self, request, context_id, message):
+        add_comment(message, context_id, request.username)
         time.sleep(1)  # give backend time to insert comment...

management/univention-management-console/src/univention/management/console/base.py
@@ -254,6 +254,10 @@ class Base(Translation):
     def tornado_routes(self):
         return []
 
+    def prepare(self, request):
+        """this function is invoked after the module process started."""
+        self.init()
+
     def init(self):
         """this function is invoked after the module process started."""
 

management/univention-management-console/src/univention/management/console/modserver.py
@@ -281,7 +281,7 @@ class ModuleServer(object):
 
             MODULE.debug('Initializing module.')
             try:
-                self.__handler.init()
+                self.__handler.prepare(msg)
             except BaseException:
                 MODULE.error('module init() failed; respond then shutdown')
                 signal.alarm(3)

commit 6f8173043edfa3269473162590b9d70286617d79

fix(umc-udm): fix licence check after LDAP bind
The previous overwritten `bind_user_connection()` did `self.require_license()` which was not overtaken.

Fixes: cf5a4ecccfdd7f07bfeca05a765ad3134ff24b5a
Bug #56201

management/univention-management-console-module-udm/umc/python/udm/__init__.py
@@ -176,7 +176,12 @@ class Instance(Base, ProgressMixin):
             raise UserWithoutDN(request.username)
 
         MODULE.info('Initializing module as user %r' % (request.user_dn,))
-        set_bind_function(request.bind_user_connection)
+
+        def bind_user_connection(lo):
+            request.bind_user_connection(lo)
+            self.require_license(lo)
+
+        set_bind_function(bind_user_connection)
 
         # read user settings and initial UDR
         self.reports_cfg = udr.Config()
@@ -193,10 +198,6 @@ class Instance(Base, ProgressMixin):
             raise LDAP_AuthenticationFailed()
         if isinstance(exc, (udm_errors.base, LDAPError)):
             MODULE.error(''.join(traceback.format_exception(etype, exc, etraceback)))
-
-    def bind_user_connection(self, lo):
-        super(Instance, self).bind_user_connection(lo)
-        self.require_license(lo)
 
     def require_license(self, lo):
         if id(lo) in self.__license_checks: