bc0e8bc26316e8c836ec2d0d4d015e9e2d4f2e55
Daniel Troeder <troeder@univention.de>
2023-09-08 13:56:51 +0200

    Issue #1733: fix Jenkins job for ox-connector

---
+++ test/product-tests/component/ox-connector.sh
@@ -0,0 +1,166 @@
+#
+# Like what you see? Join us!
+# https://www.univention.com/about-us/careers/vacancies/
+#
+# Copyright 2023 Univention GmbH
+#
+# https://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <https://www.gnu.org/licenses/>.
+
+create_proxy_http_conf () {
+  local UCS_IP="$1"
+  cat > /etc/apache2/conf-available/proxy_http.conf <<- EOF
+<IfModule mod_proxy_http.c>
+   ProxyRequests Off
+   ProxyStatus On
+   # When enabled, this option will pass the Host: line from the incoming request to the proxied host.
+   ProxyPreserveHost On
+   # Please note that the servlet path to the soap API has changed:
+   <Location /webservices>
+       # restrict access to the soap provisioning API
+       Order Deny,Allow
+       Deny from all
+       Allow from 127.0.0.1
+       # you might add more ip addresses / networks here
+       Allow from $UCS_IP
+   </Location>
+
+   # The old path is kept for compatibility reasons
+   <Location /servlet/axis2/services>
+       Order Deny,Allow
+       Deny from all
+       Allow from 127.0.0.1
+   </Location>
+
+   # Enable the balancer manager mentioned in
+   # https://oxpedia.org/wiki/index.php?title=AppSuite:Running_a_cluster#Updating_a_Cluster
+   <IfModule mod_status.c>
+     <Location /balancer-manager>
+       SetHandler balancer-manager
+       Order Deny,Allow
+       Deny from all
+       Allow from 127.0.0.1
+     </Location>
+   </IfModule>
+
+   <Proxy balancer://oxcluster>
+       Order deny,allow
+       Allow from all
+       # multiple server setups need to have the hostname inserted instead localhost
+       BalancerMember http://localhost:8009 timeout=100 smax=0 ttl=60 retry=60 loadfactor=50 route=APP1
+       # Enable and maybe add additional hosts running OX here
+       # BalancerMember http://oxhost2:8009 timeout=100 smax=0 ttl=60 retry=60 loadfactor=50 route=APP2
+      ProxySet stickysession=JSESSIONID|jsessionid scolonpathdelim=On
+      SetEnv proxy-initial-not-pooled
+      SetEnv proxy-sendchunked
+   </Proxy>
+
+   # The standalone documentconverter(s) within your setup (if installed)
+   # Make sure to restrict access to backends only
+   # See: https://httpd.apache.org/docs/$YOUR_VERSION/mod/mod_authz_host.html#allow for more infos
+   #<Proxy balancer://oxcluster_docs>
+   #    Order Deny,Allow
+   #    Deny from all
+   #    Allow from backend1IP
+   #    BalancerMember http://converter_host:8009 timeout=100 smax=0 ttl=60 retry=60 loadfactor=50 keepalive=On  route=APP3
+   #    ProxySet stickysession=JSESSIONID|jsessionid scolonpathdelim=On
+   #	   SetEnv proxy-initial-not-pooled
+   #    SetEnv proxy-sendchunked
+   #</Proxy>
+   # Define another Proxy Container with different timeout for the sync clients. Microsoft recommends a minimum value of 15 minutes.
+   # Setting the value lower than the one defined as com.openexchange.usm.eas.ping.max_heartbeat in eas.properties will lead to connection
+   # timeouts for clients.  See https://support.microsoft.com/?kbid=905013 for additional information.
+   #
+   # NOTE for Apache versions < 2.4:
+   # When using a single node system or using BalancerMembers that are assigned to other balancers please add a second hostname for that
+   # BalancerMember's IP so Apache can treat it as additional BalancerMember with a different timeout.
+   #
+   # Example from /etc/hosts: 127.0.0.1	localhost localhost_sync
+   #
+  # Alternatively select one or more hosts of your cluster to be restricted to handle only eas/usm requests
+  <Proxy balancer://eas_oxcluster>
+     Order deny,allow
+     Allow from all
+     # multiple server setups need to have the hostname inserted instead localhost
+     BalancerMember http://localhost_sync:8009 timeout=1900 smax=0 ttl=60 retry=60 loadfactor=50 route=APP1
+     # Enable and maybe add additional hosts running OX here
+     # BalancerMember http://oxhost2:8009 timeout=1900  smax=0 ttl=60 retry=60 loadfactor=50 route=APP2
+     ProxySet stickysession=JSESSIONID|jsessionid scolonpathdelim=On
+     SetEnv proxy-initial-not-pooled
+     SetEnv proxy-sendchunked
+  </Proxy>
+
+  # When specifying additional mappings via the ProxyPass directive be aware that the first matching rule wins. Overlapping urls of
+  # mappings have to be ordered from longest URL to shortest URL.
+  #
+  # Example:
+  #   ProxyPass /ajax      balancer://oxcluster_with_100s_timeout/ajax
+  #   ProxyPass /ajax/test balancer://oxcluster_with_200s_timeout/ajax/test
+  #
+  # Requests to /ajax/test would have a timeout of 100s instead of 200s
+  #
+  # See:
+  # - https://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypass Ordering ProxyPass Directives
+  # - https://httpd.apache.org/docs/current/mod/mod_proxy.html#workers Worker Sharing
+  ProxyPass /ajax balancer://oxcluster/ajax
+  ProxyPass /appsuite/api balancer://oxcluster/ajax
+  ProxyPass /drive balancer://oxcluster/drive
+  ProxyPass /infostore balancer://oxcluster/infostore
+  ProxyPass /realtime balancer://oxcluster/realtime
+  ProxyPass /servlet balancer://oxcluster/servlet
+  ProxyPass /webservices balancer://oxcluster/webservices
+
+  #ProxyPass /documentconverterws balancer://oxcluster_docs/documentconverterws
+
+  ProxyPass /usm-json balancer://eas_oxcluster/usm-json
+  ProxyPass /Microsoft-Server-ActiveSync balancer://eas_oxcluster/Microsoft-Server-ActiveSync
+
+</IfModule>
+EOF
+}
+
+create_000_default_conf () {
+  cat > /etc/apache2/sites-available/000-default.conf <<- EOF
+<VirtualHost *:80>
+       IncludeOptional /etc/apache2/ucs-sites.conf.d/*.conf
+
+       ServerAdmin webmaster@localhost
+
+       DocumentRoot /var/www/html
+       <Directory /var/www/html>
+               Options -Indexes +FollowSymLinks +MultiViews
+               AllowOverride None
+               Order allow,deny
+               allow from all
+               RedirectMatch ^/$ /appsuite/
+       </Directory>
+
+       <Directory /var/www/html/appsuite>
+               Options None +SymLinksIfOwnerMatch
+               AllowOverride Indexes FileInfo
+       </Directory>
+</VirtualHost>
+EOF
+}
