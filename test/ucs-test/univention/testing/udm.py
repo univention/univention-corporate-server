200afb4ddee2e56cfccc94f3df5102e08f3f348e
Florian Best <best@univention.de>
2023-09-19 14:55:32 +0200

    refactor(python): drop Python 2 support: inspect.getargspec()
    Bug #56604

--- test/ucs-test/univention/testing/udm.py
+++ test/ucs-test/univention/testing/udm.py
@@ -56,7 +56,7 @@ import shlex
 import subprocess
 import sys
 import time
-from inspect import getfullargspec as getargspec
+from inspect import getfullargspec
 from typing import Any, Dict, Iterable, List, Mapping, Optional, Tuple, Union  # noqa: F401
 
 import ldap
@@ -741,7 +741,7 @@ class UCSTestUDM:
         }
 
         def _func(func, prop):
-            if 'prop' in getargspec(func).args:
+            if 'prop' in getfullargspec(func).args:
                 func = functools.partial(func, prop)
             return func
 
