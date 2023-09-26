af5adbc02b753b46547313dff7f5915ec6177c69
Felix Botner <botner@univention.de>
2023-09-26 10:25:06 +0200

    Bug #56524: changed performance test settings to
    reduce errors due to timeout

--- test/utils/keycloaklocust.py
+++ test/utils/keycloaklocust.py
@@ -23,54 +23,84 @@ def on_init(environment, **kwargs):
     JmeterListener(environment)
 
 
-def logout_at_idp(client, host):
-    logout = "/univention/logout"
-    uri = host + logout
-    with client.get(uri, name="/univention/logout/", allow_redirects=True, timeout=30, catch_response=True) as req3:
-        if not (200 <= req3.status_code <= 399):
-            return
+def get_login_link(content, res):
+    if content is None or len(content) == 0:
+        res.failure("no content in response")
+        return None
+    soup = BeautifulSoup(content, features="lxml")
+    login_link = soup.find("form", {"id": "kc-form-login"})
+    if not login_link:
+        res.failure(f"no login link in response: {content}")
+        return None
+    return html.unescape(login_link.get("action"))
 
 
-def login_at_idp_with_credentials(client, login_link, user):
-    data = {'username': user.username, 'password': user.password}
-    name = login_link.split("?")[0]
-    with client.post(login_link, name=name, allow_redirects=True, timeout=30, catch_response=True, data=data) as res:
-        if res.status_code != 200:
-            return
-        soup = BeautifulSoup(res.content, features="lxml")
-        saml_response = soup.find("input", {"name": "SAMLResponse"})
-        if not saml_response:
-            res.failure(f"no saml response in: {res.text}")
-            return
-
-
-def entry(client, host, user):
-    entry = "/univention/saml/"
-    uri = host + entry
+# locust test for the login with all request
+def loginEveryRequest(client, host, user, timeout=30):
+    uri = f"{host}/univention/saml/"
     try:
-        with client.get(uri, name=uri, allow_redirects=True, timeout=30, catch_response=True) as res:
+        kerberos_redirect = None
+        login_link = None
+        with client.get(uri, name=uri, allow_redirects=True, timeout=timeout, catch_response=True) as res:
+
             if res.status_code == 401:
                 soup = BeautifulSoup(res.content.decode("utf8"), "html.parser")
                 title = soup.find("title")
                 if title and "Kerberos" in title.text:
                     res.success()
-                    action = soup.find("body").findChild("form").attrs.get("action")
-                    res = client.post(action, name=action, allow_redirects=True, timeout=30, catch_response=True)
+                    kerberos_redirect = soup.find("body").findChild("form").attrs.get("action")
+            else:
+                login_link = get_login_link(res.content, res)
 
-            if res.status_code != 200:
-                return
-            if res.content is None or len(res.content) == 0:
-                return
-            soup = BeautifulSoup(res.content, features="lxml")
-            login_link = soup.find("form", {"id": "kc-form-login"})
-            if not login_link:
-                return
-            login_link = html.unescape(login_link.get("action"))
-        login_at_idp_with_credentials(client, login_link, user)
+        if kerberos_redirect:
+            name = kerberos_redirect.split("?")[0]
+            with client.post(kerberos_redirect, name=f"{name}-KRB5", allow_redirects=True, timeout=timeout, catch_response=True) as res:
+                login_link = get_login_link(res.content, res)
+
+        if login_link:
+            name = login_link.split("?")[0]
+            data = {'username': user.username, 'password': user.password}
+            with client.post(login_link, name=name, allow_redirects=True, timeout=timeout, catch_response=True, data=data) as res:
+                soup = BeautifulSoup(res.content, features="lxml")
+                saml_response = soup.find("input", {"name": "SAMLResponse"})
+                if not saml_response:
+                    res.failure(f"no saml response in: {res.text}")
+                    return
     finally:
-        pass
-        #logout_at_idp(client, host)
-        # client.cookiejar.clear()
+        client.cookies.clear()
+
+
+# locust test for login, but only include the one UMC request for the stats
+def loginOneRequest(client, host, user, timeout=30):
+    uri = f"{host}/univention/saml/"
+    try:
+        # we only want to cound this one request to umc, everything else will be done silently
+        with client.get(uri, name=uri, allow_redirects=True, timeout=timeout, catch_response=True) as res:
+            # keycloak want kerberos by default, we have to handle this
+            # here manually by making another request to keycloak
+            if res.status_code == 401:
+                soup = BeautifulSoup(res.content.decode("utf8"), "html.parser")
+                title = soup.find("title")
+                if title and "Kerberos" in title.text:
+                    res.success()
+                    kerberos_redirect = soup.find("body").findChild("form").attrs.get("action")
+                    name = kerberos_redirect.split("?")[0]
+                    res1 = client.post(kerberos_redirect, name=name, allow_redirects=True, catch_response=True, timeout=timeout)
+                    content = res1.content
+            else:
+                content = res.content
+            # check the we got a proper response
+            login_link = get_login_link(content, res)
+            if login_link:
+                name = login_link.split("?")[0]
+                data = {'username': user.username, 'password': user.password}
+                res2 = client.post(login_link, name=name, allow_redirects=True, catch_response=True, timeout=timeout, data=data)
+                soup = BeautifulSoup(res2.content, features="lxml")
+                saml_response = soup.find("input", {"name": "SAMLResponse"})
+                if not saml_response:
+                    res.failure(f"no saml response in: {res.text}")
+    finally:
+        client.cookies.clear()
 
 
 class TestData(object):
@@ -146,15 +176,16 @@ class PrimaryAndBackup(HttpUser):
     def get_samlSession(self):
         user = self.td.walk_users()
         host = random.choice(self.hosts)
-        entry(self.client, host, user)
+        loginOneRequest(self.client, host, user)
 
 
 class PrimaryOnly(HttpUser):
     wait_time = constant_throughput(0.1)
     td = TestData()
-    host = "https://primary.ucs.test"
 
     @task
     def get_samlSession(self):
         user = self.td.walk_users()
-        entry(self.client, self.host, user)
+        host = "https://primary.ucs.test"
+        loginOneRequest(self.client, host, user)
+        #loginEveryRequest(self.client, host, user)
