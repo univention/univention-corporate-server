6a5599806fc85d3c866c3d53ebb930cf4bd1d60e
Nico Gulden <gulden@univention.de>
2023-10-05 15:46:46 +0200

    Bug #56697: Add link to architecture manual

--- doc/debian-admins/conf.py
+++ doc/debian-admins/conf.py
@@ -45,6 +45,7 @@ extensions = [
 
 intersphinx_mapping = {
     "uv-manual": ("https://docs.software-univention.de/manual/5.0/en", ("../manual/_build/html/objects.inv", None)),
+    "uv-architecture": ("https://docs.software-univention.de/architecture/5.0/en", ("../architecture/_build/html/objects.inv", None)),
 }
 
 bibtex_bibfiles = ["../bibliography.bib"]
