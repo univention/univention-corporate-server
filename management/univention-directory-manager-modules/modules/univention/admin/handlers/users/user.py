univention/univention-corporate-server

commit c62d968c35856f06c3b10bb990016dc9618b6b02

Bug #55632: improved the Python 3.11 compatiblity and the timezone handling

doc/errata/staging/univention-directory-manager-modules.yaml
@@ -1,0 +1,10 @@
+product: ucs
+release: "5.0"
+version: [3]
+scope: ucs_5.0-0-errata5.0-3
+src: univention-directory-manager-modules
+fix: 15.0.20-7
+desc: |
+ This update addresses the following issues:
+ * Future compatibility with Python 3.11 has been added.
+bug: [55632]

management/univention-directory-manager-modules/modules/univention/admin/handlers/users/user.py
@@ -46,7 +46,6 @@ from typing import List  # noqa: F401
 
 import ldap
 import passlib.hash
-import pytz
 import six
 import tzlocal
 from ldap.filter import filter_format
@@ -71,6 +70,14 @@ from univention.admin.certificate import PKIIntegration, pki_option, pki_propert
 from univention.admin.layout import Group, Tab
 from univention.lib.s4 import rids_for_well_known_security_identifiers
 
+
+try:  # Python > 3.9
+    import zoneinfo
+    utc = zoneinfo.ZoneInfo('UTC')
+except ImportError:
+    import pytz
+    zoneinfo = None
+    utc = pytz.utc
 
 if not six.PY2:
     long = int
@@ -234,7 +241,7 @@ property_descriptions = dict({
         long_description=_('This disables the account until the specified time.'),
         syntax=univention.admin.syntax.ActivationDateTimeTimezone,
         dontsearch=True,
-        default=([None, None, tzlocal.get_localzone().zone], []),
+        default=([None, None, str(tzlocal.get_localzone())], []),
     ),
     'locked': univention.admin.property(  # This property only serves two purposes: 1) filtering 2) artificial simulation of lockout
         short_description=_('Locked state of account'),
@@ -1031,13 +1038,15 @@ def datetime_from_local_datetimetimezone_tuple(local_datetimetimezone_tuple):  #
     d, t, tz = local_datetimetimezone_tuple
     # dttz_str = module.property_descriptions[key].syntax.tostring(local_datetimetimezone_tuple)
     naive_dt = datetime.strptime("%s %s" % (d, t), "%Y-%m-%d %H:%M")
+    if zoneinfo:
+        return naive_dt.astimezone(zoneinfo.ZoneInfo(tz))
     return pytz.timezone(tz).localize(naive_dt)
 
 
 def mapDateTimeTimezoneTupleToUTCDateTimeString(local_datetimetimezone_tuple, encoding=()):  # type: (List[str]) -> List[bytes]
     if local_datetimetimezone_tuple and all(local_datetimetimezone_tuple):
         dt = datetime_from_local_datetimetimezone_tuple(local_datetimetimezone_tuple)
-        return [dt.astimezone(pytz.utc).strftime("%Y%m%d%H%M%SZ").encode(*encoding)]
+        return [dt.astimezone(utc).strftime("%Y%m%d%H%M%SZ").encode(*encoding)]
     return []
 
 
@@ -1502,7 +1511,7 @@ class object(univention.admin.handlers.simpleLdap, PKIIntegration):
             self['locked'] = u'0'
         if self.hasChanged('disabled') and self['disabled'] == '0' and not self.hasChanged('accountActivationDate'):
             self['accountActivationDate'] = self.descriptions['accountActivationDate'].default(self)
-        if self['accountActivationDate'] and all(self['accountActivationDate']) and datetime.now(tz=pytz.utc) < datetime_from_local_datetimetimezone_tuple(self['accountActivationDate']):
+        if self['accountActivationDate'] and all(self['accountActivationDate']) and datetime.now(tz=utc) < datetime_from_local_datetimetimezone_tuple(self['accountActivationDate']):
             self['disabled'] = '1'
         if self['disabled'] == '1':
             self['locked'] = u'0'  # Samba/AD behavior

management/univention-directory-manager-modules/modules/univention/admin/handlers/__init__.py
@@ -80,7 +80,7 @@ except ImportError:
     ud.debug(ud.ADMIN, ud.WARN, "Failed to import univention.lib.admember")
     _prevent_to_change_ad_properties = False
 
-getfullargspec = getattr(inspect, 'getfullargspec', inspect.getargspec)
+getfullargspec = getattr(inspect, 'getfullargspec', getattr(inspect, 'getargspec', None))
 
 _Attributes = Dict[Text, Union[bytes, List[bytes]]]
 _Properties = Dict[Text, Union[Text, List[Text]]]

management/univention-directory-manager-modules/modules/univention/admin/mapping.py
@@ -52,7 +52,7 @@ try:
 except ImportError:
     pass
 
-getfullargspec = getattr(inspect, 'getfullargspec', inspect.getargspec)
+getfullargspec = getattr(inspect, 'getfullargspec', getattr(inspect, 'getargspec', None))
 
 try:
     unicode

management/univention-directory-manager-modules/modules/univention/admin/password.py
@@ -87,8 +87,12 @@ def crypt(password, method_id=None, salt=None):
             'SHA-512': '6',
         }.get(hashing_method, '6')
 
-    from crypt import crypt as _crypt
-    return _crypt(password, '$%s$%s$' % (method_id, salt))
+    if method_id == '1':
+        return passlib.hash.md5_crypt.using(salt=salt[:8]).hash(password)
+    if method_id == '5':
+        return passlib.hash.sha256_crypt.using(salt=salt, rounds=5000).hash(password)
+    if method_id == '6':
+        return passlib.hash.sha512_crypt.using(salt=salt, rounds=5000).hash(password)
 
 
 def bcrypt_hash(password):

management/univention-directory-manager-modules/modules/univention/admin/syntax.py
@@ -38,7 +38,6 @@ import base64
 import bz2
 import copy
 import datetime
-import imghdr
 import inspect
 import io
 import ipaddress
@@ -59,7 +58,6 @@ import dateutil
 import ldap
 import ldap.dn
 import PIL
-import pytz
 import six
 from ldap.filter import escape_filter_chars, filter_format
 from ldap.schema import AttributeType, ObjectClass
@@ -74,6 +72,12 @@ from univention.lib.ucs import UCS_Version
 from univention.lib.umc_module import get_mime_description, get_mime_type, image_mime_type_of_buffer
 from univention.uldap import getMachineConnection
 
+
+try:  # Python >= 3.9
+    import zoneinfo
+except ImportError:
+    zoneinfo = None
+    import pytz
 
 if TYPE_CHECKING:
     from univention.admin.uldap import access  # noqa: F401
@@ -1400,7 +1404,7 @@ class jpegPhoto(Upload):
     def parse(self, text):
         try:
             raw = base64.b64decode(text)
-            if imghdr.what(None, raw) == 'png':
+            if raw.startswith(b'\211PNG\r\n\032\n'):  # PNG
                 # convert from PNG to JPEG
                 try:
                     fp = BytesIO(raw)
@@ -1417,9 +1421,7 @@ class jpegPhoto(Upload):
                 except (KeyError, IOError, IndexError):
                     ud.debug(ud.ADMIN, ud.WARN, 'Failed to convert PNG file into JPEG: %s' % (traceback.format_exc(),))
                     raise univention.admin.uexceptions.valueError(_('Failed to convert PNG file into JPEG format.'))
-            # imghdr.what(None, base64.b64dcode(text)) == 'jpeg'  # See Bug #36304
-            # this is what imghdr.py probably does in  the future:
-            if raw[0:2] != b'\xff\xd8':
+            if raw[:2] != b'\xff\xd8' and raw[6:10] not in (b'JFIF', b'Exif'):
                 raise ValueError()
             return text
         except (base64.binascii.Error, ValueError, TypeError):
@@ -6861,6 +6863,8 @@ class TimeZone(select):
 
     @ClassProperty
     def choices(cls):
+        if zoneinfo:
+            return [(x, x) for x in zoneinfo.available_timezones()]
         return [(x, x) for x in pytz.all_timezones]
 
 

management/univention-directory-manager-modules/modules/univention/admincli/admin.py
@@ -318,7 +318,7 @@ def object_input(module, object, input, append=None, remove=None, stderr=None):
                         if "----BEGIN CERTIFICATE-----" in content:
                             content = content.replace('----BEGIN CERTIFICATE-----', '')
                             content = content.replace('----END CERTIFICATE-----', '')
-                            object[key] = base64.decodestring(content)
+                            object[key] = base64.b64decode(content.encode("utf-8")).decode("utf-8")
                         else:
                             object[key] = content
                 else:

management/univention-directory-manager-modules/modules/univention/udm/modules/generic.py
@@ -60,7 +60,7 @@ from ..exceptions import (
 from ..utils import UDebug as ud
 
 
-getfullargspec = getattr(inspect, 'getfullargspec', inspect.getargspec)
+getfullargspec = getattr(inspect, 'getfullargspec', getattr(inspect, 'getargspec', None))
 
 ucr = univention.config_registry.ConfigRegistry()
 ucr.load()

commit c19ca49c594aaba4c0e6be67a5d0b6698288bf91

fix(udm): fix Python 3.11 compatibility for timezone handling
In UCS 5.2 the removal of accountActivationDate doesn't work anymore.

$ udm users/user create --set password=univention --set lastname=foo --set username=acc1 --set accountActivationDate="$(TZ="Europe/Berlin" date -d '+1 minute' +'%Y-%m-%d %H:%M Europe/Berlin')"
WARNING: The object is not going to be created underneath of its default containers.
Object created: uid=acc1,dc=school,dc=dev
$ udm users/user modify --dn uid=acc1,$(ucr get ldap/base) --remove accountActivationDate
LDAP Error: No such attribute: modify/delete: krb5ValidStart: no such value.

[('krb5ValidStart', [b'20230901084500Z'], b''), ('displayName', b'foo', b'foo')]
(Pdb) self.oldattr['krb5ValidStart']
[b'20230901104500Z']
(Pdb) self.oldinfo['accountActivationDate']
['2023-09-01', '10:45', 'UTC']
(Pdb) univention.admin.handlers.users.user.mapDateTimeTimezoneTupleToUTCDateTimeString(self.oldinfo['accountActivationDate'])
[b'20230901084500Z']
(Pdb) univention.admin.handlers.users.user.mapDateTimeTimezoneTupleToUTCDateTimeString(self.oldinfo['accountActivationDate']) == ml[0][1]
True

→ The modlist contains the value which the map-function produces for UTC but in LDAP a different value is stored.

Fixes: c62d968c35856f06c3b10bb990016dc9618b6b02
Bug #56514

doc/errata/staging/univention-directory-manager-modules.yaml
@@ -1,0 +1,10 @@
+product: ucs
+release: "5.0"
+version: [5]
+scope: ucs_5.0-0-errata5.0-5
+src: univention-directory-manager-modules
+fix: 15.0.24-18
+desc: |
+ This update addresses the following issues:
+ * The Python 3.11 compatibiliy for timezone handling has been repaired.
+bug: [56514]

management/univention-directory-manager-modules/modules/univention/admin/handlers/users/user.py
@@ -1037,10 +1037,10 @@ def unmapWindowsFiletime(old, encoding=()):  # type: (List[bytes]) -> str
 def datetime_from_local_datetimetimezone_tuple(local_datetimetimezone_tuple):  # type: (List[str]) -> datetime.datetime
     d, t, tz = local_datetimetimezone_tuple
     # dttz_str = module.property_descriptions[key].syntax.tostring(local_datetimetimezone_tuple)
-    naive_dt = datetime.strptime("%s %s" % (d, t), "%Y-%m-%d %H:%M")
+    native_dt = datetime.strptime("%s %s" % (d, t), "%Y-%m-%d %H:%M")
     if zoneinfo:
-        return naive_dt.astimezone(zoneinfo.ZoneInfo(tz))
-    return pytz.timezone(tz).localize(naive_dt)
+        return native_dt.replace(tzinfo=zoneinfo.ZoneInfo(tz))
+    return pytz.timezone(tz).localize(native_dt)
 
 
 def mapDateTimeTimezoneTupleToUTCDateTimeString(local_datetimetimezone_tuple, encoding=()):  # type: (List[str]) -> List[bytes]