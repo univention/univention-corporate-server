50ebeb5fd0a46782f93a179e5aad01a87853f299
Felix Botner <botner@univention.de>
2023-10-05 16:43:09 +0200

    add debug param outside the loop

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -319,9 +319,11 @@ then
 		for p in ${OS_CLIENT_CONFIG_FILE:+"$OS_CLIENT_CONFIG_FILE"} "${PWD}/clouds.yaml" "${HOME}/.config/openstack/clouds.yaml" /etc/openstack/clouds.yaml
 		do
 			[ -r "$p" ] || continue
-			cmd+=(--debug -v "$p:/etc/openstack/clouds.yml:ro")
 			break
 		done
+		# TODO, remove debug after openstack testing phase
+		cmd+=(--debug)
+
 	fi
 	# interactive mode for debug
 	$debug && cmd+=("-it")
