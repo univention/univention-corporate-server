univention/univention-corporate-server

commit db0d44f0ef5a56542293538c641e4de50d07f6de

Bug #50648 UDM groups/group: performance improovement

management/univention-directory-manager-modules/modules/univention/admin/handlers/groups/group.py
@@ -250,30 +250,6 @@ mapping.register('allowedEmailUsers', 'univentionAllowedEmailUsers')
 mapping.register('allowedEmailGroups', 'univentionAllowedEmailGroups')
 
 
-def _case_insensitive_in_list(dn, list):
-	for element in list:
-		if dn.lower() == element.lower():
-			return True
-	return False
-
-
-def _case_insensitive_get_item_in_list(dn, list):
-	for element in list:
-		if dn.lower() == element.lower():
-			return element
-	return None
-
-
-def _case_insensitive_remove_from_list(dn, list):
-	remove_element = None
-	for element in list:
-		if dn.lower() == element.lower():
-			remove_element = element
-	if remove_element:
-		list.remove(remove_element)
-	return list
-
-
 class AgingCache(object):
 
 	def __new__(type, *args, **kwargs):
@@ -381,36 +357,29 @@ class object(univention.admin.handlers.simpleLdap):
 
 	def fast_member_add(self, memberdnlist, uidlist):
 		ml = []
-		uids = []
-		members = []
+		uids = set()
+		members = set()
 		searchResult = self.lo.get(self.dn, attr=['uniqueMember', 'memberUid'])
 		if searchResult:
-			uids = [x.decode('UTF-8') for x in searchResult.get('memberUid', [])]
-			members = [x.decode('UTF-8') for x in searchResult.get('uniqueMember', [])]
+			uids = {x.decode('UTF-8').lower() for x in searchResult.get('memberUid', [])}
+			members = {x.decode('UTF-8').lower() for x in searchResult.get('uniqueMember', [])}
 
-		add_uidlist = []
-		for uid in uidlist:
-			if uid and not _case_insensitive_in_list(uid, uids):
-				add_uidlist.append(uid)
+		add_uidlist = [uid for uid in uidlist if uid.lower() not in uids]
 		if add_uidlist:
-			add_uidlist = [x.encode('UTF-8') for x in add_uidlist]
-			ml.append(('memberUid', '', add_uidlist))
+			ml.append(('memberUid', b'', [x.encode('UTF-8') for x in add_uidlist]))
 
-		add_memberdnlist = []
-		for memberdn in memberdnlist:
-			if memberdn and not _case_insensitive_in_list(memberdn, members):
-				add_memberdnlist.append(memberdn)
+		add_memberdnlist = [dn for dn in memberdnlist if dn.lower() not in members]
+
 		if add_memberdnlist:
-			add_memberdnlist = [x.encode('UTF-8') for x in add_memberdnlist]
-			ml.append(('uniqueMember', '', add_memberdnlist))
+			ml.append(('uniqueMember', b'', [x.encode('UTF-8') for x in add_memberdnlist]))
 
 		if ml:
 			try:
 				return self.lo.modify(self.dn, ml)
-			except ldap.NO_SUCH_OBJECT as msg:
-				raise univention.admin.uexceptions.noObject
-			except ldap.INSUFFICIENT_ACCESS as msg:
-				raise univention.admin.uexceptions.permissionDenied
+			except ldap.NO_SUCH_OBJECT:
+				raise univention.admin.uexceptions.noObject(self.dn)
+			except ldap.INSUFFICIENT_ACCESS:
+				raise univention.admin.uexceptions.permissionDenied()
 			except ldap.LDAPError as msg:
 				raise univention.admin.uexceptions.ldapError(msg[0]['desc'])
 
@@ -419,36 +388,28 @@ class object(univention.admin.handlers.simpleLdap):
 
 	def fast_member_remove(self, memberdnlist, uidlist, ignore_license=0):
 		ml = []
-		uids = []
-		members = []
+		uids = set()
+		members = set()
 		searchResult = self.lo.get(self.dn, attr=['uniqueMember', 'memberUid'])
 		if searchResult:
-			uids = [x.decode('UTF-8') for x in searchResult.get('memberUid', [])]
-			members = [x.decode('UTF-8') for x in searchResult.get('uniqueMember', [])]
+			uids = {x.decode('UTF-8').lower() for x in searchResult.get('memberUid', [])}
+			members = {x.decode('UTF-8').lower() for x in searchResult.get('uniqueMember', [])}
 
-		remove_uidlist = []
-		for uid in uidlist:
-			if uid and _case_insensitive_in_list(uid, uids):
-				remove_uidlist.append(_case_insensitive_get_item_in_list(uid, uids))
+		remove_uidlist = [uid for uid in uidlist if uid.lower() in uids]
 		if remove_uidlist:
-			remove_uidlist = [x.encode('UTF-8') for x in remove_uidlist]
-			ml.append(('memberUid', remove_uidlist, ''))
+			ml.append(('memberUid', [x.encode('UTF-8') for x in remove_uidlist], b''))
 
-		remove_memberdnlist = []
-		for memberdn in memberdnlist:
-			if memberdn and _case_insensitive_in_list(memberdn, members):
-				remove_memberdnlist.append(_case_insensitive_get_item_in_list(memberdn, members))
+		remove_memberdnlist = [dn for dn in memberdnlist if dn.lower() in members]
 		if remove_memberdnlist:
-			remove_memberdnlist = [x.encode('UTF-8') for x in remove_memberdnlist]
-			ml.append(('uniqueMember', remove_memberdnlist, ''))
+			ml.append(('uniqueMember', [x.encode('UTF-8') for x in remove_memberdnlist], b''))
 
 		if ml:
 			try:
 				return self.lo.modify(self.dn, ml, ignore_license=ignore_license)
-			except ldap.NO_SUCH_OBJECT as msg:
-				raise univention.admin.uexceptions.noObject
-			except ldap.INSUFFICIENT_ACCESS as msg:
-				raise univention.admin.uexceptions.permissionDenied
+			except ldap.NO_SUCH_OBJECT:
+				raise univention.admin.uexceptions.noObject(self.dn)
+			except ldap.INSUFFICIENT_ACCESS:
+				raise univention.admin.uexceptions.permissionDenied()
 			except ldap.LDAPError as msg:
 				raise univention.admin.uexceptions.ldapError(msg[0]['desc'])
 
@@ -781,18 +742,14 @@ class object(univention.admin.handlers.simpleLdap):
 		# newuids = map(lambda x: x[x.find('=') + 1: x.find(',')], newmembers)
 		# self.lo.modify( group, [ ( 'memberUid', uids, newuids ) ] )
 
-	def __case_insensitive_in_list(self, dn, list):
-		for element in list:
-			if dn.lower() == element.lower():
-				return True
-		return False
+	@staticmethod
+	def __case_insensitive_in_list(dn, members):
+		return dn.lower() in (x.lower() for x in members)
 
-	def __case_insensitive_remove_from_list(self, dn, list):
-		for element in list:
-			if dn.lower() == element.lower():
-				remove_element = element
-		list.remove(remove_element)
-		return list
+	@staticmethod
+	def __case_insensitive_remove_from_list(dn, members):
+		dn_lower = dn.lower()
+		return [x for x in members if x.lower() != dn_lower]
 
 	def check_for_group_recursion(self):
 		# perform check only if membership of groups has changed
@@ -811,7 +768,7 @@ class object(univention.admin.handlers.simpleLdap):
 		# test self dependency
 		# ==> nestedGroup or memberOf contains self.dn
 		for field in ('nestedGroup', 'memberOf'):
-			if self.dn.lower() in [x.lower() for x in self.info.get(field, [])]:
+			if self.dn.lower() in (x.lower() for x in self.info.get(field, [])):
 				raise univention.admin.uexceptions.circularGroupDependency('%s ==> %s' % (cn, cn))
 
 		# test short dependencies: A -> B -> A

management/univention-directory-manager-modules/modules/univention/admin/handlers/__init__.py
@@ -3047,7 +3047,7 @@ class simpleComputer(simpleLdap):
 			groups.append(self.oldinfo.get('primaryGroup'))
 		for group in groups:
 			groupObject = univention.admin.objects.get(univention.admin.modules.get('groups/group'), self.co, self.lo, self.position, group)
-			groupObject.fast_member_remove([self.dn], self.oldattr.get('uid', []), ignore_license=1)
+			groupObject.fast_member_remove([self.dn], [x.decode('UTF-8') for x in self.oldattr.get('uid', [])], ignore_license=True)
 
 	def __update_groups_after_namechange(self):
 		oldname = self.oldinfo.get('name')

management/univention-directory-manager-modules/modules/univention/admin/handlers/users/user.py
@@ -2125,7 +2125,7 @@ class object(univention.admin.handlers.simpleLdap):
 
 		groupObjects = univention.admin.handlers.groups.group.lookup(self.co, self.lo, filter_s=filter_format(u'uniqueMember=%s', [self.dn]))
 		if groupObjects:
-			uid = univention.admin.uldap.explodeDn(self.dn, 1)[0]
+			uid = ldap.dn.str2dn(self.dn)[0][0][1]
 			for groupObject in groupObjects:
 				groupObject.fast_member_remove([self.dn], [uid], ignore_license=True)
 

commit e7e5db22c487c389b28b50c56de075ab2a1162e9

fix(udm): fix removal of non-lowercase group members in fast_member_remove()
`memberUid` is case sensitive in contrast to `uniqueMember`.
A LDAP remove operation must therefor happen with the correct case.
The lowered set put always the lowercase variant into the the remove list.
As we retrieved the current set values directly before this operation we
can use the original case.

01.12.2021 06:25:09.593 LDAP        (PROCESS): sync to ucs: Resync rejected dn: CN=Remoteuser,OU=Standard,OU=Gruppen,OU=SUB,DC=schein,DC=me
01.12.2021 06:25:09.673 LDAP        (PROCESS): sync to ucs:   [         group] [    modify] cn=remoteuser,ou=standard,ou=gruppen,ou=sub,dc=schein,dc=me
01.12.2021 06:25:10.925 LDAP        (ERROR  ): Unknown Exception during sync_to_ucs
01.12.2021 06:25:11.017 LDAP        (ERROR  ): Traceback (most recent call last):
  File "/usr/lib/python2.7/dist-packages/univention/connector/__init__.py", line 1374, in sync_to_ucs
    f(self, property_type, object)
  File "/usr/lib/python2.7/dist-packages/univention/connector/ad/__init__.py", line 187, in group_members_sync_to_ucs
    return connector.group_members_sync_to_ucs(key, object)
  File "/usr/lib/python2.7/dist-packages/univention/connector/ad/__init__.py", line 2189, in group_members_sync_to_ucs
    ucs_admin_object.fast_member_remove(uniqueMember_del, memberUid_del, ignore_license=1)
  File "/usr/lib/python2.7/dist-packages/univention/admin/handlers/groups/group.py", line 418, in fast_member_remove
    return self.lo.modify(self.dn, ml, ignore_license=ignore_license)
  File "/usr/lib/python2.7/dist-packages/univention/admin/uldap.py", line 902, in modify
    raise univention.admin.uexceptions.ldapError(_err2str(msg), original_exception=msg)
ldapError: No such attribute: modify/delete: memberUid: no such value

Fixes: db0d44f0ef5a56542293538c641e4de50d07f6de

Bug #54183

doc/errata/staging/univention-directory-manager-modules.yaml
@@ -3,8 +3,10 @@ release: "5.0"
 version: [5]
 scope: ucs_5.0-0-errata5.0-5
 src: univention-directory-manager-modules
-fix: 15.0.24-18
+fix: 15.0.24-19
 desc: |
  This update addresses the following issues:
  * The Python 3.11 compatibiliy for timezone handling has been repaired.
-bug: [56514]
+ * The case sensitivity of the attribute `memberUid` is now respected when
+   removing members from a group.
+bug: [56514, 54183]

management/univention-directory-manager-modules/modules/univention/admin/handlers/groups/group.py
@@ -407,14 +407,14 @@ class object(univention.admin.handlers.simpleLdap):
 
     def fast_member_remove(self, memberdnlist, uidlist, ignore_license=False, _retry_on_attribute_error=True):
         ml = []
-        uids = set()
+        uids = {}
         members = set()
         searchResult = self.lo.get(self.dn, attr=['uniqueMember', 'memberUid'])
         if searchResult:
-            uids = {x.decode('UTF-8').lower() for x in searchResult.get('memberUid', [])}
+            uids = {x.decode('UTF-8').lower(): x.decode('UTF-8') for x in searchResult.get('memberUid', [])}
             members = {x.decode('UTF-8').lower() for x in searchResult.get('uniqueMember', [])}
 
-        remove_uidlist = [uid for uid in uidlist if uid.lower() in uids]
+        remove_uidlist = [uids[uid.lower()] for uid in uidlist if uid.lower() in uids]
         if remove_uidlist:
             ml.append(('memberUid', [x.encode('UTF-8') for x in remove_uidlist], b''))
 