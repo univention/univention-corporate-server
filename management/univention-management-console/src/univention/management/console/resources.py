univention/univention-corporate-server

commit 1c169b5aad6508004a81a1fdd03da19460365142

refactor(umc): migrate UMC-Server only parts to Python 3 syntax
also prepare for future Tornado API changes

Bug #56198

management/univention-management-console/src/univention/management/console/resources.py
@@ -125,11 +125,9 @@ class ModuleConnection(object):
         self._client.close()
         self._client = tornado.httpclient.AsyncHTTPClient()
 
-    @tornado.gen.coroutine
-    def connect(self, connect_retries=0):
+    async def connect(self, connect_retries=0):
         pass
 
-    @tornado.gen.coroutine
     def request(self, method, uri, headers=None, body=None):
         pass
 
@@ -195,7 +193,7 @@ class ModuleConnection(object):
         #    CORE.warn('Aborted module process request: %s' % (exc,))
         #    raise CouldNotConnect(exc)
 
-        raise tornado.gen.Return(response)
+        return response
 
     def get_uri(self, uri):
         return uri
@@ -237,10 +235,9 @@ class ModuleProcess(ModuleConnection):
     def set_exit_callback(self, callback):
         self.__process.set_exit_callback(callback)
 
-    @tornado.gen.coroutine
-    def connect(self, connect_retries=0):
+    async def connect(self, connect_retries=0):
         if os.path.exists(self.socket) and stat.S_ISSOCK(os.stat(self.socket).st_mode):
-            raise tornado.gen.Return(True)
+            return True
         elif connect_retries > 200:
             raise CouldNotConnect('timeout exceeded')
         elif self.__process and self.__process.proc.poll() is not None:
@@ -253,18 +250,17 @@ class ModuleProcess(ModuleConnection):
             if connect_retries and not connect_retries % 50:
                 CORE.info('No connection to module process yet')
             connect_retries += 1
-            yield tornado.gen.sleep(0.05)
-            yield self.connect(connect_retries)
+            await tornado.gen.sleep(0.05)
+            await self.connect(connect_retries)
 
-    @tornado.gen.coroutine
-    def request(self, method, uri, headers=None, body=None):
+    async def request(self, method, uri, headers=None, body=None):
         # watch the module's activity and kill it after X seconds inactivity
         self.reset_inactivity_timer()
         request_id = uuid.uuid4()
         self._active_requests.add(request_id)
 
         try:
-            response = yield self.do_request(method, uri, headers, body, self.socket)
+            response = await self.do_request(method, uri, headers, body, self.socket)
         finally:
             self._active_requests.remove(request_id)
             self.reset_inactivity_timer()
@@ -283,12 +279,11 @@ class ModuleProcess(ModuleConnection):
         if self.__process:
             tornado.ioloop.IOLoop.current().add_callback(self.stop_process)
 
-    @tornado.gen.coroutine
-    def stop_process(self):
+    async def stop_process(self):
         proc = self.__process.proc
         if proc.poll() is None:
             proc.terminate()
-        yield tornado.gen.sleep(3.0)
+        await tornado.gen.sleep(3.0)
         if proc.poll() is None:
             proc.kill()
         CORE.info('ModuleProcess: child stopped')
@@ -358,13 +353,12 @@ class ModuleProxy(ModuleConnection):
         self.proxy_address = proxy_address
         self.unix_socket = None
 
-    @tornado.gen.coroutine
-    def connect(self, connect_retries=0):
-        raise tornado.gen.Return(not self.unix_socket or os.path.exists(self.unix_socket))
+    async def connect(self, connect_retries=0):
+        return not self.unix_socket or os.path.exists(self.unix_socket)
 
-    @tornado.gen.coroutine
-    def request(self, method, uri, headers=None, body=None):
-        raise tornado.gen.Return(self.do_request(method, uri, headers, body, self.unix_socket))
+    async def request(self, method, uri, headers=None, body=None):
+        response = await self.do_request(method, uri, headers, body, self.unix_socket)
+        return response
 
     def get_uri(self, uri):
         request = urlsplit(uri)
@@ -402,9 +396,8 @@ class Nothing(Resource):
 
     requires_authentication = False
 
-    @tornado.gen.coroutine
-    def prepare(self, *args, **kwargs):
-        yield super(Nothing, self).prepare(*args, **kwargs)
+    async def prepare(self, *args, **kwargs):
+        await super(Nothing, self).prepare(*args, **kwargs)
         raise NotFound()
 
 
@@ -466,7 +459,7 @@ class Auth(Resource):
 
     requires_authentication = False
 
-    def parse_authorization(self):
+    async def parse_authorization(self):
         return  # do not call super method: prevent basic auth
 
     @sanitize(
@@ -475,8 +468,7 @@ class Auth(Resource):
         auth_type=StringSanitizer(allow_none=True),
         new_password=StringSanitizer(required=False, allow_none=True, minimum=1),
     )
-    @tornado.gen.coroutine
-    def post(self):
+    async def post(self):
         try:
             content_length = int(self.request.headers.get("Content-Length", 0))
         except ValueError:
@@ -497,7 +489,7 @@ class Auth(Resource):
         # important: must be called before the auth, to preserve session id in case of re-auth and that a user cannot choose his own session ID by providing a cookie
         sessionid = self.create_sessionid(True)
 
-        result = yield session.authenticate(self.request.body_arguments)
+        result = await session.authenticate(self.request.body_arguments)
 
         self.set_session(sessionid)
         self.set_status(result.status)
@@ -513,9 +505,8 @@ class Modules(Resource):
 
     requires_authentication = False
 
-    @tornado.gen.coroutine
-    def prepare(self):
-        yield super(Modules, self).prepare()
+    async def prepare(self):
+        await super(Modules, self).prepare()
         self.i18n = I18N_Manager()
         self.i18n['umc-core'] = I18N()
         self.i18n.set_locale(self.locale.code)
@@ -607,9 +598,8 @@ class Categories(Resource):
 
     requires_authentication = False
 
-    @tornado.gen.coroutine
-    def prepare(self):
-        yield super(Categories, self).prepare()
+    async def prepare(self):
+        await super(Categories, self).prepare()
         self.i18n = I18N_Manager()
         self.i18n['umc-core'] = I18N()
         self.i18n.set_locale(self.locale.code)
@@ -690,8 +680,7 @@ class Command(Resource):
             except KeyError:
                 pass
 
-    @tornado.gen.coroutine
-    def get(self, umcp_command, command):
+    async def get(self, umcp_command, command):
         """
         Handles a COMMAND request. The request must contain a valid
         and known command that can be accessed by the current user. If
@@ -736,10 +725,10 @@ class Command(Resource):
         CORE.info('Passing request to module %s' % (module_name,))
 
         try:
-            yield process.connect()
+            await process.connect()
             # send first command
             self.future = process.request(self.request.method, self.request.full_url(), body=self.request.body or None, headers=headers)
-            response = yield self.future
+            response = await self.future
         except concurrent.futures.CancelledError:
             raise BadGateway('%s: %s: canceled' % (self._('Connection to module process failed'), module_name))
         #except asyncio.exceptions.CancelledError:
@@ -952,8 +941,7 @@ class Set(Resource):
         use specific pathes ("set/{password,locale,user/preferences}") instead
     """
 
-    @tornado.gen.coroutine
-    def post(self):
+    async def post(self):
         is_univention_lib = self.request.headers.get('User-Agent', '').startswith('UCS/')
         for key in self.request.body_arguments:
             cls = {'password': SetPassword, 'user': SetUserPreferences, 'locale': SetLocale}.get(key)
@@ -963,7 +951,7 @@ class Set(Resource):
                 p = cls(self.application, self.request)
                 p._ = self._
                 p.finish = self.finish
-                yield p.post()
+                await p.post()
                 return
             if key == 'password':
                 self.redirect('/univention/set/password', status=307)
@@ -1005,8 +993,7 @@ class SetPassword(Resource):
         "password": StringSanitizer(required=True),
         "new_password": StringSanitizer(required=True),
     }))
-    @tornado.gen.coroutine
-    def post(self):
+    async def post(self):
         assert self.current_user.user.authenticated
         username = self.current_user.user.username
         password = self.request.body_arguments['password']['password']
@@ -1021,7 +1008,7 @@ class SetPassword(Resource):
 
         CORE.info('Changing password of user %r' % (username,))
         try:
-            yield self.current_user.change_password(args)
+            await self.current_user.change_password(args)
         except PasswordChangeFailed as exc:
             raise UMC_Error(str(exc), 400, {'new_password': '%s' % (exc,)})  # 422
         else:

management/univention-management-console/src/univention/management/console/modserver.py
@@ -382,6 +382,7 @@ class Handler(RequestHandler):
         except _Skip:
             return
 
+        self._auto_finish = False  # if methods start threads and don't await a future of it an empty response is generated because finish() will be called immediately and twice then
         self.handler.execute(method, msg)
         MODULE.debug('Executed handler')
 

management/univention-management-console/src/univention/management/console/resource.py
@@ -87,14 +87,13 @@ class Resource(RequestHandler):
     def set_default_headers(self):
         self.set_header('Server', 'UMC-Server/1.0')
 
-    @tornado.gen.coroutine
-    def prepare(self):
+    async def prepare(self):
         super(Resource, self).prepare()
         self._proxy_uri()
         self._ = self.locale.translate
         self.request.content_negotiation_lang = 'json'
         self.decode_request_arguments()
-        yield self.parse_authorization()
+        await self.parse_authorization()
         if not self.ignore_session_timeout_reset:
             self.current_user.reset_timeout()  # FIXME: order correct?
         self.check_saml_session_validity()
@@ -223,8 +222,7 @@ class Resource(RequestHandler):
             self.request.protocol = 'https'
         self.request.uri = '/univention%s' % (self.request.uri,)
 
-    @tornado.gen.coroutine
-    def parse_authorization(self):
+    async def parse_authorization(self):
         credentials = self.request.headers.get('Authorization')
         if not credentials:
             return
@@ -236,10 +234,9 @@ class Resource(RequestHandler):
         except ValueError:
             raise BadRequest('invalid Authorization')
         if scheme.lower() == u'basic':
-            yield self.basic_authorization(credentials)
+            await self.basic_authorization(credentials)
 
-    @tornado.gen.coroutine
-    def basic_authorization(self, credentials):
+    async def basic_authorization(self, credentials):
         try:
             username, password = base64.b64decode(credentials.encode('utf-8')).decode('latin-1').split(u':', 1)
         except ValueError:
@@ -247,7 +244,7 @@ class Resource(RequestHandler):
 
         sessionid = self.sessionidhash()
         session = self.current_user
-        result = yield session.authenticate({'locale': self.locale.code, 'username': username, 'password': password})
+        result = await session.authenticate({'locale': self.locale.code, 'username': username, 'password': password})
         if not session.user.authenticated:
             raise UMC_Error(result.message, result.status, result.result)
 

management/univention-management-console/src/univention/management/console/saml.py
@@ -40,7 +40,6 @@ import traceback
 import zlib
 
 import six
-import tornado
 from saml2 import BINDING_HTTP_ARTIFACT, BINDING_HTTP_POST, BINDING_HTTP_REDIRECT
 from saml2.client import Saml2Client
 from saml2.ident import code as encode_name_id, decode as decode_name_id
@@ -218,9 +217,7 @@ class SamlACS(SAMLResource):
             CORE.warn('Startup of SAML2.0 service provider failed:\n%s' % (traceback.format_exc(),))
         return False
 
-    @tornado.web.asynchronous
-    @tornado.gen.coroutine
-    def get(self):
+    async def get(self):
         binding, message, relay_state = self._get_saml_message()
 
         if message is None:
@@ -230,16 +227,15 @@ class SamlACS(SAMLResource):
         acs = self.attribute_consuming_service
         if relay_state == 'iframe-passive':
             acs = self.attribute_consuming_service_iframe
-        yield acs(binding, message, relay_state)
+        await acs(binding, message, relay_state)
 
     post = get
 
-    @tornado.gen.coroutine
-    def attribute_consuming_service(self, binding, message, relay_state):
+    async def attribute_consuming_service(self, binding, message, relay_state):
         response = self.parse_authn_response(message, binding)
         saml = SAMLUser(response, message)
 
-        yield self.pam_saml_authentication(saml)
+        await self.pam_saml_authentication(saml)
 
         # protect against javascript:alert('XSS'), mailto:foo and other non relative links!
         location = urlparse(relay_state)
@@ -249,26 +245,24 @@ class SamlACS(SAMLResource):
 
         self.redirect(location, status=303)
 
-    @tornado.gen.coroutine
-    def attribute_consuming_service_iframe(self, binding, message, relay_state):
+    async def attribute_consuming_service_iframe(self, binding, message, relay_state):
         self.request.headers['Accept'] = 'application/json'  # enforce JSON response in case of errors
         self.request.headers['X-Iframe-Response'] = 'true'  # enforce textarea wrapping
         response = self.parse_authn_response(message, binding)
         saml = SAMLUser(response, message)
 
-        yield self.pam_saml_authentication(saml)
+        await self.pam_saml_authentication(saml)
 
         self.set_header('Content-Type', 'text/html')
         data = {"status": 200, "result": {"username": saml.username}}
         self.finish(b'<html><body><textarea>%s</textarea></body></html>' % (json.dumps(data).encode('ASCII'),))
 
-    @tornado.gen.coroutine
-    def pam_saml_authentication(self, saml):
+    async def pam_saml_authentication(self, saml):
         # important: must be called before the auth, to preserve session id in case of re-auth and that a user cannot choose his own session ID by providing a cookie
         sessionid = self.create_sessionid()
 
         # TODO: drop in the future to gain performance
-        result = yield self.current_user.authenticate({
+        result = await self.current_user.authenticate({
             'locale': self.locale.code,
             'username': saml.username,
             'password': saml.message,
@@ -413,7 +407,7 @@ class SamlACS(SAMLResource):
             self.set_header(key, value)
 
         if binding in (BINDING_HTTP_ARTIFACT, BINDING_HTTP_REDIRECT):
-            self.set_status(303 if self.request.supports_http_1_1() and self.request.method == 'POST' else 302)
+            self.set_status(303 if self.request.version != "HTTP/1.0" and self.request.method == 'POST' else 302)
             if not body:
                 self.redirect(self._headers['Location'], status=self.get_status())
                 return

management/univention-management-console/src/univention/management/console/server.py
@@ -43,7 +43,6 @@ import traceback
 from argparse import ArgumentParser
 
 import atexit
-import notifier.log as nflog
 import tornado
 from concurrent.futures import ThreadPoolExecutor
 from sdnotify import SystemdNotifier
@@ -239,10 +238,6 @@ class Server(object):
         )
         self.server = server
         server.add_sockets(sockets)
-
-        # no notifier logging
-        for handler in nflog.instance.handlers[:]:
-            nflog.instance.removeHandler(handler)
 
         if self.options.log_file in {'stdout', 'stderr', '/dev/stdout', '/dev/stderr'}:
             channel = logging.StreamHandler(sys.stdout if self.options.log_file in {'stdout', '/dev/stdout'} else sys.stderr)

management/univention-management-console/src/univention/management/console/session.py
@@ -31,6 +31,7 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
+import asyncio
 import errno
 import functools
 import traceback
@@ -126,11 +127,11 @@ class Session(object):
         self.acls = IACLs(self)
         self.processes = Processes(self)
 
-    @tornado.gen.coroutine
-    def authenticate(self, args):
+    async def authenticate(self, args):
         from .server import pool
         pam = self.__auth.get_handler(args['locale'])
-        result = yield pool.submit(self.__auth.authenticate, pam, args)
+        future = pool.submit(self.__auth.authenticate, pam, args)
+        result = await asyncio.wrap_future(future)
         pam.end()
 
         if (
@@ -149,16 +150,16 @@ class Session(object):
         else:
             self.user = User()
             self.renew()
-        raise tornado.gen.Return(result)
+        return result
 
-    @tornado.gen.coroutine
-    def change_password(self, args):
+    async def change_password(self, args):
         from .server import pool
         pam = self.__auth.get_handler(args['locale'])
         username = args['username']
         password = args['password']
         new_password = args['new_password']
-        yield pool.submit(pam.change_password, username, password, new_password)
+        future = pool.submit(pam.change_password, username, password, new_password)
+        await asyncio.wrap_future(future)
         pam.end()
         self.set_credentials(username, new_password, None)
 

commit b016b22304c97c8b365598a0177c129582fc9b37

fix(umc): fix setting user preferences e.g. favorites for old clients
Fixes: 1c169b5aad6508004a81a1fdd03da19460365142

Bug #56753

doc/errata/staging/univention-management-console.yaml
@@ -1,0 +1,12 @@
+product: ucs
+release: "5.0"
+version: [5]
+scope: ucs_5.0-0-errata5.0-5
+src: univention-management-console
+fix: 12.0.31-16
+desc: |
+ This update addresses the following issues:
+ * User preferences (such as favorite UMC modules) could not be set via old
+   UMC clients from UCS systems <= UCS 5.0-3. The functionality has been
+   restored.
+bug: [56753]

management/univention-management-console/src/univention/management/console/resources.py
@@ -985,7 +985,7 @@ class SetLocale(Resource):
     requires_authentication = False
 
     @sanitize(locale=StringSanitizer(required=True))
-    def post(self):
+    async def post(self):
         self.set_header('X-UMC-Message', json.dumps('Setting a session locale is deprecated and going to be removed. Use Accept-Language header instead!'))
         locale = self.request.body_arguments['locale'].replace('-', '_')
         try:
@@ -1063,7 +1063,7 @@ class SetUserPreferences(UserPreferences):
     @sanitize(user=DictSanitizer({
         "preferences": DictSanitizer({}, required=True),
     }))
-    def post(self):
+    async def post(self):
         lo = self.current_user.get_user_ldap_connection()
         # eliminate double entries
         preferences = self._get_user_preferences(lo)