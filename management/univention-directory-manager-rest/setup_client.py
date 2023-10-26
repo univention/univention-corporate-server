fdb1c9c0aca34335c3528349604ba0752f474327
Johannes Lohmer <lohmer@univention.de>
2023-10-26 10:30:19 +0200

    feat(udm-rest-api): build the UDM REST API Client using setup.py setuptools
    We need to build multiple pip-packages from the udm-rest debian
    package. Setuptools allows arbitrary `setup-<sub-package-name>.py` files
    unlike a `pyproject.toml` based build no matter the build backend.

---
+++ management/univention-directory-manager-rest/setup_client.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Like what you see? Join us!
+# https://www.univention.com/about-us/careers/vacancies/
+#
+# Copyright 2004-2023 Univention GmbH
+#
+# https://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <https://www.gnu.org/licenses/>.
+
+import io
+import os
+from email.utils import parseaddr
+
+from debian.changelog import Changelog
+from debian.deb822 import Deb822
+from setuptools import setup
+
+
+dch = Changelog(io.open('debian/changelog', 'r', encoding='utf-8'))
+dsc = Deb822(io.open('debian/control', 'r', encoding='utf-8'))
+realname, email_address = parseaddr(dsc['Maintainer'])
+override_version = os.environ.get('PYTHON_PACKAGE_VERSION')
+
+setup(
+    description="Python and Bash clients for the UDM-REST-API",
+    url='https://www.univention.de/',
+    license='GNU Affero General Public License v3',
+
+    packages=['univention.admin.rest.client'],
+    package_dir={"": "src"},
+
+    name="univention-directory-manager-rest-client",
+    version=override_version or dch.version.full_version,
+    maintainer=realname,
+    maintainer_email=email_address,
+
+    install_requires=[
+        "uritemplate",
+        "requests",
+    ],
+    extras_require={
+        "test": ["univention-admin-rest-client-test>=0.1.0"],
+    },
+)
