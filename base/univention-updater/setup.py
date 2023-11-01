26527d59e6b0ea9b71dd6556eafa7d62ac1cdc61
Florian Best <best@univention.de>
2023-11-01 14:37:36 +0100

    refactor(python): UP036
    Bug #56604

--- base/univention-updater/setup.py
+++ base/univention-updater/setup.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-import sys
 
 from setuptools import setup
 
 
 version = open("debian/changelog").readline().split()[1][1:-1].split('A~')[0]
 
-packages = ['univention', 'univention.updater']
-if sys.version_info >= (3,):
-    packages += ['univention.updater.scripts']
+packages = ['univention', 'univention.updater', 'univention.updater.scripts']
 
 setup(
     packages=packages,
