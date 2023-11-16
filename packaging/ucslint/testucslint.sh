57ea90fd49b0d52c025ca642aeb2a2d8a5f07f0d
Philipp Hahn <hahn@univention.de>
2023-11-16 12:52:08 +0100

    test(ucslint): package via argument or CWD
    Bug #55668

--- packaging/ucslint/testucslint.sh
+++ packaging/ucslint/testucslint.sh
@@ -2,6 +2,8 @@
 #
 # Run test suite for ucslint
 #
+set -e -u -o pipefail
+
 declare -i RETVAL=0
 
 die () { # Print error and exit
@@ -25,13 +27,14 @@ __USAGE__
 TEMP=$(getopt -o 'vqch' --long 'update,verbose,clean,quiet,color,help' -n "$0" -- "$@") ||
     usage 2 >&2
 eval set -- "$TEMP"
+update=false verbose=false clean=false quiet=false red='' green='' norm=''
 while [ $# -ge 1 ]
 do
     case "$1" in
-    --update) update=1 ;;
-    --verbose|-v) verbose=1 ;;
-    --clean) clean=1 ;;
-    --quiet|-q) quiet=1 ;;
+    --update) update=true ;;
+    --verbose|-v) verbose=true ;;
+    --clean) clean=true ;;
+    --quiet|-q) quiet=true ;;
     --color|-c) red=$(tput setaf 1) green=$(tput setaf 2) norm=$(tput op) ;;
     --help|-h) usage 0 ;;
     --) shift ; break ;;
@@ -59,36 +62,49 @@ match () {
     return 1
 }
 
+test_cd () {
+    ( cd "./$dir" && "$BINPATH" -m "$MODULE" )
+}
+
+test_arg () {
+    "$BINPATH" -m "$MODULE" "$dir" | sed -e "s|${dir%/}/||g"
+    return "${PIPESTATUS[0]}"
+}
+
 for dir in testframework/*
 do
     [ -d "$dir" ] || continue
     NAME=$(basename "$dir")
     match "$NAME" "${@%/}" || continue
 
-        [ -z "$quiet" ] && echo -n "Testing $dir "
+    "$quiet" || echo -n "Testing $dir "
 
-        MODULE="${NAME:0:4}"
+    MODULE="${NAME:0:4}"
 
-        ( cd "./$dir" && "$BINPATH" -m "$MODULE" >"$tmpresult" 2>"$tmperr" )
-        ret=$?
+    for run_test in test_cd test_arg
+    do
+        ret=0
+        "$run_test" >"$tmpresult" 2>"$tmperr" || ret=$?
         ./ucslint-sort-output.py "$tmpresult" >"${dir}.test"
 
         if diff -u "${dir}.correct" "${dir}.test" >"$tmpdiff" 2>&1 && [ 1 -ne "$ret" ]
         then
-            [ -z "$quiet" ] && echo "${green}OK${norm}"
-            [ -n "$clean" ] && rm -f "${dir}.test"
+            "$quiet" || echo -n "${green}OK${norm}[${run_test#test_}]"
+            "$clean" && rm -f "${dir}.test"
         else
-            [ -z "$quiet" ] && echo "${red}FAILED${norm}"
+            "$quiet" || echo "${red:-}FAILED${norm}[${run_test#test_}]"
             [ 2 -ne "$ret" ] && cat "$tmperr"
             RETVAL+=1
-            [ -n "$verbose" ] && sed "s/^+/${red}&/;s/^-/${green}&/;s/$/${norm}/" "$tmpdiff"
+            "$verbose" && sed "s/^+/${red}&/;s/^-/${green}&/;s/$/${norm}/" "$tmpdiff"
 
-            if [ -n "$update" ]
+            if "$update"
             then
                 echo "USING TESTRESULT AS NEW TEST TEMPLATE"
                 cp "${dir}.test" "${dir}.correct"
             fi
         fi
+    done
+    echo
 done
 
 exit "$RETVAL"
