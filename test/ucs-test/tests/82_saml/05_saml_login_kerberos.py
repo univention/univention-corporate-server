96471c1a26bcc0315a2748df9f06791440ce04a8
Nikola Radovanovic <radovanovic.extern@univention.de>
2023-11-17 10:57:37 +0100

    Issue #1717: make SAML tests compatible with Keycloak
    * use correct error message in
      82_saml.11_saml_user_expire.test_expired_account
    * fix password change for keycloak in 82_saml/samltest.py
    
    univention/ucs#1717

--- test/ucs-test/tests/82_saml/05_saml_login_kerberos.py
+++ test/ucs-test/tests/82_saml/05_saml_login_kerberos.py
@@ -4,21 +4,17 @@
 ## join: true
 ## exposure: dangerous
 ## roles: [domaincontroller_master]
-## packages:
-##   - univention-samba4
 ## tags:
 ##  - skip_admember
 
-# import subprocess
+import subprocess
 
 
 def test_umc_web_server(kerberos_ticket, saml_session_kerberos):
     saml_session_kerberos.login_with_new_session_at_IdP()
     saml_session_kerberos.test_logged_in_status()
     saml_session_kerberos.logout_at_IdP()
-
     # FIXME: if KRB ticket is not destroyed, session in Keycloak is recreated
-    # subprocess.check_call(['kdestroy'])
-
+    subprocess.check_call(['kdestroy'])
     saml_session_kerberos.test_logout_at_IdP()
     saml_session_kerberos.test_logout()
