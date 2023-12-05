8082159a7e92c5bb7f09a34721e2eabc19c401f0
Arvid Requate <requate@univention.de>
2023-12-05 15:06:58 +0100

    Bug #56772: Add testcase

---
+++ test/ucs-test/tests/52_s4connector/012_sync_posix_only_group.py
@@ -0,0 +1,34 @@
+#!/usr/share/ucs-test/runner pytest-3 -s -vv
+## desc: "Create a Posix-only group in udm and check that the S4-Connector doesn't convert it"
+## exposure: safe
+## packages:
+## - univention-s4-connector
+## bugs:
+##  - 56772
+
+import pytest
+
+from univention.testing.connector_common import (
+    NormalGroup, create_udm_group, delete_udm_group, map_udm_group_to_con, verify_udm_object,
+)
+from univention.testing.udm import UCSTestUDM
+
+import s4connector
+from s4connector import connector_running_on_this_host, connector_setup
+
+
+@pytest.mark.skipif(not connector_running_on_this_host(), reason="Univention S4 Connector not configured.")
+def test_udm_group_option_unchanged_by_s4c():
+    """check that the UDM group option "posix" is not changed by the S4-Connecfor."""
+    with connector_setup("sync") as s4, UCSTestUDM() as udm:
+        udm_group = NormalGroup()
+        udm_group.group['options'] = ['posix']
+        (udm_group_dn, s4_group_dn) = create_udm_group(udm, s4, udm_group, s4connector.wait_for_sync)
+
+        del udm_group.group['options']    # verify_object doesn't handle options
+        s4.verify_object(s4_group_dn, map_udm_group_to_con(udm_group.group))
+
+        udm_group.group['sambaRID'] = []  # make verify_object confirm that this property not set
+        verify_udm_object("groups/group", udm_group_dn, udm_group.group)
+
+        delete_udm_group(udm, s4, udm_group_dn, s4_group_dn, s4connector.wait_for_sync)
