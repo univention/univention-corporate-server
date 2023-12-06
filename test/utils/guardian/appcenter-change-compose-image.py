da80e697bf6075605ffa2e18476bc4b3147fb08a
Carlos García-Mauriño Dueñas <garcia-maurino@univention.de>
2023-12-06 17:29:16 +0100

    ci(guardian): Add guardian test scenario
    Add test scenario and utilities for guardian integration tests
    
    Issue univention/components/authorization-engine/guardian#134

---
+++ test/utils/guardian/appcenter-change-compose-image.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+"""
+Change docker compose main service image to given image.
+Run this script after "univention-app update" and before
+"univention-install".
+"""
+
+import argparse
+import re
+
+import yaml
+
+from univention.appcenter.app_cache import Apps
+from univention.appcenter.ucr import ucr_run_filter
+
+
+def change_app_compose_image(args: argparse.ArgumentParser) -> None:
+
+    apps_cache = Apps()
+    candidate = apps_cache.find(args.app, latest=True)
+    compose_file = candidate.get_cache_file("compose")
+
+    with open(compose_file) as f:
+        raw_file_content = f.read()
+        compose = yaml.safe_load(ucr_run_filter(raw_file_content))
+
+    missing_images = []
+
+    current_image = compose["services"][candidate.docker_main_service]["image"]
+
+    if current_image not in raw_file_content:
+        missing_images.append(current_image)
+
+    new_file_content = re.sub(rf"(\n\s*)image: {current_image}(\n*)", rf"\1image: {args.image}\2", raw_file_content)
+
+    if args.other_service:
+        for service_name, image in args.other_service:
+            try:
+                current_image = compose["services"][service_name]["image"]
+            except KeyError as e:
+                raise Exception(f"Service {service_name} not found in compose file.") from e
+            if current_image not in new_file_content:
+                missing_images.append(current_image)
+            new_file_content = re.sub(rf"(\n\s*)image: {current_image}(\n\s*)", rf"\1image: {image}\2", new_file_content)
+
+    if missing_images:
+        raise Exception(f"Cannot find the following images in {compose_file}: " + ",".join(missing_images))
+
+    with open(compose_file, "w") as fd:
+        fd.write(new_file_content)
+
+    print(f"changed {compose_file}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Change docker compose main service image to given image")
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("-a", "--app", required=True, help="app id")
+    parser.add_argument("-i", "--image", required=True, help="the new image for the main service of the app")
+    parser.add_argument("-o", "--other_service", nargs=2, action="append", metavar=("service", "image"), required=False, help="images for other services")
+    args = parser.parse_args()
+    change_app_compose_image(args)
