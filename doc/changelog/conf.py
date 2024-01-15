univention/univention-corporate-server

commit 3042f512419761ed08ed1369e76de0bda8865205

Bug #55177: Add Sphinx structure for changelog

doc/changelog/conf.py
@@ -1,0 +1,138 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+from datetime import date
+
+# -- Project information -----------------------------------------------------
+
+# The full version, including alpha/beta/rc tags
+release = "5.0-2"
+
+version = "5.0"
+
+project = f'UCS {version} Changelog'
+copyright = '2021-{}, Univention GmbH'.format(date.today().year)
+author = ''
+
+html_show_copyright = True
+language = 'en'
+
+html_title = project
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "univention_sphinx_extension",
+    "sphinxcontrib.spelling",
+    "sphinx_last_updated_by_git",
+    "sphinx_sitemap",
+    "sphinx_copybutton",
+    "sphinxcontrib.bibtex",
+    "sphinx.ext.intersphinx",
+]
+
+intersphinx_mapping = {
+    "uv-dev-ref": ("https://docs.software-univention.de/developer-reference/5.0/en", None),
+    "uv-manual": ("https://docs.software-univention.de/manual/5.0/en", None),
+}
+
+bibtex_bibfiles = ["../bibliography.bib"]
+bibtex_encoding = "utf-8"
+bibtex_default_style = "unsrt"
+bibtex_reference_style = "label"
+
+# For Windows prompt, we still need ``> ``
+copybutton_prompt_text = r"\$ |> |.+# "
+copybutton_prompt_is_regexp = True
+copybutton_line_continuation_character = "\\"
+copybutton_here_doc_delimiter = "EOT"
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+pdf_doc_base = os.path.basename(os.path.dirname(__file__))
+
+html_theme = 'univention_sphinx_book_theme'
+html_context = {
+    "pdf_download_filename": f"{pdf_doc_base}.pdf",
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = []  # value usually is ['_static']
+html_last_updated_fmt = "%a, %d. %b %Y at %H:%M (UTC%z)"
+
+# https://github.com/mgeier/sphinx-last-updated-by-git
+git_last_updated_timezone = 'Europe/Berlin'
+
+numfig = True
+
+if "spelling" in sys.argv:
+    spelling_lang = "en_US"
+    spelling_show_suggestions = True
+    spelling_warning = True
+    spelling_word_list = []
+    # Don't load extension to speed up the job
+    extensions.remove("sphinx_last_updated_by_git")
+    extensions.remove("sphinx_sitemap")
+    # Warnings may come up by sphinx-last-updated-by-git. Shall be suppressed in spelling job
+    suppress_warnings = ['git.too_shallow', "bibtex"]
+
+if "linkcheck" in sys.argv:
+    suppress_warnings = ['git.too_shallow', "bibtex"]
+
+root_doc = "index"
+
+rst_epilog = """
+.. include:: /../substitutions.txt
+"""
+
+latex_engine = 'lualatex'
+latex_show_pagerefs = True
+latex_show_urls = "footnote"
+latex_documents = [(root_doc, f'{pdf_doc_base}.tex', "", author, "manual", False)]
+latex_elements = {
+    "papersize": "a4paper",
+    "babel": "\\usepackage{babel}",
+}
+
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-figure_language_filename
+figure_language_filename = "{root}-{language}{ext}"
+
+linkcheck_ignore = [
+    r"https://errata\.software-univention\.de/#/\?erratum=\d\.\dx\d{1,3}",
+]
+
+linkcheck_allowed_redirects = {
+    r"https://help\.univention\.com/t/\d+": r"https://help\.univention\.com/t/[\w-]+/\d+",
+}
+
+univention_use_doc_base = True

commit 9423070e4e437ba418cd78d3889ef6a5038282a5

docs(changelog): fix Undefined substitution referenced: "UCS".
Fixes: 3042f512419761ed08ed1369e76de0bda8865205
Bug #55177

doc/changelog/conf.py
@@ -121,6 +121,7 @@ root_doc = "index"
 
 rst_epilog = """
 .. include:: /../substitutions.txt
+.. include:: /../abbreviations.txt
 """
 
 latex_engine = 'lualatex'