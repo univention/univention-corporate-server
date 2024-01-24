afd609c0884b4faf6477a13316fb2a7c327ca794
Florian Best <best@univention.de>
2024-01-24 19:26:10 +0100

    test(keycloak): Adjust keycloak tests to 5.2
    Bug #56765

--- test/ucs-test/tests/81_keycloak/02_test_portal_login.py
+++ test/ucs-test/tests/81_keycloak/02_test_portal_login.py
@@ -4,15 +4,16 @@
 ## roles: [domaincontroller_master, domaincontroller_backup]
 ## exposure: dangerous
 
+import time
 from datetime import datetime, timedelta
 
 import pytest
 from selenium.webdriver.common.by import By
-from utils import keycloak_get_request, keycloak_password_change, keycloak_sessions_by_user, wait_for_id
+from utils import get_language, keycloak_get_request, keycloak_password_change, keycloak_sessions_by_user, wait_for_id
 
 from univention.lib.umc import Unauthorized
 from univention.testing.umc import Client
-from univention.testing.utils import get_ldap_connection, wait_for_listener_replication
+from univention.testing.utils import get_ldap_connection, package_installed, wait_for_listener_replication
 
 
 def test_login(portal_login_via_keycloak, udm):
@@ -31,8 +32,8 @@ def test_login_disabled_fails(portal_login_via_keycloak, keycloak_config, udm):
 
 
 def test_password_change_pwdChangeNextLogin(portal_login_via_keycloak, keycloak_config, udm):
-    username = udm.create_user(pwdChangeNextLogin=1)[1]
-    assert portal_login_via_keycloak(username, 'univention', new_password='Univention.99')
+    username = udm.create_user(password='Univention.12', pwdChangeNextLogin=1)[1]
+    assert portal_login_via_keycloak(username, 'Univention.12', new_password='Univention.99')
     assert Client(username=username, password='Univention.99')
     with pytest.raises(Unauthorized):
         Client(username=username, password='univention')
@@ -43,9 +44,10 @@ def test_password_change_wrong_old_password_fails(portal_login_via_keycloak, key
     assert portal_login_via_keycloak(username, 'univentionBAD', fails_with=keycloak_config.wrong_password_msg)
 
 
+@pytest.mark.skipif(package_installed('univention-samba4'), reason='Univention Samba 4 is and passwordhistory is not active')
 def test_password_change_same_passwords_fails(portal_login_via_keycloak, keycloak_config, portal_config, udm):
-    username = udm.create_user(pwdChangeNextLogin=1)[1]
-    driver = portal_login_via_keycloak(username, 'univention', new_password='univention', fails_with='Changing password failed. The password was already used.')
+    username = udm.create_user(password='Univention.3', pwdChangeNextLogin=1)[1]
+    driver = portal_login_via_keycloak(username, 'Univention.3', new_password='Univention.3', fails_with=keycloak_config.pw_already_used)
     wait_for_id(driver, keycloak_config.password_id)
 
 
@@ -55,7 +57,7 @@ def test_password_change_new_password_too_short_fails(portal_login_via_keycloak,
         username,
         'univention',
         new_password='a',
-        fails_with='Changing password failed. The password is too short.',
+        fails_with=keycloak_config.pw_too_short,
     )
 
 
@@ -66,7 +68,7 @@ def test_password_change_confirm_new_passwords_fails(portal_login_via_keycloak, 
         'univention',
         new_password='univention',
         new_password_confirm='univention1',
-        fails_with="Passwords don't match.",
+        fails_with=keycloak_config.pw_no_match,
     )
     wait_for_id(driver, keycloak_config.password_id)
 
@@ -78,23 +80,25 @@ def test_password_change_empty_passwords_fails(portal_login_via_keycloak, keyclo
     # just click the button without old or new passwords
     driver.find_element(By.ID, keycloak_config.password_change_button_id).click()
     error = driver.find_element(By.CSS_SELECTOR, keycloak_config.password_update_error_css_selector)
-    assert error.text == 'Please specify password.', error.text
+    lang = get_language(driver, german=True)
+    assert error.text == keycloak_config.specify_pw[lang], error.text
     wait_for_id(driver, keycloak_config.password_id)
 
 
 def test_password_change_after_second_try(portal_login_via_keycloak, keycloak_config, portal_config, udm):
-    username = udm.create_user(pwdChangeNextLogin=1)[1]
+    username = udm.create_user(password='univention', pwdChangeNextLogin=1)[1]
     driver = portal_login_via_keycloak(
         username,
         'univention',
         new_password='univention',
-        fails_with='Changing password failed. The password was already used.',
+        fails_with=keycloak_config.changing_pw_failed,
     )
     keycloak_password_change(driver, keycloak_config, 'univention', 'Univention.99', 'Univention.99')
-    wait_for_id(driver, portal_config.header_menu_id)
+    time.sleep(2)
     assert Client(username=username, password='Univention.99')
 
 
+@pytest.mark.skipif(package_installed('univention-samba4'), reason='Univention Samba 4 is installed and wont react to shadowLastChange.')
 def test_password_change_expired_shadowLastChange(portal_login_via_keycloak, keycloak_config, udm):
     ldap = get_ldap_connection(primary=True)
     dn, username = udm.create_user()
@@ -117,8 +121,8 @@ def test_logout(portal_login_via_keycloak, portal_config, keycloak_config, udm):
     sessions = keycloak_sessions_by_user(keycloak_config, username)
     assert sessions
     logout = wait_for_id(driver, portal_config.logout_button_id)
-    lang = driver.execute_script('return window.navigator.userLanguage || window.navigator.language')
-    logout_msg = portal_config.logout_msg if lang == 'en-US' else portal_config.logout_msg_de
+    lang = get_language(driver)
+    logout_msg = portal_config.logout_msg[lang]
     assert logout.text == logout_msg
     logout.click()
     wait_for_id(driver, portal_config.categories_id)
@@ -150,8 +154,9 @@ def test_login_not_possible_with_deleted_user(keycloak_config, portal_login_via_
     #   assert portal_login_via_keycloak(username, "univention", fails_with=keycloak_config.wrong_password_msg)
     # should do it
     driver = portal_login_via_keycloak(username, 'univention', verify_login=False)
+    lang = get_language(driver, german=True)
     error = wait_for_id(driver, 'kc-error-message')
-    assert error.text == 'Unexpected error when handling authentication request to identity provider.'
+    assert error.text == keycloak_config.unexpected_error[lang]
 
     # check that user is no longer available in keycloak
     users = keycloak_get_request(keycloak_config, 'realms/ucs/users', params={'search': username})
