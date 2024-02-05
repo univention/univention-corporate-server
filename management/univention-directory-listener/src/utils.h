0c10d73e96362fce400dfe55da78b815192d99ee
Arvid Requate <requate@univention.de>
2024-02-05 14:31:34 +0100

    Bug #57024: retry in case notifier doesn't answer
    * New UCR variable listener/notifier/retries with default 10
    * Also retry in do_connection, even when running with -P
    * Improve consistency of retry timing also for LDAP_RETRY

--- management/univention-directory-listener/src/utils.h
+++ management/univention-directory-listener/src/utils.h
@@ -34,19 +34,88 @@ static inline int ldap_timeout_scans() {
 
 extern int ldap_retries;
 extern int get_ldap_retries();
-#define LDAP_RETRY(lp, cmd)                                                                       \
-	({                                                                                        \
-		int _rv, _retry = 0;                                                              \
-		if (ldap_retries < 0)                                                             \
-			ldap_retries = get_ldap_retries();                                        \
-		do {                                                                              \
-			_rv = (cmd);                                                              \
-			if (_rv != LDAP_SERVER_DOWN)                                              \
-				break;                                                            \
-			while (_retry < ldap_retries && univention_ldap_open(lp) != LDAP_SUCCESS) \
-				sleep(1 << (_retry++ % 6));                                       \
-		} while (_retry < ldap_retries);                                                  \
-		_rv;                                                                              \
+extern int notifier_retries;
+extern int get_notifier_retries();
+#define LDAP_RETRY(lp, cmd)                                                                         \
+	({                                                                                          \
+		int _rv, _delay, _retry = 0;                                                        \
+		if (ldap_retries < 0)                                                               \
+			ldap_retries = get_ldap_retries();                                          \
+		do {                                                                                \
+			_rv = (cmd);                                                                \
+			if (_rv != LDAP_SERVER_DOWN)                                                \
+				break;                                                              \
+			if (_retry < ldap_retries)                                                  \
+				univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN,                  \
+					"communication with LDAP failed (%d), connecting again",    \
+					_rv);                                                       \
+			else                                                                        \
+				univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN,                  \
+					"communication with LDAP failed (%d)", _rv);                \
+			while (_retry < ldap_retries && univention_ldap_open(lp) != LDAP_SUCCESS) { \
+				_delay = 1 << (_retry < 5 ? _retry : 5);                            \
+				if (++_retry < ldap_retries) {                                      \
+					univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN,          \
+						"connection to LDAP failed, retry #%d in %d second(s)", \
+						_retry, _delay);                                    \
+					sleep(_delay);                                              \
+				}                                                                   \
+			}                                                                           \
+		} while (_retry < ldap_retries);                                                    \
+		_rv;                                                                                \
+	})
+
+#define NOTIFIER_RETRY(cmd)                                                                         \
+	({                                                                                          \
+		int _rv, _delay, _retry = 0;                                                        \
+		if (notifier_retries < 0)                                                           \
+			notifier_retries = get_notifier_retries();                                  \
+		do {                                                                                \
+			_rv = (cmd);                                                                \
+			if (_rv == 0)                                                               \
+				break;                                                              \
+			if (_retry < notifier_retries)                                              \
+				univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN,                  \
+					"communication with notifier failed (%d), connecting again",\
+					_rv);                                                       \
+			else                                                                        \
+				univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN,                  \
+					"communication with notifier failed (%d)", _rv);            \
+			while (_retry < notifier_retries) {                                         \
+				if (notifier_client_new(NULL, NULL, 0) == 0)                        \
+					break;                                                      \
+				_delay = 1 << (_retry < 5 ? _retry : 5);                            \
+				if (++_retry < notifier_retries) {                                  \
+					univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN,          \
+						"connection to notifier failed, retry #%d in %d second(s)", \
+						_retry, _delay);                                    \
+					sleep(_delay);                                              \
+				}                                                                   \
+			}                                                                           \
+		} while (_retry < notifier_retries);                                                \
+		_rv;                                                                                \
+	})
+
+#define NOTIFIER_CLIENT_NEW_RETRY(cmd)                                                              \
+	({                                                                                          \
+		int _rv, _delay, _retry = 0;                                                        \
+		if (notifier_retries < 0)                                                           \
+			notifier_retries = get_notifier_retries();                                  \
+		do {                                                                                \
+			_rv = (cmd);                                                                \
+			if (_rv == 0)                                                               \
+				break;                                                              \
+			_delay = 1 << (_retry < 5 ? _retry : 5);                                    \
+			if (++_retry < notifier_retries) {                                          \
+				univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN,                  \
+					"connection to notifier failed (%d), retry #%d in %d second(s)", \
+					_rv, _retry, _delay);                                       \
+				sleep(_delay);                                                      \
+			} else                                                                      \
+				univention_debug(UV_DEBUG_LISTENER, UV_DEBUG_WARN,                  \
+					"communication with notifier failed (%d)", _rv);            \
+		} while (_retry < notifier_retries);                                                \
+		_rv;                                                                                \
 	})
 
 extern char *lower_utf8(const char *str);
