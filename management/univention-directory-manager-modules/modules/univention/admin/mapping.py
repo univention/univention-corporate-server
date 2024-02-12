univention/univention-corporate-server

commit 317de5c0b339ccc7426ec9f2c7e7db9c00fe8332

Bug #27547: UDM API documentation

management/univention-directory-manager-modules/modules/univention/admin/mapping.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-#
-# Univention Admin Modules
-#  mappings
-#
+"""
+Functions to map between |UDM| properties and |LDAP| attributes.
+"""
 # Copyright 2004-2019 Univention GmbH
 #
 # http://www.univention.de/
@@ -33,26 +32,67 @@
 import univention.debug
 import types
 import base64
+try:
+	from typing import List, Text, Tuple, TypeVar, Union  # noqa F401
+	_E = TypeVar('_E')
+except ImportError:
+	pass
 
 
 def DaysToSeconds(days):
+	# type: (str) -> str
+	"""
+	Convert number of days to seconds.
+
+	:param day: the number of days.
+	:returns: the number of seconds.
+
+	>>> DaysToSeconds('1')
+	'86400'
+	"""
 	return str(int(days) * 24 * 60 * 60)
 
 
 def SecondsToDays(seconds):
+	# type: (str) -> str
+	"""
+	Convert number of seconds to number of complete days.
+
+	:param seconds: 1-tuple with the number of seconds.
+	:returns: the number of complete days.
+
+	>>> SecondsToDays(('86401',))
+	'1'
+	"""
 	return str((int(seconds[0])) / (60 * 60 * 24))
 
 
 def StringToLower(string):
+	# type: (str) -> str
+	"""
+	Convert string to lower-case.
+
+	:param string: a string.
+	:returns: the lower-cased string.
+
+	>>> StringToLower("aa")
+	'aa'
+	"""
 	return string.lower()
 
 
 def ListUniq(list):
+	# type: (List[_E]) -> List[_E]
 	"""
+	Return list of unique items.
+
+	:param list: A list of elements.
+	:returns: a list with duplicate elements removed.
+
 	>>> ListUniq(['1', '1', '2'])
 	['1', '2']
 	"""
-	result = []
+	result = []  # type: List[_E]
 	if list:
 		for element in list:
 			if element not in result:
@@ -61,6 +101,19 @@ def ListUniq(list):
 
 
 def ListToString(list):
+	# type: (List[str]) -> str
+	"""
+	Return first element from list.
+	This is right mapping for single-valued properties, as |LDAP| always returns lists of values.
+
+	:param list: A list of elements.
+	:returns: the first element or the empty string.
+
+	>>> ListToString([])
+	''
+	>>> ListToString(['value'])
+	'value'
+	"""
 	if len(list) > 0:
 		return list[0]
 	else:
@@ -68,6 +121,18 @@ def ListToString(list):
 
 
 def ListToIntToString(list_):
+	# type: (List[str]) -> str
+	"""
+	Return first element from list if it is an integer.
+
+	:param list: A list of elements.
+	:returns: the first element or the empty string.
+
+	>>> ListToIntToString([])
+	''
+	>>> ListToIntToString(['1'])
+	'1'
+	"""
 	if list_:
 		try:
 			return str(int(list_[0]))
@@ -77,27 +142,85 @@ def ListToIntToString(list_):
 
 
 def ListToLowerString(list):
+	# type: (List[str]) -> str
+	"""
+	Return first element from list lower-cased.
+
+	:param list: A list of elements.
+	:returns: the first element lower-cased or the empty string.
+
+	>>> ListToLowerString([])
+	''
+	>>> ListToLowerString(['Value'])
+	'value'
+	"""
 	return StringToLower(ListToString(list))
 
 
 def ListToLowerList(list):
+	# type: (List[str]) -> List[str]
+	"""
+	Return the list with all elements converted to lower-case.
+
+	:param list: A list of elements.
+	:returns: a list of the elemets converted to lower case.
+
+	>>> ListToLowerList(['A', 'a'])
+	['a', 'a']
+	"""
 	return [StringToLower(string) for string in list]
 
 
 def ListToLowerListUniq(list):
+	# type: (List[str]) -> List[str]
+	"""
+	Return the list with all elements converted to lower-case and duplicates removed.
+
+	:param list: A list of elements.
+	:returns: a list of the elemets converted to lower case with duplicates removed.
+
+	>>> ListToLowerListUniq(['A', 'a'])
+	['a']
+	"""
 	return ListUniq(ListToLowerList(list))
 
 
 def nothing(a):
+	"""
+	'Do nothing' mapping returning `None`.
+	"""
 	pass
 
 
 def IgnoreNone(list):
+	# type: (str) -> Union[None, str]
+	"""
+	Return the value if it is not the sting `None`.
+
+	:param list: Some element(s).
+	:returns: The element(s) if it is not `None`.
+
+	>>> IgnoreNone('1')
+	'1'
+	>>> IgnoreNone('None')
+	"""
 	if list != 'None':
 		return list
 
 
 def _stringToInt(value):
+	# type: (str) -> int
+	"""
+	Try to convert string into inetger.
+
+	:param value: a srting.
+	:returns: the integer value or `0`.
+
+	>>> _stringToInt('1')
+	1
+	>>> _stringToInt('ucs')
+	0
+	"""
 	try:
 		return int(value)
 	except (ValueError, TypeError):
@@ -105,7 +228,13 @@ def _stringToInt(value):
 
 
 def unmapUNIX_TimeInterval(value):
+	# type: (Union[List[str], Tuple[str], str]) -> List[Text]
 	"""
+	Map number of seconds to a human understanable time interval.
+
+	:param value: number of seconds
+	:returns: a 2-tuple (value, unit)
+
 	>>> unmapUNIX_TimeInterval(['0'])
 	[u'0', 'days']
 	>>> unmapUNIX_TimeInterval(('1',))
@@ -134,7 +263,13 @@ def unmapUNIX_TimeInterval(value):
 
 
 def mapUNIX_TimeInterval(value):
+	# type: (Union[List[str], Tuple[str], str]) -> Text
 	"""
+	Unmap a human understanable time interval back to number of seconds.
+
+	:param value: a 2-tuple (value, unit)
+	:returns: the number of seconds.
+
 	>>> mapUNIX_TimeInterval(0)
 	u'0'
 	>>> mapUNIX_TimeInterval([1, 'days'])
@@ -160,7 +295,19 @@ def mapUNIX_TimeInterval(value):
 
 
 def unmapBase64(value):
-	'''mapBase64 converts binary data (as found in LDAP) to Base64 encoded UDM propertry values'''
+	"""
+	Convert binary data (as found in |LDAP|) to Base64 encoded |UDM| property value(s).
+
+	:param value: some binary data.
+	:returns: the base64 encoded data or the empty string on errors.
+
+	>>> unmapBase64(['a'])
+	'YQ=='
+	>>> unmapBase64(['a', 'b'])
+	['YQ==', 'Yg==']
+	>>> unmapBase64([None])
+	''
+	"""
 	if len(value) > 1:
 		try:
 			return map(base64.b64encode, value)
@@ -175,7 +322,22 @@ def unmapBase64(value):
 
 
 def mapBase64(value):
-	'''mapBase64 converts Base64 encoded UDM property values to binary data (for storage in LDAP)'''
+	# type: (Union[List[str], str]) -> Union[List[str], str]
+	# @overload (List[str]) -> List[str]
+	# @overload (str) -> str
+	"""
+	Convert Base64 encoded |UDM| property values to binary data (for storage in |LDAP|).
+
+	:param value: some base64 encoded value.
+	:returns: the decoded binary data.
+
+	>>> mapBase64('*')
+	'*'
+	>>> mapBase64(['YQ=='])
+	['a']
+	>>> mapBase64('YQ==')
+	'a'
+	"""
 	if value == '*':
 		# special case for filter pattern '*'
 		return value
@@ -193,6 +355,18 @@ def mapBase64(value):
 
 
 def BooleanListToString(list):
+	# type: (List[str]) -> str
+	"""
+	Convert |LDAP| boolean to |UDM|.
+
+	:param list: list of |LDAP| attribute values.
+	:returns: the empty string for `False` or otherwise the first element.
+
+	>>> BooleanListToString(['0'])
+	''
+	>>> BooleanListToString(['1'])
+	'1'
+	"""
 	v = ListToString(list)
 	if v == '0':
 		return ''
@@ -200,18 +374,33 @@ def BooleanListToString(list):
 
 
 def BooleanUnMap(value):
+	# type: (str) -> str
+	"""
+	Convert |LDAP| boolean to |UDM|.
+
+	:param list: One |LDAP| attribute values.
+	:returns: the empty string for `False` or otherwise the first element.
+
+	>>> BooleanUnMap('0')
+	''
+	>>> BooleanUnMap('1')
+	'1'
+	"""
 	if value == '0':
 		return ''
 	return value
 
 
 class dontMap(object):
+	"""
+	'Do nothing' mapping.
+	"""
 	pass
 
 
 class mapping(object):
 	"""
-	Map LDAP atribute names and values to UDM property names and values and back.
+	Map |LDAP| atribute names and values to |UDM| property names and values and back.
 	"""
 
 	def __init__(self):
@@ -222,13 +411,24 @@ class mapping(object):
 
 	def register(self, map_name, unmap_name, map_value=None, unmap_value=None):
 		"""
-		Register function 'map_value' to map values of UDM property 'map_name' to values as used in LDAP.
-		Register function 'unmap_value' to map values of LDAP attribute named 'unmap_name' to values as used by UDM.
+		Register a new mapping.
+
+		:param map_name: |UDM| property name.
+		:param unmap_name: |LDAP| attribute name.
+		:param map_value: function to map |UDM| property values to |LDAP| attribute values.
+		:param unmap_value: function to map |LDAP| attribute values to |UDM| property values.
 		"""
 		self._map[map_name] = (unmap_name, map_value)
 		self._unmap[unmap_name] = (map_name, unmap_value)
 
 	def unregister(self, map_name, pop_unmap=True):
+		# type: (str, bool) -> None
+		"""
+		Remove a mapping |UDM| to |LDAP| (and also the reverse).
+
+		:param map_name: |UDM| property name.
+		:param pop_unmap: `False` prevents the removal of the mapping from |LDAP| to |UDM|, which the default `True` also does.
+		"""
 		# unregister(pop_unmap=False) is used by LDAP_Search syntax classes with viewonly=True.
 		# See SimpleLdap._init_ldap_search().
 		unmap_name, map_value = self._map.pop(map_name, [None, None])
@@ -236,11 +436,17 @@ class mapping(object):
 			self._unmap.pop(unmap_name, None)
 
 	def registerUnmapping(self, unmap_name, unmap_value):
+		"""
+		Register a new unmapping from |LDAP| to |UDM|.
+
+		:param unmap_name: |LDAP| attribute name.
+		:param unmap_value: function to map |LDAP| attribute values to |UDM| property values.
+		"""
 		self._unmap_func[unmap_name] = unmap_value
 
 	def mapName(self, map_name):
 		"""
-		Map UDM property name to LDAP attribute name.
+		Map |UDM| property name to |LDAP| attribute name.
 
 		>>> map = mapping()
 		>>> map.mapName('unknown')
@@ -253,7 +459,7 @@ class mapping(object):
 
 	def unmapName(self, unmap_name):
 		"""
-		Map LDAP attribute name to UDM property name.
+		Map |LDAP| attribute name to |UDM| property name.
 
 		>>> map = mapping()
 		>>> map.unmapName('unknown')
@@ -266,7 +472,7 @@ class mapping(object):
 
 	def mapValue(self, map_name, value):
 		"""
-		Map UDM property value to LDAP attribute value.
+		Map |UDM| property value to |LDAP| attribute value.
 
 		>>> map = mapping()
 		>>> map.mapValue('unknown', None) #doctest: +IGNORE_EXCEPTION_DETAIL
@@ -300,7 +506,7 @@ class mapping(object):
 
 	def unmapValue(self, unmap_name, value):
 		"""
-		Map LDAP attribute value to UDM property value.
+		Map |LDAP| attribute value to |UDM| property value.
 
 		>>> map = mapping()
 		>>> map.unmapValue('unknown', None) #doctest: +IGNORE_EXCEPTION_DETAIL
@@ -318,7 +524,9 @@ class mapping(object):
 		return unmap_value(value) if unmap_value else value
 
 	def unmapValues(self, oldattr):
-		"""Unmaps LDAP attribute values to UDM property values"""
+		"""
+		Unmaps |LDAP| attribute values to |UDM| property values.
+		"""
 		info = mapDict(self, oldattr)
 		for key, func in self._unmap_func.items():
 			info[key] = func(oldattr)

base/univention-python/modules/uldap.py
@@ -44,7 +44,8 @@ except ImportError:
 	pass
 
 
-def parentDn(dn, base=''):  # type: (str, str) -> Optional[str]
+def parentDn(dn, base=''):
+	# type: (str, str) -> Optional[str]
 	"""
 	Return the parent container of a distinguished name.
 
@@ -60,7 +61,8 @@ def parentDn(dn, base=''):  # type: (str, str) -> Optional[str]
 	return ldap.dn.dn2str(dn[1:])
 
 
-def explodeDn(dn, notypes=0):  # type: (str, int) -> List[str]
+def explodeDn(dn, notypes=0):
+	# type: (str, int) -> List[str]
 	"""
 	Break up a DN into its component parts.
 
@@ -72,7 +74,8 @@ def explodeDn(dn, notypes=0):  # type: (str, int) -> List[str]
 	return ldap.dn.explode_dn(dn, notypes)
 
 
-def getRootDnConnection(start_tls=2, decode_ignorelist=[], reconnect=True):  # type: (int, List[str], bool) -> access
+def getRootDnConnection(start_tls=2, decode_ignorelist=[], reconnect=True):
+	# type: (int, List[str], bool) -> access
 	"""
 	Open a LDAP connection to the local LDAP server with the LDAP root account.
 
@@ -96,7 +99,8 @@ def getRootDnConnection(start_tls=2, decode_ignorelist=[], reconnect=True):  # t
 	return access(host=host, port=port, base=ucr['ldap/base'], binddn=binddn, bindpw=bindpw, start_tls=start_tls, decode_ignorelist=decode_ignorelist, reconnect=reconnect)
 
 
-def getAdminConnection(start_tls=2, decode_ignorelist=[], reconnect=True):  # type: (int, List[str], bool) -> access
+def getAdminConnection(start_tls=2, decode_ignorelist=[], reconnect=True):
+	# type: (int, List[str], bool) -> access
 	"""
 	Open a LDAP connection to the Master LDAP server using the admin credentials.
 
@@ -114,7 +118,8 @@ def getAdminConnection(start_tls=2, decode_ignorelist=[], reconnect=True):  # ty
 	return access(host=ucr['ldap/master'], port=port, base=ucr['ldap/base'], binddn='cn=admin,' + ucr['ldap/base'], bindpw=bindpw, start_tls=start_tls, decode_ignorelist=decode_ignorelist, reconnect=reconnect)
 
 
-def getBackupConnection(start_tls=2, decode_ignorelist=[], reconnect=True):  # type: (int, List[str], bool) -> access
+def getBackupConnection(start_tls=2, decode_ignorelist=[], reconnect=True):
+	# type: (int, List[str], bool) -> access
 	"""
 	Open a LDAP connection to a Backup LDAP server using the admin credentials.
 
@@ -138,7 +143,8 @@ def getBackupConnection(start_tls=2, decode_ignorelist=[], reconnect=True):  # t
 		return access(host=backup, port=port, base=ucr['ldap/base'], binddn='cn=backup,' + ucr['ldap/base'], bindpw=bindpw, start_tls=start_tls, decode_ignorelist=decode_ignorelist, reconnect=reconnect)
 
 
-def getMachineConnection(start_tls=2, decode_ignorelist=[], ldap_master=True, secret_file="/etc/machine.secret", reconnect=True):  # type: (int, List[str], bool, str, bool) -> access
+def getMachineConnection(start_tls=2, decode_ignorelist=[], ldap_master=True, secret_file="/etc/machine.secret", reconnect=True):
+	# type: (int, List[str], bool, str, bool) -> access
 	"""
 	Open a LDAP connection using the machine credentials.
 
@@ -249,7 +255,8 @@ class access:
 		else:
 			return pwd
 
-	def bind(self, binddn, bindpw):  # type: (str, str) -> None
+	def bind(self, binddn, bindpw):
+		# type: (str, str) -> None
 		"""
 		Do simple LDAP bind using DN and password.
 
@@ -261,7 +268,8 @@ class access:
 		univention.debug.debug(univention.debug.LDAP, univention.debug.INFO, 'bind binddn=%s' % self.binddn)
 		self.lo.simple_bind_s(self.binddn, self.__encode_pwd(self.bindpw))
 
-	def bind_saml(self, bindpw):  # type: (str) -> None
+	def bind_saml(self, bindpw):
+		# type: (str) -> None
 		"""
 		Do LDAP bind using SAML message.
 
@@ -277,13 +285,15 @@ class access:
 		self.binddn = re.sub('^dn:', '', self.lo.whoami_s())
 		univention.debug.debug(univention.debug.LDAP, univention.debug.INFO, 'SAML bind binddn=%s' % self.binddn)
 
-	def unbind(self):  # type: () -> None
+	def unbind(self):
+		# type: () -> None
 		"""
 		Unauthenticate.
 		"""
 		self.lo.unbind_s()
 
 	def __open(self, ca_certfile):
+		# type: (Optional[str]) -> None
 		_d = univention.debug.function('uldap.__open host=%s port=%d base=%s' % (self.host, self.port, self.base))  # noqa F841
 
 		if self.reconnect:
@@ -355,7 +365,8 @@ class access:
 	def __decode_attribute(self, attr, val):
 		return self.__recode_attribute(attr, val)
 
-	def get(self, dn, attr=[], required=False):  # type: (str, List[str], bool) -> Dict[str, List[str]]
+	def get(self, dn, attr=[], required=False):
+		# type: (str, List[str], bool) -> Dict[str, List[str]]
 		"""
 		Return multiple attributes of a single LDAP object.
 
@@ -378,7 +389,8 @@ class access:
 			raise ldap.NO_SUCH_OBJECT({'desc': 'no object'})
 		return {}
 
-	def getAttr(self, dn, attr, required=False):  # type: (str, str, bool) -> List[str]
+	def getAttr(self, dn, attr, required=False):
+		# type: (str, str, bool) -> List[str]
 		"""
 		Return a single attribute of a single LDAP object.
 
@@ -469,13 +481,13 @@ class access:
 		return [x[0] for x in self.search(filter, base, scope, ['dn'], unique, required, timeout, sizelimit, serverctrls)]
 
 	def getPolicies(self, dn, policies=None, attrs=None, result=None, fixedattrs=None):
-		# type: (Optional[str], List[str], Dict[str, List[Any]], Any, Any) -> Dict[str, Dict[str, Any]]
+		# type: (str, List[str], Dict[str, List[Any]], Any, Any) -> Dict[str, Dict[str, Any]]
 		"""
-		Return UCS policies for LDAP entry.
+		Return |UCS| policies for |LDAP| entry.
 
-		:param str dn: The distinguished name of the LDAP entry.
+		:param str dn: The distinguished name of the |LDAP| entry.
 		:param list policies: List of policy object classes...
-		:param dict attrs: LDAP attributes. If not given, the data is fetched from LDAP.
+		:param dict attrs: |LDAP| attributes. If not given, the data is fetched from LDAP.
 		:param result: UNUSED!
 		:param fixedattrs: UNUSED!
 		:returns: A mapping of policy names to
@@ -573,9 +585,9 @@ class access:
 	def get_schema(self):
 		# type: () -> ldap.schema.subentry.SubSchema
 		"""
-		Retriev LDAP schema information from LDAP server.
+		Retrieve |LDAP| schema information from |LDAP| server.
 
-		:returns: The LDAP schema.
+		:returns: The |LDAP| schema.
 		:rtype: ldap.schema.subentry.SubSchema
 		"""
 		if self.reconnect and self.lo._reconnects_done > self.__reconnects_done:
@@ -586,7 +598,8 @@ class access:
 			self.__schema = ldap.schema.SubSchema(self.lo.read_subschemasubentry_s(self.lo.search_subschemasubentry_s()), 0)
 		return self.__schema
 
-	def add(self, dn, al, serverctrls=None, response=None):  # type: (str, List[Tuple], Optional[List[ldap.controls.LDAPControl]], Optional[dict]) -> None
+	def add(self, dn, al, serverctrls=None, response=None):
+		# type: (str, List[Tuple], Optional[List[ldap.controls.LDAPControl]], Optional[dict]) -> None
 		"""
 		Add LDAP entry at distinguished name and attributes in add_list=(attribute-name, old-values. new-values) or (attribute-name, new-values).
 
@@ -697,7 +710,8 @@ class access:
 			return ldap.dn.dn2str([ldap.dn.str2dn(new_rdn)[0]] + ldap.dn.str2dn(dn)[1:]), new_rdn
 		return dn, rdn
 
-	def modify_s(self, dn, ml):  # type: (str, List[Tuple[str, Optional[List[str]], List[str]]]) -> None
+	def modify_s(self, dn, ml):
+		# type: (str, List[Tuple[str, Optional[List[str]], List[str]]]) -> None
 		"""
 		Redirect `modify_s` directly to :py:attr:`lo`.
 
@@ -712,7 +726,8 @@ class access:
 			lo_ref = self._handle_referral(exc)
 			lo_ref.modify_ext_s(dn, ml)
 
-	def modify_ext_s(self, dn, ml, serverctrls=None, response=None):  # type: (str, List[Tuple[str, Any, Any]], Optional[List[ldap.controls.LDAPControl]], Optional[dict]) -> None
+	def modify_ext_s(self, dn, ml, serverctrls=None, response=None):
+		# type: (str, List[Tuple[str, Any, Any]], Optional[List[ldap.controls.LDAPControl]], Optional[dict]) -> None
 		"""
 		Redirect `modify_ext_s` directly to :py:attr:`lo`.
 
@@ -736,7 +751,8 @@ class access:
 		if serverctrls and isinstance(response, dict):
 			response['ctrls'] = resp_ctrls
 
-	def rename(self, dn, newdn, serverctrls=None, response=None):  # type: (str, str, Optional[List[ldap.controls.LDAPControl]], Optional[dict]) -> None
+	def rename(self, dn, newdn, serverctrls=None, response=None):
+		# type: (str, str, Optional[List[ldap.controls.LDAPControl]], Optional[dict]) -> None
 		"""
 		Rename a LDAP object.
 
@@ -761,7 +777,8 @@ class access:
 			univention.debug.debug(univention.debug.LDAP, univention.debug.INFO, 'uldap.rename: modrdn %s to %s' % (dn, newrdn))
 			self.rename_ext_s(dn, newrdn, serverctrls=serverctrls, response=response)
 
-	def rename_ext_s(self, dn, newrdn, newsuperior=None, serverctrls=None, response=None):  # type: (str, str, Optional[str], Optional[List[ldap.controls.LDAPControl]], Optional[dict]) -> None
+	def rename_ext_s(self, dn, newrdn, newsuperior=None, serverctrls=None, response=None):
+		# type: (str, str, Optional[str], Optional[List[ldap.controls.LDAPControl]], Optional[dict]) -> None
 		"""
 		Redirect `rename_ext_s` directly to :py:attr:`lo`.
 
@@ -786,7 +803,8 @@ class access:
 		if serverctrls and isinstance(response, dict):
 			response['ctrls'] = resp_ctrls
 
-	def delete(self, dn):  # type: (str) -> None
+	def delete(self, dn):
+		# type: (str) -> None
 		"""
 		Delete a LDAP object.
 
@@ -803,7 +821,8 @@ class access:
 				lo_ref = self._handle_referral(exc)
 				lo_ref.delete_s(dn)
 
-	def parentDn(self, dn):  # type: (str) -> Optional[str]
+	def parentDn(self, dn):
+		# type: (str) -> Optional[str]
 		"""
 		Return the parent container of a distinguished name.
 
@@ -813,7 +832,8 @@ class access:
 		"""
 		return parentDn(dn, self.base)
 
-	def explodeDn(self, dn, notypes=False):  # type: (str, Union[bool, int]) -> List[str]
+	def explodeDn(self, dn, notypes=False):
+		# type: (str, Union[bool, int]) -> List[str]
 		"""
 		Break up a DN into its component parts.
 
@@ -825,7 +845,8 @@ class access:
 		return explodeDn(dn, notypes)
 
 	@classmethod
-	def compare_dn(cls, a, b):  # type: (str, str) -> bool
+	def compare_dn(cls, a, b):
+		# type: (str, str) -> bool
 		r"""Test DNs are same
 
 		:param str a: The first distinguished name.

management/univention-directory-manager-modules/modules/univention/admin/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-#
-# Univention Admin Modules
-#  basic functionality
-#
+"""
+|UDM| basic functionality
+"""
 # Copyright 2004-2019 Univention GmbH
 #
 # http://www.univention.de/
@@ -39,6 +38,10 @@ from ldap.filter import filter_format
 
 import univention.config_registry
 import univention.debug
+try:
+	from typing import Any, List, Optional, Type, Union  # noqa F401
+except ImportError:
+	pass
 
 __all__ = ('configRegistry', 'ucr_overwrite_properties', 'pattern_replace', 'property', 'option', 'ucr_overwrite_module_layout', 'ucr_overwrite_layout', 'extended_attribute', 'tab', 'field', 'policiesGroup', 'modules', 'objects', 'syntax', 'hook', 'mapping')
 
@@ -108,8 +111,10 @@ def ucr_overwrite_properties(module, lo):
 
 
 def pattern_replace(pattern, object):
-	"""Replaces patterns like <attribute:command,...>[range] with values
-	of the specified UDM attribute."""
+	"""
+	Replaces patterns like `<attribute:command,...>[range]` with values
+	of the specified UDM attribute.
+	"""
 
 	global_commands = []
 
@@ -246,32 +251,61 @@ class property:
 
 	def __init__(
 		self,
-		short_description='',
-		long_description='',
-		syntax=None,
-		module_search=None,
-		multivalue=False,
-		one_only=False,
-		parent=None,
-		options=[],
-		license=[],
-		required=False,
-		may_change=True,
-		identifies=False,
-		unique=False,
-		default=None,
-		prevent_umc_default_popup=False,
-		dontsearch=False,
-		show_in_lists=False,
-		editable=True,
-		configObjectPosition=None,
-		configAttributeName=None,
-		include_in_default_search=False,
-		nonempty_is_default=False,
-		readonly_when_synced=False,
-		size=None,
-		copyable=False):
+		short_description='',  # type: str
+		long_description='',  # type: str
+		syntax=None,  # type: Union[Type, Any]
+		module_search=None,  # type: None
+		multivalue=False,  # type: bool
+		one_only=False,  # type: bool
+		parent=None,  # type: str
+		options=[],  # type: List[str]
+		license=[],  # type: List[str]
+		required=False,  # type: bool
+		may_change=True,  # type: bool
+		identifies=False,  # type: bool
+		unique=False,  # type: bool
+		default=None,  # type: Any
+		prevent_umc_default_popup=False,  # type: bool
+		dontsearch=False,  # type: bool
+		show_in_lists=False,  # type: bool
+		editable=True,  # type: bool
+		configObjectPosition=None,  # type: None
+		configAttributeName=None,  # type: None
+		include_in_default_search=False,  # type: bool
+		nonempty_is_default=False,  # type: bool
+		readonly_when_synced=False,  # type: bool
+		size=None,  # type: str
+		copyable=False  # type: bool
+	):  # type: (...) -> None
+		"""
+		|UDM| property.
 
+		:param short_description: a short descriptive text - shown below the input filed in |UMC| by default.
+		:param long_description: a long descriptive text - shown only on demand in |UMC|.
+		:param syntax: a syntax class or instance to validate the value.
+		:param module_search: UNUSED?
+		:param multivalue: allow only a single value (`False`) or multiple values (`True`) .
+		:param one_only: UNUSED?
+		:param parent: UNUSED?
+		:param options: List of options, which enable this property.
+		:param license: List of license strings, which are required to use this property.
+		:param required: `True` for a required property, `False` for an optional property.
+		:param may_change: `True` if the property can be changed after the object has been created, `False` when the property can only be specified when the object is created.
+		:param identifies: `True` if the property is part of the set of properties, which are required to uniquely identify the object.
+		:param unique: `True` if the property must be unique for all object instances.
+		:param default: The default value for the property when a new object is created.
+		:param prevent_umc_default_popup: `True` to prevent a pop-up dialog in |UMC| when the default value is not set.
+		:param dontsearch: `True` to prevent searches using the property.
+		:param show_in_lists: UNUSED?
+		:param editable: `False` prevents the property from being modified by the user; it still can be modified by code.
+		:param configObjectPosition: UNUSED?
+		:param configAttributeName: UNUSED?
+		:param include_in_default_search: The default search searches this property when set to `True`.
+		:param nonempty_is_default: `True` selects the first non-empty value as the default. `False` always selects the first default value, even if it is empty.
+		:param readonly_when_synced: `True` only shows the value as read-only when synchronized from some upstream database.
+		:param size: The |UMC| widget size; one of :py:data:`univention.admin.syntax.SIZES`.
+		:param copyable: With `True` the property is copied when the object is clones; with `False` the new object will use the default value.
+		"""
 		self.short_description = short_description
 		self.long_description = long_description
 		if isinstance(syntax, types.ClassType):
@@ -295,7 +329,7 @@ class property:
 		self.editable = editable
 		self.configObjectPosition = configObjectPosition
 		self.configAttributeName = configAttributeName
-		self.templates = []
+		self.templates = []  # type: List  # univention.admin.handlers.simpleLdap
 		self.include_in_default_search = include_in_default_search
 		self.threshold = int(configRegistry.get('directory/manager/web/sizelimit', '2000') or 2000)
 		self.nonempty_is_default = nonempty_is_default
@@ -385,6 +419,9 @@ class property:
 
 
 class option(object):
+	"""
+	|UDM| option to make properties conditional.
+	"""
 
 	def __init__(self, short_description='', long_description='', default=0, editable=False, disabled=False, objectClasses=None, is_app_option=False):
 		self.short_description = short_description
@@ -426,9 +463,9 @@ def ucr_overwrite_layout(module, ucr_property, tab):
 
 
 def ucr_overwrite_module_layout(module):
-	'''
-	Overwrite the tab layout
-	'''
+	"""
+	Overwrite the tab layout through |UCR| variables.
+	"""
 	univention.debug.debug(univention.debug.ADMIN, univention.debug.INFO, "layout overwrite")
 	# there are modules without a layout definition
 	if not hasattr(module, 'layout'):
@@ -502,6 +539,9 @@ def ucr_overwrite_module_layout(module):
 
 
 class extended_attribute(object):
+	"""
+	Extended attributes extend |UDM| and |UMC| with additional properties defined in |LDAP|.
+	"""
 
 	def __init__(self, name, objClass, ldapMapping, deleteObjClass=False, syntax='string', hook=None):
 		self.name = name
@@ -519,7 +559,9 @@ class extended_attribute(object):
 
 
 class tab:
-
+	"""
+	|UDM| tab to group related properties together in |UMC|.
+	"""
 	is_app_tab = False
 
 	def __init__(self, short_description='', long_description='', fields=[], advanced=False):

commit 037ae75d94402289bfca54a85e3b547d25f3f76d

docs(udm): fix type annotations of univention.admin.mapping
Fixes: 317de5c0b339ccc7426ec9f2c7e7db9c00fe8332
Bug #27547

management/univention-directory-manager-modules/modules/univention/admin/mapping.py
@@ -134,7 +134,7 @@ def ListUniq(list):
 
 
 def ListToString(value, encoding=()):
-    # type: (List[str]) -> str
+    # type: (List[bytes]) -> str
     """
     Return first element from list.
     This is right mapping for single-valued properties, as |LDAP| always returns lists of values.
@@ -154,7 +154,7 @@ def ListToString(value, encoding=()):
 
 
 def ListToIntToString(list_):
-    # type: (List[str]) -> str
+    # type: (List[bytes]) -> str
     """
     Return first element from list if it is an integer.
 
@@ -177,7 +177,7 @@ def ListToIntToString(list_):
 
 
 def ListToLowerString(list):
-    # type: (List[str]) -> str
+    # type: (List[bytes]) -> str
     """
     Return first element from list lower-cased.
 
@@ -225,7 +225,7 @@ def nothing(a):
 
 
 def IgnoreNone(value, encoding=()):
-    # type: (str) -> Union[None, str]
+    # type: (str) -> Union[None, bytes]
     """
     Return the value if it is not the string `None`.
 
@@ -242,7 +242,7 @@ def IgnoreNone(value, encoding=()):
 
 
 def _stringToInt(value):
-    # type: (str) -> int
+    # type: (Union[bytes, str]) -> int
     """
     Try to convert string into integer.
 
@@ -261,7 +261,7 @@ def _stringToInt(value):
 
 
 def unmapUNIX_TimeInterval(value):
-    # type: (Union[List[str], Tuple[str], str]) -> List[Text]
+    # type: (Union[List[bytes], Tuple[bytes], bytes]) -> List[Text]
     """
     Map number of seconds to a human understandable time interval.
 
@@ -296,7 +296,7 @@ def unmapUNIX_TimeInterval(value):
 
 
 def mapUNIX_TimeInterval(value):
-    # type: (Union[List[str], Tuple[str], str]) -> Text
+    # type: (Union[List[str], Tuple[str], str]) -> bytes
     """
     Unmap a human understandable time interval back to number of seconds.
 
@@ -388,7 +388,7 @@ def mapBase64(value):
 
 
 def BooleanListToString(list, encoding=()):
-    # type: (List[str]) -> str
+    # type: (List[bytes]) -> str
     """
     Convert |LDAP| boolean to |UDM|.
 
@@ -407,7 +407,7 @@ def BooleanListToString(list, encoding=()):
 
 
 def BooleanUnMap(value, encoding=()):
-    # type: (str) -> str
+    # type: (str) -> bytes
     """
     Convert |UDM| boolean to |LDAP|.
 