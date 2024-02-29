4f88b167c7dca6e52fb3b1974dd82e4a2d95ab08
Maximilian Janßen <janssen@univention.de>
2024-02-29 14:25:31 +0100

    feat(uldap): make StartTLS UCR configurable
    Ref: Bug #57098

--- base/univention-python/modules/password.py
+++ base/univention-python/modules/password.py
@@ -79,9 +79,9 @@ class Check(object):
         if os.path.exists('/etc/ldap.secret'):
             self.lo = univention.uldap.getAdminConnection()
         elif os.path.exists('/etc/machine.secret'):
-            self.lo = univention.uldap.getMachineConnection(start_tls=2)
+            self.lo = univention.uldap.getMachineConnection()
         else:
-            self.lo = univention.uldap.access(host=self.ConfigRegistry.get('ldap/master'), base=self.ConfigRegistry.get('ldap/base'), start_tls=2)
+            self.lo = univention.uldap.access(host=self.ConfigRegistry.get('ldap/master'), base=self.ConfigRegistry.get('ldap/base'))
 
     def _systemPolicy(self):
         if self.ConfigRegistry.get('password/quality/credit/digits', '0'):
