univention/univention-corporate-server

commit 315924205827a1f94de76036dece041d5168594f

style: fix SIM108
Bug #31771

management/univention-directory-manager-modules/modules/univention/admin/handlers/__init__.py
@@ -2309,10 +2309,7 @@ class simpleComputer(simpleLdap):
 
     def __split_dns_line(self, entry):
         zone = entry[0]
-        if len(entry) > 1:
-            ip = self._is_ip(entry[1]) and entry[1] or None
-        else:
-            ip = None
+        ip = self._is_ip(entry[1]) and entry[1] or None if len(entry) > 1 else None
 
         ud.debug(ud.ADMIN, ud.INFO, 'Split entry %s into zone %s and ip %s' % (entry, zone, ip))
         return (zone, ip)

management/univention-appcenter/python/appcenter/actions/__init__.py
@@ -115,10 +115,7 @@ class UniventionAppAction(with_metaclass(UniventionAppActionMeta, object)):
 
     @classmethod
     def _log(cls, logger, level, msg, *args, **kwargs):
-        if logger is not None:
-            logger = cls.logger.getChild(logger)
-        else:
-            logger = cls.logger
+        logger = cls.logger.getChild(logger) if logger is not None else cls.logger
         logger.log(level, msg, *args, **kwargs)
 
     @classmethod

management/univention-appcenter/umc/python/appcenter/__init__.py
@@ -962,10 +962,7 @@ class Instance(umcm.Base, ProgressMixin):
                 size = float(size) / 1000  # MB, not MiB
             else:
                 size = size * 1000  # once too often
-            if size == int(size):
-                format_string = '%d %s'
-            else:
-                format_string = '%.2f %s'
+            format_string = '%d %s' if size == int(size) else '%.2f %s'
             result['size'] = format_string % (size, byte_mod)
 
         return result

base/univention-config-registry/python/univention/config_registry/frontend.py
@@ -504,10 +504,7 @@ def variable_info_string(key, value, variable_info, scope=None, details=_SHOW_DE
         return ''
     elif value is None:
         # if not shell filter option is set
-        if not OPT_FILTERS['shell'][2]:
-            value_string = '<empty>'
-        else:
-            value_string = ''
+        value_string = "<empty>" if not OPT_FILTERS["shell"][2] else ""
     else:
         value_string = '%s' % value
 

base/univention-config-registry/python/univention/config_registry/handler.py
@@ -105,10 +105,7 @@ def run_filter(template, directory, srcfiles=set(), opts={}):
     """
     template = _replace_variables(template, directory, srcfiles)
 
-    if six.PY3:
-        tmpl = template.encode('UTF-8')
-    else:
-        tmpl = template
+    tmpl = template.encode("UTF-8") if six.PY3 else template
 
     if opts.get('disallow-execution', False):
         return tmpl

base/univention-lib/python/ldap_extension.py
@@ -372,10 +372,7 @@ class UniventionLDAPExtension(six.with_metaclass(ABCMeta)):
             if p.returncode == 0:
                 regex = re.compile('^Object created: (.*)$', re.M)
                 m = regex.search(stdout)
-                if m:
-                    new_object_dn = m.group(1)
-                else:
-                    new_object_dn = None
+                new_object_dn = m.group(1) if m else None
 
                 appidentifier = os.environ.get('UNIVENTION_APP_IDENTIFIER')
                 if appidentifier:

base/univention-lib/python/listenerSharePath.py
@@ -90,10 +90,7 @@ def dirIsMountPoint(path):
             for line in f:
                 if line.startswith("#"):
                     continue
-                if "\t" in line:
-                    tmp = line.split("\t")
-                else:
-                    tmp = line.split()
+                tmp = line.split("\t") if "\t" in line else line.split()
                 if len(tmp) > 1:
                     tmp[1] = tmp[1].rstrip("/")
                     if tmp[1] == path:

base/univention-pam/well-known-sid-name-mapping.py
@@ -146,10 +146,7 @@ def no_relevant_change(new: Dict[str, List[bytes]], old: Dict[str, List[bytes]])
 
     ocs = new.get('objectClass', [])
 
-    if b'sambaSamAccount' in ocs:
-        name_attr = 'uid'
-    else:
-        name_attr = 'cn'
+    name_attr = "uid" if b"sambaSamAccount" in ocs else "cn"
 
     old_name = old.get(name_attr, [])
     new_name = new.get(name_attr, [])

commit 08203c780a886ad05bc0f26a7c316774a374d265

refactor(udm/handler/simpleComputer): DNS/DHCP parsing
Convert some methods into class methods.
Rewrite complicated ternary construct for readability.
Add tests.

Fixes: 315924205827a1f94de76036dece041d5168594f
Bug #50385

management/univention-directory-manager-modules/modules/univention/admin/handlers/__init__.py
@@ -2403,8 +2403,15 @@ class simpleComputer(simpleLdap):
 
         return dn
 
-    def __split_dhcp_line(self, entry):
+    @classmethod
+    def __split_dhcp_line(cls, entry):
         # type: (List[str]) -> Tuple[str, str, str]
+        """
+        >>> simpleComputer._simpleComputer__split_dhcp_line(["service", "0011.2233.4455"])
+        ('service', '', '00:11:22:33:44:55')
+        >>> simpleComputer._simpleComputer__split_dhcp_line(["service", "1.2.3.4", "00:11:22:33:44:55"])
+        ('service', '1.2.3.4', '00:11:22:33:44:55')
+        """
         service = entry[0]
         ip = ''
         try:
@@ -2412,16 +2419,23 @@ class simpleComputer(simpleLdap):
             #   0011.2233.4455 -> 00:11:22:33:44:55 -> is guaranteed to work together with our DHCP server
             #   __split_dhcp_line may be used outside of UDM which means that MAC_Address.parse may not be called.
             mac = univention.admin.syntax.MAC_Address.parse(entry[-1])
-            if self._is_ip(entry[-2]):
+            if cls._is_ip(entry[-2]):
                 ip = entry[-2]
         except univention.admin.uexceptions.valueError:
             mac = ''
         return (service, ip, mac)
 
-    def __split_dns_line(self, entry):
+    @classmethod
+    def __split_dns_line(cls, entry):
         # type: (List[str]) -> Tuple[str, Optional[str]]
+        """
+        >>> simpleComputer._simpleComputer__split_dns_line(["zoneName"])
+        ('zoneName', None)
+        >>> simpleComputer._simpleComputer__split_dns_line(["zoneName", "1.2.3.4"])
+        ('zoneName', '1.2.3.4')
+        """
         zone = entry[0]
-        ip = self._is_ip(entry[1]) and entry[1] or None if len(entry) > 1 else None
+        ip = entry[1] if len(entry) > 1 and cls._is_ip(entry[1]) else None
 
         log.debug('Split entry %s into zone %s and ip %s', entry, zone, ip)
         return (zone, ip)