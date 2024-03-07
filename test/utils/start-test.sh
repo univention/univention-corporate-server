d117d69894c2b9fea88e78278d916c0c1b6c3324
Philipp Hahn <hahn@univention.de>
2024-03-07 15:57:47 +0100

    Bug #57118: Update links from UCS 5.0-6 to UCS 5.0-7

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -4,7 +4,7 @@
 #
 
 # defaults for release
-release='5.0-6'
+release='5.0-7'
 old_release='4.4-9'
 kvm_template_version='5.0-6+e897'
 # AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 7
