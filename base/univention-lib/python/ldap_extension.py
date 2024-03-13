univention/univention-corporate-server

commit 8510249f5cb3e4ea0f9b1491606f81b178a4c87e

Bug #56698: use uldap admin connection in ldap_touch_udm_object on primary

base/univention-lib/python/ldap_extension.py
@@ -49,16 +49,13 @@ import tempfile
 import time
 from abc import ABCMeta, abstractmethod, abstractproperty
 from copy import copy
-from io import StringIO
 from optparse import Option, OptionGroup, OptionParser, OptionValueError, Values  # noqa: F401
 from typing import TYPE_CHECKING, Dict, List, Optional, Tuple  # noqa: F401
 
 import apt
-import ldap
 import six
 from ldap.dn import escape_dn_chars
 from ldap.filter import filter_format
-from ldif import LDIFWriter
 
 import univention.admin as udm
 
@@ -282,21 +279,23 @@ class UniventionLDAPExtension(six.with_metaclass(ABCMeta)):
         return (rc, object_dn, stdout)
 
     def ldap_touch_udm_object(self):
-        ldif = StringIO()
-        ldifwriter = LDIFWriter(ldif)
-        ldifwriter.unparse(self.object_dn, [(ldap.MOD_REPLACE, self.active_flag_attribute, [b'FALSE'])])
-
-        cmd = [
-            "ldapmodify", "-ZZ",
-            "-H", "ldap://%s:%s/" % (self.ucr["ldap/master"], self.ucr["ldap/master/port"]),
-            "-D", self.options.binddn,
-        ] + (
-            ["-y", self.options.bindpwdfile] if self.options.bindpwdfile else ["-w", self.options.bindpwd]
-        )
-        proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        out, _ = proc.communicate(ldif.getvalue().encode("UTF-8"))
-        stdout = out.decode('UTF-8', 'replace')
-        print(stdout)
+        if self.options.binddn and (self.options.bindpwdfile or self.options.bindpwd):
+            password = open(self.options.bindpwdfile).read().strip() if self.options.bindpwdfile else self.options.bindpwd
+            lo = udm_uldap.access(
+                host=self.ucr["ldap/master"],
+                port=self.ucr["ldap/master/port"],
+                binddn=self.options.binddn,
+                bindpw=password,
+            )
+        else:
+            try:
+                lo, _ = udm_uldap.getAdminConnection()
+            except EnvironmentError:
+                lo, _ = udm_uldap.getMachineConnection()
+        try:
+            lo.modify(self.object_dn, [(self.active_flag_attribute, [b'foo'], [b'FALSE'])])
+        except udm_errors.base as exc:
+            ud.debug(ud.LISTENER, ud.ERROR, 'Could not touch LDAP object %r: %s' % (self.object_dn, exc))
 
     def register(self, filename, options, udm_passthrough_options, target_filename=None):
         # type: (str, Values, List[str], Optional[str]) -> None
@@ -1061,8 +1060,12 @@ class UniventionDataExtension(UniventionLDAPExtension):
     filesuffix = ''
 
     def is_local_active(self):
+        """
+        There is nothing to activate for a data extension,
+        just pretend that everything is fine.
+        """
         # type: () -> Tuple[int, Optional[str]]
-        return (0, None)
+        return (0, "foo")
 
     def wait_for_activation(self, timeout=180):
         # type: (int) -> bool

doc/errata/staging/univention-lib.yaml
@@ -1,0 +1,14 @@
+product: ucs
+release: "5.0"
+version: [5, 4]
+scope: ucs_5.0-0-errata5.0-5
+src: univention-lib
+fix: 9.0.16-5
+desc: |
+ This update addresses the following issues:
+ * Erratum 785 introduced a new mechanism in `ucs_registerLDAPExtension`
+   to re-trigger the activation of an LDAP ACL or schema extension by
+   doing a trivial (i.e. no-op) LDAP modification. This failed on the
+   Primary node due to missing credentials. `ucs_registerLDAPExtension`
+   has been fixed to use the LDAP admin connection in this case.
+bug: [56698]

test/ucs-test/tests/10_ldap/79ucs_registerLDAPExtension_credentials.py
@@ -1,0 +1,103 @@
+#!/usr/share/ucs-test/runner pytest-3 -s
+## desc: Test ucs_registerLDAPExtension with and without credentials
+## tags:
+##  - ldapextensions
+## bugs: [56698]
+## exposure: dangerous
+
+
+import os
+import sys
+import time
+from types import SimpleNamespace
+
+import pytest
+
+from univention.lib.ldap_extension import UniventionLDAPExtension, ucs_registerLDAPExtension
+from univention.testing.ucr import UCSTestConfigRegistry
+from univention.testing.utils import UCSTestDomainAdminCredentials
+
+
+# mocks
+def mock_is_local_active(self):
+    return (0, None)
+
+
+def mock_wait_for_activation(self):
+    return True
+
+
+UniventionLDAPExtension.is_local_active = mock_is_local_active
+UniventionLDAPExtension.wait_for_activation = mock_is_local_active
+
+
+def get_acl_extension(lo, ucr):
+    res = lo.get(f"cn=66univention-appcenter_app,cn=ldapacl,cn=univention,{ucr['ldap/base']}")
+    return SimpleNamespace(
+        packagename=res["univentionOwnedByPackage"][0].decode("utf-8"),
+        packageversion=res["univentionOwnedByPackageVersion"][0].decode("utf-8"),
+        ucsversionstart=res["univentionUCSVersionStart"][0].decode("utf-8"),
+        ucsversionend=res["univentionUCSVersionEnd"][0].decode("utf-8"),
+        acl=os.path.join("/usr/share/univention-appcenter/", res["univentionLDAPACLFilename"][0].decode("utf-8")),
+        active=res["univentionLDAPACLActive"][0].decode("utf-8"),
+    )
+
+
+def primary_or_backup():
+    with UCSTestConfigRegistry() as ucr:
+        return ucr.get('server/role') in ["domaincontroller_backup", "domaincontroller_master"]
+
+
+@pytest.fixture()
+def acl_extension(lo, ucr):
+    return get_acl_extension(lo, ucr)
+
+
+@pytest.fixture()
+def verify_acl_extension(lo, ucr):
+    """
+    check that the objects univentionLDAPACLActive is FALSE after
+    ucs_registerLDAPExtension and that the listener set TRUE
+    after a couple of seconds
+    """
+    def _func():
+        extension = get_acl_extension(lo, ucr)
+        assert extension.active == "FALSE", "extension should be disabled after triggering ldap_touch_udm_object"
+        for i in range(1, 30):
+            time.sleep(3)
+            extension = get_acl_extension(lo, ucr)
+            if extension.active == "TRUE":
+                return
+        assert extension.active == "TRUE", "extension not activated by listener after waiting"
+
+    return _func
+
+
+@pytest.mark.skipif(not primary_or_backup(), reason="only domaincontroller_master/backup can retrigger ucs_registerLDAPExtension without credentials")
+def test_without_credentials(acl_extension, verify_acl_extension):
+    sys.argv = [
+        sys.argv[0],
+        "--packagename", acl_extension.packagename,
+        "--packageversion", acl_extension.packageversion,
+        "--ucsversionstart", acl_extension.ucsversionstart,
+        "--ucsversionend", acl_extension.ucsversionend,
+        "--acl", acl_extension.acl,
+    ]
+    ucs_registerLDAPExtension()
+    verify_acl_extension()
+
+
+def test_with_credentials(acl_extension, verify_acl_extension):
+    account = UCSTestDomainAdminCredentials()
+    sys.argv = [
+        sys.argv[0],
+        "--packagename", acl_extension.packagename,
+        "--packageversion", acl_extension.packageversion,
+        "--ucsversionstart", acl_extension.ucsversionstart,
+        "--ucsversionend", acl_extension.ucsversionend,
+        "--acl", acl_extension.acl,
+        "--binddn", account.binddn,
+        "--bindpwdfile", account.pwdfile,
+    ]
+    ucs_registerLDAPExtension()
+    verify_acl_extension()

commit 9b31e5ce1910c8acf0b7faf447a8031942ab1d26

doc(lib): Fix PEP-484 type annotation
PEP-484 type annotation comment MUST be directly after function
signature and BEFORE doc-string.

Fixes: 8510249f5cb
Bug #56698: use uldap admin connection in ldap_touch_udm_object on primary

base/univention-lib/python/ldap_extension.py
@@ -1060,11 +1060,11 @@ class UniventionDataExtension(UniventionLDAPExtension):
     filesuffix = ''
 
     def is_local_active(self):
+        # type: () -> Tuple[int, Optional[str]]
         """
         There is nothing to activate for a data extension,
         just pretend that everything is fine.
         """
-        # type: () -> Tuple[int, Optional[str]]
         return (0, "foo")
 
     def wait_for_activation(self, timeout=180):