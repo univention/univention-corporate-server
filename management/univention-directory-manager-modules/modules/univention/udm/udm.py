univention/univention-corporate-server

commit 4698a9611acc09cab68a7683b1ad98e12f418afd

doc(udm): PEP-484 type annotations
Bug #50385

management/univention-directory-manager-modules/modules/univention/udm/udm.py
@@ -108,9 +108,16 @@ from __future__ import absolute_import, unicode_literals
 
 from fnmatch import fnmatch
 from operator import itemgetter
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple, Type  # noqa: F401
 
 from .exceptions import ApiVersionMustNotChange, ApiVersionNotSupported, NoApiVersionSet, NoObject
 from .plugins import Plugins
+
+
+if TYPE_CHECKING:
+    import univention.admin.uldap  # noqa: F401
+
+    from ..base import BaseModule, BaseObject  # noqa: F401
 
 
 _MODULES_PATH = 'univention.udm.modules'
@@ -129,9 +136,10 @@ class UDM(object):
             UDM.admin().version(2).obj_by_dn(dn)
     """
 
-    _module_object_cache = {}
+    _module_object_cache = {}  # type: Dict[Tuple[str, int, int], BaseModule]
 
     def __init__(self, connection, api_version=None):
+        # type: (univention.admin.uldap.access, Optional[int]) -> None
         """
         Use the provided connection.
 
@@ -142,12 +150,13 @@ class UDM(object):
         :rtype: None
         """
         self.connection = connection
-        self._api_version = None
+        self._api_version = None  # type: Optional[int]
         if api_version is not None:
             self.version(api_version)
 
     @classmethod
     def admin(cls):
+        # type: () -> UDM
         """
         Use a cn=admin connection.
 
@@ -161,6 +170,7 @@ class UDM(object):
 
     @classmethod
     def machine(cls, prefer_local_connection=False):
+        # type: (bool) -> UDM
         """
         Use a machine connection.
 
@@ -177,13 +187,13 @@ class UDM(object):
 
     @classmethod
     def credentials(
-            cls,
-            identity,
-            password,
-            base=None,
-            server=None,
-            port=None,
-    ):
+        cls,
+        identity,  # type: str
+        password,  # type: str
+        base=None,  # type: Optional[str]
+        server=None,  # type: Optional[str]
+        port=None,  # type: Optional[int]
+    ):  # type: (...) -> UDM
         """
         Use the provided credentials to open an LDAP connection.
 
@@ -204,6 +214,7 @@ class UDM(object):
         return cls(connection)
 
     def version(self, api_version):
+        # type: (int) -> UDM
         """
         Set the version of the API that the UDM modules must support.
 
@@ -226,6 +237,7 @@ class UDM(object):
         return self
 
     def get(self, name):
+        # type: (str) -> BaseModule
         """
         Get an object of :py:class:`BaseModule` (or of a subclass) for UDM
         module `name`.
@@ -257,6 +269,7 @@ class UDM(object):
         return self._module_object_cache[key]
 
     def obj_by_dn(self, dn):
+        # type: (str) -> BaseObject
         """
         Try to load an UDM object from LDAP. Guess the required UDM module
         from the ``univentionObjectType`` LDAP attribute of the LDAP object.
@@ -282,6 +295,7 @@ class UDM(object):
 
     @property
     def api_version(self):
+        # type: () -> int
         if self._api_version is None:
             raise NoApiVersionSet()
         return self._api_version

management/univention-directory-manager-modules/modules/univention/admin/__init__.py
@@ -56,6 +56,7 @@ log = getLogger('ADMIN')
 
 if TYPE_CHECKING:
     from univention.admin.layout import Tab  # noqa: F401
+    from univention.admin.types import TypeHint  # noqa: F401
 
 
 __all__ = ('configRegistry', 'extended_attribute', 'hook', 'mapping', 'modules', 'objects', 'option', 'pattern_replace', 'policiesGroup', 'property', 'syntax', 'ucr_overwrite_layout', 'ucr_overwrite_module_layout', 'ucr_overwrite_properties')
@@ -220,7 +221,7 @@ class property:
             module_search=None,  # type: None
             multivalue=False,  # type: bool
             one_only=False,  # type: bool
-            parent=None,  # type: str
+            parent=None,  # type: None
             options=[],  # type: List[str]
             license=[],  # type: List[str]
             required=False,  # type: bool
@@ -238,9 +239,9 @@ class property:
             include_in_default_search=False,  # type: bool
             nonempty_is_default=False,  # type: bool
             readonly_when_synced=False,  # type: bool
-            size=None,  # type: str
+            size=None,  # type: Optional[str]
             copyable=False,  # type: bool
-            type_class=None,  # type: type  # univention.admin.types.TypeHint
+            type_class=None,  # type: Optional[Type[TypeHint]]
     ):  # type: (...) -> None
         """
         |UDM| property.
@@ -394,7 +395,7 @@ class option(object):
     """|UDM| option to make properties conditional."""
 
     def __init__(self, short_description='', long_description='', default=0, editable=False, disabled=False, objectClasses=None, is_app_option=False):
-        # type: (str, str, int, bool, bool, Iterable[str], bool) -> None
+        # type: (str, str, int, bool, bool, Optional[Iterable[str]], bool) -> None
         self.short_description = short_description
         self.long_description = long_description
         self.default = default

management/univention-directory-manager-modules/modules/univention/admin/allocators.py
@@ -33,6 +33,7 @@
 """|UDM| allocators to allocate and lock resources for |LDAP| object creation."""
 
 from logging import getLogger
+from typing import Dict, Optional, Sequence, overload  # noqa: F401
 
 import ldap
 from ldap.filter import filter_format
@@ -40,6 +41,15 @@ from ldap.filter import filter_format
 import univention.admin.locking
 import univention.admin.uexceptions
 from univention.admin import configRegistry, localization
+
+
+try:
+    from typing import Literal  # noqa: F401
+    _TypesUidGid = Literal["uidNumber", "gidNumber"]
+    _Types = Literal["uidNumber", "gidNumber", "uid", "gid", "sid", "domainSid", "mailPrimaryAddress", "mailAlternativeAddress", "aRecord", "mac", "groupName", "cn-uid-position", "univentionObjectIdentifier"]
+    _Scopes = Literal["base", "one", "sub", "domain"]
+except ImportError:
+    pass
 
 
 log = getLogger('ADMIN')
@@ -60,7 +70,7 @@ _type2attr = {
     'groupName': 'cn',
     'cn-uid-position': 'cn',  # ['cn', 'uid', 'ou'],
     'univentionObjectIdentifier': 'univentionObjectIdentifier',
-}
+}  # type: Dict[_Types, str]
 _type2scope = {
     'uidNumber': 'base',
     'gidNumber': 'base',
@@ -75,10 +85,14 @@ _type2scope = {
     'groupName': 'domain',
     'cn-uid-position': 'one',
     'univentionObjectIdentifier': 'domain',
-}
+}  # type: Dict[_Types, _Scopes]
 
 
-def requestUserSid(lo, position, uid_s):
+def requestUserSid(
+    lo,  # type: univention.admin.uldap.access
+    position,  # type: univention.admin.uldap.position
+    uid_s,  # type: str
+):  # type: (...) -> str
     uid = int(uid_s)
     algorithmical_rid_base = 1000
     rid = str(uid * 2 + algorithmical_rid_base)
@@ -92,7 +106,12 @@ def requestUserSid(lo, position, uid_s):
     return request(lo, position, 'sid', sid)
 
 
-def requestGroupSid(lo, position, gid_s, generateDomainLocalSid=False):
+def requestGroupSid(
+    lo,  # type: univention.admin.uldap.access
+    position,  # type: univention.admin.uldap.position
+    gid_s,  # type: str
+    generateDomainLocalSid=False,  # type: bool
+):  # type: (...) -> str
     gid = int(gid_s)
     algorithmical_rid_base = 1000
     rid = str(gid * 2 + algorithmical_rid_base + 1)
@@ -107,17 +126,24 @@ def requestGroupSid(lo, position, gid_s, generateDomainLocalSid=False):
     return request(lo, position, 'sid', sid)
 
 
-def acquireRange(lo, position, atype, attr, ranges, scope='base'):
+def acquireRange(
+    lo,  # type: univention.admin.uldap.access
+    position,  # type: univention.admin.uldap.position
+    atype,  # type: _Types
+    attr,  # type: str
+    ranges,  # type: Sequence[Dict[str, int]]
+    scope='base',  # type: _Scopes
+):  # type: (...) -> str
     log.debug('ALLOCATE: Start allocation for type = %r', atype)
-    startID = lo.getAttr('cn=%s,cn=temporary,cn=univention,%s' % (ldap.dn.escape_dn_chars(atype), position.getBase()), 'univentionLastUsedValue')
+    start_id = lo.getAttr('cn=%s,cn=temporary,cn=univention,%s' % (ldap.dn.escape_dn_chars(atype), position.getBase()), 'univentionLastUsedValue')
 
-    log.debug('ALLOCATE: Start ID = %r', startID)
+    log.debug('ALLOCATE: Start ID = %r', start_id)
 
-    if not startID:
+    if not start_id:
         startID = ranges[0]['first']
         log.debug('ALLOCATE: Set Start ID to first %r', startID)
     else:
-        startID = int(startID[0])
+        startID = int(start_id[0])
 
     for _range in ranges:
         if startID < _range['first']:
@@ -168,7 +194,14 @@ def acquireRange(lo, position, atype, attr, ranges, scope='base'):
     raise univention.admin.uexceptions.noLock(_('The attribute %r could not get locked.') % (atype,))
 
 
-def acquireUnique(lo, position, type, value, attr, scope='base'):
+def acquireUnique(
+    lo,  # type: univention.admin.uldap.access
+    position,  # type: univention.admin.uldap.position
+    type,  # type: _Types
+    value,  # type: str
+    attr,  # type: str
+    scope='base',  # type: _Scopes
+):  # type: (...) -> str
     log.debug('LOCK acquireUnique scope = %s', scope)
     searchBase = position.getDomain() if scope == "domain" else position.getBase()
 
@@ -194,6 +227,7 @@ def acquireUnique(lo, position, type, value, attr, scope='base'):
         except KeyError:
             return value
 
+        assert base is not None
         if all(ldap.dn.str2dn(x)[0][0][0] not in attrs for x in lo.searchDn(base=base, filter='(|%s)' % ''.join(filter_format('(%s=%s)', (attr, value)) for attr in attrs), scope=scope)):
             return value
         raise univention.admin.uexceptions.alreadyUsedInSubtree('name=%r position=%r' % (value, base))
@@ -216,13 +250,45 @@ def acquireUnique(lo, position, type, value, attr, scope='base'):
     raise univention.admin.uexceptions.noLock(_('The attribute %r could not get locked.') % (type,))
 
 
-def request(lo, position, type, value=None):
+@overload
+def request(
+    lo,  # type: univention.admin.uldap.access
+    position,  # type: univention.admin.uldap.position
+    type,  # type: _TypesUidGid
+    value=None,  # type: Optional[str]
+):  # type: (...) -> str
+    pass
+
+
+@overload
+def request(
+    lo,  # type: univention.admin.uldap.access
+    position,  # type: univention.admin.uldap.position
+    type,  # type: _Types
+    value,  # type: str
+):  # type: (...) -> str
+    pass
+
+
+def request(
+    lo,  # type: univention.admin.uldap.access
+    position,  # type: univention.admin.uldap.position
+    type,  # type: _Types
+    value=None,  # type: Optional[str]
+):  # type: (...) -> str
     if type in ('uidNumber', 'gidNumber'):
         return acquireRange(lo, position, type, _type2attr[type], [{'first': 1000, 'last': 55000}, {'first': 65536, 'last': 1000000}], scope=_type2scope[type])
+    assert value is not None
     return acquireUnique(lo, position, type, value, _type2attr[type], scope=_type2scope[type])
 
 
-def confirm(lo, position, type, value, updateLastUsedValue=True):
+def confirm(
+    lo,  # type: univention.admin.uldap.access
+    position,  # type: univention.admin.uldap.position
+    type,  # type: _Types
+    value,  # type: str
+    updateLastUsedValue=True,  # type: bool
+):  # type: (...) -> None
     if type in ('uidNumber', 'gidNumber') and updateLastUsedValue:
         lo.modify('cn=%s,cn=temporary,cn=univention,%s' % (ldap.dn.escape_dn_chars(type), position.getBase()), [('univentionLastUsedValue', b'1', value.encode('utf-8'))])
     elif type == 'cn-uid-position':
@@ -230,5 +296,10 @@ def confirm(lo, position, type, value, updateLastUsedValue=True):
     univention.admin.locking.unlock(lo, position, type, value.encode('utf-8'), _type2scope[type])
 
 
-def release(lo, position, type, value):
+def release(
+    lo,  # type: univention.admin.uldap.access
+    position,  # type: univention.admin.uldap.position
+    type,  # type: _Types
+    value,  # type: str
+):  # type: (...) -> None
     univention.admin.locking.unlock(lo, position, type, value.encode('utf-8'), _type2scope[type])

management/univention-directory-manager-modules/modules/univention/admin/blocklist.py
@@ -35,6 +35,7 @@
 import hashlib
 import re
 from datetime import datetime
+from typing import TYPE_CHECKING, Any, Dict, Iterable, List, Optional  # noqa: F401
 
 import ldap
 from dateutil.relativedelta import relativedelta
@@ -42,6 +43,12 @@ from dateutil.relativedelta import relativedelta
 import univention.admin.uexceptions
 import univention.admin.uldap
 from univention.admin import configRegistry
+
+
+if TYPE_CHECKING:
+    import univention.admin.handlers
+    import univention.admin.handlers.blocklists.list
+    import univention.admin.modules
 
 
 translation = univention.admin.localization.translation('univention.admin.handlers')
@@ -61,6 +68,7 @@ def hash_blocklist_value(value):  # type: (bytes) -> str
 
 
 def parse_timedelta(timedelta_string):
+    # type: (str) -> Optional[relativedelta]
     """
     Parse time delta.
 
@@ -75,6 +83,7 @@ def parse_timedelta(timedelta_string):
 
 @univention.admin._ldap_cache(ttl=120)
 def get_blocklist_config(lo):
+    # type: (univention.admin.uldap.access) -> Dict
     config = {}
     try:
         for blist in univention.admin.handlers.blocklists.list.lookup(None, lo, 'entryUUID=*', base=BLOCKLIST_BASE, scope='one'):
@@ -89,11 +98,13 @@ def get_blocklist_config(lo):
 
 
 def get_blocking_udm_properties(udm_obj):
+    # type: (univention.admin.handlers.simpleLdap) -> Dict
     config = get_blocklist_config(udm_obj.lo_machine_primary)
     return config.get(udm_obj.module, {})
 
 
 def get_blockeduntil(dn, lo):
+    # type: (str, univention.admin.uldap.access) -> str
     config = get_blocklist_config(lo)
     retention = config.get(dn, '30d')
     blocking_duration = parse_timedelta(retention)
@@ -102,10 +113,12 @@ def get_blockeduntil(dn, lo):
 
 
 def blocklist_enabled(udm_obj):
+    # type: (univention.admin.handlers.simpleLdap) -> bool
     return not udm_obj.module.startswith('blocklists/') and configRegistry.is_true('directory/manager/blocklist/enabled', False)
 
 
 def get_blocklist_values_from_udm_property(udm_property_value, udm_property_name):
+    # type: (Any, str) -> List[Any]
     if isinstance(udm_property_value, (str, unicode)):
         return [udm_property_value]
     if not isinstance(udm_property_value, list) or not all(isinstance(mem, (str, unicode)) for mem in udm_property_value):
@@ -114,6 +127,7 @@ def get_blocklist_values_from_udm_property(udm_property_value, udm_property_name
 
 
 def create_blocklistentry(udm_obj):
+    # type: (univention.admin.handlers.simpleLdap) -> List
     if not blocklist_enabled(udm_obj):
         return []
     blocklist_entries = []
@@ -136,6 +150,7 @@ def create_blocklistentry(udm_obj):
 
 
 def check_blocklistentry(udm_obj):
+    # type: (univention.admin.handlers.simpleLdap) -> None
     if not blocklist_enabled(udm_obj):
         return
     for prop, bl_dn in get_blocking_udm_properties(udm_obj).items():
@@ -149,6 +164,7 @@ def check_blocklistentry(udm_obj):
 
 
 def cleanup_blocklistentry(blocklist_entries, udm_obj):
+    # type: (Iterable, univention.admin.handlers.simpleLdap) -> None
     for entry in blocklist_entries:
         try:
             udm_obj.lo_machine_primary.delete(entry)

management/univention-directory-manager-modules/modules/univention/admin/filter.py
@@ -49,7 +49,7 @@ class conjunction(object):
     OPS = frozenset({'&', '|', '!'})
 
     def __init__(self, type, expressions):
-        # type: (str, List[Union[conjunction, expression]]) -> None
+        # type: (str, Sequence[Union[conjunction, expression]]) -> None
         """
         Create LDAP filter conjunction or disjunction.
 
@@ -118,7 +118,7 @@ class conjunction(object):
         return '%s(%r, %r)' % (self.__class__.__name__, self.type, self.expressions)
 
     def append_unmapped_filter_string(self, filter_s, rewrite_function, mapping):
-        # type: (str, Callable[[expression, Optional[T]], None], T) -> None
+        # type: (Optional[str], Callable[[expression, Optional[T]], None], T) -> None
         if filter_s:
             filter_p = parse(filter_s)
             walk(filter_p, rewrite_function, arg=mapping)

commit 313e24992364350d14d86346a6673704608ef7bf

doc(udm): fix PEP-484 type annotations
Fixes: 4698a9611acc09cab68a7683b1ad98e12f418afd
Bug #50385

management/univention-directory-manager-modules/modules/univention/udm/udm.py
@@ -117,7 +117,7 @@ from .plugins import Plugins
 if TYPE_CHECKING:
     import univention.admin.uldap  # noqa: F401
 
-    from ..base import BaseModule, BaseObject  # noqa: F401
+    from .base import BaseModule, BaseObject  # noqa: F401
 
 
 _MODULES_PATH = 'univention.udm.modules'