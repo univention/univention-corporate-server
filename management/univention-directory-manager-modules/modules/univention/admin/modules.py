univention/univention-corporate-server

commit f56259d79f47767d440695703210626f96e8233d

refactor(udm): Switch to univention.admin.modules._get()
raising an exception and lazy loading modules.

Bug #50385

doc/errata/staging/univention-directory-manager-modules.yaml
@@ -3,7 +3,7 @@ release: "5.0"
 version: [6]
 scope: ucs_5.0-0-errata5.0-6
 src: univention-directory-manager-modules
-fix: 15.0.25-22
+fix: 15.0.25-23
 desc: |
  This update addresses the following issues:
  * The Univention Configuration Registry Policy UDM module now has an

management/univention-directory-manager-modules/modules/univention/admin/modules.py
@@ -195,32 +195,20 @@ def get(module):
     return module  # type: ignore[return-value]
 
 
-@overload
-def get_module(module):
-    # type: (UdmModule) -> UdmModule
-    pass
-
-
-@overload
-def get_module(module):
-    # type: (str) -> UdmModule | None
-    pass
-
-
-def get_module(module):
-    # type: (UdmName) -> UdmModule | None
+def _get(module):
+    # type: (UdmModule | str) -> UdmModule
     """
-    interim function, must only be used by `univention-directory-manager-modules`!
-
-    .. deprecated :: UCS 4.4
+    Internal function to lazy-load and get |UDM| module.
 
     :param module: either the name (str) of a module or the module itself.
-    :returns: the module or `None` if no module exists with the requested name.
+    :returns: the module
+    :raises KeyError: if no module exists with the requested name.
     """
     if not modules:
-        log.warning('univention.admin.modules.update() was not called')
         update()
-    return get(module)
+    if isinstance(module, str):
+        return modules[module]
+    return module
 
 
 def init(lo, position, module, template_object=None, force_reload=False):
@@ -242,7 +230,7 @@ def init(lo, position, module, template_object=None, force_reload=False):
         if module.module == 'users/user':
             # users/self inherits from users/user leading to errors when not reloading it as well
             # TODO: remove when droppng Python2.7 support and all super(object, self) calls have been replaced with super()
-            reload_module(univention.admin.modules.get('users/self'))  # type: ignore
+            reload_module(univention.admin.modules._get('users/self'))  # type: ignore
     # reset property descriptions to defaults if possible
     if hasattr(module, 'default_property_descriptions'):
         module.property_descriptions = copy.deepcopy(module.default_property_descriptions)

management/univention-directory-manager-modules/debian/control
@@ -131,6 +131,7 @@ Depends:
  ${python3:Depends},
 Breaks:
  python3-univention-directory-manager (<< 15.0.19-1),
+ ucs-test-framework (<< 10.0.20-49),
 Replaces:
  python3-univention-directory-manager (<< 15.0.19-1),
 Description: UCS - Modules for administration tools

management/univention-directory-manager-modules/debian/python3-univention-directory-manager.postinst
@@ -68,7 +68,7 @@ if [ "$1" = "configure" ] && dpkg --compare-versions "$2" lt-nl "15.0.11-29"; th
 		--append translationLongDescription='"fr_FR" "Définir des drapeaux pour un objet"'
 fi
 
-if [ "$1" = "configure" ] && dpkg --compare-versions "$2" lt-nl "15.0.25-22"; then
+if [ "$1" = "configure" ] && dpkg --compare-versions "$2" lt-nl "15.0.25-23"; then
 	deb-systemd-invoke restart univention-directory-listener.service || true
 	deb-systemd-invoke restart univention-directory-manager-rest.service || true
 	# restart ucs@school importer service if available

management/univention-directory-manager-modules/modules/univention/admin/config.py
@@ -94,13 +94,13 @@ def getDefaultContainer(lo, module):
     if module == 'dns/':
         module = 'dns/dns'
     try:
-        return univention.admin.modules.get(module).object.get_default_containers(lo)[0]
+        return univention.admin.modules._get(module).object.get_default_containers(lo)[0]
     except IndexError:
         return None
 
 
 def getDefaultValue(lo, name, position=None):
-    # type: (univention.admin.uldap.access, str, univention.admin.uldap.position) -> str | None
+    # type: (univention.admin.uldap.access, str, univention.admin.uldap.position | None) -> str | None
     """
     Return the default value for a UDM module.
 

management/univention-directory-manager-modules/modules/univention/admin/handlers/__init__.py
@@ -240,7 +240,7 @@ class simpleLdap(object):
             log.warning('univention.admin.modules.update() was not called')
             univention.admin.modules.update()
 
-        m = univention.admin.modules.get(self.module)
+        m = univention.admin.modules._get(self.module)
         if not hasattr(self, 'mapping'):
             self.mapping = getattr(m, 'mapping', None)  # type: univention.admin.mapping.mapping | None
 
@@ -1030,7 +1030,7 @@ class simpleLdap(object):
             log.exception("cancel() failed:")
 
     def _falsy_boolean_extended_attributes(self, info):  # type: (_Properties) -> _Properties
-        m = univention.admin.modules.get(self.module)
+        m = univention.admin.modules._get(self.module)
         for prop in getattr(m, 'extended_udm_attributes', []):
             if prop.syntax == 'boolean' and not info.get(prop.name):
                 info[prop.name] = u'0'
@@ -1108,8 +1108,7 @@ class simpleLdap(object):
         :param dict changes: A list of changes.
         :returns: The (modified) list of changes.
         """
-        m = univention.admin.modules.get(module.module)
-        assert m is not None
+        m = univention.admin.modules._get(module.module)
         attrs = getattr(m, 'extended_udm_attributes', None)
         if attrs:
             for prop in attrs:
@@ -1334,7 +1333,7 @@ class simpleLdap(object):
                 log.exception("Post-create: cancel() failed:")
             try:
                 if self._exists:  # add succeeded but _ldap_post_create failed!
-                    obj = univention.admin.objects.get(univention.admin.modules.get(self.module), None, self.lo, self.position, self.dn)
+                    obj = univention.admin.objects.get(univention.admin.modules._get(self.module), None, self.lo, self.position, self.dn)
                     obj.open()
                     obj.remove()
             except Exception:
@@ -1348,7 +1347,7 @@ class simpleLdap(object):
 
     def _ldap_object_classes_add(self, al):
         # type: (list[tuple[str, Any]]) -> list[tuple[str, Any]]
-        m = univention.admin.modules.get(self.module)
+        m = univention.admin.modules._get(self.module)
         # evaluate extended attributes
         ocs = set()  # type: set[str]
         for prop in getattr(m, 'extended_udm_attributes', []):
@@ -1446,7 +1445,7 @@ class simpleLdap(object):
 
     def _fix_app_options(self):  # type: () -> None
         # for objects with objectClass=appObject and appObjectActivated=0 we must set appObjectActivated=1
-        for option, opt in getattr(univention.admin.modules.get(self.module), 'options', {}).items():
+        for option, opt in getattr(univention.admin.modules._get(self.module), 'options', {}).items():
             if not opt.is_app_option or not self.option_toggled(option) or option not in self.options:
                 continue
             for pname, prop in self.descriptions.items():
@@ -1455,7 +1454,7 @@ class simpleLdap(object):
 
     def _ldap_object_classes(self, ml):  # type: (list[tuple]) -> list[tuple]
         """Detects the attributes changed in the given modlist, calculates the changes of the object class and appends it to the modlist."""
-        m = univention.admin.modules.get(self.module)
+        m = univention.admin.modules._get(self.module)
 
         def lowerset(vals):
             # type: (Iterable[str]) -> set[str]
@@ -1647,7 +1646,7 @@ class simpleLdap(object):
         pathlist = []
 
         log.debug("loadPolicyObject: policy_type: %s", policy_type)
-        policy_module = univention.admin.modules.get(policy_type)
+        policy_module = univention.admin.modules._get(policy_type)
 
         # overwrite property descriptions
         univention.admin.ucr_overwrite_properties(policy_module, self.lo)
@@ -1829,8 +1828,8 @@ class simpleLdap(object):
 
         :param univention.admin.uldap.access lo: UDM LDAP access object.
         """
-        containers = univention.admin.modules.defaultContainers(univention.admin.modules.get_module(cls.module))
-        settings_directory = univention.admin.modules.get_module('settings/directory')
+        containers = univention.admin.modules.defaultContainers(univention.admin.modules._get(cls.module))
+        settings_directory = univention.admin.modules._get('settings/directory')
         try:
             default_containers = settings_directory.lookup(None, lo, '', required=True)[0]
         except univention.admin.uexceptions.noObject:
@@ -1908,7 +1907,7 @@ class simpleLdap(object):
         filter_p = cls.unmapped_lookup_filter()
         # there are instances where the lookup/lookup_filter method of an module handler is called before
         # univention.admin.modules.update() was performed. (e.g. management/univention-directory-manager-modules/univention-dnsedit)
-        module = univention.admin.modules.get_module(cls.module)
+        module = univention.admin.modules._get(cls.module)
         filter_p.append_unmapped_filter_string(filter_s, cls.rewrite_filter, module.mapping)
         return filter_p
 
@@ -1958,7 +1957,7 @@ class simpleLdap(object):
             return
 
         # management/univention-management-console/src/univention/management/console/acl.py does not call univention.admin.modules.update()
-        mod = univention.admin.modules.get_module(cls.module)
+        mod = univention.admin.modules._get(cls.module)
         property_ = mod.property_descriptions.get(key)
 
         # map options to corresponding objectClass

management/univention-directory-manager-modules/modules/univention/admin/handlers/blocklists/all.py
@@ -85,11 +85,10 @@ def lookup(co, lo, filter_s, base='', superordinate=None, scope='sub', unique=Fa
     if not superordinate:
         filter_s = ""
         base = univention.admin.blocklist.BLOCKLIST_BASE
-    sup = univention.admin.modules.get(superordinate.module) if superordinate else None
+    sup = univention.admin.modules._get(superordinate.module) if superordinate else None
     res = []  # type: list[univention.admin.handlers.simpleLdap]
     for childmodule in (sup.childmodules if sup else childmodules):
-        mod = univention.admin.modules.get(childmodule)
-        assert mod is not None
+        mod = univention.admin.modules._get(childmodule)
         res += mod.lookup(co, lo, filter_s, base, superordinate, scope, unique, required, timeout, sizelimit)
     return res
 

management/univention-directory-manager-modules/modules/univention/admin/handlers/computers/__base.py
@@ -358,7 +358,7 @@ class ComputerObject(univention.admin.handlers.simpleComputer, nagios.Support, P
     def lookup_filter(cls, filter_s=None, lo=None):
         # type: (str | None, univention.admin.uldap.access | None) -> univention.admin.filter.conjunction
         lookup_filter_obj = cls.unmapped_lookup_filter()
-        module = univention.admin.modules.get_module(cls.module)
+        module = univention.admin.modules._get(cls.module)
         # ATTENTION: has its own rewrite function.
         lookup_filter_obj.append_unmapped_filter_string(filter_s, functools.partial(cls.rewrite_filter, lo=lo), module.mapping)
         return lookup_filter_obj

commit f3e015288cb303be09c33fffa949120bd8e44fd7

fix(udm): restore Python 2.7 compatibility
UMC modules running with Python 2.7 will decode JSON objects so that each decoded string is a unicode string.
Given that to the newly introduced `univention.admin.modules._get()` will break, due to explicit only check for `str`.

Traceback (most recent call last):
  File "/usr/lib/python2.7/dist-packages/univention/management/console/base.py", line 387, in __error_handling
    six.reraise(etype, exc, etraceback)
  File "/usr/lib/python2.7/dist-packages/univention/management/console/base.py", line 285, in execute
    function.__func__(self, request, *args, **kwargs)
  File "/usr/lib/python2.7/dist-packages/univention/management/console/modules/decorators.py", line 189, in _response
    return function(self, request)
  File "/usr/lib/python2.7/dist-packages/ucsschool/lib/school_umc_ldap_connection.py", line 156, in wrapper_func
    return func(*args, **kwargs)
  File "/usr/lib/python2.7/dist-packages/univention/management/console/modules/licenses/__init__.py", line 1023, in get_license
    self._import_licenses(license_handler, license_file, school)
  File "/usr/lib/python2.7/dist-packages/univention/management/console/modules/licenses/__init__.py", line 997, in _import_licenses
    import_license(license_handler, license)
  File "/usr/lib/python2.7/dist-packages/univention/bildungslogin/license_import/__init__.py", line 134, in import_license
    license_handler.create(license)
  File "/usr/lib/python2.7/dist-packages/univention/bildungslogin/handlers.py", line 76, in create
    udm_obj = self._licenses_mod.new()
  File "/usr/lib/python2.7/dist-packages/univention/udm/modules/generic.py", line 565, in new
    return self._load_obj('', superordinate)
  File "/usr/lib/python2.7/dist-packages/univention/udm/modules/generic.py", line 768, in _load_obj
    obj._copy_from_udm_obj()
  File "/usr/lib/python2.7/dist-packages/univention/udm/modules/generic.py", line 342, in _copy_from_udm_obj
    self.position = self._udm_module._get_default_object_positions()&#91;0&#93;
  File "/usr/lib/python2.7/dist-packages/univention/udm/modules/generic.py", line 662, in _get_default_object_positions
    default_containers = self._get_default_containers()
  File "/usr/lib/python2.7/dist-packages/univention/udm/modules/generic.py", line 640, in _get_default_containers
    mod = GenericModule('settings/directory', self.connection, 0)
  File "/usr/lib/python2.7/dist-packages/univention/udm/modules/generic.py", line 554, in __init__
    self._orig_udm_module = self._get_orig_udm_module()
  File "/usr/lib/python2.7/dist-packages/univention/udm/modules/generic.py", line 691, in _get_orig_udm_module
    univention.admin.modules.init(self.connection, po, udm_module)
  File "/usr/lib/python2.7/dist-packages/univention/admin/modules.py", line 240, in init
    univention.admin.ucr_overwrite_properties(module, lo)
  File "/usr/lib/python2.7/dist-packages/univention/admin/__init__.py", line 73, in ucr_overwrite_properties
    ucr_prefix = ucr_property_prefix % module.module
AttributeError: 'unicode' object has no attribute 'module'

Fixes: f56259d79

doc/errata/staging/univention-directory-manager-modules.yaml
@@ -1,0 +1,11 @@
+product: ucs
+release: "5.0"
+version: [7]
+scope: ucs_5.0-0-errata5.0-7
+src: univention-directory-manager-modules
+fix: 15.0.26-1
+desc: |
+ This update addresses the following issues:
+ * Compatibility with Python 2.7 has been restored, which was broken by
+   erratum 991.
+bug: [57146]

management/univention-directory-manager-modules/modules/univention/admin/modules.py
@@ -206,7 +206,7 @@ def _get(module):
     """
     if not modules:
         update()
-    if isinstance(module, str):
+    if isinstance(module, six.string_types):
         return modules[module]
     return module
 