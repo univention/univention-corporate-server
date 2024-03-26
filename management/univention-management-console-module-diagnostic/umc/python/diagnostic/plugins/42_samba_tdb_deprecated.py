834fd7c5e23299bd307ab767288999938d8b99f9
Felix Botner <botner@univention.de>
2024-03-26 17:16:03 +0100

    fixup!

--- management/univention-management-console-module-diagnostic/umc/python/diagnostic/plugins/42_samba_tdb_deprecated.py
+++ management/univention-management-console-module-diagnostic/umc/python/diagnostic/plugins/42_samba_tdb_deprecated.py
@@ -39,7 +39,7 @@ _ = Translation('univention-management-console-module-diagnostic').translate
 
 
 UCS = (5, 2)
-MIGRATION_URL = "https://help.univention.com/t/pre-update-checks-for-ucs-5-0-0-aborts-warning-about-a-very-large-samba-tdb-database/18014"
+MIGRATION_URL = "https://help.univention.com/t/22821"
 title = _('Checking Samba database type')
 description = _('''As of UCS {ucs[0]}.{ucs[1]}, Samba should use mdb as for its database.
 The steps to migrate the Samba database from tdb to mdb can be found in:''').format(ucs=UCS)
