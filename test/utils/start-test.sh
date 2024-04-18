ea4254045c6d16b66123d47f23e6e8935c9afed6
Philipp Hahn <hahn@univention.de>
2024-04-18 14:09:03 +0200

    refactor(test): Drop duplicate variables
    Instead of shadowing all internal defaults (lower case) and exporting
    them ad (upper case), only use the later.
    
    Bug #57129

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -4,30 +4,42 @@
 #
 
 # defaults for release
-release='5.0-6'
-old_release='4.4-9'
-kvm_template_version='5.0-6+e897'
-openstack_image_name='UCS 5.0-6'
-# AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 7
-current_ami=ami-09fefd41ed2cea5a4
-# AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 11
-old_ami=ami-02ad9aab36aadf18a
+release='5.0-6'  #
+export CURRENT_AMI="${CURRENT_AMI:=ami-09fefd41ed2cea5a4}"  # AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 7
+old_release='4.4-9'  #
+export OLD_AMI="${OLD_AMI:=ami-02ad9aab36aadf18a}"  # AMI: Univention Corporate Server (UCS) 4.4 (official image) rev. 11
+export KVM_UCSVERSION="${KVM_UCSVERSION:=5.0-6+e897}"  #
+export OPENSTACK_IMAGE_NAME="${OPENSTACK_IMAGE_NAME:=UCS 5.0-6}"  #
 
-# defaults
-kvm_template='generic-unsafe'
-kvm_build_server='tross.knut.univention.de'
-kvm_memory='2048M'
-kvm_cpus='1'
-kvm_label_suffix=''
-exact_match='false'
-ucsschool_release='scope'
-shutdown='false'
-source_iso="/var/univention/buildsystem2/isotests/ucs_${release}-latest-amd64.iso"
+export UCS_MINORRELEASE="${release%%-*}"
+export TARGET_VERSION="${TARGET_VERSION:=$release}"
+export UCS_VERSION="${UCS_VERSION:=$release}"
+export OLD_VERSION="${OLD_VERSION:=$old_release}"
+export KVM_TEMPLATE="${KVM_TEMPLATE:=generic-unsafe}"
+export KVM_OLDUCSVERSION="${KVM_OLDUCSVERSION:=$old_release}"
+export KVM_BUILD_SERVER="${KVM_BUILD_SERVER:=tross.knut.univention.de}"
+export KVM_MEMORY="${KVM_MEMORY:=2048M}"
+export KVM_CPUS="${KVM_CPUS:=1}"
+export KVM_LABEL_SUFFIX="${KVM_LABEL_SUFFIX:=}"
+export EXACT_MATCH="${EXACT_MATCH:=false}"
+export SHUTDOWN="${SHUTDOWN:=false}"
+export TERMINATE_ON_SUCCESS="${TERMINATE_ON_SUCCESS:=false}"
+export RELEASE_UPDATE="${release_update:=public}"
+export ERRATA_UPDATE="${errata_update:=testing}"
+export COMPONENT_VERSION="${COMPONENT_VERSION:=testing}"
+export UCSSCHOOL_RELEASE=${UCSSCHOOL_RELEASE:=scope}
+export SOURCE_ISO="${SOURCE_ISO:=/var/univention/buildsystem2/isotests/ucs_${release}-latest-amd64.iso}"
+_jenkins () { if [ -n "${JENKINS_HOME:-}" ]; then echo "${1:-true}"; else echo "${2:-false}"; fi; }
+export KVM_USER="$(_jenkins build "${KVM_USER:=$USER}")"
+export REPLACE="${REPLACE:=$(_jenkins)}"
+export UCS_TEST_RUN="${UCS_TEST_RUN:=$(_jenkins)}"
+export TERMINATE="${TERMINATE:=$(_jenkins)}"
 
 # some internal stuff
 image="${DIMAGE:-gitregistry.knut.univention.de/univention/dist/ucs-ec2-tools}"
-debug="${DEBUG:=false}"
-docker="${DOCKER:=true}"
+: "${DEBUG:=false}"
+: "${DOCKER:=true}"
+
 docker_env_file="$(mktemp)"
 
 have () {
@@ -50,14 +62,13 @@ fi
 usage () {
 	echo "${GREEN}Usage:${NORM} [ENV_VAR=setting] ... ${0##*/} [options] <scenario.cfg>"
 	echo ""
-	echo "Start scenario defined in <scenario.cfg>"
+	echo "Start scenario defined in <scenario.cfg> in AWS-EC2, OpenStack or KVM"
 	echo ""
 	echo "${GREEN}Options:${NORM}"
 	echo "  ${BOLD}-h${NORM}, ${BOLD}--help${NORM}    show this help message and exit"
 	echo "  ${BOLD}-I${NORM}, ${BOLD}--ignore${NORM}  Ignore missing files"
 	echo ""
 	echo "${GREEN}Example:${NORM}"
-	echo ""
 	echo "  # start scenario with default options"
 	echo "  ./utils/start-test.sh scenarios/autotest-090-master-no-samba.cfg"
 	echo ""
@@ -71,55 +82,59 @@ usage () {
 	echo "  All can also be referenced in the .cfg file via [ENV:name]."
 	echo "  The key 'environment:' in each VM section gets exported to the VM (UCS)."
 	echo ""
-	echo "  EC2"
-	echo "    <>${BOLD}CURRENT_AMI${NORM}          - the EC2 AMI for the current UCS release (default: $current_ami)"
-	echo "    <>${BOLD}OLD_AMI${NORM}              - the EC2 AMI for the release before the current (default: $old_ami)"
-	echo ""
 	echo "  UCS"
-	echo "    <>${BOLD}TARGET_VERSION${NORM}       - the version to expect to update during update tests (default: $release)"
-	echo "    <>${BOLD}UCS_VERSION${NORM}          - the current UCS version (default: $release)"
-	echo "     >${BOLD}UCS_MINORRELEASE${NORM}     - the current minor version (default: ${release%%-*})"
-	echo "    <>${BOLD}OLD_VERSION${NORM}          - the UCS version before the current UCS release (default: $old_release)"
+	echo "    <>${BOLD}OLD_VERSION${NORM}          - previous UCS minor release [${BOLD}$OLD_VERSION${NORM}]"
+	echo "    <>${BOLD}UCS_VERSION${NORM}          - current UCS release [${BOLD}$UCS_VERSION${NORM}]"
+	echo "     >${BOLD}UCS_MINORRELEASE${NORM}     - current UCS minor release [${BOLD}$UCS_MINORRELEASE${NORM}]"
+	echo "    <>${BOLD}TARGET_VERSION${NORM}       - UCS release to expect to update during update tests [${BOLD}$TARGET_VERSION${NORM}]"
+	echo ""
+	echo "  EC2"
+	echo "    <>${BOLD}OLD_AMI${NORM}              - EC2 AMI for previous UCS minor release [${BOLD}$OLD_AMI${NORM}]"
+	echo "    <>${BOLD}CURRENT_AMI${NORM}          - EC2 AMI for current UCS release [${BOLD}$CURRENT_AMI${NORM}]"
 	echo ""
 	echo "  KVM"
-	echo "    <>${BOLD}KVM_TEMPLATE${NORM}         - the KVM template to use (default: $kvm_template)"
-	echo "    <>${BOLD}KVM_UCSVERSION${NORM}       - the KVM template version (default: $kvm_template_version)"
-	echo "    <>${BOLD}KVM_OLDUCSVERSION${NORM}    - the KVM template version for the UCS release before the current release (default: $old_release)"
-	echo "    <>${BOLD}KVM_BUILD_SERVER${NORM}     - the KVM build server to use (default: $kvm_build_server)"
-	echo "    <>${BOLD}KVM_MEMORY${NORM}           - RAM for the KVM instance (default: $kvm_memory)"
-	echo "    <>${BOLD}KVM_CPUS${NORM}             - CPU's for the KVM instance (default: $kvm_cpus)"
-	echo "    <>${BOLD}KVM_LABEL_SUFFIX${NORM}     - additional label for instance name (default: $kvm_label_suffix)"
-	echo "    | ${BOLD}KVM_KEYPAIR_PASSPHRASE${NORM} - ssh key password, also used as a fallback password for the ssh connection"
-	echo "    <>${BOLD}SOURCE_ISO${NORM}           - an ISO to mount (default: $source_iso)"
+	echo "    <>${BOLD}KVM_TEMPLATE${NORM}         - KVM template name [${BOLD}$KVM_TEMPLATE${NORM}]"
+	echo "    <>${BOLD}KVM_OLDUCSVERSION${NORM}    - KVM template version for previous minor release [${BOLD}$KVM_OLDUCSVERSION${NORM}]"
+	echo "    <>${BOLD}KVM_UCSVERSION${NORM}       - KVM template version [${BOLD}$KVM_UCSVERSION${NORM}]"
+	echo "    <>${BOLD}KVM_MEMORY${NORM}           - KVM RAM [${BOLD}$KVM_MEMORY${NORM}]"
+	echo "    <>${BOLD}KVM_CPUS${NORM}             - KVM CPU's [${BOLD}$KVM_CPUS${NORM}]"
+	echo "    <>${BOLD}KVM_LABEL_SUFFIX${NORM}     - additional label for KVM name [${BOLD}$KVM_LABEL_SUFFIX${NORM}]"
+	echo "    | ${BOLD}KVM_KEYPAIR_PASSPHRASE${NORM} - ssh key passphrase and/or password for ssh VM connection"
+	echo "    <>${BOLD}SOURCE_ISO${NORM}           - an ISO to mount [${BOLD}$SOURCE_ISO${NORM}]"
+	echo ""
+	echo "  OpenStack"
+	echo "    <>${BOLD}OPENSTACK_IMAGE_NAME${NORM} - OS template name [${BOLD}$OPENSTACK_IMAGE_NAME${NORM}]"
 	echo ""
 	echo "  OpenStack"
 	echo "    <>${BOLD}OPENSTACK_IMAGE_NAME${NORM} - OS template name [$openstack_image_name]"
 	echo ""
 	echo "  ucs-*-create"
-	echo "    <>${BOLD}EXACT_MATCH${NORM}          - do --exact-match to exactly match template names (default: $exact_match)"
-	echo "    <>${BOLD}SHUTDOWN${NORM}             - --shutdown VM after run (default: $shutdown)"
-	echo "    <>${BOLD}TERMINATE${NORM}            - --terminate-always VMs after run (default: false)"
-	echo "    <>${BOLD}TERMINATE_ON_SUCCESS${NORM} - --terminate-on-success VMs after run iff setup has been successful (default: false, Jenkins:true)"
-	echo "    <>${BOLD}REPLACE${NORM}              - --replace existing VM with same name (default: false, Jenkins:true)"
+	echo "    <>${BOLD}KVM_BUILD_SERVER${NORM}     - KVM build server|'EC2'|'OpenStack'|'Openstack'|'OS' [${BOLD}$KVM_BUILD_SERVER${NORM}]"
+	echo "                             Overwritten by '*build_server:' from '.cfg'!"
+	echo "    <>${BOLD}EXACT_MATCH${NORM}          - do --exact-match to exactly match template names [${BOLD}$EXACT_MATCH${NORM}]"
+	echo "    <>${BOLD}SHUTDOWN${NORM}             - --shutdown VM after run [${BOLD}$SHUTDOWN${NORM}]"
+	echo "    <>${BOLD}TERMINATE${NORM}            - --terminate-always VMs after run [${BOLD}$TERMINATE${NORM}][Jenkins:true]"
+	echo "    <>${BOLD}TERMINATE_ON_SUCCESS${NORM} - --terminate-on-success VMs after run iff setup has been successful [${BOLD}$TERMINATE_ON_SUCCESS${NORM}]"
+	echo "    <>${BOLD}REPLACE${NORM}              - --replace existing VM with same name [${BOLD}$REPLACE${NORM}][Jenkins:true]"
 	echo ""
 	echo "  update behaviour/dev or released version"
-	# TODO make the env var a captial letter, -> modify jenkins seed job(s) and cfg files
-	echo "    <>${BOLD}release_update${NORM}       - public, testing or none for release updates (default: public)"
-	# TODO see RELEASE_UPDATE
-	echo "    <>${BOLD}errata_update${NORM}        - public, testing or none for errata updates (default: testing)"
-	echo "    <>${BOLD}UCSSCHOOL_RELEASE${NORM}    - UCS@school release (default: $ucsschool_release)"
-	echo "    <>${BOLD}COMPONENT_VERSION${NORM}    - update component? should indicate dev/released version of non UCS component (app, ...) (default: testing)"
-	echo "    | ${BOLD}SCOPE${NORM}                - defines a extra apt repo/scope that can be included during the test (default: None)"
-	echo "    | ${BOLD}TESTING${NORM}              - indicates unreleased UCS version (e.g. testing)"
+	echo "    < ${BOLD}release_update${NORM}       - 'public', 'testing' or 'none' for release updates [${BOLD}$RELEASE_UPDATE${NORM}]"
+	echo "     >${BOLD}RELEASE_UPDATE${NORM}"
+	echo "    < ${BOLD}errata_update${NORM}        - 'public', 'testing' or 'none' for errata updates [${BOLD}$ERRATA_UPDATE${NORM}]"
+	echo "     >${BOLD}ERRATA_UPDATE${NORM}"
+	echo "    <>${BOLD}UCSSCHOOL_RELEASE${NORM}    - U@S release [${BOLD}$UCSSCHOOL_RELEASE${NORM}]"
+	echo "    <>${BOLD}COMPONENT_VERSION${NORM}    - update component? should indicate dev/released version of non UCS component (app, ...) [${BOLD}$COMPONENT_VERSION${NORM}]"
+	echo "    | ${BOLD}SCOPE${NORM}                - extra APT repo/scope that can be included during test"
+	echo "    | ${BOLD}TESTING${NORM}              - indicates unreleased UCS (e.g. testing)"
 	echo ""
 	echo "  ucs-test/fetch-results"
-	echo "    <>${BOLD}UCS_TEST_RUN${NORM}         - if true, start ucs-test in utils/utils.sh::run_tests and copy log files from instance"
-	echo "                           in utils/utils-local.sh::fetch-results (default: false, Jenkins:true)"
+	echo "    <>${BOLD}UCS_TEST_RUN${NORM}         - start 'ucs-test' in 'utils/utils.sh::run_tests' and copy log files from VM"
+	echo "                             in 'utils/utils-local.sh::fetch-results' [${BOLD}$UCS_TEST_RUN${NORM}][Jenkins:true]"
 	echo ""
 	echo "  internal"
-	echo "    <>${BOLD}DOCKER${NORM}               - use docker container instead if local ucs-ec2-tools (default: true)"
-	echo "    < ${BOLD}DIMAGE${NORM}               - docker image (default: ${DIMAGE:--})"
-	echo "    <>${BOLD}DEBUG${NORM}                - debug mode (default: ${DEBUG:--})"
+	echo "    < ${BOLD}DOCKER${NORM}               - use docker container instead if local ucs-ec2-tools [${BOLD}$DOCKER${NORM}]"
+	echo "    < ${BOLD}DIMAGE${NORM}               - docker image [${BOLD}${DIMAGE:--}${NORM}]"
+	echo "    < ${BOLD}DEBUG${NORM}                - debug mode [${BOLD}${DEBUG:--}${NORM}]"
 	echo ""
 	echo "  apps"
 	echo "    | ${BOLD}APP_ID${NORM}               - An app ID, wekan"
@@ -132,7 +147,7 @@ die () {
 }
 
 cleanup () {
-	"$debug" ||
+	"$DEBUG" ||
 		rm -f "$docker_env_file"
 }
 
@@ -184,49 +199,12 @@ fi
 	! "$check_missing" ||
 	die "./utils dir is missing!"
 
-export CURRENT_AMI=${CURRENT_AMI:=$current_ami}
-export OLD_AMI=${OLD_AMI:=$old_ami}
-export UCS_MINORRELEASE="${release%%-*}"
-export TARGET_VERSION="${TARGET_VERSION:=$release}"
-export UCS_VERSION="${UCS_VERSION:=$release}"
-export OLD_VERSION="${OLD_VERSION:=$old_release}"
-export KVM_TEMPLATE="${KVM_TEMPLATE:=$kvm_template}"
-export KVM_UCSVERSION="${KVM_UCSVERSION:=$kvm_template_version}"
-export KVM_OLDUCSVERSION="${KVM_OLDUCSVERSION:=$old_release}"
-export KVM_BUILD_SERVER="${KVM_BUILD_SERVER:=$kvm_build_server}"
-export KVM_MEMORY="${KVM_MEMORY:=$kvm_memory}"
-export KVM_CPUS="${KVM_CPUS:=$kvm_cpus}"
-export KVM_LABEL_SUFFIX="${KVM_LABEL_SUFFIX:=$kvm_label_suffix}"
-export EXACT_MATCH="${EXACT_MATCH:=$exact_match}"
-export SHUTDOWN="${SHUTDOWN:=$shutdown}"
-export RELEASE_UPDATE="${release_update:=public}"
-export ERRATA_UPDATE="${errata_update:=testing}"
-export COMPONENT_VERSION="${COMPONENT_VERSION:=testing}"
-export UCSSCHOOL_RELEASE=${UCSSCHOOL_RELEASE:=$ucsschool_release}
-export OPENSTACK_IMAGE_NAME="${OPENSTACK_IMAGE_NAME:=$openstack_image_name}"
-export SOURCE_ISO="${SOURCE_ISO:=$source_iso}"
-
 # get image from cfg if not explicitly as env var
 if [ -z "$DIMAGE" ]; then
 	i="$(sed -n 's/^docker_image: //p' "$CFG")"
 	[ -n "$i" ] && image="$i"
 fi
 
-# Jenkins defaults
-if [ -n "${JENKINS_HOME:-}" ]
-then
-	export UCS_TEST_RUN="${UCS_TEST_RUN:=true}"
-	export TERMINATE="${TERMINATE:=true}"
-	export TERMINATE_ON_SUCCESS="${TERMINATE_ON_SUCCESS:=false}"
-	export KVM_USER="build"
-	export REPLACE="${REPLACE:=true}"
-else
-	export TERMINATE="${TERMINATE:=false}"
-	export TERMINATE_ON_SUCCESS="${TERMINATE_ON_SUCCESS:=false}"
-	export UCS_TEST_RUN="${UCS_TEST_RUN:=false}"
-	export KVM_USER="${KVM_USER:=$USER}"
-	export REPLACE="${REPLACE:=false}"
-fi
 
 # build server can be overwritten per cfg file `build_server`
 build_server="$(awk -F ": " '/^\w*build_server:/{print $2}' "$CFG")"
@@ -256,7 +234,7 @@ esac
 
 # start the test
 declare -a cmd=()
-if "$docker"
+if "$DOCKER"
 then
 	# get latest version of image
 	case "$image" in *.*/*) docker pull "$image" ;; esac
@@ -301,12 +279,12 @@ then
 	fi
 	cmd+=(${os_cfg:+-v "$os_cfg:/etc/openstack/clouds.yml:ro"})
 	# interactive mode for debug
-	$debug && cmd+=("-it")
+	$DEBUG && cmd+=("-it")
 	# the image to start
 	cmd+=("$image")
 fi
 
-"$debug" && "$docker" && cmd+=("bash" '-s' '--')
+"$DEBUG" && "$DOCKER" && cmd+=("bash" '-s' '--')
 
 cmd+=("$exe" -c "$CFG")
 # TODO, add debug mode as switch by env variable or possibly there will be verbose modes instead
