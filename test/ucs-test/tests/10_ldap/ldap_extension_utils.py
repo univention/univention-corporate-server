a25c5af09787a171210962eb308e3a4fbaa535e4
Philipp Hahn <hahn@univention.de>
2024-04-25 21:15:49 +0200

    refactor(ucs-test): Use retrying
    Issue univention/ucs#1854

--- test/ucs-test/tests/10_ldap/ldap_extension_utils.py
+++ test/ucs-test/tests/10_ldap/ldap_extension_utils.py
@@ -35,9 +35,10 @@ from typing import TYPE_CHECKING, Tuple
 import ldap
 import ldap.schema
 import psutil
+from retrying import retry
 
 import univention.uldap
-from univention.config_registry import ConfigRegistry
+from univention.config_registry import ConfigRegistry, ucr_live as ucr
 from univention.testing.strings import random_int, random_name
 
 
@@ -104,22 +105,9 @@ def call_unjoin_script(unjoin_script_name: str) -> int:
     return subprocess.call([join_script, '--binddn', ucr.get('tests/domainadmin/account'), '--bindpwdfile', ucr.get('tests/domainadmin/pwdfile')], shell=False)
 
 
+@retry(retry_on_exception=ldap.SERVER_DOWN, stop_max_attempt_number=ucr.get_int('ldap/client/retry/count', 15) + 1)
 def __fetch_schema_from_uri(ldap_uri: str) -> Tuple[str, ldap.schema.subentry.SubSchema]:
-    ucr = ConfigRegistry()
-    ucr.load()
-
-    retry = ucr.get_int('ldap/client/retry/count', 15)
-    attempts = retry + 1
-
-    i = 0
-    while i < attempts:
-        try:
-            return ldap.schema.subentry.urlfetch(ldap_uri)
-        except ldap.SERVER_DOWN:
-            if i >= (attempts - 1):
-                raise
-            time.sleep(1)
-        i += 1
+    return ldap.schema.subentry.urlfetch(ldap_uri)
 
 
 def fetch_schema_from_ldap_master() -> Tuple[str, ldap.schema.subentry.SubSchema]:
