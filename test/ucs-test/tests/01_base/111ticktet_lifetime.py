fb0c92968aaeca2fa8c672775cf995d3730edbbd
Philipp Hahn <hahn@univention.de>
2024-04-26 12:27:13 +0200

    refactor(ucs-test/01/111): Simplify krb lifetime check
    Issue univention/ucs#1854

--- test/ucs-test/tests/01_base/111ticktet_lifetime.py
+++ test/ucs-test/tests/01_base/111ticktet_lifetime.py
@@ -1,6 +1,6 @@
 #!/usr/share/ucs-test/runner pytest-3 -s -l -v
 ## desc: Test ticket lifetime are changed successfully
-## exposure: safe
+## exposure: careful
 ## roles:
 ##  - domaincontroller_master
 ## tags: []
@@ -8,67 +8,23 @@
 ##  - univention-config
 ## bugs: [52987]
 
-from __future__ import annotations
-
-import os
-from typing import Iterator
-
 import pytest
 
-from univention.config_registry import ConfigRegistry
-from univention.config_registry.frontend import ucr_update
 from univention.testing.utils import package_installed
 
 
 KRB5_PATH = "/etc/krb5.conf"
 SMB_PATH = "/etc/samba/smb.conf"
 VALUE = "50"
-key = "kerberos/defaults/ticket-lifetime"
+KEY = "kerberos/defaults/ticket-lifetime"
 
 
-def file_contain(file: str, text: str, no_exist_ignore: bool = True):
-    if os.path.exists(file):
-        with open(file) as f:
-            if text not in f.read():
-                return -1
-    elif not no_exist_ignore:
-        return -2
-    return 0
-
-
-@pytest.fixture()
-def myucr() -> Iterator[ConfigRegistry]:
-    """Per `function` auto-reverting UCR instance."""
-    with ConfigRegistry() as ucr:
-        yield ucr
-
-
-def test_kerberos_lifetime(myucr):
-    old_value = myucr.get(key, None)
-    value = -3
-    try:
-        ucr_update(myucr, {
-            key: VALUE,
-        })
-        value = file_contain(KRB5_PATH, f"ticket_lifetime = { VALUE }h")
-    finally:
-        ucr_update(myucr, {
-            key: old_value,
-        })
-    assert value == 0
+def test_kerberos_lifetime(ucr) -> None:
+    ucr.handler_set([f"{KEY}={VALUE}"])
+    assert f"ticket_lifetime = {VALUE}h" in open(KRB5_PATH).read()
 
 
 @pytest.mark.skipif(not package_installed('univention-samba4'), reason='Missing software: univention-samba4')
-def test_samba_lifetime(myucr):
-    old_value = myucr.get(key, None)
-    value = -3
-    try:
-        ucr_update(myucr, {
-            key: VALUE,
-        })
-        value = file_contain(SMB_PATH, f"kdc:user ticket lifetime = { VALUE }")
-    finally:
-        ucr_update(myucr, {
-            key: old_value,
-        })
-    assert value == 0
+def test_samba_lifetime(ucr) -> None:
+    ucr.handler_set([f"{KEY}={VALUE}"])
+    assert f"kdc:user ticket lifetime = {VALUE}" in open(SMB_PATH).read()
