21793e4ba743731e8c8f075948cf4583c638a13c
Philipp Hahn <hahn@univention.de>
2024-05-20 16:55:39 +0200

    test(ucs-test/59/61): all transitions
    test all 4 transitions, e.g. {disabled,locked} for {True,False}².
    The old test ever tested 2, picked on random choice.
    
    Simplify logic for testing.
    
    Issue univention/ucs#1854

--- test/ucs-test/tests/59_udm/61_test_udm_users_modification_set_deactivation_and_locked.py
+++ test/ucs-test/tests/59_udm/61_test_udm_users_modification_set_deactivation_and_locked.py
@@ -6,81 +6,57 @@
 ## packages:
 ##   - univention-directory-manager-tools
 
-import random
-import subprocess
-import time
+from __future__ import annotations
+
+from datetime import datetime
+
+import pytest
 
 import univention.admin.uldap
+from univention.lib.account import lock
 
 
-disabled_states = ('1', '0')
-locked_states = ['0', '1']
-
-transitions_log = []
-
-
-def test_user_modification_set_deactivation_and_locked(udm, ldap_base):
+@pytest.mark.parametrize("disabled", [False, True])
+@pytest.mark.parametrize("locked", [False, True])
+def test_user_modification_set_deactivation_and_locked(ucr, udm, disabled: bool, locked: bool) -> None:
     """Test changing disabled and locked simultaneously"""
-    user_dn = None
-    locked_state2 = locked_states.pop(random.randrange(len(locked_states)))
-    while locked_states:
-        locked_state1 = locked_states.pop(random.randrange(len(locked_states)))
-        disabled_states_list = list(disabled_states)
-        disabled_state1 = disabled_states_list.pop(random.randrange(len(disabled_states_list)))
-        user_dn = modify_and_check(udm, ldap_base, user_dn, disabled_state1, locked_state1)
-        while disabled_states_list:
-            disabled_state2 = disabled_states_list.pop(random.randrange(len(disabled_states_list)))
-            modify_and_check(udm, ldap_base, user_dn, disabled_state2, locked_state2)
-            modify_and_check(udm, ldap_base, user_dn, disabled_state1, locked_state1)
+    dn, _username = udm.create_user(**user_args(disabled=disabled, locked=locked))
+    lock_account(dn, locked)
 
-    transitions_log.pop(-1)
-    print_transitions()
+    check(dn, disabled, locked)
+
+    disabled, locked = not disabled, not locked
+
+    udm.modify_object('users/user', dn=dn, **user_args(disabled=disabled, locked=locked))
+    lock_account(dn, locked)
+
+    check(dn, disabled, locked)
 
 
-def print_transitions():
-    print('    disabled         | locked   -> disabled         | locked')
-    print('------------------------------------------------------------')
-    print('\n'.join(transitions_log))
-    print(f'{len(transitions_log)} transitions tested.\n')
+def user_args(*, disabled: bool, locked: bool) -> dict[str, object]:
+    kwargs = {
+        "disabled": "1" if disabled else "0",
+        "wait_for_replication": False,  # Primary only -> no UDN/UDL
+        "wait_for": False,  # S4C is slow
+    }
+    if not locked:
+        kwargs["locked"] = "0"
+    return kwargs
 
 
-def modify_and_check(udm, ldap_base, dn, disabled_state, locked_state):
-    print(f'*** disabled_state={disabled_state!r} locked_state={locked_state!r}')
-    if dn:
-        locked = {}
-        if locked_state == '0':
-            locked['locked'] = '0'
-        udm.modify_object('users/user', dn=dn, disabled=disabled_state, **locked)
-        if locked_state == '1':
-            locktime = time.strftime("%Y%m%d%H%M%SZ", time.gmtime())
-            subprocess.call(['python3', '-m', 'univention.lib.account', 'lock', '--dn', dn, '--lock-time', locktime])
-    else:
-        dn, _username = udm.create_user(
-            position=f'cn=users,{ldap_base}',
-            disabled=disabled_state,
-        )
-        if locked_state == '1':
-            locktime = time.strftime("%Y%m%d%H%M%SZ", time.gmtime())
-            subprocess.call(['python3', '-m', 'univention.lib.account', 'lock', '--dn', dn, '--lock-time', locktime])
+def lock_account(dn: str, locked: bool) -> None:
+    """Change read-only UDM property `users.user.locked`."""
+    lock(dn, f'{datetime.utcnow():%Y%m%d%H%M%S}Z' if locked else '')
 
-    krb_state = b'254' if disabled_state == '1' or 'kerberos' in disabled_state else b'126'
-    smb_disabled = disabled_state == '1' or 'windows' in disabled_state
-    smb_locked = locked_state in ('1')
-    if transitions_log:
-        transitions_log[-1] += f' -> {disabled_state:16} | {locked_state:8}'
 
+def check(dn: str, disabled: bool, locked: bool) -> None:
     # length of whitespace in sambaAcctFlags varies. cannot use utils.verify_ldap_object() to test it
-
     lo, _pos = univention.admin.uldap.getMachineConnection(ldap_master=False)
     user = lo.get(dn)
-    print_transitions()
-    assert user['krb5KDCFlags'] == [krb_state], 'krb5KDCFlags: expected {!r} found {!r}'.format(krb_state, user['krb5KDCFlags'])
-    assert not (smb_disabled and b'D' not in user['sambaAcctFlags'][0]), 'sambaAcctFlags: expected D in flags, found {!r}'.format(user['sambaAcctFlags'])
-    assert not ((smb_locked and not smb_disabled) and b'L' not in user['sambaAcctFlags'][0]), 'sambaAcctFlags: expected L in flags, found {!r}'.format(user['sambaAcctFlags'])
-    assert not ((smb_locked and smb_disabled) and b'L' in user['sambaAcctFlags'][0]), 'sambaAcctFlags: unexpected L in flags: {!r}'.format(user['sambaAcctFlags'])
-    assert not ((disabled_state == '1' or 'posix' in disabled_state) and user['shadowExpire'][0] != b'1'), 'shadowExpire: expected {!r} found {!r}'.format(['1'], user['shadowExpire'])
-    print('*** OK.')
-    if transitions_log:
-        transitions_log[-1] = f'OK: {transitions_log[-1]}'
-    transitions_log.append(f'{disabled_state:16} | {locked_state:8}')
-    return dn
+
+    assert user['krb5KDCFlags'] == [b'254' if disabled else b'126']
+    if disabled:
+        assert user['shadowExpire'][0] == b'1'
+        assert b'D' in user['sambaAcctFlags'][0]
+    if locked:
+        assert disabled != (b'L' in user['sambaAcctFlags'][0])
