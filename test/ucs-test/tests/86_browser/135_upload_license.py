e59ef03db290b040a8489e6f4823ab94bc0d5207
Philipp Hahn <hahn@univention.de>
2024-05-21 07:24:31 +0200

    fix(ucs-test/86/135): Do not modify license file
    Re-write to to use modern `pathlib.Path` and use `pytest` fixture
    `tmp_path_factory` to handle temporary files; allows investigating files
    on failure.
    
    Do not modify file "FreeForPersonalUseTest.license` shipped by package
    `ucs-test-umc`; this prevents running the test again.
    
    Issue univention/ucs#1854

--- test/ucs-test/tests/86_browser/135_upload_license.py
+++ test/ucs-test/tests/86_browser/135_upload_license.py
@@ -9,10 +9,8 @@
 ## exposure: dangerous
 
 import subprocess
-import tempfile
 from pathlib import Path
-from subprocess import CalledProcessError
-from typing import IO
+from typing import Iterator
 
 import pytest
 from playwright.sync_api import expect
@@ -24,32 +22,31 @@ from univention.testing.browser.sidemenu import SideMenuLicense
 
 _ = Translation('ucs-test-browser').translate
 
-dir_name = Path(__file__).parent
-core_edition_license_path = Path(dir_name, 'FreeForPersonalUseTest.license')
+FFPUT = Path(__file__).with_name('FreeForPersonalUseTest.license')
+
+pytestmark = pytest.mark.skipif(not FFPUT.is_file(), reason=f'{FFPUT} not found')
 
 
 @pytest.fixture()
-def license_file_path(side_menu_license: SideMenuLicense, ucr, ldap_base):
+def license_file_path(side_menu_license: SideMenuLicense, ucr, ldap_base, tmp_path) -> Iterator[Path]:
     ucr.save()
 
-    license_temp_file = tempfile.NamedTemporaryFile('w+')
     license_dn = f'cn=admin,cn=license,cn=univention,{ldap_base}'
 
-    modify_free_license_template(license_dn)
-    dump_current_license_to_file(license_temp_file, license_dn)
+    orig = dump_current_license_to_file(license_dn, tmp_path)
+    mod = modify_free_license_template(license_dn, tmp_path)
 
-    yield core_edition_license_path
+    try:
+        yield mod
+    finally:
+        side_menu_license.navigate(do_login=False)
+        side_menu_license.import_license(orig, False)
+        ucr.revert_to_original_registry()
 
-    ucr.revert_to_original_registry()
-    side_menu_license.navigate(do_login=False)
-    side_menu_license.import_license(Path(license_temp_file.name), False)
-    license_temp_file.close()
 
-
-@pytest.mark.skipif(not core_edition_license_path.is_file(), reason='FreeForPersonalUseTest.license file not found')
 @pytest.mark.parametrize('as_text', [True, False])
 def test_upload_license(side_menu_license: SideMenuLicense, license_file_path: Path, as_text: bool):
-    logger.info('Using %s as test license' % core_edition_license_path)
+    logger.info('Using %s as test license' % license_file_path)
     side_menu_license.tester.restart_umc()
 
     side_menu_license.navigate()
@@ -63,36 +60,30 @@ def check_license_information(side_menu_license: SideMenuLicense, license_file_p
 
     page = side_menu_license.page
 
-    with open(license_file_path) as license_file:
+    with license_file_path.open() as license_file:
         expected_license_type = next((line for line in license_file if line.startswith('univentionLicenseBaseDN: ')), None)
-        assert expected_license_type is not None
-        expected_license_type = expected_license_type.split(':')[1].strip()
-        expect(page.get_by_text(expected_license_type), f'expected license type to be {expected_license_type}').to_be_visible()
-        page.get_by_role('button', name=_('Close')).click()
+    assert expected_license_type is not None
+    expected_license_type = expected_license_type.split(':')[1].strip()
+    expect(page.get_by_text(expected_license_type), f'expected license type to be {expected_license_type}').to_be_visible()
+    page.get_by_role('button', name=_('Close')).click()
 
 
-def modify_free_license_template(license_dn: str):
-    try:
-        with core_edition_license_path.open('r+') as fd:
-            lines = fd.readlines()
-            fd.seek(0)
-            for line in lines:
-                if line.startswith('dn: '):
-                    line = f'dn: {license_dn}\n'
-                fd.write(line)
-    except (IOError, ValueError):
-        logger.exception('Error while modifying FreeForPersonalUseTest')
-        raise
+def modify_free_license_template(license_dn: str, tmp: Path) -> Path:
+    path = tmp / 'Modified.license'
+    with FFPUT.open('r') as fd, path.open("w") as out:
+        for line in fd:
+            key, sep, val = line.rstrip("\n").partition(": ")
+            out.write(f"{key}{sep}{license_dn if key == 'dn' else val}\n")
+    return path
 
 
-def dump_current_license_to_file(license_file: IO[str], license_dn: str):
+def dump_current_license_to_file(license_dn: str, tmp: Path) -> Path:
     """
     Opens a given 'license_file' for writing and puts in the output of
     launched 'univention-ldapsearch' with self.license_dn argument
     """
-    logger.info("Saving original license to file: '%s'" % license_file.name)
-    try:
-        subprocess.run(['univention-ldapsearch', '-LLLb', license_dn], stdout=license_file, check=True)
-    except (IOError, ValueError, OSError, CalledProcessError):
-        logger.exception('An error occurred backing up the old license')
-        raise
+    path = tmp / 'InitiallyInstalled.license'
+    logger.info("Saving original license to file: %s", path)
+    with path.open("w") as fd:
+        subprocess.run(['univention-ldapsearch', '-LLLb', license_dn], stdout=fd, check=True)
+    return path
