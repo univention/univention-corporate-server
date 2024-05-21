0ea809484f537fecb6bd00037f37e0a79dff0a56
Philipp Hahn <hahn@univention.de>
2024-05-21 12:18:31 +0200

    refactor(ucs-test/10/111): Convert to pytest
    Handle case when packages are already installed.
    
    Purge packages after test instead of only removing them.
    
    WARNING:
    73_udm-rest/00_general_tests.py::test_all_module_descriptions[test/ip_phone]
    fails if the packages are installed:
    
    >     @pytest.mark.parametrize('module', [n for n, m in univention.admin.modules.modules.items() if not getattr(m, 'virtual', False) and 'add' in m.operations])
    >     def test_all_module_descriptions(module, udm_client):
    >         mod = udm_client.get(module)
    > >       mod.new()
    > E       AttributeError: 'NoneType' object has no attribute 'new'
    
    Issue univention/ucs#1854

--- test/ucs-test/tests/10_ldap/111_test_slapschema.py
+++ test/ucs-test/tests/10_ldap/111_test_slapschema.py
@@ -1,21 +1,28 @@
-#!/usr/share/ucs-test/runner python3
+#!/usr/share/ucs-test/runner pytest-3
 ## desc: Check the diagnostic tool for missing schemas
 ## tags:
 ##  - schemas
 ##  - slapschema
 ## roles: [domaincontroller_master]
 ## bugs: [53455]
-## exposure:
+## exposure: dangerous
 
 import importlib.machinery
 import subprocess
 import types
+from contextlib import contextmanager, nullcontext
 from subprocess import check_call
+from typing import Iterator
 
 import pytest
 
 from univention.config_registry import ucr
 from univention.management.console.modules.diagnostic import Warning
+from univention.testing.utils import package_installed
+
+
+EXAMPLE = 'univention-directory-manager-module-example'
+SCHEMA = 'univention-directory-manager-module-example-schema'
 
 
 loader = importlib.machinery.SourceFileLoader('62_check_slapschema', '/usr/lib/python3/dist-packages/univention/management/console/modules/diagnostic/plugins/62_check_slapschema.py')
@@ -23,66 +30,31 @@ check_slap = types.ModuleType(loader.name)
 loader.exec_module(check_slap)
 
 
-def setup_environment():
-    # Install the package
-    # Check if test user exist
-    # Create test user if not exist
-    print("Setting up the test environment ...")
-    check_call(
-        ['univention-install', '-y', 'univention-directory-manager-module-example'],
-        stdout=subprocess.DEVNULL,
-    )
-    check_call(
-        ['udm', 'test/ip_phone', 'create', '--set', 'name=test111', '--set', 'ip=1.2.3.4', '--set', 'priuser=test@slapschema'],
-    )
+@contextmanager
+def environment() -> Iterator[None]:
+    check_call(['univention-install', '-y', EXAMPLE, SCHEMA], stdout=subprocess.DEVNULL)
+    try:
+        yield
+    finally:
+        check_call(['apt-get', '-y', 'purge', EXAMPLE, SCHEMA], stdout=subprocess.DEVNULL)
 
 
-def clean_environment():
-    # Install dependencies to be able to use test/ip_phone module
-    # Remove the test user
-    # Uninstall the example package
-    print("Cleaning the test environment ...")
-    check_call(
-        ['univention-install', '-y', 'univention-directory-manager-module-example'],
-        stdout=subprocess.DEVNULL,
-    )
-    check_call(
-        ['udm', 'test/ip_phone', 'remove', '--dn', "cn=test111,%(ldap/base)s" % ucr],
-    )
-    check_call(
-        ['apt-get', '-y', 'remove', 'univention-directory-manager-module-example'],
-        stdout=subprocess.DEVNULL,
-    )
+@contextmanager
+def entry() -> Iterator[None]:
+    check_call(['udm', 'test/ip_phone', 'create', '--set', 'name=test111', '--set', 'ip=1.2.3.4', '--set', 'priuser=test@slapschema'])
+    yield None
+    check_call(['udm', 'test/ip_phone', 'remove', '--dn', "cn=test111,%(ldap/base)s" % ucr])
 
 
-def remove_schema():
-    # Remove the schema
-    check_call(
-        ['apt-get', '-y', 'remove', 'univention-directory-manager-module-example-schema'],
-        stdout=subprocess.DEVNULL,
-    )
-    print("Removed schema")
+def test():
+    already_installed = package_installed(SCHEMA)
 
-
-def test_schema_is_present():
-    check_slap.run(None)
-    print("Test schema is present passed.")
-
-
-def test_schema_is_not_present():
-    with pytest.raises(Warning) as schemaInfo:
+    with nullcontext() if already_installed else environment(), entry():
         check_slap.run(None)
-    assert schemaInfo
-    print("Test schema is not present passed.")
 
-
-def main():
-    setup_environment()
-    test_schema_is_present()
-    remove_schema()
-    test_schema_is_not_present()
-    clean_environment()
-
-
-if __name__ == '__main__':
-    main()
+    if already_installed:
+        assert package_installed(SCHEMA)
+    else:
+        assert not package_installed(SCHEMA)
+        with pytest.raises(Warning):
+            check_slap.run(None)
