univention/univention-corporate-server

commit da5de01341cb4cd7938d9ebb33fec81dd4574d15

test(browser): remove waiting for certain requests to finish
This seems to be unnecessary and causes a lot of flakiness. In tests on
my VMs this change resulted in a lot more reliable results.

Bug #57046

test/ucs-test/tests/86_browser/11_umc_pam-login_unclosed_fh_test.py
@@ -37,8 +37,8 @@ def test_open_fd_after_login(umc_browser_test: UMCBrowserTest, udm, try_wrong_pw
         username = udm.create_user()[1]
         logger.info('Created user with username %s' % username)
         umc_browser_test.page.goto(f'{umc_browser_test.base_url}/univention/portal')
-        umc_browser_test.page.get_by_role('link', name=_('Login')).click()
-        umc_browser_test.login(username, password, check_for_no_module_available_popup=False, login_should_fail=False, do_navigation=False, skip_xhr_check=True)
+        umc_browser_test.page.get_by_role('link', name=_('Login Same tab')).click()
+        umc_browser_test.login(username, password, check_for_no_module_available_popup=False, login_should_fail=False, do_navigation=False)
         if not try_wrong_pw:
             umc_browser_test.page.wait_for_url(re.compile('.*/univention/portal/#.*'))
         umc_browser_test.end_umc_session()

test/ucs-test/tests/86_browser/130_miscellaneous.py
@@ -113,7 +113,7 @@ def test_module_visibility_for_regular_user(umc_browser_test: UMCBrowserTest, ud
     add_user_policy(udm, umc_browser_test, username)
     add_group_policy(udm, umc_browser_test, username)
 
-    umc_browser_test.login(username, skip_xhr_check=True)
+    umc_browser_test.login(username)
 
     allowed_modules: Set[str] = {
         _('Univention Configuration Registry'),

test/ucs-test/tests/86_browser/131_miscellaneous_module_availability.py
@@ -177,7 +177,7 @@ def test_module_availability(umc_browser_test: UMCBrowserTest, ucr):
     users = determine_users_by_join_status()
 
     for user in users:
-        umc_browser_test.login(user, skip_xhr_check=user != 'Administrator')
+        umc_browser_test.login(user)
         expected_modules = set(expected_modules_for_role[role][user])
         assert set(umc_browser_test.get_available_modules()) & expected_modules == expected_modules
 

test/ucs-test/univention/testing/browser/lib.py
@@ -269,7 +269,6 @@ class UMCBrowserTest(Interactions):
         login_should_fail: bool = False,
         do_navigation: bool = True,
         expect_password_change_prompt: bool = False,
-        skip_xhr_check: bool = False,
     ):
         """
         Navigates to {base_url}/univention/login?location={location} and logs in with the given credentials
@@ -281,7 +280,6 @@ class UMCBrowserTest(Interactions):
         :param login_should_fail: Returns after failure to log in with wrong credentials
         :param do_navigation: Wether to navigate to the login page
         :param expect_password_change_prompt: Expect a password change prompt to be visible after clicking the Login button
-        :param skip_xhr_check: Skip the check for certain requests to be completed
         """
         logger.info("Starting login to '%s' " % location)
         page = self.page
@@ -306,18 +304,7 @@ class UMCBrowserTest(Interactions):
             logger.info('Login failed as expected')
             return
 
-        if '/univention/management' in location and not check_for_no_module_available_popup and not skip_xhr_check:
-            logger.info('Logging in, waiting for requests to finish')
-            join_script_query = re.compile(r'https?://.+/univention/command/join/scripts/query')
-            license_query = re.compile(r'https?://.+/univention/command/udm/license')
-
-            if ucr.get('server/role') == 'domaincontroller_master':
-                with self.page.expect_response(join_script_query), self.page.expect_response(license_query):
-                    login_button.click()
-            else:
-                with self.page.expect_response(join_script_query):
-                    login_button.click()
-        elif check_for_no_module_available_popup:
+        if check_for_no_module_available_popup:
             login_button.click()
             logger.info("Checking for the 'No module for user available popup'")
             self.check_for_no_module_available_popup()

commit 0b1f3bf20ec0cfb9aa01fa3b808cb316566a868f

test(browser): wait for license check to complete
git:da5de01341cb4cd7938d9ebb33fec81dd4574d15 introduced a regression in
the password tests. Previously the waiting for the license check was
necessary since otherwise sometimes the password was changed before this
check completed, leading to an error.

Add it back here only for the password test, since it actually did
improve the stability of the none password tests.

Bug #56054

test/ucs-test/tests/86_browser/10_product_test_password_change.py
@@ -11,6 +11,7 @@
 ## join: true
 ## exposure: dangerous
 
+import re
 import time
 from enum import Enum
 
@@ -31,6 +32,7 @@ from univention.testing.ucs_samba import wait_for_drs_replication
 from univention.testing.utils import get_ldap_connection
 
 
+license_query = re.compile(r'https?://.+/univention/command/udm/license')
 _ = Translation('ucs-test-browser').translate
 
 
@@ -151,8 +153,15 @@ def change_own_password(
     check_for_no_module_available_popup: bool = False,
     **kwargs,
 ):
+    def side_menu_navigate():
+        side_menu.navigate(user.username, user.password, do_login=do_login, check_for_no_module_available_popup=check_for_no_module_available_popup, **kwargs)
+
     # When chaning the password for the admin, sometimes the password change is faster than the request to command/udm/license
-    side_menu.navigate(user.username, user.password, do_login=do_login, check_for_no_module_available_popup=check_for_no_module_available_popup, **kwargs)
+    if not check_for_no_module_available_popup and do_login:
+        with side_menu.page.expect_response(license_query):
+            side_menu_navigate()
+    else:
+        side_menu_navigate()
     side_menu.change_password(user.password, new_password)
 
     check_password_change_outcome(outcome, side_menu.page)
@@ -216,7 +225,8 @@ def test_for_short_password_error(admin_user: User, side_menu_user: SideMenuUser
 def test_usability_of_a_module_after_password_change(admin_user: User, random_password, umc_browser_test: UMCBrowserTest):
     user_module = UserModule(umc_browser_test)
     side_menu = SideMenuUser(umc_browser_test)
-    user_module.navigate(admin_user.username, admin_user.password)
+    with side_menu.page.expect_response(license_query):
+        user_module.navigate(admin_user.username, admin_user.password)
     expect(user_module.page.get_by_role('gridcell').first).to_be_visible(timeout=2 * 60 * 1000)
 
     change_own_password(admin_user, random_password, side_menu, do_login=False)