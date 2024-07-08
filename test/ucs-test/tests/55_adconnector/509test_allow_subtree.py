ec440a05bc3278df85838c00ec9f204d1e102a17
Arvid Requate <requate@univention.de>
2024-07-09 00:04:08 +0200

    Bug #57394: additional test cases
    * test_ignored_object_is_not_moved
    * test_ignored_object_is_not_removed
    
    and additional combinations:
    
    * ignoresubtree below allowsubtree
    * ignorelist below allowsubtree

--- test/ucs-test/tests/55_adconnector/509test_allow_subtree.py
+++ test/ucs-test/tests/55_adconnector/509test_allow_subtree.py
@@ -12,13 +12,12 @@ import pytest
 
 from univention.config_registry import handler_set as ucr_set
 from univention.testing import ucr as testing_ucr
+from univention.testing.connector_common import delete_con_user
 from univention.testing.strings import random_string
 from univention.testing.udm import UCSTestUDM
 from univention.testing.utils import LDAPObjectNotFound, LDAPObjectValueMissing
 
-from adconnector import (
-    ADConnection, connector_running_on_this_host, connector_setup, restart_adconnector, wait_for_sync,
-)
+from adconnector import ADConnection, connector_running_on_this_host, restart_adconnector, wait_for_sync
 
 
 # This is something weird. The `adconnector.ADConnection()` MUST be
@@ -67,49 +66,74 @@ class DomObject:
 @contextlib.contextmanager
 def allow_subtree_setup(
     sync_mode: str,
-    create_objects: bool = False,
+    pre_create_objects: bool = False,
     only_ucs: bool = False,
 ) -> Generator[Tuple[List, List, UCSTestUDM], None, None]:
     with UCSTestUDM() as udm:
         try:
             with testing_ucr.UCSTestConfigRegistry() as ucr:
-                # allow ou=ou1-allowed,base
-                # allow cn=cn2-allowed,cn=users
-                # deny  cn=users,base
-                # deny  cn=cn3-denied,base
-                allowed1 = SubTree(name='ou1-allowed')
+                # explicitly allow cn=cn1-allowed,base
+                # explicitly allow ou=ou2-allowed,ou=ou1,base
+                # so implicitly:
+                # - don't allow ou=ou1,base
+                # - don't allow cn=users,base
+                # - don't allow ou=ou3,ou=ou2-allowed,ou=ou1,base  # using ignoresubtree
+                # NOTE: consider https://help.univention.com/t/q-a-can-i-create-ous-under-cn-computers/13539
+                allowed1 = SubTree(name='cn1-allowed')
                 allowed1.udm_position = ucr['ldap/base']
                 allowed1.ad_position = ucr['connector/ad/ldap/base']
-                allowed1.udm_dn = f'ou={allowed1.name},{allowed1.udm_position}'
-                allowed1.ad_dn = f'ou={allowed1.name},{allowed1.ad_position}'
-                allowed2 = SubTree(name='cn2-allowed')
-                allowed2.udm_position = f'cn=users,{ucr["ldap/base"]}'
-                allowed2.ad_position = f'cn=users,{ucr["connector/ad/ldap/base"]}'
-                allowed2.udm_dn = f'cn={allowed2.name},{allowed2.udm_position}'
-                allowed2.ad_dn = f'cn={allowed2.name},{allowed2.ad_position}'
-                not_allowed1 = SubTree(name='users')
+                allowed1.udm_dn = f'cn={allowed1.name},{allowed1.udm_position}'
+                allowed1.ad_dn = f'cn={allowed1.name},{allowed1.ad_position}'
+
+                not_allowed1 = SubTree(name='ou1')
                 not_allowed1.udm_position = ucr['ldap/base']
                 not_allowed1.ad_position = ucr['connector/ad/ldap/base']
-                not_allowed1.udm_dn = f'cn={not_allowed1.name},{not_allowed1.udm_position}'
-                not_allowed1.ad_dn = f'cn={not_allowed1.name},{not_allowed1.ad_position}'
-                not_allowed2 = SubTree(name='cn3-denied')
+                not_allowed1.udm_dn = f'ou={not_allowed1.name},{not_allowed1.udm_position}'
+                not_allowed1.ad_dn = f'ou={not_allowed1.name},{not_allowed1.ad_position}'
+
+                allowed2 = SubTree(name='ou2-allowed')
+                allowed2.udm_position = not_allowed1.udm_dn  # position below not explicitly allowed OU
+                allowed2.ad_position = not_allowed1.ad_dn    # position below not explicitly allowed OU
+                allowed2.udm_dn = f'ou={allowed2.name},{allowed2.udm_position}'
+                allowed2.ad_dn = f'ou={allowed2.name},{allowed2.ad_position}'
+
+                not_allowed2 = SubTree(name='users')        # test not allowing the standard cn=users container
                 not_allowed2.udm_position = ucr['ldap/base']
                 not_allowed2.ad_position = ucr['connector/ad/ldap/base']
                 not_allowed2.udm_dn = f'cn={not_allowed2.name},{not_allowed2.udm_position}'
                 not_allowed2.ad_dn = f'cn={not_allowed2.name},{not_allowed2.ad_position}'
+
+                not_allowed3 = SubTree(name='ou3')
+                not_allowed3.udm_position = allowed2.udm_dn  # position below explicitly allowed OU
+                not_allowed3.ad_position = allowed2.ad_dn    # position below explicitly allowed OU
+                not_allowed3.udm_dn = f'ou={not_allowed3.name},{not_allowed3.udm_position}'
+                not_allowed3.ad_dn = f'ou={not_allowed3.name},{not_allowed3.ad_position}'
                 # create container and optionally some objects in the containers
-                udm.create_object('container/ou', name=allowed1.name)
-                udm.create_object('container/cn', name=allowed2.name, position=allowed2.udm_position)
-                udm.create_object('container/cn', name=not_allowed2.name, position=not_allowed2.udm_position)
-                if create_objects:
+                if ucr['connector/ad/mapping/syncmode'] not in ('sync', 'write'):
+                    # configure connector for sync
+                    ucr_set(
+                        [
+                            "connector/ad/mapping/syncmode=sync",
+                        ]
+                    )
+                    restart_adconnector()
+                udm.create_object('container/cn', name=allowed1.name, position=allowed1.udm_position)
+                udm.create_object('container/ou', name=not_allowed1.name)
+                udm.create_object('container/ou', name=allowed2.name, position=allowed2.udm_position)
+                # udm.create_object('container/cn', name=not_allowed2.name)   # not necessary to provision/remove, that's cn=users
+                udm.create_object('container/ou', name=not_allowed3.name, position=not_allowed3.udm_position)
+                if pre_create_objects:
                     not_allowed1.objects = create_objects_in_ucs(udm, not_allowed1, wait=False)
                     not_allowed2.objects = create_objects_in_ucs(udm, not_allowed2, wait=False)
+                    not_allowed3.objects = create_objects_in_ucs(udm, not_allowed3, wait=False)
                     allowed1.objects = create_objects_in_ucs(udm, allowed1, wait=False)
                     allowed2.objects = create_objects_in_ucs(udm, allowed2, wait=False)
                 wait_for_sync()
                 AD.verify_object(allowed1.ad_dn, {'name': allowed1.name})
                 AD.verify_object(allowed2.ad_dn, {'name': allowed2.name})
-                AD.verify_object(not_allowed2.ad_dn, {'name': not_allowed2.name})
+                AD.verify_object(not_allowed1.ad_dn, {'name': not_allowed1.name})
+                # AD.verify_object(not_allowed2.ad_dn, {'name': not_allowed2.name})   # that's cn=users
+                AD.verify_object(not_allowed3.ad_dn, {'name': not_allowed3.name})
                 # configure connector
                 ucr_set(
                     [
@@ -117,11 +141,13 @@ def allow_subtree_setup(
                         f"connector/ad/mapping/allowsubtree/test1/ad={allowed1.ad_dn}",
                         f"connector/ad/mapping/allowsubtree/test2/ucs={allowed2.udm_dn}",
                         f"connector/ad/mapping/allowsubtree/test2/ad={allowed2.ad_dn}",
+                        f"connector/ad/mapping/ignoresubtree/test3-ucs={not_allowed3.udm_dn}",
+                        f"connector/ad/mapping/ignoresubtree/test3-ad={not_allowed3.ad_dn}",
                         f"connector/ad/mapping/syncmode={sync_mode}",
                     ]
                 )
                 restart_adconnector()
-            yield ([allowed1, allowed2], [not_allowed1, not_allowed2], udm)
+            yield ([allowed1, allowed2], [not_allowed1, not_allowed2, not_allowed3], udm)
         finally:
             restart_adconnector()
     wait_for_sync()
@@ -189,7 +215,7 @@ def test_create(sync_mode: str) -> None:
     with allow_subtree_setup(sync_mode) as (allowed, denied, udm):
         # check denied for other subtrees
         for tree in denied:
-            if sync_mode in ['sync', 'write']:
+            if sync_mode in ('sync', 'write'):
                 # check objects created in UCS are not synced to AD
                 for obj in create_objects_in_ucs(udm, tree):
                     with pytest.raises(AssertionError):
@@ -202,11 +228,11 @@ def test_create(sync_mode: str) -> None:
                         udm.verify_ldap_object(obj.udm_dn, retry_count=3, delay=1)
         # check sync works
         for tree in allowed:
-            if sync_mode in ['sync', 'write']:
+            if sync_mode in ('sync', 'write'):
                 for obj in create_objects_in_ucs(udm, tree):
                     AD.verify_object(obj.ad_dn, {'name': obj.name})
                     udm.verify_ldap_object(obj.udm_dn)
-            if sync_mode in ['sync', 'read']:
+            if sync_mode in ('sync', 'read'):
                 for obj in create_objects_in_ad(AD, tree):
                     AD.verify_object(obj.ad_dn, {'name': obj.name})
                     udm.verify_ldap_object(obj.udm_dn)
@@ -216,9 +242,9 @@ def test_create(sync_mode: str) -> None:
 @pytest.mark.parametrize("sync_mode", ["sync"])
 @pytest.mark.skipif(not connector_running_on_this_host(), reason="Univention AD Connector not configured.")
 def test_modify(sync_mode: str) -> None:
-    with allow_subtree_setup(sync_mode, create_objects=True) as (allowed, denied, udm):
+    with allow_subtree_setup(sync_mode, pre_create_objects=True) as (allowed, denied, udm):
         for tree in denied:
-            if sync_mode in ['sync', 'write']:
+            if sync_mode in ('sync', 'write'):
                 for obj in tree.objects:
                     # modify in UCS, check no change in AD
                     udm.modify_object(obj.udm_module, dn=obj.udm_dn, description='changed in UCS')
@@ -226,7 +252,7 @@ def test_modify(sync_mode: str) -> None:
                     wait_for_sync()
                     with pytest.raises(AssertionError):
                         AD.verify_object(obj.ad_dn, {'description': 'changed in UCS'})
-            if sync_mode in ['sync', 'read']:
+            if sync_mode in ('sync', 'read'):
                 for obj in tree.objects:
                     # modify in AD, check no change in UCS
                     AD.set_attribute(obj.ad_dn, 'description', 'changed in AD'.encode('UTF-8'))
@@ -236,14 +262,14 @@ def test_modify(sync_mode: str) -> None:
                         udm.verify_ldap_object(obj.udm_dn, expected_attr={'description': ['changed in AD']}, retry_count=3, delay=1)
         # check sync works
         for tree in allowed:
-            if sync_mode in ['sync', 'write']:
+            if sync_mode in ('sync', 'write'):
                 for obj in tree.objects:
                     # modify in UCS, check change in AD
                     udm.modify_object(obj.udm_module, dn=obj.udm_dn, description='changed in UCS')
                     udm.verify_ldap_object(obj.udm_dn, expected_attr={'description': ['changed in UCS']})
                     wait_for_sync()
                     AD.verify_object(obj.ad_dn, {'description': 'changed in UCS'})
-            if sync_mode in ['sync', 'read']:
+            if sync_mode in ('sync', 'read'):
                 for obj in tree.objects:
                     # modify in AD, check cange in UCS
                     AD.set_attribute(obj.ad_dn, 'description', 'changed in AD'.encode('UTF-8'))
@@ -326,23 +352,44 @@ def test_move_out_of_allowed_subtree(sync_mode: str) -> None:
 
 @pytest.mark.parametrize("sync_mode", ["sync"])
 @pytest.mark.skipif(not connector_running_on_this_host(), reason="Univention AD Connector not configured.")
-def test_ignored_object_is_not_removed_after_move(sync_mode: str, ucr) -> None:
-    with connector_setup(sync_mode), UCSTestUDM() as udm:
-        dn, username = udm.create_user()
-        wait_for_sync()
-        ad_dn = f'cn={username},cn=users,{ucr["connector/ad/ldap/base"]}'
+def test_ignored_object_is_not_moved(sync_mode: str, ucr) -> None:
+    with allow_subtree_setup(sync_mode, pre_create_objects=True) as (allowed, denied, udm):  # noqa: F841
+        username = allowed[0].objects[0].name
+        udm_dn = allowed[0].objects[0].udm_dn
+        ad_dn = allowed[0].objects[0].ad_dn
         AD.verify_object(ad_dn, {'name': username})
         # set to ignore
         ignorelist = f'{ucr["connector/ad/mapping/user/ignorelist"]},{username}'
         ucr_set([f'connector/ad/mapping/user/ignorelist={ignorelist}'])
         restart_adconnector()
-        # move object in UCS and check it still exists in AD
-        udm.move_object('users/user', dn=dn, position=ucr['ldap/base'])
+        # move object in UCS to a different allowed subtree and check it didn't got moved in AD
+        udm.move_object('users/user', dn=udm_dn, position=allowed[1].udm_dn)
         wait_for_sync()
         AD.verify_object(ad_dn, {'name': username})
+        # cleanup
+        delete_con_user(AD, ad_dn, udm_dn, wait_for_sync)
+
+
+@pytest.mark.parametrize("sync_mode", ["sync"])
+@pytest.mark.skipif(not connector_running_on_this_host(), reason="Univention AD Connector not configured.")
+def test_ignored_object_is_not_removed(sync_mode: str, ucr) -> None:
+    with allow_subtree_setup(sync_mode, pre_create_objects=True) as (allowed, denied, udm):  # noqa: F841
+        username = allowed[0].objects[0].name
+        udm_dn = allowed[0].objects[0].udm_dn
+        ad_dn = allowed[0].objects[0].ad_dn
+        AD.verify_object(ad_dn, {'name': username})
+        # set to ignore
+        ignorelist = f'{ucr["connector/ad/mapping/user/ignorelist"]},{username}'
+        ucr_set([f'connector/ad/mapping/user/ignorelist={ignorelist}'])
+        restart_adconnector()
+        # remove object in UCS and check it didn't get deleted in AD
+        udm.remove_object('users/user', dn=udm_dn)
+        wait_for_sync()
+        AD.verify_object(ad_dn, {'name': username})
+        # cleanup
+        delete_con_user(AD, ad_dn, udm_dn, wait_for_sync)
 
 
 # TODO
-# def test_remove
 # def test_only_ucs
 # def test_only_ad
