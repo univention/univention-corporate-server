29ad24b20464a80ae8e0ed204308fe2733806efd
Felix Botner <botner@univention.de>
2024-07-31 15:06:07 +0200

    test(keycloak): test clients for keycloak migration
    Bug #57320: Add test for all documented clients for keycloak migration

--- test/ucs-test/tests/81_keycloak/12_test_migration.py
+++ test/ucs-test/tests/81_keycloak/12_test_migration.py
@@ -7,7 +7,7 @@
 import os
 
 import pytest
-from DATA import GOOGLE_CLIENT, NC_CLIENT, O365CLIENT, OC_CLIENT
+from DATA import GOOGLE_CLIENT, NC_CLIENT, O365CLIENT, OC_CLIENT, UMC_CLIENT
 from utils import run_command
 
 
@@ -20,6 +20,16 @@ def get_client_by_id(connection, client_id):
 
 
 def compare_client(old, new):
+    old_mappers = old.pop('protocolMappers', [])
+    new_mappers = new.pop('protocolMappers', [])
+    for mapp in old_mappers:
+        for new_mapp in new_mappers:
+            if mapp['name'] == new_mapp['name']:
+                compare_client(mapp, new_mapp)
+                break
+        else:
+            pytest.fail("Old client contains more protocolMappers than the new one")
+
     for key in old.keys():
         print(f"assert {key} expected value: {old[key]}, given value {new[key]}")
         if key == 'id':
@@ -33,65 +43,17 @@ def compare_client(old, new):
 
 
 @pytest.mark.skipif(not os.path.isfile('/etc/keycloak.secret'), reason='fails on hosts without keycloak.secret')
-def test_create_o365_client(keycloak_administrator_connection):
-    """Creates Microsoft 365 SAML client with univention-keycloak"""
-    client_id = 'urn:federation:MicrosoftOnline'
-    args = ['univention-keycloak', 'saml/sp', 'create', '--metadata-file=ms.xml', '--metadata-url=urn:federation:MicrosoftOnline', '--idp-initiated-sso-url-name=MicrosoftOnline', '--name-id-format=persistent']
-    run_command(args)
-    try:
-        kc_id, client = get_client_by_id(keycloak_administrator_connection, client_id)
-        run_command(args)
-        O365CLIENT['id'] = kc_id
-        compare_client(client, O365CLIENT)
-    finally:
-        keycloak_administrator_connection.delete_client(kc_id)
-
-
-@pytest.mark.skipif(not os.path.isfile('/etc/keycloak.secret'), reason='fails on hosts without keycloak.secret')
-def test_create_google_client(keycloak_administrator_connection):
+@pytest.mark.parametrize("client,client_id,args", [
+    (O365CLIENT, 'urn:federation:MicrosoftOnline', ['univention-keycloak', 'saml/sp', 'create', '--metadata-file=ms.xml', '--metadata-url=urn:federation:MicrosoftOnline', '--idp-initiated-sso-url-name=MicrosoftOnline', '--name-id-format=persistent']),
+    (OC_CLIENT, 'owncloudclient', ['univention-keycloak', 'oidc/rp', 'create', '--client-secret=univention', '--app-url=https://backup.ucs.test/owncloud/apps/openidconnect/redirect', 'owncloudclient']),
+    (NC_CLIENT, 'https://backup.ucs.test/nextcloud/apps/user_saml/saml/metadata', ['univention-keycloak', 'saml/sp', 'create', '--metadata-url=https://backup.ucs.test/nextcloud/apps/user_saml/saml/metadata', '--metadata-file=nc.xml', '--role-mapping-single-value']),
+    (GOOGLE_CLIENT, 'google.com', ['univention-keycloak', 'saml/sp', 'create', '--client-id=google.com', '--assertion-consumer-url-post=https://www.google.com/a/testdomain.com/acs', '--single-logout-service-url-post=https://www.google.com/a/testdomain.com/acs', '--idp-initiated-sso-url-name=google.com', '--name-id-format=email', '--frontchannel-logout-off']),
+    (UMC_CLIENT, 'https://whatever.ucs.test/univention/saml/metadata', ['univention-keycloak', 'saml/sp', 'create', '--metadata-url=https://whatever.ucs.test/univention/saml/metadata', '--metadata-file=umc.xml'])])
+def test_create_client(keycloak_administrator_connection, client, client_id, args):
     """Creates Google SAML client with univention-keycloak"""
-    client_id = "google.com"
-    args = ['univention-keycloak', 'saml/sp', 'create', '--client-id=google.com', '--assertion-consumer-url-post=https://www.google.com/a/testdomain.com/acs', '--single-logout-service-url-post=https://www.google.com/a/testdomain.com/acs', '--idp-initiated-sso-url-name=google.com', '--name-id-format=email', '--frontchannel-logout-off']
     run_command(args)
     try:
-        kc_id, client = get_client_by_id(keycloak_administrator_connection, client_id)
-        GOOGLE_CLIENT['id'] = kc_id
-        GOOGLE_CLIENT['protocolMappers'][0]['id'] = client['protocolMappers'][0]['id']
-        compare_client(client, GOOGLE_CLIENT)
-    finally:
-        keycloak_administrator_connection.delete_client(kc_id)
-
-
-@pytest.mark.skipif(not os.path.isfile('/etc/keycloak.secret'), reason='fails on hosts without keycloak.secret')
-def test_create_nextcloud_client(keycloak_administrator_connection, ucr):
-    """Creates Nextcloud SAML client with univention-keycloak"""
-    client_id = 'https://backup.ucs.test/nextcloud/apps/user_saml/saml/metadata'
-    args = ['univention-keycloak', 'saml/sp', 'create', '--metadata-url=https://backup.ucs.test/nextcloud/apps/user_saml/saml/metadata', '--metadata-file=nc.xml', '--role-mapping-single-value']
-    run_command(args)
-    kc_id, client = get_client_by_id(keycloak_administrator_connection, client_id)
-    try:
-        compare_client(client, NC_CLIENT)
-    finally:
-        keycloak_administrator_connection.delete_client(kc_id)
-
-
-@pytest.mark.skipif(not os.path.isfile('/etc/keycloak.secret'), reason='fails on hosts without keycloak.secret')
-def test_create_owncloud_client(keycloak_administrator_connection, ucr):
-    """Creates Owncloud SAML client with univention-keycloak"""
-    client_id = 'owncloudclient'
-    args = ['univention-keycloak', 'oidc/rp', 'create', '--client-secret=univention', '--app-url=https://backup.ucs.test/owncloud/apps/openidconnect/redirect', 'owncloudclient']
-    run_command(args)
-    try:
-        kc_id, client = get_client_by_id(keycloak_administrator_connection, client_id)
-        kc_mappers = client.pop('protocolMappers')
-        oc_mappers = OC_CLIENT.pop('protocolMappers')
-        compare_client(client, OC_CLIENT)
-        for m in range(len(kc_mappers)):
-            kc_mappers[m].pop('id')
-        OC_CLIENT['id'] = kc_id
-        assert client == OC_CLIENT
-        # list of dicts that are nested...
-        for mapper in oc_mappers:
-            assert mapper in kc_mappers
+        kc_id, new_client = get_client_by_id(keycloak_administrator_connection, client_id)
+        compare_client(new_client, client)
     finally:
         keycloak_administrator_connection.delete_client(kc_id)
