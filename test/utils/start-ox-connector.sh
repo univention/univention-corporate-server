ce08fdb8fc1845ec023ff531328d83b7bc0de936
Dirk Wiesenthal <wiesenthal@univention.de>
2024-09-13 09:12:40 +0200

    Bug #57532: Add personal dev envs for OX Connector to Jenkins

---
+++ test/utils/start-ox-connector.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export KVM_BUILD_SERVER="${KVM_BUILD_SERVER:=tross.knut.univention.de}"
+export TERMINATE=false
+export DOCKER=true
+export REPLACE=true
+export UCS_TEST_RUN=false
+export SCENARIO="${SCENARIO:=ox-connector-single}"
+
+# user specific instances "username_..."
+export KVM_OWNER="${BUILD_USER_ID:=$USER}"
+export JOB_BASE_NAME="$SCENARIO"
+
+exec ./utils/start-test.sh scenarios/ox-connector/${SCENARIO}.cfg
