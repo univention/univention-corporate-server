06407a2e82373f0815cfa9bd353cd6d95ca63712
Nico Gulden <gulden@univention.de>
2024-09-20 12:18:07 +0200

    Bug #57583: Add intersphinx mapping to UCS manual
    The changelog references some UCR variables,
    where the UCS Manual provides documentation for them.
    Adding a mapping to the UCS manual allows Sphinx to resolve those
    references and provide a link to the reader.

--- doc/changelog/conf.py
+++ doc/changelog/conf.py
@@ -14,12 +14,46 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+from __future__ import annotations
+
 import os
+import re
 import sys
 from datetime import date
+from typing import Iterator
+from urllib.parse import urlparse
 
 
-# sys.path.insert(0, os.path.abspath('.'))
+def _slugify(s: str) -> str:
+    return re.sub(r"[^0-9a-z]", "-", s.lower())[:63].strip("-")
+
+
+def _inventory(name: str, language: str) -> Iterator[str | None]:
+    env = os.environ
+    # Local build
+    yield f"../{name}/_build/{language}/html/objects.inv"
+    yield f"../{name}/_build/html/objects.inv"
+    # Previous build: current branch, merge target, default branch
+    for var in ('CI_COMMIT_REF_NAME', 'CI_MERGE_REQUEST_TARGET_BRANCH_NAME', 'CI_DEFAULT_BRANCH'):
+        try:
+            branch = env[var]
+            slug = _slugify(branch)
+            url = f"{env['CI_API_V4_URL']}/projects/{env['CI_PROJECT_ID']}/packages/generic/{name}.{language}/{slug}/objects.inv"
+        except LookupError:
+            continue
+        o = urlparse(url)
+        o = o._replace(netloc=f"job:{os.environ['CI_JOB_TOKEN']}@{o.netloc}")
+        yield o.geturl()
+    # Public build
+    yield None
+
+
+def ref(name: str, *, lang: str = "en", ver: str = "") -> tuple[str, tuple[str | None, ...]]:
+    ver = ver or version
+    return (
+        f"https://docs.software-univention.de/{name}/{ver}/{lang}",
+        tuple(_inventory(name, language)),
+    )
 
 
 # -- Project information -----------------------------------------------------
@@ -50,7 +84,12 @@ extensions = [
     "sphinx_sitemap",
     "sphinx_copybutton",
     "sphinxcontrib.bibtex",
+    "sphinx.ext.intersphinx",
 ]
+
+intersphinx_mapping = {
+    "uv-manual": ref("manual"),
+}
 
 # Warnings may come up by sphinx-last-updated-by-git. Suppress such warnings for all jobs.
 suppress_warnings = ['git.too_shallow']
