62dd9053f6745596676966314ceb1fa8ca0ae743
Felix Botner <botner@univention.de>
2024-09-24 09:43:57 +0200

    Issue univention/dev-issues/dev-incidents#73: different LDAP base for UCS and AD in tests

--- test/utils/start-ucs-adconnector-w2k19.sh
+++ test/utils/start-ucs-adconnector-w2k19.sh
@@ -12,5 +12,6 @@ export UCS_TEST_RUN=false
 # user specific instances "username_..."
 export KVM_OWNER="${BUILD_USER_ID:=$USER}"
 export JOB_BASE_NAME="${JOB_BASE_NAME:=ucs-adconnector-w2k19-env}"
+export AD_BASE="${AD_BASE:=ad.test}"
 
 exec ./utils/start-test.sh scenarios/base/ucs-ad-connector-w2k19.cfg
