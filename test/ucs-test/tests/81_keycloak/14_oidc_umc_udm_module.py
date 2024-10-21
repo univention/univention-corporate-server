5b420b09c93c6a475352b50c0aee354749fe07a1
Marius Meschter <marius.meschter@univention.de>
2024-10-21 16:32:53 +0200

    Bug #57533: fix test 81_keycloak/14_oidc_umc_udm_module.py
    in keycloak external fqdn scenario

--- test/ucs-test/tests/81_keycloak/14_oidc_umc_udm_module.py
+++ test/ucs-test/tests/81_keycloak/14_oidc_umc_udm_module.py
@@ -1,7 +1,7 @@
 #!/usr/share/ucs-test/runner /usr/share/ucs-test/playwright
 ## desc: Test accessing UMC UDM LDAP module with new access token
 ## tags: [keycloak]
-## roles: [domaincontroller_master, domaincontroller_backup]
+## roles: [domaincontroller_master]
 ## exposure: dangerous
 
 import time
@@ -15,7 +15,8 @@ from utils import run_command
 from univention.config_registry.frontend import ucr_update
 
 
-ACCESS_TOKEN_LIFESPAN = 15
+ACCESS_TOKEN_LIFESPAN = 60
+OAUTH_BEARER_GRACE = 1
 
 
 class ExtractFormAction(HTMLParser):
@@ -36,10 +37,10 @@ def disable_saml_oauthbearer_grace(ucr_proper):
     ucrv = 'ldap/server/sasl/oauthbearer/grace-time'
     original = ucr_proper.get(ucrv, None)
 
-    ucr_update(ucr_proper, {ucrv: '0'})
+    ucr_update(ucr_proper, {ucrv: str(OAUTH_BEARER_GRACE)})
     run_command(['systemctl', 'restart', 'slapd'])
     run_command(['systemctl', 'restart', 'univention-management-console-server'])
-    time.sleep(10)
+    time.sleep(15)
 
     yield
 
@@ -49,8 +50,8 @@ def disable_saml_oauthbearer_grace(ucr_proper):
 
 
 @pytest.fixture()
-def umc_base_url(ucr_proper):
-    return f"https://{ucr_proper.get('hostname')}.{ucr_proper.get('domainname')}/univention"
+def umc_base_url(portal_config):
+    return f'https://{portal_config.fqdn}/univention'
 
 
 @pytest.fixture()
@@ -69,11 +70,13 @@ def test_udm_module_with_new_access_token(umc_base_url: str, client: requests.Se
     parser = ExtractFormAction()
     parser.feed(resp.text)
     next_url = parser.form_action
+    assert next_url is not None and next_url != '', f'didn\'t find a redirect URL in HTML\n{resp.text}'
     if 'Kerberos Unsupported' in resp.text:
         resp = client.post(next_url)
         print('resp:')
         parser.feed(resp.text)
         next_url = parser.form_action
+        assert next_url is not None and next_url != '', f'didn\'t find a redirect URL in HTML\n{resp.text}'
 
     body = {'username': 'Administrator', 'password': 'univention', 'credentialId': ''}
 
@@ -91,8 +94,8 @@ def test_udm_module_with_new_access_token(umc_base_url: str, client: requests.Se
     resp = client.post(f'{umc_base_url}/command/udm/containers', json=umc_request_body, headers=xsrf_header)
     assert resp.status_code == 200, f"First UMC request failed: {resp.text}"
 
-    time.sleep(ACCESS_TOKEN_LIFESPAN + 5)
+    time.sleep(ACCESS_TOKEN_LIFESPAN + OAUTH_BEARER_GRACE * 60 + 15)
     run_command(['systemctl', 'restart', 'slapd'])
-    time.sleep(5)
+    time.sleep(10)
     resp = client.post(f'{umc_base_url}/command/udm/containers', json=umc_request_body, headers=xsrf_header)
     assert resp.status_code == 200, f"Second UMC request, after token refresh failed: {resp.text}"
