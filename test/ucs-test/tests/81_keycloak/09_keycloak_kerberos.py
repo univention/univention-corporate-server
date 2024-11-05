5299341cc6ae504af5e17ece748b500cb09f4904
Julia Bremer <bremer@univention.de>
2024-11-05 17:29:45 +0100

    Bug #56474: Add kerberos conditional auth tests

--- test/ucs-test/tests/81_keycloak/09_keycloak_kerberos.py
+++ test/ucs-test/tests/81_keycloak/09_keycloak_kerberos.py
@@ -4,28 +4,10 @@
 ## roles: [domaincontroller_master, domaincontroller_backup]
 ## exposure: dangerous
 
-import subprocess
-
-import requests
-from bs4 import BeautifulSoup
-from requests_kerberos import OPTIONAL, HTTPKerberosAuth
+import pytest
+from utils import kerberos_auth
 
 
-def test_kerberos_authentication(ucr):
-    ucr.handler_set(['kerberos/defaults/rdns=false'])
-    subprocess.call(['kdestroy'])
-    hostname = ucr.get('hostname')
-    domainname = ucr.get('domainname')
-    subprocess.check_call(['kinit', '--password-file=/var/lib/ucs-test/pwdfile', 'Administrator'])
-    session = requests.Session()
-    session.auth = HTTPKerberosAuth(mutual_authentication=OPTIONAL)
-
-    headers = {'Accept-Language': 'en-US;q=0.6,en;q=0.4', 'Referer': ''}
-    url = f'https://{hostname}.{domainname}/univention/saml/'
-    page = session.get(url, data=None, verify='/etc/univention/ssl/ucsCA/CAcert.pem', headers=headers)
-    assert page.status_code == 200
-
-    soup = BeautifulSoup(page.content, features='lxml')
-    saml_resp = soup.find('input', {'name': 'SAMLResponse'})
-    print('%s\nGot SAML respose' % (saml_resp,))
-    assert saml_resp
+@pytest.mark.parametrize('protocol', ['saml', 'oidc'])
+def test_kerberos_authentication(portal_login_via_keycloak, ucr, protocol, portal_config):
+    kerberos_auth(portal_login_via_keycloak, ucr, protocol, portal_config)
