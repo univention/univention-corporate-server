1bd4487f2190d4d3be592c0113c2e170c372b26d
Jürn Brodersen <brodersen@univention.de>
2024-12-13 13:19:46 +0100

    Issue univention/ucsschool#1258: Add python2 template exception

--- test/ucs-test/tests/03_ucr/37check-ucr-templates-py3-migration-status.py
+++ test/ucs-test/tests/03_ucr/37check-ucr-templates-py3-migration-status.py
@@ -36,6 +36,7 @@ ALLOWED_DIFFERENCES = [
     '/etc/univention/templates/files/var/lib/dovecot/sieve/default.sieve',  # different date if not commited in the same second
     '/etc/univention/templates/files/usr/share/univention-management-console/i18n/de/apps.mo',  # po-lib adds date
     '/etc/univention/templates/files/etc/mysql/mariadb.conf.d/60-ucr.cnf',  # not really different, but difference due to dict iteration
+    '/etc/univention/templates/files/etc/ucs-school-import-lusd/config.ini',  # new package without python2 support
 ] + glob.glob('/etc/univention/templates/files/etc/security/*.conf')
 IGNORE: Dict[str, Set[int]] = {
 }
