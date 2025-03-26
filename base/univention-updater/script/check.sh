0755fd680b42a5ba66ca7d9407ca06f33c3b4516
Arvid Requate <requate@univention.de>
2025-03-26 18:41:59 +0100

    Issue univention/ucs#2735: Require erratum 1230

--- base/univention-updater/script/check.sh
+++ base/univention-updater/script/check.sh
@@ -44,7 +44,7 @@ fi
 VERSION="52"  # we don't put 51 here because the upgrade should look like upgrading to UCS 5.2
 VERSION_NAME="5.1"
 MIN_VERSION="5.0-7"
-MIN_VERSION_SYSTEM="5.0-9-1204"
+MIN_VERSION_SYSTEM="5.0-10-1230"
 
 # shellcheck disable=SC2034
 updateLogDir="/var/univention-backup/update-to-${UPDATE_NEXT_VERSION:-$VERSION_NAME}"
@@ -129,7 +129,7 @@ update_check_slapd_on_member () {
 update_check_ldap_schema () {
 	[ -x /usr/sbin/slapschema ] ||
 		return 0
-	/usr/sbin/slapschema -f /etc/ldap/slapd.conf 1>&2 &&
+	/usr/sbin/slapschema -f /etc/ldap/slapd.conf -c 1>&2 &&
 		return 0
 
 	echo "	There is a problem with the LDAP schema on this system."
