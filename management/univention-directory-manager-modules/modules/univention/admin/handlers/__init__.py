univention/univention-corporate-server

commit 93ae62a30c3c3d720b6f8dff264f80c008553eca

univention-directory-manager-modules (7.0.229-1) unstable; urgency=low  .    * check_group_recursion: added more error message in case LDAP schema      extensions are deemed not to be installed; Bug #26104    * added additional cleanup upon removal of a computer object: service and      host records as well as aRecords of the corresponding forward zone;      Bug #26400
git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs@31868 1b283449-9f2f-0410-b571-8eb228e1a901

management/univention-directory-manager-modules/modules/univention/admin/handlers/__init__.py
@@ -2379,6 +2379,47 @@ class simpleComputer( simpleLdap ):
 				except Exception,e:
 					self.exceptions.append([_('DNS Alias'), _('delete'), e])
 
+		# remove service record entries (see Bug #26400)
+		univention.debug.debug(univention.debug.ADMIN, univention.debug.INFO, '_ldap_post_remove: clean up service records, host records, and IP address saved at the forward zone')
+		ips = set(self['ip'] or [])
+		fqdn = self['fqdn']
+		fqdnDot = '%s.' % fqdn  # we might have entires w/ or w/out trailing '.'
+
+		# iterate over all forward zones
+		for zone in self['dnsEntryZoneForward'] or []:
+			# load zone object
+			univention.debug.debug(univention.debug.ADMIN, univention.debug.INFO, 'clean up entries for zone: %s' % zone)
+			if len(zone) < 1:
+				continue
+			zoneObj = univention.admin.objects.get(univention.admin.modules.get('dns/forward_zone'), self.co, self.lo, self.position, dn = zone[0])
+			zoneObj.open()
+			univention.debug.debug(univention.debug.ADMIN, univention.debug.INFO, 'zone aRecords: %s' % zoneObj['a'])
+
+			# clean up aRecords of zone itself
+			new_entries = list(set(zoneObj['a']) - ips)
+			if len(new_entries) != len(zoneObj['a']):
+				univention.debug.debug(univention.debug.ADMIN, univention.debug.INFO, 'Clean up zone records:\n%s ==> %s' % (zoneObj['a'], new_entries))
+				zoneObj['a'] = new_entries
+				zoneObj.modify()
+
+			# clean up service records
+			for irecord in univention.admin.modules.lookup('dns/srv_record', self.co, self.lo, base=self.lo.base, scope='sub', superordinate=zoneObj):
+				irecord.open()
+				new_entries = [ j for j in irecord['location'] if fqdn not in j and fqdnDot not in j ]
+				if len(new_entries) != len(irecord['location']):
+					univention.debug.debug(univention.debug.ADMIN, univention.debug.INFO, 'Entry found in "%s":\n%s ==> %s' % (irecord.dn, irecord['location'], new_entries))
+					irecord['location'] = new_entries
+					irecord.modify()
+
+			# clean up host records (that should probably be done correctly by Samba4)
+			for irecord in univention.admin.modules.lookup('dns/host_record', self.co, self.lo, base=self.lo.base, scope='sub', superordinate=zoneObj):
+				irecord.open()
+				new_entries = list(set(irecord['a']) - ips)
+				if len(new_entries) != len(irecord['a']):
+					univention.debug.debug(univention.debug.ADMIN, univention.debug.INFO, 'Entry found in "%s":\n%s ==> %s' % (irecord.dn, irecord['a'], new_entries))
+					irecord['a'] = new_entries
+					irecord.modify()
+
 	def __setitem__(self, key, value):
 		raise_after = None
 		if key == 'network':

management/univention-directory-manager-modules/scripts/check_group_recursion
@@ -94,7 +94,7 @@ def checkChilds(grp_module, dn, parents, verbose=False, exception=False):
 	global grp2childgrps, co, lo
 
 	if not dn in grp2childgrps:
-		grpobj = univention.admin.objects.get(grp_module, co, lo, position='', dn=dn, arg='')
+		grpobj = univention.admin.objects.get(grp_module, co, lo, position='', dn=dn, attr=None)
 		grpobj.open()
 		childs = grpobj['nestedGroup']
 		grp2childgrps[ dn ] = childs
@@ -124,6 +124,7 @@ def main():
 	parser.add_option("-v", "--verbose", help="print debug output", dest="verbose", action="store_true")
 	(options, args) = parser.parse_args()
 
+	univention.admin.modules.update()
 	lo, position = get_ldap_connection(basedn)
 	grp_module = univention.admin.modules.get( 'groups/group' )
 	univention.admin.modules.init(lo, position, grp_module)

commit 05558d5e7cfcecde3172b2108b65c7006b41606d

perf(udm): fix performance of computer removal
Performance regression was introduced in Bug #26400, because all host
records in all zones of the computer which is going to be removed, are
searched instead of only host records for the specified IP addresses.

A customer environment had 12014 host records.

Fixes: 93ae62a30c3c3d720b6f8dff264f80c008553eca

Bug #58119

doc/errata/staging/univention-directory-manager-modules.yaml
@@ -3,9 +3,11 @@ release: "5.2"
 version: [1]
 scope: ucs_5.2-0-errata5.2-1
 src: univention-directory-manager-modules
-fix: 17.0.30-1
+fix: 17.0.31
 desc: |
  This update addresses the following issues:
  * First incremental release for new experimental feature delegative
    administration.
-bug: [58113]
+ * Improved performance when removing computers in environments with
+   many DNS host records significantly.
+bug: [58113, 58119]

management/univention-directory-manager-modules/modules/univention/admin/handlers/__init__.py
@@ -3335,14 +3335,13 @@ class simpleComputer(simpleLdap):
                     zoneObj['nameserver'].remove(fqdnDot)
                     zoneObj.modify()
 
-        # iterate over all forward zones
+        # iterate over all forward zones (again, as we are doing it already above!)
         for zone in self['dnsEntryZoneForward'] or []:
             # load zone object
             log.debug('clean up entries for zone: %s', zone)
             if not zone:
                 continue
-            zoneObj = univention.admin.objects.get(
-                univention.admin.modules.get('dns/forward_zone'), self.co, self.lo, self.position, dn=zone[0])
+            zoneObj = univention.admin.objects.get(univention.admin.modules.get('dns/forward_zone'), self.co, self.lo, self.position, dn=zone[0])
             assert zoneObj is not None
             zoneObj.open()
             log.debug('zone aRecords: %s', zoneObj['a'])
@@ -3375,8 +3374,9 @@ class simpleComputer(simpleLdap):
                     irecord['location'] = new_entries
                     irecord.modify()
 
-            # clean up host records (that should probably be done correctly by Samba4)
-            for irecord in univention.admin.modules.lookup('dns/host_record', self.co, self.lo, base=self.lo.base, scope='sub', superordinate=zoneObj):
+            # clean up host records again (that should probably be done correctly by Samba4); note: this will usually not find anything as this is already done above
+            host_filter = univention.admin.filter.conjunction('|', [univention.admin.filter.expression('a', _ip, escape=True) for _ip in ips])
+            for irecord in univention.admin.modules.lookup('dns/host_record', self.co, self.lo, str(host_filter), base=self.lo.base, scope='sub', superordinate=zoneObj):
                 irecord.open()
                 new_entries = list(set(irecord['a']) - ips)
                 if len(new_entries) != len(irecord['a']):