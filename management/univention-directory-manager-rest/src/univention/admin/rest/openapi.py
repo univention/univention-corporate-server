univention/univention-corporate-server

commit ed3c5949b826b7ad79da50329ec4123ed9cfc299

fix(udm-rest): Improve the openapi.json generation filtering out routes that don't exist in the API
univention/dev/internal/team-nubus#1143

Bug #58252

management/univention-directory-manager-rest/src/univention/admin/rest/openapi.py
@@ -928,22 +928,23 @@ class _OpenAPIBase:
                 request_model_patch['superordinate'] = {
                     "$ref": '#/components/schemas/superordinate',
                 }
-            openapi_schemas[f'{model_name}.request-patch'] = {
-                "type": "object",
-                "properties": request_model_patch,
-            }
-            openapi_schemas[f'{model_name}.request'] = {
-                "allOf": [{
-                    '$ref': f'#/components/schemas/{_openapi_quote(model_name + ".request-patch")}',
-                }, {
-                    'type': 'object',
-                    'properties': {
-                        "position": {
-                            "$ref": '#/components/schemas/position',
+            if set(module.operations) & {'add', 'edit', 'move', 'move_subtree'}:
+                openapi_schemas[f'{model_name}.request-patch'] = {
+                    "type": "object",
+                    "properties": request_model_patch,
+                }
+                openapi_schemas[f'{model_name}.request'] = {
+                    "allOf": [{
+                        '$ref': f'#/components/schemas/{_openapi_quote(model_name + ".request-patch")}',
+                    }, {
+                        'type': 'object',
+                        'properties': {
+                            "position": {
+                                "$ref": '#/components/schemas/position',
+                            },
                         },
-                    },
-                }],
-            }
+                    }],
+                }
             openapi_schemas[f'{model_name}.response-mixin'] = {
                 "type": "object",
                 "properties": {

commit 84a6acc7e451d5783c0e268f4aefcd292e5afdcb

fix(udm-rest): Fix the openapi spec by filtering out dangling references
Fixes: ed3c5949b8 fix(udm-rest): Improve the openapi.json generation filtering out routes that don't exist in the API

univention/dev/internal/team-nubus#1143

Bug #58252

doc/errata/staging/univention-directory-manager-rest.yaml
@@ -3,7 +3,7 @@ release: "5.2"
 version: [1]
 scope: ucs_5.2-0-errata5.2-1
 src: univention-directory-manager-rest
-fix: 12.1.0
+fix: 12.1.2
 desc: |
  This update addresses the following issues:
  * The OpenAPI schema generation has been enhanced.

management/univention-directory-manager-rest/src/univention/admin/rest/openapi.py
@@ -1008,12 +1008,16 @@ class _OpenAPIBase:
             openapi_schemas[model_name] = {
                 'allOf': [
                     {
-                        '$ref': f'#/components/schemas/{_openapi_quote(model_name)}.request',
-                    }, {
                         '$ref': f'#/components/schemas/{_openapi_quote(model_name)}.response-mixin',
                     },
                 ],
             }
+            if set(module.operations) & {'add', 'edit', 'move', 'move_subtree'}:
+                openapi_schemas[model_name]['allOf'].append(
+                    {
+                        '$ref': f'#/components/schemas/{_openapi_quote(model_name)}.request',
+                    },
+                )
             openapi_schemas[f'{model_name}.list'] = {
                 "type": "object",
                 "properties": {