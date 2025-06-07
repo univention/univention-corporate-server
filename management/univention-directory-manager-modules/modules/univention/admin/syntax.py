0ea8ae87087cfefcb6c4168ba54e9310960081eb
Florian Best <best@univention.de>
2025-06-07 11:00:41 +0200

    perf(udm): enhance get syntax_choices

--- management/univention-directory-manager-modules/modules/univention/admin/syntax.py
+++ management/univention-directory-manager-modules/modules/univention/admin/syntax.py
@@ -6060,10 +6060,7 @@ class LDAP_Search(select):
             else:
                 modules = univention.admin.modules.objectType(None, lo, dn)
                 display = None
-            try:
-                module = next(univention.admin.modules.get(m) for m in modules)
-            except StopIteration:
-                module = None
+            module = next((univention.admin.modules.get(m) for m in modules), None)
             if not module:
                 continue
             obj = module.object(None, lo, None, dn)
