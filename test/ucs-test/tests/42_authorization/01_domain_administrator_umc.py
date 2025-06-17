d9af041fa7b7ee5a13af7058eed73ac16811ae8e
Felix Botner <botner@univention.de>
2025-06-18 00:10:29 +0200

    fixup! fixup! fixup! fixup! test(udm-authorization): update umc authz tests

---
+++ test/ucs-test/tests/42_authorization/01_domain_administrator_umc.py
@@ -0,0 +1,320 @@
+#!/usr/share/ucs-test/runner pytest-3 -s -l -vvv
+## desc: Check delegated administration in UMC
+## bugs: [58113]
+## roles:
+##  - domaincontroller_master
+##  - domaincontroller_backup
+## exposure: dangerous
+import re
+import subprocess
+
+import pytest
+
+from univention.config_registry import ucr as _ucr
+from univention.lib.umc import BadRequest
+
+
+pytestmark = pytest.mark.skipif(not _ucr.is_true('umc/udm/delegation'), reason='authz not activated')
+
+
+@pytest.fixture(autouse=True)
+def restart_umc():
+    yield
+    subprocess.call(['deb-systemd-invoke', 'restart', 'univention-management-console-server.service'])
+
+
+def test_default_containers(admin_umc_client, udm):
+    container = udm.list_objects('container/cn', attr=['userPath'])
+    user_container = {x[0] for x in container if x[1].get('userPath') == ['1']}
+    group_container = {x[0] for x in container if x[1].get('groupPath') == ['1']}
+    res = admin_umc_client.umc_command('udm/containers', {'objectType': 'users/user'}, 'users/user').result
+    assert {x['id'] for x in res} == user_container
+    res = admin_umc_client.umc_command('udm/containers', {'objectType': 'groups/group'}, 'groups/group').result
+    assert {x['id'] for x in res} == group_container
+
+
+@pytest.mark.parametrize('position', [
+    ('cn=users,{ldap_base}'),
+    ('cn=users,{ou_dn}'),
+    ('{ou_dn}'),
+    ('{ldap_base}'),
+])
+def test_user_delete(ou, ldap_base, random_username, position, udm, admin_umc_client):
+    dn, _ = udm.create_user(position=position.format(ou_dn=ou.dn, ldap_base=ldap_base))
+    res = admin_umc_client.delete_object(dn, 'users/user')
+    assert res['success']
+
+
+@pytest.mark.parametrize('position', [
+    ('cn=users,{ldap_base}'),
+    ('cn=users,{ou_dn}'),
+    ('{ou_dn}'),
+    ('{ldap_base}'),
+])
+def test_user_create(ou, ldap_base, random_username, position, admin_umc_client):
+    res = admin_umc_client.create_user(position.format(ou_dn=ou.dn, ldap_base=ldap_base))
+    assert res['success']
+    admin_umc_client.delete_object(res['$dn$'], 'users/user')
+
+
+@pytest.mark.parametrize('position', [
+    ('cn=groups,{ldap_base}'),
+    ('cn=groups,{ou_dn}'),
+    ('{ou_dn}'),
+    ('{ldap_base}'),
+])
+def test_group_create(ou, ldap_base, position, admin_umc_client):
+    res = admin_umc_client.create_group(position.format(ou_dn=ou.dn, ldap_base=ldap_base))
+    assert res['success']
+    admin_umc_client.delete_object(res['$dn$'], 'groups/group')
+
+
+@pytest.mark.parametrize('position', [
+    ('cn=groups,{ldap_base}'),
+    ('cn=groups,{ou_dn}'),
+    ('{ou_dn}'),
+    ('{ldap_base}'),
+])
+def test_delete_group(ou, ldap_base, position, udm, random_username, admin_umc_client):
+    dn = udm.create_object(
+        'groups/group',
+        name=random_username(),
+        position=position.format(ou_dn=ou.dn, ldap_base=ldap_base),
+    )
+    res = admin_umc_client.delete_object(dn, 'groups/group')
+    assert res['success']
+
+
+@pytest.mark.parametrize('group_position, group_target_position', [
+    ('cn=groups,{ou_dn}', 'cn=groups,{ldap_base}'),
+    ('cn=groups,{ldap_base}', '{ou_cn_groups}'),
+])
+def test_move_group(ldap_base, ou, group_position, group_target_position, udm, random_username, admin_umc_client):
+    dn = udm.create_object(
+        'groups/group',
+        name=random_username(),
+        position=group_position.format(ou_dn=ou.dn, ldap_base=ldap_base),
+    )
+    position = group_target_position.format(ou_dn=ou.dn, ldap_base=ldap_base, ou_cn_groups=ou.group_default_container)
+    res = admin_umc_client.move_object(dn, position, 'groups/group')
+    assert res['success']
+
+
+@pytest.mark.parametrize('group_position, changes', [
+    ('cn=groups,{ldap_base}', {'description': 'dsfdsf'}),
+    ('cn=groups,{ou_dn}', {'guardianMemberRoles': 'app:namespace:role'}),
+    ('{ou_dn}', {'description': 'dsfdsf'}),
+    ('{ldap_base}', {'guardianMemberRoles': 'app:namespace:role'}),
+])
+def test_modify_group(ou, ldap_base, random_username, udm, group_position, changes, admin_umc_client):
+    dn = udm.create_object(
+        'groups/group',
+        name=random_username(),
+        position=group_position.format(ou_dn=ou.dn, ldap_base=ldap_base),
+    )
+    res = admin_umc_client.modify_object(dn, changes, 'groups/group')
+    assert res['success']
+
+
+@pytest.mark.parametrize('objectProperty, objectPropertyValue, expected', [
+    ('None', '', ['all']),
+    ('None', '*trator', ['admin']),
+    ('description', 'test', ['cn_test']),
+    ('description', 'tes*', ['cn_test']),
+    ('description', '*est', ['cn_test']),
+])
+def test_user_search(random_username, ou, objectProperty, objectPropertyValue, expected, udm, admin_umc_client):
+    dn_test = None
+    if objectProperty != 'None':
+        config = {
+            'username': random_username(),
+            'lastname': random_username(),
+            'password': 'univention',
+            objectProperty: 'test',
+        }
+        dn_test = udm.create_object('users/user', **config)
+    options = {
+        'container': 'all',
+        'hidden': 'all' in expected,
+        'objectType': 'users/user',
+        'objectProperty': objectProperty,
+        'objectPropertyValue': objectPropertyValue,
+        'fields': [
+            'name',
+            'path',
+            'displayName',
+            'mailPrimaryAddress',
+            'firstname',
+            'lastname',
+        ],
+    }
+    res = admin_umc_client.umc_command('udm/query', options, 'users/user').result
+    names = [x['name'] for x in res]
+    assert res
+    if 'all' in expected:
+        all_objects = udm.list_objects('users/user', properties=['DN'])
+        assert {obj[0] for obj in all_objects} == {x['$dn$'] for x in res}
+    if 'admin' in expected:
+        assert 'Administrator' in names, 'Administrator not found'
+    if objectProperty != 'None':
+        rex = re.compile(objectPropertyValue.replace('*', '.*'))
+        assert all(rex.match(x[objectProperty]) for x in res)
+        assert dn_test in [x['$dn$'] for x in res]
+    if 'not-self' in expected:
+        assert ou.admin_username not in names, f'{ou.normal_user_username} found'
+
+
+@pytest.mark.parametrize('position, target_position', [
+    ('cn=users,{ou_dn}', 'cn=users,{ldap_base}'),
+    ('cn=users,{ldap_base}', '{ou_cn_users}'),
+])
+def test_user_move(ldap_base, ou, position, target_position, udm, admin_umc_client):
+    dn, _ = udm.create_user(position=position.format(ou_dn=ou.dn, ldap_base=ldap_base))
+    position = target_position.format(ou_dn=ou.dn, ldap_base=ldap_base, ou_cn_users=ou.user_default_container)
+    res = admin_umc_client.move_object(dn, position, 'users/user')
+    assert res['success']
+
+
+@pytest.mark.parametrize('user_dn, attribute', [
+    ('uid=Administrator,cn=users,{ldap_base}', 'guardianInheritedRoles'),
+    ('{admin_ou}', 'guardianRoles'),
+    ('{normal_user}', 'guardianRoles'),
+])
+def test_user_read(ldap_base, ou, user_dn, attribute, admin_umc_client):
+    dn = user_dn.format(admin_ou=ou.admin_dn, normal_user=ou.user_dn, ldap_base=ldap_base)
+    res = admin_umc_client.get_object(dn, 'users/user')
+    assert res['$dn$'] == dn
+    if attribute:
+        assert attribute in res
+        assert res[attribute]
+
+
+@pytest.mark.parametrize('position, changes', [
+    ('cn=users,{ou_dn}', {'guardianRoles': ['umc:udm:ouadmin&umc:udm:ou=bremen']}),
+    ('cn=users,{ou_dn}', {'description': 'dsfdsf'}),
+    ('cn=users,{ldap_base}', {'description': 'dsfdsf'}),
+])
+def test_user_modify(ldap_base, ou, position, changes, udm, admin_umc_client):
+    dn, _ = udm.create_user(position=position.format(ou_dn=ou.dn, ldap_base=ldap_base))
+    res = admin_umc_client.modify_object(dn, changes, 'users/user')
+    assert res['success']
+    assert res['$dn$'] == dn
+
+
+def test_mail_domain_remove(ldap_base, udm, admin_umc_client, random_username):
+    domain_name = f'{random_username()}.test.com'
+    mail_domain_dn = udm.create_object('mail/domain', name=domain_name, position=f'cn=domain,cn=mail,{ldap_base}')
+    res = admin_umc_client.delete_object(mail_domain_dn, 'mail/domain')
+    assert res['success']
+
+
+def test_mail_domain_create(ldap_base, random_username, udm, admin_umc_client):
+    domain_name = f'{random_username()}.test.com'
+    position = f'cn=domain,cn=mail,{ldap_base}'
+    res = admin_umc_client.create_mail_domain(domain_name, position)
+    assert res['success']
+    domains = udm.list_objects('mail/domain')
+    assert res['$dn$'] in [x[0] for x in domains]
+    admin_umc_client.delete_object(res['$dn$'], 'mail/domain')
+
+
+@pytest.mark.parametrize('position', [
+    ('{ou_dn}'),
+    ('{ldap_base}'),
+])
+def test_query_and_read_mail_domain(udm, ldap_base, ou, position, admin_umc_client) -> None:
+    position = position.format(ou_dn=ou.dn, ldap_base=ldap_base)
+    domain_name = f'test-{ou.user_username}.com'
+    object_dn = f'cn={domain_name},{position}'
+    query_options = {
+        'container': 'all',
+        'hidden': False,
+        'objectType': 'mail/domain',
+        'objectProperty': 'None',
+        'objectPropertyValue': '',
+        'fields': [
+            'name',
+            'labelObjectType',
+            'path',
+        ],
+    }
+    get_option = [
+        object_dn,
+    ]
+
+    # get (should be impossible, as not created yet)
+    with pytest.raises(BadRequest):
+        admin_umc_client.umc_command('udm/get', get_option, 'mail/domain')  # type: ignore[call-arg]
+
+    # create
+    udm.create_object(
+        'mail/domain',
+        name=domain_name,
+        position=position,
+    )
+
+    # query
+    res = admin_umc_client.umc_command('udm/query', query_options, 'mail/mail').result  # type: ignore[call-arg]
+    assert object_dn in [x['$dn$'] for x in res]
+    assert len(res) == len(udm.list_objects('mail/domain', properties=['DN']))
+
+    # get (should be possible)
+    res = admin_umc_client.umc_command('udm/get', get_option, 'mail/domain').result  # type: ignore[call-arg]
+    assert res
+    assert res[0]['$dn$'] == object_dn
+    assert len(res) == 1
+
+    # delete
+    udm.remove_object('mail/domain', dn=object_dn)
+
+    # query all should be empty
+    res = admin_umc_client.umc_command('udm/query', query_options, 'mail/mail').result  # type: ignore[call-arg]
+    assert len(res) == len(udm.list_objects('mail/mail', properties=['DN']))
+
+    # get (should not be possible)
+    with pytest.raises(BadRequest):
+        admin_umc_client.umc_command('udm/get', get_option, 'mail/mail')  # type: ignore[call-arg]
+
+
+def test_syntax_choices_admin(admin_umc_client):
+    for syntax in ['UserDN', 'GroupDN', 'UserID', 'GroupID']:
+        res = admin_umc_client.umc_command('udm/syntax/choices', {'syntax': syntax}, 'shares/share')
+        assert res.result
+
+
+def test_syntax_choices(udm, ou, admin_umc_client):
+    """
+    Test that UserDN filtering works differently based on user roles.
+    OU admin should only see users from their own OU.
+    Domain admin should see all users.
+    """
+    res = admin_umc_client.umc_command('udm/syntax/choices', {'syntax': 'UserDN'}, 'shares/share').result
+    assert len(res) == len(udm.list_objects('users/user', properties=['DN']))
+
+    res = admin_umc_client.umc_command('udm/syntax/choices', {'syntax': 'UserID'}, 'shares/share').result
+    # all user + root
+    assert len(res) == len(udm.list_objects('users/user', properties=['DN'])) + 1
+
+
+def test_shares_create_admin(ldap_base, random_username, admin_umc_client):
+    # get default container
+    res = admin_umc_client.umc_command('udm/containers', {'objectType': 'shares/share'}, 'shares/share').result
+    assert res
+    # syntax choices
+    res = admin_umc_client.umc_command('udm/syntax/choices', {'syntax': 'UCS_Server'}, 'shares/share').result
+    assert res
+    # create share
+    options = [{
+        'object': {
+            'name': random_username(),
+            'host': f'{random_username()}.{random_username()}',
+            'path': f'/{random_username()}',
+        },
+        'options': {
+            'container': ldap_base,
+            'objectType': 'shares/share',
+        },
+    }]
+    res = admin_umc_client.umc_command('udm/add', options, 'shares/share').result[0]
+    assert res['success']
+    admin_umc_client.delete_object(res['$dn$'], 'shares/share')
---
+++ test/ucs-test/tests/42_authorization/conftest.py
@@ -0,0 +1,235 @@
+# Like what you see? Join us!
+# https://www.univention.com/about-us/careers/vacancies/
+#
+# Copyright 2025 Univention GmbH
+#
+# https://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <https://www.gnu.org/licenses/>.
+
+import locale
+import time
+from types import SimpleNamespace
+
+import pytest
+
+from univention.admin.rest.client import UDM as UDM_REST, UnprocessableEntity
+from univention.testing.strings import random_username
+from univention.testing.umc import Client
+from univention.testing.utils import UCSTestDomainAdminCredentials
+
+
+TRANSLATIONS = {
+    'de_DE': {
+        'Permission denied.': 'Zugriff verweigert.',
+        'No such object:': 'Das Objekt existiert nicht:',
+    },
+}
+
+
+def translate(string: str) -> str:
+    code, _ = locale.getlocale()
+    return TRANSLATIONS.get(code, {}).get(string, string)
+
+
+class RestClientHelper(UDM_REST):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user_module = self.get('users/user')
+        self.mail_domain_module = self.get('mail/domain')
+        self.group_module = self.get('groups/group')
+
+    def create_user(self, position: str):
+        obj = self.user_module.new(position=position)
+        obj.properties['username'] = random_username()
+        obj.properties['password'] = 'univention'
+        obj.properties['lastname'] = random_username()
+        obj.save()
+        return obj
+
+    def delete_user(self, dn: str):
+        obj = self.user_module.get(dn)
+        obj.delete()
+        with pytest.raises(UnprocessableEntity):
+            self.user_module.get(dn)
+
+    def search_user(self, filter_s: str):
+        return list(self.user_module.search(filter_s))
+
+    def move_user(self, dn: str, position: str):
+        obj = self.user_module.get(dn)
+        obj.move(position)
+        return obj
+
+    def modify_user(self, dn: str, changes: dict):
+        obj = self.user_module.get(dn)
+        for prop, value in changes.items():
+            obj.properties[prop] = value
+        obj.save()
+        return obj
+
+    def create_mail_domain(self):
+        obj = self.mail_domain_module.new()
+        obj.properties['name'] = random_username()
+        obj.save()
+        return obj
+
+    def delete_mail_domain(self, dn: str):
+        obj = self.mail_domain_module.get(dn)
+        obj.delete()
+        with pytest.raises(UnprocessableEntity):
+            self.mail_domain_module.get(dn)
+
+    def modify_group(self, dn: str, changes: dict):
+        obj = self.group_module.get(dn)
+        for prop, value in changes.items():
+            obj.properties[prop] = value
+        obj.save()
+        return obj
+
+
+class ClientHelper(Client):
+
+    def wait_for_progress(self, progress_id: str, object_type: str):
+        while True:
+            req = self.umc_command('udm/progress', {'progress_id': progress_id}, object_type)
+            res = req.result
+            if res['finished']:
+                return req
+            time.sleep(1)
+
+    def delete_object(self, dn: str, object_type: str) -> None:
+        options = [{
+            'object': dn,
+            'options': {
+                'cleanup': True,
+                'recursive': True,
+            },
+        }]
+        return self.umc_command('udm/remove', options, object_type).result[0]
+
+    def move_object(self, dn: str, position: str, object_type: str):
+        options = [{
+            'object': dn,
+            'options': {
+                'container': position,
+            },
+        }]
+        result = self.umc_command('udm/move', options, object_type).result
+        return self.wait_for_progress(result['id'], object_type).result['intermediate'][0]
+
+    def modify_object(self, dn: str, changes: dict, object_type: str):
+        changes['$dn$'] = dn
+        return self.umc_command('udm/put', [{'object': changes}], object_type).result[0]
+
+    def get_object(self, dn: str, object_type: str):
+        options = [dn]
+        res = self.umc_command('udm/get', options, object_type)
+        return res.result[0]
+
+    def create_user(self, position: str):
+        options = [{
+            'object': {
+                'lastname': random_username(),
+                'username': random_username(),
+                'password': 'univention',
+            },
+            'options': {
+                'container': position,
+                'objectType': 'users/user',
+            },
+        }]
+        return self.umc_command('udm/add', options, 'users/user').result[0]
+
+    def create_group(self, position: str):
+        options = [{
+            'object': {
+                'name': random_username(),
+                'description': random_username(),
+            },
+            'options': {
+                'container': position,
+                'objectType': 'groups/group',
+            },
+        }]
+        return self.umc_command('udm/add', options, 'groups/group').result[0]
+
+    def create_mail_domain(self, name: str, position: str):
+        options = [{
+            'object': {
+                'name': name,
+                '$policies$': {},
+            },
+            'options': {
+                'container': position,
+                'objectType': 'mail/domain',
+                'objectTemplate': None,
+            },
+        }]
+        return self.umc_command('udm/add', options, 'mail/domain').result[0]
+
+
+@pytest.fixture
+def admin_umc_client():
+    client = ClientHelper.get_test_connection()
+    return client
+
+
+@pytest.fixture
+def ouadmin_umc_client(ou):
+    client = ClientHelper()
+    client.authenticate(ou.admin_username, 'univention')
+    return client
+
+
+@pytest.fixture
+def admin_rest_client(ucr):
+    return RestClientHelper(
+        'https://%(hostname)s.%(domainname)s/univention/udm/' % ucr,
+        username=UCSTestDomainAdminCredentials().username,
+        password=UCSTestDomainAdminCredentials().bindpw,
+    )
+
+
+@pytest.fixture
+def ouadmin_rest_client(ucr, ou):
+    return RestClientHelper(
+        'https://%(hostname)s.%(domainname)s/univention/udm/' % ucr,
+        username=ou.admin_username,
+        password='univention',
+    )
+
+
+@pytest.fixture
+def ou(ldap_base):
+    return SimpleNamespace(
+        dn=f'ou=ou1,{ldap_base}',
+        admin_username='ou1admin',
+        admin_dn=f'uid=ou1admin,cn=users,{ldap_base}',
+        user_username='user1-ou1',
+        user_dn=f'uid=user1-ou1,cn=users,ou=ou1,{ldap_base}',
+        user_default_container=f'cn=users,ou=ou1,{ldap_base}',
+        group_default_container=f'cn=groups,ou=ou1,{ldap_base}',
+    )
