00c3b0e7d0469dd157eaf7b1f28a8c57565553e6
Florian Best <best@univention.de>
2025-06-18 11:13:29 +0200

    fixup! feat(udm): adjust size of guardian roles in web UI

--- management/univention-directory-manager-modules/modules/univention/admin/syntax.py
+++ management/univention-directory-manager-modules/modules/univention/admin/syntax.py
@@ -7059,6 +7059,17 @@ class VFSObjects(combobox):
     ]
 
 
+class GuardianRole(simple):
+    # FIXME: why don't we allow LDAP DNs in the last part of context? That would be case insensitive UTF-8,
+    # see https://ldapwiki.com/wiki/Wiki.jsp?page=Distinguished%20Name%20Case%20Sensitivity and https://ldapwiki.com/wiki/Wiki.jsp?page=Ou
+    regex = re.compile(r"^([a-z0-9-_]+:[a-z0-9-_]+:[a-z0-9-_]+)(&[a-z0-9-_]+:[a-z0-9-_]+:[a-z0-9-_=,]+)?$")
+    error_message = _(
+        "Guardian role strings must be lowercase ASCII alphanumeric with hyphens and underscores, "
+        "in the format 'app:namespace:role' or 'app:namespace:role&app:namespace:context'."
+        "The final part of context additionally allows equal and comma.",
+    )
+
+
 class _EscapedDict(dict):
 
     def __init__(self, _dict):
