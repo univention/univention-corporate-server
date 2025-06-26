007756ce4cd053e338240691176eda1e56a05842
Michael Sowka <michael.sowka.extern@univention.de>
2025-06-26 12:44:01 +0200

    tests(guardian): Add Guardian attribute filtering tests for UMC and UI
    This commit introduces a new test script to verify that the Guardian property filtering does not break essential UMC (Univention Management Console) commands. The tests cover various scenarios with different sets of properties, including minimal and complete sets.
    
    Issue univention/dev/ucs#2975

--- test/ucs-test/tests/42_authorization/conftest.py
+++ test/ucs-test/tests/42_authorization/conftest.py
@@ -297,8 +297,8 @@ def linux_client_manager_rest_client(ucr, ou):
     )
 
 
-@pytest.fixture
-def ou(ldap_base, udm):
+@pytest.fixture(scope='session')
+def ou(ldap_base):
     return SimpleNamespace(
         dn=f'ou=ou1,{ldap_base}',
         dn2=f'ou=ou2,{ldap_base}',
@@ -315,6 +315,7 @@ def ou(ldap_base, udm):
         user_default_container=f'cn=users,ou=ou1,{ldap_base}',
         group_default_container=f'cn=groups,ou=ou1,{ldap_base}',
         computer_default_container=f'cn=computers,ou=ou1,{ldap_base}',
+        group_name='group1-ou1',
     )
 
 
@@ -347,7 +348,7 @@ def setup_role(ou, udm, ucr, ldap_base):
             check_call([CONFIGURE_AUTH, '--store-local', 'create-roles', '--config', tmp.name])
         _, username = udm.create_user(
             guardianRoles=[role],
-            groups=[ucr['directory/manager/rest/authorized-groups/test-api-access']],
+            groups=[ucr.get('directory/manager/rest/authorized-groups/test-api-access')],
             policy_reference=[f'cn=organizational-unit-admins,cn=UMC,cn=policies,{ldap_base}'],
         )
         check_call(['systemctl', 'restart', 'univention-management-console-server.service', 'univention-directory-manager-rest.service'])
@@ -367,3 +368,78 @@ def setup_role(ou, udm, ucr, ldap_base):
     check_output([CONFIGURE_AUTH, '--store-local', 'create-permissions'])
     check_output([CONFIGURE_AUTH, '--store-local', 'create-default-roles'])
     check_call(['systemctl', 'restart', 'univention-management-console-server.service', 'univention-directory-manager-rest.service'])
+
+
+@pytest.fixture(scope='session')
+def guardian_role():
+    def _func(acl):
+        with NamedTemporaryFile('w') as tmp:
+            tmp.write(acl)
+            tmp.flush()
+            check_call([CONFIGURE_AUTH, '--store-local', 'create-roles', '--config', tmp.name])
+        check_call(['systemctl', 'restart', 'univention-management-console-server.service', 'univention-directory-manager-rest.service'])
+    yield _func
+    check_output([CONFIGURE_AUTH, '--store-local', 'prune'])
+    check_output([CONFIGURE_AUTH, '--store-local', 'create-permissions'])
+    check_output([CONFIGURE_AUTH, '--store-local', 'create-default-roles'])
+    check_call(['systemctl', 'restart', 'univention-management-console-server.service', 'univention-directory-manager-rest.service'])
+
+
+@pytest.fixture(scope='session')
+def build_acl(ldap_base, ou):
+    def build_acl(role_name, user_properties, group_properties, actions="*"):
+        if user_properties:
+            user_properties_lines = ''.join(f'    grant properties="{p}" permission="read"\n    grant properties="{p}" permission="write"\n' for p in user_properties)
+        else:
+            user_properties_lines = ''
+        if group_properties:
+            group_properties_lines = ''.join(f'    grant properties="{p}" permission="read"\n    grant properties="{p}" permission="write"\n' for p in group_properties)
+        else:
+            group_properties_lines = ''
+
+        acl = f'''
+# Basic permissions for the target object type
+access by role="{role_name}"
+  to objecttype="users/user" position.subtree="{ou.user_default_container}"
+    grant actions="{actions}"
+{user_properties_lines}
+
+access by role="{role_name}"
+  to objecttype="groups/group" position.subtree="{ou.group_default_container}"
+    grant actions="{actions}"
+{group_properties_lines}
+
+# Container permissions - needed to navigate and create objects
+access by role="{role_name}"
+  to objecttype="container/cn" position.subtree="{ou.dn}"
+    grant actions="search,read"
+    grant properties="*" permission="read"
+
+access by role="{role_name}"
+  to objecttype="container/ou" position.subtree="{ou.dn}"
+    grant actions="search,read"
+    grant properties="*" permission="read"
+
+# Policy permissions - needed for user/group management
+access by role="{role_name}"
+  to objecttype="policies/desktop"
+    grant actions="search,read"
+    grant properties="*" permission="read"
+
+access by role="{role_name}"
+  to objecttype="policies/pwhistory"
+    grant actions="search,read"
+    grant properties="*" permission="read"
+
+access by role="{role_name}"
+  to objecttype="policies/umc"
+    grant actions="search,read"
+    grant properties="*" permission="read"
+
+# LDAP Base permissions - needed for basic LDAP operations
+access by role="{role_name}"
+  to objecttype="container/dc" position.base="{ldap_base}"
+    grant actions="search,read"
+    grant properties="*" permission="read"'''
+        return acl
+    return build_acl
