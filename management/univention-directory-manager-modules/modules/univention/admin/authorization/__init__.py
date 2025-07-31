7e7a1bb77265715239bc1445f7ac55e3c3ddfb0d
Florian Best <best@univention.de>
2025-07-31 16:47:36 +0200

    fix(udm-authorization): do not cache actor roles

--- management/univention-directory-manager-modules/modules/univention/admin/authorization/__init__.py
+++ management/univention-directory-manager-modules/modules/univention/admin/authorization/__init__.py
@@ -106,6 +106,8 @@ class Authorization:
         actor_dn = lo.binddn
         # FIXME: memory leak, use weakref.ref() ?
         actor = get_user(cls.get_privileged_connection(), actor_dn)
+        return lambda: (actor, get_user_roles(actor))
+        # FIXME: don't cache actor roles as we don't know when to invalidate the cache. Roles of groups can be changed at any time.
         if cls._user_roles_cache.get(actor_dn) is None:
             cls._user_roles_cache[actor_dn] = (actor, get_user_roles(actor))
         return lambda: cls._user_roles_cache[actor_dn]
