85c3e8849fbb8cdd39682b32bf597caf4a7b4e8c
Jürn Brodersen <brodersen@univention.de>
2025-08-12 11:01:42 +0200

    test: cleanup utils-school.sh
    Bug: #58518

--- test/utils/utils-school.sh
+++ test/utils/utils-school.sh
@@ -6,17 +6,6 @@ set -x
 
 . utils.sh
 
-install_bb_api () {
-  # do not rename function: used as install_[ENV:TEST_API]_api in autotest-241-ucsschool-HTTP-API.cfg
-  ucr set bb/http_api/users/django_debug=yes bb/http_api/users/wsgi_server_capture_output=yes bb/http_api/users/wsgi_server_loglevel=debug bb/http_api/users/enable_session_authentication=yes tests/ucsschool/http-api/bb=yes
-  cp -v /usr/share/ucs-school-import/configs/ucs-school-testuser-http-import.json /var/lib/ucs-school-import/configs/user_import.json
-  python -c 'import json; fp = open("/var/lib/ucs-school-import/configs/user_import.json", "r+w"); config = json.load(fp); config["configuration_checks"] = ["defaults", "mapped_udm_properties"]; config["mapped_udm_properties"] = ["phone", "e-mail", "organisation"]; fp.seek(0); json.dump(config, fp, indent=4, sort_keys=True); fp.close()'
-  echo -e "deb [trusted=yes] http://192.168.0.10/build2/ ucs_4.4-0-min-brandenburg/all/\ndeb [trusted=yes] http://192.168.0.10/build2/ ucs_4.4-0-min-brandenburg/amd64/" > /etc/apt/sources.list.d/30_BB.list
-  univention-install -y ucs-school-http-api-bb
-  # shellcheck disable=SC2009
-  ps aux | grep api-bb
-}
-
 install_kelvin_api () {
   install_docker_app_from_branch ucsschool-kelvin-rest-api "$UCS_ENV_KELVIN_IMAGE" ucsschool/kelvin/processes=0 ucsschool/kelvin/log_level=DEBUG || return $?
 }
@@ -26,7 +15,7 @@ install_kelvin_in_version() {
   printf '%s' univention > /tmp/univention
   if [ -n "$KELVIN_VERSION" ]; then
     univention-app install ucsschool-kelvin-rest-api="$KELVIN_VERSION" --set ucsschool/kelvin/processes=0 ucsschool/kelvin/log_level=DEBUG --username Administrator --pwdfile /tmp/univention || rv=$?
-    return $rv
+    return "$rv"
   else
     install_kelvin_api
   fi
@@ -36,7 +25,7 @@ upgrade_kelvin () {
   printf '%s' univention > /tmp/univention
   univention-app upgrade ucsschool-kelvin-rest-api --noninteractive --username Administrator --pwdfile /tmp/univention || rv=$?
   univention-app info
-  return $rv
+  return "$rv"
 }
 
 install_ucsschool_id_connector () {
@@ -47,7 +36,7 @@ install_ucsschool_id_connector_in_version() {
   printf '%s' univention > /tmp/univention
   if [ -n "$ID_CONNECTOR_VERSION" ]; then
     univention-app install ucsschool-id-connector="$ID_CONNECTOR_VERSION" --set ucsschool-id-connector/log_level=DEBUG --username Administrator --pwdfile /tmp/univention || rv=$?
-    return $rv
+    return "$rv"
   else
     install_ucsschool_id_connector
   fi
@@ -62,32 +51,32 @@ upgrade_id_connector () {
   fi
   univention-app upgrade ucsschool-id-connector --noninteractive --username Administrator --pwdfile /tmp/univention || rv=$?
   univention-app info
-  return $rv
+  return "$rv"
 }
 
 add_ca_to_host () {
-	local host_fqdn="${1:?missing fqdn}"
-	curl -k "https://$host_fqdn/ucs-root-ca.crt" > /usr/local/share/ca-certificates/"$host_fqdn".crt
-	update-ca-certificates
+    local host_fqdn="${1:?missing fqdn}"
+    curl -k "https://$host_fqdn/ucs-root-ca.crt" > /usr/local/share/ca-certificates/"$host_fqdn".crt
+    update-ca-certificates
 }
 
 add_dns_entry () {
         local hostname="${1:?missing hostname}"
-	local domain="${2:?missing domain}"
-	local ip="${3:?missing ip}"
-	udm dns/forward_zone create \
-		--set zone="$domain" \
-		--set nameserver="$(hostname -f)." \
-		--position="cn=dns,$(ucr get ldap/base)" \
+    local domain="${2:?missing domain}"
+    local ip="${3:?missing ip}"
+    udm dns/forward_zone create \
+        --set zone="$domain" \
+        --set nameserver="$(hostname -f)." \
+        --position="cn=dns,$(ucr get ldap/base)" \
                 --ignore_exists || return 1
-	udm dns/host_record create \
-		--set a="$ip" \
-		--set name="$hostname" \
-		--position "zoneName=$domain,cn=dns,$(ucr get ldap/base)" || return 1
-	while ! nslookup "$hostname.$domain" | grep -q "$ip"; do
-		echo "Waiting for DNS..."
-		sleep 1
-	done
+    udm dns/host_record create \
+        --set a="$ip" \
+        --set name="$hostname" \
+        --position "zoneName=$domain,cn=dns,$(ucr get ldap/base)" || return 1
+    while ! nslookup "$hostname.$domain" | grep -q "$ip"; do
+        echo "Waiting for DNS..."
+        sleep 1
+    done
 }
 
 install_ucsschool_apis () {
@@ -243,57 +232,57 @@ install_mv_idm_gw_receiver_ext_attrs () {
 }
 
 add_pre_join_hook_to_install_from_test_appcenter () {
-	# do not use univention-appcenter-dev, if we have a pending appcenter errata update
-	# this new version is used on the dvd, but at this point we can't install errata-test
-	# packages and so installing univention-appcenter-dev might fail due to compatibility
-	# reasons (dvd: errata-test univention-appcenter vs univention-appcenter-dev from release
-	# errata packages)
-	cat <<-'EOF' >"/tmp/appcenter-test.sh"
+    # do not use univention-appcenter-dev, if we have a pending appcenter errata update
+    # this new version is used on the dvd, but at this point we can't install errata-test
+    # packages and so installing univention-appcenter-dev might fail due to compatibility
+    # reasons (dvd: errata-test univention-appcenter vs univention-appcenter-dev from release
+    # errata packages)
+    cat <<-'EOF' >"/tmp/appcenter-test.sh"
 #!/bin/sh
 ucr set repository/app_center/server='appcenter-test.software-univention.de' update/secure_apt='false' appcenter/index/verify='no'
 univention-app update
 exit 0
 EOF
-	# shellcheck source=/dev/null
-	. /usr/share/univention-lib/ldap.sh && ucs_registerLDAPExtension \
-		--binddn "cn=admin,$(ucr get ldap/base)" \
-		--bindpwdfile=/etc/ldap.secret \
-		--packagename dummy \
-		--packageversion "1.0" \
-		--data /tmp/appcenter-test.sh \
-		--data_type="join/pre-join"
+    # shellcheck source=/dev/null
+    . /usr/share/univention-lib/ldap.sh && ucs_registerLDAPExtension \
+        --binddn "cn=admin,$(ucr get ldap/base)" \
+        --bindpwdfile=/etc/ldap.secret \
+        --packagename dummy \
+        --packageversion "1.0" \
+        --data /tmp/appcenter-test.sh \
+        --data_type="join/pre-join"
 }
 
 add_pre_join_hook_to_install_from_test_repository () {
-	# activate test repository for school-replica join
-	case "${1:?public}" in
-	public) return 0 ;;
-	esac
+    # activate test repository for school-replica join
+    case "${1:?public}" in
+    public) return 0 ;;
+    esac
 
-	cat <<-'EOF' >"/tmp/repo-test.sh"
+    cat <<-'EOF' >"/tmp/repo-test.sh"
 #!/bin/sh
 ucr set repository/online/server='http://updates-test.knut.univention.de'
 exit 0
 EOF
-	# shellcheck source=/dev/null
-	. /usr/share/univention-lib/ldap.sh && ucs_registerLDAPExtension \
-		--binddn "cn=admin,$(ucr get ldap/base)" \
-		--bindpwdfile=/etc/ldap.secret \
-		--packagename setrepo \
-		--packageversion "1.0" \
-		--data /tmp/repo-test.sh \
-		--data_type="join/pre-join"
+    # shellcheck source=/dev/null
+    . /usr/share/univention-lib/ldap.sh && ucs_registerLDAPExtension \
+        --binddn "cn=admin,$(ucr get ldap/base)" \
+        --bindpwdfile=/etc/ldap.secret \
+        --packagename setrepo \
+        --packageversion "1.0" \
+        --data /tmp/repo-test.sh \
+        --data_type="join/pre-join"
 }
 
 create_virtual_schools () {
-	local number_of_schools=${1:?missing number of schools to create}
-	local formated_school_number
-	rm -f ./virtual_schools.txt
-	for ((i=1; i <= number_of_schools; i++)); do
-		printf -v formated_school_number "%0${#number_of_schools}d" "$i"
-		/usr/share/ucs-school-import/scripts/create_ou --verbose "SchoolVirtual$formated_school_number" "r300-sV$formated_school_number" --displayName "SchuleVirtual$formated_school_number"
-		printf "SchoolVirtual%0${#number_of_schools}d\n" "$i" >> ./virtual_schools.txt  # Later used for the import script
-	done
+    local number_of_schools=${1:?missing number of schools to create}
+    local formated_school_number
+    rm -f ./virtual_schools.txt
+    for ((i=1; i <= number_of_schools; i++)); do
+        printf -v formated_school_number "%0${#number_of_schools}d" "$i"
+        /usr/share/ucs-school-import/scripts/create_ou --verbose "SchoolVirtual$formated_school_number" "r300-sV$formated_school_number" --displayName "SchuleVirtual$formated_school_number"
+        printf "SchoolVirtual%0${#number_of_schools}d\n" "$i" >> ./virtual_schools.txt  # Later used for the import script
+    done
 }
 
 
@@ -315,17 +304,17 @@ create_virtual_schools () {
 #   -> 81750 classes
 #   -> 3750 workgroups
 create_users_in_template_job () {
-	# don't delete users
-	cat <<EOF > /var/lib/ucs-school-import/configs/user_import.json
+    # don't delete users
+    cat <<EOF > /var/lib/ucs-school-import/configs/user_import.json
 {
   "no_delete": true
 }
 EOF
-	# fix record_uid
-	sed -i 's/"record_uid": "<firstname>.<lastname>"/"record_uid": "<firstname>.<lastname>.<username>"/' \
-		/usr/share/ucs-school-import/configs/ucs-school-testuser-import.json
-	# add import hook
-	cat <<EOF > /usr/share/ucs-school-import/pyhooks/testimport.py
+    # fix record_uid
+    sed -i 's/"record_uid": "<firstname>.<lastname>"/"record_uid": "<firstname>.<lastname>.<username>"/' \
+        /usr/share/ucs-school-import/configs/ucs-school-testuser-import.json
+    # add import hook
+    cat <<EOF > /usr/share/ucs-school-import/pyhooks/testimport.py
 from ucsschool.importer.utils.user_pyhook import UserPyHook
 
 class MyHook(UserPyHook):
@@ -346,41 +335,41 @@ class MyHook(UserPyHook):
         user.ucsschool_roles += custome_roles
 
 EOF
-	# create schools
-	school_count=375
-	schools_big=()
-	schools_normal=()
-	for i in $(seq 1 "$school_count"); do
-		/usr/share/ucs-school-import/scripts/create_ou "--verbose" "school$i" "replica$i" >/tmp/import.log 2>&1 || return 1
-		if [ "$i" -le 50 ]; then
-			schools_big+=("school$i")
-		else
-			schools_normal+=("school$i")
-		fi
-	done
-	# 50 big schools with 1600 students and 53 classes (~30 members)
-	/usr/share/ucs-school-import/scripts/ucs-school-testuser-import \
-		--classes 2650 \
-		--students 80000 \
-		--teachers 15000 \
-		--staff 1500 \
-		"${schools_big[@]}" >/tmp/import.log 2>&1 || return 1
-	# 325 normal schools with 640 students and 30 classes (~20 members)
-	/usr/share/ucs-school-import/scripts/ucs-school-testuser-import \
-		--classes 10000 \
-		--students 208000 \
-		--teachers 15000 \
-		--staff 1500 \
-		"${schools_normal[@]}" >/tmp/import.log 2>&1 || return 1
-	rm -f /tmp/import.log
-	# clean up
-	rm -f /usr/share/ucs-school-import/pyhooks/testimport.py
-	rm -f /var/lib/ucs-school-import/configs/user_import.json
-	# add some more
-	# * workgroups, 10 work groups per school with 30 members each
-	# * 300 classes to each of the 50 big schools (5 members)
-	# * 150 class to each of the 325 normal schools (0 members)
-	python3 - <<"EOF" || return 1
+    # create schools
+    school_count=375
+    schools_big=()
+    schools_normal=()
+    for i in $(seq 1 "$school_count"); do
+        /usr/share/ucs-school-import/scripts/create_ou "--verbose" "school$i" "replica$i" >/tmp/import.log 2>&1 || return 1
+        if [ "$i" -le 50 ]; then
+            schools_big+=("school$i")
+        else
+            schools_normal+=("school$i")
+        fi
+    done
+    # 50 big schools with 1600 students and 53 classes (~30 members)
+    /usr/share/ucs-school-import/scripts/ucs-school-testuser-import \
+        --classes 2650 \
+        --students 80000 \
+        --teachers 15000 \
+        --staff 1500 \
+        "${schools_big[@]}" >/tmp/import.log 2>&1 || return 1
+    # 325 normal schools with 640 students and 30 classes (~20 members)
+    /usr/share/ucs-school-import/scripts/ucs-school-testuser-import \
+        --classes 10000 \
+        --students 208000 \
+        --teachers 15000 \
+        --staff 1500 \
+        "${schools_normal[@]}" >/tmp/import.log 2>&1 || return 1
+    rm -f /tmp/import.log
+    # clean up
+    rm -f /usr/share/ucs-school-import/pyhooks/testimport.py
+    rm -f /var/lib/ucs-school-import/configs/user_import.json
+    # add some more
+    # * workgroups, 10 work groups per school with 30 members each
+    # * 300 classes to each of the 50 big schools (5 members)
+    # * 150 class to each of the 325 normal schools (0 members)
+    python3 - <<"EOF" || return 1
 from ucsschool.lib.models import School, User
 from ucsschool.lib.models.group import SchoolClass, WorkGroup
 from univention.admin.uldap import getAdminConnection
@@ -429,10 +418,10 @@ EOF
 
 # get first 50 schools as python diskcache
 create_and_copy_test_data_cache () {
-	local root_password="${1:?missing root password}"
-	univention-install -y python3-pip sshpass
-	pip3 install diskcache
-	python3 - <<"EOF" || return 1
+    local root_password="${1:?missing root password}"
+    univention-install -y python3-pip sshpass
+    pip3 install diskcache
+    python3 - <<"EOF" || return 1
 from ucsschool.lib.models import School, User, Group
 from univention.admin.uldap import getAdminConnection
 from diskcache import Index
@@ -476,10 +465,10 @@ for i in range(1, 51):
 db.cache.close()
 EOF
 
-	shift
-	for ip in "$@"; do
-		sshpass -p "$root_password" scp -r  -o StrictHostKeyChecking=no -o UpdateHostKeys=no /var/lib/test-data root@"$ip":/var/lib/ || return 1
-	done
+    shift
+    for ip in "$@"; do
+        sshpass -p "$root_password" scp -r  -o StrictHostKeyChecking=no -o UpdateHostKeys=no /var/lib/test-data root@"$ip":/var/lib/ || return 1
+    done
 }
 
 set_udm_properties_for_kelvin_api_tests () {
