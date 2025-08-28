415731cea1c6669432ec8c21a2c713dbb07688bf
Florian Best <best@univention.de>
2025-08-28 23:26:00 +0200

    fix(dojo): upgrade dojo libs
    update dojo from 1.16.3 to 1.17.3
    update dgrid from 1.1.0 to 1.3.3
    update dstore from 1.1.1 to 1.2.1
    upgrade dompurify from 1.0.10 to 2.5.8
    upgrade xstyle from 0.3.2 to 0.3.3
    
    GHSA-mjjq-c88q-qhr6
    CVE-2021-23450
    CVE-2024-48910
    
    Bug #58843

---
+++ management/univention-dojo/dompurify.js
@@ -0,0 +1,18 @@
+/**
+SPDX-FileCopyrightText: 2019-2025 Univention GmbH
+SPDX-License-Identifier: AGPL-3.0-only
+**/
+
+var profile = {
+	resourceTags: {
+		miniExclude: function(filename, mid) {
+			if(mid === "dompurify/purify") { return true; }
+			return false;
+		},
+
+		amd: function(filename, mid) {
+			if(mid === "dompurify/purify") { return true; } // marks UMD as AMD
+			return /\.js$/.test(filename);
+		}
+	}
+};
