4e32bfdaf1a4bd8e2537ff137aec7b885567a73b
Ian Alexander List <ian.list.extern@univention.de>
2025-09-15 15:49:53 +0200

    test(umc): test_umc_tracebacks integration test now also checks for logfile contents

--- test/ucs-test/tests/60_umc/17_traceback_handling.py
+++ test/ucs-test/tests/60_umc/17_traceback_handling.py
@@ -24,6 +24,42 @@ from univention.management.console.modules.ucstest import joinscript, unjoinscri
     ("ucstest/traceback_as_thread_result", re.compile("(univention.management.console.modules.ucstest.)?ThreadedError", re.M)),
 ])
 def test_umc_tracebacks(Client, path, expected_trace):
+    # this regex matches that the initial line of the traceback
+    # log message includes the logfmter on the line of the initial logging
+    # message
+    error_line_regex = re.compile(
+        # Structured Date + Log Level
+        r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}\+\d{2}:\d{2}\ {4}ERROR '
+        # Log-Message
+        r'\[.*] Internal server error during ".*"\. \| '
+        # Logfmt-Extras
+        r'(pid=\d*\s|logname=[A-Z]+\s|func=.*\s|umcmodule=.*\s|requester_dn=".*"\s|requester_ip=\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s){4}',
+    )
+    # The traceback parts insure that the content of the traceback is correct
+    traceback_parts = [
+        [
+            "Request: ucstest/non_threaded_traceback",
+            "Traceback (most recent call last):", "raise exc.with_traceback(etraceback)",
+            "function.__func__(self, request, *args, **kwargs)",
+            "result = _multi_response(self, request)",
+            "return function(self, request)",
+            "return list(function(self, iterator, *nones))",
+            "yield function(self, *args)",
+            "raise NonThreadedError()",
+        ],
+        [
+            "Request: ucstest/threaded_traceback",
+            "Traceback (most recent call last):",
+            "result = self._function(*args, **kwargs)  # type: Union[BaseException, _T]",
+            "raise ThreadedError()",
+        ],
+        [
+            "Request: ucstest/traceback_as_thread_result",
+            "Traceback (most recent call last):",
+        ],
+    ]
+    logfile = open('/var/log/univention/management-console-server.log')
+    logfile.read()  # ensure we're at the end of the log-file before messages are written
     joinscript()
     try:
         umc_client = Client.get_test_connection()
@@ -35,6 +71,15 @@ def test_umc_tracebacks(Client, path, expected_trace):
         assert expected_trace.search(traceback), (traceback, expected_trace)
     finally:
         unjoinscript()
+        log_text = logfile.read()
+        logfile.close()
+        assert error_line_regex.match(log_text), f"Log Text doesn't have a line that matches the error regex: {error_line_regex}"
+        assert expected_trace.search(log_text), (log_text, expected_trace)
+        assert len([True for traceback_lines in traceback_parts if all(line in log_text for line in traceback_lines)]) == 1
+        log_lines_with_trace = [line for line in log_text.replace('\\n', '').split('\n') if line]
+        log_lines_without_trace = [line for line in log_lines_with_trace if 'ERROR' in line or 'PROCESS' in line]
+        assert len(log_lines_without_trace) == 3
+        assert len(log_lines_without_trace) < len(log_lines_with_trace)
 
 
 @pytest.mark.parametrize('path,expected_error', [
