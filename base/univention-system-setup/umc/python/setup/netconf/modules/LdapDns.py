8b3d5a2e5f5179e91475c1a3c04ec9b950e350c9
Iván Barbecho Delgado <ivan.delgado@univention.de>
2025-09-16 11:36:33 +0200

    chore(univention-system-setup): update system-setup, umc deprecate --bindpwd in favor of --bindpwdfile
    Bug #20610
    Issue univention/dev/ucs#3136

--- base/univention-system-setup/umc/python/setup/netconf/modules/LdapDns.py
+++ base/univention-system-setup/umc/python/setup/netconf/modules/LdapDns.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2024-2025 Univention GmbH
 # SPDX-License-Identifier: AGPL-3.0-only
 
+import tempfile
 
 from univention import ipcalc
 from univention.management.console.modules.setup.netconf.common import AddressMap
@@ -19,31 +20,37 @@ class PhaseLdapDns(AddressMap, Ldap, Executable):
 
     def _create_reverse_dns_ipv4(self) -> None:
         for ipv4 in set(self.changeset.new_ipv4s) - set(self.changeset.old_ipv4s):
-            self.call([self.executable, "--binddn", self.binddn, "--bindpwd", self.bindpwd, "--ignore-exists", "--reverse", ipcalc.calculate_ipv4_reverse(ipv4), "add", "zone", *self._soa()])
-            self.call([
-                self.executable,
-                "--binddn", self.binddn,
-                "--bindpwd", self.bindpwd,
-                "--ignore-exists",
-                "--reverse", ipcalc.calculate_ipv4_reverse(ipv4),
-                "add", "ptr",
-                ipcalc.calculate_ipv4_pointer(ipv4),
-                "%(hostname)s.%(domainname)s." % self.changeset.ucr,
-            ])
+            with tempfile.NamedTemporaryFile(mode="w") as bindpwd_file:
+                bindpwd_file.write(self.bindpwd)
+                bindpwd_file.flush()
+                self.call([self.executable, "--binddn", self.binddn, "--bindpwdfile", bindpwd_file.name, "--ignore-exists", "--reverse", ipcalc.calculate_ipv4_reverse(ipv4), "add", "zone", *self._soa()])
+                self.call([
+                    self.executable,
+                    "--binddn", self.binddn,
+                    "--bindpwdfile", bindpwd_file.name,
+                    "--ignore-exists",
+                    "--reverse", ipcalc.calculate_ipv4_reverse(ipv4),
+                    "add", "ptr",
+                    ipcalc.calculate_ipv4_pointer(ipv4),
+                    "%(hostname)s.%(domainname)s." % self.changeset.ucr,
+                ])
 
     def _create_reverse_dns_ipv6(self) -> None:
         for ipv6 in set(self.changeset.new_ipv6s) - set(self.changeset.old_ipv6s):
-            self.call([self.executable, "--binddn", self.binddn, "--bindpwd", self.bindpwd, "--ignore-exists", "--reverse", ipcalc.calculate_ipv6_reverse(ipv6), "add", "zone", *self._soa()])
-            self.call([
-                self.executable,
-                "--binddn", self.binddn,
-                "--bindpwd", self.bindpwd,
-                "--ignore-exists",
-                "--reverse", ipcalc.calculate_ipv6_reverse(ipv6),
-                "add", "ptr",
-                ipcalc.calculate_ipv6_pointer(ipv6),
-                "%(hostname)s.%(domainname)s." % self.changeset.ucr,
-            ])
+            with tempfile.NamedTemporaryFile(mode="w") as bindpwd_file:
+                bindpwd_file.write(self.bindpwd)
+                bindpwd_file.flush()
+                self.call([self.executable, "--binddn", self.binddn, "--bindpwdfile", bindpwd_file.name, "--ignore-exists", "--reverse", ipcalc.calculate_ipv6_reverse(ipv6), "add", "zone", *self._soa()])
+                self.call([
+                    self.executable,
+                    "--binddn", self.binddn,
+                    "--bindpwdfile", bindpwd_file.name,
+                    "--ignore-exists",
+                    "--reverse", ipcalc.calculate_ipv6_reverse(ipv6),
+                    "add", "ptr",
+                    ipcalc.calculate_ipv6_pointer(ipv6),
+                    "%(hostname)s.%(domainname)s." % self.changeset.ucr,
+                ])
 
     def _soa(self) -> list[str]:
         return [
