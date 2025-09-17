fab3690ce3f2f3607cb76b751df8e0dfca267dd8
Florian Best <best@univention.de>
2025-09-17 15:29:59 +0200

    feat(config-registry): add univention_config_is_true
    Bug #58644

--- base/univention-config-registry/include/univention/config.h
+++ base/univention-config-registry/include/univention/config.h
@@ -10,20 +10,21 @@
 #define __UNIVENTION_CONFIG_H__
 
 #include <stdio.h>
+#include <stdbool.h>
 
 /**
  * Retrieve value of config registry entry associated with key.
- * @return an allocated buffer containingt the value or NULL on errors or if not found.
+ * @return an allocated buffer containing the value or NULL on errors if not found.
  */
 char *univention_config_get_string(const char *key);
 /**
  * Retrieve integer value of config registry entry associated with key.
- * @return an integer value of -1 on errors of if not found.
+ * @return an integer value of -1 on errors if not found.
  */
 int univention_config_get_int(const char *key);
 /**
  * Retrieve integer value of config registry entry associated with key.
- * @return an integer value of -1 on errors of if not found.
+ * @return an integer value of -1 on errors if not found.
  */
 long univention_config_get_long(const char *key);
 /**
@@ -31,5 +32,10 @@ long univention_config_get_long(const char *key);
  * @return 0 on success, -1 on internal errors.
  */
 int univention_config_set_string(const char *key, const char *value);
+/**
+ * Return whether the strings value of key is considered as true
+ * @return true or false if value is set otherwise default_value
+ */
+bool univention_config_is_true(const char *key, bool default_value);
 
 #endif
