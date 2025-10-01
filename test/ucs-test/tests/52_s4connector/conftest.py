be7473cddb0502895fd8df3672e7a954b4a6865b
Andreas Kosubek <andreas.kosubek.extern@univention.de>
2025-10-01 18:03:37 +0200

    test(s4-connector): Add tests for lockout time sync
    Bug #58680
    Bug #46353
    Issue: univention/dev/ucs#3120

---
+++ test/ucs-test/tests/52_s4connector/conftest.py
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
+
+import random
+
+import pytest
+
+from univention.testing.active_directory import ActiveDirectorySettings, User
+from univention.testing.udm import UCSTestUDM
+
+from s4connector import connector_setup
+
+
+@pytest.fixture
+def active_directory_settings():
+    return ActiveDirectorySettings(is_local_connect=True)
+
+
+@pytest.fixture
+def s4_connector():
+    with connector_setup('sync') as connector:
+        yield connector
+
+
+@pytest.fixture
+def ucs_user(udm: UCSTestUDM):
+    dn, username = udm.create_user()
+
+    yield {'dn': dn, 'username': username}
+
+    udm.remove_user(username)
+
+
+@pytest.fixture
+def samba_user(active_directory_settings: ActiveDirectorySettings):
+    user_api = User(active_directory_settings)
+    samba_user = user_api.create(username=f'TestSyncLockedOut{random.randrange(1, 999)}', password='Univention.99')
+
+    yield samba_user
+
+    user_api.delete(username=samba_user.name)
