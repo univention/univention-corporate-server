0d7638d649efeb4e03d2d12d29ba5040e0a078d1
Arvid Requate <requate@univention.de>
2025-10-24 19:51:23 +0200

    test(adconnector): AD-Connector cache 'DN Mapping CON' update on uid change
    * Improve the check test_user_sync_from_ad_to_udm_with_rename:
      The test_user_sync_from_ad_to_udm_with_ad_style_cn_and_full_rename
      always used CN == sAMAccountName, but AD objects usually have CN != sAMAccountName.
    
    * Add test_user_sync_from_ad_to_udm_with_ad_style_cn_and_two_stage_rename:
      The MS GUI app ADUC seems to lead admins to trigger multiple changes,
      e.g. changing the sAMAccountName first and then changing again
      the full name (e.g. lastname and sAMAccountName if lastname is part
      of the naming scheme for sAMAccountName). In one customer environment
      that lead to a stale entry in the sqlite cache table `DN to CON', and
      that stale entry then caused the second "modify"(modrdn) to get
      interpreted as "add" in sync_to_ucs, because the "olddn" was not
      correct.
    
    Bug #58737
    Issue univention/dev/ucs#3212

--- test/ucs-test/tests/55_adconnector/500test_user_sync.py
+++ test/ucs-test/tests/55_adconnector/500test_user_sync.py
@@ -22,7 +22,7 @@ AD = adconnector.ADConnection()
 import univention.testing.connector_common as tcommon  # noqa: E402
 from univention.testing.connector_common import delete_con_user  # noqa: E402
 from univention.testing.connector_common import (  # noqa: E402
-    NormalUser, SpecialUser, Utf8User, create_con_user, create_udm_user, delete_udm_user,
+    NormalUser, SpecialUser, Utf8User, create_con_user, create_udm_user, delete_udm_user, map_udm_user_to_con,
 )
 from univention.testing.udm import UCSTestUDM  # noqa: E402
 
@@ -123,6 +123,74 @@ def test_user_sync_from_ad_to_udm_with_rename(user_class, sync_mode):
             [("uid", udm_user.to_unicode(udm_user.rename).get("username"), ldap.AVA_STRING)],
             [("CN", "users", ldap.AVA_STRING)]] + ldap.dn.str2dn(tcommon.configRegistry['ldap/base']))
         tcommon.verify_udm_object("users/user", udm_user_dn, udm_user.rename)
+
+        delete_con_user(AD, ad_user_dn, udm_user_dn, adconnector.wait_for_sync)
+
+
+@pytest.mark.parametrize("user_class", [NormalUser])
+@pytest.mark.parametrize("sync_mode", ["read"])
+@pytest.mark.skipif(not connector_running_on_this_host(), reason="Univention AD Connector not configured.")
+def test_user_sync_from_ad_to_udm_with_ad_style_cn_and_full_rename(user_class, sync_mode):
+    with connector_setup(sync_mode):
+        udm_user = user_class()
+        ## we need to do some acrobatic here to get CN different from sAMAccountName, but that's how it usually is in AD
+        udm_user.basic['gecos'] = udm_user.basic["firstname"] + b' ' + udm_user.basic["lastname"]  # this way we can set the AD-`CN` in the test framework
+        basic_con_user = map_udm_user_to_con(udm_user.basic)
+        basic_con_user['cn'] = udm_user.basic['gecos']  # set this to satisfy the verify_udm_object in create_con_user
+
+        (_basic_ad_user, ad_user_dn, udm_user_dn) = create_con_user(AD, udm_user, adconnector.wait_for_sync, basic_con_user)
+
+        udm_user2 = user_class()  # just gets a new random set of names
+        unicode_udm_user_rename = udm_user.to_unicode(udm_user2.basic)
+        new_cn = f'{unicode_udm_user_rename["firstname"]} {unicode_udm_user_rename["lastname"]}'
+        print("\nRename AD username and DN {!r} to {!r}\n".format(ad_user_dn, new_cn))
+        ad_user_dn = AD.rename_or_move_user_or_group(ad_user_dn, name=new_cn)  # this does a modrdn on the CN in AD
+        udm_user2.basic['gecos'] = udm_user2.basic["firstname"] + b' ' + udm_user2.basic["lastname"]  # this way we can set the AD-`CN` in the test framework
+        AD.set_attributes(ad_user_dn, **tcommon.map_udm_user_to_con(udm_user2.basic))  # this changes the sAMAccountName too
+        adconnector.wait_for_sync()
+
+        tcommon.verify_udm_object("users/user", udm_user_dn, None)
+        udm_user_dn = ldap.dn.dn2str([
+            [("uid", udm_user.to_unicode(udm_user2.basic).get("username"), ldap.AVA_STRING)],
+            [("CN", "users", ldap.AVA_STRING)]] + ldap.dn.str2dn(tcommon.configRegistry['ldap/base']))
+        tcommon.verify_udm_object("users/user", udm_user_dn, udm_user2.basic)
+
+        delete_con_user(AD, ad_user_dn, udm_user_dn, adconnector.wait_for_sync)
+
+
+@pytest.mark.parametrize("user_class", [NormalUser])
+@pytest.mark.parametrize("sync_mode", ["read"])
+@pytest.mark.skipif(not connector_running_on_this_host(), reason="Univention AD Connector not configured.")
+def test_user_sync_from_ad_to_udm_with_ad_style_cn_and_two_stage_rename(user_class, sync_mode):
+    with connector_setup(sync_mode):
+        udm_user = user_class()
+        ## we need to do some acrobatic here to get CN different from sAMAccountName, but that's how it usually is in AD
+        udm_user.basic['gecos'] = udm_user.basic["firstname"] + b' ' + udm_user.basic["lastname"]  # this way we can set the AD-`CN` in the test framework
+        basic_con_user = map_udm_user_to_con(udm_user.basic)
+        basic_con_user['cn'] = udm_user.basic['gecos']  # set this to satisfy the verify_udm_object in create_con_user
+
+        (_basic_ad_user, ad_user_dn, udm_user_dn) = create_con_user(AD, udm_user, adconnector.wait_for_sync, basic_con_user)
+
+        udm_user2 = user_class()  # just gets a new random set of names
+        unicode_udm_user_rename = udm_user.to_unicode(udm_user2.basic)
+        print("\nRename AD username {!r} to {!r}\n".format(udm_user.basic.get('username'), udm_user2.basic.get('username')))
+        AD.set_attributes(ad_user_dn, **tcommon.map_udm_user_to_con(udm_user2.basic))  # this should only change the sAMAccountName
+        adconnector.wait_for_sync()
+
+        udm_user3 = user_class()  # yet another random set of names
+        unicode_udm_user_rename = udm_user.to_unicode(udm_user3.basic)
+        new_cn = f'{unicode_udm_user_rename["firstname"]} {unicode_udm_user_rename["lastname"]}'
+        print("\nAnd then again rename AD username to {!r} and DN {!r} to {!r}\n".format(udm_user3.basic.get('username'), ad_user_dn, new_cn))
+        ad_user_dn = AD.rename_or_move_user_or_group(ad_user_dn, name=new_cn)  # this does a modrdn on the CN in AD
+        udm_user3.basic['gecos'] = udm_user3.basic["firstname"] + b' ' + udm_user3.basic["lastname"]  # this way we can set the AD-`CN` in the test framework
+        AD.set_attributes(ad_user_dn, **tcommon.map_udm_user_to_con(udm_user3.basic))  # this changes the sAMAccountName again
+        adconnector.wait_for_sync()
+
+        tcommon.verify_udm_object("users/user", udm_user_dn, None)
+        udm_user_dn = ldap.dn.dn2str([
+            [("uid", udm_user.to_unicode(udm_user3.basic).get("username"), ldap.AVA_STRING)],
+            [("CN", "users", ldap.AVA_STRING)]] + ldap.dn.str2dn(tcommon.configRegistry['ldap/base']))
+        tcommon.verify_udm_object("users/user", udm_user_dn, udm_user3.basic)
 
         delete_con_user(AD, ad_user_dn, udm_user_dn, adconnector.wait_for_sync)
 
