595d801641bf4faca49fb487c237fcd31e69440f
Johannes Königer <koeniger@univention.de>
2025-10-31 09:00:57 +0100

    test: Add utility scripts for school template generation
    Issue univention/dev/education/ucsschool#1434

---
+++ test/utils/ram/add_legal_guardians_to_students.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2024-2025 Univention GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
+
+from itertools import cycle
+
+from ucsschool.lib.models.school import School
+from ucsschool.lib.models.user import LegalGuardian, Student
+from univention.admin.uldap import getMachineConnection
+
+
+# The ratio between students and legal guardians should be chosen such that limits are
+# not reached (10 legal wards per guardian or 4 legal guardians per ward).
+# Otherwise, this may fail during modification of a student.
+# Add 1 legal guardian to each student
+
+lo, _ = getMachineConnection()
+
+for school in School.get_all(lo):
+    print(f"Add one legal guardian to each student in {school.name}")
+    legal_guardians = LegalGuardian.get_all(lo, school.name)
+    if not legal_guardians:
+        continue
+
+    cycled_legal_guardians = cycle(legal_guardians)
+
+    for student in Student.get_all(lo, school.name):
+        legal_guardian_dn = next(cycled_legal_guardians).dn
+        student.legal_guardians = list({legal_guardian_dn}.union(student.legal_guardians or []))
+        student.modify(lo)
---
+++ test/utils/ram/add_work_groups.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2024-2025 Univention GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
+
+import argparse
+from itertools import cycle
+
+from ucsschool.lib.models.group import WorkGroup
+from ucsschool.lib.models.user import Student
+from univention.admin.uldap import getMachineConnection
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('school', nargs='+', type=str)
+parser.add_argument('--workgroup-count', default=10, type=int)
+parser.add_argument('--student-count', default=20, type=int)
+args = parser.parse_args()
+
+lo, _ = getMachineConnection()
+
+for school_name in args.school:
+    print(f'Creating {args.workgroup_count} work groups in {school_name} with {args.student_count} students each.')
+    students = cycle([student.dn for student in Student.get_all(lo, school_name)])
+    for wg_id in range(args.workgroup_count):
+        wg = WorkGroup(f'{school_name}-wg{wg_id}', school_name)
+        wg.users = [next(students) for _ in range(args.student_count)]
+        wg.create(lo)
