8deac45eb609f74fed6f875b06ae3046cb12421c
Dirk Wiesenthal <wiesenthal@univention.de>
2025-11-17 01:08:48 +0100

    test(ucs-test): Add tests for new UDM REST API endpoint
    Bug #58792

---
+++ test/ucs-test/tests/73_udm-rest/21_test_udm_rest_api_unmap_ldap_attributes.py
@@ -0,0 +1,68 @@
+#!/usr/share/ucs-test/runner pytest-3 -s -l -vv
+## desc: Test the /directory/unmap-ldap-attributes endpoint
+## tags: [udm,apptest]
+## roles: [domaincontroller_master]
+## exposure: safe
+## packages:
+##   - univention-directory-manager-rest
+
+import base64
+
+import requests
+
+
+def test_invented_object(account):
+    dn = account.binddn
+    # uid=Administrator... surely nonsense, but shows that
+    # (a) there is no lookup in case the dn actually exists
+    # (b) dn does not need to make sense for attributes
+
+    attrs = {
+        'cn': [b'fantasy'],
+        'gidNumber': [b'5011'],
+        'sambaGroupType': [b'2'],
+        'univentionGroupType': [b'-2147483646'],
+        'univentionObjectIdentifier': [b'6ab9be03-4c95-4989-829f-621e370da07a'],
+        'sambaSID': [b'S-1-5-21-3939622176-1304685295-672766427-11023'],
+        'objectClass': [b'sambaGroupMapping', b'univentionGroup', b'top', b'posixGroup', b'univentionObject'],
+        'univentionObjectType': [b'groups/group'],
+    }
+
+    _attrs = {k: [base64.b64encode(_v).decode('ASCII') for _v in v] for k, v in attrs.items()}  # the request uses b64 encoded values as we cannot send bytes (e.g. jpegPhoto) via JSON
+    auth = (account.username, account.bindpw)
+    response = requests.post('http://localhost/univention/udm/directory/unmap-ldap-attributes', json={'dn': dn, 'attributes': _attrs}, headers={'Accept': 'application/json'}, auth=auth)
+    assert response.json()['dn'] == dn
+    assert response.json()['objectType'] == 'groups/group'
+    assert response.json()['properties']['name'] == 'fantasy'
+    assert response.json()['properties']['gidNumber'] == 5011
+
+
+def test_broken_object(account):
+    dn = account.binddn
+    attrs = {
+        'cn': [b'fantasy'],
+        'gidNumber': [b'5011'],
+        'sambaGroupType': [b'2'],
+        'univentionGroupType': [b'-2147483646'],
+        'univentionObjectIdentifier': [b'6ab9be03-4c95-4989-829f-621e370da07a'],
+        'sambaSID': [b'S-1-5-21-3939622176-1304685295-672766427-11023'],
+        'objectClass': [b'sambaGroupMapping', b'top', b'posixGroup', b'univentionObject'],  # note that we are missing b'univentionGroup'
+        'univentionObjectType': [b'groups/group'],  # but we clearly want groups/group -> will UDM REST recognize?
+    }
+
+    _attrs = {k: [base64.b64encode(_v).decode('ASCII') for _v in v] for k, v in attrs.items()}  # the request uses b64 encoded values as we cannot send bytes (e.g. jpegPhoto) via JSON
+    auth = (account.username, account.bindpw)
+    response = requests.post('http://localhost/univention/udm/directory/unmap-ldap-attributes', json={'dn': dn, 'attributes': _attrs}, headers={'Accept': 'application/json'}, auth=auth)
+    assert response.status_code == 422
+
+
+def test_unknown_object(account):
+    dn = account.binddn
+    attrs = {
+        'cn': [b'fantasy'],  # this is a bit little, will UDM REST give the correct status_code?
+    }
+
+    _attrs = {k: [base64.b64encode(_v).decode('ASCII') for _v in v] for k, v in attrs.items()}  # the request uses b64 encoded values as we cannot send bytes (e.g. jpegPhoto) via JSON
+    auth = (account.username, account.bindpw)
+    response = requests.post('http://localhost/univention/udm/directory/unmap-ldap-attributes', json={'dn': dn, 'attributes': _attrs}, headers={'Accept': 'application/json'}, auth=auth)
+    assert response.status_code == 422
