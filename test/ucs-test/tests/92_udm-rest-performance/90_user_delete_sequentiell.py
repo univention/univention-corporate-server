9810c72a4c0ba2038d7f51e6e21aaf9888ab4b49
Christian Emmert <christian.emmert.extern@univention.de>
2025-11-24 14:40:13 +0800

    fixup! Adding Performance Tests with Guardian-Mock

--- test/ucs-test/tests/92_udm-rest-performance/90_user_delete_sequentiell.py
+++ test/ucs-test/tests/92_udm-rest-performance/90_user_delete_sequentiell.py
@@ -9,60 +9,38 @@
 ##   TIMEOUT: "300"
 
 
-from locust import HttpUser, events, task
-from locust.runners import MasterRunner, WorkerRunner
+from locust import HttpUser, between, task
 from locustfile import check_results  # noqa: F401
 from locustfile import jmeter_summary  # noqa: F401
-from rest_utils import UDMRestClient, get_config, setup_logging
+from rest_utils import UDMRestClient, UDMTestDataGenerator, get_ldap_containers, setup_logging
 
 
 log = setup_logging()
 
 
-def on_get_user(environment, msg, **kwargs):
-    # Fired when the master receives a message of type 'get_users'
-    user = environment.users.pop()
-    environment.runner.send_message('acknowledge_user', user, client_id=msg.node_id)
-
-
-def on_acknowledge_user(environment, msg, **kwargs):
-    # Fired when the worker receives a message of type 'acknowledge_user'
-    environment.user_dn = msg.data
-
-
-@events.init.add_listener
-def on_locust_init(environment, **_kwargs):
-    if isinstance(environment.runner, MasterRunner):
-        environment.runner.register_message('get_user', on_get_user)
-    if isinstance(environment.runner, WorkerRunner):
-        environment.runner.register_message('acknowledge_user', on_acknowledge_user)
-
-
-@events.test_start.add_listener
-def on_test_start(environment, **_kwargs):
-    start = 10000
-    end = 20000
-    name = 'testuser'
-    if isinstance(environment.runner, MasterRunner):
-        users = [f'uid={name}{x},cn=users,{get_config("LDAP_BASE")}' for x in range(start, end)]
-        environment.users = users
-    else:
-        environment.user_dn = None
-
-
 class UserDeleteSequentiell(HttpUser):
+    wait_time = between(0, 0)
 
     def on_start(self):
         log.info('User deletion test started')
         self.udm_client = UDMRestClient(self.client)
+        self.data_generator = UDMTestDataGenerator(prefix='userdelete')
+        self.containers = get_ldap_containers()
 
     @task
-    def task(self):
-        user_dn = None
-        self.environment.runner.send_message('get_user')
-        if self.environment.user_dn:
-            user_dn = self.environment.user_dn
-            self.environment.user_dn = None
-        if user_dn:
-            if not self.udm_client.delete_object('users/user', user_dn):
-                log.warning(f'Failed to delete user (may not exist): {user_dn}')
+    def create_and_delete_user(self):
+        user_data = self.data_generator.next_user_data(password='Univention.123')
+        success, user_dn = self.udm_client.create_user(
+            username=user_data['username'],
+            lastname=user_data['lastname'],
+            password=user_data['password'],
+            description=user_data['description'],
+        )
+        if not success:
+            return
+
+        if not self.udm_client.delete_object('users/user', user_dn):
+            log.warning(f'Failed to delete user: {user_dn}')
+        else:
+            if ('users/user', user_dn) in self.udm_client.created_objects:
+                self.udm_client.created_objects.remove(('users/user', user_dn))
