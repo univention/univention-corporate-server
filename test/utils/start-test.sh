3fee04b2826ccdd2598b60fa72aa3af8224c42da
Marius Meschter <marius.meschter@univention.de>
2026-03-05 10:56:18 +0100

    chore: use new AMIs and kvm or 5.2-5
    Issue: univention/dev/ucs#3408
    Bug: #59092
    Issue: univention/dev/ucs#3409
    Bug: #59093

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -8,10 +8,10 @@
 
 # defaults for release
 release='5.2-5'  #
-export CURRENT_AMI="${CURRENT_AMI:=ami-0fa240bd71b8fd012}"  # AMI: Univention Corporate Server (UCS) 5.2 (official image) rev. 6
+export CURRENT_AMI="${CURRENT_AMI:=ami-008a55f0627c343c4}"  # AMI: Univention Corporate Server (UCS) 5.2 (official image) rev. 6
 old_release='5.0-10'  #
 export OLD_AMI="${OLD_AMI:=ami-00c198b3f9bb2c1ba}"  # AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 11
-export KVM_UCSVERSION="${KVM_UCSVERSION:=5.2-5+2026-03-04}"  #
+export KVM_UCSVERSION="${KVM_UCSVERSION:=5.2-5+2026-03-05}"  #
 export OPENSTACK_IMAGE_VERSION="${OPENSTACK_IMAGE_VERSION:=5.2-4}"  # version for the openstack image
 export OPENSTACK_IMAGE_NAME="${OPENSTACK_IMAGE_NAME:=UCS $OPENSTACK_IMAGE_VERSION}"  # name of the default openstack image
 export OPENSTACK_IMAGE_NAME_OLD="UCS $old_release"
