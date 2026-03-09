89d54dc0f8be763e23387e80c1cd28351a4361c3
Christian Castens <castens@univention.de>
2026-03-09 14:58:05 +0100

    fix(test): 18_audit_event_log_levels
    univention/dev/projects/keycloak/keycloak-app#255

--- test/ucs-test/tests/81_keycloak/18_audit_event_log_levels.py
+++ test/ucs-test/tests/81_keycloak/18_audit_event_log_levels.py
@@ -22,7 +22,7 @@ def test_audit_event_log_levels(change_app_setting, portal_login_via_keycloak, u
     # Test successful login
     username = udm.create_user()[1]
     page = portal_login_via_keycloak(username, 'univention')
-    logs = run_command(['docker', 'logs', '--since', '5s', 'keycloak'])
+    logs = run_command(['docker', 'logs', 'keycloak'])
     login_pattern = r'INFO.*\[org\.keycloak\.events\].*type="LOGIN"'
     assert re.search(login_pattern, logs), f'Expected INFO level LOGIN event in logs, got:\n{logs}'
 
@@ -36,6 +36,6 @@ def test_audit_event_log_levels(change_app_setting, portal_login_via_keycloak, u
         'wrong_password',
         fails_with='The authentication has failed, please login again.',
     )
-    logs = run_command(['docker', 'logs', '--since', '5s', 'keycloak'])
+    logs = run_command(['docker', 'logs', 'keycloak'])
     login_error_pattern = r'ERROR.*\[org\.keycloak\.events\].*type="LOGIN_ERROR"'
     assert re.search(login_error_pattern, logs), f'Expected ERROR level LOGIN_ERROR event in logs, got:\n{logs}'
