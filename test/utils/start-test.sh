6e7a55fa19a00f46508ad5e604738b9cda4651ff
Marius Meschter <marius.meschter@univention.de>
2026-03-11 10:59:25 +0100

    chore: Use final Openstack image for 5.2-5
    Issue: univention/dev/ucs#3409
    Bug: #59093

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -12,7 +12,7 @@ export CURRENT_AMI="${CURRENT_AMI:=ami-008a55f0627c343c4}"  # AMI: Univention Co
 old_release='5.0-10'  #
 export OLD_AMI="${OLD_AMI:=ami-00c198b3f9bb2c1ba}"  # AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 11
 export KVM_UCSVERSION="${KVM_UCSVERSION:=5.2-5+e367}"
-export OPENSTACK_IMAGE_VERSION="${OPENSTACK_IMAGE_VERSION:=5.2-4}"  # version for the openstack image
+export OPENSTACK_IMAGE_VERSION="${OPENSTACK_IMAGE_VERSION:=5.2-5}"  # version for the openstack image
 export OPENSTACK_IMAGE_NAME="${OPENSTACK_IMAGE_NAME:=UCS $OPENSTACK_IMAGE_VERSION}"  # name of the default openstack image
 export OPENSTACK_IMAGE_NAME_OLD="UCS $old_release"
 export UCS_MINORRELEASE="${release%%-*}"
