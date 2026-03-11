univention/univention-corporate-server

commit 341ab2f70b2d0aebe6b2cc4c320e130c1c2b2ec2

feat(udm): add univentionObjectIdentifier to license objects
Bug #58384
Issue univention/dev/ucs#3312

management/univention-management-console-module-udm/umc/python/udm/tools.py
@@ -8,6 +8,7 @@
 
 import binascii
 import urllib.request
+import uuid
 from urllib.request import ProxyHandler, build_opener
 
 import ldap
@@ -71,6 +72,7 @@ class LicenseImport(ldif.LDIFParser):
 
         self.dncount += 1
 
+        entry['univentionObjectIdentifier'] = str(uuid.uuid4())
         if 'univentionLicenseBaseDN' in entry:
             self.base = entry['univentionLicenseBaseDN'][0].decode('UTF-8')
         else:

base/univention-licence/tools/univention-license-import.in
@@ -109,6 +109,12 @@ if [ "$rv" = "68" ]; then
 	ldapdelete -x -H "ldap://$ldap_master:$ldap_master_port" -ZZ -D "cn=admin,$ldap_base" -y /etc/ldap.secret "cn=admin,cn=license,cn=univention,$ldap_base"
 
 	ldapadd -x -H "ldap://$ldap_master:$ldap_master_port" -ZZ -D "cn=admin,$ldap_base" -y /etc/ldap.secret -f "$file"
+	ldapmodify -H "ldap://$ldap_master:$ldap_master_port" -ZZ -D "cn=admin,$ldap_base" -y /etc/ldap.secret <<__EOF__
+dn: cn=admin,cn=license,cn=univention,$ldap_base
+changetype: modify
+add: univentionObjectIdentifier
+univentionObjectIdentifier: $(python3 -c "import uuid; print(uuid.uuid4())")
+__EOF__
 
 	echo "Replaced license."
 elif [ "$rv" != 0 ] && [ "$rv" != 89 ]; then

management/univention-ldap/scripts/univention-update-univention-object-identifier
@@ -25,7 +25,7 @@ def setup_logging(args: argparse.Namespace):
 
 def update_univention_object_identifier(args: argparse.Namespace, lo: univention.uldap.access):
     result = lo.search(
-        filter="(&(objectClass=univentionObject)(!(objectClass=univentionLicense))(!(univentionObjectIdentifier=*)))",
+        filter="(&(objectClass=univentionObject)(!(univentionObjectIdentifier=*)))",
         attr=["univentionObjectIdentifier", "entryUUID"],
     )
 

commit f5af9ba04790d047b403a5f80daec2639e4183b0

fix(udm): fix adding univentionObjectIdentifier on license import
Fixes: 341ab2f70b2d0aebe6b2cc4c320e130c1c2b2ec2
Bug #58384

doc/errata/staging/univention-management-console-module-udm.yaml
@@ -3,7 +3,7 @@ release: "5.2"
 version: [5]
 scope: ucs_5.2-0-errata5.2-5
 src: univention-management-console-module-udm
-fix: 12.5.0
+fix: 12.5.1
 desc: |
  This update addresses the following issue:
  * The `univentionObjectIdentifier` is now set for all DNS, DHCP and license

management/univention-management-console-module-udm/umc/python/udm/tools.py
@@ -72,7 +72,7 @@ class LicenseImport(ldif.LDIFParser):
 
         self.dncount += 1
 
-        entry['univentionObjectIdentifier'] = str(uuid.uuid4())
+        entry['univentionObjectIdentifier'] = [str(uuid.uuid4()).encode('ASCII')]
         if 'univentionLicenseBaseDN' in entry:
             self.base = entry['univentionLicenseBaseDN'][0].decode('UTF-8')
         else: