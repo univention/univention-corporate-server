univention/univention-corporate-server

commit 8648de4cc28fe6d16ffa99a1a0f358965a63b2a7

Bug #58327: enhance the resilience of the App Center against attack vectors such as Cross-Site Scripting
Issue: univention/dev/ucs#2846
Issue: univention/dev/ucs#2899

doc/errata/staging/univention-appcenter.yaml
@@ -1,0 +1,11 @@
+product: ucs
+release: "5.2"
+version: [1]
+scope: ucs_5.2-0-errata5.2-1
+src: univention-appcenter
+fix: 11.1.1
+desc: |
+ This update addresses the following issues:
+ * Applied stricter content sanitization in the App Center to prevent
+   exploitation via Cross-Site Scripting (XSS) and related attack vectors.
+bug: [58327]

management/univention-appcenter/umc/js/appcenter/AppDetailsPage.js
@@ -45,6 +45,7 @@ define([
 	"dojo/store/Memory",
 	"dojo/store/Observable",
 	"dojox/html/entities",
+	"dompurify/purify",
 	"umc/app",
 	"umc/tools",
 	"umc/dialog",
@@ -62,7 +63,7 @@ define([
 	"umc/modules/appcenter/App",
 	"umc/modules/appcenter/ImageGallery",
 	"umc/i18n!umc/modules/appcenter"
-], function(declare, lang, kernel, array, when, ioQuery, topic, Deferred, domConstruct, domClass, Memory, Observable, entities, UMCApplication, tools, dialog, ContainerWidget, ProgressBar, Page, Text, Button, Grid, AppInfo, AppMoreInfo, Buy, Badges, Vote, App, ImageGallery, _) {
+], function(declare, lang, kernel, array, when, ioQuery, topic, Deferred, domConstruct, domClass, Memory, Observable, entities, purify, UMCApplication, tools, dialog, ContainerWidget, ProgressBar, Page, Text, Button, Grid, AppInfo, AppMoreInfo, Buy, Badges, Vote, App, ImageGallery, _) {
 
 	return declare("umc.modules.appcenter.AppDetailsPage", [ Page ], {
 		appLoadingDeferred: null,
@@ -462,7 +463,7 @@ define([
 			}
 			var descriptionContainer = new ContainerWidget({});
 			domClass.add(domConstruct.create('div', {
-				innerHTML: this.app.longDescription  // no HTML escape!
+				innerHTML: purify.sanitize(this.app.longDescription)
 			}, descriptionContainer.domNode));
 			parentContainer.addChild(descriptionContainer);
 
@@ -512,9 +513,9 @@ define([
 			}
 			var info = new AppInfo({
 				bgc: this.app.backgroundColor || "",
-				logo: '/univention/js/dijit/themes/umc/icons/scalable/' + this.app.logoName,
-				name: this.app.name,
-				description: this.app.description,
+				logo: '/univention/js/dijit/themes/umc/icons/scalable/' + encodeURIcomponent(this.app.logoName.replace(/(\.\.|\/|\\)/g, '')),
+				name: entities.encode(this.app.name),
+				description: purify.sanitize(this.app.description),
 				buttonLabel: buttonLabel,
 				callback: callback,
 				'class': 'umcAppDetailsPage__content__callToAction'
@@ -603,13 +604,13 @@ define([
 			domConstruct.create('span', {
 				'class': 'appDetailsSidebarText',
 				innerHTML: _('Buy %(appName)s to install version %(candidateVersion)s.',
-						{appName: this.app.name, candidateVersion: this.app.candidateVersion || this.app.version})
+						{appName: entities.encode(this.app.name), candidateVersion: entities.encode(this.app.candidateVersion) || entities.encode(this.app.version)})
 			}, parentContainer.domNode);
 
 			if (this.app.candidateInstallPermissionMessage) {
 				domConstruct.create('span', {
 					'class': 'appDetailsSidebarText',
-					innerHTML: this.app.candidateInstallPermissionMessage
+					innerHTML: entities.encode(this.app.candidateInstallPermissionMessage)
 				}, parentContainer.domNode);
 			}
 		},
@@ -706,7 +707,7 @@ define([
 					}];
 				}
 				var content = '<div style="max-height:250px; overflow:auto;">' +
-						readme +  // no HTML escape!
+						purify.sanitize(readme) +
 					'</div>';
 				dialog.confirm(content, buttons, title).then(function(response) {
 					if (response == 'yes') {
@@ -783,14 +784,14 @@ define([
 			if (license) {
 				license = entities.encode(license);
 				if (licenseAgreement) {
-					license += lang.replace(' (<a href="javascript:void(0)" onclick="require(\'dijit/registry\').byId(\'{id}\').showLicenseAgreement();">' + _('Read license agreement') + '</a>)', {
-						id: this.id
+					license += lang.replace(' (<a href="javascript:void(0)" onclick="require(\'dijit/registry\').byId({id}).showLicenseAgreement();">' + _('Read license agreement') + '</a>)', {
+						id: entities.encode(JSON.stringify(this.id))
 					});
 				}
 			} else {
 				if (licenseAgreement) {
-					license = lang.replace('<a href="javascript:void(0)" onclick="require(\'dijit/registry\').byId(\'{id}\').showLicenseAgreement();">' + _('Read license agreement') + '</a>', {
-						id: this.id
+					license = lang.replace('<a href="javascript:void(0)" onclick="require(\'dijit/registry\').byId({id}).showLicenseAgreement();">' + _('Read license agreement') + '</a>', {
+						id: entities.encode(JSON.stringify(this.id))
 					});
 				} else if (license === null) {
 					license = _('The App does not provide any information about the license. Please contact the App provider for further details.');

management/univention-appcenter/umc/js/appcenter/AppInstallWizard.js
@@ -338,6 +338,3 @@ define([
 		}
 	});
 });
-
-
-

management/univention-appcenter/umc/js/appcenter/AppInstallWizardLicenseAgreementPage.js
@@ -33,10 +33,11 @@
 
 define([
 	"dojo/_base/declare",
+	"dojox/html/entities",
 	"umc/widgets/Text",
 	"./AppText",
 	"umc/i18n!umc/modules/appcenter"
-], function(declare, Text, AppText, _) {
+], function(declare, entities, Text, AppText, _) {
 	return {
 		getPageConf: function(app) {
 			if (!app.licenseAgreement) {
@@ -55,7 +56,7 @@ define([
 					type: Text,
 					'class': 'appInstallDialog__readme',
 					name: `licenseAgreement_licenseAgreement_${app.id}`,
-					content: app.licenseAgreement
+					content: entities.encode(app.licenseAgreement)
 				}]
 			};
 		}

management/univention-appcenter/umc/js/appcenter/AppInstallWizardReadmeInstallPage.js
@@ -33,10 +33,11 @@
 
 define([
 	"dojo/_base/declare",
+	"dompurify/purify",
 	"umc/widgets/Text",
 	"./AppText",
 	"umc/i18n!umc/modules/appcenter"
-], function(declare, Text, AppText, _) {
+], function(declare, purify, Text, AppText, _) {
 	return {
 		getPageConf: function(app, readme) {
 			if (!app[readme]) {
@@ -55,14 +56,9 @@ define([
 					type: Text,
 					'class': 'appInstallDialog__readme',
 					name: `readmeInstall_readme_${app.id}`,
-					content: app[readme]
+					content: purify.sanitize(app[readme])
 				}]
 			};
 		}
 	};
 });
-
-
-
-
-

management/univention-appcenter/umc/js/appcenter/AppLiveSearchSidebar.js
@@ -39,13 +39,15 @@ define([
 	"dojo/Deferred",
 	"dojo/dom-construct",
 	"dojo/regexp",
+	"dojox/html/entities",
+	"dompurify/purify",
 	"umc/tools",
 	"umc/widgets/CheckBox",
 	"umc/widgets/ContainerWidget",
 	"umc/widgets/Form",
 	"umc/widgets/SearchBox",
 	"umc/i18n!umc/modules/appcenter"
-], function(declare, lang, array, has, Deferred, domConstruct, regexp, tools, CheckBox, ContainerWidget, Form, SearchBox, _) {
+], function(declare, lang, array, has, Deferred, domConstruct, regexp, entities, purify, tools, CheckBox, ContainerWidget, Form, SearchBox, _) {
 	return declare("umc.modules.appcenter.AppLiveSearchSidebar", [ContainerWidget], {
 		// summary:
 		//		Offers a side bar for live searching, a set of categories can be defined.
@@ -164,7 +166,7 @@ define([
 			formContainer = this._filterForms[id] = new ContainerWidget({'class': 'appLiveSearchSidebarElement'});
 			if (title) {
 				domConstruct.create('span', {
-					innerHTML: title,
+					innerHTML: entities.encode(title),
 					'class': 'mainHeader'
 				}, formContainer.domNode);
 			}
@@ -175,7 +177,7 @@ define([
 			array.forEach(choices, lang.hitch(this, function(choice) {
 				var label = choice.description;
 				if (!title && choices.length === 1) {
-					label = '<span class="searchFilterSingle">' + choice.description + '</span>';
+					label = '<span class="searchFilterSingle">' + purify.sanitize(choice.description) + '</span>';
 				}
 				widgets.push({
 					type: CheckBox,

management/univention-appcenter/umc/js/appcenter/AppSettings.js
@@ -4,6 +4,7 @@ define([
 	"dojo/_base/declare",
 	"dojo/_base/lang",
 	"dojo/_base/array",
+	"dojox/html/entities",
 	"umc/tools",
 	"umc/widgets/Text",
 	"umc/widgets/TextBox",
@@ -14,7 +15,7 @@ define([
 	"./AppSettingsFileUploader",
 	"./AppSettingsForm",
 	"umc/i18n!umc/modules/appcenter"
-], function(declare, lang, array, tools, Text, TextBox, NumberSpinner, PasswordInputBox, CheckBox, ComboBox, AppSettingsFileUploader, AppSettingsForm, _) {
+], function(declare, lang, array, entities, tools, Text, TextBox, NumberSpinner, PasswordInputBox, CheckBox, ComboBox, AppSettingsFileUploader, AppSettingsForm, _) {
 	return {
 		getWidgets: function(app, values, phase) {
 			var ret = [];
@@ -134,9 +135,9 @@ define([
 					return null;
 				}
 				var groupName = '_group' + i;
-				var content = '<h2>' + group.label + '</h2>';
+				var content = '<h2>' + entities.encode(group.label) + '</h2>';
 				if (smallHeaders) {
-					content = '<h3>' + group.label + '</h2>';
+					content = '<h3>' + entities.encode(group.label) + '</h2>';
 				}
 				widgets.push({
 					type: Text,

management/univention-appcenter/umc/js/appcenter/AppSettingsFileUploader.js
@@ -85,7 +85,7 @@ define([
 					lengthContent += _('After saving, the file will be deleted.');
 				}
 			}
-			this._content.set('content', _('File will be uploaded to %s.', '<em>' + this.fileName + '</em>') + ' ' + lengthContent);
+			this._content.set('content', _('File will be uploaded to %s.', '<em>' + entities.encode(this.fileName) + '</em>') + ' ' + lengthContent);
 		}
 	});
 });

commit 673db47ade99adcde10ca9fe308b1f5c851e92e2

fix(appcenter): open links in app details view in new window
This is a semi-regression from 8648de4cc28fe6d16ffa99a1a0f358965a63b2a7
where the description was passed into DOMPurify, which strips the target
attribute by default.
This caused the links clicked in the description or license agreement
popup of the AppCenter to be opened in the AppCenter iFrame.

Add target _blank and rel noopener and norefferrer to all `a` tags in
the description and license agreement.

Issue: univention/dev/ucs#2383
Bug: #57501

doc/errata/staging/univention-appcenter.yaml
@@ -3,10 +3,12 @@ release: "5.2"
 version: [5]
 scope: ucs_5.2-0-errata5.2-5
 src: univention-appcenter
-fix: 11.5.1
+fix: 11.5.2
 desc: |
  This update addresses the following issues:
  * The App Center now avoids allocating Docker Compose network
    subnets that overlap with host network interfaces,
    preventing potential routing conflicts.
-bug: [55073]
+ * Links in app descriptions and license agreements now open in a
+   new browser tab instead of loading inside the App Center iFrame.
+bug: [55073, 57501]

management/univention-appcenter/umc/js/appcenter/AppDetailsPage.js
@@ -435,9 +435,14 @@ define([
 				parentContainer.own(header);
 			}
 			var descriptionContainer = new ContainerWidget({});
-			domClass.add(domConstruct.create('div', {
+			var descriptionNode = domConstruct.create('div', {
 				innerHTML: purify.sanitize(this.app.longDescription)
-			}, descriptionContainer.domNode));
+			}, descriptionContainer.domNode);
+			domClass.add(descriptionNode);
+			array.forEach(descriptionNode.querySelectorAll('a'), function(link) {
+				link.setAttribute('target', '_blank');
+				link.setAttribute('rel', 'noopener noreferrer');
+			});
 			parentContainer.addChild(descriptionContainer);
 
 		},
@@ -679,8 +684,15 @@ define([
 						'default': true
 					}];
 				}
+				var sanitizedNode = domConstruct.create('div', {
+					innerHTML: purify.sanitize(readme)
+				});
+				array.forEach(sanitizedNode.querySelectorAll('a'), function(link) {
+					link.setAttribute('target', '_blank');
+					link.setAttribute('rel', 'noopener noreferrer');
+				});
 				var content = '<div style="max-height:250px; overflow:auto;">' +
-						purify.sanitize(readme) +
+						sanitizedNode.innerHTML +
 					'</div>';
 				dialog.confirm(content, buttons, title).then(function(response) {
 					if (response == 'yes') {