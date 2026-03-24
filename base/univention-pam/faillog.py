24c615c8dadced630976d00d107afadc744c8054
Arvid Requate <requate@univention.de>
2026-03-24 10:04:40 +0100

    fix(pam): add local faillog count when account was locked in UDM
    the faillog.py listener unlocks accounts if the locking state in UDM changed from locked to unlocked.
    
    But if the password lockout state changes from unlocked to locked in UDM,
    the listener doesn't set the faillog/pam_tally status to locked.
    
    Bug #52892
    Co-Authored-By: Florian Best <best@univention.de>

--- base/univention-pam/faillog.py
+++ base/univention-pam/faillog.py
@@ -23,11 +23,21 @@ def __login_is_locked(attrs: dict[str, list[bytes]]) -> bool:
 
 
 def handler(dn: str, new: dict[str, list[bytes]], old: dict[str, list[bytes]]) -> None:
+    uid = new['uid'][0].decode('UTF-8')
     if new and old and __login_is_locked(old) and not __login_is_locked(new):
         # reset local bad password count
-        ud.debug(ud.LISTENER, ud.PROCESS, 'Reset faillog for user %s' % new['uid'][0].decode('UTF-8'))
-        listener.setuid(0)
-        try:
-            listener.run('/usr/sbin/faillock', ['faillock', '--user', new['uid'][0].decode('UTF-8'), '--reset'])
-        finally:
-            listener.unsetuid()
+        ud.debug(ud.LISTENER, ud.PROCESS, 'Reset faillog for user %s' % uid)
+        with listener.SetUID(0):
+            listener.run('/usr/sbin/faillock', ['faillock', '--user', uid, '--reset'])
+    elif __login_is_locked(new) and not __login_is_locked(old):
+        if listener.configRegistry.is_true('auth/faillog'):
+            # set local bad password count high enough for this system:
+            limit = listener.configRegistry.get_int('auth/faillog/limit', 5)
+            ud.debug(ud.LISTENER, ud.PROCESS, 'Trigger faillog for user %r' % uid)
+            with listener.SetUID(0):
+                listener.run('/usr/sbin/faillock', ['faillock', '--user', uid, '--reset=%s' % (limit + 1,)])  # FIXME: doesn't work
+    elif old:
+        # clean up on delete: reset local bad password count
+        ud.debug(ud.LISTENER, ud.PROCESS, 'Reset faillog for user %r' % uid)
+        with listener.SetUID(0):
+            listener.run('/usr/sbin/faillock', ['faillock', '--user', uid, '--reset'])
