e5957cc0703c4c1c59e27cad3f684b586714ae59
Nico Gulden <gulden@univention.de>
2026-03-27 17:36:46 +0100

    docs(manual/conf): Use lib to resolve inner project docs
    Issue univention/dev/ucs#3422

--- doc/manual/conf.py
+++ doc/manual/conf.py
@@ -13,50 +13,12 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-from __future__ import annotations
 
 import os
-import re
 import sys
 from datetime import date
-from typing import Iterator
-from urllib.parse import urlparse
 
-
-def _slugify(s: str) -> str:
-    return re.sub(r"[^0-9a-z]", "-", s.lower())[:63].strip("-")
-
-
-def _inventory(name: str, language: str) -> Iterator[str | None]:
-    env = os.environ
-    # Local build
-    yield f"../{name}/_build/{language}/html/objects.inv"
-    yield f"../{name}/_build/html/objects.inv"
-    # Previous build: current branch, merge target, default branch
-    for var in ('CI_COMMIT_REF_NAME', 'CI_MERGE_REQUEST_TARGET_BRANCH_NAME', 'CI_DEFAULT_BRANCH'):
-        try:
-            branch = env[var]
-            slug = _slugify(branch)
-            url = f"{env['CI_API_V4_URL']}/projects/{env['CI_PROJECT_ID']}/packages/generic/{name}.{language}/{slug}/objects.inv"
-        except LookupError:
-            continue
-        o = urlparse(url)
-        o = o._replace(netloc=f"job:{os.environ['CI_JOB_TOKEN']}@{o.netloc}")
-        yield o.geturl()
-    # Public build
-    yield None
-
-
-def ref(name: str, *, lang: str = "en", ver: str = "") -> tuple[str, tuple[str | None, ...]]:
-    ver = ver or version
-    return (
-        f"https://docs.software-univention.de/{name}/{ver}/{lang}",
-        tuple(_inventory(name, language)),
-    )
-
-
-# sys.path.insert(0, os.path.abspath('.'))
+from univention_sphinx_conf_helper.inventory_resolver import reference_inventory
 
 
 # -- Project information -----------------------------------------------------
@@ -73,6 +35,11 @@ html_show_copyright = True
 language = 'en'
 
 html_title = project
+#
+# The doc_basename must match the documents root directory name on the public
+# target location. Otherwise the PDF link on the overview page will point to
+# the wrong file.
+doc_basename = os.path.basename(os.path.dirname(__file__))
 
 # -- General configuration ---------------------------------------------------
 
@@ -95,14 +62,29 @@ extensions = [
 suppress_warnings = ['git.too_shallow']
 
 intersphinx_mapping = {
-    "uv-dev-ref": ref("developer-reference"),
-    "uv-keycloak-ref": ("https://docs.software-univention.de/keycloak-app/latest", None),
-    "uv-nubus-kubernetes-architecture": ('https://docs.software-univention.de/nubus-kubernetes-architecture/latest/en/', None),
-    "uv-ox-connector-app": ("https://docs.software-univention.de/ox-connector-app/latest", None),
-    "uv-ext-windows": ref("ext-windows"),
-    'uv-ucs-operation': ref('ucs-operation', ver=version),
-    'uv-nubus-manual': ('https://docs.software-univention.de/nubus-manual/latest/en/', None),
-    'uv-nubus-customization': ('https://docs.software-univention.de/nubus-customization/latest/en/', None),
+    "uv-dev-ref": reference_inventory("developer-reference"),
+    "uv-keycloak-ref": (
+        "https://docs.software-univention.de/keycloak-app/latest",
+        None,
+    ),
+    "uv-nubus-kubernetes-architecture": (
+        "https://docs.software-univention.de/nubus-kubernetes-architecture/latest/en/",
+        None,
+    ),
+    "uv-ox-connector-app": (
+        "https://docs.software-univention.de/ox-connector-app/latest",
+        None,
+    ),
+    "uv-ext-windows": reference_inventory("ext-windows"),
+    "uv-ucs-operation": reference_inventory("ucs-operation", version=version),
+    "uv-nubus-manual": (
+        "https://docs.software-univention.de/nubus-manual/latest/en/",
+        None,
+    ),
+    "uv-nubus-customization": (
+        "https://docs.software-univention.de/nubus-customization/latest/en/",
+        None,
+    ),
 }
 
 bibtex_bibfiles = ["../bibliography.bib"]
@@ -129,15 +111,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-pdf_doc_base = "manual"
 
 html_theme = 'univention_sphinx_book_theme'
 html_theme_options = {
-    "pdf_download_filename": f"{pdf_doc_base}.pdf",
+    "pdf_download_filename": f"{doc_basename}.pdf",
     "show_source_license": True,
     "typesense_search": True,
-    "typesense_document": pdf_doc_base,
+    "typesense_document": doc_basename,
     "typesense_document_version": version,
     "univention_matomo_tracking": True,
     "univention_docs_deployment": True,
@@ -174,7 +154,7 @@ rst_epilog = """
 latex_engine = 'lualatex'
 latex_show_pagerefs = True
 latex_show_urls = "footnote"
-latex_documents = [(root_doc, f'{pdf_doc_base}.tex', "", author, "manual", False)]
+latex_documents = [(root_doc, f"{doc_basename}.tex", "", author, "manual", False)]
 latex_elements = {
     "papersize": "a4paper",
     "babel": "\\usepackage{babel}",
@@ -227,14 +207,33 @@ def adapt_settings_to_translation(app, config):
 .. include:: /links-de.txt
 """
         config.intersphinx_mapping = {
-            'uv-dev-ref': ref('developer-reference'),
-            'uv-keycloak-ref': ('https://docs.software-univention.de/keycloak-app/latest', None),
-            'uv-nubus-kubernetes-architecture': ('https://docs.software-univention.de/nubus-kubernetes-architecture/latest/en/', None),
-            'uv-ox-connector-app': ('https://docs.software-univention.de/ox-connector-app/latest', None),
-            'uv-ext-windows': ref('ext-windows'),
-            'uv-ucs-operation': ref('ucs-operation', ver=version, lang='de'),
-            'uv-nubus-manual': ('https://docs.software-univention.de/nubus-manual/latest/de/', None),
-            'uv-nubus-customization': ('https://docs.software-univention.de/nubus-customization/latest/en/', None),
+            "uv-dev-ref": reference_inventory("developer-reference"),
+            "uv-keycloak-ref": (
+                "https://docs.software-univention.de/keycloak-app/latest",
+                None,
+            ),
+            "uv-nubus-kubernetes-architecture": (
+                "https://docs.software-univention.de/nubus-kubernetes-architecture/latest/en/",
+                None,
+            ),
+            "uv-ox-connector-app": (
+                "https://docs.software-univention.de/ox-connector-app/latest",
+                None,
+            ),
+            "uv-ext-windows": reference_inventory("ext-windows"),
+            "uv-ucs-operation": reference_inventory(
+                "ucs-operation",
+                version=version,
+                language="de",
+            ),
+            "uv-nubus-manual": (
+                "https://docs.software-univention.de/nubus-manual/latest/de/",
+                None,
+            ),
+            "uv-nubus-customization": (
+                "https://docs.software-univention.de/nubus-customization/latest/en/",
+                None,
+            ),
         }
 
 
