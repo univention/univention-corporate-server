univention/univention-corporate-server

commit dc5a075e1e25b343ad698916f46b32c6b9fe4fa2

feat(udm): implement ABAC (attribute-based access control) authorization in UDM
Bug #58432

management/univention-directory-manager-modules/modules/univention/admin/syntax.py
@@ -856,13 +856,13 @@ class UDM_Objects(ISyntax, _UDMObjectOrAttribute):
                                 return []
 
                             if ldap_attr is not None:
-                                return lo.search(filter=str(lookup_filter), attr=ldap_attr, **module_search_options)
+                                return lo.search_filtered({'module': module.module}, filter=str(lookup_filter), attr=ldap_attr, **module_search_options)
                             else:
-                                return lo.searchDn(filter=str(lookup_filter), **module_search_options)
+                                return lo.search_dn_filtered({'module': module.module}, filter=str(lookup_filter), **module_search_options)
                         return module.lookup(None, lo, filter_s, **module_search_options)
 
                     if ldap_attr:
-                        result = _search(module, filter_s, ldap_attr)
+                        result = _search(module, filter_s, [*ldap_attr, 'univentionObjectType'])
                         for dn, ldap_map in result:
                             info = univention.admin.mapping.mapDict(mapping, ldap_map)
                             key, label = extract_key_label(cls, dn, info)
@@ -980,8 +980,12 @@ class UDM_Attribute(ISyntax, _UDMObjectOrAttribute):
 
         log.debug('Found syntax %s with udm_module property', cls.name)
         if cls.udm_filter == 'dn':
-            obj = module.object(None, lo, None, options.get('dependencies', {})[cls.depends])
-            choices = map_choice(obj)
+            target_dn = options.get('dependencies', {})[cls.depends]
+            obj = module.object(None, lo, None, target_dn)
+            objs = lo.filter_lookup_results([obj])
+            if not objs:
+                raise univention.admin.uexceptions.noObject(target_dn)
+            choices = map_choice(objs[0])
         else:
             filter_s = cls._create_ldap_filter(options, module)
             if filter_s is not None:
@@ -6036,6 +6040,8 @@ class LDAP_Search(select):
                 filter_s = univention.admin.pattern_replace(filter_s, eobj)
 
         choices = []
+        results = []
+
         for dn in lo.searchDn(filter=filter_s, base=cls.base):
             # cls.attributes: pass on all display attributes so the frontend has a chance to supoport it some day
             if cls.viewonly:
@@ -6056,18 +6062,23 @@ class LDAP_Search(select):
             obj = module.object(None, lo, None, dn)
             if not obj:
                 continue
-            obj.open()
+            results.append(obj)
 
+        results = lo.filter_lookup_results(results)
+
+        for obj in results:
+            obj.open()
+            module = univention.admin.modules.get(obj.module)
             # find the value to store
-            id = dn
+            id_ = dn
             if not cls.viewonly:
                 _mod_store, store = split_module_attr(store_pattern)
                 if store == 'dn':
-                    id = dn
+                    id_ = dn
                 elif store in obj:
-                    id = obj[store]
+                    id_ = obj[store]
                 elif obj.oldattr.get(store):
-                    id = obj.oldattr[store][0].decode(*module.mapping.getEncoding(store))
+                    id_ = obj.oldattr[store][0].decode(*module.mapping.getEncoding(store))
                 else:
                     # no valid store object, ignore
                     log.warning('LDAP_Search syntax %r: %r is no valid property for object %r - ignoring entry.', cls.name, store, dn)
@@ -6088,8 +6099,8 @@ class LDAP_Search(select):
                     label = 'Unknown attribute %r' % (display,)
 
             # TODO: remove this one day...
-            # choices.append((id, label))
-            choices.append({'objectType': module.module, 'id': id, 'label': label})
+            # choices.append((id_, label))
+            choices.append({'objectType': module.module, 'id': id_, 'label': label})
 
         # sort choices before inserting / appending some special items
         choices = cls.sort_choices(choices)

base/univention-python/modules/uldap.py
@@ -11,6 +11,8 @@
 import logging
 import random
 import re
+import traceback
+import warnings
 from collections.abc import Callable
 from functools import wraps
 from typing import Any
@@ -246,6 +248,12 @@ class access:
             log.error('No LDAP base given to uldap.access()! Can lead to mysterious errors! %s', ''.join(traceback.format_stack()))
             warnings.warn('No LDAP base given to uldap.access()! Can lead to mysterious errors!', DeprecationWarning, stacklevel=3)
             self.base = ucr['ldap/base']
+
+    @property
+    def authz_connection(self):
+        log.error('Wrong access class in use! Use univention.admin.uldap instead of univention.uldap! %s', ''.join(traceback.format_stack()))
+        warnings.warn('Wrong access class in use! Use univention.admin.uldap instead of univention.uldap!', DeprecationWarning, stacklevel=3)
+        return self
 
     @_fix_reconnect_handling
     def bind(self, binddn: str, bindpw: str) -> None:

management/univention-directory-manager-modules/debian/control
@@ -43,7 +43,7 @@ Depends:
  python3-pyasn1-modules,
  python3-tzlocal,
  python3-unidecode,
- python3-univention (>= 13.0.5-3),
+ python3-univention (>= 15.2.0),
  python3-univention-admin-diary,
  python3-univention-debug (>= 12.0.3-1),
  python3-univention-directory-manager-cli (= ${binary:Version}),

management/univention-directory-manager-modules/modules/univention/admin/authorization.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 #
-# Univention Management Console
-#  module: manages UDM modules
+# Univention Directory Manager
 #
 # Like what you see? Join us!
 # https://www.univention.com/about-us/careers/vacancies/
 #
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 # SPDX-License-Identifier: AGPL-3.0-only
+"""Authorization for UDM access."""
+
 import copy
 import json
 import os
 import re
+from logging import getLogger
 
-from univention.management.console.config import ucr
-from univention.management.console.error import Forbidden
-from univention.management.console.log import MODULE
+import univention.admin.modules
+from univention.admin._ucr import configRegistry as ucr
+from univention.admin.uexceptions import permissionDenied
 from univention.uldap import parentDn
 
+
+log = getLogger('ADMIN')
 
 # load roles
 # TODO: move to some other place
@@ -31,8 +35,8 @@ if ucr.is_true('umc/udm/delegation'):
                 with open(file) as roles:
                     ROLES.update(json.load(roles))
     except Exception as exc:
-        MODULE.error(f'Loading role failed with {exc}')
-    MODULE.info(f'Loaded roles: {ROLES}')
+        log.error('Loading role failed with %s', exc)
+    log.info('Loaded roles: {ROLES}')
 
 ldap_base = ucr.get("ldap/base")
 
@@ -46,19 +50,25 @@ def _obj2dn(obj: object | dict | str) -> str:
             return obj["id"]
         if isinstance(obj, str):
             return obj
+        if isinstance(obj, tuple) and len(obj) == 2:
+            return obj[0]
     except (AttributeError, KeyError):
         pass
-    raise ValueError("Invalid object format for extracting DN")
+    raise ValueError("Invalid object format for extracting DN: ", obj)
 
 
 def _obj2position(obj: object | dict | str) -> str:
     """Extracts the position from an object's distinguished name (DN)."""
     try:
+        if isinstance(obj, tuple):
+            return _obj2position(_obj2dn(obj[0]))
         if hasattr(obj, "position") and (not hasattr(obj, "dn") or not obj.dn):
             return obj.position.getDn().lower()
         if isinstance(obj, dict) and 'position' in obj:
             return obj['position'].lower()
-        return parentDn(_obj2dn(obj)).lower()
+        if parentDn(_obj2dn(obj), ucr['ldap/base']) is None:
+            return _obj2dn(obj).lower()
+        return parentDn(_obj2dn(obj), ucr['ldap/base']).lower()
     except (AttributeError, KeyError, IndexError):
         pass
     raise ValueError("Invalid object format for extracting position")
@@ -67,17 +77,26 @@ def _obj2position(obj: object | dict | str) -> str:
 def _obj2module(obj: object | dict | str) -> str:
     if hasattr(obj, "module"):
         return obj.module
-    if isinstance(obj, dict) and "module_name" in obj:
-        return obj["module_name"]
+    if isinstance(obj, dict) and "univentionObjectType" in obj:
+        return obj["univentionObjectType"][0].decode('UTF-8')
     if isinstance(obj, dict | str):
         dn = _obj2dn(obj)
+        if dn.startswith('dc='):
+            return 'container/dc'
         # FIXME: extract module name using dn
-        if "cn=users" in dn:
+        if dn.lower().startswith('cn=groups,') or dn.lower().startswith('cn=users,'):
+            return 'container/cn'
+        if "cn=users" in dn or 'cn=self registered users' in dn or dn.startswith('uid='):
             return "users/user"
         if "cn=groups" in dn:
             return "groups/group"
+        if dn.lower().startswith('ou='):
+            return "container/ou"
         else:
             raise NotImplementedError(f"Module extraction from DN not implemented {dn}: {obj} ")
+    if isinstance(obj, tuple):
+        return _obj2module(obj[1])
+    raise NotImplementedError(obj)
 
 
 def _get_cap_priority(target_position: str):
@@ -119,7 +138,7 @@ def _check_condition(position: str, condition: dict) -> bool:
         return True
     scope = condition.get('scope', 'base')
     condition_positions = condition.get('contexts', []) if condition['position'] == '$CONTEXT' else [condition['position']]
-    if scope == "subtree":
+    if scope in ('one', "subtree"):
         return _check_scope_subtree(position, condition_positions)
     if scope == "base":
         return _check_scope_base(position, condition_positions)
@@ -139,10 +158,10 @@ def _check_permissions(obj: object | str, caps: list[dict], action: str) -> bool
 
 def _get_attrs_from_permissions(module_name: str, permissions: dict) -> (list[str], list[str], list[str]):
     """Retrieves writable and explicitly readable attributes for a given module from permissions."""
-    attributes = permissions.get(module_name, {}).get('attributes', []) or permissions.get('*', {}).get('attributes', [])
-    readable_attributes = [attr for attr in attributes if attributes[attr] == 'read']
-    writable_attributes = [attr for attr in attributes if attributes[attr] == 'write']
-    none_attributes = [attr for attr in attributes if attributes[attr] == 'none']
+    attributes = permissions.get(module_name, {}).get('attributes', {}) or permissions.get('*', {}).get('attributes', {})
+    readable_attributes = [attr for attr in attributes if attributes[attr].get('access', '') == 'read']
+    writable_attributes = [attr for attr in attributes if attributes[attr].get('access', '') == 'write']
+    none_attributes = [attr for attr in attributes if attributes[attr].get('access', '') == 'none']
     return writable_attributes, readable_attributes, none_attributes
 
 
@@ -207,13 +226,13 @@ def _check_permissions_read(objs: list[object | dict | str], caps: list[dict]) -
                 readable_attrs, not_readable_attrs = _get_readable_attrs_from_permissions(module_name, cap['permissions'])
 
                 if readable_attrs:
-                    attrs_readable[(position, module_name)] = readable_attrs, not_readable_attrs
+                    attrs_readable[position, module_name] = readable_attrs, not_readable_attrs
                     break
 
     for (position, module_name), _objs in objs_processed.items():
         if (position, module_name) in attrs_readable:
-            if not isinstance(_objs[0], dict | str):
-                readable_attrs, not_readable_attrs = attrs_readable[(position, module_name)]
+            if not isinstance(_objs[0], dict | str | tuple):
+                readable_attrs, not_readable_attrs = attrs_readable[position, module_name]
                 for obj in _objs:
                     if "*" not in readable_attrs:
                         obj.info = {attr_name: obj.info[attr_name] for attr_name in readable_attrs if attr_name in obj.info}
@@ -243,22 +262,13 @@ def _get_capabilities(actor_roles: dict) -> list[dict]:
     return cap
 
 
-def _check_authorization() -> bool:
-    return ucr.is_true("umc/udm/delegation")
-
-
-def user_may_create(obj: object | dict | str, actor_roles_func: callable) -> None:
-    if not _check_authorization():
-        return
+def may_create(obj: object | dict | str, actor_roles_func: callable) -> None:
     actor_roles = actor_roles_func()
     cap = _get_capabilities(actor_roles)
-    if not _check_permissions_create(obj, cap):
-        raise Forbidden()
+    return _check_permissions_create(obj, cap)
 
 
-def user_may_read(objs: list[object | dict | str] | object, actor_roles_func: callable, filter_options: dict | None = None) -> list[object | dict | str] | object:
-    if not _check_authorization():
-        return objs
+def may_read(objs: list[object | dict | str] | object, actor_roles_func: callable, filter_options: dict | None = None) -> list[object | dict | str] | object:
     result = objs
     if not isinstance(objs, list):
         result = [objs]
@@ -267,7 +277,7 @@ def user_may_read(objs: list[object | dict | str] | object, actor_roles_func: ca
     result = _check_permissions_read(result, cap)
     if not isinstance(objs, list):
         if not result:
-            raise Forbidden()
+            raise permissionDenied()
         return result[0]
     if filter_options:
         attribute = filter_options.get('attribute')
@@ -281,33 +291,165 @@ def user_may_read(objs: list[object | dict | str] | object, actor_roles_func: ca
     return result
 
 
-def user_may_modify(obj: object, actor_roles_func: callable) -> None:
-    if not _check_authorization():
-        return
+def may_modify(obj: object, actor_roles_func: callable) -> None:
     actor_roles = actor_roles_func()
     cap = _get_capabilities(actor_roles)
-    if not _check_permissions_modify(obj, cap):
-        raise Forbidden()
+    return _check_permissions_modify(obj, cap)
 
 
-def user_may_delete(obj: object, actor_roles_func: callable) -> None:
-    if not _check_authorization():
-        return
+def may_delete(obj: object, actor_roles_func: callable) -> None:
     actor_roles = actor_roles_func()
     cap = _get_capabilities(actor_roles)
-    if not _check_permissions_delete(obj, cap):
-        raise Forbidden()
+    return _check_permissions_delete(obj, cap)
 
 
 # TODO: check if we need something special for move/rename
-def user_may_move(obj: object, dest: str, actor_roles_func: callable) -> None:
-    if not _check_authorization():
-        return
-    # user_may_modify(obj, actor_roles_func)  # optional
+def may_move(obj: object, dest: str, actor_roles_func: callable) -> None:
+    # may_modify(obj, actor_roles_func)  # optional
     actor_roles = actor_roles_func()
     cap = _get_capabilities(actor_roles)
-    if not _check_permissions_delete(obj, cap):
-        raise Forbidden()
-    if not _check_permissions_create(dest, cap):
-        raise Forbidden()
-    return
+    return _check_permissions_delete(obj, cap) and _check_permissions_create(dest, cap)
+
+
+def get_user_roles(lo, user_dn: str) -> None:
+    # code from components/authorization-engine/guardian/authorization-api/guardian_authorization_api/adapters/persistence.py
+    re_split_roles_and_contexts = re.compile(r"^((?P<role_app>[a-z0-9-_]+):(?P<role_namespace>[a-z0-9-_]+):(?P<role_name>[a-z0-9-_]+))(&(?P<context_app>[a-z0-9-_]+):(?P<context_namespace>[a-z0-9-_]+):(?P<context_name>[a-z0-9-_=,]+))?$")
+    # FIXME: Why doesn't this allow at least "=" and "," at least in "context_name"?
+    # Basically it should allow everything valid in an LDAP DN!? I.e.  case insensitive UTF-8 see https://ldapwiki.com/wiki/Wiki.jsp?page=Distinguished%20Name%20Case%20Sensitivity and https://ldapwiki.com/wiki/Wiki.jsp?page=Ou
+
+    data = lo.get(user_dn, attr=['univentionObjectType'])
+    mod = univention.admin.modules.get(data['univentionObjectType'][0].decode('UTF-8'))
+    obj = mod.object(None, lo, None, user_dn)
+    obj.open()
+    if hasattr(obj, 'open_guardian'):
+        obj.open_guardian()
+    role_set = set(obj.get("guardianInheritedRoles", []) + obj.get("guardianRoles", []))
+
+    __user_roles = {}
+    # simulating guardian_authorization_api.adapters.persistence.UDMPersistenceAdapter._to_policy_role()
+    for role in role_set:
+        if role.startswith("umc:udm:"):
+            res = re.search(re_split_roles_and_contexts, role)
+            if res:
+                res.groupdict()
+                __user_roles.setdefault(res["role_name"], [])
+                if res["context_name"]:
+                    __user_roles[res["role_name"]].append(res["context_name"])
+    log.info('Setting user roles to %s', __user_roles)
+    return __user_roles
+
+
+class Authorization:
+    """Check authorization via access control lists"""
+
+    global_enabled = False
+    get_privileged_connection = lambda: None  # noqa: E731
+    _cache_user_roles = {}
+
+    @classmethod
+    def enable(cls, get_privileged_connection):
+        """Enables ACL checking globally if the running service supports it"""
+        cls.global_enabled = True
+        cls.get_privileged_connection = get_privileged_connection
+
+    @classmethod
+    def inject_ldap_connection(cls, user_connection, metadata=None):
+        if cls.global_enabled:
+            user_connection.authz.enabled = True
+            user_connection.metadata = metadata
+        return user_connection
+
+    @classmethod
+    def get_authz_connection(cls, lo):
+        if cls.global_enabled:
+            return cls.get_privileged_connection()
+        return lo
+
+    @property
+    def lo(self):
+        return self.__class__.get_privileged_connection()
+
+    def __init__(self):
+        self.enabled = False
+
+    def _user_roles(self, lo):
+        actor_dn = lo.binddn
+        if self._cache_user_roles.get(actor_dn) is None:
+            self._cache_user_roles[actor_dn] = get_user_roles(self.lo, actor_dn)
+        return lambda: self._cache_user_roles[actor_dn]
+
+    def is_receive_allowed(self, obj, raise_exception=True):
+        if not self.enabled:
+            return True
+        try:
+            may_read(obj, self._user_roles(obj.lo))
+        except permissionDenied:
+            if not raise_exception:
+                return False
+            raise
+        return True
+
+    def filter_search_results(self, lo, results, options=None):
+        if not self.enabled:
+            return results
+
+        # FIXME: remove this performance intensive search!!!
+        options = options or {}
+        result_is_dn = options.pop('result-is-ldap-dn', None)
+        if result_is_dn:
+            options['result-is-udm'] = True
+            results = [univention.admin.objects.get_object(lo, dn) for dn in results]
+
+        data = may_read(results, self._user_roles(lo), filter_options=options)
+
+        if result_is_dn:
+            data = [obj.dn for obj in data]
+
+        return data
+
+    def is_create_allowed(self, obj, raise_exception=True):
+        if not self.enabled:
+            return True
+        if not may_create(obj, self._user_roles(obj.lo)):
+            if raise_exception:
+                raise permissionDenied()
+            return False
+        return True
+
+    def is_modify_allowed(self, obj, raise_exception=True):
+        if not self.enabled:
+            return True
+        if not may_modify(obj, self._user_roles(obj.lo)):
+            if raise_exception:
+                raise permissionDenied()
+            return False
+        return True
+
+    def is_rename_allowed(self, *args, **kwargs):
+        if not self.enabled:
+            return True
+        return True  # TODO: implement ?
+
+    def is_move_allowed(self, obj, dest, raise_exception=True):
+        if not self.enabled:
+            return True
+        moved_obj = copy.deepcopy(obj)
+        moved_obj.dn = dest
+        if not may_move(obj, moved_obj, self._user_roles(obj.lo)):
+            if raise_exception:
+                raise permissionDenied()
+            return False
+        return True
+
+    def is_remove_allowed(self, obj, raise_exception=True):
+        if not self.enabled:
+            return True
+        if not may_delete(obj, self._user_roles(obj.lo)):
+            if raise_exception:
+                raise permissionDenied()
+            return False
+        return True
+
+    def object_exists(self, obj):
+        if not self.is_receive_allowed(obj, raise_exception=False):
+            raise univention.admin.uexceptions.noObject(obj.dn)

management/univention-directory-manager-modules/modules/univention/admin/blocklist.py
@@ -63,7 +63,7 @@ def get_blocklist_config(lo):
     # type: (univention.admin.uldap.access) -> dict
     config = {}
     try:
-        for blist in univention.admin.handlers.blocklists.list.lookup(None, lo, 'entryUUID=*', base=BLOCKLIST_BASE, scope='one'):
+        for blist in univention.admin.handlers.blocklists.list.lookup(None, lo, 'entryUUID=*', base=BLOCKLIST_BASE, scope='one', authz=False):
             config[blist.dn] = blist.get('retentionTime', '30d')
             for mod, prop in blist.get('blockingProperties', []):
                 config.setdefault(mod, {})[prop] = blist.dn

commit 9fd2517f8e7eb0e324f816feb2614a2fde723447

fix(udm): LDAP_Search dropdown lists store the dn of the last list entry for all entries
Use obj.dn instead of stale loop variable in LDAP_Search._get_choices

Fixes: dc5a075e1e25b343ad698916f46b32c6b9fe4fa2

Issue: univention/dev/internal/dev-issues/dev-incidents#214
Bug #59104

management/univention-directory-manager-modules/modules/univention/admin/syntax.py
@@ -6149,23 +6149,23 @@ class LDAP_Search(select):
             obj.open()
             module = univention.admin.modules.get(obj.module)
             # find the value to store
-            id_ = dn
+            id_ = obj.dn
             if not cls.viewonly:
                 _mod_store, store = split_module_attr(store_pattern)
                 if store == 'dn':
-                    id_ = dn
+                    id_ = obj.dn
                 elif store in obj:
                     id_ = obj[store]
                 elif obj.oldattr.get(store):
                     id_ = obj.oldattr[store][0].decode(*module.mapping.getEncoding(store))
                 else:
                     # no valid store object, ignore
-                    log.warning('invalid property for LDAP_Seearch syntax - ignoring entry', syntax=cls.name, dn=dn, property=store)
+                    log.warning('invalid property for LDAP_Seearch syntax - ignoring entry', syntax=cls.name, dn=obj.dn, property=store)
                     continue
 
             # find the value to display
             if display == 'dn':
-                label = _normalize_dn(dn)
+                label = _normalize_dn(obj.dn)
             elif display is None:  # if view-only and in case of error
                 label = '%s: %s' % (getattr(module, 'short_description', module.module), obj.description())
             else:
@@ -6174,7 +6174,7 @@ class LDAP_Search(select):
                 elif obj.oldattr.get(display):
                     label = obj.oldattr[display][0].decode(*module.mapping.getEncoding(display))
                 else:
-                    log.warning('unknown label property for LDAP_Seearch syntax', syntax=cls.name, dn=dn, property=repr(display))
+                    log.warning('unknown label property for LDAP_Seearch syntax', syntax=cls.name, dn=obj.dn, property=repr(display))
                     label = 'Unknown attribute %r' % (display,)
 
             # TODO: remove this one day...