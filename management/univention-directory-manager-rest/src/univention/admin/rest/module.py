univention/univention-corporate-server

commit 7b4d79ec409ac6f7696553d07176cc4e66269065

refactor(udm-rest): split authentication from authorization
Bug #59176

management/univention-directory-manager-rest/src/univention/admin/rest/module.py
@@ -156,6 +156,7 @@ class ResourceBase(SanitizerBase, HAL, HTML):
         try:
             if authorization:
                 self.parse_authorization(authorization)
+                self.authorize(authorization)
         finally:
             self.request.content_negotiation_lang = self.check_acceptable()
             self.decode_request_arguments()
@@ -210,10 +211,10 @@ class ResourceBase(SanitizerBase, HAL, HTML):
             self.request.username = username
             self._set_request_context()
 
-        if not already_authenticated:
-            self._auth_check_allowed_groups()
-
         shared_memory.authenticated[authorization] = (auth_type, username, userdn, password)
+
+    def get_authorization_policy(self):
+        return ('default', [value for key, value in ucr.items() if key.startswith('directory/manager/rest/authorized-groups/')])
 
     @property
     def ldap_write_connection(self):
@@ -228,13 +229,26 @@ class ResourceBase(SanitizerBase, HAL, HTML):
             return conn
         return udm_auth.Authorization.inject_ldap_connection(conn)
 
-    def _auth_check_allowed_groups(self):
+    def authorize(self, authorization):
+        policy_id, allowed_groups = self.get_authorization_policy()
+        cache_key = (self.request.user_dn, policy_id)
+        if cache_key in shared_memory.authorized:
+            return
+        self._auth_check_allowed_groups(allowed_groups)
+        shared_memory.authorized[cache_key] = True
+
+    def _auth_check_allowed_groups(self, allowed_groups):
         if self.request.username in ('cn=admin',):
             return
-        allowed_groups = [value for key, value in ucr.items() if key.startswith('directory/manager/rest/authorized-groups/')]
-        memberof = self.ldap_connection.authz_connection.getAttr(self.request.user_dn, 'memberOf')
-        if not set(_map_normalized_dn(memberof)) & set(_map_normalized_dn(allowed_groups)):
-            raise HTTPError(403, 'Not in allowed groups.')
+        if not allowed_groups:
+            return HTTPError(403, 'No allowed groups configured.')
+
+        authz_filter = '(|(%s))' % ')('.join(filter_format('memberOf=%s', [mem]) for mem in allowed_groups)
+        for user in self.ldap_connection.authz_connection.search(filter=authz_filter, base=self.request.user_dn, scope='base', attr=['1.1']):
+            assert self.ldap_connection.compare_dn(user[0], self.request.user_dn)
+            return
+
+        raise HTTPError(403, 'Not in allowed groups.')
 
     def _auth_get_userdn(self, username):
         if username in ('cn=admin',):

management/univention-directory-manager-rest/src/univention/admin/rest/shared_memory.py
@@ -17,6 +17,7 @@ class _SharedMemory(managers.SyncManager):
     queue = {}
     search_sessions = {}
     authenticated = {}
+    authorized = {}
 
     def start(self, *args, **kwargs):
         setproctitle(proctitle + '   # multiprocessing manager')
@@ -30,6 +31,7 @@ class _SharedMemory(managers.SyncManager):
         self.queue = self.dict()
         self.search_sessions = self.dict()
         self.authenticated = self.dict()
+        self.authorized = self.dict()
 
 
 shared_memory = _SharedMemory()

commit 0992bdde927c27baa603aecb994cd1d6d8b5b096

fix(udm-rest): abort authorization if authentication failed
force_authorization stopped the request via self.finish() instead of
raising an exception. Therefore the authorize() method was called, which
couldn't do anything as no user information were set.

Traceback (most recent call last):
  File "/usr/lib/python3/dist-packages/tornado/web.py", line 1712, in _execute
    result = self.prepare()
             ^^^^^^^^^^^^^^
  File "/usr/lib/python3/dist-packages/univention/admin/rest/module.py", line 163, in prepare
    self.authorize(authorization)
  File "/usr/lib/python3/dist-packages/univention/admin/rest/module.py", line 241, in authorize
    self._auth_check_allowed_groups(allowed_groups)
  File "/usr/lib/python3/dist-packages/univention/admin/rest/module.py", line 245, in _auth_check_allowed_groups
    if self.request.username in ('cn=admin',):
       ^^^^^^^^^^^^^^^^^^^^^
AttributeError: 'HTTPServerRequest' object has no attribute 'username'

Fixes: 7b4d79ec409ac6f7696553d07176cc4e66269065
Bug #59176

management/univention-directory-manager-rest/src/univention/admin/rest/module.py
@@ -125,7 +125,7 @@ class ResourceBase(SanitizerBase, HAL, HTML):
     def debug_mode_enabled(self):
         return ucr.is_true('directory/manager/rest/debug-mode-enabled')
 
-    def force_authorization(self, auth_type=None, error_information=None):
+    def force_authorization(self, auth_type=None, error_information=None) -> bool:
         self.add_header('WWW-Authenticate', 'Basic realm="Univention Directory Manager"')
         if auth_type == 'Bearer':
             if error_information:
@@ -134,6 +134,7 @@ class ResourceBase(SanitizerBase, HAL, HTML):
                 self.add_header('WWW-Authenticate', 'Bearer realm="Univention Directory Manager"')
         self.set_status(401)
         self.finish()  # TODO: add response body
+        return False
 
     def set_default_headers(self):
         self.set_header('Server', 'Univention/1.0')  # TODO:
@@ -155,17 +156,18 @@ class ResourceBase(SanitizerBase, HAL, HTML):
 
         authorization = self.request.headers.get('Authorization')
         if not authorization and self.requires_authentication:
-            return self.force_authorization(None)
+            self.force_authorization(None)
+            return
 
         try:
-            if authorization:
-                self.parse_authorization(authorization)
+            if authorization and self.authenticate(authorization):
                 self.authorize(authorization)
         finally:
             self.request.content_negotiation_lang = self.check_acceptable()
             self.decode_request_arguments()
 
-    def parse_authorization(self, authorization):
+    def authenticate(self, authorization: str) -> bool:
+        """Parse HTTP Authorization header and authenticates with it"""
         if authorization in shared_memory.authenticated:  # cache for the userdn, which eliminates a search / request
             auth_type, username, userdn, password = shared_memory.authenticated[authorization]
             already_authenticated = True
@@ -216,6 +218,7 @@ class ResourceBase(SanitizerBase, HAL, HTML):
             self._set_request_context()
 
         shared_memory.authenticated[authorization] = (auth_type, username, userdn, password)
+        return True
 
     def get_authorization_policy(self):
         return ('default', [value for key, value in ucr.items() if key.startswith('directory/manager/rest/authorized-groups/')])