5843003232e130b16aa3d2578cb4fc4da8a4c1b6
Felix Botner <botner@univention.de>
2026-04-10 16:42:38 +0200

    test(ucs-test-udm): Update test case
    Bug #52893

--- test/ucs-test/tests/59_udm/61_test_udm_users_modification_set_deactivation_and_locked.py
+++ test/ucs-test/tests/59_udm/61_test_udm_users_modification_set_deactivation_and_locked.py
@@ -6,86 +6,63 @@
 ## packages:
 ##   - univention-directory-manager-tools
 
-import random
-import subprocess
-import time
+import pytest
 
 import univention.admin.uldap
 
 
-disabled_states = ('1', '0')
-locked_states = ['0', '1']
-
-transitions_log = []
-
-
-def test_user_modification_set_deactivation_and_locked(udm, ldap_base):
+@pytest.mark.parametrize(
+    "disabled,locked", [
+        (True, False),
+        pytest.param(True, True, marks=pytest.mark.xfail(
+            reason="setting both, disabled and locked, not supported, see Bug #59178",
+            raises=AssertionError,
+            match="krb5KDCFlags: expected 131326 found [b'254']",
+        )),
+        (False, True),
+        pytest.param(False, False, marks=pytest.mark.xfail(
+            reason="setting both, disabled and locked, not supported, see Bug #59178",
+            raises=AssertionError,
+            match="krb5KDCFlags: expected 131326 found [b'254']",
+        )),
+    ],
+    ids=[
+        "disabled-not-locked",
+        "disabled-and-locked",
+        "not-disabled-locked",
+        "not-disabled-not-locked",
+    ],
+)
+def test_user_modification_set_deactivation_and_locked(udm, ldap_base, disabled, locked):
     """Test changing disabled and locked simultaneously"""
-    user_dn = None
-    locked_state2 = locked_states.pop(random.randint(0, len(locked_states) - 1))
-    while locked_states:
-        locked_state1 = locked_states.pop(random.randint(0, len(locked_states) - 1))
-        disabled_states_list = list(disabled_states)
-        disabled_state1 = disabled_states_list.pop(random.randint(0, len(disabled_states_list) - 1))
-        user_dn = modify_and_check(udm, ldap_base, user_dn, disabled_state1, locked_state1)
-        while disabled_states_list:
-            disabled_state2 = disabled_states_list.pop(random.randint(0, len(disabled_states_list) - 1))
-            modify_and_check(udm, ldap_base, user_dn, disabled_state2, locked_state2)
-            modify_and_check(udm, ldap_base, user_dn, disabled_state1, locked_state1)
-
-    transitions_log.pop(-1)
-    print_transitions()
+    # create with inital state
+    user_dn = modify_and_check(udm, ldap_base, None, disabled, locked)
+    # invert state
+    modify_and_check(udm, ldap_base, user_dn, not disabled, not locked)
+    # back to initial state
+    modify_and_check(udm, ldap_base, user_dn, disabled, locked)
 
 
-def print_transitions():
-    print('    disabled         | locked   -> disabled         | locked')
-    print('------------------------------------------------------------')
-    print('\n'.join(transitions_log))
-    print(f'{len(transitions_log)} transitions tested.\n')
-
-
-def modify_and_check(udm, ldap_base, dn, disabled_state, locked_state):
-    print(f'*** disabled_state={disabled_state!r} locked_state={locked_state!r}')
+def modify_and_check(udm, ldap_base: str, dn: str, disabled: bool, locked: bool):
+    print(f'*** disabled state={disabled!r} locked state={locked!r}')
     if dn:
-        locked = {}
-        if locked_state == '0':
-            locked['locked'] = '0'
-        udm.modify_object('users/user', dn=dn, disabled=disabled_state, **locked)
-        if locked_state == '1':
-            locktime = time.strftime("%Y%m%d%H%M%SZ", time.gmtime())
-            subprocess.call(['python3', '-m', 'univention.lib.account', 'lock', '--dn', dn, '--lock-time', locktime])
+        udm.modify_object('users/user', dn=dn, disabled=str(int(disabled)), locked=str(int(locked)))
     else:
-        dn, _username = udm.create_user(
-            position=f'cn=users,{ldap_base}',
-            disabled=disabled_state,
-        )
-        if locked_state == '1':
-            locktime = time.strftime("%Y%m%d%H%M%SZ", time.gmtime())
-            subprocess.call(['python3', '-m', 'univention.lib.account', 'lock', '--dn', dn, '--lock-time', locktime])
+        dn, _username = udm.create_user(position=f'cn=users,{ldap_base}', disabled=str(int(disabled)), locked=str(int(locked)))
 
     krb_state = 126
-    if disabled_state == '1':
+    if disabled:
         krb_state |= 1 << 7
-    if locked_state == '1':
+    if locked:
         krb_state |= 1 << 17
 
-    smb_disabled = disabled_state == '1' or 'windows' in disabled_state
-    smb_locked = locked_state in ('1')
-    if transitions_log:
-        transitions_log[-1] += f' -> {disabled_state:16} | {locked_state:8}'
-
     # length of whitespace in sambaAcctFlags varies. cannot use utils.verify_ldap_object() to test it
-
     lo, _pos = univention.admin.uldap.getMachineConnection(ldap_master=False)
     user = lo.get(dn)
-    print_transitions()
     assert user['krb5KDCFlags'] == [str(krb_state).encode()], 'krb5KDCFlags: expected {!r} found {!r}'.format(krb_state, user['krb5KDCFlags'])
-    assert not (smb_disabled and b'D' not in user['sambaAcctFlags'][0]), 'sambaAcctFlags: expected D in flags, found {!r}'.format(user['sambaAcctFlags'])
-    assert not ((smb_locked and not smb_disabled) and b'L' not in user['sambaAcctFlags'][0]), 'sambaAcctFlags: expected L in flags, found {!r}'.format(user['sambaAcctFlags'])
-    assert not ((smb_locked and smb_disabled) and b'L' in user['sambaAcctFlags'][0]), 'sambaAcctFlags: unexpected L in flags: {!r}'.format(user['sambaAcctFlags'])
-    assert not ((disabled_state == '1' or 'posix' in disabled_state) and user['shadowExpire'][0] != b'1'), 'shadowExpire: expected {!r} found {!r}'.format(['1'], user['shadowExpire'])
-    print('*** OK.')
-    if transitions_log:
-        transitions_log[-1] = f'OK: {transitions_log[-1]}'
-    transitions_log.append(f'{disabled_state:16} | {locked_state:8}')
+    assert not (disabled and b'D' not in user['sambaAcctFlags'][0]), 'sambaAcctFlags: expected D in flags, found {!r}'.format(user['sambaAcctFlags'])
+    assert not ((locked and not disabled) and b'L' not in user['sambaAcctFlags'][0]), 'sambaAcctFlags: expected L in flags, found {!r}'.format(user['sambaAcctFlags'])
+    assert not ((locked and disabled) and b'L' in user['sambaAcctFlags'][0]), 'sambaAcctFlags: unexpected L in flags: {!r}'.format(user['sambaAcctFlags'])
+    assert not (disabled and user['shadowExpire'][0] != b'1'), 'shadowExpire: expected {!r} found {!r}'.format(['1'], user['shadowExpire'])
+
     return dn
