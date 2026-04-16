1168c9bf0aa4ebab78c655b280c6b3fd740898e4
Florian Best <best@univention.de>
2026-04-16 10:07:40 +0200

    feat(fetchmail): adjust hook to new method
    Bug #59150

--- mail/univention-fetchmail/share/hooks.d/fetchmail.py
+++ mail/univention-fetchmail/share/hooks.d/fetchmail.py
@@ -3,126 +3,58 @@
 
 import json
 
-import univention.debug as ud
 from univention.admin.hook import AttributeHook, simpleHook
 
 
-def map_value(value):
+def map_value(value, encoding=()):
     ret = []
     for elem in value:
         entry = []
         for param in elem:
             entry.append(param if isinstance(param, str) else param.decode())
-        ret.append(json.dumps(entry).encode('UTF-8'))
+        ret.append(json.dumps(entry).encode(*encoding))
     return ret
 
 
-def unmap_value(value):
+def unmap_value(value, encoding=()):
     try:
         entries = [json.loads(v) for v in value]
     except ValueError:
         # try the previous format. This should only happen once as
         # the next time the values will be already json formatted (#56008).
-        entries = [[w.strip('"') for w in v.decode('UTF-8').split('";"')] for v in value]
+        entries = [[w.strip('"') for w in v.decode(*encoding).split('";"')] for v in value]
     return entries
 
 
-if not hasattr(AttributeHook, 'version'):
-    # TODO: remove in UCS 5.1 - Bug #56036
-    class AttributeHook(simpleHook):
-        """
-        Convenience Hook that essentially implements a mapping
-        between |UDM| and |LDAP| for your extended attributes.
-        Derive from this class, set :py:attr:`attribute_name` to the name of
-        the |UDM| attribute and implement :py:meth:`map_attribute_value_to_udm`
-        and :py:meth:`map_attribute_value_to_ldap`.
-        """
-
-        udm_attribute_name = None
-        ldap_attribute_name = None
-
+if AttributeHook.version < 3:
+    class FetchMailSingleHook(AttributeHook):
         version = 2
-
-        def hook_open(self, obj):
-            """
-            Open |UDM| object by loading value from |LDAP|.
-
-            :param obj: The |UDM| object instance.
-            """
-            ud.debug(ud.ADMIN, ud.INFO, 'admin.syntax.hook.AttributeHook: Mapping %s (LDAP) -> %s (UDM)' % (self.ldap_attribute_name, self.udm_attribute_name))
-            old_value = obj.oldattr.get(self.ldap_attribute_name, [])
-            new_value = self.map_attribute_value_to_udm(old_value)
-            ud.debug(ud.ADMIN, ud.INFO, 'admin.syntax.hook.AttributeHook: Setting UDM value from %r to %r' % (old_value, new_value))
-            obj[self.udm_attribute_name] = new_value
-
-        def hook_ldap_addlist(self, obj, al):
-            """
-            Extend |LDAP| add list.
-
-            :param obj: The |UDM| object instance.
-            :param al: The add list to extend.
-            :returns: The extended add list.
-            """
-            return self.hook_ldap_modlist(obj, al)
-
-        def hook_ldap_modlist(self, obj, ml):
-            """
-            Extend |LDAP| modification list.
-
-            :param obj: The |UDM| object instance.
-            :param ml: The modification list to extend.
-            :returns: The extended modification list.
-            """
-            new_ml = [x for x in ml if x[0] != self.ldap_attribute_name]
-
-            if obj.hasChanged(self.udm_attribute_name):
-                old_value = obj.oldattr.get(self.ldap_attribute_name, [])
-                new_value = obj.info.get(self.udm_attribute_name)
-                if new_value is not None:
-                    new_value = self.map_attribute_value_to_ldap(new_value)
-                new_ml.append((self.ldap_attribute_name, old_value, new_value))
-            return new_ml
+        udm_attribute_name = 'FetchMailSingle'
+        ldap_attribute_name = 'univentionFetchmailSingle'
 
         def map_attribute_value_to_ldap(self, value):
-            """
-            Return value as it shall be saved in |LDAP|.
-
-            :param value: The |UDM| value.
-            :returns: The |LDAP| value.
-            """
-            return value
+            return map_value(value)
 
         def map_attribute_value_to_udm(self, value):
-            """
-            Return value as it shall be used in |UDM| objects.
+            return unmap_value(value)
 
-            The mapped value needs to be syntax compliant.
+    class FetchMailMultiHook(AttributeHook):
+        version = 2
+        udm_attribute_name = 'FetchMailMulti'
+        ldap_attribute_name = 'univentionFetchmailMulti'
 
-            :param value: The |LDAP| value.
-            :returns: The |UDM| value.
-            """
-            return value
+        def map_attribute_value_to_ldap(self, value):
+            return map_value(value)
 
+        def map_attribute_value_to_udm(self, value):
+            return unmap_value(value)
+else:
+    class FetchMailSingleHook(simpleHook):
 
-class FetchMailSingleHook(AttributeHook):
-    version = 2
-    udm_attribute_name = 'FetchMailSingle'
-    ldap_attribute_name = 'univentionFetchmailSingle'
+        map = staticmethod(map_value)
+        unmap = staticmethod(unmap_value)
 
-    def map_attribute_value_to_ldap(self, value):
-        return map_value(value)
+    class FetchMailMultiHook(simpleHook):
 
-    def map_attribute_value_to_udm(self, value):
-        return unmap_value(value)
-
-
-class FetchMailMultiHook(AttributeHook):
-    version = 2
-    udm_attribute_name = 'FetchMailMulti'
-    ldap_attribute_name = 'univentionFetchmailMulti'
-
-    def map_attribute_value_to_ldap(self, value):
-        return map_value(value)
-
-    def map_attribute_value_to_udm(self, value):
-        return unmap_value(value)
+        map = staticmethod(map_value)
+        unmap = staticmethod(unmap_value)
