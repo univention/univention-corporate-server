univention/univention-corporate-server

commit 7b4d79ec409ac6f7696553d07176cc4e66269065

refactor(udm-rest): split authentication from authorization
Bug #59176

management/univention-directory-manager-rest/src/univention/admin/rest/module.py
@@ -156,6 +156,7 @@ class ResourceBase(SanitizerBase, HAL, HTML):
         try:
             if authorization:
                 self.parse_authorization(authorization)
+                self.authorize(authorization)
         finally:
             self.request.content_negotiation_lang = self.check_acceptable()
             self.decode_request_arguments()
@@ -210,10 +211,10 @@ class ResourceBase(SanitizerBase, HAL, HTML):
             self.request.username = username
             self._set_request_context()
 
-        if not already_authenticated:
-            self._auth_check_allowed_groups()
-
         shared_memory.authenticated[authorization] = (auth_type, username, userdn, password)
+
+    def get_authorization_policy(self):
+        return ('default', [value for key, value in ucr.items() if key.startswith('directory/manager/rest/authorized-groups/')])
 
     @property
     def ldap_write_connection(self):
@@ -228,13 +229,26 @@ class ResourceBase(SanitizerBase, HAL, HTML):
             return conn
         return udm_auth.Authorization.inject_ldap_connection(conn)
 
-    def _auth_check_allowed_groups(self):
+    def authorize(self, authorization):
+        policy_id, allowed_groups = self.get_authorization_policy()
+        cache_key = (self.request.user_dn, policy_id)
+        if cache_key in shared_memory.authorized:
+            return
+        self._auth_check_allowed_groups(allowed_groups)
+        shared_memory.authorized[cache_key] = True
+
+    def _auth_check_allowed_groups(self, allowed_groups):
         if self.request.username in ('cn=admin',):
             return
-        allowed_groups = [value for key, value in ucr.items() if key.startswith('directory/manager/rest/authorized-groups/')]
-        memberof = self.ldap_connection.authz_connection.getAttr(self.request.user_dn, 'memberOf')
-        if not set(_map_normalized_dn(memberof)) & set(_map_normalized_dn(allowed_groups)):
-            raise HTTPError(403, 'Not in allowed groups.')
+        if not allowed_groups:
+            return HTTPError(403, 'No allowed groups configured.')
+
+        authz_filter = '(|(%s))' % ')('.join(filter_format('memberOf=%s', [mem]) for mem in allowed_groups)
+        for user in self.ldap_connection.authz_connection.search(filter=authz_filter, base=self.request.user_dn, scope='base', attr=['1.1']):
+            assert self.ldap_connection.compare_dn(user[0], self.request.user_dn)
+            return
+
+        raise HTTPError(403, 'Not in allowed groups.')
 
     def _auth_get_userdn(self, username):
         if username in ('cn=admin',):

management/univention-directory-manager-rest/src/univention/admin/rest/shared_memory.py
@@ -17,6 +17,7 @@ class _SharedMemory(managers.SyncManager):
     queue = {}
     search_sessions = {}
     authenticated = {}
+    authorized = {}
 
     def start(self, *args, **kwargs):
         setproctitle(proctitle + '   # multiprocessing manager')
@@ -30,6 +31,7 @@ class _SharedMemory(managers.SyncManager):
         self.queue = self.dict()
         self.search_sessions = self.dict()
         self.authenticated = self.dict()
+        self.authorized = self.dict()
 
 
 shared_memory = _SharedMemory()

commit 1fdb6f4f30283f00cdcd7e49a5b1da983f63aff6

fix(udm-rest-api): Fix authorization via OIDC access token
The DN-equality assert in _auth_check_allowed_groups compared DNs
case-sensitively and raised HTTP 500 for authorized users authenticated
via OIDC access token, where the DN is mapped to lowercase.
Drop the redundant assert.

Also fix `return HTTPError(...)` -> `raise` in the empty-groups branch.

Fixes: 7b4d79ec409ac6f7696553d07176cc4e66269065

Bug #59215
Issue: univention/dev/internal/team-nubus#1612

doc/errata/staging/univention-directory-manager-rest.yaml
@@ -1,0 +1,12 @@
+product: ucs
+release: "5.2"
+version: [5]
+scope: ucs_5.2-0-errata5.2-5
+src: univention-directory-manager-rest
+fix: 12.5.7
+desc: |
+ This update addresses the following issue:
+ * Fixed a regression from erratum 410 for client authenticated via OIDC where
+   the DN of authenticated users caused a case sensitive DN comparision to
+   fail.
+bug: [59176]

management/univention-directory-manager-rest/src/univention/admin/rest/module.py
@@ -248,11 +248,10 @@ class ResourceBase(SanitizerBase, HAL, HTML):
         if self.request.username in ('cn=admin',):
             return
         if not allowed_groups:
-            return HTTPError(403, 'No allowed groups configured.')
+            raise HTTPError(403, 'No allowed groups configured.')
 
         authz_filter = '(|(%s))' % ')('.join(filter_format('memberOf=%s', [mem]) for mem in allowed_groups)
         for user in self.ldap_connection.authz_connection.search(filter=authz_filter, base=self.request.user_dn, scope='base', attr=['1.1']):
-            assert self.ldap_connection.compare_dn(user[0], self.request.user_dn)
             return
 
         raise HTTPError(403, 'Not in allowed groups.')