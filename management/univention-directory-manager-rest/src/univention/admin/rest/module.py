univention/univention-corporate-server

commit 5e2a5da58b664cbb55dd03bf30a2ea2b3b9d6c00

feat(udm-rest): hide pre-calculated univentionObjectIdentifier in object template
The template might be used for multiple objects to be created. If so, it
has to be edited manually, otherwise one get a error when creating
another object with the same UOID as before.

curl -s http://Administrator:univention@localhost/univention/udm/users/user/add -H 'Accept: application/*' | jq | grep univentionObjectIdentifier
    "univentionObjectIdentifier": null,

Bug #59217

doc/errata/staging/univention-directory-manager-rest.yaml
@@ -3,13 +3,14 @@ release: "5.2"
 version: [5]
 scope: ucs_5.2-0-errata5.2-5
 src: univention-directory-manager-rest
-fix: 12.5.9
+fix: 12.5.10
 desc: |
  This update addresses the following issues:
- * The Nubus Prometheus metrics are now consistently prefixed with
-   `nubus_`, ensuring clearer namespace separation and easier identification
-   in monitoring and alerting configurations. The Nubus Prometheus metrics
-   `nubus_users_user_total` and `nubus_settings_license_users_limit_total`
-   now include the license key as a label, enabling per-license
-   observability.
-bug: [59231]
+ * The Nubus Prometheus metrics are now consistently prefixed with `nubus_`,
+   ensuring clearer namespace separation and easier identification in
+   monitoring and alerting configurations. The Nubus Prometheus metrics
+   `nubus_users_user_total` and `nubus_settings_license_users_limit_total` now
+   include the license key as a label, enabling per-license observability.
+ * A pre-calculated value for `univentionObjectIdentifier` is now hidden in
+   the object template, as this might be used to create multiple objects.
+bug: [59231, 59217]

management/univention-directory-manager-rest/src/univention/admin/rest/module.py
@@ -2610,6 +2610,7 @@ class ObjectAdd(FormBase, _OpenAPIBase, Resource):
         obj = module.module.object(dn, self.ldap_connection, ldap_position, superordinate=superordinate)
         obj.open()
         result.update(Object.get_representation(module, obj, ['*'], self.ldap_connection, copy, True))
+        result['properties']['univentionObjectIdentifier'] = None
 
         form = self.add_form(result, action=self.urljoin(''), method='POST', id='add', layout='create-form')
         self.add_form_element(form, 'position', position or '')

commit 5dd2dee979f661c3a59edaeaee89553316f7377f

fix(udm-rest): only add univentionObjectIdentifier in object template if original module provides it
otherwise it exists for blocklists/entry, which will fail to create
then.

curl -s http://Administrator:univention@localhost/univention/udm/blocklists/entry/add -H 'Accept: application/*' | jq | grep univentionObjectIdentifier
  "univentionObjectIdentifier": null,

Bug #59217

Fixes: 5e2a5da58b664cbb55dd03bf30a2ea2b3b9d6c00

doc/errata/staging/univention-directory-manager-rest.yaml
@@ -3,7 +3,7 @@ release: "5.2"
 version: [5]
 scope: ucs_5.2-0-errata5.2-5
 src: univention-directory-manager-rest
-fix: 12.5.10
+fix: 12.5.11
 desc: |
  This update addresses the following issues:
  * The Nubus Prometheus metrics are now consistently prefixed with `nubus_`,

management/univention-directory-manager-rest/src/univention/admin/rest/module.py
@@ -1900,7 +1900,7 @@ class Object(ConditionalResource, FormBase, _OpenAPIBase, Resource):
         props = {}
         props.update(self._options(object_type, obj.dn))
         props['uri'] = self.abspath(obj.module, quote_dn(obj.dn))
-        props.update(self.get_representation(module, obj, properties, self.ldap_connection, copy))
+        props.update(self.get_representation(module, obj, properties, self.ldap_connection, copy=copy))
         for reference in module.get_references(obj):
             # TODO: add a reference for the "position" object?!
             if reference['module'] != 'udm':
@@ -2136,7 +2136,7 @@ class Object(ConditionalResource, FormBase, _OpenAPIBase, Resource):
         self.set_entity_tags(obj)
 
         if not patch_document:
-            entry = Object.get_representation(module, obj, ['*'], self.ldap_write_connection, False)
+            entry = Object.get_representation(module, obj, ['*'], self.ldap_write_connection)
             if representation['options'] is None:
                 representation['options'] = entry['options']
             if representation['policies'] is None:
@@ -2611,8 +2611,9 @@ class ObjectAdd(FormBase, _OpenAPIBase, Resource):
 
         obj = module.module.object(dn, self.ldap_connection, ldap_position, superordinate=superordinate)
         obj.open()
-        result.update(Object.get_representation(module, obj, ['*'], self.ldap_connection, copy, True))
-        result['properties']['univentionObjectIdentifier'] = None
+        result.update(Object.get_representation(module, obj, ['*'], self.ldap_connection, copy=copy, add=True))
+        if 'univentionObjectIdentifier' in result['properties']:
+            result['properties']['univentionObjectIdentifier'] = None
 
         form = self.add_form(result, action=self.urljoin(''), method='POST', id='add', layout='create-form')
         self.add_form_element(form, 'position', position or '')