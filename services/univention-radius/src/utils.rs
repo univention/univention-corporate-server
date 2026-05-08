9e9ca1fea758e7e303cef01d820be216996a8adf
Florian Best <best@univention.de>
2026-05-08 23:13:18 +0200

    test(radius): migrate the pytest to rust unittests
    Bug #59042

--- services/univention-radius/src/utils.rs
+++ services/univention-radius/src/utils.rs
@@ -22,3 +22,47 @@ pub fn parse_username(username: &str) -> String {
     let without_domain = without_prefix.split('.').next().unwrap_or(without_prefix);
     format!("{}$", without_domain)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod utils_tests {
+        use crate::utils::{decode_station_id, parse_username};
+
+        #[test]
+        fn test_mac_colon() {
+            assert_eq!(decode_station_id("00:11:22:33:44:55"), "00:11:22:33:44:55");
+        }
+
+        #[test]
+        fn test_mac_dash() {
+            assert_eq!(decode_station_id("00-11-22-33-44-55"), "00:11:22:33:44:55");
+        }
+
+        #[test]
+        fn test_mac_dot() {
+            assert_eq!(decode_station_id("0011.2233.4455"), "00:11:22:33:44:55");
+        }
+
+        #[test]
+        fn test_mac_plain() {
+            assert_eq!(decode_station_id("001122334455"), "00:11:22:33:44:55");
+        }
+
+        #[test]
+        fn test_mac_empty() {
+            assert_eq!(decode_station_id(""), "");
+        }
+
+        #[test]
+        fn test_username_plain() {
+            assert_eq!(parse_username("user"), "user");
+        }
+
+        #[test]
+        fn test_username_host() {
+            assert_eq!(parse_username("host/foo.bar"), "foo$");
+        }
+    }
+}
