univention/univention-corporate-server

commit 1976532de0f3cdecd6c6e40b0090b1c140504779

feat(umc-oidc): federated account for OIDC auth
Handle federated accounts in UMC (OIDC)

Bug #58652

An federated accounts is indicated by the claims:
* mandatory "nubus_federated_account=True"
* mandatory "nubus_id" - univentionObjectIdentifier for the UDM
  federated account object
* optional "preferred_username" - display name in UMC
* optional "nubus_roles" - guardian roles for authz

If UMC detects an federated account
* creates an federated account with the "nubus_id" as
  univentionObjectIdentifier to verify uniquness of this ID in the
  current database and to block this value for the future
* no PAM authentication
* provides "nubus_roles" to module processes

Issue univention/dev/ucs#3003

management/univention-management-console/src/univention/management/console/resources.py
@@ -751,6 +751,10 @@ class Command(Resource):
         # headers['X-UMC-Flavor'] = None
         # X-UMC-IP=self.get_ip_address() ?
         headers['Authorization'] = 'basic ' + base64.b64encode(('%s:%s' % (session.user.username, session.get_umc_password())).encode('ISO8859-1')).decode('ASCII')
+        if session.user.roles is not None:
+            headers['X-UMC-Roles'] = json.dumps(session.user.roles)
+        if session.user.federated_account:
+            headers['X-UMC-Federated-Account'] = json.dumps(True)
         headers['X-UMC-Method'] = methodname
         headers['X-UMC-Command'] = umcp_command.upper()
         headers['X-UMC-Request-ID'] = self._request_id
@@ -1014,6 +1018,8 @@ class UserPreferences(Resource):
         return self.get()
 
     def _get_user_preferences(self, lo):
+        if self.current_user.user.federated_account:
+            return {}
         user_dn = self.current_user.user.user_dn
         if not user_dn or not lo:
             return {}
@@ -1036,6 +1042,8 @@ class SetUserPreferences(UserPreferences):
         "preferences": DictSanitizer({}, required=True),
     }))
     async def post(self):
+        if self.current_user.user.federated_account:
+            return
         lo = self.current_user.get_user_ldap_connection()
         # eliminate double entries
         preferences = self._get_user_preferences(lo)

management/univention-management-console/src/univention/management/console/message.py
@@ -147,6 +147,8 @@ class Request(Message):
         self.auth_type = None
         self.locale = None
         self._request_handler = None
+        self.roles = None
+        self.federated_account = False
 
     def require_password(self):
         if self.auth_type is not None:
@@ -165,7 +167,7 @@ class Request(Message):
             CORE.warning('Failed to open LDAP connection for user %s: %s', self.user_dn, exc)
 
     def bind_user_connection(self, lo):
-        CORE.process('LDAP bind for user %r.', self.user_dn)
+        CORE.process('LDAP bind', dn=self.user_dn, username=self.username)
         try:
             if self.auth_type == 'OIDC':
                 lo.lo.bind_oauthbearer(None, self.password)

management/univention-management-console/src/univention/management/console/modserver.py
@@ -316,6 +316,8 @@ class Handler(RequestHandler):
             # 'requester_ip': '',
             # 'requester_hostname': '',
         })
+        msg.roles = json.loads(self.request.headers.get('X-UMC-Roles', '[]'))
+        msg.federated_account = bool(self.request.headers.get('X-UMC-Federated-Account'))
         msg.username = username
         msg.user_dn = user_dn
         msg.password = password
@@ -348,9 +350,9 @@ class Handler(RequestHandler):
         if self.handler:
             last_request = self.handler._current_request
             if not last_request or last_request.user_dn != user_dn:
-                MODULE.process('Setting user LDAP DN: %r', user_dn)
+                MODULE.process('Setting user LDAP DN', dn=user_dn, username=username)
             if not last_request or last_request.auth_type != auth_type:
-                MODULE.process('Setting auth type: %r', auth_type)
+                MODULE.process('Setting auth type', auth_type=auth_type)
             self.handler._current_request = msg
 
         method = self.request.headers['X-UMC-Method']  # TODO: error handling if unset

management/univention-management-console/src/univention/management/console/oidc.py
@@ -6,6 +6,7 @@
 # SPDX-FileCopyrightText: 2022-2025 Univention GmbH
 # SPDX-License-Identifier: AGPL-3.0-only
 
+import asyncio
 import base64
 import binascii
 import hashlib
@@ -24,7 +25,10 @@ from tornado.auth import OAuth2Mixin
 from tornado.httpclient import HTTPClientError, HTTPRequest
 
 from univention.management.console.config import ucr
-from univention.management.console.error import BadRequest, NotFound, OpenIDProvideUnavailable, UMC_Error, Unauthorized
+from univention.management.console.error import (
+    BadRequest, Forbidden, NotFound, OpenIDProvideUnavailable, UMC_Error, Unauthorized,
+)
+from univention.management.console.ldap import get_machine_connection
 from univention.management.console.log import CORE
 from univention.management.console.resource import Resource
 from univention.management.console.session import Session
@@ -32,18 +36,49 @@ from univention.management.console.session_db import DBDisabledException, get_se
 from univention.management.console.shared_memory import shared_memory
 
 
+def create_federated_account(uuid):
+    import ldap
+
+    from univention.admin import modules, objects
+    from univention.admin.uexceptions import noObject
+
+    lo, position = get_machine_connection(write=True)
+    ldap_base = ucr['ldap/base']
+    federated_accounts = modules.get('users/federated_account')
+    modules.init(lo, position, federated_accounts)
+    position_string = f'cn=federated_accounts,cn=univention,{ldap_base}'
+    try:
+        dn = f'univentionObjectIdentifier={ldap.dn.escape_dn_chars(uuid)},{position_string}'
+        account = objects.get(federated_accounts, None, lo, position, dn=dn, authz=False)
+    except noObject:
+        account = None
+
+    if not account:
+        position.setDn(position_string)
+        account = federated_accounts.object(None, lo, position)
+        account.open()
+        account['univentionObjectIdentifier'] = uuid
+        account.create()
+    CORE.process('Created federated account', dn=account.dn)
+
+
 class OIDCUser:
     """OIDC tokens of the authenticated user."""
 
-    __slots__ = ('access_token', 'claims', 'id_token', 'refresh_token', 'session_refresh_future', 'username')
+    __slots__ = ('access_token', 'claims', 'federated_account', 'id_token', 'refresh_token', 'roles', 'session_refresh_future', 'username', 'uuid')
 
     def __init__(self, id_token, access_token, refresh_token, claims):
         self.id_token = id_token
         self.access_token = access_token
         self.refresh_token = refresh_token
         self.claims = claims
+        self.roles = None
+        self.uuid = None
+        self.federated_account = False
         self.username = claims['uid']
         self.session_refresh_future = None
+        if claims.get('nubus_federated_account', False):
+            self.federated_account = True
 
     @property
     def session_end_time(self):
@@ -137,22 +172,41 @@ class OIDCResource(OAuth2Mixin, Resource):
         CORE.log(1, 'Refresh token: %s', refresh_token)
         claims = self.verify_id_token(id_token, nonce)
         oidc = OIDCUser(id_token, access_token, refresh_token, claims)
+        if oidc.federated_account:
+            await self.handle_federated_account(oidc)
         await self.pam_oidc_authentication(oidc)
+
+    async def handle_federated_account(self, oidc):
+        from .server import pool
+        info = await self.get_user_information(oidc.access_token)
+        oidc.uuid = info.get('nubus_id')
+        if not oidc.uuid:
+            CORE.error('TOKEN CLAIM ERROR: Claim nubus_id is missing for this account!')
+            raise Forbidden('Login is not possible with this account! Please contact your adminitrator.')
+        oidc.roles = info.get('nubus_roles', [])
+        oidc.username = info.get('nubus_external_username') or oidc.uuid
+        # TODO: Move to keycloak ad-hoc federation
+        future = pool.submit(create_federated_account, oidc.uuid)
+        await asyncio.wrap_future(future)
+        CORE.process('OIDC login federated user %r with roles %r', oidc.uuid, oidc.roles)
 
     async def pam_oidc_authentication(self, oidc):
         # important: must be called before the auth, to preserve session id in case of re-auth and that a user cannot choose his own session ID by providing a cookie
         sessionid = self.create_sessionid()
 
-        # TODO: drop in the future to gain performance
-        result = await self.current_user.authenticate({
-            'locale': self.locale.code,
-            'username': oidc.username,
-            'password': oidc.access_token,
-            'auth_type': 'OIDC',
-        })
-        if not self.current_user.user.authenticated:
-            CORE.error('SECURITY WARNING: PAM OIDC Authentication failed while JWT verification succeeded!')
-            raise UMC_Error(result.message, result.status, result.result)
+        if not oidc.federated_account:
+            # TODO: drop in the future to gain performance
+            result = await self.current_user.authenticate({
+                'locale': self.locale.code,
+                'username': oidc.username,
+                'password': oidc.access_token,
+                'auth_type': 'OIDC',
+            })
+            if not self.current_user.user.authenticated:
+                CORE.error('SECURITY WARNING: PAM OIDC Authentication failed while JWT verification succeeded!')
+                raise UMC_Error(result.message, result.status, result.result)
+        else:
+            self.current_user.set_credentials(oidc.username, oidc.access_token, 'OIDC', object_id=oidc.uuid, roles=oidc.roles, federated_account=oidc.federated_account)
 
         # as an alternative to PAM we could just set the user as authenticated because jwt.decode() already ensured this.
         # but we keep the behavior for now because this is what happened prior to the UMC-Web-Server and UMC-Sever unification
@@ -315,6 +369,7 @@ class OIDCResource(OAuth2Mixin, Resource):
             )
         except HTTPClientError:
             raise  # handled in get()
+
         return escape.json_decode(response.body)
 
     async def refresh_session_tokens(self, user):
@@ -350,6 +405,8 @@ class OIDCResource(OAuth2Mixin, Resource):
         # TODO: do we need to re-authenticate?
         claims = self.verify_id_token(id_token, None)
         oidc = OIDCUser(id_token, access_token, refresh_token, claims)
+        if oidc.federated_account:
+            await self.handle_federated_account(oidc)
         await self.pam_oidc_authentication(oidc)
 
     def _logout_success(self):

management/univention-management-console/src/univention/management/console/session.py
@@ -42,7 +42,7 @@ _session_timeout = ucr.get_int('umc/http/session/timeout', 300)
 class User:
     """Information about the authenticated user"""
 
-    __slots__ = ('_locale', 'auth_type', 'authenticated', 'ip', 'password', 'session_end_time', 'user_dn', 'username')
+    __slots__ = ('_locale', 'auth_type', 'authenticated', 'federated_account', 'ip', 'object_id', 'password', 'roles', 'session_end_time', 'user_dn', 'username')
 
     def __init__(self):
         self.ip = None
@@ -51,8 +51,11 @@ class User:
         self.password = None
         self.auth_type = None
         self.user_dn = None
+        self.object_id = None
         self.session_end_time = None
         self._locale = None  # don't use!
+        self.roles = None
+        self.federated_account = False
 
     def __repr__(self):
         return '<User(authenticated=%r name=%r ip=%r dn=%r auth_type=%r session_end_time=%r password="keep dreaming :-P")>' % (self.authenticated, self.username, self.ip, self.user_dn, self.auth_type, self.session_end_time)
@@ -139,10 +142,14 @@ class Session:
         pam.end()
         self.set_credentials(username, new_password, None)
 
-    def set_credentials(self, username, password, auth_type):
+    def set_credentials(self, username, password, auth_type, object_id=None, roles=None, federated_account=False):
         self.user.authenticated = True
         self.user.username = username
+        self.user.object_id = object_id
         self.user.auth_type = auth_type
+        self.user.federated_account = federated_account
+        if roles is not None:
+            self.user.roles = roles
         if self.user.auth_type is None:
             # important! there might be a password already. in case of SAML we must not set/overwrite the password.
             self.user.password = password
@@ -158,10 +165,17 @@ class Session:
 
     def _search_user_dn(self):
         lo = get_machine_connection(write=False)[0]
-        if lo and self.user.username:
+        if self.user.federated_account:
+            username = self.user.object_id
+            s_filter = filter_format('(&(univentionObjectIdentifier=%s)(objectClass=univentionFederatedAccount))', (self.user.object_id,))
+        else:
+            username = self.user.username
+            s_filter = filter_format('(&(uid=%s)(objectClass=person))', (username,))
+
+        if lo and username:
             # get the LDAP DN of the authorized user
             try:
-                ldap_dn = lo.searchDn(filter_format('(&(uid=%s)(objectClass=person))', (self.user.username,)))
+                ldap_dn = lo.searchDn(s_filter)
             except (ldap.LDAPError, udm_errors.base):
                 reset_ldap_connection_cache(lo)
                 ldap_dn = None
@@ -305,6 +319,10 @@ class IACLs:
             # We need to set empty ACL's for unauthenticated requests
             return ACLs()
         else:
+            if sess.user.federated_account:
+                # TODO: return UMC_ACLS_FROM GUARDIAN
+                # return ROLES_ACLS(...)
+                pass
             return LDAP_ACLs(sess.user.username, sess.user.user_dn, ucr['ldap/base'])
 
     def is_command_allowed(self, command, options, flavor):

commit e277419d0e90d74328b11b735e4b1aee65556e6c

fix(umc): prevent request header smuggling
`X-UMC-Federated-Account` and `X-UMC-Roles` could be smuggled in certain conditions:

`X-UMC-Federated-Account`: a boolean to indicate that the client is a users/federated_account.
`X-UMC-Roles`: The Guardian roles of this user, used by e.g. UDM module process to do authorization with.

Every user can define himself as a federated-account by adding a request header `X-UMC-Federated-Account: true`.
In case the user has no roles at all defined, he can just specifying them via `X-UMC-Roles: ["guardian::super-admin"]`.

Bug #59280

Fixes: 1976532de0f3cdecd6c6e40b0090b1c140504779

doc/errata/staging/univention-management-console.yaml
@@ -1,0 +1,13 @@
+product: ucs
+release: "5.2"
+version: [5]
+scope: ucs_5.2-0-errata5.2-5
+src: univention-management-console
+fix: 14.5.1
+desc: |
+ This update addresses the following issue:
+ * The UMC server allowed HTTP request header smuggling for the
+   `X-UMC-Federated-Account` and `X-UMC-Roles` values, which were used for
+   interprocess communication between UMC server and UMC module processes, if
+   delegative administration was enabled.
+bug: [59280]

management/univention-management-console/src/univention/management/console/resources.py
@@ -565,14 +565,14 @@ class Categories(Resource):
     def get(self):
         categoryManager.load()
         ucr.load()
-        _ucr_dict = dict(ucr.items())
+        ucr_dict = dict(ucr.items())
         categories = []
         for category in categoryManager.values():
             categories.append({
                 'id': category.id,
                 'icon': category.icon,
                 'color': category.color,
-                'name': self.i18n._(category.name, category.domain).format(**_ucr_dict),
+                'name': self.i18n._(category.name, category.domain).format(**ucr_dict),
                 'priority': category.priority,
             })
         CORE.debug('Categories: %s', categories)
@@ -740,9 +740,29 @@ class Command(Resource):
             self.finish()
 
     def get_request_header(self, session, methodname, umcp_command):
-        headers = dict(self.request.headers)
-        for header in ('Content-Length', 'Transfer-Encoding', 'Content-Encoding', 'Connection', 'X-Http-Reason', 'Range', 'Trailer', 'Server', 'Set-Cookie', 'X-UMC-AuthType'):
-            headers.pop(header, None)
+        headers = {
+            k: v for k, v in self.request.headers.items()
+            if k.lower() not in {
+                'content-length',
+                'transfer-encoding',
+                'content-encoding',
+                'connection',
+                'x-http-reason',
+                'range',
+                'trailer',
+                'server',
+                'set-cookie',
+                'x-user-dn',
+                'x-umc-authtype',
+                'x-umc-flavor',
+                'x-umc-roles',
+                'x-umc-federated-account',
+                'x-umc-method',
+                'x-umc-command',
+                'x-umc-request-id',
+            }
+        }
+
         headers['Cookie'] = '; '.join([m.OutputString(attrs=[]) for name, m in self.cookies.items() if not name.startswith('UMCUsername')])
         headers['X-User-Dn'] = json.dumps(session.user.user_dn)
         # headers['X-UMC-Flavor'] = None