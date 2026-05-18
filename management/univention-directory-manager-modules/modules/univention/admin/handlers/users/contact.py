univention/univention-corporate-server

commit a6781471c0bb154c98821a2207df6d38b8b234da

feat(udm): allow to set users/contact cn
Bug #59281

management/univention-directory-manager-modules/modules/univention/admin/handlers/users/contact.py
@@ -5,6 +5,7 @@
 
 from __future__ import annotations
 
+import sys
 from typing import Any
 
 import univention.admin
@@ -323,15 +324,34 @@ class object(univention.admin.handlers.simpleLdap):
     module = module
 
     def description(self) -> str:
-        description = '%s %s' % (self['firstname'] or '', self['lastname'])
+        return self._pattern(self)
+
+    @staticmethod
+    def _pattern(obj) -> str:
+        description = '%s %s' % (obj['firstname'] or '', obj['lastname'])
         return description.strip()
 
-    def get_candidate_dn(self) -> str:
-        dn = self._ldap_dn()
+    def _ldap_pre_ready(self) -> None:
+        super()._ldap_pre_ready()
+
         if self.exists():
-            rdn = self.lo.explodeDn(dn)[0]
-            dn = '%s,%s' % (rdn, self.lo.parentDn(self.dn))
-        return dn
+            if (
+                self.hasChanged(('firstname', 'lastname'))  # change of pattern candidates
+                and not self.hasChanged('cn')  # no explicit CN given
+                and self.oldinfo['cn'].startswith(f'{self._pattern(self.oldinfo)} ')  # former CN consists of the pattern
+            ):
+                self.acquire_unique_dn()
+
+        elif not self.info.get('cn'):  # no explicit CN given
+            self.acquire_unique_dn()
+
+    def acquire_unique_dn(self) -> str:
+        pattern = self._pattern(self)
+        for nonce in range(1, sys.maxsize):
+            self['cn'] = f'{pattern} {nonce}'
+            if self.unique_dn():
+                break
+        return self.get_candidate_dn()
 
     def unique_dn(self) -> bool:
         candidate_dn = self.get_candidate_dn()
@@ -342,21 +362,12 @@ class object(univention.admin.handlers.simpleLdap):
         else:
             return False
 
-    def acquire_unique_dn(self) -> str:
-        nonce = 1
-        cn = '%s %s %d' % (self['firstname'] or '', self['lastname'], nonce)
-        self['cn'] = cn.strip()
-        while not self.unique_dn():
-            nonce += 1
-            cn = '%s %s %d' % (self['firstname'] or '', self['lastname'], nonce)
-            self['cn'] = cn.strip()
-        return self.get_candidate_dn()
-
-    def _ldap_pre_ready(self) -> None:
-        super()._ldap_pre_ready()
-
-        if not self.exists() or self.hasChanged(('firstname', 'lastname')):
-            self.acquire_unique_dn()
+    def get_candidate_dn(self) -> str:
+        dn = self._ldap_dn()
+        if self.exists():
+            rdn = self.lo.explodeDn(dn)[0]
+            dn = '%s,%s' % (rdn, self.lo.parentDn(self.dn))
+        return dn
 
     def _ldap_modlist(self) -> list[tuple[str, Any, Any]]:
         ml = univention.admin.handlers.simpleLdap._ldap_modlist(self)
@@ -391,12 +402,19 @@ class object(univention.admin.handlers.simpleLdap):
     def _move(self, newdn: str, modify_childs: bool = True, ignore_license: bool = False) -> str:
         olddn = self.dn
 
-        # acquire unique dn in new position
-        self.dn = newdn
-        newdn = self.acquire_unique_dn()
-        self.dn = olddn
+        try:
+            self.lo.authz_connection.rename(self.dn, newdn)
+        except univention.admin.uexceptions.objectExists:
+            if not self.oldinfo['cn'].startswith(f'{self._pattern(self.oldinfo)} '):
+                raise
+            # acquire unique DN in new position
+            self.dn = newdn
+            try:
+                newdn = self.acquire_unique_dn()
+            finally:
+                self.dn = olddn
 
-        self.lo.authz_connection.rename(self.dn, newdn)
+            self.lo.authz_connection.rename(self.dn, newdn)
         self.dn = newdn
 
         try:

doc/errata/staging/univention-directory-manager-modules.yaml
@@ -1,0 +1,11 @@
+product: ucs
+release: "5.2"
+version: [5]
+scope: ucs_5.2-0-errata5.2-5
+src: univention-directory-manager-modules
+fix: 17.5.19
+desc: |
+ This update addresses the following issue:
+ * The `users/contact` UDM module now supports to set the `cn` property
+   explicitly, so that objects can be created in a deterministic fashion.
+bug: [59281]

management/univention-directory-manager-modules/modules/univention/admin/uldap.py
@@ -687,6 +687,8 @@ class access:
             raise univention.admin.uexceptions.permissionDenied()
         except ldap.INVALID_DN_SYNTAX as msg:
             raise univention.admin.uexceptions.ldapError('%s: %s' % (_err2str(msg), dn), original_exception=msg)
+        except ldap.ALREADY_EXISTS:
+            raise univention.admin.uexceptions.objectExists(dn)
         except ldap.LDAPError as msg:
             log.debug('rename failed', dn=dn, error=msg)
             raise univention.admin.uexceptions.ldapError(_err2str(msg), original_exception=msg)

commit 8b6a20b3b8a82486519b2c6ddbf56eedd16d1c21

fix(udm): allow users/contact without firstname
Bug #59281

Fixes: a6781471c0bb154c98821a2207df6d38b8b234da

management/univention-directory-manager-modules/modules/univention/admin/handlers/users/contact.py
@@ -328,7 +328,7 @@ class object(univention.admin.handlers.simpleLdap):
 
     @staticmethod
     def _pattern(obj) -> str:
-        description = '%s %s' % (obj['firstname'] or '', obj['lastname'])
+        description = '%s %s' % (obj.get('firstname', ''), obj['lastname'])
         return description.strip()
 
     def _ldap_pre_ready(self) -> None: