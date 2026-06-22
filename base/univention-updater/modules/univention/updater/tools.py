ae66f28f1478dfd229e48cb7107723871dc9fba2
Florian Best <best@univention.de>
2026-06-22 17:52:38 +0200

    fixup! fix(updater): replace obsolete apt-key

--- base/univention-updater/modules/univention/updater/tools.py
+++ base/univention-updater/modules/univention/updater/tools.py
@@ -46,8 +46,8 @@ if TYPE_CHECKING:
     from typing import Self  # type: ignore[attr-defined]
 
 
-RE_ALLOWED_DEBIAN_PKGNAMES = re.compile('^[a-z0-9][a-z0-9.+-]+$')
-RE_SPLIT_MULTI = re.compile('[ ,]+')
+RE_ALLOWED_DEBIAN_PKGNAMES = re.compile(r'^[a-z0-9][a-z0-9.+-]+$')
+RE_SPLIT_MULTI = re.compile(r'[ ,]+')
 RE_COMPONENT = re.compile(r'^repository/online/component/([^/]+)$')
 RE_CREDENTIALS = re.compile(r'^repository/credentials/(?:(?P<realm>[^/]+)/)?(?P<key>[^/]+)$')
 
@@ -66,27 +66,35 @@ def verify_script(script: bytes, signature: bytes) -> str | None:
 
     .. code-block: python
 
-        verify_script(open("script.sh", "r").read(), open("script.sh.gpg", "r").read())
+        verify_script(open("script.sh", "rb").read(), open("script.sh.gpg", "rb").read())
 
     :param str script: The script text to verify.
     :param str signature: The detached signature.
     :return: None or the error output.
     :rtype: None or str
     """
-    # write signature to temporary file
     sig_fd, sig_name = tempfile.mkstemp()
-    os.write(sig_fd, signature)
-    os.close(sig_fd)
+    script_fd, script_name = tempfile.mkstemp()
 
-    # verify script
-    cmd = ['gpgv']
-    for keyring in glob.glob('/usr/share/keyrings/univention-archive-key-ucs-*.gpg'):
-        cmd.extend(['--keyring', keyring])
+    try:
+        # write signature and script to temporary file
+        with os.fdopen(sig_fd, 'wb') as sig_file:
+            sig_file.write(signature)
+        with os.fdopen(script_fd, 'wb') as script_file:
+            script_file.write(script)
 
-    cmd.extend([sig_name, '-'])
-    proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
-    stdout, _stderr = proc.communicate(script)
-    return stdout.decode('UTF-8', 'replace') if proc.returncode != 0 else None
+        # verify script
+        cmd = ['sqv', '--policy-as-of', '2020-06-26T00:00:00Z']  # we need to support univention-archive-key-ucs-4x.gpg!?!
+        for keyring in glob.glob('/usr/share/keyrings/univention-archive-key-ucs-*.gpg'):
+            cmd.extend(['--keyring', keyring])
+
+        cmd.extend(['--signature-file', sig_name, script_name])
+
+        proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
+        stdout, _stderr = proc.communicate(script)
+        return stdout.decode('UTF-8', 'replace') if proc.returncode else None
+    finally:
+        os.unlink(sig_name)
 
 
 class _UCSRepo(UCS_Version):  # noqa: PLW1641
@@ -985,7 +993,7 @@ class Component:
                     if not testserver:
                         continue
                     if prefix:  # append prefix if defined
-                        testserver = testserver + '%s/' % (prefix.strip('/'),)
+                        testserver += '%s/' % (prefix.strip('/'),)
                     try:
                         assert testserver.access(None, '')
                         return testserver
--- doc/changelog/staged/changelog-deployment.59512.rst
+++ doc/changelog/staged/changelog-deployment.59512.rst
@@ -1,1 +1,1 @@
-The Univention Updater no longer uses the deprecated :program:`apt-key` command for signature verification and now verifies signatures with :program:`gpgv` and the UCS archive keyrings.
+The Univention Updater no longer uses the deprecated :program:`apt-key` command for signature verification and now verifies signatures with :program:`sqv` and the UCS archive keyrings.
