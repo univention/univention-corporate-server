ad9716a2045af631b1c5ba40017f9e34644d6d70
Felix Botner <botner@univention.de>
2026-06-24 13:28:13 +0200

    fix(udm): set '*' as default search value when auto_substring_search is disabled
    Move auto_substring_search evaluation from updateSearch to renderSearchPage
    to only set the initial default for the search page.
    
    Bug #59444
    
    Issue: univention/dev/ucs#3561
    Fixes: 7878a9732ca8780469c787b59e1fd073af4c4db0

--- management/univention-management-console-module-udm/umc/js/udm.js
+++ management/univention-management-console-module-udm/umc/js/udm.js
@@ -520,7 +520,16 @@ define([
 			}));
 
 			// register event to update hiding/showing of form fields
-			this._searchForm.ready().then(lang.hitch(this, '_updateSearch'));
+			this._searchForm.ready().then(lang.hitch(this, function() {
+				this._updateSearch();
+				// initialize search filter with * in case auto substring search is disabled
+				var propValWidget = this._searchForm._widgets.objectPropertyValue;
+				var autoSubstringSearch = this._ucr['directory/manager/web/auto_substring_search'] === undefined || tools.isTrue(this._ucr['directory/manager/web/auto_substring_search'])
+				var wildcardSearch = this._ucr['directory/manager/web/allow_wildcard_search'] === undefined  || tools.isTrue(this._ucr['directory/manager/web/allow_wildcard_search'])
+				if (!propValWidget.get('value') && !autoSubstringSearch && wildcardSearch) {
+					propValWidget.set('value', '*');
+				}
+			}));
 			this._grid.on('filterDone', lang.hitch(this, '_updateSearch'));
 
 			// focus and select text when the objectPropertyValue has been loaded
@@ -1441,13 +1450,6 @@ define([
 					toggleButton.set('label', _('Advanced options'));
 				}
 				this.layout();
-			}
-
-			var propValWidget = this._searchForm._widgets.objectPropertyValue;
-			var autoSubstringSearch = this._ucr['directory/manager/web/auto_substring_search'] === undefined || tools.isTrue(this._ucr['directory/manager/web/auto_substring_search'])
-			var wildcardSearch = this._ucr['directory/manager/web/allow_wildcard_search'] === undefined  || tools.isTrue(this._ucr['directory/manager/web/allow_wildcard_search'])
-			if (!propValWidget.get('value') && !autoSubstringSearch && wildcardSearch) {
-				propValWidget.set('value', '*');
 			}
 
 			// GUI setup is done when this method has been called for the first time

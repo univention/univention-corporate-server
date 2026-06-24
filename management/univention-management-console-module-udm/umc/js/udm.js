univention/univention-corporate-server

commit 7878a9732ca8780469c787b59e1fd073af4c4db0

feat(udm): set '*' as default search value when auto_substring_search is disabled
Bug #59444
Issue univention/dev/ucs#3561

Co-authored-by: Florian Best <best@univention.de>

doc/errata/staging/univention-management-console-module-udm.yaml
@@ -1,0 +1,10 @@
+product: ucs
+release: "5.2"
+version: [6]
+scope: ucs_5.2-0-errata5.2-6
+src: univention-management-console-module-udm
+fix: 12.6.0
+desc: |
+ This update addresses the following issues:
+ * Set * as default search value when automatic substring search is disabled.
+bug: [59444]

management/univention-management-console-module-udm/umc/js/udm.js
@@ -1443,6 +1443,13 @@ define([
 				this.layout();
 			}
 
+			var propValWidget = this._searchForm._widgets.objectPropertyValue;
+			var autoSubstringSearch = this._ucr['directory/manager/web/auto_substring_search'] === undefined || tools.isTrue(this._ucr['directory/manager/web/auto_substring_search'])
+			var wildcardSearch = this._ucr['directory/manager/web/allow_wildcard_search'] === undefined  || tools.isTrue(this._ucr['directory/manager/web/allow_wildcard_search'])
+			if (!propValWidget.get('value') && !autoSubstringSearch && wildcardSearch) {
+				propValWidget.set('value', '*');
+			}
+
 			// GUI setup is done when this method has been called for the first time
 			if (!this._finishedDeferred.isFulfilled()) {
 				this._finishedDeferred.resolve();

commit ad9716a2045af631b1c5ba40017f9e34644d6d70

fix(udm): set '*' as default search value when auto_substring_search is disabled
Move auto_substring_search evaluation from updateSearch to renderSearchPage
to only set the initial default for the search page.

Bug #59444

Issue: univention/dev/ucs#3561
Fixes: 7878a9732ca8780469c787b59e1fd073af4c4db0

doc/errata/staging/univention-management-console-module-udm.yaml
@@ -3,7 +3,7 @@ release: "5.2"
 version: [6]
 scope: ucs_5.2-0-errata5.2-6
 src: univention-management-console-module-udm
-fix: 12.6.0
+fix: 12.6.1
 desc: |
  This update addresses the following issues:
  * Set * as default search value when automatic substring search is disabled.

management/univention-management-console-module-udm/umc/js/udm.js
@@ -520,7 +520,16 @@ define([
 			}));
 
 			// register event to update hiding/showing of form fields
-			this._searchForm.ready().then(lang.hitch(this, '_updateSearch'));
+			this._searchForm.ready().then(lang.hitch(this, function() {
+				this._updateSearch();
+				// initialize search filter with * in case auto substring search is disabled
+				var propValWidget = this._searchForm._widgets.objectPropertyValue;
+				var autoSubstringSearch = this._ucr['directory/manager/web/auto_substring_search'] === undefined || tools.isTrue(this._ucr['directory/manager/web/auto_substring_search'])
+				var wildcardSearch = this._ucr['directory/manager/web/allow_wildcard_search'] === undefined  || tools.isTrue(this._ucr['directory/manager/web/allow_wildcard_search'])
+				if (!propValWidget.get('value') && !autoSubstringSearch && wildcardSearch) {
+					propValWidget.set('value', '*');
+				}
+			}));
 			this._grid.on('filterDone', lang.hitch(this, '_updateSearch'));
 
 			// focus and select text when the objectPropertyValue has been loaded
@@ -1441,13 +1450,6 @@ define([
 					toggleButton.set('label', _('Advanced options'));
 				}
 				this.layout();
-			}
-
-			var propValWidget = this._searchForm._widgets.objectPropertyValue;
-			var autoSubstringSearch = this._ucr['directory/manager/web/auto_substring_search'] === undefined || tools.isTrue(this._ucr['directory/manager/web/auto_substring_search'])
-			var wildcardSearch = this._ucr['directory/manager/web/allow_wildcard_search'] === undefined  || tools.isTrue(this._ucr['directory/manager/web/allow_wildcard_search'])
-			if (!propValWidget.get('value') && !autoSubstringSearch && wildcardSearch) {
-				propValWidget.set('value', '*');
 			}
 
 			// GUI setup is done when this method has been called for the first time