56e240da5a6cfa467cfcc5ffc54c523166093fe2
Florian Best <best@univention.de>
2026-06-27 00:05:00 +0200

    test(end): group coredumps by executable using pytest-subtests and print stack traces of coredumps
    Group detected coredumps by executable and report each executable as a
    separate pytest subtest instead of a single aggregated failure.
    Each test will print additional infos e.g. the stack traces of the
    coredump.
    
    Bug #59418

--- test/ucs-test/tests/99_end/07_coredumps.py
+++ test/ucs-test/tests/99_end/07_coredumps.py
@@ -6,10 +6,12 @@
 ## tags: []
 ## exposure: safe
 
+import collections
 import json
 import subprocess
 
 import pytest
+from pytest_subtests import SubTests
 
 
 IGNORE_EXE = {
@@ -21,47 +23,67 @@ IGNORE_EXE = {
 def coredumpctl_json():
     proc = subprocess.run(['coredumpctl', '--json=short', 'list'], capture_output=True, text=True, check=False)
 
+    # coredumpctl returns 1 if no cores are found. stdout is empty then.
     if not proc.stdout.strip():
         return []
+
+    if proc.returncode != 0:
+        pytest.fail('coredumpctl failed with exit code %s\nSTDOUT:\n%s\nSTDERR:\n%s' % (proc.returncode, proc.stdout, proc.stderr))
 
     try:
         return json.loads(proc.stdout)
     except json.JSONDecodeError as exc:
         pytest.fail('Could not parse coredumpctl JSON output: %s\n%s' % (exc, proc.stdout))
 
-    # coredumpctl returns 1 if no cores are found. but stdout is empty then. so we already returned.
-    if proc.returncode != 0:
-        pytest.fail('coredumpctl failed with exit code %s\nSTDOUT:\n%s\nSTDERR:\n%s' % (proc.returncode, proc.stdout, proc.stderr))
+
+def dump_id(dump):
+    # PID is usually enough for this test run. Add time/exe to printed output for humans.
+    return str(dump.get('pid'))
 
 
-# provoke a coredump, to test this via:
+def dump_summary(dump):
+    return 'PID=%s EXE=%s COMM=%s SIGNAL=%s TIME=%s' % (
+        dump.get('pid'),
+        dump.get('exe'),
+        dump.get('comm'),
+        dump.get('signal'),
+        dump.get('time'),
+    )
+
+
+def print_coredump_info(dump):
+    ident = dump_id(dump)
+
+    print('\n===== coredumpctl info %s =====' % ident)
+    subprocess.run(['coredumpctl', 'info', ident], check=False)
+
+
+def group_by_exe(dumps):
+    grouped = collections.defaultdict(list)
+    for dump in dumps:
+        grouped[dump.get('exe') or '<unknown exe>'].append(dump)
+    return dict(sorted(grouped.items()))
+
+
+# provoke a coredump to test this via:
 # python3 -c 'import ctypes; ctypes.string_at(0)'
-
-# analyse with: apt install univention-directory-listener-dbgsym univention-directory-notifier-dbgsym libc6-dbg libldap2-dbgsym python3-dbg
-# coredumpctl debug "$ID" will open gdb
-def test_no_coredumps():
+#
+# analyse with:
+# apt install univention-directory-listener-dbgsym univention-directory-notifier-dbgsym libc6-dbg libldap2-dbgsym python3-dbg
+# coredumpctl debug "$ID" will open gdb interactively
+def test_no_coredumps(subtests: SubTests):
     subprocess.call(['coredumpctl', 'list'])
     dumps = coredumpctl_json()
 
-    unexpected = []
-    ignored = []
+    grouped = group_by_exe(dumps)
 
-    for dump in dumps:
-        exe = dump.get('exe')
-        if exe in IGNORE_EXE:
-            ignored.append(dump)
-        else:
-            unexpected.append(dump)
+    for exe, exe_dumps in grouped.items():
+        with subtests.test(exe=exe):
+            for dump in exe_dumps:
+                print('  %s' % dump_summary(dump))
+                print_coredump_info(dump)
 
-    if ignored:
-        print('Ignored coredumps:')
-        for dump in ignored:
-            print('  PID=%s EXE=%s COMM=%s TIME=%s' % (dump.get('pid'), dump.get('exe'), dump.get('comm'), dump.get('time')))
+            if exe in IGNORE_EXE:
+                pytest.skip('Ignored coredumps for EXE=%s' % exe)
 
-    assert not unexpected, '\n'.join(
-        ['Unexpected coredumps found:']
-        + [
-            '  PID=%s EXE=%s COMM=%s SIGNAL=%s TIME=%s' % (dump.get('pid'), dump.get('exe'), dump.get('comm'), dump.get('signal'), dump.get('time'))
-            for dump in unexpected
-        ],
-    )
+            pytest.fail('\n'.join(['Unexpected coredumps found for EXE=%s:' % exe] + ['  %s' % dump_summary(dump) for dump in exe_dumps]))
