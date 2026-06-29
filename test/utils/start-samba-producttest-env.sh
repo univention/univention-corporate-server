f2e590ab4226c9576c953d78fa42c04d73a1db91
Julia Bremer <bremer@univention.de>
2026-06-29 19:52:03 +0200

    chore(tests): Add setup script for multiserver tests
    Issue univention/dev/ucs#3555

---
+++ test/utils/start-samba-producttest-env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2026 Univention GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
+
+set -x
+set -e
+
+export KVM_BUILD_SERVER="${KVM_BUILD_SERVER:=tross.knut.univention.de}"
+export TERMINATE=false
+export DOCKER=true
+export REPLACE=true
+export UCS_TEST_RUN=false
+
+# user specific instances "username_..."
+export KVM_OWNER="${BUILD_USER_ID:=$USER}"
+export JOB_BASE_NAME="${JOB_BASE_NAME:=product-test-samba-multi-server-setup}"
+
+
+exec ./utils/start-test.sh product-tests/samba/multi-server-setup.cfg
