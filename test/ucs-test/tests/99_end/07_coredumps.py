eaa93b1400b2865f6086b56ddbc15210fbb78e00
Florian Best <best@univention.de>
2026-07-08 12:30:37 +0200

    test(coredumps): ignore /usr/sbin/sssd
    Bug #59597

--- test/ucs-test/tests/99_end/07_coredumps.py
+++ test/ucs-test/tests/99_end/07_coredumps.py
@@ -22,6 +22,7 @@ import pytest
 IGNORE_EXE = {
     '/usr/sbin/rsyslogd',
     '/usr/libexec/samba/rpcd_spoolss',  # Bug #59550: triggered by 59_udm.64_test_udm_printers function test_create_printer_and_check_printing_works
+    '/usr/sbin/sssd',  # Bug #59597
 }
 
 
