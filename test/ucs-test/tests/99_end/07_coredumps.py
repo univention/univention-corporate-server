ef6d1457071f005fb6e542290131b627fcf41074
Florian Best <best@univention.de>
2026-07-08 12:31:42 +0200

    test(coredumps): ignore /usr/sbin/inetd
    Bug #59598

--- test/ucs-test/tests/99_end/07_coredumps.py
+++ test/ucs-test/tests/99_end/07_coredumps.py
@@ -23,6 +23,7 @@ IGNORE_EXE = {
     '/usr/sbin/rsyslogd',
     '/usr/libexec/samba/rpcd_spoolss',  # Bug #59550: triggered by 59_udm.64_test_udm_printers function test_create_printer_and_check_printing_works
     '/usr/sbin/sssd',  # Bug #59597
+    '/usr/sbin/inetd',  # Bug #59598
 }
 
 
