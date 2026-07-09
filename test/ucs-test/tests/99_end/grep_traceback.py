52316f2ba7c8113b268df1d803d006cf22b72dbf
Florian Best <best@univention.de>
2026-07-09 23:18:24 +0200

    test(end): ignore new traceback signature
    Bug #51671

--- test/ucs-test/tests/99_end/grep_traceback.py
+++ test/ucs-test/tests/99_end/grep_traceback.py
@@ -292,7 +292,7 @@ COMMON_EXCEPTIONS = (
     E(r'ldap.ALREADY_EXISTS:.*00000526: UpdErr: DSID-031A119B, problem 6005 \(ENTRY_EXISTS\)', 'connector/__init__.py".*in __sync_file_from_ucs', 58676),
     E(r'ldap.ALREADY_EXISTS:.*00000524: UpdErr: DSID-031A11DA, problem 6005 \(ENTRY_EXISTS\)', 'connector/__init__.py".*in __sync_file_from_ucs', 58676),
     # E(r'^ldap\.NO_SUCH_OBJECT: .*', [r'quota\.py'], 52765),
-    E(r'.*OperationalError.*FATAL:.*admindiary.*', [r'admindiary_backend_wrapper\.py', '_wrap_pool_connect'], 51671),
+    E(r'.*OperationalError.*FATAL:.*admindiary.*', [r'admindiary_backend_wrapper\.py', 'sqlalchemy/pool/base.py.*in connect'], 51671),
     E(r"(OSError|FileNotFoundError): \[Errno 2\] .*: '/var/lib/samba/sysvol/.*/Policies/'", [r'sysvol-cleanup\.py'], 51670),
     # E("AttributeError: 'NoneType' object has no attribute 'lower'", ['_remove_subtree_in_s4'], 50282),
     E("AttributeError: 'NoneType' object has no attribute 'get'", ['primary_group_sync_from_ucs', 'group_members_sync_to_ucs'], 49879),
