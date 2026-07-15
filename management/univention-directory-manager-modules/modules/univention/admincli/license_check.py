univention/univention-corporate-server

commit 650bd13a3f04a690aea0616df11ac49d7a8bb643

refactor(udm): remove unused license features
* remove license version 1
* drop license disabling features of UCS < 3.0
* fix bitmask evaluation of license return codes

Bug #59060

management/univention-directory-manager-modules/modules/univention/admincli/license_check.py
@@ -7,15 +7,11 @@ from __future__ import annotations
 
 import datetime
 import getopt
-import traceback
 from typing import TYPE_CHECKING
-
-from ldap.filter import filter_format
 
 import univention.admin.license
 import univention.admin.uldap
 import univention.config_registry
-import univention.license
 from univention.admin import uexceptions
 
 
@@ -75,38 +71,15 @@ def format(label: str, num: int, max: str, expired: bool, cmp: Callable, ignored
     return '%s %s of %s... %s' % tuple(args)
 
 
-def find_licenses(lo: univention.admin.uldap.access, baseDN: str, module: str = '*') -> list[str]:
-    def find_wrap(dir: str) -> list[str]:
-        try:
-            return lo.searchDn(base=dir, filter='(univentionLicenseObject=*)')
-        except uexceptions.noObject:
-            return []
-    filter = filter_format('univentionLicenseModule=%s', [module])
-    dirs = ['cn=default containers,cn=univention,%s' % baseDN]
-    objects = [o for d in dirs for o in find_wrap(d)]
-    containers = [c.decode('UTF-8') for o in objects for c in lo.get(o)['univentionLicenseObject']]
-    licenses = [license for c in containers for license in lo.searchDn(base=c, filter=filter)]
-    return licenses
-
-
-def choose_license(lo: univention.admin.uldap.access, dns: list[str]) -> tuple[str | None, int]:
-    for dn in dns:
-        retval = univention.license.check(dn)
-        if retval == -1:
-            continue
-        return dn, retval
-    return None, -1
-
-
-def check_license(lo: univention.admin.uldap.access, dn: str, list_dns: list[str], expired: int) -> list[str]:
+def check_license(lo: univention.admin.uldap.access, dn: str | None, list_dns: bool, expired: int) -> list[str]:
     if expired == -1:
         return ['No valid license object found', 'OPERATION FAILED']
     out: list[str] = []
 
     def check_code(code: int) -> None:
-        for label, value in [('searchpath', 8), ('basedn', 4), ('enddate', 2), ('signature', 1)]:
-            if code >= value:
-                code -= value
+        for label, value in [('basedn', 4), ('enddate', 2), ('signature', 1)]:
+            if code & value == value:
+                code ^= value
                 ok = 'FAILED'
             else:
                 ok = 'OK'
@@ -116,36 +89,23 @@ def check_license(lo: univention.admin.uldap.access, dn: str, list_dns: list[str
         assert _license is not None
         v = _license.version
         types = _license.licenses[v]
-        if dn is None:
-            maximum = [_license.licenses[v][type] for type in types]
-        else:
-            maximum = [lo.get(dn)[_license.keys[v][type]][0].decode('utf-8') for type in types]
-        objs = [lo.searchDn(filter=_license.filters[v][type]) for type in types]
-        num = [len(obj or '') for obj in objs]
-        _license.checkObjectCounts(maximum, num)
-        for i, m, n, odn in zip(range(len(types)), maximum, num, objs):
-            if i in (License.USERS, License.ACCOUNT):
+        maximum = [_license.licenses[v][type] for type in types]
+        num = [_license.real[v][type] for type in types]
+        for i, m, n in zip(range(len(types)), maximum, num):
+            if i == License.USERS:
                 n -= _license.sysAccountsFound
                 if n < 0:
                     n = 0
             ln = _license.names[v][i]
             if m:
-                if list_dns:
-                    out.append("")
-
-                ignored = False
-                if v == '2' and i == License.SERVERS:
-                    # Ignore the server count
-                    ignored = True
+                ignored = v == '2' and i == License.SERVERS  # Ignore the server count
                 out.append(format(ln, n, m, False, _license.compare, ignored))
-                if list_dns and maximum != 'unlimited':
-                    out.extend("  %s" % dnout for dnout in odn)
-                if list_dns and (i in (License.USERS, License.ACCOUNT)):
+                if list_dns and i == License.USERS:
                     out.append("  %s Systemaccounts are ignored." % _license.sysAccountsFound)
 
     def check_time() -> None:
         now = datetime.date.today()
-        then = lo.get(dn)['univentionLicenseEndDate'][0].decode('UTF-8')
+        then = _license.endDate
         if then != 'unlimited':
             (day, month, year) = then.split('.')
             then_ = datetime.date(int(year), int(month), int(day))
@@ -156,10 +116,9 @@ def check_license(lo: univention.admin.uldap.access, dn: str, list_dns: list[str
 
     if dn is not None and list_dns:
         out.append('License found at: %s' % dn)
-    check_code(expired)
+    check_code(_license.error)
     check_type()
-    if dn is not None:
-        check_time()
+    check_time()
     return out
 
 
@@ -185,23 +144,15 @@ def main(argv: list[str]) -> list[str]:
     out = ['Base DN: %s' % baseDN]
     try:
         _license.init_select(lo, 'admin')
-        out.extend(check_license(lo, None, 'list-dns' in options, 0))
-    except uexceptions.base:
-        dns = find_licenses(lo, baseDN, 'admin')
-        dn, expired = choose_license(lo, dns)
-        out.extend(check_license(lo, dn, 'list-dns' in options, expired))
-    except Exception:
-        # output any other tracebacks
-        trace_out = traceback.format_exc().splitlines()
-        out.extend(trace_out)
-    finally:
-        return out  # noqa: B012
+    except uexceptions.base:  # ignore license... Exceptions
+        pass
+    out.extend(check_license(lo, None, 'list-dns' in options, 0))
+    return out
 
 
 def doit(argv: list[str]) -> list[str]:
     try:
-        out = main(argv[1:])
-        return out
+        return main(argv[1:])
     except UsageError as msg:
         return usage(str(msg))
 

management/univention-directory-manager-modules/modules/univention/admin/handlers/settings/license.py
@@ -32,12 +32,12 @@ options = {
     'Version 1': univention.admin.option(
         short_description=_('Version 1 license'),
         editable=False,
-        default=0,
+        default=False,
     ),
     'Version 2': univention.admin.option(
         short_description=_('Version 2 license'),
         editable=False,
-        default=1,
+        default=True,
     ),
 }
 property_descriptions = {
@@ -57,14 +57,6 @@ property_descriptions = {
         required=True,
         may_change=False,
     ),
-    'module': univention.admin.property(
-        short_description=_('Module'),
-        long_description=_('Module the license is valid for'),
-        syntax=univention.admin.syntax.string,
-        options=['Version 1'],
-        required=True,
-        may_change=False,
-    ),
     'base': univention.admin.property(
         short_description=_('Base DN'),
         long_description=_('Base DN the license is valid for'),
@@ -78,42 +70,6 @@ property_descriptions = {
         long_description=_('This Signature is used to verify the authenticity of the license.'),
         syntax=univention.admin.syntax.string,
         required=True,
-        may_change=False,
-    ),
-    'accounts': univention.admin.property(
-        short_description=_('Max. user accounts'),
-        long_description=_('Maximum number of user accounts managed with the UCS infrastructure'),
-        syntax=univention.admin.syntax.string,
-        options=['Version 1'],
-        may_change=False,
-    ),
-    'clients': univention.admin.property(
-        short_description=_('Max. clients'),
-        long_description=_('Maximum number of client hosts managed with the UCS infrastructure'),
-        syntax=univention.admin.syntax.string,
-        options=['Version 1'],
-        may_change=False,
-    ),
-    'groupwareaccounts': univention.admin.property(
-        short_description=_('Max. groupware accounts'),
-        long_description=_('Maximum number of groupware accounts managed with the UCS infrastructure'),
-        syntax=univention.admin.syntax.string,
-        options=['Version 1'],
-        may_change=False,
-    ),
-    'desktops': univention.admin.property(
-        short_description=_('Max. desktops'),
-        long_description=_('Maximum number of Univention desktop accounts managed with the UCS infrastructure'),
-        syntax=univention.admin.syntax.string,
-        options=['Version 1'],
-        may_change=False,
-    ),
-    'productTypes': univention.admin.property(
-        short_description=_('Valid product types'),
-        long_description=_('Product types this license allows.'),
-        syntax=univention.admin.syntax.string,
-        multivalue=True,
-        options=['Version 1'],
         may_change=False,
     ),
     'oemProductTypes': univention.admin.property(
@@ -207,16 +163,10 @@ layout = [
     Tab(_('License'), _('Licensing Information'), layout=[
         Group(_('General license settings'), layout=[
             'name',
-            'module',
             'expires',
             'base',
             'oemProductTypes',
             'signature',
-        ]),
-        Group(_('Version 1 license informations'), layout=[
-            'productTypes',
-            ['accounts', 'groupwareaccounts'],
-            ['clients', 'desktops'],
         ]),
         Group(_('Version 2 license informations'), layout=[
             'keyID',
@@ -232,14 +182,8 @@ layout = [
 mapping = univention.admin.mapping.mapping()
 mapping.register('name', 'cn', None, univention.admin.mapping.ListToString)
 mapping.register('expires', 'univentionLicenseEndDate', None, univention.admin.mapping.ListToString, encoding='ASCII')
-mapping.register('module', 'univentionLicenseModule', None, univention.admin.mapping.ListToString, encoding='ASCII')
 mapping.register('base', 'univentionLicenseBaseDN', None, univention.admin.mapping.ListToString, encoding='ASCII')
 mapping.register('signature', 'univentionLicenseSignature', None, univention.admin.mapping.ListToString, encoding='ASCII')
-mapping.register('accounts', 'univentionLicenseAccounts', None, univention.admin.mapping.ListToString, encoding='ASCII')
-mapping.register('groupwareaccounts', 'univentionLicenseGroupwareAccounts', None, univention.admin.mapping.ListToString, encoding='ASCII')
-mapping.register('clients', 'univentionLicenseClients', None, univention.admin.mapping.ListToString, encoding='ASCII')
-mapping.register('desktops', 'univentionLicenseuniventionDesktops', None, univention.admin.mapping.ListToString, encoding='ASCII')
-mapping.register('productTypes', 'univentionLicenseType', encoding='ASCII')
 mapping.register('oemProductTypes', 'univentionLicenseOEMProduct', encoding='ASCII')
 mapping.register('product', 'univentionLicenseProduct', None, univention.admin.mapping.ListToString, encoding='ASCII')
 mapping.register('keyID', 'univentionLicenseKeyID', None, univention.admin.mapping.ListToString, encoding='ASCII')

management/univention-directory-manager-modules/modules/univention/admin/license_data.py
@@ -1,56 +1,0 @@
-# SPDX-FileCopyrightText: 2004-2026 Univention GmbH
-# SPDX-License-Identifier: AGPL-3.0-only
-
-"""|UDM| licence data."""
-
-UCS = ['UCS', 'Univention Corporate Server']
-
-
-class Attributes:
-
-    def __init__(self, required_license=None, options={}):
-        self.required_license = required_license
-        self._options = options
-
-    def options(self, license_type):
-        if not self._options:
-            return ()
-        if not isinstance(license_type, list):
-            license_type = list(license_type)
-        license_type.sort()
-
-        for key in self._options.keys():
-            skey = sorted(key)
-            if license_type == skey:
-                return self._options[key]
-
-        return ()
-
-    def valid(self, license_type):
-        if not isinstance(license_type, list):
-            license_type = list(license_type)
-
-        if not self.required_license:
-            return True
-
-        if isinstance(self.required_license, list):
-            return any(rl in license_type for rl in self.required_license)
-        else:
-            return self.required_license in license_type
-
-
-def moreGroupware(license):
-    return False, (license.compare(license.licenses[license.ACCOUNT], license.licenses[license.GROUPWARE]) != 1)
-
-
-# Examples:
-#    'computers/ipmanagedclient': Attributes( UCS + [ 'OEM1'] ),
-#    'computers/domaincontroller_master': Attributes( UCS + ['OEM2'] ,options =
-#                {
-#                    ( UCS, ) : ( ( 'nagios', (False, False) ), ),
-#                    ( OEM2 ) : ( ( 'nagios', (True, False) ), ),
-#                    ( UCS + ['OEM2'] ) : ( ( 'nagios', (False, False) ), ),
-#                } ),
-
-
-modules = {}

management/univention-directory-manager-rest/src/univention/admin/rest/module.py
@@ -3112,35 +3112,19 @@ class License(Resource):
 
         import univention.admin.license as udm_license
         license_data['licenseVersion'] = udm_license._license.version
-        if udm_license._license.version == '1':
-            for item in ('licenses', 'real'):
-                license_data[item] = {}
-                for lic_type in ('CLIENT', 'ACCOUNT', 'DESKTOP', 'GROUPWARE'):
-                    count = getattr(udm_license._license, item)[udm_license._license.version][getattr(udm_license.License, lic_type)]
-                    if isinstance(count, str):
-                        try:
-                            count = int(count)
-                        except ValueError:
-                            count = None
-                    license_data[item][lic_type.lower()] = count
-
-            if 'UGS' in udm_license._license.types:
-                udm_license._license.types = filter(lambda x: x != 'UGS', udm_license._license.types)
-        elif udm_license._license.version == '2':
-            for item in ('licenses', 'real'):
-                license_data[item] = {}
-                for lic_type in ('SERVERS', 'USERS', 'MANAGEDCLIENTS', 'CORPORATECLIENTS'):
-                    count = getattr(udm_license._license, item)[udm_license._license.version][getattr(udm_license.License, lic_type)]
-                    if isinstance(count, str):
-                        try:
-                            count = int(count)
-                        except ValueError:
-                            count = None
-                    license_data[item][lic_type.lower()] = count
-            license_data['keyID'] = udm_license._license.licenseKeyID
-            license_data['support'] = udm_license._license.licenseSupport
-            license_data['premiumSupport'] = udm_license._license.licensePremiumSupport
-
+        for item in ('licenses', 'real'):
+            license_data[item] = {}
+            for lic_type in ('SERVERS', 'USERS', 'MANAGEDCLIENTS', 'CORPORATECLIENTS'):
+                count = getattr(udm_license._license, item)[udm_license._license.version][getattr(udm_license.License, lic_type)]
+                if isinstance(count, str):
+                    try:
+                        count = int(count)
+                    except ValueError:
+                        count = None
+                license_data[item][lic_type.lower()] = count
+        license_data['keyID'] = udm_license._license.licenseKeyID
+        license_data['support'] = udm_license._license.licenseSupport
+        license_data['premiumSupport'] = udm_license._license.licensePremiumSupport
         license_data['licenseTypes'] = udm_license._license.types
         license_data['oemProductTypes'] = udm_license._license.oemProductTypes
         license_data['endDate'] = udm_license._license.endDate

management/univention-ldap/schema/license.schema
@@ -25,27 +25,27 @@ attributetype ( 1.3.6.1.4.1.10176.1705 NAME ( 'univentionLicenseSignature' )
 	SYNTAX 1.3.6.1.4.1.1466.115.121.1.26 SINGLE-VALUE)
 
 attributetype ( 1.3.6.1.4.1.10176.1706 NAME ( 'univentionLicenseAccounts' )
-	DESC 'Deprecated: License maximum number of users'
+	DESC 'Obsolete: License maximum number of users'
 	EQUALITY caseIgnoreIA5Match
 	SYNTAX 1.3.6.1.4.1.1466.115.121.1.26 SINGLE-VALUE)
 
 attributetype ( 1.3.6.1.4.1.10176.1707 NAME ( 'univentionLicenseClients' )
-	DESC 'Deprecated: License maximum number of clients'
+	DESC 'Obsolete: License maximum number of clients'
 	EQUALITY caseIgnoreIA5Match
 	SYNTAX 1.3.6.1.4.1.1466.115.121.1.26 SINGLE-VALUE)
 
 attributetype ( 1.3.6.1.4.1.10176.1708 NAME ( 'univentionLicenseGroupwareAccounts' )
-	DESC 'Deprecated: License maximum number of groupware accounts'
+	DESC 'Obsolete: License maximum number of groupware accounts'
 	EQUALITY caseIgnoreIA5Match
 	SYNTAX 1.3.6.1.4.1.1466.115.121.1.26 SINGLE-VALUE)
 
 attributetype ( 1.3.6.1.4.1.10176.1709 NAME ( 'univentionLicenseuniventionDesktops' )
-	DESC 'Deprecated: License maximum number of UCS managed desktops'
+	DESC 'Obsolete: License maximum number of UCS managed desktops'
 	EQUALITY caseIgnoreIA5Match
 	SYNTAX 1.3.6.1.4.1.1466.115.121.1.26 SINGLE-VALUE)
 
 attributetype ( 1.3.6.1.4.1.10176.1710 NAME ( 'univentionLicenseType' )
-	DESC 'Deprecated: License type containing the products this license used for'
+	DESC 'Obsolete: License type containing the products this license used for'
 	EQUALITY caseIgnoreIA5Match
 	SYNTAX 1.3.6.1.4.1.1466.115.121.1.26)
 
@@ -55,22 +55,22 @@ attributetype ( 1.3.6.1.4.1.10176.1711 NAME ( 'univentionLicenseOEMProduct' )
 	SYNTAX 1.3.6.1.4.1.1466.115.121.1.26)
 
 attributetype ( 1.3.6.1.4.1.10176.1712.1.1 NAME ( 'univentionLicensePhysicalServers' )
-	DESC 'Deprecated: License maximum number of physical UCS servers'
+	DESC 'Obsolete: License maximum number of physical UCS servers'
 	EQUALITY caseIgnoreIA5Match
 	SYNTAX 1.3.6.1.4.1.1466.115.121.1.26)
 
 attributetype ( 1.3.6.1.4.1.10176.1712.1.2 NAME ( 'univentionLicenseServerInstances' )
-	DESC 'Deprecated: License maximum number of UCS server instances'
+	DESC 'Obsolete: License maximum number of UCS server instances'
 	EQUALITY caseIgnoreIA5Match
 	SYNTAX 1.3.6.1.4.1.1466.115.121.1.26)
 
 attributetype ( 1.3.6.1.4.1.10176.1712.1.3 NAME ( 'univentionLicenseThinClients' )
-	DESC 'Deprecated: License maximum number of thin clients'
+	DESC 'Obsolete: License maximum number of thin clients'
 	EQUALITY caseIgnoreIA5Match
 	SYNTAX 1.3.6.1.4.1.1466.115.121.1.26)
 
 attributetype ( 1.3.6.1.4.1.10176.1712.1.4 NAME ( 'univentionLicenseVirtualDesktops' )
-	DESC 'Deprecated: License maximum number of virtual desktops'
+	DESC 'Obsolete: License maximum number of virtual desktops'
 	EQUALITY caseIgnoreIA5Match
 	SYNTAX 1.3.6.1.4.1.1466.115.121.1.26)
 
@@ -100,7 +100,7 @@ attributetype ( 1.3.6.1.4.1.10176.1712.1.10 NAME ( 'univentionLicensePremiumSupp
 	SYNTAX 1.3.6.1.4.1.1466.115.121.1.26)
 
 attributetype ( 1.3.6.1.4.1.10176.1712.1.11 NAME ( 'univentionLicenseManagedClients' )
-	DESC 'License maximum of servers with premium support'
+	DESC 'License maximum of (Windows, Linux, Ubuntu, MacOS) clients'
 	EQUALITY caseIgnoreIA5Match
 	SYNTAX 1.3.6.1.4.1.1466.115.121.1.26)
 

management/univention-management-console-module-adtakeover/umc/python/adtakeover/takeover.py
@@ -45,7 +45,6 @@ import univention.config_registry
 import univention.lib.admember
 import univention.lib.s4
 from univention.admin import uexceptions
-from univention.admincli import license_check
 from univention.config_registry.frontend import ucr_update
 from univention.config_registry.interfaces import Interfaces
 from univention.management.console import Translation
@@ -521,40 +520,7 @@ class UCS_License_detection:
         self._license = univention.admin.license._license
         self.ignored_users_list = self._license.sysAccountNames
 
-    def determine_license(self, lo, dn):
-        def mylen(xs):
-            if xs is None:
-                return 0
-            return len(xs)
-        v = self._license.version
-        types = self._license.licenses[v]
-        if dn is None:
-            max = [self._license.licenses[v][type] for type in types]
-        else:
-            max = [lo.get(dn)[self._license.keys[v][type]][0] for type in types]
-
-        objs = [lo.searchDn(filter=self._license.filters[v][type]) for type in types]
-        num = [mylen(obj) for obj in objs]
-        self._license.checkObjectCounts(max, num)
-        result = []
-        for i in list(types.keys()):
-            types[i]
-            m = max[i]
-            n = num[i]
-            objs[i]
-            if i in (self.License.USERS, self.License.ACCOUNT):
-                n -= self._license.sysAccountsFound
-                if n < 0:
-                    n = 0
-            li = self._license.names[v][i]
-            if m and (i in (self.License.USERS, self.License.ACCOUNT)):
-                log.debug("determine_license for current UCS %s: %s of %s", li, n, m)
-                log.debug("  %s Systemaccounts are ignored.", self._license.sysAccountsFound)
-                result.append((li, n, m))
-        return result
-
     def check_license(self, domain_info):
-
         binddn = self.ucr['ldap/hostdn']
         with open('/etc/machine.secret') as pwfile:
             bindpw = pwfile.readline().strip()
@@ -572,24 +538,21 @@ class UCS_License_detection:
 
         try:
             self._license.init_select(lo, 'admin')
-            check_array = self.determine_license(lo, None)
-        except uexceptions.base:
-            dns = license_check.find_licenses(lo, self.ucr['ldap/base'], 'admin')
-            dn, _expired = license_check.choose_license(lo, dns)
-            check_array = self.determine_license(lo, dn)
-
-        # some name translation
-        object_displayname_for_licensetype = {'Accounts': _('users'), 'Users': _('users')}
-        ad_object_count_for_licensetype = {'Accounts': domain_info["users"], 'Users': domain_info["users"]}
+        except uexceptions.base:  # licenseInvalid, ...
+            pass
 
         license_sufficient = True
         error_msg = None
-        for object_type, num, max_objs in check_array:
-            object_displayname = object_displayname_for_licensetype.get(object_type, object_type)
-            log.info("Found %s %s objects on the remote server.", ad_object_count_for_licensetype[object_type], object_displayname)
-            sum_objs = num + ad_object_count_for_licensetype[object_type]
-            domain_info["licensed_%s" % (object_displayname,)] = max_objs
-            domain_info["estimated_%s" % (object_displayname,)] = sum_objs
+
+        max_objs = self._license.licenses['2'][self.License.USERS]
+        num = self._license.real['2'][self.License.USERS]
+
+        if max_objs and num:
+            object_displayname = _('users')
+            log.info("Found %s %s objects on the remote server.", domain_info["users"], object_displayname)
+            sum_objs = num + domain_info["users"] - self._license.sysAccountsFound
+            domain_info["licensed_users"] = max_objs
+            domain_info["estimated_users"] = sum_objs
             if self._license.compare(sum_objs, max_objs) > 0:
                 license_sufficient = False
                 error_msg = _("Number of %(object_name)s after takeover would be %(sum)s. This would exceed the number of licensed objects (%(max)s).") % {
@@ -606,7 +569,6 @@ class UCS_License_detection:
 class AD_Connection:
 
     def __init__(self, hostname_or_ip, lp=None):
-
         self.hostname_or_ip = hostname_or_ip
         self.ldap_uri = ldap_uri_for_host(hostname_or_ip)
 
@@ -699,7 +661,6 @@ class AD_Connection:
                 return ""
 
     def count_objects(self, ignored_users_list):
-
         ignored_user_objects = 0
         ad_user_objects = 0
         ad_group_objects = 0

management/univention-management-console-module-udm/umc/js/udm/LicenseDialog.js
@@ -104,26 +104,6 @@ define([
 					'': _('UCS License')
 				}[this.licenseInfo.freeLicense];
 
-				if (this.licenseInfo.licenseVersion === '1') {
-
-					// subtract system accounts
-					if (this.licenseInfo.real.account >= this.licenseInfo.sysAccountsFound) {
-						this.licenseInfo.real.account -= this.licenseInfo.sysAccountsFound;
-					}
-
-					entries = [
-						[_('License type'),	licenseTypeLabel],
-						[_('LDAP base'), this.licenseInfo.baseDN],
-						[_('User accounts'), this._limitInfo('account')],
-						[_('Clients'), this._limitInfo('client')],
-						[_('Desktops'), this._limitInfo('desktop')],
-						[_('Expiry date'), _(this.licenseInfo.endDate)],
-						[_('Valid product types'), product]
-					];
-
-
-				} else if (this.licenseInfo.licenseVersion === '2') {
-
 					// subtract system accounts
 					if (this.licenseInfo.real.users >= this.licenseInfo.sysAccountsFound) {
 						this.licenseInfo.real.users -= this.licenseInfo.sysAccountsFound;
@@ -140,7 +120,6 @@ define([
 						[_('Expiry date'), _(this.licenseInfo.endDate)],
 						[_('Valid product types'), product]
 					];
-				}
 
 			// render information
 			var html = array.map(entries, function(ientry) {

commit 3855f72b4d47253f4efec84e893f1adcc8d547b5

fix(udm): fix displaying of actual DNs in license count
Bug #59633

Fixes: 650bd13a3f04a690aea0616df11ac49d7a8bb643

doc/errata/staging/univention-directory-manager-modules.yaml
@@ -1,0 +1,11 @@
+product: ucs
+release: "5.2"
+version: [6]
+scope: ucs_5.2-0-errata5.2-6
+src: univention-directory-manager-modules
+fix: 17.6.9
+desc: |
+ This update addresses the following issues:
+ * The `--list-dns` option of `univention-license-check` was broken by erratum
+   394 and has been repaired.
+bug: [59633]

management/univention-directory-manager-modules/modules/univention/admincli/license_check.py
@@ -98,10 +98,14 @@ def check_license(lo: univention.admin.uldap.access, dn: str | None, list_dns: b
                     n = 0
             ln = _license.names[v][i]
             if m:
+                if list_dns:
+                    out.append("")
                 ignored = v == '2' and i == License.SERVERS  # Ignore the server count
                 out.append(format(ln, n, m, False, _license.compare, ignored))
-                if list_dns and i == License.USERS:
-                    out.append("  %s Systemaccounts are ignored." % _license.sysAccountsFound)
+                if list_dns:
+                    out.extend("  %s" % dnout for dnout in lo.searchDn(filter=_license.filters[v][i]))
+                    if i == License.USERS:
+                        out.append("  %s Systemaccounts are ignored." % _license.sysAccountsFound)
 
     def check_time() -> None:
         now = datetime.date.today()