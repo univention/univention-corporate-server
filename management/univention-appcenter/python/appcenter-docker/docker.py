univention/univention-corporate-server

commit 17bef93c1100422f8ef53a139417be1fdb31b561

refactor(appcenter): adjust to ruamel API changes
Traceback (most recent call last):
  File "/usr/bin/univention-app", line 80, in <module>
    main()
    ~~~~^^
  File "/usr/bin/univention-app", line 67, in main
    ret = action(args)
  File "/usr/lib/python3/dist-packages/univention/appcenter/actions/__init__.py", line 161, in call_with_namespace
    result = self.main(namespace)
  File "/usr/lib/python3/dist-packages/univention/appcenter/actions/install.py", line 66, in main
    return self.do_it(args)
           ~~~~~~~~~~^^^^^^
  File "/usr/lib/python3/dist-packages/univention/appcenter/actions/install_base.py", line 140, in do_it
    success = self.do_it_once(app, args)
  File "/usr/lib/python3/dist-packages/univention/appcenter/actions/install.py", line 182, in do_it_once
    return super().do_it_once(app, args)
           ~~~~~~~~~~~~~~~~~~^^^^^^^^^^^
  File "/usr/lib/python3/dist-packages/univention/appcenter/actions/install_base.py", line 185, in do_it_once
    self._do_it(app, args)
    ~~~~~~~~~~~^^^^^^^^^^^
  File "/usr/lib/python3/dist-packages/univention/appcenter/actions/docker_install.py", line 46, in _do_it
    ret = super()._do_it(app, args)
  File "/usr/lib/python3/dist-packages/univention/appcenter/actions/install.py", line 89, in _do_it
    self._register_app(app, args)
    ~~~~~~~~~~~~~~~~~~^^^^^^^^^^^
  File "/usr/lib/python3/dist-packages/univention/appcenter/actions/register.py", line 412, in _register_app
    updates.update(self._register_docker_variables(app))
                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^^^^^
  File "/usr/lib/python3/dist-packages/univention/appcenter/actions/register.py", line 465, in _register_docker_variables
    updates[app.ucr_image_key] = app.get_docker_image_name()
                                 ~~~~~~~~~~~~~~~~~~~~~~~~~^^
  File "/usr/lib/python3/dist-packages/univention/appcenter/app.py", line 1134, in get_docker_image_name

doc/errata/staging/univention-appcenter.yaml
@@ -1,0 +1,11 @@
+product: ucs
+release: "5.2"
+version: [6]
+scope: ucs_5.2-0-errata5.2-6
+src: univention-appcenter
+fix: 11.6.0
+desc: |
+ This update addresses the following issue:
+ * The App Center has been adapted to be compatible with future `ruamel.yaml`
+   API deprecations.
+bug: [59521]

management/univention-appcenter/python/appcenter-docker/docker.py
@@ -26,6 +26,7 @@ from tempfile import NamedTemporaryFile
 
 import docker
 from ruamel import yaml
+from ruamel.yaml import YAML
 
 from univention.appcenter.app_cache import Apps
 from univention.appcenter.exceptions import DockerCouldNotStartContainer, DockerImagePullFailed
@@ -43,6 +44,13 @@ DOCKER_READ_USER_CRED = {
     'username': 'ucs',
     'password': 'readonly',
 }
+
+
+def yaml_rt():
+    yaml_rt = YAML(typ='rt')
+    yaml_rt.preserve_quotes = True
+    yaml_rt.allow_unicode = True
+    return yaml_rt
 
 
 class DockerImageVerificationFailedChecksum(Exception):
@@ -518,7 +526,9 @@ class MultiDocker(Docker):
         with open(yml_file, 'w') as fd:
             os.chmod(yml_file, 0o600)
             fd.write(content)
-        content = yaml.load(open(yml_file), yaml.RoundTripLoader, preserve_quotes=True)
+        _yaml = yaml_rt()
+        with open(yml_file) as fd:
+            content = _yaml.load(ucr_run_filter(fd.read()))
         container_def = content['services'][self.app.docker_main_service]
         volumes = container_def.get('volumes', [])
         for volume in self._app_volumes():
@@ -600,8 +610,9 @@ class MultiDocker(Docker):
                     else:
                         service['env_file'] = []
                         service['env_file'].append(self.env_file_created)
+        _yaml = yaml_rt()
         with open(yml_file, 'w') as fd:
-            yaml.dump(content, fd, Dumper=yaml.RoundTripDumper, encoding='utf-8', allow_unicode=True)
+            _yaml.dump(content, fd)
         shutil.copy2(yml_file, yml_run_file)  # "backup"
 
     def _setup_env(self, env=None):
@@ -626,7 +637,8 @@ class MultiDocker(Docker):
     def get_container_id(self, service_name=None):
         name = None
         yml_file = self.app.get_compose_file('docker-compose.yml')
-        content = yaml.load(open(yml_file), yaml.RoundTripLoader, preserve_quotes=True)
+        _yaml = yaml_rt()
+        content = _yaml.load(open(yml_file))
         # name from yaml
         service_name = service_name or self.app.docker_main_service
         if service_name in content['services'] and content['services'][service_name].get('container_name'):
@@ -709,7 +721,8 @@ class MultiDocker(Docker):
     def rmi(self):
         images = []
         yml_file = self.app.get_compose_file('docker-compose.yml.bak')
-        content = yaml.load(open(yml_file), yaml.RoundTripLoader, preserve_quotes=True)
+        _yaml = yaml_rt()
+        content = _yaml.load(open(yml_file))
         services = content.get('services', {})
         for service in services.values():
             image = service.get('image')

management/univention-appcenter/python/appcenter/app.py
@@ -1126,12 +1126,15 @@ class App(metaclass=AppMetaClass):  # noqa: PLW1641
     def get_docker_image_name(self):
         if self.uses_docker_compose():
             try:
-                from ruamel import yaml
+                from ruamel.yaml import YAML
             except ImportError:
                 # appcenter-docker is not installed
                 return None
             yml_file = self.get_cache_file('compose')
-            content = yaml.load(ucr_run_filter(open(yml_file).read()), yaml.RoundTripLoader, preserve_quotes=True)
+            _yaml = YAML(typ="rt")
+            _yaml.preserve_quotes = True
+            with open(yml_file) as fd:
+                content = _yaml.load(ucr_run_filter(fd.read()))
             image = content['services'][self.docker_main_service]['image']
             return image
         else:

commit bc629ffe9103c7b38d54711d5b572c989d2d2de5

fix(appcenter): don't run UCR template filtering twice
Bug #59521

Fixes: 17bef93c1100422f8ef53a139417be1fdb31b561

doc/errata/staging/univention-appcenter.yaml
@@ -3,7 +3,7 @@ release: "5.2"
 version: [6]
 scope: ucs_5.2-0-errata5.2-6
 src: univention-appcenter
-fix: 11.6.0
+fix: 11.6.1
 desc: |
  This update addresses the following issue:
  * The App Center has been adapted to be compatible with future `ruamel.yaml`

management/univention-appcenter/python/appcenter-docker/docker.py
@@ -528,7 +528,7 @@ class MultiDocker(Docker):
             fd.write(content)
         _yaml = yaml_rt()
         with open(yml_file) as fd:
-            content = _yaml.load(ucr_run_filter(fd.read()))
+            content = _yaml.load(fd.read())
         container_def = content['services'][self.app.docker_main_service]
         volumes = container_def.get('volumes', [])
         for volume in self._app_volumes():

pyproject.toml
@@ -4,7 +4,7 @@ combine_as_imports = true
 filter_files = true
 force_grid_wrap = false
 known_first_party = "ucsschool,univention"
-known_third_party = "apt,apt_pkg,atexit,bzrlib,cherrypy,concurrent,Crypto,cryptography,daemon,debian,defusedxml,genshi,OpenSSL,PAM,pexpect,PIL,pyasn1,PyQt4,saml2,StringIO,sqlalchemy,tornado,yattag,colorlog,commands,cups,dateutil,dns,DNS,ipaddr,lazy_object_proxy,ldap,ldb,ldif,magic,mock,netifaces,notifier,prometheus_client,psutil,pycurl,pytest,pytz,requests,samba,six,smbpasswd,tdb,urllib3,urlparse,utils"
+known_third_party = "apt,apt_pkg,atexit,bzrlib,cherrypy,concurrent,Crypto,cryptography,daemon,debian,defusedxml,docker,genshi,OpenSSL,PAM,pexpect,PIL,pyasn1,PyQt4,saml2,StringIO,sqlalchemy,tornado,yattag,colorlog,commands,cups,dateutil,dns,DNS,ipaddr,lazy_object_proxy,ldap,ldb,ldif,magic,mock,netifaces,notifier,prometheus_client,psutil,pycurl,pytest,pytz,requests,samba,six,smbpasswd,tdb,urllib3,urlparse,utils"
 known_local_folder = "listener,adconnector,appcentertest,appcenteruninstalltest,dockertest,s4connector,dnstests,grep_traceback,essential,samltest,ldap_glue,ldap_glue_s4,quota_test,quota_cache,umc"
 line_length = 120
 multi_line_output = 5