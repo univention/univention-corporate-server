univention/univention-corporate-server

commit 1a735b90e0a9d99b84e13760bae96ef2ed80f786

fix(umc-ipchange): react to keycloak/server/sso/fqdn
Issue #2020

management/univention-management-console-module-ipchange/umc/python/ipchange/__init__.py
@@ -117,8 +117,8 @@ class Instance(Base):
         # Change ucs-sso entry
         # FIXME: this should be done for UCS-in-AD domains as well!
         ucr.load()
-        sso_fqdn = ucr.get('ucs/server/sso/fqdn')
-        if ucr.is_true('ucs/server/sso/autoregistraton', True):
+        sso_fqdn = ucr.get('keycloak/server/sso/fqdn')
+        if ucr.is_true('keycloak/server/sso/autoregistraton', True) and sso_fqdn:
             fmodule = univention.admin.modules.get('dns/forward_zone')
             forwardobjects = univention.admin.modules.lookup(fmodule, None, lo, scope='sub', superordinate=None, filter=None)
             for forwardobject in forwardobjects:

commit 8f772775aea1ee6f8989c3da37a25f4534b59771

fix(ipchange): honor SSO autoregistration UCR variable
Since UCS 5.2 / Keycloak the UCR variable was renamed into
`keycloak/server/sso/autoregistration`.

Bug #59759

Fixes: 1a735b90e0a9d99b84e13760bae96ef2ed80f786

base/univention-system-setup/umc/python/setup/netconf/modules/LdapSSO.py
@@ -24,7 +24,7 @@ class PhaseLdapSSO(AddressMap, LdapChange):
 
     def post(self) -> None:
         try:
-            if self.changeset.ucr.is_true('ucs/server/sso/autoregistraton', True):
+            if self.changeset.ucr.is_true('keycloak/server/sso/autoregistration', True):
                 self.open_ldap()
                 self._update_sso()
         except (LDAPError, UniventionBaseException) as ex:

doc/errata/staging/univention-management-console-module-ipchange.yaml
@@ -11,5 +11,7 @@ desc: |
    "DomainDnsZones" and "ForestDnsZones" even if the corresponding services
    weren't installed on that host. The automatic IP registration now checks
    again that the records are only changed if the current IP address of the
-   server is part of the record.
+   server is part of the record. Additionally, automatic updates of the SSO
+   DNS record now correctly honor the UCR variable
+   `keycloak/server/sso/autoregistration`.
 bug: [59759]

doc/errata/staging/univention-system-setup.yaml
@@ -1,0 +1,11 @@
+product: ucs
+release: "5.2"
+version: [6]
+scope: ucs_5.2-0-errata5.2-6
+src: univention-system-setup
+fix: 15.6.2
+desc: |
+ This update addresses the following issue:
+ * Automatic DNS updates of the SSO record during IP address changes now
+   correctly honor the UCR variable `keycloak/server/sso/autoregistration`.
+bug: [59759]

management/univention-management-console-module-ipchange/umc/python/ipchange/__init__.py
@@ -83,7 +83,7 @@ class Instance(Base):
         fqdns = {fqdn}
 
         sso_fqdn = urlparse(ucr.get('ucs/server/sso/uri', '').lower()).hostname
-        if ucr.is_true('keycloak/server/sso/autoregistraton', True) and sso_fqdn:
+        if ucr.is_true('keycloak/server/sso/autoregistration', True) and sso_fqdn:
             fqdns.add(sso_fqdn)
 
         if ucr.get('dns/backend') == 'samba4':