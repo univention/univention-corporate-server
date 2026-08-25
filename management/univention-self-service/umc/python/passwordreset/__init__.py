462404d0c814a670bcb625729d2ce8ef5fbf9ed2
Fabian Schneider <fabian.schneider@univention.de>
2026-08-25 13:54:09 +0200

    match log message formatting to most of the file

--- management/univention-self-service/umc/python/passwordreset/__init__.py
+++ management/univention-self-service/umc/python/passwordreset/__init__.py
@@ -820,7 +820,7 @@ class Instance(Base):
             user_info = self._extract_user_properties(user)
             self.send_message(username, method, user[plugin.udm_property], user_info)
         else:
-            MODULE.info("send_token() property '%s' not set for username '%s'.", plugin.udm_property, username)
+            MODULE.info("send_token(): property '%s' not set for username '%s'.", plugin.udm_property, username)
 
         raise TokenSendMessage()
 
