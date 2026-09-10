0da59df5746581f837abf5926fbd3fee8e5533cb
Rehema Owino <rehema.owino@univention.de>
2026-09-10 16:46:41 +0300

    chore: Update links from UCS 5.2-6 to UCS 5.2-7
    Bug #59722
    Issues: univention/dev/ucs#3732

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -7,7 +7,7 @@
 #
 
 # defaults for release
-release='5.2-6'  #
+release='5.2-7'  #
 export CURRENT_AMI="${CURRENT_AMI:=ami-08e58af159c930ff2}"  # AMI: Univention Corporate Server (UCS) 5.2 (official image) rev. 7
 old_release='5.0-10'  #
 export OLD_AMI="${OLD_AMI:=ami-00c198b3f9bb2c1ba}"  # AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 11
