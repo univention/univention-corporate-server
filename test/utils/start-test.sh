f980249c4985c277ed34abf610494d7e1b0c19fa
Rehema Owino <rehema.owino@univention.de>
2026-09-11 14:15:50 +0300

    chore(start-script): Revert the OLD_AMI and update the CURRENT_AMI

--- test/utils/start-test.sh
+++ test/utils/start-test.sh
@@ -8,9 +8,9 @@
 
 # defaults for release
 release='5.2-7'  #
-export CURRENT_AMI="${CURRENT_AMI:=ami-08e58af159c930ff2}"  # AMI: Univention Corporate Server (UCS) 5.2 (official image) rev. 7
+export CURRENT_AMI="${CURRENT_AMI:=ami-063173dfea68712bc}"  # AMI: Univention Corporate Server (UCS) 5.2 (official image) rev. 7
 old_release='5.0-10'  #
-export OLD_AMI="${OLD_AMI:=ami-063173dfea68712bc}"  # AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 11
+export OLD_AMI="${OLD_AMI:=ami-00c198b3f9bb2c1ba}"  # AMI: Univention Corporate Server (UCS) 5.0 (official image) rev. 11
 export KVM_UCSVERSION="${KVM_UCSVERSION:=5.2-7+2026-09-11}"
 export OPENSTACK_IMAGE_VERSION="${OPENSTACK_IMAGE_VERSION:=5.2-6}"  # version for the openstack image
 export OPENSTACK_IMAGE_NAME="${OPENSTACK_IMAGE_NAME:=UCS $OPENSTACK_IMAGE_VERSION}"  # name of the default openstack image
