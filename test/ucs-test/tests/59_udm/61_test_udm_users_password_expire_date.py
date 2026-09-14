2a45570b316fc812731ad0c75724939d6e9f89db
Florian Best <best@univention.de>
2026-09-14 15:05:50 +0200

    test(udm): migrate to pytest
    Bug #53538

--- test/ucs-test/tests/59_udm/61_test_udm_users_password_expire_date.py
+++ test/ucs-test/tests/59_udm/61_test_udm_users_password_expire_date.py
@@ -1,5 +1,6 @@
-#!/usr/share/ucs-test/runner python3
+#!/usr/share/ucs-test/runner pytest-3 -s -l -v
 ## desc: Check UDM users/user --set userexpiry
+## tags: [udm]
 ## roles: [domaincontroller_master]
 ## exposure: dangerous
 ## packages: [python3-univention-directory-manager]
@@ -10,15 +11,15 @@ import copy
 import re
 import time
 
+import pytest
+
 import univention.admin.password
 import univention.testing.strings as uts
 import univention.testing.udm as udm_test
 from univention.testing import utils
-from univention.uldap import getMachineConnection
 
 
-def ldap_obj_search(dn=None, ldapfilter='(objectClass=*)'):
-    lo = getMachineConnection()
+def ldap_obj_search(lo, dn=None, ldapfilter='(objectClass=*)'):
     if dn:
         result = lo.search(base=dn, scope='base', filter=ldapfilter)
     else:
@@ -62,22 +63,22 @@ def check_dict_values(obj, expectation):
             assert obj[attribute] == value, f"Attribute '{attribute}' found with value '{obj[attribute]}'. Expected value: '{value}'"
 
 
-def modify_and_diff(udm, **kwargs):
-    before = ldap_obj_search(kwargs['dn'])
+def modify_and_diff(udm, lo, **kwargs):
+    before = ldap_obj_search(lo, kwargs['dn'])
     udm.modify_object('users/user', **kwargs)
     utils.wait_for_connector_replication()
-    after = ldap_obj_search(kwargs['dn'])
+    after = ldap_obj_search(lo, kwargs['dn'])
     return dictdiff(before, after)
 
 
-def modify_and_check_expectation(udm, expected_diff=None, **kwargs):
+def modify_and_check_expectation(udm, lo, expected_diff=None, **kwargs):
     if expected_diff:
-        diff = modify_and_diff(udm, **kwargs)
+        diff = modify_and_diff(udm, lo, **kwargs)
         ddiff = dictdiff(diff, expected_diff)
         assert not ddiff, "Unexpected diff: %s" % ddiff
     else:
         try:
-            diff = modify_and_diff(udm, **kwargs)
+            diff = modify_and_diff(udm, lo, **kwargs)
         except udm_test.UCSTestUDM_NoModification:
             pass
         else:
@@ -115,139 +116,136 @@ KRB5KDCFLAGS_NORMAL = str(int('01111110', 2)).encode('ASCII')
 KRB5KDCFLAGS_REQUIRE_AS_REQ = str(int(KRB5KDCFLAGS_NORMAL) | (1 << 7)).encode('ASCII')
 
 
-def run():
-    with udm_test.UCSTestUDM() as udm:
-        passwd = uts.random_string()
-        uts.random_name()
+@pytest.mark.roles('domaincontroller_master')
+@pytest.mark.exposure('dangerous')
+def test_udm_users_password_expire_date(udm, lo):
+    passwd = uts.random_string()
+    uts.random_name()
 
-        # Prepare user and check
-        userdn, _username = udm.create_user(password=passwd, disabled="0")
+    # Prepare user and check
+    userdn, _username = udm.create_user(password=passwd, disabled="0")
 
-        # since we don't expect a later diff, we have to wait for the domain SID
-        # TODO: this should be done in a more generic way:1
-        time.sleep(16)
+    # since we don't expect a later diff, we have to wait for the domain SID
+    # TODO: this should be done in a more generic way:1
+    time.sleep(16)
 
-        expected_after = ldap_obj_search(userdn)
-        check_dict_values(expected_after, {
-            'sambaAcctFlags': [SAMBAACCTFLAGS_NORMAL],
-            'sambaKickoffTime': None,
-            'krb5KDCFlags': [KRB5KDCFLAGS_NORMAL],
-            'krb5ValidEnd': None,
-            'shadowExpire': None,
-        })
+    expected_after = ldap_obj_search(lo, userdn)
+    check_dict_values(expected_after, {
+        'sambaAcctFlags': [SAMBAACCTFLAGS_NORMAL],
+        'sambaKickoffTime': None,
+        'krb5KDCFlags': [KRB5KDCFLAGS_NORMAL],
+        'krb5ValidEnd': None,
+        'shadowExpire': None,
+    })
 
-        # Modify and check
-        new_uexp = "02.02.15"
-        expected_before = copy.deepcopy(expected_after)
-        expected_after.update({
-            'sambaAcctFlags': [SAMBAACCTFLAGS_NORMAL],
-            'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
-            'krb5KDCFlags': [KRB5KDCFLAGS_NORMAL],
-            'krb5ValidEnd': [b'20150202000000Z'],
-            'shadowExpire': [udm_formula_for_shadowExpire(new_uexp)],
-        })
-        expected_diff = dictdiff(expected_before, expected_after)
-        modify_and_check_expectation(udm, expected_diff, dn=userdn, userexpiry=new_uexp)
+    # Modify and check
+    new_uexp = "02.02.15"
+    expected_before = copy.deepcopy(expected_after)
+    expected_after.update({
+        'sambaAcctFlags': [SAMBAACCTFLAGS_NORMAL],
+        'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
+        'krb5KDCFlags': [KRB5KDCFLAGS_NORMAL],
+        'krb5ValidEnd': [b'20150202000000Z'],
+        'shadowExpire': [udm_formula_for_shadowExpire(new_uexp)],
+    })
+    expected_diff = dictdiff(expected_before, expected_after)
+    modify_and_check_expectation(udm, lo, expected_diff, dn=userdn, userexpiry=new_uexp)
 
-        # Modify and check
-        expected_before = copy.deepcopy(expected_after)
-        expected_after.update({
-            'sambaAcctFlags': [SAMBAACCTFLAGS_DISABLED],
-            'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
-            'krb5KDCFlags': [KRB5KDCFLAGS_REQUIRE_AS_REQ],
-            'krb5ValidEnd': [b'20150202000000Z'],
-            'shadowExpire': [b'1'],
-            'userPassword': [univention.admin.password.lock_password(expected_before['userPassword'][0].decode('ASCII')).encode('ASCII')],
-        })
-        expected_diff = dictdiff(expected_before, expected_after)
-        modify_and_check_expectation(udm, expected_diff, dn=userdn, userexpiry="", disabled="1")
+    # Modify and check
+    expected_before = copy.deepcopy(expected_after)
+    expected_after.update({
+        'sambaAcctFlags': [SAMBAACCTFLAGS_DISABLED],
+        'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
+        'krb5KDCFlags': [KRB5KDCFLAGS_REQUIRE_AS_REQ],
+        'krb5ValidEnd': [b'20150202000000Z'],
+        'shadowExpire': [b'1'],
+        'userPassword': [univention.admin.password.lock_password(expected_before['userPassword'][0].decode('ASCII')).encode('ASCII')],
+    })
+    expected_diff = dictdiff(expected_before, expected_after)
+    modify_and_check_expectation(udm, lo, expected_diff, dn=userdn, userexpiry="", disabled="1")
 
-        # Modify and check
-        expected_before = copy.deepcopy(expected_after)
-        expected_after.update({
-            'sambaAcctFlags': [SAMBAACCTFLAGS_NORMAL],
-            'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
-            'krb5KDCFlags': [KRB5KDCFLAGS_NORMAL],
-            'krb5ValidEnd': [b'20150202000000Z'],
-            'shadowExpire': [udm_formula_for_shadowExpire(new_uexp)],
-            'userPassword': [univention.admin.password.unlock_password(expected_before['userPassword'][0].decode('ASCII')).encode('ASCII')],
-        })
-        expected_diff = dictdiff(expected_before, expected_after)
-        modify_and_check_expectation(udm, expected_diff, dn=userdn, userexpiry=new_uexp, disabled="0")
+    # Modify and check
+    expected_before = copy.deepcopy(expected_after)
+    expected_after.update({
+        'sambaAcctFlags': [SAMBAACCTFLAGS_NORMAL],
+        'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
+        'krb5KDCFlags': [KRB5KDCFLAGS_NORMAL],
+        'krb5ValidEnd': [b'20150202000000Z'],
+        'shadowExpire': [udm_formula_for_shadowExpire(new_uexp)],
+        'userPassword': [univention.admin.password.unlock_password(expected_before['userPassword'][0].decode('ASCII')).encode('ASCII')],
+    })
+    expected_diff = dictdiff(expected_before, expected_after)
+    modify_and_check_expectation(udm, lo, expected_diff, dn=userdn, userexpiry=new_uexp, disabled="0")
 
-        # Modify and check
-        new_uexp = "2014-01-01"
-        expected_before = copy.deepcopy(expected_after)
-        expected_after.update({
-            'sambaAcctFlags': [SAMBAACCTFLAGS_NORMAL],
-            'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
-            'krb5ValidEnd': [b'20140101000000Z'],
-            'krb5KDCFlags': [KRB5KDCFLAGS_NORMAL],
-            'shadowExpire': [udm_formula_for_shadowExpire(new_uexp)],
-        })
-        expected_diff = dictdiff(expected_before, expected_after)
-        modify_and_check_expectation(udm, expected_diff, dn=userdn, userexpiry=new_uexp)
+    # Modify and check
+    new_uexp = "2014-01-01"
+    expected_before = copy.deepcopy(expected_after)
+    expected_after.update({
+        'sambaAcctFlags': [SAMBAACCTFLAGS_NORMAL],
+        'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
+        'krb5ValidEnd': [b'20140101000000Z'],
+        'krb5KDCFlags': [KRB5KDCFLAGS_NORMAL],
+        'shadowExpire': [udm_formula_for_shadowExpire(new_uexp)],
+    })
+    expected_diff = dictdiff(expected_before, expected_after)
+    modify_and_check_expectation(udm, lo, expected_diff, dn=userdn, userexpiry=new_uexp)
 
-        # Modify and check
-        expected_before = copy.deepcopy(expected_after)
-        expected_after.update({
-            'sambaAcctFlags': [SAMBAACCTFLAGS_DISABLED],
-            'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
-            'krb5ValidEnd': [b'20140101000000Z'],
-            'krb5KDCFlags': [KRB5KDCFLAGS_REQUIRE_AS_REQ],
-            'shadowExpire': [b'1'],
-            'userPassword': [univention.admin.password.lock_password(expected_before['userPassword'][0].decode('ASCII')).encode('ASCII')],
-        })
-        expected_diff = dictdiff(expected_before, expected_after)
-        modify_and_check_expectation(udm, expected_diff, dn=userdn, disabled="1")
+    # Modify and check
+    expected_before = copy.deepcopy(expected_after)
+    expected_after.update({
+        'sambaAcctFlags': [SAMBAACCTFLAGS_DISABLED],
+        'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
+        'krb5ValidEnd': [b'20140101000000Z'],
+        'krb5KDCFlags': [KRB5KDCFLAGS_REQUIRE_AS_REQ],
+        'shadowExpire': [b'1'],
+        'userPassword': [univention.admin.password.lock_password(expected_before['userPassword'][0].decode('ASCII')).encode('ASCII')],
+    })
+    expected_diff = dictdiff(expected_before, expected_after)
+    modify_and_check_expectation(udm, lo, expected_diff, dn=userdn, disabled="1")
 
-        # Modify and check
-        new_uexp = "2015-02-02"
-        expected_before = copy.deepcopy(expected_after)
-        expected_after.update({
-            'sambaAcctFlags': [SAMBAACCTFLAGS_DISABLED],
-            'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
-            'krb5ValidEnd': [b'20150202000000Z'],
-            'krb5KDCFlags': [KRB5KDCFLAGS_REQUIRE_AS_REQ],
-            'shadowExpire': [udm_formula_for_shadowExpire(new_uexp)],
-        })
-        expected_diff = dictdiff(expected_before, expected_after)
-        modify_and_check_expectation(udm, expected_diff, dn=userdn, userexpiry=new_uexp)
+    # Modify and check
+    new_uexp = "2015-02-02"
+    expected_before = copy.deepcopy(expected_after)
+    expected_after.update({
+        'sambaAcctFlags': [SAMBAACCTFLAGS_DISABLED],
+        'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
+        'krb5ValidEnd': [b'20150202000000Z'],
+        'krb5KDCFlags': [KRB5KDCFLAGS_REQUIRE_AS_REQ],
+        'shadowExpire': [udm_formula_for_shadowExpire(new_uexp)],
+    })
+    expected_diff = dictdiff(expected_before, expected_after)
+    modify_and_check_expectation(udm, lo, expected_diff, dn=userdn, userexpiry=new_uexp)
 
-        # Modify and check
-        new_uexp = "01.01.14"
-        expected_before = copy.deepcopy(expected_after)
-        expected_after.update({
-            'sambaAcctFlags': [SAMBAACCTFLAGS_DISABLED],
-            'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
-            'krb5KDCFlags': [KRB5KDCFLAGS_REQUIRE_AS_REQ],
-            'krb5ValidEnd': [b'20140101000000Z'],
-            'shadowExpire': [udm_formula_for_shadowExpire(new_uexp)],
-        })
-        expected_diff = dictdiff(expected_before, expected_after)
-        modify_and_check_expectation(udm, expected_diff, dn=userdn, userexpiry=new_uexp, disabled="1")
+    # Modify and check
+    new_uexp = "01.01.14"
+    expected_before = copy.deepcopy(expected_after)
+    expected_after.update({
+        'sambaAcctFlags': [SAMBAACCTFLAGS_DISABLED],
+        'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
+        'krb5KDCFlags': [KRB5KDCFLAGS_REQUIRE_AS_REQ],
+        'krb5ValidEnd': [b'20140101000000Z'],
+        'shadowExpire': [udm_formula_for_shadowExpire(new_uexp)],
+    })
+    expected_diff = dictdiff(expected_before, expected_after)
+    modify_and_check_expectation(udm, lo, expected_diff, dn=userdn, userexpiry=new_uexp, disabled="1")
 
-        # Modify and check
-        new_uexp = "02.02.15"
-        expected_before = copy.deepcopy(expected_after)
-        expected_after.update({
-            'sambaAcctFlags': [SAMBAACCTFLAGS_NORMAL],
-            'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
-            'krb5KDCFlags': [KRB5KDCFLAGS_NORMAL],
-            'krb5ValidEnd': [b'20150202000000Z'],
-            'shadowExpire': [udm_formula_for_shadowExpire(new_uexp)],
-            'userPassword': [univention.admin.password.unlock_password(expected_before['userPassword'][0].decode('ASCII')).encode('ASCII')],
-        })
-        expected_diff = dictdiff(expected_before, expected_after)
-        modify_and_check_expectation(udm, expected_diff, dn=userdn, userexpiry=new_uexp, disabled="0")
+    # Modify and check
+    new_uexp = "02.02.15"
+    expected_before = copy.deepcopy(expected_after)
+    expected_after.update({
+        'sambaAcctFlags': [SAMBAACCTFLAGS_NORMAL],
+        'sambaKickoffTime': [udm_formula_for_sambaKickoffTime(new_uexp)],
+        'krb5KDCFlags': [KRB5KDCFLAGS_NORMAL],
+        'krb5ValidEnd': [b'20150202000000Z'],
+        'shadowExpire': [udm_formula_for_shadowExpire(new_uexp)],
+        'userPassword': [univention.admin.password.unlock_password(expected_before['userPassword'][0].decode('ASCII')).encode('ASCII')],
+    })
+    expected_diff = dictdiff(expected_before, expected_after)
+    modify_and_check_expectation(udm, lo, expected_diff, dn=userdn, userexpiry=new_uexp, disabled="0")
 
-        # Modify and check
-        expected_diff = {}
-        modify_and_check_expectation(udm, expected_diff, dn=userdn, disabled="0")
+    # Modify and check
+    expected_diff = {}
+    modify_and_check_expectation(udm, lo, expected_diff, dn=userdn, disabled="0")
 
-        # Modify and check
-        modify_and_check_expectation(udm, expected_diff, dn=userdn, userexpiry="")
-
-
-if __name__ == '__main__':
-    run()
+    # Modify and check
+    modify_and_check_expectation(udm, lo, expected_diff, dn=userdn, userexpiry="")
